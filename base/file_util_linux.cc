bb86ebb8888a2382077b46eca7be6e14d774b670
evanm@google.com <evanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-15 01:04:11 +0000

    To pass string_util_unittest, we need the ICU data file.  To get the ICU data file, we need some of PathService, which brings along with it a bunch of other needed functions.  So here are a bunch of stubs, along with another file's worth of passing tests.  (Based on a patch from Dean.)
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@921 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util_linux.cc
+++ base/file_util_linux.cc
@@ -32,9 +32,17 @@
 #include <string>
 
 #include "base/logging.h"
+#include "base/notimplemented.h"
 #include "base/string_util.h"
 
 namespace file_util {
+
+const wchar_t kPathSeparator = L'/';
+
+bool AbsolutePath(std::wstring* path) {
+  NOTIMPLEMENTED();
+  return false;
+}
   
 bool GetTempDir(std::wstring* path) {
   const char* tmp = getenv("TMPDIR");
@@ -47,7 +55,27 @@ bool GetTempDir(std::wstring* path) {
 
 bool CopyFile(const std::wstring& from_path, const std::wstring& to_path) {
   // TODO(erikkay): implement
-  DCHECK(false);
+  NOTIMPLEMENTED();
+  return false;
+}
+
+bool PathExists(const std::wstring& path) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+bool GetCurrentDirectory(std::wstring* path) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+bool CreateDirectory(const std::wstring& full_path) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+bool SetCurrentDirectory(const std::wstring& current_directory) {
+  NOTIMPLEMENTED();
   return false;
 }
 
