d9298b5df013318810c47d771b00c7dd9f64b627
thomasvl@chromium.org <thomasvl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-13 17:05:21 +0000

    OFFICIAL_BUILD is only set in windows builds, so add some complexity to get the logging turned off the mac/linux builds as intended. The referenced bug will stay open since this probably shouldn't be the long term fix.
    BUG=16512
    TEST=logging shows up for debug builds, but all platforms don't have logging in official builds.
    Review URL: http://codereview.chromium.org/149534
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20497 0039d316-1c4b-4281-b951-d872f2087c98

--- base/logging.h
+++ base/logging.h
@@ -280,7 +280,11 @@ std::string* MakeCheckOpString(const int& v1,
 //     foo.CheckThatFoo();
 //   #endif
 
-#ifdef OFFICIAL_BUILD
+// http://crbug.com/16512 is open for a real fix for this.  For now, Windows
+// uses OFFICIAL_BUILD and !Windows uses the branding flag when NDEBUG is
+// defined.
+#if ( defined(OS_WIN) && defined(OFFICIAL_BUILD)) || \
+    (!defined(OS_WIN) && defined(NDEBUG) && defined(GOOGLE_CHROME_BUILD))
 // We want to have optimized code for an official build so we remove DLOGS and
 // DCHECK from the executable.
 
