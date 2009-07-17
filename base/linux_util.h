b0df6ec5f9244112caffe4a0af9d960331a0e39e
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-17 05:55:51 +0000

    Include output of "lsb_release -d" in crash reports.
    /etc/lsb-release does not exist on all distros, and sometimes it does
    not provide any useful info. Whereas all LSB complaint distros return
    useful data with the lsb_release command.
    Review URL: http://codereview.chromium.org/155653
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20936 0039d316-1c4b-4281-b951-d872f2087c98

--- base/linux_util.h
+++ base/linux_util.h
@@ -7,12 +7,18 @@
 
 #include <stdint.h>
 
+#include <string>
+
 namespace base {
 
 // Makes a copy of |pixels| with the ordering changed from BGRA to RGBA.
 // The caller is responsible for free()ing the data. If |stride| is 0,
 // it's assumed to be 4 * |width|.
 uint8_t* BGRAToRGBA(const uint8_t* pixels, int width, int height, int stride);
+
+// Get the Linux Distro if we can, or return "Unknown", similar to
+// GetWinVersion() in base/win_util.h.
+std::string GetLinuxDistro();
 
 }  // namespace base
 
