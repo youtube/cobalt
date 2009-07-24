ba235f4255992f55f148f69ea48a9cfb4a7c91b4
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-24 03:48:16 +0000

    Implement OCSP handler for NSS.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/126046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21508 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/ocsp/nss_ocsp.h
@@ -0,0 +1,22 @@
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_OCSP_NSS_OCSP_H_
+#define NET_OCSP_NSS_OCSP_H_
+
+class URLRequestContext;
+
+namespace net {
+
+// Initializes OCSP handlers for NSS.  This must be called before any
+// certificate verification functions.  This function is thread-safe, and OCSP
+// handlers will only ever be initialized once.
+void EnsureOCSPInit();
+
+// Set URLRequestContext for OCSP handlers.
+void SetURLRequestContextForOCSP(URLRequestContext* request_context);
+
+}  // namespace net
+
+#endif  // NET_OCSP_NSS_OCSP_H_
