337b80f808f967b3eda0dba99a1c00d1f2b8c8b5
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-29 20:29:23 +0000

    Fix crash in client_socket_pool_base.cc.
    The CHECK that was being hit showed a real problem: group's IsEmpty
    should take into account the pending requests queue too.
    
    I also made IsEmpty check for connecting requests queue emptiness,
    so we can be sure that IsEmpty really means empty.
    
    TEST=Added a regression test ClientSocketPoolBaseTest.GroupWithPendingRequestsIsNotEmpty to net_unittests.
    http://crbug.com/17985
    
    Review URL: http://codereview.chromium.org/159597
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21988 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/client_socket_pool_base.h
+++ net/socket/client_socket_pool_base.h
@@ -196,7 +196,8 @@ class ClientSocketPoolBase
     Group() : active_socket_count(0) {}
 
     bool IsEmpty() const {
-      return active_socket_count == 0 && idle_sockets.empty() && jobs.empty();
+      return active_socket_count == 0 && idle_sockets.empty() && jobs.empty() &&
+          pending_requests.empty() && connecting_requests.empty();
     }
 
     bool HasAvailableSocketSlot(int max_sockets_per_group) const {
