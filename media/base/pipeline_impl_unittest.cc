cb39368d95371a7933f6fb79e5c5abbda8e3c912
kylep@chromium.org <kylep@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-30 18:51:52 +0000

    Pipeline will execute a callback whenever an run-time error has happened. BUG=16738 TEST=pipeline_impl_unittest.cc Review URL: http://codereview.chromium.org/160298
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22075 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/pipeline_impl_unittest.cc
+++ media/base/pipeline_impl_unittest.cc
@@ -32,6 +32,7 @@ class CallbackHelper {
   MOCK_METHOD0(OnStart, void());
   MOCK_METHOD0(OnSeek, void());
   MOCK_METHOD0(OnStop, void());
+  MOCK_METHOD0(OnError, void());
 
  private:
   DISALLOW_COPY_AND_ASSIGN(CallbackHelper);
@@ -48,6 +49,9 @@ class PipelineImplTest : public ::testing::Test {
   PipelineImplTest()
       : pipeline_(new PipelineImpl(&message_loop_)),
         mocks_(new MockFilterFactory()) {
+    pipeline_->SetPipelineErrorCallback(NewCallback(
+        reinterpret_cast<CallbackHelper*>(&callbacks_),
+        &CallbackHelper::OnError));
   }
 
   virtual ~PipelineImplTest() {
@@ -234,6 +238,7 @@ TEST_F(PipelineImplTest, NeverInitializes) {
 }
 
 TEST_F(PipelineImplTest, RequiredFilterMissing) {
+  EXPECT_CALL(callbacks_, OnError());
   mocks_->set_creation_successful(false);
 
   InitializePipeline();
@@ -247,6 +252,7 @@ TEST_F(PipelineImplTest, URLNotFound) {
       .WillOnce(DoAll(SetError(mocks_->data_source(),
                                PIPELINE_ERROR_URL_NOT_FOUND),
                       Invoke(&RunFilterCallback)));
+  EXPECT_CALL(callbacks_, OnError());
   EXPECT_CALL(*mocks_->data_source(), Stop());
 
   InitializePipeline();
@@ -266,6 +272,7 @@ TEST_F(PipelineImplTest, NoStreams) {
   EXPECT_CALL(*mocks_->demuxer(), GetNumberOfStreams())
       .WillRepeatedly(Return(0));
   EXPECT_CALL(*mocks_->demuxer(), Stop());
+  EXPECT_CALL(callbacks_, OnError());
 
   InitializePipeline();
   EXPECT_FALSE(pipeline_->IsInitialized());
