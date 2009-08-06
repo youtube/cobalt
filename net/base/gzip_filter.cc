d70561a358bf73e9fb1b2143cd0fb5cac3a1fdd9
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-06 18:48:45 +0000

    Linux: add GYP flag to build with system zlib.
    This follows the general style of r21663, which we agreed was the way
    to do this.
    
    Note that, currently, one cannot build with system zlib on a 64-bit
    system since it's missing a symlink to libz.so in /usr/lib32.
    
    http://codereview.chromium.org/164027
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22638 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/gzip_filter.cc
+++ net/base/gzip_filter.cc
@@ -4,9 +4,20 @@
 
 #include "net/base/gzip_filter.h"
 
+#if defined(USE_SYSTEM_ZLIB)
+#include <zlib.h>
+#define MOZ_Z_inflate inflate
+#define MOZ_Z_inflateEnd inflateEnd
+#define MOZ_Z_inflateInit2_ inflateInit2_
+#define MOZ_Z_inflateInit_ inflateInit_
+#define MOZ_Z_inflateReset inflateReset
+#else
+#include "third_party/zlib/zlib.h"
+#endif
+
 #include "base/logging.h"
 #include "net/base/gzip_header.h"
-#include "third_party/zlib/zlib.h"
+
 
 GZipFilter::GZipFilter(const FilterContext& filter_context)
     : Filter(filter_context),
