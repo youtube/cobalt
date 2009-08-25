0eb5150208d9b11632ccca746f0e1215b4d1473a
mbelshe@google.com <mbelshe@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-25 07:49:52 +0000

    Add UMA Histogram for FieldTrial for Async Socket SlowStart algorithm to monitor the time between the connect and the first byte on a new connection.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/174390
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24221 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_network_transaction.cc
+++ net/http/http_network_transaction.cc
@@ -1249,12 +1249,25 @@ void HttpNetworkTransaction::LogTransactionConnectedMetrics() const {
         100);
   }
 
-  if (!reused_socket_)
+  if (!reused_socket_) {
     UMA_HISTOGRAM_CLIPPED_TIMES(
         "Net.Transaction_Connected_New",
         total_duration,
         base::TimeDelta::FromMilliseconds(1), base::TimeDelta::FromMinutes(10),
         100);
+
+    // Also record for our field trial.
+    static bool use_async_tcp(FieldTrialList::Find("AsyncSlowStart") &&
+      !FieldTrialList::Find("AsyncSlowStart")->group_name().empty());
+    if (use_async_tcp) {
+      UMA_HISTOGRAM_CLIPPED_TIMES(
+        FieldTrial::MakeName("Net.Transaction_Connected_New",
+                             "AsyncSlowStart").data(),
+        total_duration,
+        base::TimeDelta::FromMilliseconds(1), base::TimeDelta::FromMinutes(10),
+        100);
+    }
+  }
 
   // Currently, non-zero priority requests are frame or sub-frame resource
   // types.  This will change when we also prioritize certain subresources like
