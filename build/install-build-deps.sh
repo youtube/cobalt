49ea37e834447d731f09bd6d1309e527adfa5b3f
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-25 20:59:27 +0000

    Add license information to our shell scripts.
    Downstream packagers are (rightly) careful about the license of code
    that they distribute and so we want to dot our i's.
    
    http://codereview.chromium.org/174372
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24311 0039d316-1c4b-4281-b951-d872f2087c98

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+# Copyright (c) 2009 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 # Script to install everything needed to build chromium (well, ideally, anyway)
 # See http://code.google.com/p/chromium/wiki/LinuxBuildInstructions
 # and http://code.google.com/p/chromium/wiki/LinuxBuild64Bit
