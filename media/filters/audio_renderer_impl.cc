0f9f80759c612d2712ddf089d5baec8ba5e71079
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-27 00:25:18 +0000

    Fix build bustage r24568
    TBR=scherkus
    
    Review URL: http://codereview.chromium.org/173549
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24570 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/audio_renderer_impl.cc
+++ media/filters/audio_renderer_impl.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <math.h>
+
 #include "media/base/filter_host.h"
 #include "media/filters/audio_renderer_impl.h"
 
