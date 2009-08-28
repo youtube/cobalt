b3cef601f89a1d2754605e0e85024d89c0ff1a54
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-28 20:41:10 +0000

    Fix a ton of compiler warnings.
    Most of these are classes with virtual methods lacking virtual destructors
    or NULL used in non-pointer context.
    
    BUG=none
    TEST=app_unittests && base_unittests
    --gtest_filter=-ConditionVariableTest.LargeFastTaskTest
    
    patch by Jacob Mandelson <jlmjlm [at] gmail>
    http://codereview.chromium.org/171028/show
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@24792 0039d316-1c4b-4281-b951-d872f2087c98

--- base/third_party/dmg_fp/dtoa.cc
+++ base/third_party/dmg_fp/dtoa.cc
@@ -1559,7 +1559,7 @@ hexnan
 	CONST char *s;
 	int c1, havedig, udx0, xshift;
 
-	if (!hexdig['0'])
+	if (!hexdig[static_cast<int>('0')])
 		hexdig_init();
 	x[0] = x[1] = 0;
 	havedig = xshift = 0;
@@ -3283,7 +3283,7 @@ strtod
 #ifdef Avoid_Underflow
 			if (bc.scale && y <= 2*P*Exp_msk1) {
 				if (aadj <= 0x7fffffff) {
-					if ((z = aadj) <= 0)
+					if ((z = static_cast<ULong>(aadj)) <= 0)
 						z = 1;
 					aadj = z;
 					aadj1 = bc.dsign ? aadj : -aadj;
@@ -3837,7 +3837,7 @@ dtoa
 			 */
 			dval(&eps) = 0.5/tens[ilim-1] - dval(&eps);
 			for(i = 0;;) {
-				L = dval(&u);
+				L = static_cast<long>(dval(&u));
 				dval(&u) -= L;
 				*s++ = '0' + (int)L;
 				if (dval(&u) < dval(&eps))
