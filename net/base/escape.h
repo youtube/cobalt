033079426bb0e40920c955fd6d97b2b8e212c3d9
brg@chromium.com <brg@chromium.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-01 09:19:29 +0000

    Add EscapeURL to the ASCII escape methods.EscapeURL escapes all forbidden ascii characters in an URL and repalces spaces with '+'.
    Test=Escape.EscapeUrl
    BUG=23029
    Review URL: http://codereview.chromium.org/244056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27713 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/escape.h
+++ net/base/escape.h
@@ -11,9 +11,14 @@
 
 // Escaping --------------------------------------------------------------------
 
-// Escape a file or url path.  This includes:
+// Escape a file.  This includes:
 // non-printable, non-7bit, and (including space)  "#%:<>?[\]^`{|}
 std::string EscapePath(const std::string& path);
+
+// Escape an url.  This includes:
+// non-printable, non-7bit, and (including space)  ?>=<;+'&%$#"![\]^`{|}
+// Space is escaped as + and other special characters as %XX (hex).
+std::string EscapeUrl(const std::string& path);
 
 // Escape all non-ASCII input.
 std::string EscapeNonASCII(const std::string& input);
