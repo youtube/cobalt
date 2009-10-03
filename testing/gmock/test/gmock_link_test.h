0fdf33665ae0035906f2eb68230cd0159dbf839f
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-03 20:47:21 +0000

    Update gmock and gtest.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27953 0039d316-1c4b-4281-b951-d872f2087c98

--- testing/gmock/test/gmock_link_test.h
+++ testing/gmock/test/gmock_link_test.h
@@ -116,7 +116,7 @@
 
 #include <gmock/gmock.h>
 
-#ifndef _WIN32_WCE
+#if !GTEST_OS_WINDOWS_MOBILE
 #include <errno.h>
 #endif
 
@@ -147,6 +147,7 @@ using testing::IgnoreResult;
 using testing::Invoke;
 using testing::InvokeArgument;
 using testing::InvokeWithoutArgs;
+using testing::IsNull;
 using testing::Le;
 using testing::Lt;
 using testing::Matcher;
@@ -176,18 +177,18 @@ using testing::WithArg;
 using testing::WithArgs;
 using testing::WithoutArgs;
 
-#ifndef _WIN32_WCE
+#if !GTEST_OS_WINDOWS_MOBILE
 using testing::SetErrnoAndReturn;
-#endif  // _WIN32_WCE
+#endif
 
 #if GTEST_HAS_EXCEPTIONS
 using testing::Throw;
-#endif  // GTEST_HAS_EXCEPTIONS
+#endif
 
 #if GMOCK_HAS_REGEX
 using testing::ContainsRegex;
 using testing::MatchesRegex;
-#endif  // GMOCK_HAS_REGEX
+#endif
 
 class Interface {
  public:
@@ -297,7 +298,7 @@ TEST(LinkTest, TestSetArrayArgument) {
   mock.VoidFromString(&ch);
 }
 
-#ifndef _WIN32_WCE
+#if !GTEST_OS_WINDOWS_MOBILE
 
 // Tests the linkage of the SetErrnoAndReturn action.
 TEST(LinkTest, TestSetErrnoAndReturn) {
@@ -309,7 +310,7 @@ TEST(LinkTest, TestSetErrnoAndReturn) {
   errno = saved_errno;
 }
 
-#endif  // _WIN32_WCE
+#endif  // !GTEST_OS_WINDOWS_MOBILE
 
 // Tests the linkage of the Invoke(function) and Invoke(object, method) actions.
 TEST(LinkTest, TestInvoke) {
@@ -489,6 +490,13 @@ TEST(LinkTest, TestMatcherNotNull) {
   Mock mock;
 
   ON_CALL(mock, VoidFromString(NotNull())).WillByDefault(Return());
+}
+
+// Tests the linkage of the IsNull matcher.
+TEST(LinkTest, TestMatcherIsNull) {
+  Mock mock;
+
+  ON_CALL(mock, VoidFromString(IsNull())).WillByDefault(Return());
 }
 
 // Tests the linkage of the Ref matcher.
