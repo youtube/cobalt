c169d002591308b18583152643aa70d637b2d8eb
fbarchard@chromium.org <fbarchard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-08 20:05:01 +0000

    MD5 header cleanup for lint and items brought up when reviewing code based on md5.
    Review URL: http://codereview.chromium.org/266021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28433 0039d316-1c4b-4281-b951-d872f2087c98

--- base/md5.h
+++ base/md5.h
@@ -1,27 +1,34 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef BASE_MD5_H__
-#define BASE_MD5_H__
+#ifndef BASE_MD5_H_
+#define BASE_MD5_H_
 
 #include <string>
 
-// These functions perform MD5 operations. The simplest call is MD5Sum to
+// MD5 stands for Message Digest algorithm 5.
+// MD5 is a robust hash function, designed for cyptography, but often used
+// for file checksums.  The code is complex and slow, but has few
+// collisions.
+// See Also:
+//   http://en.wikipedia.org/wiki/MD5
+
+// These functions perform MD5 operations. The simplest call is MD5Sum() to
 // generate the MD5 sum of the given data.
 //
 // You can also compute the MD5 sum of data incrementally by making multiple
-// calls to MD5Update:
-//    MD5Context ctx; // intermediate MD5 data: do not use
-//    MD5Init(&ctx);
-//    MD5Update(&ctx, data1, length1);
-//    MD5Update(&ctx, data2, length2);
-//    ...
+// calls to MD5Update():
+//   MD5Context ctx; // intermediate MD5 data: do not use
+//   MD5Init(&ctx);
+//   MD5Update(&ctx, data1, length1);
+//   MD5Update(&ctx, data2, length2);
+//   ...
 //
-//    MD5Digest digest; // the result of the computation
-//    MD5Final(&digest, &ctx);
+//   MD5Digest digest; // the result of the computation
+//   MD5Final(&digest, &ctx);
 //
-// You can call MD5DigestToBase16 to generate a string of the digest.
+// You can call MD5DigestToBase16() to generate a string of the digest.
 
 // The output of an MD5 operation
 typedef struct MD5Digest_struct {
@@ -37,12 +44,12 @@ typedef char MD5Context[88];
 void MD5Sum(const void* data, size_t length, MD5Digest* digest);
 
 // Initializes the given MD5 context structure for subsequent calls to
-// MD5Update.
+// MD5Update().
 void MD5Init(MD5Context* context);
 
 // For the given buffer of data, updates the given MD5 context with the sum of
 // the data. You can call this any number of times during the computation,
-// exept that MD5Init must have been called first.
+// except that MD5Init() must have been called first.
 void MD5Update(MD5Context* context, const void* buf, size_t len);
 
 // Finalizes the MD5 operation and fills the buffer with the digest.
@@ -54,4 +61,5 @@ std::string MD5DigestToBase16(const MD5Digest& digest);
 // Returns the MD5 (in hexadecimal) of a string.
 std::string MD5String(const std::string& str);
 
-#endif // BASE_MD5_H__
+#endif  // BASE_MD5_H_
+
