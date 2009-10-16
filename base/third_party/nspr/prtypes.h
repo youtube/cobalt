7bcd310b653b24a77a63d584115c1b0db665e81e
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-16 21:37:26 +0000

    Adjust comment following r29182 post-commit feedback.
    Put now-unused macros back to how they are upstream.
    
    BUG=24911
    TEST=none
    Review URL: http://codereview.chromium.org/289003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29328 0039d316-1c4b-4281-b951-d872f2087c98

--- base/third_party/nspr/prtypes.h
+++ base/third_party/nspr/prtypes.h
@@ -91,11 +91,10 @@
 #if 1
 
 /*
-** Chrome-local change: the portions of NSPR used for Chrome are
-** implementation details of Chrome's base module.  NSPR symbols do not need
-** to be exported beyond the module in which they are used.  For all
-** platforms, avoid decorating functions with specific visibility and access
-** keywords.
+** Local change: the portions of NSPR used by the base module are
+** implementation details.  NSPR symbols do not need to be exported beyond
+** the base module.  For all platforms, avoid decorating functions with
+** specific visibility and access keywords.
 */
 
 #define PR_EXPORT(__type) extern __type
@@ -116,11 +115,11 @@
 
 #define PR_EXPORT(__type) extern __declspec(dllexport) __type
 #define PR_EXPORT_DATA(__type) extern __declspec(dllexport) __type
-#define PR_IMPORT(__type) extern __type
+#define PR_IMPORT(__type) __declspec(dllimport) __type
 #define PR_IMPORT_DATA(__type) __declspec(dllimport) __type
 
 #define PR_EXTERN(__type) extern __declspec(dllexport) __type
-#define PR_IMPLEMENT(__type) __type
+#define PR_IMPLEMENT(__type) __declspec(dllexport) __type
 #define PR_EXTERN_DATA(__type) extern __declspec(dllexport) __type
 #define PR_IMPLEMENT_DATA(__type) __declspec(dllexport) __type
 
