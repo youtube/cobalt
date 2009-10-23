5ac3ba4315e36763b50049259bc914615073d41c
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-23 16:58:37 +0000

    Adds support for the <keygen> tag for client certificate enrollment under Linux. Currently, no notifications are given to the user that the certificate was successfully enrolled.
    Patch by Gaurav Shah <gauravsh@chromium.org> of Google.
    Original review URL: http://codereview.chromium.org/261035
    
    BUG=148
    TEST=Can test on the following sites:
    http://foaf.me/simple_KEYGEN_CreateClientCertificate.php
    http://www.myopenid.com
    Review URL: http://codereview.chromium.org/271112
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@29900 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/base/cert_database_mac.cc
@@ -0,0 +1,24 @@
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/base/cert_database.h"
+
+#include "base/logging.h"
+
+namespace net {
+
+CertDatabase::CertDatabase() {
+  NOTIMPLEMENTED();
+}
+
+bool CertDatabase::AddUserCert(const char* data, int len) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+void CertDatabase::Init() {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace net
