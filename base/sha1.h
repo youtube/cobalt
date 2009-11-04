aada36a42341124f07d37703aaffec8616839657
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-04 19:29:58 +0000

    Implement FormStructure and an initial method, EncodeUploadRequest.  This also adds SHA1HashString, a utility method to get the SHA-1 hash of an input string, with appropriate unit tests.
    BUG=18201
    TEST=none
    Review URL: http://codereview.chromium.org/355003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30980 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ base/sha1.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_SHA1_H_
+#define BASE_SHA1_H_
+
+#include <string>
+
+namespace base {
+
+// This function performs SHA-1 operations.
+
+enum {
+  SHA1_LENGTH = 20  // Length in bytes of a SHA-1 hash.
+};
+
+// Computes the SHA-1 hash of the input string |str| and returns the full
+// hash.
+std::string SHA1HashString(const std::string& str);
+
+}  // namespace base
+
+#endif  // BASE_SHA1_H_
