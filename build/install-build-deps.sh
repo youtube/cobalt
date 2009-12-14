42c463c2045aea68d8444ae5c0eb2f72db296cfe
cmasone@google.com <cmasone@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-14 22:20:37 +0000

    Adding libpam0g-dev to the build deps -- we need this for doing login with Chromium on Chromium OS
    BUG=30328
    
    Review URL: http://codereview.chromium.org/503002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34506 0039d316-1c4b-4281-b951-d872f2087c98

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -111,9 +111,9 @@ fi
 # Packages need for development
 dev_list="apache2 bison fakeroot flex g++ g++-multilib gperf libapache2-mod-php5
           libasound2-dev libbz2-dev libcairo2-dev libgconf2-dev libglib2.0-dev
-          libgtk2.0-dev libjpeg62-dev libnspr4-dev libnss3-dev libsqlite3-dev
-          libxslt1-dev lighttpd msttcorefonts patch perl php5-cgi pkg-config
-          python rpm subversion wdiff"
+          libgtk2.0-dev libjpeg62-dev libnspr4-dev libnss3-dev libpam0g-dev
+          libsqlite3-dev libxslt1-dev lighttpd msttcorefonts patch perl
+          php5-cgi pkg-config python rpm subversion wdiff"
 
 # Full list of required run-time libraries
 lib_list="libatk1.0-0 libc6 libasound2 libcairo2 libexpat1
