youtube/cobalt

commit 355261e7f22705c197fc846890ceb87a5f7cb2ba

Fix race in directory creation on Windows.  The file_util::CreateDirectory() function is supposed to succeed if the directory already exists, but in the previous implementation when two processes/threads called the function at the same time for the same path, one would see a failure.
Also, get rid of the use of SHCreateDirectoryEx function.  For one, it
is highly serializing because it calls SHChangeNotify.  The race was
actually easy to reproduce by starting two instances of Chrome both at
the same time, with a profile directory flag indicating a profile
directory that did not previously exist.

For another, SHCreateDirectoryEx would fail with ERROR_CANCELLED if
any of the path components were not visible, according to its MSDN
documentation.

BUG=none
TEST=[base_unittests.exe --gtest_filter=*CreateDirectory* ], and/or install Chrome Frame on a fresh machine and make sure the first page you navigate to has two Chrome Frame instances on it - you should get no DCHECK on failure of PathService::Override

Review URL: http://codereview.chromium.org/437090

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@33225 0039d316-1c4b-4281-b951-d872f2087c98

base/file_util_win.cc
@@ -509,10 +509,45 @@ bool CreateNewTempDirectory(const FilePath::StringType& prefix,
 }
 
 bool CreateDirectory(const FilePath& full_path) {
-  if (DirectoryExists(full_path))
+  // If the path exists, we've succeeded if it's a directory, failed otherwise.
+  const wchar_t* full_path_str = full_path.value().c_str();
+  DWORD fileattr = ::GetFileAttributes(full_path_str);
+  if (fileattr != INVALID_FILE_ATTRIBUTES) {
+    if ((fileattr & FILE_ATTRIBUTE_DIRECTORY) != 0) {
+      DLOG(INFO) << "CreateDirectory(" << full_path_str << "), " <<
+          "directory already exists.";
+      return true;
+    } else {
+      LOG(WARNING) << "CreateDirectory(" << full_path_str << "), " <<
+          "conflicts with existing file.";
+    }
+  }
+
+  // Invariant:  Path does not exist as file or directory.
+
+  // Attempt to create the parent recursively.  This will immediately return
+  // true if it already exists, otherwise will create all required parent
+  // directories starting with the highest-level missing parent.
+  if (!CreateDirectory(full_path.DirName())) {
+    DLOG(WARNING) << "Failed to create one of the parent directories.";
+    return false;
+  }
+
+  if (!::CreateDirectory(full_path_str, NULL)) {
+    DWORD error_code = ::GetLastError();
+    if (error_code == ERROR_ALREADY_EXISTS && DirectoryExists(full_path)) {
+      // This error code doesn't indicate whether we were racing with someone
+      // creating the same directory, or a file with the same path, therefore
+      // we check.
+      return true;
+    } else {
+      LOG(WARNING) << "Failed to create directory " << full_path_str <<
+          ", le=" << error_code;
+      return false;
+    }
+  } else {
     return true;
-  int err = SHCreateDirectoryEx(NULL, full_path.value().c_str(), NULL);
-  return err == ERROR_SUCCESS;
+  }
 }
 
 bool GetFileInfo(const FilePath& file_path, FileInfo* results) {

base/file_util_unittest.cc
@@ -1137,6 +1137,23 @@ TEST_F(FileUtilTest, CreateDirectoryTest) {
   EXPECT_TRUE(file_util::Delete(test_root, true));
   EXPECT_FALSE(file_util::PathExists(test_root));
   EXPECT_FALSE(file_util::PathExists(test_path));
+
+  // Verify assumptions made by the Windows implementation:
+  // 1. The current directory always exists.
+  // 2. The root directory always exists.
+  ASSERT_TRUE(file_util::DirectoryExists(
+      FilePath(FilePath::kCurrentDirectory)));
+  FilePath top_level = test_root;
+  while (top_level != top_level.DirName()) {
+    top_level = top_level.DirName();
+  }
+  ASSERT_TRUE(file_util::DirectoryExists(top_level));
+
+  // Given these assumptions hold, it should be safe to
+  // test that "creating" these directories succeeds.
+  EXPECT_TRUE(file_util::CreateDirectory(
+      FilePath(FilePath::kCurrentDirectory)));
+  EXPECT_TRUE(file_util::CreateDirectory(top_level));
 }
 
 TEST_F(FileUtilTest, DetectDirectoryTest) {

commit 0f6514d94399c0fa71a1042a60edfb3395453e89

Fix a regression introduced by r33225.
On windows, if CreateDirectory is called with an path on an
inaccessible drive, it will enter an infinite loop.

BUG=30415
TEST=base_unittests.exe --gtest_filter=FileUtilTest.CreateDirectoryTest

Review URL: http://codereview.chromium.org/500075

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34871 0039d316-1c4b-4281-b951-d872f2087c98

base/file_util_win.cc
@@ -528,7 +528,11 @@ bool CreateDirectory(const FilePath& full_path) {
   // Attempt to create the parent recursively.  This will immediately return
   // true if it already exists, otherwise will create all required parent
   // directories starting with the highest-level missing parent.
-  if (!CreateDirectory(full_path.DirName())) {
+  FilePath parent_path(full_path.DirName());
+  if (parent_path.value() == full_path.value()) {
+    return false;
+  }
+  if (!CreateDirectory(parent_path)) {
     DLOG(WARNING) << "Failed to create one of the parent directories.";
     return false;
   }

base/file_util_unittest.cc
@@ -1156,6 +1156,15 @@ TEST_F(FileUtilTest, CreateDirectoryTest) {
   EXPECT_TRUE(file_util::CreateDirectory(
       FilePath(FilePath::kCurrentDirectory)));
   EXPECT_TRUE(file_util::CreateDirectory(top_level));
+
+#if defined(OS_WIN)
+  FilePath invalid_drive(FILE_PATH_LITERAL("o:\\"));
+  FilePath invalid_path =
+      invalid_drive.Append(FILE_PATH_LITERAL("some\\inaccessible\\dir"));
+  if (!file_util::PathExists(invalid_drive)) {
+    EXPECT_FALSE(file_util::CreateDirectory(invalid_path));
+  }
+#endif
 }
 
 TEST_F(FileUtilTest, DetectDirectoryTest) {