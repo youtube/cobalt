9d319ef3aae8bf31ea4a31980e722cc46d498c0c
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-24 01:03:05 +0000

    linux: add gl headers/libs for gpu plugin
    Review URL: http://codereview.chromium.org/522007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35248 0039d316-1c4b-4281-b951-d872f2087c98

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -110,10 +110,11 @@ fi
 
 # Packages need for development
 dev_list="apache2 bison fakeroot flex g++ g++-multilib gperf libapache2-mod-php5
-          libasound2-dev libbz2-dev libcairo2-dev libgconf2-dev libglib2.0-dev
-          libgtk2.0-dev libjpeg62-dev libnspr4-dev libnss3-dev libpam0g-dev
-          libsqlite3-dev libxslt1-dev lighttpd msttcorefonts patch perl
-          php5-cgi pkg-config python rpm subversion wdiff"
+          libasound2-dev libbz2-dev libcairo2-dev libgconf2-dev libgl1-mesa-dev
+          libglu1-mesa-dev libglib2.0-dev libgtk2.0-dev libjpeg62-dev
+          libnspr4-dev libnss3-dev libpam0g-dev libsqlite3-dev libxslt1-dev
+          lighttpd mesa-common-dev msttcorefonts patch perl php5-cgi pkg-config
+          python rpm subversion wdiff"
 
 # Full list of required run-time libraries
 lib_list="libatk1.0-0 libc6 libasound2 libcairo2 libexpat1
