3c4b8606b8df5ceb0fc99ecd73877e4b9bdceed8
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-29 00:59:31 +0000

    Clarify licenses on a bunch of files.
    In this process, I also filed bugs on various projects we depend
    on that also have unclear licenses.
    
    BUG=28291
    
    Review URL: http://codereview.chromium.org/521009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35323 0039d316-1c4b-4281-b951-d872f2087c98

--- build/linux/rewrite_dirs.py
+++ build/linux/rewrite_dirs.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+# Copyright (c) 2009 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 """Rewrites paths in -I, -L and other option to be relative to a sysroot."""
 
