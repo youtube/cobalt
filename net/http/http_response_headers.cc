a59f171bebb52b5bede7e1bd17e56c97835d34a3
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-08 01:13:36 +0000

    Http cache: Avoid resuming (and keeping) truncated entries if the server doesn't provide a strong validator.
    We require:
    - A strong etag or a strong last modified date.
    - The total Content length.
    - Lack of an explicit rejection of ranges (Accept-ranges: none) 
    
    This aligns better with the conditions used by Firefox.
    
    BUG=30220, b/2329250
    TEST=unittests
    
    Review URL: http://codereview.chromium.org/517043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35761 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_response_headers.cc
+++ net/http/http_response_headers.cc
@@ -1001,6 +1001,32 @@ bool HttpResponseHeaders::IsKeepAlive() const {
   return keep_alive;
 }
 
+bool HttpResponseHeaders::HasStrongValidators() const {
+  std::string etag_value;
+  EnumerateHeader(NULL, "etag", &etag_value);
+  if (!etag_value.empty()) {
+    size_t slash = etag_value.find('/');
+    if (slash == std::string::npos || slash == 0)
+      return true;
+
+    std::string::const_iterator i = etag_value.begin();
+    std::string::const_iterator j = etag_value.begin() + slash;
+    HttpUtil::TrimLWS(&i, &j);
+    if (!LowerCaseEqualsASCII(i, j, "w"))
+      return true;
+  }
+
+  Time last_modified;
+  if (!GetLastModifiedValue(&last_modified))
+    return false;
+
+  Time date;
+  if (!GetDateValue(&date))
+    return false;
+
+  return ((date - last_modified).InSeconds() >= 60);
+}
+
 // From RFC 2616:
 // Content-Length = "Content-Length" ":" 1*DIGIT
 int64 HttpResponseHeaders::GetContentLength() const {
