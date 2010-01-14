15a477b194796a90c3a7271a03975cd274f29cbc
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-14 19:36:32 +0000

    This CL contains the back-end implementation of the translate feature. It adds a Translate method to the renderer. On invocation this method triggers a traversal of the DOM page to retrieve the text nodes.  The text node contents are then sent to the browser for actual translation (at this point, we just up-case the text for testing purpose). The browser sends back the translated text to the renderer that replace the DOM text node values with the translated text.
    BUG=None
    TEST=Run the unit-tests.
    Review URL: http://codereview.chromium.org/547013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36258 0039d316-1c4b-4281-b951-d872f2087c98

--- base/string_util_win.h
+++ base/string_util_win.h
@@ -28,6 +28,10 @@ inline int strncasecmp(const char* s1, const char* s2, size_t count) {
   return _strnicmp(s1, s2, count);
 }
 
+inline int strncmp16(const char16* s1, const char16* s2, size_t count) {
+  return ::wcsncmp(s1, s2, count);
+}
+
 inline int vsnprintf(char* buffer, size_t size,
                      const char* format, va_list arguments) {
   int length = vsnprintf_s(buffer, size, size - 1, format, arguments);
