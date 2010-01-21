e22573820398219cfb6940bc64d3c6d56aef8cec
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-21 21:38:05 +0000

    Added WWW-Authenticate Negotate support using shared HttpAuthSspi.
    BUG=29862
    TEST=None
    
    Review URL: http://codereview.chromium.org/551015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36785 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_auth_handler_ntlm_win.cc
+++ net/http/http_auth_handler_ntlm_win.cc
@@ -17,138 +17,23 @@
 
 #pragma comment(lib, "secur32.lib")
 
-namespace {
-
-void ZapString(string16* s) {
-  memset(&(*s)[0], 0, s->length() * 2);
-}
-
-}  // namespace
-
 namespace net {
 
-HttpAuthHandlerNTLM::HttpAuthHandlerNTLM() :  max_token_len_(0) {
-  SecInvalidateHandle(&cred_);
-  SecInvalidateHandle(&ctxt_);
+HttpAuthHandlerNTLM::HttpAuthHandlerNTLM() :  auth_sspi_("NTLM", NTLMSP_NAME) {
 }
 
 HttpAuthHandlerNTLM::~HttpAuthHandlerNTLM() {
-  ResetSecurityContext();
-  if (SecIsValidHandle(&cred_)) {
-    FreeCredentialsHandle(&cred_);
-    SecInvalidateHandle(&cred_);
-  }
-  ZapString(&password_);
-}
-
-int HttpAuthHandlerNTLM::InitializeBeforeFirstChallenge() {
-  DCHECK_EQ("ntlm", scheme_) << "This is not ntlm scheme";
-
-  int rv = DetermineMaxTokenLength(NTLMSP_NAME, &max_token_len_);
-  if (rv != OK) {
-    return rv;
-  }
-  rv = AcquireCredentials(NTLMSP_NAME, domain_, username_, password_, &cred_);
-  return rv;
-}
-
-int HttpAuthHandlerNTLM::GetNextToken(const void* in_token,
-                                      uint32 in_token_len,
-                                      void** out_token,
-                                      uint32* out_token_len) {
-  SECURITY_STATUS status;
-  TimeStamp expiry;
-
-  DWORD ctxt_attr;
-  CtxtHandle* ctxt_ptr;
-  SecBufferDesc in_buffer_desc, out_buffer_desc;
-  SecBufferDesc* in_buffer_desc_ptr;
-  SecBuffer in_buffer, out_buffer;
-
-  if (in_token) {
-    // Prepare input buffer.
-    in_buffer_desc.ulVersion = SECBUFFER_VERSION;
-    in_buffer_desc.cBuffers = 1;
-    in_buffer_desc.pBuffers = &in_buffer;
-    in_buffer.BufferType = SECBUFFER_TOKEN;
-    in_buffer.cbBuffer = in_token_len;
-    in_buffer.pvBuffer = const_cast<void*>(in_token);
-    ctxt_ptr = &ctxt_;
-    in_buffer_desc_ptr = &in_buffer_desc;
-  } else {
-    // If there is no input token, then we are starting a new authentication
-    // sequence.  If we have already initialized our security context, then
-    // we're incorrectly reusing the auth handler for a new sequence.
-    if (SecIsValidHandle(&ctxt_)) {
-      LOG(ERROR) << "Cannot restart authentication sequence";
-      return ERR_UNEXPECTED;
-    }
-    ctxt_ptr = NULL;
-    in_buffer_desc_ptr = NULL;
-  }
-
-  // Prepare output buffer.
-  out_buffer_desc.ulVersion = SECBUFFER_VERSION;
-  out_buffer_desc.cBuffers = 1;
-  out_buffer_desc.pBuffers = &out_buffer;
-  out_buffer.BufferType = SECBUFFER_TOKEN;
-  out_buffer.cbBuffer = max_token_len_;
-  out_buffer.pvBuffer = malloc(out_buffer.cbBuffer);
-  if (!out_buffer.pvBuffer)
-    return ERR_OUT_OF_MEMORY;
-
-  // The service principal name of the destination server.  See
-  // http://msdn.microsoft.com/en-us/library/ms677949%28VS.85%29.aspx
-  std::wstring target(L"HTTP/");
-  target.append(ASCIIToWide(GetHostAndPort(origin_)));
-  wchar_t* target_name = const_cast<wchar_t*>(target.c_str());
-
-  // This returns a token that is passed to the remote server.
-  status = InitializeSecurityContext(&cred_,  // phCredential
-                                     ctxt_ptr,  // phContext
-                                     target_name,  // pszTargetName
-                                     0,  // fContextReq
-                                     0,  // Reserved1 (must be 0)
-                                     SECURITY_NATIVE_DREP,  // TargetDataRep
-                                     in_buffer_desc_ptr,  // pInput
-                                     0,  // Reserved2 (must be 0)
-                                     &ctxt_,  // phNewContext
-                                     &out_buffer_desc,  // pOutput
-                                     &ctxt_attr,  // pfContextAttr
-                                     &expiry);  // ptsExpiry
-  // On success, the function returns SEC_I_CONTINUE_NEEDED on the first call
-  // and SEC_E_OK on the second call.  On failure, the function returns an
-  // error code.
-  if (status != SEC_I_CONTINUE_NEEDED && status != SEC_E_OK) {
-    LOG(ERROR) << "InitializeSecurityContext failed: " << status;
-    ResetSecurityContext();
-    free(out_buffer.pvBuffer);
-    return ERR_UNEXPECTED;  // TODO(wtc): map error code.
-  }
-  if (!out_buffer.cbBuffer) {
-    free(out_buffer.pvBuffer);
-    out_buffer.pvBuffer = NULL;
-  }
-  *out_token = out_buffer.pvBuffer;
-  *out_token_len = out_buffer.cbBuffer;
-  return OK;
 }
 
 // Require identity on first pass instead of second.
 bool HttpAuthHandlerNTLM::NeedsIdentity() {
-  return auth_data_.empty();
+  return auth_sspi_.NeedsIdentity();
 }
 
 bool HttpAuthHandlerNTLM::IsFinalRound() {
-  return !auth_data_.empty();
+  return auth_sspi_.IsFinalRound();
 }
 
-void HttpAuthHandlerNTLM::ResetSecurityContext() {
-  if (SecIsValidHandle(&ctxt_)) {
-    DeleteSecurityContext(&ctxt_);
-    SecInvalidateHandle(&ctxt_);
-  }
-}
 
 }  // namespace net
 
--- net/http/http_auth_sspi_win.h
+++ net/http/http_auth_sspi_win.h
@@ -16,7 +16,52 @@
 
 #include <string>
 
+class GURL;
+
 namespace net {
+
+class HttpRequestInfo;
+class ProxyInfo;
+
+class HttpAuthSSPI {
+ public:
+  HttpAuthSSPI(const std::string& scheme,
+               SEC_WCHAR* security_package);
+  ~HttpAuthSSPI();
+
+  bool NeedsIdentity() const;
+  bool IsFinalRound() const;
+
+  bool ParseChallenge(std::string::const_iterator challenge_begin,
+                      std::string::const_iterator challenge_end);
+
+  int GenerateCredentials(const std::wstring& username,
+                          const std::wstring& password,
+                          const GURL& origin,
+                          const HttpRequestInfo* request,
+                          const ProxyInfo* proxy,
+                          std::string* out_credentials);
+
+ private:
+  int OnFirstRound(const std::wstring& domain,
+                   const std::wstring& user,
+                   const std::wstring& password);
+
+  int GetNextSecurityToken(
+      const GURL& origin,
+      const void* in_token,
+      int in_token_len,
+      void** out_token,
+      int* out_token_len);
+
+  void ResetSecurityContext();
+  std::string scheme_;
+  SEC_WCHAR* security_package_;
+  std::string decoded_server_auth_token_;
+  ULONG max_token_length_;
+  CredHandle cred_;
+  CtxtHandle ctxt_;
+};
 
 // Splits |combined| into domain and username.
 // If |combined| is of form "FOO\bar", |domain| will contain "FOO" and |user|
@@ -41,7 +86,7 @@ int AcquireCredentials(const SEC_WCHAR* package,
                        const std::wstring& user,
                        const std::wstring& password,
                        CredHandle* cred);
-}  // namespace net
 
+}  // namespace net
 #endif  // NET_HTTP_HTTP_AUTH_SSPI_WIN_H_
 
