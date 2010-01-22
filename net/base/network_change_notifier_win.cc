300ae72e40f1932faeb486ff3c7ff751bc62f7d0
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-22 02:22:10 +0000

    Implement NetworkChangeNotifierWin. Uses the NotifyAddrChange() API to detect ip address changes. BUG=http://crbug.com/26156
    Review URL: http://codereview.chromium.org/551077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36837 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/network_change_notifier_win.cc
+++ net/base/network_change_notifier_win.cc
@@ -3,10 +3,68 @@
 // found in the LICENSE file.
 
 #include "net/base/network_change_notifier_win.h"
+#include <iphlpapi.h>
+#include <windows.h>
+#include <winsock2.h>
+#include "base/basictypes.h"
+#include "base/compiler_specific.h"
+#include "base/logging.h"
+#include "base/object_watcher.h"
 
 namespace net {
 
-NetworkChangeNotifierWin::NetworkChangeNotifierWin() {}
+class NetworkChangeNotifierWin::Impl
+    : public base::ObjectWatcher::Delegate {
+ public:
+  explicit Impl(NetworkChangeNotifierWin* notifier);
+  virtual ~Impl();
+
+  void WatchForAddressChange();
+
+  // ObjectWatcher::Delegate methods:
+
+  virtual void OnObjectSignaled(HANDLE object);
+
+ private:
+  NetworkChangeNotifierWin* const notifier_;
+  base::ObjectWatcher addr_watcher_;
+  OVERLAPPED addr_overlapped_;
+
+  DISALLOW_COPY_AND_ASSIGN(Impl);
+};
+
+NetworkChangeNotifierWin::Impl::Impl(NetworkChangeNotifierWin* notifier)
+    : notifier_(notifier) {
+  memset(&addr_overlapped_, 0, sizeof(addr_overlapped_));
+  addr_overlapped_.hEvent = WSACreateEvent();
+}
+
+NetworkChangeNotifierWin::Impl::~Impl() {
+  CancelIPChangeNotify(&addr_overlapped_);
+  addr_watcher_.StopWatching();
+  WSACloseEvent(addr_overlapped_.hEvent);
+  memset(&addr_overlapped_, 0, sizeof(addr_overlapped_));
+}
+
+void NetworkChangeNotifierWin::Impl::WatchForAddressChange() {
+  HANDLE handle = NULL;
+  DWORD ret = NotifyAddrChange(&handle, &addr_overlapped_);
+  CHECK(ret == ERROR_IO_PENDING);
+  addr_watcher_.StartWatching(addr_overlapped_.hEvent, this);
+}
+
+void NetworkChangeNotifierWin::Impl::OnObjectSignaled(HANDLE object) {
+  notifier_->OnIPAddressChanged();
+
+  // Start watching for further address changes.
+  WatchForAddressChange();
+}
+
+NetworkChangeNotifierWin::NetworkChangeNotifierWin()
+    : impl_(new Impl(ALLOW_THIS_IN_INITIALIZER_LIST(this))) {
+  impl_->WatchForAddressChange();
+}
+
 NetworkChangeNotifierWin::~NetworkChangeNotifierWin() {}
 
 }  // namespace net
