29d1445b821274dae9560ef23c424cea6503e0d9
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-22 03:04:26 +0000

    Revert 36837 (caused XP unit_tests crashes) - Implement NetworkChangeNotifierWin. Uses the NotifyAddrChange() API to detect ip address changes. BUG=http://crbug.com/26156
    Review URL: http://codereview.chromium.org/551077
    
    TBR=willchan@chromium.org
    Review URL: http://codereview.chromium.org/543167
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36842 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/network_change_notifier_win.h
+++ net/base/network_change_notifier_win.h
@@ -6,7 +6,6 @@
 #define NET_BASE_NETWORK_CHANGE_NOTIFIER_WIN_H_
 
 #include "base/basictypes.h"
-#include "base/object_watcher.h"
 #include "net/base/network_change_notifier_helper.h"
 
 namespace net {
@@ -14,11 +13,6 @@ namespace net {
 class NetworkChangeNotifierWin : public NetworkChangeNotifier {
  public:
   NetworkChangeNotifierWin();
-
-  // Called by NetworkChangeNotifierWin::Impl.
-  void OnIPAddressChanged() { helper_.OnIPAddressChanged(); }
-
-  // NetworkChangeNotifier methods:
 
   virtual void AddObserver(Observer* observer) {
     helper_.AddObserver(observer);
@@ -29,12 +23,11 @@ class NetworkChangeNotifierWin : public NetworkChangeNotifier {
   }
 
  private:
-  class Impl;
-
   virtual ~NetworkChangeNotifierWin();
 
+  void OnIPAddressChanged() { helper_.OnIPAddressChanged(); }
+
   internal::NetworkChangeNotifierHelper helper_;
-  scoped_ptr<Impl> impl_;
 
   DISALLOW_COPY_AND_ASSIGN(NetworkChangeNotifierWin);
 };
