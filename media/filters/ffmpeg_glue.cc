e6052761dc39a322de26726f8843c7c22f7bcf3f
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-26 01:49:32 +0000

    Refactor some code that depends on FFmpeg
    Some FFmpeg header and source files are misplaced:
    
    media/filters/ffmpeg_common.{cc, h} -> media/ffmpeg
    media/tools/media_bench/file_protocol.{cc, h} -> media/ffmpeg
    
    And the necessary refactoring for the new location is done.
    
    Review URL: http://codereview.chromium.org/555087
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37080 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/ffmpeg_glue.cc
+++ media/filters/ffmpeg_glue.cc
@@ -1,10 +1,10 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "base/string_util.h"
 #include "media/base/filters.h"
-#include "media/filters/ffmpeg_common.h"
+#include "media/ffmpeg/ffmpeg_common.h"
 #include "media/filters/ffmpeg_glue.h"
 
 namespace {
