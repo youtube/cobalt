youtube/cobalt

commit a1c33424604026bdd0e6eae959160258e742bd65

Remove the implicit fallback to DIRECT when proxies fail. This better matches other browsers, and simplifies the code.
To better understand what this means, here are some examples how the behaviors will differ for the user:

(1) You start chrome with --proxy-server="foobar:80".
The server "foobar:80" is refusing connections.

  Before: Would fallback to direct after failing to connect through foobar:80.
  Now: Will error-out with connection refused after failing to connect through foobar:80.

(2) You start chrome with --proxy-pac-url="file:///foobar.pac".
The server "foobar:80" is unreachable, and foobar.pac reads:
  function FindProxyForURL(url, host) {
    return "PROXY foobar:80";
  }

  Before: Would fallback to direct after failing to connect through foobar:80.
  Now: Will error-out with connection refused after failing to connect through foobar:80.

(3) You start chrome with --proxy-pac-url="file:///foobar.pac".
The server "foobar:80" is unreachable, and foobar.pac reads:
  function FindProxyForURL(url, host) {
    return "PROXY foobar:80; DIRECT";
  }

  *No change, since the fallback to DIRECT is explicit in the PAC script*

BUG=12303

Review URL: http://codereview.chromium.org/502068

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35549 0039d316-1c4b-4281-b951-d872f2087c98

net/http/http_network_transaction.cc
@@ -576,24 +576,65 @@ int HttpNetworkTransaction::DoResolveProxy() {
 }
 
 int HttpNetworkTransaction::DoResolveProxyComplete(int result) {
-  next_state_ = STATE_INIT_CONNECTION;
+
+  pac_request_ = NULL;
 
   // Remove unsupported proxies from the list.
   proxy_info_.RemoveProxiesWithoutScheme(
       ProxyServer::SCHEME_DIRECT | ProxyServer::SCHEME_HTTP |
       ProxyServer::SCHEME_SOCKS4 | ProxyServer::SCHEME_SOCKS5);
 
-  pac_request_ = NULL;
+  // There are four possible outcomes of having run the ProxyService:
+  //   (1) The ProxyService decided we should connect through a proxy.
+  //   (2) The ProxyService decided we should direct-connect.
+  //   (3) The ProxyService decided we should give up, as there are no more
+  //       proxies to try (this is more likely to happen during
+  //       ReconsiderProxyAfterError()).
+  //   (4) The ProxyService failed (which can happen if the PAC script
+  //       we were configured with threw a runtime exception).
+  //
+  // It is important that we fail the connection in case (3) rather than
+  // falling-back to a direct connection, since sending traffic through
+  // a proxy may be integral to the user's privacy/security model.
+  //
+  // For example if a user had configured traffic to go through the TOR
+  // anonymizing proxy to protect their privacy, it would be bad if we
+  // silently fell-back to direct connect if the proxy server were to
+  // become unreachable.
+  //
+  // In case (4) it is less obvious what the right thing to do is. On the
+  // one hand, for consistency it would be natural to hard-fail as well.
+  // However, both Firefox 3.5 and Internet Explorer 8 will silently fall-back
+  // to DIRECT in this case, so we will do the same for compatibility.
+  //
+  // For more information, see:
+  // http://www.chromium.org/developers/design-documents/proxy-settings-fallback
+
+  if (proxy_info_.is_empty()) {
+    // No proxies/direct to choose from. This can happen when:
+    //   a. We don't support any of the proxies in the returned list.
+    //   b. The proxy service returned us an empty list.
+    //      1. this can happen if all the proxies were marked as bad already.
+    //
+    // TODO(eroman): in case (b.1) it would be better to just try the bad
+    // proxies again rather than failing without having tried anything!
+    return ERR_EMPTY_PROXY_LIST;
+  }
 
   if (result != OK) {
     DLOG(ERROR) << "Failed to resolve proxy: " << result;
+    // Fall-back to direct when there were runtime errors in the PAC script,
+    // or some other failure with the settings.
     proxy_info_.UseDirect();
   }
+
+  next_state_ = STATE_INIT_CONNECTION;
   return OK;
 }
 
 int HttpNetworkTransaction::DoInitConnection() {
   DCHECK(!connection_->is_initialized());
+  DCHECK(proxy_info_.proxy_server().is_valid());
 
   next_state_ = STATE_INIT_CONNECTION_COMPLETE;
 
@@ -1550,6 +1591,10 @@ int HttpNetworkTransaction::ReconsiderProxyAfterError(int error) {
     connection_->Reset();
     next_state_ = STATE_RESOLVE_PROXY_COMPLETE;
   } else {
+    // If ReconsiderProxyAfterError() failed synchronously, it means
+    // there was nothing left to fall-back to, so fail the transaction
+    // with the last connection error we got.
+    // TODO(eroman): This is a confusing contract, make it more obvious.
     rv = error;
   }
 

net/http/http_network_transaction_unittest.cc
@@ -3556,6 +3556,8 @@ TEST_F(HttpNetworkTransactionTest, ReconsiderProxyAfterFailedConnection) {
   SessionDependencies session_deps(
       CreateFixedProxyService("myproxy:70;foobar:80"));
 
+  // This simulates failure resolving all hostnames; that means we will fail
+  // connecting to both proxies (myproxy:70 and foobar:80).
   session_deps.host_resolver->rules()->AddSimulatedFailure("*");
 
   scoped_ptr<HttpTransaction> trans(

net/base/net_error_list.h
@@ -288,6 +288,9 @@ NET_ERROR(UNRECOGNIZED_FTP_DIRECTORY_LISTING_FORMAT, -334)
 // Attempted use of an unknown FLIP stream id.
 NET_ERROR(INVALID_FLIP_STREAM, -335)
 
+// There are no supported proxies in the provided list.
+NET_ERROR(EMPTY_PROXY_LIST, -336)
+
 // The cache does not have the requested entry.
 NET_ERROR(CACHE_MISS, -400)
 

net/proxy/proxy_info.cc
@@ -6,9 +6,7 @@
 
 namespace net {
 
-ProxyInfo::ProxyInfo()
-    : config_id_(ProxyConfig::INVALID_ID),
-      config_was_tried_(false) {
+ProxyInfo::ProxyInfo() : config_id_(ProxyConfig::INVALID_ID) {
 }
 
 void ProxyInfo::Use(const ProxyInfo& other) {
@@ -16,7 +14,7 @@ void ProxyInfo::Use(const ProxyInfo& other) {
 }
 
 void ProxyInfo::UseDirect() {
-  proxy_list_.Set(std::string());
+  proxy_list_.SetSingleProxyServer(ProxyServer::Direct());
 }
 
 void ProxyInfo::UseNamedProxy(const std::string& proxy_uri_list) {

net/proxy/proxy_info.h
@@ -43,9 +43,20 @@ class ProxyInfo {
   }
 
   // Returns true if this proxy info specifies a direct connection.
-  bool is_direct() const { return proxy_list_.Get().is_direct(); }
+  bool is_direct() const {
+    // We don't implicitly fallback to DIRECT unless it was added to the list.
+    if (is_empty())
+      return false;
+    return proxy_list_.Get().is_direct();
+  }
 
-  // Returns the first valid proxy server.
+  // Returns true if this proxy info has no proxies left to try.
+  bool is_empty() const {
+    return proxy_list_.IsEmpty();
+  }
+
+  // Returns the first valid proxy server. is_empty() must be false to be able
+  // to call this function.
   ProxyServer proxy_server() const { return proxy_list_.Get(); }
 
   // See description in ProxyList::ToPacString().
@@ -70,17 +81,12 @@ class ProxyInfo {
  private:
   friend class ProxyService;
 
-  // If proxy_list_ is set to empty, then a "direct" connection is indicated.
+  // The ordered list of proxy servers (including DIRECT attempts) remaining to
+  // try. If proxy_list_ is empty, then there is nothing left to fall back to.
   ProxyList proxy_list_;
 
   // This value identifies the proxy config used to initialize this object.
   ProxyConfig::ID config_id_;
-
-  // This flag is false when the proxy configuration was known to be bad when
-  // this proxy info was initialized.  In such cases, we know that if this
-  // proxy info does not yield a connection that we might want to reconsider
-  // the proxy config given by config_id_.
-  bool config_was_tried_;
 };
 
 }  // namespace net

net/proxy/proxy_list.cc
@@ -61,10 +61,13 @@ void ProxyList::RemoveProxiesWithoutScheme(int scheme_bit_field) {
   }
 }
 
-ProxyServer ProxyList::Get() const {
-  if (!proxies_.empty())
-    return proxies_[0];
-  return ProxyServer(ProxyServer::SCHEME_DIRECT, std::string(), -1);
+bool ProxyList::IsEmpty() const {
+  return proxies_.empty();
+}
+
+const ProxyServer& ProxyList::Get() const {
+  DCHECK(!proxies_.empty());
+  return proxies_[0];
 }
 
 std::string ProxyList::ToPacString() const {
@@ -75,7 +78,7 @@ std::string ProxyList::ToPacString() const {
       proxy_list += ";";
     proxy_list += iter->ToPacString();
   }
-  return proxy_list.empty() ? "DIRECT" : proxy_list;
+  return proxy_list.empty() ? std::string() : proxy_list;
 }
 
 void ProxyList::SetFromPacString(const std::string& pac_string) {
@@ -88,30 +91,51 @@ void ProxyList::SetFromPacString(const std::string& pac_string) {
     if (uri.is_valid())
       proxies_.push_back(uri);
   }
+
+  // If we failed to parse anything from the PAC results list, fallback to
+  // DIRECT (this basically means an error in the PAC script).
+  if (proxies_.empty()) {
+    proxies_.push_back(ProxyServer::Direct());
+  }
 }
 
 bool ProxyList::Fallback(ProxyRetryInfoMap* proxy_retry_info) {
   // Number of minutes to wait before retrying a bad proxy server.
   const TimeDelta kProxyRetryDelay = TimeDelta::FromMinutes(5);
 
+  // TODO(eroman): It would be good if instead of removing failed proxies
+  // from the list, we simply annotated them with the error code they failed
+  // with. Of course, ProxyService::ReconsiderProxyAfterError() would need to
+  // be given this information by the network transaction.
+  //
+  // The advantage of this approach is when the network transaction
+  // fails, we could output the full list of proxies that were attempted, and
+  // why each one of those failed (as opposed to just the last failure).
+  //
+  // And also, before failing the transaction wholesale, we could go back and
+  // retry the "bad proxies" which we never tried to begin with.
+  // (RemoveBadProxies would annotate them as 'expected bad' rather then delete
+  // them from the list, so we would know what they were).
+
   if (proxies_.empty()) {
     NOTREACHED();
     return false;
   }
 
-  std::string key = proxies_[0].ToURI();
-
-  // Mark this proxy as bad.
-  ProxyRetryInfoMap::iterator iter = proxy_retry_info->find(key);
-  if (iter != proxy_retry_info->end()) {
-    // TODO(nsylvain): This is not the first time we get this. We should
-    // double the retry time. Bug 997660.
-    iter->second.bad_until = TimeTicks::Now() + iter->second.current_delay;
-  } else {
-    ProxyRetryInfo retry_info;
-    retry_info.current_delay = kProxyRetryDelay;
-    retry_info.bad_until = TimeTicks().Now() + retry_info.current_delay;
-    (*proxy_retry_info)[key] = retry_info;
+  if (!proxies_[0].is_direct()) {
+    std::string key = proxies_[0].ToURI();
+    // Mark this proxy as bad.
+    ProxyRetryInfoMap::iterator iter = proxy_retry_info->find(key);
+    if (iter != proxy_retry_info->end()) {
+      // TODO(nsylvain): This is not the first time we get this. We should
+      // double the retry time. Bug 997660.
+      iter->second.bad_until = TimeTicks::Now() + iter->second.current_delay;
+    } else {
+      ProxyRetryInfo retry_info;
+      retry_info.current_delay = kProxyRetryDelay;
+      retry_info.bad_until = TimeTicks().Now() + retry_info.current_delay;
+      (*proxy_retry_info)[key] = retry_info;
+    }
   }
 
   // Remove this proxy from our list.

net/proxy/proxy_list.h
@@ -32,8 +32,12 @@ class ProxyList {
   // |scheme_bit_field| is a bunch of ProxyServer::Scheme bitwise ORed together.
   void RemoveProxiesWithoutScheme(int scheme_bit_field);
 
-  // Returns the first valid proxy server in the list.
-  ProxyServer Get() const;
+  // Returns true if there is nothing left in the ProxyList.
+  bool IsEmpty() const;
+
+  // Returns the first proxy server in the list. It is only valid to call
+  // this if !IsEmpty().
+  const ProxyServer& Get() const;
 
   // Set the list by parsing the pac result |pac_string|.
   // Some examples for |pac_string|:

net/proxy/proxy_list_unittest.cc
@@ -24,9 +24,20 @@ TEST(ProxyListTest, SetFromPacString) {
     {  "proxy foopy1 ; SOCKS foopy2",
        "PROXY foopy1:80;SOCKS foopy2:1080",
     },
+    // Try putting DIRECT first.
+    {  "DIRECT ; proxy foopy1 ; DIRECT ; SOCKS5 foopy2;DIRECT ",
+       "DIRECT;PROXY foopy1:80;DIRECT;SOCKS5 foopy2:1080;DIRECT",
+    },
+    // Try putting DIRECT consecutively.
+    {  "DIRECT ; proxy foopy1:80; DIRECT ; DIRECT",
+       "DIRECT;PROXY foopy1:80;DIRECT;DIRECT",
+    },
 
     // Invalid inputs (parts which aren't understood get
     // silently discarded):
+    //
+    // If the proxy list string parsed to empty, automatically fall-back to
+    // DIRECT.
     {  "PROXY-foopy:10",
        "DIRECT",
     },
@@ -42,6 +53,7 @@ TEST(ProxyListTest, SetFromPacString) {
     net::ProxyList list;
     list.SetFromPacString(tests[i].pac_input);
     EXPECT_EQ(tests[i].pac_output, list.ToPacString());
+    EXPECT_FALSE(list.IsEmpty());
   }
 }
 
@@ -56,10 +68,10 @@ TEST(ProxyListTest, RemoveProxiesWithoutScheme) {
        net::ProxyServer::SCHEME_DIRECT | net::ProxyServer::SCHEME_HTTP,
        "PROXY foopy:10;PROXY foopy3:80;DIRECT",
     },
-    {  "PROXY foopy:10 | SOCKS5 foopy2",
+    {  "PROXY foopy:10 ; SOCKS5 foopy2",
        // Remove anything that isn't HTTP or SOCKS5.
        net::ProxyServer::SCHEME_DIRECT | net::ProxyServer::SCHEME_SOCKS4,
-       "DIRECT",
+       "",
     },
   };
 

commit 2160bbee23d9f5c52ab2cf5de38276cd1f7962c1

Fix a regression from r35549 that caused failure in evaluation of PAC script to result in ERR_NO_SUPPORTED_PROXIES rather than falling back to DIRECT.
BUG=32316
TEST=
No unit-test yet.
To test manually:
(1) create a PAC script with contents:
  function FindProxyForURL(url, host) {
    throw "FAILURE!"
  }
(2) Change the proxy settings to point at this file (can use a file:// URL if it is local).
(3) Load http://www.google.com/. Should succeed. Without the bug fix, it would fail with ERR_NO_SUPPORTED_PROXIES.

Review URL: http://codereview.chromium.org/549204

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37566 0039d316-1c4b-4281-b951-d872f2087c98

net/http/http_network_transaction.cc
@@ -579,6 +579,13 @@ int HttpNetworkTransaction::DoResolveProxyComplete(int result) {
 
   pac_request_ = NULL;
 
+  if (result != OK) {
+    DLOG(ERROR) << "Failed to resolve proxy: " << result;
+    // Fall-back to direct when there were runtime errors in the PAC script,
+    // or some other failure with the settings.
+    proxy_info_.UseDirect();
+  }
+
   // Remove unsupported proxies from the list.
   proxy_info_.RemoveProxiesWithoutScheme(
       ProxyServer::SCHEME_DIRECT | ProxyServer::SCHEME_HTTP |
@@ -614,13 +621,6 @@ int HttpNetworkTransaction::DoResolveProxyComplete(int result) {
     // No proxies/direct to choose from. This happens when we don't support any
     // of the proxies in the returned list.
     return ERR_NO_SUPPORTED_PROXIES;
-  }
-
-  if (result != OK) {
-    DLOG(ERROR) << "Failed to resolve proxy: " << result;
-    // Fall-back to direct when there were runtime errors in the PAC script,
-    // or some other failure with the settings.
-    proxy_info_.UseDirect();
   }
 
   next_state_ = STATE_INIT_CONNECTION;

net/http/http_network_transaction_unittest.cc
@@ -28,6 +28,10 @@
 
 //-----------------------------------------------------------------------------
 
+// TODO(eroman): Add a regression test for http://crbug.com/32316 -- when the
+// proxy service returns an error, we should fallback to DIRECT instead of
+// failing with ERR_NO_SUPPORTED_PROXIES.
+
 namespace net {
 
 // Create a proxy service which fails on all requests (falls back to direct).