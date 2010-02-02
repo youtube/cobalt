6816d8fce386e5ed509af334ff925fd104c964ce
jorlow@chromium.org <jorlow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-02 19:01:42 +0000

    Add support for fine grained permissions to use LocalStorage.
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/565004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37856 0039d316-1c4b-4281-b951-d872f2087c98

--- base/nullable_string16.h
+++ base/nullable_string16.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -13,6 +13,7 @@
 class NullableString16 {
  public:
   NullableString16() : is_null_(false) { }
+  explicit NullableString16(bool is_null) : is_null_(is_null) { }
   NullableString16(const string16& string, bool is_null)
       : string_(string), is_null_(is_null) {
   }
