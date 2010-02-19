1fdbcd6d58d2224af92893958ad193dfe4381a6c
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-19 18:44:58 +0000

    Re-lands r39417: "Implement NetworkChangeNotifierLinux." The change broke certain linux buildbots due to netlink.h forward declaring struct net, which conflicts with the net namespace.  The fix is to move the netlink code into a separate file which is not in the net namespace.
    Review URL: http://codereview.chromium.org/650009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39465 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/network_change_notifier_linux.h
+++ net/base/network_change_notifier_linux.h
@@ -6,13 +6,17 @@
 #define NET_BASE_NETWORK_CHANGE_NOTIFIER_LINUX_H_
 
 #include "base/basictypes.h"
+#include "base/message_loop.h"
 #include "net/base/network_change_notifier_helper.h"
 
 namespace net {
 
-class NetworkChangeNotifierLinux : public NetworkChangeNotifier {
+class NetworkChangeNotifierLinux
+    : public NetworkChangeNotifier, public MessageLoopForIO::Watcher {
  public:
   NetworkChangeNotifierLinux();
+
+  // NetworkChangeNotifier methods:
 
   virtual void AddObserver(Observer* observer) {
     helper_.AddObserver(observer);
@@ -22,12 +26,31 @@ class NetworkChangeNotifierLinux : public NetworkChangeNotifier {
     helper_.RemoveObserver(observer);
   }
 
+  // MessageLoopForIO::Watcher methods:
+
+  virtual void OnFileCanReadWithoutBlocking(int fd);
+  virtual void OnFileCanWriteWithoutBlocking(int /* fd */);
+
  private:
   virtual ~NetworkChangeNotifierLinux();
 
-  void OnIPAddressChanged() { helper_.OnIPAddressChanged(); }
+  // Starts listening for netlink messages.  Also handles the messages if there
+  // are any available on the netlink socket.
+  void ListenForNotifications();
+
+  // Attempts to read from the netlink socket into |buf| of length |len|.
+  // Returns the bytes read on synchronous success and ERR_IO_PENDING if the
+  // recv() would block.  Otherwise, it returns a net error code.
+  int ReadNotificationMessage(char* buf, size_t len);
+
+  // Handles the netlink message and notifies the observers.
+  void HandleNotifications(const char* buf, size_t len);
 
   internal::NetworkChangeNotifierHelper helper_;
+
+  int netlink_fd_;  // This is the netlink socket descriptor.
+  MessageLoopForIO* const loop_;
+  MessageLoopForIO::FileDescriptorWatcher netlink_watcher_;
 
   DISALLOW_COPY_AND_ASSIGN(NetworkChangeNotifierLinux);
 };
