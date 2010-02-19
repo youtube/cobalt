youtube/cobalt

commit 6ebfa35a3550d65b8d68eb841663d491dca12b9d

Fix LOAD_IGNORE_CERT_* on Mac
SSLClientSocketMac reports certificate error before SSL handshake is completed,
so just returning OK for LOAD_IGNORE_CERT_* won't work (completed_handshake_ is false yet, so we can't Read()/Write() on the socket).

Add the cert in allowed_bad_certs, and reconnect again.

BUG=35108
TEST=none

Review URL: http://codereview.chromium.org/593013

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39037 0039d316-1c4b-4281-b951-d872f2087c98

net/http/http_network_transaction.cc
@@ -790,12 +790,15 @@ int HttpNetworkTransaction::DoSSLConnectComplete(int result) {
                          proto == kSpdyProto);
 
   if (IsCertificateError(result)) {
-    if (use_spdy) {
-      // TODO(agl/willchan/wtc): We currently ignore certificate errors for
-      // spdy but we shouldn't. http://crbug.com/32020
-      result = OK;
-    } else {
-      result = HandleCertificateError(result);
+    result = HandleCertificateError(result);
+    // TODO(wtc): We currently ignore certificate errors for
+    // spdy but we shouldn't. http://crbug.com/32020
+    if ((result == OK || use_spdy) &&
+        !connection_->socket()->IsConnectedAndIdle()) {
+        connection_->socket()->Disconnect();
+        connection_->Reset();
+        next_state_ = STATE_INIT_CONNECTION;
+        return OK;
     }
   }
 
@@ -1379,6 +1382,20 @@ void HttpNetworkTransaction::LogBlockedTunnelResponse(
 
 int HttpNetworkTransaction::HandleCertificateError(int error) {
   DCHECK(using_ssl_);
+  DCHECK(IsCertificateError(error));
+
+  SSLClientSocket* ssl_socket =
+    reinterpret_cast<SSLClientSocket*>(connection_->socket());
+  ssl_socket->GetSSLInfo(&response_.ssl_info);
+
+  // Add the bad certificate to the set of allowed certificates in the
+  // SSL info object. This data structure will be consulted after calling
+  // RestartIgnoringLastError(). And the user will be asked interactively
+  // before RestartIgnoringLastError() is ever called.
+  SSLConfig::CertAndStatus bad_cert;
+  bad_cert.cert = response_.ssl_info.cert;
+  bad_cert.cert_status = response_.ssl_info.cert_status;
+  ssl_config_.allowed_bad_certs.push_back(bad_cert);
 
   const int kCertFlags = LOAD_IGNORE_CERT_COMMON_NAME_INVALID |
                          LOAD_IGNORE_CERT_DATE_INVALID |
@@ -1399,21 +1416,6 @@ int HttpNetworkTransaction::HandleCertificateError(int error) {
           error = OK;
         break;
     }
-  }
-
-  if (error != OK) {
-    SSLClientSocket* ssl_socket =
-        reinterpret_cast<SSLClientSocket*>(connection_->socket());
-    ssl_socket->GetSSLInfo(&response_.ssl_info);
-
-    // Add the bad certificate to the set of allowed certificates in the
-    // SSL info object. This data structure will be consulted after calling
-    // RestartIgnoringLastError(). And the user will be asked interactively
-    // before RestartIgnoringLastError() is ever called.
-    SSLConfig::CertAndStatus bad_cert;
-    bad_cert.cert = response_.ssl_info.cert;
-    bad_cert.cert_status = response_.ssl_info.cert_status;
-    ssl_config_.allowed_bad_certs.push_back(bad_cert);
   }
   return error;
 }

commit 015f49308ca149b440f2f5d3a2ec6d878d92c833

Fix a SPDY regression introduced in r39037.  When we ignore certificate errors for SPDY, we need to set |result| to OK.
Add rudimentary support for restarting a SPDY network transaction after
the user ignores a certificate error.  This requires turning |use_spdy|
into a class member.

R=willchan
BUG=35108
TEST=Testing SPDY with NPN against a server with a broken certificate
should not result in an SSL certificate error page.
Review URL: http://codereview.chromium.org/651054

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39508 0039d316-1c4b-4281-b951-d872f2087c98

net/http/http_network_transaction.cc
@@ -158,6 +158,7 @@ HttpNetworkTransaction::HttpNetworkTransaction(HttpNetworkSession* session)
       using_ssl_(false),
       proxy_mode_(kDirectConnection),
       establishing_tunnel_(false),
+      use_spdy_(false),
       embedded_identity_used_(false),
       read_buf_len_(0),
       next_state_(STATE_NONE) {
@@ -191,7 +192,13 @@ int HttpNetworkTransaction::Start(const HttpRequestInfo* request_info,
 int HttpNetworkTransaction::RestartIgnoringLastError(
     CompletionCallback* callback) {
   if (connection_->socket()->IsConnectedAndIdle()) {
-    next_state_ = STATE_SEND_REQUEST;
+    // TODO(wtc): Should we update any of the connection histograms that we
+    // update in DoSSLConnectComplete if |result| is OK?
+    if (use_spdy_) {
+      next_state_ = STATE_SPDY_SEND_REQUEST;
+    } else {
+      next_state_ = STATE_SEND_REQUEST;
+    }
   } else {
     connection_->socket()->Disconnect();
     connection_->Reset();
@@ -326,11 +333,12 @@ int HttpNetworkTransaction::Read(IOBuffer* buf, int buf_len,
   State next_state = STATE_NONE;
 
   // Are we using SPDY or HTTP?
-  if (spdy_stream_.get()) {
+  if (use_spdy_) {
     DCHECK(!http_stream_.get());
     DCHECK(spdy_stream_->GetResponseInfo()->headers);
     next_state = STATE_SPDY_READ_BODY;
   } else {
+    DCHECK(!spdy_stream_.get());
     scoped_refptr<HttpResponseHeaders> headers = GetResponseHeaders();
     DCHECK(headers.get());
     next_state = STATE_READ_BODY;
@@ -786,19 +794,20 @@ int HttpNetworkTransaction::DoSSLConnectComplete(int result) {
   if (result == OK || IsCertificateError(result))
     status = ssl_socket->GetNextProto(&proto);
   static const char kSpdyProto[] = "spdy";
-  const bool use_spdy = (status == SSLClientSocket::kNextProtoNegotiated &&
-                         proto == kSpdyProto);
+  use_spdy_ = (status == SSLClientSocket::kNextProtoNegotiated &&
+               proto == kSpdyProto);
 
   if (IsCertificateError(result)) {
     result = HandleCertificateError(result);
     // TODO(wtc): We currently ignore certificate errors for
     // spdy but we shouldn't. http://crbug.com/32020
-    if ((result == OK || use_spdy) &&
-        !connection_->socket()->IsConnectedAndIdle()) {
-        connection_->socket()->Disconnect();
-        connection_->Reset();
-        next_state_ = STATE_INIT_CONNECTION;
-        return OK;
+    if (use_spdy_)
+      result = OK;
+    if (result == OK && !connection_->socket()->IsConnectedAndIdle()) {
+      connection_->socket()->Disconnect();
+      connection_->Reset();
+      next_state_ = STATE_INIT_CONNECTION;
+      return result;
     }
   }
 
@@ -807,7 +816,7 @@ int HttpNetworkTransaction::DoSSLConnectComplete(int result) {
     base::TimeDelta connect_duration =
         base::TimeTicks::Now() - ssl_connect_start_time_;
 
-    if (use_spdy) {
+    if (use_spdy_) {
       UMA_HISTOGRAM_CUSTOM_TIMES("Net.SpdyConnectionLatency",
           connect_duration,
           base::TimeDelta::FromMilliseconds(1),

net/http/http_network_transaction.h
@@ -311,6 +311,10 @@ class HttpNetworkTransaction : public HttpTransaction {
   // the real request/response of the transaction.
   bool establishing_tunnel_;
 
+  // True if SPDY has been negotiated using the TLS next protocol negotiation
+  // (NPN) extension.
+  bool use_spdy_;
+
   // True if we've used the username/password embedded in the URL.  This
   // makes sure we use the embedded identity only once for the transaction,
   // preventing an infinite auth restart loop.