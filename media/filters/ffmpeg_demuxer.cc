534ab080f95fadf4bb27a1abd5c2fd3cec9e25ef
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-20 03:25:41 +0000

    Some fixes to player_x11 when OpenMAX is enabled
    - remove the --enable-h264-annexb-filter flag
    - OmxVideoDecodeEngine have some problems dealing EOS buffers
    
    TEST=player_x11 runs with --enable-openmax
    
    Review URL: http://codereview.chromium.org/652008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39542 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/ffmpeg_demuxer.cc
+++ media/filters/ffmpeg_demuxer.cc
@@ -492,11 +492,11 @@ void FFmpegDemuxer::DemuxTask() {
   DCHECK_LT(packet->stream_index, static_cast<int>(packet_streams_.size()));
   FFmpegDemuxerStream* demuxer_stream = packet_streams_[packet->stream_index];
   if (demuxer_stream) {
-    using switches::kEnableH264AnnexbFilter;
+    using switches::kEnableOpenMax;
     AVCodecContext* stream_context =
         format_context_->streams[packet->stream_index]->codec;
     if (stream_context->codec_id == CODEC_ID_H264 &&
-        CommandLine::ForCurrentProcess()->HasSwitch(kEnableH264AnnexbFilter)) {
+        CommandLine::ForCurrentProcess()->HasSwitch(kEnableOpenMax)) {
       // TODO(ajwong): Unittest this branch of the if statement.
       // Also, move this code into the FFmpegDemuxerStream, so that the decoder
       // can enable a filter in the stream as needed.
