8d58f0e54820e7526cb0d4c89c33a03977cb6cc9
vandebo@google.com <vandebo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-24 00:58:46 +0000

    Add ERR_END_OF_STREAM
    Add new error code "END_OF_STREAM" and use it in HttpStreamParser.
    Update a test to expect failure instead of success on an incomplete stream.
    
    BUG=25032
    TEST=updated unittests
    
    Review URL: http://codereview.chromium.org/650190
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39822 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_stream.h
+++ net/http/http_stream.h
@@ -53,12 +53,14 @@ class HttpStream {
 
   // Reads response body data, up to |buf_len| bytes. |buf_len| should be a
   // reasonable size (<2MB). The number of bytes read is returned, or an
-  // error is returned upon failure.  ERR_CONNECTION_CLOSED is returned to
-  // indicate end-of-connection.  ERR_IO_PENDING is returned if the operation
-  // could not be completed synchronously, in which case the result will be
-  // passed to the callback when available. If the operation is not completed
-  // immediately, the socket acquires a reference to the provided buffer until
-  // the callback is invoked or the socket is destroyed.
+  // error is returned upon failure.  ERR_END_OF_STREAM indicates that the
+  // request has been fully satisfied and there is no more data to read.
+  // ERR_CONNECTION_CLOSED is returned when the connection has been closed
+  // prematurely.  ERR_IO_PENDING is returned if the operation could not be
+  // completed synchronously, in which case the result will be passed to the
+  // callback when available. If the operation is not completed immediately,
+  // the socket acquires a reference to the provided buffer until the callback
+  // is invoked or the socket is destroyed.
   virtual int ReadResponseBody(IOBuffer* buf, int buf_len,
                                CompletionCallback* callback) = 0;
 
