67d4bb3e46ded019044d9fe23a337da83e6a6ecc
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-26 16:48:29 +0000

    SSL False Start Support
     * Adds TLS false start support. This allows us to start sending encrypted
       data before we have validated the server's Finished message. (This
       behaviour is already enabled on Android.)
    
       I've verified that this works using netem to add a 200ms delay on the
       loopback adaptor. I've also checked that an incorrect Finished message from
       the server causes an error by hacking the Go TLS server.
    
       Beware when looking at packet traces that the time taken in NSS's SQLite
       calls can exceed the RTT of the connection and make it appear that this
       code isn't functioning.
    
     * Adds DEBUG and TRACE defines to libssl when building Chromium in Debug
       mode. This means that setting SSLTRACE in the environment now works for
       debug builds.
    
    (Reland. First landed in r39905, reverted in r40024 because it uncovered a
    bug.)
    
    http://codereview.chromium.org/518065
    BUG=none
    TEST=none
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40124 0039d316-1c4b-4281-b951-d872f2087c98

--- net/third_party/nss/ssl/ssl3gthr.c
+++ net/third_party/nss/ssl/ssl3gthr.c
@@ -188,6 +188,7 @@ ssl3_GatherCompleteHandshake(sslSocket *ss, int flags)
 {
     SSL3Ciphertext cText;
     int            rv;
+    PRBool         canFalseStart = PR_FALSE;
 
     PORT_Assert( ss->opt.noLocks || ssl_HaveRecvBufLock(ss) );
     do {
@@ -207,7 +208,17 @@ ssl3_GatherCompleteHandshake(sslSocket *ss, int flags)
 	if (rv < 0) {
 	    return ss->recvdCloseNotify ? 0 : rv;
 	}
-    } while (ss->ssl3.hs.ws != idle_handshake && ss->gs.buf.len == 0);
+
+	if (ss->opt.enableFalseStart) {
+	    ssl_GetSSL3HandshakeLock(ss);
+	    canFalseStart = (ss->ssl3.hs.ws == wait_change_cipher ||
+			     ss->ssl3.hs.ws == wait_new_session_ticket) &&
+		            ssl3_CanFalseStart(ss);
+	    ssl_ReleaseSSL3HandshakeLock(ss);
+	}
+    } while (ss->ssl3.hs.ws != idle_handshake &&
+             !canFalseStart &&
+             ss->gs.buf.len == 0);
 
     ss->gs.readOffset = 0;
     ss->gs.writeOffset = ss->gs.buf.len;
