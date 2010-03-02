2c38298f578841b7a460996d7a8e33137758c4a3
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-02 20:50:56 +0000

    Generate stubs for OpenMAX IL
    Generate stubs for OpenMAX IL so we don't need a real OpenMAX library for
    building. The actual library is loaded during runtime.
    
    TEST=Build is green
    TEST=Running omx_test works on hardware with OpenMAX support
    
    Review URL: http://codereview.chromium.org/661135
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40418 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/media.h
+++ media/base/media.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -20,7 +20,11 @@ namespace media {
 // Returns true if everything was successfully initialized, false otherwise.
 bool InitializeMediaLibrary(const FilePath& module_dir);
 
+// Attempts to initialize OpenMAX library.
+//
+// Returns true if OpenMAX was successfully initialized and loaded.
+bool InitializeOpenMaxLibrary(const FilePath& module_dir);
+
 }  // namespace media
 
 #endif  // MEDIA_BASE_MEDIA_H_
-
