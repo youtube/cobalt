youtube/cobalt

commit 7d387caa75951f043a063bdf3888f1417837ee37

Don't ignore certificate errors with SPDY.
For benchmarking and testing, we still want to be able to ignore the 
certificate errors, so I added a flag to the use-spdy option to do so:

   --ignore-certificate-errors

BUG=32020
TEST=none

Review URL: http://codereview.chromium.org/661375

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40465 0039d316-1c4b-4281-b951-d872f2087c98

net/http/http_network_transaction.cc
@@ -143,6 +143,7 @@ void BuildTunnelRequest(const HttpRequestInfo* request_info,
 //-----------------------------------------------------------------------------
 
 std::string* HttpNetworkTransaction::g_next_protos = NULL;
+bool HttpNetworkTransaction::g_ignore_certificate_errors = false;
 
 HttpNetworkTransaction::HttpNetworkTransaction(HttpNetworkSession* session)
     : pending_auth_target_(HttpAuth::AUTH_NONE),
@@ -172,6 +173,11 @@ HttpNetworkTransaction::HttpNetworkTransaction(HttpNetworkSession* session)
 void HttpNetworkTransaction::SetNextProtos(const std::string& next_protos) {
   delete g_next_protos;
   g_next_protos = new std::string(next_protos);
+}
+
+// static
+void HttpNetworkTransaction::IgnoreCertificateErrors(bool enabled) {
+  g_ignore_certificate_errors = enabled;
 }
 
 int HttpNetworkTransaction::Start(const HttpRequestInfo* request_info,
@@ -776,27 +782,8 @@ int HttpNetworkTransaction::DoSSLConnect() {
 }
 
 int HttpNetworkTransaction::DoSSLConnectComplete(int result) {
-  SSLClientSocket* ssl_socket =
-      reinterpret_cast<SSLClientSocket*>(connection_->socket());
-
-  SSLClientSocket::NextProtoStatus status =
-      SSLClientSocket::kNextProtoUnsupported;
-  std::string proto;
-  // GetNextProto will fail and and trigger a NOTREACHED if we pass in a socket
-  // that hasn't had SSL_ImportFD called on it. If we get a certificate error
-  // here, then we know that we called SSL_ImportFD.
-  if (result == OK || IsCertificateError(result))
-    status = ssl_socket->GetNextProto(&proto);
-  static const char kSpdyProto[] = "spdy";
-  using_spdy_ = (status == SSLClientSocket::kNextProtoNegotiated &&
-                 proto == kSpdyProto);
-
   if (IsCertificateError(result)) {
     result = HandleCertificateError(result);
-    // TODO(wtc): We currently ignore certificate errors for
-    // spdy but we shouldn't. http://crbug.com/32020
-    if (using_spdy_)
-      result = OK;
     if (result == OK && !connection_->socket()->IsConnectedAndIdle()) {
       connection_->socket()->Disconnect();
       connection_->Reset();
@@ -806,6 +793,14 @@ int HttpNetworkTransaction::DoSSLConnectComplete(int result) {
   }
 
   if (result == OK) {
+    static const char kSpdyProto[] = "spdy";
+    std::string proto;
+    SSLClientSocket* ssl_socket =
+        reinterpret_cast<SSLClientSocket*>(connection_->socket());
+    SSLClientSocket::NextProtoStatus status = ssl_socket->GetNextProto(&proto);
+    using_spdy_ = (status == SSLClientSocket::kNextProtoNegotiated &&
+                   proto == kSpdyProto);
+
     DCHECK(ssl_connect_start_time_ != base::TimeTicks());
     base::TimeDelta connect_duration =
         base::TimeTicks::Now() - ssl_connect_start_time_;
@@ -1391,6 +1386,9 @@ void HttpNetworkTransaction::LogBlockedTunnelResponse(
 int HttpNetworkTransaction::HandleCertificateError(int error) {
   DCHECK(using_ssl_);
   DCHECK(IsCertificateError(error));
+
+  if (g_ignore_certificate_errors)
+    return OK;
 
   SSLClientSocket* ssl_socket =
     reinterpret_cast<SSLClientSocket*>(connection_->socket());

net/http/http_network_transaction.h
@@ -42,6 +42,10 @@ class HttpNetworkTransaction : public HttpTransaction {
   // Sets the next protocol negotiation value used during the SSL handshake.
   static void SetNextProtos(const std::string& next_protos);
 
+  // Sets the HttpNetworkTransaction into a mode where it can ignore
+  // certificate errors.  This is for testing.
+  static void IgnoreCertificateErrors(bool enabled);
+
   // HttpTransaction methods:
   virtual int Start(const HttpRequestInfo* request_info,
                     CompletionCallback* callback,
@@ -257,6 +261,8 @@ class HttpNetworkTransaction : public HttpTransaction {
   static std::string AuthTargetString(HttpAuth::Target target);
 
   static std::string* g_next_protos;
+
+  static bool g_ignore_certificate_errors;
 
   // The following three auth members are arrays of size two -- index 0 is
   // for the proxy server, and index 1 is for the origin server.

commit 06ca487b476314ceda56fe46eeb22121d1f1a7f8

Fix a regression introduced in r40465.
In DoSSLConnectComplete, we still need to set using_spdy_
if we get a certificate error.

In HandleCertificateError, we need to ignore the error
after adding the certificate to the allowed_bad_certs list.

R=mbelshe
BUG=37367
TEST=Connect to a SPDY server with a bad certificate. Clicking
"Proceed anyway" in the SSL blocking page should not result in
a download.
Review URL: http://codereview.chromium.org/668111

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40687 0039d316-1c4b-4281-b951-d872f2087c98

net/http/http_network_transaction.cc
@@ -782,6 +782,21 @@ int HttpNetworkTransaction::DoSSLConnect() {
 }
 
 int HttpNetworkTransaction::DoSSLConnectComplete(int result) {
+  SSLClientSocket* ssl_socket =
+      reinterpret_cast<SSLClientSocket*>(connection_->socket());
+
+  SSLClientSocket::NextProtoStatus status =
+      SSLClientSocket::kNextProtoUnsupported;
+  std::string proto;
+  // GetNextProto will fail and and trigger a NOTREACHED if we pass in a socket
+  // that hasn't had SSL_ImportFD called on it. If we get a certificate error
+  // here, then we know that we called SSL_ImportFD.
+  if (result == OK || IsCertificateError(result))
+    status = ssl_socket->GetNextProto(&proto);
+  static const char kSpdyProto[] = "spdy";
+  using_spdy_ = (status == SSLClientSocket::kNextProtoNegotiated &&
+                 proto == kSpdyProto);
+
   if (IsCertificateError(result)) {
     result = HandleCertificateError(result);
     if (result == OK && !connection_->socket()->IsConnectedAndIdle()) {
@@ -793,14 +808,6 @@ int HttpNetworkTransaction::DoSSLConnectComplete(int result) {
   }
 
   if (result == OK) {
-    static const char kSpdyProto[] = "spdy";
-    std::string proto;
-    SSLClientSocket* ssl_socket =
-        reinterpret_cast<SSLClientSocket*>(connection_->socket());
-    SSLClientSocket::NextProtoStatus status = ssl_socket->GetNextProto(&proto);
-    using_spdy_ = (status == SSLClientSocket::kNextProtoNegotiated &&
-                   proto == kSpdyProto);
-
     DCHECK(ssl_connect_start_time_ != base::TimeTicks());
     base::TimeDelta connect_duration =
         base::TimeTicks::Now() - ssl_connect_start_time_;
@@ -1355,9 +1362,6 @@ int HttpNetworkTransaction::HandleCertificateError(int error) {
   DCHECK(using_ssl_);
   DCHECK(IsCertificateError(error));
 
-  if (g_ignore_certificate_errors)
-    return OK;
-
   SSLClientSocket* ssl_socket =
     reinterpret_cast<SSLClientSocket*>(connection_->socket());
   ssl_socket->GetSSLInfo(&response_.ssl_info);
@@ -1370,6 +1374,9 @@ int HttpNetworkTransaction::HandleCertificateError(int error) {
   bad_cert.cert = response_.ssl_info.cert;
   bad_cert.cert_status = response_.ssl_info.cert_status;
   ssl_config_.allowed_bad_certs.push_back(bad_cert);
+
+  if (g_ignore_certificate_errors)
+    return OK;
 
   const int kCertFlags = LOAD_IGNORE_CERT_COMMON_NAME_INVALID |
                          LOAD_IGNORE_CERT_DATE_INVALID |