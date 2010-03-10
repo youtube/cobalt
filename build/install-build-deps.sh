e3d7b5bd81bc529de7fbada319ae81cdbb4d49f6
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-10 19:04:58 +0000

    Add libdbus-glib dependency into install-build-deps.sh Needed as a precursor to http://codereview.chromium.org/787003
    BUG=http://crbug.com/37199
    TEST=None
    
    Review URL: http://codereview.chromium.org/791003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41182 0039d316-1c4b-4281-b951-d872f2087c98

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -141,7 +141,7 @@ dev_list="apache2 bison fakeroot flex g++ g++-multilib gperf libapache2-mod-php5
           pkg-config python python2.5-dev rpm subversion wdiff"
 
 # Full list of required run-time libraries
-lib_list="libatk1.0-0 libc6 libasound2 libcairo2 libexpat1
+lib_list="libatk1.0-0 libc6 libasound2 libcairo2 libdbus-glib-1-dev libexpat1
           libfontconfig1 libfreetype6 libglib2.0-0 libgtk2.0-0 libnspr4-0d
           libnss3-1d libpango1.0-0 libpcre3 libpixman-1-0 libpng12-0 libstdc++6
           libsqlite3-0 libx11-6 libxau6 libxcb1 libxcomposite1
@@ -149,10 +149,10 @@ lib_list="libatk1.0-0 libc6 libasound2 libcairo2 libexpat1
           libxinerama1 libxrandr2 libxrender1 zlib1g"
 
 # Debugging symbols for all of the run-time libraries
-dbg_list="libatk1.0-dbg libc6-dbg libcairo2-dbg libfontconfig1-dbg
-          libglib2.0-0-dbg libgtk2.0-0-dbg libnspr4-0d-dbg libnss3-1d-dbg
-          libpango1.0-0-dbg libpcre3-dbg libpixman-1-0-dbg libx11-6-dbg
-          libxau6-dbg libxcb1-dbg libxcomposite1-dbg
+dbg_list="libatk1.0-dbg libc6-dbg libcairo2-dbg libdbus-glib-1-2-dbg
+          libfontconfig1-dbg libglib2.0-0-dbg libgtk2.0-0-dbg libnspr4-0d-dbg
+          libnss3-1d-dbg libpango1.0-0-dbg libpcre3-dbg libpixman-1-0-dbg
+          libx11-6-dbg libxau6-dbg libxcb1-dbg libxcomposite1-dbg
           libxcursor1-dbg libxdamage1-dbg libxdmcp6-dbg libxext6-dbg
           libxfixes3-dbg libxi6-dbg libxinerama1-dbg libxrandr2-dbg
           libxrender1-dbg zlib1g-dbg"
