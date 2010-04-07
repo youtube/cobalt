060e06c3f94f81f5eac8fa1748559959532014d8
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-07 14:41:40 +0000

    Revert 43826 - HostResolver now adds AI_CANONNAME to the hint flags if a requester needs the information.
    Requests which want the canonical name should be treated differently from requests that do not for the same host in both the HostCache as well as in the HostResolver when combining multiple outstanding requests into a job.
    
    The motivation for this is that Kerberos SPN's for a web server are typically generated using the canonical name of the server rather than a DNS alias (both Firefox and IE have this behavior).
    
    BUG=29862
    TEST=net_unittests
    
    Review URL: http://codereview.chromium.org/1566012
    
    TBR=cbentzel@chromium.org
    Review URL: http://codereview.chromium.org/1629005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43833 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/address_list.cc
+++ net/base/address_list.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -131,14 +131,6 @@ int AddressList::GetPort() const {
     return -1;
 
   return ntohs(*port_field);
-}
-
-bool AddressList::GetCanonicalName(std::string* canonical_name) const {
-  DCHECK(canonical_name);
-  if (!data_->head || !data_->head->ai_canonname)
-    return false;
-  canonical_name->assign(data_->head->ai_canonname);
-  return true;
 }
 
 void AddressList::SetFrom(const AddressList& src, int port) {
