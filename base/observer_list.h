74108ed163ee167eb2d907b13d20ed588aa34f3a
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-29 18:17:29 +0000

    Clean up some Observer code.  Original patch by Thiago Farina (see http://codereview.chromium.org/1725007 ), r=me.
    BUG=none
    TEST=none
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45963 0039d316-1c4b-4281-b951-d872f2087c98

--- base/observer_list.h
+++ base/observer_list.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -149,11 +149,9 @@ class ObserverListBase {
     }
   }
 
- protected:
-  size_t size() const {
-    return observers_.size();
-  }
+  size_t size() const { return observers_.size(); }
 
+ protected:
   void Compact() {
     typename ListType::iterator it = observers_.begin();
     while (it != observers_.end()) {
