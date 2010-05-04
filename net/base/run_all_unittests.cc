45dff81ceef40147c84204992fa19365cb4bd317
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-04 18:15:56 +0000

    Include "build/build_config.h" explicitly because we test the OS_WIN macro.
    R=willchan
    BUG=43142
    TEST=No compilation error.
    Review URL: http://codereview.chromium.org/1929005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46367 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/run_all_unittests.cc
+++ net/base/run_all_unittests.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "build/build_config.h"
 #include "base/histogram.h"
 #include "net/base/net_test_suite.h"
 #if defined(OS_WIN)
