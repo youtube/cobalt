76441cc4024cf966ffc258918872edea3cfbc02b
pvalchev@google.com <pvalchev@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-13 22:06:01 +0000

    Move non-linux specific code from base/linux_util to app/gtk_util from sprewell
    Review URL: http://codereview.chromium.org/2058004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47201 0039d316-1c4b-4281-b951-d872f2087c98

--- base/linux_util.cc
+++ base/linux_util.cc
@@ -122,26 +122,6 @@ bool ProcPathGetInode(ino_t* inode_out, const char* path, bool log = false) {
 
 namespace base {
 
-uint8_t* BGRAToRGBA(const uint8_t* pixels, int width, int height, int stride) {
-  if (stride == 0)
-    stride = width * 4;
-
-  uint8_t* new_pixels = static_cast<uint8_t*>(malloc(height * stride));
-
-  // We have to copy the pixels and swap from BGRA to RGBA.
-  for (int i = 0; i < height; ++i) {
-    for (int j = 0; j < width; ++j) {
-      int idx = i * stride + j * 4;
-      new_pixels[idx] = pixels[idx + 2];
-      new_pixels[idx + 1] = pixels[idx + 1];
-      new_pixels[idx + 2] = pixels[idx];
-      new_pixels[idx + 3] = pixels[idx + 3];
-    }
-  }
-
-  return new_pixels;
-}
-
 // We use this static string to hold the Linux distro info. If we
 // crash, the crash handler code will send this in the crash dump.
 std::string linux_distro =
