c36e0bfacf9b5635e6117dbc1552ce13b33205c5
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-18 22:59:15 +0000

    Reland my close on idle socket change.
    This reverts 44402 which reverted r44150 (fix for r43882) and r43882 (original change).  These changes were suspected for causing the infinite loop on the IO thread and also a crash.  It turned out that the backup socket was the cause.  These should be able to be safely relanded.  There's one minor change from the original, due to merging in the weeks of changes in between.
    BUG=32817
    
    Review URL: http://codereview.chromium.org/2077004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47583 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/client_socket_pool_base.h
+++ net/socket/client_socket_pool_base.h
@@ -225,7 +225,7 @@ class ClientSocketPoolBaseHelper
     return connect_job_factory_->ConnectionTimeout();
   }
 
-  void enable_backup_jobs() { backup_jobs_enabled_ = true; };
+  void EnableBackupJobs() { backup_jobs_enabled_ = true; };
 
  private:
   friend class base::RefCounted<ClientSocketPoolBaseHelper>;
@@ -387,6 +387,13 @@ class ClientSocketPoolBaseHelper
   // Called when the backup socket timer fires.
   void OnBackupSocketTimerFired(const std::string& group_name);
 
+  // Closes one idle socket.  Picks the first one encountered.
+  // TODO(willchan): Consider a better algorithm for doing this.  Perhaps we
+  // should keep an ordered list of idle sockets, and close them in order.
+  // Requires maintaining more state.  It's not clear if it's worth it since
+  // I'm not sure if we hit this situation often.
+  void CloseOneIdleSocket();
+
   GroupMap group_map_;
 
   // Timer used to periodically prune idle sockets that timed out or can't be
@@ -426,9 +433,13 @@ class ClientSocketPoolBaseHelper
   // |max_sockets_per_group_| limit. So choosing the next request involves
   // selecting the highest priority request across *all* groups.
   //
-  // Since reaching the maximum number of sockets is an edge case, we make note
-  // of when it happens, and thus avoid doing the slower "scan all groups"
-  // in the common case.
+  // |may_have_stalled_group_| is not conclusive, since when we cancel pending
+  // requests, we may reach the situation where we have the maximum number of 
+  // sockets, but not request is stalled because of the global socket limit
+  // (although some requests may be blocked on the socket per group limit).
+  // We don't strictly maintain |may_have_stalled_group_|, since that would
+  // require a linear search through all groups in |group_map_| to see if one 
+  // of them is stalled.
   bool may_have_stalled_group_;
 
   const scoped_ptr<ConnectJobFactory> connect_job_factory_;
@@ -568,7 +579,7 @@ class ClientSocketPoolBase {
 
   const std::string& name() const { return name_; }
 
-  void enable_backup_jobs() { helper_->enable_backup_jobs(); };
+  void EnableBackupJobs() { helper_->EnableBackupJobs(); };
 
  private:
   // This adaptor class exists to bridge the
