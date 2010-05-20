fc20c80d044125056de2b09d406c636b0a6f6dde
jiesun@google.com <jiesun@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-20 15:58:47 +0000

    refactoring engine interface for openmax.
    currently only fillbufferdone callback and emptythisbuffer is used.
    
    Review URL: http://codereview.chromium.org/2095002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47802 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/ffmpeg_audio_decoder.h
+++ media/filters/ffmpeg_audio_decoder.h
@@ -29,7 +29,7 @@ class FFmpegAudioDecoder : public DecoderBase<AudioDecoder, Buffer> {
 
   virtual void DoSeek(base::TimeDelta time, Task* done_cb);
 
-  virtual void DoDecode(Buffer* input, Task* done_cb);
+  virtual void DoDecode(Buffer* input);
 
  private:
   friend class FilterFactoryImpl0<FFmpegAudioDecoder>;
