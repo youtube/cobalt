40342c12f6f4641edaa90dcf029c3871245bbfef
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-21 02:08:35 +0000

    Track whether a resource was loaded via a proxy.  I'm going to use this so I can more precisely refine some histograms and filter whether individual resources came through proxies or not.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/2097012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47877 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_response_info.h
+++ net/http/http_response_info.h
@@ -28,12 +28,17 @@ class HttpResponseInfo {
   // request_time may corresponds to a time "far" in the past.  Note that
   // stale content (perhaps un-cacheable) may be fetched from cache subject to
   // the load flags specified on the request info.  For example, this is done
-  // when a user presses the back button to re-render pages, or at startup, when
-  // reloading previously visited pages (without going over the network).
+  // when a user presses the back button to re-render pages, or at startup,
+  // when reloading previously visited pages (without going over the network).
   bool was_cached;
 
   // True if the request was fetched over a SPDY channel.
   bool was_fetched_via_spdy;
+
+  // True if the request was fetched via an explicit proxy.  The proxy could
+  // be any type of proxy, HTTP or SOCKS.  Note, we do not know if a
+  // transparent proxy may have been involved.
+  bool was_fetched_via_proxy;
 
   // The time at which the request was made that resulted in this response.
   // For cached responses, this is the last time the cache entry was validated.
