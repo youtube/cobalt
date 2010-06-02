a255944266d90cc839b98eacbaaf424a70673d37
boliu@google.com <boliu@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-02 21:52:48 +0000

    Change MediaFilter::Stop() to accept a callback so that Stop() is asynchronous. So far PipelineImpl handles asynchronous MediaFilter::Stop(). The actual change to make MediaFilter::Stop() asynchronous will be in another check in.
    BUG=16059
    TEST=Unit tests still runs
    
    Review URL: http://codereview.chromium.org/2101015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48769 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/mock_filters.cc
+++ media/base/mock_filters.cc
@@ -1,6 +1,6 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.  Use of this
-// source code is governed by a BSD-style license that can be found in the
-// LICENSE file.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #include "media/base/mock_filters.h"
 
@@ -12,6 +12,11 @@ void RunFilterCallback(::testing::Unused, FilterCallback* callback) {
 }
 
 void DestroyFilterCallback(::testing::Unused, FilterCallback* callback) {
+  delete callback;
+}
+
+void RunStopFilterCallback(FilterCallback* callback) {
+  callback->Run();
   delete callback;
 }
 
