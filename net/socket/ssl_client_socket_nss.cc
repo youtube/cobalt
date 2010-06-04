youtube/cobalt

commit 71d1091ff459454bd3de9c27499cb2dd49f4ffd3

Use SSLClientSocketNSS on Mac OS X. By default, chrome still uses SSLClientSocketMac. Specify the --use-nss-for-ssl command-line option to use SSLClientSocketNSS.
The nss.gyp in src/net/third_party/nss is renamed ssl.gyp to avoid
a naming conflict with the nss.gyp in src/third_party/nss. The
GYP generator for Xcode project files disallows same-named .gyp files.

SSL client authentication doesn't work yet.

R=mark
BUG=30689
TEST=No build and test failures on Mac and Windows.
Review URL: http://codereview.chromium.org/2322008

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48650 0039d316-1c4b-4281-b951-d872f2087c98

net/socket/ssl_client_socket_nss.cc
@@ -318,7 +318,7 @@ int SSLClientSocketNSS::Init() {
   EnsureNSSSSLInit();
   if (!NSS_IsInitialized())
     return ERR_UNEXPECTED;
-#if !defined(OS_WIN)
+#if !defined(OS_MACOSX) && !defined(OS_WIN)
   // We must call EnsureOCSPInit() here, on the IO thread, to get the IO loop
   // by MessageLoopForIO::current().
   // X509Certificate::Verify() runs on a worker thread of CertVerifier.
@@ -663,16 +663,12 @@ X509Certificate *SSLClientSocketNSS::UpdateServerCert() {
   if (server_cert_ == NULL) {
     server_cert_nss_ = SSL_PeerCertificate(nss_fd_);
     if (server_cert_nss_) {
-#if defined(OS_WIN)
-      // TODO(wtc): close cert_store_ at shutdown.
-      if (!cert_store_)
-        cert_store_ = CertOpenStore(CERT_STORE_PROV_MEMORY, 0, NULL, 0, NULL);
-
+#if defined(OS_MACOSX) || defined(OS_WIN)
       // Get each of the intermediate certificates in the server's chain.
       // These will be added to the server's X509Certificate object, making
       // them available to X509Certificate::Verify() for chain building.
       X509Certificate::OSCertHandles intermediate_ca_certs;
-      PCCERT_CONTEXT cert_context = NULL;
+      X509Certificate::OSCertHandle cert_handle = NULL;
       CERTCertList* cert_list = CERT_GetCertChainFromCert(
           server_cert_nss_, PR_Now(), certUsageSSLCA);
       if (cert_list) {
@@ -681,6 +677,7 @@ X509Certificate *SSLClientSocketNSS::UpdateServerCert() {
              node = CERT_LIST_NEXT(node)) {
           if (node->cert == server_cert_nss_)
             continue;
+#if defined(OS_WIN)
           // Work around http://crbug.com/43538 by not importing the
           // problematic COMODO EV SGC CA certificate.  CryptoAPI will
           // download a good certificate for that CA, issued by COMODO
@@ -688,35 +685,27 @@ X509Certificate *SSLClientSocketNSS::UpdateServerCert() {
           // certificate.
           if (IsProblematicComodoEVCACert(*node->cert))
             continue;
-          cert_context = NULL;
-          BOOL ok = CertAddEncodedCertificateToStore(
-              cert_store_,
-              X509_ASN_ENCODING | PKCS_7_ASN_ENCODING,
-              node->cert->derCert.data,
-              node->cert->derCert.len,
-              CERT_STORE_ADD_USE_EXISTING,
-              &cert_context);
-          DCHECK(ok);
-          intermediate_ca_certs.push_back(cert_context);
+#endif
+          cert_handle = X509Certificate::CreateOSCertHandleFromBytes(
+              reinterpret_cast<char*>(node->cert->derCert.data),
+              node->cert->derCert.len);
+          DCHECK(cert_handle);
+          intermediate_ca_certs.push_back(cert_handle);
         }
         CERT_DestroyCertList(cert_list);
       }
 
       // Finally create the X509Certificate object.
-      BOOL ok = CertAddEncodedCertificateToStore(
-          cert_store_,
-          X509_ASN_ENCODING | PKCS_7_ASN_ENCODING,
-          server_cert_nss_->derCert.data,
-          server_cert_nss_->derCert.len,
-          CERT_STORE_ADD_USE_EXISTING,
-          &cert_context);
-      DCHECK(ok);
+      cert_handle = X509Certificate::CreateOSCertHandleFromBytes(
+          reinterpret_cast<char*>(server_cert_nss_->derCert.data),
+          server_cert_nss_->derCert.len);
+      DCHECK(cert_handle);
       server_cert_ = X509Certificate::CreateFromHandle(
-          cert_context,
+          cert_handle,
           X509Certificate::SOURCE_FROM_NETWORK,
           intermediate_ca_certs);
       for (size_t i = 0; i < intermediate_ca_certs.size(); ++i)
-        CertFreeCertificateContext(intermediate_ca_certs[i]);
+        X509Certificate::FreeOSCertHandle(intermediate_ca_certs[i]);
 #else
       server_cert_ = X509Certificate::CreateFromHandle(
           CERT_DupCertificate(server_cert_nss_),
@@ -1215,6 +1204,10 @@ SECStatus SSLClientSocketNSS::ClientAuthHandler(
 
   PCCERT_CHAIN_CONTEXT chain_context = NULL;
 
+  // TODO(wtc): close cert_store_ at shutdown.
+  if (!cert_store_)
+    cert_store_ = CertOpenStore(CERT_STORE_PROV_MEMORY, 0, NULL, 0, NULL);
+
   for (;;) {
     // Find a certificate chain.
     chain_context = CertFindChainInStore(my_cert_store,
@@ -1255,6 +1248,11 @@ SECStatus SSLClientSocketNSS::ClientAuthHandler(
   // Tell NSS to suspend the client authentication.  We will then abort the
   // handshake by returning ERR_SSL_CLIENT_AUTH_CERT_NEEDED.
   return SECWouldBlock;
+#elif defined(OS_MACOSX)
+  // TODO(wtc): see http://crbug.com/45369.
+  // Not implemented.  Send no client certificate.
+  PORT_SetError(PR_NOT_IMPLEMENTED_ERROR);
+  return SECFailure;
 #else
   CERTCertificate* cert = NULL;
   SECKEYPrivateKey* privkey = NULL;

net/socket/ssl_client_socket_nss.h
@@ -165,10 +165,8 @@ class SSLClientSocketNSS : public SSLClientSocket {
   BoundNetLog net_log_;
 
 #if defined(OS_WIN)
-  // A CryptoAPI in-memory certificate store.  We use it for two purposes:
-  // 1. Import server certificates into this store so that we can verify and
-  //    display the certificates using CryptoAPI.
-  // 2. Copy client certificates from the "MY" system certificate store into
+  // A CryptoAPI in-memory certificate store.  We use it for one purpose:
+  // 1. Copy client certificates from the "MY" system certificate store into
   //    this store so that we can close the system store when we finish
   //    searching for client certificates.
   static HCERTSTORE cert_store_;

base/base.gypi
@@ -332,14 +332,7 @@
               'sources/': [ ['exclude', '_openbsd\\.cc$'] ],
             },
           ],
-          [ 'OS == "mac"', {
-              'sources!': [
-                # TODO(wtc): Remove nss_util.{cc,h} when http://crbug.com/30689
-                # is fixed.
-                'nss_util.cc',
-                'nss_util.h',
-              ],
-          }, {  # OS != "mac"
+          [ 'OS != "mac"', {
               'sources!': [
                 'crypto/cssm_init.cc',
                 'crypto/cssm_init.h',
@@ -453,11 +446,12 @@
               ],
             },
         },],
-        [ 'OS == "win"', {
+        [ 'OS == "mac" or OS == "win"', {
             'dependencies': [
               '../third_party/nss/nss.gyp:nss',
             ],
-        }, {  # OS != "win"
+        },],
+        [ 'OS != "win"', {
             'dependencies': ['../third_party/libevent/libevent.gyp:libevent'],
             'sources!': [
               'third_party/purify/pure_api.c',

build/all.gyp
@@ -19,6 +19,7 @@
         '../ipc/ipc.gyp:*',
         '../media/media.gyp:*',
         '../net/net.gyp:*',
+        '../net/third_party/nss/ssl.gyp:*',
         '../printing/printing.gyp:*',
         '../sdch/sdch.gyp:*',
         '../skia/skia.gyp:*',
@@ -58,6 +59,11 @@
             '../third_party/yasm/yasm.gyp:*#host',
            ],
         }],
+        ['OS=="mac" or OS=="win"', {
+          'dependencies': [
+            '../third_party/nss/nss.gyp:*',
+           ],
+        }],
         ['OS=="mac"', {
           'dependencies': [
             '../third_party/ocmock/ocmock.gyp:*',
@@ -81,7 +87,6 @@
           'dependencies': [
             '../tools/gtk_clipboard_dump/gtk_clipboard_dump.gyp:*',
             '../tools/xdisplaycheck/xdisplaycheck.gyp:*',
-            '../net/third_party/nss/nss.gyp:*',
           ],
         }],
         ['OS=="win"', {

build/linux/system.gyp
@@ -76,7 +76,7 @@
           'conditions': [
             ['use_system_ssl==0', {
               'dependencies': [
-                '../../net/third_party/nss/nss.gyp:ssl',
+                '../../net/third_party/nss/ssl.gyp:ssl',
                 '../../third_party/zlib/zlib.gyp:zlib',
               ],
               'direct_dependent_settings': {

net/base/x509_certificate.h
@@ -210,6 +210,16 @@ class X509Certificate : public base::RefCountedThreadSafe<X509Certificate> {
   // Returns true if two OSCertHandles refer to identical certificates.
   static bool IsSameOSCert(OSCertHandle a, OSCertHandle b);
 
+  // Creates an OS certificate handle from the BER-encoded representation.
+  // Returns NULL on failure.
+  static OSCertHandle CreateOSCertHandleFromBytes(const char* data,
+                                                  int length);
+
+  // Duplicates (or adds a reference to) an OS certificate handle.
+  static OSCertHandle DupOSCertHandle(OSCertHandle cert_handle);
+
+  // Frees (or releases a reference to) an OS certificate handle.
+  static void FreeOSCertHandle(OSCertHandle cert_handle);
 
  private:
   friend class base::RefCountedThreadSafe<X509Certificate>;
@@ -253,17 +263,6 @@ class X509Certificate : public base::RefCountedThreadSafe<X509Certificate> {
   void Initialize();
 
   bool VerifyEV() const;
-
-  // Creates an OS certificate handle from the BER-encoded representation.
-  // Returns NULL on failure.
-  static OSCertHandle CreateOSCertHandleFromBytes(const char* data,
-                                                  int length);
-
-  // Duplicates (or adds a reference to) an OS certificate handle.
-  static OSCertHandle DupOSCertHandle(OSCertHandle cert_handle);
-
-  // Frees (or releases a reference to) an OS certificate handle.
-  static void FreeOSCertHandle(OSCertHandle cert_handle);
 
   // Calculates the SHA-1 fingerprint of the certificate.  Returns an empty
   // (all zero) fingerprint on failure.

net/net.gyp
@@ -175,12 +175,20 @@
       ],
       'conditions': [
         [ 'OS == "linux" or OS == "freebsd" or OS == "openbsd"', {
-          'dependencies': [
-            '../build/linux/system.gyp:gconf',
-            '../build/linux/system.gyp:gdk',
-            '../build/linux/system.gyp:nss',
-          ],
-        }],
+            'dependencies': [
+              '../build/linux/system.gyp:gconf',
+              '../build/linux/system.gyp:gdk',
+              '../build/linux/system.gyp:nss',
+            ],
+          },
+          {  # else: OS is not in the above list
+            'sources!': [
+              'base/cert_database_nss.cc',
+              'base/keygen_handler_nss.cc',
+              'base/x509_certificate_nss.cc',
+            ],
+          },
+        ],
         [ 'OS == "win"', {
             'dependencies': [
               # For nss_memio.{c,h}, which require only NSPR.
@@ -194,22 +202,10 @@
             ],
           },
         ],
-        [ 'OS == "linux" or OS == "freebsd" or OS == "openbsd"', {
-          },
-          {  # else: OS is not in the above list
-            'sources!': [
-              'base/cert_database_nss.cc',
-              'base/keygen_handler_nss.cc',
-              'base/x509_certificate_nss.cc',
-            ],
-          },
-        ],
         [ 'OS == "mac"', {
-            'sources!': [
-              # TODO(wtc): Remove nss_memio.{c,h} when http://crbug.com/30689
-              # is fixed.
-              'base/nss_memio.c',
-              'base/nss_memio.h',
+            'dependencies': [
+              # For nss_memio.{c,h}, which require only NSPR.
+              '../third_party/nss/nss.gyp:nspr',
             ],
             'link_settings': {
               'libraries': [
@@ -552,12 +548,23 @@
           ],
         }],
         [ 'OS == "linux" or OS == "freebsd" or OS == "openbsd"', {
-          'dependencies': [
-            '../build/linux/system.gyp:gconf',
-            '../build/linux/system.gyp:gdk',
-            '../build/linux/system.gyp:nss',
-          ],
-        }],
+            'sources!': [
+              'socket/ssl_client_socket_nss_factory.cc',
+              'socket/ssl_client_socket_nss_factory.h',
+            ],
+            'dependencies': [
+              '../build/linux/system.gyp:gconf',
+              '../build/linux/system.gyp:gdk',
+              '../build/linux/system.gyp:nss',
+            ],
+          },
+          {  # else: OS is not in the above list
+            'sources!': [
+              'ocsp/nss_ocsp.cc',
+              'ocsp/nss_ocsp.h',
+            ],
+          },
+        ],
         [ 'OS == "win"', {
             'sources!': [
               'http/http_auth_handler_ntlm_portable.cc',
@@ -565,7 +572,7 @@
             ],
             'dependencies': [
               '../third_party/nss/nss.gyp:nss',
-              'third_party/nss/nss.gyp:ssl',
+              'third_party/nss/ssl.gyp:ssl',
               'tld_cleanup',
             ],
             'link_settings': {
@@ -577,26 +584,13 @@
           {  # else: OS != "win"
             'sources!': [
               'proxy/proxy_resolver_winhttp.cc',
-              'socket/ssl_client_socket_nss_factory.cc',
-              'socket/ssl_client_socket_nss_factory.h',
-            ],
-          },
-        ],
-        [ 'OS == "linux" or OS == "freebsd" or OS == "openbsd"', {
-          },
-          {  # else: OS != "linux"
-            'sources!': [
-              'ocsp/nss_ocsp.cc',
-              'ocsp/nss_ocsp.h',
             ],
           },
         ],
         [ 'OS == "mac"', {
-            'sources!': [
-              # TODO(wtc): Remove ssl_client_socket_nss.{cc,h} when
-              # http://crbug.com/30689 is fixed.
-              'socket/ssl_client_socket_nss.cc',
-              'socket/ssl_client_socket_nss.h',
+            'dependencies': [
+              '../third_party/nss/nss.gyp:nss',
+              'third_party/nss/ssl.gyp:ssl',
             ],
             'link_settings': {
               'libraries': [

net/third_party/nss/ssl.gyp
@@ -1,0 +1,149 @@
+# Copyright (c) 2009 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+{
+  'conditions': [
+    [ 'OS == "linux" or OS == "freebsd" or OS == "openbsd"', {
+      'conditions': [
+        ['sysroot!=""', {
+          'variables': {
+            'pkg-config': '../../../build/linux/pkg-config-wrapper "<(sysroot)"',
+          },
+        }, {
+          'variables': {
+            'pkg-config': 'pkg-config'
+          },
+        }],
+      ],
+    }],
+  ],
+
+  'targets': [
+    {
+      'target_name': 'ssl',
+      'product_name': 'ssl',
+      'type': '<(library)',
+      'sources': [
+        'ssl/authcert.c',
+        'ssl/cmpcert.c',
+        'ssl/derive.c',
+        'ssl/nsskea.c',
+        'ssl/os2_err.c',
+        'ssl/os2_err.h',
+        'ssl/preenc.h',
+        'ssl/prelib.c',
+        'ssl/ssl.h',
+        'ssl/ssl3con.c',
+        'ssl/ssl3ecc.c',
+        'ssl/ssl3ext.c',
+        'ssl/ssl3gthr.c',
+        'ssl/ssl3prot.h',
+        'ssl/sslauth.c',
+        'ssl/sslcon.c',
+        'ssl/ssldef.c',
+        'ssl/sslenum.c',
+        'ssl/sslerr.c',
+        'ssl/sslerr.h',
+        'ssl/sslgathr.c',
+        'ssl/sslimpl.h',
+        'ssl/sslinfo.c',
+        'ssl/sslmutex.c',
+        'ssl/sslmutex.h',
+        'ssl/sslnonce.c',
+        'ssl/sslproto.h',
+        'ssl/sslreveal.c',
+        'ssl/sslsecur.c',
+        'ssl/sslsnce.c',
+        'ssl/sslsock.c',
+        'ssl/sslt.h',
+        'ssl/ssltrace.c',
+        'ssl/sslver.c',
+        'ssl/unix_err.c',
+        'ssl/unix_err.h',
+        'ssl/win32err.c',
+        'ssl/win32err.h',
+        'ssl/bodge/loader.c',
+        'ssl/bodge/loader.h',
+        'ssl/bodge/secure_memcmp.c',
+      ],
+      'sources!': [
+        'ssl/os2_err.c',
+        'ssl/os2_err.h',
+      ],
+      'defines': [
+        'NSS_ENABLE_ECC',
+        'NSS_ENABLE_ZLIB',
+        'USE_UTIL_DIRECTLY',
+      ],
+      'defines!': [
+        # Regrettably, NSS can't be compiled with NO_NSPR_10_SUPPORT yet.
+        'NO_NSPR_10_SUPPORT',
+      ],
+      'conditions': [
+        [ 'OS == "win"', {
+            'sources!': [
+              'ssl/unix_err.c',
+              'ssl/unix_err.h',
+            ],
+          },
+          {  # else: OS != "win"
+            'sources!': [
+              'ssl/win32err.c',
+              'ssl/win32err.h',
+            ],
+          },
+        ],
+        [ 'OS == "linux" or OS == "freebsd" or OS == "openbsd"', {
+          'defines': [
+            # These macros are needed only for compiling the files in
+            # ssl/bodge.
+            'SHLIB_PREFIX="lib"',
+            'SHLIB_SUFFIX="so"',
+            'SHLIB_VERSION="3"',
+            'SOFTOKEN_SHLIB_VERSION="3"',
+          ],
+          'include_dirs': [
+            'ssl/bodge',
+          ],
+          'cflags': [
+            '<!@(<(pkg-config) --cflags nss)',
+          ],
+          'ldflags': [
+            '<!@(<(pkg-config) --libs-only-L --libs-only-other nss)',
+          ],
+          'libraries': [
+            '<!@(<(pkg-config) --libs-only-l nss | sed -e "s/-lssl3//")',
+          ],
+        }],
+        [ 'OS == "mac" or OS == "win"', {
+          'sources/': [
+            ['exclude', 'ssl/bodge/'],
+          ],
+          'dependencies': [
+            '../../../third_party/zlib/zlib.gyp:zlib',
+            '../../../third_party/nss/nss.gyp:nss',
+          ],
+          'direct_dependent_settings': {
+            'include_dirs': [
+              'ssl',
+            ],
+          },
+        }],
+      ],
+      'configurations': {
+        'Debug_Base': {
+          'defines': [
+            'DEBUG',
+          ],
+        },
+      },
+    },
+  ],
+}
+
+# Local Variables:
+# tab-width:2
+# indent-tabs-mode:nil
+# End:
+# vim: set expandtab tabstop=2 shiftwidth=2:

commit 003018e81ebbf5660938dc1b4e6fcb5d43f7d229

Fix a regression on Windows introduced by r48650.
For some reason, passing NULL as the hCertStore argument
to CertAddEncodedCertificateToStore causes CertGetCertificateChain
to fail to build the certificate chain for some servers,
even though the hCertStore member of the server certificate
context should contain its CA certificates.

So I reverted to passing our in-memory certificate store
as the hCertStore argument to CertAddEncodedCertificateToStore.

R=agl,mark
BUG=45706
TEST=Visit https://moversguide.usps.com/ on Windows.  Should
not get the "certificate is not trusted" SSL error page.
Review URL: http://codereview.chromium.org/2605007

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48953 0039d316-1c4b-4281-b951-d872f2087c98

net/socket/ssl_client_socket_nss.cc
@@ -656,6 +656,29 @@ bool SSLClientSocketNSS::SetSendBufferSize(int32 size) {
   return transport_->SetSendBufferSize(size);
 }
 
+#if defined(OS_WIN)
+// static
+X509Certificate::OSCertHandle SSLClientSocketNSS::CreateOSCert(
+    const SECItem& der_cert) {
+  // TODO(wtc): close cert_store_ at shutdown.
+  if (!cert_store_)
+    cert_store_ = CertOpenStore(CERT_STORE_PROV_MEMORY, 0, NULL, 0, NULL);
+
+  X509Certificate::OSCertHandle cert_handle = NULL;
+  BOOL ok = CertAddEncodedCertificateToStore(
+      cert_store_, X509_ASN_ENCODING | PKCS_7_ASN_ENCODING,
+      der_cert.data, der_cert.len, CERT_STORE_ADD_USE_EXISTING, &cert_handle);
+  return ok ? cert_handle : NULL;
+}
+#elif defined(OS_MACOSX)
+// static
+X509Certificate::OSCertHandle SSLClientSocketNSS::CreateOSCert(
+    const SECItem& der_cert) {
+  return X509Certificate::CreateOSCertHandleFromBytes(
+      reinterpret_cast<char*>(der_cert.data), der_cert.len);
+}
+#endif
+
 X509Certificate *SSLClientSocketNSS::UpdateServerCert() {
   // We set the server_cert_ from OwnAuthCertHandler(), but this handler
   // does not necessarily get called if we are continuing a cached SSL
@@ -686,9 +709,7 @@ X509Certificate *SSLClientSocketNSS::UpdateServerCert() {
           if (IsProblematicComodoEVCACert(*node->cert))
             continue;
 #endif
-          cert_handle = X509Certificate::CreateOSCertHandleFromBytes(
-              reinterpret_cast<char*>(node->cert->derCert.data),
-              node->cert->derCert.len);
+          cert_handle = CreateOSCert(node->cert->derCert);
           DCHECK(cert_handle);
           intermediate_ca_certs.push_back(cert_handle);
         }
@@ -696,9 +717,7 @@ X509Certificate *SSLClientSocketNSS::UpdateServerCert() {
       }
 
       // Finally create the X509Certificate object.
-      cert_handle = X509Certificate::CreateOSCertHandleFromBytes(
-          reinterpret_cast<char*>(server_cert_nss_->derCert.data),
-          server_cert_nss_->derCert.len);
+      cert_handle = CreateOSCert(server_cert_nss_->derCert);
       DCHECK(cert_handle);
       server_cert_ = X509Certificate::CreateFromHandle(
           cert_handle,

net/socket/ssl_client_socket_nss.h
@@ -19,13 +19,13 @@
 #include "net/base/net_log.h"
 #include "net/base/nss_memio.h"
 #include "net/base/ssl_config_service.h"
+#include "net/base/x509_certificate.h"
 #include "net/socket/ssl_client_socket.h"
 
 namespace net {
 
 class BoundNetLog;
 class CertVerifier;
-class X509Certificate;
 
 // An SSL client socket implemented with Mozilla NSS.
 class SSLClientSocketNSS : public SSLClientSocket {
@@ -65,6 +65,10 @@ class SSLClientSocketNSS : public SSLClientSocket {
   int InitializeSSLOptions();
 
   void InvalidateSessionIfBadCertificate();
+#if defined(OS_MACOSX) || defined(OS_WIN)
+  // Creates an OS certificate from a DER-encoded certificate.
+  static X509Certificate::OSCertHandle CreateOSCert(const SECItem& der_cert);
+#endif
   X509Certificate* UpdateServerCert();
   void CheckSecureRenegotiation() const;
   void DoReadCallback(int result);
@@ -165,8 +169,10 @@ class SSLClientSocketNSS : public SSLClientSocket {
   BoundNetLog net_log_;
 
 #if defined(OS_WIN)
-  // A CryptoAPI in-memory certificate store.  We use it for one purpose:
-  // 1. Copy client certificates from the "MY" system certificate store into
+  // A CryptoAPI in-memory certificate store.  We use it for two purposes:
+  // 1. Import server certificates into this store so that we can verify and
+  //    display the certificates using CryptoAPI.
+  // 2. Copy client certificates from the "MY" system certificate store into
   //    this store so that we can close the system store when we finish
   //    searching for client certificates.
   static HCERTSTORE cert_store_;