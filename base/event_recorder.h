8e8ced4f373ff01bbe8bb3e00dc9a61e8b74dda4
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-04 16:13:51 +0000

    Use DISALLOW_COPY_AND_ASSIGN instead of DISALLOW_EVIL_CONSTRUCTORS in app and base directories.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/2654001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48931 0039d316-1c4b-4281-b951-d872f2087c98

--- base/event_recorder.h
+++ base/event_recorder.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -94,7 +94,7 @@ class EventRecorder {
   int playback_first_msg_time_;
   int playback_start_time_;
 
-  DISALLOW_EVIL_CONSTRUCTORS(EventRecorder);
+  DISALLOW_COPY_AND_ASSIGN(EventRecorder);
 };
 
 }  // namespace base
