0aa53a7c989b40d624cac5aed5f3a452a3b7dff9
ahendrickson@google.com <ahendrickson@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-10 17:28:05 +0000

    Removed gssapi namespace.
    BUG=33033.
    TEST=None.
    
    Review URL: http://codereview.chromium.org/2682006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49414 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_auth_gssapi_posix.h
+++ net/http/http_auth_gssapi_posix.h
@@ -21,6 +21,9 @@ namespace net {
 class HttpRequestInfo;
 class ProxyInfo;
 
+extern gss_OID CHROME_GSS_C_NT_HOSTBASED_SERVICE_X;
+extern gss_OID CHROME_GSS_C_NT_HOSTBASED_SERVICE;
+
 // GSSAPILibrary is introduced so unit tests can mock the calls to the GSSAPI
 // library. The default implementation attempts to load one of the standard
 // GSSAPI library implementations, then simply passes the arguments on to
@@ -33,45 +36,45 @@ class GSSAPILibrary {
   virtual bool Init() = 0;
 
   // These methods match the ones in the GSSAPI library.
-  virtual gssapi::OM_uint32 import_name(
-      gssapi::OM_uint32* minor_status,
-      const gssapi::gss_buffer_t input_name_buffer,
-      const gssapi::gss_OID input_name_type,
-      gssapi::gss_name_t* output_name) = 0;
-  virtual gssapi::OM_uint32 release_name(
-      gssapi::OM_uint32* minor_status,
-      gssapi::gss_name_t* input_name) = 0;
-  virtual gssapi::OM_uint32 release_buffer(
-      gssapi::OM_uint32* minor_status,
-      gssapi::gss_buffer_t buffer) = 0;
-  virtual gssapi::OM_uint32 display_status(
-      gssapi::OM_uint32* minor_status,
-      gssapi::OM_uint32 status_value,
+  virtual OM_uint32 import_name(
+      OM_uint32* minor_status,
+      const gss_buffer_t input_name_buffer,
+      const gss_OID input_name_type,
+      gss_name_t* output_name) = 0;
+  virtual OM_uint32 release_name(
+      OM_uint32* minor_status,
+      gss_name_t* input_name) = 0;
+  virtual OM_uint32 release_buffer(
+      OM_uint32* minor_status,
+      gss_buffer_t buffer) = 0;
+  virtual OM_uint32 display_status(
+      OM_uint32* minor_status,
+      OM_uint32 status_value,
       int status_type,
-      const gssapi::gss_OID mech_type,
-      gssapi::OM_uint32* message_contex,
-      gssapi::gss_buffer_t status_string) = 0;
-  virtual gssapi::OM_uint32 init_sec_context(
-      gssapi::OM_uint32* minor_status,
-      const gssapi::gss_cred_id_t initiator_cred_handle,
-      gssapi::gss_ctx_id_t* context_handle,
-      const gssapi::gss_name_t target_name,
-      const gssapi::gss_OID mech_type,
-      gssapi::OM_uint32 req_flags,
-      gssapi::OM_uint32 time_req,
-      const gssapi::gss_channel_bindings_t input_chan_bindings,
-      const gssapi::gss_buffer_t input_token,
-      gssapi::gss_OID* actual_mech_type,
-      gssapi::gss_buffer_t output_token,
-      gssapi::OM_uint32* ret_flags,
-      gssapi::OM_uint32* time_rec) = 0;
-  virtual gssapi::OM_uint32 wrap_size_limit(
-      gssapi::OM_uint32* minor_status,
-      const gssapi::gss_ctx_id_t context_handle,
+      const gss_OID mech_type,
+      OM_uint32* message_contex,
+      gss_buffer_t status_string) = 0;
+  virtual OM_uint32 init_sec_context(
+      OM_uint32* minor_status,
+      const gss_cred_id_t initiator_cred_handle,
+      gss_ctx_id_t* context_handle,
+      const gss_name_t target_name,
+      const gss_OID mech_type,
+      OM_uint32 req_flags,
+      OM_uint32 time_req,
+      const gss_channel_bindings_t input_chan_bindings,
+      const gss_buffer_t input_token,
+      gss_OID* actual_mech_type,
+      gss_buffer_t output_token,
+      OM_uint32* ret_flags,
+      OM_uint32* time_rec) = 0;
+  virtual OM_uint32 wrap_size_limit(
+      OM_uint32* minor_status,
+      const gss_ctx_id_t context_handle,
       int conf_req_flag,
-      gssapi::gss_qop_t qop_req,
-      gssapi::OM_uint32 req_output_size,
-      gssapi::OM_uint32* max_input_size) = 0;
+      gss_qop_t qop_req,
+      OM_uint32 req_output_size,
+      OM_uint32* max_input_size) = 0;
 
   // Get the default GSSPILibrary instance. The object returned is a singleton
   // instance, and the caller should not delete it.
@@ -86,45 +89,45 @@ class GSSAPISharedLibrary : public GSSAPILibrary {
 
   // GSSAPILibrary methods:
   virtual bool Init();
-  virtual gssapi::OM_uint32 import_name(
-      gssapi::OM_uint32* minor_status,
-      const gssapi::gss_buffer_t input_name_buffer,
-      const gssapi::gss_OID input_name_type,
-      gssapi::gss_name_t* output_name);
-  virtual gssapi::OM_uint32 release_name(
-      gssapi::OM_uint32* minor_status,
-      gssapi::gss_name_t* input_name);
-  virtual gssapi::OM_uint32 release_buffer(
-      gssapi::OM_uint32* minor_status,
-      gssapi::gss_buffer_t buffer);
-  virtual gssapi::OM_uint32 display_status(
-      gssapi::OM_uint32* minor_status,
-      gssapi::OM_uint32 status_value,
+  virtual OM_uint32 import_name(
+      OM_uint32* minor_status,
+      const gss_buffer_t input_name_buffer,
+      const gss_OID input_name_type,
+      gss_name_t* output_name);
+  virtual OM_uint32 release_name(
+      OM_uint32* minor_status,
+      gss_name_t* input_name);
+  virtual OM_uint32 release_buffer(
+      OM_uint32* minor_status,
+      gss_buffer_t buffer);
+  virtual OM_uint32 display_status(
+      OM_uint32* minor_status,
+      OM_uint32 status_value,
       int status_type,
-      const gssapi::gss_OID mech_type,
-      gssapi::OM_uint32* message_contex,
-      gssapi::gss_buffer_t status_string);
-  virtual gssapi::OM_uint32 init_sec_context(
-      gssapi::OM_uint32* minor_status,
-      const gssapi::gss_cred_id_t initiator_cred_handle,
-      gssapi::gss_ctx_id_t* context_handle,
-      const gssapi::gss_name_t target_name,
-      const gssapi::gss_OID mech_type,
-      gssapi::OM_uint32 req_flags,
-      gssapi::OM_uint32 time_req,
-      const gssapi::gss_channel_bindings_t input_chan_bindings,
-      const gssapi::gss_buffer_t input_token,
-      gssapi::gss_OID* actual_mech_type,
-      gssapi::gss_buffer_t output_token,
-      gssapi::OM_uint32* ret_flags,
-      gssapi::OM_uint32* time_rec);
-  virtual gssapi::OM_uint32 wrap_size_limit(
-      gssapi::OM_uint32* minor_status,
-      const gssapi::gss_ctx_id_t context_handle,
+      const gss_OID mech_type,
+      OM_uint32* message_contex,
+      gss_buffer_t status_string);
+  virtual OM_uint32 init_sec_context(
+      OM_uint32* minor_status,
+      const gss_cred_id_t initiator_cred_handle,
+      gss_ctx_id_t* context_handle,
+      const gss_name_t target_name,
+      const gss_OID mech_type,
+      OM_uint32 req_flags,
+      OM_uint32 time_req,
+      const gss_channel_bindings_t input_chan_bindings,
+      const gss_buffer_t input_token,
+      gss_OID* actual_mech_type,
+      gss_buffer_t output_token,
+      OM_uint32* ret_flags,
+      OM_uint32* time_rec);
+  virtual OM_uint32 wrap_size_limit(
+      OM_uint32* minor_status,
+      const gss_ctx_id_t context_handle,
       int conf_req_flag,
-      gssapi::gss_qop_t qop_req,
-      gssapi::OM_uint32 req_output_size,
-      gssapi::OM_uint32* max_input_size);
+      gss_qop_t qop_req,
+      OM_uint32 req_output_size,
+      OM_uint32* max_input_size);
 
  private:
   FRIEND_TEST_ALL_PREFIXES(HttpAuthGSSAPIPOSIXTest, GSSAPIStartup);
@@ -139,12 +142,12 @@ class GSSAPISharedLibrary : public GSSAPILibrary {
   base::NativeLibrary gssapi_library_;
 
   // Function pointers
-  gssapi::gss_import_name_type import_name_;
-  gssapi::gss_release_name_type release_name_;
-  gssapi::gss_release_buffer_type release_buffer_;
-  gssapi::gss_display_status_type display_status_;
-  gssapi::gss_init_sec_context_type init_sec_context_;
-  gssapi::gss_wrap_size_limit_type wrap_size_limit_;
+  gss_import_name_type import_name_;
+  gss_release_name_type release_name_;
+  gss_release_buffer_type release_buffer_;
+  gss_display_status_type display_status_;
+  gss_init_sec_context_type init_sec_context_;
+  gss_wrap_size_limit_type wrap_size_limit_;
 };
 
 // TODO(cbentzel): Share code with HttpAuthSSPI.
@@ -152,7 +155,7 @@ class HttpAuthGSSAPI {
  public:
   HttpAuthGSSAPI(GSSAPILibrary* library,
                  const std::string& scheme,
-                 const gssapi::gss_OID gss_oid);
+                 const gss_OID gss_oid);
   ~HttpAuthGSSAPI();
 
   bool NeedsIdentity() const;
@@ -179,16 +182,16 @@ class HttpAuthGSSAPI {
   int OnFirstRound(const std::wstring* username,
                    const std::wstring* password);
   int GetNextSecurityToken(const std::wstring& spn,
-                           gssapi::gss_buffer_t in_token,
-                           gssapi::gss_buffer_t out_token);
+                           gss_buffer_t in_token,
+                           gss_buffer_t out_token);
 
   std::string scheme_;
   std::wstring username_;
   std::wstring password_;
-  gssapi::gss_OID gss_oid_;
+  gss_OID gss_oid_;
   GSSAPILibrary* library_;
   std::string decoded_server_auth_token_;
-  gssapi::gss_ctx_id_t sec_context_;
+  gss_ctx_id_t sec_context_;
 };
 
 }  // namespace net
