276a2786ebd73d891b216466d705dc1febc06361
huanr@chromium.org <huanr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-14 22:27:44 +0000

    Fix a regression that accesses file system synchronously in URLRequestFileJob::Factory.
    This regression came from revision 5944. In addition to
    performance penalty, the change in 5944 introduced a new
    issue: the FileJob -> FileDirJob redirection code was
    not hit for directory path without trailing slash. Revision
    5999 tried to fix that with a questionable approach. 
    
    The meat of this change is reverting the relevant code in
    5944 and 5999. Due to the reverting, this change also
    need to have the following code changes:
    
    (1) revision 10408 fixes a bug on "file:///". We don't
    regress the fix but the scenario goes through a different
    code part. Some comments are changed accordingly and a
    unit test is added.
    
    (2) revision 23944 fixes a related crash. The code change
    in 23944 is still valid but the unit test needs to be
    rewritten.
    
    
    BUG=43116,1474,18686
    TEST=URLRequestTest.FileDir*
    
    Review URL: http://codereview.chromium.org/2748004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49737 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_unittest.cc
+++ net/url_request/url_request_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2006-2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -962,25 +962,28 @@ TEST_F(URLRequestTest, FileDirRedirectNoCrash) {
   path = path.Append(FILE_PATH_LITERAL("url_request_unittest"));
 
   TestDelegate d;
-  d.set_quit_on_redirect(true);
   TestURLRequest req(net::FilePathToFileURL(path), &d);
   req.Start();
   MessageLoop::current()->Run();
 
-  // Let the directory lister have time to finish its work, which will
-  // cause the URLRequestFileDirJob's ref count to drop to 1.
-  URLRequestFileDirJob* job = static_cast<URLRequestFileDirJob*>(req.job());
-  while (!job->list_complete()) {
-    PlatformThread::Sleep(10);
-    MessageLoop::current()->RunAllPending();
-  }
-
-  // Should not crash during this call!
-  req.FollowDeferredRedirect();
-
-  // Flush event queue.
-  MessageLoop::current()->RunAllPending();
+  ASSERT_EQ(1, d.received_redirect_count());
+  ASSERT_LT(0, d.bytes_received());
+  ASSERT_FALSE(d.request_failed());
+  ASSERT_TRUE(req.status().is_success());
 }
+
+#if defined(OS_WIN)
+// Don't accept the url "file:///" on windows. See http://crbug.com/1474.
+TEST_F(URLRequestTest, FileDirRedirectSingleSlash) {
+  TestDelegate d;
+  TestURLRequest req(GURL("file:///"), &d);
+  req.Start();
+  MessageLoop::current()->Run();
+
+  ASSERT_EQ(1, d.received_redirect_count());
+  ASSERT_FALSE(req.status().is_success());
+}
+#endif
 
 TEST_F(URLRequestTestHTTP, RestrictRedirects) {
   ASSERT_TRUE(NULL != server_.get());
