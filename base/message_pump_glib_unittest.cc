c7bb7e69873b03e44495c256d867911d0e509cee
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-16 02:17:23 +0000

    Add DISABLE_RUNNABLE_METHOD_REFCOUNT to make disabling refcounts in RunnableMethods easier.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/2830006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49887 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_pump_glib_unittest.cc
+++ base/message_pump_glib_unittest.cc
@@ -182,12 +182,7 @@ class MessagePumpGLibTest : public testing::Test {
 }  // namespace
 
 // EventInjector is expected to always live longer than the runnable methods.
-// This lets us call NewRunnableMethod on EventInjector instances.
-template<>
-struct RunnableMethodTraits<EventInjector> {
-  void RetainCallee(EventInjector* obj) { }
-  void ReleaseCallee(EventInjector* obj) { }
-};
+DISABLE_RUNNABLE_METHOD_REFCOUNT(EventInjector);
 
 TEST_F(MessagePumpGLibTest, TestQuit) {
   // Checks that Quit works and that the basic infrastructure is working.
