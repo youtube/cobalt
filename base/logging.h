811e2256de0efd860cdd34f1babe601d9b6e6a84
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-16 18:39:53 +0000

    Revert 49982 - patch
    TBR=thakis@chromium.org
    Review URL: http://codereview.chromium.org/2825006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50002 0039d316-1c4b-4281-b951-d872f2087c98

--- base/logging.h
+++ base/logging.h
@@ -101,18 +101,6 @@
 // There is also the special severity of DFATAL, which logs FATAL in
 // debug mode, ERROR_REPORT in normal mode.
 
-// XXX better comment -- must be before we use << and in global namespace
-// These functions are provided as a convenience for logging, which is where we
-// use streams (it is against Google style to use streams in other places). It
-// is designed to allow you to emit non-ASCII Unicode strings to the log file,
-// which is normally ASCII. It is relatively slow, so try not to use it for
-// common cases. Non-ASCII characters will be converted to UTF-8 by these
-// operators.
-std::ostream& operator<<(std::ostream& out, const wchar_t* wstr);
-inline std::ostream& operator<<(std::ostream& out, const std::wstring& wstr) {
-  return out << wstr.c_str();
-}
-
 namespace logging {
 
 // Where to record logging output? A flat file and/or system debug log via
@@ -824,6 +812,17 @@ void RawLog(int level, const char* message);
   } while (0)
 
 }  // namespace logging
+
+// These functions are provided as a convenience for logging, which is where we
+// use streams (it is against Google style to use streams in other places). It
+// is designed to allow you to emit non-ASCII Unicode strings to the log file,
+// which is normally ASCII. It is relatively slow, so try not to use it for
+// common cases. Non-ASCII characters will be converted to UTF-8 by these
+// operators.
+std::ostream& operator<<(std::ostream& out, const wchar_t* wstr);
+inline std::ostream& operator<<(std::ostream& out, const std::wstring& wstr) {
+  return out << wstr.c_str();
+}
 
 // The NOTIMPLEMENTED() macro annotates codepaths which have
 // not been implemented yet.
