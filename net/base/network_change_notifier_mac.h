b9bd23b856a6134212bf13080caf46eb8dcb712f
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-17 21:24:02 +0000

    Implement a TODO: Move "NonThreadSafe" inheritance from NetworkChangeNotifier interface to implementations.  Also add CalledOnValidThread() DCHECKs in as many places as possible.
    Some minor style changes.
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/2813019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50139 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/network_change_notifier_mac.h
+++ net/base/network_change_notifier_mac.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,6 +6,7 @@
 #define NET_BASE_NETWORK_CHANGE_NOTIFIER_MAC_H_
 
 #include "base/basictypes.h"
+#include "base/non_thread_safe.h"
 #include "base/observer_list.h"
 #include "base/ref_counted.h"
 #include "base/scoped_ptr.h"
@@ -19,27 +20,20 @@ class Thread;
 
 namespace net {
 
-class NetworkChangeNotifierMac : public NetworkChangeNotifier {
+class NetworkChangeNotifierMac : public NetworkChangeNotifier,
+                                 public NonThreadSafe {
  public:
   NetworkChangeNotifierMac();
 
-  void OnIPAddressChanged() {
-    FOR_EACH_OBSERVER(Observer, observers_, OnIPAddressChanged());
-  }
+  void OnIPAddressChanged();
 
   // NetworkChangeNotifier methods:
-
-  virtual void AddObserver(Observer* observer) {
-    observers_.AddObserver(observer);
-  }
-
-  virtual void RemoveObserver(Observer* observer) {
-    observers_.RemoveObserver(observer);
-  }
+  virtual void AddObserver(Observer* observer);
+  virtual void RemoveObserver(Observer* observer);
 
  private:
   friend class base::RefCounted<NetworkChangeNotifierMac>;
-  
+
   virtual ~NetworkChangeNotifierMac();
 
   // Initializes the notifier thread.  The SystemConfiguration calls in this
