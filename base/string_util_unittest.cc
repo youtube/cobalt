2730b92c2234f44e905e8d16b54f9d0c72124ad3
jerrica@google.com <jerrica@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-17 16:20:33 +0000

    Made changes to TruncateUtF8ToByteSize to cleanup the code.
    I made some corrections to my previously submitted function 
    TruncateUTF8ToByteSize, in order to clean up the code, 
    and use it to truncate sync node titles above 255 bytes. 
    BUG=43675
    TEST=base/string_util_unittest.cc
    Review URL: http://codereview.chromium.org/2632005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50112 0039d316-1c4b-4281-b951-d872f2087c98

--- base/string_util_unittest.cc
+++ base/string_util_unittest.cc
@@ -85,7 +85,7 @@ bool Truncated(const std::string& input, const size_t byte_size,
                std::string* output) {
     size_t prev = input.length();
     TruncateUTF8ToByteSize(input, byte_size, output);
-    return prev != (*output).length();
+    return prev != output->length();
 }
 
 }  // namespace
