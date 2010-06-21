6e1e5ee8a738da2d775f4daafe66586f6f202f39
ziadh@chromium.org <ziadh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-21 18:46:00 +0000

    A/B test for determining a value for unused socket timeout.  Currently the timeout defaults to 10 seconds.  Having this value set too low won't allow us to take advantage of idle sockets.  Setting it to too high could possibly result in more ERR_CONNECT_RESETs, requiring one RTT to receive the RST packet and possibly another RTT to re-establish the connection.
    r=jar
    
    
    Review URL: http://codereview.chromium.org/2827016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50364 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/socket/client_socket_pool.cc
@@ -0,0 +1,29 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/socket/client_socket_pool.h"
+
+namespace {
+
+// The maximum duration, in seconds, to keep used idle persistent sockets
+// alive.
+// TODO(ziadh): Change this timeout after getting histogram data on how long it
+// should be.
+int g_unused_idle_socket_timeout = 10;
+
+}  // namespace
+
+namespace net {
+
+// static
+int ClientSocketPool::unused_idle_socket_timeout() {
+  return g_unused_idle_socket_timeout;
+}
+
+// static
+void ClientSocketPool::set_unused_idle_socket_timeout(int timeout) {
+  g_unused_idle_socket_timeout = timeout;
+}
+
+}  // namespace net
--- net/socket/socks_client_socket_pool.cc
+++ net/socket/socks_client_socket_pool.cc
@@ -166,7 +166,8 @@ SOCKSClientSocketPool::SOCKSClientSocketPool(
     NetworkChangeNotifier* network_change_notifier,
     NetLog* net_log)
     : base_(max_sockets, max_sockets_per_group, histograms,
-            base::TimeDelta::FromSeconds(kUnusedIdleSocketTimeout),
+            base::TimeDelta::FromSeconds(
+                ClientSocketPool::unused_idle_socket_timeout()),
             base::TimeDelta::FromSeconds(kUsedIdleSocketTimeout),
             new SOCKSConnectJobFactory(tcp_pool, host_resolver, net_log),
             network_change_notifier) {}
