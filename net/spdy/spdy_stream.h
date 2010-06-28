edca20c06a93aabeafa4525238c2902c7436af34
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-28 19:08:34 +0000

    Revert "Streams send a Rst frame upon being closed by client. Some minor editorial fixes."
    This reverts commit 699468c59a6563db5fb3fb4739c6710a279a4067.
    
    TBR=erikchen@google.com
    
    Review URL: http://codereview.chromium.org/2852029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51011 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_stream.h
+++ net/spdy/spdy_stream.h
@@ -109,7 +109,8 @@ class SpdyStream : public base::RefCounted<SpdyStream> {
   void SetRequestTime(base::Time t);
 
   // Called by the SpdySession when a response (e.g. a SYN_REPLY) has been
-  // received for this stream.
+  // received for this stream.  |path| is the path of the URL for a server
+  // initiated stream, otherwise is empty.
   // Returns a status code.
   int OnResponseReceived(const spdy::SpdyHeaderBlock& response);
 
@@ -119,7 +120,7 @@ class SpdyStream : public base::RefCounted<SpdyStream> {
   // |buffer| contains the data received.  The stream must copy any data
   //          from this buffer before returning from this callback.
   // |length| is the number of bytes received or an error.
-  //          A length of zero indicates end-of-stream.
+  //         A zero-length count does not indicate end-of-stream.
   void OnDataReceived(const char* buffer, int bytes);
 
   // Called by the SpdySession when a write has completed.  This callback
@@ -158,20 +159,6 @@ class SpdyStream : public base::RefCounted<SpdyStream> {
   bool is_idle() const { return io_state_ == STATE_NONE; }
   bool response_complete() const { return response_complete_; }
   int response_status() const { return response_status_; }
-
-  // If this function returns true, then the spdy_session is guaranteeing that
-  // this spdy_stream has been removed from active_streams.
-  bool half_closed_client_side() const { return half_closed_client_side_; }
-
-  bool half_closed_server_side() const { return half_closed_server_side_; }
-  bool half_closed_both_sides() const {
-    return half_closed_client_side_ && half_closed_server_side_;
-  }
-
-  // These two functions should only ever be called by spdy_session, and should
-  // only be called once.
-  void HalfCloseClientSide() { half_closed_client_side_ = true; }
-  void HalfCloseServerSide() { half_closed_server_side_ = true; }
 
  private:
   enum State {
@@ -248,8 +235,6 @@ class SpdyStream : public base::RefCounted<SpdyStream> {
   bool histograms_recorded_;
   // Data received before delegate is attached.
   std::vector<scoped_refptr<IOBufferWithSize> > pending_buffers_;
-  bool half_closed_client_side_;
-  bool half_closed_server_side_;
 
   DISALLOW_COPY_AND_ASSIGN(SpdyStream);
 };
