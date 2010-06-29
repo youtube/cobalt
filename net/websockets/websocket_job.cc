37cdbd92e890f1730d8e748a9613bfa5a959a910
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-29 09:00:49 +0000

    Ignore OnConnected event if the job state is already CLOSED.
    There is a case while SocketStream is connecting, WebSocketJob is closed and SocketStream notifies it is connected.  In this case, we should just ignore OnConnected event.
    
    BUG=47818
    TEST=websocket/tests/url-with-credential.html
    
    Review URL: http://codereview.chromium.org/2823034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51108 0039d316-1c4b-4281-b951-d872f2087c98

--- net/websockets/websocket_job.cc
+++ net/websockets/websocket_job.cc
@@ -173,6 +173,8 @@ int WebSocketJob::OnStartOpenConnection(
 
 void WebSocketJob::OnConnected(
     SocketStream* socket, int max_pending_send_allowed) {
+  if (state_ == CLOSED)
+    return;
   DCHECK_EQ(CONNECTING, state_);
   if (delegate_)
     delegate_->OnConnected(socket, max_pending_send_allowed);
