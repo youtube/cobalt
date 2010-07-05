afd971a323b475a9b8ee75b48421a393423f2fe4
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-05 11:32:40 +0000

    Brushed up listen socket: - Upstreamed support for partial results from devtools' version - Made DidRead receive data and length (in order to support websockets data) - Fixed all the clients. Added net/server with http socket implementation that supports websockets. Will remove net/tools fetch client and server later.
    Review URL: http://codereview.chromium.org/2868036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51635 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/server/http_server_request_info.h
@@ -0,0 +1,34 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_SERVER_HTTP_SERVER_REQUEST_INFO_H_
+#define NET_SERVER_HTTP_SERVER_REQUEST_INFO_H_
+
+#include <string>
+
+#include "net/http/http_request_info.h"
+
+// Meta information about an HTTP request.
+// This is geared toward servers in that it keeps a map of the headers and
+// values rather than just a list of header strings (which net::HttpRequestInfo
+// does).
+class HttpServerRequestInfo {
+ public:
+  HttpServerRequestInfo() {}
+
+  // Request method.
+  std::string method;
+
+  // Request line.
+  std::string path;
+
+  // Request data.
+  std::string data;
+
+  // A map of the names -> values for HTTP headers.
+  typedef std::map<std::string, std::string> HeadersMap;
+  HeadersMap headers;
+};
+
+#endif  // NET_SERVER_HTTP_SERVER_REQUEST_INFO_H_
