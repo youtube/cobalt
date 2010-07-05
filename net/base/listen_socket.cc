5db62827c6150d77700547119b3ab0a0fcaa2756
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-05 11:32:40 +0000

    Brushed up listen socket: - Upstreamed support for partial results from devtools' version - Made DidRead receive data and length (in order to support websockets data) - Fixed all the clients. Added net/server with http socket implementation that supports websockets. Will remove net/tools fetch client and server later.
    Review URL: http://codereview.chromium.org/2868036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51635 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/listen_socket.cc
+++ net/base/listen_socket.cc
@@ -31,7 +31,7 @@ typedef int socklen_t;
 
 namespace {
 
-const int kReadBufSize = 200;
+const int kReadBufSize = 4096;
 
 }  // namespace
 
@@ -160,7 +160,7 @@ void ListenSocket::Read() {
       // TODO(ibrar): maybe change DidRead to take a length instead
       DCHECK(len > 0 && len <= kReadBufSize);
       buf[len] = 0;  // already create a buffer with +1 length
-      socket_delegate_->DidRead(this, buf);
+      socket_delegate_->DidRead(this, buf, len);
     }
   } while (len == kReadBufSize);
 }
@@ -206,19 +206,32 @@ void ListenSocket::WatchSocket(WaitState state) {
 }
 
 void ListenSocket::SendInternal(const char* bytes, int len) {
-  int sent = HANDLE_EINTR(send(socket_, bytes, len, 0));
-  if (sent == kSocketError) {
-#if defined(OS_WIN)
-  int err = WSAGetLastError();
-  if (err == WSAEWOULDBLOCK) {
-#elif defined(OS_POSIX)
-    if (errno == EWOULDBLOCK || errno == EAGAIN) {
-#endif
-    // TODO(ibrar): there should be logic here to handle this because
-    // it is not an error
+  char* send_buf = const_cast<char *>(bytes);
+  int len_left = len;
+  while (true) {
+    int sent = HANDLE_EINTR(send(socket_, send_buf, len_left, 0));
+    if (sent == len_left) {  // A shortcut to avoid extraneous checks.
+      break;
     }
-  } else if (sent != len) {
-    LOG(ERROR) << "send failed: ";
+    if (sent == kSocketError) {
+#if defined(OS_WIN)
+      if (WSAGetLastError() != WSAEWOULDBLOCK) {
+        LOG(ERROR) << "send failed: WSAGetLastError()==" << WSAGetLastError();
+#elif defined(OS_POSIX)
+      if (errno != EWOULDBLOCK && errno != EAGAIN) {
+        LOG(ERROR) << "send failed: errno==" << errno;
+#endif
+        break;
+      }
+      // Otherwise we would block, and now we have to wait for a retry.
+      // Fall through to PlatformThread::YieldCurrentThread()
+    } else {
+      // sent != len_left according to the shortcut above.
+      // Shift the buffer start and send the remainder after a short while.
+      send_buf += sent;
+      len_left -= sent;
+    }
+    PlatformThread::YieldCurrentThread();
   }
 }
 
