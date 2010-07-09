70a8ca5b518f2c35202be33c691d875b3a2524ec
satish@chromium.org <satish@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-09 23:24:12 +0000

    Renamed AudioController to AudioOutputController. This is in preparation to add an AudioInputController for audio recording in subsequent changes. BUG=none TEST=no functional change
    Review URL: http://codereview.chromium.org/2964005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52015 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ media/audio/audio_output_controller.h
@@ -0,0 +1,214 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_AUDIO_AUDIO_OUTPUT_CONTROLLER_H_
+#define MEDIA_AUDIO_AUDIO_OUTPUT_CONTROLLER_H_
+
+#include "base/lock.h"
+#include "base/ref_counted.h"
+#include "base/scoped_ptr.h"
+#include "base/thread.h"
+#include "base/time.h"
+#include "media/audio/audio_output.h"
+#include "media/audio/simple_sources.h"
+
+// An AudioOutputController controls an AudioOutputStream and provides data
+// to this output stream. It has an important function that it executes
+// audio operations like play, pause, stop, etc. on a separate thread,
+// namely the audio controller thread.
+//
+// All the public methods of AudioOutputController are non-blocking except
+// close, the actual operations are performed on the audio controller thread.
+//
+// Here is a state diagram for the AudioOutputController:
+//
+//                    .---->  [ Closed / Error ]  <------.
+//                    |                ^                 |
+//                    |                |                 |
+//               [ Created ]  -->  [ Playing ]  -->  [ Paused ]
+//                    ^                ^                 |
+//                    |                |                 |
+//              *[  Empty  ]           `-----------------'
+//
+// * Initial state
+//
+// There are two modes of buffering operations supported by this class.
+//
+// Regular latency mode:
+//   In this mode we receive signals from AudioOutputController and then we
+//   enqueue data into it.
+//
+// Low latency mode:
+//   In this mode a DataSource object is given to the AudioOutputController
+//   and AudioOutputController reads from it synchronously.
+//
+namespace media {
+
+class AudioOutputController
+    : public base::RefCountedThreadSafe<AudioOutputController>,
+      public AudioOutputStream::AudioSourceCallback {
+ public:
+  // Internal state of the source.
+  enum State {
+    kEmpty,
+    kCreated,
+    kPlaying,
+    kPaused,
+    kClosed,
+    kError,
+  };
+
+  // An event handler that receives events from the AudioOutputController. The
+  // following methods are called on the audio controller thread.
+  class EventHandler {
+   public:
+    virtual ~EventHandler() {}
+    virtual void OnCreated(AudioOutputController* controller) = 0;
+    virtual void OnPlaying(AudioOutputController* controller) = 0;
+    virtual void OnPaused(AudioOutputController* controller) = 0;
+    virtual void OnError(AudioOutputController* controller, int error_code) = 0;
+
+    // Audio controller asks for more data.
+    // |pending_bytes| is the number of bytes still on the controller.
+    // |timestamp| is then time when |pending_bytes| is recorded.
+    virtual void OnMoreData(AudioOutputController* controller,
+                            base::Time timestamp,
+                            uint32 pending_bytes) = 0;
+  };
+
+  // A synchronous reader interface used by AudioOutputController for
+  // synchronous reading.
+  class SyncReader {
+   public:
+    virtual ~SyncReader() {}
+
+    // Notify the synchronous reader the number of bytes in the
+    // AudioOutputController not yet played. This is used by SyncReader to
+    // prepare more data and perform synchronization.
+    virtual void UpdatePendingBytes(uint32 bytes) = 0;
+
+    // Read certain amount of data into |data|. This method returns if some
+    // data is available.
+    virtual uint32 Read(void* data, uint32 size) = 0;
+
+    // Close this synchronous reader.
+    virtual void Close() = 0;
+  };
+
+  virtual ~AudioOutputController();
+
+  // Factory method for creating an AudioOutputController.
+  // If successful, an audio controller thread is created. The audio device
+  // will be created on the audio controller thread and when that is done
+  // event handler will receive a OnCreated() call.
+  static scoped_refptr<AudioOutputController> Create(
+      EventHandler* event_handler,
+      AudioManager::Format format,    // Format of the stream.
+      int channels,                   // Number of channels.
+      int sample_rate,                // Sampling frequency/rate.
+      int bits_per_sample,            // Number of bits per sample.
+      uint32 hardware_buffer_size,    // Size of the hardware buffer.
+
+      // Soft limit for buffer capacity in this controller. This parameter
+      // is used only in regular latency mode.
+      uint32 buffer_capacity);
+
+  // Factory method for creating a low latency audio stream.
+  static scoped_refptr<AudioOutputController> CreateLowLatency(
+      EventHandler* event_handler,
+      AudioManager::Format format,    // Format of the stream.
+      int channels,                   // Number of channels.
+      int sample_rate,                // Sampling frequency/rate.
+      int bits_per_sample,            // Number of bits per sample.
+      uint32 hardware_buffer_size,    // Size of the hardware buffer.
+
+      // External synchronous reader for audio controller.
+      SyncReader* sync_reader);
+
+  // Methods to control playback of the stream.
+
+  // Starts the playback of this audio output stream.
+  void Play();
+
+  // Pause this audio output stream.
+  void Pause();
+
+  // Discard all audio data buffered in this output stream. This method only
+  // has effect when the stream is paused.
+  void Flush();
+
+  // Closes the audio output stream and shutdown the audio controller thread.
+  // This method returns only after all operations are completed. This
+  // controller cannot be used after this method is called.
+  //
+  // It is safe to call this method more than once. Calls after the first one
+  // will have no effect.
+  void Close();
+
+  // Sets the volume of the audio output stream.
+  void SetVolume(double volume);
+
+  // Enqueue audio |data| into the controller. This method is used only in
+  // the regular latency mode and it is illegal to call this method when
+  // SyncReader is present.
+  void EnqueueData(const uint8* data, uint32 size);
+
+  bool LowLatencyMode() const { return sync_reader_ != NULL; }
+
+  ///////////////////////////////////////////////////////////////////////////
+  // AudioSourceCallback methods.
+  virtual uint32 OnMoreData(AudioOutputStream* stream, void* dest,
+                            uint32 max_size, uint32 pending_bytes);
+  virtual void OnClose(AudioOutputStream* stream);
+  virtual void OnError(AudioOutputStream* stream, int code);
+
+ private:
+  AudioOutputController(EventHandler* handler,
+                        uint32 capacity, SyncReader* sync_reader);
+
+  // The following methods are executed on the audio controller thread.
+  void DoCreate(AudioManager::Format format, int channels,
+                int sample_rate, int bits_per_sample,
+                uint32 hardware_buffer_size);
+  void DoPlay();
+  void DoPause();
+  void DoFlush();
+  void DoClose();
+  void DoSetVolume(double volume);
+  void DoReportError(int code);
+
+  // Helper method to submit a OnMoreData() call to the event handler.
+  void SubmitOnMoreData_Locked();
+
+  EventHandler* handler_;
+  AudioOutputStream* stream_;
+
+  // The current volume of the audio stream.
+  double volume_;
+
+  // |state_| is written on the audio controller thread and is read on the
+  // hardware audio thread. These operations need to be locked. But lock
+  // is not required for reading on the audio controller thread.
+  State state_;
+
+  uint32 hardware_pending_bytes_;
+  base::Time last_callback_time_;
+  Lock lock_;
+
+  // PushSource role is to buffer and it's only used in regular latency mode.
+  PushSource push_source_;
+  uint32 buffer_capacity_;
+
+  // SyncReader is used only in low latency mode for synchronous reading.
+  SyncReader* sync_reader_;
+
+  // The audio controller thread that this object runs on.
+  base::Thread thread_;
+
+  DISALLOW_COPY_AND_ASSIGN(AudioOutputController);
+};
+
+}  // namespace media
+
+#endif  //  MEDIA_AUDIO_AUDIO_OUTPUT_CONTROLLER_H_
---
+++ media/audio/audio_output_controller_unittest.cc
@@ -0,0 +1,237 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/env_var.h"
+#include "base/basictypes.h"
+#include "base/waitable_event.h"
+#include "media/audio/audio_output_controller.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using ::testing::_;
+using ::testing::AtLeast;
+using ::testing::Exactly;
+using ::testing::InvokeWithoutArgs;
+using ::testing::NotNull;
+
+static const int kSampleRate = AudioManager::kAudioCDSampleRate;
+static const int kBitsPerSample = 16;
+static const int kChannels = 2;
+static const int kHardwareBufferSize = kSampleRate * kBitsPerSample *
+                                       kChannels / 8;
+static const int kBufferCapacity = 3 * kHardwareBufferSize;
+
+namespace media {
+
+class MockAudioOutputControllerEventHandler
+    : public AudioOutputController::EventHandler {
+ public:
+  MockAudioOutputControllerEventHandler() {}
+
+  MOCK_METHOD1(OnCreated, void(AudioOutputController* controller));
+  MOCK_METHOD1(OnPlaying, void(AudioOutputController* controller));
+  MOCK_METHOD1(OnPaused, void(AudioOutputController* controller));
+  MOCK_METHOD2(OnError, void(AudioOutputController* controller,
+                             int error_code));
+  MOCK_METHOD3(OnMoreData,
+               void(AudioOutputController* controller,
+                    base::Time timestamp, uint32 pending_bytes));
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(MockAudioOutputControllerEventHandler);
+};
+
+class MockAudioOutputControllerSyncReader
+    : public AudioOutputController::SyncReader {
+ public:
+  MockAudioOutputControllerSyncReader() {}
+
+  MOCK_METHOD1(UpdatePendingBytes, void(uint32 bytes));
+  MOCK_METHOD2(Read, uint32(void* data, uint32 size));
+  MOCK_METHOD0(Close, void());
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(MockAudioOutputControllerSyncReader);
+};
+
+static bool HasAudioDevices() {
+  AudioManager* audio_man = AudioManager::GetAudioManager();
+  CHECK(audio_man);
+  return audio_man->HasAudioDevices();
+}
+
+static bool IsRunningHeadless() {
+  scoped_ptr<base::EnvVarGetter> env(base::EnvVarGetter::Create());
+  if (env->HasEnv("CHROME_HEADLESS"))
+    return true;
+  return false;
+}
+
+ACTION_P3(SignalEvent, event, count, limit) {
+  if (++*count >= limit) {
+    event->Signal();
+  }
+}
+
+TEST(AudioOutputControllerTest, CreateAndClose) {
+  if (!HasAudioDevices() || IsRunningHeadless())
+    return;
+
+  MockAudioOutputControllerEventHandler event_handler;
+  scoped_refptr<AudioOutputController> controller =
+      AudioOutputController::Create(&event_handler,
+                                    AudioManager::AUDIO_PCM_LINEAR, kChannels,
+                                    kSampleRate, kBitsPerSample,
+                                    kHardwareBufferSize, kBufferCapacity);
+  ASSERT_TRUE(controller.get());
+
+  // Close the controller immediately.
+  controller->Close();
+
+  // TODO(hclam): Make sure releasing the reference to this
+  // object actually destruct it.
+  controller = NULL;
+}
+
+TEST(AudioOutputControllerTest, PlayAndClose) {
+  if (!HasAudioDevices() || IsRunningHeadless())
+    return;
+
+  MockAudioOutputControllerEventHandler event_handler;
+  base::WaitableEvent event(false, false);
+  int count = 0;
+
+  // If OnCreated is called then signal the event.
+  EXPECT_CALL(event_handler, OnCreated(NotNull()))
+      .WillOnce(InvokeWithoutArgs(&event, &base::WaitableEvent::Signal));
+
+  // OnPlaying() will be called only once.
+  EXPECT_CALL(event_handler, OnPlaying(NotNull()))
+      .Times(Exactly(1));
+
+  // If OnMoreData is called enough then signal the event.
+  EXPECT_CALL(event_handler, OnMoreData(NotNull(), _, 0))
+      .Times(AtLeast(10))
+      .WillRepeatedly(SignalEvent(&event, &count, 10));
+
+  scoped_refptr<AudioOutputController> controller =
+      AudioOutputController::Create(&event_handler,
+                                    AudioManager::AUDIO_PCM_LINEAR, kChannels,
+                                    kSampleRate, kBitsPerSample,
+                                    kHardwareBufferSize, kBufferCapacity);
+  ASSERT_TRUE(controller.get());
+
+  // Wait for OnCreated() to be called.
+  event.Wait();
+  event.Reset();
+
+  // Play and then wait for the event to be signaled.
+  controller->Play();
+  event.Wait();
+
+  // Now stop the controller. This should shutdown the internal
+  // thread and we hold the only reference to it.
+  controller->Close();
+
+  // TODO(hclam): Make sure releasing the reference to this
+  // object actually destruct it.
+  controller = NULL;
+}
+
+TEST(AudioOutputControllerTest, PlayPauseClose) {
+  if (!HasAudioDevices() || IsRunningHeadless())
+    return;
+
+  MockAudioOutputControllerEventHandler event_handler;
+  base::WaitableEvent event(false, false);
+  int count = 0;
+
+  // If OnCreated is called then signal the event.
+  EXPECT_CALL(event_handler, OnCreated(NotNull()))
+      .Times(Exactly(1))
+      .WillOnce(InvokeWithoutArgs(&event, &base::WaitableEvent::Signal));
+
+  // OnPlaying() will be called only once.
+  EXPECT_CALL(event_handler, OnPlaying(NotNull()))
+      .Times(Exactly(1));
+
+  // If OnMoreData is called enough then signal the event.
+  EXPECT_CALL(event_handler, OnMoreData(NotNull(), _, 0))
+      .Times(AtLeast(10))
+      .WillRepeatedly(SignalEvent(&event, &count, 10));
+
+  // And then OnPaused() will be called.
+  EXPECT_CALL(event_handler, OnPaused(NotNull()))
+      .Times(Exactly(1))
+      .WillOnce(InvokeWithoutArgs(&event, &base::WaitableEvent::Signal));
+
+  scoped_refptr<AudioOutputController> controller =
+      AudioOutputController::Create(&event_handler,
+                                    AudioManager::AUDIO_PCM_LINEAR, kChannels,
+                                    kSampleRate, kBitsPerSample,
+                                    kHardwareBufferSize, kBufferCapacity);
+  ASSERT_TRUE(controller.get());
+
+  // Wait for OnCreated() to be called.
+  event.Wait();
+  event.Reset();
+
+  // Play and then wait for the event to be signaled.
+  controller->Play();
+  event.Wait();
+  event.Reset();
+
+  // And then wait for pause to complete.
+  controller->Pause();
+  event.Wait();
+
+  // Now stop the controller. This should shutdown the internal
+  // thread and we hold the only reference to it.
+  controller->Close();
+
+  // TODO(hclam): Make sure releasing the reference to this
+  // object actually destruct it.
+  controller = NULL;
+}
+
+TEST(AudioOutputControllerTest, HardwareBufferTooLarge) {
+  if (!HasAudioDevices() || IsRunningHeadless())
+    return;
+
+  // Create an audio device with a very large hardware buffer size.
+  MockAudioOutputControllerEventHandler event_handler;
+  scoped_refptr<AudioOutputController> controller =
+      AudioOutputController::Create(&event_handler,
+                                    AudioManager::AUDIO_PCM_LINEAR, kChannels,
+                                    kSampleRate, kBitsPerSample,
+                                    kHardwareBufferSize * 1000,
+                                    kBufferCapacity);
+
+  // Use assert because we don't stop the device and assume we can't
+  // create one.
+  ASSERT_FALSE(controller);
+}
+
+TEST(AudioOutputControllerTest, CloseTwice) {
+  if (!HasAudioDevices() || IsRunningHeadless())
+    return;
+
+  MockAudioOutputControllerEventHandler event_handler;
+  scoped_refptr<AudioOutputController> controller =
+      AudioOutputController::Create(&event_handler,
+                                    AudioManager::AUDIO_PCM_LINEAR, kChannels,
+                                    kSampleRate, kBitsPerSample,
+                                    kHardwareBufferSize, kBufferCapacity);
+  ASSERT_TRUE(controller.get());
+
+  // Close the controller immediately.
+  controller->Close();
+  controller->Close();
+
+  // TODO(hclam): Make sure releasing the reference to this
+  // object actually destruct it.
+  controller = NULL;
+}
+
+}  // namespace media
