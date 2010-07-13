3e886c789c95193decfd99dc236da62e7de7717b
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-13 18:01:50 +0000

    Add the URLRequest's priority to the net-log.
    BUG=NONE
    TEST=open about:net-internals, look at a request dump. there should be a "priority" field.
    Review URL: http://codereview.chromium.org/2905007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52186 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_netlog_params.cc
+++ net/url_request/url_request_netlog_params.cc
@@ -7,14 +7,21 @@
 #include "base/values.h"
 
 URLRequestStartEventParameters::URLRequestStartEventParameters(
-    const GURL& url, const std::string& method, int load_flags)
-    : url_(url), method_(method), load_flags_(load_flags) {}
+    const GURL& url,
+    const std::string& method,
+    int load_flags,
+    net::RequestPriority priority)
+    : url_(url),
+      method_(method),
+      load_flags_(load_flags),
+      priority_(priority) {
+}
 
 Value* URLRequestStartEventParameters::ToValue() const {
   DictionaryValue* dict = new DictionaryValue();
   dict->SetString(L"url", url_.possibly_invalid_spec());
   dict->SetString(L"method", method_);
   dict->SetInteger(L"load_flags", load_flags_);
+  dict->SetInteger(L"priority", static_cast<int>(priority_));
   return dict;
 }
-
--- net/url_request/url_request_netlog_params.h
+++ net/url_request/url_request_netlog_params.h
@@ -7,15 +7,18 @@
 
 #include <string>
 
+#include "base/basictypes.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/net_log.h"
+#include "net/base/request_priority.h"
 
 // Holds the parameters to emit to the NetLog when starting a URLRequest.
 class URLRequestStartEventParameters : public net::NetLog::EventParameters {
  public:
   URLRequestStartEventParameters(const GURL& url,
                                  const std::string& method,
-                                 int load_flags);
+                                 int load_flags,
+                                 net::RequestPriority priority);
 
   const GURL& url() const {
     return url_;
@@ -27,6 +30,9 @@ class URLRequestStartEventParameters : public net::NetLog::EventParameters {
   const GURL url_;
   const std::string method_;
   const int load_flags_;
+  const net::RequestPriority priority_;
+
+  DISALLOW_COPY_AND_ASSIGN(URLRequestStartEventParameters);
 };
 
 #endif  // NET_URL_REQUEST_URL_REQUEST_NETLOG_PARAMS_H_
