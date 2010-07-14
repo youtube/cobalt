890be5cdcf65b164bf02ec85ea92f705332ee7ee
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-14 18:58:17 +0000

    Revert everything related to the relanding.
    Review URL: http://codereview.chromium.org/2982009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52367 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/host_resolver_impl.cc
+++ net/base/host_resolver_impl.cc
@@ -17,7 +17,6 @@
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/debug_util.h"
-#include "base/histogram.h"
 #include "base/lock.h"
 #include "base/message_loop.h"
 #include "base/stl_util-inl.h"
