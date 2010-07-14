cf901e2fb25ff959d6a7b122f43704cbdc5ccc4e
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-14 17:47:13 +0000

    Revert 52336 - More header cleanup:
    - Some cleanup to url_request.h
    - Remove task.h from headers that don't need it.
    - Remove histogram.h from message_loop.h
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/2986002
    
    TBR=erg@chromium.org
    Review URL: http://codereview.chromium.org/2969006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52343 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/ffmpeg_glue_unittest.cc
+++ media/filters/ffmpeg_glue_unittest.cc
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/scoped_ptr.h"
 #include "media/base/mock_ffmpeg.h"
 #include "media/base/mock_filters.h"
 #include "media/ffmpeg/ffmpeg_common.h"
