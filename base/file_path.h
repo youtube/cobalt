00f2a7e2bd06609d01908fb3c604316c54ea4739
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-21 03:23:56 +0000

    Rename the download to its final name only after the download is finished
    BUG=27687, 28928
    TEST=DownloadManagerTest.DownloadRenameTest
    
    Review URL: http://codereview.chromium.org/2877008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53149 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_path.h
+++ base/file_path.h
@@ -357,9 +357,11 @@ class FilePath {
 #if defined(OS_POSIX)
 #define FILE_PATH_LITERAL(x) x
 #define PRFilePath "s"
+#define PRFilePathLiteral "%s"
 #elif defined(OS_WIN)
 #define FILE_PATH_LITERAL(x) L ## x
 #define PRFilePath "ls"
+#define PRFilePathLiteral L"%ls"
 #endif  // OS_WIN
 
 // Provide a hash function so that hash_sets and maps can contain FilePath
