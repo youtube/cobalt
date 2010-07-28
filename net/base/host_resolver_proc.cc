6baaa6088a0d063f271e50d5738083475956df61
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-28 17:23:04 +0000

    Split off the DNS reload timer
    BUG=none
    TEST=none
    
    http://codereview.chromium.org/3053017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53967 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/host_resolver_proc.cc
+++ net/base/host_resolver_proc.cc
@@ -11,15 +11,10 @@
 #endif
 
 #include "base/logging.h"
-#include "base/time.h"
 #include "net/base/address_list.h"
+#include "net/base/dns_reload_timer.h"
 #include "net/base/net_errors.h"
 #include "net/base/sys_addrinfo.h"
-
-#if defined(OS_POSIX) && !defined(OS_MACOSX)
-#include "base/singleton.h"
-#include "base/thread_local_storage.h"
-#endif
 
 namespace net {
 
@@ -82,79 +77,6 @@ int HostResolverProc::ResolveUsingPrevious(
   return SystemHostResolverProc(host, address_family, host_resolver_flags,
                                 addrlist, os_error);
 }
-
-#if defined(OS_POSIX) && !defined(OS_MACOSX) && !defined(OS_OPENBSD)
-// On Linux/BSD, changes to /etc/resolv.conf can go unnoticed thus resulting
-// in DNS queries failing either because nameservers are unknown on startup
-// or because nameserver info has changed as a result of e.g. connecting to
-// a new network. Some distributions patch glibc to stat /etc/resolv.conf
-// to try to automatically detect such changes but these patches are not
-// universal and even patched systems such as Jaunty appear to need calls
-// to res_ninit to reload the nameserver information in different threads.
-//
-// We adopt the Mozilla solution here which is to call res_ninit when
-// lookups fail and to rate limit the reloading to once per second per
-// thread.
-//
-// OpenBSD does not have thread-safe res_ninit/res_nclose so we can't do
-// the same trick there.
-
-// Keep a timer per calling thread to rate limit the calling of res_ninit.
-class DnsReloadTimer {
- public:
-  // Check if the timer for the calling thread has expired. When no
-  // timer exists for the calling thread, create one.
-  bool Expired() {
-    const base::TimeDelta kRetryTime = base::TimeDelta::FromSeconds(1);
-    base::TimeTicks now = base::TimeTicks::Now();
-    base::TimeTicks* timer_ptr =
-      static_cast<base::TimeTicks*>(tls_index_.Get());
-
-    if (!timer_ptr) {
-      timer_ptr = new base::TimeTicks();
-      *timer_ptr = base::TimeTicks::Now();
-      tls_index_.Set(timer_ptr);
-      // Return true to reload dns info on the first call for each thread.
-      return true;
-    } else if (now - *timer_ptr > kRetryTime) {
-      *timer_ptr = now;
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  // Free the allocated timer.
-  static void SlotReturnFunction(void* data) {
-    base::TimeTicks* tls_data = static_cast<base::TimeTicks*>(data);
-    delete tls_data;
-  }
-
- private:
-  friend struct DefaultSingletonTraits<DnsReloadTimer>;
-
-  DnsReloadTimer() {
-    // During testing the DnsReloadTimer Singleton may be created and destroyed
-    // multiple times. Initialize the ThreadLocalStorage slot only once.
-    if (!tls_index_.initialized())
-      tls_index_.Initialize(SlotReturnFunction);
-  }
-
-  ~DnsReloadTimer() {
-  }
-
-  // We use thread local storage to identify which base::TimeTicks to
-  // interact with.
-  static ThreadLocalStorage::Slot tls_index_ ;
-
-  DISALLOW_COPY_AND_ASSIGN(DnsReloadTimer);
-};
-
-// A TLS slot to the TimeTicks for the current thread.
-// static
-ThreadLocalStorage::Slot DnsReloadTimer::tls_index_(base::LINKER_INITIALIZED);
-
-#endif  // defined(OS_POSIX) && !defined(OS_MACOSX) && !defined(OS_OPENBSD)
 
 int SystemHostResolverProc(const std::string& host,
                            AddressFamily address_family,
@@ -230,10 +152,9 @@ int SystemHostResolverProc(const std::string& host,
 
   int err = getaddrinfo(host.c_str(), NULL, &hints, &ai);
 #if defined(OS_POSIX) && !defined(OS_MACOSX) && !defined(OS_OPENBSD)
-  net::DnsReloadTimer* dns_timer = Singleton<net::DnsReloadTimer>::get();
   // If we fail, re-initialise the resolver just in case there have been any
   // changes to /etc/resolv.conf and retry. See http://crbug.com/11380 for info.
-  if (err && dns_timer->Expired()) {
+  if (err && DnsReloadTimerHasExpired()) {
     res_nclose(&_res);
     if (!res_ninit(&_res))
       err = getaddrinfo(host.c_str(), NULL, &hints, &ai);
