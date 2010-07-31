96c277e08260b9219e1d2faf58bf1c87f54ee512
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-31 23:10:07 +0000

    Fix bad license header in app/ and net/ directories.
    BUG=50266
    TEST=None
    
    Review URL: http://codereview.chromium.org/3013049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54482 0039d316-1c4b-4281-b951-d872f2087c98

--- net/ftp/ftp_directory_listing_parser_windows.h
+++ net/ftp/ftp_directory_listing_parser_windows.h
@@ -1,6 +1,6 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.  Use of this
-// source code is governed by a BSD-style license that can be found in the
-// LICENSE file.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef NET_FTP_FTP_DIRECTORY_LISTING_PARSER_WINDOWS_H_
 #define NET_FTP_FTP_DIRECTORY_LISTING_PARSER_WINDOWS_H_
