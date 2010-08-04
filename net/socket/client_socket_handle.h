77a9aed742405124bd421bbd0fe7a5533cc20aa2
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-04 18:03:11 +0000

    Revert 54771 - Enable speculative preconnection by default
    Added histogram to track utilization (vs waste).
    
    [The stability bot was reporting problems, so I'm reverting]
    
    r=mbelshe
    Review URL: http://codereview.chromium.org/3026038
    
    TBR=jar@chromium.org
    Review URL: http://codereview.chromium.org/3090011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54930 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/client_socket_handle.h
+++ net/socket/client_socket_handle.h
@@ -132,15 +132,7 @@ class ClientSocketHandle {
   const std::string& group_name() const { return group_name_; }
   int id() const { return pool_id_; }
   ClientSocket* socket() { return socket_.get(); }
-  ClientSocket* release_socket() {
-    // Update the base class to record things like whether we've ever
-    // transmitted data, and whether the connection was able to be established.
-    // We use this data to construct histograms indicating whether a
-    // speculative connection was ever used, etc., when the ClientSocket is
-    // eventually discarded.
-    socket_->UpdateConnectivityState(is_reused());
-    return socket_.release();
-  }
+  ClientSocket* release_socket() { return socket_.release(); }
   bool is_reused() const { return is_reused_; }
   base::TimeDelta idle_time() const { return idle_time_; }
   SocketReuseType reuse_type() const {
