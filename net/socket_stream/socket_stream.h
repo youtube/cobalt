140b9edb23512dc29029e4773b80701f22c96e6c
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-05 09:02:44 +0000

    Fix WebSocket crash bug.
    If SocketStream closes while waiting ResolveProxy, it badly
    calls DoResolveProxyComplete from DoLoop invoked by SocketStream::Close,
    and real callback of ResolveProxy failed to call SocketStream::OnIOCompleted.
    
    So, don't run DoLoop when closed, if SocketStream is calling APIs of
    proxy_service or resolver. Check closing_ after the API calls back
    and closes the SocketStream.
    
    r54707 broke SocketStreamTest::CloseFlushPendingWrite.
    This CL fixes this by not closing socket if it is still writing on the socket.
    
    BUG=50394, 46750
    TEST=websocket/tests doesn't crash, and net_unittests passes
    
    Review URL: http://codereview.chromium.org/3076024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55043 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket_stream/socket_stream.h
+++ net/socket_stream/socket_stream.h
@@ -220,6 +220,8 @@ class SocketStream : public base::RefCountedThreadSafe<SocketStream> {
   // Used for WebSocketThrottleTest.
   void CopyAddrInfo(struct addrinfo* head);
 
+  void DoClose();
+
   // Finishes the job.
   // Calls OnError and OnClose of delegate, and no more
   // notifications will be sent to delegate.
@@ -321,6 +323,7 @@ class SocketStream : public base::RefCountedThreadSafe<SocketStream> {
   PendingDataQueue pending_write_bufs_;
 
   bool closing_;
+  bool server_closed_;
 
   scoped_ptr<SocketStreamMetrics> metrics_;
 
