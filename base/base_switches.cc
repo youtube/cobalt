77ebab53871be2eeadf13d65164f80ae15c77760
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-09 17:32:32 +0000

    Revert 55400 - Cleanup in base. This moves the implementation (and a bunch of header file dependencies) from the multiprocess test and the test_suite headers to .cc files. Moves multiprocess_test to the test directory, and all of this stuff to the existing base_test_support project. I also used the base namespace.
    Previously other projects included this functionality just by #include because
    it was all inline, so I had to add dependencies on base_test_support in a few
    places.
    
    Moves and renames the command line switch this was using to base_switches. Move
    the base switch for process type to chrome switches.
    
    TEST=none
    BUG=none
    Review URL: http://codereview.chromium.org/3026055
    
    TBR=brettw@chromium.org
    Review URL: http://codereview.chromium.org/3035062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55416 0039d316-1c4b-4281-b951-d872f2087c98

--- base/base_switches.cc
+++ base/base_switches.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,27 +11,27 @@ namespace switches {
 // seconds for the debugger to attach to itself. Then a break point will be hit.
 const char kDebugOnStart[]                  = "debug-on-start";
 
-// Disables the crash reporting.
-const char kDisableBreakpad[]               = "disable-breakpad";
-
-// Enable DCHECKs in release mode.
-const char kEnableDCHECK[]                  = "enable-dcheck";
-
-// Generates full memory crash dump.
-const char kFullMemoryCrashReport[]         = "full-memory-crash-report";
+// Will wait for 60 seconds for a debugger to come to attach to the process.
+const char kWaitForDebugger[]               = "wait-for-debugger";
 
 // Suppresses all error dialogs when present.
 const char kNoErrorDialogs[]                = "noerrdialogs";
 
+// Disables the crash reporting.
+const char kDisableBreakpad[]               = "disable-breakpad";
+
+// Generates full memory crash dump.
+const char kFullMemoryCrashReport[]         = "full-memory-crash-report";
+
+// The value of this switch determines whether the process is started as a
+// renderer or plugin host.  If it's empty, it's the browser.
+const char kProcessType[]                   = "type";
+
+// Enable DCHECKs in release mode.
+const char kEnableDCHECK[]                  = "enable-dcheck";
+
 // Disable win_util::MessageBox.  This is useful when running as part of
 // scripts that do not have a user interface.
 const char kNoMessageBox[]                  = "no-message-box";
-
-// When running certain tests that spawn child processes, this switch indicates
-// to the test framework that the current process is a child process.
-const char kTestChildProcess[]              = "test-child-process";
-
-// Will wait for 60 seconds for a debugger to come to attach to the process.
-const char kWaitForDebugger[]               = "wait-for-debugger";
 
 }  // namespace switches
