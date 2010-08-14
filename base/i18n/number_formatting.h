48c7c3862a0a3c852c88d2e52b2b3f0cf4ee2526
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-14 17:44:30 +0000

    FBTF: Remove unneeded #includes of <string> and <vector> from src/app and src/base.
    BUG=none
    TEST=builds
    
    Review URL: http://codereview.chromium.org/3173020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56144 0039d316-1c4b-4281-b951-d872f2087c98

--- base/i18n/number_formatting.h
+++ base/i18n/number_formatting.h
@@ -1,12 +1,10 @@
-// Copyright (c) 2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #ifndef BASE_I18N_NUMBER_FORMATTING_H_
 #define BASE_I18N_NUMBER_FORMATTING_H_
 #pragma once
-
-#include <string>
 
 #include "base/basictypes.h"
 #include "base/string16.h"
