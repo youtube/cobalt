bdc10231d428c8dc5e4756a611c516eb1183c2f6
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-17 20:07:11 +0000

    Test server cleanup patch of death:
    - reduce the number of different classes
    - clean up the internal code
    - modify the interface to expose less internal details
      (this will allow more flexibility with port numbers)
    
    TEST=many
    BUG=49680
    
    Review URL: http://codereview.chromium.org/3080029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56405 0039d316-1c4b-4281-b951-d872f2087c98

--- net/test/test_server.h
+++ net/test/test_server.h
@@ -13,52 +13,40 @@
 #include "base/compiler_specific.h"
 #include "base/file_path.h"
 #include "base/process_util.h"
-#include "base/ref_counted.h"
-#include "base/string_number_conversions.h"
-//#include "base/string_util.h"
-#include "googleurl/src/gurl.h"
+#include "net/base/host_port_pair.h"
 
 #if defined(OS_WIN)
 #include "base/scoped_handle_win.h"
 #endif
 
 #if defined(USE_NSS)
+#include "base/ref_counted.h"
 #include "net/base/x509_certificate.h"
 #endif
 
+class GURL;
+
 namespace net {
 
-const int kHTTPDefaultPort = 1337;
-const int kFTPDefaultPort = 1338;
+class AddressList;
 
-const char kDefaultHostName[] = "localhost";
-
-// This object bounds the lifetime of an external python-based HTTP/HTTPS/FTP
-// server that can provide various responses useful for testing.
-class TestServerLauncher {
+// This object bounds the lifetime of an external python-based HTTP/FTP server
+// that can provide various responses useful for testing.
+class TestServer {
  public:
-  TestServerLauncher();
-  virtual ~TestServerLauncher();
-
-  enum Protocol {
-    ProtoHTTP, ProtoFTP
+  enum Type {
+    TYPE_FTP,
+    TYPE_HTTP,
+    TYPE_HTTPS,
+    TYPE_HTTPS_CLIENT_AUTH,
+    TYPE_HTTPS_MISMATCHED_HOSTNAME,
+    TYPE_HTTPS_EXPIRED_CERTIFICATE,
   };
 
-  // Load the test root cert, if it hasn't been loaded yet.
-  bool LoadTestRootCert() WARN_UNUSED_RESULT;
+  TestServer(Type type, const FilePath& document_root);
+  ~TestServer();
 
-  // Start src/net/tools/testserver/testserver.py and
-  // ask it to serve the given protocol.
-  // If protocol is HTTP, and cert_path is not empty, serves HTTPS.
-  // file_root_url specifies the root url on the server that documents will be
-  // served out of. This is /files/ by default.
-  // Returns true on success, false if files not found or root cert
-  // not trusted.
-  bool Start(net::TestServerLauncher::Protocol protocol,
-             const std::string& host_name, int port,
-             const FilePath& document_root,
-             const FilePath& cert_path,
-             const std::wstring& file_root_url) WARN_UNUSED_RESULT;
+  bool Start() WARN_UNUSED_RESULT;
 
   // Stop the server started by Start().
   bool Stop();
@@ -69,56 +57,56 @@ class TestServerLauncher {
   // It returns true if the server exited cleanly.
   bool WaitToFinish(int milliseconds) WARN_UNUSED_RESULT;
 
-  // Paths to a good, an expired, and an invalid server certificate
-  // (use as arguments to Start()).
-  FilePath GetOKCertPath();
-  FilePath GetExpiredCertPath();
+  const FilePath& document_root() const { return document_root_; }
+  const HostPortPair& host_port_pair() const { return host_port_pair_; }
+  std::string GetScheme() const;
+  bool GetAddressList(AddressList* address_list) const WARN_UNUSED_RESULT;
 
-  FilePath GetDocumentRootPath() { return document_root_dir_; }
+  GURL GetURL(const std::string& path);
 
-  // When Start is called, if protocol is HTTPS and ssl_client_auth_ is true,
-  // the server will request a client certificate on each connection. Must be
-  // called before Start to take effect.
-  void set_ssl_client_auth(bool ssl_client_auth) {
-    ssl_client_auth_ = ssl_client_auth;
-  }
+  GURL GetURLWithUser(const std::string& path,
+                      const std::string& user);
 
-  // Issuer name of the root cert that should be trusted for the test to work.
-  static const wchar_t kCertIssuerName[];
-
-  // Hostname to use for test server
-  static const char kHostName[];
-
-  // Different hostname to use for test server (that still resolves to same IP)
-  static const char kMismatchedHostName[];
-
-  // Port to use for test server
-  static const int kOKHTTPSPort;
-
-  // Port to use for bad test server
-  static const int kBadHTTPSPort;
+  GURL GetURLWithUserAndPassword(const std::string& path,
+                                 const std::string& user,
+                                 const std::string& password);
 
  private:
-  // Wait a while for the server to start, return whether
-  // we were able to make a connection to it.
-  bool WaitToStart(const std::string& host_name, int port) WARN_UNUSED_RESULT;
+  // Appends |dir| to PYTHONPATH.
+  static void AppendToPythonPath(const FilePath& dir);
 
-  // Append to PYTHONPATH so Python can find pyftpdlib and tlslite.
-  void SetPythonPath();
+  // Modify PYTHONPATH to contain libraries we need.
+  bool SetPythonPath() WARN_UNUSED_RESULT;
 
-  // Path to our test root certificate.
-  FilePath GetRootCertPath();
+  // Launches the Python test server. Returns true on success.
+  bool LaunchPython(const FilePath& testserver_path) WARN_UNUSED_RESULT;
+
+  // Waits for the server to start. Returns true on success.
+  bool WaitToStart() WARN_UNUSED_RESULT;
+
+  // Returns path to the root certificate.
+  FilePath GetRootCertificatePath();
 
   // Returns false if our test root certificate is not trusted.
   bool CheckCATrusted() WARN_UNUSED_RESULT;
 
-  // Initilize the certificate path.
-  void InitCertPath();
+  // Load the test root cert, if it hasn't been loaded yet.
+  bool LoadTestRootCert() WARN_UNUSED_RESULT;
 
-  FilePath document_root_dir_;
+  // Returns path to the SSL certificate we should use, or empty path
+  // if not applicable.
+  FilePath GetCertificatePath();
 
-  FilePath cert_dir_;
+  // Document root of the test server.
+  FilePath document_root_;
 
+  // Directory that contains the SSL certificates.
+  FilePath certificates_dir_;
+
+  // Address the test server listens on.
+  HostPortPair host_port_pair_;
+
+  // Handle of the Python process running the test server.
   base::ProcessHandle process_handle_;
 
 #if defined(OS_WIN)
@@ -130,237 +118,10 @@ class TestServerLauncher {
   scoped_refptr<X509Certificate> cert_;
 #endif
 
-  bool ssl_client_auth_;
+  Type type_;
 
-  DISALLOW_COPY_AND_ASSIGN(TestServerLauncher);
+  DISALLOW_COPY_AND_ASSIGN(TestServer);
 };
-
-#if defined(OS_WIN)
-// Launch test server as a job so that it is not orphaned if the test case is
-// abnormally terminated.
-bool LaunchTestServerAsJob(const std::wstring& cmdline,
-                           bool start_hidden,
-                           base::ProcessHandle* process_handle,
-                           ScopedHandle* job_handle);
-#endif
-
-// This object bounds the lifetime of an external python-based HTTP/FTP server
-// that can provide various responses useful for testing.
-class BaseTestServer : public base::RefCounted<BaseTestServer> {
- protected:
-  BaseTestServer() {}
-
- public:
-  bool WaitToFinish(int milliseconds) {
-    return launcher_.WaitToFinish(milliseconds);
-  }
-
-  bool Stop() {
-    return launcher_.Stop();
-  }
-
-  GURL TestServerPage(const std::string& base_address,
-      const std::string& path) {
-    return GURL(base_address + path);
-  }
-
-  GURL TestServerPage(const std::string& path) {
-    // TODO(phajdan.jr): Check for problems with IPv6.
-    return GURL(scheme_ + "://" + host_name_ + ":" + port_str_ + "/" + path);
-  }
-
-  GURL TestServerPage(const std::string& path,
-                      const std::string& user,
-                      const std::string& password) {
-    // TODO(phajdan.jr): Check for problems with IPv6.
-
-    if (password.empty())
-      return GURL(scheme_ + "://" + user + "@" +
-                  host_name_ + ":" + port_str_ + "/" + path);
-
-    return GURL(scheme_ + "://" + user + ":" + password +
-                "@" + host_name_ + ":" + port_str_ + "/" + path);
-  }
-
-  FilePath GetDataDirectory() {
-    return launcher_.GetDocumentRootPath();
-  }
-
- protected:
-  friend class base::RefCounted<BaseTestServer>;
-  virtual ~BaseTestServer() { }
-
-  bool Start(net::TestServerLauncher::Protocol protocol,
-             const std::string& host_name, int port,
-             const FilePath& document_root,
-             const FilePath& cert_path,
-             const std::wstring& file_root_url) {
-    if (!launcher_.Start(protocol,
-        host_name, port, document_root, cert_path, file_root_url))
-      return false;
-
-    if (protocol == net::TestServerLauncher::ProtoFTP)
-      scheme_ = "ftp";
-    else
-      scheme_ = "http";
-    if (!cert_path.empty())
-      scheme_.push_back('s');
-
-    host_name_ = host_name;
-    port_str_ = base::IntToString(port);
-    return true;
-  }
-
-  net::TestServerLauncher launcher_;
-  std::string scheme_;
-  std::string host_name_;
-  std::string port_str_;
-};
-
-class HTTPTestServer : public BaseTestServer {
- protected:
-  HTTPTestServer() {}
-
- public:
-  // Creates and returns a new HTTPTestServer.
-  static scoped_refptr<HTTPTestServer> CreateServer(
-      const std::wstring& document_root) {
-    return CreateServerWithFileRootURL(document_root, std::wstring());
-  }
-
-  static scoped_refptr<HTTPTestServer> CreateServerWithFileRootURL(
-      const std::wstring& document_root,
-      const std::wstring& file_root_url) {
-    scoped_refptr<HTTPTestServer> test_server(new HTTPTestServer());
-    FilePath no_cert;
-    FilePath docroot = FilePath::FromWStringHack(document_root);
-    if (!StartTestServer(test_server.get(), docroot, no_cert, file_root_url))
-      return NULL;
-    return test_server;
-  }
-
-  static bool StartTestServer(HTTPTestServer* server,
-                              const FilePath& document_root,
-                              const FilePath& cert_path,
-                              const std::wstring& file_root_url) {
-    return server->Start(net::TestServerLauncher::ProtoHTTP, kDefaultHostName,
-                         kHTTPDefaultPort, document_root, cert_path,
-                         file_root_url);
-  }
-};
-
-class HTTPSTestServer : public HTTPTestServer {
- protected:
-  HTTPSTestServer() {}
-
- public:
-  // Create a server with a valid certificate
-  // TODO(dkegel): HTTPSTestServer should not require an instance to specify
-  // stock test certificates
-  static scoped_refptr<HTTPSTestServer> CreateGoodServer(
-      const std::wstring& document_root) {
-    scoped_refptr<HTTPSTestServer> test_server = new HTTPSTestServer();
-    FilePath docroot = FilePath::FromWStringHack(document_root);
-    FilePath certpath = test_server->launcher_.GetOKCertPath();
-    if (!test_server->Start(net::TestServerLauncher::ProtoHTTP,
-        net::TestServerLauncher::kHostName,
-        net::TestServerLauncher::kOKHTTPSPort,
-        docroot, certpath, std::wstring())) {
-      return NULL;
-    }
-    return test_server;
-  }
-
-  // Create a server which requests SSL client auth
-  static scoped_refptr<HTTPSTestServer> CreateClientAuthServer(
-      const std::wstring& document_root) {
-    scoped_refptr<HTTPSTestServer> test_server = new HTTPSTestServer();
-    FilePath docroot = FilePath::FromWStringHack(document_root);
-    FilePath certpath = test_server->launcher_.GetOKCertPath();
-    test_server->launcher_.set_ssl_client_auth(true);
-    if (!test_server->Start(net::TestServerLauncher::ProtoHTTP,
-        net::TestServerLauncher::kHostName,
-        net::TestServerLauncher::kOKHTTPSPort,
-        docroot, certpath, std::wstring())) {
-      return NULL;
-    }
-    return test_server;
-  }
-
-  // Create a server with an up to date certificate for the wrong hostname
-  // for this host
-  static scoped_refptr<HTTPSTestServer> CreateMismatchedServer(
-      const std::wstring& document_root) {
-    scoped_refptr<HTTPSTestServer> test_server = new HTTPSTestServer();
-    FilePath docroot = FilePath::FromWStringHack(document_root);
-    FilePath certpath = test_server->launcher_.GetOKCertPath();
-    if (!test_server->Start(net::TestServerLauncher::ProtoHTTP,
-        net::TestServerLauncher::kMismatchedHostName,
-        net::TestServerLauncher::kOKHTTPSPort,
-        docroot, certpath, std::wstring())) {
-      return NULL;
-    }
-    return test_server;
-  }
-
-  // Create a server with an expired certificate
-  static scoped_refptr<HTTPSTestServer> CreateExpiredServer(
-      const std::wstring& document_root) {
-    scoped_refptr<HTTPSTestServer> test_server = new HTTPSTestServer();
-    FilePath docroot = FilePath::FromWStringHack(document_root);
-    FilePath certpath = test_server->launcher_.GetExpiredCertPath();
-    if (!test_server->Start(net::TestServerLauncher::ProtoHTTP,
-        net::TestServerLauncher::kHostName,
-        net::TestServerLauncher::kBadHTTPSPort,
-        docroot, certpath, std::wstring())) {
-      return NULL;
-    }
-    return test_server;
-  }
-
-  // Create a server with an arbitrary certificate
-  static scoped_refptr<HTTPSTestServer> CreateServer(
-      const std::string& host_name, int port,
-      const std::wstring& document_root,
-      const std::wstring& cert_path) {
-    scoped_refptr<HTTPSTestServer> test_server = new HTTPSTestServer();
-    FilePath docroot = FilePath::FromWStringHack(document_root);
-    FilePath certpath = FilePath::FromWStringHack(cert_path);
-    if (!test_server->Start(net::TestServerLauncher::ProtoHTTP,
-        host_name, port, docroot, certpath, std::wstring())) {
-      return NULL;
-    }
-    return test_server;
-  }
-
- protected:
-  std::wstring cert_path_;
-
- private:
-  virtual ~HTTPSTestServer() {}
-};
-
-class FTPTestServer : public BaseTestServer {
- public:
-  FTPTestServer() {
-  }
-
-  static scoped_refptr<FTPTestServer> CreateServer(
-      const std::wstring& document_root) {
-    scoped_refptr<FTPTestServer> test_server = new FTPTestServer();
-    FilePath docroot = FilePath::FromWStringHack(document_root);
-    FilePath no_cert;
-    if (!test_server->Start(net::TestServerLauncher::ProtoFTP,
-        kDefaultHostName, kFTPDefaultPort, docroot, no_cert, std::wstring())) {
-      return NULL;
-    }
-    return test_server;
-  }
-
- private:
-  ~FTPTestServer() {}
-};
-
 
 }  // namespace net
 
