9a2dccd0fa574d12f6c5d7536e94face2c4ac58e
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-21 02:12:42 +0000

    Add a feature to the HostResolverRules to be able to match rules only for a specific port.  This allows me to alter resolutions for certain ports like send :80 lookups to one location and :443 lookups to a different location.
    This enables me to mock out support for AlternateProtocol.
    
    BUG=none
    TEST=HostMappingRulesTest.PortSpecificMatching
    
    Review URL: http://codereview.chromium.org/3177026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56970 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/host_mapping_rules_unittest.cc
+++ net/base/host_mapping_rules_unittest.cc
@@ -37,6 +37,36 @@ TEST(HostMappingRulesTest, SetRulesFromString) {
   EXPECT_EQ(666u, host_port.port());
 }
 
+TEST(HostMappingRulesTest, PortSpecificMatching) {
+  HostMappingRules rules;
+  rules.SetRulesFromString(
+      "map *.com:80 baz:111 , map *.com:443 blat:333, EXCLUDE *.foo.com");
+
+  // No match
+  HostPortPair host_port("test.com", 1234);
+  EXPECT_FALSE(rules.RewriteHost(&host_port));
+  EXPECT_EQ("test.com", host_port.host());
+  EXPECT_EQ(1234u, host_port.port());
+
+  // Match port 80
+  host_port = HostPortPair("crack.com", 80);
+  EXPECT_TRUE(rules.RewriteHost(&host_port));
+  EXPECT_EQ("baz", host_port.host());
+  EXPECT_EQ(111u, host_port.port());
+
+  // Match port 443
+  host_port = HostPortPair("wtf.com", 443);
+  EXPECT_TRUE(rules.RewriteHost(&host_port));
+  EXPECT_EQ("blat", host_port.host());
+  EXPECT_EQ(333u, host_port.port());
+
+  // Match port 443, but excluded.
+  host_port = HostPortPair("wtf.foo.com", 443);
+  EXPECT_FALSE(rules.RewriteHost(&host_port));
+  EXPECT_EQ("wtf.foo.com", host_port.host());
+  EXPECT_EQ(443u, host_port.port());
+}
+
 // Parsing bad rules should silently discard the rule (and never crash).
 TEST(HostMappingRulesTest, ParseInvalidRules) {
   HostMappingRules rules;
