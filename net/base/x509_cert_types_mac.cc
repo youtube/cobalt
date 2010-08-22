ad724ca3259d4a1bf2cb91cd3a75bd9d13e7a715
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-22 03:24:07 +0000

    Whitespace change to trigger the bots.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3191018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57009 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/x509_cert_types_mac.cc
+++ net/base/x509_cert_types_mac.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2006-2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -235,7 +235,7 @@ bool CertPrincipal::ParseDistinguishedName(const void* ber_name_data,
   return true;
 }
 
-  
+
 // SUBROUTINES:
 
 static std::string DataToString(CSSM_DATA data) {
