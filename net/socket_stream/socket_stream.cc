80f2e5dbbbcc20c5abf14d43a9992c1295580bc7
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-25 17:08:07 +0000

    In about:net-internals, each SOCKET created by a CONNECT_JOB holds a reference to the CONNECT_JOB that spawned it.
    BUG=47082
    TEST=manual
    
    Review URL: http://codereview.chromium.org/3164005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57339 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket_stream/socket_stream.cc
+++ net/socket_stream/socket_stream.cc
@@ -549,7 +549,8 @@ int SocketStream::DoTcpConnect(int result) {
   next_state_ = STATE_TCP_CONNECT_COMPLETE;
   DCHECK(factory_);
   socket_.reset(factory_->CreateTCPClientSocket(addresses_,
-                                                 net_log_.net_log()));
+                                                net_log_.net_log(),
+                                                net_log_.source()));
   metrics_->OnStartConnection();
   return socket_->Connect(&io_callback_);
 }
