f681b35a82815948716a06839e89b92f87d91546
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-30 21:07:05 +0000

    Add RandomNumberGenerator adapter to base/rand_util.h
    BUG=46679
    TEST=none (yet...)
    
    Review URL: http://codereview.chromium.org/3053050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57904 0039d316-1c4b-4281-b951-d872f2087c98

--- base/rand_util.h
+++ base/rand_util.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -15,6 +15,13 @@ uint64 RandUint64();
 
 // Returns a random number between min and max (inclusive). Thread-safe.
 int RandInt(int min, int max);
+
+// Returns a random number in range [0, max).  Thread-safe.
+//
+// Note that this can be used as an adapter for std::random_shuffle():
+// Given a pre-populated |std::vector<int> myvector|, shuffle it as
+//   std::random_shuffle(myvector.begin(), myvector.end(), base::RandGenerator);
+uint64 RandGenerator(uint64 max);
 
 // Returns a random double in range [0, 1). Thread-safe.
 double RandDouble();
