407b4d6f3865b14929699d03307964a1ee5eebbd
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-31 11:10:55 +0000

    Map SECURITY_STATUS from InitializeSecurityContext to net error codes.
    The SSPI implementation of Negotiate+NTLM used to have too many ways to generate an ERR_UNEXPECTED return code, which made it difficult to diagnose user reported issues.
    
    BUG=53850
    TEST=net_unittests
    
    Review URL: http://codereview.chromium.org/3234007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57979 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/net_error_list.h
+++ net/base/net_error_list.h
@@ -363,6 +363,14 @@ NET_ERROR(ENCODING_DETECTION_FAILED, -340)
 // (GSSAPI) No Kerberos credentials were available during HTTP Authentication.
 NET_ERROR(MISSING_AUTH_CREDENTIALS, -341)
 
+// A valid SSPI return code that does not have an equivalent error
+// code.
+NET_ERROR(UNMAPPED_SSPI_ERROR, -342)
+
+// The environment was not set up correctly for authentication (for
+// example, no KDC could be found or the principal is unknown.
+NET_ERROR(MISCONFIGURED_AUTH_ENVIRONMENT, -343)
+
 // The cache does not have the requested entry.
 NET_ERROR(CACHE_MISS, -400)
 
