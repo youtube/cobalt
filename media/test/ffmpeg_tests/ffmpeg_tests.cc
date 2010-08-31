63b58899ebaedc7abc00dd5f654482105cd9aba7
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-31 20:33:15 +0000

    Revert "re-enable ogg threads"
    Because http/tests/media/video-cookie.html is failing.
    
    This reverts commit r58043.
    
    TBR=fbarchard
    
    Review URL: http://codereview.chromium.org/3231011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58057 0039d316-1c4b-4281-b951-d872f2087c98

--- media/test/ffmpeg_tests/ffmpeg_tests.cc
+++ media/test/ffmpeg_tests/ffmpeg_tests.cc
@@ -221,6 +221,13 @@ int main(int argc, const char** argv) {
     return 1;
   }
 
+  // TODO(fbarchard): On next ffmpeg roll, retest if this work around is needed.
+  if (codec_context->codec_id == CODEC_ID_THEORA) {
+    std::cerr << "Warning: Disabling threads to avoid Theora bug "
+              << in_path.value() << std::endl;
+    video_threads = 1;
+  }
+
   codec_context->error_concealment = FF_EC_GUESS_MVS | FF_EC_DEBLOCK;
   codec_context->error_recognition = FF_ER_CAREFUL;
 
