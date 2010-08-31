65ec65e30ed5dce4e09afd06b277de3db91d045f
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-31 22:13:52 +0000

    Add NSS cert trust setting, cert deleting, and CA cert import to CertDatabase.
    BUG=19991
    TEST=net/base/cert_database_nss_unittest.cc
    
    Review URL: http://codereview.chromium.org/3106028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58077 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/base/cert_database.cc
@@ -0,0 +1,16 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/base/cert_database.h"
+
+#include "net/base/x509_certificate.h"
+
+namespace net {
+
+CertDatabase::ImportCertResult::ImportCertResult(
+    X509Certificate* cert, int err)
+    : certificate(cert), net_error(err) {
+}
+
+}  // namespace net
