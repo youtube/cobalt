b8c4976770cf75c0102280d34205ac57f428e14b
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-01 17:04:52 +0000

    net: add *.yodlee.com to False Start blacklist
    (At the request of A10 networks.)
    
    BUG=53690
    TEST=net_unittests
    
    http://codereview.chromium.org/3235009/show
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58191 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/base/ssl_config_service_unittest.cc
@@ -0,0 +1,33 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/base/ssl_config_service.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+
+class SSLConfigServiceTest : public testing::Test {
+};
+
+bool IsFalseStartIncompatible(const std::string& hostname) {
+  return net::SSLConfigService::IsKnownFalseStartIncompatibleServer(
+      hostname);
+}
+
+}  // namespace
+
+TEST(SSLConfigServiceTest, FalseStartDisabledHosts) {
+  EXPECT_TRUE(IsFalseStartIncompatible("www.picnik.com"));
+  EXPECT_FALSE(IsFalseStartIncompatible("picnikfoo.com"));
+  EXPECT_FALSE(IsFalseStartIncompatible("foopicnik.com"));
+}
+
+TEST(SSLConfigServiceTest, FalseStartDisabledDomains) {
+  EXPECT_TRUE(IsFalseStartIncompatible("yodlee.com"));
+  EXPECT_TRUE(IsFalseStartIncompatible("a.yodlee.com"));
+  EXPECT_TRUE(IsFalseStartIncompatible("b.a.yodlee.com"));
+  EXPECT_FALSE(IsFalseStartIncompatible("ayodlee.com"));
+  EXPECT_FALSE(IsFalseStartIncompatible("yodleea.com"));
+  EXPECT_FALSE(IsFalseStartIncompatible("yodlee.org"));
+}
