fa83d5514e3477013ada19dc664d4c04c94ce623
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-07 19:41:36 +0000

    Reland r58230.
    Simple 2-line fix.
    
    BUG=53138
    TEST=Make sure ethernet is plugged in.  Run Chrome.  Unplug ethernet.  Does not crash.
    
    Review URL: http://codereview.chromium.org/3371001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58736 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/network_change_notifier_mac.h
+++ net/base/network_change_notifier_mac.h
@@ -14,17 +14,39 @@
 
 namespace net {
 
-class NetworkChangeNotifierMac : public NetworkConfigWatcherMac,
-                                 public NetworkChangeNotifier {
+class NetworkChangeNotifierMac: public NetworkChangeNotifier {
  public:
   NetworkChangeNotifierMac();
-
- protected:
-  // NetworkConfigWatcherMac implementation:
-  virtual void SetDynamicStoreNotificationKeys(SCDynamicStoreRef store);
-  virtual void OnNetworkConfigChange(CFArrayRef changed_keys);
+  virtual ~NetworkChangeNotifierMac();
 
  private:
+  // Forwarder just exists to keep the NetworkConfigWatcherMac API out of
+  // NetworkChangeNotifierMac's public API.
+  class Forwarder : public NetworkConfigWatcherMac::Delegate {
+   public:
+    explicit Forwarder(NetworkChangeNotifierMac* net_config_watcher)
+        : net_config_watcher_(net_config_watcher) {}
+
+    // NetworkConfigWatcherMac::Delegate implementation:
+    virtual void SetDynamicStoreNotificationKeys(SCDynamicStoreRef store) {
+      net_config_watcher_->SetDynamicStoreNotificationKeys(store);
+    }
+    virtual void OnNetworkConfigChange(CFArrayRef changed_keys) {
+      net_config_watcher_->OnNetworkConfigChange(changed_keys);
+    }
+
+   private:
+    NetworkChangeNotifierMac* const net_config_watcher_;
+    DISALLOW_COPY_AND_ASSIGN(Forwarder);
+  };
+
+  // NetworkConfigWatcherMac::Delegate implementation:
+  void SetDynamicStoreNotificationKeys(SCDynamicStoreRef store);
+  void OnNetworkConfigChange(CFArrayRef changed_keys);
+
+  Forwarder forwarder_;
+  const NetworkConfigWatcherMac config_watcher_;
+
   DISALLOW_COPY_AND_ASSIGN(NetworkChangeNotifierMac);
 };
 
