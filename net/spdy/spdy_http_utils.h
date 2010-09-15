9600a83c4c81a1126e0a7192e4db674873d6f082
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-15 23:36:29 +0000

    Move two methods in spdy_http_stream.cc from an anonymous namespace into public static methods of SpdyHttpStream.  This is in preparation for the SpdyProxyClientSocket which desires to make use of this functionality.
     // Convert a SpdyHeaderBlock into an HttpResponseInfo.
     // |headers| input parameter with the SpdyHeaderBlock.
     // |info| output parameter for the HttpResponseInfo.
     // Returns true if successfully converted.  False if there was a failure
     // or if the SpdyHeaderBlock was invalid.
     static bool SpdyHeadersToHttpResponse(const spdy::SpdyHeaderBlock& headers,
                                           net::HttpResponseInfo* response);
    
     // Create a SpdyHeaderBlock for a Spdy SYN_STREAM Frame from
     // a HttpRequestInfo block.
     static void CreateSpdyHeadersFromHttpRequest(const net::HttpRequestInfo& info,
                                                  spdy::SpdyHeaderBlock* headers,
                                                  bool direct);
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/3441004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59580 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/spdy/spdy_http_utils.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_SPDY_SPDY_HTTP_UTILS_H_
+#define NET_SPDY_SPDY_HTTP_UTILS_H_
+#pragma once
+
+#include "net/spdy/spdy_framer.h"
+
+namespace net {
+
+class HttpResponseInfo;
+class HttpRequestInfo;
+
+// Convert a SpdyHeaderBlock into an HttpResponseInfo.
+// |headers| input parameter with the SpdyHeaderBlock.
+// |info| output parameter for the HttpResponseInfo.
+// Returns true if successfully converted.  False if there was a failure
+// or if the SpdyHeaderBlock was invalid.
+bool SpdyHeadersToHttpResponse(const spdy::SpdyHeaderBlock& headers,
+                               HttpResponseInfo* response);
+
+// Create a SpdyHeaderBlock for a Spdy SYN_STREAM Frame from
+// a HttpRequestInfo block.
+void CreateSpdyHeadersFromHttpRequest(const HttpRequestInfo& info,
+                                      spdy::SpdyHeaderBlock* headers,
+                                      bool direct);
+
+}  // namespace net
+
+#endif  // NET_SPDY_SPDY_HTTP_UTILS_H_
