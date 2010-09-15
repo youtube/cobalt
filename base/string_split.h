492179ecfd36d2464deefb6deae14bcba93e2083
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-15 13:36:22 +0000

    base: Move SplitStringDontTrim functions from string_util.h to string_split.h
    BUG=None
    TEST=trybots
    
    Review URL: http://codereview.chromium.org/3366011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59493 0039d316-1c4b-4281-b951-d872f2087c98

--- base/string_split.h
+++ base/string_split.h
@@ -33,6 +33,25 @@ void SplitStringUsingSubstr(const std::string& str,
                             const std::string& s,
                             std::vector<std::string>* r);
 
+// The same as SplitString, but don't trim white space.
+// Where wchar_t is char16 (i.e. Windows), |c| must be in BMP
+// (Basic Multilingual Plane). Elsewhere (Linux/Mac), wchar_t
+// should be a valid Unicode code point (32-bit).
+void SplitStringDontTrim(const std::wstring& str,
+                         wchar_t c,
+                         std::vector<std::wstring>* r);
+// NOTE: |c| must be in BMP (Basic Multilingual Plane)
+void SplitStringDontTrim(const string16& str,
+                         char16 c,
+                         std::vector<string16>* r);
+// |str| should not be in a multi-byte encoding like Shift-JIS or GBK in which
+// the trailing byte of a multi-byte character can be in the ASCII range.
+// UTF-8, and other single/multi-byte ASCII-compatible encodings are OK.
+// Note: |c| must be in the ASCII range.
+void SplitStringDontTrim(const std::string& str,
+                         char c,
+                         std::vector<std::string>* r);
+
 }  // namespace base
 
 #endif  // BASE_STRING_SPLIT_H
