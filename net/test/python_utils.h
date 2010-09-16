b4aa7d8983baaad1693e08da5a26ad3aab6309d2
lzheng@chromium.org <lzheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-16 17:00:38 +0000

    Refactor test_server so some python related functions could be shared by other test servers. BUG=none TEST=python_utils_unittest.cc Review URL: http://codereview.chromium.org/3366026
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59671 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/test/python_utils.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_TEST_PYTHON_UTILS_H_
+#define NET_TEST_PYTHON_UTILS_H_
+#pragma once
+
+#include "base/compiler_specific.h"
+
+class FilePath;
+
+// This is the python path variable name.
+extern const char kPythonPathEnv[];
+
+// Appends the dir to python path environment variable.
+void AppendToPythonPath(const FilePath& dir);
+
+// Returns the path that should be used to launch Python.
+bool GetPythonRunTime(FilePath* path) WARN_UNUSED_RESULT;
+
+#endif  // NET_TEST_PYTHON_UTILS_H_
+
