e69768b6d9c40e4ae031dfcab1816803b4c77212
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-19 23:05:07 +0000

    Add HttpResponseBodyDrainer.  Use it for unfinished HttpStreams.
    Hopefully this will improve our reuse of sockets, and thus performance, since many redirects have response bodies, so we end up discarding those sockets rather than draining the bodies and reusing the socket.
    
    Fix a bunch of tests since we now try to read from the socket in HttpNetworkTransaction's destructor, which often executes after the StaticSocketDataProvider has already been destroyed, so we end up trying to invoke pure virtuals via stale pointers.
    
    BUG=54277
    TEST=
    1) Start up chrome with an empty cache (I just use --user-data-dir=/tmp/newprofile).
    2) Open up about:net-internals, browse to its socket tab.
    3) In a separate Chrome tab, open up http://google.com (NOT www.google.com).
    NOTE: This will get a 301 redirect with a response body for us to drain.
          Previously we wouldn't drain it, so we wouldn't reuse the socket.
    4) In the about:net-internals's socket page, wait for it to reload (or force a reload).
      Look for the google.com row under tcp_socket_pool.  If it doesn't exist, then that
    means it failed.  If it does exist _and_ the idle column says '1', then it worked.
    
    Review URL: http://codereview.chromium.org/3293015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59908 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_util.h
+++ net/http/http_util.h
@@ -10,6 +10,7 @@
 
 #include "base/string_tokenizer.h"
 #include "googleurl/src/gurl.h"
+#include "net/base/completion_callback.h"
 #include "net/http/http_byte_range.h"
 
 // This is a macro to support extending this string literal at compile time.
@@ -17,6 +18,8 @@
 #define HTTP_LWS " \t"
 
 namespace net {
+
+class HttpStream;
 
 class HttpUtil {
  public:
@@ -247,6 +250,10 @@ class HttpUtil {
     std::string::const_iterator value_begin_;
     std::string::const_iterator value_end_;
   };
+
+  // Attempts to read all of the response body of |stream|.  Closes |stream| and
+  // deletes it when complete.
+  static void DrainStreamBodyAndClose(HttpStream* stream);
 };
 
 }  // namespace net
