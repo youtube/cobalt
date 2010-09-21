a596ae50e3bd7d161f2f7bb36309972bca291095
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-21 06:13:49 +0000

    Revert 59972 (chromium os leaks) - Reland r59511: Eagerly set the IO loop used for OCSP.
    ChromeOS will create a special Profile for login.  Previously, OCSP initialization was done for the "default" ChromeURLRequestContext for each Profile.  Since we can have multiple profiles, this causes the initialization (and uninitialization) to happen multiple times, which causes problems for OCSP since we use statics.  The solution is to identify the "main" Profile.  We create said Profile in BrowserMain.  I add an "is_main_" variable to URLRequestContextGetter and URLRequestContext, so that only the "main" URLRequestContext will initialize OCSP.
    
    Other than minor plumbing of "is_main_", this changelist is identical to r59511.
    
    BUG=55940
    TEST=Startup ChromeOS's Chrome.  Should not crash on startup.
    
    Review URL: http://codereview.chromium.org/3470001
    
    TBR=willchan@chromium.org
    Review URL: http://codereview.chromium.org/3468005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60025 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_context.cc
+++ net/url_request/url_request_context.cc
@@ -15,8 +15,7 @@ URLRequestContext::URLRequestContext()
       http_auth_handler_factory_(NULL),
       network_delegate_(NULL),
       cookie_policy_(NULL),
-      transport_security_state_(NULL),
-      is_main_(false) {
+      transport_security_state_(NULL) {
 }
 
 const std::string& URLRequestContext::GetUserAgent(const GURL& url) const {
