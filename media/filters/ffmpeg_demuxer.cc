5eaa59afbb098aea6b8e0a3413dd7adcf386228e
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-23 22:43:53 +0000

    FBTF: Move virtual methods to implementation files.
    Remove logging.h and other headers where possible.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3461019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60369 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/ffmpeg_demuxer.cc
+++ media/filters/ffmpeg_demuxer.cc
@@ -4,6 +4,7 @@
 
 #include "base/callback.h"
 #include "base/command_line.h"
+#include "base/message_loop.h"
 #include "base/scoped_ptr.h"
 #include "base/stl_util-inl.h"
 #include "base/string_util.h"
