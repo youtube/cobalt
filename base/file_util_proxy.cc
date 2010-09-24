0506073389f0bd2eae6f1de9a96b761f50cd576f
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-24 20:07:41 +0000

    file_util_proxy: fix a typo that caused us to ignore a path.
    This was found by the Clang bot!
    
    TEST=compiles
    
    Review URL: http://codereview.chromium.org/3470015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60512 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util_proxy.cc
+++ base/file_util_proxy.cc
@@ -564,7 +564,7 @@ class RelayTouchFilePath : public RelayWithStatusCallback {
                      const base::Time& last_modified_time,
                      base::FileUtilProxy::StatusCallback* callback)
       : RelayWithStatusCallback(callback),
-        file_path_(file_path_),
+        file_path_(file_path),
         last_access_time_(last_access_time),
         last_modified_time_(last_modified_time) {
   }
