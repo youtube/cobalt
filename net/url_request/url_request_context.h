007648df30b1e223744af4d59e92a090d6408127
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-28 00:40:11 +0000

    Reland r59972: Eagerly set the IO loop used for OCSP.
    ChromeOS will create a special Profile for login. Previously, OCSP initialization was done for the "default" ChromeURLRequestContext for each Profile. Since we can have multiple profiles, this causes the initialization (and uninitialization) to happen multiple times, which causes problems for OCSP since we use statics. The solution is to identify the "main" Profile. We create said Profile in BrowserMain. I add an "is_main_" variable to URLRequestContextGetter and URLRequestContext, so that only the "main" URLRequestContext will initialize OCSP.
    
    The only change from r59972 is the location that I set_is_main().  I set it when we set the default request context.  This is because, by the time I had called set_is_main() in BrowserMain, the ChromeURLRequestContextGetter was already created (it gets created during the CreateProfile() function).  So I was too late.
    
    BUG=55940
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/3391028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60739 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_context.h
+++ net/url_request/url_request_context.h
@@ -104,6 +104,11 @@ class URLRequestContext
     referrer_charset_ = charset;
   }
 
+  // Controls whether or not the URLRequestContext considers itself to be the
+  // "main" URLRequestContext.
+  bool is_main() const { return is_main_; }
+  void set_is_main(bool is_main) { is_main_ = is_main; }
+
  protected:
   friend class base::RefCountedThreadSafe<URLRequestContext>;
 
@@ -131,6 +136,9 @@ class URLRequestContext
   std::string referrer_charset_;
 
  private:
+  // Indicates whether or not this is the main URLRequestContext.
+  bool is_main_;
+
   DISALLOW_COPY_AND_ASSIGN(URLRequestContext);
 };
 
