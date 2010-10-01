51cc534ebf26cdf61a17f9b6becdee98492c095a
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-01 20:03:11 +0000

    Clang: make DCHECK_EQ(string16, string16) work.
    The problem is that string16 is a typedef for a std::basic_string with a custom base::char_traits, hence ADL looks for operator<< only in std and base, not in the global namespace. Since adding stuff to the global namespace isn't permitted, move the operator to namespace base instead.
    
    Also give WebString an explicit operator<< because clang can't figure out that it can use WebString's |operator string16| to print WebStrings.
    
    string16 is just wstring on windows, and gtest has special code to make printing wstrings to non-wide ostreams work already, so nothing is required on windows.
    
    Fix a few other minor issues.
    
    Based on a patch by hans@chromium.org
    
    BUG=57294
    TEST=still compiles with gcc, fewer build errors in tests with clang
    
    Review URL: http://codereview.chromium.org/3515003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61218 0039d316-1c4b-4281-b951-d872f2087c98

--- base/string16.h
+++ base/string16.h
@@ -167,7 +167,9 @@ extern template class std::basic_string<char16, base::string16_char_traits>;
 
 typedef std::basic_string<char16, base::string16_char_traits> string16;
 
+namespace base {
 extern std::ostream& operator<<(std::ostream& out, const string16& str);
+}
 
 #endif  // WCHAR_T_IS_UTF32
 
