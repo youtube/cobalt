d10cdb986483475e60f8154c2d96fa04429dac48
pvalchev@chromium.org <pvalchev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-04 23:10:18 +0000

    I added FreeBSD-generated config files for libevent and tcmalloc, along with some remaining gyp file tweaks. Review URL: http://codereview.chromium.org/3423021
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61433 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/libevent/event-config.h
+++ third_party/libevent/event-config.h
@@ -9,6 +9,8 @@
 #include "mac/event-config.h"
 #elif defined(__linux__)
 #include "linux/event-config.h"
+#elif defined(__FreeBSD__)
+#include "freebsd/event-config.h"
 #else
 #error generate event-config.h for your platform
 #endif
