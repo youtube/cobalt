0509d033ac99f35ea53b81a95dafaa7b7d791344
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-05 19:03:05 +0000

    Move TestTimeouts from chrome/test to base/test.
    BUG=None
    TEST=unittests
    
    Review URL: http://codereview.chromium.org/3544008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61540 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ base/test/test_switches.cc
@@ -0,0 +1,17 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/test/test_switches.h"
+
+// Time (in milliseconds) for which we wait for live operations to complete.
+const char switches::kLiveOperationTimeout[] = "live-operation-timeout";
+
+// Time (in milliseconds) that the tests should wait before timing out.
+// TODO(phajdan.jr): Clean up the switch names.
+const char switches::kTestLargeTimeout[] = "test-large-timeout";
+const char switches::kUiTestActionTimeout[] = "ui-test-action-timeout";
+const char switches::kUiTestActionMaxTimeout[] = "ui-test-action-max-timeout";
+const char switches::kUiTestCommandExecutionTimeout[] = "ui-test-timeout";
+const char switches::kUiTestTerminateTimeout[] = "ui-test-terminate-timeout";
+const char switches::kUiTestTimeout[] = "test-timeout";
---
+++ base/test/test_timeouts.cc
@@ -0,0 +1,89 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/test/test_timeouts.h"
+
+#include "base/command_line.h"
+#include "base/logging.h"
+#include "base/string_number_conversions.h"
+#include "base/test/test_switches.h"
+
+namespace {
+
+// Sets value to the greatest of:
+// 1) value's current value.
+// 2) min_value.
+// 3) the numerical value given by switch_name on the command line.
+void InitializeTimeout(const char* switch_name, int min_value, int* value) {
+  DCHECK(value);
+  if (CommandLine::ForCurrentProcess()->HasSwitch(switch_name)) {
+    std::string string_value(
+        CommandLine::ForCurrentProcess()->GetSwitchValueASCII(switch_name));
+    int timeout;
+    base::StringToInt(string_value, &timeout);
+    *value = std::max(*value, timeout);
+  }
+  *value = std::max(*value, min_value);
+}
+
+// Sets value to the greatest of:
+// 1) value's current value.
+// 2) 0
+// 3) the numerical value given by switch_name on the command line.
+void InitializeTimeout(const char* switch_name, int* value) {
+  InitializeTimeout(switch_name, 0, value);
+}
+
+}  // namespace
+
+// static
+bool TestTimeouts::initialized_ = false;
+
+// The timeout values should increase in the order they appear in this block.
+// static
+int TestTimeouts::action_timeout_ms_ = 2000;
+int TestTimeouts::action_max_timeout_ms_ = 15000;
+int TestTimeouts::large_test_timeout_ms_ = 3 * 60 * 1000;
+int TestTimeouts::huge_test_timeout_ms_ = 10 * 60 * 1000;
+
+// static
+int TestTimeouts::command_execution_timeout_ms_ = 25000;
+
+// static
+int TestTimeouts::wait_for_terminate_timeout_ms_ = 15000;
+
+// static
+int TestTimeouts::live_operation_timeout_ms_ = 30000;
+
+// static
+void TestTimeouts::Initialize() {
+  if (initialized_) {
+    NOTREACHED();
+    return;
+  }
+  initialized_ = true;
+
+  // Note that these timeouts MUST be initialized in the correct order as
+  // per the CHECKS below.
+  InitializeTimeout(switches::kUiTestActionTimeout, &action_timeout_ms_);
+  InitializeTimeout(switches::kUiTestActionMaxTimeout, action_timeout_ms_,
+                    &action_max_timeout_ms_);
+  InitializeTimeout(switches::kTestLargeTimeout, action_max_timeout_ms_,
+                    &large_test_timeout_ms_);
+  InitializeTimeout(switches::kUiTestTimeout, large_test_timeout_ms_,
+                    &huge_test_timeout_ms_);
+
+  // The timeout values should be increasing in the right order.
+  CHECK(action_timeout_ms_ <= action_max_timeout_ms_);
+  CHECK(action_max_timeout_ms_ <= large_test_timeout_ms_);
+  CHECK(large_test_timeout_ms_ <= huge_test_timeout_ms_);
+
+  InitializeTimeout(switches::kUiTestCommandExecutionTimeout,
+                    &command_execution_timeout_ms_);
+  InitializeTimeout(switches::kUiTestTerminateTimeout,
+                    &wait_for_terminate_timeout_ms_);
+
+  InitializeTimeout(switches::kLiveOperationTimeout,
+                    &live_operation_timeout_ms_);
+}
