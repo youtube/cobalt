bea2f51fc402a32eab654b9368530ffc16735c37
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-10 00:21:35 +0000

    Move PerfTestSuite implementation into a .cc file and add it to the base namespace.
    TEST=it compiles
    BUG=none
    Review URL: http://codereview.chromium.org/3695001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62095 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/perf_test_suite.h
+++ base/test/perf_test_suite.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,46 +6,18 @@
 #define BASE_TEST_PERF_TEST_SUITE_H_
 #pragma once
 
-#include "base/command_line.h"
-#include "base/debug_util.h"
-#include "base/file_path.h"
-#include "base/path_service.h"
-#include "base/perftimer.h"
-#include "base/process_util.h"
-#include "base/string_util.h"
 #include "base/test/test_suite.h"
-#include "testing/gtest/include/gtest/gtest.h"
+
+namespace base {
 
 class PerfTestSuite : public TestSuite {
  public:
-  PerfTestSuite(int argc, char** argv) : TestSuite(argc, argv) {
-  }
+  PerfTestSuite(int argc, char** argv);
 
-  virtual void Initialize() {
-    TestSuite::Initialize();
-
-    // Initialize the perf timer log
-    FilePath log_path =
-        CommandLine::ForCurrentProcess()->GetSwitchValuePath("log-file");
-    if (log_path.empty()) {
-      FilePath exe;
-      PathService::Get(base::FILE_EXE, &exe);
-      log_path = exe.ReplaceExtension(FILE_PATH_LITERAL("log"));
-      log_path = log_path.InsertBeforeExtension(FILE_PATH_LITERAL("_perf"));
-    }
-    ASSERT_TRUE(InitPerfLog(log_path));
-
-    // Raise to high priority to have more precise measurements. Since we don't
-    // aim at 1% precision, it is not necessary to run at realtime level.
-    if (!DebugUtil::BeingDebugged())
-      base::RaiseProcessToHighPriority();
-  }
-
-  virtual void Shutdown() {
-    TestSuite::Shutdown();
-
-    FinalizePerfLog();
-  }
+  virtual void Initialize();
+  virtual void Shutdown();
 };
+
+}  // namespace base
 
 #endif  // BASE_TEST_PERF_TEST_SUITE_H_
