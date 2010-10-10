5f66e97f34cffc01e8769d1dda5e157792bb3709
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-10 23:29:16 +0000

    clang: Let everything compile in Release builds.
    Release builds also require http://code.google.com/p/googletest/issues/detail?id=321
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/3700001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62128 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/trace.cc
+++ net/disk_cache/trace.cc
@@ -31,6 +31,7 @@ struct TraceBuffer {
   char buffer[kNumberOfEntries][kEntrySize];
 };
 
+#if ENABLE_TRACING
 void DebugOutput(const char* msg) {
 #if defined(OS_WIN)
   OutputDebugStringA(msg);
@@ -38,6 +39,7 @@ void DebugOutput(const char* msg) {
   NOTIMPLEMENTED();
 #endif
 }
+#endif  // ENABLE_TRACING
 
 }  // namespace
 
