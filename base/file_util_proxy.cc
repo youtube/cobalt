b2d66e60df8b8df079786febc96fdd9e1a9f4744
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-12 02:17:14 +0000

    Return NOT_FOUND error for an attempt to create a file under nonexistent directory.
    BUG=none
    TEST=fast/filesystem/op-get-entry.html
    
    Review URL: http://codereview.chromium.org/3729001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62229 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util_proxy.cc
+++ base/file_util_proxy.cc
@@ -138,6 +138,11 @@ class RelayCreateOrOpen : public MessageLoopRelay {
   }
 
   virtual void RunWork() {
+    if (!file_util::DirectoryExists(file_path_.DirName())) {
+      // If its parent does not exist, should return NOT_FOUND error.
+      set_error_code(base::PLATFORM_FILE_ERROR_NOT_FOUND);
+      return;
+    }
     base::PlatformFileError error_code = base::PLATFORM_FILE_OK;
     file_handle_ = base::CreatePlatformFile(file_path_, file_flags_,
                                             &created_, &error_code);
