5c68056260919fc4b2571200d483a467f2798d21
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-18 19:36:52 +0000

    Convert LOG(INFO) to VLOG(1) - base/.
    Also removes an "else" after "return" and aligns "<<" per style guide.
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/3845002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62959 0039d316-1c4b-4281-b951-d872f2087c98

--- base/observer_list_unittest.cc
+++ base/observer_list_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -126,7 +126,7 @@ class AddRemoveThread : public PlatformThread::Delegate,
     count_addtask_++;
 
     if ((Time::Now() - start_).InMilliseconds() > kThreadRunTime) {
-      LOG(INFO) << "DONE!";
+      VLOG(1) << "DONE!";
       return;
     }
 
