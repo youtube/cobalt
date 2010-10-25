36861a0a75f680185872f9c5b01dfb78419f0f2a
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-25 09:38:56 +0000

    Revert 63648 - Revert 63643 - Add Reset to ClientSocket::UseHistory, and call on TCP disconnect.
    The failures which caused me to revert the original looked sporadic. Relanding.
    
    The HttpProxyClientSocket reuses the same TcpClientSocket in the case that it is trying to establish a tunnel through
    an authenticating proxy, but the proxy closed the TCP connection.
    
    Also, fix SetOmniboxSpeculation on the TCPClientSocket implementations.
    
    BUG=54062
    TEST=Tried normal connections to make sure histograms were only emitted once, as well as https tunnels through an authenticating proxy to ensure that the use_history was correctly reset.
    
    Review URL: http://codereview.chromium.org/3360007
    
    TBR=cbentzel@chromium.org
    Review URL: http://codereview.chromium.org/4006004
    
    TBR=cbentzel@chromium.org
    Review URL: http://codereview.chromium.org/3978008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63716 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/tcp_client_socket_win.h
+++ net/socket/tcp_client_socket_win.h
@@ -117,6 +117,9 @@ class TCPClientSocketWin : public ClientSocket, NonThreadSafe {
 
   BoundNetLog net_log_;
 
+  // This socket was previously disconnected and has not been re-connected.
+  bool previously_disconnected_;
+
   // Record of connectivity and transmissions, for use in speculative connection
   // histograms.
   UseHistory use_history_;
