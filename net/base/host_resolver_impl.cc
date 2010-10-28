b6aab2a86f81eee49db886c03a6e81a72f6a71ed
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-28 04:59:30 +0000

    Revert 64202 - Initial support for built-in DNS resolver/cache.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/3693001
    
    TBR=agayev@chromium.org
    Review URL: http://codereview.chromium.org/4208002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64204 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/host_resolver_impl.cc
+++ net/base/host_resolver_impl.cc
@@ -72,7 +72,6 @@ HostCache* CreateDefaultCache() {
 }  // anonymous namespace
 
 HostResolver* CreateSystemHostResolver(size_t max_concurrent_resolves,
-                                       HostResolverProc* resolver_proc,
                                        NetLog* net_log) {
   // Maximum of 50 concurrent threads.
   // TODO(eroman): Adjust this, do some A/B experiments.
@@ -82,7 +81,7 @@ HostResolver* CreateSystemHostResolver(size_t max_concurrent_resolves,
     max_concurrent_resolves = kDefaultMaxJobs;
 
   HostResolverImpl* resolver =
-      new HostResolverImpl(resolver_proc, CreateDefaultCache(),
+      new HostResolverImpl(NULL, CreateDefaultCache(),
                            max_concurrent_resolves, net_log);
 
   return resolver;
