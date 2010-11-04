cafa07a87a9810961e4a322d9ab92eeaf801a4a9
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-04 23:45:12 +0000

    On Windows, merge X509Certificate::VerifyEV into X509Certificate::Verify, so that we call the Windows CertGetCertificateChain function only once in the common case.  This allows us to do EV verification, with little overhead, even if the caller doesn't ask for it.  This requires adding new methods to the EVRootCAMetadata class.
    R=rvargas
    BUG=41267
    TEST=none
    Review URL: http://codereview.chromium.org/4124012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65139 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/ev_root_ca_metadata.h
+++ net/base/ev_root_ca_metadata.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -40,21 +40,40 @@ class EVRootCAMetadata {
                     PolicyOID* policy_oid) const;
 
   const PolicyOID* GetPolicyOIDs() const { return &policy_oids_[0]; }
+#if defined(OS_WIN)
+  int NumPolicyOIDs() const { return num_policy_oids_; }
+#else
   int NumPolicyOIDs() const { return policy_oids_.size(); }
+#endif
+
+  // Returns true if policy_oid is an EV policy OID of some root CA.
+  bool IsEVPolicyOID(PolicyOID policy_oid) const;
+
+  // Returns true if the root CA with the given certificate fingerprint has
+  // the EV policy OID policy_oid.
+  bool HasEVPolicyOID(const SHA1Fingerprint& fingerprint,
+                      PolicyOID policy_oid) const;
 
  private:
-  EVRootCAMetadata();
-  ~EVRootCAMetadata();
-
   friend struct DefaultSingletonTraits<EVRootCAMetadata>;
 
   typedef std::map<SHA1Fingerprint, PolicyOID,
                    SHA1FingerprintLessThan> PolicyOidMap;
 
+  EVRootCAMetadata();
+  ~EVRootCAMetadata();
+
+  static bool PolicyOIDsAreEqual(PolicyOID a, PolicyOID b);
+
   // Maps an EV root CA cert's SHA-1 fingerprint to its EV policy OID.
   PolicyOidMap ev_policy_;
 
+#if defined(OS_WIN)
+  static const PolicyOID policy_oids_[];
+  int num_policy_oids_;
+#else
   std::vector<PolicyOID> policy_oids_;
+#endif
 
   DISALLOW_COPY_AND_ASSIGN(EVRootCAMetadata);
 };
