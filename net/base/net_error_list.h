cb7078d4c367add64cfa8784475f78cc00ada462
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-05 14:07:46 +0000

    net: Silently disable False Start when NetNanny is detected.
    In r62209 we added code to detect ESET and NetNanny and to give users
    instructions for disabling their HTTPS scanning. This was because these
    products intercept Chrome's HTTPS connections and break because of False
    Start.
    
    NetNanny has been responsive to the issue and is rolling out a fix. In
    the mean time, we silently disable False Start when NetNanny is detected
    to give them time to update their users etc.
    
    BUG=59798
    TEST=Navigate to https://www.paypal.com on Windows with NetNanny installed.
    
    http://codereview.chromium.org/4449003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65199 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/net_error_list.h
+++ net/base/net_error_list.h
@@ -191,10 +191,7 @@ NET_ERROR(SSL_SNAP_START_NPN_MISPREDICTION, -131)
 // give the user a helpful error message rather than have the connection hang.
 NET_ERROR(ESET_ANTI_VIRUS_SSL_INTERCEPTION, -132)
 
-// We detected NetNanny intercepting our HTTPS connections. Since this product
-// is False Start intolerant, we return this error so that we can give the user
-// a helpful error message rather than have the connection hang.
-NET_ERROR(NETNANNY_SSL_INTERCEPTION, -133)
+// Missing -133. Feel free to reuse in the future.
 
 // The permission to use the SSL client certificate's private key was denied.
 NET_ERROR(SSL_CLIENT_AUTH_PRIVATE_KEY_ACCESS_DENIED, -134)
