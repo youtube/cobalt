6d6244ad9d74c625c52d556e5a92890f586e5960
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-10 23:47:56 +0000

    Adding CHECKs to troubleshoot the crash issue intuple.h/browser_render_process_host.cc (see bug description)
     This is chromeos only and will be removed once I correct data.
    
    BUG=chromium-os:7327
    TEST=none
    
    Review URL: http://codereview.chromium.org/4752003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65729 0039d316-1c4b-4281-b951-d872f2087c98

--- base/tuple.h
+++ base/tuple.h
@@ -544,6 +544,13 @@ inline void DispatchToMethod(ObjT* obj, Method method, const A& arg) {
 
 template <class ObjT, class Method, class A>
 inline void DispatchToMethod(ObjT* obj, Method method, const Tuple1<A>& arg) {
+
+#if defined(OS_CHROMEOS) && defined(CHECK)
+  // To troubleshoot crosbug.com/7327.
+  CHECK(obj);
+  CHECK(&arg);
+  CHECK(method);
+#endif
   (obj->*method)(arg.a);
 }
 
