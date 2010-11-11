3f1b9ff87023b0ddd6ab9ba7cdc6aa7029e44251
fbarchard@chromium.org <fbarchard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-11 08:51:52 +0000

    roll ffmpeg to nov 4 to fix clang issue BUG=61931 TEST=none
    Review URL: http://codereview.chromium.org/4770001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65784 0039d316-1c4b-4281-b951-d872f2087c98

--- media/ffmpeg/ffmpeg_unittest.cc
+++ media/ffmpeg/ffmpeg_unittest.cc
@@ -272,7 +272,7 @@ class FFmpegTest : public testing::TestWithParam<const char*> {
       if (result > 0) {
         // TODO(scherkus): move this to ffmpeg_util.h and dedup.
         int64 denominator = av_audio_context()->channels *
-            av_get_bits_per_sample_format(av_audio_context()->sample_fmt) / 8 *
+            av_get_bits_per_sample_fmt(av_audio_context()->sample_fmt) / 8 *
             av_audio_context()->sample_rate;
         double microseconds = size_out /
             (denominator /
