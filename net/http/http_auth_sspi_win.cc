0329c89595bc65fe8e8e4a6d53bfdb1ed7f4479f
danno@chromium.org <danno@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-12 12:47:05 +0000

    Support specifying the GSSAPI library that will be used.
    This preference can be set either via command-line or via group policy.
    
    BUG=53625
    TEST=unittests: ConfigurationPolicyPrefStore*; net_unittests: HttpAuthHandlerNegotiateTest.*:HttpAuthGSSAPIPOSIXTest.*; manually: start Chrome with command-line switch --gssapi-library-name=XYZ and see if this results in the Chrome process loading /usr/lib/whatever/XYZ as soon as an authenticated HTTP site is encountered.
    
    Review URL: http://codereview.chromium.org/4560001
    Patch from Jakob Kummerow <jkummerow@google.com>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65939 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_auth_sspi_win.cc
+++ net/http/http_auth_sspi_win.cc
@@ -9,7 +9,6 @@
 
 #include "base/base64.h"
 #include "base/logging.h"
-#include "base/singleton.h"
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "net/base/net_errors.h"
@@ -423,70 +422,6 @@ int DetermineMaxTokenLength(SSPILibrary* library,
     return rv;
   *max_token_length = token_length;
   return OK;
-}
-
-class SSPILibraryDefault : public SSPILibrary {
- public:
-  SSPILibraryDefault() {}
-  virtual ~SSPILibraryDefault() {}
-
-  virtual SECURITY_STATUS AcquireCredentialsHandle(LPWSTR pszPrincipal,
-                                                   LPWSTR pszPackage,
-                                                   unsigned long fCredentialUse,
-                                                   void* pvLogonId,
-                                                   void* pvAuthData,
-                                                   SEC_GET_KEY_FN pGetKeyFn,
-                                                   void* pvGetKeyArgument,
-                                                   PCredHandle phCredential,
-                                                   PTimeStamp ptsExpiry) {
-    return ::AcquireCredentialsHandle(pszPrincipal, pszPackage, fCredentialUse,
-                                      pvLogonId, pvAuthData, pGetKeyFn,
-                                      pvGetKeyArgument, phCredential,
-                                      ptsExpiry);
-  }
-
-  virtual SECURITY_STATUS InitializeSecurityContext(PCredHandle phCredential,
-                                                    PCtxtHandle phContext,
-                                                    SEC_WCHAR* pszTargetName,
-                                                    unsigned long fContextReq,
-                                                    unsigned long Reserved1,
-                                                    unsigned long TargetDataRep,
-                                                    PSecBufferDesc pInput,
-                                                    unsigned long Reserved2,
-                                                    PCtxtHandle phNewContext,
-                                                    PSecBufferDesc pOutput,
-                                                    unsigned long* contextAttr,
-                                                    PTimeStamp ptsExpiry) {
-    return ::InitializeSecurityContext(phCredential, phContext, pszTargetName,
-                                       fContextReq, Reserved1, TargetDataRep,
-                                       pInput, Reserved2, phNewContext, pOutput,
-                                       contextAttr, ptsExpiry);
-  }
-
-  virtual SECURITY_STATUS QuerySecurityPackageInfo(LPWSTR pszPackageName,
-                                                   PSecPkgInfoW *pkgInfo) {
-    return ::QuerySecurityPackageInfo(pszPackageName, pkgInfo);
-  }
-
-  virtual SECURITY_STATUS FreeCredentialsHandle(PCredHandle phCredential) {
-    return ::FreeCredentialsHandle(phCredential);
-  }
-
-  virtual SECURITY_STATUS DeleteSecurityContext(PCtxtHandle phContext) {
-    return ::DeleteSecurityContext(phContext);
-  }
-
-  virtual SECURITY_STATUS FreeContextBuffer(PVOID pvContextBuffer) {
-    return ::FreeContextBuffer(pvContextBuffer);
-  }
-
- private:
-  friend struct DefaultSingletonTraits<SSPILibraryDefault>;
-};
-
-// static
-SSPILibrary* SSPILibrary::GetDefault() {
-  return Singleton<SSPILibraryDefault>::get();
 }
 
 }  // namespace net
