youtube/cobalt

commit 0b956253dc59ddb84385c3b5ee9fb6a340bc7475

Fix CheckFalseTest.CheckFails on Linux after my change to ui_test.
This makes PathService clear its cache after overriding a path.
We have many paths depending on each other, so this is necessary
to avoid inconsistencies.

TEST=ui_tests in Release mode
BUG=49838

Review URL: http://codereview.chromium.org/2805100

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59021 0039d316-1c4b-4281-b951-d872f2087c98

base/path_service.cc
@@ -31,7 +31,6 @@ namespace base {
 namespace {
 
 typedef base::hash_map<int, FilePath> PathMap;
-typedef base::hash_set<int> PathSet;
 
 // We keep a linked list of providers.  In a debug build we ensure that no two
 // providers claim overlapping keys.
@@ -94,8 +93,8 @@ static Provider base_provider_posix = {
 
 struct PathData {
   Lock      lock;
-  PathMap   cache;      // Track mappings from path key to path value.
-  PathSet   overrides;  // Track whether a path has been overridden.
+  PathMap   cache;      // Cache mappings from path key to path value.
+  PathMap   overrides;  // Track path overrides.
   Provider* providers;  // Linked list of path service providers.
 
   PathData() {
@@ -141,6 +140,20 @@ bool PathService::GetFromCache(int key, FilePath* result) {
 }
 
 // static
+bool PathService::GetFromOverrides(int key, FilePath* result) {
+  PathData* path_data = GetPathData();
+  AutoLock scoped_lock(path_data->lock);
+
+  // check for an overriden version.
+  PathMap::const_iterator it = path_data->overrides.find(key);
+  if (it != path_data->overrides.end()) {
+    *result = it->second;
+    return true;
+  }
+  return false;
+}
+
+// static
 void PathService::AddToCache(int key, const FilePath& path) {
   PathData* path_data = GetPathData();
   AutoLock scoped_lock(path_data->lock);
@@ -163,6 +176,9 @@ bool PathService::Get(int key, FilePath* result) {
     return file_util::GetCurrentDirectory(result);
 
   if (GetFromCache(key, result))
+    return true;
+
+  if (GetFromOverrides(key, result))
     return true;
 
   FilePath path;
@@ -199,14 +215,6 @@ bool PathService::Get(int key, std::wstring* result) {
 }
 #endif
 
-bool PathService::IsOverridden(int key) {
-  PathData* path_data = GetPathData();
-  DCHECK(path_data);
-
-  AutoLock scoped_lock(path_data->lock);
-  return path_data->overrides.find(key) != path_data->overrides.end();
-}
-
 bool PathService::Override(int key, const FilePath& path) {
   PathData* path_data = GetPathData();
   DCHECK(path_data);
@@ -228,8 +236,14 @@ bool PathService::Override(int key, const FilePath& path) {
     return false;
 
   AutoLock scoped_lock(path_data->lock);
+
+  // Clear the cache now. Some of its entries could have depended
+  // on the value we are overriding, and are now out of sync with reality.
+  path_data->cache.clear();
+
   path_data->cache[key] = file_path;
-  path_data->overrides.insert(key);
+  path_data->overrides[key] = file_path;
+
   return true;
 }
 

base/path_service.h
@@ -45,9 +45,6 @@ class PathService {
   // over the lifetime of the app, so this method should be used with caution.
   static bool Override(int key, const FilePath& path);
 
-  // Return whether a path was overridden.
-  static bool IsOverridden(int key);
-
   // To extend the set of supported keys, you can register a path provider,
   // which is just a function mirroring PathService::Get.  The ProviderFunc
   // returns false if it cannot provide a non-empty path for the given key.
@@ -65,6 +62,7 @@ class PathService {
                                int key_end);
  private:
   static bool GetFromCache(int key, FilePath* path);
+  static bool GetFromOverrides(int key, FilePath* path);
   static void AddToCache(int key, const FilePath& path);
 };
 

commit 03d8fc5096f820816b5c1c87d73980786a764dee

linux: fix cache corruption when using multiple --user-data-dir
This regressed in r59021.  I think I can fix it in a cleaner way
with some refactoring, but for now since I want to cherry-pick this
onto older branches let's revert some of r59021 to fix this in
the simplest way possible.

BUG=59824
TEST=manual (will write a test in my followup that refactors it)

Review URL: http://codereview.chromium.org/4911002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66066 0039d316-1c4b-4281-b951-d872f2087c98

base/path_service.cc
@@ -215,6 +215,14 @@ bool PathService::Get(int key, std::wstring* result) {
 }
 #endif
 
+// TODO(evan): remove me -- see comments in header.
+bool PathService::IsOverridden(int key) {
+  PathData* path_data = GetPathData();
+  DCHECK(path_data);
+  AutoLock scoped_lock(path_data->lock);
+  return path_data->overrides.find(key) != path_data->overrides.end();
+}
+
 bool PathService::Override(int key, const FilePath& path) {
   PathData* path_data = GetPathData();
   DCHECK(path_data);

base/path_service.h
@@ -45,6 +45,11 @@ class PathService {
   // over the lifetime of the app, so this method should be used with caution.
   static bool Override(int key, const FilePath& path);
 
+  // Return whether a path was overridden.
+  // TODO(evan): temporarily restoring this to quick-fix a regression.
+  // Remove me again once it's fixed properly.
+  static bool IsOverridden(int key);
+
   // To extend the set of supported keys, you can register a path provider,
   // which is just a function mirroring PathService::Get.  The ProviderFunc
   // returns false if it cannot provide a non-empty path for the given key.