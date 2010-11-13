fd36b338c17d95723068f04b58ee51914edf86d0
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-13 23:44:20 +0000

    Revert 65996 (test breakage) - Disallow Singleton and LazyInstance on non-joinable threads.
    Fix all known instances or explicitly allow them.  Usually the fix involves switching from Default traits to Lazy traits.
    
    BUG=61753
    TEST=none
    
    Review URL: http://codereview.chromium.org/4635012
    
    TBR=willchan@chromium.org
    Review URL: http://codereview.chromium.org/4980001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66071 0039d316-1c4b-4281-b951-d872f2087c98

--- base/lazy_instance.h
+++ base/lazy_instance.h
@@ -36,18 +36,14 @@
 #define BASE_LAZY_INSTANCE_H_
 #pragma once
 
-#include <new>  // For placement new.
 #include "base/atomicops.h"
 #include "base/basictypes.h"
 #include "base/third_party/dynamic_annotations/dynamic_annotations.h"
-#include "base/thread_restrictions.h"
 
 namespace base {
 
 template <typename Type>
 struct DefaultLazyInstanceTraits {
-  static const bool kAllowedToAccessOnNonjoinableThread = false;
-
   static Type* New(void* instance) {
     // Use placement new to initialize our instance in our preallocated space.
     // The parenthesis is very important here to force POD type initialization.
@@ -61,8 +57,6 @@ struct DefaultLazyInstanceTraits {
 
 template <typename Type>
 struct LeakyLazyInstanceTraits {
-  static const bool kAllowedToAccessOnNonjoinableThread = true;
-
   static Type* New(void* instance) {
     return DefaultLazyInstanceTraits<Type>::New(instance);
   }
@@ -118,9 +112,6 @@ class LazyInstance : public LazyInstanceHelper {
   }
 
   Type* Pointer() {
-    if (!Traits::kAllowedToAccessOnNonjoinableThread)
-      base::ThreadRestrictions::AssertSingletonAllowed();
-
     // We will hopefully have fast access when the instance is already created.
     if ((base::subtle::NoBarrier_Load(&state_) != STATE_CREATED) &&
         NeedsInstance()) {
