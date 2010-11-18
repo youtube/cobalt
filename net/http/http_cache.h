f6da449d9ada8ce547061787d5e504a30d8de1fa
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-18 22:26:29 +0000

    Revert "net: Plumb DnsCertProvenanceChecker around."
    This reverts commit r66623. Some how it causes libnspr4.dll to be linked in
    Chrome Frame builds.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66687 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_cache.h
+++ net/http/http_cache.h
@@ -41,7 +41,6 @@ class Entry;
 
 namespace net {
 
-class DnsCertProvenanceChecker;
 class DnsRRResolver;
 class HostResolver;
 class HttpAuthHandlerFactory;
@@ -118,7 +117,6 @@ class HttpCache : public HttpTransactionFactory,
   // The HttpCache takes ownership of the |backend_factory|.
   HttpCache(HostResolver* host_resolver,
             DnsRRResolver* dnsrr_resolver,
-            DnsCertProvenanceChecker* dns_cert_checker,
             ProxyService* proxy_service,
             SSLConfigService* ssl_config_service,
             HttpAuthHandlerFactory* http_auth_handler_factory,
