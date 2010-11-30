1c120a93a08e3c3ac6bfa469cc5f3e824063cb83
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-30 03:55:47 +0000

    Revert 67662 - FBTF: Remove unneeded headers from base/ (part 10)
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/5139006
    
    TBR=thestig@chromium.org
    Review URL: http://codereview.chromium.org/5270010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67674 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket_stream/socket_stream.h
+++ net/socket_stream/socket_stream.h
@@ -14,6 +14,7 @@
 #include "base/ref_counted.h"
 #include "base/scoped_ptr.h"
 #include "base/string16.h"
+#include "base/task.h"
 #include "net/base/address_list.h"
 #include "net/base/completion_callback.h"
 #include "net/base/io_buffer.h"
