3ce59251ee9e7d427a369ce0283ae08159448825
gspencer@chromium.org <gspencer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-03 17:37:54 +0000

    Start using file_util symlink code, now that it's available.
    In CL http://codereview.chromium.org/5349007
    I added some base API for manipulating symlinks (since I needed it for some ChromeOS code and noticed that other places could use it too), and this just starts using that API.
    
    BUG=none
    TEST=Ran ui_tests, passed trybots.
    
    Review URL: http://codereview.chromium.org/5286010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68181 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util_posix.cc
+++ base/file_util_posix.cc
@@ -388,11 +388,12 @@ bool ReadSymbolicLink(const FilePath& symlink_path,
   char buf[PATH_MAX];
   ssize_t count = ::readlink(symlink_path.value().c_str(), buf, arraysize(buf));
 
-  if (count <= 0)
+  if (count <= 0) {
+    target_path->clear();
     return false;
+  }
 
   *target_path = FilePath(FilePath::StringType(buf, count));
-
   return true;
 }
 
