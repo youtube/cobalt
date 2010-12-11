ee5d71d7e197d7e70dd09d9050038919d5c2aba6
satish@chromium.org <satish@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-11 10:23:37 +0000

    Revert 68932 - Make members of Singleton<T> private and only visible to the singleton type. This enforces that the Singleton<T> pattern can only be used within classes which want singleton-ness.
    As part of this CL I have also fixed up files which got missed in my previous CLs to use a GetInstance() method and use Singleton<T> from the source file.
    
    There are a small number of places where I have also switched to LazyInstance as that was more appropriate for types used in a single source file.
    
    BUG=65298
    TEST=all existing tests should continue to pass.
    
    Review URL: http://codereview.chromium.org/5682008
    
    TBR=satish@chromium.org
    Review URL: http://codereview.chromium.org/5721005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68936 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_mac.cc
+++ net/socket/ssl_client_socket_mac.cc
@@ -11,8 +11,8 @@
 
 #include <algorithm>
 
-#include "base/lazy_instance.h"
 #include "base/mac/scoped_cftyperef.h"
+#include "base/singleton.h"
 #include "base/string_util.h"
 #include "net/base/address_list.h"
 #include "net/base/cert_verifier.h"
@@ -475,7 +475,7 @@ class EnabledCipherSuites {
   const std::vector<SSLCipherSuite>& ciphers() const { return ciphers_; }
 
  private:
-  friend struct base::DefaultLazyInstanceTraits<EnabledCipherSuites>;
+  friend struct DefaultSingletonTraits<EnabledCipherSuites>;
   EnabledCipherSuites();
   ~EnabledCipherSuites() {}
 
@@ -483,9 +483,6 @@ class EnabledCipherSuites {
 
   DISALLOW_COPY_AND_ASSIGN(EnabledCipherSuites);
 };
-
-static base::LazyInstance<EnabledCipherSuites> g_enabled_cipher_suites(
-    base::LINKER_INITIALIZED);
 
 EnabledCipherSuites::EnabledCipherSuites() {
   SSLContextRef ssl_context;
@@ -789,7 +786,7 @@ int SSLClientSocketMac::InitializeSSLContext() {
     return NetErrorFromOSStatus(status);
 
   std::vector<SSLCipherSuite> enabled_ciphers =
-      g_enabled_cipher_suites.Get().ciphers();
+      Singleton<EnabledCipherSuites>::get()->ciphers();
 
   CipherSuiteIsDisabledFunctor is_disabled_cipher(
       ssl_config_.disabled_cipher_suites);
