191c77ce00e64a19f806a83c06670a4bb8e14ee0
satish@chromium.org <satish@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-11 09:13:54 +0000

    Make members of Singleton<T> private and only visible to the singleton type. This enforces that the Singleton<T> pattern can only be used within classes which want singleton-ness.
    As part of this CL I have also fixed up files which got missed in my previous CLs to use a GetInstance() method and use Singleton<T> from the source file.
    
    There are a small number of places where I have also switched to LazyInstance as that was more appropriate for types used in a single source file.
    
    BUG=65298
    TEST=all existing tests should continue to pass.
    
    Review URL: http://codereview.chromium.org/5682008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68932 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/bandwidth_metrics.cc
+++ net/base/bandwidth_metrics.cc
@@ -2,14 +2,35 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/singleton.h"
+#include "base/lazy_instance.h"
 #include "net/base/bandwidth_metrics.h"
+
+static base::LazyInstance<net::BandwidthMetrics> g_bandwidth_metrics(
+    base::LINKER_INITIALIZED);
 
 namespace net {
 
 ScopedBandwidthMetrics::ScopedBandwidthMetrics()
-    : metrics_(Singleton<BandwidthMetrics>::get()),
-      started_(false) {
+    : started_(false) {
+}
+
+ScopedBandwidthMetrics::~ScopedBandwidthMetrics() {
+  if (started_)
+    g_bandwidth_metrics.Get().StopStream();
+}
+
+void ScopedBandwidthMetrics::StartStream() {
+  started_ = true;
+  g_bandwidth_metrics.Get().StartStream();
+}
+
+void ScopedBandwidthMetrics::StopStream() {
+  started_ = false;
+  g_bandwidth_metrics.Get().StopStream();
+}
+
+void ScopedBandwidthMetrics::RecordBytes(int bytes) {
+  g_bandwidth_metrics.Get().RecordBytes(bytes);
 }
 
 }  // namespace net
