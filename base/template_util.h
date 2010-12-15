f397623388d799d4d4d9d734d61f9f51cf2a3707
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-15 08:07:02 +0000

    Fix raw_scoped_refptr_mismatch_checker.h.
    BUG=28083
    TEST=none
    
    Review URL: http://codereview.chromium.org/3549010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69237 0039d316-1c4b-4281-b951-d872f2087c98

--- base/template_util.h
+++ base/template_util.h
@@ -6,6 +6,8 @@
 #define BASE_TEMPLATE_UTIL_H_
 #pragma once
 
+#include "build/build_config.h"
+
 namespace base {
 
 // template definitions from tr1
@@ -24,6 +26,51 @@ typedef integral_constant<bool, false> false_type;
 
 template <class T> struct is_pointer : false_type {};
 template <class T> struct is_pointer<T*> : true_type {};
+
+namespace internal {
+
+// Types small_ and big_ are guaranteed such that sizeof(small_) <
+// sizeof(big_)
+typedef char small_;
+
+struct big_ {
+  small_ dummy[2];
+};
+
+#if !defined(OS_WIN)
+
+// This class is an implementation detail for is_convertible, and you
+// don't need to know how it works to use is_convertible. For those
+// who care: we declare two different functions, one whose argument is
+// of type To and one with a variadic argument list. We give them
+// return types of different size, so we can use sizeof to trick the
+// compiler into telling us which function it would have chosen if we
+// had called it with an argument of type From.  See Alexandrescu's
+// _Modern C++ Design_ for more details on this sort of trick.
+
+template <typename From, typename To>
+struct ConvertHelper {
+  static small_ Test(To);
+  static big_ Test(...);
+  static From Create();
+};
+
+#endif  // !defined(OS_WIN)
+
+}  // namespace internal
+
+#if !defined(OS_WIN)
+
+// Inherits from true_type if From is convertible to To, false_type otherwise.
+template <typename From, typename To>
+struct is_convertible
+    : integral_constant<bool,
+                        sizeof(internal::ConvertHelper<From, To>::Test(
+                                  internal::ConvertHelper<From, To>::Create()))
+                        == sizeof(internal::small_)> {
+};
+
+#endif  // !defined(OS_WIN)
 
 }  // namespace base
 
