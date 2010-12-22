2948819611d7bf4a0ab425b8c8b9801589ea3803
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-22 15:08:08 +0000

    Update the time formatting APIs to use string16.
    BUG=23581
    TEST=everything still works
    
    Review URL: http://codereview.chromium.org/6064003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69948 0039d316-1c4b-4281-b951-d872f2087c98

--- base/i18n/time_formatting.h
+++ base/i18n/time_formatting.h
@@ -9,32 +9,32 @@
 #define BASE_I18N_TIME_FORMATTING_H_
 #pragma once
 
-#include <string>
+#include "base/string16.h"
 
 namespace base {
 
 class Time;
 
 // Returns the time of day, e.g., "3:07 PM".
-std::wstring TimeFormatTimeOfDay(const Time& time);
+string16 TimeFormatTimeOfDay(const Time& time);
 
 // Returns a shortened date, e.g. "Nov 7, 2007"
-std::wstring TimeFormatShortDate(const Time& time);
+string16 TimeFormatShortDate(const Time& time);
 
 // Returns a numeric date such as 12/13/52.
-std::wstring TimeFormatShortDateNumeric(const Time& time);
+string16 TimeFormatShortDateNumeric(const Time& time);
 
 // Formats a time in a friendly sentence format, e.g.
 // "Monday, March 6, 2008 2:44:30 PM".
-std::wstring TimeFormatShortDateAndTime(const Time& time);
+string16 TimeFormatShortDateAndTime(const Time& time);
 
 // Formats a time in a friendly sentence format, e.g.
 // "Monday, March 6, 2008 2:44:30 PM".
-std::wstring TimeFormatFriendlyDateAndTime(const Time& time);
+string16 TimeFormatFriendlyDateAndTime(const Time& time);
 
 // Formats a time in a friendly sentence format, e.g.
 // "Monday, March 6, 2008".
-std::wstring TimeFormatFriendlyDate(const Time& time);
+string16 TimeFormatFriendlyDate(const Time& time);
 
 }  // namespace base
 
