202166c13d744541767737167bea05027caa9dce
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-30 01:01:28 +0000

    net: Add namespace net to URLRequestFileDirJob.
    BUG=64263
    TEST=compiled locally and trybots
    
    Review URL: http://codereview.chromium.org/5958008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70291 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_file_dir_job.h
+++ net/url_request/url_request_file_dir_job.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NET_URL_REQUEST_URL_REQUEST_FILE_DIR_JOB_H__
-#define NET_URL_REQUEST_URL_REQUEST_FILE_DIR_JOB_H__
+#ifndef NET_URL_REQUEST_URL_REQUEST_FILE_DIR_JOB_H_
+#define NET_URL_REQUEST_URL_REQUEST_FILE_DIR_JOB_H_
 #pragma once
 
 #include <string>
@@ -14,23 +14,25 @@
 #include "net/base/directory_lister.h"
 #include "net/url_request/url_request_job.h"
 
-class URLRequestFileDirJob
-  : public net::URLRequestJob,
-    public net::DirectoryLister::DirectoryListerDelegate {
- public:
-  URLRequestFileDirJob(net::URLRequest* request, const FilePath& dir_path);
+namespace net {
 
-  // net::URLRequestJob methods:
+class URLRequestFileDirJob
+  : public URLRequestJob,
+    public DirectoryLister::DirectoryListerDelegate {
+ public:
+  URLRequestFileDirJob(URLRequest* request, const FilePath& dir_path);
+
+  // Overridden from URLRequestJob:
   virtual void Start();
   virtual void StartAsync();
   virtual void Kill();
-  virtual bool ReadRawData(net::IOBuffer* buf, int buf_size, int *bytes_read);
+  virtual bool ReadRawData(IOBuffer* buf, int buf_size, int *bytes_read);
   virtual bool GetMimeType(std::string* mime_type) const;
   virtual bool GetCharset(std::string* charset);
 
-  // DirectoryLister::DirectoryListerDelegate methods:
+  // Overridden from DirectoryLister::DirectoryListerDelegate:
   virtual void OnListFile(
-      const net::DirectoryLister::DirectoryListerData& data);
+      const DirectoryLister::DirectoryListerData& data);
   virtual void OnListDone(int error);
 
   bool list_complete() const { return list_complete_; }
@@ -47,7 +49,7 @@ class URLRequestFileDirJob
   // Fills a buffer with the output.
   bool FillReadBuffer(char *buf, int buf_size, int *bytes_read);
 
-  scoped_refptr<net::DirectoryLister> lister_;
+  scoped_refptr<DirectoryLister> lister_;
   FilePath dir_path_;
   std::string data_;
   bool canceled_;
@@ -62,11 +64,13 @@ class URLRequestFileDirJob
   // we wait for IO to complete.  When done, we fill the buffer
   // manually.
   bool read_pending_;
-  scoped_refptr<net::IOBuffer> read_buffer_;
+  scoped_refptr<IOBuffer> read_buffer_;
   int read_buffer_length_;
   ScopedRunnableMethodFactory<URLRequestFileDirJob> method_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(URLRequestFileDirJob);
 };
 
-#endif  // NET_URL_REQUEST_URL_REQUEST_FILE_DIR_JOB_H__
+}  // namespace net
+
+#endif  // NET_URL_REQUEST_URL_REQUEST_FILE_DIR_JOB_H_
