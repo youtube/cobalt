6698452d985175cc04fc44a35a06cd797651c659
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-01 19:55:22 +0000

    Move base/debug_on_start to base/debugger and rename it to be _win since it only does anything on Windows.
    TEST=it compiles
    BUG=none
    Review URL: http://codereview.chromium.org/5984007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70362 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/test_suite.cc
+++ base/test/test_suite.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,7 +8,7 @@
 #include "base/base_paths.h"
 #include "base/base_switches.h"
 #include "base/command_line.h"
-#include "base/debug_on_start.h"
+#include "base/debug/debug_on_start_win.h"
 #include "base/debug/debugger.h"
 #include "base/file_path.h"
 #include "base/i18n/icu_util.h"
