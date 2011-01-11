youtube/cobalt

commit 35cd170520ea47c3bd7301c8f46387dc9b6d6d57

Made ObserverListThreadSafe's destructor private since it's ref-counted.
BUG=None
TEST=Compiles

Review URL: http://codereview.chromium.org/3832006

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70933 0039d316-1c4b-4281-b951-d872f2087c98

base/observer_list_threadsafe.h
@@ -60,13 +60,6 @@ class ObserverListThreadSafe
       : type_(ObserverListBase<ObserverType>::NOTIFY_ALL) {}
   explicit ObserverListThreadSafe(NotificationType type) : type_(type) {}
 
-  ~ObserverListThreadSafe() {
-    typename ObserversListMap::const_iterator it;
-    for (it = observer_lists_.begin(); it != observer_lists_.end(); ++it)
-      delete (*it).second;
-    observer_lists_.clear();
-  }
-
   // Add an observer to the list.
   void AddObserver(ObserverType* obs) {
     ObserverList<ObserverType>* list = NULL;
@@ -137,6 +130,15 @@ class ObserverListThreadSafe
   // TODO(mbelshe):  Add more wrappers for Notify() with more arguments.
 
  private:
+  friend class
+      base::RefCountedThreadSafe<ObserverListThreadSafe<ObserverType> >;
+  ~ObserverListThreadSafe() {
+    typename ObserversListMap::const_iterator it;
+    for (it = observer_lists_.begin(); it != observer_lists_.end(); ++it)
+      delete (*it).second;
+    observer_lists_.clear();
+  }
+
   template <class Method, class Params>
   void Notify(const UnboundMethod<ObserverType, Method, Params>& method) {
     AutoLock lock(list_lock_);

commit 119b714a44c157ce7fd71e93408b0771a642b241

Add workaround for VS2005 compile error introduced by r70933
BUG=None
TEST=compiles on VS2005

Review URL: http://codereview.chromium.org/6198003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71081 0039d316-1c4b-4281-b951-d872f2087c98

base/observer_list_threadsafe.h
@@ -49,9 +49,32 @@
 //   will notify its regular ObserverList.
 //
 ///////////////////////////////////////////////////////////////////////////////
+
+// Forward declaration for ObserverListThreadSafeTraits.
+template <class ObserverType>
+class ObserverListThreadSafe;
+
+// This class is used to work around VS2005 not accepting:
+//
+// friend class
+//     base::RefCountedThreadSafe<ObserverListThreadSafe<ObserverType> >;
+//
+// Instead of friending the class, we could friend the actual function
+// which calls delete.  However, this ends up being
+// RefCountedThreadSafe::DeleteInternal(), which is private.  So we
+// define our own templated traits class so we can friend it.
+template <class T>
+struct ObserverListThreadSafeTraits {
+  static void Destruct(const ObserverListThreadSafe<T>* x) {
+    delete x;
+  }
+};
+
 template <class ObserverType>
 class ObserverListThreadSafe
-    : public base::RefCountedThreadSafe<ObserverListThreadSafe<ObserverType> > {
+    : public base::RefCountedThreadSafe<
+        ObserverListThreadSafe<ObserverType>,
+        ObserverListThreadSafeTraits<ObserverType> > {
  public:
   typedef typename ObserverList<ObserverType>::NotificationType
       NotificationType;
@@ -130,8 +153,9 @@ class ObserverListThreadSafe
   // TODO(mbelshe):  Add more wrappers for Notify() with more arguments.
 
  private:
-  friend class
-      base::RefCountedThreadSafe<ObserverListThreadSafe<ObserverType> >;
+  // See comment above ObserverListThreadSafeTraits' definition.
+  friend struct ObserverListThreadSafeTraits<ObserverType>;
+
   ~ObserverListThreadSafe() {
     typename ObserversListMap::const_iterator it;
     for (it = observer_lists_.begin(); it != observer_lists_.end(); ++it)