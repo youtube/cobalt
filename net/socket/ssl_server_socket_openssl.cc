be4fb9c1caefab723bbed40761cde12975ebe3e1
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-27 11:21:07 +0000

    Abstracts SHA256 context for NSS / OpenSSL. Stubs out SslServerSocket for OpenSSL.
    BUG=none
    TEST=Sha256Test.TestContext (and compiles with openssl flag).
    
    Review URL: http://codereview.chromium.org/6276002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72782 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/socket/ssl_server_socket_openssl.cc
@@ -0,0 +1,59 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/logging.h"
+#include "net/socket/ssl_server_socket.h"
+
+namespace net {
+
+namespace {
+
+class SSLServerSocketOpenSSL : public SSLServerSocket {
+ public:
+  virtual ~SSLServerSocketOpenSSL() {}
+
+  // SSLServerSocket
+  virtual int Accept(CompletionCallback* callback) {
+    // TODO(bulach): implement.
+    NOTIMPLEMENTED();
+    return 0;
+  }
+
+  // Socket
+  virtual int Read(IOBuffer* buf, int buf_len,
+                   CompletionCallback* callback) {
+    // TODO(bulach): implement.
+    NOTIMPLEMENTED();
+    return 0;
+  }
+  virtual int Write(IOBuffer* buf, int buf_len,
+                    CompletionCallback* callback) {
+    // TODO(bulach): implement.
+    NOTIMPLEMENTED();
+    return 0;
+  }
+
+  virtual bool SetReceiveBufferSize(int32 size) {
+    // TODO(bulach): implement.
+    NOTIMPLEMENTED();
+    return false;
+  }
+
+  virtual bool SetSendBufferSize(int32 size) {
+    // TODO(bulach): implement.
+    NOTIMPLEMENTED();
+    return false;
+  }
+};
+
+}  // namespace
+
+SSLServerSocket* CreateSSLServerSocket(Socket* socket,
+                                       X509Certificate* certificate,
+                                       base::RSAPrivateKey* key,
+                                       const SSLConfig& ssl_config) {
+  return new SSLServerSocketOpenSSL();
+}
+
+}  // namespace net
