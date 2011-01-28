d9189a93edc3450f6daa3958924f7f38c44b9980
suzhe@google.com <suzhe@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-28 02:40:46 +0000

    Change UTF8ToUTF16 to accept const StringPiece&.
    BUG=70936
    TEST=All unit tests should pass.
    
    Review URL: http://codereview.chromium.org/6317016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72921 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/dump_cache/url_to_filename_encoder.cc
+++ net/tools/dump_cache/url_to_filename_encoder.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <stdlib.h>
+
 #include "base/logging.h"
 #include "base/string_util.h"
 #include "net/base/net_util.h"
