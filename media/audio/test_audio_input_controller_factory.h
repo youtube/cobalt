934903e3686374318da694dc8371ec0c0e8e8e14
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-07 17:55:50 +0000

    Clean up warnings found in our testing code.
    Time wise, this only speeds things up by a minute, which I think is within the
    margin of error. I can't get a good size measurement since we aren't building
    .a files for a lot of this code.
    
    BUG=none
    TEST=compiles
    
    Review URL: http://codereview.chromium.org/6312137
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73999 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/test_audio_input_controller_factory.h
+++ media/audio/test_audio_input_controller_factory.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -72,7 +72,7 @@ class TestAudioInputControllerFactory : public AudioInputController::Factory {
   TestAudioInputControllerFactory();
 
   // AudioInputController::Factory methods.
-  AudioInputController* Create(
+  virtual AudioInputController* Create(
       AudioInputController::EventHandler* event_handler,
       AudioParameters params);
 
