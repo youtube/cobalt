988d5fd84e5f679bdf42922a58545b0a75444626
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-11 19:48:10 +0000

    GTTF: Prefetch the browser with a longer timeout before running browser tests.
    Also remove hacky code to increase timeout for specific tests.
    Instead, allow to increase test termination timeout
    via SLOW_ prefix (should be used as last resort).
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/6410059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74649 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/test_timeouts.cc
+++ base/test/test_timeouts.cc
@@ -44,7 +44,7 @@ bool TestTimeouts::initialized_ = false;
 // static
 int TestTimeouts::tiny_timeout_ms_ = 100;
 int TestTimeouts::action_timeout_ms_ = 2000;
-int TestTimeouts::action_max_timeout_ms_ = 15000;
+int TestTimeouts::action_max_timeout_ms_ = 20000;
 int TestTimeouts::large_test_timeout_ms_ = 3 * 60 * 1000;
 int TestTimeouts::huge_test_timeout_ms_ = 10 * 60 * 1000;
 
