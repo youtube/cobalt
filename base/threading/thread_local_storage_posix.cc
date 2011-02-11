fc0ce274a5cca9795fa439e3854470bbc2830342
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-11 16:02:27 +0000

    Initialize members of ThreadLocalStorage::Slot.
    BUG=None
    TEST=None
    CID=14347
    
    Review URL: http://codereview.chromium.org/6476028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74593 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/thread_local_storage_posix.cc
+++ base/threading/thread_local_storage_posix.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,7 +9,8 @@
 namespace base {
 
 ThreadLocalStorage::Slot::Slot(TLSDestructorFunc destructor)
-    : initialized_(false) {
+    : initialized_(false),
+      key_(0) {
   Initialize(destructor);
 }
 
