4d75d90c9c04c7ec10203a2dc031c8a7f280d9d6
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-16 18:56:09 +0000

    Revert 75143 - Reland r74842
    The bug was that the ProfilePolicyContext was getting initialized before the ProfileImplIOData::Handle was initialized.
    
    BUG=67237
    TEST=none
    
    Review URL: http://codereview.chromium.org/6526029
    
    TBR=willchan@chromium.org
    Review URL: http://codereview.chromium.org/6480120
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75152 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_context.cc
+++ net/url_request/url_request_context.cc
@@ -7,24 +7,22 @@
 #include "base/string_util.h"
 #include "net/base/cookie_store.h"
 #include "net/base/host_resolver.h"
-#include "net/ftp/ftp_transaction_factory.h"
-#include "net/http/http_transaction_factory.h"
 
 namespace net {
 
 URLRequestContext::URLRequestContext()
-    : is_main_(false),
-      net_log_(NULL),
+    : net_log_(NULL),
       host_resolver_(NULL),
       cert_verifier_(NULL),
       dnsrr_resolver_(NULL),
       dns_cert_checker_(NULL),
+      http_transaction_factory_(NULL),
+      ftp_transaction_factory_(NULL),
       http_auth_handler_factory_(NULL),
       network_delegate_(NULL),
       cookie_policy_(NULL),
       transport_security_state_(NULL),
-      http_transaction_factory_(NULL),
-      ftp_transaction_factory_(NULL) {
+      is_main_(false) {
 }
 
 void URLRequestContext::set_cookie_store(CookieStore* cookie_store) {
