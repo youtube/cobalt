31c0d3e5d3a18c0c03ca27d931c690c5dfff929b
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-24 20:23:03 +0000

    Mac specific chromium-style issues now that the trybot is sane.
    BUG=carnitas
    TEST=compiles
    
    Review URL: http://codereview.chromium.org/6575020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75939 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_mac.cc
+++ net/socket/ssl_client_socket_mac.cc
@@ -618,6 +618,10 @@ int SSLClientSocketMac::GetPeerAddress(AddressList* address) const {
   return transport_->socket()->GetPeerAddress(address);
 }
 
+const BoundNetLog& SSLClientSocketMac::NetLog() const {
+  return net_log_;
+}
+
 void SSLClientSocketMac::SetSubresourceSpeculation() {
   if (transport_.get() && transport_->socket()) {
     transport_->socket()->SetSubresourceSpeculation();
