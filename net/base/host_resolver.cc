531f1203b4d8630cf693a42fa8697c703bec634b
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-02 08:09:40 +0000

    Add a flag to a host resolver request to request that resolution only be done from the cache (i.e. don't do a network lookup, and return results only synchronously).
    BUG=none
    TEST=HostResolverImplTest::DisallowNonCachedResponses
    
    
    
    Review URL: http://codereview.chromium.org/6602038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76515 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/host_resolver.cc
+++ net/base/host_resolver.cc
@@ -15,6 +15,7 @@ HostResolver::RequestInfo::RequestInfo(const HostPortPair& host_port_pair)
       address_family_(ADDRESS_FAMILY_UNSPECIFIED),
       host_resolver_flags_(0),
       allow_cached_response_(true),
+      only_use_cached_response_(false),
       is_speculative_(false),
       priority_(MEDIUM) {
 }
