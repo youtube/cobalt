64e4b0e89c07d429ba3ed81d899291d21a7d47c1
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-03 23:38:51 +0000

    Modify ThreadChecker and NonThreadSafe so that their functionality is available in release builds if explicitly requested by using their Impl types. The default usage remains that they do nothing in release mode.
    Also, update unit tests to run in release mode, verifying that the release versions of NonThreadSafe and ThreadChecker do nothing in release builds.
    
    BUG=none
    TEST=base unit tests
    
    Review URL: http://codereview.chromium.org/6599004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76833 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/non_thread_safe.h
+++ base/threading/non_thread_safe.h
@@ -6,13 +6,29 @@
 #define BASE_THREADING_NON_THREAD_SAFE_H_
 #pragma once
 
-#include "base/threading/thread_checker.h"
+#ifndef NDEBUG
+#include "base/threading/non_thread_safe_impl.h"
+#endif
 
 namespace base {
 
-// A helper class used to help verify that methods of a class are
-// called from the same thread.  One can inherit from this class and use
-// CalledOnValidThread() to verify.
+// Do nothing implementation of NonThreadSafe, for release mode.
+//
+// Note: You should almost always use the NonThreadSafe class to get
+// the right version of the class for your build configuration.
+class NonThreadSafeDoNothing {
+ public:
+  bool CalledOnValidThread() const {
+    return true;
+  }
+
+ protected:
+  void DetachFromThread() {}
+};
+
+// NonThreadSafe is a helper class used to help verify that methods of a
+// class are called from the same thread.  One can inherit from this class
+// and use CalledOnValidThread() to verify.
 //
 // This is intended to be used with classes that appear to be thread safe, but
 // aren't.  For example, a service or a singleton like the preferences system.
@@ -29,33 +45,10 @@ namespace base {
 // In Release mode, CalledOnValidThread will always return true.
 //
 #ifndef NDEBUG
-class NonThreadSafe {
- public:
-  ~NonThreadSafe();
-
-  bool CalledOnValidThread() const;
-
- protected:
-  // Changes the thread that is checked for in CalledOnValidThread. The next
-  // call to CalledOnValidThread will attach this class to a new thread. It is
-  // up to the NonThreadSafe derived class to decide to expose this or not.
-  // This may be useful when an object may be created on one thread and then
-  // used exclusively on another thread.
-  void DetachFromThread();
-
- private:
-  ThreadChecker thread_checker_;
+class NonThreadSafe : public NonThreadSafeImpl {
 };
 #else
-// Do nothing in release mode.
-class NonThreadSafe {
- public:
-  bool CalledOnValidThread() const {
-    return true;
-  }
-
- protected:
-  void DetachFromThread() {}
+class NonThreadSafe : public NonThreadSafeDoNothing {
 };
 #endif  // NDEBUG
 
--- base/threading/thread_checker.h
+++ base/threading/thread_checker.h
@@ -7,22 +7,34 @@
 #pragma once
 
 #ifndef NDEBUG
-#include "base/synchronization/lock.h"
-#include "base/threading/platform_thread.h"
-#endif // NDEBUG
+#include "base/threading/thread_checker_impl.h"
+#endif
 
 namespace base {
+
+// Do nothing implementation, for use in release mode.
+//
+// Note: You should almost always use the ThreadChecker class to get the
+// right version for your build configuration.
+class ThreadCheckerDoNothing {
+ public:
+  bool CalledOnValidThread() const {
+    return true;
+  }
+
+  void DetachFromThread() {}
+};
 
 // Before using this class, please consider using NonThreadSafe as it
 // makes it much easier to determine the nature of your class.
 //
-// A helper class used to help verify that some methods of a class are
-// called from the same thread.  One can inherit from this class and use
-// CalledOnValidThread() to verify.
+// ThreadChecker is a helper class used to help verify that some methods of a
+// class are called from the same thread.  One can inherit from this class and
+// use CalledOnValidThread() to verify.
 //
-// Inheriting from class indicates that one must be careful when using the class
-// with multiple threads. However, it is up to the class document to indicate
-// how it can be used with threads.
+// Inheriting from class indicates that one must be careful when using the
+// class with multiple threads. However, it is up to the class document to
+// indicate how it can be used with threads.
 //
 // Example:
 // class MyClass : public ThreadChecker {
@@ -34,37 +46,11 @@ namespace base {
 // }
 //
 // In Release mode, CalledOnValidThread will always return true.
-//
 #ifndef NDEBUG
-class ThreadChecker {
- public:
-  ThreadChecker();
-  ~ThreadChecker();
-
-  bool CalledOnValidThread() const;
-
-  // Changes the thread that is checked for in CalledOnValidThread.  This may
-  // be useful when an object may be created on one thread and then used
-  // exclusively on another thread.
-  void DetachFromThread();
-
- private:
-  void EnsureThreadIdAssigned() const;
-
-  mutable base::Lock lock_;
-  // This is mutable so that CalledOnValidThread can set it.
-  // It's guarded by |lock_|.
-  mutable PlatformThreadId valid_thread_id_;
+class ThreadChecker : public ThreadCheckerImpl {
 };
 #else
-// Do nothing in release mode.
-class ThreadChecker {
- public:
-  bool CalledOnValidThread() const {
-    return true;
-  }
-
-  void DetachFromThread() {}
+class ThreadChecker : public ThreadCheckerDoNothing {
 };
 #endif  // NDEBUG
 
