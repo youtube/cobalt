06cfb1cd1fd5a4186fb0efeeec6dbd7e77884cc4
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-06 17:06:54 +0000

    Fix clang after r77075.
    BUG=none
    TEST=builds
    
    Review URL: http://codereview.chromium.org/6626044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77077 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_http_stream.h
+++ net/spdy/spdy_http_stream.h
@@ -65,7 +65,7 @@ class SpdyHttpStream : public SpdyStream::Delegate, public HttpStream {
   virtual void SetConnectionReused();
   virtual void GetSSLInfo(SSLInfo* ssl_info);
   virtual void GetSSLCertRequestInfo(SSLCertRequestInfo* cert_request_info);
-  virtual bool IsSpdyHttpStream() const { return true; }
+  virtual bool IsSpdyHttpStream() const;
 
   // SpdyStream::Delegate methods:
   virtual bool OnSendHeadersComplete(int status);
