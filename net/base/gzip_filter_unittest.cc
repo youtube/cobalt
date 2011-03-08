df7c0bd0cbf49fa4669fd5cab1473de11a698e8e
adamk@chromium.org <adamk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-08 23:44:22 +0000

    Put Filter into namespace net.
    BUG=64263
    TEST=existing unit_tests
    
    Review URL: http://codereview.chromium.org/6639002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77373 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/gzip_filter_unittest.cc
+++ net/base/gzip_filter_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -52,6 +52,8 @@ enum EncodeMode {
 };
 
 }  // namespace
+
+namespace net {
 
 // These tests use the path service, which uses autoreleased objects on the
 // Mac, so this needs to be a PlatformTest.
@@ -391,3 +393,5 @@ TEST_F(GZipUnitTest, DecodeCorruptedHeader) {
   // Expect failures
   EXPECT_TRUE(code == Filter::FILTER_ERROR);
 }
+
+}  // namespace net
