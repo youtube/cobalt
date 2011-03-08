a9b6fa49a6d23d3ac317d57efffae4a176e4d85f
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-08 00:48:45 +0000

    Add shell script based unit test system for the clang plugin.
    The scripts compile a c++ test case and compares the compiler output against a golden file.
    
    BUG=carnitas
    TEST=(For developers only; QA ignore): ./test.sh <path to Release+Asserts>
    
    Review URL: http://codereview.chromium.org/6623065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77217 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/clang/plugins/tests/test.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Hacky, primitive testing: This runs the style plugin for a set of input files
+# and compares the output with golden result files.
+
+E_BADARGS=65
+
+# Prints usage information.
+function usage() {
+  echo "Usage: `basename $0` <Path to the llvm build dir, usually Release+Asserts>"
+  echo ""
+  echo "  Runs all the libFindBadConstructs unit tests"
+  echo ""
+}
+
+# Runs a single test case.
+function do_testcase {
+  local output=`${CLANG_DIR}/bin/clang -cc1 \
+      -load ${CLANG_DIR}/lib/libFindBadConstructs.so \
+      -plugin find-bad-constructs ${1} 2>&1`
+  local diffout=`echo "${output}" | diff - ${2}`
+  if [[ ${diffout} == "" ]]; then
+    echo "PASS: ${1}"
+  else
+    echo "FAIL: ${1}"
+    echo "Output of compiler:"
+    echo "${output}"
+  fi
+}
+
+# Validate input to the script.
+if [[ -z "${1}" ]]; then
+  usage
+  exit ${E_BADARGS}
+elif [[ ! -d "${1}" ]]; then
+  echo "${1} is not a directory."
+  usage
+  exit ${E_BADARGS}
+else
+  echo "Using clang directory ${1}..."
+  export CLANG_DIR="${1}"
+fi
+
+for input in *.cpp; do
+  do_testcase $input ${input%cpp}txt
+done
