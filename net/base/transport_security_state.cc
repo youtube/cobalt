b3fd3aeacb65510c4cf3b5261c209ccea3d66d00
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-09 19:35:00 +0000

    Add Docs-related properties to HSTS list, with permission from that team.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6648026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77500 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/transport_security_state.cc
+++ net/base/transport_security_state.cc
@@ -469,6 +469,9 @@ bool TransportSecurityState::IsPreloadedSTS(
     {19, true, "\006chrome\006google\003com"},
     {28, false, "\016aladdinschools\007appspot\003com"},
     {14, true, "\011ottospora\002nl"},
+    {17, true, "\004docs\006google\003com"},
+    {18, true, "\005sites\006google\003com"},
+    {25, true, "\014spreadsheets\006google\003com"},
   };
   static const size_t kNumPreloadedSTS = ARRAYSIZE_UNSAFE(kPreloadedSTS);
 
