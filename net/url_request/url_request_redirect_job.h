5249fdedd93f6ad6ad61e41da638a0d30bbf488f
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-11 17:45:05 +0000

    Coverity: Fix unnecessary call by values.
    CID 13152, 14252
    
    BUG=NONE
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/6665016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77826 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_redirect_job.h
+++ net/url_request/url_request_redirect_job.h
@@ -18,7 +18,7 @@ namespace net {
 class URLRequestRedirectJob : public URLRequestJob {
  public:
   // Constructs a job that redirects to the specified URL.
-  URLRequestRedirectJob(URLRequest* request, GURL redirect_destination);
+  URLRequestRedirectJob(URLRequest* request, const GURL& redirect_destination);
 
   virtual void Start();
   virtual bool IsRedirectResponse(GURL* location, int* http_status_code);
