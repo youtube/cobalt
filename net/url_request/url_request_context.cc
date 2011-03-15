dee4d1c69aeecd23af622d81aa229efb190a5608
creis@google.com <creis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-15 22:45:09 +0000

    Initial support for partitioning cookies for isolated apps. This CL adds experimental support for letting installed apps request isolated storage in their manifest.  An isolated app will have its own cookie store that is not shared with other apps or normal pages, even if they share an origin.  The feature is currently behind a --enable-experimental-app-manifests flag.
    BUG=69335
    TEST=ExtensionManifestTest.IsolatedApps
    TEST=IsolatedAppApiTest.CookieIsolation*
    
    Review URL: http://codereview.chromium.org/6201005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78301 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_context.cc
+++ net/url_request/url_request_context.cc
@@ -27,6 +27,29 @@ URLRequestContext::URLRequestContext()
       ftp_transaction_factory_(NULL) {
 }
 
+void URLRequestContext::CopyFrom(URLRequestContext* other) {
+  // Copy URLRequestContext parameters.
+  // Do not copy is_main_.
+  set_net_log(other->net_log());
+  set_host_resolver(other->host_resolver());
+  set_cert_verifier(other->cert_verifier());
+  set_dnsrr_resolver(other->dnsrr_resolver());
+  set_dns_cert_checker(other->dns_cert_checker());
+  set_http_auth_handler_factory(other->http_auth_handler_factory());
+  set_proxy_service(other->proxy_service());
+  set_ssl_config_service(other->ssl_config_service());
+  set_network_delegate(other->network_delegate());
+  set_cookie_store(other->cookie_store());
+  set_cookie_policy(other->cookie_policy());
+  set_transport_security_state(other->transport_security_state());
+  // FTPAuthCache is unique per context.
+  set_accept_language(other->accept_language());
+  set_accept_charset(other->accept_charset());
+  set_referrer_charset(other->referrer_charset());
+  set_http_transaction_factory(other->http_transaction_factory());
+  set_ftp_transaction_factory(other->ftp_transaction_factory());
+}
+
 void URLRequestContext::set_cookie_store(CookieStore* cookie_store) {
   cookie_store_ = cookie_store;
 }
