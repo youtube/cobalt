6158752c6b3fb5494ca0867d494bb75eb35eeff7
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-16 20:43:06 +0000

    Include the right cpu config file for native client. Review URL: http://codereview.chromium.org/6673068
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78423 0039d316-1c4b-4281-b951-d872f2087c98

--- base/third_party/nspr/prcpucfg.h
+++ base/third_party/nspr/prcpucfg.h
@@ -34,7 +34,9 @@
 #include "base/third_party/nspr/prcpucfg_win.h"
 #elif defined(__APPLE__)
 #include "base/third_party/nspr/prcpucfg_mac.h"
-#elif defined(__linux__) || defined(__native_client__)
+#elif defined(__native_client__)
+#include "base/third_party/nspr/prcpucfg_nacl.h"
+#elif defined(__linux__)
 #include "base/third_party/nspr/prcpucfg_linux.h"
 #elif defined(__FreeBSD__)
 #include "base/third_party/nspr/prcpucfg_freebsd.h"
