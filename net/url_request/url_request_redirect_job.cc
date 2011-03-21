d712ab020a7ea63a2e96491c32e1e282aa7b9164
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-21 19:11:20 +0000

    Fix up the last easy URLRequestJob refcounting offenders.
    Also clean up net:: use in URLRequestJob while I'm there.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/6709050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78902 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_redirect_job.cc
+++ net/url_request/url_request_redirect_job.cc
@@ -1,21 +1,24 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "net/url_request/url_request_redirect_job.h"
 
+#include "base/compiler_specific.h"
 #include "base/message_loop.h"
 
 namespace net {
 
 URLRequestRedirectJob::URLRequestRedirectJob(URLRequest* request,
                                              const GURL& redirect_destination)
-    : URLRequestJob(request), redirect_destination_(redirect_destination) {
-}
+    : URLRequestJob(request),
+      redirect_destination_(redirect_destination),
+      ALLOW_THIS_IN_INITIALIZER_LIST(method_factory_(this)) {}
 
 void URLRequestRedirectJob::Start() {
-  MessageLoop::current()->PostTask(FROM_HERE, NewRunnableMethod(
-      this, &URLRequestRedirectJob::StartAsync));
+  MessageLoop::current()->PostTask(
+      FROM_HERE,
+      method_factory_.NewRunnableMethod(&URLRequestRedirectJob::StartAsync));
 }
 
 bool URLRequestRedirectJob::IsRedirectResponse(GURL* location,
