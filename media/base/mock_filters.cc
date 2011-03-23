4b5c47ebd47fb92d753268c71b88113201c76b07
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-23 18:27:21 +0000

    Revert 79149 - Build a raw video pipeline for the <video> with a special src attribute (media://...). The raw video pipeline graph only has two filters - one pass-thru decoder filter and one renderer filter.
    Contributed by ronghuawu@google.com
    
    Original code reviews:
    http://codereview.chromium.org/6658001/ (pipeline changes)
    http://codereview.chromium.org/6621049/ (pass-thru filter)
    
    BUG=none
    TEST=media_unittests
    
    Review URL: http://codereview.chromium.org/6726006
    
    TBR=acolwell@chromium.org
    Review URL: http://codereview.chromium.org/6722031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79158 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/mock_filters.cc
+++ media/base/mock_filters.cc
@@ -135,7 +135,6 @@ MockFilterCollection::~MockFilterCollection() {}
 FilterCollection* MockFilterCollection::filter_collection(
     bool include_demuxer,
     bool run_build_callback,
-    bool run_build,
     PipelineStatus build_status) const {
   FilterCollection* collection = new FilterCollection();
 
@@ -152,9 +151,7 @@ FilterCollection* MockFilterCollection::filter_collection(
     ON_CALL(*demuxer_factory, Build(_, NotNull())).WillByDefault(Invoke(
         demuxer_factory, &MockDemuxerFactory::DestroyBuildCallback));
   }
-
-  if (run_build)
-    EXPECT_CALL(*demuxer_factory, Build(_, NotNull()));
+  EXPECT_CALL(*demuxer_factory, Build(_, NotNull()));
 
   collection->SetDemuxerFactory(demuxer_factory);
   collection->AddVideoDecoder(video_decoder_);
