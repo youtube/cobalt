a2e8ac1076971a246bf6ad216723ee76fd6fca01
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-28 10:45:25 +0000

    Reduce pass by value for FilePath.
    BUG=None
    TEST=None
    CID=4902
    Review URL: http://codereview.chromium.org/6696110
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79543 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_path.cc
+++ base/file_path.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -63,8 +63,8 @@ StringType::size_type FindDriveLetter(const StringType& path) {
 }
 
 #if defined(FILE_PATH_USES_DRIVE_LETTERS)
-bool EqualDriveLetterCaseInsensitive(const StringType a,
-                                     const StringType b) {
+bool EqualDriveLetterCaseInsensitive(const StringType& a,
+                                     const StringType& b) {
   size_t a_letter_pos = FindDriveLetter(a);
   size_t b_letter_pos = FindDriveLetter(b);
 
