12c768be8516a4d22b03c3e7f99fa908021ff8e7
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-31 20:34:25 +0000

    Use DCHECK_GE() where possible in base/.  Patch by Kushal Pisavadia (see http://codereview.chromium.org/6688056/ ), r=me.
    BUG=58409
    TEST=none
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80063 0039d316-1c4b-4281-b951-d872f2087c98

--- base/path_service.cc
+++ base/path_service.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -171,7 +171,7 @@ bool PathService::Get(int key, FilePath* result) {
   PathData* path_data = GetPathData();
   DCHECK(path_data);
   DCHECK(result);
-  DCHECK(key >= base::DIR_CURRENT);
+  DCHECK_GE(key, base::DIR_CURRENT);
 
   // special case the current directory because it can never be cached
   if (key == base::DIR_CURRENT)
