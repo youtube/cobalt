deeec06478f17ad4e86a667da0fa90f2cce35701
wjia@google.com <wjia@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-12 01:36:25 +0000

    Add IPC messages and renderer process side API for video capture.
    BUG=none 
    TEST=try bots
    Review URL: http://codereview.chromium.org/6815011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81199 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/video_frame.h
+++ media/base/video_frame.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -37,6 +37,7 @@ class VideoFrame : public StreamSample {
     NV12,        // 12bpp YVU planar 1x1 Y, 2x2 UV interleaving samples
     EMPTY,       // An empty frame.
     ASCII,       // A frame with ASCII content. For testing only.
+    I420,        // 12bpp YVU planar 1x1 Y, 2x2 UV samples.
   };
 
   enum SurfaceType {
