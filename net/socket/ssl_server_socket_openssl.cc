8b73499908fa85e6613343002b89d8f6293752db
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-15 13:09:26 +0000

    Fix openssl build In follow up to http://codereview.chromium.org/6805019/ fixes a couple namespace issues.
    BUG=None
    TEST=Builds with use_openssl=1 set.
    
    Review URL: http://codereview.chromium.org/6865011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81736 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_server_socket_openssl.cc
+++ net/socket/ssl_server_socket_openssl.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -51,7 +51,7 @@ class SSLServerSocketOpenSSL : public SSLServerSocket {
 
 SSLServerSocket* CreateSSLServerSocket(Socket* socket,
                                        X509Certificate* certificate,
-                                       base::RSAPrivateKey* key,
+                                       crypto::RSAPrivateKey* key,
                                        const SSLConfig& ssl_config) {
   return new SSLServerSocketOpenSSL();
 }
