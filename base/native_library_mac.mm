97662edd829bbd2d00151d6b760376f1c45cbd98
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-18 22:06:22 +0000

    linux: don't always print dlopen errors from LoadNativeLibrary
    Instead, return them to the caller and let the caller decide whether
    the error is worth notifying the user about.
    
    BUG=79068
    
    Review URL: http://codereview.chromium.org/6864020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82008 0039d316-1c4b-4281-b951-d872f2087c98

--- base/native_library_mac.mm
+++ base/native_library_mac.mm
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -16,7 +16,8 @@
 namespace base {
 
 // static
-NativeLibrary LoadNativeLibrary(const FilePath& library_path) {
+NativeLibrary LoadNativeLibrary(const FilePath& library_path,
+                                std::string* error) {
   // dlopen() etc. open the file off disk.
   if (library_path.Extension() == "dylib" ||
       !file_util::DirectoryExists(library_path)) {
