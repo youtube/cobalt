670278795788e14f7b33f1b2726e5ba7bd8edf7a
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-18 00:17:41 +0000

    net: Put net/server/* into namespace net.
    BUG=64263
    TEST=None
    
    R=willchan@chromium.org
    
    Review URL: http://codereview.chromium.org/6875024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81921 0039d316-1c4b-4281-b951-d872f2087c98

--- net/server/http_server_request_info.cc
+++ net/server/http_server_request_info.cc
@@ -1,9 +1,13 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "net/server/http_server_request_info.h"
 
+namespace net {
+
 HttpServerRequestInfo::HttpServerRequestInfo() {}
 
 HttpServerRequestInfo::~HttpServerRequestInfo() {}
+
+}  // namespace net
