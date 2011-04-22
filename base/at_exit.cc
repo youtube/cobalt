c7e2e4215675caab173edfbe442678ea07d3badb
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-22 22:09:35 +0000

    Base: Change the AtExitManager so that if base is built as a dll, multiple managers could coexist in the same address space.
    BUG=76996
    TEST=none
    Review URL: http://codereview.chromium.org/6897015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82724 0039d316-1c4b-4281-b951-d872f2087c98

--- base/at_exit.cc
+++ base/at_exit.cc
@@ -12,13 +12,19 @@
 namespace base {
 
 // Keep a stack of registered AtExitManagers.  We always operate on the most
-// recent, and we should never have more than one outside of testing, when we
-// use the shadow version of the constructor.  We don't protect this for
-// thread-safe access, since it will only be modified in testing.
+// recent, and we should never have more than one outside of testing (for a
+// statically linked version of this library).  Testing may use the shadow
+// version of the constructor, and if we are building a dynamic library we may
+// end up with multiple AtExitManagers on the same process.  We don't protect
+// this for thread-safe access, since it will only be modified in testing.
 static AtExitManager* g_top_manager = NULL;
 
-AtExitManager::AtExitManager() : next_manager_(NULL) {
+AtExitManager::AtExitManager() : next_manager_(g_top_manager) {
+// If multiple modules instantiate AtExitManagers they'll end up living in this
+// module... they have to coexist.
+#if !defined(BASE_DLL)
   DCHECK(!g_top_manager);
+#endif
   g_top_manager = this;
 }
 
