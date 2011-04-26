c76162109a0643589ff713176c6e540578a5c631
chrome-bot@google.com <chrome-bot@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-26 19:10:01 +0000

    Revert r83048 "Removed wchar_t from Time::FromString."
    This causes regression on KeywordProviderTest.Edit on clang produced executable.
    
    TBR=shinyak
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/6893031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83055 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util_unittest.cc
+++ base/file_util_unittest.cc
@@ -1779,13 +1779,13 @@ TEST_F(FileUtilTest, TouchFile) {
   base::Time access_time;
   // This timestamp is divisible by one day (in local timezone),
   // to make it work on FAT too.
-  ASSERT_TRUE(base::Time::FromString("Wed, 16 Nov 1994, 00:00:00",
+  ASSERT_TRUE(base::Time::FromString(L"Wed, 16 Nov 1994, 00:00:00",
                                      &access_time));
 
   base::Time modification_time;
   // Note that this timestamp is divisible by two (seconds) - FAT stores
   // modification times with 2s resolution.
-  ASSERT_TRUE(base::Time::FromString("Tue, 15 Nov 1994, 12:45:26 GMT",
+  ASSERT_TRUE(base::Time::FromString(L"Tue, 15 Nov 1994, 12:45:26 GMT",
               &modification_time));
 
   ASSERT_TRUE(file_util::TouchFile(foobar, access_time, modification_time));
