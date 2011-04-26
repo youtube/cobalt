be011db0e69328dde3b4410d2fa6858bc8851c8e
shinyak@google.com <shinyak@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-26 18:17:03 +0000

    Removed wchar_t from Time::FromString. Also, some of the test case are moved from pr_time_unittests to time_unittests.
    BUG=77962
    TEST=base_unittests:TimeTest.*
    
    
    Review URL: http://codereview.chromium.org/6903022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83048 0039d316-1c4b-4281-b951-d872f2087c98

--- base/third_party/nspr/prtime.h
+++ base/third_party/nspr/prtime.h
@@ -230,7 +230,7 @@ NSPR_API(PRTimeParameters) PR_GMTParameters(const PRExplodedTime *gmt);
  * from the unit test.
  */
 
-BASE_API PRStatus PR_ParseTimeString (
+NSPR_API(PRStatus) PR_ParseTimeString (
 	const char *string,
 	PRBool default_to_gmt,
 	PRTime *result);
--- net/http/http_response_headers.cc
+++ net/http/http_response_headers.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -1047,8 +1047,7 @@ bool HttpResponseHeaders::GetTimeValuedHeader(const std::string& name,
   if (!EnumerateHeader(NULL, name, &value))
     return false;
 
-  std::wstring value_wide(value.begin(), value.end());  // inflate ascii
-  return Time::FromString(value_wide.c_str(), result);
+  return Time::FromString(value.c_str(), result);
 }
 
 bool HttpResponseHeaders::IsKeepAlive() const {
