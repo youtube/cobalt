e82849ccf4795ad64c93097d9670159060f6524a
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-27 21:51:46 +0000

    Rename VideoCodecConfig to VideoDecoderConfig and move to media/base/video_decoder_config.{cc,h}.
    BUG=none
    TEST=compiles
    
    Review URL: http://codereview.chromium.org/6905065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83226 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ media/base/video_decoder_config.cc
@@ -0,0 +1,61 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/base/video_decoder_config.h"
+
+#include "base/logging.h"
+
+namespace media {
+
+VideoDecoderConfig::VideoDecoderConfig(VideoCodec codec,
+                                       int width,
+                                       int height,
+                                       int frame_rate_numerator,
+                                       int frame_rate_denominator,
+                                       uint8* extra_data,
+                                       size_t extra_data_size)
+    : codec_(codec),
+      width_(width),
+      height_(height),
+      frame_rate_numerator_(frame_rate_numerator),
+      frame_rate_denominator_(frame_rate_denominator),
+      extra_data_size_(extra_data_size) {
+  CHECK(extra_data_size_ == 0 || extra_data);
+  if (extra_data_size_ > 0) {
+    extra_data_.reset(new uint8[extra_data_size_]);
+    memcpy(extra_data_.get(), extra_data, extra_data_size_);
+  }
+}
+
+VideoDecoderConfig::~VideoDecoderConfig() {}
+
+VideoCodec VideoDecoderConfig::codec() const {
+  return codec_;
+}
+
+int VideoDecoderConfig::width() const {
+  return width_;
+}
+
+int VideoDecoderConfig::height() const {
+  return height_;
+}
+
+int VideoDecoderConfig::frame_rate_numerator() const {
+  return frame_rate_numerator_;
+}
+
+int VideoDecoderConfig::frame_rate_denominator() const {
+  return frame_rate_denominator_;
+}
+
+uint8* VideoDecoderConfig::extra_data() const {
+  return extra_data_.get();
+}
+
+size_t VideoDecoderConfig::extra_data_size() const {
+  return extra_data_size_;
+}
+
+}  // namespace media
---
+++ media/base/video_decoder_config.h
@@ -0,0 +1,59 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_BASE_VIDEO_DECODER_CONFIG_H_
+#define MEDIA_BASE_VIDEO_DECODER_CONFIG_H_
+
+#include "base/basictypes.h"
+#include "base/memory/scoped_ptr.h"
+
+namespace media {
+
+enum VideoCodec {
+  kUnknown,
+  kCodecH264,
+  kCodecVC1,
+  kCodecMPEG2,
+  kCodecMPEG4,
+  kCodecTheora,
+  kCodecVP8,
+};
+
+class VideoDecoderConfig {
+ public:
+  VideoDecoderConfig(VideoCodec codec, int width, int height,
+                     int frame_rate_numerator, int frame_rate_denominator,
+                     uint8* extra_data, size_t extra_data_size);
+  ~VideoDecoderConfig();
+
+  VideoCodec codec() const;
+  int width() const;
+  int height() const;
+  int frame_rate_numerator() const;
+  int frame_rate_denominator() const;
+  uint8* extra_data() const;
+  size_t extra_data_size() const;
+
+ private:
+  VideoCodec codec_;
+
+  // Container's concept of width and height of this video.
+  int width_;
+  int height_;
+
+  // Frame rate in seconds expressed as a fraction.
+  // TODO(scherkus): fairly certain decoders don't require frame rates.
+  int frame_rate_numerator_;
+  int frame_rate_denominator_;
+
+  // Optional byte data required to initialize video decoders.
+  scoped_array<uint8> extra_data_;
+  size_t extra_data_size_;
+
+  DISALLOW_IMPLICIT_CONSTRUCTORS(VideoDecoderConfig);
+};
+
+}  // namespace media
+
+#endif  // MEDIA_BASE_VIDEO_DECODER_CONFIG_H_
