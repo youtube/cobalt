d68bce2d4b62289fefd10c4deeda4a8f46365c98
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-27 19:50:34 +0000

    Fix DirectoryLister not to require refcounting.
    Also prevent DirectoryLister from blocking the IO thread when cancelled (blocked on joining file IO thread).
    Allows us to remove refcounting from URLRequestFileDirJob.
    
    BUG=65331
    TEST=Browse to file:/// and make sure it still works. Test cancellation by closing the tab.
    
    Review URL: http://codereview.chromium.org/6898047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83193 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_file_dir_job.h
+++ net/url_request/url_request_file_dir_job.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -51,7 +51,7 @@ class URLRequestFileDirJob
   // Fills a buffer with the output.
   bool FillReadBuffer(char *buf, int buf_size, int *bytes_read);
 
-  scoped_refptr<DirectoryLister> lister_;
+  DirectoryLister lister_;
   FilePath dir_path_;
   std::string data_;
   bool canceled_;
