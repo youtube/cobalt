7c1c5362abc51624ead2be23bf74d392daff4a23
asanka@chromium.org <asanka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-05 16:38:46 +0000

    Re-connect if Keep-Alive connection has been closed by the time we get around to reusing it.
    BUG=none 
    TEST=net_unittests --gtest_filter=HttpNetworkTransactionTest.BasicAuthKeepAliveImpatientServer:*.BasicAuthKeepAliveNoBody:*.BasicAuthKeepAliveLargeBody && unit_tests --gtest_filter=*TransportClientSocketTest.IsConnected* 
    
    Review URL: http://codereview.chromium.org/6902165
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84257 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_stream.h
+++ net/http/http_stream.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -115,6 +115,10 @@ class HttpStream {
   // connection is reused or has been connected and idle for some time.
   virtual bool IsConnectionReused() const = 0;
   virtual void SetConnectionReused() = 0;
+
+  // Checks whether the current state of the underlying connection
+  // allows it to be reused.
+  virtual bool IsConnectionReusable() const = 0;
 
   // Get the SSLInfo associated with this stream's connection.  This should
   // only be called for streams over SSL sockets, otherwise the behavior is
