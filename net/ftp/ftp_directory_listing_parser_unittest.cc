cfd2b17046ae267913339f2a6bdf7b904691d9f3
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-06 08:36:26 +0000

    FTP: split the directory listing parser test and re-enable it on TSan bots
    The test was disabled on TSan bots becuase it was taking too much time.
    This change converts one huge test which parses ~30 directory listings
    to ~30 separate test cases.
    
    This change also moves ToLower/ToUpper from ui/base/l10n_util
    to base/i18n/case_conversion so that they can be used in net/ftp.
    
    BUG=79022
    Review URL: http://codereview.chromium.org/6905027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84413 0039d316-1c4b-4281-b951-d872f2087c98

--- net/ftp/ftp_directory_listing_parser_unittest.cc
+++ net/ftp/ftp_directory_listing_parser_unittest.cc
@@ -19,51 +19,11 @@ namespace net {
 
 namespace {
 
-TEST(FtpDirectoryListingBufferTest, Parse) {
-  const char* test_files[] = {
-    "dir-listing-ls-1",
-    "dir-listing-ls-1-utf8",
-    "dir-listing-ls-2",
-    "dir-listing-ls-3",
-    "dir-listing-ls-4",
-    "dir-listing-ls-5",
-    "dir-listing-ls-6",
-    "dir-listing-ls-7",
-    "dir-listing-ls-8",
-    "dir-listing-ls-9",
-    "dir-listing-ls-10",
-    "dir-listing-ls-11",
-    "dir-listing-ls-12",
-    "dir-listing-ls-13",
-    "dir-listing-ls-14",
-    "dir-listing-ls-15",
-    "dir-listing-ls-16",
-    "dir-listing-ls-17",
-    "dir-listing-ls-18",
-    "dir-listing-ls-19",
-    "dir-listing-ls-20",  // TODO(phajdan.jr): should use windows-1251 encoding.
-    "dir-listing-ls-21",  // TODO(phajdan.jr): should use windows-1251 encoding.
-    "dir-listing-ls-22",  // TODO(phajdan.jr): should use windows-1251 encoding.
-    "dir-listing-ls-23",
-    "dir-listing-ls-24",
+class FtpDirectoryListingParserTest
+    : public testing::TestWithParam<const char*> {
+};
 
-    // Tests for Russian listings. The only difference between those
-    // files is character encoding:
-    "dir-listing-ls-25",  // UTF-8
-    "dir-listing-ls-26",  // KOI8-R
-    "dir-listing-ls-27",  // windows-1251
-
-    "dir-listing-netware-1",
-    "dir-listing-netware-2",
-    "dir-listing-vms-1",
-    "dir-listing-vms-2",
-    "dir-listing-vms-3",
-    "dir-listing-vms-4",
-    "dir-listing-vms-5",
-    "dir-listing-windows-1",
-    "dir-listing-windows-2",
-  };
-
+TEST_P(FtpDirectoryListingParserTest, Parse) {
   FilePath test_dir;
   PathService::Get(base::DIR_SOURCE_ROOT, &test_dir);
   test_dir = test_dir.AppendASCII("net");
@@ -79,70 +39,115 @@ TEST(FtpDirectoryListingBufferTest, Parse) {
   base::Time mock_current_time(
       base::Time::FromLocalExploded(mock_current_time_exploded));
 
-  for (size_t i = 0; i < arraysize(test_files); i++) {
-    SCOPED_TRACE(base::StringPrintf("Test[%" PRIuS "]: %s", i, test_files[i]));
+  SCOPED_TRACE(base::StringPrintf("Test case: %s", GetParam()));
 
-    std::string test_listing;
-    EXPECT_TRUE(file_util::ReadFileToString(test_dir.AppendASCII(test_files[i]),
-                                            &test_listing));
+  std::string test_listing;
+  EXPECT_TRUE(file_util::ReadFileToString(test_dir.AppendASCII(GetParam()),
+                                          &test_listing));
 
-    std::vector<FtpDirectoryListingEntry> entries;
-    EXPECT_EQ(OK, ParseFtpDirectoryListing(test_listing,
-                                           mock_current_time,
-                                           &entries));
+  std::vector<FtpDirectoryListingEntry> entries;
+  EXPECT_EQ(OK, ParseFtpDirectoryListing(test_listing,
+                                         mock_current_time,
+                                         &entries));
 
-    std::string expected_listing;
-    ASSERT_TRUE(file_util::ReadFileToString(
-        test_dir.AppendASCII(std::string(test_files[i]) + ".expected"),
-        &expected_listing));
+  std::string expected_listing;
+  ASSERT_TRUE(file_util::ReadFileToString(
+                  test_dir.AppendASCII(std::string(GetParam()) + ".expected"),
+                  &expected_listing));
 
-    std::vector<std::string> lines;
-    StringTokenizer tokenizer(expected_listing, "\r\n");
-    while (tokenizer.GetNext())
-      lines.push_back(tokenizer.token());
+  std::vector<std::string> lines;
+  StringTokenizer tokenizer(expected_listing, "\r\n");
+  while (tokenizer.GetNext())
+    lines.push_back(tokenizer.token());
 
-    ASSERT_EQ(8 * entries.size(), lines.size());
+  ASSERT_EQ(8 * entries.size(), lines.size());
 
-    for (size_t i = 0; i < lines.size() / 8; i++) {
-      std::string type(lines[8 * i]);
-      std::string name(lines[8 * i + 1]);
-      int64 size;
-      base::StringToInt64(lines[8 * i + 2], &size);
+  for (size_t i = 0; i < lines.size() / 8; i++) {
+    std::string type(lines[8 * i]);
+    std::string name(lines[8 * i + 1]);
+    int64 size;
+    base::StringToInt64(lines[8 * i + 2], &size);
 
-      SCOPED_TRACE(base::StringPrintf("Filename: %s", name.c_str()));
+    SCOPED_TRACE(base::StringPrintf("Filename: %s", name.c_str()));
 
-      int year, month, day_of_month, hour, minute;
-      base::StringToInt(lines[8 * i + 3], &year);
-      base::StringToInt(lines[8 * i + 4], &month);
-      base::StringToInt(lines[8 * i + 5], &day_of_month);
-      base::StringToInt(lines[8 * i + 6], &hour);
-      base::StringToInt(lines[8 * i + 7], &minute);
+    int year, month, day_of_month, hour, minute;
+    base::StringToInt(lines[8 * i + 3], &year);
+    base::StringToInt(lines[8 * i + 4], &month);
+    base::StringToInt(lines[8 * i + 5], &day_of_month);
+    base::StringToInt(lines[8 * i + 6], &hour);
+    base::StringToInt(lines[8 * i + 7], &minute);
 
-      const FtpDirectoryListingEntry& entry = entries[i];
+    const FtpDirectoryListingEntry& entry = entries[i];
 
-      if (type == "d") {
-        EXPECT_EQ(FtpDirectoryListingEntry::DIRECTORY, entry.type);
-      } else if (type == "-") {
-        EXPECT_EQ(FtpDirectoryListingEntry::FILE, entry.type);
-      } else if (type == "l") {
-        EXPECT_EQ(FtpDirectoryListingEntry::SYMLINK, entry.type);
-      } else {
-        ADD_FAILURE() << "invalid gold test data: " << type;
-      }
-
-      EXPECT_EQ(UTF8ToUTF16(name), entry.name);
-      EXPECT_EQ(size, entry.size);
-
-      base::Time::Exploded time_exploded;
-      entry.last_modified.LocalExplode(&time_exploded);
-      EXPECT_EQ(year, time_exploded.year);
-      EXPECT_EQ(month, time_exploded.month);
-      EXPECT_EQ(day_of_month, time_exploded.day_of_month);
-      EXPECT_EQ(hour, time_exploded.hour);
-      EXPECT_EQ(minute, time_exploded.minute);
+    if (type == "d") {
+      EXPECT_EQ(FtpDirectoryListingEntry::DIRECTORY, entry.type);
+    } else if (type == "-") {
+      EXPECT_EQ(FtpDirectoryListingEntry::FILE, entry.type);
+    } else if (type == "l") {
+      EXPECT_EQ(FtpDirectoryListingEntry::SYMLINK, entry.type);
+    } else {
+      ADD_FAILURE() << "invalid gold test data: " << type;
     }
+
+    EXPECT_EQ(UTF8ToUTF16(name), entry.name);
+    EXPECT_EQ(size, entry.size);
+
+    base::Time::Exploded time_exploded;
+    entry.last_modified.LocalExplode(&time_exploded);
+    EXPECT_EQ(year, time_exploded.year);
+    EXPECT_EQ(month, time_exploded.month);
+    EXPECT_EQ(day_of_month, time_exploded.day_of_month);
+    EXPECT_EQ(hour, time_exploded.hour);
+    EXPECT_EQ(minute, time_exploded.minute);
   }
 }
+
+const char* kTestFiles[] = {
+  "dir-listing-ls-1",
+  "dir-listing-ls-1-utf8",
+  "dir-listing-ls-2",
+  "dir-listing-ls-3",
+  "dir-listing-ls-4",
+  "dir-listing-ls-5",
+  "dir-listing-ls-6",
+  "dir-listing-ls-7",
+  "dir-listing-ls-8",
+  "dir-listing-ls-9",
+  "dir-listing-ls-10",
+  "dir-listing-ls-11",
+  "dir-listing-ls-12",
+  "dir-listing-ls-13",
+  "dir-listing-ls-14",
+  "dir-listing-ls-15",
+  "dir-listing-ls-16",
+  "dir-listing-ls-17",
+  "dir-listing-ls-18",
+  "dir-listing-ls-19",
+  "dir-listing-ls-20",  // TODO(phajdan.jr): should use windows-1251 encoding.
+  "dir-listing-ls-21",  // TODO(phajdan.jr): should use windows-1251 encoding.
+  "dir-listing-ls-22",  // TODO(phajdan.jr): should use windows-1251 encoding.
+  "dir-listing-ls-23",
+  "dir-listing-ls-24",
+
+  // Tests for Russian listings. The only difference between those
+  // files is character encoding:
+  "dir-listing-ls-25",  // UTF-8
+  "dir-listing-ls-26",  // KOI8-R
+  "dir-listing-ls-27",  // windows-1251
+
+  "dir-listing-netware-1",
+  "dir-listing-netware-2",
+  "dir-listing-vms-1",
+  "dir-listing-vms-2",
+  "dir-listing-vms-3",
+  "dir-listing-vms-4",
+  "dir-listing-vms-5",
+  "dir-listing-windows-1",
+  "dir-listing-windows-2",
+};
+
+INSTANTIATE_TEST_CASE_P(, FtpDirectoryListingParserTest,
+                        testing::ValuesIn(kTestFiles));
 
 }  // namespace
 
