3806a38898c88915c49c87400fdf2e05603e0fce
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-09 16:41:53 +0000

    net: fix matching ".google.com" at the end of a string.
    BUG=none
    TEST=none
    
    http://codereview.chromium.org/6975002/
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84630 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_pool.cc
+++ net/socket/ssl_client_socket_pool.cc
@@ -338,7 +338,8 @@ int SSLConnectJob::DoSSLConnectComplete(int result) {
 
       const std::string& host = params_->host_and_port().host();
       bool is_google = host == "google.com" ||
-                       host.rfind(".google.com") == host.size() - 11;
+                       (host.size() > 11 &&
+                        host.rfind(".google.com") == host.size() - 11);
       if (is_google) {
         UMA_HISTOGRAM_CUSTOM_TIMES("Net.SSL_Connection_Latency_Google",
                                    connect_duration,
