a67159db495bb6539af67c0a76c15f6389e7d1d5
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-16 22:35:14 +0000

    Support binding WeakPtr<> to methods with void return types.
    This should give functionality similar to ScopedRunnableMethodFactory.
    
    Note that binding a WeakPtr only make sense with methods with void return types.  If the return type is not void, then it is unclear what the function should return when the pointer is invalidated.
    
    This code adds a compile time assert to check the return type.
    
    BUG=35223
    TEST=unittests
    
    Review URL: http://codereview.chromium.org/7015064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85549 0039d316-1c4b-4281-b951-d872f2087c98

--- base/template_util.h
+++ base/template_util.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -36,6 +36,9 @@ template<class T> struct is_array<T[]> : public true_type {};
 template <class T> struct is_non_const_reference : false_type {};
 template <class T> struct is_non_const_reference<T&> : true_type {};
 template <class T> struct is_non_const_reference<const T&> : false_type {};
+
+template <class T> struct is_void : false_type {};
+template <> struct is_void<void> : true_type {};
 
 namespace internal {
 
