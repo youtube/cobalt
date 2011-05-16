f1e21d95aefec73f4131257be83b6f06a87d9f08
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-16 23:37:25 +0000

    Switch to using the system request context for OCSP.
    This gets rid of all the ugly "is_main" logic for URLRequestContext.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7013071
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85555 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_context_getter.h
+++ net/url_request/url_request_context_getter.h
@@ -36,14 +36,6 @@ class URLRequestContextGetter
   virtual scoped_refptr<base::MessageLoopProxy>
       GetIOMessageLoopProxy() const = 0;
 
-  // Controls whether or not the URLRequestContextGetter considers itself to be
-  // the the "main" URLRequestContextGetter.  Note that each Profile will have a
-  // "default" URLRequestContextGetter.  Therefore, "is_main" refers to the
-  // default URLRequestContextGetter for the "main" Profile.
-  // TODO(willchan): Move this code to ChromeURLRequestContextGetter, since this
-  // ia a browser process specific concept.
-  void set_is_main(bool is_main) { is_main_ = is_main; }
-
  protected:
   friend class DeleteTask<const URLRequestContextGetter>;
   friend struct URLRequestContextGetterTraits;
@@ -51,16 +43,10 @@ class URLRequestContextGetter
   URLRequestContextGetter();
   virtual ~URLRequestContextGetter();
 
-  bool is_main() const { return is_main_; }
-
  private:
   // OnDestruct is meant to ensure deletion on the thread on which the request
   // IO happens.
   void OnDestruct() const;
-
-  // Indicates whether or not this is the default URLRequestContextGetter for
-  // the main Profile.
-  bool is_main_;
 };
 
 struct URLRequestContextGetterTraits {
