38d1d4712e247ab3618c8aedcdc1b059b3a1282c
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-18 20:01:35 +0000

    Linux: Install the sqlite debugging package for developers.
    BUG=82894
    TEST=none
    Review URL: http://codereview.chromium.org/6975038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85798 0039d316-1c4b-4281-b951-d872f2087c98

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -178,6 +178,7 @@ lib_list="libatk1.0-0 libc6 libasound2 libcairo2 libdbus-glib-1-2 libexpat1
 dbg_list="libatk1.0-dbg libc6-dbg libcairo2-dbg
           libfontconfig1-dbg libglib2.0-0-dbg libgtk2.0-0-dbg libnspr4-0d-dbg
           libnss3-1d-dbg libpango1.0-0-dbg libpcre3-dbg libpixman-1-0-dbg
+          libsqlite3-0-dbg
           libx11-6-dbg libxau6-dbg libxcb1-dbg libxcomposite1-dbg
           libxcursor1-dbg libxdamage1-dbg libxdmcp6-dbg libxext6-dbg
           libxfixes3-dbg libxi6-dbg libxinerama1-dbg libxrandr2-dbg
