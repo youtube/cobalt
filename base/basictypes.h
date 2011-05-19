b95620f98936d3faa9b0c281a292020766ebb9ad
wyck@chromium.org <wyck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 19:35:44 +0000

    Fixed spelling mistake typo in comment.
    BUG=none
    TEST=none
    R=darin@chromium.org
    
    
    Review URL: http://codereview.chromium.org/7038009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85953 0039d316-1c4b-4281-b951-d872f2087c98

--- base/basictypes.h
+++ base/basictypes.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -238,7 +238,7 @@ struct CompileAssert {
 //   expr is a compile-time constant.  (Template arguments must be
 //   determined at compile-time.)
 //
-// - The outter parentheses in CompileAssert<(bool(expr))> are necessary
+// - The outer parentheses in CompileAssert<(bool(expr))> are necessary
 //   to work around a bug in gcc 3.4.4 and 4.0.1.  If we had written
 //
 //     CompileAssert<bool(expr)>
