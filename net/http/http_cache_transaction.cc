3ec70a4bf62f9d7c5f8086cdad5aff61dcc56b01
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 00:26:43 +0000

    Revert 85821 - Http cache: Make sure that we don't use http 1.1 features for http 1.0 resources.
    BUG=74061
    TEST=netunittests
    Review URL: http://codereview.chromium.org/6995010
    
    TBR=rvargas@google.com
    Review URL: http://codereview.chromium.org/7034044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85853 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_cache_transaction.cc
+++ net/http/http_cache_transaction.cc
@@ -1694,9 +1694,6 @@ bool HttpCache::Transaction::ConditionalizeRequest() {
   response_.headers->EnumerateHeader(NULL, "last-modified",
                                      &last_modified_value);
 
-  if (response_.headers->GetHttpVersion() < HttpVersion(1, 1))
-    etag_value.clear();
-
   if (etag_value.empty() && last_modified_value.empty())
     return false;
 
