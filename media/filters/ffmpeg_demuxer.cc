2ff8607ddef8d784dabc68fc1b71a5c9e57534c0
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 23:33:27 +0000

    Introduce a quick and dirty hack to skip the implicit first seek for FFmpegDemuxer (again).
    The real fix is to split Seek() into explicit Seek() and Preroll() actions as Seek() can have unintended consequences (i.e., resetting internal FFmpeg state).
    
    BUG=40344,80187
    TEST=media_unittests
    
    Review URL: http://codereview.chromium.org/6894044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86008 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/ffmpeg_demuxer.cc
+++ media/filters/ffmpeg_demuxer.cc
@@ -264,7 +264,8 @@ FFmpegDemuxer::FFmpegDemuxer(MessageLoop* message_loop)
       last_read_bytes_(0),
       read_position_(0),
       max_duration_(base::TimeDelta::FromMicroseconds(-1)),
-      deferred_status_(PIPELINE_OK) {
+      deferred_status_(PIPELINE_OK),
+      first_seek_hack_(true) {
   DCHECK(message_loop_);
 }
 
@@ -530,6 +531,14 @@ void FFmpegDemuxer::InitializeTask(DataSource* data_source,
 
 void FFmpegDemuxer::SeekTask(base::TimeDelta time, const FilterStatusCB& cb) {
   DCHECK_EQ(MessageLoop::current(), message_loop_);
+
+  // TODO(scherkus): remove this by separating Seek() from Flush() from
+  // Preroll() states (i.e., the implicit Seek(0) should really be a Preroll()).
+  if (first_seek_hack_) {
+    first_seek_hack_ = false;
+    cb.Run(PIPELINE_OK);
+    return;
+  }
 
   // Tell streams to flush buffers due to seeking.
   StreamVector::iterator iter;
