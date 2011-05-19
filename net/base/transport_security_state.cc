d863953853406a35d3e41bdcf08582b3b30d85e3
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 16:49:40 +0000

    net: add betnet.fr to HSTS preloaded list
    Requested by: Philippe Lafoucrière
    
    BUG=none
    TEST=none
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85924 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/transport_security_state.cc
+++ net/base/transport_security_state.cc
@@ -639,6 +639,7 @@ bool TransportSecurityState::IsPreloadedSTS(
     {27, true, "\025cloudsecurityalliance\003org", true, 0 },
     {15, true, "\005login\004sapo\002pt", true, 0 },
     {19, true, "\015mattmccutchen\003net", true, 0 },
+    {11, true, "\006betnet\002fr", true, 0 },
 #if defined(OS_CHROMEOS)
     {13, false, "\007twitter\003com", true, 0 },
     {17, false, "\003www\007twitter\003com", true, 0 },
