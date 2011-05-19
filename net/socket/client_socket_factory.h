07bf1bf00e37698030b3d9d779dc3d7800312a4b
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 21:53:58 +0000

    Net: Add net_api to the socket code
    BUG=76997
    TEST=NONE
    Review URL: http://codereview.chromium.org/7011056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85982 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/client_socket_factory.h
+++ net/socket/client_socket_factory.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,6 +9,7 @@
 #include <string>
 
 #include "base/basictypes.h"
+#include "net/base/net_api.h"
 #include "net/base/net_log.h"
 
 namespace net {
@@ -25,7 +26,7 @@ class StreamSocket;
 
 // An interface used to instantiate StreamSocket objects.  Used to facilitate
 // testing code with mock socket implementations.
-class ClientSocketFactory {
+class NET_API ClientSocketFactory {
  public:
   virtual ~ClientSocketFactory() {}
 
