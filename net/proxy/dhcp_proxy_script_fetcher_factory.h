89c0244badb012f14d98ad7737bdb8434aba4f66
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-21 00:18:30 +0000

    net: Add NET_API to a few more files.
    BUG=76997
    TEST=NONE
    Review URL: http://codereview.chromium.org/7056019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86181 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/dhcp_proxy_script_fetcher_factory.h
+++ net/proxy/dhcp_proxy_script_fetcher_factory.h
@@ -9,6 +9,7 @@
 #include "base/basictypes.h"
 #include "base/memory/singleton.h"
 #include "net/base/completion_callback.h"
+#include "net/base/net_api.h"
 #include "net/url_request/url_request_context.h"
 
 namespace net {
@@ -29,7 +30,7 @@ class DhcpProxyScriptFetcher;
 //
 // Therefore, we have platform-specific implementations, and so we use
 // this factory to select the right one.
-class DhcpProxyScriptFetcherFactory {
+class NET_API DhcpProxyScriptFetcherFactory {
  public:
   // Creates a new factory object with default settings.
   DhcpProxyScriptFetcherFactory();
