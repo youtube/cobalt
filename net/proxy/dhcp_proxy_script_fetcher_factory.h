7e12d01bf4234f3c657aa1a39b70713af6a46a91
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-24 14:14:02 +0000

    Make DHCP WPAD on by default.
    BUG=18575
    TEST=Run Chrome on Windows without any flags. Enable auto-detect in proxy configuration. Net log should show attempts to auto-detect via DHCP.
    
    Review URL: http://codereview.chromium.org/7034035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86422 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/dhcp_proxy_script_fetcher_factory.h
+++ net/proxy/dhcp_proxy_script_fetcher_factory.h
@@ -47,7 +47,7 @@ class NET_API DhcpProxyScriptFetcherFactory {
   // Attempts to enable/disable the DHCP WPAD feature.  Does nothing
   // if |IsSupported()| returns false.
   //
-  // The current default is |enabled() == false|.
+  // The default is |enabled() == true|.
   void set_enabled(bool enabled);
 
   // Returns true if the DHCP WPAD feature is enabled.  Always returns
