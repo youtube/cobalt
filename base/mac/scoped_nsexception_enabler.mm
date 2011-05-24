c476bc4e4272f4937cef7ae87afeb5fd0d155fe7
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-24 22:07:05 +0000

    [Mac] Allow NSExceptions in certain cases.
    Thirdy-party print drivers seem to be a source of NSExceptions which
    Chromium will never be able to fix.  ScopedNSExceptionEnabler causes
    the code which makes throwing an NSException fatal to allow throws.
    The flag will be reset in -reportException: in most cases.
    
    For now, allow exceptions to be thrown for -selectPDE: (bug 80686) and
    PrintingContextMac::AskUserForSettings() (bug 82589).
    
    BUG=80686, 82589
    TEST=Monitor crash server.
    
    
    Review URL: http://codereview.chromium.org/7038010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86503 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ base/mac/scoped_nsexception_enabler.mm
@@ -0,0 +1,44 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "base/mac/scoped_nsexception_enabler.h"
+
+#import "base/lazy_instance.h"
+#import "base/threading/thread_local.h"
+
+// To make the |g_exceptionsAllowed| declaration readable.
+using base::LazyInstance;
+using base::LeakyLazyInstanceTraits;
+using base::ThreadLocalBoolean;
+
+namespace {
+
+// Whether to allow NSExceptions to be raised on the current thread.
+LazyInstance<ThreadLocalBoolean, LeakyLazyInstanceTraits<ThreadLocalBoolean> >
+    g_exceptionsAllowed(base::LINKER_INITIALIZED);
+
+}  // namespace
+
+namespace base {
+namespace mac {
+
+bool GetNSExceptionsAllowed() {
+  return g_exceptionsAllowed.Get().Get();
+}
+
+void SetNSExceptionsAllowed(bool allowed) {
+  return g_exceptionsAllowed.Get().Set(allowed);
+}
+
+ScopedNSExceptionEnabler::ScopedNSExceptionEnabler() {
+  was_enabled_ = GetNSExceptionsAllowed();
+  SetNSExceptionsAllowed(true);
+}
+
+ScopedNSExceptionEnabler::~ScopedNSExceptionEnabler() {
+  SetNSExceptionsAllowed(was_enabled_);
+}
+
+}  // namespace mac
+}  // namespace base
