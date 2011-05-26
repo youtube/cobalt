7b1433b4ea8320b1d4b899f6e290020dc9d06a3d
annacc@chromium.org <annacc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 17:52:17 +0000

    Surround sound swizzling for Mac based on device channel layout preferences.
    This patch gets the surround sound channel layout from the user's device using CoreAudio.
    It then swizzles the channel layout from the source to match the user's prefered channel layout (if needed).
    
    BUG=none.
    TEST=none.
    
    
    Review URL: http://codereview.chromium.org/7047020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86851 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/channel_layout.h
+++ media/base/channel_layout.h
@@ -52,9 +52,38 @@ enum ChannelLayout {
   CHANNEL_LAYOUT_7POINT1_WIDE,
 
   // Stereo L, Stereo R
-  CHANNEL_LAYOUT_STEREO_DOWNMIX
+  CHANNEL_LAYOUT_STEREO_DOWNMIX,
+
+  // Total number of layouts.
+  CHANNEL_LAYOUT_MAX
 };
 
+enum Channels {
+  LEFT = 0,
+  RIGHT,
+  CENTER,
+  LFE,
+  BACK_LEFT,
+  BACK_RIGHT,
+  LEFT_OF_CENTER,
+  RIGHT_OF_CENTER,
+  BACK_CENTER,
+  SIDE_LEFT,
+  SIDE_RIGHT,
+  STEREO_LEFT,
+  STEREO_RIGHT,
+  CHANNELS_MAX
+};
+
+// The channel orderings for each layout as specified by FFmpeg.
+// Values represent the index of each channel in each layout. For example, the
+// left side surround sound channel in FFmpeg's 5.1 layout is in the 5th
+// position (because the order is L, R, C, LFE, LS, RS), so
+// kChannelOrderings[CHANNEL_LAYOUT_5POINT1][SIDE_LEFT] = 4;
+// Values of -1 mean the channel at that index is not used for that layout.
+extern const int kChannelOrderings[CHANNEL_LAYOUT_MAX][CHANNELS_MAX];
+
+// Returns the number of channels in a given ChannelLayout.
 int ChannelLayoutToChannelCount(ChannelLayout layout);
 
 #endif  // MEDIA_BASE_CHANNEL_LAYOUT_H_
