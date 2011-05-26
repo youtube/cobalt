f638ba5500ba8c96134b51a349575e568cf38cd3
bryner@chromium.org <bryner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 05:34:23 +0000

    Add BASE_API to the remaining functions in linux_util.
    These symbols are used by chrome/browser/crash_handler_host_linux.cc when building with branding==Chrome, so they need to be exported for shared-library builds to work in that configuration.
    
    BUG=none
    TEST=builds
    
    Review URL: http://codereview.chromium.org/7076007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86782 0039d316-1c4b-4281-b951-d872f2087c98

--- base/linux_util.h
+++ base/linux_util.h
@@ -19,26 +19,27 @@ static const char kFindInodeSwitch[] = "--find-inode";
 
 // This is declared here so the crash reporter can access the memory directly
 // in compromised context without going through the standard library.
-extern char g_linux_distro[];
+BASE_API extern char g_linux_distro[];
 
 // Get the Linux Distro if we can, or return "Unknown".
-std::string GetLinuxDistro();
+BASE_API std::string GetLinuxDistro();
 
 // Set the Linux Distro string.
-void SetLinuxDistro(const std::string& distro);
+BASE_API void SetLinuxDistro(const std::string& distro);
 
 // Return the inode number for the UNIX domain socket |fd|.
 BASE_API bool FileDescriptorGetInode(ino_t* inode_out, int fd);
 
 // Find the process which holds the given socket, named by inode number. If
 // multiple processes hold the socket, this function returns false.
-bool FindProcessHoldingSocket(pid_t* pid_out, ino_t socket_inode);
+BASE_API bool FindProcessHoldingSocket(pid_t* pid_out, ino_t socket_inode);
 
 // For a given process |pid|, look through all its threads and find the first
 // thread with /proc/[pid]/task/[thread_id]/syscall whose first N bytes matches
 // |expected_data|, where N is the length of |expected_data|.
 // Returns the thread id or -1 on error.
-pid_t FindThreadIDWithSyscall(pid_t pid, const std::string& expected_data);
+BASE_API pid_t FindThreadIDWithSyscall(pid_t pid,
+                                       const std::string& expected_data);
 
 }  // namespace base
 
