ec4c910bf80dc3df8d4ddf706f7c43c7fcca9adf
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-02 18:37:51 +0000

    More POSIX support for Chromium, consisting mostly of broadening ifdefs. This patch cuts across modules, as there's only a handful necessary for each, with the most in chrome/test/. ALSA is enabled on FreeBSD and Solaris, as libasound has been ported to those two platforms, and I moved resolv.h in host_resolver_proc.cc, because it depends on headers from sys_addrinfo.h on FreeBSD.
    Patch by ruben (chromium@hybridsource.org)
    
    Review URL: http://codereview.chromium.org/6976055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87641 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/host_resolver_proc.cc
+++ net/base/host_resolver_proc.cc
@@ -6,15 +6,15 @@
 
 #include "build/build_config.h"
 
-#if defined(OS_POSIX) && !defined(OS_MACOSX)
-#include <resolv.h>
-#endif
-
 #include "base/logging.h"
 #include "net/base/address_list.h"
 #include "net/base/dns_reload_timer.h"
 #include "net/base/net_errors.h"
 #include "net/base/sys_addrinfo.h"
+
+#if defined(OS_POSIX) && !defined(OS_MACOSX)
+#include <resolv.h>
+#endif
 
 namespace net {
 
@@ -248,7 +248,7 @@ int SystemHostResolverProc(const std::string& host,
 #if defined(OS_WIN)
     if (err != WSAHOST_NOT_FOUND && err != WSANO_DATA)
       return ERR_NAME_RESOLUTION_FAILED;
-#elif defined(OS_POSIX)
+#elif defined(OS_POSIX) && !defined(OS_FREEBSD)
     if (err != EAI_NONAME && err != EAI_NODATA)
       return ERR_NAME_RESOLUTION_FAILED;
 #endif
