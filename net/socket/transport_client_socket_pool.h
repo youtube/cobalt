4469fcfd6287195e348b0d6abd89ec074c73fbea
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-06 18:25:19 +0000

    Add unit-tests for SingleRequestHostResolver.
    To facilitate this, moved it out of host_resolver.cc to its own set of files.
    
    BUG=84261
    Review URL: http://codereview.chromium.org/6993015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88006 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/transport_client_socket_pool.h
+++ net/socket/transport_client_socket_pool.h
@@ -15,6 +15,7 @@
 #include "base/timer.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/host_resolver.h"
+#include "net/base/single_request_host_resolver.h"
 #include "net/socket/client_socket_pool_base.h"
 #include "net/socket/client_socket_pool_histograms.h"
 #include "net/socket/client_socket_pool.h"
