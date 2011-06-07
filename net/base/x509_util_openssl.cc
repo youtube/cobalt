42f708c19e7e31dcbef24d2a689dc8351ac5e7e0
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-07 22:52:10 +0000

    Rename the x509_openssl_util namespace to x509_util
    R=joth@chromium.org
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7112004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88224 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/base/x509_util_openssl.cc
@@ -0,0 +1,74 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/base/x509_util_openssl.h"
+
+#include <algorithm>
+
+#include "base/logging.h"
+#include "base/string_piece.h"
+#include "net/base/x509_cert_types.h"
+
+namespace net {
+
+namespace x509_util {
+
+bool ParsePrincipalKeyAndValueByIndex(X509_NAME* name,
+                                      int index,
+                                      std::string* key,
+                                      std::string* value) {
+  X509_NAME_ENTRY* entry = X509_NAME_get_entry(name, index);
+  if (!entry)
+    return false;
+
+  if (key) {
+    ASN1_OBJECT* object = X509_NAME_ENTRY_get_object(entry);
+    key->assign(OBJ_nid2sn(OBJ_obj2nid(object)));
+  }
+
+  ASN1_STRING* data = X509_NAME_ENTRY_get_data(entry);
+  if (!data)
+    return false;
+
+  unsigned char* buf = NULL;
+  int len = ASN1_STRING_to_UTF8(&buf, data);
+  if (len <= 0)
+    return false;
+
+  value->assign(reinterpret_cast<const char*>(buf), len);
+  OPENSSL_free(buf);
+  return true;
+}
+
+bool ParsePrincipalValueByIndex(X509_NAME* name,
+                                int index,
+                                std::string* value) {
+  return ParsePrincipalKeyAndValueByIndex(name, index, NULL, value);
+}
+
+bool ParsePrincipalValueByNID(X509_NAME* name, int nid, std::string* value) {
+  int index = X509_NAME_get_index_by_NID(name, nid, -1);
+  if (index < 0)
+    return false;
+
+  return ParsePrincipalValueByIndex(name, index, value);
+}
+
+bool ParseDate(ASN1_TIME* x509_time, base::Time* time) {
+  if (!x509_time ||
+      (x509_time->type != V_ASN1_UTCTIME &&
+       x509_time->type != V_ASN1_GENERALIZEDTIME))
+    return false;
+
+  base::StringPiece str_date(reinterpret_cast<const char*>(x509_time->data),
+                             x509_time->length);
+
+  CertDateFormat format = x509_time->type == V_ASN1_UTCTIME ?
+      CERT_DATE_FORMAT_UTC_TIME : CERT_DATE_FORMAT_GENERALIZED_TIME;
+  return ParseCertificateDate(str_date, format, time);
+}
+
+}  // namespace x509_util
+
+}  // namespace net
