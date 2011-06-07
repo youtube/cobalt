2a83a8c5b7060a90920f23073dc85c58371be56d
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-07 17:43:20 +0000

    Clang needs dtor in the cc file (build fix)
    BUG=none
    TEST=one
    
    TBR=shess
    Review URL: http://codereview.chromium.org/7131008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88145 0039d316-1c4b-4281-b951-d872f2087c98

--- base/version.cc
+++ base/version.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,6 +12,9 @@
 #include "base/string_util.h"
 
 Version::Version() {
+}
+
+Version::~Version() {
 }
 
 Version::Version(const std::string& version_str) {
