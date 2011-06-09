14f4acfe97775980c40843ba67303d31c5185b7d
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-09 16:35:24 +0000

    Add revocation checking field trial.
    We wish to measure the impact of revocation checking on SSL connection times.
    This change sets up a 50/50 field trial where members of the trial don't do
    revocation checking on sites that we currently have certificate pins for.
    
    BUG=none
    TEST=none
    
    http://codereview.chromium.org/7113008/
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88542 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/ssl_config_service.h
+++ net/base/ssl_config_service.h
@@ -142,6 +142,11 @@ class NET_API SSLConfigService
   static void EnableDNSCertProvenanceChecking();
   static bool dns_cert_provenance_checking_enabled();
 
+  // Disabled revocation checking for some sites that we have additional
+  // security on.
+  static void DisableRevCheckingForPinnedSites();
+  static bool rev_checking_disabled_for_pinned_sites();
+
   // Is SNI available in this configuration?
   static bool IsSNIAvailable(SSLConfigService* service);
 
