7363c80a004d9a47c2f6300bb2e5f0a15a614946
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-14 19:03:16 +0000

    Change the files in base and net to include foundation_util.h instead of mac_util.h.
    R=rohitrao@chromium.org,mark@chromium.org
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7145016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89040 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/proxy_server_mac.cc
+++ net/proxy/proxy_server_mac.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,7 +9,7 @@
 #include <string>
 
 #include "base/logging.h"
-#include "base/mac/mac_util.h"
+#include "base/mac/foundation_util.h"
 #include "base/sys_string_conversions.h"
 
 namespace net {
