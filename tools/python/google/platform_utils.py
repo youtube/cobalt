7507fe4c51e3e78078e9beffe37bc4a6d88eb4a6
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-16 17:15:47 +0000

    Add support for Linux kernels >=3.0-rc1 in Python related build files by:  - replacing occurences of sys.platform == 'linux2' with     sys.platform.startswith('linux')  - congregating occurences of sys.platform in ('linux[X]', 'linuxY', ...)     to sys.platform.startswith('linux')   - adding the key 'linux3' to all relevant lookup dicts
    BUG=85845
    TEST=Try building chromium on Linux >=3.0-r1
    
    Review URL: http://codereview.chromium.org/7172016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89354 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/python/google/platform_utils.py
+++ tools/python/google/platform_utils.py
@@ -1,5 +1,5 @@
 #!/bin/env python
-# Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -20,5 +20,5 @@ if sys.platform in ('cygwin', 'win32'):
   from platform_utils_win import *
 elif sys.platform == 'darwin':
   from platform_utils_mac import *
-elif sys.platform == 'linux2':
+elif sys.platform.startswith('linux'):
   from platform_utils_linux import *
