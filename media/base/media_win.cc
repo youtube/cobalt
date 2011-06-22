e4e5d89332199c93dc5865653f5a7516eca2ff17
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-22 17:42:43 +0000

    Remove unused testing code from media/base/media_win.cc
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7230003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90051 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/media_win.cc
+++ media/base/media_win.cc
@@ -12,14 +12,6 @@
 #include "base/native_library.h"
 #include "base/path_service.h"
 
-// Enable timing code by turning on TESTING macro.
-//#define TESTING 1
-
-#ifdef TESTING
-#include "base/string_util.h"
-#include "base/time.h"
-#endif
-
 namespace media {
 
 enum FFmpegDLLKeys {
@@ -61,9 +53,6 @@ bool InitializeMediaLibrary(const FilePath& base_path) {
 
   for (size_t i = 0; i < arraysize(path_keys); ++i) {
     FilePath path = base_path.Append(GetDLLName(path_keys[i]));
-#ifdef TESTING
-    base::TimeTicks dll_loadtime_start = base::TimeTicks::HighResNow();
-#endif
 
     // Use alternate DLL search path so we don't load dependencies from the
     // system path.  Refer to http://crbug.com/35857
@@ -71,19 +60,12 @@ bool InitializeMediaLibrary(const FilePath& base_path) {
     libs[i] = LoadLibraryEx(cpath, NULL, LOAD_WITH_ALTERED_SEARCH_PATH);
     if (!libs[i])
       break;
-#ifdef TESTING
-    base::TimeTicks dll_loadtime_end = base::TimeTicks::HighResNow();
-    std::wstring outputbuf = StringPrintf(L"DLL loadtime %5.2f ms, %ls\n",
-        (dll_loadtime_end - dll_loadtime_start).InMillisecondsF(),
-        cpath);
-    OutputDebugStringW(outputbuf.c_str());
-#endif
   }
 
   // Check that we loaded all libraries successfully.  We only need to check the
   // last array element because the loop above will break without initializing
   // it on any prior error.
-  g_media_library_is_initialized = (libs[arraysize(libs)-1] != NULL);
+  g_media_library_is_initialized = (libs[arraysize(libs) - 1] != NULL);
 
   if (!g_media_library_is_initialized) {
     // Free any loaded libraries if we weren't successful.
