0e09d1d63c469c6e51f7d5b17f73283b24307004
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-30 23:51:24 +0000

    net: Add NET_API to more code to enable building a shared net library on Linux, and update base and crypto API definitions.
    BUG=76997
    TEST=none
    Review URL: http://codereview.chromium.org/7240021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91234 0039d316-1c4b-4281-b951-d872f2087c98

--- net/ocsp/nss_ocsp.h
+++ net/ocsp/nss_ocsp.h
@@ -6,6 +6,8 @@
 #define NET_OCSP_NSS_OCSP_H_
 #pragma once
 
+#include "net/base/net_api.h"
+
 namespace net {
 
 class URLRequestContext;
@@ -13,19 +15,19 @@ class URLRequestContext;
 // Sets the MessageLoop for OCSP to the current message loop.
 // This should be called before EnsureOCSPInit() if you want to
 // control the message loop for OCSP.
-void SetMessageLoopForOCSP();
+NET_API void SetMessageLoopForOCSP();
 
 // Initializes OCSP handlers for NSS.  This must be called before any
 // certificate verification functions.  This function is thread-safe, and OCSP
 // handlers will only ever be initialized once.  ShutdownOCSP() must be called
 // on shutdown.
-void EnsureOCSPInit();
+NET_API void EnsureOCSPInit();
 
 // This should be called once on shutdown to stop issuing URLRequests for OCSP.
-void ShutdownOCSP();
+NET_API void ShutdownOCSP();
 
 // Set URLRequestContext for OCSP handlers.
-void SetURLRequestContextForOCSP(URLRequestContext* request_context);
+NET_API void SetURLRequestContextForOCSP(URLRequestContext* request_context);
 
 }  // namespace net
 
