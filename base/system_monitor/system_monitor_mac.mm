5cd77a01c302f198ca846b9976c23782f5f205c4
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-04 04:18:41 +0000

    Mac: Split SystemMonitor initialization so it's not blocked by the Sandbox.
    In OS X 10.7 Lion, the IO port we listen on to monitor system power events is blocked by the Sandbox.
    
    Move the allocation of the IO port so it happens early on during Chrome startup.
    
    BUG=83783
    TEST=When putting a system to sleep with Chrome running, the browser shouldn't crash.  Also, need to monitor crash logs to see that this crash goes away.
    
    Review URL: http://codereview.chromium.org/7235023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91462 0039d316-1c4b-4281-b951-d872f2087c98

--- base/system_monitor/system_monitor_mac.mm
+++ base/system_monitor/system_monitor_mac.mm
@@ -15,13 +15,15 @@ namespace base {
 namespace {
 
 io_connect_t g_system_power_io_port = 0;
+IONotificationPortRef g_notification_port_ref = 0;
+io_object_t g_notifier_object = 0;
 
-void SystemPowerEventCallback(void* system_monitor,
+void SystemPowerEventCallback(void*,
                               io_service_t service,
                               natural_t message_type,
                               void* message_argument) {
-  DCHECK(system_monitor);
-  SystemMonitor* sys_monitor = reinterpret_cast<SystemMonitor*>(system_monitor);
+  SystemMonitor* sys_monitor = SystemMonitor::Get();
+  DCHECK(sys_monitor);
   switch (message_type) {
     case kIOMessageSystemWillSleep:
       sys_monitor->ProcessPowerMessage(SystemMonitor::SUSPEND_EVENT);
@@ -37,22 +39,36 @@ void SystemPowerEventCallback(void* system_monitor,
 
 }  // namespace
 
-void SystemMonitor::PlatformInit() {
+// The reason we can't include this code in the constructor is because
+// PlatformInit() requires an active runloop and the IO port needs to be
+// allocated at sandbox initialization time, before there's a runloop.
+// See crbug.com/83783 .
+
+// static
+void SystemMonitor::AllocateSystemIOPorts() {
   DCHECK_EQ(g_system_power_io_port, 0u);
 
   // Notification port allocated by IORegisterForSystemPower.
 
   g_system_power_io_port = IORegisterForSystemPower(
-      this, &notification_port_ref_, SystemPowerEventCallback,
-      &notifier_object_);
+      NULL, &g_notification_port_ref, SystemPowerEventCallback,
+      &g_notifier_object);
+
+  DCHECK_NE(g_system_power_io_port, 0u);
+}
+
+void SystemMonitor::PlatformInit() {
+  // Need to call AllocateSystemIOPorts() before constructing a SystemMonitor
+  // object.
   DCHECK_NE(g_system_power_io_port, 0u);
   if (g_system_power_io_port == 0)
     return;
 
   // Add the notification port to the application runloop
-  CFRunLoopAddSource(CFRunLoopGetCurrent(),
-                     IONotificationPortGetRunLoopSource(notification_port_ref_),
-                     kCFRunLoopCommonModes);
+  CFRunLoopAddSource(
+      CFRunLoopGetCurrent(),
+      IONotificationPortGetRunLoopSource(g_notification_port_ref),
+      kCFRunLoopCommonModes);
 }
 
 void SystemMonitor::PlatformDestroy() {
@@ -63,11 +79,11 @@ void SystemMonitor::PlatformDestroy() {
   // Remove the sleep notification port from the application runloop
   CFRunLoopRemoveSource(
       CFRunLoopGetCurrent(),
-      IONotificationPortGetRunLoopSource(notification_port_ref_),
+      IONotificationPortGetRunLoopSource(g_notification_port_ref),
       kCFRunLoopCommonModes);
 
   // Deregister for system sleep notifications
-  IODeregisterForSystemPower(&notifier_object_);
+  IODeregisterForSystemPower(&g_notifier_object);
 
   // IORegisterForSystemPower implicitly opens the Root Power Domain IOService,
   // so we close it here.
@@ -76,7 +92,7 @@ void SystemMonitor::PlatformDestroy() {
   g_system_power_io_port = 0;
 
   // Destroy the notification port allocated by IORegisterForSystemPower.
-  IONotificationPortDestroy(notification_port_ref_);
+  IONotificationPortDestroy(g_notification_port_ref);
 }
 
 }  // namespace base
