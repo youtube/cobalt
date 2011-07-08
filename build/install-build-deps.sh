c84a88ac71dca82e22daa6264527af6680b8071d
bevc@google.com <bevc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-08 20:31:57 +0000

    Updated gold to 2.21.1 and removed the patching for the older version of Gold. Review URL: http://codereview.chromium.org/7326019
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91889 0039d316-1c4b-4281-b951-d872f2087c98

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -46,9 +46,9 @@ install_gold() {
     return
   fi
 
-  BINUTILS=binutils-2.21
+  BINUTILS=binutils-2.21.1
   BINUTILS_URL=http://ftp.gnu.org/gnu/binutils/$BINUTILS.tar.bz2
-  BINUTILS_SHA1=ef93235588eb443e4c4a77f229a8d131bccaecc6
+  BINUTILS_SHA1=a22801a9cad45c85e9ff6afc10537d72
 
   test -f $BINUTILS.tar.bz2 || wget $BINUTILS_URL
   if test "`sha1sum $BINUTILS.tar.bz2|cut -d' ' -f1`" != "$BINUTILS_SHA1"
@@ -59,56 +59,6 @@ install_gold() {
 
   tar -xjvf $BINUTILS.tar.bz2
   cd $BINUTILS
-  patch -p1 <<EOF
-diff -u -r1.103 -r1.103.2.1
---- src/gold/object.h	2010/09/08 23:54:51	1.103
-+++ src/gold/object.h	2011/02/10 01:15:28	1.103.2.1
-@@ -1,6 +1,6 @@
- // object.h -- support for an object file for linking in gold  -*- C++ -*-
- 
--// Copyright 2006, 2007, 2008, 2009, 2010 Free Software Foundation, Inc.
-+// Copyright 2006, 2007, 2008, 2009, 2010, 2011 Free Software Foundation, Inc.
- // Written by Ian Lance Taylor <iant@google.com>.
- 
- // This file is part of gold.
-@@ -2165,15 +2165,6 @@
- 		      Output_symtab_xindex*,
- 		      Output_symtab_xindex*);
- 
--  // Clear the local symbol information.
--  void
--  clear_local_symbols()
--  {
--    this->local_values_.clear();
--    this->local_got_offsets_.clear();
--    this->local_plt_offsets_.clear();
--  }
--
-   // Record a mapping from discarded section SHNDX to the corresponding
-   // kept section.
-   void
-diff -u -r1.60 -r1.60.2.1
---- src/gold/reloc.cc	2010/10/14 22:10:22	1.60
-+++ src/gold/reloc.cc	2011/02/10 01:15:28	1.60.2.1
-@@ -1,6 +1,6 @@
- // reloc.cc -- relocate input files for gold.
- 
--// Copyright 2006, 2007, 2008, 2009, 2010 Free Software Foundation, Inc.
-+// Copyright 2006, 2007, 2008, 2009, 2010, 2011 Free Software Foundation, Inc.
- // Written by Ian Lance Taylor <iant@google.com>.
- 
- // This file is part of gold.
-@@ -685,9 +685,6 @@
-   // Write out the local symbols.
-   this->write_local_symbols(of, layout->sympool(), layout->dynpool(),
- 			    layout->symtab_xindex(), layout->dynsym_xindex());
--
--  // We should no longer need the local symbol values.
--  this->clear_local_symbols();
- }
- 
- // Sort a Read_multiple vector by file offset.
-EOF
   ./configure --prefix=/usr/local/gold --enable-gold --enable-threads
   make maybe-all-binutils maybe-all-gold -j4
   if sudo make maybe-install-binutils maybe-install-gold
