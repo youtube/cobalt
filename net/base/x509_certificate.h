03001bda6d861481a8561bca036a3e665f8f9763
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-11 14:26:01 +0000

    Revert 92006 - Make the host name verification conform to rfc6125 - its making Windows net_unittests hang on startup e.g. http://build.chromium.org/p/chromium/builders/XP%20Tests%20%282%29/builds/5386/steps/net_unittests/logs/stdio
    Full IP support
    Add suppport for partial wildcards in hostnames
    
    BUG=62973
    TEST=X509CertificateNameVerifyTest.VerifyHostname
    
    Review URL: http://codereview.chromium.org/7218011
    
    TBR=joth@chromium.org
    Review URL: http://codereview.chromium.org/7329046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92009 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/x509_certificate.h
+++ net/base/x509_certificate.h
@@ -342,7 +342,7 @@ class NET_API X509Certificate
   //
   // WARNING:  This function may return false negatives (for example, if
   //           |hostname| is an IP address literal) on some platforms.  Only
-  //           use in cases where some false-negatives are acceptable.
+  //           use in cases where some false-positives are acceptible.
   bool VerifyNameMatch(const std::string& hostname) const;
 
   // This method returns the DER encoded certificate.
@@ -410,19 +410,15 @@ class NET_API X509Certificate
   // (all zero) fingerprint on failure.
   static SHA1Fingerprint CalculateFingerprint(OSCertHandle cert_handle);
 
-  // Verifies that |hostname| matches one of the certificate names or IP
-  // addresses supplied, based on TLS name matching rules - specifically,
-  // following http://tools.ietf.org/html/rfc6125.
-  // |cert_common_name| is the Subject CN, e.g. from X509Certificate::subject().
-  // The members of |cert_san_dns_names| and |cert_san_ipaddrs| must be filled
-  // from the dNSName and iPAddress components of the subject alternative name
-  // extension, if present. Note these IP addresses are NOT ascii-encoded:
-  // they must be 4 or 16 bytes of network-ordered data, for IPv4 and IPv6
-  // addresses, respectively.
+  // Verifies that |hostname| matches one of the names in |cert_names|, based on
+  // TLS name matching rules, specifically following http://tools.ietf.org/html/draft-saintandre-tls-server-id-check-09#section-4.4.3
+  // The members of |cert_names| must have been extracted from the Subject CN or
+  // SAN fields of a certificate.
+  // WARNING:  This function may return false negatives (for example, if
+  //           |hostname| is an IP address literal) on some platforms.  Only
+  //           use in cases where some false-negatives are acceptible.
   static bool VerifyHostname(const std::string& hostname,
-                             const std::string& cert_common_name,
-                             const std::vector<std::string>& cert_san_dns_names,
-                             const std::vector<std::string>& cert_san_ip_addrs);
+                             const std::vector<std::string>& cert_names);
 
   // Performs the platform-dependent part of the Verify() method, verifiying
   // this certificate against the platform's root CA certificates.
