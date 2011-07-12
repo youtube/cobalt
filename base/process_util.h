e45abfab9c795b66cf5871cf897b50b536018e06
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-12 18:09:46 +0000

    Clean up users of a deprecated base::LaunchApp API.
    BUG=88990
    
    Review URL: http://codereview.chromium.org/7258005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92200 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process_util.h
+++ base/process_util.h
@@ -358,19 +358,6 @@ inline bool LaunchApp(const std::vector<std::string>& argv,
   return LaunchProcess(argv, options);
 }
 
-// TODO(evan): deprecated; change callers to use LaunchProcess, remove.
-inline bool LaunchApp(const std::vector<std::string>& argv,
-                      const environment_vector& environ,
-                      const file_handle_mapping_vector& fds_to_remap,
-                      bool wait, ProcessHandle* process_handle) {
-  LaunchOptions options;
-  options.environ = &environ;
-  options.fds_to_remap = &fds_to_remap;
-  options.wait = wait;
-  options.process_handle = process_handle;
-  return LaunchProcess(argv, options);
-}
-
 // AlterEnvironment returns a modified environment vector, constructed from the
 // given environment and the list of changes given in |changes|. Each key in
 // the environment is matched against the first element of the pairs. In the
