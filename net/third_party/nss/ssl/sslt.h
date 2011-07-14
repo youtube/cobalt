d37e4e90f74253aafc5b06a040edefa82b8cb27c
rkn@chromium.org <rkn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-14 18:45:31 +0000

    Add client-side support for the origin bound certificate TLS extension.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/7327029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92576 0039d316-1c4b-4281-b951-d872f2087c98

--- net/third_party/nss/ssl/sslt.h
+++ net/third_party/nss/ssl/sslt.h
@@ -206,9 +206,10 @@ typedef enum {
     ssl_session_ticket_xtn           = 35,
     ssl_next_proto_neg_xtn           = 13172,
     ssl_cached_info_xtn              = 13173,
-    ssl_renegotiation_info_xtn       = 0xff01	/* experimental number */
+    ssl_renegotiation_info_xtn       = 0xff01,	/* experimental number */
+    ssl_ob_cert_xtn                  = 0xff0f	/* experimental number */
 } SSLExtensionType;
 
-#define SSL_MAX_EXTENSIONS             8
+#define SSL_MAX_EXTENSIONS             9
 
 #endif /* __sslt_h_ */
