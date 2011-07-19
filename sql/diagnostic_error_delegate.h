15f04ca9b344414b9259f705c7241f1826e79069
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-19 18:40:21 +0000

    Move app/sql/* files to sql/ directory.
    I can't remove app/app.gyp and app/app_base.gypi yet because they are referenced
    by third_party gyp files :(
    
    BUG=72317
    TEST=None
    
    R=rsesek@chromium.org
    
    move app/sql to sql
    
    Review URL: http://codereview.chromium.org/7353026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93069 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ sql/diagnostic_error_delegate.h
@@ -0,0 +1,50 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SQL_DIAGNOSTIC_ERROR_DELEGATE_H_
+#define SQL_DIAGNOSTIC_ERROR_DELEGATE_H_
+#pragma once
+
+#include "base/logging.h"
+#include "base/metrics/histogram.h"
+#include "sql/connection.h"
+
+namespace sql {
+
+// This class handles the exceptional sqlite errors that we might encounter
+// if for example the db is corrupted. Right now we just generate a UMA
+// histogram for release and an assert for debug builds.
+//
+// Why is it a template you ask? well, that is a funny story. The histograms
+// need to be singletons that is why they are always static at the function
+// scope, but we cannot use the Singleton class because they are not default
+// constructible. The template parameter makes the compiler to create unique
+// classes that don't share the same static variable.
+template <class UniqueT>
+class DiagnosticErrorDelegate : public ErrorDelegate {
+ public:
+
+  virtual int OnError(int error, Connection* connection,
+                      Statement* stmt) {
+    NOTREACHED() << "sqlite error " << error
+                 << ", errno " << connection->GetLastErrno()
+                 << ": " << connection->GetErrorMessage();
+    RecordErrorInHistogram(error);
+    return error;
+  }
+
+ private:
+  static void RecordErrorInHistogram(int error) {
+    // Trim off the extended error codes.
+    error &= 0xff;
+
+    // The histogram values from sqlite result codes go currently from 1 to
+    // 26 currently but 50 gives them room to grow.
+    UMA_HISTOGRAM_ENUMERATION(UniqueT::name(), error, 50);
+  }
+};
+
+}  // namespace sql
+
+#endif  // SQL_DIAGNOSTIC_ERROR_DELEGATE_H_
---
+++ sql/sqlite_features_unittest.cc
@@ -0,0 +1,100 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+
+#include "base/file_path.h"
+#include "base/file_util.h"
+#include "base/path_service.h"
+#include "sql/connection.h"
+#include "sql/statement.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/sqlite/sqlite3.h"
+
+// Test that certain features are/are-not enabled in our SQLite.
+
+namespace {
+
+
+class StatementErrorHandler : public sql::ErrorDelegate {
+ public:
+  StatementErrorHandler() : error_(SQLITE_OK) {}
+
+  virtual int OnError(int error, sql::Connection* connection,
+                      sql::Statement* stmt) {
+    error_ = error;
+    const char* sql_txt = stmt ? stmt->GetSQLStatement() : NULL;
+    sql_text_ = sql_txt ? sql_txt : "no statement available";
+    return error;
+  }
+
+  int error() const { return error_; }
+
+  void reset_error() {
+    sql_text_.clear();
+    error_ = SQLITE_OK;
+  }
+
+  const char* sql_statement() const { return sql_text_.c_str(); }
+
+ private:
+  int error_;
+  std::string sql_text_;
+};
+
+class SQLiteFeaturesTest : public testing::Test {
+ public:
+  SQLiteFeaturesTest() : error_handler_(new StatementErrorHandler) {}
+
+  void SetUp() {
+    ASSERT_TRUE(PathService::Get(base::DIR_TEMP, &path_));
+    path_ = path_.AppendASCII("SQLStatementTest.db");
+    file_util::Delete(path_, false);
+    ASSERT_TRUE(db_.Open(path_));
+    // The |error_handler_| will be called if any sqlite statement operation
+    // returns an error code.
+    db_.set_error_delegate(error_handler_);
+  }
+
+  void TearDown() {
+    // If any error happened the original sql statement can be found in
+    // error_handler_->sql_statement().
+    EXPECT_EQ(SQLITE_OK, error_handler_->error());
+    db_.Close();
+    // If this fails something is going on with cleanup and later tests may
+    // fail, so we want to identify problems right away.
+    ASSERT_TRUE(file_util::Delete(path_, false));
+  }
+
+  sql::Connection& db() { return db_; }
+
+  int sqlite_error() const { return error_handler_->error(); }
+  void reset_error() const { error_handler_->reset_error(); }
+
+ private:
+  FilePath path_;
+  sql::Connection db_;
+  scoped_refptr<StatementErrorHandler> error_handler_;
+};
+
+// Do not include fts1 support, it is not useful, and nobody is
+// looking at it.
+TEST_F(SQLiteFeaturesTest, NoFTS1) {
+  ASSERT_FALSE(db().Execute("CREATE VIRTUAL TABLE foo USING fts1(x)"));
+}
+
+// fts2 is used for older history files, so we're signed on for
+// keeping our version up-to-date.
+// TODO(shess): Think up a crazy way to get out from having to support
+// this forever.
+TEST_F(SQLiteFeaturesTest, FTS2) {
+  ASSERT_TRUE(db().Execute("CREATE VIRTUAL TABLE foo USING fts2(x)"));
+}
+
+// fts3 is used for current history files, and also for WebDatabase.
+TEST_F(SQLiteFeaturesTest, FTS3) {
+  ASSERT_TRUE(db().Execute("CREATE VIRTUAL TABLE foo USING fts3(x)"));
+}
+
+}  // namespace
