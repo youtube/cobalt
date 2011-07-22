221bbe39b6d0f15514424ef9573bdfe0cc667300
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-22 15:41:45 +0000

    net: add NSS support for RFC 5705
    (Keying Material Exporters for TLS).
    
    This is a reworked version of the patch from
    https://bugzilla.mozilla.org/show_bug.cgi?id=507359.
    
    BUG=none
    TEST=none yet
    
    
    Review URL: http://codereview.chromium.org/7464031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93635 0039d316-1c4b-4281-b951-d872f2087c98

--- net/third_party/nss/patches/applypatches.sh
+++ net/third_party/nss/patches/applypatches.sh
@@ -26,3 +26,5 @@ patch -p6 < $patches_dir/didhandshakeresume.patch
 patch -p6 < $patches_dir/cbcrandomiv.patch
 
 patch -p6 < $patches_dir/origin_bound_certs.patch
+
+patch -p6 < $patches_dir/secret_exporter.patch
