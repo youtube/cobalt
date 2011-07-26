78f0211a2e8c887cc808763cbc6509c556edcaa3
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-26 23:09:27 +0000

    Remove ChromeNetLog dependency from content/browser/debugger.
    Add Observer concept to net::NetLog.
    
    Use net::NetLog::Observer in ChromeNetLog.
    
    Remove ChromeNetLog dependency in content/browser/debugger.
    
    Forked from http://codereview.chromium.org/7310029/ to finish Jói's
    patch.
    
    BUG=84078
    TEST=existing
    
    Review URL: http://codereview.chromium.org/7468019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94196 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/capturing_net_log.h
+++ net/base/capturing_net_log.h
@@ -10,6 +10,7 @@
 
 #include "base/atomicops.h"
 #include "base/basictypes.h"
+#include "base/compiler_specific.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/synchronization/lock.h"
@@ -60,9 +61,11 @@ class NET_API CapturingNetLog : public NetLog {
                         const base::TimeTicks& time,
                         const Source& source,
                         EventPhase phase,
-                        EventParameters* extra_parameters);
-  virtual uint32 NextID();
-  virtual LogLevel GetLogLevel() const;
+                        EventParameters* extra_parameters) OVERRIDE;
+  virtual uint32 NextID() OVERRIDE;
+  virtual LogLevel GetLogLevel() const OVERRIDE;
+  virtual void AddThreadSafeObserver(ThreadSafeObserver* observer) OVERRIDE;
+  virtual void RemoveThreadSafeObserver(ThreadSafeObserver* observer) OVERRIDE;
 
  private:
   // Needs to be "mutable" so can use it in GetEntries().
@@ -115,4 +118,3 @@ class NET_TEST CapturingBoundNetLog {
 }  // namespace net
 
 #endif  // NET_BASE_CAPTURING_NET_LOG_H_
-
