613b5be2e389ac59ea147707f05685cf34f06aef
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-26 06:48:44 +0000

    Make Profile own ProfileIOData.
    Notably this makes ChromeURLRequestContextGetter not own its ChromeURLRequestContext, and instead it keeps a base::WeakPtr. If anything is holding onto a ChromeURLRequestContextGetter longer than it should, and tries to use it, then it will crash. Currently no tests fail with this assumption, so hopefully this will prevent regressions. All ChromeURLRequestContextGetters should not outlive the Profile to which they belong.
    
    Note that this will cause problems if there are any URLRequests which outlive the Profile to which they belong, as they will reference URLRequestContexts which point to deleted memory. AFAICT this never happens, and I will enforce this later on by making URLRequests acquire weak pointers to URLRequestContexts. After that, I can stop refcounting URLRequestContext.
    
    I'm hopeful this changelist will fix a large number of leaks in browser_tests. Removing the refcounting of ProfileIOData will bring sanity to the lifecycle management of ProfileIOData and a large number of objects it owns. I've used base::WeakPtr in a number of places in order to prevent use-after-free bugs and force them to be NULL dereferences (crashes). In previous changelists, I've flushed out bugs revealed by this behavior. So currently, this changelist is clean on the trybots, although it's possible new code paths will be discovered via crash reports.
    
    Next steps:
    * URLRequests use base::WeakPtr<URLRequestContext>
    * Fix all other URLRequestContextGetters not to acquire references to URLRequestContext.
    * Stop refcounting URLRequestContext.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7432002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94047 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_context.h
+++ net/url_request/url_request_context.h
@@ -13,6 +13,7 @@
 
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/threading/non_thread_safe.h"
 #include "net/base/net_api.h"
 #include "net/base/net_log.h"
@@ -44,6 +45,10 @@ class NET_API URLRequestContext
       NON_EXPORTED_BASE(public base::NonThreadSafe) {
  public:
   URLRequestContext();
+
+  base::WeakPtr<URLRequestContext> GetWeakPtr() {
+    return weak_factory_.GetWeakPtr();
+  }
 
   // Copies the state from |other| into this context.
   void CopyFrom(URLRequestContext* other);
@@ -188,6 +193,8 @@ class NET_API URLRequestContext
   virtual ~URLRequestContext();
 
  private:
+  base::WeakPtrFactory<URLRequestContext> weak_factory_;
+
   // ---------------------------------------------------------------------------
   // Important: When adding any new members below, consider whether they need to
   // be added to CopyFrom.
