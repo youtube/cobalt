a7f799b91906f740febf4595d52991a0b1d24a34
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-05 03:20:24 +0000

    Revert 95496 - Create media.dll Review URL: http://codereview.chromium.org/7523051
    TBR=darin@chromium.org
    Review URL: http://codereview.chromium.org/7491048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95561 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/audio_file_reader.h
+++ media/filters/audio_file_reader.h
@@ -18,7 +18,7 @@ namespace media {
 
 class FFmpegURLProtocol;
 
-class MEDIA_EXPORT AudioFileReader {
+class AudioFileReader {
  public:
   // Audio file data will be read using the given protocol.
   // The AudioFileReader does not take ownership of |protocol| and
