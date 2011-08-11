6d7de772921d3544c562fa65cc22b2f04a5836a6
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-11 00:18:12 +0000

    FTP: add directory listing parser for OS/2 format.
    BUG=92154
    TEST=navigate to ftp://ftp.os4.su/ - no errors should appear
    
    Review URL: http://codereview.chromium.org/7590011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96275 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/ftp/ftp_directory_listing_parser_os2.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_FTP_FTP_DIRECTORY_LISTING_PARSER_OS2_H_
+#define NET_FTP_FTP_DIRECTORY_LISTING_PARSER_OS2_H_
+#pragma once
+
+#include <vector>
+
+#include "base/string16.h"
+#include "net/base/net_api.h"
+
+namespace net {
+
+struct FtpDirectoryListingEntry;
+
+// Parses OS/2 FTP directory listing. Returns true on success.
+NET_TEST bool ParseFtpDirectoryListingOS2(
+    const std::vector<string16>& lines,
+    std::vector<FtpDirectoryListingEntry>* entries);
+
+}  // namespace net
+
+#endif  // NET_FTP_FTP_DIRECTORY_LISTING_PARSER_OS2_H_
