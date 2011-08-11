3cc4c1a0fcd99cf26db435e2032f228a13792dce
steveblock@google.com <steveblock@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-11 11:23:52 +0000

    Update base/android to use new ScopedJava(Global)Reference
    See http://src.chromium.org/viewvc/chrome?view=rev&revision=95821
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7491099
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96359 0039d316-1c4b-4281-b951-d872f2087c98

--- base/android/jni_array.h
+++ base/android/jni_array.h
@@ -1,6 +1,6 @@
-// Copyright 2011 Google Inc. All Rights Reserved.
-// Author: michaelbai@google.com (Tao Bai)
-
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef BASE_ANDROID_JNI_ARRAY_H_
 #define BASE_ANDROID_JNI_ARRAY_H_
