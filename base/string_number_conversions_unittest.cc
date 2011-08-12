62f52c857fcd1d12317521c896aa367232e42391
ali.akbar@gmail.com <ali.akbar@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-12 20:13:20 +0000

    Fixes a corner case bug in the HexStringToInt conversion function which caused the string "0x" to be treated as a valid hexadecimal number. Although the parsed result was 0, the boolean flag returned indicated that the hex number was in proper format
    The IteratorRangeToNumber class's Invoke function increments the string pointer by 2 if the string starts with "0x" or "0X" and the length is greater than *or equal* to 2
    If the length of the string is 2, i.e "0x", after the pointer increment, the string would be empty and the function returns true as the parse result
    
    Changed the condition from "greater than or equal to" to "greater than"
    
    Added another test cases which now properly treats "0x" as an invalid hex string
    
    Contributed by ali.akbar@gmail.com
    
    BUG=92054
    
    Review URL: http://codereview.chromium.org/7584031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96605 0039d316-1c4b-4281-b951-d872f2087c98

--- base/string_number_conversions_unittest.cc
+++ base/string_number_conversions_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -306,6 +306,7 @@ TEST(StringNumberConversionsTest, HexStringToInt) {
     {"100000000", -1, false},  // don't care about |output|, just |success|
     {"-", 0, false},
     {"", 0, false},
+    {"0x", 0, false},
   };
 
   for (size_t i = 0; i < ARRAYSIZE_UNSAFE(cases); ++i) {
