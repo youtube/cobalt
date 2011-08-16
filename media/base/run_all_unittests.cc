0327dd1ea5f33348e7a47df9becaf9a91d7f15b4
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-16 18:10:42 +0000

    Revert 96974 - Remove mock_ffmpeg and update media unittests.
    BUG=92429
    TEST=BitstreamConverterTest.*, ChunkDemuxerTest.*, FFmpegDemuxerTest.*, FFmpegGlueTest.*, FFmpegVideoDecoderTest.*, FFmpegH264BitstreamConverterTest.*, FFmpegVideoDecodeEngineTest.*
    
    Review URL: http://codereview.chromium.org/7587012
    
    TBR=acolwell@chromium.org
    Review URL: http://codereview.chromium.org/7658017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96977 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/run_all_unittests.cc
+++ media/base/run_all_unittests.cc
@@ -1,14 +1,9 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "base/test/test_suite.h"
-#include "media/base/media.h"
 
 int main(int argc, char** argv) {
-  base::TestSuite suite(argc, argv);
-
-  media::InitializeMediaLibraryForTesting();
-
-  return suite.Run();
+  return base::TestSuite(argc, argv).Run();
 }
