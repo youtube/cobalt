f2a1ac3c2f41a4f806998758242fecdd92220ee4
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-22 18:56:59 +0000

    Revert "Fix sync integration tests by fixing Python path."
    This reverts commit r97696.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97697 0039d316-1c4b-4281-b951-d872f2087c98

--- net/test/python_utils.cc
+++ net/test/python_utils.cc
@@ -16,8 +16,6 @@
 const char kPythonPathEnv[] = "PYTHONPATH";
 
 void AppendToPythonPath(const FilePath& dir) {
-  CHECK(file_util::DirectoryExists(dir));
-
   scoped_ptr<base::Environment> env(base::Environment::Create());
   std::string old_path;
   std::string dir_path;
