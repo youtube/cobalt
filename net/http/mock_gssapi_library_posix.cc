7554718677a8381ddfcfa6cdc17e8f6ebbf9b904
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-24 12:38:19 +0000

    Revert 97925 - Linux: allow linking directly with Kerberos instead of using dlopen.
    dlopen is still the default for Google Chrome. This option
    is intended for Linux distro packagers.
    
    BUG=92689
    
    Review URL: http://codereview.chromium.org/7655046
    
    TBR=phajdan.jr@chromium.org
    Review URL: http://codereview.chromium.org/7719024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98028 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/mock_gssapi_library_posix.cc
+++ net/http/mock_gssapi_library_posix.cc
@@ -7,6 +7,7 @@
 #include "base/logging.h"
 #include "base/string_util.h"
 #include "base/stringprintf.h"
+#include "net/third_party/gssapi/gssapi.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace net {
@@ -278,7 +279,7 @@ OM_uint32 MockGSSAPILibrary::import_name(
   // Save the data.
   output->name = BufferToString(input_name_buffer);
   CopyOid(&output->name_type, input_name_type);
-  *output_name = reinterpret_cast<gss_name_t>(output);
+  *output_name = output;
 
   return GSS_S_COMPLETE;
 }
