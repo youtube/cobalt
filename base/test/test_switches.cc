5ea42c97b0ba9f3a133dd31223907909262b254f
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-25 17:34:05 +0000

    Remove unused test command-line switches.
    BUG=none
    
    Review URL: http://codereview.chromium.org/7743006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98263 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/test_switches.cc
+++ base/test/test_switches.cc
@@ -4,16 +4,10 @@
 
 #include "base/test/test_switches.h"
 
-// Time (in milliseconds) for which we wait for live operations to complete.
-const char switches::kLiveOperationTimeout[] = "live-operation-timeout";
-
 // Time (in milliseconds) that the tests should wait before timing out.
 // TODO(phajdan.jr): Clean up the switch names.
 const char switches::kTestLargeTimeout[] = "test-large-timeout";
-// TODO(phajdan.jr): 'Fix callers and remove --test-terminate-timeout.
-const char switches::kTestTerminateTimeout[] = "test-terminate-timeout";
 const char switches::kTestTinyTimeout[] = "test-tiny-timeout";
 const char switches::kUiTestActionTimeout[] = "ui-test-action-timeout";
 const char switches::kUiTestActionMaxTimeout[] = "ui-test-action-max-timeout";
 const char switches::kUiTestTerminateTimeout[] = "ui-test-terminate-timeout";
-const char switches::kUiTestTimeout[] = "test-timeout";
