ac5d825df5bb804d2f53f39de96b8bb1f09ce538
sehr@google.com <sehr@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-26 19:03:20 +0000

    Allow __pnacl__ target to build code included from Chrome. Committing for pdox@google.com. BUG=none TEST=trybots
    Review URL: http://codereview.chromium.org/7744034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98461 0039d316-1c4b-4281-b951-d872f2087c98

--- build/build_config.h
+++ build/build_config.h
@@ -110,6 +110,8 @@
 #define ARCH_CPU_32_BITS 1
 #define ARCH_CPU_LITTLE_ENDIAN 1
 #define WCHAR_T_IS_UNSIGNED 1
+#elif defined(__pnacl__)
+#define ARCH_CPU_32_BITS 1
 #else
 #error Please add support for your architecture in build/build_config.h
 #endif
