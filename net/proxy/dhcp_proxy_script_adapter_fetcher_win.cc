452031274c518b2a68a532d049755d84b491df55
jbates@chromium.org <jbates@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-02 18:23:02 +0000

    Update base/timer.h code to pass through Location from call sites. (reland) original CL w/LGTMs: http://codereview.chromium.org/7812036/
    Review URL: http://codereview.chromium.org/7824041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99409 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/dhcp_proxy_script_adapter_fetcher_win.cc
+++ net/proxy/dhcp_proxy_script_adapter_fetcher_win.cc
@@ -58,7 +58,7 @@ void DhcpProxyScriptAdapterFetcher::Fetch(
   state_ = STATE_WAIT_DHCP;
   callback_ = callback;
 
-  wait_timer_.Start(ImplGetTimeout(),
+  wait_timer_.Start(FROM_HERE, ImplGetTimeout(),
                     this, &DhcpProxyScriptAdapterFetcher::OnTimeout);
   worker_thread_ = ImplCreateWorkerThread(AsWeakPtr());
   worker_thread_->Start(adapter_name);
