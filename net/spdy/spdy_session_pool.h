ce483bc4d06a0941f622c23f1ad391280e538369
kristianm@chromium.org <kristianm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-12 15:39:10 +0000

    Mark SpdySessionPool as not threadsafe
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/7865012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100691 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_session_pool.h
+++ net/spdy/spdy_session_pool.h
@@ -14,6 +14,7 @@
 #include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/threading/non_thread_safe.h"
 #include "net/base/cert_database.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/ip_endpoint.h"
@@ -38,7 +39,8 @@ class SpdySession;
 class NET_EXPORT SpdySessionPool
     : public NetworkChangeNotifier::IPAddressObserver,
       public SSLConfigService::Observer,
-      public CertDatabase::Observer {
+      public CertDatabase::Observer,
+      public base::NonThreadSafe {
  public:
   SpdySessionPool(HostResolver* host_resolver,
                   SSLConfigService* ssl_config_service);
