4c84bacbd4ec3605b7c6078cc870b61e9e77d0d1
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-18 20:59:06 +0000

    Remove base/scoped_ptr.h
    Fix remaining users to use base/memory/scoped_ptr.h.
    
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/7930009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101715 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/simd/convert_rgb_to_yuv_unittest.cc
+++ media/base/simd/convert_rgb_to_yuv_unittest.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/scoped_ptr.h"
+#include "base/memory/scoped_ptr.h"
 #include "media/base/cpu_features.h"
 #include "media/base/simd/convert_rgb_to_yuv.h"
 #include "testing/gtest/include/gtest/gtest.h"
