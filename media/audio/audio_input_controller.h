c24bc8f6532decfc1a8a1db0a06e257cfdcd5bb5
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-22 19:30:12 +0000

    Fix more symbol exports and a compile failure for the component build of content.
    R=jam@chromium.org,crogers@google.com,allanwoj@chromium.org
    BUG=90442
    TEST=none
    
    Review URL: http://codereview.chromium.org/7981049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102332 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/audio_input_controller.h
+++ media/audio/audio_input_controller.h
@@ -41,7 +41,7 @@ class MEDIA_EXPORT AudioInputController
  public:
   // An event handler that receives events from the AudioInputController. The
   // following methods are called on the audio input controller thread.
-  class EventHandler {
+  class MEDIA_EXPORT EventHandler {
    public:
     virtual ~EventHandler() {}
     virtual void OnCreated(AudioInputController* controller) = 0;
