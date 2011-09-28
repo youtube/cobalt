46782d7a864d825228c76067e6898a75890f20ff
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-28 21:15:27 +0000

    Update SHA1_LENGTH -> kSHA1Length to match previous change to SHA256_LENGTH.
    (I didn't try and understand or fix why kSHA1Length is in base:: while kSHA256Length is in crypto::.)
    
    BUG=92247
    TEST=compiles
    Review URL: http://codereview.chromium.org/7972024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103179 0039d316-1c4b-4281-b951-d872f2087c98

--- crypto/sha2.h
+++ crypto/sha2.h
@@ -16,7 +16,7 @@ namespace crypto {
 //
 // Functions for SHA-384 and SHA-512 can be added when the need arises.
 
-static const size_t kSHA256Length = 32;  // length in bytes of a SHA-256 hash
+static const size_t kSHA256Length = 32;  // Length in bytes of a SHA-256 hash.
 
 // Computes the SHA-256 hash of the input string 'str' and stores the first
 // 'len' bytes of the hash in the output buffer 'output'.  If 'len' > 32,
