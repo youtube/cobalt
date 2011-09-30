e8e7d29050acab83b173af46b6fcc1787ed4108b
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-30 00:28:14 +0000

    Here are more exports needed for content_unittests to link in the component build.
    R=jam@chromium.org, darin@chromium.org
    BUG=90442
    TEST=
    
    Review URL: http://codereview.chromium.org/8054037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103394 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/speech/endpointer/energy_endpointer_params.h
+++ content/browser/speech/endpointer/energy_endpointer_params.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,11 +6,12 @@
 #define CONTENT_BROWSER_SPEECH_ENDPOINTER_ENERGY_ENDPOINTER_PARAMS_H_
 
 #include "base/basictypes.h"
+#include "content/common/content_export.h"
 
 namespace speech_input {
 
 // Input parameters for the EnergyEndpointer class.
-class EnergyEndpointerParams {
+class CONTENT_EXPORT EnergyEndpointerParams {
  public:
   EnergyEndpointerParams();
 
