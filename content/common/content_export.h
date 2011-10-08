1febe5569ebb7abfec7a6be5b84a11281fe73cad
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-08 02:08:08 +0000

    enable component build for content and appcache.
    R=jam@chromium.org
    BUG=90442
    TEST=
    
    Review URL: http://codereview.chromium.org/7888053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104632 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/content_export.h
+++ content/common/content_export.h
@@ -6,7 +6,7 @@
 #define CONTENT_COMMON_CONTENT_EXPORT_H_
 #pragma once
 
-#if 0 // TODO(dpranke): Uncomment: defined(COMPONENT_BUILD).
+#if defined(COMPONENT_BUILD) && !defined(COMPILE_CONTENT_STATICALLY)
 #if defined(WIN32)
 
 #if defined(CONTENT_IMPLEMENTATION)
