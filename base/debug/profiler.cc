1fb6dd176368cec523a76db2ff8f5ffff63a1877
scottbyer@chromium.org <scottbyer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-12 20:01:37 +0000

    Profiling for the renderer process.
    This lets you use --profiling-at-start=renderer and get profiler output for
    each renderer process started on Linux.
    
    The profiling classes are initialized at static initialization time, and the main thread is registered and the profile timer is started at that time. However, static initialization is done in the zygote, and the timer doesn't propagate across the fork, so trying to use the CPU profiler on the renderer never sampled. This patch re-registers the (new) main thread after the fork, which causes the profiler to check the state of the timer, and ends up re-starting it. A separate timer for flushing the profile and termination signal handler are added before the renderer process enters the sandbox in order to make sure the samples are written to disk.
    
    BUG=none
    TEST=Use it, get lovely traces, pperf away.
    
    
    Review URL: http://codereview.chromium.org/8083025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105134 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/profiler.cc
+++ base/debug/profiler.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -44,6 +44,10 @@ bool BeingProfiled() {
   return ProfilingIsEnabledForAllThreads();
 }
 
+void RestartProfilingAfterFork() {
+  ProfilerRegisterThread();
+}
+
 #else
 
 void StartProfiling(const std::string& name) {
@@ -57,6 +61,9 @@ void FlushProfiling() {
 
 bool BeingProfiled() {
   return false;
+}
+
+void RestartProfilingAfterFork() {
 }
 
 #endif
