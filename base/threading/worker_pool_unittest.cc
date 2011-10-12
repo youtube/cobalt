5ea236d6e71ce4ecd64068801b7e6e0f4c2bc5c9
steveblock@chromium.org <steveblock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-12 03:09:42 +0000

    Modify WaitableEvent::Wait() to return void
    Currently, WaitableEvent::Wait() returns bool. However, the Windows
    implementation DCHECKs that the return value is true and the POSIX
    implementation can never return false. Also, all call sites that use the return
    value simply DCHECK that it's true.
    
    This change modifies the method to return void, adds a DCHECK in the POSIX
    implementation and updates call sites.
    
    
    Review URL: http://codereview.chromium.org/8221021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104990 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/worker_pool_unittest.cc
+++ base/threading/worker_pool_unittest.cc
@@ -34,15 +34,12 @@ class PostTaskTestTask : public Task {
 TEST_F(WorkerPoolTest, PostTask) {
   WaitableEvent test_event(false, false);
   WaitableEvent long_test_event(false, false);
-  bool signaled;
 
   WorkerPool::PostTask(FROM_HERE, new PostTaskTestTask(&test_event), false);
   WorkerPool::PostTask(FROM_HERE, new PostTaskTestTask(&long_test_event), true);
 
-  signaled = test_event.Wait();
-  EXPECT_TRUE(signaled);
-  signaled = long_test_event.Wait();
-  EXPECT_TRUE(signaled);
+  test_event.Wait();
+  long_test_event.Wait();
 }
 
 }  // namespace base
