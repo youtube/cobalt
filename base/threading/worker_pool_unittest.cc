7d551d6e9143363788bfb2cf428a072006fcada0
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-14 17:30:37 +0000

    Add WorkerPool::PostTaskAndReply and use in DHCP code.
    This factors out the PostTaskAndReply implementation out of MessageLoopProxy so that it can be used for any destination thread accessible via a PostTask-like interface, and uses that code from both MessageLoopProxy and WorkerPool.
    
    The DhcpProxyScriptFetcherWin and DhcpProxyScriptAdapterFetcher classes were both using a PostTaskAndReply-like mechanism with a WorkerPool thread, and on inspection it looks like there are several places in net/ where this is done, and this motivated the larger change (vs. patch set 1 which was a mechanical switch to base::Bind from NewRunnableMethod).
    
    BUG=97516
    TEST=net_unittests
    
    Review URL: http://codereview.chromium.org/8139028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105512 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/worker_pool_unittest.cc
+++ base/threading/worker_pool_unittest.cc
@@ -4,8 +4,13 @@
 
 #include "base/threading/worker_pool.h"
 
+#include "base/bind.h"
 #include "base/location.h"
+#include "base/message_loop.h"
 #include "base/task.h"
+#include "base/test/test_timeouts.h"
+#include "base/time.h"
+#include "base/threading/thread_checker_impl.h"
 #include "base/synchronization/waitable_event.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
@@ -29,6 +34,47 @@ class PostTaskTestTask : public Task {
   WaitableEvent* event_;
 };
 
+class PostTaskAndReplyTester
+    : public base::RefCountedThreadSafe<PostTaskAndReplyTester> {
+ public:
+  PostTaskAndReplyTester() : finished_(false), test_event_(false, false) {
+  }
+
+  void RunTest() {
+    ASSERT_TRUE(thread_checker_.CalledOnValidThread());
+    WorkerPool::PostTaskAndReply(
+      FROM_HERE,
+      base::Bind(&PostTaskAndReplyTester::OnWorkerThread, this),
+      base::Bind(&PostTaskAndReplyTester::OnOriginalThread, this),
+      false);
+
+    test_event_.Wait();
+  }
+
+  void OnWorkerThread() {
+    // We're not on the original thread.
+    EXPECT_FALSE(thread_checker_.CalledOnValidThread());
+
+    test_event_.Signal();
+  }
+
+  void OnOriginalThread() {
+    EXPECT_TRUE(thread_checker_.CalledOnValidThread());
+    finished_ = true;
+  }
+
+  bool finished() const {
+    return finished_;
+  }
+
+ private:
+  bool finished_;
+  WaitableEvent test_event_;
+
+  // The Impl version performs its checks even in release builds.
+  ThreadCheckerImpl thread_checker_;
+};
+
 }  // namespace
 
 TEST_F(WorkerPoolTest, PostTask) {
@@ -40,6 +86,20 @@ TEST_F(WorkerPoolTest, PostTask) {
 
   test_event.Wait();
   long_test_event.Wait();
+}
+
+TEST_F(WorkerPoolTest, PostTaskAndReply) {
+  MessageLoop message_loop;
+  scoped_refptr<PostTaskAndReplyTester> tester(new PostTaskAndReplyTester());
+  tester->RunTest();
+
+  const TimeDelta kMaxDuration =
+      TimeDelta::FromMilliseconds(TestTimeouts::tiny_timeout_ms());
+  TimeTicks start = TimeTicks::Now();
+  while (!tester->finished() && TimeTicks::Now() - start < kMaxDuration) {
+    MessageLoop::current()->RunAllPending();
+  }
+  EXPECT_TRUE(tester->finished());
 }
 
 }  // namespace base
