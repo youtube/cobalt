47ab04757409d3e34a2342286acd88cc4de09c6e
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-18 17:41:07 +0000

    OpenBSD patches for base / split from CR #8275005
    base/base.gypi:
    	- Add native_library_linux.cc to the openbsd build.
    	- Add '..' to include_dirs so that OS_* definitions are
    	  available in symbolize.cc
    
    base/debug/debugger_posix.cc:
    	- Add support for figuring out if the process is being
    	  debugged on OpenBSD by sharing some code with Mac.
    
    base/process_util_unittest.cc:
    	- Disable the OutOfMemoryTest on OpenBSD
    
    base/third_party/symbolize/symbolize.cc:
    	- Include the correct elf header on OpenBSD
    
    build/linux/system.gyp:
    	- The dl library is linux only, so only use it there.
    
    BUG=
    TEST=
    Review URL: http://codereview.chromium.org/8329023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106078 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/debugger_posix.cc
+++ base/debug/debugger_posix.cc
@@ -59,7 +59,7 @@ bool SpawnDebuggerOnProcess(unsigned /* process_id */) {
   return false;
 }
 
-#if defined(OS_MACOSX)
+#if defined(OS_MACOSX) || defined(OS_OPENBSD)
 
 // Based on Apple's recommended method as described in
 // http://developer.apple.com/qa/qa2004/qa1361.html
@@ -80,12 +80,23 @@ bool BeingDebugged() {
     KERN_PROC,
     KERN_PROC_PID,
     getpid()
+#if defined(OS_OPENBSD)
+    , sizeof(struct kinfo_proc),
+    0
+#endif
   };
 
   // Caution: struct kinfo_proc is marked __APPLE_API_UNSTABLE.  The source and
   // binary interfaces may change.
   struct kinfo_proc info;
   size_t info_size = sizeof(info);
+
+#if defined(OS_OPENBSD)
+  if (sysctl(mib, arraysize(mib), NULL, &info_size, NULL, 0) < 0)
+    return -1;
+
+  mib[5] = (info_size / sizeof(struct kinfo_proc));
+#endif
 
   int sysctl_result = sysctl(mib, arraysize(mib), &info, &info_size, NULL, 0);
   DCHECK_EQ(sysctl_result, 0);
@@ -97,7 +108,11 @@ bool BeingDebugged() {
 
   // This process is being debugged if the P_TRACED flag is set.
   is_set = true;
+#if defined(OS_OPENBSD)
+  being_debugged = (info.p_flag & P_TRACED) != 0;
+#else
   being_debugged = (info.kp_proc.p_flag & P_TRACED) != 0;
+#endif
   return being_debugged;
 }
 
