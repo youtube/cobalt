b4bebdd34ea8e8408a783cf1ccad70d19df0dad0
kushi.p@gmail.com <kushi.p@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-24 18:04:16 +0000

    Create ObjCCast<>() and ObjCCastStrict<>() methods
    These methods exist for casting a basic id to a more specific (NSObject derived) type.
    
    BUG=86004
    TEST=Initialise common NS objects and confirm ObjCCast*<>() casts to the expected result.
    
    
    Review URL: http://codereview.chromium.org/8356024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106946 0039d316-1c4b-4281-b951-d872f2087c98

--- base/mac/foundation_util.h
+++ base/mac/foundation_util.h
@@ -224,6 +224,47 @@ T CFCast(const CFTypeRef& cf_val);
 BASE_EXPORT template<class T>
 T CFCastStrict(const CFTypeRef& cf_val);
 
+#if defined(__OBJC__)
+
+// ObjCCast<>() and ObjCCastStrict<>() cast a basic id to a more
+// specific (NSObject-derived) type. The compatibility of the passed
+// object is found by checking if it's a kind of the requested type
+// identifier. If the supplied object is not compatible with the
+// requested return type, ObjCCast<>() returns nil and
+// ObjCCastStrict<>() will DCHECK. Providing a nil pointer to either
+// variant results in nil being returned without triggering any DCHECK.
+//
+// The strict variant is useful when retrieving a value from a
+// collection which only has values of a specific type, e.g. an
+// NSArray of NSStrings. The non-strict variant is useful when
+// retrieving values from data that you can't fully control. For
+// example, a plist read from disk may be beyond your exclusive
+// control, so you'd only want to check that the values you retrieve
+// from it are of the expected types, but not crash if they're not.
+//
+// Example usage:
+// NSString* version = base::mac::ObjCCast<NSString>(
+//     [bundle objectForInfoDictionaryKey:@"CFBundleShortVersionString"]);
+//
+// NSString* str = base::mac::ObjCCastStrict<NSString>(
+//     [ns_arr_of_ns_strs objectAtIndex:0]);
+BASE_EXPORT template<class T>
+T* ObjCCast(id objc_val) {
+  if ([objc_val isKindOfClass:[T class]]) {
+    return reinterpret_cast<T*>(objc_val);
+  }
+  return nil;
+}
+
+BASE_EXPORT template<class T>
+T* ObjCCastStrict(id objc_val) {
+  T* rv = ObjCCast<T>(objc_val);
+  DCHECK(objc_val == nil || rv);
+  return rv;
+}
+
+#endif  // defined(__OBJC__)
+
 }  // namespace mac
 }  // namespace base
 
