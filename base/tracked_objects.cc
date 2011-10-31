09a5ecc780833a58354ed8432078a64dcc27602c
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-31 13:14:45 +0000

    Avoid all unnecessary calls to TLS during thread teardown
    Mac base_unittests is still flaky....
    
    Pthread is still sometimes crashing Mac on shutdown.
    
    I'm suspicious that my handlers are tickling it.
    This removes a DCHECK(), which checked status in
    TLS during the teardown.
    
    TBR=rtenneti
    Review URL: http://codereview.chromium.org/8424013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107944 0039d316-1c4b-4281-b951-d872f2087c98

--- base/tracked_objects.cc
+++ base/tracked_objects.cc
@@ -203,7 +203,6 @@ ThreadData* ThreadData::Get() {
 void ThreadData::OnThreadTermination(void* thread_data) {
   if (!kTrackAllTaskObjects)
     return;  // Not compiled in.
-  DCHECK(tls_index_.initialized());
   if (!thread_data)
     return;
   reinterpret_cast<ThreadData*>(thread_data)->OnThreadTerminationCleanup();
