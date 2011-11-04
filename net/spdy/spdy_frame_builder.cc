00a32039707fd067f452d07dc014013bca04ade4
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-04 00:01:57 +0000

    Merge spdy_framer changes from server.
    R=willchan
    TEST=network unit tests
    
    Review URL: http://codereview.chromium.org/8437098
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108585 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_frame_builder.cc
+++ net/spdy/spdy_frame_builder.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -18,6 +18,14 @@ SpdyFrameBuilder::SpdyFrameBuilder()
       length_(0),
       variable_buffer_offset_(0) {
   Resize(kInitialPayload);
+}
+
+SpdyFrameBuilder::SpdyFrameBuilder(size_t size)
+    : buffer_(NULL),
+      capacity_(0),
+      length_(0),
+      variable_buffer_offset_(0) {
+  Resize(size);
 }
 
 SpdyFrameBuilder::SpdyFrameBuilder(const char* data, int data_len)
