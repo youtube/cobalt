b170381a14a5514c5cd60902c43f1303c4f0aedf
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-07 22:10:16 +0000

    Revert 108910 - Remove HostResolverImpl from Mock*HostResolver.
    MockHostResolver should not depend on the job dispatch machinery of
    HostResolverImpl. This implementation aims to be more readable/reliable
    while providing the same testing utility.
    
    MockHostResolver does not use extra threads hence cannot support 
    WaitingHostResolverProc. To mock slow resolution, HangingHostResolver is
    provided for unit_tests:PredictorTest.
    
    Updated host_resolver_impl_unittest to test HostResolverImpl directly
    (rather than through MockHostResolver).
    
    BUG=99889
    TEST=./net_unittests ; ./unit_tests --gtest_filter='PredictorTest.*' ; ./jingle_unittests
    
    
    Review URL: http://codereview.chromium.org/8267001
    
    TBR=szym@chromium.org
    Review URL: http://codereview.chromium.org/8491023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108922 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/mapped_host_resolver_unittest.cc
+++ net/base/mapped_host_resolver_unittest.cc
@@ -1,10 +1,9 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "net/base/mapped_host_resolver.h"
 
-#include "net/base/address_list.h"
 #include "net/base/mock_host_resolver.h"
 #include "net/base/net_errors.h"
 #include "net/base/net_log.h"
