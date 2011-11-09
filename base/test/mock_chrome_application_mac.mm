4337a182ecf75c92d75a33247ac0cd2d5a72f45c
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-09 00:27:08 +0000

    [Mac] Make ScopedSendingEvent depend on protocol instead of app class.
    Also provide appropriate test support, which will allow content/
    clients of ScopedSendingEvent to participate.
    
    BUG=102224
    
    
    Review URL: http://codereview.chromium.org/8432014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109146 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/mock_chrome_application_mac.mm
+++ base/test/mock_chrome_application_mac.mm
@@ -1,8 +1,10 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "base/test/mock_chrome_application_mac.h"
+
+#include "base/logging.h"
 
 @implementation MockCrApp
 - (BOOL)isHandlingSendEvent {
@@ -13,7 +15,11 @@
 namespace mock_cr_app {
 
 void RegisterMockCrApp() {
-  [MockCrApp sharedApplication];
+  NSApplication* app = [MockCrApp sharedApplication];
+
+  // Would prefer ASSERT_TRUE() to provide better test failures, but
+  // this class is used by remoting/ for a non-test use.
+  DCHECK([app conformsToProtocol:@protocol(CrAppProtocol)]);
 }
 
 }  // namespace mock_cr_app
