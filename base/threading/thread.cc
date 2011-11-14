ee74e350f4821ceb2d22d1446b2fa5b2b4788670
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-14 23:52:33 +0000

    Revert 109962 - Revert 109903 - Revert 109898 - Set the profiler's thread name at a central place
    [I've removed the code, via a separate CL, that DCHECK() earlier today]
    
    This will automatically catch thread names in several of the
    child processes we are after, and it also catches the name
    for a webkcore thread.
    
    r=rtenneti
    Review URL: http://codereview.chromium.org/8558003
    
    TBR=jar@chromium.org
    Review URL: http://codereview.chromium.org/8564014
    
    TBR=jar@chromium.org
    Review URL: http://codereview.chromium.org/8565030
    
    TBR=jar@chromium.org
    Review URL: http://codereview.chromium.org/8555022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109980 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/thread.cc
+++ base/threading/thread.cc
@@ -8,6 +8,7 @@
 #include "base/third_party/dynamic_annotations/dynamic_annotations.h"
 #include "base/threading/thread_local.h"
 #include "base/synchronization/waitable_event.h"
+#include "base/tracked_objects.h"
 
 namespace base {
 
@@ -151,6 +152,7 @@ void Thread::ThreadMain() {
     ANNOTATE_THREAD_NAME(name_.c_str());  // Tell the name to race detector.
     message_loop.set_thread_name(name_);
     message_loop_ = &message_loop;
+    tracked_objects::ThreadData::InitializeThreadContext(name_);
 
     // Let the thread do extra initialization.
     // Let's do this before signaling we are started.
