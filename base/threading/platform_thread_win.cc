c22545a51471f86b35d18d34ea38122285dcca67
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-14 17:41:34 +0000

    Revert 109898 - Set the profiler's thread name at a central place
    This will automatically catch thread names in several of the
    child processes we are after, and it also catches the name
    for a webkcore thread.
    
    r=rtenneti
    Review URL: http://codereview.chromium.org/8558003
    
    TBR=jar@chromium.org
    Review URL: http://codereview.chromium.org/8564014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109903 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/platform_thread_win.cc
+++ base/threading/platform_thread_win.cc
@@ -8,7 +8,6 @@
 #include "base/logging.h"
 #include "base/threading/thread_local.h"
 #include "base/threading/thread_restrictions.h"
-#include "base/tracked_objects.h"
 
 #include "base/win/windows_version.h"
 
@@ -101,7 +100,6 @@ void PlatformThread::Sleep(int duration_ms) {
 // static
 void PlatformThread::SetName(const char* name) {
   current_thread_name.Set(const_cast<char*>(name));
-  tracked_objects::ThreadData::InitializeThreadContext(name);
 
   // The debugger needs to be around to catch the name in the exception.  If
   // there isn't a debugger, we are just needlessly throwing an exception.
