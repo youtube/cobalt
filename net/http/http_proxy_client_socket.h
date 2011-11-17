c712fb375c95e9d75f9cdc6b015debb312fb8c20
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-17 18:31:03 +0000

    Allow chrome to handle 407 auth challenges to CONNECT requests through HTTPS Proxies.  This also changes the mechanism used to restart HttpProxyClientSocket requests with auth.  Previously the transport socket would be Disconnected, and then re-Connected (which was not implemented for SSLClientSockets).  However, the approach was problematic in the face of, for example, ipv6.  The new approach is to close the HttpProxyClientSocket, and request a new socket from the pool.
    Review URL: http://codereview.chromium.org/8502024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110529 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_proxy_client_socket.h
+++ net/http/http_proxy_client_socket.h
@@ -50,15 +50,6 @@ class HttpProxyClientSocket : public ProxyClientSocket {
   // On destruction Disconnect() is called.
   virtual ~HttpProxyClientSocket();
 
-  // If Connect (or its callback) returns PROXY_AUTH_REQUESTED, then
-  // credentials should be added to the HttpAuthController before calling
-  // RestartWithAuth.
-  int RestartWithAuth(OldCompletionCallback* callback);
-
-  const scoped_refptr<HttpAuthController>& auth_controller() {
-    return auth_;
-  }
-
   bool using_spdy() {
     return using_spdy_;
   }
@@ -66,6 +57,8 @@ class HttpProxyClientSocket : public ProxyClientSocket {
   // ProxyClientSocket methods:
   virtual const HttpResponseInfo* GetConnectResponseInfo() const OVERRIDE;
   virtual HttpStream* CreateConnectResponseStream() OVERRIDE;
+  virtual int RestartWithAuth(OldCompletionCallback* callback) OVERRIDE;
+  virtual const scoped_refptr<HttpAuthController>& auth_controller() OVERRIDE;
 
   // StreamSocket methods:
   virtual int Connect(OldCompletionCallback* callback) OVERRIDE;
@@ -103,8 +96,6 @@ class HttpProxyClientSocket : public ProxyClientSocket {
     STATE_READ_HEADERS_COMPLETE,
     STATE_DRAIN_BODY,
     STATE_DRAIN_BODY_COMPLETE,
-    STATE_TCP_RESTART,
-    STATE_TCP_RESTART_COMPLETE,
     STATE_DONE,
   };
 
@@ -115,8 +106,6 @@ class HttpProxyClientSocket : public ProxyClientSocket {
 
   int PrepareForAuthRestart();
   int DidDrainBodyForAuthRestart(bool keep_alive);
-
-  int HandleAuthChallenge();
 
   void LogBlockedTunnelResponse(int response_code) const;
 
@@ -132,8 +121,6 @@ class HttpProxyClientSocket : public ProxyClientSocket {
   int DoReadHeadersComplete(int result);
   int DoDrainBody();
   int DoDrainBodyComplete(int result);
-  int DoTCPRestart();
-  int DoTCPRestartComplete(int result);
 
   OldCompletionCallbackImpl<HttpProxyClientSocket> io_callback_;
   State next_state_;
