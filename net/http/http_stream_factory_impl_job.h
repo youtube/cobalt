5d6e2c163d38921ae12f52ad9a6bc11d08ea8321
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-18 01:50:08 +0000

    Cleanup: Remove forward declarations in various directories.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/8567022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110631 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_stream_factory_impl_job.h
+++ net/http/http_stream_factory_impl_job.h
@@ -24,7 +24,6 @@ class ClientSocketHandle;
 class HttpAuthController;
 class HttpNetworkSession;
 class HttpStream;
-class TransportSocketParams;
 
 // An HttpStreamRequestImpl exists for each stream which is in progress of being
 // created for the StreamFactory.
