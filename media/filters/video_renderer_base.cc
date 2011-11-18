9ab088e88a262d9c3b0a05f87cc051d039ca0d6d
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-18 22:53:40 +0000

    Clear VideoRendererBase::current_frame_ when stopped.
    I previously assumed that PipelineImpl would go through a clean Pause/Flush/Stop on an error but it turns out that Stop() can be called at anytime. Fortunately a DCHECK() would catch this condition and was easily triggerable using the test case attached in bug 100434.
    
    Review URL: http://codereview.chromium.org/8561019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110778 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/video_renderer_base.cc
+++ media/filters/video_renderer_base.cc
@@ -477,6 +477,7 @@ void VideoRendererBase::DoStopOrError_Locked() {
   DCHECK(!pending_paint_);
   DCHECK(!pending_paint_with_last_available_);
   lock_.AssertAcquired();
+  current_frame_ = NULL;
   last_available_frame_ = NULL;
   DCHECK(!pending_read_);
 }
