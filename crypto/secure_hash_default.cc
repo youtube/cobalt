e2d784bf2a9093896a4595b2f4ef1f7e150e6c7a
ahendrickson@chromium.org <ahendrickson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-19 16:54:13 +0000

    Added serialization to SecureHash.
    These will be used to resume hash generation when a download is interrupted and later resumed.
    
    BUG=None.
    TEST=SecureHashTest.TestSerialization
    
    
    Review URL: http://codereview.chromium.org/8588057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110842 0039d316-1c4b-4281-b951-d872f2087c98

--- crypto/secure_hash_default.cc
+++ crypto/secure_hash_default.cc
@@ -5,6 +5,7 @@
 #include "crypto/secure_hash.h"
 
 #include "base/logging.h"
+#include "base/pickle.h"
 #include "crypto/third_party/nss/chromium-blapi.h"
 #include "crypto/third_party/nss/chromium-sha256.h"
 
@@ -12,8 +13,12 @@ namespace crypto {
 
 namespace {
 
+const char kSHA256Descriptor[] = "NSS";
+
 class SecureHashSHA256NSS : public SecureHash {
  public:
+  static const int kSecureHashVersion = 1;
+
   SecureHashSHA256NSS() {
     SHA256_Begin(&ctx_);
   }
@@ -30,9 +35,52 @@ class SecureHashSHA256NSS : public SecureHash {
                static_cast<unsigned int>(len));
   }
 
+  virtual bool Serialize(Pickle* pickle);
+  virtual bool Deserialize(void** data_iterator, Pickle* pickle);
+
  private:
   SHA256Context ctx_;
 };
+
+bool SecureHashSHA256NSS::Serialize(Pickle* pickle) {
+  if (!pickle)
+    return false;
+
+  if (!pickle->WriteInt(kSecureHashVersion) ||
+      !pickle->WriteString(kSHA256Descriptor) ||
+      !pickle->WriteBytes(&ctx_, sizeof(ctx_))) {
+    return false;
+  }
+
+  return true;
+}
+
+bool SecureHashSHA256NSS::Deserialize(void** data_iterator, Pickle* pickle) {
+  if (!pickle)
+    return false;
+
+  int version;
+  if (!pickle->ReadInt(data_iterator, &version))
+    return false;
+
+  if (version > kSecureHashVersion)
+    return false;  // We don't know how to deal with this.
+
+  std::string type;
+  if (!pickle->ReadString(data_iterator, &type))
+    return false;
+
+  if (type != kSHA256Descriptor)
+    return false;  // It's the wrong kind.
+
+  const char* data = NULL;
+  if (!pickle->ReadBytes(data_iterator, &data, sizeof(ctx_)))
+    return false;
+
+  memcpy(&ctx_, data, sizeof(ctx_));
+
+  return true;
+}
 
 }  // namespace
 
