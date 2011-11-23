6c4cc698204ffc710d3661cf6a9fbeed0a99c73e
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-23 21:53:30 +0000

    Whitespace change to force a relink of media/media_unittests on all builders.
    BUG=105234
    
    Review URL: http://codereview.chromium.org/8662029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111418 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/media_export.h
+++ media/base/media_export.h
@@ -6,7 +6,7 @@
 #define MEDIA_BASE_MEDIA_EXPORT_H_
 #pragma once
 
-// Defines MEDIA_EXPORT so that functionality implemented by the Media module
+// Define MEDIA_EXPORT so that functionality implemented by the Media module
 // can be exported to consumers.
 
 #if defined(COMPONENT_BUILD)
