cff45a227d1008b66e5551b072eb521eda6e222d
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-23 18:39:42 +0000

    fix a racing condition when renderer process requests StopCapture before host gets controller fix a typo (thanks ihf@)
    BUG=104581
    TEST=trybots
    Review URL: http://codereview.chromium.org/8549010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111373 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/video_capture.h
+++ media/video/capture/video_capture.h
@@ -18,17 +18,6 @@ namespace media {
 
 class MEDIA_EXPORT VideoCapture {
  public:
-  // Current status of the video capture device in the browser process. Browser
-  // process sends information about the current capture state and error to the
-  // renderer process using this type.
-  enum State {
-    kStarted,
-    kPaused,
-    kStopped,
-    kStopping,
-    kError,
-  };
-
   // TODO(wjia): consider merging with media::VideoFrame if possible.
   class VideoFrameBuffer : public base::RefCountedThreadSafe<VideoFrameBuffer> {
    public:
