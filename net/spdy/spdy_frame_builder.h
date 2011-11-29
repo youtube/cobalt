9d4d024ee94188305de8b718a7d2c670a3d5121d
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 03:13:03 +0000

    SPDY changes from server.
    R=willchan
    Review URL: http://codereview.chromium.org/8473007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111854 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_frame_builder.h
+++ net/spdy/spdy_frame_builder.h
@@ -28,6 +28,8 @@ namespace spdy {
 // track of the type of data written to it.
 class NET_EXPORT_PRIVATE SpdyFrameBuilder {
  public:
+  ~SpdyFrameBuilder();
+
   SpdyFrameBuilder();
 
   // Initiailizes a SpdyFrameBuilder with a buffer of given size.
@@ -39,8 +41,6 @@ class NET_EXPORT_PRIVATE SpdyFrameBuilder {
   // SpdyFrameBuilder.  Only const methods should be used when initialized
   // this way.
   SpdyFrameBuilder(const char* data, int data_len);
-
-  ~SpdyFrameBuilder();
 
   // Returns the size of the SpdyFrameBuilder's data.
   int length() const { return length_; }
@@ -77,7 +77,7 @@ class NET_EXPORT_PRIVATE SpdyFrameBuilder {
     return WriteBytes(&value, sizeof(value));
   }
   bool WriteString(const std::string& value);
-  bool WriteBytes(const void* data, uint16 data_len);
+  bool WriteBytes(const void* data, uint32 data_len);
 
   // Write an integer to a particular offset in the data buffer.
   bool WriteUInt32ToOffset(int offset, uint32 value) {
