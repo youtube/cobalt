70b642a4cb6bbf58a9cac20e488f121507fe3ccd
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 18:12:26 +0000

    Force annotation of overridden methods with OVERRIDE.
    BUG=104314
    TEST=green builds
    
    Review URL: http://codereview.chromium.org/8727007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111969 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/clang/scripts/plugin_flags.sh
+++ tools/clang/scripts/plugin_flags.sh
@@ -17,5 +17,4 @@ else
 fi
 
 echo -Xclang -load -Xclang $CLANG_LIB_PATH/libFindBadConstructs.$LIBSUFFIX \
-  -Xclang -add-plugin -Xclang find-bad-constructs \
-  -Xclang -plugin-arg-find-bad-constructs -Xclang skip-override
+  -Xclang -add-plugin -Xclang find-bad-constructs
