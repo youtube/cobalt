0fcd2e07e135ad5bc352e5898c76386facf41e89
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-30 18:18:45 +0000

    [Mac] Move ScopedSendingEvent from content/common/mac to base/mac.
    Also merge content/ MockCrControlApp into base/ MockCrApp.
    
    BUG=102224
    
    Review URL: http://codereview.chromium.org/8724004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112249 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/mock_chrome_application_mac.mm
+++ base/test/mock_chrome_application_mac.mm
@@ -4,22 +4,38 @@
 
 #include "base/test/mock_chrome_application_mac.h"
 
+#include "base/auto_reset.h"
 #include "base/logging.h"
 
 @implementation MockCrApp
-- (BOOL)isHandlingSendEvent {
-  return NO;
+
++ (NSApplication*)sharedApplication {
+  NSApplication* app = [super sharedApplication];
+  DCHECK([app conformsToProtocol:@protocol(CrAppControlProtocol)])
+      << "Existing NSApp (class " << [[app className] UTF8String]
+      << ") does not conform to required protocol.";
+  return app;
 }
+
+- (void)sendEvent:(NSEvent*)event {
+  AutoReset<BOOL> scoper(&handlingSendEvent_, YES);
+  [super sendEvent:event];
+}
+
+- (void)setHandlingSendEvent:(BOOL)handlingSendEvent {
+  handlingSendEvent_ = handlingSendEvent;
+}
+
+- (BOOL)isHandlingSendEvent {
+  return handlingSendEvent_;
+}
+
 @end
 
 namespace mock_cr_app {
 
 void RegisterMockCrApp() {
-  NSApplication* app = [MockCrApp sharedApplication];
-
-  // Would prefer ASSERT_TRUE() to provide better test failures, but
-  // this class is used by remoting/ for a non-test use.
-  DCHECK([app conformsToProtocol:@protocol(CrAppProtocol)]);
+  [MockCrApp sharedApplication];
 }
 
 }  // namespace mock_cr_app
