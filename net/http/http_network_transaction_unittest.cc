0ee377b8ad2c1dfcf74ec89634ce1341931bc300
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-08 04:31:37 +0000

    Revert 110505 - Report ERR_CONTENT_LENGTH_MISMATCH when the count of bytes received doesn't match Content-Length.
    This used to be reported as ERR_CONNECTION_CLOSED. Unfortunately, there are a number of misconfigured servers on the web which report a Content-Length completely divorced from reality. Other browsers display whatever data was received without reporting errors in this case, and Chrome now does the same.
    
    An earlier CL simply removed error reporting for this case. However, some uses may want to still treat this like an error (such as URLFetcher), or could eventually treat the case as an error and recover (such as downloads).
    
    Data received will be populated in the disk cache as a truncated entry, if the server advertises byte range support.
    
    WebURLLoaderImpl ignores this for all async and sync resource loads. At some point it may want to selectively do this based on content type.
    
    Downloads ignore the error, but no longer ignore ERR_CONNECTION_CLOSED.
    
    Other uses will treat this like an error.
    
    BUG=52847
    TEST=Manually, go to a site that advertises a large content-length and closes the connection before writing so many bytes, and notice that the page now displays.
    
    Automatic: HttpNetworkTransactionTest.LargeContentLengthThenClose, URLRequestTestHttp.GetZippedTest
    
    
    Review URL: http://codereview.chromium.org/8496016
    
    TBR=cbentzel@chromium.org
    
    Review URL: http://codereview.chromium.org/8863002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113562 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_network_transaction_unittest.cc
+++ net/http/http_network_transaction_unittest.cc
@@ -6491,7 +6491,7 @@ TEST_F(HttpNetworkTransactionTest, LargeContentLengthThenClose) {
 
   std::string response_data;
   rv = ReadTransaction(trans.get(), &response_data);
-  EXPECT_EQ(ERR_CONTENT_LENGTH_MISMATCH, rv);
+  EXPECT_EQ(ERR_CONNECTION_CLOSED, rv);
 }
 
 TEST_F(HttpNetworkTransactionTest, UploadFileSmallerThanLength) {
