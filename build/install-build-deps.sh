91d344e532ceb2538551f13a53253fb60ad54519
dalecurtis@chromium.org <dalecurtis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-13 01:42:48 +0000

    Remove psutil from tree, install via install-build-deps.sh
    Per discussion with thakis, cmp we should remove this dep from the tree and instead install it via install-build-deps.sh.
    
    BUG=106105
    TEST=Installed dep, ran tests.
    
    
    Review URL: http://codereview.chromium.org/8919026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114146 0039d316-1c4b-4281-b951-d872f2087c98

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -102,17 +102,17 @@ fi
 chromeos_dev_list="libpulse-dev"
 
 # Packages need for development
-dev_list="apache2.2-bin bison curl elfutils
-          fakeroot flex g++ gperf language-pack-fr
-          libapache2-mod-php5 libasound2-dev libbz2-dev libcairo2-dev
-          libcups2-dev libdbus-glib-1-dev libelf-dev libgconf2-dev
-          libgl1-mesa-dev libglu1-mesa-dev libglib2.0-dev libgnome-keyring-dev
-          libgtk2.0-dev libjpeg62-dev libkrb5-dev libnspr4-dev libnss3-dev
-          libpam0g-dev libsctp-dev libsqlite3-dev libssl-dev libxslt1-dev
-          libxss-dev libxt-dev libxtst-dev mesa-common-dev msttcorefonts patch
-          perl libwww-perl php5-cgi pkg-config python python-dev rpm subversion
-          ttf-dejavu-core ttf-kochi-gothic ttf-kochi-mincho wdiff ruby
-          libcurl4-gnutls-dev ttf-indic-fonts ttf-thai-tlwg
+dev_list="apache2.2-bin bison curl elfutils fakeroot flex g++ gperf
+          language-pack-fr libapache2-mod-php5 libasound2-dev libbz2-dev
+          libcairo2-dev libcups2-dev libcurl4-gnutls-dev libdbus-glib-1-dev
+          libelf-dev libgconf2-dev libgl1-mesa-dev libglib2.0-dev
+          libglu1-mesa-dev libgnome-keyring-dev libgtk2.0-dev libjpeg62-dev
+          libkrb5-dev libnspr4-dev libnss3-dev libpam0g-dev libsctp-dev
+          libsqlite3-dev libssl-dev libwww-perl libxslt1-dev libxss-dev
+          libxt-dev libxtst-dev mesa-common-dev msttcorefonts patch perl
+          php5-cgi pkg-config python python-dev python-psutil rpm ruby
+          subversion ttf-dejavu-core ttf-indic-fonts ttf-kochi-gothic
+          ttf-kochi-mincho ttf-thai-tlwg wdiff
           $chromeos_dev_list"
 
 # Run-time libraries required by chromeos only
