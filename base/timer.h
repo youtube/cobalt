3df4f7249a1229f8744409d885db2440206f8981
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-15 19:35:49 +0000

    Remove Task inheritance from base::Timer.
    BUG=none
    TEST=trybots
    TBR=brettw
    
    Review URL: http://codereview.chromium.org/8898019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114679 0039d316-1c4b-4281-b951-d872f2087c98

--- base/timer.h
+++ base/timer.h
@@ -50,7 +50,6 @@
 #include "base/base_export.h"
 #include "base/location.h"
 #include "base/logging.h"
-#include "base/task.h"
 #include "base/time.h"
 
 class MessageLoop;
@@ -86,7 +85,7 @@ class BASE_EXPORT BaseTimer_Helper {
   BaseTimer_Helper() : delayed_task_(NULL) {}
 
   // We have access to the timer_ member so we can orphan this task.
-  class TimerTask : public Task {
+  class TimerTask {
    public:
     TimerTask(const tracked_objects::Location& posted_from,
               TimeDelta delay)
@@ -95,6 +94,7 @@ class BASE_EXPORT BaseTimer_Helper {
           delay_(delay) {
     }
     virtual ~TimerTask() {}
+    virtual void Run() = 0;
     tracked_objects::Location posted_from_;
     BaseTimer_Helper* timer_;
     TimeDelta delay_;
@@ -170,7 +170,7 @@ class BaseTimer : public BaseTimer_Helper {
         ResetBaseTimer();
       else
         ClearBaseTimer();
-      DispatchToMethod(receiver_, method_, Tuple0());
+      (receiver_->*method_)();
     }
 
     TimerTask* Clone() const {
