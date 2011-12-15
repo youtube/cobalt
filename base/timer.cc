8f509097b5ee2d469de49de142b75ad3fe54671d
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-15 19:35:49 +0000

    Remove Task inheritance from base::Timer.
    BUG=none
    TEST=trybots
    TBR=brettw
    
    Review URL: http://codereview.chromium.org/8898019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114679 0039d316-1c4b-4281-b951-d872f2087c98

--- base/timer.cc
+++ base/timer.cc
@@ -1,9 +1,11 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "base/timer.h"
 
+#include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/message_loop.h"
 
 namespace base {
@@ -21,7 +23,8 @@ void BaseTimer_Helper::InitiateDelayedTask(TimerTask* timer_task) {
   delayed_task_ = timer_task;
   delayed_task_->timer_ = this;
   MessageLoop::current()->PostDelayedTask(
-      timer_task->posted_from_, timer_task,
+      timer_task->posted_from_,
+      base::Bind(&TimerTask::Run, base::Owned(timer_task)),
       static_cast<int>(timer_task->delay_.InMillisecondsRoundedUp()));
 }
 
