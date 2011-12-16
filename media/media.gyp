youtube/cobalt

commit 667914eed9e850287f28364a8bae2d20221358a0

Removing DataSource from Filter hierarchy and refactoring FilterHost into DemuxerHost & DataSourceHost.
Over the last year, several refactorings have caused DataSource to have almost nothing in common with the Filter interface. This change removes it from the Filter class hierarchy and splits up the FilterHost interface so that DataSource, Demuxer, and Filter have their own host interfaces. Splitting FilterHost improves encapsulation and makes it easier to reason about which host methods are required by different parts of the code.

BUG=
TEST=media_unittests


Review URL: http://codereview.chromium.org/8936014

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114819 0039d316-1c4b-4281-b951-d872f2087c98

media/media.gyp
@@ -115,6 +115,8 @@
         'base/composite_filter.h',
         'base/data_buffer.cc',
         'base/data_buffer.h',
+        'base/data_source.cc',
+        'base/data_source.h',
         'base/demuxer.cc',
         'base/demuxer.h',
         'base/demuxer_stream.cc',
@@ -150,6 +152,7 @@
         'base/pipeline_impl.cc',
         'base/pipeline_impl.h',
         'base/pipeline_status.h',
+        'base/preload.h',
         'base/pts_heap.cc',
         'base/pts_heap.h',
         'base/pts_stream.cc',
@@ -678,6 +681,10 @@
         'audio/test_audio_input_controller_factory.h',
         'base/mock_callback.cc',
         'base/mock_callback.h',
+        'base/mock_data_source_host.cc',
+        'base/mock_ddata_source_host.h',
+        'base/mock_demuxer_host.cc',
+        'base/mock_demuxer_host.h',
         'base/mock_filter_host.cc',
         'base/mock_filter_host.h',
         'base/mock_filters.cc',

media/base/data_source.cc
@@ -1,0 +1,30 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/base/data_source.h"
+
+#include "base/logging.h"
+
+namespace media {
+
+// static
+const size_t DataSource::kReadError = static_cast<size_t>(-1);
+
+DataSourceHost::~DataSourceHost() {}
+
+DataSource::DataSource() : host_(NULL) {}
+
+DataSource::~DataSource() {}
+
+void DataSource::set_host(DataSourceHost* host) {
+  DCHECK(host);
+  DCHECK(!host_);
+  host_ = host;
+}
+
+void DataSource::SetPlaybackRate(float playback_rate) {}
+
+DataSourceHost* DataSource::host() { return host_; }
+
+}  // namespace media

media/base/data_source.h
@@ -1,0 +1,86 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_BASE_DATA_SOURCE_H_
+#define MEDIA_BASE_DATA_SOURCE_H_
+
+#include "base/callback.h"
+#include "base/memory/ref_counted.h"
+#include "media/base/media_export.h"
+#include "media/base/preload.h"
+
+namespace media {
+
+class MEDIA_EXPORT DataSourceHost {
+ public:
+  virtual ~DataSourceHost();
+
+  // Set the total size of the media file.
+  virtual void SetTotalBytes(int64 total_bytes) = 0;
+
+  // Sets the total number of bytes that are buffered on the client and ready to
+  // be played.
+  virtual void SetBufferedBytes(int64 buffered_bytes) = 0;
+
+  // Sets the flag to indicate current network activity.
+  virtual void SetNetworkActivity(bool is_downloading_data) = 0;
+};
+
+class MEDIA_EXPORT DataSource : public base::RefCountedThreadSafe<DataSource> {
+ public:
+  typedef base::Callback<void(int64, int64)> StatusCallback;
+  typedef base::Callback<void(size_t)> ReadCallback;
+  static const size_t kReadError;
+
+  DataSource();
+
+  virtual void set_host(DataSourceHost* host);
+
+  // Reads |size| bytes from |position| into |data|. And when the read is done
+  // or failed, |read_callback| is called with the number of bytes read or
+  // kReadError in case of error.
+  // TODO(hclam): should change |size| to int! It makes the code so messy
+  // with size_t and int all over the place..
+  virtual void Read(int64 position, size_t size,
+                    uint8* data,
+                    const DataSource::ReadCallback& read_callback) = 0;
+
+  // Notifies the DataSource of a change in the current playback rate.
+  virtual void SetPlaybackRate(float playback_rate);
+
+  // Stops the DataSource. Once this is called all future Read() calls will
+  // return an error.
+  virtual void Stop(const base::Closure& callback) = 0;
+
+  // Returns true and the file size, false if the file size could not be
+  // retrieved.
+  virtual bool GetSize(int64* size_out) = 0;
+
+  // Returns true if we are performing streaming. In this case seeking is
+  // not possible.
+  virtual bool IsStreaming() = 0;
+
+  // Alert the DataSource that the video preload value has been changed.
+  virtual void SetPreload(Preload preload) = 0;
+
+  // Notify the DataSource of the bitrate of the media.
+  // Values of |bitrate| <= 0 are invalid and should be ignored.
+  virtual void SetBitrate(int bitrate) = 0;
+
+ protected:
+  // Only allow scoped_refptr<> to delete DataSource.
+  friend class base::RefCountedThreadSafe<DataSource>;
+  virtual ~DataSource();
+
+  DataSourceHost* host();
+
+ private:
+  DataSourceHost* host_;
+
+  DISALLOW_COPY_AND_ASSIGN(DataSource);
+};
+
+}  // namespace media
+
+#endif  // MEDIA_BASE_DATA_SOURCE_H_

media/base/demuxer.cc
@@ -8,11 +8,13 @@
 
 namespace media {
 
+DemuxerHost::~DemuxerHost() {}
+
 Demuxer::Demuxer() : host_(NULL) {}
 
 Demuxer::~Demuxer() {}
 
-void Demuxer::set_host(FilterHost* host) {
+void Demuxer::set_host(DemuxerHost* host) {
   DCHECK(host);
   DCHECK(!host_);
   host_ = host;

media/base/demuxer.h
@@ -6,28 +6,45 @@
 #define MEDIA_BASE_DEMUXER_H_
 
 #include "base/memory/ref_counted.h"
+#include "base/time.h"
+#include "media/base/data_source.h"
 #include "media/base/demuxer_stream.h"
 #include "media/base/media_export.h"
-
-// TODO(acolwell): Remove include once DataSource & Preload are moved
-// out of "media/base/filters.h" and Stop() is converted to use new callbacks.
-#include "media/base/filters.h"
 #include "media/base/pipeline_status.h"
+#include "media/base/preload.h"
 
 namespace media {
 
-class FilterHost;
+class MEDIA_EXPORT DemuxerHost : public DataSourceHost {
+ public:
+  virtual ~DemuxerHost();
+
+  // Get the duration of the media in microseconds.  If the duration has not
+  // been determined yet, then returns 0.
+  virtual void SetDuration(base::TimeDelta duration) = 0;
+
+  // Set the approximate amount of playable data buffered so far in micro-
+  // seconds.
+  virtual void SetBufferedTime(base::TimeDelta buffered_time) = 0;
+
+  // Sets the byte offset at which the client is requesting the video.
+  virtual void SetCurrentReadPosition(int64 offset) = 0;
+
+  // Stops execution of the pipeline due to a fatal error.  Do not call this
+  // method with PIPELINE_OK.
+  virtual void OnDemuxerError(PipelineStatus error) = 0;
+};
 
 class MEDIA_EXPORT Demuxer
     : public base::RefCountedThreadSafe<Demuxer> {
  public:
+  Demuxer();
+
   // Sets the private member |host_|. This is the first method called by
-  // the FilterHost after a demuxer is created.  The host holds a strong
+  // the DemuxerHost after a demuxer is created.  The host holds a strong
   // reference to the demuxer.  The reference held by the host is guaranteed
   // to be released before the host object is destroyed by the pipeline.
-  //
-  // TODO(acolwell): Change this to a narrow DemuxerHost interface.
-  virtual void set_host(FilterHost* host);
+  virtual void set_host(DemuxerHost* host);
 
   // The pipeline playback rate has been changed.  Demuxers may implement this
   // method if they need to respond to this call.
@@ -69,14 +86,18 @@ class MEDIA_EXPORT Demuxer
   virtual bool IsSeekable() = 0;
 
  protected:
-  Demuxer();
-  FilterHost* host() { return host_; }
+  // Only allow derived objects access to the DemuxerHost. This is
+  // kept out of the public interface because demuxers need to be
+  // aware of all calls made to the host object so they can insure
+  // the state presented to the host is always consistent with its own
+  // state.
+  DemuxerHost* host() { return host_; }
 
   friend class base::RefCountedThreadSafe<Demuxer>;
   virtual ~Demuxer();
 
  private:
-  FilterHost* host_;
+  DemuxerHost* host_;
 
   DISALLOW_COPY_AND_ASSIGN(Demuxer);
 };

media/base/mock_data_source_host.cc
@@ -1,0 +1,13 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/base/mock_data_source_host.h"
+
+namespace media {
+
+MockDataSourceHost::MockDataSourceHost() {}
+
+MockDataSourceHost::~MockDataSourceHost() {}
+
+}  // namespace media

media/base/mock_data_source_host.h
@@ -1,0 +1,32 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+#ifndef MEDIA_BASE_MOCK_DATA_SOURCE_HOST_H_
+#define MEDIA_BASE_MOCK_DATA_SOURCE_HOST_H_
+
+#include <string>
+
+#include "base/memory/scoped_ptr.h"
+#include "media/base/data_source.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace media {
+
+class MockDataSourceHost : public DataSourceHost {
+ public:
+  MockDataSourceHost();
+  virtual ~MockDataSourceHost();
+
+  // DataSourceHost implementation.
+  MOCK_METHOD1(SetTotalBytes, void(int64 total_bytes));
+  MOCK_METHOD1(SetBufferedBytes, void(int64 buffered_bytes));
+  MOCK_METHOD1(SetNetworkActivity, void(bool network_activity));
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(MockDataSourceHost);
+};
+
+}  // namespace media
+
+#endif  // MEDIA_BASE_MOCK_DATA_SOURCE_HOST_H_

media/base/mock_demuxer_host.cc
@@ -1,0 +1,13 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/base/mock_demuxer_host.h"
+
+namespace media {
+
+MockDemuxerHost::MockDemuxerHost() {}
+
+MockDemuxerHost::~MockDemuxerHost() {}
+
+}  // namespace media

commit 0787ec606485603bc79227d6095ab3afeafc3add

Fix typo introduced in r114819
TBR=jennb@chromium.org

Review URL: http://codereview.chromium.org/8970030

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114828 0039d316-1c4b-4281-b951-d872f2087c98

media/media.gyp
@@ -682,7 +682,7 @@
         'base/mock_callback.cc',
         'base/mock_callback.h',
         'base/mock_data_source_host.cc',
-        'base/mock_ddata_source_host.h',
+        'base/mock_data_source_host.h',
         'base/mock_demuxer_host.cc',
         'base/mock_demuxer_host.h',
         'base/mock_filter_host.cc',