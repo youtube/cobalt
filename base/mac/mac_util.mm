ca29230120cc384e173991545d4173543437a481
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-17 21:59:52 +0000

    Revert 114929 - Standardize StringToInt{,64} interface.
    These changes address issue #106655.  All variants of StringToInt have been
    converted to use the StringPiece class.  One instance of conversion, in
    chrome/browser/history/text_database.cc, required copying an underlying
    string.  This is because the string type in question could use 8 or 16
    bit characters depending on the OS type, and because StringPiece is not
    implemented as a template, the code cannot specify whether to create a
    StringPiece or StringPiece16.  This should be remedied in a future CL.
    
    R=erikwright@chromium.org
    BUG=106655
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8921006
    
    TBR=tedvessenes@gmail.com
    Review URL: http://codereview.chromium.org/8984007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114930 0039d316-1c4b-4281-b951-d872f2087c98

--- base/mac/mac_util.mm
+++ base/mac/mac_util.mm
@@ -14,7 +14,6 @@
 #include "base/mac/scoped_cftyperef.h"
 #include "base/memory/scoped_nsobject.h"
 #include "base/string_number_conversions.h"
-#include "base/string_piece.h"
 #include "base/sys_string_conversions.h"
 
 namespace base {
@@ -522,9 +521,7 @@ int DarwinMajorVersionInternal() {
   int darwin_major_version = 0;
   char* dot = strchr(uname_info.release, '.');
   if (dot) {
-    if (!base::StringToInt(base::StringPiece(uname_info.release,
-                                             dot - uname_info.release),
-                           &darwin_major_version)) {
+    if (!base::StringToInt(uname_info.release, dot, &darwin_major_version)) {
       dot = NULL;
     }
   }
