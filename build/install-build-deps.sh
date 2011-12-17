7c0907e89d6b04862e4058f4dd994d17e1d5757e
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-17 03:36:32 +0000

    Add libudev to install-build-deps.sh
    BUG=79050
    
    
    Review URL: http://codereview.chromium.org/8981007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114905 0039d316-1c4b-4281-b951-d872f2087c98

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -108,11 +108,11 @@ dev_list="apache2.2-bin bison curl elfutils fakeroot flex g++ gperf
           libelf-dev libgconf2-dev libgl1-mesa-dev libglib2.0-dev
           libglu1-mesa-dev libgnome-keyring-dev libgtk2.0-dev libjpeg62-dev
           libkrb5-dev libnspr4-dev libnss3-dev libpam0g-dev libsctp-dev
-          libsqlite3-dev libssl-dev libwww-perl libxslt1-dev libxss-dev
-          libxt-dev libxtst-dev mesa-common-dev msttcorefonts patch perl
-          php5-cgi pkg-config python python-cherrypy3 python-dev python-psutil
-          rpm ruby subversion ttf-dejavu-core ttf-indic-fonts ttf-kochi-gothic
-          ttf-kochi-mincho ttf-thai-tlwg wdiff
+          libsqlite3-dev libssl-dev libudev-dev libwww-perl libxslt1-dev
+          libxss-dev libxt-dev libxtst-dev mesa-common-dev msttcorefonts patch
+          perl php5-cgi pkg-config python python-cherrypy3 python-dev
+          python-psutil rpm ruby subversion ttf-dejavu-core ttf-indic-fonts
+          ttf-kochi-gothic ttf-kochi-mincho ttf-thai-tlwg wdiff
           $chromeos_dev_list"
 
 # Run-time libraries required by chromeos only
@@ -122,7 +122,7 @@ chromeos_lib_list="libpulse0 libbz2-1.0 libcurl4-gnutls-dev"
 lib_list="libatk1.0-0 libc6 libasound2 libcairo2 libcups2 libdbus-glib-1-2
           libexpat1 libfontconfig1 libfreetype6 libglib2.0-0 libgnome-keyring0
           libgtk2.0-0 libpam0g libpango1.0-0 libpcre3 libpixman-1-0
-          libpng12-0 libstdc++6 libsqlite3-0 libx11-6 libxau6 libxcb1
+          libpng12-0 libstdc++6 libsqlite3-0 libudev0 libx11-6 libxau6 libxcb1
           libxcomposite1 libxcursor1 libxdamage1 libxdmcp6 libxext6 libxfixes3
           libxi6 libxinerama1 libxrandr2 libxrender1 libxtst6 zlib1g
           $chromeos_lib_list"
