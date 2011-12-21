66b6ba136be98442d8fb00bdbb09cfe363ccf4b7
dpapad@chromium.org <dpapad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-21 02:00:48 +0000

    Coverity: Fixing pass by value. CID=101462, 101458, 101437, 101471, 101467
    BUG=NONE
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/9006023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115257 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/net_util.cc
+++ net/base/net_util.cc
@@ -160,7 +160,7 @@ static const int kAllowedFtpPorts[] = {
 };
 
 std::string::size_type CountTrailingChars(
-    const std::string input,
+    const std::string& input,
     const std::string::value_type trailing_chars[]) {
   const size_t last_good_char = input.find_last_not_of(trailing_chars);
   return (last_good_char == std::string::npos) ?
