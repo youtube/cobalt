6150502ac832d32a998314106fd9b88b2822f8ff
scr@chromium.org <scr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-22 22:31:44 +0000

    Coverity fix NEGATIVE_RETURNS.
    CID=102629
    R=mcgrathr@chromium.org, jar@chromium.org
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9007018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115630 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util_posix.cc
+++ base/file_util_posix.cc
@@ -927,7 +927,10 @@ bool DetermineDevShmExecutable() {
   if (fd >= 0) {
     ScopedFD shm_fd_closer(&fd);
     Delete(path, false);
-    size_t pagesize = sysconf(_SC_PAGESIZE);
+    long sysconf_result = sysconf(_SC_PAGESIZE);
+    CHECK_GE(sysconf_result, 0);
+    size_t pagesize = static_cast<size_t>(sysconf_result);
+    CHECK_GE(sizeof(pagesize), sizeof(sysconf_result));
     void *mapping = mmap(NULL, pagesize, PROT_READ, MAP_SHARED, fd, 0);
     if (mapping != MAP_FAILED) {
       if (mprotect(mapping, pagesize, PROT_READ | PROT_EXEC) == 0)
