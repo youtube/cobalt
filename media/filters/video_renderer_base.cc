61c5221a796513f602657c809986718471688847
jeremya@chromium.org <jeremya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-22 01:02:14 +0000

    Revert 115402 - Clean up VideoRendererBase timing code.
    Tiny step towards simplifying VideoRendererBase code and respective subclasses.
    
    The biggest change was to replace if statements adjusting the value of |remaining_time| with explicit statements to sleep then retry the loop from the start. The nice part about doing this is that we guarantee we exercise all conditions in the scenario where a spurious signal to |frame_available_| causes the thread to wake up.
    
    BUG=28208
    
    Review URL: http://codereview.chromium.org/8794001
    
    TBR=scherkus@chromium.org
    Review URL: http://codereview.chromium.org/9020037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115457 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/video_renderer_base.cc
+++ media/filters/video_renderer_base.cc
@@ -13,6 +13,19 @@
 
 namespace media {
 
+// This equates to ~16.67 fps, which is just slow enough to be tolerable when
+// our video renderer is ahead of the audio playback.
+//
+// A higher value will be a slower frame rate, which looks worse but allows the
+// audio renderer to catch up faster.  A lower value will be a smoother frame
+// rate, but results in the video being out of sync for longer.
+static const int64 kMaxSleepMilliseconds = 60;
+
+// The number of milliseconds to idle when we do not have anything to do.
+// Nothing special about the value, other than we're being more OS-friendly
+// than sleeping for 1 millisecond.
+static const int kIdleMilliseconds = 10;
+
 VideoRendererBase::VideoRendererBase()
     : frame_available_(&lock_),
       state_(kUninitialized),
@@ -151,15 +164,7 @@ bool VideoRendererBase::HasEnded() {
 // PlatformThread::Delegate implementation.
 void VideoRendererBase::ThreadMain() {
   base::PlatformThread::SetName("CrVideoRenderer");
-
-  // The number of milliseconds to idle when we do not have anything to do.
-  // Nothing special about the value, other than we're being more OS-friendly
-  // than sleeping for 1 millisecond.
-  //
-  // TOOD(scherkus): switch to pure event-driven frame timing instead of this
-  // kIdleTimeDelta business http://crbug.com/106874
-  const base::TimeDelta kIdleTimeDelta =
-      base::TimeDelta::FromMilliseconds(10);
+  base::TimeDelta remaining_time;
 
   uint32 frames_dropped = 0;
 
@@ -174,83 +179,91 @@ void VideoRendererBase::ThreadMain() {
 
     base::AutoLock auto_lock(lock_);
 
-    // Thread exit condition.
+    const base::TimeDelta kIdleTimeDelta =
+        base::TimeDelta::FromMilliseconds(kIdleMilliseconds);
+
     if (state_ == kStopped)
       return;
 
-    // Remain idle as long as we're not playing.
     if (state_ != kPlaying || playback_rate_ == 0) {
-      frame_available_.TimedWait(kIdleTimeDelta);
-      continue;
+      remaining_time = kIdleTimeDelta;
+    } else if (frames_queue_ready_.empty() ||
+               frames_queue_ready_.front()->IsEndOfStream()) {
+      if (current_frame_)
+        remaining_time = CalculateSleepDuration(NULL, playback_rate_);
+      else
+        remaining_time = kIdleTimeDelta;
+    } else {
+      // Calculate how long until we should advance the frame, which is
+      // typically negative but for playback rates < 1.0f may be long enough
+      // that it makes more sense to idle and check again.
+      scoped_refptr<VideoFrame> next_frame = frames_queue_ready_.front();
+      remaining_time = CalculateSleepDuration(next_frame, playback_rate_);
     }
 
-    // Remain idle until we have the next frame ready for rendering.
-    if (ready_frames_.empty()) {
-      frame_available_.TimedWait(kIdleTimeDelta);
-      continue;
-    }
+    // TODO(jiesun): I do not think we should wake up every 10ms.
+    // We should only wait up when following is true:
+    // 1. frame arrival (use event);
+    // 2. state_ change (use event);
+    // 3. playback_rate_ change (use event);
+    // 4. next frame's pts (use timeout);
+    if (remaining_time > kIdleTimeDelta)
+      remaining_time = kIdleTimeDelta;
 
-    // Remain idle until we've initialized |current_frame_| via prerolling.
-    if (!current_frame_) {
-      // This can happen if our preroll only contains end of stream frames.
-      if (ready_frames_.front()->IsEndOfStream()) {
-        state_ = kEnded;
-        host()->NotifyEnded();
+    // We can not do anything about this until next frame arrival.
+    // We do not want to spin in this case though.
+    if (remaining_time.InMicroseconds() < 0 && frames_queue_ready_.empty())
+      remaining_time = kIdleTimeDelta;
 
-        // No need to sleep here as we idle when |state_ != kPlaying|.
-        continue;
-      }
-
-      frame_available_.TimedWait(kIdleTimeDelta);
-      continue;
-    }
-
-    // Calculate how long until we should advance the frame, which is
-    // typically negative but for playback rates < 1.0f may be long enough
-    // that it makes more sense to idle and check again.
-    base::TimeDelta remaining_time =
-        CalculateSleepDuration(ready_frames_.front(), playback_rate_);
-
-    // Sleep up to a maximum of our idle time until we're within the time to
-    // render the next frame.
-    if (remaining_time.InMicroseconds() > 0) {
-      remaining_time = std::min(remaining_time, kIdleTimeDelta);
+    if (remaining_time.InMicroseconds() > 0)
       frame_available_.TimedWait(remaining_time);
+
+    if (state_ != kPlaying || playback_rate_ == 0)
       continue;
-    }
 
+    // Otherwise we're playing, so advance the frame and keep reading from the
+    // decoder when following condition is satisfied:
+    // 1. We had at least one backup frame.
+    // 2. We had not reached end of stream.
+    // 3. Current frame is out-dated.
+    if (frames_queue_ready_.empty())
+      continue;
 
-    // We're almost there!
-    //
-    // At this point we've rendered |current_frame_| for the proper amount
-    // of time and also have the next frame that ready for rendering.
-
-
-    // If the next frame is end of stream then we are truly at the end of the
-    // video stream.
-    //
-    // TODO(scherkus): deduplicate this end of stream check after we get rid of
-    // |current_frame_|.
-    if (ready_frames_.front()->IsEndOfStream()) {
+    scoped_refptr<VideoFrame> next_frame = frames_queue_ready_.front();
+    if (next_frame->IsEndOfStream()) {
       state_ = kEnded;
+      DVLOG(1) << "Video render gets EOS";
       host()->NotifyEnded();
-
-      // No need to sleep here as we idle when |state_ != kPlaying|.
       continue;
     }
 
-    // We cannot update |current_frame_| until we've completed the pending
-    // paint. Furthermore, the pending paint might be really slow: check to
-    // see if we have any ready frames that we can drop if they've already
-    // expired.
+    // Normally we're ready to loop again at this point, but there are
+    // exceptions that cause us to drop a frame and/or consider painting a
+    // "next" frame.
+    if (next_frame->GetTimestamp() > host()->GetTime() + kIdleTimeDelta &&
+        current_frame_ &&
+        current_frame_->GetTimestamp() <= host()->GetDuration()) {
+      continue;
+    }
+
+    // If we got here then:
+    // 1. next frame's timestamp is already current; or
+    // 2. we do not have a current frame yet; or
+    // 3. a special case when the stream is badly formatted and
+    //    we got a frame with timestamp greater than overall duration.
+    //    In this case we should proceed anyway and try to obtain the
+    //    end-of-stream packet.
+
     if (pending_paint_) {
-      while (!ready_frames_.empty()) {
+      // The pending paint might be really slow. Check if we have any frames
+      // available that we can drop if they've already expired.
+      while (!frames_queue_ready_.empty()) {
         // Can't drop anything if we're at the end.
-        if (ready_frames_.front()->IsEndOfStream())
+        if (frames_queue_ready_.front()->IsEndOfStream())
           break;
 
         base::TimeDelta remaining_time =
-            ready_frames_.front()->GetTimestamp() - host()->GetTime();
+            frames_queue_ready_.front()->GetTimestamp() - host()->GetTime();
 
         // Still a chance we can render the frame!
         if (remaining_time.InMicroseconds() > 0)
@@ -258,23 +271,21 @@ void VideoRendererBase::ThreadMain() {
 
         // Frame dropped: read again.
         ++frames_dropped;
-        ready_frames_.pop_front();
+        frames_queue_ready_.pop_front();
         AttemptRead_Locked();
       }
       // Continue waiting for the current paint to finish.
-      frame_available_.TimedWait(kIdleTimeDelta);
       continue;
     }
-
 
     // Congratulations! You've made it past the video frame timing gauntlet.
     //
     // We can now safely update the current frame, request another frame, and
     // signal to the client that a new frame is available.
     DCHECK(!pending_paint_);
-    DCHECK(!ready_frames_.empty());
-    current_frame_ = ready_frames_.front();
-    ready_frames_.pop_front();
+    DCHECK(!frames_queue_ready_.empty());
+    current_frame_ = frames_queue_ready_.front();
+    frames_queue_ready_.pop_front();
     AttemptRead_Locked();
 
     base::AutoUnlock auto_unlock(lock_);
@@ -360,18 +371,9 @@ void VideoRendererBase::FrameReady(scoped_refptr<VideoFrame> frame) {
     return;
   }
 
-  // Adjust the incoming frame if it's rendering stop time is past the duration
-  // of the video itself. This is typically the last frame of the video and
-  // occurs if the container specifies a duration that isn't a multiple of the
-  // frame rate.
-  if (!frame->IsEndOfStream() &&
-      (frame->GetTimestamp() + frame->GetDuration()) > host()->GetDuration()) {
-    frame->SetDuration(host()->GetDuration() - frame->GetTimestamp());
-  }
-
   // This one's a keeper! Place it in the ready queue.
-  ready_frames_.push_back(frame);
-  DCHECK_LE(ready_frames_.size(),
+  frames_queue_ready_.push_back(frame);
+  DCHECK_LE(frames_queue_ready_.size(),
             static_cast<size_t>(limits::kMaxVideoFrames));
   frame_available_.Signal();
 
@@ -383,7 +385,7 @@ void VideoRendererBase::FrameReady(scoped_refptr<VideoFrame> frame) {
   // purposes:
   //   1) Prerolling while paused
   //   2) Keeps decoding going if video rendering thread starts falling behind
-  if (ready_frames_.size() < limits::kMaxVideoFrames &&
+  if (frames_queue_ready_.size() < limits::kMaxVideoFrames &&
       !frame->IsEndOfStream()) {
     AttemptRead_Locked();
     return;
@@ -392,15 +394,14 @@ void VideoRendererBase::FrameReady(scoped_refptr<VideoFrame> frame) {
   // If we're at capacity or end of stream while seeking we need to transition
   // to prerolled.
   if (state_ == kSeeking) {
-    DCHECK(!current_frame_);
     state_ = kPrerolled;
 
     // Because we might remain in the prerolled state for an undetermined amount
     // of time (i.e., we were not playing before we received a seek), we'll
     // manually update the current frame and notify the subclass below.
-    if (!ready_frames_.front()->IsEndOfStream()) {
-      current_frame_ = ready_frames_.front();
-      ready_frames_.pop_front();
+    if (!frames_queue_ready_.front()->IsEndOfStream()) {
+      current_frame_ = frames_queue_ready_.front();
+      frames_queue_ready_.pop_front();
     }
 
     // ...and we're done seeking!
@@ -416,8 +417,9 @@ void VideoRendererBase::AttemptRead_Locked() {
   lock_.AssertAcquired();
   DCHECK_NE(kEnded, state_);
 
-  if (pending_read_ || ready_frames_.size() == limits::kMaxVideoFrames)
+  if (pending_read_ || frames_queue_ready_.size() == limits::kMaxVideoFrames) {
     return;
+  }
 
   pending_read_ = true;
   decoder_->Read(read_cb_);
@@ -428,8 +430,8 @@ void VideoRendererBase::AttemptFlush_Locked() {
   DCHECK_EQ(kFlushing, state_);
 
   // Get rid of any ready frames.
-  while (!ready_frames_.empty()) {
-    ready_frames_.pop_front();
+  while (!frames_queue_ready_.empty()) {
+    frames_queue_ready_.pop_front();
   }
 
   if (!pending_paint_ && !pending_read_) {
@@ -440,20 +442,30 @@ void VideoRendererBase::AttemptFlush_Locked() {
 }
 
 base::TimeDelta VideoRendererBase::CalculateSleepDuration(
-    const scoped_refptr<VideoFrame>& next_frame,
-    float playback_rate) {
+    VideoFrame* next_frame, float playback_rate) {
   // Determine the current and next presentation timestamps.
   base::TimeDelta now = host()->GetTime();
   base::TimeDelta this_pts = current_frame_->GetTimestamp();
   base::TimeDelta next_pts;
-  if (!next_frame->IsEndOfStream()) {
+  if (next_frame) {
     next_pts = next_frame->GetTimestamp();
   } else {
     next_pts = this_pts + current_frame_->GetDuration();
   }
 
+  // Determine our sleep duration based on whether time advanced.
+  base::TimeDelta sleep;
+  if (now == previous_time_) {
+    // Time has not changed, assume we sleep for the frame's duration.
+    sleep = next_pts - this_pts;
+  } else {
+    // Time has changed, figure out real sleep duration.
+    sleep = next_pts - now;
+    previous_time_ = now;
+  }
+
   // Scale our sleep based on the playback rate.
-  base::TimeDelta sleep = next_pts - now;
+  // TODO(scherkus): floating point badness and degrade gracefully.
   return base::TimeDelta::FromMicroseconds(
       static_cast<int64>(sleep.InMicroseconds() / playback_rate));
 }
