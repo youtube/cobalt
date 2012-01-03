378f81f392c2a2c8e6e9eddb8cc8bdef6ad08b3d
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-03 19:36:57 +0000

    base::Bind: Remove callback_old.h.
    BUG=none
    TEST=none
    R=groby,awong,csilv
    
    Review URL: http://codereview.chromium.org/9028009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116169 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/pipeline_status.h
+++ media/base/pipeline_status.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,7 +6,6 @@
 #define MEDIA_BASE_PIPELINE_STATUS_H_
 
 #include "base/callback.h"
-#include "base/callback_old.h"
 
 namespace media {
 
