9ae57684afba7be0815e1a7cecd9fe889f406f02
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-03 18:52:10 +0000

    base::Bind: Remove OldCompletionCallback.
    BUG=none
    TEST=none
    R=groby,awong,csilv
    
    Review URL: http://codereview.chromium.org/9064007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116158 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/completion_callback.h
+++ net/base/completion_callback.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,67 +6,16 @@
 #define NET_BASE_COMPLETION_CALLBACK_H__
 #pragma once
 
-#include "base/callback_old.h"
 #include "base/callback.h"
 #include "base/cancelable_callback.h"
-#include "net/base/net_export.h"
 
 namespace net {
 
-// A callback specialization that takes a single int parameter.  Usually this
-// is used to report a byte count or network error code.
-typedef Callback1<int>::Type OldCompletionCallback;
+// A callback specialization that takes a single int parameter. Usually this is
+// used to report a byte count or network error code.
 typedef base::Callback<void(int)> CompletionCallback;
 
-// Temporary (in the code) adapter of OldCompletionCallback for use at call
-// sites that expect a CompletionCallback.
-//
-// TODO(jhawkins): Remove once OldCompletionCallback is removed.
-void NET_EXPORT OldCompletionCallbackAdapter(OldCompletionCallback* callback,
-                                             int result);
-
-// Used to implement a OldCompletionCallback.
-template <class T>
-class OldCompletionCallbackImpl :
-    public CallbackImpl< T, void (T::*)(int), Tuple1<int> > {
- public:
-  OldCompletionCallbackImpl(T* obj, void (T::* meth)(int))
-    : CallbackImpl< T, void (T::*)(int),
-                    Tuple1<int> >::CallbackImpl(obj, meth) {
-  }
-};
-
 typedef base::CancelableCallback<void(int)> CancelableCompletionCallback;
-
-// CancelableOldCompletionCallback is used for completion callbacks
-// which may outlive the target for the method dispatch. In such a case, the
-// provider of the callback calls Cancel() to mark the callback as
-// "canceled". When the canceled callback is eventually run it does nothing
-// other than to decrement the refcount to 0 and free the memory.
-template <class T>
-class CancelableOldCompletionCallback :
-    public OldCompletionCallbackImpl<T>,
-    public base::RefCounted<CancelableOldCompletionCallback<T> > {
- public:
-  CancelableOldCompletionCallback(T* obj, void (T::* meth)(int))
-    : OldCompletionCallbackImpl<T>(obj, meth), is_canceled_(false) {
-  }
-
-  void Cancel() {
-    is_canceled_ = true;
-  }
-
-  virtual void RunWithParams(const Tuple1<int>& params) {
-    if (is_canceled_) {
-      base::RefCounted<CancelableOldCompletionCallback<T> >::Release();
-    } else {
-      OldCompletionCallbackImpl<T>::RunWithParams(params);
-    }
-  }
-
- private:
-  bool is_canceled_;
-};
 
 }  // namespace net
 
