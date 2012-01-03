d0773abc9caa7ef2de1285c8a5782407d7b2a019
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-03 19:36:57 +0000

    base::Bind: Remove callback_old.h.
    BUG=none
    TEST=none
    R=groby,awong,csilv
    
    Review URL: http://codereview.chromium.org/9028009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116169 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/video_decode_accelerator.h
+++ media/video/video_decode_accelerator.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,7 +8,6 @@
 #include <vector>
 
 #include "base/basictypes.h"
-#include "base/callback_old.h"
 #include "media/base/bitstream_buffer.h"
 #include "media/base/video_decoder_config.h"
 #include "media/video/picture.h"
--- net/disk_cache/disk_cache_test_util.h
+++ net/disk_cache/disk_cache_test_util.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,7 +8,6 @@
 
 #include <string>
 
-#include "base/callback_old.h"
 #include "base/file_path.h"
 #include "base/message_loop.h"
 #include "base/timer.h"
