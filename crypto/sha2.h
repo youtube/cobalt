2a8e63c690d91a6009533bb5300cd7a129158ee7
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-10 19:10:33 +0000

    net: allow CRLSets to block specific SPKIs.
    This change allows CRLSets to include a list of blocked SPKI fingerprints,
    which may save us doing emergency binary pushes in the future.
    
    It also corrects a bug where the NSS code was passing in the full SPKI rather
    than the SHA256 hash.
    
    BUG=none
    TEST=net_unittests
    
    
    Review URL: http://codereview.chromium.org/9149010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117069 0039d316-1c4b-4281-b951-d872f2087c98

--- crypto/sha2.h
+++ crypto/sha2.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,6 +8,7 @@
 
 #include <string>
 
+#include "base/string_piece.h"
 #include "crypto/crypto_export.h"
 
 namespace crypto {
@@ -21,12 +22,12 @@ static const size_t kSHA256Length = 32;  // Length in bytes of a SHA-256 hash.
 // Computes the SHA-256 hash of the input string 'str' and stores the first
 // 'len' bytes of the hash in the output buffer 'output'.  If 'len' > 32,
 // only 32 bytes (the full hash) are stored in the 'output' buffer.
-CRYPTO_EXPORT void SHA256HashString(const std::string& str,
+CRYPTO_EXPORT void SHA256HashString(const base::StringPiece& str,
                                     void* output, size_t len);
 
 // Convenience version of the above that returns the result in a 32-byte
 // string.
-CRYPTO_EXPORT std::string SHA256HashString(const std::string& str);
+CRYPTO_EXPORT std::string SHA256HashString(const base::StringPiece& str);
 
 }  // namespace crypto
 
