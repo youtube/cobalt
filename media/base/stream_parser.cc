6418acee2133ba932f2a683aa74996ca586baf28
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-10 23:26:07 +0000

    Move WebM specific code from ChunkDemuxer to WebMStreamParser.
    BUG=108329 
    TEST=Existing ChunkDemuxer unittests
    TBR=acolwell@chromium.org
    
    
    Review URL: http://codereview.chromium.org/9170002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117113 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ media/base/stream_parser.cc
@@ -0,0 +1,17 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/base/stream_parser.h"
+
+namespace media {
+
+StreamParserHost::StreamParserHost() {}
+
+StreamParserHost::~StreamParserHost() {}
+
+StreamParser::StreamParser() {}
+
+StreamParser::~StreamParser() {}
+
+}  // namespace media
