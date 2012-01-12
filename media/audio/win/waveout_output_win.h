3ce27dfb2c6723b9a1c6e19b4b92a2f91f024b84
enal@chromium.org <enal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-12 20:58:28 +0000

    Revert 117268 - Add CHECKs to find root cause of Windows-only crash. Checks are temporary and would be in canary only. For gory details please see original bug 108685.
    BUG=109757
    
    Review URL: http://codereview.chromium.org/9147039
    
    TBR=enal@chromium.org
    Review URL: http://codereview.chromium.org/9139072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117497 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/win/waveout_output_win.h
+++ media/audio/win/waveout_output_win.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -128,9 +128,6 @@ class PCMWaveOutAudioOutputStream : public AudioOutputStream {
 
   // Lock used to avoid the conflict when callbacks are called simultaneously.
   base::Lock lock_;
-
-  // Temporary here, for CHECK().
-  DWORD thread_id_;
 
   DISALLOW_COPY_AND_ASSIGN(PCMWaveOutAudioOutputStream);
 };
