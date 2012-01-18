dac24659fc13adf2be0f74c097b178adac24553a
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-18 20:46:11 +0000

    SPDY - FlipServer - changes to use BufferedSpdyFramer.
    TEST=network unittests
    R=wtc
    
    Review URL: http://codereview.chromium.org/9174026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118139 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/buffered_spdy_framer.h
+++ net/spdy/buffered_spdy_framer.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -62,6 +62,9 @@ class NET_EXPORT_PRIVATE BufferedSpdyFramer {
   void Reset();
   SpdyFramer::SpdyError error_code() const;
   SpdyFramer::SpdyState state() const;
+  bool MessageFullyRead();
+  bool HasError();
+  bool ParseHeaderBlock(const SpdyFrame* frame, SpdyHeaderBlock* block);
   SpdySynStreamControlFrame* CreateSynStream(SpdyStreamId stream_id,
                                              SpdyStreamId associated_stream_id,
                                              int priority,
