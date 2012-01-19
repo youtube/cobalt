556faff865c4ab74d874ba138454b0ed4b34dd6c
simonjam@chromium.org <simonjam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 20:34:54 +0000

    Disable pipelining if we encounter HTTP Authentication.
    BUG=102970
    TEST=net_unittests
    
    
    Review URL: http://codereview.chromium.org/9241028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118346 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_pipelined_connection.h
+++ net/http/http_pipelined_connection.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -26,6 +26,7 @@ class NET_EXPORT_PRIVATE HttpPipelinedConnection {
     PIPELINE_SOCKET_ERROR,
     OLD_HTTP_VERSION,
     MUST_CLOSE_CONNECTION,
+    AUTHENTICATION_REQUIRED,
   };
 
   class Delegate {
