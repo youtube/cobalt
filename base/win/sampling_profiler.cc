3949ce21a184fdf5f6c7d614b05154a030c20c2e
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-23 20:29:07 +0000

    windows: Fix a few things clang complains about.
    No functionality change.
    
    BUG=82385
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9200009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118725 0039d316-1c4b-4281-b951-d872f2087c98

--- base/win/sampling_profiler.cc
+++ base/win/sampling_profiler.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -104,7 +104,7 @@ ProfilerFuncs::ProfilerFuncs()
   }
 }
 
-base::LazyInstance<ProfilerFuncs, base::LeakyLazyInstanceTraits<ProfilerFuncs>>
+base::LazyInstance<ProfilerFuncs, base::LeakyLazyInstanceTraits<ProfilerFuncs> >
     funcs = LAZY_INSTANCE_INITIALIZER;
 
 }  // namespace
