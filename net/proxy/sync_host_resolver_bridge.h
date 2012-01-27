807f8dc47b3fc2ad00768e039a4956ce14235cba
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-27 23:10:08 +0000

    Add a source to V8 proxy resolver-initiated DNS requests, when we have a source to add.
    R=eroman@chromium.org
    BUG=111641
    
    Review URL: http://codereview.chromium.org/9121055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119517 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/sync_host_resolver_bridge.h
+++ net/proxy/sync_host_resolver_bridge.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -24,7 +24,8 @@ class NET_EXPORT_PRIVATE SyncHostResolverBridge : public SyncHostResolver {
 
   // SyncHostResolver methods:
   virtual int Resolve(const HostResolver::RequestInfo& info,
-                      AddressList* addresses) OVERRIDE;
+                      AddressList* addresses,
+                      const BoundNetLog& net_log) OVERRIDE;
 
   // The Shutdown() method should be called prior to destruction, from
   // |host_resolver_loop_|. It aborts any in progress synchronous resolves, to
