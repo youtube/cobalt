82539555b11b6a8af1f31f3a8aff729f44d3622f
tedvessenes@gmail.com <tedvessenes@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-28 00:57:00 +0000

    Convert use of int ms to TimeDelta in files owned by jar.
    R=jar@chromium.org
    BUG=108171
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9190027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119547 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/eviction.cc
+++ net/disk_cache/eviction.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -204,7 +204,8 @@ void Eviction::PostDelayedTrim() {
   delay_trim_ = true;
   trim_delays_++;
   MessageLoop::current()->PostDelayedTask(FROM_HERE,
-      base::Bind(&Eviction::DelayedTrim, ptr_factory_.GetWeakPtr()), 1000);
+      base::Bind(&Eviction::DelayedTrim, ptr_factory_.GetWeakPtr()),
+      base::TimeDelta::FromMilliseconds(1000));
 }
 
 void Eviction::DelayedTrim() {
