b0c081935494a1de761efd0b4aa782d0c48e7035
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 17:27:49 +0000

    Initialize logging early enough to see VLOGs emitted by InitializeMediaLibraryForTesting(). (specifically, this makes ffmpeg_stubs.cc VLOGs actually get emitted if -v=1 is specified)
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9320048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120345 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/run_all_unittests.cc
+++ media/base/run_all_unittests.cc
@@ -1,14 +1,34 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/at_exit.h"
+#include "base/command_line.h"
+#include "base/logging.h"
 #include "base/test/test_suite.h"
 #include "media/base/media.h"
 
+class TestSuiteNoAtExit : public base::TestSuite {
+ public:
+  TestSuiteNoAtExit(int argc, char** argv) : TestSuite(argc, argv, false) {}
+  virtual ~TestSuiteNoAtExit() {}
+};
+
 int main(int argc, char** argv) {
-  base::TestSuite suite(argc, argv);
+  // By default command-line parsing happens only in TestSuite::Run(), but
+  // that's too late to get VLOGs and so on from
+  // InitializeMediaLibraryForTesting() below.  Instead initialize logging
+  // explicitly here (and have it get re-initialized by TestSuite::Run()).
+  CommandLine::Init(argc, argv);
+  CHECK(logging::InitLogging(
+      NULL,
+      logging::LOG_ONLY_TO_SYSTEM_DEBUG_LOG,
+      logging::DONT_LOCK_LOG_FILE,
+      logging::APPEND_TO_OLD_LOG_FILE,
+      logging::ENABLE_DCHECK_FOR_NON_OFFICIAL_RELEASE_BUILDS));
+  base::AtExitManager exit_manager;
 
   media::InitializeMediaLibraryForTesting();
 
-  return suite.Run();
+  return TestSuiteNoAtExit(argc, argv).Run();
 }
