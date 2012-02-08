cbef19cca7be4455764e8ae2258ce74184653536
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-08 00:14:54 +0000

    Use a forward declaration of RSAPrivateKey in crypto/signature_creator.h.
    R=rch@chromium.org
    BUG=none
    TEST=no compilation errors
    
    Review URL: http://codereview.chromium.org/9348001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120872 0039d316-1c4b-4281-b951-d872f2087c98

--- crypto/signature_creator_win.cc
+++ crypto/signature_creator_win.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,6 +6,7 @@
 
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
+#include "crypto/rsa_private_key.h"
 
 namespace crypto {
 
