c97bf9f7d687e1854818013241e282d455eed6a4
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-09 02:04:05 +0000

    Add EnsureNSSInit in SymmetricKey::Import.
    BUG=none
    TEST=crypto unit tests
    
    
    Review URL: http://codereview.chromium.org/9359027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121152 0039d316-1c4b-4281-b951-d872f2087c98

--- crypto/symmetric_key_nss.cc
+++ crypto/symmetric_key_nss.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -83,6 +83,7 @@ SymmetricKey* SymmetricKey::DeriveKeyFromPassword(Algorithm algorithm,
 // static
 SymmetricKey* SymmetricKey::Import(Algorithm algorithm,
                                    const std::string& raw_key) {
+  EnsureNSSInit();
   CK_MECHANISM_TYPE cipher =
       algorithm == AES ? CKM_AES_CBC : CKM_SHA_1_HMAC;
 
