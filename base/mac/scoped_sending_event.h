239af9fefdf3008ccc1e8ae0a9cc73ff93858cad
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-09 18:51:35 +0000

    mac: Add _EXPORTS macros, base/.
    BUG=90078
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9373015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121256 0039d316-1c4b-4281-b951-d872f2087c98

--- base/mac/scoped_sending_event.h
+++ base/mac/scoped_sending_event.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,6 +6,7 @@
 #define BASE_MAC_SCOPED_SENDING_EVENT_H_
 #pragma once
 
+#include "base/base_export.h"
 #include "base/basictypes.h"
 #include "base/memory/scoped_nsobject.h"
 #include "base/message_pump_mac.h"
@@ -29,7 +30,7 @@
 namespace base {
 namespace mac {
 
-class ScopedSendingEvent {
+class BASE_EXPORT ScopedSendingEvent {
  public:
   ScopedSendingEvent();
   ~ScopedSendingEvent();
