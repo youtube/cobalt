2ab7e9e513678ed11ca5301ae4cd41d1ec9ec8e3
benm@chromium.org <benm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-10 13:31:59 +0000

    Create a class to represent a Dom Storage Database.
    Add a class that can read/write a local storage database in the same format as WebCore's StorageArea.
        
    Also add a method to sql::Statement to query the declared type of a column and a method to return a string16 from a Blob.
        
    BUG=106763
    
    
    Review URL: http://codereview.chromium.org/9159020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121442 0039d316-1c4b-4281-b951-d872f2087c98

--- sql/diagnostic_error_delegate.h
+++ sql/diagnostic_error_delegate.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -28,9 +28,9 @@ class DiagnosticErrorDelegate : public ErrorDelegate {
 
   virtual int OnError(int error, Connection* connection,
                       Statement* stmt) {
-    NOTREACHED() << "sqlite error " << error
-                 << ", errno " << connection->GetLastErrno()
-                 << ": " << connection->GetErrorMessage();
+    LOG(ERROR) << "sqlite error " << error
+               << ", errno " << connection->GetLastErrno()
+               << ": " << connection->GetErrorMessage();
     RecordErrorInHistogram(error);
     return error;
   }
