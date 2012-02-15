6e6fb7b68d93196d79740426e117730078b6362c
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-15 02:53:01 +0000

    Revert 121379 - net: Simplify SpdyHttpStream::OnSendBody() and OnSendBodyComplete().
    Broke speech input.
    
    BUG=113710
    
    I don't know why I didn't come up with this simpler code from
    the beginning.
    
    BUG=113107
    TEST=net_unittests
    
    
    Review URL: http://codereview.chromium.org/9362023
    
    TBR=satorux@chromium.org
    
    Review URL: http://codereview.chromium.org/9363053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122011 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_stream.h
+++ net/spdy/spdy_stream.h
@@ -208,7 +208,8 @@ class NET_EXPORT_PRIVATE SpdyStream
   void Close();
   bool cancelled() const { return cancelled_; }
   bool closed() const { return io_state_ == STATE_DONE; }
-  // This is only for testing.
+  // TODO(satorux): This is only for testing. We should be able to remove
+  // this once crbug.com/113107 is addressed.
   bool body_sent() const { return io_state_ > STATE_SEND_BODY_COMPLETE; }
 
   // Interface for Spdy[Http|WebSocket]Stream to use.
