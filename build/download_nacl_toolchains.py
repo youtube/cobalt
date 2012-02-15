380c97ebf2e1cf800e0e030904da7b51353f3b04
noelallen@chromium.org <noelallen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-15 21:14:00 +0000

    Fix PNaCl SDK Builder
    Change download script to make sure 'pnacl' sdk builder gets toolchain.
    Change tarball name for pnacl build to avoid collision.
    For now have pnacl toolchain only build pnacl pieces.
    
    TBR= sehr@google.com
    BUG=11292
    
    Review URL: http://codereview.chromium.org/9408003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122149 0039d316-1c4b-4281-b951-d872f2087c98

--- build/download_nacl_toolchains.py
+++ build/download_nacl_toolchains.py
@@ -34,7 +34,8 @@ def Main(args):
   # gclient sync time for developers, or standard Chrome bots.
   if '--optional-pnacl' in args:
     args.remove('--optional-pnacl')
-    if os.environ.get('BUILDBOT_BUILDERNAME', '') == 'linux_pnacl_sdk':
+    buildbot_name = os.environ.get('BUILDBOT_BUILDERNAME', '')
+    if buildbot_name.find('pnacl') >= 0 and  buildbot_name.find('sdk') >= 0:
       print '\n*** DOWNLOADING PNACL TOOLCHAIN ***\n'
     else:
       args.append('--no-pnacl')
