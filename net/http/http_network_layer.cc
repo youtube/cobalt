4fc013bc2f4519081f4fba54c20df25fe4cd7ae3
rtenneti@google.com <rtenneti@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-16 00:08:04 +0000

    SPDY - enable SPDY/2.1 (flow control). This change is an almost a revert of the following CL.
    http://src.chromium.org/viewvc/chrome?view=rev&revision=121964
    
    R=willchan
    BUG=114312
    TEST=network unit tests, upload large file and test with SPDY proxy
    Review URL: https://chromiumcodereview.appspot.com/9363067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122192 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_network_layer.cc
+++ net/http/http_network_layer.cc
@@ -101,6 +101,7 @@ void HttpNetworkLayer::EnableSpdy(const std::string& mode) {
       std::vector<std::string> next_protos;
       next_protos.push_back("http/1.1");
       next_protos.push_back("spdy/2");
+      next_protos.push_back("spdy/2.1");
       HttpStreamFactory::set_next_protos(next_protos);
     } else if (option == kEnableNpnHttpOnly) {
       // Avoid alternate protocol in this case. Otherwise, browser will try SSL
@@ -129,7 +130,7 @@ void HttpNetworkLayer::EnableSpdy(const std::string& mode) {
     } else if (option == kForceAltProtocols) {
       PortAlternateProtocolPair pair;
       pair.port = 443;
-      pair.protocol = NPN_SPDY_2;
+      pair.protocol = NPN_SPDY_21;
       HttpServerPropertiesImpl::ForceAlternateProtocol(pair);
     } else if (option == kSingleDomain) {
       SpdySessionPool::ForceSingleDomain();
