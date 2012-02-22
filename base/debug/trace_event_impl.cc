c03c6bba2b2dae302e222df520702dfba7fed9d2
jbates@chromium.org <jbates@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 02:10:36 +0000

    Implement proper atomic code for trace macros.
    This is not meant to prevent races on what is pointed to by the uint8*. Instead, it is meant to prevent the compiler from assuming that consecutive loads of the uint8 will be the same value. This prevents the scenario described in Clarification 4: http://code.google.com/p/data-race-test/wiki/AboutRaces#Objection_4
    
    R=dvyukov
    BUG=98417,115107
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=122552
    
    Review URL: http://codereview.chromium.org/9416044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122949 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/trace_event_impl.cc
+++ base/debug/trace_event_impl.cc
@@ -20,6 +20,7 @@
 #include "base/utf_string_conversions.h"
 #include "base/stl_util.h"
 #include "base/sys_info.h"
+#include "base/third_party/dynamic_annotations/dynamic_annotations.h"
 #include "base/time.h"
 
 #if defined(OS_WIN)
@@ -323,6 +324,17 @@ TraceLog* TraceLog::GetInstance() {
 TraceLog::TraceLog()
     : enabled_(false)
     , dispatching_to_observer_list_(false) {
+  // Trace is enabled or disabled on one thread while other threads are
+  // accessing the enabled flag. We don't care whether edge-case events are
+  // traced or not, so we allow races on the enabled flag to keep the trace
+  // macros fast.
+  // TODO(jbates): ANNOTATE_BENIGN_RACE_SIZED crashes windows TSAN bots:
+  // ANNOTATE_BENIGN_RACE_SIZED(g_category_enabled, sizeof(g_category_enabled),
+  //                            "trace_event category enabled");
+  for (int i = 0; i < TRACE_EVENT_MAX_CATEGORIES; ++i) {
+    ANNOTATE_BENIGN_RACE(&g_category_enabled[i],
+                         "trace_event category enabled");
+  }
   SetProcessID(static_cast<int>(base::GetCurrentProcId()));
 }
 
@@ -362,8 +374,6 @@ static void EnableMatchingCategory(int category_index,
     if (is_match)
       break;
   }
-  ANNOTATE_BENIGN_RACE(&g_category_enabled[category_index],
-                       "trace_event category enabled");
   g_category_enabled[category_index] = is_match ?
       is_included : (is_included ^ 1);
 }
@@ -402,8 +412,6 @@ const unsigned char* TraceLog::GetCategoryEnabledInternal(const char* name) {
       else
         EnableMatchingCategory(new_index, excluded_categories_, 0);
     } else {
-      ANNOTATE_BENIGN_RACE(&g_category_enabled[new_index],
-                           "trace_event category enabled");
       g_category_enabled[new_index] = 0;
     }
     return &g_category_enabled[new_index];
