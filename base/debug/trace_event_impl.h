45f7e76a6d14875f1bebbea4a53f2e6b75a0ab35
jbates@chromium.org <jbates@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 02:10:36 +0000

    Implement proper atomic code for trace macros.
    This is not meant to prevent races on what is pointed to by the uint8*. Instead, it is meant to prevent the compiler from assuming that consecutive loads of the uint8 will be the same value. This prevents the scenario described in Clarification 4: http://code.google.com/p/data-race-test/wiki/AboutRaces#Objection_4
    
    R=dvyukov
    BUG=98417,115107
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=122552
    
    Review URL: http://codereview.chromium.org/9416044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122949 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/trace_event_impl.h
+++ base/debug/trace_event_impl.h
@@ -18,7 +18,6 @@
 #include "base/observer_list.h"
 #include "base/string_util.h"
 #include "base/synchronization/lock.h"
-#include "base/third_party/dynamic_annotations/dynamic_annotations.h"
 #include "base/timer.h"
 
 // Older style trace macros with explicit id and extra data
