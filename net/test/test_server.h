bf69cb6129792bb06162fdb3c55d9c5ca127c70c
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 16:41:10 +0000

    Add '--host' option to testserver.py and expose it via a new TestServer constructor.
    By default the value 127.0.0.1 is used (this is also the default if no option is passed to the testserver.py script). For HTTPS cert mismatch tests, the value localhost is used. This causes the server to listen on localhost/127.0.0.1 but causes the client to expect a cert matching localhost (this corresponds to the previous behaviour).
    
    BUG=114369
    TEST=net_unittests still pass
    
    Review URL: http://codereview.chromium.org/9369029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123040 0039d316-1c4b-4281-b951-d872f2087c98

--- net/test/test_server.h
+++ net/test/test_server.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -110,7 +110,16 @@ class TestServer {
     bool record_resume;
   };
 
+  // Pass as the 'host' parameter during construction to server on 127.0.0.1
+  static const char* kLocalhost;
+
+  // Initialize a TestServer listening on 127.0.0.1.
   TestServer(Type type, const FilePath& document_root);
+
+  // Initialize a TestServer listening on a specific host (IP or hostname).
+  TestServer(Type type,
+             const std::string& host,
+             const FilePath& document_root);
 
   // Initialize a HTTPS TestServer with a specific set of HTTPSOptions.
   TestServer(const HTTPSOptions& https_options,
@@ -144,7 +153,7 @@ class TestServer {
       std::string* replacement_path);
 
  private:
-  void Init(const FilePath& document_root);
+  void Init(const std::string& host, const FilePath& document_root);
 
   // Modify PYTHONPATH to contain libraries we need.
   bool SetPythonPath() WARN_UNUSED_RESULT;
