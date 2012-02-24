da400dda601ba17f981b6615a4306da047380b64
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-24 16:16:20 +0000

    Remove usage of a deprecated TestServer constructor.
    Hostname must now be explicitly specified (previously default was 127.0.0.1). See the following CL for further details:
    
    http://codereview.chromium.org/9369029/
    
    A follow-up CL will remove the deprecated constructor:
    
    http://codereview.chromium.org/9431002/
    
    BUG=114369
    TEST=everything still compiles and passes
    
    
    Review URL: http://codereview.chromium.org/9430050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123486 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/proxy_script_fetcher_impl_unittest.cc
+++ net/proxy/proxy_script_fetcher_impl_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -118,7 +118,9 @@ GURL GetTestFileUrl(const std::string& relpath) {
 class ProxyScriptFetcherImplTest : public PlatformTest {
  public:
   ProxyScriptFetcherImplTest()
-      : test_server_(TestServer::TYPE_HTTP, FilePath(kDocRoot)) {
+      : test_server_(TestServer::TYPE_HTTP,
+                     net::TestServer::kLocalhost,
+                     FilePath(kDocRoot)) {
   }
 
   static void SetUpTestCase() {
