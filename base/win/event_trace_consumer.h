9af3b03166196efd1e2555b1ca7752970549be2d
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-24 23:36:04 +0000

    Plug trace session handle leak.
    BUG=none
    TEST=none
    R=siggi@chromium.org,jar@chromium.org
    
    Review URL: http://codereview.chromium.org/9464015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123584 0039d316-1c4b-4281-b951-d872f2087c98

--- base/win/event_trace_consumer.h
+++ base/win/event_trace_consumer.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -137,9 +137,8 @@ HRESULT EtwTraceConsumerBase<ImplClass>::Close() {
       if (FAILED(HRESULT_FROM_WIN32(ret)))
         hr = HRESULT_FROM_WIN32(ret);
     }
-
-    trace_handles_.clear();
   }
+  trace_handles_.clear();
 
   return hr;
 }
