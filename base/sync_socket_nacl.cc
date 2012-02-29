3dffe37a1893dbb277790823cdb4686a7887ff69
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-29 16:10:45 +0000

    Add NaCl SyncSocket and SharedMemory stubs for the PPAPI Proxy. BUG=none TEST=none
    Review URL: http://codereview.chromium.org/9515018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124181 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ base/sync_socket_nacl.cc
@@ -0,0 +1,63 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/sync_socket.h"
+
+#include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+#include "base/file_util.h"
+#include "base/logging.h"
+
+
+namespace base {
+
+const SyncSocket::Handle SyncSocket::kInvalidHandle = -1;
+
+SyncSocket::SyncSocket() : handle_(kInvalidHandle) {
+  NOTREACHED();
+}
+
+SyncSocket::~SyncSocket() {
+}
+
+// static
+bool SyncSocket::CreatePair(SyncSocket* socket_a, SyncSocket* socket_b) {
+  return false;
+}
+
+bool SyncSocket::Close() {
+ return false;
+}
+
+size_t SyncSocket::Send(const void* buffer, size_t length) {
+  return 0;
+}
+
+size_t SyncSocket::Receive(void* buffer, size_t length) {
+  return 0;
+}
+
+size_t SyncSocket::Peek() {
+  return 0;
+}
+
+CancelableSyncSocket::CancelableSyncSocket() {}
+CancelableSyncSocket::CancelableSyncSocket(Handle handle)
+    : SyncSocket(handle) {
+}
+
+bool CancelableSyncSocket::Shutdown() {
+  return false;
+}
+
+// static
+bool CancelableSyncSocket::CreatePair(CancelableSyncSocket* socket_a,
+                                      CancelableSyncSocket* socket_b) {
+  return SyncSocket::CreatePair(socket_a, socket_b);
+}
+
+}  // namespace base
