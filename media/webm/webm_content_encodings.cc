4e36659e88fa947e79f886baf18d40253e4d7c0d
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-05 23:46:07 +0000

    Fix memory leak in WebMContentEncodingsClient.
    - Make ContentEncoding a class instead of a struct.
    - Remove ref count from ContentEncoding.
    - Revert r123446: "Suppress leak in WebMListParser"
    - Revert r123451: "Update suppression for 115606"
    - Revert r123499: "Suppress WebMContentEncodingsClient::OnListStart leak for drmemory."
        
    BUG=115606
    TEST=media_unittest with HeapCheck
    
    Review URL: http://codereview.chromium.org/9474022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125024 0039d316-1c4b-4281-b951-d872f2087c98

--- media/webm/webm_content_encodings.cc
+++ media/webm/webm_content_encodings.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/logging.h"
 #include "media/webm/webm_content_encodings.h"
 
 namespace media {
@@ -15,5 +16,14 @@ ContentEncoding::ContentEncoding()
 }
 
 ContentEncoding::~ContentEncoding() {}
+
+void ContentEncoding::SetEncryptionKeyId(const uint8* encryption_key_id,
+                                         int size) {
+  DCHECK(encryption_key_id);
+  DCHECK_GT(size, 0);
+  encryption_key_id_.reset(new uint8[size]);
+  memcpy(encryption_key_id_.get(), encryption_key_id, size);
+  encryption_key_id_size_ = size;
+}
 
 }  // namespace media
