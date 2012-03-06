44753a3e4188580a3f0d657cfedc7851c3463281
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-06 01:33:50 +0000

    Fold media::MessageLoopFactoryImpl into media::MessageLoopFactory.
    We haven't had a second implementation of the interface since MessageLoopFactory was added in r71548.
    
    Review URL: https://chromiumcodereview.appspot.com/9597016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125064 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/message_loop_factory.cc
+++ media/base/message_loop_factory.cc
@@ -1,11 +1,50 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "media/base/message_loop_factory.h"
 
+#include "base/threading/thread.h"
+
 namespace media {
 
-MessageLoopFactory::~MessageLoopFactory() {}
+MessageLoopFactory::MessageLoopFactory() {}
+
+MessageLoopFactory::~MessageLoopFactory() {
+  for (ThreadMap::iterator iter = thread_map_.begin();
+       iter != thread_map_.end();
+       ++iter) {
+    base::Thread* thread = (*iter).second;
+
+    if (thread) {
+      thread->Stop();
+      delete thread;
+    }
+  }
+  thread_map_.clear();
+}
+
+MessageLoop* MessageLoopFactory::GetMessageLoop(const std::string& name) {
+  return GetThread(name)->message_loop();
+}
+
+scoped_refptr<base::MessageLoopProxy>
+MessageLoopFactory::GetMessageLoopProxy(const std::string& name) {
+  return GetThread(name)->message_loop_proxy();
+}
+
+base::Thread* MessageLoopFactory::GetThread(const std::string& name) {
+  DCHECK(!name.empty());
+
+  base::AutoLock auto_lock(lock_);
+  ThreadMap::iterator it = thread_map_.find(name);
+  if (it != thread_map_.end())
+    return (*it).second;
+
+  base::Thread* thread = new base::Thread(name.c_str());
+  CHECK(thread->Start()) << "Failed to start thread: " << name;
+  thread_map_[name] = thread;
+  return thread;
+}
 
 }  // namespace media
