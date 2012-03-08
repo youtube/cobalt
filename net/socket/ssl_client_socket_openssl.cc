3d94c3e248b3c8252558083f8665063abbd90c42
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-08 23:20:42 +0000

    Roll OpenSSL deps, implement keying material exporter and test
    Changes included:
    1. OpenSSL deps to include support for keying material exporter.
    2. Implement keying material exported in SSLClientSocket.
    3. Add unit test to make the feature runs.
    
    BUG=None
    TEST=net_unittests
    
    
    Review URL: http://codereview.chromium.org/9648005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125720 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_openssl.cc
+++ net/socket/ssl_client_socket_openssl.cc
@@ -626,7 +626,23 @@ void SSLClientSocketOpenSSL::GetSSLCertRequestInfo(
 int SSLClientSocketOpenSSL::ExportKeyingMaterial(
     const base::StringPiece& label, const base::StringPiece& context,
     unsigned char *out, unsigned int outlen) {
-  return ERR_NOT_IMPLEMENTED;
+  crypto::OpenSSLErrStackTracer err_tracer(FROM_HERE);
+
+  int rv = SSL_export_keying_material(
+      ssl_, out, outlen, const_cast<char*>(label.data()),
+      label.size(),
+      reinterpret_cast<unsigned char*>(const_cast<char*>(context.data())),
+      context.length(),
+      context.length() > 0);
+
+  if (rv != 1) {
+    int ssl_error = SSL_get_error(ssl_, rv);
+    LOG(ERROR) << "Failed to export keying material;"
+               << " returned " << rv
+               << ", SSL error code " << ssl_error;
+    return MapOpenSSLError(ssl_error, err_tracer);
+  }
+  return OK;
 }
 
 SSLClientSocket::NextProtoStatus SSLClientSocketOpenSSL::GetNextProto(
