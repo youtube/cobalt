13cd9e0990b1db652f575f0b7b3fcdf2d1bb2d16
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-12 21:35:32 +0000

    Fix a buffer length bug and nits in the next protocol negotiation (NPN) functions.
    R=agl@chromium.org
    BUG=116617
    TEST=none
    
    Review URL: http://codereview.chromium.org/9663034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126239 0039d316-1c4b-4281-b951-d872f2087c98

--- net/third_party/nss/ssl/ssl3ext.c
+++ net/third_party/nss/ssl/ssl3ext.c
@@ -606,10 +606,7 @@ ssl3_ClientHandleNextProtoNegoXtn(sslSocket *ss, PRUint16 ex_type,
     unsigned char resultBuffer[255];
     SECItem result = { siBuffer, resultBuffer, 0 };
 
-    if (ss->firstHsDone) {
-	PORT_SetError(SSL_ERROR_NEXT_PROTOCOL_DATA_INVALID);
-	return SECFailure;
-    }
+    PORT_Assert(!ss->firstHsDone);
 
     rv = ssl3_ValidateNextProtoNego(data->data, data->len);
     if (rv != SECSuccess)
@@ -621,6 +618,8 @@ ssl3_ClientHandleNextProtoNegoXtn(sslSocket *ss, PRUint16 ex_type,
      */
     PORT_Assert(ss->nextProtoCallback != NULL);
     if (!ss->nextProtoCallback) {
+	/* XXX Use a better error code. This is an application error, not an
+	 * NSS bug. */
 	PORT_SetError(SEC_ERROR_LIBRARY_FAILURE);
 	return SECFailure;
     }
@@ -631,7 +630,7 @@ ssl3_ClientHandleNextProtoNegoXtn(sslSocket *ss, PRUint16 ex_type,
 	return rv;
     /* If the callback wrote more than allowed to |result| it has corrupted our
      * stack. */
-    if (result.len > sizeof result) {
+    if (result.len > sizeof resultBuffer) {
 	PORT_SetError(SEC_ERROR_OUTPUT_LEN);
 	return SECFailure;
     }
