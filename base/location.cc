353e9f4fe7d38ec067bacd043b762268fe386d00
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-16 21:43:41 +0000

    Remove "base/values.h" import from "base/location.h", fix up downstream files.
    BUG=none
    TEST=none
    TBR=sky@chromium.org,dmazzoni@chromium.org,thestig@chromium.org,joaodasilva@chromium.org,willchan@chromium.org,hclam@chromium.org
    
    Review URL: http://codereview.chromium.org/9703098
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127256 0039d316-1c4b-4281-b951-d872f2087c98

--- base/location.cc
+++ base/location.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -14,6 +14,7 @@ extern "C" {
 #include "base/location.h"
 #include "base/string_number_conversions.h"
 #include "base/stringprintf.h"
+#include "base/values.h"
 
 namespace tracked_objects {
 
