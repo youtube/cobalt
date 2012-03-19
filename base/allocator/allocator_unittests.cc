23b4aad083a4b61dc0a17ee043197493398395d7
hans@chromium.org <hans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-19 20:35:09 +0000

    Make allocator_unittests a Win-only target.
    It doesn't build on Linux and Mac.
    
    BUG=118376
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9720046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127522 0039d316-1c4b-4281-b951-d872f2087c98

--- base/allocator/allocator_unittests.cc
+++ base/allocator/allocator_unittests.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,7 +6,6 @@
 #include <stdlib.h>
 #include <algorithm>   // for min()
 #include "base/atomicops.h"
-#include "base/logging.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 // Number of bits in a size_t.
