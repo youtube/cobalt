4c7605a323a0999d63f0e19b2922165fc9abdccf
yutak@chromium.org <yutak@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-21 20:29:52 +0000

    Increase number of max sockets per group for WebSocket connections.
    This change makes ClientSocketPoolManager aware of SocketPoolType so it can
    pick a different limit for normal socket pools and WebSocket socket pools.
    
    As an initial (experimental) setting, number of max sockets per group for
    WebSocket connections is raised to 30, and other limits are set to the same
    value as normal socket pools. This configuration should be revisited later
    when WebSocket protocol stack starts to work and a good amount of usage metrics
    are collected.
    
    BUG=118268
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9764003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128044 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_network_session_peer.cc
+++ net/http/http_network_session_peer.cc
@@ -7,6 +7,7 @@
 #include "net/http/http_network_session.h"
 #include "net/http/http_proxy_client_socket_pool.h"
 #include "net/proxy/proxy_service.h"
+#include "net/socket/client_socket_pool_manager.h"
 #include "net/socket/socks_client_socket_pool.h"
 #include "net/socket/ssl_client_socket_pool.h"
 #include "net/socket/transport_client_socket_pool.h"
