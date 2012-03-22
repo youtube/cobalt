7b15cc6c88824d22a302db8423c0411573377e89
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 04:50:24 +0000

    Move X509Certificate::Verify into CertVerifyProc
    With this split, CertVerifyProc is responsible for
    interacting with the underlying PKIX path building and
    verification library, while X509Certificate is responsible
    for parsing certificates with the underlying crypto library
    and exposing a common interface for higher-level code such
    as UI.
    
    BUG=114343
    TEST=net_unittests 
    Review URL: https://chromiumcodereview.appspot.com/9691054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128172 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/x509_certificate.h
+++ net/base/x509_certificate.h
@@ -26,7 +26,6 @@
 #include <CoreFoundation/CFArray.h>
 #include <Security/SecBase.h>
 
-#include "base/synchronization/lock.h"
 #elif defined(USE_OPENSSL)
 // Forward declaration; real one in <x509.h>
 typedef struct x509_st X509;
@@ -224,6 +223,9 @@ class NET_EXPORT X509Certificate
   // Appends a representation of this object to the given pickle.
   void Persist(Pickle* pickle);
 
+  // The serial number, DER encoded, possibly including a leading 00 byte.
+  const std::string& serial_number() const { return serial_number_; }
+
   // The subject of the certificate.  For HTTPS server certificates, this
   // represents the web server.  The common name of the subject should match
   // the host name of the web server.
@@ -281,26 +283,6 @@ class NET_EXPORT X509Certificate
 
   // Do any of the given issuer names appear in this cert's chain of trust?
   bool IsIssuedBy(const std::vector<CertPrincipal>& valid_issuers);
-
-  // Creates a security policy for certificates used as client certificates
-  // in SSL.
-  // If a policy is successfully created, it will be stored in
-  // |*policy| and ownership transferred to the caller.
-  static OSStatus CreateSSLClientPolicy(SecPolicyRef* policy);
-
-  // Creates a security policy for basic X.509 validation. If the policy is
-  // successfully created, it will be stored in |*policy| and ownership
-  // transferred to the caller.
-  static OSStatus CreateBasicX509Policy(SecPolicyRef* policy);
-
-  // Creates security policies to control revocation checking (OCSP and CRL).
-  // If |enable_revocation_checking| is false, the policies returned will be
-  // explicitly disabled from accessing the network or the cache. This may be
-  // used to override system settings regarding revocation checking.
-  // If the policies are successfully created, they will be appended to
-  // |policies|.
-  static OSStatus CreateRevocationPolicies(bool enable_revocation_checking,
-                                           CFMutableArrayRef policies);
 
   // Adds all available SSL client identity certs to the given vector.
   // |server_domain| is a hint for which domain the cert is to be sent to
@@ -361,12 +343,6 @@ class NET_EXPORT X509Certificate
   // os_cert_handle(). For this reason, the returned PCCERT_CONTEXT *MUST NOT*
   // be stored in an X509Certificate.
   PCCERT_CONTEXT CreateOSCertChainForCert() const;
-#endif
-
-#if defined(OS_ANDROID)
-  // |chain_bytes| will contain the chain (including this certificate) encoded
-  // using GetChainDEREncodedBytes below.
-  void GetChainDEREncodedBytes(std::vector<std::string>* chain_bytes) const;
 #endif
 
 #if defined(USE_OPENSSL)
@@ -455,11 +431,8 @@ class NET_EXPORT X509Certificate
  private:
   friend class base::RefCountedThreadSafe<X509Certificate>;
   friend class TestRootCerts;  // For unit tests
-  // TODO(rsleevi): Temporary refactoring - http://crbug.com/114343
-  friend class CertVerifyProcStub;
 
   FRIEND_TEST_ALL_PREFIXES(X509CertificateNameVerifyTest, VerifyHostname);
-  FRIEND_TEST_ALL_PREFIXES(X509CertificateTest, DigiNotarCerts);
   FRIEND_TEST_ALL_PREFIXES(X509CertificateTest, SerialNumbers);
 
   // Construct an X509Certificate from a handle to the certificate object
@@ -472,44 +445,6 @@ class NET_EXPORT X509Certificate
   // Common object initialization code.  Called by the constructors only.
   void Initialize();
 
-  // Verifies the certificate against the given hostname.  Returns OK if
-  // successful or an error code upon failure.
-  //
-  // The |*verify_result| structure, including the |verify_result->cert_status|
-  // bitmask, is always filled out regardless of the return value.  If the
-  // certificate has multiple errors, the corresponding status flags are set in
-  // |verify_result->cert_status|, and the error code for the most serious
-  // error is returned.
-  //
-  // |flags| is bitwise OR'd of VerifyFlags:
-  //
-  // If VERIFY_REV_CHECKING_ENABLED is set in |flags|, online certificate
-  // revocation checking is performed (i.e. OCSP and downloading CRLs). CRLSet
-  // based revocation checking is always enabled, regardless of this flag, if
-  // |crl_set| is given.
-  //
-  // If VERIFY_EV_CERT is set in |flags| too, EV certificate verification is
-  // performed.
-  //
-  // |crl_set| points to an optional CRLSet structure which can be used to
-  // avoid revocation checks over the network.
-  int Verify(const std::string& hostname,
-             int flags,
-             CRLSet* crl_set,
-             CertVerifyResult* verify_result) const;
-
-#if defined(OS_WIN)
-  bool CheckEV(PCCERT_CHAIN_CONTEXT chain_context,
-               bool rev_checking_enabled,
-               const char* policy_oid) const;
-  static bool IsIssuedByKnownRoot(PCCERT_CHAIN_CONTEXT chain_context);
-#endif
-#if defined(OS_MACOSX)
-  static bool IsIssuedByKnownRoot(CFArrayRef chain);
-#endif
-#if defined(USE_NSS)
-  bool VerifyEV(int flags, CRLSet* crl_set) const;
-#endif
 #if defined(USE_OPENSSL)
   // Resets the store returned by cert_store() to default state. Used by
   // TestRootCerts to undo modifications.
@@ -529,32 +464,6 @@ class NET_EXPORT X509Certificate
                              const std::string& cert_common_name,
                              const std::vector<std::string>& cert_san_dns_names,
                              const std::vector<std::string>& cert_san_ip_addrs);
-
-  // Performs the platform-dependent part of the Verify() method, verifiying
-  // this certificate against the platform's root CA certificates.
-  //
-  // Parameters and return value are as per Verify().
-  int VerifyInternal(const std::string& hostname,
-                     int flags,
-                     CRLSet* crl_set,
-                     CertVerifyResult* verify_result) const;
-
-  // The serial number, DER encoded, possibly including a leading 00 byte.
-  const std::string& serial_number() const { return serial_number_; }
-
-  // IsBlacklisted returns true if this certificate is explicitly blacklisted.
-  bool IsBlacklisted() const;
-
-  // IsPublicKeyBlacklisted returns true iff one of |public_key_hashes| (which
-  // are SHA1 hashes of SubjectPublicKeyInfo structures) is explicitly blocked.
-  static bool IsPublicKeyBlacklisted(
-      const std::vector<SHA1Fingerprint>& public_key_hashes);
-
-  // IsSHA1HashInSortedArray returns true iff |hash| is in |array|, a sorted
-  // array of SHA1 hashes.
-  static bool IsSHA1HashInSortedArray(const SHA1Fingerprint& hash,
-                                      const uint8* array,
-                                      size_t array_byte_len);
 
   // Reads a single certificate from |pickle| and returns a platform-specific
   // certificate handle. The format of the certificate stored in |pickle| is
@@ -601,12 +510,6 @@ class NET_EXPORT X509Certificate
   // If this is empty, then GetDefaultNickname will return a generated name
   // based on the type of the certificate.
   std::string default_nickname_;
-#endif
-
-#if defined(OS_MACOSX)
-  // Blocks multiple threads from verifying the cert simultaneously.
-  // (Marked mutable because it's used in a const method.)
-  mutable base::Lock verification_lock_;
 #endif
 
   DISALLOW_COPY_AND_ASSIGN(X509Certificate);
