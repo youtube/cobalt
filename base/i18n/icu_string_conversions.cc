d0d330a7c964e5786f1c0a2d7dbc626e6e1a4e92
steveblock@chromium.org <steveblock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-23 19:38:36 +0000

    Fix a typo in the comment for ToUnicodeCallbackSubstitute()
    See http://codereview.chromium.org/151065
    
    Review URL: https://chromiumcodereview.appspot.com/9724033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128557 0039d316-1c4b-4281-b951-d872f2087c98

--- base/i18n/icu_string_conversions.cc
+++ base/i18n/icu_string_conversions.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -20,7 +20,7 @@
 namespace base {
 
 namespace {
-// ToUnicodeCallbackSubstitute() is based on UCNV_TO_U_CALLBACK_SUSBSTITUTE
+// ToUnicodeCallbackSubstitute() is based on UCNV_TO_U_CALLBACK_SUBSTITUTE
 // in source/common/ucnv_err.c.
 
 // Copyright (c) 1995-2006 International Business Machines Corporation
