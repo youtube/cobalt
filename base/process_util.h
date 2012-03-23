youtube/cobalt

commit 0ab2fe36a37d60a12f4d807bdcee75c10b4e8685

Linux: Report the failed memory allocation size on OOM crashes.
BUG=111733
TEST=none

Review URL: http://codereview.chromium.org/9834001

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128351 0039d316-1c4b-4281-b951-d872f2087c98

base/process_util.h
@@ -134,6 +134,10 @@ enum TerminationStatus {
   TERMINATION_STATUS_MAX_ENUM
 };
 
+#if defined(USE_LINUX_BREAKPAD)
+extern size_t g_oom_size;
+#endif
+
 // Returns the id of the current process.
 BASE_EXPORT ProcessId GetCurrentProcId();
 

base/process_util_linux.cc
@@ -276,6 +276,10 @@ int ReadProcStatusAndGetFieldAsInt(pid_t pid, const std::string& field) {
 
 namespace base {
 
+#if defined(USE_LINUX_BREAKPAD)
+size_t g_oom_size = 0U;
+#endif
+
 ProcessId GetParentProcessId(ProcessHandle process) {
   ProcessId pid = ReadProcStatsAndGetFieldAsInt(process, VM_PPID);
   if (pid)
@@ -643,6 +647,10 @@ size_t GetSystemCommitCharge() {
 namespace {
 
 void OnNoMemorySize(size_t size) {
+#if defined(USE_LINUX_BREAKPAD)
+  g_oom_size = size;
+#endif
+
   if (size != 0)
     LOG(FATAL) << "Out of memory, size = " << size;
   LOG(FATAL) << "Out of memory.";

commit 02fe89be903933f0f662572061190d1aee4c0f4e

Fix shared_library build break on Chrome OS caused by r128351.
BUG=111733
TEST=In the cros chroot, ran 'USE=component_build emerge-$BOARD chromeos-chrome'

Review URL: https://chromiumcodereview.appspot.com/9826041

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128416 0039d316-1c4b-4281-b951-d872f2087c98

base/process_util.h
@@ -135,7 +135,7 @@ enum TerminationStatus {
 };
 
 #if defined(USE_LINUX_BREAKPAD)
-extern size_t g_oom_size;
+BASE_EXPORT extern size_t g_oom_size;
 #endif
 
 // Returns the id of the current process.