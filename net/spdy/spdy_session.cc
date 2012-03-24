38cbb4eb07236e10f0be2df41397ca39f18d25bd
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-24 16:19:24 +0000

    Fix a crash in SpdySession::WriteStreamData which can happen if the specified stream_id is not active.
    BUG=116825
    
    
    Review URL: http://codereview.chromium.org/9808083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128747 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_session.cc
+++ net/spdy/spdy_session.cc
@@ -747,9 +747,9 @@ int SpdySession::WriteStreamData(SpdyStreamId stream_id,
   // Find our stream
   DCHECK(IsStreamActive(stream_id));
   scoped_refptr<SpdyStream> stream = active_streams_[stream_id];
-  CHECK_EQ(stream->stream_id(), stream_id);
   if (!stream)
     return ERR_INVALID_SPDY_STREAM;
+  CHECK_EQ(stream->stream_id(), stream_id);
 
   if (len > kMaxSpdyFrameChunkSize) {
     len = kMaxSpdyFrameChunkSize;
