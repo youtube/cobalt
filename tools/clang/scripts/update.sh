5fefe3abef69579e48da65856db9aca87d3790d6
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-24 05:57:53 +0000

    roll clang 151385:153254
    New stuff:
    * ObjC number, array, dict literals
    * ASan fixes, visibility fixes (see bugs)
    * (c++11: User-defined literals)
    * -Wstring-plus-int
    * Fix for a bug that made the last roll not work with goma
    
    BUG=114996,112539,119119
    TEST=none
    
    Review URL: http://codereview.chromium.org/9836038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128696 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/clang/scripts/update.sh
+++ tools/clang/scripts/update.sh
@@ -8,7 +8,7 @@
 # Do NOT CHANGE this if you don't know what you're doing -- see
 # https://code.google.com/p/chromium/wiki/UpdatingClang
 # Reverting problematic clang rolls is safe, though.
-CLANG_REVISION=151385
+CLANG_REVISION=153254
 
 THIS_DIR="$(dirname "${0}")"
 LLVM_DIR="${THIS_DIR}/../../../third_party/llvm"
