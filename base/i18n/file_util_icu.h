9887770d9ae6de2fb7dc427c17a4b97d033f78ff
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-27 04:12:31 +0000

    Normalize download file name on chromeos
    ChromeOS file manager cannot handle non NFC encoded utf8 file names (because of
    file path normalization done in webkit layer), so let's ensure we normalize
    downloaded file names.
    
    BUG=chromium-os:26028
    TEST=downloaded file with nfd encoded names and made sure they can be handled by file manager
    
    Review URL: https://chromiumcodereview.appspot.com/9854011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129128 0039d316-1c4b-4281-b951-d872f2087c98

--- base/i18n/file_util_icu.h
+++ base/i18n/file_util_icu.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -34,6 +34,10 @@ BASE_I18N_EXPORT void ReplaceIllegalCharactersInPath(
 // std::sort.
 BASE_I18N_EXPORT bool LocaleAwareCompareFilenames(const FilePath& a,
                                                   const FilePath& b);
+
+// Calculates the canonical file-system representation of |file_name| base name.
+// Modifies |file_name| in place. No-op if not on ChromeOS.
+BASE_I18N_EXPORT void NormalizeFileNameEncoding(FilePath* file_name);
 
 }  // namespace file_util
 
