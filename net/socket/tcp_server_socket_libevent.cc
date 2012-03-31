youtube/cobalt

commit 592b1cc60767acd3716f6cc5a4e1b8c3a3da2ec0

base::Bind: Convert ServerSocket::Accept.
BUG=none
TEST=none
R=csilv

Review URL: http://codereview.chromium.org/8771030

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112863 0039d316-1c4b-4281-b951-d872f2087c98

net/socket/tcp_server_socket_libevent.cc
@@ -34,7 +34,6 @@ TCPServerSocketLibevent::TCPServerSocketLibevent(
     const net::NetLog::Source& source)
     : socket_(kInvalidSocket),
       accept_socket_(NULL),
-      accept_callback_(NULL),
       net_log_(BoundNetLog::Make(net_log, NetLog::SOURCE_SOCKET)) {
   scoped_refptr<NetLog::EventParameters> params;
   if (source.is_valid())
@@ -106,11 +105,11 @@ int TCPServerSocketLibevent::GetLocalAddress(IPEndPoint* address) const {
 }
 
 int TCPServerSocketLibevent::Accept(
-    scoped_ptr<StreamSocket>* socket, OldCompletionCallback* callback) {
+    scoped_ptr<StreamSocket>* socket, const CompletionCallback& callback) {
   DCHECK(CalledOnValidThread());
   DCHECK(socket);
-  DCHECK(callback);
-  DCHECK(!accept_callback_);
+  DCHECK(!callback.is_null());
+  DCHECK(accept_callback_.is_null());
 
   net_log_.BeginEvent(NetLog::TYPE_TCP_ACCEPT, NULL);
 
@@ -185,12 +184,11 @@ void TCPServerSocketLibevent::OnFileCanReadWithoutBlocking(int fd) {
 
   int result = AcceptInternal(accept_socket_);
   if (result != ERR_IO_PENDING) {
-    OldCompletionCallback* c = accept_callback_;
-    accept_callback_ = NULL;
     accept_socket_ = NULL;
     bool ok = accept_socket_watcher_.StopWatchingFileDescriptor();
     DCHECK(ok);
-    c->Run(result);
+    accept_callback_.Run(result);
+    accept_callback_.Reset();
   }
 }
 

net/socket/tcp_server_socket_win.cc
@@ -20,7 +20,6 @@ TCPServerSocketWin::TCPServerSocketWin(net::NetLog* net_log,
     : socket_(INVALID_SOCKET),
       socket_event_(WSA_INVALID_EVENT),
       accept_socket_(NULL),
-      accept_callback_(NULL),
       net_log_(BoundNetLog::Make(net_log, NetLog::SOURCE_SOCKET)) {
   scoped_refptr<NetLog::EventParameters> params;
   if (source.is_valid())
@@ -99,11 +98,11 @@ int TCPServerSocketWin::GetLocalAddress(IPEndPoint* address) const {
 }
 
 int TCPServerSocketWin::Accept(
-    scoped_ptr<StreamSocket>* socket, OldCompletionCallback* callback) {
+    scoped_ptr<StreamSocket>* socket, const CompletionCallback& callback) {
   DCHECK(CalledOnValidThread());
   DCHECK(socket);
-  DCHECK(callback);
-  DCHECK(!accept_callback_);
+  DCHECK(!callback.is_null());
+  DCHECK(accept_callback_.is_null());
 
   net_log_.BeginEvent(NetLog::TYPE_TCP_ACCEPT, NULL);
 
@@ -182,10 +181,9 @@ void TCPServerSocketWin::OnObjectSignaled(HANDLE object) {
   if (ev.lNetworkEvents & FD_ACCEPT) {
     int result = AcceptInternal(accept_socket_);
     if (result != ERR_IO_PENDING) {
-      OldCompletionCallback* c = accept_callback_;
-      accept_callback_ = NULL;
       accept_socket_ = NULL;
-      c->Run(result);
+      accept_callback_.Run(result);
+      accept_callback_.Reset();
     }
   }
 }

net/socket/server_socket.h
@@ -29,7 +29,7 @@ class NET_EXPORT ServerSocket {
   // Accept connection. Callback is called when new connection is
   // accepted.
   virtual int Accept(scoped_ptr<StreamSocket>* socket,
-                     OldCompletionCallback* callback) = 0;
+                     const CompletionCallback& callback) = 0;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(ServerSocket);

net/socket/tcp_client_socket_unittest.cc
@@ -41,9 +41,9 @@ TEST(TCPClientSocketTest, BindLoopbackToLoopback) {
   TestOldCompletionCallback connect_callback;
   EXPECT_EQ(ERR_IO_PENDING, socket.Connect(&connect_callback));
 
-  TestOldCompletionCallback accept_callback;
+  TestCompletionCallback accept_callback;
   scoped_ptr<StreamSocket> accepted_socket;
-  int result = server.Accept(&accepted_socket, &accept_callback);
+  int result = server.Accept(&accepted_socket, accept_callback.callback());
   if (result == ERR_IO_PENDING)
     result = accept_callback.WaitForResult();
   ASSERT_EQ(OK, result);

net/socket/tcp_server_socket_libevent.h
@@ -29,7 +29,7 @@ class NET_EXPORT_PRIVATE TCPServerSocketLibevent
   virtual int Listen(const net::IPEndPoint& address, int backlog) OVERRIDE;
   virtual int GetLocalAddress(IPEndPoint* address) const OVERRIDE;
   virtual int Accept(scoped_ptr<StreamSocket>* socket,
-                     OldCompletionCallback* callback) OVERRIDE;
+                     const CompletionCallback& callback) OVERRIDE;
 
   // MessageLoopForIO::Watcher implementation.
   virtual void OnFileCanReadWithoutBlocking(int fd) OVERRIDE;
@@ -44,7 +44,7 @@ class NET_EXPORT_PRIVATE TCPServerSocketLibevent
   MessageLoopForIO::FileDescriptorWatcher accept_socket_watcher_;
 
   scoped_ptr<StreamSocket>* accept_socket_;
-  OldCompletionCallback* accept_callback_;
+  CompletionCallback accept_callback_;
 
   BoundNetLog net_log_;
 };

net/socket/tcp_server_socket_unittest.cc
@@ -82,9 +82,9 @@ TEST_F(TCPServerSocketTest, Accept) {
                                     NULL, NetLog::Source());
   connecting_socket.Connect(&connect_callback);
 
-  TestOldCompletionCallback accept_callback;
+  TestCompletionCallback accept_callback;
   scoped_ptr<StreamSocket> accepted_socket;
-  int result = socket_.Accept(&accepted_socket, &accept_callback);
+  int result = socket_.Accept(&accepted_socket, accept_callback.callback());
   if (result == ERR_IO_PENDING)
     result = accept_callback.WaitForResult();
   ASSERT_EQ(OK, result);
@@ -102,10 +102,11 @@ TEST_F(TCPServerSocketTest, Accept) {
 TEST_F(TCPServerSocketTest, AcceptAsync) {
   ASSERT_NO_FATAL_FAILURE(SetUpIPv4());
 
-  TestOldCompletionCallback accept_callback;
+  TestCompletionCallback accept_callback;
   scoped_ptr<StreamSocket> accepted_socket;
 
-  ASSERT_EQ(ERR_IO_PENDING, socket_.Accept(&accepted_socket, &accept_callback));
+  ASSERT_EQ(ERR_IO_PENDING,
+            socket_.Accept(&accepted_socket, accept_callback.callback()));
 
   TestOldCompletionCallback connect_callback;
   TCPClientSocket connecting_socket(local_address_list(),
@@ -126,11 +127,11 @@ TEST_F(TCPServerSocketTest, AcceptAsync) {
 TEST_F(TCPServerSocketTest, Accept2Connections) {
   ASSERT_NO_FATAL_FAILURE(SetUpIPv4());
 
-  TestOldCompletionCallback accept_callback;
+  TestCompletionCallback accept_callback;
   scoped_ptr<StreamSocket> accepted_socket;
 
   ASSERT_EQ(ERR_IO_PENDING,
-            socket_.Accept(&accepted_socket, &accept_callback));
+            socket_.Accept(&accepted_socket, accept_callback.callback()));
 
   TestOldCompletionCallback connect_callback;
   TCPClientSocket connecting_socket(local_address_list(),
@@ -144,9 +145,9 @@ TEST_F(TCPServerSocketTest, Accept2Connections) {
 
   EXPECT_EQ(OK, accept_callback.WaitForResult());
 
-  TestOldCompletionCallback accept_callback2;
+  TestCompletionCallback accept_callback2;
   scoped_ptr<StreamSocket> accepted_socket2;
-  int result = socket_.Accept(&accepted_socket2, &accept_callback2);
+  int result = socket_.Accept(&accepted_socket2, accept_callback2.callback());
   if (result == ERR_IO_PENDING)
     result = accept_callback2.WaitForResult();
   ASSERT_EQ(OK, result);
@@ -174,9 +175,9 @@ TEST_F(TCPServerSocketTest, AcceptIPv6) {
                                     NULL, NetLog::Source());
   connecting_socket.Connect(&connect_callback);
 
-  TestOldCompletionCallback accept_callback;
+  TestCompletionCallback accept_callback;
   scoped_ptr<StreamSocket> accepted_socket;
-  int result = socket_.Accept(&accepted_socket, &accept_callback);
+  int result = socket_.Accept(&accepted_socket, accept_callback.callback());
   if (result == ERR_IO_PENDING)
     result = accept_callback.WaitForResult();
   ASSERT_EQ(OK, result);

net/socket/tcp_server_socket_win.h
@@ -11,6 +11,7 @@
 #include "base/message_loop.h"
 #include "base/win/object_watcher.h"
 #include "base/threading/non_thread_safe.h"
+#include "net/base/completion_callback.h"
 #include "net/base/net_log.h"
 #include "net/socket/server_socket.h"
 
@@ -31,7 +32,7 @@ class NET_EXPORT_PRIVATE TCPServerSocketWin
   virtual int Listen(const net::IPEndPoint& address, int backlog);
   virtual int GetLocalAddress(IPEndPoint* address) const;
   virtual int Accept(scoped_ptr<StreamSocket>* socket,
-                     OldCompletionCallback* callback);
+                     const CompletionCallback& callback);
 
   // base::ObjectWatcher::Delegate implementation.
   virtual void OnObjectSignaled(HANDLE object);
@@ -46,7 +47,7 @@ class NET_EXPORT_PRIVATE TCPServerSocketWin
   base::win::ObjectWatcher accept_watcher_;
 
   scoped_ptr<StreamSocket>* accept_socket_;
-  OldCompletionCallback* accept_callback_;
+  CompletionCallback accept_callback_;
 
   BoundNetLog net_log_;
 };

commit 1260aa7ff2255bf85dc76d5d7266642ac50a8693

Reset accept_callback_ before calling it from TCPServerSocket.
TCPServerSocket worked properly before, but then it was broken by r112863.

Review URL: https://chromiumcodereview.appspot.com/9837122

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130008 0039d316-1c4b-4281-b951-d872f2087c98

net/socket/tcp_server_socket_libevent.cc
@@ -187,8 +187,9 @@ void TCPServerSocketLibevent::OnFileCanReadWithoutBlocking(int fd) {
     accept_socket_ = NULL;
     bool ok = accept_socket_watcher_.StopWatchingFileDescriptor();
     DCHECK(ok);
-    accept_callback_.Run(result);
+    CompletionCallback callback = accept_callback_;
     accept_callback_.Reset();
+    callback.Run(result);
   }
 }
 

net/socket/tcp_server_socket_win.cc
@@ -182,8 +182,9 @@ void TCPServerSocketWin::OnObjectSignaled(HANDLE object) {
     int result = AcceptInternal(accept_socket_);
     if (result != ERR_IO_PENDING) {
       accept_socket_ = NULL;
-      accept_callback_.Run(result);
+      CompletionCallback callback = accept_callback_;
       accept_callback_.Reset();
+      callback.Run(result);
     }
   }
 }