715ec043c22b3dfa1cab2127179cffba72ca2224
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-17 19:05:15 +0000

    net: support SHA512 hashes in DNSSEC chains.
    Patch-by: Simon Arlott
    
    BUG=122239
    TEST=https://chromium-122239.test.lp0.eu/
    
    
    Review URL: http://codereview.chromium.org/10082010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132612 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/dns_util.h
+++ net/base/dns_util.h
@@ -60,6 +60,7 @@ static const uint16 kDNS_TESTING = 0xfffe;  // in private use area.
 static const uint8 kDNSSEC_RSA_SHA1 = 5;
 static const uint8 kDNSSEC_RSA_SHA1_NSEC3 = 7;
 static const uint8 kDNSSEC_RSA_SHA256 = 8;
+static const uint8 kDNSSEC_RSA_SHA512 = 10;
 
 // RFC 4509
 static const uint8 kDNSSEC_SHA1 = 1;
