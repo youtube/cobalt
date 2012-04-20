ce1b08edf9c5814743d6d3b0026c402adedea15c
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-20 06:05:02 +0000

    Add field trial for domain bound certificates.
    BUG=124105
    TEST=local hack
    
    
    Review URL: http://codereview.chromium.org/10035049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133151 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/ssl_config_service.cc
+++ net/base/ssl_config_service.cc
@@ -57,6 +57,7 @@ SSLConfigService::SSLConfigService()
 }
 
 static bool g_cached_info_enabled = false;
+static bool g_domain_bound_certs_trial = false;
 
 // GlobalCRLSet holds a reference to the global CRLSet. It simply wraps a lock
 // around a scoped_refptr so that getting a reference doesn't race with
@@ -100,6 +101,11 @@ bool SSLConfigService::cached_info_enabled() {
   return g_cached_info_enabled;
 }
 
+// static
+void SSLConfigService::EnableDomainBoundCertsTrial() {
+  g_domain_bound_certs_trial = true;
+}
+
 void SSLConfigService::AddObserver(Observer* observer) {
   observer_list_.AddObserver(observer);
 }
@@ -114,6 +120,8 @@ SSLConfigService::~SSLConfigService() {
 // static
 void SSLConfigService::SetSSLConfigFlags(SSLConfig* ssl_config) {
   ssl_config->cached_info_enabled = g_cached_info_enabled;
+  if (g_domain_bound_certs_trial)
+    ssl_config->domain_bound_certs_enabled = true;
 }
 
 void SSLConfigService::ProcessConfigUpdate(const SSLConfig& orig_config,
