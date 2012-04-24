906cbd0e786e61fb7a62582cddd19b6ee00f4575
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-24 21:13:28 +0000

    Use base::MessageLoopProxy instead of MessageLoop in ProxyConfigServiceLinux.
    This will allow using that class on the main NPAPI plugin thread in 
    chromoting.
    
    BUG=124728
    
    
    Review URL: http://codereview.chromium.org/10140010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133767 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/proxy_config_service_linux_unittest.cc
+++ net/proxy/proxy_config_service_linux_unittest.cc
@@ -175,7 +175,7 @@ class MockSettingGetter
     values = zero_values;
   }
 
-  virtual bool Init(MessageLoop* glib_default_loop,
+  virtual bool Init(base::MessageLoopProxy* glib_default_loop,
                     MessageLoopForIO* file_loop) OVERRIDE {
     return true;
   }
@@ -187,7 +187,7 @@ class MockSettingGetter
     return true;
   }
 
-  virtual MessageLoop* GetNotificationLoop() OVERRIDE {
+  virtual base::MessageLoopProxy* GetNotificationLoop() OVERRIDE {
     return NULL;
   }
 
@@ -292,7 +292,7 @@ class SynchConfigGetter {
     DCHECK_EQ(MessageLoop::TYPE_IO, file_loop->type());
     // We pass the mock IO thread as both the IO and file threads.
     config_service_->SetupAndFetchInitialConfig(
-        MessageLoop::current(), io_thread_.message_loop(),
+        base::MessageLoopProxy::current(), io_thread_.message_loop_proxy(),
         static_cast<MessageLoopForIO*>(file_loop));
   }
   // Synchronously gets the proxy config.
