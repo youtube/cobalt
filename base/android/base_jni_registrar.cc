60257dae2a533f057fe1a99d839167db90f2a363
tedchoc@chromium.org <tedchoc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-25 03:18:46 +0000

    Implement the skeleton of an android content shell.
    Many TODOs here that will allow a quicker distribution of work to get the
    remaining pieces upstreamed.
    
    BUG=118591
    TEST=
    
    
    Review URL: http://codereview.chromium.org/10035034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133857 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ base/android/base_jni_registrar.cc
@@ -0,0 +1,32 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/android/base_jni_registrar.h"
+
+#include "base/basictypes.h"
+#include "base/android/build_info.h"
+#include "base/android/jni_android.h"
+#include "base/android/jni_registrar.h"
+#include "base/android/path_utils.h"
+
+namespace base {
+bool RegisterSystemMessageHandler(JNIEnv* env);
+}
+
+namespace base {
+namespace android {
+
+static RegistrationMethod kBaseRegisteredMethods[] = {
+  { "BuildInfo", base::android::RegisterBuildInfo },
+  { "PathUtils", base::android::RegisterPathUtils },
+  { "SystemMessageHandler", base::RegisterSystemMessageHandler },
+};
+
+bool RegisterJni(JNIEnv* env) {
+  return RegisterNativeMethods(env, kBaseRegisteredMethods,
+                               arraysize(kBaseRegisteredMethods));
+}
+
+}  // namespace android
+}  // namespace base
