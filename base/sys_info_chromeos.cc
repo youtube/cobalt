60da186d1b7dc8c704a20f16bf0f4a5782684e23
khorimoto@chromium.org <khorimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-25 01:59:44 +0000

    Implemented a "Last Updated" field for the about page of Chrome OS.
    BUG=117517
    TEST=
    
    
    Review URL: http://codereview.chromium.org/10038034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133840 0039d316-1c4b-4281-b951-d872f2087c98

--- base/sys_info_chromeos.cc
+++ base/sys_info_chromeos.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -111,6 +111,11 @@ void SysInfo::ParseLsbRelease(const std::string& lsb_release,
                   bugfix_version);
     }
   }
+}
+
+// static
+FilePath SysInfo::GetLsbReleaseFilePath() {
+  return FilePath(kLinuxStandardBaseReleaseFile);
 }
 
 }  // namespace base
