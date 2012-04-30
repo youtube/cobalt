7f0f1e3fe434b80bf87ae25d8cad45517910ad11
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-30 20:25:19 +0000

    net: don't set NSS options in a callback.
    This debugger traps in debug mode due to the locks held at the time that the callback is made.
    
    BUG=125299
    TEST=none
    
    Review URL: http://codereview.chromium.org/10221018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134584 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_nss.cc
+++ net/socket/ssl_client_socket_nss.cc
@@ -2105,16 +2105,6 @@ SECStatus SSLClientSocketNSS::OwnAuthCertHandler(void* arg,
                                                  PRFileDesc* socket,
                                                  PRBool checksig,
                                                  PRBool is_server) {
-#ifdef SSL_ENABLE_FALSE_START
-  PRBool npn;
-  SECStatus rv =
-      SSL_HandshakeNegotiatedExtension(socket, ssl_next_proto_nego_xtn, &npn);
-  if (rv != SECSuccess || !npn) {
-    // If the server doesn't support NPN, then we don't do False Start with it.
-    SSL_OptionSet(socket, SSL_ENABLE_FALSE_START, PR_FALSE);
-  }
-#endif
-
   // Tell NSS to not verify the certificate.
   return SECSuccess;
 }
