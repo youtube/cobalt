2ebb1eea9dd149dd0685d5b6826e7dfdeff5ff71
gspencer@chromium.org <gspencer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-05 19:41:04 +0000

    Fix file access on Chrome for ChromeOS on Linux so that we can open files in the user's Downloads directory.
    Shouldn't affect actual ChromeOS or other platforms.
    
    BUG=chromium-os:29447
    TEST=Ran on Linux, opened files from Downloads folder.
    
    
    Review URL: http://codereview.chromium.org/10068021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135553 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_context_builder.cc
+++ net/url_request/url_request_context_builder.cc
@@ -84,14 +84,19 @@ class BasicNetworkDelegate : public NetworkDelegate {
     return NetworkDelegate::AUTH_REQUIRED_RESPONSE_NO_ACTION;
   }
 
-  virtual bool CanGetCookies(const URLRequest* request,
-                             const CookieList& cookie_list) OVERRIDE {
+  virtual bool OnCanGetCookies(const URLRequest& request,
+                               const CookieList& cookie_list) OVERRIDE {
     return true;
   }
 
-  virtual bool CanSetCookie(const URLRequest* request,
-                            const std::string& cookie_line,
-                            CookieOptions* options) OVERRIDE {
+  virtual bool OnCanSetCookie(const URLRequest& request,
+                              const std::string& cookie_line,
+                              CookieOptions* options) OVERRIDE {
+    return true;
+  }
+
+  virtual bool OnCanAccessFile(const net::URLRequest& request,
+                               const FilePath& path) const OVERRIDE {
     return true;
   }
 
