4bf46b1887baf4912e2291150f72e391783a51cf
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-09 20:16:14 +0000

    Add base::ThreadTaskRunnerHandler class.
    The class will be used to hold a reference to the thread's task runner.
    
    Review URL: https://chromiumcodereview.appspot.com/10380016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136110 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_loop.h
+++ base/message_loop.h
@@ -42,7 +42,8 @@
 
 namespace base {
 class Histogram;
-}
+class ThreadTaskRunnerHandle;
+}  // namespace base
 
 // A MessageLoop is used to process events for a particular thread.  There is
 // at most one MessageLoop instance per thread.
@@ -519,6 +520,7 @@ class BASE_EXPORT MessageLoop : public base::MessagePump::Delegate {
 
   // The message loop proxy associated with this message loop, if one exists.
   scoped_refptr<base::MessageLoopProxy> message_loop_proxy_;
+  scoped_ptr<base::ThreadTaskRunnerHandle> thread_task_runner_handle_;
 
  private:
   template <class T, class R> friend class base::subtle::DeleteHelperInternal;
