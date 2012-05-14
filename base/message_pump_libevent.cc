52405bd2404ec28455e59f9de4fdd01cad3d2e26
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-14 16:49:20 +0000

    Explicitly #include <unistd.h> to avoid a compile error reported in https://bugs.gentoo.org/show_bug.cgi?id=415601
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10399002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136884 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_pump_libevent.cc
+++ base/message_pump_libevent.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,6 +6,7 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 #include "base/auto_reset.h"
 #include "base/compiler_specific.h"
