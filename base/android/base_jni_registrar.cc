35d072362f136d96e5ae5ed761b5fe640c8f7cf4
nileshagrawal@chromium.org <nileshagrawal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-16 03:46:39 +0000

    Run APK tests on the android_test trybot.
    Test failures are ignored for now.
    
    TBR=mark@chromium.org,tsepez@chromium.org
    BUG=125059
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10387086
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137352 0039d316-1c4b-4281-b951-d872f2087c98

--- base/android/base_jni_registrar.cc
+++ base/android/base_jni_registrar.cc
@@ -6,6 +6,7 @@
 
 #include "base/basictypes.h"
 #include "base/android/build_info.h"
+#include "base/android/locale_utils.h"
 #include "base/android/jni_android.h"
 #include "base/android/jni_registrar.h"
 #include "base/android/path_utils.h"
@@ -19,6 +20,7 @@ namespace android {
 
 static RegistrationMethod kBaseRegisteredMethods[] = {
   { "BuildInfo", base::android::RegisterBuildInfo },
+  { "LocaleUtils", base::android::RegisterLocaleUtils },
   { "PathUtils", base::android::RegisterPathUtils },
   { "SystemMessageHandler", base::RegisterSystemMessageHandler },
 };
