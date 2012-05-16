3f905ea048a0956b3a3f2ce98f696cd9bc443e74
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-16 07:12:06 +0000

    Cleanup: Remove unneeded scoped_ptr.h includes from ash, base, media, and ui.
    BUG=none
    TEST=none
    TBR=ben,brettw,hclam
    
    Review URL: https://chromiumcodereview.appspot.com/10391121
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137375 0039d316-1c4b-4281-b951-d872f2087c98

--- base/win/iunknown_impl.h
+++ base/win/iunknown_impl.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,7 +11,6 @@
 #include "base/atomic_ref_count.h"
 #include "base/base_export.h"
 #include "base/compiler_specific.h"
-#include "base/memory/scoped_ptr.h"
 
 namespace base {
 namespace win {
