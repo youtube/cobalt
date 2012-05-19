4f77f1e4b237d609e6d2cd464f8d02af3a5945df
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-19 02:50:03 +0000

    Revert 138014 - Base: Don't overwrite the thread id when ThreadMain exits. There's no need to do that, and it makes debugging easier to have the value.
    BUG=127931
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/10388202
    
    TBR=rvargas@google.com
    Review URL: https://chromiumcodereview.appspot.com/10383256
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138018 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/thread.cc
+++ base/threading/thread.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -171,6 +171,7 @@ void Thread::ThreadMain() {
     // We can't receive messages anymore.
     message_loop_ = NULL;
   }
+  thread_id_ = kInvalidThreadId;
 }
 
 }  // namespace base
