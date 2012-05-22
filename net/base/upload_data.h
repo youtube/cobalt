c4e9e9034c766f97d06a1ac8f85f38d7cf57af48
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-22 01:11:41 +0000

    net: Fix a regression that broke file uploading with http authentication
    This is a reland of r138168, which was reverted speculatively, but identified unrelated.
    
    When uploading a file with http authentication, UploadData is reused
    for a new UploadDataStream. Hence, we should close the files in UploadData
    if already opened and read, so we can reread the files from the beginning.
    
    The regression was caused by r123677.  Special thanks to thomas.themel for
    identifying the offending patch and helping us to fix the issue.
    
    BUG=128574
    TEST=added a unit test; manually confirmed that uploading a file with basic authentication, and multi-round authentication with NTLM and Negotiate worked.
    
    Review URL: https://chromiumcodereview.appspot.com/10408042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138191 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/upload_data.h
+++ net/base/upload_data.h
@@ -129,8 +129,9 @@ class NET_EXPORT UploadData
     // Returns the number of bytes remaining to read.
     uint64 BytesRemaining();
 
-    // Resets the offset to zero, so that the element can be reread.
-    void ResetOffset() { offset_ = 0; }
+    // Resets the offset to zero and closes the file stream if opened, so
+    // that the element can be reread.
+    void ResetOffset();
 
    private:
     // Returns a FileStream opened for reading for this element, positioned
