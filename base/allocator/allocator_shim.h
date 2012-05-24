ca69e0bc37d813eff4c7a479305127c511d51726
kaiwang@chromium.org <kaiwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-24 11:13:23 +0000

    1. Enable large object pointer offset check in release build. Following code will now cause a check error: char* p = reinterpret_cast<char*>(malloc(kMaxSize + 1)); free(p + 1);
    2. Remove a duplicated error reporting function "DieFromBadFreePointer", can
    use "InvalidGetAllocatedSize".
    
    Review URL: https://chromiumcodereview.appspot.com/10391178
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138775 0039d316-1c4b-4281-b951-d872f2087c98

--- base/allocator/allocator_shim.h
+++ base/allocator/allocator_shim.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -13,6 +13,11 @@ namespace allocator {
 // CHROME_ALLOCATOR_2 (if specified), then CHROME_ALLOCATOR (if specified), and
 // then a default value (typically set to TCMALLOC).
 void SetupSubprocessAllocator();
+
+// Expose some of tcmalloc functions for test.
+void* TCMallocDoMallocForTest(size_t size);
+void TCMallocDoFreeForTest(void* ptr);
+size_t ExcludeSpaceForMarkForTest(size_t size);
 
 }  // namespace allocator.
 }  // namespace base.
