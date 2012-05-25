b871972f04e3c6e5256364bf166456db43eb4781
dgreid@chromium.org <dgreid@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-25 00:16:46 +0000

    alsa_output: Drain and pause pcm when stopping.
    Call snd_pcm_drain when the output's Stop() function is called.  This
    will keep some alsa interfaces from repeating the last samples played
    until Close() is called.  This will also guarantee that all samples are
    played back if Close() is called immediately following Stop().
    
    BUG=chromium-os:31211
    TEST=Manual, play and pause youtube, both FLASH and HTML5 videos using
    the following alsa plugins as a sink for alsa_output:  plug, hw, dmix,
    pulse, cras, jack.  Observe that pauses are clean without repeated
    samples.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10413075
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138939 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/linux/alsa_wrapper.h
+++ media/audio/linux/alsa_wrapper.h
@@ -27,6 +27,7 @@ class MEDIA_EXPORT AlsaWrapper {
                       snd_pcm_stream_t stream, int mode);
   virtual int PcmClose(snd_pcm_t* handle);
   virtual int PcmPrepare(snd_pcm_t* handle);
+  virtual int PcmDrain(snd_pcm_t* handle);
   virtual int PcmDrop(snd_pcm_t* handle);
   virtual int PcmDelay(snd_pcm_t* handle, snd_pcm_sframes_t* delay);
   virtual snd_pcm_sframes_t PcmWritei(snd_pcm_t* handle,
