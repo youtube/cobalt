337293639df316d1b7c33c08cca251909c75a64b
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-02 00:06:27 +0000

    media: No need to tag enum types with MEDIA_EXPORT in header files.
    R=fischman@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10454098
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140131 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/video_decoder_config.h
+++ media/base/video_decoder_config.h
@@ -14,7 +14,7 @@
 
 namespace media {
 
-enum MEDIA_EXPORT VideoCodec {
+enum VideoCodec {
   // These values are histogrammed over time; do not change their ordinal
   // values.  When deleting a codec replace it with a dummy value; when adding a
   // codec, do so at the bottom (and update kVideoCodecMax).
@@ -35,7 +35,7 @@ enum MEDIA_EXPORT VideoCodec {
 
 // Video stream profile.  This *must* match PP_VideoDecoder_Profile.
 // (enforced in webkit/plugins/ppapi/ppb_video_decoder_impl.cc)
-enum MEDIA_EXPORT VideoCodecProfile {
+enum VideoCodecProfile {
   // Keep the values in this enum unique, as they imply format (h.264 vs. VP8,
   // for example), and keep the values for a particular format grouped
   // together for clarity.
