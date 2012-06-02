cdd967f36f53f38878ec3ecb8a5f0b0456af1bb1
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-02 01:31:11 +0000

    Implement media::VideoDecodeAccelerator on Mac
    This CL implements media::VideoDecodeAccelerator for Mac. This will be used to implement hardware video decoding for Pepper Flash.
    
    Currently a couple things are still missing:
      - the new VideoDecodeAcceleratorMac class needs to be hooked up to GpuVideoDecodeAccelerator
      - the PPB_VideoDecoder_Dev::Create() API needs to be extended so clients can specify the extra AVC data.
    
    BUG=127414
    TEST=Ran on Mac and Windows. Sending ARM try bots now.
    
    Review URL: https://chromiumcodereview.appspot.com/10388108
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140153 0039d316-1c4b-4281-b951-d872f2087c98

--- base/mac/scoped_cftyperef.h
+++ base/mac/scoped_cftyperef.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,6 +10,7 @@
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "base/memory/scoped_policy.h"
 
 namespace base {
 namespace mac {
@@ -19,18 +20,32 @@ namespace mac {
 // CFTypeRef.  Style deviations here are solely for compatibility with
 // scoped_ptr<>'s interface, with which everyone is already familiar.
 //
-// When ScopedCFTypeRef<> takes ownership of an object (in the constructor or
-// in reset()), it takes over the caller's existing ownership claim.  The
-// caller must own the object it gives to ScopedCFTypeRef<>, and relinquishes
-// an ownership claim to that object.  ScopedCFTypeRef<> does not call
-// CFRetain().
+// By default, ScopedCFTypeRef<> takes ownership of an object (in the
+// constructor or in reset()) by taking over the caller's existing ownership
+// claim.  The caller must own the object it gives to ScopedCFTypeRef<>, and
+// relinquishes an ownership claim to that object.  ScopedCFTypeRef<> does not
+// call CFRetain(). This behavior is parameterized by the |OwnershipPolicy|
+// enum. If the value |RETAIN| is passed (in the constructor or in reset()),
+// then ScopedCFTypeRef<> will call CFRetain() on the object, and the initial
+// ownership is not changed.
+
 template<typename CFT>
 class ScopedCFTypeRef {
  public:
   typedef CFT element_type;
 
-  explicit ScopedCFTypeRef(CFT object = NULL)
+  explicit ScopedCFTypeRef(
+      CFT object = NULL,
+      base::scoped_policy::OwnershipPolicy policy = base::scoped_policy::ASSUME)
       : object_(object) {
+    if (object_ && policy == base::scoped_policy::RETAIN)
+      CFRetain(object_);
+  }
+
+  ScopedCFTypeRef(const ScopedCFTypeRef<CFT>& that)
+      : object_(that.object_) {
+    if (object_)
+      CFRetain(object_);
   }
 
   ~ScopedCFTypeRef() {
@@ -38,7 +53,16 @@ class ScopedCFTypeRef {
       CFRelease(object_);
   }
 
-  void reset(CFT object = NULL) {
+  ScopedCFTypeRef& operator=(const ScopedCFTypeRef<CFT>& that) {
+    reset(that.get(), base::scoped_policy::RETAIN);
+    return *this;
+  }
+
+  void reset(CFT object = NULL,
+             base::scoped_policy::OwnershipPolicy policy =
+                base::scoped_policy::ASSUME) {
+    if (object && policy == base::scoped_policy::RETAIN)
+      CFRetain(object);
     if (object_)
       CFRelease(object_);
     object_ = object;
@@ -77,8 +101,6 @@ class ScopedCFTypeRef {
 
  private:
   CFT object_;
-
-  DISALLOW_COPY_AND_ASSIGN(ScopedCFTypeRef);
 };
 
 }  // namespace mac
