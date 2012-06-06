6af07770f98390b354a5ae768760cf4abc69f820
pliard@chromium.org <pliard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-06 12:57:44 +0000

    Upstream Android proxy config service.
    This change adds in an Android-specific system proxy provider called
    ProxyConfigServiceAndroid. This is created as required from C++. In
    turn ProxyConfigServiceAndroid creates a java object which provides
    the link to Android's proxy changed broadcast, as well as the
    functionality to read the Java system properties where the current
    proxy settings are located.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10206014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140748 0039d316-1c4b-4281-b951-d872f2087c98

--- net/android/net_jni_registrar.cc
+++ net/android/net_jni_registrar.cc
@@ -7,8 +7,9 @@
 #include "base/basictypes.h"
 #include "base/android/jni_android.h"
 #include "base/android/jni_registrar.h"
-#include "net/android/network_library.h"
 #include "net/android/network_change_notifier_android.h"
+#include "net/android/network_library.h"
+#include "net/proxy/proxy_config_service_android.h"
 
 namespace net {
 namespace android {
@@ -16,6 +17,7 @@ namespace android {
 static base::android::RegistrationMethod kNetRegisteredMethods[] = {
   { "AndroidNetworkLibrary", net::android::RegisterNetworkLibrary },
   { "NetworkChangeNotifier", net::android::NetworkChangeNotifier::Register },
+  { "ProxyConfigService",  net::ProxyConfigServiceAndroid::Register },
 };
 
 bool RegisterJni(JNIEnv* env) {
