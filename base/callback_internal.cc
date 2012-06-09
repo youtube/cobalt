fabb2c099a4cf631821026ea3e28083e5378d6f2
bryeung@chromium.org <bryeung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-09 02:11:01 +0000

    Fix comment typo.
    TEST=none
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10541081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141342 0039d316-1c4b-4281-b951-d872f2087c98

--- base/callback_internal.cc
+++ base/callback_internal.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -32,5 +32,5 @@ CallbackBase::CallbackBase(BindStateBase* bind_state)
 CallbackBase::~CallbackBase() {
 }
 
-}  // namespace base
 }  // namespace internal
+}  // namespace base
