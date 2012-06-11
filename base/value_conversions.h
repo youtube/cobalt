c74bb4d004a1de4f80d0dbc7c644dd479fdf76b7
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-11 23:49:55 +0000

    Revert r141537: Add extensions::StateStore and use that instead of ExtensionPrefs for persisting alarm state to disk.
    More to come:
    - context menus
    - omnibox
    - declarative webRequest
    
    TBR=yoz@chromium.org
    BUG=123366
    TEST=extension alarms are still remembered after chrome restart
    
    Reason for revert: breaks tests on Win7.
    
    Review URL: https://chromiumcodereview.appspot.com/10542114
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141564 0039d316-1c4b-4281-b951-d872f2087c98

--- base/value_conversions.h
+++ base/value_conversions.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,7 +6,7 @@
 #define BASE_VALUE_CONVERSIONS_H_
 #pragma once
 
-// This file contains methods to convert things to a |Value| and back.
+// This file contains methods to convert a |FilePath| to a |Value| and back.
 
 #include "base/base_export.h"
 
@@ -14,16 +14,12 @@ class FilePath;
 
 namespace base {
 
-class Time;
 class StringValue;
 class Value;
 
 // The caller takes ownership of the returned value.
 BASE_EXPORT StringValue* CreateFilePathValue(const FilePath& in_value);
 BASE_EXPORT bool GetValueAsFilePath(const Value& value, FilePath* file_path);
-
-BASE_EXPORT StringValue* CreateTimeValue(const Time& time);
-BASE_EXPORT bool GetValueAsTime(const Value& value, Time* time);
 
 }  // namespace
 
