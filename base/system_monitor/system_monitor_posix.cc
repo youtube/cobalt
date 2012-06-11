c07497993748dea2f9c2036b47ce002d82b5579d
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-11 21:10:27 +0000

    Revert 138011 - Base: Add power requirements to the System monitor.
    The only thing we need in the short term is CPU_REQUIRED,
    and that part is implemented for all platforms (as far as it
    makes sense). The other options are implemented for Windows,
    but given that currently there's no user, I expect the
    implementation for other platforms to be written when there's
    an actual need for it.
    
    BUG=126591
    TEST=base_unittests
    
    TBR=brettw
    Review URL: https://chromiumcodereview.appspot.com/10546101
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141497 0039d316-1c4b-4281-b951-d872f2087c98

--- base/system_monitor/system_monitor_posix.cc
+++ base/system_monitor/system_monitor_posix.cc
@@ -1,26 +1,10 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "base/system_monitor/system_monitor.h"
 
 namespace base {
-
-void SystemMonitor::BeginPowerRequirement(PowerRequirement requirement,
-                                          const std::string& reason) {
-  if (requirement == CPU_REQUIRED)
-    return;
-
-  NOTIMPLEMENTED();
-}
-
-void SystemMonitor::EndPowerRequirement(PowerRequirement requirement,
-                                        const std::string& reason) {
-  if (requirement == CPU_REQUIRED)
-    return;  // Nothing to do.
-
-  NOTIMPLEMENTED();
-}
 
 bool SystemMonitor::IsBatteryPower() {
   NOTIMPLEMENTED();
