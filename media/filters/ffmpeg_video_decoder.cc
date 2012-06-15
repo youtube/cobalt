42dc76b278de0688e0303cd52222d543ca85f18c
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-15 00:16:08 +0000

    Revert 142268 - Implement ISO BMFF support in Media Source.
    BUG=129072
    TEST=MP4StreamParserTest
    
    
    Review URL: https://chromiumcodereview.appspot.com/10536014
    
    TBR=strobe@google.com
    Review URL: https://chromiumcodereview.appspot.com/10533164
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142283 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/ffmpeg_video_decoder.cc
+++ media/filters/ffmpeg_video_decoder.cc
@@ -18,7 +18,6 @@
 #include "media/base/video_frame.h"
 #include "media/base/video_util.h"
 #include "media/ffmpeg/ffmpeg_common.h"
-#include "media/filters/ffmpeg_glue.h"
 
 namespace media {
 
@@ -65,9 +64,6 @@ FFmpegVideoDecoder::FFmpegVideoDecoder(
 void FFmpegVideoDecoder::Initialize(const scoped_refptr<DemuxerStream>& stream,
                                     const PipelineStatusCB& status_cb,
                                     const StatisticsCB& statistics_cb) {
-  // Ensure FFmpeg has been initialized
-  FFmpegGlue::GetInstance();
-
   if (!message_loop_) {
     message_loop_ = message_loop_factory_cb_.Run();
     message_loop_factory_cb_.Reset();
