a2aa256664ce3581c6d3b86e12070e62329a9898
siggi@chromium.org <siggi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-18 20:40:06 +0000

    Implement base::win::IsMetroProcess. Change GetMetroModule callers that don't need the HMODULE.
    TBR=stevenjb@chromium.org,brettw@chromium.org,mirandac@chromium.org,sky@chromium.org,kalman@chromium.org
    
    BUG=None
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/10560015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142801 0039d316-1c4b-4281-b951-d872f2087c98

--- base/win/metro.h
+++ base/win/metro.h
@@ -51,6 +51,10 @@ BASE_EXPORT extern const char kActivateApplication[];
 // indicates that the metro dll was not loaded in the process.
 BASE_EXPORT HMODULE GetMetroModule();
 
+// Returns true if this process is running as an immersive program
+// in Windows Metro mode.
+BASE_EXPORT bool IsMetroProcess();
+
 // Allocates and returns the destination string via the LocalAlloc API after
 // copying the src to it.
 BASE_EXPORT wchar_t* LocalAllocAndCopyString(const string16& src);
