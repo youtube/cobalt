0ca3255893a7e809c4d6a5f2cba47db0d3744e40
siggi@chromium.org <siggi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-18 20:40:06 +0000

    Implement base::win::IsMetroProcess. Change GetMetroModule callers that don't need the HMODULE.
    TBR=stevenjb@chromium.org,brettw@chromium.org,mirandac@chromium.org,sky@chromium.org,kalman@chromium.org
    
    BUG=None
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/10560015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142801 0039d316-1c4b-4281-b951-d872f2087c98

--- base/win/metro.cc
+++ base/win/metro.cc
@@ -12,7 +12,62 @@ namespace win {
 const char kActivateApplication[] = "ActivateApplication";
 
 HMODULE GetMetroModule() {
-  return GetModuleHandleA("metro_driver.dll");
+  const HMODULE kUninitialized = reinterpret_cast<HMODULE>(1);
+  static HMODULE metro_module = kUninitialized;
+
+  if (metro_module == kUninitialized) {
+    // Initialize the cache, note that the initialization is idempotent
+    // under the assumption that metro_driver is never unloaded, so the
+    // race to this assignment is safe.
+    metro_module = GetModuleHandleA("metro_driver.dll");
+    if (metro_module != NULL) {
+      // This must be a metro process if the metro_driver is loaded.
+      DCHECK(IsMetroProcess());
+    }
+  }
+
+  DCHECK(metro_module != kUninitialized);
+  return metro_module;
+}
+
+bool IsMetroProcess() {
+  enum ImmersiveState {
+    kImmersiveUnknown,
+    kImmersiveTrue,
+    kImmersiveFalse
+  };
+  typedef BOOL (WINAPI* IsImmersiveProcessFunc)(HANDLE process);
+
+  // The immersive state of a process can never change.
+  // Look it up once and cache it here.
+  static ImmersiveState state = kImmersiveUnknown;
+
+  if (state == kImmersiveUnknown) {
+    // The lookup hasn't been done yet. Note that the code below here is
+    // idempotent, so it doesn't matter if it races to assignment on multiple
+    // threads.
+    HMODULE user32 = ::GetModuleHandleA("user32.dll");
+    DCHECK(user32 != NULL);
+
+    IsImmersiveProcessFunc is_immersive_process =
+        reinterpret_cast<IsImmersiveProcessFunc>(
+            ::GetProcAddress(user32, "IsImmersiveProcess"));
+
+    if (is_immersive_process != NULL) {
+      if (is_immersive_process(::GetCurrentProcess())) {
+        state = kImmersiveTrue;
+      } else {
+        state = kImmersiveFalse;
+      }
+    } else {
+      // No "IsImmersiveProcess" export on user32.dll, so this is pre-Windows8
+      // and therefore not immersive.
+      state = kImmersiveFalse;
+    }
+  }
+  DCHECK_NE(kImmersiveUnknown, state);
+
+  return state == kImmersiveTrue;
 }
 
 wchar_t* LocalAllocAndCopyString(const string16& src) {
