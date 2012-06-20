19952c026b8732acfa03771f2afd82bbc66421cb
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-20 05:31:12 +0000

    [net] Implement MappedHostResolver::ResolveFromCache.
    BUG=133608
    TEST=./net_unittests --gtest_filter=MappedHostResolverTest.*
    
    
    Review URL: https://chromiumcodereview.appspot.com/10581032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143148 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/mapped_host_resolver.h
+++ net/base/mapped_host_resolver.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -57,6 +57,9 @@ class NET_EXPORT MappedHostResolver : public HostResolver {
   virtual HostCache* GetHostCache() OVERRIDE;
 
  private:
+  // Modify the request |info| according to |rules_|.
+  RequestInfo ApplyRules(const RequestInfo& info) const;
+
   scoped_ptr<HostResolver> impl_;
 
   HostMappingRules rules_;
