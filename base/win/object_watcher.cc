0051c029c4f5931eb08f0dc01b7a3da2001f6067
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-21 22:13:04 +0000

    Revert 143251 - Log stack trace when ObjectWatcher::StartWatching() is called.
    Something isn't being cleaned up properly and we're seeing crashes where we call
    into a destroyed class.
    
    BUG=121086
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10535041
    
    TBR=dcheng@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10633015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143468 0039d316-1c4b-4281-b951-d872f2087c98

--- base/win/object_watcher.cc
+++ base/win/object_watcher.cc
@@ -5,7 +5,6 @@
 #include "base/win/object_watcher.h"
 
 #include "base/bind.h"
-#include "base/debug/alias.h"
 #include "base/logging.h"
 
 namespace base {
@@ -41,7 +40,6 @@ bool ObjectWatcher::StartWatching(HANDLE object, Delegate* delegate) {
                          delegate);
   object_ = object;
   origin_loop_ = MessageLoop::current();
-  stack_trace_ = base::debug::StackTrace();
 
   if (!RegisterWaitForSingleObject(&wait_object_, object, DoneWaiting,
                                    this, INFINITE, wait_flags)) {
@@ -99,10 +97,6 @@ void ObjectWatcher::Signal(Delegate* delegate) {
   // StartWatching(). As a result, we save any state we need and clear previous
   // watcher state before signaling the delegate.
   HANDLE object = object_;
-  // Alias the stack trace where the watch was started so it'll be available in
-  // minidumps.
-  base::debug::StackTrace stack_trace = stack_trace_;
-  base::debug::Alias(&stack_trace);
   StopWatching();
   delegate->OnObjectSignaled(object);
 }
