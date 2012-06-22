8ec22e7436d3bd3c1870f1731ee88cb0ce43d5b1
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-22 19:32:37 +0000

    Remove unused member variables found by clang's -Wunused-private-field
    BUG=none
    TEST=none
    TBR=sergeyu
    
    
    Review URL: https://chromiumcodereview.appspot.com/10647002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143676 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/leak_tracker.h
+++ base/debug/leak_tracker.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -53,6 +53,7 @@ namespace debug {
 template<typename T>
 class LeakTracker {
  public:
+  ~LeakTracker() {}
   static void CheckForLeaks() {}
   static int NumLiveInstances() { return -1; }
 };
