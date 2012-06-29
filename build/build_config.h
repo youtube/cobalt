b5bb97b5cd3d06d7a2166a714b0528ed7aabd6d0
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-29 22:12:20 +0000

    Add iOS support to common.gypi
    There are basically three categories of changes here:
    1) Most OS==mac becomes OS==mac or OS==ios
    2) Some Xcode-related settings move into OS==mac blocks since Xcode is no longer Mac-only
    3) Addition of iOS-specific settings (SDK, compile flags, etc)
    
    BUG=None
    TEST=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10704039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145008 0039d316-1c4b-4281-b951-d872f2087c98

--- build/build_config.h
+++ build/build_config.h
@@ -14,9 +14,16 @@
 #ifndef BUILD_BUILD_CONFIG_H_
 #define BUILD_BUILD_CONFIG_H_
 
+#if defined(__APPLE__)
+#include <TargetConditionals.h>
+#endif
+
 // A set of macros to use for platform detection.
 #if defined(__APPLE__)
 #define OS_MACOSX 1
+#if defined(TARGET_OS_IPHONE)
+#define OS_IOS 1
+#endif  // defined(TARGET_OS_IPHONE)
 #elif defined(ANDROID)
 #define OS_ANDROID 1
 #elif defined(__native_client__)
@@ -99,7 +106,6 @@
 #define ARCH_CPU_ARMEL 1
 #define ARCH_CPU_32_BITS 1
 #define ARCH_CPU_LITTLE_ENDIAN 1
-#define WCHAR_T_IS_UNSIGNED 1
 #elif defined(__pnacl__)
 #define ARCH_CPU_32_BITS 1
 #elif defined(__MIPSEL__)
@@ -107,7 +113,6 @@
 #define ARCH_CPU_MIPSEL 1
 #define ARCH_CPU_32_BITS 1
 #define ARCH_CPU_LITTLE_ENDIAN 1
-#define WCHAR_T_IS_UNSIGNED 0
 #else
 #error Please add support for your architecture in build/build_config.h
 #endif
@@ -129,6 +134,12 @@
 #define WCHAR_T_IS_UTF16
 #else
 #error Please add support for your compiler in build/build_config.h
+#endif
+
+#if defined(__ARMEL__) && !defined(OS_IOS)
+#define WCHAR_T_IS_UNSIGNED 1
+#elif defined(__MIPSEL__)
+#define WCHAR_T_IS_UNSIGNED 0
 #endif
 
 #if defined(OS_CHROMEOS)
