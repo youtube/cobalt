0b2d50b1122ecc2b3931df26e622cd9a681e6c3a
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-11 07:04:07 +0000

    Remove #pragma once from content
    Review URL: https://chromiumcodereview.appspot.com/10696166
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146071 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/content_export.h
+++ content/common/content_export.h
@@ -1,10 +1,9 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #ifndef CONTENT_COMMON_CONTENT_EXPORT_H_
 #define CONTENT_COMMON_CONTENT_EXPORT_H_
-#pragma once
 
 #if defined(COMPONENT_BUILD) && !defined(COMPILE_CONTENT_STATICALLY)
 #if defined(WIN32)
