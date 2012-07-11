54aaf78b65ac3cee0eca5404d14e16f55aca40a6
james.wei@intel.com <james.wei@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-11 10:12:52 +0000

    fix the error of emulator.py
    AndroidCommand only needs two args while emulator.py pass three
    
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10700157
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146095 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/emulator.py
+++ build/android/emulator.py
@@ -196,7 +196,7 @@ class Emulator(object):
     After confirming a wait-for-device can be successful, make sure
     it returns the right answer.
     """
-    a = android_commands.AndroidCommands(self.device, False)
+    a = android_commands.AndroidCommands(self.device)
     seconds_waited = 0
     number_of_waits = 2  # Make sure we can wfd twice
     adb_cmd = "adb -s %s %s" % (self.device, 'wait-for-device')
