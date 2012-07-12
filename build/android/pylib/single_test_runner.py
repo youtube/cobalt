2a272d31ffb146dd855833d88078ed77e0f53725
james.wei@intel.com <james.wei@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-12 10:52:52 +0000

    as we will use sdcard to store test data, so add sd card when creating avd.
    And when testing with emulator, sometimes the test broken due to push failed because /sdcard/ is not ready. 
    
    Adding WaitForSdCardReady to make the test more reliable. 
    
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10702162
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146311 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/single_test_runner.py
+++ build/android/pylib/single_test_runner.py
@@ -248,6 +248,8 @@ class SingleTestRunner(BaseTestRunner):
     if test_data and not self.fast_and_loose:
       # Due to the large size of certain test data, we use sdcard to store the
       # test data and create symbolic links to map them to data/local/tmp/.
+      # Before that, make sure SD card is ready.
+      self.adb.WaitForSdCardReady(20)
       for data in test_data:
         self.CopyTestData([data], '/sdcard/')
       self.LinkSdCardPathsToTempDir(test_data)
