e7abe6a1e75984f2ad0b412a5ccebb527696d17a
pauljensen@chromium.org <pauljensen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-13 19:27:48 +0000

    Account for server vs host clock skew in cookie expiration times. When setting a cookie's expiration time in the cookie store we need to take into account any difference between the HTTP server and the host machine's real time clock.
    BUG=135131
    TEST=net_unittests --gtest_filter=CookieMonster/CookieStoreTest/0.TestCookieDeletion
    
    
    
    Review URL: https://chromiumcodereview.appspot.com/10692137
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146616 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_http_job.cc
+++ net/url_request/url_request_http_job.cc
@@ -493,6 +493,9 @@ void URLRequestHttpJob::SaveCookiesAndNotifyHeadersComplete(int result) {
 
   FetchResponseCookies(&response_cookies_);
 
+  if (!GetResponseHeaders()->GetDateValue(&response_date_))
+    response_date_ = base::Time();
+
   // Now, loop over the response cookies, and attempt to persist each.
   SaveNextCookie();
 }
@@ -519,6 +522,7 @@ void URLRequestHttpJob::SaveNextCookie() {
       response_cookies_.size() > 0) {
     CookieOptions options;
     options.set_include_httponly();
+    options.set_server_time(response_date_);
 
     net::CookieStore::SetCookiesCallback callback(
         base::Bind(&URLRequestHttpJob::OnCookieSaved,
