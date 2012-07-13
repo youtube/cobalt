7d34b12e82667c5d114b83e4c15210e367a5804a
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-13 18:55:22 +0000

    Modify sql unittests to pass on iOS.
    BUG=None
    TEST=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10702203
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146604 0039d316-1c4b-4281-b951-d872f2087c98

--- sql/sqlite_features_unittest.cc
+++ sql/sqlite_features_unittest.cc
@@ -83,13 +83,16 @@ TEST_F(SQLiteFeaturesTest, NoFTS1) {
       "CREATE VIRTUAL TABLE foo USING fts1(x)"));
 }
 
-// fts2 is used for older history files, so we're signed on for
-// keeping our version up-to-date.
+#if !defined(OS_IOS)
+// fts2 is used for older history files, so we're signed on for keeping our
+// version up-to-date.  iOS does not include fts2, so this test does not run on
+// iOS.
 // TODO(shess): Think up a crazy way to get out from having to support
 // this forever.
 TEST_F(SQLiteFeaturesTest, FTS2) {
   ASSERT_TRUE(db().Execute("CREATE VIRTUAL TABLE foo USING fts2(x)"));
 }
+#endif
 
 // fts3 is used for current history files, and also for WebDatabase.
 TEST_F(SQLiteFeaturesTest, FTS3) {
