2c72ff7d0f424a6c6eba717520db99bd6d86f26e
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-16 11:52:34 +0000

    Added broadcasting feature to UDP server sockets.
    BUG=136797
    TEST=net_unittests:UDPSocketTest.Broadcast
    
    Review URL: https://chromiumcodereview.appspot.com/10739002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146790 0039d316-1c4b-4281-b951-d872f2087c98

--- net/udp/udp_server_socket.cc
+++ net/udp/udp_server_socket.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -59,6 +59,14 @@ int UDPServerSocket::GetLocalAddress(IPEndPoint* address) const {
 
 const BoundNetLog& UDPServerSocket::NetLog() const {
   return socket_.NetLog();
+}
+
+void UDPServerSocket::AllowAddressReuse() {
+  socket_.AllowAddressReuse();
+}
+
+void UDPServerSocket::AllowBroadcast() {
+  socket_.AllowBroadcast();
 }
 
 }  // namespace net
