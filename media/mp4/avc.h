412537e9406b75ce593a598492cc4cf9b9f87ca4
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-17 00:08:40 +0000

    Revert 145769 (broke test stream, removed needed HasMoreRBSPData, added extra ops) - Add HE AAC support to ISO BMFF.
    Also abstract common code in H264BitReader into BitReader for reusing.
    
    Was: https://chromiumcodereview.appspot.com/10710002/
    
    In the submitted patch of the last issue, the media.gyp included a non-existing file. This error hadn't been caught by try but later got caught by the build process and reverted the commit. So I create this issue to submit the fixed patch.
    
    BUG=134445
    TEST=BitReaderTest, AACTest
    
    
    Review URL: https://chromiumcodereview.appspot.com/10753005
    
    TBR=xiaomings@google.com
    Review URL: https://chromiumcodereview.appspot.com/10779025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146927 0039d316-1c4b-4281-b951-d872f2087c98

--- media/mp4/avc.h
+++ media/mp4/avc.h
@@ -8,6 +8,7 @@
 #include <vector>
 
 #include "base/basictypes.h"
+#include "media/base/channel_layout.h"
 #include "media/base/media_export.h"
 
 namespace media {
@@ -22,7 +23,10 @@ class MEDIA_EXPORT AVC {
   static bool InsertParameterSets(
       const AVCDecoderConfigurationRecord& avc_config,
       std::vector<uint8>* buffer);
+
+  static ChannelLayout ConvertAACChannelCountToChannelLayout(int count);
 };
+
 
 }  // namespace mp4
 }  // namespace media
