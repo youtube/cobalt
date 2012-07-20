050a8b3d72a509ad56fb4d0c4b815b65ac83bf96
smckay@chromium.org <smckay@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-20 01:52:02 +0000

    Convert ProtocolHandlerRegistry to be a ProfileKeyedService.
    BUG=129200
    TEST=protocol_handler_registry_browsertest.cc,protocol_handler_registry_unittest.cc
    
    willchan@ -> profile_io changes
    +bauerb -> content_settings changes
    
    
    Review URL: https://chromiumcodereview.appspot.com/10546083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147597 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_loop.cc
+++ base/message_loop.cc
@@ -322,6 +322,10 @@ void MessageLoop::QuitNow() {
   }
 }
 
+bool MessageLoop::IsType(Type type) const {
+  return type_ == type;
+}
+
 static void QuitCurrentWhenIdle() {
   MessageLoop::current()->QuitWhenIdle();
 }
