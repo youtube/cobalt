4c3266134aa61cd535f0b8fa8f7276cdd6b0c03b
noyau@chromium.org <noyau@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-25 17:47:58 +0000

    Adding the possibility to build a closure from an ObjectiveC block.
    This lead to more readeable ObjectiveC code as the bound code is right next to where it is scheduled as opposed to be outside the class–usually in a separate function in an anonymous namespace.
    
    BUG=None
    TEST=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10818034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148357 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ base/mac/bind_objc_block.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_MAC_BIND_OBJC_BLOCK_H_
+#define BASE_MAC_BIND_OBJC_BLOCK_H_
+
+#include "base/callback_forward.h"
+
+namespace base {
+
+// Construct a closure from an objective-C block.
+base::Closure BindBlock(void(^block)());
+
+}  // namespace base
+
+#endif  // BASE_MAC_BIND_OBJC_BLOCK_H_
