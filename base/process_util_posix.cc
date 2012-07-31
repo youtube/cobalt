bb5285a33d1a07955277d6877d9d3a09fc338ff8
tedvessenes@gmail.com <tedvessenes@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-31 23:22:48 +0000

    Fix CleanupProcesses() interface to use TimeDelta.
    BUG=108171
    
    
    Review URL: https://chromiumcodereview.appspot.com/10821062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149312 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process_util_posix.cc
+++ base/process_util_posix.cc
@@ -1227,14 +1227,10 @@ bool WaitForProcessesToExit(const FilePath::StringType& executable_name,
 }
 
 bool CleanupProcesses(const FilePath::StringType& executable_name,
-                      int64 wait_milliseconds,
+                      base::TimeDelta wait,
                       int exit_code,
                       const ProcessFilter* filter) {
-  bool exited_cleanly =
-      WaitForProcessesToExit(
-          executable_name,
-          base::TimeDelta::FromMilliseconds(wait_milliseconds),
-          filter);
+  bool exited_cleanly = WaitForProcessesToExit(executable_name, wait, filter);
   if (!exited_cleanly)
     KillProcesses(executable_name, exit_code, filter);
   return exited_cleanly;
