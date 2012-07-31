cfdd4932b18fac0a813d3cfe22b2b41691b9f0c7
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-31 20:38:18 +0000

    Revert 149261 - Support SHA-256 in public key pins for HTTPS.
    Broke the compile on CrOS.  Looks like const-ness problem:
    net/socket/ssl_client_socket_nss.cc: In member function 'int net::SSLClientSocketNSS::DoVerifyCertComplete(int)':
    net/socket/ssl_client_socket_nss.cc:3458:error: no matching function for call to 'net::TransportSecurityState::DomainState::IsChainOfPublicKeysPermitted(std::vector<std::vector<net::HashValue, std::allocator<net::HashValue> >, std::allocator<std::vector<net::HashValue, std::allocator<net::HashValue> > > >&)'
    ./net/base/transport_security_state.h:94: note: candidates are: bool net::TransportSecurityState::DomainState::IsChainOfPublicKeysPermitted(const net::HashValueVector&) const
    
    
    The HTTP-based Public Key Pinning Internet Draft
    (tools.ietf.org/html/draft-ietf-websec-key-pinning) requires this.
    
    Per wtc, give the *Fingeprint* types more meaningful *HashValue* names.
    
    Cleaning up lint along the way.
    
    BUG=117914
    TEST=net_unittests, unit_tests TransportSecurityPersisterTest
    
    Review URL: https://chromiumcodereview.appspot.com/10545166
    
    TBR=palmer@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10827104
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149268 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/cert_verify_result.h
+++ net/base/cert_verify_result.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -45,15 +45,10 @@ class NET_EXPORT CertVerifyResult {
   bool has_md5_ca;
   bool has_md2_ca;
 
-  // If the certificate was successfully verified then this contains the
-  // fingerprints, in several hash algorithms, of the SubjectPublicKeyInfos
-  // of the chain. The fingerprint from the leaf certificate will be the
-  // first element of each sub-vector.
-  //
-  // This is a vector of vectors: Index the outer vector with
-  // FingerprintTag, and then the inner HashValueVectors will be
-  // fingerprints made with the algorithm named by the FingerprintTag.
-  std::vector<HashValueVector> public_key_hashes;
+  // If the certificate was successfully verified then this contains the SHA1
+  // fingerprints of the SubjectPublicKeyInfos of the chain. The fingerprint
+  // from the leaf certificate will be the first element of the vector.
+  std::vector<SHA1Fingerprint> public_key_hashes;
 
   // is_issued_by_known_root is true if we recognise the root CA as a standard
   // root.  If it isn't then it's probably the case that this certificate was
