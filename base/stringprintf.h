d00f82c246573aa27e3eaae6937a657c1fdc69ca
husky@chromium.org <husky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-01 10:30:02 +0000

    Remove wchar_t versions of StringPrintf from Android
    BUG=137864
    
    TEST=StringPrintfTest
    
    
    Review URL: https://chromiumcodereview.appspot.com/10800078
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149397 0039d316-1c4b-4281-b951-d872f2087c98

--- base/stringprintf.h
+++ base/stringprintf.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -17,8 +17,11 @@ namespace base {
 // Return a C++ string given printf-like input.
 BASE_EXPORT std::string StringPrintf(const char* format, ...)
     PRINTF_FORMAT(1, 2);
+// OS_ANDROID's libc does not support wchar_t, so several overloads are omitted.
+#if !defined(OS_ANDROID)
 BASE_EXPORT std::wstring StringPrintf(const wchar_t* format, ...)
     WPRINTF_FORMAT(1, 2);
+#endif
 
 // Return a C++ string given vprintf-like input.
 BASE_EXPORT std::string StringPrintV(const char* format, va_list ap)
@@ -28,25 +31,31 @@ BASE_EXPORT std::string StringPrintV(const char* format, va_list ap)
 BASE_EXPORT const std::string& SStringPrintf(std::string* dst,
                                              const char* format, ...)
     PRINTF_FORMAT(2, 3);
+#if !defined(OS_ANDROID)
 BASE_EXPORT const std::wstring& SStringPrintf(std::wstring* dst,
                                               const wchar_t* format, ...)
     WPRINTF_FORMAT(2, 3);
+#endif
 
 // Append result to a supplied string.
 BASE_EXPORT void StringAppendF(std::string* dst, const char* format, ...)
     PRINTF_FORMAT(2, 3);
+#if !defined(OS_ANDROID)
 // TODO(evanm): this is only used in a few places in the code;
 // replace with string16 version.
 BASE_EXPORT void StringAppendF(std::wstring* dst, const wchar_t* format, ...)
     WPRINTF_FORMAT(2, 3);
+#endif
 
 // Lower-level routine that takes a va_list and appends to a specified
 // string.  All other routines are just convenience wrappers around it.
 BASE_EXPORT void StringAppendV(std::string* dst, const char* format, va_list ap)
     PRINTF_FORMAT(2, 0);
+#if !defined(OS_ANDROID)
 BASE_EXPORT void StringAppendV(std::wstring* dst,
                                const wchar_t* format, va_list ap)
     WPRINTF_FORMAT(2, 0);
+#endif
 
 }  // namespace base
 
