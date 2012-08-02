5a665a07c97dcd5e3c73284372aa58b89ae85122
fgalligan@chromium.org <fgalligan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-02 20:57:39 +0000

    Add support for v0.3 of the encrypted WebM specification. (Revert Fix)
    - Fixed HmacCheckFailure from aes_decryptor_unittest.cc that was causing a
    failure on the ASAN bots.
    - Added code to handle the signal_byte contained within WebM
    encrypted Blocks.
    - Added a unittest to aes_decryptor to handle an encrypted WebM
    Block with an unencrypted frame.
    - Added two unittests to aes_decryptor to try an encrypted WebM
    frame as an unencrypted frame and an unencrypted WebM frame as an
    encrypted frame.
    
    Revert "Revert 149449 - Add support for v0.3 of the encrypted WebM specification."
    
    This reverts commit 672f667ed68e59ec0aaf5c40167e890673ef3f7c.
    
    BUG=139876
    TEST=Run media_unittests --gtest_filter=AesDecryptor* and all
    tests must pass. media_unittests must pass on linux_asan and mac_asan.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10824136
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149696 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/decrypt_config.cc
+++ media/base/decrypt_config.cc
@@ -19,7 +19,8 @@ DecryptConfig::DecryptConfig(const std::string& key_id,
       data_offset_(data_offset),
       subsamples_(subsamples) {
   CHECK_GT(key_id.size(), 0u);
-  CHECK_EQ(iv.size(), static_cast<size_t>(DecryptConfig::kDecryptionKeySize));
+  CHECK(iv.size() == static_cast<size_t>(DecryptConfig::kDecryptionKeySize) ||
+        iv.empty());
   CHECK_GE(data_offset, 0);
 }
 
