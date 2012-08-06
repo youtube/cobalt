20cccf7d5b8eb73e5ed1fa87163d0d3fabc751d0
dimich@chromium.org <dimich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-06 23:09:00 +0000

    Revert 150124 - Implement SHA-256 fingerprint support.
    The HTTP-based Public Key Pinning Internet Draft
    (tools.ietf.org/html/draft-ietf-websec-key-pinning) requires this.
    
    Per wtc, give the *Fingeprint* types more meaningful *HashValue* names.
    
    Cleaning up lint along the way.
    
    This CL reverts 149268, which reverted 149261 the previous version of this
    CL. It includes a fix to the compile problem that necessitated 149268.
    
    BUG=117914
    TEST=net_unittests, unit_tests TransportSecurityPersisterTest
    Review URL: https://chromiumcodereview.appspot.com/10836062
    
    TBR=palmer@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10836120
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150166 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/multi_threaded_cert_verifier_unittest.cc
+++ net/base/multi_threaded_cert_verifier_unittest.cc
@@ -243,10 +243,10 @@ TEST_F(MultiThreadedCertVerifierTest, CancelRequestThenQuit) {
 }
 
 TEST_F(MultiThreadedCertVerifierTest, RequestParamsComparators) {
-  SHA1HashValue a_key;
+  SHA1Fingerprint a_key;
   memset(a_key.data, 'a', sizeof(a_key.data));
 
-  SHA1HashValue z_key;
+  SHA1Fingerprint z_key;
   memset(z_key.data, 'z', sizeof(z_key.data));
 
   struct {
