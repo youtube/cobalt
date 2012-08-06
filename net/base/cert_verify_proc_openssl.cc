96d822deb744710c689a336de879886c8d6ac27b
dimich@chromium.org <dimich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-06 23:09:00 +0000

    Revert 150124 - Implement SHA-256 fingerprint support.
    The HTTP-based Public Key Pinning Internet Draft
    (tools.ietf.org/html/draft-ietf-websec-key-pinning) requires this.
    
    Per wtc, give the *Fingeprint* types more meaningful *HashValue* names.
    
    Cleaning up lint along the way.
    
    This CL reverts 149268, which reverted 149261 the previous version of this
    CL. It includes a fix to the compile problem that necessitated 149268.
    
    BUG=117914
    TEST=net_unittests, unit_tests TransportSecurityPersisterTest
    Review URL: https://chromiumcodereview.appspot.com/10836062
    
    TBR=palmer@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10836120
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150166 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/cert_verify_proc_openssl.cc
+++ net/base/cert_verify_proc_openssl.cc
@@ -6,13 +6,9 @@
 
 #include <openssl/x509v3.h>
 
-#include <string>
-#include <vector>
-
 #include "base/logging.h"
 #include "base/sha1.h"
 #include "crypto/openssl_util.h"
-#include "crypto/sha2.h"
 #include "net/base/asn1_util.h"
 #include "net/base/cert_status_flags.h"
 #include "net/base/cert_verify_result.h"
@@ -135,7 +131,7 @@ void GetCertChainInfo(X509_STORE_CTX* store_ctx,
 }
 
 void AppendPublicKeyHashes(X509_STORE_CTX* store_ctx,
-                           std::vector<HashValueVector>* hashes) {
+                           std::vector<SHA1Fingerprint>* hashes) {
   STACK_OF(X509)* chain = X509_STORE_CTX_get_chain(store_ctx);
   for (int i = 0; i < sk_X509_num(chain); ++i) {
     X509* cert = sk_X509_value(chain, i);
@@ -149,16 +145,10 @@ void AppendPublicKeyHashes(X509_STORE_CTX* store_ctx,
     if (!asn1::ExtractSPKIFromDERCert(der_bytes, &spki_bytes))
       continue;
 
-    HashValue sha1;
-    sha1.tag = HASH_VALUE_SHA1;
+    SHA1Fingerprint hash;
     base::SHA1HashBytes(reinterpret_cast<const uint8*>(spki_bytes.data()),
-                        spki_bytes.size(), sha1.data());
-    (*hashes)[HASH_VALUE_SHA1].push_back(sha1);
-
-    HashValue sha256;
-    sha256.tag = HASH_VALUE_SHA256;
-    crypto::SHA256HashString(spki_bytes, sha1.data(), crypto::kSHA256Length);
-    (*hashes)[HASH_VALUE_SHA256].push_back(sha256);
+                        spki_bytes.size(), hash.data);
+    hashes->push_back(hash);
   }
 }
 
