2a374e7efc88f257cbceb03582023a4f426df936
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 19:06:11 +0000

    Remove log message for 10.8 CFAllocator patching.
    BUG=117476
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10854027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150385 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process_util_mac.mm
+++ base/process_util_mac.mm
@@ -770,14 +770,6 @@ void oom_killer_new() {
 // === Core Foundation CFAllocators ===
 
 bool CanGetContextForCFAllocator() {
-  // TODO(avi): remove at final release; http://crbug.com/117476
-  if (base::mac::IsOSMountainLion()) {
-    NSLog(@"Unsure about the internals of CFAllocator but going to patch them "
-           "anyway. If there is a crash inside of CFAllocatorAllocate, please "
-           "report it at http://crbug.com/117476 . If there is a crash and it "
-           "is NOT inside of CFAllocatorAllocate, it is NOT RELATED. DO NOT "
-           "REPORT IT THERE but rather FILE A NEW BUG.");
-  }
   return !base::mac::
       IsOSDangerouslyLaterThanMountainLionForUseByCFAllocatorReplacement();
 }
