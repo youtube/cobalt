eaba8fff68c24759a24960cd169352ab0f7f09d4
palmer@chromium.org <palmer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-08 06:32:23 +0000

    Revert 150375 -  Implement SHA-256 fingerprint support
    The HTTP-based Public Key Pinning Internet Draft
    (tools.ietf.org/html/draft-ietf-websec-key-pinning) requires this.
    
    Per wtc, give the *Fingeprint* types more meaningful *HashValue* names.
    
    Cleaning up lint along the way.
    
    BUG=117914
    TEST=net_unittests, unit_tests TransportSecurityPersisterTest
    Review URL: https://chromiumcodereview.appspot.com/10825211
    
    TBR=palmer@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10836150
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150507 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/ev_root_ca_metadata.h
+++ net/base/ev_root_ca_metadata.h
@@ -12,7 +12,6 @@
 #endif
 
 #include <map>
-#include <string>
 #include <vector>
 
 #include "net/base/net_export.h"
@@ -40,7 +39,7 @@ class NET_EXPORT_PRIVATE EVRootCAMetadata {
 #if defined(USE_NSS)
   // If the root CA cert has an EV policy OID, returns true and appends the
   // policy OIDs to |*policy_oids|.  Otherwise, returns false.
-  bool GetPolicyOIDsForCA(const SHA1HashValue& fingerprint,
+  bool GetPolicyOIDsForCA(const SHA1Fingerprint& fingerprint,
                           std::vector<PolicyOID>* policy_oids) const;
   const PolicyOID* GetPolicyOIDs() const;
   int NumPolicyOIDs() const;
@@ -50,18 +49,18 @@ class NET_EXPORT_PRIVATE EVRootCAMetadata {
 
   // Returns true if the root CA with the given certificate fingerprint has
   // the EV policy OID policy_oid.
-  bool HasEVPolicyOID(const SHA1HashValue& fingerprint,
+  bool HasEVPolicyOID(const SHA1Fingerprint& fingerprint,
                       PolicyOID policy_oid) const;
 #endif
 
   // AddEVCA adds an EV CA to the list of known EV CAs with the given policy.
   // |policy| is expressed as a string of dotted numbers. It returns true on
   // success.
-  bool AddEVCA(const SHA1HashValue& fingerprint, const char* policy);
+  bool AddEVCA(const SHA1Fingerprint& fingerprint, const char* policy);
 
   // RemoveEVCA removes an EV CA that was previously added by AddEVCA. It
   // returns true on success.
-  bool RemoveEVCA(const SHA1HashValue& fingerprint);
+  bool RemoveEVCA(const SHA1Fingerprint& fingerprint);
 
  private:
   friend struct base::DefaultLazyInstanceTraits<EVRootCAMetadata>;
@@ -70,8 +69,8 @@ class NET_EXPORT_PRIVATE EVRootCAMetadata {
   ~EVRootCAMetadata();
 
 #if defined(USE_NSS)
-  typedef std::map<SHA1HashValue, std::vector<PolicyOID>,
-                   SHA1HashValueLessThan> PolicyOIDMap;
+  typedef std::map<SHA1Fingerprint, std::vector<PolicyOID>,
+                   SHA1FingerprintLessThan> PolicyOIDMap;
 
   // RegisterOID registers |policy|, a policy OID in dotted string form, and
   // writes the memoized form to |*out|. It returns true on success.
@@ -80,8 +79,8 @@ class NET_EXPORT_PRIVATE EVRootCAMetadata {
   PolicyOIDMap ev_policy_;
   std::vector<PolicyOID> policy_oids_;
 #elif defined(OS_WIN)
-  typedef std::map<SHA1HashValue, std::string,
-                   SHA1HashValueLessThan> ExtraEVCAMap;
+  typedef std::map<SHA1Fingerprint, std::string,
+                   SHA1FingerprintLessThan> ExtraEVCAMap;
 
   // extra_cas_ contains any EV CA metadata that was added at runtime.
   ExtraEVCAMap extra_cas_;
