99f85278ec207dbcc705f88dd91ebbf662bce774
rtenneti@google.com <rtenneti@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-10 01:43:23 +0000

    Disable getting histogram data from child processes other than the renderer processes. Store the histogram name in the local variable for debugging purposes when we crash.
    R=jar
    TBR=jam
    BUG=140688
    TEST=base (histogram unit tests) and unit_tests.
    Review URL: https://chromiumcodereview.appspot.com/10831247
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150969 0039d316-1c4b-4281-b951-d872f2087c98

--- base/metrics/histogram_snapshot_manager.cc
+++ base/metrics/histogram_snapshot_manager.cc
@@ -4,7 +4,10 @@
 
 #include "base/metrics/histogram_snapshot_manager.h"
 
+#include "base/compiler_specific.h"
+#include "base/debug/alias.h"
 #include "base/metrics/statistics_recorder.h"
+#include "base/string_util.h"
 
 using base::Histogram;
 using base::StatisticsRecorder;
@@ -43,6 +46,12 @@ void HistogramSnapshotManager::PrepareDelta(const Histogram& histogram) {
   const std::string& histogram_name = histogram.histogram_name();
 
   int corruption = histogram.FindCorruption(snapshot);
+
+  char histogram_name_buf[128];
+  base::strlcpy(histogram_name_buf,
+                histogram_name.c_str(),
+                arraysize(histogram_name_buf));
+  base::debug::Alias(histogram_name_buf);
 
   // Crash if we detect that our histograms have been overwritten.  This may be
   // a fair distance from the memory smasher, but we hope to correlate these
