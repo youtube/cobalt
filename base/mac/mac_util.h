004c845ffea79b110533bcea7789f384bbc58649
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-10 22:47:14 +0000

    mac: Remove more 10.5-only code
    BUG=137676
    TBR=jamiewalch,tony
    
    Review URL: https://chromiumcodereview.appspot.com/10825302
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151142 0039d316-1c4b-4281-b951-d872f2087c98

--- base/mac/mac_util.h
+++ base/mac/mac_util.h
@@ -128,8 +128,6 @@ BASE_EXPORT bool WasLaunchedAsHiddenLoginItem();
 
 // Snow Leopard is Mac OS X 10.6, Darwin 10.
 BASE_EXPORT bool IsOSSnowLeopard();
-BASE_EXPORT bool IsOSSnowLeopardOrEarlier();
-BASE_EXPORT bool IsOSSnowLeopardOrLater();
 
 // Lion is Mac OS X 10.7, Darwin 11.
 BASE_EXPORT bool IsOSLion();
@@ -151,17 +149,10 @@ BASE_EXPORT
 // constant-value inline functions. The MAC_OS_X_VERSION_MIN_REQUIRED macro
 // contains the value of the deployment target.
 
-#if defined(MAC_OS_X_VERSION_10_6) && \
-    MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_6
-#define BASE_MAC_MAC_UTIL_H_INLINED_GE_10_6
-inline bool IsOSSnowLeopardOrLater() { return true; }
-#endif
-
 #if defined(MAC_OS_X_VERSION_10_7) && \
     MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_7
 #define BASE_MAC_MAC_UTIL_H_INLINED_GE_10_7
 inline bool IsOSSnowLeopard() { return false; }
-inline bool IsOSSnowLeopardOrEarlier() { return false; }
 inline bool IsOSLionOrLater() { return true; }
 #endif
 
