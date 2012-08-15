786583a7e302ddb5b7d95813b52e5e1df9f436ec
rbyers@chromium.org <rbyers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-15 20:54:06 +0000

    Set linux distro to CHROMEOS_RELEASE_DESCRIPTION on ChromeOS
    This makes ChromeOS more consistent with Linux, which uses the DISTRIB_DESCRIPTION from /etc/lsb-release here.  We have a different pre-existing mechanism in the code for reading the value (since ChromeOS doesn't have the lsb_release tool), but it's conceptually the same thing.
    
    This makes it possible to search / catagorize ChromeOS crash reports based on the build, or other information about the ChromeOS version.  It also allows the matching build/symbols to be more easily found given a crash report.
    
    There's no need for Aura customization of distro anymore - that was added when
    Aura was experimental.  All ChromeOS is now Aura.
    
    BUG=142630
    
    
    Review URL: https://chromiumcodereview.appspot.com/10827322
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151741 0039d316-1c4b-4281-b951-d872f2087c98

--- base/linux_util.cc
+++ base/linux_util.cc
@@ -132,9 +132,7 @@ static const int kDistroSize = 128 + 1;
 // We use this static string to hold the Linux distro info. If we
 // crash, the crash handler code will send this in the crash dump.
 char g_linux_distro[kDistroSize] =
-#if defined(OS_CHROMEOS) && defined(USE_AURA)
-    "CrOS Aura";
-#elif defined(OS_CHROMEOS)
+#if defined(OS_CHROMEOS)
     "CrOS";
 #elif defined(OS_ANDROID)
     "Android";
