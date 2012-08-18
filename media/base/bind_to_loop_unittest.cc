d177693cc5f7c34cd9385e9a8c56c822634578b4
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-18 04:10:19 +0000

    media::BindToLoop() is born, with example uses to slim down WebMediaPlayerProxy.
    WMPP could use further slimming-down / flamethrowering, but this pass just removes
    the lowest-hanging fruit, where WMPP is used as a silent trampoline to WMPI on
    the render thread.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10855188
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152239 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ media/base/bind_to_loop_unittest.cc
@@ -0,0 +1,98 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/base/bind_to_loop.h"
+
+#include "base/message_loop.h"
+#include "base/synchronization/waitable_event.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace media {
+
+void BoundBoolSet(bool* var, bool val) {
+  *var = val;
+}
+
+void BoundBoolSetFromScopedPtr(bool* var, scoped_ptr<bool> val) {
+  *var = *val;
+}
+
+void BoundBoolSetFromConstRef(bool* var, const bool& val) {
+  *var = val;
+}
+
+void BoundIntegersSet(int* a_var, int* b_var, int a_val, int b_val) {
+  *a_var = a_val;
+  *b_var = b_val;
+}
+
+// Various tests that check that the bound function is only actually executed
+// on the message loop, not during the original Run.
+class BindToLoopTest : public ::testing::Test {
+ public:
+  BindToLoopTest() : proxy_(loop_.message_loop_proxy()) {}
+
+ protected:
+  MessageLoop loop_;
+  scoped_refptr<base::MessageLoopProxy> proxy_;
+};
+
+TEST_F(BindToLoopTest, Closure) {
+  // Test the closure is run inside the loop, not outside it.
+  base::WaitableEvent waiter(false, false);
+  base::Closure cb = BindToLoop(proxy_, base::Bind(
+      &base::WaitableEvent::Signal, base::Unretained(&waiter)));
+  cb.Run();
+  EXPECT_FALSE(waiter.IsSignaled());
+  loop_.RunUntilIdle();
+  EXPECT_TRUE(waiter.IsSignaled());
+}
+
+TEST_F(BindToLoopTest, Bool) {
+  bool bool_var = false;
+  base::Callback<void(bool)> cb = BindToLoop(proxy_, base::Bind(
+      &BoundBoolSet, &bool_var));
+  cb.Run(true);
+  EXPECT_FALSE(bool_var);
+  loop_.RunUntilIdle();
+  EXPECT_TRUE(bool_var);
+}
+
+TEST_F(BindToLoopTest, ScopedPtrBool) {
+  bool bool_val = false;
+  scoped_ptr<bool> scoped_ptr_bool(new bool(true));
+  base::Closure cb = BindToLoop(proxy_, base::Bind(
+      &BoundBoolSetFromScopedPtr, &bool_val, base::Passed(&scoped_ptr_bool)));
+  cb.Run();
+  EXPECT_FALSE(bool_val);
+  loop_.RunUntilIdle();
+  EXPECT_TRUE(bool_val);
+}
+
+TEST_F(BindToLoopTest, BoolConstRef) {
+  bool bool_var = false;
+  bool true_var = true;
+  const bool& true_ref = true_var;
+  base::Closure cb = BindToLoop(proxy_, base::Bind(
+      &BoundBoolSetFromConstRef, &bool_var, true_ref));
+  cb.Run();
+  EXPECT_FALSE(bool_var);
+  loop_.RunUntilIdle();
+  EXPECT_TRUE(bool_var);
+}
+
+TEST_F(BindToLoopTest, Integers) {
+  int a = 0;
+  int b = 0;
+  base::Callback<void(int, int)> cb = BindToLoop(proxy_, base::Bind(
+      &BoundIntegersSet, &a, &b));
+  cb.Run(1, -1);
+  EXPECT_EQ(a, 0);
+  EXPECT_EQ(b, 0);
+  loop_.RunUntilIdle();
+  EXPECT_EQ(a, 1);
+  EXPECT_EQ(b, -1);
+}
+
+}  // namespace media
