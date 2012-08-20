53b61c2ccc81b0daccacbac3124b6918c96fdd43
xians@chromium.org <xians@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-20 11:17:19 +0000

    On mono mode, SL_SPEAKER_FRONT_CENTER is the channel mask; On Stereo mode, SL_SPEAKER_FRONT_LEFT | SL_SPEAKER_FRONT_RIGHT should be used.
    For channel more than 2, we don't support it yet.
    
    
    BUG=143352
    
    Review URL: https://chromiumcodereview.appspot.com/10855218
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152313 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/android/opensles_input.cc
+++ media/audio/android/opensles_input.cc
@@ -24,8 +24,13 @@ OpenSLESInputStream::OpenSLESInputStream(AudioManagerAndroid* audio_manager,
   format_.samplesPerSec = static_cast<SLuint32>(params.sample_rate() * 1000);
   format_.bitsPerSample = params.bits_per_sample();
   format_.containerSize = params.bits_per_sample();
-  format_.channelMask = SL_SPEAKER_FRONT_CENTER;
   format_.endianness = SL_BYTEORDER_LITTLEENDIAN;
+  if (format_.numChannels == 1)
+    format_.channelMask = SL_SPEAKER_FRONT_CENTER;
+  else if (format_.numChannels == 2)
+    format_.channelMask = SL_SPEAKER_FRONT_LEFT | SL_SPEAKER_FRONT_RIGHT;
+  else
+    NOTREACHED() << "Unsupported number of channels: " << format_.numChannels;
 
   buffer_size_bytes_ = params.GetBytesPerBuffer();
 
