9349ef41c2d7273cc2c5b1293320ac2d9b7174dc
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-21 02:27:32 +0000

    WebSocket over SPDY: unit test cleanup
    To prepare for new framing implementation and tests.
     - update test data to mimic real cases and follow the latest spec
     - reorganize utility functions for tests
    
    BUG=42320
    TEST=net_unittests
    
    Review URL: https://chromiumcodereview.appspot.com/10832335
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152484 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_websocket_test_util_spdy2.cc
+++ net/spdy/spdy_websocket_test_util_spdy2.cc
@@ -19,6 +19,49 @@ namespace net {
 
 namespace test_spdy2 {
 
+SpdyFrame* ConstructSpdyWebSocketSynStream(int stream_id,
+                                           const char* path,
+                                           const char* host,
+                                           const char* origin) {
+  const char* const kWebSocketHeaders[] = {
+    "path",
+    path,
+    "host",
+    host,
+    "version",
+    "WebSocket/13",
+    "scheme",
+    "ws",
+    "origin",
+    origin
+  };
+  return ConstructSpdyControlFrame(/*extra_headers*/ NULL,
+                                   /*extra_header_count*/ 0,
+                                   /*compressed*/ false,
+                                   stream_id,
+                                   HIGHEST,
+                                   SYN_STREAM,
+                                   CONTROL_FLAG_NONE,
+                                   kWebSocketHeaders,
+                                   arraysize(kWebSocketHeaders));
+}
+
+SpdyFrame* ConstructSpdyWebSocketSynReply(int stream_id) {
+  static const char* const kStandardWebSocketHeaders[] = {
+    "status",
+    "101"
+  };
+  return ConstructSpdyControlFrame(NULL,
+                                   0,
+                                   false,
+                                   stream_id,
+                                   LOWEST,
+                                   SYN_REPLY,
+                                   CONTROL_FLAG_NONE,
+                                   kStandardWebSocketHeaders,
+                                   arraysize(kStandardWebSocketHeaders));
+}
+
 SpdyFrame* ConstructSpdyWebSocketHandshakeRequestFrame(
     const char* const headers[],
     int header_count,
@@ -75,6 +118,28 @@ SpdyFrame* ConstructSpdyWebSocketHandshakeResponseFrame(
       kDefaultExtraHeaderCount,
       headers,
       header_count);
+}
+
+SpdyFrame* ConstructSpdyWebSocketHeadersFrame(int stream_id,
+                                              const char* length,
+                                              bool fin) {
+  static const char* const kHeaders[] = {
+    "opcode",
+    "1",  // text frame
+    "length",
+    length,
+    "fin",
+    fin ? "1" : "0"
+  };
+  return ConstructSpdyControlFrame(/*extra_headers*/ NULL,
+                                   /*extra_header_count*/ 0,
+                                   /*compression*/ false,
+                                   stream_id,
+                                   LOWEST,
+                                   HEADERS,
+                                   CONTROL_FLAG_NONE,
+                                   kHeaders,
+                                   arraysize(kHeaders));
 }
 
 SpdyFrame* ConstructSpdyWebSocketDataFrame(
