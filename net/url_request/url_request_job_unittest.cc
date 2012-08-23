9b01a78b6b74334e15808809b9ec26a7630c9a46
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-23 18:19:03 +0000

    Revert 153025 - Automatically retry failed network requests.
    This is a temporary experiment to see if this behavior is
    worth implementing in a cleaner fashion.
    
    BUG=143425
    
    Review URL: https://chromiumcodereview.appspot.com/10854204
    
    TBR=mmenke@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10883011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153031 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_job_unittest.cc
+++ net/url_request/url_request_job_unittest.cc
@@ -4,7 +4,6 @@
 
 #include "net/url_request/url_request_job.h"
 
-#include "base/basictypes.h"
 #include "net/http/http_transaction_unittest.h"
 #include "net/url_request/url_request_test_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -20,15 +19,7 @@ void GZipServer(const net::HttpRequestInfo* request,
   response_data->assign(kGzipGata, sizeof(kGzipGata));
 }
 
-void NullServer(const net::HttpRequestInfo* request,
-                std::string* response_status,
-                std::string* response_headers,
-                std::string* response_data) {
-  FAIL();
-}
-
 const MockTransaction kGZip_Transaction = {
-  net::OK,
   "http://www.google.com/gzyp",
   "GET",
   base::Time(),
@@ -85,59 +76,4 @@ TEST(URLRequestJob, SyncTransactionNotifiedWhenDone) {
   EXPECT_TRUE(network_layer.done_reading_called());
 
   RemoveMockTransaction(&transaction);
-}
-
-// Tests that automatic retry triggers for certain errors on GETs, and is not
-// triggered for other errors or with other methods.
-TEST(URLRequestJob, RetryRequests) {
-  const struct {
-    net::Error transaction_result;
-    const char* method;
-    int expected_transaction_count;
-  } tests[] = {
-    {net::ERR_EMPTY_RESPONSE, "GET", 2},
-    {net::ERR_CONNECTION_REFUSED, "GET", 2},
-    {net::ERR_CONNECTION_REFUSED, "POST", 1},
-    {net::ERR_CONNECTION_REFUSED, "PUT", 1},
-    {net::ERR_ACCESS_DENIED, "GET", 1},
-    {net::ERR_CONTENT_DECODING_FAILED, "GET", 1},
-  };
-
-  for (size_t i = 0; i < ARRAYSIZE_UNSAFE(tests); ++i) {
-    const MockTransaction dead_transaction = {
-      tests[i].transaction_result,
-      "http://www.dead.com/",
-      tests[i].method,
-      base::Time(),
-      "",
-      net::LOAD_NORMAL,
-      "",
-      "",
-      base::Time(),
-      "",
-      TEST_MODE_NORMAL,
-      &NullServer,
-      0
-    };
-
-    MockNetworkLayer network_layer;
-    TestURLRequestContext context;
-    context.set_http_transaction_factory(&network_layer);
-
-    TestDelegate d;
-    TestURLRequest req(GURL(dead_transaction.url), &d, &context);
-    MockTransaction transaction(dead_transaction);
-    transaction.test_mode = TEST_MODE_SYNC_ALL;
-    AddMockTransaction(&transaction);
-
-    req.set_method(tests[i].method);
-    req.Start();
-
-    MessageLoop::current()->Run();
-
-    EXPECT_EQ(tests[i].expected_transaction_count,
-              network_layer.transaction_count());
-
-    RemoveMockTransaction(&transaction);
-  }
 }
