e126476e4b1a036d8b9af1c1be85b7d668bb3573
shalev@chromium.org <shalev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-24 17:42:49 +0000

    Turned job_factory into a pure virtual class
    This makes it possible to create wrappers for job_factory that handle a particular job type, and forward the rest to the old job_factory.
    
    TBR=michaeln@chromium.org, brettw@chromium.org, davemoore@chromium.org, kalman@chromium.org, benjhayden@chromium.org, jochen@chromium.org
    
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/10836248
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153241 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_job_factory.cc
+++ net/url_request/url_request_job_factory.cc
@@ -4,11 +4,6 @@
 
 #include "net/url_request/url_request_job_factory.h"
 
-#include "base/stl_util.h"
-#include "googleurl/src/gurl.h"
-#include "net/base/load_flags.h"
-#include "net/url_request/url_request_job_manager.h"
-
 namespace net {
 
 URLRequestJobFactory::ProtocolHandler::~ProtocolHandler() {}
@@ -22,117 +17,6 @@ bool URLRequestJobFactory::Interceptor::WillHandleProtocol(
 
 URLRequestJobFactory::URLRequestJobFactory() {}
 
-URLRequestJobFactory::~URLRequestJobFactory() {
-  STLDeleteValues(&protocol_handler_map_);
-  STLDeleteElements(&interceptors_);
-}
-
-bool URLRequestJobFactory::SetProtocolHandler(
-    const std::string& scheme,
-    ProtocolHandler* protocol_handler) {
-  DCHECK(CalledOnValidThread());
-
-  if (!protocol_handler) {
-    ProtocolHandlerMap::iterator it = protocol_handler_map_.find(scheme);
-    if (it == protocol_handler_map_.end())
-      return false;
-
-    delete it->second;
-    protocol_handler_map_.erase(it);
-    return true;
-  }
-
-  if (ContainsKey(protocol_handler_map_, scheme))
-    return false;
-  protocol_handler_map_[scheme] = protocol_handler;
-  return true;
-}
-
-void URLRequestJobFactory::AddInterceptor(Interceptor* interceptor) {
-  DCHECK(CalledOnValidThread());
-  CHECK(interceptor);
-
-  interceptors_.push_back(interceptor);
-}
-
-URLRequestJob* URLRequestJobFactory::MaybeCreateJobWithInterceptor(
-    URLRequest* request, NetworkDelegate* network_delegate) const {
-  DCHECK(CalledOnValidThread());
-  URLRequestJob* job = NULL;
-
-  if (!(request->load_flags() & LOAD_DISABLE_INTERCEPT)) {
-    InterceptorList::const_iterator i;
-    for (i = interceptors_.begin(); i != interceptors_.end(); ++i) {
-      job = (*i)->MaybeIntercept(request, network_delegate);
-      if (job)
-        return job;
-    }
-  }
-  return NULL;
-}
-
-URLRequestJob* URLRequestJobFactory::MaybeCreateJobWithProtocolHandler(
-    const std::string& scheme,
-    URLRequest* request,
-    NetworkDelegate* network_delegate) const {
-  DCHECK(CalledOnValidThread());
-  ProtocolHandlerMap::const_iterator it = protocol_handler_map_.find(scheme);
-  if (it == protocol_handler_map_.end())
-    return NULL;
-  return it->second->MaybeCreateJob(request, network_delegate);
-}
-
-URLRequestJob* URLRequestJobFactory::MaybeInterceptRedirect(
-    const GURL& location,
-    URLRequest* request,
-    NetworkDelegate* network_delegate) const {
-  DCHECK(CalledOnValidThread());
-  URLRequestJob* job = NULL;
-
-  if (!(request->load_flags() & LOAD_DISABLE_INTERCEPT)) {
-    InterceptorList::const_iterator i;
-    for (i = interceptors_.begin(); i != interceptors_.end(); ++i) {
-      job = (*i)->MaybeInterceptRedirect(location, request, network_delegate);
-      if (job)
-        return job;
-    }
-  }
-  return NULL;
-}
-
-URLRequestJob* URLRequestJobFactory::MaybeInterceptResponse(
-    URLRequest* request, NetworkDelegate* network_delegate) const {
-  DCHECK(CalledOnValidThread());
-  URLRequestJob* job = NULL;
-
-  if (!(request->load_flags() & LOAD_DISABLE_INTERCEPT)) {
-    InterceptorList::const_iterator i;
-    for (i = interceptors_.begin(); i != interceptors_.end(); ++i) {
-      job = (*i)->MaybeInterceptResponse(request, network_delegate);
-      if (job)
-        return job;
-    }
-  }
-  return NULL;
-}
-
-bool URLRequestJobFactory::IsHandledProtocol(const std::string& scheme) const {
-  DCHECK(CalledOnValidThread());
-  InterceptorList::const_iterator i;
-  for (i = interceptors_.begin(); i != interceptors_.end(); ++i) {
-    if ((*i)->WillHandleProtocol(scheme))
-      return true;
-  }
-  return ContainsKey(protocol_handler_map_, scheme) ||
-      URLRequestJobManager::GetInstance()->SupportsScheme(scheme);
-}
-
-bool URLRequestJobFactory::IsHandledURL(const GURL& url) const {
-  if (!url.is_valid()) {
-    // We handle error cases.
-    return true;
-  }
-  return IsHandledProtocol(url.scheme());
-}
+URLRequestJobFactory::~URLRequestJobFactory() {}
 
 }  // namespace net
