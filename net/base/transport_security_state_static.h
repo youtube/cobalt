381663eed87fd0e66982d939b2b8c677b42a5115
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-27 14:56:03 +0000

    net: remove duplicate HSTS entry for arivo.com.br
    This change also adds a check in the script to prevent this occuring
    again.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153482 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/transport_security_state_static.h
+++ net/base/transport_security_state_static.h
@@ -434,7 +434,6 @@ static const struct HSTSPreload kPreloadedSTS[] = {
   {17, true, "\003www\007neonisi\003com", true, kNoPins, DOMAIN_NOT_PINNED },
   {19, true, "\005shops\007neonisi\003com", true, kNoPins, DOMAIN_NOT_PINNED },
   {17, true, "\014piratenlogin\002de", true, kNoPins, DOMAIN_NOT_PINNED },
-  {14, true, "\005arivo\003com\002br", true, kNoPins, DOMAIN_NOT_PINNED },
   {15, true, "\011howrandom\003org", true, kNoPins, DOMAIN_NOT_PINNED },
   {13, false, "\010intercom\002io", true, kNoPins, DOMAIN_NOT_PINNED },
   {17, false, "\003api\010intercom\002io", true, kNoPins, DOMAIN_NOT_PINNED },
