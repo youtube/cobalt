d99c67111d974defb6f79710b181ebf250163b3e
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-31 19:52:40 +0000

    [net] Move DnsConfigService to NetworkChangeNotifier.
    This merges DnsConfigWatcher back into DnsConfigService and installs
    DnsConfigService at NetworkChangeNotifier. It removes |detail| from
    OnDNSChanged callback, and exposes NetworkChangeNotifier::GetDnsConfig.
    
    BUG=142142
    
    
    Review URL: https://chromiumcodereview.appspot.com/10873018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154485 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/network_change_notifier_linux.h
+++ net/base/network_change_notifier_linux.h
@@ -40,7 +40,7 @@ class NET_EXPORT_PRIVATE NetworkChangeNotifierLinux
   // The thread used to listen for notifications.  This relays the notification
   // to the registered observers without posting back to the thread the object
   // was created on.
-  // Also used for DnsConfigWatcher which requires TYPE_IO message loop.
+  // Also used for DnsConfigService which requires TYPE_IO message loop.
   scoped_ptr<Thread> notifier_thread_;
 
   DISALLOW_COPY_AND_ASSIGN(NetworkChangeNotifierLinux);
