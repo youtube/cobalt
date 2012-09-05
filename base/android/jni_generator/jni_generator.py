33d52208f6c9adb0c52faada6991cdfc6459e8f7
leandrogracia@chromium.org <leandrogracia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-05 03:46:31 +0000

    Upstream the Android port find-in-page feature.
    This patch provides the find-in-page Chromium code for the Android port.
    Further code implementing the feature for Android WebView will be added later in a different patch.
    
    Unlike the previous approach (CL 10699024) this patch uses the FindTabHelper class as Desktop Chrome does, introducing its new required APIs there. It also introduces FindMatchRects in a way symmetrical to the current Find/FindReply implementation in order to simplify any possible future adoption of this API by other platforms.
    
    
    BUG=136762
    
    Review URL: https://chromiumcodereview.appspot.com/10905058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154893 0039d316-1c4b-4281-b951-d872f2087c98

--- base/android/jni_generator/jni_generator.py
+++ base/android/jni_generator/jni_generator.py
@@ -162,6 +162,8 @@ def JavaParamToJni(param):
       'Lorg/chromium/android_webview/AwWebContentsDelegate',
       'Lorg/chromium/base/SystemMessageHandler',
       'Lorg/chromium/chrome/browser/ChromeWebContentsDelegateAndroid',
+      'Lorg/chromium/chrome/browser/FindMatchRectsDetails',
+      'Lorg/chromium/chrome/browser/FindNotificationDetails',
       'Lorg/chromium/chrome/browser/JavascriptAppModalDialog',
       'Lorg/chromium/chrome/browser/ProcessUtils',
       'Lorg/chromium/chrome/browser/SelectFileDialog',
