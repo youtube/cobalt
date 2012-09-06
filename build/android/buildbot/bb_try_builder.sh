1b9bb4cb0398bd69715d80c75bf61f9a09b479f3
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-06 23:50:37 +0000

    Move buildbot files to a separate buildbot dir
    - Adding placeholder scripts in old locations until new locations can be
      updated in buildbot master
    - Removed buildbot.sh, unused.
    
    TBR=bulach
    
    Review URL: https://chromiumcodereview.appspot.com/10914132
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155280 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/buildbot/bb_try_builder.sh
+++ build/android/buildbot/bb_try_builder.sh
@@ -5,8 +5,9 @@
 #
 # Buildbot annotator script for trybots.  Compile only.
 
-BB_SRC_ROOT="$(cd "$(dirname $0)/../.."; pwd)"
-. "${BB_SRC_ROOT}/build/android/buildbot_functions.sh"
+BB_DIR="$(dirname $0)"
+BB_SRC_ROOT="$(cd  "$BB_DIR/../../.."; pwd)"
+. "$BB_DIR/buildbot_functions.sh"
 
 # SHERIFF: if you need to quickly turn "android" trybots green,
 # uncomment the next line (and send appropriate email out):
