004d9179f05522c5452f5d7ee4b5ac00a9762025
rsimha@google.com <rsimha@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-12 21:17:21 +0000

    Revert 156361 - Take 2: Force python test server output to be unbuffered, so it doesn't mix with gtest output
    In browser tests that use a local python server, the python output in
    the test logs sometimes overlaps with gtest output, resulting in gtest
    falsely detecting passing tests as incomplete. This is a result of
    python's default use of buffered output, which gets written to the log
    file out of order.
    
    This patch forces the python process for local test servers to use
    unbuffered mode via the -u switch. This way, by the time gtest is ready
    to log a passing test, all testserver output is already written to the
    log file.
    
    BUG=147368
    TEST= See sync integration test output when it is redirected to a log file, and make sure there are no false negatives.
    
    Review URL: https://chromiumcodereview.appspot.com/10907162
    
    TBR=rsimha@google.com
    Review URL: https://codereview.chromium.org/10911264
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156376 0039d316-1c4b-4281-b951-d872f2087c98

--- net/test/python_utils.h
+++ net/test/python_utils.h
@@ -7,7 +7,6 @@
 
 #include "base/compiler_specific.h"
 
-class CommandLine;
 class FilePath;
 
 // This is the python path variable name.
@@ -19,7 +18,7 @@ void AppendToPythonPath(const FilePath& dir);
 // Return the location of the compiler-generated python protobuf.
 bool GetPyProtoPath(FilePath* dir);
 
-// Returns the command that should be used to launch Python.
-bool GetPythonCommand(CommandLine* python_cmd) WARN_UNUSED_RESULT;
+// Returns the path that should be used to launch Python.
+bool GetPythonRunTime(FilePath* path) WARN_UNUSED_RESULT;
 
 #endif  // NET_TEST_PYTHON_UTILS_H_
