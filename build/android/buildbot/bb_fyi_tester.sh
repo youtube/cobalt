624058952212451a89d16ca7c04ba202f48779f5
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-13 18:08:16 +0000

    Touchup android trybot annotator scripts
    - Adding FYI trybot scripts to mirror fyi steps
    
    BUG=147462
    
    Review URL: https://chromiumcodereview.appspot.com/10933040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156582 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/buildbot/bb_fyi_tester.sh
+++ build/android/buildbot/bb_fyi_tester.sh
@@ -3,11 +3,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
-# Buildbot annotator script for a FYI waterfall tester.
+# Buildbot annotator script for the fyi waterfall and fyi trybots.
 # Downloads and extracts a build from the builder and runs tests.
-
-# SHERIFF: there should be no need to disable this bot.
-# The FYI waterfall does not close the tree.
 
 BB_DIR="$(dirname $0)"
 BB_SRC_ROOT="$(cd  "$BB_DIR/../../.."; pwd)"
