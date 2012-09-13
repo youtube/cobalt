2520eb4b598db79b077365238b4415ea969094e2
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-13 18:08:16 +0000

    Touchup android trybot annotator scripts
    - Adding FYI trybot scripts to mirror fyi steps
    
    BUG=147462
    
    Review URL: https://chromiumcodereview.appspot.com/10933040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156582 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/buildbot/bb_try_tester.sh
+++ build/android/buildbot/bb_try_tester.sh
@@ -12,10 +12,8 @@ BB_SRC_ROOT="$(cd  "$BB_DIR/../../.."; pwd)"
 # SHERIFF: if you need to quickly turn "android" trybots green,
 # uncomment the next line (and send appropriate email out):
 ## bb_force_bot_green_and_exit
-# You will also need to change buildbot_try_builder.sh
 
 bb_baseline_setup "$BB_SRC_ROOT" "$@"
 bb_extract_build
 bb_reboot_phones
 bb_run_unit_tests
-bb_run_instrumentation_tests
