58c9074e5657151babf2053692044eac5d6c548a
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-15 00:23:07 +0000

    Enable TLS channeld id by default.
    Replace --enable-origin-bound-certs command line flag with
    --disable-tls-channel-id.
    
    Remove field trial.
    
    BUG=136462,129174
    
    
    Review URL: https://chromiumcodereview.appspot.com/10910240
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156939 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/ssl_config_service.cc
+++ net/base/ssl_config_service.cc
@@ -38,7 +38,7 @@ SSLConfig::SSLConfig()
       version_min(g_default_version_min),
       version_max(g_default_version_max),
       cached_info_enabled(false),
-      channel_id_enabled(false),
+      channel_id_enabled(true),
       false_start_enabled(true),
       send_client_cert(false),
       verify_ev_cert(false),
@@ -74,7 +74,6 @@ SSLConfigService::SSLConfigService()
 }
 
 static bool g_cached_info_enabled = false;
-static bool g_channel_id_trial = false;
 
 // GlobalCRLSet holds a reference to the global CRLSet. It simply wraps a lock
 // around a scoped_refptr so that getting a reference doesn't race with
@@ -133,11 +132,6 @@ uint16 SSLConfigService::default_version_max() {
   return g_default_version_max;
 }
 
-// static
-void SSLConfigService::EnableChannelIDTrial() {
-  g_channel_id_trial = true;
-}
-
 void SSLConfigService::AddObserver(Observer* observer) {
   observer_list_.AddObserver(observer);
 }
@@ -152,8 +146,6 @@ SSLConfigService::~SSLConfigService() {
 // static
 void SSLConfigService::SetSSLConfigFlags(SSLConfig* ssl_config) {
   ssl_config->cached_info_enabled = g_cached_info_enabled;
-  if (g_channel_id_trial)
-    ssl_config->channel_id_enabled = true;
 }
 
 void SSLConfigService::ProcessConfigUpdate(const SSLConfig& orig_config,
