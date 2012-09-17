c9daf71e86bfcf151c5145ee7d7046c0f366689c
alexeif@chromium.org <alexeif@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-17 11:44:10 +0000

    Expose memory allocator internal stats and properties.
    Review URL: https://chromiumcodereview.appspot.com/10825250
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157102 0039d316-1c4b-4281-b951-d872f2087c98

--- base/allocator/allocator_extension.h
+++ base/allocator/allocator_extension.h
@@ -5,12 +5,22 @@
 #ifndef BASE_ALLOCATOR_ALLOCATOR_EXTENSION_H
 #define BASE_ALLOCATOR_ALLOCATOR_EXTENSION_H
 
+#include <stddef.h> // for size_t
+
 #include "base/allocator/allocator_extension_thunks.h"
 #include "base/base_export.h"
 #include "build/build_config.h"
 
 namespace base {
 namespace allocator {
+
+// Request the allocator to report value of its internal state variable.
+//
+// |name| name of the variable
+// |value| pointer to the returned value, must be not NULL.
+// Returns true if the value has been returned, false if a variable with such
+// name does not exist.
+BASE_EXPORT bool GetProperty(const char* name, size_t* value);
 
 // Request that the allocator print a human-readable description of the current
 // state of the allocator into a null-terminated string in the memory segment
@@ -33,11 +43,15 @@ BASE_EXPORT void ReleaseFreeMemory();
 // No threading promises are made.  The caller is responsible for making sure
 // these pointers are set before any other threads attempt to call the above
 // functions.
+BASE_EXPORT void SetGetPropertyFunction(
+    thunks::GetPropertyFunction get_property_function);
+
 BASE_EXPORT void SetGetStatsFunction(
-    thunks::GetStatsFunction* get_stats_function);
+    thunks::GetStatsFunction get_stats_function);
 
 BASE_EXPORT void SetReleaseFreeMemoryFunction(
-    thunks::ReleaseFreeMemoryFunction* release_free_memory_function);
+    thunks::ReleaseFreeMemoryFunction release_free_memory_function);
+
 }  // namespace allocator
 }  // namespace base
 
