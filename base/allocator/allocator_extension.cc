ac7de0abd5b0640886f37856ed0b8537e462374c
alexeif@chromium.org <alexeif@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-17 11:44:10 +0000

    Expose memory allocator internal stats and properties.
    Review URL: https://chromiumcodereview.appspot.com/10825250
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157102 0039d316-1c4b-4281-b951-d872f2087c98

--- base/allocator/allocator_extension.cc
+++ base/allocator/allocator_extension.cc
@@ -9,31 +9,46 @@
 namespace base {
 namespace allocator {
 
+bool GetProperty(const char* name, size_t* value) {
+  thunks::GetPropertyFunction get_property_function =
+      base::allocator::thunks::GetGetPropertyFunction();
+  return get_property_function != NULL && get_property_function(name, value);
+}
+
 void GetStats(char* buffer, int buffer_length) {
   DCHECK_GT(buffer_length, 0);
-  if (thunks::GetStatsFunction* get_stats_function =
-          base::allocator::thunks::GetGetStatsFunction())
+  thunks::GetStatsFunction get_stats_function =
+      base::allocator::thunks::GetGetStatsFunction();
+  if (get_stats_function)
     get_stats_function(buffer, buffer_length);
   else
     buffer[0] = '\0';
 }
 
 void ReleaseFreeMemory() {
-  if (thunks::ReleaseFreeMemoryFunction* release_free_memory_function =
-          base::allocator::thunks::GetReleaseFreeMemoryFunction())
+  thunks::ReleaseFreeMemoryFunction release_free_memory_function =
+      base::allocator::thunks::GetReleaseFreeMemoryFunction();
+  if (release_free_memory_function)
     release_free_memory_function();
 }
 
-void SetGetStatsFunction(thunks::GetStatsFunction* get_stats_function) {
+void SetGetPropertyFunction(
+    thunks::GetPropertyFunction get_property_function) {
+  DCHECK_EQ(base::allocator::thunks::GetGetPropertyFunction(),
+            reinterpret_cast<thunks::GetPropertyFunction>(NULL));
+  base::allocator::thunks::SetGetPropertyFunction(get_property_function);
+}
+
+void SetGetStatsFunction(thunks::GetStatsFunction get_stats_function) {
   DCHECK_EQ(base::allocator::thunks::GetGetStatsFunction(),
-            reinterpret_cast<thunks::GetStatsFunction*>(NULL));
+            reinterpret_cast<thunks::GetStatsFunction>(NULL));
   base::allocator::thunks::SetGetStatsFunction(get_stats_function);
 }
 
 void SetReleaseFreeMemoryFunction(
-    thunks::ReleaseFreeMemoryFunction* release_free_memory_function) {
+    thunks::ReleaseFreeMemoryFunction release_free_memory_function) {
   DCHECK_EQ(base::allocator::thunks::GetReleaseFreeMemoryFunction(),
-            reinterpret_cast<thunks::ReleaseFreeMemoryFunction*>(NULL));
+            reinterpret_cast<thunks::ReleaseFreeMemoryFunction>(NULL));
   base::allocator::thunks::SetReleaseFreeMemoryFunction(
       release_free_memory_function);
 }
--- base/allocator/allocator_extension_thunks.cc
+++ base/allocator/allocator_extension_thunks.cc
@@ -17,23 +17,32 @@ namespace thunks {
 // can depend on it. This file can't depend on anything else in base, including
 // logging.
 
-static GetStatsFunction* g_get_stats_function = NULL;
-static ReleaseFreeMemoryFunction* g_release_free_memory_function = NULL;
+static GetPropertyFunction g_get_property_function = NULL;
+static GetStatsFunction g_get_stats_function = NULL;
+static ReleaseFreeMemoryFunction g_release_free_memory_function = NULL;
 
-void SetGetStatsFunction(GetStatsFunction* get_stats_function) {
+void SetGetPropertyFunction(GetPropertyFunction get_property_function) {
+  g_get_property_function = get_property_function;
+}
+
+GetPropertyFunction GetGetPropertyFunction() {
+  return g_get_property_function;
+}
+
+void SetGetStatsFunction(GetStatsFunction get_stats_function) {
   g_get_stats_function = get_stats_function;
 }
 
-GetStatsFunction* GetGetStatsFunction() {
+GetStatsFunction GetGetStatsFunction() {
   return g_get_stats_function;
 }
 
 void SetReleaseFreeMemoryFunction(
-      ReleaseFreeMemoryFunction* release_free_memory_function) {
+    ReleaseFreeMemoryFunction release_free_memory_function) {
   g_release_free_memory_function = release_free_memory_function;
 }
 
-ReleaseFreeMemoryFunction* GetReleaseFreeMemoryFunction() {
+ReleaseFreeMemoryFunction GetReleaseFreeMemoryFunction() {
   return g_release_free_memory_function;
 }
 
