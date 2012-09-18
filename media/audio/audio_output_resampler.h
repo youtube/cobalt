3083ff1c21a9d02ebc0147f1770ab2a419be1512
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-18 05:29:37 +0000

    Fix output resampler to handle multiple StartStream() calls.
    AudioOutputResampler only handled the first StartStream() call,
    despite the fact that since AudioOutputDispatchers are shared
    StartStream() can be called many times.
    
    The new approach puts each resampler/fifo pair in a new object
    which is tied to the AudioOutputProxy pointer passed to the
    dispatcher (similar to AudioOutputMixer).
    
    BUG=150003
    TEST=Multiple YouTube tabs open, sound plays fine. Unit tests.
    
    Review URL: https://codereview.chromium.org/10913310
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157306 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/audio_output_resampler.h
+++ media/audio/audio_output_resampler.h
@@ -5,6 +5,8 @@
 #ifndef MEDIA_AUDIO_AUDIO_OUTPUT_RESAMPLER_H_
 #define MEDIA_AUDIO_AUDIO_OUTPUT_RESAMPLER_H_
 
+#include <map>
+
 #include "base/basictypes.h"
 #include "base/memory/ref_counted.h"
 #include "base/synchronization/lock.h"
@@ -16,8 +18,7 @@
 
 namespace media {
 
-class AudioPullFifo;
-class MultiChannelResampler;
+class OnMoreDataResampler;
 
 // AudioOutputResampler is a browser-side resampling and rebuffering solution
 // which ensures audio data is always output at given parameters.  The rough
@@ -43,8 +44,7 @@ class MultiChannelResampler;
 // Currently channel downmixing and upmixing is not supported.
 // TODO(dalecurtis): Add channel remixing.  http://crbug.com/138762
 class MEDIA_EXPORT AudioOutputResampler
-    : public AudioOutputDispatcher,
-      public AudioOutputStream::AudioSourceCallback {
+    : public AudioOutputDispatcher {
  public:
   AudioOutputResampler(AudioManager* audio_manager,
                        const AudioParameters& input_params,
@@ -61,15 +61,6 @@ class MEDIA_EXPORT AudioOutputResampler
   virtual void CloseStream(AudioOutputProxy* stream_proxy) OVERRIDE;
   virtual void Shutdown() OVERRIDE;
 
-  // AudioSourceCallback interface.
-  virtual int OnMoreData(AudioBus* dest,
-                         AudioBuffersState buffers_state) OVERRIDE;
-  virtual int OnMoreIOData(AudioBus* source,
-                           AudioBus* dest,
-                           AudioBuffersState buffers_state) OVERRIDE;
-  virtual void OnError(AudioOutputStream* stream, int code) OVERRIDE;
-  virtual void WaitTillDataReady() OVERRIDE;
-
  private:
   friend class base::RefCountedThreadSafe<AudioOutputResampler>;
   virtual ~AudioOutputResampler();
@@ -77,36 +68,18 @@ class MEDIA_EXPORT AudioOutputResampler
   // Used to initialize the FIFO and resamplers.
   void Initialize();
 
-  // Called by MultiChannelResampler when more data is necessary.
-  void ProvideInput(AudioBus* audio_bus);
-
-  // Called by AudioPullFifo when more data is necessary.  Requires
-  // |source_lock_| to have been acquired.
-  void SourceCallback_Locked(AudioBus* dest);
-
-  // Passes through |source| to the |source_callback_| OnMoreIOData() call.
-  void SourceIOCallback_Locked(AudioBus* source, AudioBus* dest);
-
-  // Used by StopStream()/CloseStream()/Shutdown() to clear internal state.
-  // TODO(dalecurtis): Probably only one of these methods needs to call this,
-  // the rest should DCHECK()/CHECK() that the values were reset.
-  void Reset();
-
-  // Handles resampling.
-  scoped_ptr<MultiChannelResampler> resampler_;
+  // Used by StopStream()/CloseStream() to clear internal state.  When
+  // |delete_callback| is set, the callbacks[stream_proxy] is destroyed.
+  void Reset(AudioOutputProxy* stream_proxy, bool delete_callback);
 
   // Dispatcher to proxy all AudioOutputDispatcher calls too.
   scoped_refptr<AudioOutputDispatcher> dispatcher_;
 
-  // Source callback and associated lock.
-  base::Lock source_lock_;
-  AudioOutputStream::AudioSourceCallback* source_callback_;
-
-  // Used to buffer data between the client and the output device in cases where
-  // the client buffer size is not the same as the output device buffer size.
-  // Bound to SourceCallback_Locked() so must only be used when |source_lock_|
-  // has already been acquired.
-  scoped_ptr<AudioPullFifo> audio_fifo_;
+  // Vector of each outstanding OnMoreDataResampler object.  A new instance is
+  // created on every StartStream() call.  Access must be locked.
+  typedef std::map<AudioOutputProxy*, OnMoreDataResampler*> CallbackMap;
+  CallbackMap callbacks_;
+  base::Lock callbacks_lock_;
 
   // Ratio of input bytes to output bytes used to correct playback delay with
   // regard to buffering and resampling.
@@ -114,14 +87,6 @@ class MEDIA_EXPORT AudioOutputResampler
 
   // Used by AudioOutputDispatcherImpl; kept so we can reinitialize on the fly.
   base::TimeDelta close_delay_;
-
-  // Last AudioBuffersState object received via OnMoreData(), used to correct
-  // playback delay by ProvideInput() and passed on to |source_callback_|.
-  AudioBuffersState current_buffers_state_;
-
-  // Total number of bytes (in terms of output parameters) stored in resampler
-  // or FIFO buffers which have not been sent to the audio device.
-  int outstanding_audio_bytes_;
 
   // AudioParameters used to setup the output stream.
   AudioParameters output_params_;
