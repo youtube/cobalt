bbdd14db0aac7d1f46a2715f460ebc3e6ce39007
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-18 05:29:37 +0000

    Fix output resampler to handle multiple StartStream() calls.
    AudioOutputResampler only handled the first StartStream() call,
    despite the fact that since AudioOutputDispatchers are shared
    StartStream() can be called many times.
    
    The new approach puts each resampler/fifo pair in a new object
    which is tied to the AudioOutputProxy pointer passed to the
    dispatcher (similar to AudioOutputMixer).
    
    BUG=150003
    TEST=Multiple YouTube tabs open, sound plays fine. Unit tests.
    
    Review URL: https://codereview.chromium.org/10913310
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157306 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/audio_output_resampler.cc
+++ media/audio/audio_output_resampler.cc
@@ -23,6 +23,68 @@
 
 namespace media {
 
+class OnMoreDataResampler : public AudioOutputStream::AudioSourceCallback {
+ public:
+  OnMoreDataResampler(double io_ratio,
+                      const AudioParameters& input_params,
+                      const AudioParameters& output_params,
+                      AudioOutputStream::AudioSourceCallback* callback);
+  virtual ~OnMoreDataResampler();
+
+  // AudioSourceCallback interface.
+  virtual int OnMoreData(AudioBus* dest,
+                         AudioBuffersState buffers_state) OVERRIDE;
+  virtual int OnMoreIOData(AudioBus* source,
+                           AudioBus* dest,
+                           AudioBuffersState buffers_state) OVERRIDE;
+  virtual void OnError(AudioOutputStream* stream, int code) OVERRIDE;
+  virtual void WaitTillDataReady() OVERRIDE;
+
+  // Clears |source_callback_| and flushes the resampler.
+  void Stop();
+
+ private:
+  // Called by MultiChannelResampler when more data is necessary.
+  void ProvideInput(AudioBus* audio_bus);
+
+  // Called by AudioPullFifo when more data is necessary.  Requires
+  // |source_lock_| to have been acquired.
+  void SourceCallback_Locked(AudioBus* audio_bus);
+
+  // Passes through |source| to the |source_callback_| OnMoreIOData() call.
+  void SourceIOCallback_Locked(AudioBus* source, AudioBus* dest);
+
+  // Ratio of input bytes to output bytes used to correct playback delay with
+  // regard to buffering and resampling.
+  double io_ratio_;
+
+  // Source callback and associated lock.
+  base::Lock source_lock_;
+  AudioOutputStream::AudioSourceCallback* source_callback_;
+
+  // Last AudioBuffersState object received via OnMoreData(), used to correct
+  // playback delay by ProvideInput() and passed on to |source_callback_|.
+  AudioBuffersState current_buffers_state_;
+
+  // Total number of bytes (in terms of output parameters) stored in resampler
+  // or FIFO buffers which have not been sent to the audio device.
+  int outstanding_audio_bytes_;
+
+  // Used to buffer data between the client and the output device in cases where
+  // the client buffer size is not the same as the output device buffer size.
+  // Bound to SourceCallback_Locked() so must only be used when |source_lock_|
+  // has already been acquired.
+  scoped_ptr<AudioPullFifo> audio_fifo_;
+
+  // Handles resampling.
+  scoped_ptr<MultiChannelResampler> resampler_;
+
+  int output_bytes_per_frame_;
+  int input_bytes_per_frame_;
+
+  DISALLOW_COPY_AND_ASSIGN(OnMoreDataResampler);
+};
+
 // Record UMA statistics for hardware output configuration.
 static void RecordStats(const AudioParameters& output_params) {
   UMA_HISTOGRAM_ENUMERATION(
@@ -107,10 +169,8 @@ AudioOutputResampler::AudioOutputResampler(AudioManager* audio_manager,
                                            const AudioParameters& output_params,
                                            const base::TimeDelta& close_delay)
     : AudioOutputDispatcher(audio_manager, input_params),
-      source_callback_(NULL),
       io_ratio_(1),
       close_delay_(close_delay),
-      outstanding_audio_bytes_(0),
       output_params_(output_params) {
   DCHECK_EQ(output_params_.format(), AudioParameters::AUDIO_PCM_LOW_LATENCY);
 
@@ -120,7 +180,7 @@ AudioOutputResampler::AudioOutputResampler(AudioManager* audio_manager,
   // Immediately fallback if we're given invalid output parameters.  This may
   // happen if the OS provided us junk values for the hardware configuration.
   if (!output_params_.IsValid()) {
-    LOG(ERROR) << "Invalid audio output parameters recieved; using fallback "
+    LOG(ERROR) << "Invalid audio output parameters received; using fallback "
                << "path. Channels: " << output_params_.channels() << ", "
                << "Sample Rate: " << output_params_.sample_rate() << ", "
                << "Bits Per Sample: " << output_params_.bits_per_sample()
@@ -147,17 +207,11 @@ void AudioOutputResampler::Initialize() {
       params_.sample_rate() != output_params_.sample_rate() ||
       params_.bits_per_sample() != output_params_.bits_per_sample() ||
       params_.frames_per_buffer() != output_params_.frames_per_buffer()) {
-    // Only resample if necessary since it's expensive.
     if (params_.sample_rate() != output_params_.sample_rate()) {
-      DVLOG(1) << "Resampling from " << params_.sample_rate() << " to "
-               << output_params_.sample_rate();
       double io_sample_rate_ratio = params_.sample_rate() /
           static_cast<double>(output_params_.sample_rate());
       // Include the I/O resampling ratio in our global I/O ratio.
       io_ratio_ *= io_sample_rate_ratio;
-      resampler_.reset(new MultiChannelResampler(
-          output_params_.channels(), io_sample_rate_ratio, base::Bind(
-              &AudioOutputResampler::ProvideInput, base::Unretained(this))));
     }
 
     // Include bits per channel differences.
@@ -167,19 +221,6 @@ void AudioOutputResampler::Initialize() {
     // Include channel count differences.
     io_ratio_ *= static_cast<double>(params_.channels()) /
         output_params_.channels();
-
-    // Since the resampler / output device may want a different buffer size than
-    // the caller asked for, we need to use a FIFO to ensure that both sides
-    // read in chunk sizes they're configured for.
-    if (params_.sample_rate() != output_params_.sample_rate() ||
-        params_.frames_per_buffer() != output_params_.frames_per_buffer()) {
-      DVLOG(1) << "Rebuffering from " << params_.frames_per_buffer()
-               << " to " << output_params_.frames_per_buffer();
-      audio_fifo_.reset(new AudioPullFifo(
-          params_.channels(), params_.frames_per_buffer(), base::Bind(
-              &AudioOutputResampler::SourceCallback_Locked,
-              base::Unretained(this))));
-    }
 
     DVLOG(1) << "I/O ratio is " << io_ratio_;
   } else {
@@ -232,11 +273,19 @@ bool AudioOutputResampler::OpenStream() {
 bool AudioOutputResampler::StartStream(
     AudioOutputStream::AudioSourceCallback* callback,
     AudioOutputProxy* stream_proxy) {
+  OnMoreDataResampler* resampler_callback = NULL;
   {
-    base::AutoLock auto_lock(source_lock_);
-    source_callback_ = callback;
+    base::AutoLock auto_lock(callbacks_lock_);
+    CallbackMap::iterator it = callbacks_.find(stream_proxy);
+    if (it == callbacks_.end()) {
+      resampler_callback = new OnMoreDataResampler(
+          io_ratio_, params_, output_params_, callback);
+      callbacks_[stream_proxy] = resampler_callback;
+    } else {
+      resampler_callback = it->second;
+    }
   }
-  return dispatcher_->StartStream(this, stream_proxy);
+  return dispatcher_->StartStream(resampler_callback, stream_proxy);
 }
 
 void AudioOutputResampler::StreamVolumeSet(AudioOutputProxy* stream_proxy,
@@ -244,7 +293,71 @@ void AudioOutputResampler::StreamVolumeSet(AudioOutputProxy* stream_proxy,
   dispatcher_->StreamVolumeSet(stream_proxy, volume);
 }
 
-void AudioOutputResampler::Reset() {
+void AudioOutputResampler::Reset(AudioOutputProxy* stream_proxy,
+                                 bool delete_callback) {
+  base::AutoLock auto_lock(callbacks_lock_);
+  CallbackMap::iterator it = callbacks_.find(stream_proxy);
+  if (it != callbacks_.end()) {
+    it->second->Stop();
+    if (delete_callback) {
+      delete it->second;
+      callbacks_.erase(it);
+    }
+  }
+}
+
+void AudioOutputResampler::StopStream(AudioOutputProxy* stream_proxy) {
+  dispatcher_->StopStream(stream_proxy);
+  Reset(stream_proxy, false);
+}
+
+void AudioOutputResampler::CloseStream(AudioOutputProxy* stream_proxy) {
+  dispatcher_->CloseStream(stream_proxy);
+  Reset(stream_proxy, true);
+}
+
+void AudioOutputResampler::Shutdown() {
+  dispatcher_->Shutdown();
+  DCHECK(callbacks_.empty());
+}
+
+OnMoreDataResampler::OnMoreDataResampler(
+    double io_ratio, const AudioParameters& input_params,
+    const AudioParameters& output_params,
+    AudioOutputStream::AudioSourceCallback* callback)
+    : io_ratio_(io_ratio),
+      source_callback_(callback),
+      outstanding_audio_bytes_(0),
+      output_bytes_per_frame_(output_params.GetBytesPerFrame()),
+      input_bytes_per_frame_(input_params.GetBytesPerFrame()) {
+  // Only resample if necessary since it's expensive.
+  if (input_params.sample_rate() != output_params.sample_rate()) {
+    DVLOG(1) << "Resampling from " << input_params.sample_rate() << " to "
+             << output_params.sample_rate();
+    double io_sample_rate_ratio = input_params.sample_rate() /
+        static_cast<double>(output_params.sample_rate());
+    resampler_.reset(new MultiChannelResampler(
+        output_params.channels(), io_sample_rate_ratio, base::Bind(
+            &OnMoreDataResampler::ProvideInput, base::Unretained(this))));
+  }
+
+  // Since the resampler / output device may want a different buffer size than
+  // the caller asked for, we need to use a FIFO to ensure that both sides
+  // read in chunk sizes they're configured for.
+  if (input_params.sample_rate() != output_params.sample_rate() ||
+      input_params.frames_per_buffer() != output_params.frames_per_buffer()) {
+    DVLOG(1) << "Rebuffering from " << input_params.frames_per_buffer()
+             << " to " << output_params.frames_per_buffer();
+    audio_fifo_.reset(new AudioPullFifo(
+        input_params.channels(), input_params.frames_per_buffer(), base::Bind(
+            &OnMoreDataResampler::SourceCallback_Locked,
+            base::Unretained(this))));
+  }
+}
+
+OnMoreDataResampler::~OnMoreDataResampler() {}
+
+void OnMoreDataResampler::Stop() {
   base::AutoLock auto_lock(source_lock_);
   source_callback_ = NULL;
   outstanding_audio_bytes_ = 0;
@@ -254,29 +367,14 @@ void AudioOutputResampler::Reset() {
     resampler_->Flush();
 }
 
-void AudioOutputResampler::StopStream(AudioOutputProxy* stream_proxy) {
-  Reset();
-  dispatcher_->StopStream(stream_proxy);
-}
-
-void AudioOutputResampler::CloseStream(AudioOutputProxy* stream_proxy) {
-  Reset();
-  dispatcher_->CloseStream(stream_proxy);
-}
-
-void AudioOutputResampler::Shutdown() {
-  Reset();
-  dispatcher_->Shutdown();
-}
-
-int AudioOutputResampler::OnMoreData(AudioBus* dest,
-                                     AudioBuffersState buffers_state) {
+int OnMoreDataResampler::OnMoreData(AudioBus* dest,
+                                    AudioBuffersState buffers_state) {
   return OnMoreIOData(NULL, dest, buffers_state);
 }
 
-int AudioOutputResampler::OnMoreIOData(AudioBus* source,
-                                       AudioBus* dest,
-                                       AudioBuffersState buffers_state) {
+int OnMoreDataResampler::OnMoreIOData(AudioBus* source,
+                                      AudioBus* dest,
+                                      AudioBuffersState buffers_state) {
   base::AutoLock auto_lock(source_lock_);
   // While we waited for |source_lock_| the callback might have been cleared.
   if (!source_callback_) {
@@ -299,8 +397,7 @@ int AudioOutputResampler::OnMoreIOData(AudioBus* source,
     ProvideInput(dest);
 
   // Calculate how much data is left in the internal FIFO and resampler buffers.
-  outstanding_audio_bytes_ -=
-      dest->frames() * output_params_.GetBytesPerFrame();
+  outstanding_audio_bytes_ -= dest->frames() * output_bytes_per_frame_;
 
   // Due to rounding errors while multiplying against |io_ratio_|,
   // |outstanding_audio_bytes_| might (rarely) slip below zero.
@@ -315,12 +412,12 @@ int AudioOutputResampler::OnMoreIOData(AudioBus* source,
   return dest->frames();
 }
 
-void AudioOutputResampler::SourceCallback_Locked(AudioBus* dest) {
+void OnMoreDataResampler::SourceCallback_Locked(AudioBus* dest) {
   SourceIOCallback_Locked(NULL, dest);
 }
 
-void AudioOutputResampler::SourceIOCallback_Locked(
-    AudioBus* source, AudioBus* dest) {
+void OnMoreDataResampler::SourceIOCallback_Locked(AudioBus* source,
+                                                  AudioBus* dest) {
   source_lock_.AssertAcquired();
 
   // Adjust playback delay to include the state of the internal buffers used by
@@ -338,20 +435,20 @@ void AudioOutputResampler::SourceIOCallback_Locked(
   // Scale the number of frames we got back in terms of input bytes to output
   // bytes accordingly.
   outstanding_audio_bytes_ +=
-      (dest->frames() * params_.GetBytesPerFrame()) / io_ratio_;
+      (dest->frames() * input_bytes_per_frame_) / io_ratio_;
 }
 
-void AudioOutputResampler::ProvideInput(AudioBus* audio_bus) {
+void OnMoreDataResampler::ProvideInput(AudioBus* audio_bus) {
   audio_fifo_->Consume(audio_bus, audio_bus->frames());
 }
 
-void AudioOutputResampler::OnError(AudioOutputStream* stream, int code) {
+void OnMoreDataResampler::OnError(AudioOutputStream* stream, int code) {
   base::AutoLock auto_lock(source_lock_);
   if (source_callback_)
     source_callback_->OnError(stream, code);
 }
 
-void AudioOutputResampler::WaitTillDataReady() {
+void OnMoreDataResampler::WaitTillDataReady() {
   base::AutoLock auto_lock(source_lock_);
   if (source_callback_ && !outstanding_audio_bytes_)
     source_callback_->WaitTillDataReady();
