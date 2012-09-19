dbff726d786e0ca1ad002af9c1dbaf5adab0cc4f
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-19 13:04:01 +0000

    Android: uses a fifo rather than logcat for the test runner.
    - This uses the same mechanism as webkit drt.
    - It fixes the "timestamping" bug (147800), as the fifo
    order is guaranteed (as opposed to logcat).
    - It also removes AndroidLogPrinter which was injected in the gtest
    framework.
    - Downside is that we won't get inline crashstack. However, those
    were normally useless as they were not symbolized anyways.
    We can still get tombstones if we detect a crash.
    
    BUG=147800
    TEST=build/android/run_tests.py
    
    
    Review URL: https://chromiumcodereview.appspot.com/10938014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157541 0039d316-1c4b-4281-b951-d872f2087c98

--- testing/android/java/src/org/chromium/native_test/ChromeNativeTestActivity.java
+++ testing/android/java/src/org/chromium/native_test/ChromeNativeTestActivity.java
@@ -69,13 +69,11 @@ public class ChromeNativeTestActivity extends Activity {
     }
 
     private void runTests() {
-        Log.e(TAG, ">>nativeRunTests");
         // This directory is used by build/android/pylib/test_package_apk.py.
         File filesDir = new File(Environment.getExternalStorageDirectory(),
                                  "native_tests/");
         filesDir.mkdirs();
         nativeRunTests(filesDir.getAbsolutePath(), getApplicationContext());
-        Log.e(TAG, "<<nativeRunTests");
     }
 
     // Signal a failure of the native test loader to python scripts
