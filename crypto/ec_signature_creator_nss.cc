8e5cd16cbb5519baa9cc529e11326323e0907725
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-19 15:11:33 +0000

    crypto: add DecodeSignature and use SHA-256 with ECDSA.
    This changes ECSignatureCreator to use the hash function that SPDY
    expects (SHA-256). There are no other users of ECSignatureCreator in
    the tree so I'm going to defer making these choices parameters until there's
    a benefit to be had.
    
    It also adds DecodeSignature to convert from ASN.1 signatures to the `raw'
    form that SPDY needs.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/10910226
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157551 0039d316-1c4b-4281-b951-d872f2087c98

--- crypto/ec_signature_creator_nss.cc
+++ crypto/ec_signature_creator_nss.cc
@@ -24,7 +24,8 @@ namespace {
 SECStatus SignData(SECItem* result,
                    SECItem* input,
                    SECKEYPrivateKey* key,
-                   HASH_HashType hash_type) {
+                   HASH_HashType hash_type,
+                   size_t* out_signature_len) {
   if (key->keyType != ecKey) {
     DLOG(FATAL) << "Should be using an EC key.";
     PORT_SetError(SEC_ERROR_INVALID_ARGS);
@@ -49,6 +50,8 @@ SECStatus SignData(SECItem* result,
   if (rv != SECSuccess)
     return rv;
 
+  *out_signature_len = sig.len;
+
   // DER encode the signature.
   return DSAU_EncodeDerSigWithLen(result, &sig, sig.len);
 }
@@ -56,7 +59,8 @@ SECStatus SignData(SECItem* result,
 }  // namespace
 
 ECSignatureCreatorImpl::ECSignatureCreatorImpl(ECPrivateKey* key)
-    : key_(key) {
+    : key_(key),
+      signature_len_(0) {
   EnsureNSSInit();
 }
 
@@ -79,7 +83,7 @@ bool ECSignatureCreatorImpl::Sign(const uint8* data,
 
   // Sign the secret data and save it to |result|.
   SECStatus rv =
-      SignData(&result, &secret, key_->key(), HASH_AlgSHA1);
+      SignData(&result, &secret, key_->key(), HASH_AlgSHA256, &signature_len_);
   if (rv != SECSuccess) {
     DLOG(ERROR) << "DerSignData: " << PORT_GetError();
     return false;
@@ -88,6 +92,22 @@ bool ECSignatureCreatorImpl::Sign(const uint8* data,
   // Copy the signed data into the output vector.
   signature->assign(result.data, result.data + result.len);
   SECITEM_FreeItem(&result, PR_FALSE /* only free |result.data| */);
+  return true;
+}
+
+bool ECSignatureCreatorImpl::DecodeSignature(
+    const std::vector<uint8>& der_sig,
+    std::vector<uint8>* out_raw_sig) {
+  SECItem der_sig_item;
+  der_sig_item.type = siBuffer;
+  der_sig_item.len = der_sig.size();
+  der_sig_item.data = const_cast<uint8*>(&der_sig[0]);
+
+  SECItem* raw_sig = DSAU_DecodeDerSigToLen(&der_sig_item, signature_len_);
+  if (!raw_sig)
+    return false;
+  out_raw_sig->assign(raw_sig->data, raw_sig->data + raw_sig->len);
+  SECITEM_FreeItem(raw_sig, PR_TRUE /* free SECItem structure itself. */);
   return true;
 }
 
