2b702dd4d22464a369fc5b155a7443da50885615
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-23 04:51:04 +0000

    base: No need to inherit testing::Test in base64 unittest.
    TEST=base_unittests
    TBR=willchan@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10969059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158203 0039d316-1c4b-4281-b951-d872f2087c98

--- base/base64_unittest.cc
+++ base/base64_unittest.cc
@@ -1,29 +1,28 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "base/base64.h"
+
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace {
-
-class Base64Test : public testing::Test {
-};
-
-}  // namespace
+namespace base {
 
 TEST(Base64Test, Basic) {
   const std::string kText = "hello world";
   const std::string kBase64Text = "aGVsbG8gd29ybGQ=";
 
-  std::string encoded, decoded;
+  std::string encoded;
+  std::string decoded;
   bool ok;
 
-  ok = base::Base64Encode(kText, &encoded);
+  ok = Base64Encode(kText, &encoded);
   EXPECT_TRUE(ok);
   EXPECT_EQ(kBase64Text, encoded);
 
-  ok = base::Base64Decode(encoded, &decoded);
+  ok = Base64Decode(encoded, &decoded);
   EXPECT_TRUE(ok);
   EXPECT_EQ(kText, decoded);
 }
+
+}  // namespace base
