youtube/cobalt

commit d415774cc8f768d263bd1cce26bb8c7d57442ff9

Call Decryptor::Stop() in FFmpegVideoDecoder::Reset().
To avoid freeze when user trying to seek() when a DecrptCB is pending.

It also fix an issue that the stop_cb isn't fired when FFmpegVideoDecoder::Stop() is called when a DecryptCB is pending.

BUG=151708
TEST=added new unittest to catch this

Review URL: https://chromiumcodereview.appspot.com/10964055

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158457 0039d316-1c4b-4281-b951-d872f2087c98

media/base/decryptor.h
@@ -81,9 +81,9 @@ class MEDIA_EXPORT Decryptor {
   virtual void Decrypt(const scoped_refptr<DecoderBuffer>& encrypted,
                        const DecryptCB& decrypt_cb) = 0;
 
-  // Stops the decryptor and fires any pending DecryptCB immediately with kError
-  // and NULL buffer.
-  virtual void Stop() = 0;
+  // Cancel scheduled decryption operations and fires any pending DecryptCB
+  // immediately with kError and NULL buffer.
+  virtual void CancelDecrypt() = 0;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(Decryptor);

media/base/mock_filters.h
@@ -207,7 +207,7 @@ class MockDecryptor : public Decryptor {
                                       const std::string& session_id));
   MOCK_METHOD2(Decrypt, void(const scoped_refptr<DecoderBuffer>& encrypted,
                              const DecryptCB& decrypt_cb));
-  MOCK_METHOD0(Stop, void());
+  MOCK_METHOD0(CancelDecrypt, void());
 
  private:
   DISALLOW_COPY_AND_ASSIGN(MockDecryptor);

media/crypto/aes_decryptor.cc
@@ -229,7 +229,7 @@ void AesDecryptor::Decrypt(const scoped_refptr<DecoderBuffer>& encrypted,
   decrypt_cb.Run(kSuccess, decrypted);
 }
 
-void AesDecryptor::Stop() {
+void AesDecryptor::CancelDecrypt() {
 }
 
 void AesDecryptor::SetKey(const std::string& key_id,

media/crypto/aes_decryptor.h
@@ -50,7 +50,7 @@ class MEDIA_EXPORT AesDecryptor : public Decryptor {
   // |decrypt_cb|.
   virtual void Decrypt(const scoped_refptr<DecoderBuffer>& encrypted,
                        const DecryptCB& decrypt_cb) OVERRIDE;
-  virtual void Stop() OVERRIDE;
+  virtual void CancelDecrypt() OVERRIDE;
 
  private:
   // TODO(fgalligan): Remove this and change KeyMap to use crypto::SymmetricKey

media/filters/ffmpeg_video_decoder.cc
@@ -179,6 +179,9 @@ void FFmpegVideoDecoder::Reset(const base::Closure& closure) {
     return;
   }
 
+  if (decryptor_)
+    decryptor_->CancelDecrypt();
+
   reset_cb_ = closure;
 
   // Defer the reset if a read is pending.
@@ -205,7 +208,7 @@ void FFmpegVideoDecoder::Stop(const base::Closure& closure) {
   }
 
   if (decryptor_)
-    decryptor_->Stop();
+    decryptor_->CancelDecrypt();
 
   stop_cb_ = closure;
 
@@ -321,6 +324,12 @@ void FFmpegVideoDecoder::DoBufferDecrypted(
   DCHECK_NE(state_, kUninitialized);
   DCHECK_NE(state_, kDecodeFinished);
   DCHECK(!read_cb_.is_null());
+
+  if (!stop_cb_.is_null()) {
+    base::ResetAndReturn(&read_cb_).Run(kOk, NULL);
+    DoStop();
+    return;
+  }
 
   if (!reset_cb_.is_null()) {
     base::ResetAndReturn(&read_cb_).Run(kOk, NULL);

media/filters/ffmpeg_video_decoder_unittest.cc
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <deque>
 #include <string>
 #include <vector>
 
@@ -62,6 +61,10 @@ ACTION_P(ReturnBuffer, buffer) {
 
 ACTION_P2(RunDecryptCB, status, buffer) {
   arg1.Run(status, buffer);
+}
+
+ACTION_P3(RunDecryptCB3, decrypt_cb, status, buffer) {
+  decrypt_cb.Run(status, buffer);
 }
 
 class FFmpegVideoDecoderTest : public testing::Test {
@@ -233,9 +236,6 @@ class FFmpegVideoDecoderTest : public testing::Test {
   scoped_refptr<DecoderBuffer> i_frame_buffer_;
   scoped_refptr<DecoderBuffer> corrupt_i_frame_buffer_;
   scoped_refptr<DecoderBuffer> encrypted_i_frame_buffer_;
-
-  // Used for generating timestamped buffers.
-  std::deque<int64> timestamps_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(FFmpegVideoDecoderTest);
@@ -574,6 +574,30 @@ TEST_F(FFmpegVideoDecoderTest, Reset_DuringPendingRead) {
   message_loop_.RunAllPending();
 }
 
+// Test resetting when there is a pending decrypt on the decryptor.
+TEST_F(FFmpegVideoDecoderTest, Reset_DuringPendingDecrypt) {
+  InitializeWithEncryptedConfig();
+
+  EXPECT_CALL(*demuxer_, Read(_))
+      .WillRepeatedly(ReturnBuffer(encrypted_i_frame_buffer_));
+
+  Decryptor::DecryptCB decrypt_cb;
+  EXPECT_CALL(*decryptor_, Decrypt(encrypted_i_frame_buffer_, _))
+      .WillOnce(SaveArg<1>(&decrypt_cb));
+
+  decoder_->Read(read_cb_);
+  message_loop_.RunAllPending();
+  // Make sure the Read() on the decoder triggers a Decrypt() on the decryptor.
+  EXPECT_FALSE(decrypt_cb.is_null());
+
+  EXPECT_CALL(*decryptor_, CancelDecrypt())
+      .WillOnce(RunDecryptCB3(decrypt_cb, Decryptor::kError,
+                              scoped_refptr<DecoderBuffer>(NULL)));
+  EXPECT_CALL(*this, FrameReady(VideoDecoder::kOk, IsNull()));
+  Reset();
+  message_loop_.RunAllPending();
+}
+
 // Test stopping when decoder has initialized but not decoded.
 TEST_F(FFmpegVideoDecoderTest, Stop_Initialized) {
   Initialize();
@@ -606,8 +630,7 @@ TEST_F(FFmpegVideoDecoderTest, Stop_DuringPendingRead) {
   decoder_->Read(read_cb_);
   message_loop_.RunAllPending();
 
-  // Make sure the Read() on the decoder triggers a Read() on
-  // the demuxer.
+  // Make sure the Read() on the decoder triggers a Read() on the demuxer.
   EXPECT_FALSE(read_cb.is_null());
 
   Stop();
@@ -618,6 +641,29 @@ TEST_F(FFmpegVideoDecoderTest, Stop_DuringPendingRead) {
   message_loop_.RunAllPending();
 }
 
+// Test stopping when there is a pending decrypt on the decryptor.
+TEST_F(FFmpegVideoDecoderTest, Stop_DuringPendingDecrypt) {
+  InitializeWithEncryptedConfig();
+
+  EXPECT_CALL(*demuxer_, Read(_))
+      .WillRepeatedly(ReturnBuffer(encrypted_i_frame_buffer_));
+
+  Decryptor::DecryptCB decrypt_cb;
+  EXPECT_CALL(*decryptor_, Decrypt(encrypted_i_frame_buffer_, _))
+      .WillOnce(SaveArg<1>(&decrypt_cb));
+
+  decoder_->Read(read_cb_);
+  message_loop_.RunAllPending();
+  // Make sure the Read() on the decoder triggers a Decrypt() on the decryptor.
+  EXPECT_FALSE(decrypt_cb.is_null());
+
+  EXPECT_CALL(*decryptor_, CancelDecrypt())
+      .WillOnce(RunDecryptCB3(decrypt_cb, Decryptor::kError,
+                              scoped_refptr<DecoderBuffer>(NULL)));
+  EXPECT_CALL(*this, FrameReady(VideoDecoder::kOk, IsNull()));
+  Stop();
+  message_loop_.RunAllPending();
+}
 
 // Test aborted read on the demuxer stream.
 TEST_F(FFmpegVideoDecoderTest, AbortPendingRead) {

commit 0aa45e7f3962253fc6134d72a7d0afd6bc483934

Refactor ProxyDecryptor.
- Add a message loop so that we don't need the DecryptOnMessageLoop magic (and headache).
- Only supports one pending DecryptCB and removes the vector of pending callbacks.
- Fix the Seek() bug/regression introduced in r158457.
- Guard against more edge cases (see the updated unittests).
- Updated unittests.

BUG=152589
TEST=media_unittests, content_unittests, demo page plays/seeks well.


Review URL: https://chromiumcodereview.appspot.com/10979084

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159847 0039d316-1c4b-4281-b951-d872f2087c98

media/base/decryptor.h
@@ -67,7 +67,8 @@ class MEDIA_EXPORT Decryptor {
   // Decrypts the |encrypted| buffer. The decrypt status and decrypted buffer
   // are returned via the provided callback |decrypt_cb|. The |encrypted| buffer
   // must not be NULL.
-  //
+  // Decrypt() should not be called until any previous DecryptCB has completed.
+  // Thus, only one DecryptCB may be pending at a time.
   // Note that the callback maybe called synchronously or asynchronously.
   //
   // If the returned status is kSuccess, the |encrypted| buffer is successfully
@@ -81,8 +82,9 @@ class MEDIA_EXPORT Decryptor {
   virtual void Decrypt(const scoped_refptr<DecoderBuffer>& encrypted,
                        const DecryptCB& decrypt_cb) = 0;
 
-  // Cancel scheduled decryption operations and fires any pending DecryptCB
-  // immediately with kError and NULL buffer.
+  // Cancels the scheduled decryption operation and fires the pending DecryptCB
+  // immediately with kSuccess and NULL.
+  // Decrypt() should not be called again before the pending DecryptCB is fired.
   virtual void CancelDecrypt() = 0;
 
  private: