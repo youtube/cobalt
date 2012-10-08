e69ba00ec3a491bb16dce77fb52bb39a32f77bfd
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-08 00:53:10 +0000

    Add webkit tester top level script
    - Stubbed out webkit test call.  This will let us make sure
      the bot is working without being blocked on the webkit test
      script.
    
    TBR=nileshagrawal@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/11070008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160620 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ build/android/buildbot/bb_webkit_latest_webkit_tester.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -ex
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Buildbot annotator script for the Webkit latest webkit tester on the
+# webkit canary waterfall.
+
+BB_DIR="$(dirname $0)"
+BB_SRC_ROOT="$(cd  "$BB_DIR/../../.."; pwd)"
+. "$BB_DIR/buildbot_functions.sh"
+
+bb_baseline_setup "$BB_SRC_ROOT" "$@"
+bb_spawn_logcat_monitor_and_status
+bb_extract_build
+bb_reboot_phones
+# TODO(peter@): Replace this with a test call
+print "@@@BUILD_STEP TODO Run webit tests@@@"
+bb_print_logcat
