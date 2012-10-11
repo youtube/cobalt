4571554589a0efa233f1fc949d16af21f47b5886
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-11 22:24:25 +0000

    Add profile and extra_data info in VideoDecoderConfig::AsHumanReadableString.
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11087081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161422 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/video_decoder_config.cc
+++ media/base/video_decoder_config.cc
@@ -134,6 +134,7 @@ std::string VideoDecoderConfig::AsHumanReadableString() const {
   std::ostringstream s;
   s << "codec: " << codec()
     << " format: " << format()
+    << " profile: " << profile()
     << " coded size: [" << coded_size().width()
     << "," << coded_size().height() << "]"
     << " visible rect: [" << visible_rect().x()
@@ -142,7 +143,8 @@ std::string VideoDecoderConfig::AsHumanReadableString() const {
     << "," << visible_rect().height() << "]"
     << " natural size: [" << natural_size().width()
     << "," << natural_size().height() << "]"
-    << " encryption: [" << (is_encrypted() ? "true" : "false") << "]";
+    << " has extra data? " << (extra_data() ? "true" : "false")
+    << " encrypted? " << (is_encrypted() ? "true" : "false");
   return s.str();
 }
 
