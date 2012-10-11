68d75fdb8084ee33dd10d19d14cc8582e37b25e0
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-11 21:52:22 +0000

    Implicit coversion operators from integer geometry types to floating point.
    This change allows you to call a function that expects a floating point
    object with an integer object and have things just work. The addition
    operator for Points is outside the Point classes so it can add/subtract integer
    and float points together implicitly.
    
    Tested to verify compilation with:
    ui_unittests:RectTest.ToRectF
    ui_unittests:SizeTest.ToSizeF
    ui_unittests:PointTest.ToPointF
    
    BUG=147395
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/11028127
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161415 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/rect.h
+++ ui/gfx/rect.h
@@ -68,7 +68,7 @@ class UI_EXPORT Rect : public RectBase<Rect, Point, Size, Insets, int> {
   CGRect ToCGRect() const;
 #endif
 
-  RectF ToRectF() const {
+  operator RectF() const {
     return RectF(origin().x(), origin().y(), size().width(), size().height());
   }
 
@@ -77,11 +77,20 @@ class UI_EXPORT Rect : public RectBase<Rect, Point, Size, Insets, int> {
   }
 
   RectF Scale(float x_scale, float y_scale) const WARN_UNUSED_RESULT {
-    return ToRectF().Scale(x_scale, y_scale);
+    RectF original = *this;
+    return original.Scale(x_scale, y_scale);
   }
 
   std::string ToString() const;
 };
+
+inline bool operator==(const Rect& lhs, const Rect& rhs) {
+  return lhs.origin() == rhs.origin() && lhs.size() == rhs.size();
+}
+
+inline bool operator!=(const Rect& lhs, const Rect& rhs) {
+  return !(lhs == rhs);
+}
 
 #if !defined(COMPILER_MSVC)
 extern template class RectBase<Rect, Point, Size, Insets, int>;
