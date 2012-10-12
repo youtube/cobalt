12ab3b04dec4feb5ddf6a309c0f279747d0ff184
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 19:59:39 +0000

    linux: Crash browser on too-big messages to zygote.
    This adds CHECKs to the browser if it attempts to send a
    message to the zygote that exceeds the maximum message size
    (which causes an EMSGSIZE error in the zygote) or that
    contains too many file descriptors.
    
    I'm hoping that this will help make the source of the
    problem more apparent when we hit the message size limit,
    which we appear to have done multiple times (it was
    originally 1 KB and is now 2 KB).
    
    BUG=154409
    
    
    Review URL: https://chromiumcodereview.appspot.com/11108019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161645 0039d316-1c4b-4281-b951-d872f2087c98

--- base/posix/unix_domain_socket.cc
+++ base/posix/unix_domain_socket.cc
@@ -14,6 +14,8 @@
 #include "base/pickle.h"
 #include "base/stl_util.h"
 
+const size_t UnixDomainSocket::kMaxFileDescriptors = 16;
+
 // static
 bool UnixDomainSocket::SendMsg(int fd,
                                const void* buf,
@@ -52,8 +54,6 @@ ssize_t UnixDomainSocket::RecvMsg(int fd,
                                   void* buf,
                                   size_t length,
                                   std::vector<int>* fds) {
-  static const unsigned kMaxDescriptors = 16;
-
   fds->clear();
 
   struct msghdr msg;
@@ -62,7 +62,7 @@ ssize_t UnixDomainSocket::RecvMsg(int fd,
   msg.msg_iov = &iov;
   msg.msg_iovlen = 1;
 
-  char control_buffer[CMSG_SPACE(sizeof(int) * kMaxDescriptors)];
+  char control_buffer[CMSG_SPACE(sizeof(int) * kMaxFileDescriptors)];
   msg.msg_control = control_buffer;
   msg.msg_controllen = sizeof(control_buffer);
 
