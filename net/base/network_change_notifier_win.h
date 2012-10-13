02bfff8dab29394c373a43a39b27e74b5562a5af
pauljensen@chromium.org <pauljensen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-13 16:52:32 +0000

    Make NetworkChangeNotifierWin poll more than once when looking for offline to online transitions.  Histogram data suggests that continued polling greatly aides in detecting slow transitions to online.
    BUG=124069
    
    Review URL: https://chromiumcodereview.appspot.com/10984052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161764 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/network_change_notifier_win.h
+++ net/base/network_change_notifier_win.h
@@ -93,6 +93,12 @@ class NET_EXPORT_PRIVATE NetworkChangeNotifierWin
   // Thread on which we can run DnsConfigService.
   scoped_ptr<DnsConfigServiceThread> dns_config_service_thread_;
 
+  // Result of IsOffline() when NotifyObserversOfConnectionTypeChange()
+  // was last called.
+  bool last_announced_offline_;
+  // Number of times polled to check if still offline.
+  int offline_polls_;
+
   DISALLOW_COPY_AND_ASSIGN(NetworkChangeNotifierWin);
 };
 
