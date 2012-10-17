82c48a1802ba9ee41c845bcc8f39129bda452afb
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-17 18:09:03 +0000

    Add QuicFramer and friends.
    Also fixes an unsigned constant problem from 162259.
    
    Revert 162263 - Revert 162259 - Add QuicFramer and friends.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11188017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162462 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/quic/crypto/null_encrypter.h
@@ -0,0 +1,30 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_QUIC_CRYPTO_NULL_ENCRYPTER_H_
+#define NET_QUIC_CRYPTO_NULL_ENCRYPTER_H_
+
+#include "base/compiler_specific.h"
+#include "net/base/net_export.h"
+#include "net/quic/crypto/quic_encrypter.h"
+
+namespace net {
+
+// A NullEncrypter is a QuicEncrypter used before a crypto negotiation
+// has occurred.  It does not actually encrypt the payload, but does
+// generate a MAC (fnv128) over both the payload and associated data.
+class NET_EXPORT_PRIVATE NullEncrypter : public QuicEncrypter {
+ public:
+  virtual ~NullEncrypter() {}
+
+  // QuicEncrypter implementation
+  virtual QuicData* Encrypt(base::StringPiece associated_data,
+                            base::StringPiece plaintext) OVERRIDE;
+  virtual size_t GetMaxPlaintextSize(size_t ciphertext_size) OVERRIDE;
+  virtual size_t GetCiphertextSize(size_t plaintext_size) OVERRIDE;
+};
+
+}  // namespace net
+
+#endif  // NET_QUIC_CRYPTO_NULL_ENCRYPTER_H_
