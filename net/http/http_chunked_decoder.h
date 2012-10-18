3791f5e326c933412c5964261d7f3358be8be6e6
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 19:18:00 +0000

    Fix a crash when a line containing the length of an HTTP chunk-encoded response is too long.
    R=willchan@chromium.org
    BUG=155711
    
    Review URL: https://chromiumcodereview.appspot.com/11191003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162756 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_chunked_decoder.h
+++ net/http/http_chunked_decoder.h
@@ -77,6 +77,10 @@ namespace net {
 //
 class NET_EXPORT_PRIVATE HttpChunkedDecoder {
  public:
+  // The maximum length of |line_buf_| between calls to FilterBuff().
+  // Exposed for tests.
+  static const size_t kMaxLineBufLen;
+
   HttpChunkedDecoder();
 
   // Indicates that a previous call to FilterBuf encountered the final CRLF.
