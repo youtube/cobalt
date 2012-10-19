f54929ab3790ded0d34ccb9a4070b7f406661b06
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-19 23:16:11 +0000

    Temporarily revert clang back to 163674 on android
    Patch co-authored with ilevy.
    Logic:
    $ANDROID_SDK_VERSION will fix dev and clank internal bots, which don't have android in path.  It will also fix fyi bot, for now.
    PWD matching 'android' will fix trybots
    PWD matching 'Android' will keep FYI bot working if updateclang starts getting calling in update stage (if we start using gclient_env, like trybots do)
    
    BUG=156947
    
    Review URL: https://codereview.chromium.org/11232020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163093 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/clang/scripts/update.sh
+++ tools/clang/scripts/update.sh
@@ -8,7 +8,13 @@
 # Do NOT CHANGE this if you don't know what you're doing -- see
 # https://code.google.com/p/chromium/wiki/UpdatingClang
 # Reverting problematic clang rolls is safe, though.
-CLANG_REVISION=165787
+if [[ $ANDROID_SDK_VERSION || $PWD =~ [Aa]ndroid ]]; then
+  # Temporarily revert the last clang roll on android due to
+  # http://crbug.com/156947
+  CLANG_REVISION=163674
+else
+  CLANG_REVISION=165787
+fi
 
 THIS_DIR="$(dirname "${0}")"
 LLVM_DIR="${THIS_DIR}/../../../third_party/llvm"
