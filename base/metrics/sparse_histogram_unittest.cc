e7030336da29931c2d4c410b33679d36caa73e4b
kaiwang@chromium.org <kaiwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-20 02:56:18 +0000

    Move SnapshotSamples to HistogramBase
    BUG=139612
    
    
    Review URL: https://chromiumcodereview.appspot.com/11192077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163138 0039d316-1c4b-4281-b951-d872f2087c98

--- base/metrics/sparse_histogram_unittest.cc
+++ base/metrics/sparse_histogram_unittest.cc
@@ -20,18 +20,18 @@ class SparseHistogramTest : public testing::Test {
 
 TEST_F(SparseHistogramTest, BasicTest) {
   scoped_ptr<SparseHistogram> histogram(NewSparseHistogram("Sparse1"));
-  scoped_ptr<SampleMap> snapshot(histogram->SnapshotSamples());
+  scoped_ptr<HistogramSamples> snapshot(histogram->SnapshotSamples());
   EXPECT_EQ(0, snapshot->TotalCount());
   EXPECT_EQ(0, snapshot->sum());
 
   histogram->Add(100);
-  scoped_ptr<SampleMap> snapshot1(histogram->SnapshotSamples());
+  scoped_ptr<HistogramSamples> snapshot1(histogram->SnapshotSamples());
   EXPECT_EQ(1, snapshot1->TotalCount());
   EXPECT_EQ(1, snapshot1->GetCount(100));
 
   histogram->Add(100);
   histogram->Add(101);
-  scoped_ptr<SampleMap> snapshot2(histogram->SnapshotSamples());
+  scoped_ptr<HistogramSamples> snapshot2(histogram->SnapshotSamples());
   EXPECT_EQ(3, snapshot2->TotalCount());
   EXPECT_EQ(2, snapshot2->GetCount(100));
   EXPECT_EQ(1, snapshot2->GetCount(101));
