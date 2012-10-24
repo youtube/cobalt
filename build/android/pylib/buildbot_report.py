5df56bd4672c4d64a560a220c714e10efbaeaed4
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-24 11:18:31 +0000

    Android: upstream latest changes for build/android.
    - add instrumentation test options for printing buildbot failures;
    - allow test_apk option to point to a fully qualified path;
    
    BUG=136980
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11231076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163810 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/buildbot_report.py
+++ build/android/pylib/buildbot_report.py
@@ -44,3 +44,11 @@ def PrintWarning():
 
 def PrintNamedStep(step):
   print '@@@BUILD_STEP %s@@@' % step
+
+
+def PrintStepResultIfNeeded(options, result):
+  if result:
+    if options.buildbot_step_failure:
+      PrintError()
+    else:
+      PrintWarning()
