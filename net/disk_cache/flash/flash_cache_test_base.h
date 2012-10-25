7aa541167a0d5c0362188cd4d065230b48ede97c
agayev@chromium.org <agayev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-25 19:09:18 +0000

    Cache backend for devices with flash storage
    Fixed tests.
    
    BUG=157187
    TEST=net_unittests --gtest_filter="FlashCacheTest.*" --gtest_repeat=10  --shuffle
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=164064
    
    Review URL: https://chromiumcodereview.appspot.com/11269008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164133 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/disk_cache/flash/flash_cache_test_base.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_DISK_CACHE_DISK_CACHE_FLASH_TEST_BASE_H_
+#define NET_DISK_CACHE_DISK_CACHE_FLASH_TEST_BASE_H_
+
+#include "base/basictypes.h"
+#include "base/compiler_specific.h"
+#include "base/scoped_temp_dir.h"
+#include "base/memory/scoped_ptr.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace disk_cache {
+
+class Storage;
+
+}  // namespace disk_cache
+
+class FlashCacheTest : public testing::Test {
+ protected:
+  FlashCacheTest();
+  virtual ~FlashCacheTest();
+
+  virtual void SetUp() OVERRIDE;
+  virtual void TearDown() OVERRIDE;
+
+  scoped_ptr<disk_cache::Storage> storage_;
+  ScopedTempDir temp_dir_;
+  int32 num_segments_in_storage_;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(FlashCacheTest);
+};
+
+#endif  // NET_DISK_CACHE_DISK_CACHE_FLASH_TEST_BASE_H_
