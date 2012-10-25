6ff3b8c5435f1c64561af5c33a6821203cedb4ff
digit@chromium.org <digit@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-25 16:28:44 +0000

    Fix Android cryptographic key pair storage.
    This patch fixes an issue with the way public/private key pairs
    that were generated from keygen are installed on the system.
    
    - First, the generated data bytes were not in a format that
      the system supports.
    
    - Second, the public and private key pairs were swapped when
      they were sent to the CertInstaller activity.
    
    This fixes both issues at the same time.
    
    BUG=124660
    
    
    Review URL: https://chromiumcodereview.appspot.com/11260015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164088 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/openssl_private_key_store_android.cc
+++ net/base/openssl_private_key_store_android.cc
@@ -5,6 +5,7 @@
 #include "net/base/openssl_private_key_store.h"
 
 #include <openssl/evp.h>
+#include <openssl/x509.h>
 
 #include "base/logging.h"
 #include "base/memory/singleton.h"
@@ -19,19 +20,30 @@ class OpenSSLKeyStoreAndroid : public OpenSSLPrivateKeyStore {
  public:
   ~OpenSSLKeyStoreAndroid() {}
 
-  // TODO(joth): Use the |url| to help identify this key to the user.
-  // Currently Android has no UI to list these stored private keys (and no
-  // API to associate a name with them), so this is a non-issue.
   virtual bool StorePrivateKey(const GURL& url, EVP_PKEY* pkey) {
-    uint8* public_key = NULL;
-    int public_len = i2d_PublicKey(pkey, &public_key);
-    uint8* private_key = NULL;
-    int private_len = i2d_PrivateKey(pkey, &private_key);
+    // Always clear openssl errors on exit.
+    crypto::OpenSSLErrStackTracer err_trace(FROM_HERE);
 
+    // Important: Do not use i2d_PublicKey() here, which returns data in
+    // PKCS#1 format, use i2d_PUBKEY() which returns it as DER-encoded
+    // SubjectPublicKeyInfo (X.509), as expected by the platform.
+    unsigned char* public_key = NULL;
+    int public_len = i2d_PUBKEY(pkey, &public_key);
+
+    // Important: Do not use i2d_PrivateKey() here, it returns data
+    // in a format that is incompatible with what the platform expects.
+    unsigned char* private_key = NULL;
+    int private_len = 0;
+    crypto::ScopedOpenSSL<PKCS8_PRIV_KEY_INFO,
+        PKCS8_PRIV_KEY_INFO_free> pkcs8(EVP_PKEY2PKCS8(pkey));
+    if (pkcs8.get() != NULL) {
+      private_len = i2d_PKCS8_PRIV_KEY_INFO(pkcs8.get(), &private_key);
+    }
     bool ret = false;
-    if (public_len && private_len) {
-      ret = net::android::StoreKeyPair(public_key, public_len, private_key,
-                                       private_len);
+    if (public_len > 0 && private_len > 0) {
+      ret = net::android::StoreKeyPair(
+            static_cast<const uint8*>(public_key), public_len,
+            static_cast<const uint8*>(private_key), private_len);
     }
     LOG_IF(ERROR, !ret) << "StorePrivateKey failed. pub len = " << public_len
                         << " priv len = " << private_len;
