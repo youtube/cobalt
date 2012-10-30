f0366e83bc91db479dd2cd9d6d4bcac4c501bf1a
palmer@chromium.org <palmer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-30 20:39:35 +0000

    Add an HSTS preload for another alias of Chromium Rietveld.
    BUG=158522
    TEST=Navigate to codereview.chromium.org and expect to get https://.
    Review URL: https://codereview.chromium.org/11346037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164999 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/transport_security_state_static.h
+++ net/base/transport_security_state_static.h
@@ -325,6 +325,7 @@ static const struct HSTSPreload kPreloadedSTS[] = {
   {17, true, "\004apis\006google\003com", true, kGooglePins, DOMAIN_GOOGLE_COM },
   {32, true, "\022chromiumcodereview\007appspot\003com", true, kGooglePins, DOMAIN_APPSPOT_COM },
   {24, true, "\012codereview\007appspot\003com", true, kGooglePins, DOMAIN_APPSPOT_COM },
+  {25, true, "\012codereview\010chromium\003org", true, kGooglePins, DOMAIN_APPSPOT_COM },
   {23, true, "\005chart\004apis\006google\003com", false, kGooglePins, DOMAIN_GOOGLE_COM },
   {11, true, "\005ytimg\003com", false, kGooglePins, DOMAIN_YTIMG_COM },
   {23, true, "\021googleusercontent\003com", false, kGooglePins, DOMAIN_GOOGLEUSERCONTENT_COM },
