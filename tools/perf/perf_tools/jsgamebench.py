27f7e87fa2ee4e43f58d107aefc8b9d456a3e33d
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-02 00:07:30 +0000

    Automate JSGameBench with Chrome Remote Control.
    BUG=124813
    TEST=Manual on linux
    
    Review URL: https://codereview.chromium.org/11364037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165561 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/perf/perf_tools/jsgamebench.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from chrome_remote_control import multi_page_benchmark
+from chrome_remote_control import util
+
+class JsGameBench(multi_page_benchmark.MultiPageBenchmark):
+  def MeasurePage(self, _, tab, results):
+    tab.runtime.Execute('UI.call({}, "perftest")')
+
+    js_is_done = 'document.getElementById("perfscore0") != null'
+    def _IsDone():
+      return bool(tab.runtime.Evaluate(js_is_done))
+    util.WaitFor(_IsDone, 600)
+
+    js_get_results = 'document.getElementById("perfscore0").innerHTML'
+    result = int(tab.runtime.Evaluate(js_get_results))
+    results.Add('Score', 'score (bigger is better)', result)
