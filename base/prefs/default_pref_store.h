c8fbb218148eb44b41d2b4f252a367061202fdc4
aberent@chromium.org <aberent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-07 18:06:02 +0000

    Rewrite DefaultPrefStore for performance
    Derive directly from PrefStore, and ignore observers
    since they can't usefully observe anything. Also remove now redundant members
    from ValueMapPrefStore.
    
    BUG=158330
    
    
    Review URL: https://chromiumcodereview.appspot.com/11358123
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166469 0039d316-1c4b-4281-b951-d872f2087c98

--- base/prefs/default_pref_store.h
+++ base/prefs/default_pref_store.h
@@ -8,14 +8,20 @@
 #include <string>
 
 #include "base/prefs/base_prefs_export.h"
-#include "base/prefs/value_map_pref_store.h"
+#include "base/prefs/pref_store.h"
+#include "base/prefs/pref_value_map.h"
 #include "base/values.h"
 
 // This PrefStore keeps track of default preference values set when a
 // preference is registered with the PrefService.
-class BASE_PREFS_EXPORT DefaultPrefStore : public ValueMapPrefStore {
+class BASE_PREFS_EXPORT DefaultPrefStore : public PrefStore {
  public:
+  typedef PrefValueMap::const_iterator const_iterator;
+
   DefaultPrefStore();
+
+  virtual ReadResult GetValue(const std::string& key,
+                              const base::Value** result) const OVERRIDE;
 
   // Stores a new |value| for |key|. Assumes ownership of |value|.
   void SetDefaultValue(const std::string& key, Value* value);
@@ -27,10 +33,15 @@ class BASE_PREFS_EXPORT DefaultPrefStore : public ValueMapPrefStore {
   // has not been registered.
   base::Value::Type GetType(const std::string& key) const;
 
+  const_iterator begin() const;
+  const_iterator end() const;
+
  protected:
   virtual ~DefaultPrefStore();
 
  private:
+  PrefValueMap prefs_;
+
   DISALLOW_COPY_AND_ASSIGN(DefaultPrefStore);
 };
 
