1cbb87a1316a6e2aafe6b6303c66f9530c38e367
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-07 20:41:29 +0000

    Add a Chrome-specific QuicConnectionHelper class.
    Review URL: https://chromiumcodereview.appspot.com/11273066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166514 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/quic/quic_connection_helper.h
@@ -0,0 +1,77 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// The Chrome-specific helper for QuicConnection which uses
+// a TaskRunner for alarms, and uses a DatagramClientSocket for writing data.
+
+#ifndef NET_QUIC_QUIC_CONNECTION_HELPER_H_
+#define NET_QUIC_QUIC_CONNECTION_HELPER_H_
+
+#include "net/quic/quic_connection.h"
+
+#include <set>
+
+#include "base/memory/weak_ptr.h"
+#include "net/base/ip_endpoint.h"
+#include "net/quic/quic_clock.h"
+#include "net/quic/quic_protocol.h"
+#include "net/udp/datagram_client_socket.h"
+
+namespace base {
+class TaskRunner;
+}  // namespace base
+
+namespace net {
+
+class NET_EXPORT_PRIVATE QuicConnectionHelper
+    : public QuicConnectionHelperInterface {
+ public:
+  QuicConnectionHelper(base::TaskRunner* task_runner,
+                       QuicClock* clock,
+                       DatagramClientSocket* socket);
+  virtual ~QuicConnectionHelper();
+
+  // QuicConnectionHelperInterface
+  virtual void SetConnection(QuicConnection* connection) OVERRIDE;
+  virtual QuicClock* GetClock() OVERRIDE;
+  virtual int WritePacketToWire(QuicPacketSequenceNumber number,
+                                const QuicEncryptedPacket& packet,
+                                bool resend,
+                                int* error) OVERRIDE;
+  virtual void SetResendAlarm(QuicPacketSequenceNumber sequence_number,
+                              uint64 delay_in_us) OVERRIDE;
+  virtual void SetSendAlarm(uint64 delay_in_us) OVERRIDE;
+  virtual void SetTimeoutAlarm(uint64 delay_in_us) OVERRIDE;
+  virtual bool IsSendAlarmSet() OVERRIDE;
+  virtual void UnregisterSendAlarmIfRegistered() OVERRIDE;
+
+  // An alarm is scheduled for each data-bearing packet as it is sent out.
+  // When the alarm goes off, the connection checks to see if the packet has
+  // been acked, and resends if it has not.
+  void OnResendAlarm(QuicPacketSequenceNumber sequence_number);
+  // An alarm that is scheduled when the sent scheduler requires a
+  // a delay before sending packets and fires when the packet may be sent.
+  void OnSendAlarm();
+  // An alarm which fires when the connection may have timed out.
+  void OnTimeoutAlarm();
+
+ private:
+  friend class QuicConnectionHelperPeer;
+
+  base::WeakPtrFactory<QuicConnectionHelper> weak_factory_;
+
+  base::TaskRunner* task_runner_;
+  DatagramClientSocket* socket_;
+  QuicConnection* connection_;
+  QuicClock* clock_;
+
+  bool send_alarm_registered_;
+  bool timeout_alarm_registered_;
+
+  DISALLOW_COPY_AND_ASSIGN(QuicConnectionHelper);
+};
+
+}  // namespace net
+
+#endif  // NET_QUIC_QUIC_CONNECTION_HELPER_H_
