4efe966e071e02b0d200f792087844e3d26973fd
pastarmovj@google.com <pastarmovj@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-07 08:49:12 +0000

    Revert 166365 - roll clang 165787:167488
    Reverted because it is assumed to be the cause for multiple compile failures on  mac and linux.
    
    Fixes crash in arm codegen.
    Doesn't warn on -Wno-arc-abi.
    Nicer diagnostics for nested macro instantiations.
    Includes asan support for android on linux (shout if you need that on mac).
    Fixes issue with debug information in .S files.
    Renames -faddress-sanitizer to -fsanitize=address (continue to use -faddress-sanitizer in chromium for now, until we've updated all third-party gyp files)
    
    BUG=159712,159452,159475,159502,159598,59580
    TBR=hans
    
    Review URL: https://codereview.chromium.org/11361133
    
    TBR=thakis@chromium.org
    Review URL: https://codereview.chromium.org/11377020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166369 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/clang/scripts/update.sh
+++ tools/clang/scripts/update.sh
@@ -13,7 +13,7 @@ if [[ $ANDROID_SDK_VERSION || $PWD =~ [Aa]ndroid ]]; then
   # http://crbug.com/156947
   CLANG_REVISION=163674
 else
-  CLANG_REVISION=167488
+  CLANG_REVISION=165787
 fi
 
 THIS_DIR="$(dirname "${0}")"
