8666b03665399553f0208d83abfc805bf2c0fa6d
aberent@chromium.org <aberent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-07 18:06:02 +0000

    Rewrite DefaultPrefStore for performance
    Derive directly from PrefStore, and ignore observers
    since they can't usefully observe anything. Also remove now redundant members
    from ValueMapPrefStore.
    
    BUG=158330
    
    
    Review URL: https://chromiumcodereview.appspot.com/11358123
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166469 0039d316-1c4b-4281-b951-d872f2087c98

--- base/prefs/default_pref_store.cc
+++ base/prefs/default_pref_store.cc
@@ -3,24 +3,39 @@
 // found in the LICENSE file.
 
 #include "base/prefs/default_pref_store.h"
+#include "base/logging.h"
 
 using base::Value;
 
 DefaultPrefStore::DefaultPrefStore() {}
 
+PrefStore::ReadResult DefaultPrefStore::GetValue(
+    const std::string& key,
+    const base::Value** result) const {
+  return prefs_.GetValue(key, result) ? READ_OK : READ_NO_VALUE;
+}
+
 void DefaultPrefStore::SetDefaultValue(const std::string& key, Value* value) {
   CHECK(GetValue(key, NULL) == READ_NO_VALUE);
-  SetValue(key, value);
+  prefs_.SetValue(key, value);
 }
 
 void DefaultPrefStore::RemoveDefaultValue(const std::string& key) {
   CHECK(GetValue(key, NULL) == READ_OK);
-  RemoveValue(key);
+  prefs_.RemoveValue(key);
 }
 
 base::Value::Type DefaultPrefStore::GetType(const std::string& key) const {
   const Value* value;
   return GetValue(key, &value) == READ_OK ? value->GetType() : Value::TYPE_NULL;
+}
+
+DefaultPrefStore::const_iterator DefaultPrefStore::begin() const {
+  return prefs_.begin();
+}
+
+DefaultPrefStore::const_iterator DefaultPrefStore::end() const {
+  return prefs_.end();
 }
 
 DefaultPrefStore::~DefaultPrefStore() {}
