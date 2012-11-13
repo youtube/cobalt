f049a29223bab5a8d6b94881d3e59710faeda351
blundell@chromium.org <blundell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-13 14:39:19 +0000

    Fix TestGetExtensionsForMimeType unittest on iOS.
    On iOS, "mpeg" is not one of the extensions for "video/*", as the mime type
    "video/mpeg" is mapped to the extension "mpg".
    
    
    Review URL: https://chromiumcodereview.appspot.com/11360190
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167389 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/mime_util_unittest.cc
+++ net/base/mime_util_unittest.cc
@@ -240,7 +240,7 @@ TEST(MimeUtilTest, TestGetExtensionsForMimeType) {
     { "MeSsAge/*",  1, "eml" },
     { "image/bmp",  1, "bmp" },
     { "video/*",    6, "mp4" },
-#if defined(OS_LINUX) || defined(OS_ANDROID)
+#if defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_IOS)
     { "video/*",    6, "mpg" },
 #else
     { "video/*",    6, "mpeg" },
