515f478a022f763927a78f355e80235e441843d3
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-15 13:02:43 +0000

    Telemetry multi page testing FW: Add a proof-of-concept memory test.
    We currently measure memory usage in page cycler tests, but we'd
    need more realistic and longer running tests to get better
    memory statistics.
    
    The test works against Web Page Replay data, and uses Telemetry to
    execute actions on pages.
    
    Actual tests still need to be added.
    
    BUG=158323
    
    Review URL: https://codereview.chromium.org/11273081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167908 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/perf/perf_tools/memory_benchmark.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+from telemetry import multi_page_benchmark
+
+MEMORY_HISTOGRAMS = [
+    {'name': 'V8.MemoryExternalFragmentationTotal', 'units': 'percent'},
+    {'name': 'V8.MemoryHeapSampleTotalCommitted', 'units': 'kb'},
+    {'name': 'V8.MemoryHeapSampleTotalUsed', 'units': 'kb'}]
+
+class MemoryBenchmark(multi_page_benchmark.MultiPageBenchmark):
+  def __init__(self):
+    super(MemoryBenchmark, self).__init__('stress_memory')
+
+  def CustomizeBrowserOptions(self, options):
+    options.AppendExtraBrowserArg('--dom-automation')
+
+  def CanRunForPage(self, page):
+    return hasattr(page, 'stress_memory')
+
+  def MeasurePage(self, page, tab, results):
+    for histogram in MEMORY_HISTOGRAMS:
+      name = histogram['name']
+      data = tab.runtime.Evaluate(
+          'window.domAutomationController.getHistogram("%s")' % name)
+      results.Add(name, histogram['units'], data, data_type='histogram')
