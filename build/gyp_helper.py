3cadac46d58b2a3218f00a88888a2feb155a65d5
iannucci@chromium.org <iannucci@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-15 01:51:54 +0000

    Use universal newlines in gyp_helper.
    This avoids a stack trace for developers who have dos line endings in their
    chromium.gyp_env files.
    
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11293286
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167817 0039d316-1c4b-4281-b951-d872f2087c98

--- build/gyp_helper.py
+++ build/gyp_helper.py
@@ -15,7 +15,7 @@ def apply_gyp_environment_from_file(file_path):
   """Reads in a *.gyp_env file and applies the valid keys to os.environ."""
   if not os.path.exists(file_path):
     return
-  with open(file_path) as f:
+  with open(file_path, 'rU') as f:
     file_contents = f.read()
   try:
     file_data = eval(file_contents, {'__builtins__': None}, None)
