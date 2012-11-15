43451db056f56256a55e30a634fc9ce472de8432
markusheintz@chromium.org <markusheintz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-15 17:54:20 +0000

    The "Create" method for creating a CanonicalCookie from a ParsedCookie uses the wrong default value for cookie-domain and for cookie-path. Apply the correct default values. 1) By default a cookie with no explicit cookie-domain attribute should be a "host-only" cookie. 2) By default the path of the request URL should be used for cookies with no explicit cookie-path attribute.
    Move logic for validating cookie attributes from the CookieMonster to the CanonicalCookie. This way code outside the CookieMonster (e.g. BrowsingDataCookieHelper) can use the same code path.
    
    TEST=CanonicalCookieTest.Create
    BUG=160993
    
    Review URL: https://chromiumcodereview.appspot.com/11341045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167950 0039d316-1c4b-4281-b951-d872f2087c98

--- net/cookies/canonical_cookie_unittest.cc
+++ net/cookies/canonical_cookie_unittest.cc
@@ -4,7 +4,9 @@
 
 #include "net/cookies/canonical_cookie.h"
 
+#include "base/memory/scoped_ptr.h"
 #include "googleurl/src/gurl.h"
+#include "net/cookies/cookie_options.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace net {
@@ -37,6 +39,75 @@ TEST(CanonicalCookieTest, GetCookieSourceFromURL) {
   EXPECT_EQ("http://example.com/",
             CanonicalCookie::GetCookieSourceFromURL(
                 GURL("http://example.com/test#foo")));
+}
+
+TEST(CanonicalCookieTest, Constructor) {
+  GURL url("http://www.example.com/test");
+  base::Time current_time = base::Time::Now();
+
+  CanonicalCookie cookie(url, "A", "2", "www.example.com", "/test", "", "",
+                         current_time, base::Time(), current_time, false,
+                         false);
+  EXPECT_EQ(url.GetOrigin().spec(), cookie.Source());
+  EXPECT_EQ("A", cookie.Name());
+  EXPECT_EQ("2", cookie.Value());
+  EXPECT_EQ("www.example.com", cookie.Domain());
+  EXPECT_EQ("/test", cookie.Path());
+  EXPECT_FALSE(cookie.IsSecure());
+
+  CanonicalCookie cookie2(url, "A", "2", "", "", "", "", current_time,
+                          base::Time(), current_time, false, false);
+  EXPECT_EQ(url.GetOrigin().spec(), cookie.Source());
+  EXPECT_EQ("A", cookie2.Name());
+  EXPECT_EQ("2", cookie2.Value());
+  EXPECT_EQ("", cookie2.Domain());
+  EXPECT_EQ("", cookie2.Path());
+  EXPECT_FALSE(cookie2.IsSecure());
+
+}
+
+TEST(CanonicalCookieTest, Create) {
+  GURL url("http://www.example.com/test/foo.html");
+  base::Time creation_time = base::Time::Now();
+  CookieOptions options;
+
+  scoped_ptr<CanonicalCookie> cookie(
+        CanonicalCookie::Create(url, "A=2", creation_time, options));
+  EXPECT_EQ(url.GetOrigin().spec(), cookie->Source());
+  EXPECT_EQ("A", cookie->Name());
+  EXPECT_EQ("2", cookie->Value());
+  EXPECT_EQ("www.example.com", cookie->Domain());
+  EXPECT_EQ("/test", cookie->Path());
+  EXPECT_FALSE(cookie->IsSecure());
+
+  GURL url2("http://www.foo.com");
+  cookie.reset(CanonicalCookie::Create(url2, "B=1", creation_time, options));
+  EXPECT_EQ(url2.GetOrigin().spec(), cookie->Source());
+  EXPECT_EQ("B", cookie->Name());
+  EXPECT_EQ("1", cookie->Value());
+  EXPECT_EQ("www.foo.com", cookie->Domain());
+  EXPECT_EQ("/", cookie->Path());
+  EXPECT_FALSE(cookie->IsSecure());
+
+  cookie.reset(CanonicalCookie::Create(
+      url, "A", "2", "www.example.com", "/test", "", "", creation_time,
+      base::Time(), false, false));
+  EXPECT_EQ(url.GetOrigin().spec(), cookie->Source());
+  EXPECT_EQ("A", cookie->Name());
+  EXPECT_EQ("2", cookie->Value());
+  EXPECT_EQ(".www.example.com", cookie->Domain());
+  EXPECT_EQ("/test", cookie->Path());
+  EXPECT_FALSE(cookie->IsSecure());
+
+  cookie.reset(CanonicalCookie::Create(
+      url, "A", "2", ".www.example.com", "/test", "", "", creation_time,
+      base::Time(), false, false));
+  EXPECT_EQ(url.GetOrigin().spec(), cookie->Source());
+  EXPECT_EQ("A", cookie->Name());
+  EXPECT_EQ("2", cookie->Value());
+  EXPECT_EQ(".www.example.com", cookie->Domain());
+  EXPECT_EQ("/test", cookie->Path());
+  EXPECT_FALSE(cookie->IsSecure());
 }
 
 }  // namespace net
