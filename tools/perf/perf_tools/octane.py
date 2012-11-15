5b5d6b776c567bc274fd14c9cf690318d199ea70
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-15 04:40:02 +0000

    Telemetry: adds v8 octane benchmark.
    As part of revamping the android infrastructure, we'll be running v8 octane
    benchmark using telemetry (formerly known as "chrome remote control") so that we
    can have dashboards for it in all chrome platforms.
    
    BUG=147652
    TEST=tools/perf/run_multipage_benchmarks --show-stdout --browser=system octane tools/perf/page_sets/octane/octane.json
    
    
    Review URL: https://chromiumcodereview.appspot.com/11275226
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167857 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/perf/perf_tools/octane.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from telemetry import multi_page_benchmark
+from telemetry import util
+
+class Octane(multi_page_benchmark.MultiPageBenchmark):
+  def MeasurePage(self, _, tab, results):
+    js_is_done = """
+completed && !document.getElementById("progress-bar-container")"""
+    def _IsDone():
+      return bool(tab.runtime.Evaluate(js_is_done))
+    util.WaitFor(_IsDone, 300, poll_interval=5)
+
+    js_get_results = """
+var results = {}
+var result_divs = document.querySelectorAll('.p-result');
+for (var r in result_divs) {
+  if (result_divs[r].id && result_divs[r].id.indexOf('Result-') == 0)
+    results[result_divs[r].id] = result_divs[r].innerHTML;
+}
+var main_banner = document.getElementById("main-banner").innerHTML;
+var octane_score = main_banner.substr(main_banner.lastIndexOf(':') + 2);
+results['Result-Octane'] = octane_score;
+JSON.stringify(results);
+"""
+    result_dict = eval(tab.runtime.Evaluate(js_get_results))
+    for key, value in result_dict.iteritems():
+      if value == '...':
+        continue
+      results.Add(key, '', value)
