e334b678aa39e35ad53c6388f56ccc23390583a7
agayev@chromium.org <agayev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-16 19:50:41 +0000

    Another approach to adding the layer for managing segment objects.
    BUG=157187
    TEST=net_unittests --gtest_filter="FlashCacheTest.*" --gtest_repeat=10 --shuffle
    
    
    Review URL: https://chromiumcodereview.appspot.com/11361113
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168265 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/flash/format.h
+++ net/disk_cache/flash/format.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NET_DISK_CACHE_FLASH_FORMAT_H
-#define NET_DISK_CACHE_FLASH_FORMAT_H
+#ifndef NET_DISK_CACHE_FLASH_FORMAT_H_
+#define NET_DISK_CACHE_FLASH_FORMAT_H_
 
 namespace disk_cache {
 
@@ -20,7 +20,8 @@ const size_t kFlashMaxEntryCount = kFlashSegmentSize / kFlashSmallEntrySize - 1;
 // containing a counter specifying the number of saved offsets followed by the
 // offsets.
 const int32 kFlashSummarySize = (1 + kFlashMaxEntryCount) * sizeof(int32);
+const int32 kFlashSegmentFreeSpace = kFlashSegmentSize - kFlashSummarySize;
 
 }  // namespace disk_cache
 
-#endif  // NET_DISK_CACHE_FLASH_FORMAT_H
+#endif  // NET_DISK_CACHE_FLASH_FORMAT_H_
