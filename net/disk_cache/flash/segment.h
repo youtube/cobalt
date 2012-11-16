6064ac78f74db938a59ec8aa96620ffee9acd9a0
agayev@chromium.org <agayev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-16 19:50:41 +0000

    Another approach to adding the layer for managing segment objects.
    BUG=157187
    TEST=net_unittests --gtest_filter="FlashCacheTest.*" --gtest_repeat=10 --shuffle
    
    
    Review URL: https://chromiumcodereview.appspot.com/11361113
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168265 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/flash/segment.h
+++ net/disk_cache/flash/segment.h
@@ -9,7 +9,6 @@
 
 #include "base/basictypes.h"
 #include "base/gtest_prod_util.h"
-#include "base/memory/ref_counted.h"
 #include "net/base/net_export.h"
 
 namespace disk_cache {
@@ -41,8 +40,8 @@ class Storage;
 // mutating functions cannot be called on the object after that.
 //
 // Segment can only be used as a log, i.e. all writes are laid out sequentially
-// on a segment.  As a result, WriteData() function does not take an offset, but
-// can return an offset of where the write took place.
+// on a segment.  As a result, WriteData() function does not take an offset.
+// Current write offset can be learned by calling write_offset().
 //
 // Once the entries are written to the Segment and Close() called on it and the
 // object destroyed, we should later be able to instantiate a read-only Segment
@@ -50,14 +49,14 @@ class Storage;
 // achieve this, a tiny region of Segment is used for its metadata and Segment
 // provides two calls for interacting with metadata: StoreOffset() and
 // GetOffsets().  The former can be used to store an offset that was returned by
-// WriteData() and the latter can be used to retrieve all the offsets that were
-// stored in the Segment.  Before attempting to write an entry, the client
+// write_offset() and the latter can be used to retrieve all the offsets that
+// were stored in the Segment.  Before attempting to write an entry, the client
 // should call CanHold() to make sure that there is enough space in the segment.
 //
 // ReadData can be called over the range that was previously written with
 // WriteData.  Reading from area that was not written will fail.
 
-class NET_EXPORT_PRIVATE Segment : public base::RefCounted<Segment> {
+class NET_EXPORT_PRIVATE Segment {
  public:
   // |index| is the index of this segment on |storage|.  If the storage size is
   // X and the segment size is Y, where X >> Y and X % Y == 0, then the valid
@@ -65,20 +64,29 @@ class NET_EXPORT_PRIVATE Segment : public base::RefCounted<Segment> {
   // |index| is given value Z, then it covers bytes on storage starting at the
   // offset Z*Y and ending at the offset Z*Y+Y-1.
   Segment(int32 index, bool read_only, Storage* storage);
+  ~Segment();
 
+  int32 index() const { return index_; }
+  int32 write_offset() const { return write_offset_; }
+
+  bool HaveOffset(int32 offset) const;
   std::vector<int32> GetOffsets() const { return offsets_; }
+
+  // Manage the number of users of this segment.
+  void AddUser();
+  void ReleaseUser();
+  bool HasNoUsers() const;
 
   // Performs segment initialization.  Must be the first function called on the
   // segment and further calls should be made only if it is successful.
   bool Init();
 
   // Writes |size| bytes of data from |buffer| to segment, returns false if
-  // fails and true if succeeds and sets the |offset|, if it is not NULL.  Can
-  // block for a long time.
-  bool WriteData(const void* buffer, int32 size, int32* offset);
+  // fails and true if succeeds.  Can block for a long time.
+  bool WriteData(const void* buffer, int32 size);
 
-  // Reads |size| bytes of data living at |offset| into |buffer|, returns true
-  // on success and false on failure.
+  // Reads |size| bytes of data living at |offset| into |buffer| returns true on
+  // success and false on failure.
   bool ReadData(void* buffer, int32 size, int32 offset) const;
 
   // Stores the offset in the metadata.
@@ -92,9 +100,8 @@ class NET_EXPORT_PRIVATE Segment : public base::RefCounted<Segment> {
   bool CanHold(int32 size) const;
 
  private:
-  friend class base::RefCounted<Segment>;
-  ~Segment();
-
+  int32 index_;
+  int32 num_users_;
   bool read_only_;  // Indicates whether the segment can be written to.
   bool init_;  // Indicates whether segment was initialized.
   Storage* storage_;  // Storage on which the segment resides.
