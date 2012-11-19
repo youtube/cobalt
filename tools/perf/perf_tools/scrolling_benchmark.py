4cb8d31787859a9a85365a2ff171e202c71baf00
hartmanng@chromium.org <hartmanng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-19 21:37:03 +0000

    Refactoring scrolling_benchmark code into scrolling_interaction.py
    This is the "separate patch: mentioned in http://codereview.chromium.org/11366197/ to move scrolling logic into scrolling_interaction.py.
    
    BUG=160149
    
    Review URL: https://chromiumcodereview.appspot.com/11316017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168610 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/perf_tools/scrolling_benchmark.py
+++ tools/perf/perf_tools/scrolling_benchmark.py
@@ -1,10 +1,7 @@
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-import os
-
 from telemetry import multi_page_benchmark
-from telemetry import util
 
 class DidNotScrollException(multi_page_benchmark.MeasurementFailure):
   def __init__(self):
@@ -66,7 +63,7 @@ def CalcScrollResults(rendering_stats_deltas, results):
 
 class ScrollingBenchmark(multi_page_benchmark.MultiPageBenchmark):
   def __init__(self):
-    super(ScrollingBenchmark, self).__init__()
+    super(ScrollingBenchmark, self).__init__('scrolling')
 
   def AddCommandLineOptions(self, parser):
     parser.add_option('--no-gpu-benchmarking-extension', action='store_true',
@@ -76,52 +73,20 @@ class ScrollingBenchmark(multi_page_benchmark.MultiPageBenchmark):
                       action='store_true',
                       help='Reports all data collected, not just FPS')
 
-  @staticmethod
-  def ScrollPageFully(page, tab):
-    scroll_js_path = os.path.join(os.path.dirname(__file__), 'scroll.js')
-    scroll_js = open(scroll_js_path, 'r').read()
-
-    # Run scroll test.
-    tab.runtime.Execute(scroll_js)
-
-    with tab.browser.platform.GetSurfaceCollector(''):
-
-      start_scroll_js = """
-        window.__renderingStatsDeltas = null;
-        new __ScrollTest(function(rendering_stats_deltas) {
-          window.__renderingStatsDeltas = rendering_stats_deltas;
-        }).start(element);
-      """
-      # scrollable_element_function is a function that passes the scrollable
-      # element on the page to a callback. For example:
-      #   function (callback) {
-      #     callback(document.getElementById('foo'));
-      #   }
-      if hasattr(page, 'scrollable_element_function'):
-        tab.runtime.Execute('(%s)(function(element) { %s });' %
-                            (page.scrollable_element_function, start_scroll_js))
-      else:
-        tab.runtime.Execute(
-            '(function() { var element = document.body; %s})();' %
-            start_scroll_js)
-
-      # Poll for scroll benchmark completion.
-      util.WaitFor(lambda: tab.runtime.Evaluate(
-          'window.__renderingStatsDeltas'), 60)
-
-      rendering_stats_deltas = tab.runtime.Evaluate(
-        'window.__renderingStatsDeltas')
-
-      if not (rendering_stats_deltas['numFramesSentToScreen'] > 0):
-        raise DidNotScrollException()
-    return rendering_stats_deltas
-
   def CustomizeBrowserOptions(self, options):
     if not options.no_gpu_benchmarking_extension:
       options.extra_browser_args.append('--enable-gpu-benchmarking')
 
+  def CanRunForPage(self, page):
+    return hasattr(page, 'scrolling')
+
   def MeasurePage(self, page, tab, results):
-    rendering_stats_deltas = self.ScrollPageFully(page, tab)
+    rendering_stats_deltas = tab.runtime.Evaluate(
+      'window.__renderingStatsDeltas')
+
+    if not (rendering_stats_deltas['numFramesSentToScreen'] > 0):
+      raise DidNotScrollException()
+
     CalcScrollResults(rendering_stats_deltas, results)
     if self.options.report_all_results:
       for k, v in rendering_stats_deltas.iteritems():
