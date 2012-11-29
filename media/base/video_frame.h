102b86b57c44d7c521b9bb2033a2655b73e4afac
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-29 22:21:47 +0000

    Encrypted Media: Remove memcpy of decoded video frame in ContentDecryptorDelegate.
    Memory copy of decoded video frame is expensive and unnecessary. This CL wraps the decoded YUV data into media::VideoFrame directly without copying any data. The VideoFrame does not own the YUV data passed in. When the YUV data is no longer needed, VideoFrame notifies the owner of that data by calling no_longer_needed_cb.
    
    On Linux debug build, this saves about 0.6ms per frame. On slower (memory bandwidth limited) platforms the saving can be even larger.
    
    BUG=162193
    TEST=test passes and EME demo still works.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11316211
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170256 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/video_frame.h
+++ media/base/video_frame.h
@@ -65,7 +65,7 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
   typedef base::Callback<void(void*)> ReadPixelsCB;
 
   // Wraps a native texture of the given parameters with a VideoFrame.  When the
-  // frame is destroyed |no_longer_needed.Run()| will be called.
+  // frame is destroyed |no_longer_needed_cb.Run()| will be called.
   // |coded_size| is the width and height of the frame data in pixels.
   // |visible_rect| is the visible portion of |coded_size|, after cropping (if
   // any) is applied.
@@ -81,12 +81,29 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
       const gfx::Size& natural_size,
       base::TimeDelta timestamp,
       const ReadPixelsCB& read_pixels_cb,
-      const base::Closure& no_longer_needed);
+      const base::Closure& no_longer_needed_cb);
 
   // Read pixels from the native texture backing |*this| and write
   // them to |*pixels| as BGRA.  |pixels| must point to a buffer at
   // least as large as 4*visible_rect().width()*visible_rect().height().
   void ReadPixelsFromNativeTexture(void* pixels);
+
+  // Wraps external YUV data of the given parameters with a VideoFrame.
+  // The returned VideoFrame does not own the data passed in. When the frame
+  // is destroyed |no_longer_needed_cb.Run()| will be called.
+  static scoped_refptr<VideoFrame> WrapExternalYuvData(
+      Format format,
+      const gfx::Size& coded_size,
+      const gfx::Rect& visible_rect,
+      const gfx::Size& natural_size,
+      int32 y_stride,
+      int32 u_stride,
+      int32 v_stride,
+      uint8* y_data,
+      uint8* u_data,
+      uint8* v_data,
+      base::TimeDelta timestamp,
+      const base::Closure& no_longer_needed_cb);
 
   // Creates a frame with format equals to VideoFrame::EMPTY, width, height,
   // and timestamp are all 0.
@@ -184,7 +201,8 @@ class MEDIA_EXPORT VideoFrame : public base::RefCountedThreadSafe<VideoFrame> {
   uint32 texture_id_;
   uint32 texture_target_;
   ReadPixelsCB read_pixels_cb_;
-  base::Closure texture_no_longer_needed_;
+
+  base::Closure no_longer_needed_cb_;
 
   base::TimeDelta timestamp_;
 
