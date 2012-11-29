62f123f9333837b262063325c22380c611fc4316
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-29 14:00:12 +0000

    [cros] RlzValueStore made protected by a cross-process lock and not persisted over browser lifetime (like on Mac).
    *) Moved RecursiveCrossProcessLock out of .mm file to a common _posix file.
    *) Added static method to ImportantFileWriter that does blocking write on the current thread.
    *) Dedicated RLZ thread gone, replaced back with shutdown-blocking worker pool.
    
    
    BUG=157348,62328
    
    
    Review URL: https://chromiumcodereview.appspot.com/11308196
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170179 0039d316-1c4b-4281-b951-d872f2087c98

--- base/files/important_file_writer.cc
+++ base/files/important_file_writer.cc
@@ -42,7 +42,11 @@ void LogFailure(const FilePath& path, TempFileFailure failure_code,
                  << " : " << message;
 }
 
-void WriteToDiskTask(const FilePath& path, const std::string& data) {
+}  // namespace
+
+// static
+bool ImportantFileWriter::WriteFileAtomically(const FilePath& path,
+                                              const std::string& data) {
   // Write the data to a temp file then rename to avoid data loss if we crash
   // while writing the file. Ensure that the temp file is on the same volume
   // as target file, so it can be moved in one step, and that the temp file
@@ -50,7 +54,7 @@ void WriteToDiskTask(const FilePath& path, const std::string& data) {
   FilePath tmp_file_path;
   if (!file_util::CreateTemporaryFileInDir(path.DirName(), &tmp_file_path)) {
     LogFailure(path, FAILED_CREATING, "could not create temporary file");
-    return;
+    return false;
   }
 
   int flags = PLATFORM_FILE_OPEN | PLATFORM_FILE_WRITE;
@@ -58,7 +62,7 @@ void WriteToDiskTask(const FilePath& path, const std::string& data) {
       CreatePlatformFile(tmp_file_path, flags, NULL, NULL);
   if (tmp_file == kInvalidPlatformFileValue) {
     LogFailure(path, FAILED_OPENING, "could not open temporary file");
-    return;
+    return false;
   }
 
   // If this happens in the wild something really bad is going on.
@@ -70,24 +74,24 @@ void WriteToDiskTask(const FilePath& path, const std::string& data) {
   if (!ClosePlatformFile(tmp_file)) {
     LogFailure(path, FAILED_CLOSING, "failed to close temporary file");
     file_util::Delete(tmp_file_path, false);
-    return;
+    return false;
   }
 
   if (bytes_written < static_cast<int>(data.length())) {
     LogFailure(path, FAILED_WRITING, "error writing, bytes_written=" +
                IntToString(bytes_written));
     file_util::Delete(tmp_file_path, false);
-    return;
+    return false;
   }
 
   if (!file_util::ReplaceFile(tmp_file_path, path)) {
     LogFailure(path, FAILED_RENAMING, "could not rename temporary file");
     file_util::Delete(tmp_file_path, false);
-    return;
+    return false;
   }
-}
 
-}  // namespace
+  return true;
+}
 
 ImportantFileWriter::ImportantFileWriter(
     const FilePath& path, base::SequencedTaskRunner* task_runner)
@@ -122,14 +126,17 @@ void ImportantFileWriter::WriteNow(const std::string& data) {
   if (HasPendingWrite())
     timer_.Stop();
 
-  if (!task_runner_->PostTask(FROM_HERE,
-          MakeCriticalClosure(Bind(&WriteToDiskTask, path_, data)))) {
+  if (!task_runner_->PostTask(
+          FROM_HERE,
+          MakeCriticalClosure(
+              Bind(IgnoreResult(&ImportantFileWriter::WriteFileAtomically),
+                   path_, data)))) {
     // Posting the task to background message loop is not expected
     // to fail, but if it does, avoid losing data and just hit the disk
     // on the current thread.
     NOTREACHED();
 
-    WriteToDiskTask(path_, data);
+    WriteFileAtomically(path_, data);
   }
 }
 
