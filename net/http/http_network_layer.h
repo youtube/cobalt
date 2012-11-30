277775bab5e90b9cda86f1576afe9b6869564398
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-30 02:56:46 +0000

    Move EnableSpdy from HttpNetworkLayer to IOThread. Add HttpNetworkLayer::ForceAlternateProtocol()
    BUG=162571
    
    
    Review URL: https://chromiumcodereview.appspot.com/11416205
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170373 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_network_layer.h
+++ net/http/http_network_layer.h
@@ -36,15 +36,9 @@ class NET_EXPORT HttpNetworkLayer
   // when network session is shared.
   static HttpTransactionFactory* CreateFactory(HttpNetworkSession* session);
 
-  // Enable the spdy protocol.
-  // Without calling this function, SPDY is disabled.  The mode can be:
-  //   ""            : (default) SSL and compression are enabled, flow
-  //                   control disabled.
-  //   "no-ssl"      : disables SSL.
-  //   "no-compress" : disables compression.
-  //   "flow-control": enables flow control.
-  //   "none"        : disables both SSL and compression.
-  static void EnableSpdy(const std::string& mode);
+  // Forces an alternate protocol of SPDY/2 on port 443.
+  // TODO(rch): eliminate this method.
+  static void ForceAlternateProtocol();
 
   // HttpTransactionFactory methods:
   virtual int CreateTransaction(scoped_ptr<HttpTransaction>* trans,
