fa592dbada749459f14b279e957156ec39725856
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-04 15:56:20 +0000

    Add a QuicHttpStream class.
    Review URL: https://chromiumcodereview.appspot.com/11364068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170968 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_reliable_client_stream.cc
+++ net/quic/quic_reliable_client_stream.cc
@@ -5,19 +5,26 @@
 #include "net/quic/quic_reliable_client_stream.h"
 
 #include "net/base/net_errors.h"
+#include "net/quic/quic_session.h"
 
 namespace net {
 
 QuicReliableClientStream::QuicReliableClientStream(QuicStreamId id,
                                                    QuicSession* session)
-    : ReliableQuicStream(id, session) {
+    : ReliableQuicStream(id, session),
+      delegate_(NULL) {
 }
 
 QuicReliableClientStream::~QuicReliableClientStream() {
+  if (delegate_) {
+    delegate_->OnClose(error());
+  }
 }
 
 uint32 QuicReliableClientStream::ProcessData(const char* data,
                                              uint32 data_len) {
+  // TODO(rch): buffer data if we don't have a delegate.
+  DCHECK(delegate_);
   int rv = delegate_->OnDataReceived(data, data_len);
   if (rv != OK) {
     DLOG(ERROR) << "Delegate refused data, rv: " << rv;
@@ -28,11 +35,15 @@ uint32 QuicReliableClientStream::ProcessData(const char* data,
 }
 
 void QuicReliableClientStream::TerminateFromPeer(bool half_close) {
-  delegate_->OnClose(error());
+  if (delegate_) {
+    delegate_->OnClose(error());
+    delegate_ = NULL;
+  }
 }
 
 void QuicReliableClientStream::SetDelegate(
     QuicReliableClientStream::Delegate* delegate) {
+  DCHECK((!delegate_ && delegate) || (delegate_ && !delegate));
   delegate_ = delegate;
 }
 
