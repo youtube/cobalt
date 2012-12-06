db5cd6147fd5416da41677cca9a15639e17e8c89
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-06 06:58:38 +0000

    Replace WaitableEvents and ConditionalVariables in VideoRendererBase tests with MessageLoop.
    Review URL: https://chromiumcodereview.appspot.com/11316293
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171438 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/decrypting_audio_decoder_unittest.cc
+++ media/filters/decrypting_audio_decoder_unittest.cc
@@ -13,8 +13,8 @@
 #include "media/base/decoder_buffer.h"
 #include "media/base/decrypt_config.h"
 #include "media/base/gmock_callback_support.h"
-#include "media/base/mock_callback.h"
 #include "media/base/mock_filters.h"
+#include "media/base/test_helpers.h"
 #include "media/filters/decrypting_audio_decoder.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
