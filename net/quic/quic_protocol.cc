2a2762c0b09aef4160bbc44218b30b440212add3
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-07 15:33:53 +0000

    * Enhance the ack logging Merge internal change: 39207942
    * Fixing a bug in our ack logic where we didn't update least-unacked when
    we decided to resend a packet (and invalidate the original sequence number)
    
    Merge internal change: 39279598
    
    * Fixing a bug in our ack logic where we'd end up with an ack packet as least
    unacked, and then we'd never update it.
    
    Merge internal change: 39284385
    
    * Getting rid of the special value '0' for "all packets are acked". It
    doesn't work with the new ack design.
    
    Merge internal change: 39285222
    
    * Fixing (yet another) bug in our ack logic: uint8s just aren't large enough 
    for our ack deltas.
    
    Merge internal change: 39425978
    
    * Incremental improvement for our nack support: auto-resending a packet's 
    payload if we get something explicitly nacked multiple times.
    
    Merge internal change: 39428178
    
    * Fix to re-add a missing packet set to received info and only transmitting
    ack times once. Also adding extra ack validation to ensure acks do not 
    have invalid nack sequence numbers, ack sequence numbers, or least 
    awaiting ack.
    
    Merge internal change: 39618914
    
    * Cleanup CL based on Jim's chromium comments.
    
    Merge internal change: 39680680
    
    
    Review URL: https://chromiumcodereview.appspot.com/11446029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171780 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_protocol.cc
+++ net/quic/quic_protocol.cc
@@ -7,6 +7,7 @@
 
 using base::StringPiece;
 using std::map;
+using std::numeric_limits;
 using std::ostream;
 
 namespace net {
@@ -28,62 +29,100 @@ ReceivedPacketInfo::ReceivedPacketInfo() : largest_received(0) {}
 
 ReceivedPacketInfo::~ReceivedPacketInfo() {}
 
-void ReceivedPacketInfo::RecordAck(QuicPacketSequenceNumber sequence_number,
-                                   QuicTime time) {
-  DCHECK(!ContainsAck(sequence_number));
+void ReceivedPacketInfo::RecordReceived(
+    QuicPacketSequenceNumber sequence_number,
+    QuicTime time) {
+  DCHECK(IsAwaitingPacket(sequence_number));
   received_packet_times[sequence_number] = time;
   if (largest_received < sequence_number) {
+    DCHECK_LT(sequence_number - largest_received,
+              numeric_limits<uint16>::max());
+    // We've got a new high sequence number.  Note any new intermediate missing
+    // packets, and update the last_ack data.
+    for (QuicPacketSequenceNumber i = largest_received + 1;
+         i < sequence_number; ++i) {
+      DVLOG(1) << "missing " << i;
+      missing_packets.insert(i);
+    }
     largest_received = sequence_number;
+  } else {
+    // We've gotten one of the out of order packets - remove it from our
+    // "missing packets" list.
+    DVLOG(1) << "Removing "  << sequence_number << " from missing list";
+    missing_packets.erase(sequence_number);
   }
 }
 
-bool ReceivedPacketInfo::ContainsAck(
+bool ReceivedPacketInfo::IsAwaitingPacket(
     QuicPacketSequenceNumber sequence_number) const {
-  return ContainsKey(received_packet_times, sequence_number);
+  return sequence_number > largest_received ||
+      ContainsKey(missing_packets, sequence_number);
 }
 
 void ReceivedPacketInfo::ClearAcksBefore(
     QuicPacketSequenceNumber least_unacked) {
+  missing_packets.erase(missing_packets.begin(),
+                        missing_packets.lower_bound(least_unacked));
+  // Clearing received_packet_times may not be necessary, since they're cleared
+  // every time an ack is sent.
   for (QuicPacketSequenceNumber i = received_packet_times.begin()->first;
        i < least_unacked; ++i) {
     received_packet_times.erase(i);
   }
 }
 
+void ReceivedPacketInfo::ClearAckTimes() {
+  received_packet_times.clear();
+}
+
 SentPacketInfo::SentPacketInfo() {}
 
 SentPacketInfo::~SentPacketInfo() {}
 
+// Testing convenience method.
 QuicAckFrame::QuicAckFrame(QuicPacketSequenceNumber largest_received,
                            QuicTime time_received,
                            QuicPacketSequenceNumber least_unacked) {
-  received_info.largest_received = largest_received;
   for (QuicPacketSequenceNumber seq_num = 1;
        seq_num <= largest_received; ++seq_num) {
-    received_info.RecordAck(seq_num, time_received);
+    received_info.RecordReceived(seq_num, time_received);
   }
+  received_info.largest_received = largest_received;
   sent_info.least_unacked = least_unacked;
   congestion_info.type = kNone;
 }
 
-ostream& operator<<(ostream& os, const QuicAckFrame& s) {
-  os << "sent info { least_waiting: " << s.sent_info.least_unacked << " } "
-     << "received info { largest_received: "
-     << s.received_info.largest_received
-     << " received packets: [ ";
-  for (map<QuicPacketSequenceNumber, QuicTime>::const_iterator it =
-           s.received_info.received_packet_times.begin();
-       it != s.received_info.received_packet_times.end(); ++it) {
-    os << it->first << "@" << it->second.ToMilliseconds() << " ";
+ostream& operator<<(ostream& os, const SentPacketInfo& s) {
+  os << "least_waiting: " << s.least_unacked;
+  return os;
+}
+
+ostream& operator<<(ostream& os, const ReceivedPacketInfo& r) {
+  os << "largest_received: "
+     << r.largest_received
+     << " missing_packets: [ ";
+  for (SequenceSet::const_iterator it = r.missing_packets.begin();
+       it != r.missing_packets.end(); ++it) {
+    os << *it << " ";
   }
 
-  os << "] } congestion info { type: " << s.congestion_info.type;
-  switch (s.congestion_info.type) {
+  os << "] received packets: [ ";
+  for (TimeMap::const_iterator it = r.received_packet_times.begin();
+       it != r.received_packet_times.end(); ++it) {
+    os << it->first << "@" << it->second.ToMilliseconds() << " ";
+  }
+  os << "]";
+  return os;
+}
+
+ostream& operator<<(ostream& os, const CongestionInfo& c) {
+  os << "type: " << c.type;
+  switch (c.type) {
     case kNone:
       break;
     case kInterArrival: {
       const CongestionFeedbackMessageInterArrival& inter_arrival =
-          s.congestion_info.inter_arrival;
+          c.inter_arrival;
       os << " accumulated_number_of_lost_packets: "
          << inter_arrival.accumulated_number_of_lost_packets;
       os << " offset_time: " << inter_arrival.offset_time;
@@ -92,18 +131,28 @@ ostream& operator<<(ostream& os, const QuicAckFrame& s) {
     }
     case kFixRate: {
       os << " bitrate_in_bytes_per_second: "
-         << s.congestion_info.fix_rate.bitrate_in_bytes_per_second;
+         << c.fix_rate.bitrate_in_bytes_per_second;
       break;
     }
     case kTCP: {
-      const CongestionFeedbackMessageTCP& tcp = s.congestion_info.tcp;
+      const CongestionFeedbackMessageTCP& tcp = c.tcp;
       os << " accumulated_number_of_lost_packets: "
-         << tcp.accumulated_number_of_lost_packets;
+         << c.tcp.accumulated_number_of_lost_packets;
       os << " receive_window: " << tcp.receive_window;
       break;
     }
+    default: {
+      DLOG(FATAL) << "Unsupported congestion info type: "
+                  << c.type;
+    }
   }
-  os << " }\n";
+ return os;
+}
+
+ostream& operator<<(ostream& os, const QuicAckFrame& a) {
+  os << "sent info { " << a.sent_info << " } "
+     << "received info { " << a.received_info << " } "
+     << "congestion info { " << a.congestion_info << " }\n";
   return os;
 }
 
