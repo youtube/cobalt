a4c48041686dd2591ed06a322956ecc65ba74291
pasko@google.com <pasko@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-10 12:49:38 +0000

    Use SCHEME_HTTP for HTTPS proxies on Android.
    This makes the ProxyServer setup aligned with the Linux implementation (see
    proxy_config_service_linux.cc) which never uses SCHEME_HTTPS.
    
    BUG=162908
    TEST=Set up proxy in Android WiFi settings, go to https://google.com, observe
         the page gets loaded.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11438047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172045 0039d316-1c4b-4281-b951-d872f2087c98

--- net/android/tools/proxy_test_cases.py
+++ net/android/tools/proxy_test_cases.py
@@ -154,7 +154,7 @@ test_cases = [
       "https.proxyPort" : "8080",
     },
     "mappings" : {
-      "https://example.com/" : "HTTPS httpproxy.com:8080",
+      "https://example.com/" : "PROXY httpproxy.com:8080",
       "http://example.com/" : "DIRECT",
       "ftp://example.com/" : "DIRECT",
     }
@@ -162,11 +162,14 @@ test_cases = [
   {
     "name": "HttpsProxyHostOnly",
     "description" : "Test https.proxyHost and default port.",
+    # Chromium differs from the Android platform by connecting to port 80 for
+    # HTTPS connections by default, hence cpp-only.
+    "cpp-only" : "",
     "properties" : {
       "https.proxyHost" : "httpproxy.com",
     },
     "mappings" : {
-      "https://example.com/" : "HTTPS httpproxy.com:443",
+      "https://example.com/" : "PROXY httpproxy.com:80",
       "http://example.com/" : "DIRECT",
       "ftp://example.com/" : "DIRECT",
     }
@@ -221,19 +224,22 @@ test_cases = [
     },
     "mappings" : {
       "http://example.com/" : "PROXY defaultproxy.com:8080",
-      "https://example.com/" : "HTTPS defaultproxy.com:8080",
+      "https://example.com/" : "PROXY defaultproxy.com:8080",
       "ftp://example.com/" : "PROXY httpproxy.com:8080",
     }
   },
   {
     "name": "DefaultProxyDefaultPort",
     "description" : "Check that the default proxy port is as expected.",
+    # Chromium differs from the Android platform by connecting to port 80 for
+    # HTTPS connections by default, hence cpp-only.
+    "cpp-only" : "",
     "properties" : {
       "proxyHost" : "defaultproxy.com",
     },
     "mappings" : {
       "http://example.com/" : "PROXY defaultproxy.com:80",
-      "https://example.com/" : "HTTPS defaultproxy.com:443",
+      "https://example.com/" : "PROXY defaultproxy.com:80",
     }
   },
   {
@@ -310,7 +316,7 @@ class GenerateJava:
       if "description" in test_case:
         self._GenerateDescription(test_case["description"]);
       print "    @SmallTest"
-      print "    @Feature({\"Android-WebView\"})"
+      print "    @Feature({\"AndroidWebView\"})"
       print "    public void test%s() throws Exception {" % test_case["name"]
       self._GenerateConfiguration(test_case["properties"])
       self._GenerateMappings(test_case["mappings"])
