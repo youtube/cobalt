6203af0fa36ed67f4545e12c4460b864e268fff3
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-11 09:44:41 +0000

    DevTools: add /json/activate/ command to the discovery protocol.
    This change adds suppot for /activate command and refactors the json commands handling so that all commands supported jsonp properly. For the jsonp support, we always respond with 200 OK and specify the actual code in the jsonp call parameter. 
    
    For the above purposes, migrates net/server/http_server from Send404() scheme to Send(net::HTTP_NOT_FOUND).
    
    BUG=157495
    
    
    Review URL: https://chromiumcodereview.appspot.com/11499004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172285 0039d316-1c4b-4281-b951-d872f2087c98

--- net/server/http_server.cc
+++ net/server/http_server.cc
@@ -45,42 +45,28 @@ void HttpServer::SendOverWebSocket(int connection_id,
   connection->web_socket_->Send(data);
 }
 
-void HttpServer::Send(int connection_id, const std::string& data) {
+void HttpServer::Send(int connection_id,
+                      HttpStatusCode status_code,
+                      const std::string& data,
+                      const std::string& content_type) {
   HttpConnection* connection = FindConnection(connection_id);
   if (connection == NULL)
     return;
-  connection->Send(data);
-}
-
-void HttpServer::Send(int connection_id, const char* bytes, int len) {
-  HttpConnection* connection = FindConnection(connection_id);
-  if (connection == NULL)
-    return;
-
-  connection->Send(bytes, len);
+  connection->Send(status_code, data, content_type);
 }
 
 void HttpServer::Send200(int connection_id,
                          const std::string& data,
                          const std::string& content_type) {
-  HttpConnection* connection = FindConnection(connection_id);
-  if (connection == NULL)
-    return;
-  connection->Send200(data, content_type);
+  Send(connection_id, HTTP_OK, data, content_type);
 }
 
 void HttpServer::Send404(int connection_id) {
-  HttpConnection* connection = FindConnection(connection_id);
-  if (connection == NULL)
-    return;
-  connection->Send404();
+  Send(connection_id, HTTP_NOT_FOUND, "", "text/html");
 }
 
 void HttpServer::Send500(int connection_id, const std::string& message) {
-  HttpConnection* connection = FindConnection(connection_id);
-  if (connection == NULL)
-    return;
-  connection->Send500(message);
+  Send(connection_id, HTTP_INTERNAL_SERVER_ERROR, message, "text/html");
 }
 
 void HttpServer::Close(int connection_id) {
