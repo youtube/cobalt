fc96973bd3da54b41e87b37ec1d0ffab9c1146bc
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-12 05:00:32 +0000

    Add a factory for creating QuicHttpStream objects from an internal collection of QuicClientSessions.  Consumers of this factory requests a stream by creating a QuicStreamRequest object, and then calling the Request() method.
    Review URL: https://chromiumcodereview.appspot.com/11365279
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172531 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/quic/quic_stream_factory.h
@@ -0,0 +1,138 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_QUIC_QUIC_STREAM_FACTORY_H_
+#define NET_QUIC_QUIC_STREAM_FACTORY_H_
+
+#include <map>
+
+#include "base/callback.h"
+#include "base/memory/weak_ptr.h"
+#include "net/base/address_list.h"
+#include "net/base/completion_callback.h"
+#include "net/base/host_port_pair.h"
+#include "net/base/net_log.h"
+#include "net/proxy/proxy_server.h"
+#include "net/quic/quic_http_stream.h"
+#include "net/quic/quic_protocol.h"
+
+namespace net {
+
+class HostResolver;
+class ClientSocketFactory;
+class QuicClock;
+class QuicClientSession;
+class QuicStreamFactory;
+
+// Encapsulates a pending request for a QuicHttpStream.
+// If the request is still pending when it is destroyed, it will
+// cancel the request with the factory.
+class NET_EXPORT_PRIVATE QuicStreamRequest {
+ public:
+  explicit QuicStreamRequest(QuicStreamFactory* factory);
+  ~QuicStreamRequest();
+
+  int Request(const HostPortProxyPair& host_port_proxy_pair,
+              const BoundNetLog& net_log,
+              const CompletionCallback& callback);
+
+  void OnRequestComplete(int rv);
+
+  scoped_ptr<QuicHttpStream> ReleaseStream();
+
+  void set_stream(scoped_ptr<QuicHttpStream> stream);
+
+  const BoundNetLog& net_log() const{
+    return net_log_;
+  }
+
+ private:
+  QuicStreamFactory* factory_;
+  HostPortProxyPair host_port_proxy_pair_;
+  BoundNetLog net_log_;
+  CompletionCallback callback_;
+  scoped_ptr<QuicHttpStream> stream_;
+
+  DISALLOW_COPY_AND_ASSIGN(QuicStreamRequest);
+};
+
+// A factory for creating new QuicHttpStreams on top of a pool of
+// QuicClientSessions.
+class NET_EXPORT_PRIVATE QuicStreamFactory {
+ public:
+  typedef base::Callback<uint64()> RandomUint64Callback;
+
+  QuicStreamFactory(HostResolver* host_resolver,
+                    ClientSocketFactory* client_socket_factory,
+                    const RandomUint64Callback& random_uint64_callback,
+                    const QuicClock* clock);
+  virtual ~QuicStreamFactory();
+
+  // Creates a new QuicHttpStream to |host_port_proxy_pair| which will be
+  // owned by |request|. If a matching session already exists, this
+  // method will return OK.  If no matching session exists, this will
+  // return ERR_IO_PENDING and will invoke OnRequestComplete asynchronously.
+  int Create(const HostPortProxyPair& host_port_proxy_pair,
+             const BoundNetLog& net_log,
+             QuicStreamRequest* request);
+
+  // Returns a newly created QuicHttpStream owned by the caller, if a
+  // matching session already exists.  Returns NULL otherwise.
+  scoped_ptr<QuicHttpStream> CreateIfSessionExists(
+      const HostPortProxyPair& host_port_proxy_pair,
+      const BoundNetLog& net_log);
+
+  // Called by a session when it becomes idle.
+  void OnIdleSession(QuicClientSession* session);
+
+  // Called by a session after it shuts down.
+  void OnSessionClose(QuicClientSession* session);
+
+  // Cancels a pending request.
+  void CancelRequest(QuicStreamRequest* request);
+
+ private:
+  class Job;
+
+  typedef std::map<HostPortProxyPair, QuicClientSession*> SessionMap;
+  typedef std::set<HostPortProxyPair> AliasSet;
+  typedef std::map<QuicClientSession*, AliasSet> SessionAliasMap;
+  typedef std::set<QuicClientSession*> SessionSet;
+  typedef std::map<HostPortProxyPair, Job*> JobMap;
+  typedef std::map<QuicStreamRequest*, Job*> RequestMap;
+  typedef std::set<QuicStreamRequest*> RequestSet;
+  typedef std::map<Job*, RequestSet> JobRequestsMap;
+
+  void OnJobComplete(Job* job, int rv);
+  bool HasActiveSession(const HostPortProxyPair& host_port_proxy_pair);
+  bool HasActiveJob(const HostPortProxyPair& host_port_proxy_pair);
+  QuicClientSession* CreateSession(const AddressList& address_list_,
+                     const BoundNetLog& net_log);
+  void ActivateSession(const HostPortProxyPair& host_port_proxy_pair,
+                       QuicClientSession* session);
+
+  HostResolver* host_resolver_;
+  ClientSocketFactory* client_socket_factory_;
+  RandomUint64Callback random_uint64_callback_;
+  const QuicClock* clock_;
+
+  // Contains owning pointers to all sessions that currently exist.
+  SessionSet all_sessions_;
+  // Contains non-owning pointers to currently active session
+  // (not going away session, once they're implemented).
+  SessionMap active_sessions_;
+  SessionAliasMap session_aliases_;
+
+  JobMap active_jobs_;
+  JobRequestsMap job_requests_map_;
+  RequestMap active_requests_;
+
+  base::WeakPtrFactory<QuicStreamFactory> weak_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(QuicStreamFactory);
+};
+
+}  // namespace net
+
+#endif  // NET_QUIC_QUIC_STREAM_FACTORY_H_
---
+++ net/quic/quic_stream_factory_test.cc
@@ -0,0 +1,213 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/quic/quic_stream_factory.h"
+
+#include "base/rand_util.h"
+#include "base/run_loop.h"
+#include "base/string_util.h"
+#include "net/base/mock_host_resolver.h"
+#include "net/http/http_response_headers.h"
+#include "net/http/http_response_info.h"
+#include "net/http/http_util.h"
+#include "net/quic/quic_http_stream.h"
+#include "net/quic/test_tools/mock_clock.h"
+#include "net/quic/test_tools/quic_test_utils.h"
+#include "net/socket/socket_test_util.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace net {
+
+namespace test {
+
+class QuicStreamFactoryTest : public ::testing::Test {
+ protected:
+  QuicStreamFactoryTest()
+      : factory_(&host_resolver_, &socket_factory_,
+                 base::Bind(&QuicStreamFactoryTest::GenerateGuid), &clock_),
+        host_port_proxy_pair_(HostPortPair("www.google.com", 443),
+                              ProxyServer::Direct()) {
+  }
+
+  scoped_ptr<QuicEncryptedPacket> ConstructChlo() {
+    scoped_ptr<QuicPacket> chlo(ConstructHandshakePacket(0xDEADBEEF, kCHLO));
+    QuicFramer framer(QuicDecrypter::Create(kNULL),
+                      QuicEncrypter::Create(kNULL));
+    return scoped_ptr<QuicEncryptedPacket>(framer.EncryptPacket(*chlo));
+  }
+
+  scoped_ptr<QuicEncryptedPacket> ConstructShlo() {
+    scoped_ptr<QuicPacket> shlo(ConstructHandshakePacket(0xDEADBEEF, kSHLO));
+    QuicFramer framer(QuicDecrypter::Create(kNULL),
+                      QuicEncrypter::Create(kNULL));
+    return scoped_ptr<QuicEncryptedPacket>(framer.EncryptPacket(*shlo));
+  }
+
+  scoped_ptr<QuicEncryptedPacket> ConstructRstPacket(
+      QuicPacketSequenceNumber num,
+      QuicStreamId stream_id) {
+    QuicPacketHeader header;
+    header.guid = 0xDEADBEEF;
+    header.packet_sequence_number = num;
+    header.flags = PACKET_FLAGS_NONE;
+    header.fec_group = 0;
+
+    QuicRstStreamFrame rst(stream_id, 0, QUIC_NO_ERROR);
+    return scoped_ptr<QuicEncryptedPacket>(
+        ConstructPacket(header, QuicFrame(&rst)));
+  }
+
+  scoped_ptr<QuicEncryptedPacket> ConstructAckPacket(
+      QuicPacketSequenceNumber largest_received,
+      QuicPacketSequenceNumber least_unacked) {
+    QuicPacketHeader header;
+    header.guid = 0xDEADBEEF;
+    header.packet_sequence_number = 2;
+    header.flags = PACKET_FLAGS_NONE;
+    header.fec_group = 0;
+
+    QuicAckFrame ack(largest_received, QuicTime(), least_unacked);
+    ack.congestion_info.type = kTCP;
+    ack.congestion_info.tcp.accumulated_number_of_lost_packets = 0;
+    ack.congestion_info.tcp.receive_window = 16000;
+
+    return scoped_ptr<QuicEncryptedPacket>(
+        ConstructPacket(header, QuicFrame(&ack)));
+  }
+
+  scoped_ptr<QuicEncryptedPacket> ConstructPacket(
+      const QuicPacketHeader& header,
+      const QuicFrame& frame) {
+    QuicFramer framer(QuicDecrypter::Create(kNULL),
+                      QuicEncrypter::Create(kNULL));
+    QuicFrames frames;
+    frames.push_back(frame);
+    QuicPacket* packet;
+    framer.ConstructFrameDataPacket(header, frames, &packet);
+    QuicEncryptedPacket* encrypted = framer.EncryptPacket(*packet);
+    delete packet;
+    return scoped_ptr<QuicEncryptedPacket>(encrypted);
+  }
+
+  MockHostResolver host_resolver_;
+  MockClientSocketFactory socket_factory_;
+  MockClock clock_;
+  QuicStreamFactory factory_;
+  HostPortProxyPair host_port_proxy_pair_;
+  BoundNetLog net_log_;
+  TestCompletionCallback callback_;
+
+ private:
+  static uint64 GenerateGuid() {
+    return 0xDEADBEEF;
+  }
+};
+
+TEST_F(QuicStreamFactoryTest, CreateIfSessionExists) {
+  EXPECT_EQ(NULL, factory_.CreateIfSessionExists(host_port_proxy_pair_,
+                                                 net_log_).get());
+}
+
+TEST_F(QuicStreamFactoryTest, Create) {
+  scoped_ptr<QuicEncryptedPacket> chlo(ConstructChlo());
+  scoped_ptr<QuicEncryptedPacket> ack(ConstructAckPacket(1, 1));
+  scoped_ptr<QuicEncryptedPacket> rst3(ConstructRstPacket(3, 3));
+  scoped_ptr<QuicEncryptedPacket> rst5(ConstructRstPacket(4, 5));
+  scoped_ptr<QuicEncryptedPacket> rst7(ConstructRstPacket(5, 7));
+  MockWrite writes[] = {
+    MockWrite(SYNCHRONOUS, chlo->data(), chlo->length()),
+    MockWrite(SYNCHRONOUS, ack->data(), ack->length()),
+    MockWrite(SYNCHRONOUS, rst3->data(), rst3->length()),
+    MockWrite(SYNCHRONOUS, rst5->data(), rst5->length()),
+    MockWrite(SYNCHRONOUS, rst7->data(), rst7->length()),
+  };
+  scoped_ptr<QuicEncryptedPacket> shlo(ConstructShlo());
+  scoped_ptr<QuicEncryptedPacket> ack2(ConstructAckPacket(2, 0));
+  MockRead reads[] = {
+    MockRead(SYNCHRONOUS, shlo->data(), shlo->length()),
+    MockRead(SYNCHRONOUS, ack2->data(), ack2->length()),
+    MockRead(ASYNC, OK),  // EOF
+  };
+  StaticSocketDataProvider socket_data(reads, arraysize(reads),
+                                       writes, arraysize(writes));
+  socket_factory_.AddSocketDataProvider(&socket_data);
+
+  QuicStreamRequest request(&factory_);
+  EXPECT_EQ(ERR_IO_PENDING, request.Request(host_port_proxy_pair_, net_log_,
+                                            callback_.callback()));
+
+  EXPECT_EQ(OK, callback_.WaitForResult());
+  scoped_ptr<QuicHttpStream> stream = request.ReleaseStream();
+
+  // Will reset stream 3.
+  stream = factory_.CreateIfSessionExists(host_port_proxy_pair_, net_log_);
+  EXPECT_TRUE(stream.get());
+
+  QuicStreamRequest request2(&factory_);
+  EXPECT_EQ(OK, request2.Request(host_port_proxy_pair_, net_log_,
+                                 callback_.callback()));
+  stream = request2.ReleaseStream();  // Will reset stream 5.
+  stream.reset();  // Will reset stream 7.
+
+  EXPECT_TRUE(socket_data.at_read_eof());
+  EXPECT_TRUE(socket_data.at_write_eof());
+}
+
+TEST_F(QuicStreamFactoryTest, CreateError) {
+  StaticSocketDataProvider socket_data(NULL, 0, NULL, 0);
+  socket_factory_.AddSocketDataProvider(&socket_data);
+
+  host_resolver_.rules()->AddSimulatedFailure("www.google.com");
+
+  QuicStreamRequest request(&factory_);
+  EXPECT_EQ(ERR_IO_PENDING, request.Request(host_port_proxy_pair_, net_log_,
+                                            callback_.callback()));
+
+  EXPECT_EQ(ERR_NAME_NOT_RESOLVED, callback_.WaitForResult());
+
+  EXPECT_TRUE(socket_data.at_read_eof());
+  EXPECT_TRUE(socket_data.at_write_eof());
+}
+
+TEST_F(QuicStreamFactoryTest, CancelCreate) {
+  scoped_ptr<QuicEncryptedPacket> chlo(ConstructChlo());
+  scoped_ptr<QuicEncryptedPacket> ack(ConstructAckPacket(1, 1));
+  scoped_ptr<QuicEncryptedPacket> rst3(ConstructRstPacket(3, 3));
+
+  MockWrite writes[] = {
+    MockWrite(SYNCHRONOUS, chlo->data(), chlo->length()),
+    MockWrite(SYNCHRONOUS, ack->data(), ack->length()),
+    MockWrite(SYNCHRONOUS, rst3->data(), rst3->length()),
+  };
+  scoped_ptr<QuicEncryptedPacket> shlo(ConstructShlo());
+  scoped_ptr<QuicEncryptedPacket> ack2(ConstructAckPacket(2, 0));
+  MockRead reads[] = {
+    MockRead(SYNCHRONOUS, shlo->data(), shlo->length()),
+    MockRead(SYNCHRONOUS, ack2->data(), ack2->length()),
+    MockRead(ASYNC, OK),  // EOF
+  };
+  StaticSocketDataProvider socket_data(reads, arraysize(reads),
+                                       writes, arraysize(writes));
+  socket_factory_.AddSocketDataProvider(&socket_data);
+  {
+    QuicStreamRequest request(&factory_);
+    EXPECT_EQ(ERR_IO_PENDING, request.Request(host_port_proxy_pair_, net_log_,
+                                              callback_.callback()));
+  }
+
+  base::RunLoop run_loop;
+  run_loop.RunUntilIdle();
+
+  scoped_ptr<QuicHttpStream> stream(
+      factory_.CreateIfSessionExists(host_port_proxy_pair_, net_log_));
+  EXPECT_TRUE(stream.get());
+  stream.reset();
+
+  EXPECT_TRUE(socket_data.at_read_eof());
+  EXPECT_TRUE(socket_data.at_write_eof());
+}
+
+}  // namespace test
+
+}  // namespace net
