89a3bc744f34239d77422f4c05487aaca9afdda9
agayev@chromium.org <agayev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-12 20:47:18 +0000

    Renamed classes for consistency.
    BUG=157187
    TEST=net_unittests --gtest_filter="FlashCacheTest.*" --gtest_repeat=10 --shuffle
    
    
    Review URL: https://chromiumcodereview.appspot.com/11532011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172651 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/flash/format.h
+++ net/disk_cache/flash/format.h
@@ -23,9 +23,9 @@ const int32 kFlashSummarySize = (1 + kFlashMaxEntryCount) * sizeof(int32);
 const int32 kFlashSegmentFreeSpace = kFlashSegmentSize - kFlashSummarySize;
 
 // An entry consists of a fixed number of streams.
-const int32 kFlashCacheEntryNumStreams = 4;
-const int32 kFlashCacheEntryHeaderSize =
-    kFlashCacheEntryNumStreams * sizeof(int32);
+const int32 kFlashLogStoreEntryNumStreams = 4;
+const int32 kFlashLogStoreEntryHeaderSize =
+    kFlashLogStoreEntryNumStreams * sizeof(int32);
 
 }  // namespace disk_cache
 
