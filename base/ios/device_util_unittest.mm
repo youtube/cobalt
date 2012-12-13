497d072d9977866b291c49f10583436d3c55ab0d
qsr@chromium.org <qsr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-13 17:39:47 +0000

    Fixing identifier for device returning a 0 identifierForVendor.
    R=stuartmorgan@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11576008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172888 0039d316-1c4b-4281-b951-d872f2087c98

--- base/ios/device_util_unittest.mm
+++ base/ios/device_util_unittest.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 #include "base/ios/device_util.h"
 #include "base/ios/ios_util.h"
@@ -17,6 +17,13 @@ namespace {
 // don't choke. Additional checks are added for particular APIs when needed.
 
 typedef PlatformTest DeviceUtilTest;
+
+void CleanNSUserDefaultsForDeviceId() {
+  NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+  [defaults removeObjectForKey:@"ChromeClientID"];
+  [defaults removeObjectForKey:@"ChromiumClientID"];
+  [defaults synchronize];
+}
 
 TEST_F(DeviceUtilTest, GetPlatform) {
   GTEST_ASSERT_GT(ios::device_util::GetPlatform().length(), 0U);
@@ -39,19 +46,60 @@ TEST_F(DeviceUtilTest, GetRandomId) {
 }
 
 TEST_F(DeviceUtilTest, GetDeviceIdentifier) {
+  CleanNSUserDefaultsForDeviceId();
+
   std::string default_id = ios::device_util::GetDeviceIdentifier(NULL);
   std::string other_id = ios::device_util::GetDeviceIdentifier("ForTest");
   EXPECT_NE(default_id, other_id);
 
-  NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
-  [defaults removeObjectForKey:@"ChromiumClientID"];
-  [defaults synchronize];
+  CleanNSUserDefaultsForDeviceId();
 
   std::string new_default_id = ios::device_util::GetDeviceIdentifier(NULL);
-  if (base::ios::IsRunningOnIOS6OrLater())
+  if (base::ios::IsRunningOnIOS6OrLater() &&
+      ![[[[UIDevice currentDevice] identifierForVendor] UUIDString]
+          isEqualToString:@"00000000-0000-0000-0000-000000000000"]) {
     EXPECT_EQ(default_id, new_default_id);
-  else
+  } else {
     EXPECT_NE(default_id, new_default_id);
+  }
+
+  CleanNSUserDefaultsForDeviceId();
+}
+
+TEST_F(DeviceUtilTest, CheckMigration) {
+  CleanNSUserDefaultsForDeviceId();
+
+  NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+  [defaults setObject:@"10000000-0000-0000-0000-000000000000"
+               forKey:@"ChromeClientID"];
+  [defaults synchronize];
+  std::string expected_id = ios::device_util::GetDeviceIdentifier(NULL);
+  [defaults removeObjectForKey:@"ChromeClientID"];
+  [defaults setObject:@"10000000-0000-0000-0000-000000000000"
+               forKey:@"ChromiumClientID"];
+  [defaults synchronize];
+  std::string new_id = ios::device_util::GetDeviceIdentifier(NULL);
+  EXPECT_EQ(expected_id, new_id);
+
+  CleanNSUserDefaultsForDeviceId();
+}
+
+TEST_F(DeviceUtilTest, CheckMigrationFromZero) {
+  CleanNSUserDefaultsForDeviceId();
+
+  NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+  [defaults setObject:@"00000000-0000-0000-0000-000000000000"
+               forKey:@"ChromeClientID"];
+  [defaults synchronize];
+  std::string zero_id = ios::device_util::GetDeviceIdentifier(NULL);
+  [defaults removeObjectForKey:@"ChromeClientID"];
+  [defaults setObject:@"00000000-0000-0000-0000-000000000000"
+               forKey:@"ChromiumClientID"];
+  [defaults synchronize];
+  std::string new_id = ios::device_util::GetDeviceIdentifier(NULL);
+  EXPECT_NE(zero_id, new_id);
+
+  CleanNSUserDefaultsForDeviceId();
 }
 
 }  // namespace
