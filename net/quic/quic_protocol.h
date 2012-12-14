5e397c763427e8b9d8810d9f40e26e427fab0f9b
dpapad@chromium.org <dpapad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-14 17:43:48 +0000

    Revert 173170 > Move the packet receive time map from the AckFrame to the InterArrival congestion control feedback message > > Merge internal change: 39792871 > > > Review URL: https://chromiumcodereview.appspot.com/11485005
    TBR=rch@chromium.org
    Review URL: https://codereview.chromium.org/11572047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173174 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_protocol.h
+++ net/quic/quic_protocol.h
@@ -180,21 +180,29 @@ struct NET_EXPORT_PRIVATE ReceivedPacketInfo {
       std::ostream& os, const ReceivedPacketInfo& s);
 
   // Records a packet receipt.
-  void RecordReceived(QuicPacketSequenceNumber sequence_number);
+  void RecordReceived(QuicPacketSequenceNumber sequence_number, QuicTime time);
 
   // True if the sequence number is greater than largest_received or is listed
   // as missing.
   // Always returns false for sequence numbers less than least_unacked.
   bool IsAwaitingPacket(QuicPacketSequenceNumber sequence_number) const;
 
-  // Clears all missing packets less than |least_unacked|.
-  void ClearMissingBefore(QuicPacketSequenceNumber least_unacked);
+  // Clears all missing packets and ack times less than least_unacked.
+  void ClearAcksBefore(QuicPacketSequenceNumber least_unacked);
+
+  // Clears all packet sequence number and time pairs, preventing future
+  // transmission.
+  void ClearAckTimes();
 
   // The highest packet sequence number we've received from the peer.
   QuicPacketSequenceNumber largest_received;
 
   // The set of packets which we're expecting and have not received.
   SequenceSet missing_packets;
+
+  // The set of received packets since the last ack was sent and their arrival
+  // times.
+  std::map<QuicPacketSequenceNumber, QuicTime> received_packet_times;
 };
 
 struct NET_EXPORT_PRIVATE SentPacketInfo {
@@ -210,8 +218,9 @@ struct NET_EXPORT_PRIVATE SentPacketInfo {
 struct NET_EXPORT_PRIVATE QuicAckFrame {
   QuicAckFrame() {}
   // Testing convenience method to construct a QuicAckFrame with all packets
-  // from least_unacked to largest_received acked.
+  // from least_unacked to largest_received acked at time_received.
   QuicAckFrame(QuicPacketSequenceNumber largest_received,
+               QuicTime time_received,
                QuicPacketSequenceNumber least_unacked);
 
   NET_EXPORT_PRIVATE friend std::ostream& operator<<(
@@ -236,14 +245,9 @@ struct NET_EXPORT_PRIVATE CongestionFeedbackMessageTCP {
 };
 
 struct NET_EXPORT_PRIVATE CongestionFeedbackMessageInterArrival {
-  CongestionFeedbackMessageInterArrival();
-  ~CongestionFeedbackMessageInterArrival();
   uint16 accumulated_number_of_lost_packets;
   int16 offset_time;
   uint16 delta_time;  // delta time is described below.
-  // The set of received packets since the last feedback was sent, along with
-  // their arrival times.
-  TimeMap received_packet_times;
 };
 
 /*
@@ -268,18 +272,15 @@ struct NET_EXPORT_PRIVATE CongestionFeedbackMessageFixRate {
 };
 
 struct NET_EXPORT_PRIVATE QuicCongestionFeedbackFrame {
-  QuicCongestionFeedbackFrame();
-  ~QuicCongestionFeedbackFrame();
-
+  CongestionFeedbackType type;
   NET_EXPORT_PRIVATE friend std::ostream& operator<<(
       std::ostream& os, const QuicCongestionFeedbackFrame& c);
 
-  CongestionFeedbackType type;
-  // This should really be a union, but since the inter arrival struct
-  // is non-trivial, C++ prohibits it.
-  CongestionFeedbackMessageTCP tcp;
-  CongestionFeedbackMessageInterArrival inter_arrival;
-  CongestionFeedbackMessageFixRate fix_rate;
+  union {
+    CongestionFeedbackMessageTCP tcp;
+    CongestionFeedbackMessageInterArrival inter_arrival;
+    CongestionFeedbackMessageFixRate fix_rate;
+  };
 };
 
 struct NET_EXPORT_PRIVATE QuicRstStreamFrame {
