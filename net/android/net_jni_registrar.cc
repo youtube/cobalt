88500ecbabea8389eeded0c548231bd2b6814985
kristianm@chromium.org <kristianm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-15 22:19:44 +0000

    Adding java implementation for Geolocation
    Review URL: https://chromiumcodereview.appspot.com/11416347
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173328 0039d316-1c4b-4281-b951-d872f2087c98

--- net/android/net_jni_registrar.cc
+++ net/android/net_jni_registrar.cc
@@ -7,6 +7,7 @@
 #include "base/basictypes.h"
 #include "base/android/jni_android.h"
 #include "base/android/jni_registrar.h"
+#include "net/android/gurl_utils.h"
 #include "net/android/network_change_notifier_android.h"
 #include "net/android/network_library.h"
 #include "net/proxy/proxy_config_service_android.h"
@@ -16,6 +17,7 @@ namespace android {
 
 static base::android::RegistrationMethod kNetRegisteredMethods[] = {
   { "AndroidNetworkLibrary", net::android::RegisterNetworkLibrary },
+  { "GURLUtils", net::RegisterGURLUtils },
   { "NetworkChangeNotifierAndroid",
     net::NetworkChangeNotifierAndroid::Register },
   { "ProxyConfigService", net::ProxyConfigServiceAndroid::Register },
