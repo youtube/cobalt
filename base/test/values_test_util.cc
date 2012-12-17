426ea7febb7aab5c180e0feeb7eef82fe33f18d3
jyasskin@chromium.org <jyasskin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-17 21:45:37 +0000

    Add a base::ParseJson() function to help tests construct Values.
    This is very similar to base::JSONReader::Read() except that it allows
    trailing commas and EXPECT-fails with a good message when the json is
    malformed.
    
    I migrated two tests to use this as a demonstration that it's widely
    useful and makes things more readable.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11567027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173540 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/values_test_util.cc
+++ base/test/values_test_util.cc
@@ -2,9 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/test/values_test_util.h"
+
+#include "base/json/json_reader.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/string_number_conversions.h"
-#include "base/test/values_test_util.h"
 #include "base/values.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -58,4 +60,19 @@ void ExpectStringValue(const std::string& expected_str,
   EXPECT_EQ(expected_str, actual_str);
 }
 
+namespace test {
+
+scoped_ptr<Value> ParseJson(base::StringPiece json) {
+  std::string error_msg;
+  scoped_ptr<Value> result(base::JSONReader::ReadAndReturnError(
+      json, base::JSON_ALLOW_TRAILING_COMMAS,
+      NULL, &error_msg));
+  if (!result) {
+    ADD_FAILURE() << "Failed to parse \"" << json << "\": " << error_msg;
+    result.reset(Value::CreateNullValue());
+  }
+  return result.Pass();
+}
+
+}  // namespace test
 }  // namespace base
