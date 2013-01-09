85f2a1ccd2448adce736d6337ac9ff0de6ecce93
Cobalt Team <cobalt-team@google.com>
2013-01-08 16:29:16 -0800

    Remove BLUE references and Wii U build fixes
    Change-Id: I07a43ed59cb7d0564bb95d7fb44fed4208ebb751

--- net/base/net_util.cc
+++ net/base/net_util.cc
@@ -1548,7 +1548,7 @@ int SetNonBlocking(int fd) {
 #if defined(OS_WIN)
   unsigned long no_block = 1;
   return ioctlsocket(fd, FIONBIO, &no_block);
-#elif defined(__LB_PS3__) || defined(__LB_BLUE__)
+#elif defined(__LB_PS3__) || defined(__LB_WIIU__)
   int val = 1;
   return setsockopt(fd, SOL_SOCKET, SO_NBIO, &val, sizeof(int));
 #elif defined(OS_POSIX)
