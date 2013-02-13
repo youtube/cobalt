13ba00a10e4a18fcee38a687aeed932fe2cc6a41
Warren Wang <wpwang@google.com>
2013-02-12 16:16:01 -0800

    PS3 doesn't have std::hash()
    Use the MSVC path, along with some templating fixes
    
    Change-Id: Icb7994387a105bf90c70f89154b6e29889b56a9d

--- net/http/infinite_cache.cc
+++ net/http/infinite_cache.cc
@@ -222,8 +222,9 @@ void OnComplete(const net::CompletionCallback& callback, int* result) {
 }  // namespace
 
 namespace BASE_HASH_NAMESPACE {
-#if defined(COMPILER_MSVC)
-inline size_t hash_value(const Key& key) {
+#if defined(COMPILER_MSVC) || defined(__LB_PS3__)
+template <>
+inline size_t hash_value<Key>(const Key& key) {
   return base::Hash(key.value, kKeySizeBytes);
 }
 #elif defined(COMPILER_GCC)
@@ -408,7 +409,7 @@ class InfiniteCache::Worker : public base::RefCountedThreadSafe<Worker> {
 
  private:
   friend class base::RefCountedThreadSafe<Worker>;
-#if defined(COMPILER_MSVC)
+#if defined(COMPILER_MSVC) || defined(__LB_PS3__)
   typedef BASE_HASH_NAMESPACE::hash_map<
       Key, Details, BASE_HASH_NAMESPACE::hash_compare<Key, Key_less> > KeyMap;
 #elif defined(COMPILER_GCC)
