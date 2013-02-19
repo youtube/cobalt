0683473dabc9cb2444ff3294a13d34fed5754781
Cobalt Team <cobalt-team@google.com>
2013-02-19 14:31:48 -0800

    Add EnableInProcessStackDumping function definition.
    Change-Id: I21dea6ae32354b02087ed2c39b8a775df79021e2

--- base/debug/stack_trace_shell.cc
+++ base/debug/stack_trace_shell.cc
@@ -1,2 +1,24 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // We use the same code as posix
 #include "stack_trace_posix.cc"
+
+namespace base {
+namespace debug {
+
+// But re-implement this:
+bool EnableInProcessStackDumping() {
+#if defined(__LB_LINUX__) || defined(__LB_PS3__)
+  return true;
+#elif defined(__LB_WIIU__)
+  return false;
+#else
+#error Must specify EnableInProcessStackDumping for platform.
+#endif
+}
+
+} // namespace debug
+} // namespace base
+
