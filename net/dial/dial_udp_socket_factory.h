504801309d1321ed3b810d2137865ade11626f45
Cobalt Team <cobalt-team@google.com>
2013-02-25 17:23:50 -0800

    Dial UDP server
    Change-Id: I1e15f921145df0a62b19412f45ce4abf22a6bd1d

---
+++ net/dial/dial_udp_socket_factory.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_DIAL_DIAL_UDP_SOCKET_FACTORY_H
+#define NET_DIAL_DIAL_UDP_SOCKET_FACTORY_H
+
+#include "net/udp/udp_listen_socket.h"
+
+namespace net {
+
+class IPEndPoint;
+
+class UdpSocketFactory {
+ public:
+  virtual scoped_refptr<UDPListenSocket> CreateAndBind(
+      const IPEndPoint& address, UDPListenSocket::Delegate* del);
+  virtual ~UdpSocketFactory() {}
+
+ protected:
+  virtual void SetupSocketAfterCreate(SocketDescriptor) { }
+  virtual void SetupSocketAfterBind(SocketDescriptor) { }
+};
+
+class DialUdpSocketFactory : public UdpSocketFactory {
+  virtual ~DialUdpSocketFactory();
+ protected:
+  // UdpSocketFactory implementation
+  virtual void SetupSocketAfterCreate(SocketDescriptor) OVERRIDE;
+  virtual void SetupSocketAfterBind(SocketDescriptor) OVERRIDE;
+};
+
+} // namespace net
+
+#endif // NET_DIAL_DIAL_UDP_SOCKET_FACTORY_H
+
