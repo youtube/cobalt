5ef0c7eb162c7bd3365b39332859be81d1e315e4
Joey Parrish <joeyparrish@google.com>
2013-03-11 14:39:03 -0700

    remove extra SSL checks
    manual cherry-pick from GOLD_0105
    
    Change-Id: I5e520b0e51f04c7d28d28a63877c84a5fbaa2f0e

--- net/base/cert_status_flags.h
+++ net/base/cert_status_flags.h
@@ -37,9 +37,6 @@ static const CertStatus CERT_STATUS_WEAK_KEY                   = 1 << 11;
 static const CertStatus CERT_STATUS_IS_EV                      = 1 << 16;
 static const CertStatus CERT_STATUS_REV_CHECKING_ENABLED       = 1 << 17;
 // bit 18 was CERT_STATUS_IS_DNSSEC.
-#if defined(__LB_SHELL__)
-static const CertStatus CERT_STATUS_CONFIRM_SAFE               = 1 << 31;
-#endif
 
 // Returns true if the specified cert status has an error set.
 static inline bool IsCertStatusError(CertStatus status) {
