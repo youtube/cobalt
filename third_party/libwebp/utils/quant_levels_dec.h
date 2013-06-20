6f52530dbc05e5ae63995d5349f9caa7e3a059d9
jzern@chromium.org <jzern@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 17:23:14 +0000

    libwebp-0.3.1
    Bug fix rollup + incremental decode support for extended format files
    (alpha/icc + vp8).
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/16871017
    
    LB_SHELL
    rollup of the following commits:
    b2e6996bb42e5a2cd41da0dbd2c94f368330b111
    bcc141027b20b1cb782bd1e3b02a495462afbcba
    73cdbdbfd9e342f7fca0b24949e32a0ddda24707
    9d5101ca47eee8bb51075ee9f5869e5e55876cb6
    a01b341a9822bd37809ebc1e25a2aa99d7ee1cab
    668e5cae5df64bd2d074774512dadcee1cc51d7a
    21f871d9aba51273ccdb8b79fd5f44f7ed980b6e
    ca9d88d5993993b2975ab684b86e98367f20cf05
    
    Change-Id: I423218b275796f6fd98809fb016351ffee777ff9
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207471 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/libwebp/utils/quant_levels_dec.h
+++ third_party/libwebp/utils/quant_levels_dec.h
@@ -1,4 +1,4 @@
-// Copyright 2011 Google Inc. All Rights Reserved.
+// Copyright 2013 Google Inc. All Rights Reserved.
 //
 // Use of this source code is governed by a BSD-style license
 // that can be found in the COPYING file in the root of the source
@@ -7,14 +7,12 @@
 // be found in the AUTHORS file in the root of the source tree.
 // -----------------------------------------------------------------------------
 //
-// Alpha plane quantization utility
+// Alpha plane de-quantization utility
 //
 // Author:  Vikas Arora (vikasa@google.com)
 
-#ifndef WEBP_UTILS_QUANT_LEVELS_H_
-#define WEBP_UTILS_QUANT_LEVELS_H_
-
-#include <stdlib.h>
+#ifndef WEBP_UTILS_QUANT_LEVELS_DEC_H_
+#define WEBP_UTILS_QUANT_LEVELS_DEC_H_
 
 #include "../webp/types.h"
 
@@ -22,15 +20,13 @@
 extern "C" {
 #endif
 
-// Replace the input 'data' of size 'width'x'height' with 'num-levels'
-// quantized values. If not NULL, 'sse' will contain the sum of squared error.
-// Valid range for 'num_levels' is [2, 256].
-// Returns false in case of error (data is NULL, or parameters are invalid).
-int QuantizeLevels(uint8_t* const data, int width, int height, int num_levels,
-                   uint64_t* const sse);
+// Apply post-processing to input 'data' of size 'width'x'height' assuming
+// that the source was quantized to a reduced number of levels.
+// Returns false in case of error (data is NULL, invalid parameters, ...).
+int DequantizeLevels(uint8_t* const data, int width, int height);
 
 #if defined(__cplusplus) || defined(c_plusplus)
 }    // extern "C"
 #endif
 
-#endif  /* WEBP_UTILS_QUANT_LEVELS_H_ */
+#endif  /* WEBP_UTILS_QUANT_LEVELS_DEC_H_ */
