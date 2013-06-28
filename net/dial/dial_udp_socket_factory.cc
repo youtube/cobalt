5c65033c90c0bd2982232d976d72bb569a4ad9c8
Cobalt Team <cobalt-team@google.com>
2013-06-28 15:08:31 -0700

    Cleanup for open source release
    Change-Id: Icaa636bae5693f526ff62461fb61ced22ced8bd3

--- net/dial/dial_udp_socket_factory.cc
+++ net/dial/dial_udp_socket_factory.cc
@@ -73,7 +73,6 @@ void DialUdpSocketFactory::SetupSocketAfterBind(SocketDescriptor s) {
   struct ip_mreq imreq;
   imreq.imr_multiaddr.s_addr = ::inet_addr("239.255.255.250");
 #if defined(__LB_WIIU__)
-  // The WiiU INADDR_ANY is unreliable.
   struct in_addr src_addr;
   ignore_result(LB::Platform::GetLocalIpAddress(&src_addr));
   imreq.imr_interface.s_addr = htonl(src_addr.s_addr);
