1186b922edfe17aa133f39c218c63ea77a57fe7d
Cobalt Team <cobalt-team@google.com>
2013-07-11 14:30:38 -0700

    Get base compiling with /sdl flag.
    - MSVC does not like some of the posix function names, so working around that.
    - Disable a test that deals with shared memory since that does not compile.
    - Move stack_trace_shell to the posix version.
    
    Change-Id: I403c6d2f7c69c739f0060d583a4049eefe31897b

--- base/debug/stack_trace_shell.cc
+++ base/debug/stack_trace_shell.cc
@@ -2,11 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#if defined(__LB_XB1__)
-#include "stack_trace_win.cc"
-#else
 #include "stack_trace_posix.cc"
-#endif
 
 namespace base {
 namespace debug {
