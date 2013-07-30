61afe4f64927b9b550f182fdbd9d2da6d7a9c7d2
Cobalt Team <cobalt-team@google.com>
2013-07-30 11:51:51 -0700

    Fixes to get file tests not crash on unit_tests_base.
    1. Use our own debugger_shell
    2. Use windows style paths for XB1
    3. Fix PlatformCloseFile to check for invalid file ids.
    
    Change-Id: I52c77abc219410c1605be08a620f3427c211fd23

--- base/file_path.h
+++ base/file_path.h
@@ -117,10 +117,10 @@
 // enabled and disabled independently, to aid testing.  These #defines are
 // here so that the same setting can be used in both the implementation and
 // in the unit test.
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(__LB_XB1__)
 #define FILE_PATH_USES_DRIVE_LETTERS
 #define FILE_PATH_USES_WIN_SEPARATORS
-#endif  // OS_WIN
+#endif  // OS_WIN || __LB_XB1__
 
 class Pickle;
 class PickleIterator;
