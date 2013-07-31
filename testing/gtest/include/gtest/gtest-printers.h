650d58e2a55aae0a45e494649d9cc7dbd7d10524
Cobalt Team <cobalt-team@google.com>
2013-07-31 16:41:33 -0700

    Turn off this spammy warning on GHS
    It was already disabled for SNC, follow suit.
    
    Change-Id: I3e8625728b8f7c1c4b77e61383d8e4b39a6422cd

--- testing/gtest/include/gtest/gtest-printers.h
+++ testing/gtest/include/gtest/gtest-printers.h
@@ -323,14 +323,17 @@ void DefaultPrintTo(IsNotContainer /* dummy */,
       // even using reinterpret_cast, as earlier versions of gcc
       // (e.g. 3.4.5) cannot compile the cast when p is a function
       // pointer.  Casting to UInt64 first solves the problem.
-#if defined(__LB_PS3__)
+#if defined(__SNC__)
 #pragma diag_push
-#pragma diag_suppress=1067  // Suppress warning 1067
+#pragma diag_suppress=1067
+#elif defined(__ghs__)
+#pragma diag_push
+#pragma diag_suppress=1053
 #endif
       *os << reinterpret_cast<const void*>(
           reinterpret_cast<internal::UInt64>(p));
-#if defined(__LB_PS3__)
-#pragma diag_pop  // Renable warning 1067
+#if defined(__SNC__) || defined (__ghs__)
+#pragma diag_pop  // Renable warnings
 #endif
     }
   }
