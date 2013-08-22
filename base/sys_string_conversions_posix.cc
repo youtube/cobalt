388ffad59b3800d9fe18aeace900ed91ceb7eaec
Fritz Koenig <frkoenig@google.com>
2013-08-21 17:36:28 -0700

    Don't convert to native strings using wcrtomb.
    Use UTF-8 all the time instead of trying to use the system locale.
    
    Change-Id: I4fa4de941caec53925aa9903d26b5fdd30ebcf39

--- base/sys_string_conversions_posix.cc
+++ base/sys_string_conversions_posix.cc
@@ -24,7 +24,7 @@ std::wstring SysUTF8ToWide(const StringPiece& utf8) {
   return out;
 }
 
-#if defined(OS_CHROMEOS) || defined(OS_ANDROID)
+#if defined(OS_CHROMEOS) || defined(OS_ANDROID) || defined(__LB_SHELL__)
 // TODO(port): Consider reverting the OS_ANDROID when we have wcrtomb()
 // support and a better understanding of what calls these routines.
 
