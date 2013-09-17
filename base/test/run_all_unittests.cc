d6b163c5d4a0597aa8f83dc1d2c4ecf405926f72
Cobalt Team <cobalt-team@google.com>
2013-09-17 16:34:05 -0700

    Get rid of obsolete and messy main stack size test harness code.
    Change-Id: I980c924f3fddca5fef321b25ce90f4bc467fe527

--- base/test/run_all_unittests.cc
+++ base/test/run_all_unittests.cc
@@ -5,47 +5,7 @@
 #include "base/test/main_hook.h"
 #include "base/test/test_suite.h"
 
-#if defined(__LB_SHELL__)
-
-#include "lb_shell/lb_shell_constants.h"
-
-struct Args {
-  int argc;
-  char** argv;
-  int ret_val;
-};
-
-static void* RunTests(void* params) {
-  Args* args = static_cast<Args*>(params);
-
-  MainHook hook(NULL, args->argc, args->argv);
-  args->ret_val = base::TestSuite(args->argc, args->argv).Run();
-
-  return NULL;
-}
-
-int main(int argc, char **argv) {
-  // Run the actual tests in a helper method run from another thread
-  // so that we can control that thread's stack size.
-  pthread_attr_t attributes;
-  pthread_attr_init(&attributes);
-  pthread_attr_setstacksize(&attributes, kWebKitThreadStackSize);
-
-  pthread_t helper_thread;
-  Args args;
-  args.argc = argc;
-  args.argv = argv;
-  args.ret_val = -1;
-  pthread_create(&helper_thread, &attributes, RunTests, &args);
-  pthread_join(helper_thread, NULL);
-  return args.ret_val;
-}
-
-#else
-
 int main(int argc, char** argv) {
   MainHook hook(main, argc, argv);
   return base::TestSuite(argc, argv).Run();
 }
-
-#endif
