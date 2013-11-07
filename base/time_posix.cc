b924c24a88c8ae4df27308de405e3b1de8ada77f
Cobalt Team <cobalt-team@google.com>
2013-11-06 21:45:17 -0800

    Use android specific time
    Change-Id: Ib38bc2dd253ba3413eaa6416d1c90444ee773ff4

--- base/time_posix.cc
+++ base/time_posix.cc
@@ -6,7 +6,7 @@
 
 #include <sys/time.h>
 #include <time.h>
-#if defined(OS_ANDROID)
+#if defined(OS_ANDROID) || defined(__LB_ANDROID__)
 #include <time64.h>
 #endif
 #include <unistd.h>
@@ -27,7 +27,7 @@ namespace {
 // Define a system-specific SysTime that wraps either to a time_t or
 // a time64_t depending on the host system, and associated convertion.
 // See crbug.com/162007
-#if defined(OS_ANDROID)
+#if defined(OS_ANDROID) || defined(__LB_ANDROID__)
 typedef time64_t SysTime;
 
 SysTime SysTimeFromTimeStruct(struct tm* timestruct, bool is_local) {
