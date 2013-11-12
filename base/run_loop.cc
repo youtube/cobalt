204df4e37b08905345d675d815526827bd9076d2
Fritz Koenig <frkoenig@google.com>
2013-11-12 11:50:48 -0800

    Use native message loop for __LB_ANDROID__
    Change-Id: Ia5750565145a58179121ce655a5b64268e3e2e5a

--- base/run_loop.cc
+++ base/run_loop.cc
@@ -17,12 +17,12 @@ RunLoop::RunLoop()
       quit_called_(false),
       running_(false),
       quit_when_idle_received_(false) {
-#if !defined(OS_MACOSX) && !defined(OS_ANDROID)
+#if !defined(OS_MACOSX) && !defined(OS_ANDROID) && !defined(__LB_ANDROID__)
    dispatcher_ = NULL;
 #endif
 }
 
-#if !defined(OS_MACOSX) && !defined(OS_ANDROID)
+#if !defined(OS_MACOSX) && !defined(OS_ANDROID) && !defined(__LB_ANDROID__)
 RunLoop::RunLoop(MessageLoop::Dispatcher* dispatcher)
     : loop_(MessageLoop::current()),
       weak_factory_(this),
