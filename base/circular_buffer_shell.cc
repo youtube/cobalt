67d0aa0d166edaa0a291f9ab4e7aef5a52f4a62b
Cobalt Team <cobalt-team@google.com>
2014-05-30 18:40:40 -0700

    Adding a circular buffer to base.
    In XB1, a circular buffer implementation was added for the
    platform-specific HTTP implementation in chromium/net. This is a
    generally useful data structure, so rather than fork it for XB360,
    I'm instead promoting it to chromium/base (with traditional _shell
    name mangling). I also (arguably) cleaned it up a bit and added
    some more tests.
    
    Change-Id: Ib3c1d530c406129d21a9a0009be73a02440418c7
    (cherry picked from commit 83ee3c8478002f728a8075921b44d3e5ee8d447f)

---
+++ base/circular_buffer_shell.cc
@@ -0,0 +1,189 @@
+// Copyright (c) 2014 Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/circular_buffer_shell.h"
+
+#include <algorithm>
+
+#include "base/logging.h"
+
+static inline void *add_to_pointer(void *pointer, size_t amount) {
+    return static_cast<uint8_t *>(pointer) + amount;
+}
+
+static inline const void *add_to_pointer(const void *pointer, size_t amount) {
+    return static_cast<const uint8_t *>(pointer) + amount;
+}
+
+namespace base {
+
+CircularBufferShell::CircularBufferShell(size_t max_capacity)
+    : max_capacity_(max_capacity),
+      capacity_(0),
+      length_(0),
+      buffer_(NULL),
+      read_position_(0),
+      write_position_(0) {
+}
+
+CircularBufferShell::~CircularBufferShell() {
+  Clear();
+}
+
+void CircularBufferShell::Clear() {
+  base::AutoLock l(lock_);
+  if (buffer_ != NULL) {
+    free(buffer_);
+    buffer_ = NULL;
+  }
+
+  capacity_ = 0;
+  length_ = 0;
+  read_position_ = 0;
+  write_position_ = 0;
+}
+
+void CircularBufferShell::Read(void *destination, size_t length,
+                               size_t *bytes_read) {
+  base::AutoLock l(lock_);
+  DCHECK(destination != NULL || length == 0);
+  if (destination == NULL)
+    length = 0;
+
+  ReadUnchecked(destination, length, bytes_read);
+}
+
+bool CircularBufferShell::Write(const void *source, size_t length,
+                                size_t *bytes_written) {
+  base::AutoLock l(lock_);
+  DCHECK(source != NULL || length == 0);
+  if (source == NULL)
+    length = 0;
+
+  if (!EnsureCapacityToWrite(length)) {
+    return false;
+  }
+
+  size_t produced = 0;
+  while (true) {
+    size_t remaining = length - produced;
+
+    // In this pass, write up to the contiguous space left.
+    size_t to_write = std::min(remaining, capacity_ - write_position_);
+    if (to_write == 0)
+      break;
+
+    // Copy this segment and do the accounting.
+    void *destination = GetWritePointer();
+    const void *src = add_to_pointer(source, produced);
+    memcpy(destination, src, to_write);
+    length_ += to_write;
+    write_position_ = (write_position_ + to_write) % capacity_;
+    produced += to_write;
+  }
+
+  if (bytes_written)
+    *bytes_written = produced;
+  return true;
+}
+
+size_t CircularBufferShell::GetLength() {
+  base::AutoLock l(lock_);
+  return length_;
+}
+
+
+void CircularBufferShell::ReadUnchecked(void *destination, size_t length,
+                                        size_t *bytes_read) {
+  size_t consumed = 0;
+  while (true) {
+    size_t remaining = std::min(length_, length - consumed);
+
+    // In this pass, read the remaining data that is contiguous.
+    size_t to_read = std::min(remaining, capacity_ - read_position_);
+    if (to_read == 0)
+      break;
+
+    // Copy this segment and do the accounting.
+    const void *source = GetReadPointer();
+    void *dest = add_to_pointer(destination, consumed);
+    memcpy(dest, source, to_read);
+    length_ -= to_read;
+    read_position_ = (read_position_ + to_read) % capacity_;
+    consumed += to_read;
+  }
+
+  if (bytes_read)
+    *bytes_read = consumed;
+}
+
+const void *CircularBufferShell::GetReadPointer() {
+  return add_to_pointer(buffer_, read_position_);
+}
+
+void *CircularBufferShell::GetWritePointer() {
+  return add_to_pointer(buffer_, write_position_);
+}
+
+bool CircularBufferShell::EnsureCapacityToWrite(size_t length) {
+  if (capacity_ - length_ < length) {
+    size_t capacity = std::max(2 * capacity_, length_ + length);
+    if (capacity > max_capacity_)
+      capacity = max_capacity_;
+
+    // New capacity still won't be enough.
+    if (capacity - length_ < length) {
+      return false;
+    }
+
+    return IncreaseCapacityTo(capacity);
+  }
+
+  return true;
+}
+
+bool CircularBufferShell::IncreaseCapacityTo(size_t capacity) {
+  if (capacity <= capacity_) {
+    return true;
+  }
+
+  // If the data isn't wrapped, we can just use realloc.
+  if (buffer_ != NULL && read_position_ + length_ <= capacity_) {
+    void *result = realloc(buffer_, capacity);
+    if (result == NULL) {
+      return false;
+    }
+    capacity_ = capacity;
+    buffer_ = result;
+
+    // Edge case for when write_position has wrapped to the old capacity. We
+    // can still realloc, but we have to pop the write position back to the end
+    // of the data.
+    if (write_position_ == 0)
+      write_position_ = length_;
+    return true;
+  }
+
+  void *buffer = malloc(capacity);
+  if (buffer == NULL) {
+    return false;
+  }
+
+  // Read does everything we want, but it will trounce length_.
+  size_t length = length_;
+
+  // Copy the data over to the new buffer.
+  ReadUnchecked(buffer, length_, NULL);
+
+  // Adjust the accounting.
+  length_ = length;
+  read_position_ = 0;
+  write_position_ = length_;
+  capacity_ = capacity;
+  free(buffer_);
+  buffer_ = buffer;
+  return true;
+}
+
+}  // namespace base
