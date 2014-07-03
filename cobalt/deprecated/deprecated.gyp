1761af8d75751735b9acc49e2e0c4d4b92008d28
Cobalt Team <cobalt-team@google.com>
2014-07-02 17:11:14 -0700

    Moving and cleaning up LBShellPlatformDelegate into cobalt.
    PlatformDelegate is located in a deprecated directory to indicate that this
    functionality is only temporary and that chromium/base should be refactored
    to remove any dependencies on cobalt.
    
    Change-Id: I4074b2ed64ce3f70e0969f552fc56b8d217c7374

---
+++ cobalt/deprecated/deprecated.gyp
@@ -0,0 +1,19 @@
+{
+  'targets': [
+    {
+      'target_name': 'platform_delegate',
+      'type': '<(component)',
+      'toolsets': ['target'],
+
+      'include_dirs': [
+        '<(DEPTH)',
+      ],
+
+      'sources': [
+        'platform_delegate.h',
+        'platform_delegate.cc',
+        'platform_delegate_<(target_arch).cc',
+      ],
+    },
+  ],
+}
---
+++ cobalt/deprecated/platform_delegate.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2012 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SRC_COBALT_DEPERECATED_PLATFORM_DELEGATE_H
+#define SRC_COBALT_DEPERECATED_PLATFORM_DELEGATE_H
+
+#include "cobalt/cobalt_export.h"
+
+#include <string>
+
+namespace cobalt {
+
+namespace deprecated {
+
+class COBALT_EXPORT PlatformDelegate {
+ public:
+  // first method called by main()
+  static void Init();
+  static void PlatformInit();
+  static void PlatformMediaInit();
+  // gets the system langauge
+  // NOTE: should be in the format described by bcp47.
+  // http://www.rfc-editor.org/rfc/bcp/bcp47.txt
+  // Example: "en-US" or "de"
+  static std::string GetSystemLanguage();
+  // last method called by main()
+  static void Teardown();
+  static void PlatformTeardown();
+  static void PlatformMediaTeardown();
+  // perform system update tasks at checkpoints during startup
+  static void PlatformUpdateDuringStartup();
+  // true if exit game was requested by the platform.
+  static bool ExitGameRequested();
+  // gets the title id of the game
+  static const char *GameTitleId();
+  // check parental control settings.
+  static void CheckParentalControl();
+  // enable output protection (HDCP or CGMS) and block until complete
+  static bool ProtectOutput();
+};
+
+}   // namespace deprecated
+
+}   // namespace cobalt
+
+#endif  // SRC_COBALT_DEPERECATED_PLATFORM_DELEGATE_H
