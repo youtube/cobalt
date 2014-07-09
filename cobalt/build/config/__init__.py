792a1b6ca2f2bc34059f48cb50cf79a3e083431e
Cobalt Team <cobalt-team@google.com>
2014-07-09 02:00:39 -0700

    GYP build support for Cobalt
    - Added cobalt/build/gyp_cobalt tool. Similar to gyp_driver, run with '--help'
      for more information.
    - Added toolchain configuration for Linux.
    - Copied the ninja binaries from lbshell.
    - Adjusted all.gyp and samples.gyp to support some new GYP variables.
    
    Change-Id: I858f693b79ae35f470c2eb232740f68da58b6c8c

--- cobalt/build/config/__init__.py
+++ cobalt/build/config/__init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # Copyright 2014 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,19 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Initialization for the config package."""
 
-# This files contains all targets that should be created by gyp_cobalt by
-# default.
 
-{
-  'targets': [
-    {
-      'target_name': 'All',
-      'type': 'none',
-      'dependencies': [
-        '<(DEPTH)/cobalt/samples/samples.gyp:simple_example_deploy',
-        '<(DEPTH)/cobalt/samples/samples.gyp:simple_example_test_deploy',
-      ]
-    }
-  ],
-}
+from base import LoadPlatformConfig
+from base import VALID_BUILD_CONFIGS
+from base import VALID_PLATFORMS
