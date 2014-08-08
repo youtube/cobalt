10406c9171d748133d63ee96d989228d0ef04e28
Xiaoming Shi <xiaomings@google.com>
2014-08-07 18:22:33 -0700

    Refactor ShellVideoDecoderImpl so it can handle retry.
    Also did some code clean up.
    
    Change-Id: I9a90f521ecc32558a5796c549acb080f829dc357

--- media/filters/shell_video_decoder_impl.cc
+++ media/filters/shell_video_decoder_impl.cc
@@ -21,15 +21,13 @@
 #include "base/callback_helpers.h"
 #include "base/debug/trace_event.h"
 #include "base/logging.h"
-#include "base/stringprintf.h"
 #include "lb_shell/lb_shell_constants.h"
 #include "lb_video_decoder.h"
 #include "media/base/bind_to_loop.h"
 #include "media/base/pipeline_status.h"
 #include "media/base/shell_buffer_factory.h"
-#include "media/base/shell_filter_graph_log.h"
-#include "media/base/shell_filter_graph_log_constants.h"
 #include "media/base/shell_media_statistics.h"
+#include "media/base/video_decoder_config.h"
 #include "media/base/video_frame.h"
 
 using LB::LBVideoDecoder;
@@ -54,9 +52,7 @@ ShellVideoDecoderImpl::ShellVideoDecoderImpl(
     const scoped_refptr<base::MessageLoopProxy>& message_loop)
     : state_(kUninitialized)
     , media_pipeline_message_loop_(message_loop)
-    , raw_decoder_(NULL)
-    , read_pending_(false)
-    , decoder_thread_("H264 Decoder") {
+    , decoder_thread_("Video Decoder") {
 }
 
 ShellVideoDecoderImpl::~ShellVideoDecoderImpl() {
@@ -64,8 +60,6 @@ ShellVideoDecoderImpl::~ShellVideoDecoderImpl() {
   if (decoder_thread_.message_loop())
     decoder_thread_.message_loop()->AssertIdle();
 #endif
-
-  delete raw_decoder_;
 }
 
 void ShellVideoDecoderImpl::Initialize(
@@ -81,28 +75,28 @@ void ShellVideoDecoderImpl::Initialize(
     return;
   }
   demuxer_stream_ = stream;
-  status_cb_ = status_cb;
-  filter_graph_log_ = demuxer_stream_->filter_graph_log();
-  filter_graph_log_->LogEvent(kObjectIdVideoDecoder, kEventInitialize);
 
-  decoder_config_.CopyFrom(demuxer_stream_->video_decoder_config());
-  DLOG(INFO) << "Configuration at Start: " <<
-      decoder_config_.AsHumanReadableString();
+  TRACE_EVENT0("lb_shell", "ShellVideoDecoderImpl::Initialize()");
 
-  raw_decoder_ = LBVideoDecoder::Create(
-      decoder_config_, demuxer_stream_->GetDecryptor(),
-      demuxer_stream_->StreamWasEncrypted());
+  VideoDecoderConfig decoder_config;
+  decoder_config.CopyFrom(demuxer_stream_->video_decoder_config());
+  DLOG(INFO) << "Configuration at Start: "
+             << decoder_config.AsHumanReadableString();
+
+  raw_decoder_.reset(LBVideoDecoder::Create(
+      decoder_config, demuxer_stream_->GetDecryptor(),
+      demuxer_stream_->StreamWasEncrypted()));
 
   if (!raw_decoder_) {
-    status_cb_.Run(DECODER_ERROR_NOT_SUPPORTED);
+    status_cb.Run(DECODER_ERROR_NOT_SUPPORTED);
     return;
   }
 
-  UPDATE_MEDIA_STATISTICS(STAT_TYPE_VIDEO_CODEC, decoder_config_.codec());
+  UPDATE_MEDIA_STATISTICS(STAT_TYPE_VIDEO_CODEC, decoder_config.codec());
   UPDATE_MEDIA_STATISTICS(STAT_TYPE_VIDEO_WIDTH,
-                          decoder_config_.natural_size().width());
+                          decoder_config.natural_size().width());
   UPDATE_MEDIA_STATISTICS(STAT_TYPE_VIDEO_HEIGHT,
-                          decoder_config_.natural_size().height());
+                          decoder_config.natural_size().height());
 
   base::Thread::Options options;
 
@@ -112,29 +106,29 @@ void ShellVideoDecoderImpl::Initialize(
 
   if (decoder_thread_.StartWithOptions(options)) {
     state_ = kNormal;
-    status_cb_.Run(PIPELINE_OK);
+    status_cb.Run(PIPELINE_OK);
   } else {
-    status_cb_.Run(PIPELINE_ERROR_DECODE);
+    status_cb.Run(PIPELINE_ERROR_DECODE);
   }
 }
 
 void ShellVideoDecoderImpl::Read(const ReadCB& read_cb) {
-  DCHECK(media_pipeline_message_loop_->BelongsToCurrentThread());
-  decoder_thread_.message_loop_proxy()->PostTask(
-      FROM_HERE,
-      base::Bind(
-          &ShellVideoDecoderImpl::DoRead,
-          this,
-          read_cb));
-}
+  if (!decoder_thread_.message_loop_proxy()->BelongsToCurrentThread()) {
+    decoder_thread_.message_loop_proxy()->PostTask(
+        FROM_HERE,
+        base::Bind(
+            &ShellVideoDecoderImpl::Read,
+            this,
+            read_cb));
+    return;
+  }
 
-void ShellVideoDecoderImpl::DoRead(const ReadCB& read_cb) {
-  DCHECK(decoder_thread_.message_loop_proxy()->BelongsToCurrentThread());
   DCHECK(!read_cb.is_null());
   DCHECK(read_cb_.is_null()) << "overlapping reads not supported";
   DCHECK_NE(state_, kUninitialized);
 
-  filter_graph_log_->LogEvent(kObjectIdVideoDecoder, kEventRead);
+  TRACE_EVENT0("lb_shell", "ShellVideoDecoderImpl::DoRead()");
+
   read_cb_ = BindToLoop(media_pipeline_message_loop_, read_cb);
 
   // if an error has occurred, return error
@@ -157,27 +151,14 @@ void ShellVideoDecoderImpl::DoRead(const ReadCB& read_cb) {
   }
 }
 
-void ShellVideoDecoderImpl::ReadFromDemuxerStreamTask(
-    const DemuxerStream::ReadCB& callback) {
-  // Reads must be done from the pipeline thread for encrypted content.
-  DCHECK(media_pipeline_message_loop_->BelongsToCurrentThread());
-  demuxer_stream_->Read(callback);
-}
-
 void ShellVideoDecoderImpl::ReadFromDemuxerStream() {
   DCHECK(decoder_thread_.message_loop_proxy()->BelongsToCurrentThread());
   DCHECK_NE(state_, kDecodeFinished);
 
-  // only one read to demuxer stream at a time, and only if we have a free
-  // buffer to decode into
-  // TODO(***REMOVED***) : Check if HasFreeTexture can be handled in the decoder?
-  if (!read_pending_ /* && HasFreeTexture() */) {
-    read_pending_ = true;
-    media_pipeline_message_loop_->PostTask(FROM_HERE, base::Bind(
-        &ShellVideoDecoderImpl::ReadFromDemuxerStreamTask, this,
-        BindToCurrentLoop(
-            base::Bind(&ShellVideoDecoderImpl::BufferReady, this))));
-  }
+  media_pipeline_message_loop_->PostTask(FROM_HERE, base::Bind(
+      &DemuxerStream::Read, demuxer_stream_,
+      BindToCurrentLoop(
+          base::Bind(&ShellVideoDecoderImpl::BufferReady, this))));
 }
 
 void ShellVideoDecoderImpl::BufferReady(
@@ -187,12 +168,10 @@ void ShellVideoDecoderImpl::BufferReady(
   // must either be Ok and have a buffer or not Ok and no buffer
   DCHECK_EQ(demuxer_status != DemuxerStream::kOk, !buffer) << demuxer_status;
 
-  // Clear the read_pending_ flag since our callback has been called
-  read_pending_ = false;
-
   if (state_ == kUninitialized) {
-    // pending reads sometimes don't complete until after we're done.
-    DLOG(WARNING) << "read returned but decoder is Uninitialized";
+    // Stop has been called before BufferReady is posted. read_cb_ should be
+    // called and cleared inside Stop().
+    DCHECK(read_cb_.is_null());
     return;
   }
 
@@ -201,39 +180,37 @@ void ShellVideoDecoderImpl::BufferReady(
     return;
   }
 
+  // if we deferred reset based on a pending read, process that reset now
+  // after returning an empty frame
+  if (!reset_cb_.is_null()) {
+    DoReset();
+    return;
+  }
+
   if (demuxer_status == DemuxerStream::kConfigChanged) {
-    DLOG(INFO) << "Configuration Changed: " <<
-        demuxer_stream_->video_decoder_config().AsHumanReadableString();
+    VideoDecoderConfig decoder_config;
+    decoder_config.CopyFrom(demuxer_stream_->video_decoder_config());
+    DLOG(INFO) << "Configuration Changed: "
+               << decoder_config.AsHumanReadableString();
     // One side effect of asking for the video configuration is that
     // the MediaSource demuxer stack uses that request to determine
     // that the video decoder has updated its configuration.
     // We therefore must ask for the updated video configuration
     // before requesting any data, or the MediaSource stack will
     // assert.
-    if (!raw_decoder_->UpdateConfig(demuxer_stream_->video_decoder_config())) {
+    if (!raw_decoder_->UpdateConfig(decoder_config)) {
       DLOG(ERROR) << "Error Reconfig H264 decoder";
       DecoderFatalError();
       return;
     }
 
-    decoder_config_.CopyFrom(demuxer_stream_->video_decoder_config());
-
-    UPDATE_MEDIA_STATISTICS(STAT_TYPE_VIDEO_CODEC, decoder_config_.codec());
+    UPDATE_MEDIA_STATISTICS(STAT_TYPE_VIDEO_CODEC, decoder_config.codec());
     UPDATE_MEDIA_STATISTICS(STAT_TYPE_VIDEO_WIDTH,
-                            decoder_config_.natural_size().width());
+                            decoder_config.natural_size().width());
     UPDATE_MEDIA_STATISTICS(STAT_TYPE_VIDEO_HEIGHT,
-                            decoder_config_.natural_size().height());
+                            decoder_config.natural_size().height());
 
-    // it's assumed ConfigChanged is not received during a reset
-    DCHECK(reset_cb_.is_null());
     ReadFromDemuxerStream();
-    return;
-  }
-
-  // if we deferred reset based on a pending read, process that reset now
-  // after returning an empty frame
-  if (!reset_cb_.is_null()) {
-    DoReset();
     return;
   }
 
@@ -246,15 +223,14 @@ void ShellVideoDecoderImpl::BufferReady(
     return;
   }
 
+  DCHECK(buffer);
   if (!buffer) {
-    DLOG(WARNING) << "read returned empty buffer.";
+    DecoderFatalError();
     return;
   }
 
   // at this point demuxerstream state should be OK
   DCHECK_EQ(demuxer_status, DemuxerStream::kOk);
-  // and buffer should be valid
-  DCHECK(buffer);
 
   // Decode this one
   DecodeBuffer(buffer);
@@ -268,16 +244,21 @@ void ShellVideoDecoderImpl::DecodeBuffer(
   DCHECK(decoder_thread_.message_loop_proxy()->BelongsToCurrentThread());
   DCHECK_NE(state_, kUninitialized);
   DCHECK_NE(state_, kDecodeFinished);
-  DCHECK(reset_cb_.is_null());
   DCHECK(buffer);
-  filter_graph_log_->LogEvent(kObjectIdVideoDecoder, kEventDecode);
+
+  // if we deferred reset based on a pending read, process that reset now
+  // after returning an empty frame
+  if (!reset_cb_.is_null()) {
+    DoReset();
+    return;
+  }
 
   // if we've encountered an EOS buffer then attempt no more reads from upstream
   // empty queue and prepare to transition to kDecodeFinished.
   if (buffer->IsEndOfStream()) {
     eof_buffer_ = buffer;
-    filter_graph_log_->LogEvent(kObjectIdVideoDecoder,
-                                kEventEndOfStreamReceived);
+    TRACE_EVENT0("lb_shell",
+                 "ShellVideoDecoderImpl::DecodeBuffer() EOS Received");
     // We pipeline reads, so it is possible that we will receive more than one
     // read callback with EOS after the first
     if (state_ == kNormal) {
@@ -287,8 +268,7 @@ void ShellVideoDecoderImpl::DecodeBuffer(
 
   scoped_refptr<VideoFrame> frame;
 
-  LBVideoDecoder::DecodeStatus status =
-      raw_decoder_->Decode(buffer, &frame);
+  LBVideoDecoder::DecodeStatus status = raw_decoder_->Decode(buffer, &frame);
 
   if (status == LBVideoDecoder::FRAME_DECODED) {
     DCHECK(frame);
@@ -304,8 +284,13 @@ void ShellVideoDecoderImpl::DecodeBuffer(
       return;
     }
 
-    read_pending_ = false;
     ReadFromDemuxerStream();
+    return;
+  }
+
+  if (status == LBVideoDecoder::RETRY_WITH_SAME_BUFFER) {
+    decoder_thread_.message_loop_proxy()->PostTask(FROM_HERE,
+        base::Bind(&ShellVideoDecoderImpl::DecodeBuffer, this, buffer));
     return;
   }
 
@@ -327,12 +312,11 @@ void ShellVideoDecoderImpl::Reset(const base::Closure& closure) {
     return;
   }
 
-  DCHECK(decoder_thread_.message_loop_proxy()->BelongsToCurrentThread());
-  filter_graph_log_->LogEvent(kObjectIdVideoDecoder, kEventReset);
+  TRACE_EVENT0("lb_shell", "ShellVideoDecoderImpl::Reset()");
   reset_cb_ = BindToLoop(media_pipeline_message_loop_, closure);
 
   // Defer the reset if a read is pending.
-  if (read_pending_)
+  if (!read_cb_.is_null())
     return;
 
   DoReset();
@@ -340,7 +324,7 @@ void ShellVideoDecoderImpl::Reset(const base::Closure& closure) {
 
 void ShellVideoDecoderImpl::DoReset() {
   // any pending read must have been serviced before calling me
-  DCHECK(!read_pending_);
+  DCHECK(read_cb_.is_null());
   DCHECK(!reset_cb_.is_null());
   DCHECK(decoder_thread_.message_loop_proxy()->BelongsToCurrentThread());
 
@@ -352,7 +336,6 @@ void ShellVideoDecoderImpl::DoReset() {
   if (!raw_decoder_->Flush()) {
     DLOG(ERROR) << "Error Flush Decoder";
     DecoderFatalError();
-    return;
   }
 
   base::ResetAndReturn(&reset_cb_).Run();
@@ -360,7 +343,7 @@ void ShellVideoDecoderImpl::DoReset() {
 
 void ShellVideoDecoderImpl::DecoderFatalError() {
   DCHECK(decoder_thread_.message_loop_proxy()->BelongsToCurrentThread());
-  filter_graph_log_->LogEvent(kObjectIdVideoDecoder, kEventFatalError);
+  TRACE_EVENT0("lb_shell", "ShellVideoDecoderImpl::DecoderFatalError()");
   // fatal error within the decoder
   DLOG(ERROR) << "fatal video decoder error.";
   // service any read callbacks with error
@@ -383,10 +366,8 @@ void ShellVideoDecoderImpl::Stop(const base::Closure& closure) {
     decoder_thread_.Stop();
     return;
   }
-  DCHECK(decoder_thread_.message_loop_proxy()->BelongsToCurrentThread());
 
-  delete raw_decoder_;
-  raw_decoder_ = NULL;
+  raw_decoder_.reset(NULL);
 
   // terminate playback
   state_ = kUninitialized;
@@ -398,4 +379,3 @@ void ShellVideoDecoderImpl::Stop(const base::Closure& closure) {
 }
 
 }  // namespace media
-
