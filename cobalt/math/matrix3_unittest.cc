43a881b14c6a09af2d62c2d2d041163c8165eb42
Cobalt Team <cobalt-team@google.com>
2014-09-22 10:51:49 -0700

    Math library cleanup
    - Added Set(Type) (ex. SetVector and SetMatrix) methods to all classes.
    - Improved the interface of Matrix.
    - Added documentation for CubicBezier.
    - Removed a redundant c'tor in BoxF and adjusted the code.
    
    Change-Id: I4845f7f41b3af974ba620c33f52a8a3109e8099f

--- cobalt/math/matrix3_unittest.cc
+++ cobalt/math/matrix3_unittest.cc
@@ -23,12 +23,23 @@ TEST(Matrix3fTest, Constructors) {
       Vector3dF(1.0f, 1.0f, 1.0f), Vector3dF(1.0f, 1.0f, 1.0f));
   Matrix3F product_zeros = Matrix3F::FromOuterProduct(
       Vector3dF(1.0f, 1.0f, 1.0f), Vector3dF(0.0f, 0.0f, 0.0f));
+
+  const float kDataValues[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+  Matrix3F from_array = Matrix3F::FromArray(kDataValues);
+  Matrix3F from_values =
+      Matrix3F::FromValues(kDataValues[0], kDataValues[1], kDataValues[2],
+                           kDataValues[3], kDataValues[4], kDataValues[5],
+                           kDataValues[6], kDataValues[7], kDataValues[8]);
+
   EXPECT_EQ(ones, product_ones);
   EXPECT_EQ(zeros, product_zeros);
 
   for (int i = 0; i < 3; ++i) {
-    for (int j = 0; j < 3; ++j)
-      EXPECT_EQ(i == j ? 1.0f : 0.0f, identity.get(i, j));
+    for (int j = 0; j < 3; ++j) {
+      EXPECT_EQ(i == j ? 1.0f : 0.0f, identity(i, j));
+      EXPECT_EQ(kDataValues[i * 3 + j], from_array(i, j));
+      EXPECT_EQ(from_array(i, j), from_values(i, j));
+    }
   }
 }
 
@@ -36,14 +47,14 @@ TEST(Matrix3fTest, DataAccess) {
   Matrix3F matrix = Matrix3F::Ones();
   Matrix3F identity = Matrix3F::Identity();
 
-  EXPECT_EQ(Vector3dF(0.0f, 1.0f, 0.0f), identity.get_column(1));
-  matrix.set(0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f);
-  EXPECT_EQ(Vector3dF(2.0f, 5.0f, 8.0f), matrix.get_column(2));
+  EXPECT_EQ(Vector3dF(0.0f, 1.0f, 0.0f), identity.column(1));
+  matrix.SetMatrix(0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f);
+  EXPECT_EQ(Vector3dF(2.0f, 5.0f, 8.0f), matrix.column(2));
   matrix.set_column(0, Vector3dF(0.1f, 0.2f, 0.3f));
-  EXPECT_EQ(Vector3dF(0.1f, 0.2f, 0.3f), matrix.get_column(0));
+  EXPECT_EQ(Vector3dF(0.1f, 0.2f, 0.3f), matrix.column(0));
 
-  EXPECT_EQ(0.1f, matrix.get(0, 0));
-  EXPECT_EQ(5.0f, matrix.get(1, 2));
+  EXPECT_EQ(0.1f, matrix(0, 0));
+  EXPECT_EQ(5.0f, matrix(1, 2));
 }
 
 TEST(Matrix3fTest, Determinant) {
@@ -53,15 +64,15 @@ TEST(Matrix3fTest, Determinant) {
 
   // Now for something non-trivial...
   Matrix3F matrix = Matrix3F::Zeros();
-  matrix.set(0, 5, 6, 8, 7, 0, 1, 9, 0);
+  matrix.SetMatrix(0, 5, 6, 8, 7, 0, 1, 9, 0);
   EXPECT_EQ(390.0f, matrix.Determinant());
-  matrix.set(2, 0, 3 * matrix.get(0, 0));
-  matrix.set(2, 1, 3 * matrix.get(0, 1));
-  matrix.set(2, 2, 3 * matrix.get(0, 2));
+  matrix(2, 0) = 3 * matrix(0, 0);
+  matrix(2, 1) = 3 * matrix(0, 1);
+  matrix(2, 2) = 3 * matrix(0, 2);
   EXPECT_EQ(0, matrix.Determinant());
 
-  matrix.set(0.57f, 0.205f, 0.942f, 0.314f, 0.845f, 0.826f, 0.131f, 0.025f,
-             0.962f);
+  matrix.SetMatrix(0.57f, 0.205f, 0.942f, 0.314f, 0.845f, 0.826f, 0.131f,
+                   0.025f, 0.962f);
   EXPECT_NEAR(0.3149f, matrix.Determinant(), 0.0001f);
 }
 
@@ -71,17 +82,17 @@ TEST(Matrix3fTest, Inverse) {
   EXPECT_EQ(identity, inv_identity);
 
   Matrix3F singular = Matrix3F::Zeros();
-  singular.set(1.0f, 3.0f, 4.0f, 2.0f, 11.0f, 5.0f, 0.5f, 1.5f, 2.0f);
+  singular.SetMatrix(1.0f, 3.0f, 4.0f, 2.0f, 11.0f, 5.0f, 0.5f, 1.5f, 2.0f);
   EXPECT_EQ(0, singular.Determinant());
   EXPECT_EQ(Matrix3F::Zeros(), singular.Inverse());
 
   Matrix3F regular = Matrix3F::Zeros();
-  regular.set(0.57f, 0.205f, 0.942f, 0.314f, 0.845f, 0.826f, 0.131f, 0.025f,
-              0.962f);
+  regular.SetMatrix(0.57f, 0.205f, 0.942f, 0.314f, 0.845f, 0.826f, 0.131f,
+                    0.025f, 0.962f);
   Matrix3F inv_regular = regular.Inverse();
-  regular.set(2.51540616f, -0.55138018f, -1.98968043f, -0.61552266f,
-              1.34920184f, -0.55573636f, -0.32653861f, 0.04002158f,
-              1.32488726f);
+  regular.SetMatrix(2.51540616f, -0.55138018f, -1.98968043f, -0.61552266f,
+                    1.34920184f, -0.55573636f, -0.32653861f, 0.04002158f,
+                    1.32488726f);
   EXPECT_TRUE(regular.IsNear(inv_regular, 0.00001f));
 }
 
@@ -96,16 +107,16 @@ TEST(Matrix3fTest, EigenvectorsDiagonal) {
   // This block tests the another trivial case of eigenvalues of a diagonal
   // matrix. Here we expect values to be sorted.
   Matrix3F matrix = Matrix3F::Zeros();
-  matrix.set(0, 0, 1.0f);
-  matrix.set(1, 1, -2.5f);
-  matrix.set(2, 2, 3.14f);
+  matrix(0, 0) = 1.0f;
+  matrix(1, 1) = -2.5f;
+  matrix(2, 2) = 3.14f;
   Matrix3F eigenvectors = Matrix3F::Zeros();
   Vector3dF eigenvals = matrix.SolveEigenproblem(&eigenvectors);
   EXPECT_EQ(Vector3dF(3.14f, 1.0f, -2.5f), eigenvals);
 
-  EXPECT_EQ(Vector3dF(0.0f, 0.0f, 1.0f), eigenvectors.get_column(0));
-  EXPECT_EQ(Vector3dF(1.0f, 0.0f, 0.0f), eigenvectors.get_column(1));
-  EXPECT_EQ(Vector3dF(0.0f, 1.0f, 0.0f), eigenvectors.get_column(2));
+  EXPECT_EQ(Vector3dF(0.0f, 0.0f, 1.0f), eigenvectors.column(0));
+  EXPECT_EQ(Vector3dF(1.0f, 0.0f, 0.0f), eigenvectors.column(1));
+  EXPECT_EQ(Vector3dF(0.0f, 1.0f, 0.0f), eigenvectors.column(2));
 }
 
 TEST(Matrix3fTest, EigenvectorsNiceNotPositive) {
@@ -114,7 +125,7 @@ TEST(Matrix3fTest, EigenvectorsNiceNotPositive) {
   Matrix3F matrix = Matrix3F::Zeros();
   // This is not a positive-definite matrix but eigenvalues and the first
   // eigenvector should nonetheless be computed correctly.
-  matrix.set(3, 2, 4, 2, 0, 2, 4, 2, 3);
+  matrix.SetMatrix(3, 2, 4, 2, 0, 2, 4, 2, 3);
   Matrix3F eigenvectors = Matrix3F::Zeros();
   Vector3dF eigenvals = matrix.SolveEigenproblem(&eigenvectors);
 
@@ -130,7 +141,7 @@ TEST(Matrix3fTest, EigenvectorsNiceNotPositive) {
   EXPECT_NEAR(-1.0f, eigenvals.z(), 0.000001f);
 
   Vector3dF expected_principal(0.66666667f, 0.33333333f, 0.66666667f);
-  EXPECT_NEAR(0.0f, (expected_principal - eigenvectors.get_column(0)).Length(),
+  EXPECT_NEAR(0.0f, (expected_principal - eigenvectors.column(0)).Length(),
               0.000001f);
 }
 
@@ -140,14 +151,14 @@ TEST(Matrix3fTest, EigenvectorsPositiveDefinite) {
   Matrix3F matrix = Matrix3F::Zeros();
   Matrix3F eigenvectors = Matrix3F::Zeros();
   Matrix3F expected_eigenvectors = Matrix3F::Zeros();
-  matrix.set(1, -1, 2, -1, 4, 5, 2, 5, 0);
+  matrix.SetMatrix(1, -1, 2, -1, 4, 5, 2, 5, 0);
   Vector3dF eigenvals = matrix.SolveEigenproblem(&eigenvectors);
   Vector3dF expected_eigv(7.3996266f, 1.91197255f, -4.31159915f);
   expected_eigv -= eigenvals;
   EXPECT_NEAR(0, expected_eigv.LengthSquared(), 0.00001f);
-  expected_eigenvectors.set(0.04926317f, -0.92135662f, -0.38558414f,
-                            0.82134249f, 0.25703273f, -0.50924521f, 0.56830419f,
-                            -0.2916096f, 0.76941158f);
+  expected_eigenvectors.SetMatrix(0.04926317f, -0.92135662f, -0.38558414f,
+                                  0.82134249f, 0.25703273f, -0.50924521f,
+                                  0.56830419f, -0.2916096f, 0.76941158f);
   EXPECT_TRUE(expected_eigenvectors.IsNear(eigenvectors, 0.00001f));
 }
 
