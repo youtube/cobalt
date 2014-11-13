3cf2cf59b60010a35222edf25e3332123bd5afcf
Cobalt Team <cobalt-team@google.com>
2014-11-12 18:50:17 -0800

    Implemented basic CSSOM classes.
    Change-Id: Ic9fe7345b3244c9ed47663b22fbf172e9fa7458b

---
+++ cobalt/cssom/style_sheet_list.h
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CSSOM_STYLE_SHEET_LIST_H_
+#define CSSOM_STYLE_SHEET_LIST_H_
+
+#include <vector>
+
+#include "base/memory/ref_counted.h"
+
+namespace cobalt {
+namespace cssom {
+
+class CSSStyleSheet;
+
+// The StyleSheetList interface represents an ordered collection of CSS
+// style sheets.
+//   http://dev.w3.org/csswg/cssom/#the-stylesheetlist-interface
+//
+// While the specification theoretically allows style sheets of type other
+// than CSS, Cobalt is hard-coded to support CSS only.
+class StyleSheetList : public base::RefCounted<StyleSheetList> {
+ public:
+  static scoped_refptr<StyleSheetList> Create();
+
+  // Web API: StyleSheetList
+  //
+
+  // Returns the index-th CSS style sheet in the collection.
+  // Returns null if there is no index-th object in the collection.
+  scoped_refptr<CSSStyleSheet> Item(unsigned int index) const;
+
+  // Returns the number of CSS style sheets represented by the collection.
+  unsigned int length() const;
+
+  // Custom, not in any spec.
+  //
+  void Append(const scoped_refptr<CSSStyleSheet>& style_sheet);
+
+ private:
+  StyleSheetList();
+  ~StyleSheetList();
+
+  std::vector<scoped_refptr<CSSStyleSheet> > style_sheets_;
+
+  friend class base::RefCounted<StyleSheetList>;
+  DISALLOW_COPY_AND_ASSIGN(StyleSheetList);
+};
+
+}  // namespace cssom
+}  // namespace cobalt
+
+#endif  // CSSOM_STYLE_SHEET_LIST_H_
