051644fe2c5c08ba3ed74ff91d262b47e107b83a
Cobalt Team <cobalt-team@google.com>
2014-11-19 20:18:43 -0800

    Add matrix 2D transformation helper functions.
    It is common to create a transformation matrix for, say,
    composition render tree nodes by composing primitive
    transformation matrices for translations, scales and rotations.
    
    There are other types of transformations, but these are the
    most useful and others can be added if they are found to be
    useful.
    
    Note that since these are specific to the render tree, the
    rotation transformation matrix is defined assuming that moving
    up corresponds to decreasing y values.
    
    Change-Id: I34e9af33615b561776866618cd6f9c38f5de7334

---
+++ cobalt/math/transform_2d.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MATH_TRANSFORM_2D_H_
+#define MATH_TRANSFORM_2D_H_
+
+#include "cobalt/math/matrix3_f.h"
+
+namespace cobalt {
+namespace math {
+
+cobalt::math::Matrix3F TranslateMatrix(float x, float y);
+cobalt::math::Matrix3F ScaleMatrix(float x_scale, float y_scale);
+cobalt::math::Matrix3F ScaleMatrix(float scale);
+cobalt::math::Matrix3F RotateMatrix(float theta_in_radians);
+
+}  // namespace math
+}  // namespace cobalt
+
+#endif  // MATH_TRANSFORM_2D_H_
