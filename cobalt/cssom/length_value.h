2b9e3d71ef582746a3524f7b5bf24ee490680313
Cobalt Team <cobalt-team@google.com>
2014-12-02 13:02:22 -0800

    Implemented CSS types used by Cobalt Oxide.
    Change-Id: I8a9b8e3c513f63f45c04bdbe8ac5030aac05d1e5

---
+++ cobalt/cssom/length_value.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CSSOM_LENGTH_VALUE_H_
+#define CSSOM_LENGTH_VALUE_H_
+
+#include <iostream>
+
+#include "base/basictypes.h"
+#include "base/compiler_specific.h"
+#include "base/logging.h"
+#include "cobalt/cssom/property_value.h"
+
+namespace cobalt {
+namespace cssom {
+
+// TODO(***REMOVED***): Add more units.
+enum Unit { kPixelsUnit };
+
+// Represents distance or size.
+// Applies to properties such as left, width, font-size, etc.
+// See http://www.w3.org/TR/css3-values/#lengths for details.
+class LengthValue : public PropertyValue {
+ public:
+  LengthValue(float value, Unit unit) : value_(value), unit_(unit) {}
+  ~LengthValue() OVERRIDE {}
+
+  virtual void Accept(PropertyValueVisitor* visitor) OVERRIDE;
+
+  float value() const { return value_; }
+  Unit unit() const { return unit_; }
+
+ private:
+  const float value_;
+  const Unit unit_;
+
+  DISALLOW_COPY_AND_ASSIGN(LengthValue);
+};
+
+inline bool operator==(const LengthValue& lhs, const LengthValue& rhs) {
+  return lhs.value() == rhs.value() && lhs.unit() == rhs.unit();
+}
+
+// Used by tests.
+inline std::ostream& operator<<(std::ostream& stream,
+                                const LengthValue& length) {
+  stream << length.value();
+
+  switch (length.unit()) {
+    case kPixelsUnit:
+      stream << "px";
+      break;
+    default:
+      NOTREACHED();
+  }
+
+  return stream;
+}
+
+}  // namespace cssom
+}  // namespace cobalt
+
+#endif  // CSSOM_LENGTH_VALUE_H_
---
+++ cobalt/cssom/property_value_visitor.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CSSOM_PROPERTY_VALUE_VISITOR_H_
+#define CSSOM_PROPERTY_VALUE_VISITOR_H_
+
+namespace cobalt {
+namespace cssom {
+
+class RGBAColorValue;
+class InheritedValue;
+class InitialValue;
+class LengthValue;
+class StringValue;
+
+// Type-safe branching on a class hierarchy of CSS property values,
+// implemented after a classical GoF pattern (see
+// http://en.wikipedia.org/wiki/Visitor_pattern#Java_example).
+class PropertyValueVisitor {
+ public:
+  virtual void VisitColor(RGBAColorValue* color_value) = 0;
+  virtual void VisitInherited(InheritedValue* inherited_value) = 0;
+  virtual void VisitInitial(InitialValue* initial_value) = 0;
+  virtual void VisitLength(LengthValue* length_value) = 0;
+  virtual void VisitString(StringValue* string_value) = 0;
+
+ protected:
+  ~PropertyValueVisitor() {}
+};
+
+}  // namespace cssom
+}  // namespace cobalt
+
+#endif  // CSSOM_PROPERTY_VALUE_VISITOR_H_
