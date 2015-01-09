7d3e56e9944cc4bd334c4b2241d6c4f466f60aaa
Xiaoming Shi <xiaomings@google.com>
2015-01-09 13:26:11 -0800

    Make media compile and link.
    Add ui components required by media.
    Remove dependency to Skia from media.
    
    Change-Id: Ic249852826c75b5b3f12c526f10ab79900a0e957

---
+++ ui/gfx/rect_f.h
@@ -0,0 +1,14 @@
+#ifndef UI_GFX_RECT_F_H_
+#define UI_GFX_RECT_F_H_
+
+#include "cobalt/math/rect_f.h"
+
+namespace gfx {
+
+// Introduce cobalt::math::RectF into namespace gfx as gfx::RectF is used by
+// media.
+using cobalt::math::RectF;
+
+}  // namespace gfx
+
+#endif  // UI_GFX_RECT_F_H_
