2f03c863d1a9af6dd41bfb239c8541b0729c1d6d
Xiaoming Shi <xiaomings@google.com>
2015-01-09 13:26:11 -0800

    Make media compile and link.
    Add ui components required by media.
    Remove dependency to Skia from media.
    
    Change-Id: Ic249852826c75b5b3f12c526f10ab79900a0e957

---
+++ ui/gfx/size.h
@@ -0,0 +1,14 @@
+#ifndef UI_GFX_SIZE_H_
+#define UI_GFX_SIZE_H_
+
+#include "cobalt/math/size.h"
+
+namespace gfx {
+
+// Introduce cobalt::math::Size into namespace gfx as gfx::Size is used by
+// media.
+using cobalt::math::Size;
+
+}  // namespace gfx
+
+#endif  // UI_GFX_SIZE_H_
