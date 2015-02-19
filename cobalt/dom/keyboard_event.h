448d41cdd26159d069758a857a27954ab6befcec
Cobalt Team <cobalt-team@google.com>
2015-02-18 18:07:36 -0800

    Add keyboard event dom
    Add keyboard event dom, and ui event dom.
    
    Change-Id: I064ac0b5d1952f45d3b6588a2c2164d72690924a

---
+++ cobalt/dom/keyboard_event.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DOM_KEYBOARD_EVENT_H_
+#define DOM_KEYBOARD_EVENT_H_
+
+#include "base/memory/scoped_ptr.h"
+#include "cobalt/dom/ui_event_with_key_state.h"
+
+namespace cobalt {
+namespace dom {
+
+// The KeyboardEvent provides specific contextual information associated with
+// keyboard devices. Each keyboard event references a key using a value.
+// Keyboard events are commonly directed at the element that has the focus.
+//   http://www.w3.org/TR/DOM-Level-3-Events/#events-keyboardevents
+class KeyboardEvent : public UIEventWithKeyState {
+ public:
+  enum KeyLocationCode {
+    kLocationStandard = 0x00,
+    kLocationLeft = 0x01,
+    kLocationRight = 0x02,
+    kLocationNumpad = 0x03,
+  };
+
+  KeyboardEvent(KeyLocationCode location, Type type, Modifiers modifiers,
+                int key_code, int char_code, bool is_repeat,
+                const scoped_refptr<Window>& view);
+
+  bool repeat() const { return repeat_; }
+  // key code for keydown and keyup, character for keypress
+  int key_code() const;
+  int char_code() const;
+
+ private:
+  ~KeyboardEvent() OVERRIDE {}
+
+  KeyLocationCode location_;
+  int key_code_;
+  int char_code_;
+  bool repeat_;
+};
+
+}  // namespace dom
+}  // namespace cobalt
+
+#endif  // DOM_KEYBOARD_EVENT_H_
