c6f4cec525c442aa1ef1504143e7960714a6b41c
Cobalt Team <cobalt-team@google.com>
2015-03-05 13:36:19 -0800

    Support bindings for nullable types
    - Refactor primitive type conversion simplify jinja templates
    - Add unit tests for nullable boolean, numeric, string, and objects
    - Set Node.textContent property to be non-nullable temporarily
    
    Change-Id: I7180e417c1f71a7dda8b90792f613d81463e4750

--- cobalt/script/javascriptcore/conversion_helpers.h
+++ cobalt/script/javascriptcore/conversion_helpers.h
@@ -25,6 +25,7 @@
 #undef LOG  // Defined by WTF, redefined in chromium.
 
 #include "base/logging.h"
+#include "base/optional.h"
 #include "cobalt/base/enable_if.h"
 #include "third_party/WebKit/Source/JavaScriptCore/runtime/JSGlobalData.h"
 #include "third_party/WebKit/Source/JavaScriptCore/runtime/JSValue.h"
@@ -38,20 +39,35 @@ JSC::JSValue BooleanToJSValue(bool in_boolean);
 WTF::String ToWTFString(const std::string& utf8_string);
 JSC::JSValue StringToJSValue(JSC::JSGlobalData* global_data,
                              const std::string& utf8_string);
+inline JSC::JSValue StringToJSValue(
+    JSC::JSGlobalData* global_data,
+    const base::optional<std::string>& optional_utf8_string) {
+  if (!optional_utf8_string) {
+    return JSC::jsNull();
+  }
+  return StringToJSValue(global_data, optional_utf8_string.value());
+}
+
 JSC::JSValue JSObjectToJSValue(JSC::JSObject* js_object);
 
 bool JSValueToBoolean(JSC::ExecState* exec_state, JSC::JSValue value);
 void JSValueToString(JSC::ExecState* exec_state, JSC::JSValue value,
                      std::string* out_string);
+inline void JSValueToString(JSC::ExecState* exec_state, JSC::JSValue value,
+                            base::optional<std::string>* out_optional) {
+  if (value.isNull()) {
+    *out_optional = base::nullopt;
+  } else {
+    // Ensure the optional is engaged so we can parse the string value in place.
+    if (!(*out_optional)) {
+      *out_optional = std::string();
+    }
+    JSValueToString(exec_state, value, &(out_optional->value()));
+  }
+}
+
 JSC::JSObject* JSValueToJSObject(JSC::ExecState* exec_state,
                                  JSC::JSValue value);
-
-template <typename T>
-JSC::JSValue NumberToJSValue(T in_number) {
-  JSC::JSValue number_value = JSC::jsNumber(in_number);
-  DCHECK(number_value.isNumber());
-  return number_value;
-}
 
 template <typename T>
 typename base::enable_if<
@@ -82,6 +98,49 @@ JSValueToNumber(JSC::ExecState* exec_state, JSC::JSValue value) {
   DCHECK(isfinite(double_value))
       << "unrestricted doubles/floats are not yet supported.";
   return double_value;
+}
+
+template <typename T>
+JSC::JSValue PrimitiveToJSValue(T in_number) {
+  JSC::JSValue number_value = JSC::jsNumber(in_number);
+  DCHECK(number_value.isNumber());
+  return number_value;
+}
+
+template <>
+inline JSC::JSValue PrimitiveToJSValue(bool in_boolean) {
+  return BooleanToJSValue(in_boolean);
+}
+
+template <typename T>
+inline JSC::JSValue PrimitiveToJSValue(base::optional<T> in_optional) {
+  if (!in_optional) {
+    return JSC::jsNull();
+  }
+  return PrimitiveToJSValue<T>(in_optional.value());
+}
+
+template <typename T>
+void JSValueToPrimitive(JSC::ExecState* exec_state, JSC::JSValue value,
+                        T* out_primitive) {
+  *out_primitive = JSValueToNumber<T>(exec_state, value);
+}
+
+template <typename T>
+void JSValueToPrimitive(JSC::ExecState* exec_state, JSC::JSValue value,
+                        base::optional<T>* out_optional) {
+  if (value.isNull()) {
+    *out_optional = base::nullopt;
+  } else {
+    *out_optional = T();
+    JSValueToPrimitive(exec_state, value, &(out_optional->value()));
+  }
+}
+
+template <>
+inline void JSValueToPrimitive(JSC::ExecState* exec_state, JSC::JSValue value,
+                               bool* out_boolean) {
+  *out_boolean = JSValueToBoolean(exec_state, value);
 }
 
 }  // namespace javascriptcore
