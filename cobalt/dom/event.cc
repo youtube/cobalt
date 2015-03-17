1ba066b1098bdf458d95896509502772a6222e4c
Xiaoming Shi <xiaomings@google.com>
2015-03-16 19:45:20 -0700

    Implement event propagation.
    1. Fully implement Event for Cobalt.
    2. Implemented DOM event propagation inside Node::dispatchEvent.
    3. Add tests for Event, EventTarget and Node.
    4. Modify gtest_workarounds.h so the order of the arguments in the
       comparison no longer matters.
    5. Fix InputDeviceAdapter so it works with the changes.
    
    This is for b/19385829.
    
    Change-Id: I99e645a2ba692d16b0ad0e4187cf55a5498f63de

--- cobalt/dom/event.cc
+++ cobalt/dom/event.cc
@@ -16,18 +16,57 @@
 
 #include "cobalt/dom/event.h"
 
+#include "base/time.h"
 #include "cobalt/dom/event_target.h"
 
 namespace cobalt {
 namespace dom {
 
 Event::Event(const std::string& type)
-    : type_(type), default_prevented_(false) {}
+    : event_phase_(kNone), time_stamp_(base::Time::Now().ToJsTime()) {
+  InitEvent(type, false, false);
+}
+
+Event::Event(const std::string& type, Bubbles bubbles, Cancelable cancelable)
+    : event_phase_(kNone), time_stamp_(base::Time::Now().ToJsTime()) {
+  InitEvent(type, bubbles == kBubbles, cancelable == kCancelable);
+}
 
 const scoped_refptr<EventTarget>& Event::target() const { return target_; }
 
-void Event::SetTarget(const scoped_refptr<EventTarget>& target) {
+const scoped_refptr<EventTarget>& Event::current_target() const {
+  return current_target_;
+}
+
+void Event::InitEvent(const std::string& type, bool bubbles, bool cancelable) {
+  // Our event is for single use only.
+  DCHECK(!IsBeingDispatched());
+  DCHECK(!target());
+  DCHECK(!current_target());
+
+  static bool first_time = true;
+  DLOG_IF(WARNING, first_time) << "Event.initEvent() is deprecated.";
+  first_time = false;
+
+  if (IsBeingDispatched() || target() || current_target()) {
+    return;
+  }
+
+  type_ = type;
+  bubbles_ = bubbles;
+  cancelable_ = cancelable;
+
+  propagation_stopped_ = false;
+  immediate_propagation_stopped_ = false;
+  default_prevented_ = false;
+}
+
+void Event::set_target(const scoped_refptr<EventTarget>& target) {
   target_ = target;
+}
+
+void Event::set_current_target(const scoped_refptr<EventTarget>& target) {
+  current_target_ = target;
 }
 
 }  // namespace dom
