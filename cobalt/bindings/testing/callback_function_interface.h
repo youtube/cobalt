ea72a752379679c166865116dfbabdd9de20932a
Cobalt Team <cobalt-team@google.com>
2015-04-02 12:54:28 -0700

    Bindings for callback functions
    - Add bindings for callbacks that take parameters
    
    b/19969317
    
    Change-Id: I3441555f448d9abae1182885b74b44b039142e82

--- cobalt/bindings/testing/callback_function_interface.h
+++ cobalt/bindings/testing/callback_function_interface.h
@@ -18,6 +18,8 @@
 #define BINDINGS_TESTING_CALLBACK_FUNCTION_INTERFACE_H_
 
 #include "base/memory/ref_counted.h"
+#include "base/optional.h"
+#include "cobalt/bindings/testing/arbitrary_interface.h"
 #include "cobalt/script/wrappable.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
@@ -28,7 +30,21 @@ namespace testing {
 class CallbackFunctionInterface : public script::Wrappable {
  public:
   typedef base::Callback<void()> VoidFunction;
+  typedef base::Callback<void(int32_t)> FunctionWithOneParameter;
+  typedef base::Callback<void(double, const std::string&,
+                              const scoped_refptr<ArbitraryInterface>&)>
+      FunctionWithSeveralParameters;
+  typedef base::Callback<void(
+      base::optional<bool>, const base::optional<std::string>&,
+      const scoped_refptr<ArbitraryInterface>&)> FunctionWithNullableParameters;
+
   MOCK_METHOD1(TakesVoidFunction, void(const VoidFunction&));
+  MOCK_METHOD1(TakesFunctionWithOneParameter,
+               void(const FunctionWithOneParameter&));
+  MOCK_METHOD1(TakesFunctionWithSeveralParameters,
+               void(const FunctionWithSeveralParameters&));
+  MOCK_METHOD1(TakesFunctionWithNullableParameters,
+               void(const FunctionWithNullableParameters&));
 };
 
 }  // namespace testing
