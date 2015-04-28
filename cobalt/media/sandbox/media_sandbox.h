79a6874397ced66c605fe7f38c319c553d318d54
Xiaoming Shi <xiaomings@google.com>
2015-04-28 16:02:58 -0700

    Implement media sandbox.
    1. Created a standalone media sandbox, which works directly with
       the renderer to render videos.
    2. Expose current video frame from MediaModule.
    
    Change-Id: I3f8b0a216b524bcb106aa585fd88677c71256bb9

---
+++ cobalt/media/sandbox/media_sandbox.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MEDIA_SANDBOX_MEDIA_SANDBOX_H_
+#define MEDIA_SANDBOX_MEDIA_SANDBOX_H_
+
+#include "base/compiler_specific.h"
+#include "base/memory/ref_counted.h"
+#include "cobalt/media/media_module.h"
+#include "cobalt/render_tree/image.h"
+#include "cobalt/render_tree/resource_provider.h"
+#include "googleurl/src/gurl.h"
+#include "media/base/video_frame.h"
+#include "media/player/web_media_player.h"
+#include "media/player/web_media_player_impl.h"
+
+namespace cobalt {
+namespace media {
+namespace sandbox {
+
+class MediaSandbox : public ::media::WebMediaPlayerClient {
+ public:
+  typedef render_tree::Image Image;
+
+  explicit MediaSandbox(
+      cobalt::render_tree::ResourceProvider* resource_provider);
+
+  void LoadAndPlay(const GURL& url);
+  scoped_refptr<Image> GetCurrentFrame();
+
+ private:
+  typedef ::media::WebMediaPlayerImpl WebMediaPlayerImpl;
+
+  // WebMediaPlayerClient methods
+  void NetworkStateChanged() OVERRIDE {}
+  void ReadyStateChanged() OVERRIDE {}
+  void VolumeChanged(float volume) OVERRIDE {}
+  void MuteChanged(bool mute) OVERRIDE {}
+  void TimeChanged() OVERRIDE {}
+  void DurationChanged() OVERRIDE {}
+  void RateChanged() OVERRIDE {}
+  void SizeChanged() OVERRIDE {}
+  void PlaybackStateChanged() OVERRIDE {}
+  void Repaint() OVERRIDE {}
+  void SawUnsupportedTracks() OVERRIDE {}
+  float Volume() const OVERRIDE { return 1.f; }
+  void SourceOpened() OVERRIDE {}
+  std::string SourceURL() const OVERRIDE { return ""; }
+
+  scoped_ptr<MediaModule> media_module_;
+  scoped_ptr<WebMediaPlayerImpl> player_;
+};
+
+}  // namespace sandbox
+}  // namespace media
+}  // namespace cobalt
+
+#endif  // MEDIA_SANDBOX_MEDIA_SANDBOX_H_
