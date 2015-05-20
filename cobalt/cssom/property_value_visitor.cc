7878f062cc2c2fbbb8af33c3e802897cc95a650c
Cobalt Team <cobalt-team@google.com>
2015-05-19 19:07:30 -0700

    Implemented defaulting visitor for property value types.
    Change-Id: I84d5beead76b8cbb8d699b7210f8b4f6edf09b0d

--- cobalt/cssom/property_value_visitor.cc
+++ cobalt/cssom/property_value_visitor.cc
@@ -17,61 +17,73 @@
 #include "cobalt/cssom/property_value_visitor.h"
 
 #include "base/logging.h"
+#include "cobalt/cssom/absolute_url_value.h"
+#include "cobalt/cssom/const_string_list_value.h"
+#include "cobalt/cssom/font_weight_value.h"
+#include "cobalt/cssom/keyword_value.h"
+#include "cobalt/cssom/length_value.h"
+#include "cobalt/cssom/number_value.h"
+#include "cobalt/cssom/rgba_color_value.h"
+#include "cobalt/cssom/string_value.h"
+#include "cobalt/cssom/time_list_value.h"
+#include "cobalt/cssom/transform_function_list_value.h"
+#include "cobalt/cssom/url_value.h"
 
 namespace cobalt {
 namespace cssom {
 
-void NotReachedPropertyValueVisitor::VisitAbsoluteURL(
-    AbsoluteURLValue* /*url_value*/) {
-  NOTREACHED();
+void DefaultingPropertyValueVisitor::VisitAbsoluteURL(
+    AbsoluteURLValue* url_value) {
+  VisitDefault(url_value);
 }
 
-void NotReachedPropertyValueVisitor::VisitConstStringList(
-    ConstStringListValue* /*const_string_list_value*/) {
-  NOTREACHED();
+void DefaultingPropertyValueVisitor::VisitConstStringList(
+    ConstStringListValue* const_string_list_value) {
+  VisitDefault(const_string_list_value);
 }
 
-void NotReachedPropertyValueVisitor::VisitFontWeight(
-    FontWeightValue* /*font_weight_value*/) {
-  NOTREACHED();
+void DefaultingPropertyValueVisitor::VisitFontWeight(
+    FontWeightValue* font_weight_value) {
+  VisitDefault(font_weight_value);
 }
 
-void NotReachedPropertyValueVisitor::VisitKeyword(
-    KeywordValue* /*keyword_value*/) {
-  NOTREACHED();
+void DefaultingPropertyValueVisitor::VisitKeyword(KeywordValue* keyword_value) {
+  VisitDefault(keyword_value);
 }
 
-void NotReachedPropertyValueVisitor::VisitLength(
-    LengthValue* /*length_value*/) {
-  NOTREACHED();
+void DefaultingPropertyValueVisitor::VisitLength(LengthValue* length_value) {
+  VisitDefault(length_value);
 }
 
-void NotReachedPropertyValueVisitor::VisitNumber(
-    NumberValue* /*number_value*/) {
-  NOTREACHED();
+void DefaultingPropertyValueVisitor::VisitNumber(NumberValue* number_value) {
+  VisitDefault(number_value);
 }
 
-void NotReachedPropertyValueVisitor::VisitRGBAColor(
-    RGBAColorValue* /*color_value*/) {
-  NOTREACHED();
+void DefaultingPropertyValueVisitor::VisitRGBAColor(
+    RGBAColorValue* color_value) {
+  VisitDefault(color_value);
 }
 
-void NotReachedPropertyValueVisitor::VisitString(
-    StringValue* /*string_value*/) {
-  NOTREACHED();
+void DefaultingPropertyValueVisitor::VisitString(StringValue* string_value) {
+  VisitDefault(string_value);
 }
 
-void NotReachedPropertyValueVisitor::VisitTimeList(
-    TimeListValue* /*time_list_value*/) {
-  NOTREACHED();
+void DefaultingPropertyValueVisitor::VisitTimeList(
+    TimeListValue* time_list_value) {
+  VisitDefault(time_list_value);
 }
 
-void NotReachedPropertyValueVisitor::VisitTransformFunctionList(
-    TransformFunctionListValue* /*transform_function_list_value*/) {
-  NOTREACHED();
+void DefaultingPropertyValueVisitor::VisitTransformFunctionList(
+    TransformFunctionListValue* transform_function_list_value) {
+  VisitDefault(transform_function_list_value);
 }
 
-void NotReachedPropertyValueVisitor::VisitURL(URLValue* /*url_value*/) {
+void DefaultingPropertyValueVisitor::VisitURL(URLValue* url_value) {
+  VisitDefault(url_value);
+}
+
+void NotReachedPropertyValueVisitor::VisitDefault(
+    PropertyValue* /*property_value*/) {
   NOTREACHED();
 }
 
