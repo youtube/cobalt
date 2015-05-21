0dbfd6fb525320b97fc693509d42c178c8a576af
Xiaoming Shi <xiaomings@google.com>
2015-05-20 22:28:42 -0700

    Implement URL.
    Also created a stub implementation for MediaSource.
    
    Change-Id: I914dbc8ac58d4bc554b28e80bc1f514253e35d77

---
+++ cobalt/dom/url.cc
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cobalt/dom/url.h"
+
+#include "base/guid.h"
+#include "base/logging.h"
+#include "googleurl/src/gurl.h"
+
+namespace cobalt {
+namespace dom {
+
+namespace {
+
+const char kBlobUrlProtocol[] = "blob";
+
+}  // namespace
+
+std::string URL::CreateObjectURL(
+    const scoped_refptr<MediaSource>& media_source) {
+  std::string blob_url = kBlobUrlProtocol;
+  blob_url += ':' + base::GenerateGUID();
+  MediaSource::Registry::Register(blob_url, media_source);
+  return blob_url;
+}
+
+void URL::RevokeObjectURL(const std::string& url) {
+  DCHECK(GURL(url).SchemeIs(kBlobUrlProtocol)) << url << " is not a blob url";
+  MediaSource::Registry::Unregister(url);
+}
+
+}  // namespace dom
+}  // namespace cobalt
