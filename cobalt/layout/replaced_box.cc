9bbe02719ebd720b73b5f692688e04945cf2b776
Jelle Foks <jfoks@google.com>
2015-06-03 17:04:46 -0700

    Set the ReplacedBox size.
    This sets the width and height of the ReplacedBox from the computed
    style.
    
    This also adds the replaced box of the media element if there is no
    video frame provider, so that the layout tests will work on windows
    (which doesn't have a video frame provider at the moment).
    
    b/21471877
    
    Change-Id: I711a70bd77f3f22385d41e1cf92f815c4eff5af0

--- cobalt/layout/replaced_box.cc
+++ cobalt/layout/replaced_box.cc
@@ -18,6 +18,7 @@
 
 #include "base/bind.h"
 #include "base/logging.h"
+#include "cobalt/layout/used_style.h"
 #include "cobalt/math/matrix3_f.h"
 #include "cobalt/render_tree/image_node.h"
 
@@ -28,13 +29,22 @@ using render_tree::ImageNode;
 
 namespace {
 
+// Default intrinsic width as defined in:
+// http://www.w3.org/TR/CSS21/visudet.html#inline-replaced-width
+const float kDefaultIntrinsicWidth = 300.0;
+// Default intrinsic height as defined in:
+// http://www.w3.org/TR/CSS21/visudet.html#inline-replaced-height
+const float kDefaultIntrinsicHeight = 150.0;
+
 void AnimateCB(ReplacedBox::ReplaceImageCB replace_image_cb,
                ImageNode::Builder* image_node, base::Time /*time*/) {
   DCHECK(!replace_image_cb.is_null());
   DCHECK(image_node);
   image_node->source = replace_image_cb.Run();
-  // TODO(***REMOVED***): Properly set the size of the image node.
-  if (image_node->source) {
+  // TODO(***REMOVED***): Detect better when the intrinsic video size is used for the
+  //   node size, and trigger a re-layout from the media element when the size
+  //   changes.
+  if (image_node->source && 0 == image_node->destination_size.height()) {
     image_node->destination_size = image_node->source->GetSize();
   }
 }
@@ -53,10 +63,110 @@ ReplacedBox::ReplacedBox(
 
 Box::Level ReplacedBox::GetLevel() const { return kInlineLevel; }
 
-void ReplacedBox::UpdateUsedSize(const LayoutParams& /*layout_params*/) {
-  // TODO(***REMOVED***) : Properly set the size of the image box from its style.
-  set_used_width(1280);
-  set_used_height(720);
+namespace {
+
+// Calculate the replaced box width, as defined in:
+//   http://www.w3.org/TR/CSS21/visudet.html#inline-replaced-width
+//   http://www.w3.org/TR/CSS21/visudet.html#block-replaced-width
+//   http://www.w3.org/TR/CSS21/visudet.html#inlineblock-replaced-width
+// TODO(***REMOVED***): Implement correct behaviour for absolutely positioned elements:
+//   http://www.w3.org/TR/CSS21/visudet.html#abs-replaced-width
+class ReplacedBoxUsedWidthProvider : public UsedWidthProvider {
+ public:
+  ReplacedBoxUsedWidthProvider(float containing_block_height,
+                               float intrinsic_width);
+
+  void VisitAuto() OVERRIDE;
+
+  // This is used to determine if the width must be calculated from the height
+  // and ratio.
+  bool width_is_auto() const { return width_is_auto_; }
+
+ private:
+  float intrinsic_width_;
+  bool width_is_auto_;
+};
+
+ReplacedBoxUsedWidthProvider::ReplacedBoxUsedWidthProvider(
+    float containing_block_width, float intrinsic_width)
+    : UsedWidthProvider(containing_block_width),
+      intrinsic_width_(intrinsic_width),
+      width_is_auto_(false) {}
+
+void ReplacedBoxUsedWidthProvider::VisitAuto() {
+  width_is_auto_ = true;
+  // Note: This value will be ignored if the height is not auto.
+  set_used_width(intrinsic_width_);
+}
+
+// Calculate the replaced box height, as defined in:
+//   http://www.w3.org/TR/CSS21/visudet.html#inline-replaced-height
+// TODO(***REMOVED***): Implement correct behaviour for absolutely positioned elements:
+//   http://www.w3.org/TR/CSS21/visudet.html#abs-replaced-height
+class ReplacedBoxUsedHeightProvider : public UsedHeightProvider {
+ public:
+  ReplacedBoxUsedHeightProvider(float containing_block_height,
+                                float intrinsic_height);
+
+  void VisitAuto() OVERRIDE;
+
+  // This is used to determine if the height must be calculated from the width
+  // and ratio.
+  bool height_is_auto() const { return height_is_auto_; }
+
+ private:
+  float intrinsic_height_;
+  bool height_is_auto_;
+};
+
+ReplacedBoxUsedHeightProvider::ReplacedBoxUsedHeightProvider(
+    float containing_block_height, float intrinsic_height)
+    : UsedHeightProvider(containing_block_height),
+      intrinsic_height_(intrinsic_height),
+      height_is_auto_(false) {}
+
+void ReplacedBoxUsedHeightProvider::VisitAuto() {
+  height_is_auto_ = true;
+  // Note: This value will be ignored if the width is not auto.
+  set_used_height(intrinsic_height_);
+}
+
+}  // namespace
+
+void ReplacedBox::UpdateUsedSize(const LayoutParams& layout_params) {
+  // TODO(***REMOVED***): See if we can determine and use the intrinsic element size.
+  float intrinsic_width = kDefaultIntrinsicWidth;
+  float intrinsic_height = kDefaultIntrinsicHeight;
+  float intrinsic_ratio = kDefaultIntrinsicWidth / kDefaultIntrinsicHeight;
+
+  ReplacedBoxUsedWidthProvider used_width_provider(
+      layout_params.containing_block_size.width(), intrinsic_width);
+  computed_style()->width()->Accept(&used_width_provider);
+
+  ReplacedBoxUsedHeightProvider used_height_provider(
+      layout_params.containing_block_size.height(), intrinsic_height);
+  computed_style()->height()->Accept(&used_height_provider);
+
+  float new_used_width = used_width_provider.used_width();
+  float new_used_height = used_height_provider.used_height();
+
+  if (used_width_provider.width_is_auto() &&
+      !used_height_provider.height_is_auto()) {
+    // If width is auto and height is not, then use the intrinsic ratio to
+    // calculate the width, according to:
+    // http://www.w3.org/TR/CSS21/visudet.html#inline-replaced-width
+    new_used_width = used_height_provider.used_height() * intrinsic_ratio;
+  } else if (used_height_provider.height_is_auto() &&
+             !used_width_provider.width_is_auto()) {
+    // If height is auto and width is not, then use the intrinsic ratio to
+    // calculate the height, according to:
+    // http://www.w3.org/TR/CSS21/visudet.html#inline-replaced-height
+    new_used_height = used_width_provider.used_width() / intrinsic_ratio;
+  }
+
+  // TODO(***REMOVED***): Implement margins, borders, and paddings.
+  set_used_width(new_used_width);
+  set_used_height(new_used_height);
 }
 
 scoped_ptr<Box> ReplacedBox::TrySplitAt(float /*available_width*/) {
@@ -69,7 +179,8 @@ void ReplacedBox::AddContentToRenderTree(
     render_tree::CompositionNode::Builder* composition_node_builder,
     render_tree::animations::NodeAnimationsMap::Builder*
         node_animations_map_builder) const {
-  scoped_refptr<ImageNode> image_node = new render_tree::ImageNode(NULL);
+  scoped_refptr<ImageNode> image_node =
+      new render_tree::ImageNode(NULL, used_size());
   node_animations_map_builder->Add(image_node,
                                    base::Bind(AnimateCB, replace_image_cb_));
   composition_node_builder->AddChild(image_node, math::Matrix3F::Identity());
