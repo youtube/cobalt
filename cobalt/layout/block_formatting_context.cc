7122177d15ec8b592e45140c90e976527f1ba425
Cobalt Team <cobalt-team@google.com>
2015-06-04 12:58:33 -0700

    Add layout tests and (very) partial support for position: absolute.
    - Propogate the 'position' style through the style cascade
      process.
    
    - Do not modify the formatting context's state when positioning
      absolute elements, since they are out-of-flow and should not
      affect the layout of their siblings.
    
    With this very basic support, video tiles are now visible in
    Performance Spike (though the red sidebar menu background is
    lost).
    
    Most of the added layout tests are currently disabled since
    only partial support is implemented and there is still more
    work to do.
    
    Change-Id: I7d5ae1e91f9877c37dc0331560d2234d3b0e61bf

--- cobalt/layout/block_formatting_context.cc
+++ cobalt/layout/block_formatting_context.cc
@@ -16,6 +16,7 @@
 
 #include "cobalt/layout/block_formatting_context.h"
 
+#include "cobalt/cssom/keyword_value.h"
 #include "cobalt/layout/box.h"
 
 namespace cobalt {
@@ -37,25 +38,35 @@ void BlockFormattingContext::UpdateUsedRect(Box* child_box) {
   //   http://www.w3.org/TR/CSS21/visuren.html#block-formatting
   child_box->set_used_left(next_child_box_used_position_.x());
   child_box->set_used_top(next_child_box_used_position_.y());
-  next_child_box_used_position_.Offset(0, child_box->used_height());
 
-  // The vertical distance between two sibling boxes is determined by
-  // the "margin" properties. Vertical margins between adjacent block-level
-  // boxes collapse.
-  //   http://www.w3.org/TR/CSS21/visuren.html#block-formatting
-  // TODO(***REMOVED***): Implement the above.
+  // If the position is absolute, then it should not affect the layout of
+  // its siblings and thus we should not update the block formatting context's
+  // state.
+  //   http://www.w3.org/TR/CSS21/visuren.html#absolute-positioning
+  // TODO(***REMOVED***): A position of "fixed" is also out-of-flow and should not
+  //               affect the block formatting context's state.
+  if (child_box->computed_style()->position() !=
+          cssom::KeywordValue::GetAbsolute()) {
+    next_child_box_used_position_.Offset(0, child_box->used_height());
 
-  // Shrink-to-fit width cannot be less than the width of the widest child.
-  //   http://www.w3.org/TR/CSS21/visudet.html#float-width
-  shrink_to_fit_width_ =
-      std::max(shrink_to_fit_width_, child_box->used_width());
+    // The vertical distance between two sibling boxes is determined by
+    // the "margin" properties. Vertical margins between adjacent block-level
+    // boxes collapse.
+    //   http://www.w3.org/TR/CSS21/visuren.html#block-formatting
+    // TODO(***REMOVED***): Implement the above.
 
-  // The baseline of an "inline-block" is the baseline of its last line box
-  // in the normal flow, unless it has no in-flow line boxes.
-  //   http://www.w3.org/TR/CSS21/visudet.html#line-height
-  if (child_box->AffectsBaselineInBlockFormattingContext()) {
-    set_height_above_baseline(child_box->used_top() +
-                              child_box->GetHeightAboveBaseline());
+    // Shrink-to-fit width cannot be less than the width of the widest child.
+    //   http://www.w3.org/TR/CSS21/visudet.html#float-width
+    shrink_to_fit_width_ =
+        std::max(shrink_to_fit_width_, child_box->used_width());
+
+    // The baseline of an "inline-block" is the baseline of its last line box
+    // in the normal flow, unless it has no in-flow line boxes.
+    //   http://www.w3.org/TR/CSS21/visudet.html#line-height
+    if (child_box->AffectsBaselineInBlockFormattingContext()) {
+      set_height_above_baseline(child_box->used_top() +
+                                child_box->GetHeightAboveBaseline());
+    }
   }
 }
 
