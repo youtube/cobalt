90f763e0fd9a06fe6df3d2c77ae992505dc67299
Cobalt Team <cobalt-team@google.com>
2015-07-06 18:48:45 -0700

    Implemented parsing of "padding" properties.
    This is almost a copy of a "margin" parsing CL.
    
    Change-Id: I4e707427b591a7c49a656fc021eb82db6ed12a4d

--- cobalt/css_parser/margin_or_padding_shorthand.h
+++ cobalt/css_parser/margin_or_padding_shorthand.h
@@ -17,6 +17,7 @@
 #ifndef CSS_PARSER_MARGIN_OR_PADDING_SHORTHAND_H_
 #define CSS_PARSER_MARGIN_OR_PADDING_SHORTHAND_H_
 
+#include "base/memory/scoped_ptr.h"
 #include "cobalt/cssom/property_value.h"
 
 namespace cobalt {
@@ -25,11 +26,11 @@ namespace css_parser {
 // A helper used in YYSTYPE to represent the value of shorthand properties such
 // as margin and padding.
 struct MarginOrPaddingShorthand {
-  MarginOrPaddingShorthand(const scoped_refptr<cssom::PropertyValue>& top,
-                           const scoped_refptr<cssom::PropertyValue>& right,
-                           const scoped_refptr<cssom::PropertyValue>& bottom,
-                           const scoped_refptr<cssom::PropertyValue>& left)
-      : top(top), right(right), bottom(bottom), left(left) {}
+  static scoped_ptr<MarginOrPaddingShorthand> TryCreate(
+      const scoped_refptr<cssom::PropertyValue>& top,
+      const scoped_refptr<cssom::PropertyValue>& right,
+      const scoped_refptr<cssom::PropertyValue>& bottom,
+      const scoped_refptr<cssom::PropertyValue>& left);
 
   scoped_refptr<cssom::PropertyValue> top;
   scoped_refptr<cssom::PropertyValue> right;
