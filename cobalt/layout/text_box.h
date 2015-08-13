3595d5e27f73f3486a9481bb5ff91c069a95bfa5
Cobalt Team <cobalt-team@google.com>
2015-08-12 17:12:54 -0700

    Implemented paragraph level text collection and analysis
    Previously, text boxes stored their text internally and were created
    at every soft wrap opportunity within a text node. This limited any
    analysis of the text to what was inside the box. This has been changed
    so that text is collected within paragraphs during box generation. Text
    boxes simply reference their containing text paragraph, along with the
    start and end positions of their text within the paragraph. Text boxes
    are no longer split at every soft wrap opportunity, but only split at
    soft wrap opportunities during layout as dictated by the width
    requirements of the line. Line breaking analysis is now provided by the
    paragraph.
    
    Additionally, when the end of a paragraph is reached, the paragraph is
    made immutable and the bidirectional algorithm is run on it, generating
    text runs for the paragraph, which will be used for sorting lines of
    boxes according to their bidi level.
    
    Change-Id: I0b86023d5ff2a8401f9c2f10f8c99d7865a7e7e9

--- cobalt/layout/text_box.h
+++ cobalt/layout/text_box.h
@@ -18,6 +18,7 @@
 #define LAYOUT_TEXT_BOX_H_
 
 #include "cobalt/layout/box.h"
+#include "cobalt/layout/paragraph.h"
 #include "cobalt/render_tree/font.h"
 
 namespace cobalt {
@@ -31,11 +32,14 @@ namespace layout {
 // replaced boxes.
 class TextBox : public Box {
  public:
+  static const float kInvalidTextWidth;
+
   TextBox(
       const scoped_refptr<const cssom::CSSStyleDeclarationData>& computed_style,
-      const cssom::TransitionSet* transitions, const std::string& text,
-      bool has_leading_white_space, bool has_trailing_white_space,
-      const UsedStyleProvider* used_style_provider);
+      const cssom::TransitionSet* transitions,
+      const UsedStyleProvider* used_style_provider,
+      const scoped_refptr<Paragraph>& paragraph, int32 paragraph_start,
+      int32 paragraph_end, float text_width);
 
   // From |Box|.
   Level GetLevel() const OVERRIDE;
@@ -67,34 +71,63 @@ class TextBox : public Box {
   void DumpChildrenWithIndent(std::ostream* stream, int indent) const OVERRIDE;
 
  private:
+  void UpdateFontData();
+  void UpdateTextData();
+  void UpdateParagraphStartPositionWhiteSpace();
+  void UpdateParagraphEndPositionWhiteSpace();
+  void UpdateTextWidthIfInvalid();
+
+  std::string GetText() const;
+
+  // Split the text box into two.
+  // |split_start_position| indicates the position within the paragraph where
+  // the split occurs and the second box begins
+  // |pre_split_width| indicates the width of the text that is remaining part
+  // of the first box
+  scoped_ptr<Box> SplitAtPosition(int32 split_start_position,
+                                  float pre_split_width);
+
   // Width of a space character in the used font, if the box has leading white
   // space.
   float GetLeadingWhiteSpaceWidth() const;
   // Width of a space character in the used font, if the box has trailing white
-  // space. If the box has no text, returns 0 (because the leading white space
-  // has already contributed to the width).
+  // space.
   float GetTrailingWhiteSpaceWidth() const;
 
-  // A processed text in which:
-  //   - collapsible white space has been collapsed and trimmed for both ends;
-  //   - segment breaks have been transformed;
-  //   - letter case has been transformed.
-  std::string text_;
-  // Whether the box has a collapsible leading white space. Note that actual
-  // white space characters has been removed from the beginning of |text_|.
-  // If the text is empty, |has_leading_white_space_| must be equal to
-  // |has_trailing_white_space_|.
-  bool has_leading_white_space_;
-  // Whether the box has a collapsible trailing white space. Note that actual
-  // white space characters has been removed from the end of |text_|.
-  // If the text is empty, |has_leading_white_space_| must be equal to
-  // |has_trailing_white_space_|.
-  bool has_trailing_white_space_;
+  bool HasText() const;
+  int32 GetTextStartPosition() const;
+  int32 GetTextEndPosition() const;
+
+  bool IsTextWidthInvalid() const;
+
+  // The paragraph that this text box is part of. It contains access to the
+  // underlying text, and handles the logic for determining bidi levels and
+  // where to split the text box during line breaking when the text box does
+  // not fully fit on a line.
+  const scoped_refptr<Paragraph> paragraph_;
+  // The position within the paragraph where the text contained in this box
+  // begins.
+  int32 paragraph_start_position_;
+  // The position within the paragraph where the text contained in this box
+  // ends.
+  int32 paragraph_end_position_;
+  // The total width of the text in the box, based on the font.
+  float text_width_;
+
+  // Whitespace tracking
+  bool is_paragraph_start_position_white_space_;
+  bool is_paragraph_end_position_white_space_;
+  bool is_trailing_white_space_collapsed_;
+  bool is_leading_white_space_collapsed_;
+
   // A font used for text width and line height calculations.
   const scoped_refptr<render_tree::Font> used_font_;
   // A vertical offset of the baseline relatively to the origin of the text box.
   float height_above_baseline_;
-  // A width of a space character in the used font, measured during layout.
+  // The height of the inline box encloses all glyphs and their half-leading on
+  // each side
+  float used_line_height_;
+  // A width of the space character in the used font, measured during layout.
   float space_width_;
 };
 
