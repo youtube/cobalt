8fe1cc64e25349e928e3254d946b1afa13e68d40
Cobalt Team <cobalt-team@google.com>
2015-08-20 18:43:26 -0700

    Support background size property with length value
    b/23352141
    
    Change-Id: Iac309112a9110c5570b2ffab2e79d14be374bc69

--- cobalt/cssom/computed_style_test.cc
+++ cobalt/cssom/computed_style_test.cc
@@ -23,6 +23,7 @@
 #include "cobalt/cssom/initial_style.h"
 #include "cobalt/cssom/keyword_value.h"
 #include "cobalt/cssom/length_value.h"
+#include "cobalt/cssom/percentage_value.h"
 #include "cobalt/cssom/property_list_value.h"
 #include "cobalt/cssom/property_names.h"
 #include "cobalt/cssom/url_value.h"
@@ -218,5 +219,56 @@ TEST(PromoteToComputedStyle, BackgroundImageNone) {
             computed_style->background_image().get());
 }
 
+TEST(PromoteToComputedStyle, BackgroundSizeEmToPixel) {
+  scoped_refptr<cssom::CSSStyleDeclarationData> computed_style(
+      new cssom::CSSStyleDeclarationData());
+  computed_style->AssignFrom(*cssom::GetInitialStyle());
+
+  scoped_ptr<cssom::PropertyListValue::Builder> background_size_builder(
+      new cssom::PropertyListValue::Builder());
+  background_size_builder->push_back(
+      new cssom::LengthValue(3, cssom::kFontSizesAkaEmUnit));
+  background_size_builder->push_back(
+      new cssom::LengthValue(40, cssom::kPixelsUnit));
+  scoped_refptr<cssom::PropertyListValue> background_size(
+      new cssom::PropertyListValue(background_size_builder.Pass()));
+  computed_style->set_background_size(background_size);
+
+  scoped_refptr<const cssom::CSSStyleDeclarationData> parent_computed_style =
+      cssom::GetInitialStyle();
+  PromoteToComputedStyle(computed_style, parent_computed_style, NULL);
+
+  scoped_refptr<cssom::PropertyListValue> background_size_list =
+      dynamic_cast<cssom::PropertyListValue*>(
+          computed_style->background_size().get());
+  ASSERT_TRUE(background_size_list);
+  ASSERT_EQ(2, background_size_list->value().size());
+
+  cssom::LengthValue* first_value =
+      base::polymorphic_downcast<cssom::LengthValue*>(
+          background_size_list->value()[0].get());
+  EXPECT_FLOAT_EQ(48.0f, first_value->value());
+  EXPECT_EQ(cssom::kPixelsUnit, first_value->unit());
+
+  cssom::LengthValue* second_value =
+      base::polymorphic_downcast<cssom::LengthValue*>(
+          background_size_list->value()[1].get());
+  EXPECT_FLOAT_EQ(40.0f, second_value->value());
+  EXPECT_EQ(cssom::kPixelsUnit, second_value->unit());
+}
+
+TEST(PromoteToComputedStyle, BackgroundSizeKeywordNotChanged) {
+  scoped_refptr<cssom::CSSStyleDeclarationData> computed_style(
+      new cssom::CSSStyleDeclarationData());
+  computed_style->AssignFrom(*cssom::GetInitialStyle());
+  computed_style->set_background_size(cssom::KeywordValue::GetContain());
+
+  scoped_refptr<const cssom::CSSStyleDeclarationData> parent_computed_style =
+      cssom::GetInitialStyle();
+  PromoteToComputedStyle(computed_style, parent_computed_style, NULL);
+
+  EXPECT_EQ(cssom::KeywordValue::GetContain(),
+            computed_style->background_size());
+}
 }  // namespace cssom
 }  // namespace cobalt
