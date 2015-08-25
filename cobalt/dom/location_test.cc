65be09d7d40ce31c6c5115c6fa7760ad99988b17
Cobalt Team <cobalt-team@google.com>
2015-08-25 15:01:01 -0700

    Add new attributes and methods on Location
    Currently only support changing of hash.
    
    Change-Id: I608dba5c3095c3a79c7481466377f045db5dae68

--- cobalt/dom/location_test.cc
+++ cobalt/dom/location_test.cc
@@ -1,38 +1,84 @@
 /*
- * Copyright 2015 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2015 Google Inc. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     https://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 #include "cobalt/dom/location.h"
 
-#include "base/memory/scoped_ptr.h"
+#include "base/memory/ref_counted.h"
+#include "googleurl/src/gurl.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace cobalt {
 namespace dom {
 
-TEST(LocationTest, LocationTest) {
-  scoped_refptr<Location> location;
+TEST(LocationTest, AssignShouldNotChangeURLPartsOtherThanHash) {
+  scoped_refptr<Location> location =
+      new Location(GURL("https://www.youtube.com/tv/foo;bar?q=a#ref"));
+  location->Assign("https://www.youtube.com/tv/foo;bar?q=a#new-ref");
+  EXPECT_EQ("https://www.youtube.com/tv/foo;bar?q=a#new-ref", location->href());
 
-  location =
-      new Location(GURL("http://user:pass@google.com:99/foo;bar?q=a#ref"));
-  EXPECT_EQ("http://user:pass@google.com:99/foo;bar?q=a#ref", location->href());
+  location->Assign("https://www.google.com/");
+  EXPECT_EQ("https://www.youtube.com/tv/foo;bar?q=a#new-ref", location->href());
+}
+
+TEST(LocationTest, Href) {
+  scoped_refptr<Location> location =
+      new Location(GURL("https://www.youtube.com/tv/foo;bar?q=a#ref"));
+  EXPECT_EQ("https://www.youtube.com/tv/foo;bar?q=a#ref", location->href());
+
+  location->set_href("https://www.youtube.com/tv/foo;bar?q=a#new-ref");
+  EXPECT_EQ("https://www.youtube.com/tv/foo;bar?q=a#new-ref", location->href());
+}
+
+TEST(LocationTest, Protocol) {
+  scoped_refptr<Location> location =
+      new Location(GURL("https://user:pass@google.com:99/foo;bar?q=a#ref"));
+  EXPECT_EQ("https:", location->protocol());
+}
+
+TEST(LocationTest, Host) {
+  scoped_refptr<Location> location =
+      new Location(GURL("https://user:pass@google.com:99/foo;bar?q=a#ref"));
+  EXPECT_EQ("google.com:99", location->host());
+}
+
+TEST(LocationTest, Hostname) {
+  scoped_refptr<Location> location =
+      new Location(GURL("https://user:pass@google.com:99/foo;bar?q=a#ref"));
+  EXPECT_EQ("google.com", location->hostname());
+}
+
+TEST(LocationTest, Port) {
+  scoped_refptr<Location> location =
+      new Location(GURL("https://user:pass@google.com:99/foo;bar?q=a#ref"));
+  EXPECT_EQ("99", location->port());
+}
+
+TEST(LocationTest, Hash) {
+  scoped_refptr<Location> location =
+      new Location(GURL("https://user:pass@google.com:99/foo;bar?q=a#ref"));
+  EXPECT_EQ("#ref", location->hash());
+
+  location->set_hash("new-ref");
+  EXPECT_EQ("#new-ref", location->hash());
+}
+
+TEST(LocationTest, Search) {
+  scoped_refptr<Location> location =
+      new Location(GURL("https://user:pass@google.com:99/foo;bar?q=a#ref"));
   EXPECT_EQ("?q=a", location->search());
-
-  location = new Location(GURL("http://user:pass@google.com:99/foo;bar#ref"));
-  EXPECT_EQ("http://user:pass@google.com:99/foo;bar#ref", location->href());
-  EXPECT_EQ("", location->search());
 }
 
 }  // namespace dom
