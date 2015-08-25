a285c407a7e9db69cd0efccea9563b3a6edddbd2
Cobalt Team <cobalt-team@google.com>
2015-08-25 15:01:01 -0700

    Add new attributes and methods on Location
    Currently only support changing of hash.
    
    Change-Id: I608dba5c3095c3a79c7481466377f045db5dae68

--- cobalt/dom/location.cc
+++ cobalt/dom/location.cc
@@ -19,12 +19,90 @@
 namespace cobalt {
 namespace dom {
 
-Location::Location(const GURL& url) : url_(url) {}
+Location::Location(const GURL& url) : url_(url) {
+  empty_component_.len = 0;
+  remove_ref_.SetRef("", empty_component_);
+}
+
+void Location::Assign(const std::string& url) { AssignInternal(GURL(url)); }
+
+void Location::Reload() {}
 
 std::string Location::href() const { return url_.spec(); }
 
+void Location::set_href(const std::string& href) { Assign(href); }
+
+std::string Location::protocol() const {
+  return url_.is_empty() ? ":" : url_.scheme() + ":";
+}
+
+void Location::set_protocol(const std::string& protocol) {
+  UNREFERENCED_PARAMETER(protocol);
+  NOTREACHED();
+}
+
+std::string Location::host() const {
+  if (url_.is_empty()) {
+    return "";
+  }
+  return url_.host() + (url_.has_port() ? ":" + url_.port() : "");
+}
+
+void Location::set_host(const std::string& host) {
+  UNREFERENCED_PARAMETER(host);
+  NOTREACHED();
+}
+
+std::string Location::hostname() const {
+  return url_.is_empty() ? "" : url_.host();
+}
+
+void Location::set_hostname(const std::string& hostname) {
+  UNREFERENCED_PARAMETER(hostname);
+  NOTREACHED();
+}
+
+std::string Location::port() const {
+  return url_.is_empty() ? "" : url_.port();
+}
+
+void Location::set_port(const std::string& port) {
+  UNREFERENCED_PARAMETER(port);
+  NOTREACHED();
+}
+
+std::string Location::hash() const {
+  return url_.ref().empty() ? "" : "#" + url_.ref();
+}
+
+void Location::set_hash(const std::string& hash) {
+  url_parse::Component comp;
+  comp.len = static_cast<int>(hash.length());
+  GURL::Replacements repl;
+  repl.SetRef(hash.c_str(), comp);
+  GURL new_url = url_.ReplaceComponents(repl);
+  AssignInternal(new_url);
+}
+
 std::string Location::search() const {
   return url_.query().empty() ? "" : "?" + url_.query();
+}
+
+void Location::set_search(const std::string& search) {
+  UNREFERENCED_PARAMETER(search);
+  NOTREACHED();
+}
+
+void Location::AssignInternal(const GURL& url) {
+  if (url.ReplaceComponents(remove_ref_) ==
+      url_.ReplaceComponents(remove_ref_)) {
+    url_ = url;
+    // TODO(***REMOVED***): Fire "hashchange" event at the window object.
+  } else {
+    LOG(WARNING) << "Only the hash of the URL in Location can be changed. The "
+                    "following change is ignored: from " << url_ << " to "
+                 << url << ".";
+  }
 }
 
 }  // namespace dom
