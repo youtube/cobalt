2d455e6c99cba4d40081b09311a64f9963668682
Cobalt Team <cobalt-team@google.com>
2015-09-11 17:52:39 -0700

    Starboard build configuration and atomicops
    This adds Starboard detection and configuration to build_config.h,
    provides a Starboard implementation of atomicops, and generalizes
    hash_tables.h to be driven from Starboard configuration in that
    case, but in the classic way for non-Starboard targets.
    
    Change-Id: Ib2e6d34898536774b308be2ee05ee579954670ce

--- base/hash_tables.h
+++ base/hash_tables.h
@@ -21,22 +21,45 @@
 #ifndef BASE_HASH_TABLES_H_
 #define BASE_HASH_TABLES_H_
 
+#include <string>
+
 #include "build/build_config.h"
 
 #include "base/string16.h"
 
-#if defined(COMPILER_MSVC)
-#include <hash_map>
-#include <hash_set>
-
+#if defined(OS_STARBOARD)
+#include "starboard/configuration.h"
+#define BASE_HASH_DEFINE_LONG_LONG_HASHES !SB_HAS(LONG_LONG_HASH)
+#define BASE_HASH_DEFINE_STRING_HASHES !SB_HAS(STRING_HASH)
+#define BASE_HASH_USE_HASH !SB_HAS(HASH_USING)
+#define BASE_HASH_MAP_INCLUDE SB_HASH_MAP_INCLUDE
+#define BASE_HASH_NAMESPACE SB_HASH_NAMESPACE
+#define BASE_HASH_SET_INCLUDE SB_HASH_SET_INCLUDE
+#elif defined(COMPILER_MSVC)
+#define BASE_HASH_DEFINE_LONG_LONG_HASHES 0
+#define BASE_HASH_DEFINE_STRING_HASHES 0
+#define BASE_HASH_USE_HASH 0
+#define BASE_HASH_MAP_INCLUDE <hash_map>
 #define BASE_HASH_NAMESPACE stdext
-
+#define BASE_HASH_SET_INCLUDE <hash_set>
 #elif defined(COMPILER_GCC)
 #if defined(OS_ANDROID) || (defined(__LB_SHELL__) && !defined(__LB_LINUX__))
+#define BASE_HASH_DEFINE_LONG_LONG_HASHES 0
+#define BASE_HASH_DEFINE_STRING_HASHES !defined(__LB_SHELL__)
+#define BASE_HASH_MAP_INCLUDE <hash_map>
 #define BASE_HASH_NAMESPACE std
+#define BASE_HASH_SET_INCLUDE <hash_set>
 #else
+#define BASE_HASH_DEFINE_LONG_LONG_HASHES 1
+#define BASE_HASH_DEFINE_STRING_HASHES 1
+#define BASE_HASH_MAP_INCLUDE <ext/hash_map>
 #define BASE_HASH_NAMESPACE __gnu_cxx
+#define BASE_HASH_SET_INCLUDE <ext/hash_set>
 #endif
+#define BASE_HASH_USE_HASH defined(__LB_LINUX__)
+#else  // COMPILER
+#error define BASE_HASH_NAMESPACE for your compiler
+#endif  // COMPILER
 
 // This is a hack to disable the gcc 4.4 warning about hash_map and hash_set
 // being deprecated.  We can get rid of this when we upgrade to VS2008 and we
@@ -46,24 +69,15 @@
 #undef __DEPRECATED
 #endif
 
-#if defined(OS_ANDROID) || (defined(__LB_SHELL__) && !defined(__LB_LINUX__))
-#include <hash_map>
-#include <hash_set>
-#else
-#include <ext/hash_map>
-#include <ext/hash_set>
-#endif
-
-#include <string>
+#include BASE_HASH_MAP_INCLUDE
+#include BASE_HASH_SET_INCLUDE
 
 #ifdef CHROME_OLD__DEPRECATED
 #define __DEPRECATED CHROME_OLD__DEPRECATED
 #undef CHROME_OLD__DEPRECATED
 #endif
 
-namespace BASE_HASH_NAMESPACE {
-
-#if !defined(OS_ANDROID) && !(defined(__LB_SHELL__) && !defined(__LB_LINUX__))
+#if BASE_HASH_DEFINE_LONG_LONG_HASHES
 // The GNU C++ library provides identity hash functions for many integral types,
 // but not for |long long|.  This hash function will truncate if |size_t| is
 // narrower than |long long|.  This is probably good enough for what we will
@@ -77,6 +91,7 @@ namespace BASE_HASH_NAMESPACE {
       } \
     }
 
+namespace BASE_HASH_NAMESPACE {
 DEFINE_TRIVIAL_HASH(long long);
 DEFINE_TRIVIAL_HASH(unsigned long long);
 
@@ -87,11 +102,13 @@ struct hash<T*> {
         reinterpret_cast<uintptr_t>(value));
   }
 };
+}  // namespace BASE_HASH_NAMESPACE
 
 #undef DEFINE_TRIVIAL_HASH
-#endif // !defined(OS_ANDROID) && !(defined(__LB_SHELL__) && !defined(__LB_LINUX__))
+#endif  // BASE_HASH_DEFINE_LONG_LONG_HASHES
 
-#if !(defined(__LB_SHELL__) && !defined(__LB_LINUX__))
+
+#if BASE_HASH_DEFINE_STRING_HASHES
 // Implement string hash functions so that strings of various flavors can
 // be used as keys in STL maps and sets.  The hash algorithm comes from the
 // GNU C++ library, in <tr1/functional>.  It is duplicated here because GCC
@@ -109,20 +126,17 @@ struct hash<T*> {
       } \
     }
 
+namespace BASE_HASH_NAMESPACE {
 DEFINE_STRING_HASH(std::string);
 DEFINE_STRING_HASH(string16);
-
-#undef DEFINE_STRING_HASH
-#endif
-
 }  // namespace BASE_HASH_NAMESPACE
 
-#else  // COMPILER
-#error define BASE_HASH_NAMESPACE for your compiler
-#endif  // COMPILER
+#undef DEFINE_STRING_HASH
+#endif  // BASE_HASH_DEFINE_STRING_HASHES
+
 
 namespace base {
-#if defined(__LB_LINUX__)
+#if BASE_HASH_USE_HASH
 using BASE_HASH_NAMESPACE::hash;
 #endif
 using BASE_HASH_NAMESPACE::hash_map;
@@ -130,5 +144,11 @@ using BASE_HASH_NAMESPACE::hash_multimap;
 using BASE_HASH_NAMESPACE::hash_multiset;
 using BASE_HASH_NAMESPACE::hash_set;
 }
+
+#undef BASE_HASH_DEFINE_LONG_LONG_HASHES
+#undef BASE_HASH_DEFINE_STRING_HASHES
+#undef BASE_HASH_MAP_INCLUDE
+#undef BASE_HASH_SET_INCLUDE
+#undef BASE_HASH_USE_HASH
 
 #endif  // BASE_HASH_TABLES_H_
