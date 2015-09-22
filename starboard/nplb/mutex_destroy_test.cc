022834420a912b3618bf1172e20f1712c22e72be
Cobalt Team <cobalt-team@google.com>
2015-09-22 15:35:13 -0700

    Starboard Mutex
    This adds a mutex API, a shared pthreads-based implementation, and an
    initial set of NPLB tests that only handle single-threaded use cases,
    as there is no thread API yet.
    
    It also adds an "internal_only.h" that internal headers can include to
    audit that they are not included externally.
    
    It also fixes a bad bug in the realloc tests that somehow only
    manifested when I used an absolute path to run the binary.
    
    Change-Id: Ia57ecead623ea6189128389383e2272c0b3e98a9

---
+++ starboard/nplb/mutex_destroy_test.cc
@@ -0,0 +1,48 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/configuration.h"
+#include "starboard/mutex.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+
+// Destroy is mostly tested in Create.
+
+TEST(SbMutexDestroyTest, DestroyInvalid) {
+  SbMutex mutex = kSbMutexInvalid;
+  EXPECT_FALSE(SbMutexIsValid(mutex));
+  bool result = SbMutexDestroy(mutex);
+  EXPECT_FALSE(result);
+}
+
+TEST(SbMutexDestroyTest, DestroyHeld) {
+  SbMutex mutex = SbMutexCreate();
+  EXPECT_TRUE(SbMutexIsValid(mutex));
+
+  SbMutexResult result = SbMutexAcquire(mutex);
+  EXPECT_EQ(result, kSbMutexAcquired);
+  EXPECT_TRUE(SbMutexIsSuccess(result));
+
+  bool destroy_result = SbMutexDestroy(mutex);
+  EXPECT_FALSE(destroy_result);
+
+  bool release_result = SbMutexRelease(mutex);
+  EXPECT_TRUE(release_result);
+
+  destroy_result = SbMutexDestroy(mutex);
+  EXPECT_TRUE(destroy_result);
+}
+
+}  // namespace
