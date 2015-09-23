46845d62bf7a6ee2331859e179784551cebc7f21
Cobalt Team <cobalt-team@google.com>
2015-09-22 19:34:13 -0700

    Starboard Time
    Time is one of the early dependencies of lots of things. This adds a
    Linux implementation, adapted from base/time_posix.cc, and some
    attempt at tests. I was able to pare down the interface slightly from
    my prototype, at least for now. And I split the timezone functions out
    into another module, which gives the functions nicer names.
    
    Note that some of our platforms don't support exploding time with
    timezone information properly, so we should always be using ICU to do
    these kinds of things. When I update base::Time for Starboard, I'm
    going to assert that no one uses Explode functions with is_local equal
    to true.
    
    That being the case, we may not want to support exploding on any
    platform, and just disable Base support for them, and remove them
    from Starboard. That will have to be determined further down the line,
    though.
    
    Change-Id: I8431e17f15420f3a730d4bee405e9a4a8b8a7f01

---
+++ starboard/time.h
@@ -0,0 +1,99 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Access to system time and timers.
+
+#ifndef STARBOARD_TIME_H_
+#define STARBOARD_TIME_H_
+
+#include "starboard/export.h"
+#include "starboard/types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// The number of microseconds since the epoch of January 1, 1601 UTC, or the
+// number of microseconds between two times. Always microseconds, ALWAYS UTC.
+typedef int64_t SbTime;
+
+// A number of microseconds from some point. The main property of this time is
+// that it increases monotonically. It should also be as high-resolution a timer
+// as we can get on a platform. So, it is good for measuring the time between
+// two calls without worrying about a system clock adjustment.  It's not good
+// for getting the wall clock time.
+typedef int64_t SbTimeMonotonic;
+
+// Represents an exploded SbTime that can be formatted nicely. This is adapted
+// directly from Chromium's base::Time::Exploded structure.
+typedef struct SbTimeExploded {
+  int year;          // Four digit year "2007"
+  int month;         // 1-based month (values 1 = January, etc.)
+  int day_of_week;   // 0-based day of week (0 = Sunday, etc.)
+  int day_of_month;  // 1-based day of month (1-31)
+  int hour;          // Hour within the current day (0-23)
+  int minute;        // Minute within the current hour (0-59)
+  int second;        // Second within the current minute (0-59 plus leap
+                     //   seconds which may take it up to 60).
+  int millisecond;   // Milliseconds within the current second (0-999)
+} SbTimeExploded;
+
+// One millisecond in SbTime units (microseconds).
+const SbTime kSbTimeMillisecond = 1000;
+
+// One second in SbTime units (microseconds).
+const SbTime kSbTimeSecond = kSbTimeMillisecond * 1000;
+
+// One minute in SbTime units (microseconds).
+const SbTime kSbTimeMinute = kSbTimeSecond * 60;
+
+// One hour in SbTime units (microseconds).
+const SbTime kSbTimeHour = kSbTimeMinute * 60;
+
+// One day in SbTime units (microseconds).
+const SbTime kSbTimeDay = kSbTimeHour * 24;
+
+// A term that can be added to an SbTime to convert it into the number of
+// microseconds since the POSIX epoch.
+const SbTime kSbTimeToPosixDelta = SB_INT64_C(-11644473600) * kSbTimeSecond;
+
+// Converts an SbTime into microseconds from the POSIX epoch.
+SB_C_FORCE_INLINE int64_t SbTimeToPosix(SbTime time) {
+  return time + kSbTimeToPosixDelta;
+}
+
+// Converts microseconds from the POSIX epoch into an SbTime.
+SB_C_FORCE_INLINE SbTime SbTimeFromPosix(int64_t time) {
+  return time - kSbTimeToPosixDelta;
+}
+
+// Gets the current system time as a SbTime.
+SB_EXPORT SbTime SbTimeGetNow();
+
+// Gets a monotonically increasing time representing right now.
+SB_EXPORT SbTimeMonotonic SbTimeGetMonotonicNow();
+
+// Converts |time| into an SbTimeExploded, placing the result in |out_exploded|.
+SB_EXPORT void SbTimeExplode(
+    SbTime time,
+    SbTimeExploded *out_exploded);
+
+// Converts |exploded| into a SbTime.
+SB_EXPORT SbTime SbTimeImplode(SbTimeExploded *exploded);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif  // STARBOARD_TIME_H_
