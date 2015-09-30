97d6767a7d5002099f285ade149826f5e87b0ff6
Xiaoming Shi <xiaomings@google.com>
2015-09-30 11:01:26 -0700

    Make Fetcher support ranges.
    b/21471585
    
    Change-Id: I061ee6d1ff03cb5a350578f2a062ffd8591642db

--- cobalt/loader/fetcher_factory.cc
+++ cobalt/loader/fetcher_factory.cc
@@ -18,8 +18,10 @@
 
 #include <string>
 
+#include "base/bind.h"
 #include "base/file_path.h"
 #include "base/path_service.h"
+#include "base/string_number_conversions.h"
 #include "cobalt/loader/file_fetcher.h"
 #include "cobalt/loader/net_fetcher.h"
 #include "cobalt/network/network_module.h"
@@ -35,6 +37,14 @@ bool FileURLToFilePath(const GURL& url, FilePath* file_path) {
   path.erase(0, 1);
   *file_path = FilePath(path);
   return !file_path->empty();
+}
+
+void SetRequestRange(int64 start_offset, int64 bytes_to_read,
+                     net::URLFetcher* url_fetcher) {
+  std::string range_request =
+      "Range: bytes=" + base::Int64ToString(start_offset) + "-" +
+      base::Int64ToString(start_offset + bytes_to_read - 1);
+  url_fetcher->AddExtraRequestHeader(range_request);
 }
 
 }  // namespace
@@ -62,6 +72,45 @@ scoped_ptr<Fetcher> FetcherFactory::CreateFetcher(const GURL& url,
   } else {
     DCHECK(network_module_) << "Network module required.";
     NetFetcher::Options options;
+    fetcher.reset(new NetFetcher(url, handler, network_module_, options));
+  }
+  return fetcher.Pass();
+}
+
+scoped_ptr<Fetcher> FetcherFactory::CreateFetcherWithRange(
+    const GURL& url, int64 start_offset, int64 bytes_to_read,
+    Fetcher::Handler* handler) {
+  DCHECK_GE(start_offset, 0);
+  DCHECK_GT(bytes_to_read, 0);
+
+  if (!url.is_valid()) {
+    DLOG(WARNING) << "Invalid url: " << url << ".";
+    return scoped_ptr<Fetcher>(NULL);
+  }
+
+  if (start_offset < 0 || bytes_to_read <= 0) {
+    DLOG_IF(WARNING, start_offset < 0) << "Invalid start offset : "
+                                       << start_offset;
+    DLOG_IF(WARNING, bytes_to_read <= 0) << "Invalid size to read : "
+                                         << bytes_to_read;
+    return scoped_ptr<Fetcher>(NULL);
+  }
+
+  scoped_ptr<Fetcher> fetcher;
+  if (url.SchemeIsFile()) {
+    FilePath file_path;
+    if (FileURLToFilePath(url, &file_path)) {
+      FileFetcher::Options options;
+      options.message_loop_proxy = file_thread_.message_loop_proxy();
+      options.start_offset = start_offset;
+      options.bytes_to_read = bytes_to_read;
+      fetcher.reset(new FileFetcher(file_path, handler, options));
+    }
+  } else {
+    DCHECK(network_module_) << "Network module required.";
+    NetFetcher::Options options;
+    options.setup_callback =
+        base::Bind(SetRequestRange, start_offset, bytes_to_read);
     fetcher.reset(new NetFetcher(url, handler, network_module_, options));
   }
   return fetcher.Pass();
