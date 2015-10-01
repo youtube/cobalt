89184c43763350bcaabe48f5aaa3951f2f008330
Cobalt Team <cobalt-team@google.com>
2015-10-01 14:35:43 -0700

    Splits debug console JS into multiple files.
    No new functionality, just refactoring the debug console JS code into
    multiple source files before I add more stuff and it gets unwieldy.
    
    Change-Id: If0351f859d1d01cfaa31534ecd0f67c19b13c85c

---
+++ cobalt/browser/debug_console/console_values.js
@@ -0,0 +1,148 @@
+// Console values constructor
+function ConsoleValues() {
+  this.allCVals = [];
+  this.activeCVals = [];
+  this.cvalTree = null;
+}
+
+// Gets the space-separated list of console value names from Cobalt and splits
+// into an array.
+ConsoleValues.prototype.getNames = function() {
+  var names = window.debugHub.getConsoleValueNames();
+  this.allCVals = names.split(' ');
+  // For now, include all CVals in the active list. When we have input support
+  // and command execution, the user will be able to select the active set.
+  this.activeCVals = this.allCVals;
+}
+
+// Console value tree node constructor
+function ConsoleValueNode() {
+  this.value = null;
+  this.children = null;
+}
+
+// Helper function for addToTree.
+// Finds a child node with a specified name, or creates it if necessary.
+ConsoleValues.prototype.findOrCreateNode = function (name, node) {
+  var child = null;
+  if (node.children.hasOwnProperty(name)) {
+    child = node.children[name];
+  } else {
+    child =  new ConsoleValueNode;
+    node.children[name] = child;
+  }
+  return child;
+}
+
+// Adds a single console value to the tree.
+// Console value names are split into components by dot separator. If a name
+// has a single component, it is added directly at this level of the tree with
+// its value, retrieved from Cobalt. If the name has multiple components, a node
+// is created for the first component, then this function is called recursively
+// on the remainder of the name, passing in the prefix (prior components) so the
+// full name can be constructed for evaluation.
+ConsoleValues.prototype.addToTree = function(cval, tree, prefix) {
+  if (tree.children == null) {
+    tree.children = new Object;
+  }
+  if (prefix == null) {
+    prefix = '';
+  }
+  var components = cval.split('.');
+  if (components.length == 1) {
+    var node = this.findOrCreateNode(cval, tree);
+    var fullName = prefix.length > 0 ? prefix + '.' + cval : cval;
+    node.value = window.debugHub.getConsoleValue(fullName);
+  } else {
+    var newPrefix = components[0];
+    var suffix = cval.substring(newPrefix.length + 1);
+    node = this.findOrCreateNode(newPrefix, tree);
+    if (prefix.length > 0) {
+      prefix += '.';
+    }
+    prefix += newPrefix;
+    this.addToTree(suffix, node, prefix);
+  }
+  return tree;
+}
+
+// Creates a tree from the console values.
+// Each console value name consists of one or more dot-separated components.
+// The last component of each console value name corresponds to a leaf node in
+// the tree; other components correspond to branch nodes.
+// A display string is then created from this tree, where leaf nodes with
+// a common ancestor are displayed more compactly.
+// This function assumes names are sorted alphabetically.
+ConsoleValues.prototype.buildTree = function(cvals) {
+  var root = new ConsoleValueNode;
+  for (var i = 0; i < cvals.length; i++) {
+    this.addToTree(cvals[i], root, '');
+  }
+  return root;
+}
+
+// Helper function for consoleValueTreeToString.
+// Creates the formatted string for a single console value node. In the simplest
+// case, where the node has no children, this will just be the name and value.
+// If the node has a single child, we call this function recursively to output
+// that child, with the full name of this node as a prefix. If the node has
+// multiple children, we enclose those children in braces and recursively call
+// treeToString on the sub-tree.
+ConsoleValues.prototype.nodeToString = function(name, node, prefix) {
+  var result  = '';
+  var value = node.value;
+  var children = node.children;
+  var childNames = children != null ? Object.keys(children) : null;
+  var numChildren = childNames != null ? childNames.length : 0;
+  if (prefix.length > 0) {
+    name = prefix + '.' + name;
+  }
+  if (value != null || numChildren > 1) {
+    result += name + ': ';
+  }
+  if (value != null) {
+    result += value;
+    if (numChildren > 0) {
+      result += ', ';
+    }
+  }
+  if (numChildren == 1) {
+    result += this.nodeToString(childNames[0], children[childNames[0]], name);
+  } else if (numChildren > 1) {
+    result += '{';
+    result += this.treeToString(children, null);
+    result += '}';
+  }
+  return result;
+}
+
+// Generates a formatted string from the parsed console value tree.
+// Uses the helper function nodeToString to process each node.
+// This function may in turn be called recursively by nodeToString
+// to generate the string for a sub-tree (when a node has multiple children).
+ConsoleValues.prototype.treeToString = function(cvalTree, prefix) {
+  if (prefix == null) {
+    prefix = '';
+  }
+  var result = '';
+  var names = Object.keys(cvalTree);
+  var numNames = names.length;
+  for (var i = 0; i < numNames; i++) {
+    result += this.nodeToString(names[i], cvalTree[names[i]], prefix)
+    if (i < numNames - 1) {
+      result += ', ';
+    }
+  }
+  return result;
+}
+
+// Updates the tree.
+ConsoleValues.prototype.update = function() {
+  this.getNames();
+  this.cvalTree = this.buildTree(this.activeCVals);
+}
+
+// Creates a formatted string from the current tree.
+ConsoleValues.prototype.toString = function() {
+  return this.treeToString(this.cvalTree.children, null);
+}
--- cobalt/browser/debug_console/debug_console.js
+++ cobalt/browser/debug_console/debug_console.js
@@ -1,131 +1,30 @@
-// First, extremely basic, version of a class to manage message buffering.
-function MessageBuffer(messageBox) {
-  this.maxBufferSize = 50;
-  this.messageBox = messageBox;
-}
-
-MessageBuffer.prototype.addMessage = function(severity, message) {
-  // Create the new text element with the message.
-  var elem = document.createElement('div');
-  var text = document.createTextNode(message);
-
-  if (severity == window.debugHub.LOG_INFO) {
-    elem.style.color = '#FFFFFF';
-  } else if (severity == window.debugHub.LOG_WARNING) {
-    elem.style.color = '#FFFF80';
-  } else if (severity == window.debugHub.LOG_ERROR ||
-             severity == window.debugHub.LOG_ERROR_REPORT ||
-             severity == window.debugHub.LOG_FATAL) {
-    elem.style.color = '#FF9080';
-  } else {
-    elem.style.color = '#FFFFFF';
-  }
-  elem.appendChild(text);
-
-  // Remove any older elements that exceed the buffer size and
-  // add the new element.
-  while (this.messageBox.childElementCount >= this.maxBufferSize) {
-    this.messageBox.removeChild(this.messageBox.firstChild);
-  }
-  this.messageBox.appendChild(elem);
-}
-
-MessageBuffer.prototype.show = function(doShow) {
-  var display = doShow ? 'block' : 'none';
-  var elem = document.getElementById('messageLog');
-  elem.style.display = display;
-}
-
 var inputText = '';
 var messageBuffer = null;
-var allCVals = [];
-var activeCVals = [];
+var consoleValues = null;
 var mode = window.debugHub.DEBUG_CONSOLE_HUD;
 
-function createMessageBuffer(visible) {
+function createMessageBuffer() {
   var messageBox = document.getElementById('messageBox');
   messageBuffer = new MessageBuffer(messageBox);
-  messageBuffer.show(visible);
 }
 
-// Gets the space-separated list of console value names from Cobalt and splits
-// into an array.
-function getConsoleValueNames() {
-  var names = window.debugHub.getConsoleValueNames();
-  allCVals = names.split(' ');
-  // For now, include all CVals in the active list. When we have input support
-  // and command execution, the user will be able to select the active set.
-  activeCVals = allCVals;
+function showBlockElem(elem, doShow) {
+  if (elem) {
+    var display = doShow ? 'block' : 'none';
+    elem.style.display = display;
+  }
 }
 
-// Console value tree node constructor
-function ConsoleValueNode() {
-  this.value = null;
-  this.children = null;
-}
-
-// Helper function for addConsoleValueToTree.
-// Finds a child node with a specified name, or creates it if necessary.
-function findOrCreateConsoleValueNode(name, node) {
-  var child = null;
-  if (node.children.hasOwnProperty(name)) {
-    child = node.children[name];
-  } else {
-    child =  new ConsoleValueNode;
-    node.children[name] = child;
-  }
-  return child;
-}
-
-// Adds a single console value to the tree.
-// Console value names are split into components by dot separator. If a name
-// has a single component, it is added directly at this level of the tree with
-// its value, retrieved from Cobalt. If the name has multiple components, a node
-// is created for the first component, then this function is called recursively
-// on the remainder of the name, passing in the prefix (prior components) so the
-// full name can be constructed for evaluation.
-function addConsoleValueToTree(cval, tree, prefix) {
-  if (tree.children == null) {
-    tree.children = new Object;
-  }
-  if (prefix == null) {
-    prefix = '';
-  }
-  var components = cval.split('.');
-  if (components.length == 1) {
-    var node = findOrCreateConsoleValueNode(cval, tree);
-    var fullName = prefix.length > 0 ? prefix + '.' + cval : cval;
-    node.value = window.debugHub.getConsoleValue(fullName);
-  } else {
-    var newPrefix = components[0];
-    var suffix = cval.substring(newPrefix.length + 1);
-    node = findOrCreateConsoleValueNode(newPrefix, tree);
-    if (prefix.length > 0) {
-      prefix += '.';
-    }
-    prefix += newPrefix;
-    addConsoleValueToTree(suffix, node, prefix);
-  }
-  return tree;
-}
-
-// Creates a tree from the console values.
-// Each console value name consists of one or more dot-separated components.
-// The last component of each console value name corresponds to a leaf node in
-// the tree; other components correspond to branch nodes.
-// A display string is then created from this tree, where leaf nodes with
-// a common ancestor are displayed more compactly.
-// This function assumes names are sorted alphabetically.
-function buildConsoleValueTree(cvals) {
-  var root = new ConsoleValueNode;
-  for (var i = 0; i < cvals.length; i++) {
-    addConsoleValueToTree(cvals[i], root, '');
-  }
-  return root;
+function showConsole(doShow) {
+  showBlockElem(document.getElementById('consoleFrame'), doShow);
 }
 
 function printToMessageLog(severity, message) {
   messageBuffer.addMessage(severity, message);
+}
+
+function showHud(doShow) {
+  showBlockElem(document.getElementById('hud'), doShow);
 }
 
 function printToHud(message) {
@@ -133,83 +32,21 @@ function printToHud(message) {
   elem.textContent = message;
 }
 
-// Helper function for consoleValueTreeToString.
-// Creates the formatted string for a single console value node. In the simplest
-// case, where the node has no children, this will just be the name and value.
-// If the node has a single child, we call this function recursively to output
-// that child, with the full name of this node as a prefix. If the node has
-// multiple children, we enclose those children in braces and recursively call
-// consoleValueTreeToString on the sub-tree.
-function consoleValueNodeToString(name, node, prefix) {
-  var result  = '';
-  var value = node.value;
-  var children = node.children;
-  var childNames = children != null ? Object.keys(children) : null;
-  var numChildren = childNames != null ? childNames.length : 0;
-  if (prefix.length > 0) {
-    name = prefix + '.' + name;
-  }
-  if (value != null || numChildren > 1) {
-    result += name + ': ';
-  }
-  if (value != null) {
-    result += value;
-    if (numChildren > 0) {
-      result += ', ';
-    }
-  }
-  if (numChildren == 1) {
-    result += consoleValueNodeToString(childNames[0], children[childNames[0]],
-                                       name);
-  } else if (numChildren > 1) {
-    result += '{';
-    result += consoleValueTreeToString(children, null);
-    result += '}';
-  }
-  return result;
-}
-
-// Generates a formatted string from the parsed console value tree.
-// Uses the helper function consoleValueNodeToString to process each node.
-// This function may in turn be called recursively by consoleValueNodeToString
-// to generate the string for a sub-tree (when a node has multiple children).
-function consoleValueTreeToString(cvalTree, prefix) {
-  if (prefix == null) {
-    prefix = '';
-  }
-  var result = '';
-  var names = Object.keys(cvalTree);
-  var numNames = names.length;
-  for (var i = 0; i < numNames; i++) {
-    result += consoleValueNodeToString(names[i], cvalTree[names[i]], prefix)
-    if (i < numNames - 1) {
-      result += ', ';
-    }
-  }
-  return result;
-}
-
 function updateHud(time) {
-  getConsoleValueNames();
-  var cvalTree = buildConsoleValueTree(activeCVals);
-  var cvalString = consoleValueTreeToString(cvalTree.children, null);
+  consoleValues.update();
+  var cvalString = consoleValues.toString();
   printToHud(cvalString);
 }
 
 function updateMode() {
   var mode = window.debugHub.getDebugConsoleMode();
-  messageBuffer.show(mode >= window.debugHub.DEBUG_CONSOLE_ON);
-  var input = document.getElementById('in');
-  var display = (mode >= window.debugHub.DEBUG_CONSOLE_ON) ? 'block' : 'none';
-  input.style.display = display;
-  var hud = document.getElementById('hud');
-  var display = (mode >= window.debugHub.DEBUG_CONSOLE_HUD) ? 'block' : 'none';
-  hud.style.display = display;
+  showConsole(mode >= window.debugHub.DEBUG_CONSOLE_ON);
+  showHud(mode >= window.debugHub.DEBUG_CONSOLE_HUD);
 }
 
 function animate(time) {
-  updateHud(time);
   updateMode();
+  updateHud(time);
   window.requestAnimationFrame(animate);
 }
 
@@ -223,13 +60,15 @@ function onKeypress(event) {
     event.preventDefault();
     event.stopPropagation();
     var i = document.querySelector('#in');
-    var k = event.keyCode;
+    var k = event.charCode;
     if (k == 8) {
       inputText = inputText.substring(0, inputText.length - 1);
+    } else if (k == 13) {
+      inputText = "";
     } else if (k >= 0x20 && k < 0x7e) {
       inputText += String.fromCharCode(k);
     }
-    i.textContent = '> ' + inputText;
+    i.textContent = '> ' + inputText + '_';
   }
 }
 
@@ -245,7 +84,9 @@ function addLogMessageCallback() {
 }
 
 function start() {
-  createMessageBuffer(false);
+  createMessageBuffer();
+  showConsole(false);
+  consoleValues = new ConsoleValues();
   addLogMessageCallback();
   document.addEventListener('keypress', onKeypress);
   document.addEventListener('keydown', onKeydown);
