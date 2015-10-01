c47b0890de81e73f322c5ff6fba800a9c54888a9
Jelle Foks <jfoks@google.com>
2015-09-30 18:49:42 -0700

    Block Level Replaced Box Content Size and Margins
    This implements calculation of the content size and margins to the
    block level replaced box, reusing shared logic with the inline level
    replaced box and the blox level non replaced box.
    
    * Refactor block level element horizontal margin calculation to Box so it
      can be shared by replaced and non-replaced elements.
    
    * Refactor UpdateContentSizeAndMargins() from InlineLevelReplacedBox
      to ReplacedBox, leaving calculation of the horizontal margins as
      UpdateHorizontalMargins() to implement the different behaviour
      of InlineLevelReplacedBox and BlockLevelReplacedBox.
    
    Change-Id: I5409defcb06b53deeaf2583000081d52d535e25a

--- cobalt/layout/replaced_box.cc
+++ cobalt/layout/replaced_box.cc
@@ -19,6 +19,7 @@
 #include "base/bind.h"
 #include "base/logging.h"
 #include "cobalt/layout/create_letterboxed_image.h"
+#include "cobalt/layout/used_style.h"
 #include "cobalt/math/transform_2d.h"
 #include "cobalt/render_tree/color_rgba.h"
 
@@ -33,6 +34,10 @@ namespace {
 // Used when intrinsic ratio cannot be determined,
 // as per http://www.w3.org/TR/CSS21/visudet.html#inline-replaced-width.
 const float kFallbackIntrinsicRatio = 2.0f;
+
+// Becomes a used value of "width" if it cannot be determined by any other
+// means, as per http://www.w3.org/TR/CSS21/visudet.html#inline-replaced-width.
+const float kFallbackWidth = 300.0f;
 
 }  // namespace
 
@@ -143,6 +148,89 @@ void ReplacedBox::RenderAndAnimateContent(
       composition_node,
       math::TranslateMatrix(border_left_width() + padding_left(),
                             border_top_width() + padding_top()));
+}
+
+void ReplacedBox::UpdateContentSizeAndMargins(
+    const LayoutParams& layout_params) {
+  base::optional<float> maybe_width = GetUsedWidthIfNotAuto(
+      computed_style(), layout_params.containing_block_size, NULL);
+  base::optional<float> maybe_height = GetUsedHeightIfNotAuto(
+      computed_style(), layout_params.containing_block_size);
+
+  if (!maybe_width) {
+    if (!maybe_height) {
+      if (maybe_intrinsic_width_) {
+        // If "height" and "width" both have computed values of "auto" and
+        // the element also has an intrinsic width, then that intrinsic width
+        // is the used value of "width".
+        //   http://www.w3.org/TR/CSS21/visudet.html#inline-replaced-width
+        set_width(*maybe_intrinsic_width_);
+      } else if (maybe_intrinsic_height_) {
+        // If "height" and "width" both have computed values of "auto" and
+        // the element has no intrinsic width, but does have an intrinsic height
+        // and intrinsic ratio then the used value of "width" is:
+        //     (intrinsic height) * (intrinsic ratio)
+        //   http://www.w3.org/TR/CSS21/visudet.html#inline-replaced-width
+        set_width(*maybe_intrinsic_height_ * intrinsic_ratio_);
+      } else {
+        // Otherwise, if "width" has a computed value of "auto", but none of
+        // the conditions above are met, then the used value of "width" becomes
+        // 300px.
+        //   http://www.w3.org/TR/CSS21/visudet.html#inline-replaced-width
+        set_width(kFallbackWidth);
+      }
+    } else {
+      // If "width" has a computed value of "auto", "height" has some other
+      // computed value, and the element does have an intrinsic ratio then
+      // the used value of "width" is:
+      //     (used height) * (intrinsic ratio)
+      //   http://www.w3.org/TR/CSS21/visudet.html#inline-replaced-width
+      set_width(*maybe_height * intrinsic_ratio_);
+    }
+  } else {
+    set_width(*maybe_width);
+  }
+
+  // The horizontal margin rules are difference for block level replaced boxes
+  // versus inline level replaced boxes.
+  //   http://www.w3.org/TR/CSS21/visudet.html#inline-replaced-width
+  //   http://www.w3.org/TR/CSS21/visudet.html#block-replaced-width
+  base::optional<float> maybe_margin_left = GetUsedMarginLeftIfNotAuto(
+      computed_style(), layout_params.containing_block_size);
+  base::optional<float> maybe_margin_right = GetUsedMarginRightIfNotAuto(
+      computed_style(), layout_params.containing_block_size);
+  float border_box_width = GetBorderBoxWidth();
+  UpdateHorizontalMargins(layout_params.containing_block_size.width(),
+                          border_box_width, maybe_margin_left,
+                          maybe_margin_right);
+
+  base::optional<float> maybe_margin_top = GetUsedMarginTopIfNotAuto(
+      computed_style(), layout_params.containing_block_size);
+  base::optional<float> maybe_margin_bottom = GetUsedMarginBottomIfNotAuto(
+      computed_style(), layout_params.containing_block_size);
+
+  // If "margin-top", or "margin-bottom" are "auto", their used value is 0.
+  //   http://www.w3.org/TR/CSS21/visudet.html#inline-replaced-height
+  set_margin_top(maybe_margin_top.value_or(0.0f));
+  set_margin_bottom(maybe_margin_bottom.value_or(0.0f));
+
+  if (!maybe_height) {
+    if (!maybe_width && maybe_intrinsic_height_) {
+      // If "height" and "width" both have computed values of "auto" and
+      // the element also has an intrinsic height, then that intrinsic height
+      // is the used value of "height".
+      //   http://www.w3.org/TR/CSS21/visudet.html#inline-replaced-height
+      set_height(*maybe_intrinsic_height_);
+    } else {
+      // Otherwise, if "height" has a computed value of "auto", and the element
+      // has an intrinsic ratio then the used value of "height" is:
+      //     (used width) / (intrinsic ratio)
+      //   http://www.w3.org/TR/CSS21/visudet.html#inline-replaced-height
+      set_height(width() / intrinsic_ratio_);
+    }
+  } else {
+    set_height(*maybe_height);
+  }
 }
 
 #ifdef COBALT_BOX_DUMP_ENABLED
