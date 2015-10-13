8e990d50ec7d75e884eeb6e399275c3b42497aa1
Xiaoming Shi <xiaomings@google.com>
2015-10-12 20:21:52 -0700

    Add OnURLFetchResponseStarted() to URLFetcher.
    URLFetcher used to require that all accessors should be called after
    OnURLFetchComplete() is called.  It does this because it may retry
    internally so unless a response is completed, it may not be the
    response the user of URLFetcher will end up see.
    However, it is useful if its user can retrieve the response code and
    headers before the whole response is completed.  This is especially
    useful when dealing with range requests sent to a server that doesn't
    support range request, in which case the server may just send back
    the whole resource and the user of URLFetcher has no other way to
    predict this unless it can get an early access of the response code
    and headers.
    Adding OnURLFetchResponseStarted() to URLFetcher allows the user to
    access the response code and headers before receiving any data.
    Note that as the headers can be overwritten in a later retry, the
    user should call accessors to retrieve the response and headers
    inside this callback.  We may need to disable retry to make it safer.
    
    b/21471585
    
    Change-Id: I57ea1ab527a2140b55c027cd51deb79325635789

--- net/url_request/test_url_fetcher_factory.h
+++ net/url_request/test_url_fetcher_factory.h
@@ -115,6 +115,9 @@ class TestURLFetcher : public URLFetcher {
       scoped_refptr<base::TaskRunner> file_task_runner) OVERRIDE;
   virtual void SaveResponseToTemporaryFile(
       scoped_refptr<base::TaskRunner> file_task_runner) OVERRIDE;
+#if defined(COBALT)
+  virtual void DiscardResponse() OVERRIDE;
+#endif
   virtual HttpResponseHeaders* GetResponseHeaders() const OVERRIDE;
   virtual HostPortPair GetSocketAddress() const OVERRIDE;
   virtual bool WasFetchedViaProxy() const OVERRIDE;
