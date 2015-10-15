651297621ab1805e31dd84e47d7a6f28559aa48a
Cobalt Team <cobalt-team@google.com>
2015-10-15 15:59:39 -0700

    Flush storage to disk more frequently
    Change the meaning of StorageManager::Flush(), to now indicate that
    the client wants to write the data to disk, but at some point in the
    future. Anytime a write is performed on the database, Flush() should
    be called. This starts a timer which will initiate the write
    after a few seconds.  This will effectively batch up consecutive
    writes.
    
    Add FlushNow(), which means don't wait, and should only be used if
    some piece of essential data has been written and the app is going
    to be shutting down.
    
    Change-Id: If984363c983c3d762fc35b12b38153fbb73133b9

--- cobalt/storage/storage_manager_test.cc
+++ cobalt/storage/storage_manager_test.cc
@@ -26,12 +26,24 @@
 #include "base/path_service.h"
 #include "cobalt/base/cobalt_paths.h"
 #include "cobalt/storage/savegame_fake.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+using ::testing::_;
+using ::testing::InvokeWithoutArgs;
 
 namespace cobalt {
 namespace storage {
 
 namespace {
+
+// Used to be able to intercept FlushInternal().
+class MockStorageManager : public StorageManager {
+ public:
+  explicit MockStorageManager(const Options& options)
+      : StorageManager(options) {}
+  MOCK_METHOD1(FlushInternal, void(const base::Closure& callback));
+};
 
 class CallbackWaiter {
  public:
@@ -107,7 +119,8 @@ class StorageManagerTest : public ::testing::Test {
 
   ~StorageManagerTest() { storage_manager_.reset(NULL); }
 
-  void InitStorageManager(bool delete_savegame = true) {
+  template <typename StorageManagerType>
+  void Init(bool delete_savegame = true) {
     // Destroy the current one first. We can't have two VFSs with the same name
     // concurrently.
     storage_manager_.reset(NULL);
@@ -115,7 +128,7 @@ class StorageManagerTest : public ::testing::Test {
 
     options.savegame_options.delete_on_destruction = delete_savegame;
     options.savegame_options.factory = &SavegameFake::Create;
-    storage_manager_.reset(new StorageManager(options));
+    storage_manager_.reset(new StorageManagerType(options));
   }
 
   MessageLoop message_loop_;
@@ -124,7 +137,7 @@ class StorageManagerTest : public ::testing::Test {
 
 TEST_F(StorageManagerTest, ObtainConnection) {
   // Verify that the SQL connection is non-null.
-  InitStorageManager();
+  Init<StorageManager>();
   SqlWaiter waiter;
   storage_manager_->GetSqlContext(
       base::Bind(&SqlWaiter::OnSqlConnection, base::Unretained(&waiter)));
@@ -132,31 +145,55 @@ TEST_F(StorageManagerTest, ObtainConnection) {
   EXPECT_EQ(true, waiter.TimedWait());
 }
 
-TEST_F(StorageManagerTest, Flush) {
+TEST_F(StorageManagerTest, FlushNow) {
   // Ensure the Flush callback is called.
-  InitStorageManager();
+  Init<StorageManager>();
   storage_manager_->GetSqlContext(base::Bind(&FlushCallback));
   message_loop_.RunUntilIdle();
   FlushWaiter waiter;
-  storage_manager_->Flush(
+  storage_manager_->FlushNow(
       base::Bind(&FlushWaiter::OnFlushDone, base::Unretained(&waiter)));
   EXPECT_EQ(true, waiter.TimedWait());
 }
 
 TEST_F(StorageManagerTest, QuerySchemaVersion) {
-  InitStorageManager(false /* delete_savegame */);
+  Init<StorageManager>(false /* delete_savegame */);
   storage_manager_->GetSqlContext(base::Bind(&QuerySchemaCallback));
   message_loop_.RunUntilIdle();
 
   // Force a write to disk and wait until it's done.
   FlushWaiter waiter;
-  storage_manager_->Flush(
+  storage_manager_->FlushNow(
       base::Bind(&FlushWaiter::OnFlushDone, base::Unretained(&waiter)));
   EXPECT_EQ(true, waiter.TimedWait());
 
-  InitStorageManager();
+  Init<StorageManager>();
   storage_manager_->GetSqlContext(base::Bind(&InspectSchemaVersionCallback));
   message_loop_.RunUntilIdle();
+}
+
+TEST_F(StorageManagerTest, Flush) {
+  // Test that the Flush callback is called once (and only once, despite us
+  // calling Flush() multiple times).
+  Init<MockStorageManager>();
+
+  storage_manager_->GetSqlContext(base::Bind(&FlushCallback));
+  message_loop_.RunUntilIdle();
+
+  FlushWaiter waiter;
+  MockStorageManager& storage_manager =
+      *dynamic_cast<MockStorageManager*>(storage_manager_.get());
+
+  // When FlushInternal() is called, have it also call
+  // FlushWaiter::OnFlushDone(). We will wait for this in TimedWait().
+  ON_CALL(storage_manager, FlushInternal(_))
+      .WillByDefault(InvokeWithoutArgs(&waiter, &FlushWaiter::OnFlushDone));
+  EXPECT_CALL(storage_manager, FlushInternal(_)).Times(1);
+
+  for (int i = 0; i < 10; ++i) {
+    storage_manager_->Flush();
+  }
+  EXPECT_EQ(true, waiter.TimedWait());
 }
 
 }  // namespace storage
