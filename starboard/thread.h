5d3dc261b4f83c329b4fd4a42bf88d9b43d79da9
Cobalt Team <cobalt-team@google.com>
2015-10-22 12:52:31 -0700

    Starboard thread priority tweak
    I am mainly adding a lot of comments, and the "real-time" priority.
    
    Change-Id: I1e7021898823d21353898cc5db93ae85aa2a086f

--- starboard/thread.h
+++ starboard/thread.h
@@ -26,16 +26,50 @@
 extern "C" {
 #endif
 
-// A spectrum of thread priorities. Platforms should map them appropriately to
-// their own priority system.
+// A spectrum of thread priorities. Platforms map them appropriately to their
+// own priority system. Note that scheduling is platform-specific, and what
+// these priorities mean, if they mean anything at all, is also
+// platform-specific.
+//
+// In particular, several of these priority values can map to the same priority
+// on a given platform. The only guarantee is that each lower priority should be
+// treated less-than-or-equal-to a higher priority.
 typedef enum SbThreadPriority {
-  kSbThreadPriorityLowest = -2,
-  kSbThreadPriorityLow = -1,
-  kSbThreadPriorityNormal = 0,
-  kSbThreadPriorityHigh = 1,
-  kSbThreadPriorityHighest = 2,
+  // The lowest thread priority available on the current platform.
+  kSbThreadPriorityLowest,
 
-  // Well-defined constant value to mean "no priority."
+  // A lower-than-normal thread priority, if available on the current platform.
+  kSbThreadPriorityLow,
+
+  // Really, what is normal? You should spend time pondering that question more
+  // than you consider less-important things, but less than you think about
+  // more-important things.
+  kSbThreadPriorityNormal,
+
+  // A higher-than-normal thread priority, if available on the current platform.
+  kSbThreadPriorityHigh,
+
+  // The highest thread priority available on the current platform that isn't
+  // considered "real-time" or "time-critical," if those terms have any meaning
+  // on the current platform.
+  kSbThreadPriorityHighest,
+
+  // If the platform provides any kind of real-time or time-critical scheduling,
+  // this priority will request that treatment. Real-time scheduling generally
+  // means that the thread will have more consistency in scheduling than
+  // non-real-time scheduled threads, often by being more deterministic in how
+  // threads run in relation to each other. But exactly how being real-time
+  // affects the thread scheduling is platform-specific.
+  //
+  // For platforms where that is not offered, or otherwise not meaningful, this
+  // will just be the highest priority available in the platform's scheme, which
+  // may be the same as kThreadPriority_Highest.
+  kSbThreadPriorityRealTime,
+
+  // Well-defined constant value to mean "no priority."  This means to use the
+  // default priority assignment method of that platform. This may mean to
+  // inherit the priority of the spawning thread, or it may mean a specific
+  // default priority, or it may mean something else, depending on the platform.
   kSbThreadNoPriority = kSbInvalidInt,
 } SbThreadPriority;
 
