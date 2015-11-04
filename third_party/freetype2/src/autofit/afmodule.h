acf0986a2fce20df5397e87f4ace55aefda06c20
Cobalt Team <cobalt-team@google.com>
2015-11-04 14:08:45 -0800

    Upgrade FreeType2 from 2.3.11 to 2.6
    For some reason, a change made in FreeType2 version 2.4.6 was breaking
    text scaling in Cobalt:
    
      2011-07-16  Steven Chu  <steven.f.chu@gmail.com>
    
      [truetype] Fix metrics on size request for scalable fonts.
    
      * src/truetype/ttdriver.c (tt_size_request): Fix copying metrics
      from TT_Size to FT_Size if scalable font.
    
    For now, I'm commenting out the offending line:
      ttsize->root.metrics = ttsize->metrics;
    
    This was the only change in the CL, and had a minimal visual impact
    according to the screenshots showing the before and after with the CL,
    http://lists.gnu.org/archive/html/freetype-devel/2011-07/msg00049.html,
    but we definitely want to track down why this is breaking things for us
    soon. Filed as b/25501544
    
    Change-Id: I133d83257d69633a91e762d63aeea8e637b1e222

--- third_party/freetype2/src/autofit/afmodule.h
+++ third_party/freetype2/src/autofit/afmodule.h
@@ -4,7 +4,7 @@
 /*                                                                         */
 /*    Auto-fitter module implementation (specification).                   */
 /*                                                                         */
-/*  Copyright 2003, 2004, 2005 by                                          */
+/*  Copyright 2003-2015 by                                                 */
 /*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
 /*                                                                         */
 /*  This file is part of the FreeType project, and may only be used,       */
@@ -26,7 +26,26 @@
 
 FT_BEGIN_HEADER
 
-FT_DECLARE_MODULE(autofit_module_class)
+
+  /*
+   *  This is the `extended' FT_Module structure that holds the
+   *  autofitter's global data.
+   */
+
+  typedef struct  AF_ModuleRec_
+  {
+    FT_ModuleRec  root;
+
+    FT_UInt       fallback_style;
+    FT_UInt       default_script;
+#ifdef AF_CONFIG_OPTION_USE_WARPER
+    FT_Bool       warping;
+#endif
+
+  } AF_ModuleRec, *AF_Module;
+
+
+FT_DECLARE_MODULE( autofit_module_class )
 
 
 FT_END_HEADER
