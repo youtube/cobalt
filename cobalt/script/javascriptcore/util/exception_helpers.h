7c94baeb51d69310ca5c287383e64bd92f02c99e
Cobalt Team <cobalt-team@google.com>
2015-11-09 16:44:35 -0800

    Fix crash on exception in an event handler
    - Refactor code for creating an exception logging string into
      one place.
    - Update how exception logging strings are created for event
      handlers to be consistent with callback functions and elsewhere.
    
    b/25378266
    
    Change-Id: Iae3fe5a216bd93d3bba1d451d932ac09525794dd

---
+++ cobalt/script/javascriptcore/util/exception_helpers.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SCRIPT_JAVASCRIPTCORE_UTIL_EXCEPTION_HELPERS_H_
+#define SCRIPT_JAVASCRIPTCORE_UTIL_EXCEPTION_HELPERS_H_
+
+#include <string>
+
+#include "third_party/WebKit/Source/JavaScriptCore/config.h"
+#include "third_party/WebKit/Source/JavaScriptCore/interpreter/CallFrame.h"
+#include "third_party/WebKit/Source/JavaScriptCore/runtime/JSValue.h"
+
+namespace cobalt {
+namespace script {
+namespace javascriptcore {
+namespace util {
+
+std::string GetExceptionString(JSC::ExecState* exec_state);
+
+std::string GetExceptionString(JSC::ExecState* exec_state,
+                               JSC::JSValue exception);
+
+}  // namespace util
+}  // namespace javascriptcore
+}  // namespace script
+}  // namespace cobalt
+
+#endif  // SCRIPT_JAVASCRIPTCORE_UTIL_EXCEPTION_HELPERS_H_
