df4b600503eeb248514f3c828a43881261a6205f
Cobalt Team <cobalt-team@google.com>
2015-11-12 18:16:49 -0800

    Update run_all_unittests for Starboard
    This makes unit test runners work with Starboard's new event system.
    
    Change-Id: I13bc3905131a9b4f750fca5bc6f929a68404196a

--- base/test/run_all_unittests.cc
+++ base/test/run_all_unittests.cc
@@ -4,8 +4,23 @@
 
 #include "base/test/main_hook.h"
 #include "base/test/test_suite.h"
+#include "build/build_config.h"
 
+#if !defined(OS_STARBOARD)
 int main(int argc, char** argv) {
   MainHook hook(main, argc, argv);
   return base::TestSuite(argc, argv).Run();
 }
+#else
+#include "starboard/event.h"
+#include "starboard/system.h"
+
+void SbEventHandle(const SbEvent* event) {
+  if (event->type == kSbEventTypeStart) {
+    SbEventStartData* data = static_cast<SbEventStartData*>(event->data);
+    MainHook hook(NULL, data->argument_count, data->argument_values);
+    SbSystemRequestStop(
+        base::TestSuite(data->argument_count, data->argument_values).Run());
+  }
+}
+#endif
