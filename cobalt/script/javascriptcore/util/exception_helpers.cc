5c068df0e2e6749bd456d6184aae87db4be7ee61
Cobalt Team <cobalt-team@google.com>
2015-11-16 17:17:07 -0800

    Better log output for JS exceptions
    Change-Id: Idc541387ee163a1a938eec0a7089229fd3d49e17

--- cobalt/script/javascriptcore/util/exception_helpers.cc
+++ cobalt/script/javascriptcore/util/exception_helpers.cc
@@ -16,7 +16,11 @@
 
 #include "cobalt/script/javascriptcore/util/exception_helpers.h"
 
+#include <sstream>
+
+#include "base/logging.h"
 #include "base/stringprintf.h"
+#include "third_party/WebKit/Source/JavaScriptCore/interpreter/Interpreter.h"
 #include "third_party/WebKit/Source/JavaScriptCore/runtime/JSDestructibleObject.h"
 #include "third_party/WebKit/Source/JavaScriptCore/runtime/JSScope.h"
 #include "third_party/WebKit/Source/WTF/wtf/text/WTFString.h"
@@ -32,13 +36,72 @@ std::string GetExceptionString(JSC::ExecState* exec_state) {
 
 std::string GetExceptionString(JSC::ExecState* exec_state,
                                JSC::JSValue exception) {
-  WTF::String exception_string = exception.toWTFString(exec_state);
+  WTF::String wtf_exception_string = exception.toWTFString(exec_state);
   JSC::JSObject* exception_object = exception.toObject(exec_state);
   int line_number =
       exception_object->get(exec_state, JSC::Identifier(exec_state, "line"))
           .toInt32(exec_state);
-  return base::StringPrintf("%s : line %d", exception_string.utf8().data(),
+  WTF::String source_url =
+      exception_object->get(exec_state, JSC::Identifier(exec_state, "sourceURL"))
+          .toWTFString(exec_state);
+  std::string exception_string =
+      base::StringPrintf("%s : %s:%d", wtf_exception_string.utf8().data(),
+                            source_url.utf8().data(),
                             line_number);
+  exception_string += "\n";
+  exception_string += GetStackTrace(exec_state);
+  return exception_string;
+}
+
+std::string GetStackTrace(JSC::ExecState* exec) {
+  std::stringstream backtrace_stream;
+
+  JSC::JSLockHolder lock(exec);
+
+  JSC::CallFrame* call_frame = exec->globalData().topCallFrame;
+  if (!call_frame) {
+    return std::string();
+  }
+  int call_frame_count = 0;
+
+  // TODO(***REMOVED***): Initialize |callee()| in bindings.
+  if (exec->callee()) {
+    if (asObject(exec->callee())->inherits(&JSC::InternalFunction::s_info)) {
+      WTF::String function_name =
+          asInternalFunction(exec->callee())->name(exec);
+      backtrace_stream << "#0 " << function_name.utf8().data() << "() ";
+      ++call_frame_count;
+    }
+  }
+
+  while (true) {
+    DCHECK(call_frame);
+
+    int line_number;
+    intptr_t source_id;
+    WTF::String url_string;
+    JSC::JSValue function;
+    exec->interpreter()->retrieveLastCaller(call_frame, line_number, source_id,
+                                            url_string, function);
+    WTF::String function_name =
+        function ? JSC::jsCast<JSC::JSFunction*>(function)->name(exec)
+                 : "(anonymous function)";
+
+    if (call_frame_count > 0) {
+      backtrace_stream << '\n';
+    }
+    backtrace_stream << function_name.utf8().data() << " @ "
+                     << url_string.utf8().data() << ':'
+                     << std::max(0, line_number);
+
+    if (!function) {
+      break;
+    }
+    call_frame = call_frame->callerFrame();
+    ++call_frame_count;
+  }
+
+  return backtrace_stream.str();
 }
 
 }  // namespace util
