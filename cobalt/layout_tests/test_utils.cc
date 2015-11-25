dd4e260710412bca10b317c56bf48b96128d7795
Cobalt Team <cobalt-team@google.com>
2015-11-24 18:06:18 -0800

    Add LogFilter to suppress unwanted output.
    Register with the LogMessageHandler to intercept log messages and
    prevent them from being output.
    
    Allow LogMessageHandler callbacks to return |true| to indicate the
    message should not be forwarded to the default logger.
    
    Add some suppressions to web_platform_tests. "help" and "author"
    tags are present in all tests, so we disregard them.
    Lots of the XHR tests are synchronous, so we can ignore the error.
    We are testing for expected failure.
    
    Change-Id: I66706367b090b8a5247e5b30a7a3a6d6c4e791ff

--- cobalt/layout_tests/test_utils.cc
+++ cobalt/layout_tests/test_utils.cc
@@ -16,8 +16,10 @@
 
 #include "cobalt/layout_tests/test_utils.h"
 
+#include "base/bind.h"
 #include "base/logging.h"
 #include "base/path_service.h"
+#include "base/string_util.h"
 #include "cobalt/base/cobalt_paths.h"
 #include "googleurl/src/gurl.h"
 
@@ -46,6 +48,29 @@ FilePath GetTestOutputRootDirectory() {
   FilePath dir_cobalt_test_out;
   PathService::Get(cobalt::paths::DIR_COBALT_TEST_OUT, &dir_cobalt_test_out);
   return dir_cobalt_test_out.Append(GetDirSourceRootRelativePath());
+}
+
+LogFilter::LogFilter() {
+  callback_id_ = base::LogMessageHandler::GetInstance()->AddCallback(
+      base::Bind(&LogFilter::OnLogMessage, base::Unretained(this)));
+}
+
+LogFilter::~LogFilter() {
+  base::LogMessageHandler::GetInstance()->RemoveCallback(callback_id_);
+}
+
+void LogFilter::Add(const std::string& s) { filtered_strings_.insert(s); }
+
+bool LogFilter::OnLogMessage(int /*severity*/, const char* /*file*/,
+                             int /*line*/, size_t message_start,
+                             const std::string& str) {
+  std::string trimmed_message = str.substr(message_start);
+  TrimWhitespaceASCII(trimmed_message, TRIM_ALL, &trimmed_message);
+  if (filtered_strings_.find(trimmed_message) != filtered_strings_.end()) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 }  // namespace layout_tests
