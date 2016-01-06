a275df0e1bfb1b16bbd18af0c1fd5b975d1d7b32
Xiaoming Shi <xiaomings@google.com>
2016-01-05 17:34:06 -0800

    Implement MediaModule pause/resume support.
    Now the app can call MediaModule::PauseAllPlayers() and
    ResumeAllPlayers() to pause and resume all players.
    
    b/26279417
    
    Change-Id: Idc5600581f65480ab2200465d696b642bc920c2e

--- cobalt/media/media_module.h
+++ cobalt/media/media_module.h
@@ -17,6 +17,8 @@
 #ifndef MEDIA_MEDIA_MODULE_H_
 #define MEDIA_MEDIA_MODULE_H_
 
+#include <map>
+
 #include "base/basictypes.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
@@ -25,26 +27,72 @@
 #include "cobalt/render_tree/resource_provider.h"
 #include "media/base/shell_media_platform.h"
 #include "media/base/shell_video_frame_provider.h"
+#include "media/player/web_media_player_delegate.h"
 
 namespace cobalt {
 namespace media {
 
 // TODO(***REMOVED***): Collapse MediaModule into ShellMediaPlatform.
-class MediaModule : public WebMediaPlayerFactory {
+class MediaModule : public WebMediaPlayerFactory,
+                    public ::media::WebMediaPlayerDelegate {
  public:
+  typedef ::media::WebMediaPlayer WebMediaPlayer;
   typedef render_tree::Image Image;
 
   virtual ~MediaModule() {}
 
-  // Provide a default implementation to create a dummy WebMediaPlayer instance.
-  // Inherited class can override it and create real WebMediaPlayer accordingly.
-  scoped_ptr<WebMediaPlayer> CreateWebMediaPlayer(
-      ::media::WebMediaPlayerClient* client) = 0;
+  // TODO(***REMOVED***): Move the following methods into class like MediaModuleBase
+  // to ensure that MediaModule is an interface.
+  // WebMediaPlayerDelegate methods
+  void RegisterPlayer(WebMediaPlayer* player) OVERRIDE {
+    DCHECK(players_.find(player) == players_.end());
+    players_.insert(std::make_pair(player, false));
+  }
+
+  void UnregisterPlayer(WebMediaPlayer* player) OVERRIDE {
+    DCHECK(players_.find(player) != players_.end());
+    players_.erase(players_.find(player));
+  }
+
+  // Functions to allow pause/resume of all active players.  Note that the
+  // caller should block the main thread to ensure that media related JS/DOM
+  // code doesn't get a chance to run between the call to PauseAllPlayers() and
+  // ResumeAllPlayers().  Otherwise the JS/DOM code might bring the players back
+  // to playback states.
+  void PauseAllPlayers() {
+    for (Players::iterator iter = players_.begin(); iter != players_.end();
+         ++iter) {
+      DCHECK(!iter->second);
+      if (!iter->second && !iter->first->IsPaused()) {
+        iter->first->Pause();
+        iter->second = true;
+      }
+    }
+  }
+
+  void ResumeAllPlayers() {
+    for (Players::iterator iter = players_.begin(); iter != players_.end();
+         ++iter) {
+      if (iter->second) {
+        DCHECK(iter->first->IsPaused());
+        if (iter->first->IsPaused()) {
+          iter->first->Play();
+        }
+        iter->second = false;
+      }
+    }
+  }
 
   // This function should be defined on individual platform to create the
   // platform specific MediaModule.
   static scoped_ptr<MediaModule> Create(
       render_tree::ResourceProvider* resource_provider);
+
+ private:
+  // When the value of a particular player is true, it means the player is
+  // paused by us.
+  typedef std::map<WebMediaPlayer*, bool> Players;
+  Players players_;
 };
 
 }  // namespace media
