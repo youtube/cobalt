e824fd826f4b343cacc4c76330854790216f6607
Cobalt Team <cobalt-team@google.com>
2016-01-11 09:23:15 -0800

    Add SB_NOTIMPLEMENTED() calls for all unimplemented GL functions.
    Change-Id: Id751f13c00d7b4f71f61b2967e02e7f4eb25b538

--- glimp/entry_points/gles_2_0_ext.cc
+++ glimp/entry_points/gles_2_0_ext.cc
@@ -17,6 +17,8 @@
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 
+#include "starboard/log.h"
+
 extern "C" {
 
 void GL_APIENTRY glBlitFramebufferANGLE(GLint srcX0,
@@ -28,48 +30,71 @@ void GL_APIENTRY glBlitFramebufferANGLE(GLint srcX0,
                                         GLint dstX1,
                                         GLint dstY1,
                                         GLbitfield mask,
-                                        GLenum filter) {}
+                                        GLenum filter) {
+  SB_NOTIMPLEMENTED();
+}
 
 void GL_APIENTRY glRenderbufferStorageMultisampleANGLE(GLenum target,
                                                        GLsizei samples,
                                                        GLenum internalformat,
                                                        GLsizei width,
-                                                       GLsizei height) {}
+                                                       GLsizei height) {
+  SB_NOTIMPLEMENTED();
+}
 
 void GL_APIENTRY glDiscardFramebufferEXT(GLenum target,
                                          GLsizei numAttachments,
-                                         const GLenum* attachments) {}
+                                         const GLenum* attachments) {
+  SB_NOTIMPLEMENTED();
+}
 
-void GL_APIENTRY glDeleteFencesNV(GLsizei n, const GLuint* fences) {}
+void GL_APIENTRY glDeleteFencesNV(GLsizei n, const GLuint* fences) {
+  SB_NOTIMPLEMENTED();
+}
 
-void GL_APIENTRY glGenFencesNV(GLsizei n, GLuint* fences) {}
+void GL_APIENTRY glGenFencesNV(GLsizei n, GLuint* fences) {
+  SB_NOTIMPLEMENTED();
+}
 
 GLboolean GL_APIENTRY glIsFenceNV(GLuint fence) {
+  SB_NOTIMPLEMENTED();
   return false;
 }
 
 GLboolean GL_APIENTRY glTestFenceNV(GLuint fence) {
+  SB_NOTIMPLEMENTED();
   return false;
 }
 
-void GL_APIENTRY glGetFenceivNV(GLuint fence, GLenum pname, GLint* params) {}
+void GL_APIENTRY glGetFenceivNV(GLuint fence, GLenum pname, GLint* params) {
+  SB_NOTIMPLEMENTED();
+}
 
-void GL_APIENTRY glFinishFenceNV(GLuint fence) {}
+void GL_APIENTRY glFinishFenceNV(GLuint fence) {
+  SB_NOTIMPLEMENTED();
+}
 
-void GL_APIENTRY glSetFenceNV(GLuint fence, GLenum condition) {}
+void GL_APIENTRY glSetFenceNV(GLuint fence, GLenum condition) {
+  SB_NOTIMPLEMENTED();
+}
 
 void GL_APIENTRY glGetTranslatedShaderSourceANGLE(GLuint shader,
                                                   GLsizei bufsize,
                                                   GLsizei* length,
-                                                  GLchar* source) {}
+                                                  GLchar* source) {
+  SB_NOTIMPLEMENTED();
+}
 
 void GL_APIENTRY glTexStorage2DEXT(GLenum target,
                                    GLsizei levels,
                                    GLenum internalformat,
                                    GLsizei width,
-                                   GLsizei height) {}
+                                   GLsizei height) {
+  SB_NOTIMPLEMENTED();
+}
 
 GLenum GL_APIENTRY glGetGraphicsResetStatusEXT(void) {
+  SB_NOTIMPLEMENTED();
   return 0;
 }
 
@@ -80,104 +105,159 @@ void GL_APIENTRY glReadnPixelsEXT(GLint x,
                                   GLenum format,
                                   GLenum type,
                                   GLsizei bufSize,
-                                  void* data) {}
+                                  void* data) {
+  SB_NOTIMPLEMENTED();
+}
 
 void GL_APIENTRY glGetnUniformfvEXT(GLuint program,
                                     GLint location,
                                     GLsizei bufSize,
-                                    float* params) {}
+                                    float* params) {
+  SB_NOTIMPLEMENTED();
+}
 
 void GL_APIENTRY glGetnUniformivEXT(GLuint program,
                                     GLint location,
                                     GLsizei bufSize,
-                                    GLint* params) {}
+                                    GLint* params) {
+  SB_NOTIMPLEMENTED();
+}
 
-void GL_APIENTRY glGenQueriesEXT(GLsizei n, GLuint* ids) {}
+void GL_APIENTRY glGenQueriesEXT(GLsizei n, GLuint* ids) {
+  SB_NOTIMPLEMENTED();
+}
 
-void GL_APIENTRY glDeleteQueriesEXT(GLsizei n, const GLuint* ids) {}
+void GL_APIENTRY glDeleteQueriesEXT(GLsizei n, const GLuint* ids) {
+  SB_NOTIMPLEMENTED();
+}
 
 GLboolean GL_APIENTRY glIsQueryEXT(GLuint id) {
+  SB_NOTIMPLEMENTED();
   return false;
 }
 
-void GL_APIENTRY glBeginQueryEXT(GLenum target, GLuint id) {}
+void GL_APIENTRY glBeginQueryEXT(GLenum target, GLuint id) {
+  SB_NOTIMPLEMENTED();
+}
 
-void GL_APIENTRY glEndQueryEXT(GLenum target) {}
+void GL_APIENTRY glEndQueryEXT(GLenum target) {
+  SB_NOTIMPLEMENTED();
+}
 
-void GL_APIENTRY glGetQueryivEXT(GLenum target, GLenum pname, GLint* params) {}
+void GL_APIENTRY glGetQueryivEXT(GLenum target, GLenum pname, GLint* params) {
+  SB_NOTIMPLEMENTED();
+}
 
 void GL_APIENTRY glGetQueryObjectuivEXT(GLuint id,
                                         GLenum pname,
-                                        GLuint* params) {}
+                                        GLuint* params) {
+  SB_NOTIMPLEMENTED();
+}
 
-void GL_APIENTRY glDrawBuffersEXT(GLsizei n, const GLenum* bufs) {}
+void GL_APIENTRY glDrawBuffersEXT(GLsizei n, const GLenum* bufs) {
+  SB_NOTIMPLEMENTED();
+}
 
 void GL_APIENTRY glDrawArraysInstancedANGLE(GLenum mode,
                                             GLint first,
                                             GLsizei count,
-                                            GLsizei primcount) {}
+                                            GLsizei primcount) {
+  SB_NOTIMPLEMENTED();
+}
 
 void GL_APIENTRY glDrawElementsInstancedANGLE(GLenum mode,
                                               GLsizei count,
                                               GLenum type,
                                               const void* indices,
-                                              GLsizei primcount) {}
+                                              GLsizei primcount) {
+  SB_NOTIMPLEMENTED();
+}
 
-void GL_APIENTRY glVertexAttribDivisorANGLE(GLuint index, GLuint divisor) {}
+void GL_APIENTRY glVertexAttribDivisorANGLE(GLuint index, GLuint divisor) {
+  SB_NOTIMPLEMENTED();
+}
 
 void GL_APIENTRY glGetProgramBinaryOES(GLuint program,
                                        GLsizei bufSize,
                                        GLsizei* length,
                                        GLenum* binaryFormat,
-                                       GLvoid* binary) {}
+                                       GLvoid* binary) {
+  SB_NOTIMPLEMENTED();
+}
 
 void GL_APIENTRY glProgramBinaryOES(GLuint program,
                                     GLenum binaryFormat,
                                     const GLvoid* binary,
-                                    GLint length) {}
+                                    GLint length) {
+  SB_NOTIMPLEMENTED();
+}
 
 void* GL_APIENTRY glMapBufferOES(GLenum target, GLenum access) {
+  SB_NOTIMPLEMENTED();
   return NULL;
 }
 
 GLboolean GL_APIENTRY glUnmapBufferOES(GLenum target) {
+  SB_NOTIMPLEMENTED();
   return false;
 }
 
 void GL_APIENTRY glGetBufferPointervOES(GLenum target,
                                         GLenum pname,
-                                        GLvoid** params) {}
+                                        GLvoid** params) {
+  SB_NOTIMPLEMENTED();
+}
 
 void* GL_APIENTRY glMapBufferRangeEXT(GLenum target,
                                       GLintptr offset,
                                       GLsizeiptr length,
                                       GLbitfield access) {
+  SB_NOTIMPLEMENTED();
   return NULL;
 }
 
 void GL_APIENTRY glFlushMappedBufferRangeEXT(GLenum target,
                                              GLintptr offset,
-                                             GLsizeiptr length) {}
+                                             GLsizeiptr length) {
+  SB_NOTIMPLEMENTED();
+}
 
-void GL_APIENTRY glInsertEventMarkerEXT(GLsizei length, const char* marker) {}
+void GL_APIENTRY glInsertEventMarkerEXT(GLsizei length, const char* marker) {
+  SB_NOTIMPLEMENTED();
+}
 
-void GL_APIENTRY glPushGroupMarkerEXT(GLsizei length, const char* marker) {}
+void GL_APIENTRY glPushGroupMarkerEXT(GLsizei length, const char* marker) {
+  SB_NOTIMPLEMENTED();
+}
 
-void GL_APIENTRY glPopGroupMarkerEXT() {}
+void GL_APIENTRY glPopGroupMarkerEXT() {
+  SB_NOTIMPLEMENTED();
+}
 
 void GL_APIENTRY glEGLImageTargetTexture2DOES(GLenum target,
-                                              GLeglImageOES image) {}
+                                              GLeglImageOES image) {
+  SB_NOTIMPLEMENTED();
+}
 
 void GL_APIENTRY glEGLImageTargetRenderbufferStorageOES(GLenum target,
-                                                        GLeglImageOES image) {}
+                                                        GLeglImageOES image) {
+  SB_NOTIMPLEMENTED();
+}
 
-void GL_APIENTRY glBindVertexArrayOES(GLuint array) {}
+void GL_APIENTRY glBindVertexArrayOES(GLuint array) {
+  SB_NOTIMPLEMENTED();
+}
 
-void GL_APIENTRY glDeleteVertexArraysOES(GLsizei n, const GLuint* arrays) {}
+void GL_APIENTRY glDeleteVertexArraysOES(GLsizei n, const GLuint* arrays) {
+  SB_NOTIMPLEMENTED();
+}
 
-void GL_APIENTRY glGenVertexArraysOES(GLsizei n, GLuint* arrays) {}
+void GL_APIENTRY glGenVertexArraysOES(GLsizei n, GLuint* arrays) {
+  SB_NOTIMPLEMENTED();
+}
 
 GLboolean GL_APIENTRY glIsVertexArrayOES(GLuint array) {
+  SB_NOTIMPLEMENTED();
   return false;
 }
 
