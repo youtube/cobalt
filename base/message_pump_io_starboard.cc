4ee23f34cfc671bd2af746a59548879e311ed7a6
Cobalt Team <cobalt-team@google.com>
2016-01-13 15:46:11 -0800

    Update comment about very tricky race condition
    Change-Id: I39f9dda6fd50737d7423468fa3c0bae994b721da

--- base/message_pump_io_starboard.cc
+++ base/message_pump_io_starboard.cc
@@ -172,15 +172,21 @@ void MessagePumpIOStarboard::Run(Delegate* delegate) {
     if (!keep_running_)
       break;
 
-    // NOTE: We need to have a wake-up pending any time there is work queued. If
-    // any work is scheduled here, the next call to wait could consume a
-    // wake-up, but leave the work undone. So we need to say that we did work if
-    // the waiter was woken up rather than timed out.
+    // NOTE: We need to have a wake-up pending any time there is work queued,
+    // and the MessageLoop only wakes up the pump when the work queue goes from
+    // 0 tasks to 1 task. If any work is scheduled on this MessageLoop (from
+    // another thread) anywhere in between the call to DoWork() above and the
+    // call to SbSocketWaiterWaitTimed() below, SbSocketWaiterWaitTimed() will
+    // consume a wake-up, but leave the work queued. This will cause the
+    // blocking wait further below to hang forever, no matter how many more
+    // items are added to the queue. To resolve this, if this wait consumes a
+    // wake-up, we set did_work to true so we will jump back to the top of the
+    // loop and call delegate->DoWork() before we decide to block.
 
     SbSocketWaiterResult result = SbSocketWaiterWaitTimed(waiter_, 0);
     DCHECK_NE(kSbSocketWaiterResultInvalid, result);
-    did_work |= (result == kSbSocketWaiterResultWokenUp) ||
-                processed_io_events_;
+    did_work |=
+        (result == kSbSocketWaiterResultWokenUp) || processed_io_events_;
     processed_io_events_ = false;
     if (!keep_running_)
       break;
