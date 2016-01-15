0fee82e3570b2a6da8b5ae92bf9e1433ab5fe500
Cobalt Team <cobalt-team@google.com>
2016-01-14 16:43:51 -0800

    Add Cobalt YUV shader support for glimp.
    Change-Id: I9e0b746e9a7e4ee9ec212b0344df76a9dd958335

--- cobalt/renderer/glimp_shaders/glsl/vertex_skia_yuv.glsl
+++ cobalt/renderer/glimp_shaders/glsl/vertex_skia_yuv.glsl
@@ -4,17 +4,12 @@ uniform mat3 uStageMatrix_Stage0;
 uniform vec4 urtAdjustment;
 attribute vec2 inPosition;
 attribute vec2 inLocalCoords;
-attribute vec4 inColor;
-varying vec4 vColor;
 varying vec2 vMatrixCoord_Stage0;
-
-void main()
-{
+void main() {
   vec3 pos3 = uViewM * vec3(inPosition, 1);
-  vColor = inColor;
   vMatrixCoord_Stage0 = (uStageMatrix_Stage0 * vec3(inLocalCoords, 1)).xy;
   {
-    // Stage 0: Texture
+    // Stage 0: YUV to RGB
   }
   gl_Position = vec4(dot(pos3.xz, urtAdjustment.xy),
                      dot(pos3.yz, urtAdjustment.zw), 0, pos3.z);
