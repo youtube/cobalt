3b8de6c91586a7753d83d066051ab62ab0a213c7
Cobalt Team <cobalt-team@google.com>
2016-01-21 09:55:47 -0800

    Add DialServiceProxy, for threadsafe DialService
    DialServiceProxy is a thread-safe refcounted proxy for
    interacting with DialService. It takes a weak pointer
    to the DialService instance and forwards all calls to the
    proper message loop.
    
    Make DialServiceHandler threadsafe-refcounted, to ensure that
    DialService always has a valid handler in its map.
    
    This fixes some shutdown ordering issues where a DialServiceHandler
    could have a dangling pointer to the DialService, if it lived
    beyond the DialService instance.
    
    Properly check the return value of DialHttpServer::GetLocalAddress().
    
    Move some destructors to private, to ensure only a Release() can
    destroy the refcounted object.
    
    b/26696189
    
    Change-Id: Iacf2acfec2221522b763801d88bf439f43f11649

--- net/dial/dial_service_unittest.cc
+++ net/dial/dial_service_unittest.cc
@@ -22,30 +22,32 @@ class DialServiceTest : public testing::Test {
     service_.reset();
   }
 
-  void GetHandlerTest(const std::string& path,
-                      const std::string& exp_path,
-                      const DialServiceHandler* exp_handler) {
+  void GetHandlerTest(
+      const std::string& path,
+      const std::string& expected_path,
+      const scoped_refptr<DialServiceHandler>& expected_handler) {
     std::string ret_path;
-    DialServiceHandler* ret_handler = service_->GetHandler(path, &ret_path);
-    EXPECT_EQ(ret_path, exp_path);
-    EXPECT_EQ(ret_handler, exp_handler);
+    scoped_refptr<DialServiceHandler> ret_handler =
+        service_->GetHandler(path, &ret_path);
+    EXPECT_EQ(ret_path, expected_path);
+    EXPECT_EQ(ret_handler, expected_handler);
   }
 };
 
 TEST_F(DialServiceTest, GetHandler) {
   service_.reset(new DialService());
 
-  MockServiceHandler foo_handler("Foo");
-  MockServiceHandler baz_handler("Baz");
+  scoped_refptr<MockServiceHandler> foo_handler = new MockServiceHandler("Foo");
+  scoped_refptr<MockServiceHandler> baz_handler = new MockServiceHandler("Baz");
 
-  service_->Register(&foo_handler);
-  service_->Register(&baz_handler);
+  service_->Register(foo_handler);
+  service_->Register(baz_handler);
 
-  GetHandlerTest("/apps/Foo/run", "/run", &foo_handler);
-  GetHandlerTest("/apps/Baz/404", "/404", &baz_handler);
-  GetHandlerTest("/apps/Baz/404/etc", "/404/etc", &baz_handler);
-  GetHandlerTest("/apps/Foo/", "/", &foo_handler);
-  GetHandlerTest("/apps/Foo", "/", &foo_handler);
+  GetHandlerTest("/apps/Foo/run", "/run", foo_handler);
+  GetHandlerTest("/apps/Baz/404", "/404", baz_handler);
+  GetHandlerTest("/apps/Baz/404/etc", "/404/etc", baz_handler);
+  GetHandlerTest("/apps/Foo/", "/", foo_handler);
+  GetHandlerTest("/apps/Foo", "/", foo_handler);
 
   GetHandlerTest("", "", NULL);
   GetHandlerTest("/", "", NULL);
@@ -54,23 +56,20 @@ TEST_F(DialServiceTest, GetHandler) {
   GetHandlerTest("/apps/FooBar/", "", NULL);
   GetHandlerTest("/apps/BarFoo/", "", NULL);
 
-  service_->Deregister(&foo_handler);
-  service_->Deregister(&baz_handler);
+  service_->Deregister(foo_handler);
+  service_->Deregister(baz_handler);
   service_->Terminate();
 }
 
 // Test the case where Deregister is called and immediately after the handler
-// is destroyed. This makes a call to the dial_service thread with a pointer
-// that is destroyed. Make sure that it handles appropriately.
-TEST_F(DialServiceTest, DestructedHandler) {
+// ref is released.
+TEST_F(DialServiceTest, ReleasedHandler) {
   service_.reset(new DialService());
 
-  MockServiceHandler* foo_handler = new MockServiceHandler("Foo");
+  scoped_refptr<MockServiceHandler> foo_handler = new MockServiceHandler("Foo");
   service_->Register(foo_handler);
-
-  // Immediately delete foo_handler after calling Deregister.
   service_->Deregister(foo_handler);
-  delete foo_handler;
+  foo_handler = NULL;
 
   service_->Terminate();
 }
