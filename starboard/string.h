3f4f82956c89a8bf065dcc8a3cc5c106742edd13
Cobalt Team <cobalt-team@google.com>
2016-01-29 15:28:07 -0800

    More OpenSSL requirements
    Added:
      SbStringScan
      SbStringParseUnsignedInteger
      SbStringParseSignedInteger
      SbStringAToI
      SbSystemSort
    
    b/23755433
    
    Change-Id: I152f179036993f1dd5ae448a94ffabfa5877caeb

--- starboard/string.h
+++ starboard/string.h
@@ -28,103 +28,104 @@ extern "C" {
 #endif
 
 // Returns the length, in characters, of |str|, a zero-terminated ASCII string.
-size_t SbStringGetLength(const char* str);
+SB_EXPORT size_t SbStringGetLength(const char* str);
 
 // Same as SbStringGetLength but for wide characters. This assumes that there
 // are no multi-element characters.
-size_t SbStringGetLengthWide(const wchar_t* str);
+SB_EXPORT size_t SbStringGetLengthWide(const wchar_t* str);
 
 // Copies as much |source| as possible into |out_destination| and
 // null-terminates it, given that it has |destination_size| characters of
 // storage available. Returns the length of |source|.
-int SbStringCopy(char* out_destination,
-                 const char* source,
-                 int destination_size);
+SB_EXPORT int SbStringCopy(char* out_destination,
+                           const char* source,
+                           int destination_size);
 
 // Same as SbStringCopy but for wide characters.
-int SbStringCopyWide(wchar_t* out_destination,
-                     const wchar_t* source,
-                     int destination_size);
+SB_EXPORT int SbStringCopyWide(wchar_t* out_destination,
+                               const wchar_t* source,
+                               int destination_size);
 
 // Concatenates |source| onto the end of |out_destination|, presuming it has
 // |destination_size| total characters of storage available. Returns the length
 // of |source|.
-int SbStringConcat(char* out_destination,
-                   const char* source,
-                   int destination_size);
+SB_EXPORT int SbStringConcat(char* out_destination,
+                             const char* source,
+                             int destination_size);
 
 // Same as SbStringConcat but for wide characters.
-int SbStringConcatWide(wchar_t* out_destination,
-                       const wchar_t* source,
-                       int destination_size);
+SB_EXPORT int SbStringConcatWide(wchar_t* out_destination,
+                                 const wchar_t* source,
+                                 int destination_size);
 
 // Copies the string |source| into a buffer allocated by this function that can
 // be freed with SbMemoryFree. Meant to be a drop-in replacement for strdup.
-char* SbStringDuplicate(const char* source);
+SB_EXPORT char* SbStringDuplicate(const char* source);
 
 // Finds the first occurence of |character| in |str|, returning a pointer to
 // the found character in the given string, or NULL if not found.
-const char* SbStringFindCharacter(const char* str, char character);
+SB_EXPORT const char* SbStringFindCharacter(const char* str, char character);
 
 // Finds the last occurence of |character| in |str|, returning a pointer to
 // the found character in the given string, or NULL if not found.
-const char* SbStringFindLastCharacter(const char* str, char character);
+SB_EXPORT const char* SbStringFindLastCharacter(const char* str,
+                                                char character);
 
 // Compares |string1| and |string2|, ignoring differences in case. Returns < 0
 // if |string1| is ASCII-betically lower than |string2|, 0 if they are equal,
 // and > 0 if |string1| is ASCII-betically higher than |string2|. Meant to be a
 // drop-in replacement for strcasecmp.
-int SbStringCompareNoCase(const char* string1, const char* string2);
+SB_EXPORT int SbStringCompareNoCase(const char* string1, const char* string2);
 
 // Compares the first |count| characters of |string1| and |string2|, ignoring
 // differences in case. Returns < 0 if |string1| is ASCII-betically lower than
 // |string2|, 0 if they are equal, and > 0 if |string1| is ASCII-betically
 // higher than |string2|. Meant to be a drop-in replacement for strncasecmp.
-int SbStringCompareNoCaseN(const char* string1,
-                           const char* string2,
-                           size_t count);
+SB_EXPORT int SbStringCompareNoCaseN(const char* string1,
+                                     const char* string2,
+                                     size_t count);
 
 // Formats the given |format| and |arguments|, placing as much of the result
 // will fit in |out_buffer|, whose size is specified by |buffer_size|. Returns
 // the number of characters the format would produce if |buffer_size| was
 // infinite. Meant to be a drop-in replacement for vsnprintf.
-int SbStringFormat(char* out_buffer,
-                   size_t buffer_size,
-                   const char* format,
-                   va_list arguments) SB_PRINTF_FORMAT(3, 0);
+SB_EXPORT int SbStringFormat(char* out_buffer,
+                             size_t buffer_size,
+                             const char* format,
+                             va_list arguments) SB_PRINTF_FORMAT(3, 0);
 
 // Inline wrapper of SbStringFormat to convert from ellipsis to va_args.
-inline int SbStringFormatF(char* out_buffer,
-                           size_t buffer_size,
-                           const char* format,
-                           ...) SB_PRINTF_FORMAT(3, 4);
-inline int SbStringFormatF(char* out_buffer,
-                           size_t buffer_size,
-                           const char* format,
-                           ...) {
-  va_list args;
-  va_start(args, format);
-  int result = SbStringFormat(out_buffer, buffer_size, format, args);
-  va_end(args);
+static SB_C_INLINE int SbStringFormatF(char* out_buffer,
+                                       size_t buffer_size,
+                                       const char* format,
+                                       ...) SB_PRINTF_FORMAT(3, 4);
+static SB_C_INLINE int SbStringFormatF(char* out_buffer,
+                                       size_t buffer_size,
+                                       const char* format,
+                                       ...) {
+  va_list arguments;
+  va_start(arguments, format);
+  int result = SbStringFormat(out_buffer, buffer_size, format, arguments);
+  va_end(arguments);
   return result;
 }
 
 // Same as SbStringFormat, but for wide characters. Meant to be a drop-in
 // replacement for vswprintf.
-int SbStringFormatWide(wchar_t* out_buffer,
-                       size_t buffer_size,
-                       const wchar_t* format,
-                       va_list arguments);
+SB_EXPORT int SbStringFormatWide(wchar_t* out_buffer,
+                                 size_t buffer_size,
+                                 const wchar_t* format,
+                                 va_list arguments);
 
 // Inline wrapper of SbStringFormatWide to convert from ellipsis to va_args.
-inline int SbStringFormatWideF(wchar_t* out_buffer,
-                               size_t buffer_size,
-                               const wchar_t* format,
-                               ...) {
-  va_list args;
-  va_start(args, format);
-  int result = SbStringFormatWide(out_buffer, buffer_size, format, args);
-  va_end(args);
+static SB_C_INLINE int SbStringFormatWideF(wchar_t* out_buffer,
+                                           size_t buffer_size,
+                                           const wchar_t* format,
+                                           ...) {
+  va_list arguments;
+  va_start(arguments, format);
+  int result = SbStringFormatWide(out_buffer, buffer_size, format, arguments);
+  va_end(arguments);
   return result;
 }
 
@@ -132,22 +133,67 @@ inline int SbStringFormatWideF(wchar_t* out_buffer,
 // 16-bit character strings. Returns < 0 if |string1| is ASCII-betically lower
 // than |string2|, 0 if they are equal, and > 0 if |string1| is ASCII-betically
 // higher than |string2|. Meant to be a drop-in replacement of wcsncmp.
-int SbStringCompareWide(const wchar_t* string1,
-                        const wchar_t* string2,
-                        size_t count);
+SB_EXPORT int SbStringCompareWide(const wchar_t* string1,
+                                  const wchar_t* string2,
+                                  size_t count);
 
 // Compares the first |count| characters of |string1| and |string2|, which are
 // 8-bit character strings. Returns < 0 if |string1| is ASCII-betically lower
 // than |string2|, 0 if they are equal, and > 0 if |string1| is ASCII-betically
 // higher than |string2|. Meant to be a drop-in replacement of strncmp.
-int SbStringCompare(const char* string1, const char* string2, size_t count);
+SB_EXPORT int SbStringCompare(const char* string1,
+                              const char* string2,
+                              size_t count);
 
 // Compares all the characters of |string1| and |string2|, which are 8-bit
 // character strings, up to their natural termination. Returns < 0 if |string1|
 // is ASCII-betically lower than |string2|, 0 if they are equal, and > 0 if
 // |string1| is ASCII-betically higher than |string2|. Meant to be a drop-in
 // replacement of strcmp.
-int SbStringCompareAll(const char* string1, const char* string2);
+SB_EXPORT int SbStringCompareAll(const char* string1, const char* string2);
+
+// Scans |buffer| for |pattern|, placing the extracted values in |arguments|.
+// Returns the number of successfully matched items, which may be zero. Meant to
+// be a drop-in replacement for vsscanf.
+SB_EXPORT int SbStringScan(const char* buffer,
+                           const char* pattern,
+                           va_list arguments);
+
+// Inline wrapper of SbStringScan to convert from ellipsis to va_args. Meant to
+// be a drop-in replacement for sscanf.
+static SB_C_INLINE int SbStringScanF(const char* buffer,
+                                     const char* pattern,
+                                     ...) {
+  va_list arguments;
+  va_start(arguments, pattern);
+  int result = SbStringScan(buffer, pattern, arguments);
+  va_end(arguments);
+  return result;
+}
+
+// Parses a string at the beginning of |start| into a signed integer in the
+// given |base|.  It will place the pointer to the end of the consumed portion
+// of the string in |out_end|, if it is provided. Meant to be a drop-in
+// replacement for strtol.
+// NOLINTNEXTLINE(runtime/int)
+SB_EXPORT long SbStringParseSignedInteger(const char* start,
+                                          char** out_end,
+                                          int base);
+
+// Shorthand replacement for atoi. Parses |value| into a base-10 int.
+static SB_C_INLINE int SbStringAToI(const char* value) {
+  // NOLINTNEXTLINE(readability/casting)
+  return (int)SbStringParseSignedInteger(value, NULL, 10);
+}
+
+// Parses a string at the beginning of |start| into a unsigned integer in the
+// given |base|.  It will place the pointer to the end of the consumed portion
+// of the string in |out_end|, if it is provided. Meant to be a drop-in
+// replacement for strtol.
+// NOLINTNEXTLINE(runtime/int)
+SB_EXPORT unsigned long SbStringParseUnsignedInteger(const char* start,
+                                                     char** out_end,
+                                                     int base);
 
 #ifdef __cplusplus
 }  // extern "C"
