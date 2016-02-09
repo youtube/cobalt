75c0a708cf65bf0f5720873a240f408e0ac57e17
Cobalt Team <cobalt-team@google.com>
2016-02-09 10:26:02 -0800

    Adds DOM Inspector to debug server.
    b/26575382.
    b/26745859.
    
    Adds the most important commands from the DOM domain of the remote
    debugging protocol:
    https://developer.chrome.com/devtools/docs/protocol/1.1/dom
    
    Currently, this is enough to display the DOM in devtools to an
    arbitrary depth and highlight a DOM node in Cobalt if the mouse is
    hovering over it in the displayed list. Commands that allow
    interactive modification of the DOM are not yet supported.
    This implementation also doesn't yet handle modification of the DOM
    by Cobalt itself. This should almost certainly be the next priority.
    
    Adds a debug overlay, which allows the debugger to add elements to
    Cobalt's render tree, used in this case to enable DOM element
    highlighting.
    
    To prevent the |DebugServer| class growing inexorably as new devtools
    functionality is added, each functional component is now broken out
    into a separate class inheriting from |DebugServer::Component|. This
    also isolates dependencies better, as |DebugServer| doesn't have to
    know the details of the functionality supported by the components
    that have been added to it.
    
    Change-Id: I5231aa4a5df820edd9c057911e893b8fd029ff84

---
+++ cobalt/debug/dom_component.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef COBALT_DEBUG_DOM_COMPONENT_H_
+#define COBALT_DEBUG_DOM_COMPONENT_H_
+
+#include <string>
+
+#include "base/memory/weak_ptr.h"
+#include "cobalt/debug/debug_server.h"
+#include "cobalt/debug/dom_inspector.h"
+#include "cobalt/debug/json_object.h"
+#include "cobalt/debug/render_overlay.h"
+#include "cobalt/dom/document.h"
+
+namespace cobalt {
+namespace debug {
+
+class DOMComponent : public DebugServer::Component {
+ public:
+  DOMComponent(const base::WeakPtr<DebugServer>& server,
+               dom::Document* document, RenderOverlay* debug_overlay);
+
+ private:
+  // Called by |dom_inspector_| to provide notifications to this component.
+  void OnNotification(const std::string& method, const JSONObject& params);
+
+  // Gets a JSON representation of the document object, including its children
+  // to a few levels deep (subsequent levels will be returned via notification
+  // when the client calls |RequestChildNodes|).
+  JSONObject GetDocument(const JSONObject& params);
+
+  // Requests that the children of a specified node should be returned via
+  // notification.
+  JSONObject RequestChildNodes(const JSONObject& params);
+
+  // Highlights a specified node according to highlight parameters.
+  JSONObject HighlightNode(const JSONObject& params);
+
+  // Hides the node highlighting.
+  JSONObject HideHighlight(const JSONObject& params);
+
+  // Handles debugging interaction with the DOM.
+  scoped_ptr<DOMInspector> dom_inspector_;
+};
+
+}  // namespace debug
+}  // namespace cobalt
+
+#endif  // COBALT_DEBUG_DOM_COMPONENT_H_
