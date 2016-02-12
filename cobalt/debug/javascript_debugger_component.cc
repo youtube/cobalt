eec64f491c7f7d192220b5d56c47ab89a9c5b3b0
Cobalt Team <cobalt-team@google.com>
2016-02-11 16:35:15 -0800

    Implements enable/disable for DOM and Debugger.
    b/27129295.
    
    Chrome devtools defines enable/disable methods for each domain. This
    adds handling of those methods for the two currently partially
    implemented domains: DOM and Debugger. Subsequent domains will use the
    same framework.
    
    No longer creates the objects used by these components at construction,
    instead delays their creation until the domain is enabled, and destroys
    them again when the domain is disabled. This could in theory make
    operation much more efficient (especially for heavyweight objects like
    the JavaScript debugger), although in practice, devtools generally
    doesn't disable these domains. Still, it conforms better to the
    protocol.
    
    Adds a DebugServer::Component::ErrorResponse function that any
    component can call to generate an error response with a specified
    message.
    
    Change-Id: Ibbda7e30aef5e7c5ff5e7f50190e1ff08c6c133e

--- cobalt/debug/javascript_debugger_component.cc
+++ cobalt/debug/javascript_debugger_component.cc
@@ -17,6 +17,7 @@
 #include "cobalt/debug/javascript_debugger_component.h"
 
 #include "base/bind.h"
+#include "base/values.h"
 
 namespace cobalt {
 namespace debug {
@@ -24,36 +25,61 @@ namespace debug {
 namespace {
 // Command "methods" (names) from the set specified here:
 // https://developer.chrome.com/devtools/docs/protocol/1.1/debugger
+const char kDisable[] = "Debugger.disable";
+const char kEnable[] = "Debugger.enable";
 const char kGetScriptSource[] = "Debugger.getScriptSource";
 }  // namespace
 
 JavaScriptDebuggerComponent::JavaScriptDebuggerComponent(
     const base::WeakPtr<DebugServer>& server,
     script::GlobalObjectProxy* global_object_proxy)
-    : DebugServer::Component(server) {
-  script::JavaScriptDebuggerInterface::OnEventCallback on_event_callback =
-      base::Bind(&JavaScriptDebuggerComponent::OnNotification,
-                 base::Unretained(this));
-
-  script::JavaScriptDebuggerInterface::OnDetachCallback on_detach_callback;
-
-  javascript_debugger_ = script::JavaScriptDebuggerInterface::CreateDebugger(
-      global_object_proxy, on_event_callback, on_detach_callback);
-
+    : DebugServer::Component(server),
+      global_object_proxy_(global_object_proxy) {
+  AddCommand(kDisable, base::Bind(&JavaScriptDebuggerComponent::Disable,
+                                  base::Unretained(this)));
+  AddCommand(kEnable, base::Bind(&JavaScriptDebuggerComponent::Enable,
+                                 base::Unretained(this)));
   AddCommand(kGetScriptSource,
              base::Bind(&JavaScriptDebuggerComponent::GetScriptSource,
                         base::Unretained(this)));
+}
+
+JSONObject JavaScriptDebuggerComponent::Enable(const JSONObject& params) {
+  UNREFERENCED_PARAMETER(params);
+
+  script::JavaScriptDebuggerInterface::OnEventCallback on_event_callback =
+      base::Bind(&JavaScriptDebuggerComponent::OnNotification,
+                 base::Unretained(this));
+  script::JavaScriptDebuggerInterface::OnDetachCallback on_detach_callback;
+  javascript_debugger_ = script::JavaScriptDebuggerInterface::CreateDebugger(
+      global_object_proxy_, on_event_callback, on_detach_callback);
+  DCHECK(javascript_debugger_);
+
+  if (javascript_debugger_) {
+    return JSONObject(new base::DictionaryValue());
+  } else {
+    return ErrorResponse("Cannot create JavaScript debugger.");
+  }
+}
+
+JSONObject JavaScriptDebuggerComponent::Disable(const JSONObject& params) {
+  UNREFERENCED_PARAMETER(params);
+  javascript_debugger_.reset(NULL);
+  return JSONObject(new base::DictionaryValue());
+}
+
+JSONObject JavaScriptDebuggerComponent::GetScriptSource(
+    const JSONObject& params) {
+  if (javascript_debugger_) {
+    return javascript_debugger_->GetScriptSource(params);
+  } else {
+    return ErrorResponse("JavaScript debugger is not enabled.");
+  }
 }
 
 void JavaScriptDebuggerComponent::OnNotification(const std::string& method,
                                                  const JSONObject& params) {
   SendNotification(method, params);
 }
-
-JSONObject JavaScriptDebuggerComponent::GetScriptSource(
-    const JSONObject& params) {
-  return javascript_debugger_->GetScriptSource(params);
-}
-
 }  // namespace debug
 }  // namespace cobalt
