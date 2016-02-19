9dc1d50e76cfac92d22fd8118400806044461483
Cobalt Team <cobalt-team@google.com>
2016-02-18 16:14:57 -0800

    Upgrade libxml - introduce Cobalt's functionality
    Change-Id: I65a99d303358388eb96e868397e581a700c6eec6

--- third_party/libxml/src/HTMLparser.c
+++ third_party/libxml/src/HTMLparser.c
@@ -55,6 +55,9 @@
 /* #define DEBUG_PUSH */
 
 static int htmlOmittedDefaultValue = 1;
+#if defined(COBALT)
+static int htmlEmitImpliedRootLevelParagraphValue = 1;
+#endif  // defined(COBALT)
 
 xmlChar * htmlDecodeEntities(htmlParserCtxtPtr ctxt, int len,
 			     xmlChar end, xmlChar  end2, xmlChar end3);
@@ -1558,6 +1561,10 @@ htmlCheckParagraph(htmlParserCtxtPtr ctxt) {
 	return(-1);
     tag = ctxt->name;
     if (tag == NULL) {
+#if defined(COBALT)
+	if (!htmlEmitImpliedRootLevelParagraphValue)
+	    return(0);
+#endif  // defined(COBALT)
 	htmlAutoClose(ctxt, BAD_CAST"p");
 	htmlCheckImplied(ctxt, BAD_CAST"p");
 	htmlnamePush(ctxt, BAD_CAST"p");
@@ -6381,6 +6388,20 @@ htmlHandleOmittedElem(int val) {
     htmlOmittedDefaultValue = val;
     return(old);
 }
+
+#if defined(COBALT)
+/**
+ * htmlEmitImpliedRootLevelParagraph:
+ * @val:  int 0 or 1
+ *
+ * Set the value for emitting implied root level paragraph elements.
+ */
+
+void
+htmlEmitImpliedRootLevelParagraph(int val) {
+    htmlEmitImpliedRootLevelParagraphValue = val;
+}
+#endif  // defined(COBALT)
 
 /**
  * htmlElementAllowedHere:
