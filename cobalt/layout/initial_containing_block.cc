b4e44b0b82c0637012b00fc1b5a455547ecece4d
Cobalt Team <cobalt-team@google.com>
2016-03-03 17:48:01 -0800

    Do not propagate background style to intial containing block if display: none
    b/27338148
    Change-Id: I1f3e9a952f9d175e23641d91a93103f6510a66f9

--- cobalt/layout/initial_containing_block.cc
+++ cobalt/layout/initial_containing_block.cc
@@ -32,17 +32,23 @@ namespace layout {
 
 // Conditionally copies the background property. Returns true if anything is
 // copied.
-// The background color is copied if it is not transparent
-// The background image is copied if it is not 'None'.
-bool ConditionalCopyBackgroundStyle(
-    const scoped_refptr<const cssom::CSSStyleDeclarationData>& source_style,
+bool PropagateBackgroundStyleAndTestIfChanged(
+    const scoped_refptr<dom::HTMLElement>& element,
     scoped_refptr<cssom::CSSStyleDeclarationData> destination_style) {
+  if (!element || !element->computed_style() ||
+      element->computed_style()->display() == cssom::KeywordValue::GetNone()) {
+    return false;
+  }
+
+  const scoped_refptr<const cssom::CSSStyleDeclarationData>& computed_style =
+      element->computed_style();
+
   bool background_color_is_transparent =
-      GetUsedColor(source_style->background_color()).a() == 0.0f;
+      GetUsedColor(computed_style->background_color()).a() == 0.0f;
 
   cssom::PropertyListValue* background_image_list =
       base::polymorphic_downcast<cssom::PropertyListValue*>(
-          source_style->background_image().get());
+          computed_style->background_image().get());
   DCHECK_GT(background_image_list->value().size(), 0u);
 
   bool background_image_is_none =
@@ -50,40 +56,19 @@ bool ConditionalCopyBackgroundStyle(
       background_image_list->value()[0] == cssom::KeywordValue::GetNone();
 
   if (!background_color_is_transparent || !background_image_is_none) {
+    // The background color is copied if it is not transparent.
     if (!background_color_is_transparent) {
-      destination_style->set_background_color(source_style->background_color());
+      destination_style->set_background_color(
+          computed_style->background_color());
     }
+    // The background image is copied if it is not 'None'.
     if (!background_image_is_none) {
-      destination_style->set_background_image(source_style->background_image());
+      destination_style->set_background_image(
+          computed_style->background_image());
     }
     return true;
   }
   return false;
-}
-
-// This propagates the computed background style of the <html> or <body> element
-// to the given style for the initial containing block.
-//   https://www.w3.org/TR/css3-background/#body-background
-void PropagateBackgroundStyleToInitialStyle(
-    const scoped_refptr<dom::Document>& document,
-    scoped_refptr<cssom::CSSStyleDeclarationData>
-        initial_containing_block_computed_style) {
-  dom::HTMLHtmlElement* html_element = document->html();
-  if (html_element) {
-    // Propagate the background style from the <html> element if there is any
-    if (!ConditionalCopyBackgroundStyle(
-            html_element->computed_style(),
-            initial_containing_block_computed_style)) {
-      dom::HTMLBodyElement* body_element = document->body();
-      if (body_element && body_element->computed_style()) {
-        // Otherwise, propagate the background style from the <body> element.
-        // It is possible that the computed style of body element is NULL, when
-        // the display of html element is set to 'none'.
-        ConditionalCopyBackgroundStyle(body_element->computed_style(),
-                                       initial_containing_block_computed_style);
-      }
-    }
-  }
 }
 
 scoped_refptr<BlockLevelBlockContainerBox> CreateInitialContainingBlock(
@@ -93,11 +78,14 @@ scoped_refptr<BlockLevelBlockContainerBox> CreateInitialContainingBlock(
     UsedStyleProvider* used_style_provider) {
   TRACE_EVENT0("cobalt::layout", "CreateInitialContainingBlock");
 
-  // The background color and image style may need to be propagated up from the
-  // <body> element to the parent <html> element.
+  // Propagate the computed background style of the <html> or <body> element
+  // to the given style for the initial containing block.
   //   https://www.w3.org/TR/css3-background/#body-background
-  PropagateBackgroundStyleToInitialStyle(
-      document, initial_containing_block_style);
+  if (!PropagateBackgroundStyleAndTestIfChanged(
+          document->html(), initial_containing_block_style)) {
+    PropagateBackgroundStyleAndTestIfChanged(
+        document->body(), initial_containing_block_style);
+  }
 
   scoped_refptr<cssom::CSSComputedStyleDeclaration> initial_style_state =
       new cssom::CSSComputedStyleDeclaration();
