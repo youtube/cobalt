8953c3b6210146ce0f4688b1aa76e13f0dc2701b
Cobalt Team <cobalt-team@google.com>
2016-03-15 16:19:29 -0700

    Adds initial implementation of Command Line API.
    b/27613751.
    
    Implements a mechanism to add Command Line API shortcuts during each
    call to Runtime.evaluate, if specified in the parameters. Adds an
    initial set of such shortcuts:
      $_ - (last result)
      $ - document.querySelector
      $$ - document.querySelectorAll
      keys - Object.keys
      values - like keys, but returns an array of corresponding values
    
    Tweaks the object preview results to be closer to those generated
    by Chrome.
    
    Prefixes internal object members and functions with _ to indicate that
    these are not for external use.
    
    Change-Id: I1c2272131451a5488f40acecdf53153dd88aaa76

--- cobalt/debug/content/backend/runtime.js
+++ cobalt/debug/content/backend/runtime.js
@@ -29,7 +29,7 @@ devtoolsBackend.runtime.callFunctionOn = function(params) {
   var value = null;
   try {
     eval('var f = ' + params.functionDeclaration);
-    var objectEntry = this.objectStore[params.objectId];
+    var objectEntry = this._objectStore[params.objectId];
     var thisArg = objectEntry.object;
     var objectGroup = objectEntry.objectGroup;
     value = f.apply(thisArg, params.arguments);
@@ -51,10 +51,22 @@ devtoolsBackend.runtime.evaluate = function(params) {
   var result = {};
   var value = null;
   try {
+    if (params.includeCommandLineAPI) {
+      this._addCommandLineAPI();
+    }
+
     // Use |eval| indirectly, to cause evaluation at global scope.
     // This is so subsequent calls can access variables declared here, etc.
     var geval = eval;
     value = geval(params.expression);
+
+    // Store the last result if we're doing a "real" evaluation, not an
+    // auto-complete. Seems a little special case-y, but this is taken from
+    // Chrome's implementation, and looks like the only way to differentiate.
+    if (params.objectGroup == 'console') {
+      this._lastResult = value;
+    }
+
     result.wasThrown = false;
   } catch(e) {
     value = e;
@@ -74,6 +86,10 @@ devtoolsBackend.runtime.evaluate = function(params) {
     }
   }
 
+  if (params.includeCommandLineAPI) {
+    this._removeCommandLineAPI();
+  }
+
   return JSON.stringify(result);
 }
 
@@ -87,7 +103,7 @@ devtoolsBackend.runtime.getProperties = function(params) {
   var result = {};
   var properties = [];
   try {
-    var objectEntry = this.objectStore[params.objectId];
+    var objectEntry = this._objectStore[params.objectId];
     var object = objectEntry.object;
     var objectGroup = objectEntry.objectGroup;
     this.addProperties(object, objectGroup, !params.ownProperties, properties);
@@ -111,16 +127,16 @@ devtoolsBackend.runtime.getProperties = function(params) {
 // Releases our reference to a previously accessed object.
 // https://developer.chrome.com/devtools/docs/protocol/1.1/runtime#command-releaseObject
 devtoolsBackend.runtime.releaseObject = function(params) {
-  delete this.objectStore[params.objectId];
+  delete this._objectStore[params.objectId];
 }
 
 // Releases our references to a group of previously accessed objects.
 // https://developer.chrome.com/devtools/docs/protocol/1.1/runtime#command-releaseObjectGroup
 devtoolsBackend.runtime.releaseObjectGroup = function(params) {
-  for (var objectId in this.objectStore) {
-    var objectEntry = this.objectStore[objectId];
+  for (var objectId in this._objectStore) {
+    var objectEntry = this._objectStore[objectId];
     if (objectEntry && objectEntry.objectGroup == params.objectGroup) {
-      delete this.objectStore[objectId];
+      delete this._objectStore[objectId];
     }
   }
 }
@@ -176,26 +192,26 @@ devtoolsBackend.runtime.addProperties =
 
 // Gets an object from the internal object store.
 devtoolsBackend.runtime.getObject = function(objectId) {
-  return this.objectStore[objectId].object;
+  return this._objectStore[objectId].object;
 }
 
 // Adds an object to the internal object store and returns a unique id that can
 // be used to access it again.
 devtoolsBackend.runtime.addObject = function(object, objectGroup) {
   // If we've already added this object, then use the same objectId.
-  for (var objectId in this.objectStore) {
-    var objectEntry = this.objectStore[objectId];
+  for (var objectId in this._objectStore) {
+    var objectEntry = this._objectStore[objectId];
     if (objectEntry.object === object &&
         objectEntry.objectGroup == objectGroup) {
       return objectId;
     }
   }
 
-  var objectId = this.nextObjectId.toString();
-  this.nextObjectId += 1;
-  this.objectStore[objectId] = {};
-  this.objectStore[objectId].object = object;
-  this.objectStore[objectId].objectGroup = objectGroup;
+  var objectId = this._nextObjectId.toString();
+  this._nextObjectId += 1;
+  this._objectStore[objectId] = {};
+  this._objectStore[objectId].object = object;
+  this._objectStore[objectId].objectGroup = objectGroup;
   return objectId;
 }
 
@@ -245,7 +261,7 @@ devtoolsBackend.runtime.getClassName = function(object) {
 
 // Creates a RemoteObject, which is the type used to return many values to
 // devtools. If |value| is an object, then is it inserted into
-// |devtoolsBackend.runtime.objectStore| and the |objectId| key used to
+// |devtoolsBackend.runtime._objectStore| and the |objectId| key used to
 // access it is included in the RemoteObject. If |value| is not an object,
 // or |returnByValue| is true, then |value| is directly included in the
 // RemoteObject.
@@ -287,7 +303,7 @@ devtoolsBackend.runtime.RemoteObject = function(value, objectGroup,
 // Creates a PropertyDescriptor for |property| of |object|,
 // which is the type used to return object properties to devtools.
 // Some properties may be objects, in which case new RemoteObjects are created
-// and inserted into |devtoolsBackend.runtime.objectStore| using the specified
+// and inserted into |devtoolsBackend.runtime._objectStore| using the specified
 // |objectGroup|, which should be that of |object|.
 devtoolsBackend.runtime.PropertyDescriptor = function(object, objectGroup,
                                                       property) {
@@ -340,25 +356,44 @@ devtoolsBackend.runtime.ObjectPreview = function(value) {
   // Use the className as the preview description. This matches Chrome.
   this.description = devtoolsBackend.runtime.getClassName(value);
 
-  // Add array items.
-  for (var i = 0; i < value.length; i++) {
-    var property = new devtoolsBackend.runtime.PropertyPreview(i, value[i]);
-    this.properties.push(property);
+  // If we have an array-like object, add the array items, or append the
+  // length to the description if there's too many.
+  if (value.length != null) {
+    var MAX_ARRAY_ITEMS = 99;
+    if (value.length <= MAX_ARRAY_ITEMS) {
+      for (var i = 0; i < value.length; i++) {
+        var property = new devtoolsBackend.runtime.PropertyPreview(i, value[i]);
+        this.properties.push(property);
+        if (typeof value[i] == 'object') {
+          this.lossless = false;
+          this.overflow = true;
+        }
+      }
+    } else {
+      this.description += '[' + value.length + ']';
+      this.lossless = false;
+      this.overflow = true;
+    }
+    return;
   }
 
   // Add object properties, up to a maximum.
   var MAX_PROPERTIES = 5;
-  for (name in value) {
+  var numProperties = 0;
+  for (var name in value) {
     if (value[name] != null) {
-      var property = new devtoolsBackend.runtime.PropertyPreview(name,
-                                                                 value[name]);
-      this.properties.push(property);
-      if (this.properties.length > MAX_PROPERTIES) {
-        this.properties.pop();
+      if (++numProperties > MAX_PROPERTIES) {
         this.lossless = false;
         this.overflow = true;
         break;
       }
+      if (typeof property == 'object') {
+        this.lossless = false;
+        this.overflow = true;
+      }
+      var property = new devtoolsBackend.runtime.PropertyPreview(name,
+                                                                 value[name]);
+      this.properties.push(property);
     }
   }
 }
@@ -368,7 +403,10 @@ devtoolsBackend.runtime.ObjectPreview = function(value) {
 devtoolsBackend.runtime.PropertyPreview = function(name, value) {
   this.name = name.toString();
   this.type = typeof value;
-  this.value = value.toString();
+
+  try {
+    this.value = value.toString();
+  } catch(e) {}
 
   if (this.type == 'object') {
     this.subtype = devtoolsBackend.runtime.getSubtype(value);
@@ -376,5 +414,59 @@ devtoolsBackend.runtime.PropertyPreview = function(name, value) {
 }
 
 // The object store used to reference objects by internally generated id.
-devtoolsBackend.runtime.objectStore = {};
-devtoolsBackend.runtime.nextObjectId = 0;
+devtoolsBackend.runtime._objectStore = {};
+devtoolsBackend.runtime._nextObjectId = 0;
+
+// The last evaluated result.
+devtoolsBackend.runtime._lastResult = null;
+
+// Values in the global scope that have been overridden by corresponding
+// members of the Command Line API for the duration of an evaluation. We use
+// this to restore the original values after the evaluation.
+devtoolsBackend.runtime._globalOverrides = {};
+
+// Command Line API implementation.
+// This is a set of convenience variables/functions that are not present in
+// the global scope by default, but can be specified as available to the
+// Runtime.evaluate function by the includeCommandLineAPI parameter.
+// https://developers.google.com/web/tools/chrome-devtools/debug/command-line/command-line-reference
+devtoolsBackend.runtime._commandLineAPI = {};
+
+devtoolsBackend.runtime._commandLineAPI.$_ =
+    devtoolsBackend.runtime._lastResult;
+
+devtoolsBackend.runtime._commandLineAPI.$ =
+    document.querySelector.bind(document);
+
+devtoolsBackend.runtime._commandLineAPI.$$ =
+    document.querySelectorAll.bind(document);
+
+devtoolsBackend.runtime._commandLineAPI.keys = Object.keys;
+
+devtoolsBackend.runtime._commandLineAPI.values = function(object) {
+  var keys = Object.keys(object);
+  var result = [];
+  for (var i = 0; i < keys.length; i++) {
+    result.push(object[keys[i]]);
+  }
+  return result;
+}
+
+devtoolsBackend.runtime._addCommandLineAPI = function() {
+  this._commandLineAPI.$_ = this._lastResult;
+  for (var property in this._commandLineAPI) {
+    if (this._commandLineAPI.hasOwnProperty(property)) {
+      this._globalOverrides[property] = window[property];
+      window[property] = this._commandLineAPI[property];
+    }
+  }
+}
+
+devtoolsBackend.runtime._removeCommandLineAPI = function() {
+  for (var property in this._globalOverrides) {
+    if (this._globalOverrides.hasOwnProperty(property)) {
+      window[property] = this._globalOverrides[property];
+      delete this._globalOverrides[property];
+    }
+  }
+}
