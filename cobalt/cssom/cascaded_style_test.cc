8d20c04032bc9982efab7874afd7ca11dcf6e368
Cobalt Team <cobalt-team@google.com>
2016-03-16 14:56:30 -0700

    Location url should be updated for each matching rules
    Location url is not fixed for all of the matching rules and it required to
    be updated for each one of them.
    
    This change fixed the issue that we resolved a wrong URL, and speeds up
    UpdateComputedStyle 0.6ms.
    
    b/27680686
    
    Change-Id: Ibf0db386c8184e127b273ad2e9d13246ed7a3fcb

--- cobalt/cssom/cascaded_style_test.cc
+++ cobalt/cssom/cascaded_style_test.cc
@@ -23,6 +23,7 @@
 #include "cobalt/cssom/css_declared_style_data.h"
 #include "cobalt/cssom/css_rule_style_declaration.h"
 #include "cobalt/cssom/css_style_rule.h"
+#include "cobalt/cssom/css_style_sheet.h"
 #include "cobalt/cssom/keyword_value.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -105,6 +106,59 @@ TEST(CascadedStyleTest, PromoteToCascadedStyle) {
   EXPECT_EQ(computed_style->text_align(),
             css_style_rule_2->declared_style_data()->GetPropertyValue(
                 cssom::kTextAlignProperty));
+}
+
+TEST(CascadedStyleTest, PromoteToCascadedStyleWithBackgroundImage) {
+  scoped_ptr<css_parser::Parser> css_parser = css_parser::Parser::Create();
+  scoped_refptr<CSSDeclaredStyleData> style = new CSSDeclaredStyleData();
+  RulesWithCascadePriority rules_with_cascade_priority;
+  cssom::GURLMap property_key_to_base_url_map;
+
+  scoped_refptr<CSSStyleRule> css_style_rule_1 =
+      css_parser->ParseRule(
+                    "div {"
+                    "  left: 100px !important;"
+                    "  right: 100px;"
+                    "}",
+                    base::SourceLocation("[object CascadedStyleTest]", 1, 1))
+          ->AsCSSStyleRule();
+  CascadePriority cascade_priority_1(kNormalUserAgent);  // Lowest priority.
+  rules_with_cascade_priority.push_back(
+      std::make_pair(css_style_rule_1, cascade_priority_1));
+
+  scoped_refptr<CSSStyleRule> css_style_rule_2 =
+      css_parser->ParseRule(
+                    "div {"
+                    "  left: 200px;"
+                    "  right: 200px !important;"
+                    "  background-image: url(foo.png);"
+                    "}",
+                    base::SourceLocation("[object CascadedStyleTest]", 1, 1))
+          ->AsCSSStyleRule();
+  CascadePriority cascade_priority_2(kNormalOverride);  // Highest priority.
+  rules_with_cascade_priority.push_back(
+      std::make_pair(css_style_rule_2, cascade_priority_2));
+
+  scoped_refptr<CSSStyleSheet> parent_style_sheet(new CSSStyleSheet());
+  parent_style_sheet->SetLocationUrl(GURL("https:///www.youtube.com/tv/img"));
+  css_style_rule_2->set_parent_style_sheet(parent_style_sheet.get());
+
+  scoped_refptr<cssom::CSSComputedStyleData> computed_style =
+      PromoteToCascadedStyle(style, &rules_with_cascade_priority,
+                             &property_key_to_base_url_map);
+
+  EXPECT_EQ(computed_style->left(),
+            css_style_rule_1->declared_style_data()->GetPropertyValue(
+                cssom::kLeftProperty));
+  EXPECT_EQ(computed_style->right(),
+            css_style_rule_2->declared_style_data()->GetPropertyValue(
+                cssom::kRightProperty));
+  EXPECT_EQ(computed_style->background_image(),
+            css_style_rule_2->declared_style_data()->GetPropertyValue(
+                cssom::kBackgroundImageProperty));
+  ASSERT_FALSE(property_key_to_base_url_map.empty());
+  EXPECT_EQ(property_key_to_base_url_map[kBackgroundImageProperty].spec(),
+            "https://www.youtube.com/tv/img");
 }
 
 }  // namespace cssom
