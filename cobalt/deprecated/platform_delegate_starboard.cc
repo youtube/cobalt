b1279560c0eea571e598318b4672ee3074e777c7
Cobalt Team <cobalt-team@google.com>
2016-03-22 15:32:15 -0700

    Implement and Hook-up Starboard PlatformDelegate
    Change-Id: I4c622dbd6b7f38aa804c79539405d71d51a7420e

--- cobalt/deprecated/platform_delegate_starboard.cc
+++ cobalt/deprecated/platform_delegate_starboard.cc
@@ -16,7 +16,9 @@
 #include "cobalt/deprecated/platform_delegate.h"
 
 #include "base/logging.h"
-#include "lbshell/src/lb_globals.h"
+#include "base/memory/scoped_ptr.h"
+#include "base/string_util.h"
+#include "starboard/system.h"
 #include "starboard/types.h"
 
 namespace cobalt {
@@ -28,16 +30,90 @@ class PlatformDelegateStarboard : public PlatformDelegate {
  public:
   PlatformDelegateStarboard();
   ~PlatformDelegateStarboard() OVERRIDE;
-  std::string GetPlatformName() const OVERRIDE { return "Starboard"; }
+
+  std::string GetSystemLanguage() OVERRIDE;
+  bool AreKeysReversed() const OVERRIDE;
+  std::string GetPlatformName() const OVERRIDE;
 };
 
-PlatformDelegateStarboard::PlatformDelegateStarboard() {}
+PlatformDelegateStarboard::PlatformDelegateStarboard() {
+  scoped_array<char> path(new char[SB_FILE_MAX_PATH]);
+  path[0] = '\0';
+
+  if (!SbSystemGetPath(kSbSystemPathSourceDirectory, path.get(),
+                       SB_FILE_MAX_PATH)) {
+    DLOG(FATAL) << "Failed to get path for dir_source_root_";
+  } else {
+    dir_source_root_ = path.get();
+  }
+
+  if (!SbSystemGetPath(kSbSystemPathContentDirectory, path.get(),
+                       SB_FILE_MAX_PATH)) {
+    DLOG(FATAL) << "Failed to get path for game_content_path_";
+  } else {
+    game_content_path_ = path.get();
+  }
+
+  if (!SbSystemGetPath(kSbSystemPathDebugOutputDirectory, path.get(),
+                       SB_FILE_MAX_PATH)) {
+    DLOG(FATAL) << "Failed to get path for screenshot_output_path_";
+  } else {
+    screenshot_output_path_ = path.get();
+    logging_output_path_ = path.get();
+  }
+
+  if (!SbSystemGetPath(kSbSystemPathTempDirectory, path.get(),
+                       SB_FILE_MAX_PATH)) {
+    DLOG(FATAL) << "Failed to get path for temp_path_";
+  } else {
+    temp_path_ = path.get();
+  }
+}
+
 PlatformDelegateStarboard::~PlatformDelegateStarboard() {}
+
+std::string PlatformDelegateStarboard::GetSystemLanguage() {
+  // Get Locale ID (e.g. "en_US.UTF8").
+  std::string locale_id = SbSystemGetLocaleId();
+
+  // Split Locale ID up into components, if there are any.
+  std::vector<std::string> components;
+  size_t count = Tokenize(locale_id, ".", &components);
+  if (count == 0 || components[0].length() == 0) {
+    DLOG(FATAL) << "Unable to get valid locale_id: " << locale_id;
+    return "en-US";
+  }
+
+  // Take only the first component, and replace '_' with '-'.
+  std::string language;
+  ReplaceChars(components[0], "_", "-", &language);
+
+  // We should end up with something like "en-US".
+  return language;
+}
+
+bool PlatformDelegateStarboard::AreKeysReversed() const {
+  return SbSystemHasCapability(kSbSystemCapabilityReversedEnterAndBack);
+}
+
+std::string PlatformDelegateStarboard::GetPlatformName() const {
+  char property[512];
+
+  std::string result;
+  if (!SbSystemGetProperty(kSbSystemPropertyPlatformName, property,
+                           SB_ARRAY_SIZE_INT(property))) {
+    DLOG(FATAL) << "Failed to get kSbSystemPropertyPlatformName.";
+  } else {
+    result = property;
+  }
+
+  return result;
+}
 
 }  // namespace
 
 PlatformDelegate* PlatformDelegate::Create() {
-  return new PlatformDelegateStarboard;
+  return new PlatformDelegateStarboard();
 }
 
 }  // namespace deprecated
