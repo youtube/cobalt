4f7dc142716a694d002308ad0ef69b58725f71bb
Cobalt Team <cobalt-team@google.com>
2016-03-23 18:49:55 -0700

    Implement no-user User API for Linux
    Add a kSbEventTypeUser event type to notify user changes.
    
    Change-Id: I3ed53f268cd075842e20f4c8ef053431bdd8e562

---
+++ starboard/nplb/user_get_current_test.cc
@@ -0,0 +1,37 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/user.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace starboard {
+namespace nplb {
+namespace {
+
+TEST(SbUserGetCurrentTest, SunnyDay) {
+  SbUser users[10] = {0};
+  int result = SbUserGetSignedIn(users, SB_ARRAY_SIZE_INT(users));
+  EXPECT_LE(0, result);
+
+  SbUser current = SbUserGetCurrent();
+  if (result > 0) {
+    EXPECT_NE(kSbUserInvalid, current);
+  } else {
+    EXPECT_EQ(kSbUserInvalid, current);
+  }
+}
+
+}  // namespace
+}  // namespace nplb
+}  // namespace starboard
---
+++ starboard/nplb/user_get_property_test.cc
@@ -0,0 +1,61 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/memory.h"
+#include "starboard/user.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace starboard {
+namespace nplb {
+namespace {
+
+void TestProperty(SbUser user, SbUserPropertyId property_id) {
+  int size = SbUserGetPropertySize(user, property_id);
+  EXPECT_LE(0, size);
+
+  if (size > 0) {
+    char* property = new char[size + 10];
+    SbMemorySet(property, 0, size + 10);
+    EXPECT_TRUE(SbUserGetProperty(user, property_id, property, size));
+    for (int i = 0; i < size - 1; ++i) {
+      EXPECT_NE(property[i], '\0') << "position " << i;
+    }
+    for (int i = size - 1; i < size + 10; ++i) {
+      EXPECT_EQ(property[i], '\0') << "position " << i;
+    }
+    delete[] property;
+  } else {
+    char property[10] = {0};
+    bool result = SbUserGetProperty(user, property_id, property, 10);
+    for (int i = 0; i < size; ++i) {
+      EXPECT_EQ(property[i], '\0') << "position " << i;
+    }
+  }
+}
+
+TEST(SbUserGetPropertyTest, SunnyDay) {
+  SbUser current = SbUserGetCurrent();
+  if (!SbUserIsValid(current)) {
+    // Sadly, we can't really test in this case.
+    return;
+  }
+
+  TestProperty(current, kSbUserPropertyUserName);
+  TestProperty(current, kSbUserPropertyUserId);
+  TestProperty(current, kSbUserPropertyAvatarUrl);
+}
+
+}  // namespace
+}  // namespace nplb
+}  // namespace starboard
