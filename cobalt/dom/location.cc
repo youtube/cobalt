8b4877afb0ded9b717a45fe4a6ba6ad4321d7f70
Cobalt Team <cobalt-team@google.com>
2016-03-30 12:04:08 -0700

    Implement URLUtils and use it on HTMLAnchorElement
    - Separated the URL operation logics from location to URLUtils.
    - Implemented HTMLAnchorElement's URL operations.
    b/27338570
    
    Change-Id: I51ca1cf3b75db70bb286e23ae7898a61b865e0e5

--- cobalt/dom/location.cc
+++ cobalt/dom/location.cc
@@ -16,6 +16,8 @@
 
 #include "cobalt/dom/location.h"
 
+#include "base/bind.h"
+#include "base/compiler_specific.h"
 #include "base/logging.h"
 
 namespace cobalt {
@@ -24,7 +26,8 @@ namespace dom {
 Location::Location(const GURL& url,
                    const base::Callback<void(const GURL&)>& navigation_callback,
                    const csp::SecurityCallback& security_callback)
-    : url_(url),
+    : ALLOW_THIS_IN_INITIALIZER_LIST(url_utils_(
+          url, base::Bind(&Location::Replace, base::Unretained(this)))),
       navigation_callback_(navigation_callback),
       security_callback_(security_callback) {}
 
@@ -33,140 +36,17 @@ void Location::Replace(const std::string& url) {
   // relative to the API base URL specified by the entry settings object, and if
   // that is successful, navigate the browsing context to the specified url with
   // replacement enabled and exceptions enabled.
-  GURL new_url = url_.Resolve(url);
-  Navigate(new_url);
-}
+  GURL new_url = url_utils_.url().Resolve(url);
+  if (new_url.is_valid()) {
+    if (!security_callback_.Run(new_url, false /* did redirect */)) {
+      DLOG(INFO) << "URL " << new_url
+                 << " is rejected by policy, aborting the navigation.";
+      return;
+    }
 
-void Location::Reload() {
-  if (!navigation_callback_.is_null()) {
-    navigation_callback_.Run(GURL());
-  }
-}
-
-std::string Location::href() const { return url_.spec(); }
-
-void Location::set_href(const std::string& href) { Replace(href); }
-
-std::string Location::protocol() const {
-  return url_.is_empty() ? ":" : url_.scheme() + ":";
-}
-
-void Location::set_protocol(const std::string& protocol) {
-  url_parse::Component component;
-  component.len = static_cast<int>(protocol.length());
-  GURL::Replacements replacements;
-  replacements.SetScheme(protocol.c_str(), component);
-  GURL new_url = url_.ReplaceComponents(replacements);
-  Navigate(new_url);
-}
-
-std::string Location::host() const {
-  if (url_.is_empty()) {
-    return "";
-  }
-  return url_.host() + (url_.has_port() ? ":" + url_.port() : "");
-}
-
-void Location::set_host(const std::string& host_and_port) {
-  // Host may include an optional port.
-  std::string host = host_and_port;
-  std::string port;
-  size_t host_end = host.find(':');
-  if (host_end != std::string::npos) {
-    host = host_and_port.substr(0, host_end);
-    port = host_and_port.substr(host_end + 1);
-  }
-
-  GURL::Replacements replacements;
-  replacements.SetHostStr(host);
-  if (port.length() > 0) {
-    replacements.SetPortStr(port);
-  } else {
-    replacements.ClearPort();
-  }
-  GURL new_url = url_.ReplaceComponents(replacements);
-  Navigate(new_url);
-}
-
-std::string Location::hostname() const {
-  return url_.is_empty() ? "" : url_.host();
-}
-
-void Location::set_hostname(const std::string& hostname) {
-  url_parse::Component component;
-  component.len = static_cast<int>(hostname.length());
-  GURL::Replacements replacements;
-  replacements.SetHost(hostname.c_str(), component);
-  GURL new_url = url_.ReplaceComponents(replacements);
-  Navigate(new_url);
-}
-
-std::string Location::port() const {
-  return url_.is_empty() ? "" : url_.port();
-}
-
-void Location::set_port(const std::string& port) {
-  url_parse::Component component;
-  component.len = static_cast<int>(port.length());
-  GURL::Replacements replacements;
-  replacements.SetPort(port.c_str(), component);
-  GURL new_url = url_.ReplaceComponents(replacements);
-  Navigate(new_url);
-}
-
-std::string Location::pathname() const {
-  return url_.is_empty() ? "" : url_.path();
-}
-
-void Location::set_pathname(const std::string& pathname) {
-  url_parse::Component component;
-  component.len = static_cast<int>(pathname.length());
-  GURL::Replacements replacements;
-  replacements.SetPath(pathname.c_str(), component);
-  GURL new_url = url_.ReplaceComponents(replacements);
-  Navigate(new_url);
-}
-
-std::string Location::hash() const {
-  return url_.ref().empty() ? "" : "#" + url_.ref();
-}
-
-void Location::set_hash(const std::string& hash) {
-  url_parse::Component component;
-  component.len = static_cast<int>(hash.length());
-  GURL::Replacements replacements;
-  replacements.SetRef(hash.c_str(), component);
-  GURL new_url = url_.ReplaceComponents(replacements);
-  Navigate(new_url);
-}
-
-std::string Location::search() const {
-  return url_.query().empty() ? "" : "?" + url_.query();
-}
-
-void Location::set_search(const std::string& search) {
-  url_parse::Component component;
-  component.len = static_cast<int>(search.length());
-  GURL::Replacements replacements;
-  replacements.SetQuery(search.c_str(), component);
-  GURL new_url = url_.ReplaceComponents(replacements);
-  Navigate(new_url);
-}
-
-void Location::Navigate(const GURL& url) {
-  // Custom, not in any spec.
-  if (!url.is_valid()) {
-    DLOG(INFO) << "URL " << url << " is not valid, aborting the navigation.";
-    return;
-  }
-  if (!security_callback_.Run(url, false /* did redirect */)) {
-    DLOG(INFO) << "URL " << url
-               << " is rejected by policy, aborting the navigation.";
-    return;
-  }
-
-  if (!navigation_callback_.is_null()) {
-    navigation_callback_.Run(url);
+    if (!navigation_callback_.is_null()) {
+      navigation_callback_.Run(new_url);
+    }
   }
 }
 
