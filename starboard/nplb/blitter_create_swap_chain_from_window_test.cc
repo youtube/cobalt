b5a9327cd37e97a8f3d0a4792efaf103f75bedbf
Cobalt Team <cobalt-team@google.com>
2016-04-26 16:27:12 -0700

    Add NPLB tests for the Blitter API.
    No pixel tests are introduced yet, however.
    
    Rename SbBlitterCreateSurfaceWithRenderTarget() to
    SbBlitterCreateRenderTargetSurface() since it sounds more
    natural that way.
    
    b/28269737
    
    Change-Id: I4728e5a339bdf700c58413979c84124eecdcf251

---
+++ starboard/nplb/blitter_create_swap_chain_from_window_test.cc
@@ -0,0 +1,99 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/blitter.h"
+#include "starboard/window.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+#if SB_HAS(BLITTER)
+
+namespace starboard {
+namespace nplb {
+namespace {
+
+const int kALot = 10;
+
+TEST(SbBlitterCreateSwapChainFromWindowTest, SunnyDay) {
+  SbWindowOptions options;
+  SbWindowSetDefaultOptions(&options);
+  options.size.width = 1920;
+  options.size.height = 1080;
+  SbWindow window = SbWindowCreate(NULL);
+  ASSERT_TRUE(SbWindowIsValid(window));
+
+  SbBlitterDevice device = SbBlitterCreateDefaultDevice();
+  ASSERT_TRUE(SbBlitterIsDeviceValid(device));
+
+  SbBlitterSwapChain swap_chain =
+      SbBlitterCreateSwapChainFromWindow(device, window);
+  EXPECT_TRUE(SbBlitterIsSwapChainValid(swap_chain));
+
+  EXPECT_TRUE(SbBlitterDestroySwapChain(swap_chain));
+  EXPECT_TRUE(SbBlitterDestroyDevice(device));
+  EXPECT_TRUE(SbWindowDestroy(window));
+}
+
+TEST(SbBlitterCreateSwapChainFromWindowTest, SunnyDayMultipleTimes) {
+  SbWindowOptions options;
+  SbWindowSetDefaultOptions(&options);
+  options.size.width = 1920;
+  options.size.height = 1080;
+  SbWindow window = SbWindowCreate(NULL);
+  ASSERT_TRUE(SbWindowIsValid(window));
+
+  SbBlitterDevice device = SbBlitterCreateDefaultDevice();
+  ASSERT_TRUE(SbBlitterIsDeviceValid(device));
+
+  for (int i = 0; i < kALot; ++i) {
+    SbBlitterSwapChain swap_chain =
+        SbBlitterCreateSwapChainFromWindow(device, window);
+    EXPECT_TRUE(SbBlitterIsSwapChainValid(swap_chain));
+    EXPECT_TRUE(SbBlitterDestroySwapChain(swap_chain));
+  }
+
+  EXPECT_TRUE(SbBlitterDestroyDevice(device));
+  EXPECT_TRUE(SbWindowDestroy(window));
+}
+
+TEST(SbBlitterCreateSwapChainFromWindowTest, RainyDayBadWindow) {
+  SbBlitterDevice device = SbBlitterCreateDefaultDevice();
+  ASSERT_TRUE(SbBlitterIsDeviceValid(device));
+
+  SbBlitterSwapChain swap_chain =
+      SbBlitterCreateSwapChainFromWindow(device, kSbWindowInvalid);
+  EXPECT_FALSE(SbBlitterIsSwapChainValid(swap_chain));
+
+  EXPECT_TRUE(SbBlitterDestroyDevice(device));
+}
+
+TEST(SbBlitterCreateSwapChainFromWindowTest, RainyDayBadDevice) {
+  SbWindowOptions options;
+  SbWindowSetDefaultOptions(&options);
+  options.size.width = 1920;
+  options.size.height = 1080;
+  SbWindow window = SbWindowCreate(NULL);
+  ASSERT_TRUE(SbWindowIsValid(window));
+
+  SbBlitterSwapChain swap_chain =
+      SbBlitterCreateSwapChainFromWindow(kSbBlitterInvalidDevice, window);
+  EXPECT_FALSE(SbBlitterIsSwapChainValid(swap_chain));
+
+  EXPECT_TRUE(SbWindowDestroy(window));
+}
+
+}  // namespace
+}  // namespace nplb
+}  // namespace starboard
+
+#endif  // SB_HAS(BLITTER)
---
+++ starboard/nplb/blitter_get_render_target_from_swap_chain_test.cc
@@ -0,0 +1,93 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/blitter.h"
+#include "starboard/window.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+#if SB_HAS(BLITTER)
+
+namespace starboard {
+namespace nplb {
+namespace {
+
+const int kALot = 10;
+
+TEST(SbBlitterGetRenderTargetFromSwapChainTest, SunnyDay) {
+  SbWindowOptions options;
+  SbWindowSetDefaultOptions(&options);
+  options.size.width = 1920;
+  options.size.height = 1080;
+  SbWindow window = SbWindowCreate(NULL);
+  ASSERT_TRUE(SbWindowIsValid(window));
+
+  SbBlitterDevice device = SbBlitterCreateDefaultDevice();
+  ASSERT_TRUE(SbBlitterIsDeviceValid(device));
+
+  SbBlitterSwapChain swap_chain =
+      SbBlitterCreateSwapChainFromWindow(device, window);
+  ASSERT_TRUE(SbBlitterIsSwapChainValid(swap_chain));
+
+  SbBlitterRenderTarget render_target =
+      SbBlitterGetRenderTargetFromSwapChain(swap_chain);
+  EXPECT_TRUE(SbBlitterIsRenderTargetValid(render_target));
+
+  EXPECT_TRUE(SbBlitterDestroySwapChain(swap_chain));
+  EXPECT_TRUE(SbBlitterDestroyDevice(device));
+  EXPECT_TRUE(SbWindowDestroy(window));
+}
+
+TEST(SbBlitterGetRenderTargetFromSwapChainTest, SunnyDayCanDraw) {
+  SbWindowOptions options;
+  SbWindowSetDefaultOptions(&options);
+  options.size.width = 1920;
+  options.size.height = 1080;
+  SbWindow window = SbWindowCreate(NULL);
+  ASSERT_TRUE(SbWindowIsValid(window));
+
+  SbBlitterDevice device = SbBlitterCreateDefaultDevice();
+  ASSERT_TRUE(SbBlitterIsDeviceValid(device));
+
+  SbBlitterSwapChain swap_chain =
+      SbBlitterCreateSwapChainFromWindow(device, window);
+  ASSERT_TRUE(SbBlitterIsSwapChainValid(swap_chain));
+
+  SbBlitterRenderTarget render_target =
+      SbBlitterGetRenderTargetFromSwapChain(swap_chain);
+  ASSERT_TRUE(SbBlitterIsRenderTargetValid(render_target));
+
+  SbBlitterContext context = SbBlitterCreateContext(device);
+  ASSERT_TRUE(SbBlitterIsContextValid(context));
+
+  EXPECT_TRUE(SbBlitterSetRenderTarget(context, render_target));
+  EXPECT_TRUE(SbBlitterFillRect(context, SbBlitterMakeRect(0, 0, 1920, 1080)));
+  EXPECT_TRUE(SbBlitterFlushContext(context));
+
+  EXPECT_TRUE(SbBlitterDestroyContext(context));
+  EXPECT_TRUE(SbBlitterDestroySwapChain(swap_chain));
+  EXPECT_TRUE(SbBlitterDestroyDevice(device));
+  EXPECT_TRUE(SbWindowDestroy(window));
+}
+
+TEST(SbBlitterGetRenderTargetFromSwapChainTest, RainyDayInvalidSwapChain) {
+  SbBlitterRenderTarget render_target =
+      SbBlitterGetRenderTargetFromSwapChain(kSbBlitterInvalidSwapChain);
+  EXPECT_FALSE(SbBlitterIsRenderTargetValid(render_target));
+}
+
+}  // namespace
+}  // namespace nplb
+}  // namespace starboard
+
+#endif  // SB_HAS(BLITTER)
