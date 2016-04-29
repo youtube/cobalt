a7f2400ae7111edae983d3e9f4928fa805fe5e1f
Cobalt Team <cobalt-team@google.com>
2016-04-28 17:31:26 -0700

    Pass a FakeScriptRunner into the HTMLElementContext for webdriver tests
    b/28431367
    
    Change-Id: I843bdd3a91ea41900e3560eda6d78e7576e3a389

---
+++ cobalt/script/fake_script_runner.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef COBALT_SCRIPT_FAKE_SCRIPT_RUNNER_H_
+#define COBALT_SCRIPT_FAKE_SCRIPT_RUNNER_H_
+
+#include <string>
+
+#include "cobalt/base/source_location.h"
+#include "cobalt/script/fake_global_object_proxy.h"
+#include "cobalt/script/script_runner.h"
+
+namespace cobalt {
+namespace script {
+
+class FakeScriptRunner : public ScriptRunner {
+ public:
+  FakeScriptRunner() : fake_global_object_proxy_(new FakeGlobalObjectProxy()) {}
+  std::string Execute(
+      const std::string& /*script_utf8*/,
+      const base::SourceLocation& /*script_location*/) OVERRIDE {
+    return "";
+  }
+  GlobalObjectProxy* GetGlobalObjectProxy() const OVERRIDE {
+    return fake_global_object_proxy_.get();
+  }
+
+ private:
+  scoped_refptr<FakeGlobalObjectProxy> fake_global_object_proxy_;
+};
+
+}  // namespace script
+}  // namespace cobalt
+
+#endif  // COBALT_SCRIPT_FAKE_SCRIPT_RUNNER_H_
