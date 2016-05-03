6ef022052273c6dbb2d4f877e078199688016af4
Cobalt Team <cobalt-team@google.com>
2016-05-03 15:01:31 -0700

    Handle NotSupported attribute in code generation pipeline
    - Ensure that this attribute goes into the interfaces_info object
      so that we can ensure that constructors for such interfaces are
      not generated
    
    Change-Id: I32e86ff40aa5a85dba9e8b410ed678b2d5e366b5

--- third_party/blink/Source/bindings/scripts/generate_global_constructors.py
+++ third_party/blink/Source/bindings/scripts/generate_global_constructors.py
@@ -94,8 +94,8 @@ def record_global_constructors(idl_filename):
 
 def generate_global_constructors_list(interface_name, extended_attributes):
     extended_attributes_list = [
-            name + '=' + extended_attributes[name]
-            for name in 'Conditional', 'PerContextEnabled', 'RuntimeEnabled'
+            name + ('=' + extended_attributes[name] if extended_attributes[name] else "")
+            for name in 'Conditional', 'PerContextEnabled', 'RuntimeEnabled', 'NotSupported'
             if name in extended_attributes]
     if extended_attributes_list:
         extended_string = '[%s] ' % ', '.join(extended_attributes_list)
