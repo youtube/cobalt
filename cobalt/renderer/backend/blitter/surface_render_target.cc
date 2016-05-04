d7ec4d2885171f6ed3ef708f4c153f800688ee24
Cobalt Team <cobalt-team@google.com>
2016-05-04 15:21:13 -0700

    Add (software) support for rendering via Starboard Blitter.
    Do not build Skia GPU components if EGL/GLES is not available.
    
    Some work still needs to be done to get renderer_tests completely
    passing.
    
    Change-Id: I4021d579fa0b99a79e0ee77f2306259aafe2f44e

---
+++ cobalt/renderer/backend/blitter/surface_render_target.cc
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cobalt/renderer/backend/blitter/surface_render_target.h"
+
+#include "starboard/blitter.h"
+
+namespace cobalt {
+namespace renderer {
+namespace backend {
+
+SurfaceRenderTargetBlitter::SurfaceRenderTargetBlitter(
+    SbBlitterDevice device, const math::Size& dimensions) {
+  surface_ = SbBlitterCreateRenderTargetSurface(device, dimensions.width(),
+                                                dimensions.height(),
+                                                kSbBlitterSurfaceFormatRGBA8);
+  CHECK(SbBlitterIsSurfaceValid(surface_));
+
+  render_target_ = SbBlitterGetRenderTargetFromSurface(surface_);
+  CHECK(SbBlitterIsRenderTargetValid(render_target_));
+
+  surface_info_.size = dimensions;
+  surface_info_.format = SurfaceInfo::kFormatRGBA8;
+}
+
+SurfaceRenderTargetBlitter::~SurfaceRenderTargetBlitter() {
+  SbBlitterDestroySurface(surface_);
+}
+
+const SurfaceInfo& SurfaceRenderTargetBlitter::GetSurfaceInfo() {
+  return surface_info_;
+}
+
+SbBlitterRenderTarget SurfaceRenderTargetBlitter::GetSbRenderTarget() const {
+  return render_target_;
+}
+
+}  // namespace backend
+}  // namespace renderer
+}  // namespace cobalt
