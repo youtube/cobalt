35da6af93803aab7c15d2c64579e3afc86c864e4
Cobalt Team <cobalt-team@google.com>
2016-05-11 16:04:56 -0700

    Implement input for RasPi
    This adds /dev/input-based support for Rasbperry Pi. This still needs to
    do modifier processing, but the API needs to change for that, so will be
    addressed in a future CL.
    
    b/28602225
    
    Change-Id: I8f0c944486b04e0055a1d4f26f6d9a08f20022de

--- starboard/raspi/shared/application_dispmanx.h
+++ starboard/raspi/shared/application_dispmanx.h
@@ -21,6 +21,7 @@
 
 #include "starboard/configuration.h"
 #include "starboard/shared/internal_only.h"
+#include "starboard/shared/linux/dev_input/dev_input.h"
 #include "starboard/shared/starboard/application.h"
 #include "starboard/shared/starboard/queue_application.h"
 #include "starboard/types.h"
@@ -54,7 +55,7 @@ class ApplicationDispmanx
   // --- QueueApplication overrides ---
   bool MayHaveSystemEvents() SB_OVERRIDE;
   Event* PollNextSystemEvent() SB_OVERRIDE;
-  Event* WaitForSystemEventWithTimeout(SbTime time) SB_OVERRIDE;
+  Event* WaitForSystemEventWithTimeout(SbTime duration) SB_OVERRIDE;
   void WakeSystemEventWait() SB_OVERRIDE;
 
  private:
@@ -67,9 +68,14 @@ class ApplicationDispmanx
   // Shuts X down.
   void ShutdownDispmanx();
 
+  // The DISPMANX display.
   DISPMANX_DISPLAY_HANDLE_T display_;
 
+  // The single open window, if any.
   SbWindow window_;
+
+  // The /dev/input input handler. Only set when there is an open window.
+  ::starboard::shared::dev_input::DevInput* input_;
 };
 
 }  // namespace shared
