e6842fb30b49f95e75918a239b50313801094ae2
Cobalt Team <cobalt-team@google.com>
2016-05-11 18:30:34 -0700

    Add speech recognition stubs
    1. Speech recogniton manager is used for interacting with
       microphone, speech recognition service and audio encoder.
    2. Speech recognizer is owned by speech recognition manager to
       interact with Google speech recognition service, parse recognition
       results and create speech recognition event.
    3. An abstract class Mic is used for creating platform specific
       microphone classes. Each microphone platform specific class manages
       its own mic audio handling.
    
    b/27696921
    
    Change-Id: Ib68960f2c603d59ff83cec2fc148080ba57645b3

---
+++ cobalt/speech/speech_recognition_manager.cc
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cobalt/speech/speech_recognition_manager.h"
+
+#include "base/bind.h"
+
+namespace cobalt {
+namespace speech {
+
+namespace {
+const int kSampleRate = 16000;
+}  // namespace
+
+SpeechRecognitionManager::SpeechRecognitionManager(
+    loader::FetcherFactory* fetcher_factory)
+    : ALLOW_THIS_IN_INITIALIZER_LIST(weak_ptr_factory_(this)),
+      weak_this_(weak_ptr_factory_.GetWeakPtr()),
+      main_message_loop_(base::MessageLoopProxy::current()),
+      recognizer_(fetcher_factory),
+      ALLOW_THIS_IN_INITIALIZER_LIST(mic_(Mic::Create(
+          kSampleRate, base::Bind(&SpeechRecognitionManager::OnDataReceived,
+                                  base::Unretained(this)),
+          base::Bind(&SpeechRecognitionManager::OnDataCompletion,
+                     base::Unretained(this))))),
+      started_(false) {}
+
+SpeechRecognitionManager::~SpeechRecognitionManager() { Stop(); }
+
+void SpeechRecognitionManager::Start(const SpeechRecognitionConfig& config) {
+  DCHECK(main_message_loop_->BelongsToCurrentThread());
+  if (started_) {
+    // Already started.
+    return;
+  }
+
+  recognizer_.Start(config);
+  mic_->Start();
+  started_ = true;
+}
+
+void SpeechRecognitionManager::Stop() {
+  DCHECK(main_message_loop_->BelongsToCurrentThread());
+  if (!started_) {
+    // Not started.
+    return;
+  }
+
+  mic_->Stop();
+  recognizer_.Stop();
+  started_ = false;
+}
+
+void SpeechRecognitionManager::OnDataReceived(scoped_ptr<AudioBus> audio_bus) {
+  if (!main_message_loop_->BelongsToCurrentThread()) {
+    // Called from mic thread.
+    main_message_loop_->PostTask(
+        FROM_HERE, base::Bind(&SpeechRecognitionManager::OnDataReceived,
+                              weak_this_, base::Passed(&audio_bus)));
+    return;
+  }
+
+  // TODO(***REMOVED***): Encode audio data, and then send it to recognizer. After
+  // receiving the recognition result from recognizer, fire a speech recognition
+  // event.
+}
+
+void SpeechRecognitionManager::OnDataCompletion() {
+  if (!main_message_loop_->BelongsToCurrentThread()) {
+    // Called from mic thread.
+    main_message_loop_->PostTask(
+        FROM_HERE,
+        base::Bind(&SpeechRecognitionManager::OnDataCompletion, weak_this_));
+    return;
+  }
+
+  // TODO(***REMOVED***): Handle the case that no audio data would be received
+  // afterwards.
+}
+
+}  // namespace speech
+}  // namespace cobalt
