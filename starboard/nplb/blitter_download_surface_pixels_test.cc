baff28a865036a3538812fd13a7b2c828cfdedd2
Cobalt Team <cobalt-team@google.com>
2016-06-03 11:39:07 -0700

    Generalize surface pixel data downloading from RGBA to any format.
    Change-Id: I43b7a1008d851da65e40d95f0e9edc3460d0ae23

---
+++ starboard/nplb/blitter_download_surface_pixels_test.cc
@@ -0,0 +1,204 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/blitter.h"
+#include "starboard/nplb/blitter_helpers.h"
+#include "starboard/window.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+#if SB_HAS(BLITTER)
+
+namespace starboard {
+namespace nplb {
+namespace {
+
+const int kWidth = 32;
+const int kHeight = 32;
+
+// Tests that we can download pixel data from surfaces created via
+// SbBlitterCreateRenderTargetSurface().
+TEST(SbBlitterDownloadSurfacePixelsTest, SunnyDayRGBARenderTargetSurface) {
+  SbBlitterDevice device = SbBlitterCreateDefaultDevice();
+  ASSERT_TRUE(SbBlitterIsDeviceValid(device));
+
+  if (!SbBlitterIsSurfaceFormatSupportedByRenderTargetSurface(
+          device, kSbBlitterSurfaceFormatRGBA8)) {
+    // If the device doesn't support RGBA surfaces as render targets, we cannot
+    // proceed with this test.
+    return;
+  }
+  SbBlitterSurface surface = SbBlitterCreateRenderTargetSurface(
+      device, kWidth, kHeight, kSbBlitterSurfaceFormatRGBA8);
+  ASSERT_TRUE(SbBlitterIsSurfaceValid(surface));
+
+  SbBlitterRenderTarget render_target =
+      SbBlitterGetRenderTargetFromSurface(surface);
+  ASSERT_TRUE(SbBlitterIsRenderTargetValid(render_target));
+
+  SbBlitterContext context = SbBlitterCreateContext(device);
+  ASSERT_TRUE(SbBlitterIsContextValid(context));
+
+  ASSERT_TRUE(SbBlitterSetRenderTarget(context, render_target));
+
+  // Fill the top half of the surface to transparent green, and the bottom half
+  // to opaque white.
+  EXPECT_TRUE(SbBlitterSetBlending(context, false));
+  EXPECT_TRUE(SbBlitterSetColor(context, SbBlitterColorFromRGBA(0, 1, 2, 255)));
+  EXPECT_TRUE(
+      SbBlitterFillRect(context, SbBlitterMakeRect(0, 0, kWidth, kHeight)));
+  EXPECT_TRUE(SbBlitterFlushContext(context));
+
+  uint8_t* downloaded_pixels = new uint8_t[kWidth * kHeight * 4];
+  EXPECT_TRUE(SbBlitterDownloadSurfacePixels(
+      surface, kSbBlitterPixelDataFormatRGBA8,
+      kSbBlitterAlphaFormatPremultiplied, kWidth * 4,
+      static_cast<void*>(downloaded_pixels)));
+
+  bool all_correct_color = true;
+  for (int pixel = 0; pixel < kWidth * kHeight; ++pixel) {
+    uint8_t* downloaded_pixel = downloaded_pixels + pixel * 4;
+    all_correct_color &= downloaded_pixel[0] == 0;
+    all_correct_color &= downloaded_pixel[1] == 1;
+    all_correct_color &= downloaded_pixel[2] == 2;
+    all_correct_color &= downloaded_pixel[3] == 255;
+  }
+  EXPECT_TRUE(all_correct_color);
+
+  delete[] downloaded_pixels;
+
+  EXPECT_TRUE(SbBlitterDestroyContext(context));
+  EXPECT_TRUE(SbBlitterDestroySurface(surface));
+  EXPECT_TRUE(SbBlitterDestroyDevice(device));
+}
+
+// Tests that we can download pixel data from surfaces created via
+// SbBlitterCreateSurfaceFromPixelData().
+TEST(SbBlitterDownloadSurfacePixelsTest, SunnyDayPixelDataSurface) {
+  const int kWidth = 32;
+  const int kHeight = 32;
+  SbBlitterDevice device = SbBlitterCreateDefaultDevice();
+
+  // A list of formats to test, assuming they are supported by the device.
+  // This test only supports permutations of 32-bit RGBA formats.
+  SbBlitterPixelDataFormat test_formats[] = {
+      kSbBlitterPixelDataFormatARGB8, kSbBlitterPixelDataFormatBGRA8,
+      kSbBlitterPixelDataFormatRGBA8,
+  };
+
+  for (size_t i = 0; i < SB_ARRAY_SIZE(test_formats); ++i) {
+    for (int j = 0; j < kSbBlitterNumAlphaFormats; ++j) {
+      if (!SbBlitterIsPixelFormatSupportedByPixelData(
+              device, test_formats[i], static_cast<SbBlitterAlphaFormat>(j))) {
+        continue;
+      }
+
+      SbBlitterPixelData pixel_data =
+          SbBlitterCreatePixelData(device, kWidth, kHeight, test_formats[i],
+                                   static_cast<SbBlitterAlphaFormat>(j));
+      ASSERT_TRUE(SbBlitterIsPixelDataValid(pixel_data));
+
+      int pitch_in_bytes = SbBlitterGetPixelDataPitchInBytes(pixel_data);
+      ASSERT_NE(-1, pitch_in_bytes);
+
+      uint8_t* pixels =
+          static_cast<uint8_t*>(SbBlitterGetPixelDataPointer(pixel_data));
+      ASSERT_TRUE(pixels);
+
+      // Fill the pixels with dummy data.
+      for (int y = 0; y < kHeight; ++y) {
+        for (int x = 0; x < kWidth; ++x) {
+          pixels[x * 4 + 0] = 0;
+          pixels[x * 4 + 1] = 1;
+          pixels[x * 4 + 2] = 2;
+          pixels[x * 4 + 3] = 3;
+        }
+        pixels += pitch_in_bytes;
+      }
+
+      // Create the surface.
+      SbBlitterSurface surface =
+          SbBlitterCreateSurfaceFromPixelData(device, pixel_data);
+      EXPECT_TRUE(SbBlitterIsSurfaceValid(surface));
+
+      // Now download the data and test that the downloaded data is as we
+      // expect it to be.
+      uint8_t* downloaded_pixels = new uint8_t[kWidth * kHeight * 4];
+      EXPECT_TRUE(SbBlitterDownloadSurfacePixels(
+          surface, kSbBlitterPixelDataFormatRGBA8,
+          kSbBlitterAlphaFormatPremultiplied, kWidth * 4, downloaded_pixels));
+
+      bool all_correct_color = true;
+      for (int pixel = 0; pixel < kWidth * kHeight; ++pixel) {
+        uint8_t* downloaded_pixel = downloaded_pixels + pixel * 4;
+
+        // Take into account the fact that the pixels may have been swizzeled
+        // from their input format in order for them to be output as RGBA.
+        switch (test_formats[i]) {
+          case kSbBlitterPixelDataFormatBGRA8: {
+            all_correct_color &= downloaded_pixel[0] == 2;
+            all_correct_color &= downloaded_pixel[1] == 1;
+            all_correct_color &= downloaded_pixel[2] == 0;
+            all_correct_color &= downloaded_pixel[3] == 3;
+          } break;
+          case kSbBlitterPixelDataFormatARGB8: {
+            all_correct_color &= downloaded_pixel[0] == 1;
+            all_correct_color &= downloaded_pixel[1] == 2;
+            all_correct_color &= downloaded_pixel[2] == 3;
+            all_correct_color &= downloaded_pixel[3] == 0;
+          } break;
+          case kSbBlitterPixelDataFormatRGBA8: {
+            all_correct_color &= downloaded_pixel[0] == 0;
+            all_correct_color &= downloaded_pixel[1] == 1;
+            all_correct_color &= downloaded_pixel[2] == 2;
+            all_correct_color &= downloaded_pixel[3] == 3;
+          } break;
+          default: { SB_NOTREACHED(); }
+        }
+      }
+      EXPECT_TRUE(all_correct_color);
+
+      delete[] downloaded_pixels;
+
+      EXPECT_TRUE(SbBlitterDestroySurface(surface));
+    }
+  }
+
+  ASSERT_TRUE(SbBlitterIsDeviceValid(device));
+  EXPECT_TRUE(SbBlitterDestroyDevice(device));
+}
+
+TEST(SbBlitterDownloadSurfacePixelsTest, RainyDayInvalidSurface) {
+  const int kWidth = 32;
+  const int kHeight = 32;
+  uint32_t* pixel_data = new uint32_t[kWidth * kHeight];
+  EXPECT_FALSE(SbBlitterDownloadSurfacePixels(
+      kSbBlitterInvalidSurface, kSbBlitterPixelDataFormatRGBA8,
+      kSbBlitterAlphaFormatPremultiplied, kWidth * 4, pixel_data));
+  delete[] pixel_data;
+}
+
+TEST(SbBlitterDownloadSurfacePixelsTest, RainyDayNullDataPointer) {
+  const int kWidth = 32;
+  const int kHeight = 32;
+  ContextTestEnvironment env(kWidth, kHeight);
+  EXPECT_FALSE(SbBlitterDownloadSurfacePixels(
+      env.surface(), kSbBlitterPixelDataFormatRGBA8,
+      kSbBlitterAlphaFormatPremultiplied, kWidth * 4, NULL));
+}
+
+}  // namespace
+}  // namespace nplb
+}  // namespace starboard
+
+#endif  // SB_HAS(BLITTER)
---
+++ starboard/shared/directfb/blitter_download_surface_pixels.cc
@@ -0,0 +1,178 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <directfb.h>
+
+#include "starboard/blitter.h"
+#include "starboard/log.h"
+#include "starboard/memory.h"
+#include "starboard/shared/directfb/blitter_internal.h"
+
+namespace {
+int WordToByteOrder(int bytes_per_pixel, int byte_index) {
+// Since DirectFB stores color in word-order but we wish to output in
+// byte-order fomat, we perform that conversion here.
+#if SB_IS_LITTLE_ENDIAN
+  return bytes_per_pixel - 1 - byte_index;
+#else
+  return byte_index;
+#endif
+}
+
+void SwizzlePixels(void* in_data,
+                   void* out_data,
+                   int width,
+                   int height,
+                   int in_bytes_per_pixel,
+                   int in_pitch_in_bytes,
+                   int out_pitch_in_bytes,
+                   int channel_1,
+                   int channel_2,
+                   int channel_3,
+                   int channel_4) {
+  uint8_t* in_data_bytes = static_cast<uint8_t*>(in_data);
+  uint8_t* out_data_bytes = static_cast<uint8_t*>(out_data);
+
+  bool needs_swizzle = true;
+  if (WordToByteOrder(in_bytes_per_pixel, channel_1) == 0 &&
+      WordToByteOrder(in_bytes_per_pixel, channel_2) == 1 &&
+      WordToByteOrder(in_bytes_per_pixel, channel_3) == 2 &&
+      WordToByteOrder(in_bytes_per_pixel, channel_4) == 3) {
+    SB_DCHECK(in_bytes_per_pixel == 4);
+    needs_swizzle = false;
+  }
+
+  for (int y = 0; y < height; ++y) {
+    if (needs_swizzle) {
+      for (int x = 0; x < width; ++x) {
+        out_data_bytes[0] =
+            in_data_bytes[WordToByteOrder(in_bytes_per_pixel, channel_1)];
+        out_data_bytes[1] =
+            in_data_bytes[WordToByteOrder(in_bytes_per_pixel, channel_2)];
+        out_data_bytes[2] =
+            in_data_bytes[WordToByteOrder(in_bytes_per_pixel, channel_3)];
+        out_data_bytes[3] =
+            in_data_bytes[WordToByteOrder(in_bytes_per_pixel, channel_4)];
+
+        out_data_bytes += 4;
+        in_data_bytes += in_bytes_per_pixel;
+      }
+
+      // Increment through the gab between the input data pitch and its
+      // width * bpp.
+      in_data_bytes += in_pitch_in_bytes - width * in_bytes_per_pixel;
+      out_data_bytes += out_pitch_in_bytes - width * 4;
+    } else {
+      SbMemoryCopy(out_data_bytes, in_data_bytes, width * 4);
+      in_data_bytes += in_pitch_in_bytes;
+      out_data_bytes += out_pitch_in_bytes;
+    }
+  }
+}
+}  // namespace
+
+bool SbBlitterDownloadSurfacePixels(SbBlitterSurface surface,
+                                    SbBlitterPixelDataFormat pixel_format,
+                                    SbBlitterAlphaFormat alpha_format,
+                                    int pitch_in_bytes,
+                                    void* out_pixel_data) {
+  if (!SbBlitterIsSurfaceValid(surface)) {
+    SB_DLOG(ERROR) << __FUNCTION__ << ": Invalid surface.";
+    return false;
+  }
+  if (out_pixel_data == NULL) {
+    SB_DLOG(ERROR) << __FUNCTION__ << ": |out_pixel_data| cannot be NULL.";
+    return false;
+  }
+  if (!SbBlitterIsPixelFormatSupportedByDownloadSurfacePixels(
+          surface, pixel_format, alpha_format)) {
+    SB_DLOG(ERROR) << __FUNCTION__ << ": Unsupported pixel format.";
+    return false;
+  }
+  if (pitch_in_bytes <
+      surface->info.width * SbBlitterBytesPerPixelForFormat(pixel_format)) {
+    SB_DLOG(ERROR)
+        << __FUNCTION__
+        << ": Output pitch must be at least as large as (width * BPP).";
+    return false;
+  }
+
+  {
+    // Wait for all previously flushed draw calls to be executed.
+    starboard::ScopedLock lock(surface->device->mutex);
+    if (surface->device->dfb->WaitIdle(surface->device->dfb) != DFB_OK) {
+      SB_DLOG(ERROR) << __FUNCTION__ << ": WaitIdle() failed.";
+      return false;
+    }
+  }
+
+  IDirectFBSurface* dfb_surface = surface->surface;
+
+  DFBSurfacePixelFormat surface_pixel_format;
+  dfb_surface->GetPixelFormat(dfb_surface, &surface_pixel_format);
+
+  // Lock the surface's pixels and then copy them out into the user's provided
+  // memory.  Swizzel the pixel's bytes as we copy them out, if necessary.
+  void* data;
+  int surface_pitch_in_bytes;
+  if (dfb_surface->Lock(dfb_surface, DSLF_READ, &data,
+                        &surface_pitch_in_bytes) != DFB_OK) {
+    SB_DLOG(ERROR) << __FUNCTION__ << ": Error calling Lock().";
+    return false;
+  }
+
+  SB_DCHECK(alpha_format == kSbBlitterAlphaFormatPremultiplied);
+
+  switch (DFB_PIXELFORMAT_INDEX(surface_pixel_format)) {
+    case DFB_PIXELFORMAT_INDEX(DSPF_ARGB): {
+      switch (pixel_format) {
+        case kSbBlitterPixelDataFormatARGB8: {
+          SwizzlePixels(data, out_pixel_data, surface->info.width,
+                        surface->info.height, 4, surface_pitch_in_bytes,
+                        pitch_in_bytes, 0, 1, 2, 3);
+        } break;
+        case kSbBlitterPixelDataFormatBGRA8: {
+          SwizzlePixels(data, out_pixel_data, surface->info.width,
+                        surface->info.height, 4, surface_pitch_in_bytes,
+                        pitch_in_bytes, 3, 2, 1, 0);
+        } break;
+        case kSbBlitterPixelDataFormatRGBA8: {
+          SwizzlePixels(data, out_pixel_data, surface->info.width,
+                        surface->info.height, 4, surface_pitch_in_bytes,
+                        pitch_in_bytes, 1, 2, 3, 0);
+        } break;
+        default: { SB_NOTREACHED(); }
+      }
+    } break;
+
+    case DFB_PIXELFORMAT_INDEX(DSPF_A8): {
+      // Convert A8 to either ARGB, BGRA, or RGBA.
+      SwizzlePixels(data, out_pixel_data, surface->info.width,
+                    surface->info.height, 1, surface_pitch_in_bytes,
+                    pitch_in_bytes, 0, 0, 0, 0);
+    } break;
+
+    default: {
+      SB_DLOG(ERROR) << __FUNCTION__ << ": Unsupported pixel format index ("
+                     << DFB_PIXELFORMAT_INDEX(surface_pixel_format) << ").";
+      dfb_surface->Unlock(dfb_surface);
+      return false;
+    }
+  }
+
+  // Okay the copy is complete, unlock the pixels and carry on.
+  dfb_surface->Unlock(dfb_surface);
+
+  return true;
+}
