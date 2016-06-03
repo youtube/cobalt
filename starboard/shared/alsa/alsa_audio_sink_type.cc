a5e11bb2d028a518a53f6fac383bc86b094d3646
Xiaoming Shi <xiaomings@google.com>
2016-06-02 19:32:26 -0700

    Refactor AlsaAudioSink.
    No longer handle preroll inside audio sink.  This makes the audio sink
    no longer requires certain number of frames are available before start
    playing back.  The preroll/underflow should be handled by the
    AudioRenderer if needed.
    
    Change-Id: I49644abf5581932eeb800cb0f6ac9559f75b84d8

--- starboard/shared/alsa/alsa_audio_sink_type.cc
+++ starboard/shared/alsa/alsa_audio_sink_type.cc
@@ -33,25 +33,21 @@ using starboard::ScopedTryLock;
 using starboard::shared::alsa::AlsaGetBufferedFrames;
 using starboard::shared::alsa::AlsaWriteFrames;
 
-// TODO: Expose the following settings.
-//
-// How many frames left inside ALSA.  It has to be powers of 2.  The lower the
-// number is, more CPU will be used as the callbacks will be called more
-// frequently and also it will be more likely to underflow.  As
-// |kMinimumFramesInALSA| has to be strictly greater than |kFramesPerRequest|,
-// the larger the value is, the longer the delay after Pause().
+// The maximum number of frames that can be written to ALSA once.  It must be a
+// power of 2.  It is also used as the ALSA polling size.  A small number will
+// lead to more CPU being used as the callbacks will be called more
+// frequently and also it will be more likely to cause underflow but can make
+// the audio clock more accurate.
 const int kFramesPerRequest = 512;
-// The source must have |kPrerollSizeInFrames| frames before the playback
-// starts.  The larger the number is, the less likely an underflow will happen
-// under bad connection, but it also increases the startup latency.
-const int kPrerollSizeInFrames = kFramesPerRequest;
-// When the frames inside ALSA buffer is less than |kMinimumDelayInFrames|, the
-// class will try to write more frames to ALSA.  The larger the number is, the
-// less likely an underflow will happen.  Use a larger number will cause longer
+// When the frames inside ALSA buffer is less than |kMinimumFramesInALSA|, the
+// class will try to write more frames.  The larger the number is, the less
+// likely an underflow will happen but to use a larger number will cause longer
 // delays after pause and stop.
-// Note that |kMinimumFramesInALSA| must be less than or equal to
-// |kPrerollSizeInFrames|.
-const int kMinimumFramesInALSA = kFramesPerRequest * 4;
+const int kMinimumFramesInALSA = 2048;
+// The size of the audio buffer ALSA allocates internally.  Ideally this value
+// should be greater than the sum of the above two constants.  Choose a value
+// that is too large can waste some memory as the extra buffer is never used.
+const int kALSABufferSizeInFrames = 8192;
 
 // This class is an ALSA based audio sink with the following features:
 // 1. It doesn't cache any data internally and maintains minimum data inside
@@ -81,12 +77,11 @@ class AlsaAudioSink : public SbAudioSinkPrivate {
   void AudioThreadFunc();
   // Write silence to ALSA when there is not enough data in source or when the
   // sink is paused.
-  // Return true when preroll is finished.  Return false when destroying.
-  bool PrerollLoop();
-  // Keep pulling frames from source until there is not enough frames to keep
-  // playback.  When the sink is paused or there is not enough frames in
-  // source, it returns true so we can continue into the PrerollLoop().  It
-  // returns false when destroying.
+  // Return true to continue to play.  Return false when destroying.
+  bool IdleLoop();
+  // Keep pulling frames from source until there is no frames to keep playback.
+  // When the sink is paused or there is no frames in source, it returns true
+  // so we can continue into the IdleLoop().  It returns false when destroying.
   bool PlaybackLoop();
   // Helper function to write frames contained in a ring buffer to ALSA.
   void WriteFrames(int frames_to_write,
@@ -170,19 +165,16 @@ void* AlsaAudioSink::ThreadEntryPoint(void* context) {
 }
 
 void AlsaAudioSink::AudioThreadFunc() {
-  SB_CHECK(kFramesPerRequest < kMinimumFramesInALSA);
-  SB_CHECK(kFramesPerRequest <= kPrerollSizeInFrames);
-
   playback_handle_ = starboard::shared::alsa::AlsaOpenPlaybackDevice(
       channels_, sampling_frequency_hz_, kFramesPerRequest,
-      kPrerollSizeInFrames + kMinimumFramesInALSA * 2);
+      kALSABufferSizeInFrames);
   creation_signal_.Signal();
   if (!playback_handle_) {
     return;
   }
 
   for (;;) {
-    if (!PrerollLoop()) {
+    if (!IdleLoop()) {
       break;
     }
     if (!PlaybackLoop()) {
@@ -195,8 +187,8 @@ void AlsaAudioSink::AudioThreadFunc() {
   playback_handle_ = NULL;
 }
 
-bool AlsaAudioSink::PrerollLoop() {
-  SB_DLOG(INFO) << "alsa::AlsaAudioSink enters prerolling loop";
+bool AlsaAudioSink::IdleLoop() {
+  SB_DLOG(INFO) << "alsa::AlsaAudioSink enters idle loop";
 
   for (;;) {
     {
@@ -209,22 +201,12 @@ bool AlsaAudioSink::PrerollLoop() {
     bool is_playing, is_eos_reached;
     update_source_status_func_(&frames_in_buffer, &offset_in_frames,
                                &is_playing, &is_eos_reached, context_);
-    if (is_playing) {
-      if (frames_in_buffer >= kPrerollSizeInFrames) {
-        WriteFrames(kPrerollSizeInFrames, frames_in_buffer, offset_in_frames);
-        return true;
-      }
-      if (is_eos_reached && frames_in_buffer > 0) {
-        WriteFrames(frames_in_buffer, frames_in_buffer, offset_in_frames);
-        return true;
-      }
+    if (is_playing && frames_in_buffer > 0) {
+      return true;
     }
     int delayed_frame = AlsaGetBufferedFrames(playback_handle_);
     if (delayed_frame < kMinimumFramesInALSA) {
-      int consumed = AlsaWriteFrames(playback_handle_, &silence_frames_[0],
-                                     kFramesPerRequest);
-      SB_DCHECK(consumed == kFramesPerRequest) << consumed << " "
-                                               << delayed_frame;
+      AlsaWriteFrames(playback_handle_, &silence_frames_[0], kFramesPerRequest);
     } else {
       SbThreadSleep(time_to_wait_);
     }
@@ -253,14 +235,11 @@ bool AlsaAudioSink::PlaybackLoop() {
       bool is_playing, is_eos_reached;
       update_source_status_func_(&frames_in_buffer, &offset_in_frames,
                                  &is_playing, &is_eos_reached, context_);
-      if (!is_playing ||
-          (frames_in_buffer < kFramesPerRequest && !is_eos_reached)) {
+      if (!is_playing || frames_in_buffer == 0) {
         return true;
       }
-      if (frames_in_buffer > 0) {
-        WriteFrames(std::min(kFramesPerRequest, frames_in_buffer),
-                    frames_in_buffer, offset_in_frames);
-      }
+      WriteFrames(std::min(kFramesPerRequest, frames_in_buffer),
+                  frames_in_buffer, offset_in_frames);
     } else {
       SbThreadSleep(time_to_wait_);
     }
@@ -279,9 +258,11 @@ void AlsaAudioSink::WriteFrames(int frames_to_write,
     int consumed = AlsaWriteFrames(playback_handle_,
                                    frame_buffer_ + offset_in_frames * channels_,
                                    frames_to_buffer_end);
-    SB_DCHECK(consumed == frames_to_buffer_end) << consumed << " "
-                                                << frames_to_buffer_end;
-    consume_frame_func_(frames_to_buffer_end, context_);
+    consume_frame_func_(consumed, context_);
+    if (consumed != frames_to_buffer_end) {
+      return;
+    }
+
     frames_to_write -= frames_to_buffer_end;
     offset_in_frames = 0;
   }
@@ -289,8 +270,7 @@ void AlsaAudioSink::WriteFrames(int frames_to_write,
   int consumed = AlsaWriteFrames(playback_handle_,
                                  frame_buffer_ + offset_in_frames * channels_,
                                  frames_to_write);
-  SB_DCHECK(consumed == frames_to_write) << consumed << " " << frames_to_write;
-  consume_frame_func_(frames_to_write, context_);
+  consume_frame_func_(consumed, context_);
 }
 
 }  // namespace
