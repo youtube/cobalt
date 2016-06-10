cbff20b1f38811cdf5b2808927ab88217cfd9ccc
Cobalt Team <cobalt-team@google.com>
2016-06-09 19:03:01 -0700

    Stub for Starboard atomic operations.
    Change-Id: I99cc3289d53728d1b0e15a3056dd585822581928

--- starboard/stub/atomic_public.h
+++ starboard/stub/atomic_public.h
@@ -17,4 +17,175 @@
 
 #include "starboard/atomic.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+SB_C_FORCE_INLINE SbAtomic32
+SbAtomicNoBarrier_CompareAndSwap(volatile SbAtomic32* /*ptr*/,
+                                 SbAtomic32 /*old_value*/,
+                                 SbAtomic32 /*new_value*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+SB_C_FORCE_INLINE SbAtomic32
+SbAtomicNoBarrier_Exchange(volatile SbAtomic32* /*ptr*/,
+                           SbAtomic32 /*new_value*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+SB_C_FORCE_INLINE SbAtomic32
+SbAtomicNoBarrier_Increment(volatile SbAtomic32* /*ptr*/,
+                            SbAtomic32 /*increment*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+SB_C_FORCE_INLINE SbAtomic32
+SbAtomicBarrier_Increment(volatile SbAtomic32* /*ptr*/,
+                          SbAtomic32 /*increment*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+SB_C_FORCE_INLINE SbAtomic32
+SbAtomicAcquire_CompareAndSwap(volatile SbAtomic32* /*ptr*/,
+                               SbAtomic32 /*old_value*/,
+                               SbAtomic32 /*new_value*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+SB_C_FORCE_INLINE SbAtomic32
+SbAtomicRelease_CompareAndSwap(volatile SbAtomic32* /*ptr*/,
+                               SbAtomic32 /*old_value*/,
+                               SbAtomic32 /*new_value*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+SB_C_FORCE_INLINE void SbAtomicMemoryBarrier() {
+  SB_NOTIMPLEMENTED();
+}
+
+SB_C_FORCE_INLINE void SbAtomicNoBarrier_Store(volatile SbAtomic32* /*ptr*/,
+                                               SbAtomic32 /*value*/) {
+  SB_NOTIMPLEMENTED();
+}
+
+SB_C_FORCE_INLINE void SbAtomicAcquire_Store(volatile SbAtomic32* /*ptr*/,
+                                             SbAtomic32 /*value*/) {
+  SB_NOTIMPLEMENTED();
+}
+
+SB_C_FORCE_INLINE void SbAtomicRelease_Store(volatile SbAtomic32* /*ptr*/,
+                                             SbAtomic32 /*value*/) {
+  SB_NOTIMPLEMENTED();
+}
+
+SB_C_FORCE_INLINE SbAtomic32
+SbAtomicNoBarrier_Load(volatile const SbAtomic32* /*ptr*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+SB_C_FORCE_INLINE SbAtomic32
+SbAtomicAcquire_Load(volatile const SbAtomic32* /*ptr*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+SB_C_FORCE_INLINE SbAtomic32
+SbAtomicRelease_Load(volatile const SbAtomic32* /*ptr*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+// 64-bit atomic operations (only available on 64-bit processors).
+#if SB_HAS(64_BIT_ATOMICS)
+SB_C_FORCE_INLINE SbAtomic64
+SbAtomicNoBarrier_CompareAndSwap64(volatile SbAtomic64* /*ptr*/,
+                                   SbAtomic64 /*old_value*/,
+                                   SbAtomic64 /*new_value*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+SB_C_FORCE_INLINE SbAtomic64
+SbAtomicNoBarrier_Exchange64(volatile SbAtomic64* /*ptr*/,
+                             SbAtomic64 /*new_value*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+SB_C_FORCE_INLINE SbAtomic64
+SbAtomicNoBarrier_Increment64(volatile SbAtomic64* /*ptr*/,
+                              SbAtomic64 /*increment*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+SB_C_FORCE_INLINE SbAtomic64
+SbAtomicBarrier_Increment64(volatile SbAtomic64* /*ptr*/,
+                            SbAtomic64 /*increment*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+SB_C_FORCE_INLINE SbAtomic64
+SbAtomicAcquire_CompareAndSwap64(volatile SbAtomic64* /*ptr*/,
+                                 SbAtomic64 /*old_value*/,
+                                 SbAtomic64 /*new_value*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+SB_C_FORCE_INLINE SbAtomic64
+SbAtomicRelease_CompareAndSwap64(volatile SbAtomic64* /*ptr*/,
+                                 SbAtomic64 /*old_value*/,
+                                 SbAtomic64 /*new_value*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+SB_C_FORCE_INLINE void SbAtomicNoBarrier_Store64(volatile SbAtomic64* /*ptr*/,
+                                                 SbAtomic64 /*value*/) {
+  SB_NOTIMPLEMENTED();
+}
+
+SB_C_FORCE_INLINE void SbAtomicAcquire_Store64(volatile SbAtomic64* /*ptr*/,
+                                               SbAtomic64 /*value*/) {
+  SB_NOTIMPLEMENTED();
+}
+
+SB_C_FORCE_INLINE void SbAtomicRelease_Store64(volatile SbAtomic64* /*ptr*/,
+                                               SbAtomic64 /*value*/) {
+  SB_NOTIMPLEMENTED();
+}
+
+SB_C_FORCE_INLINE SbAtomic64
+SbAtomicNoBarrier_Load64(volatile const SbAtomic64* /*ptr*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+SB_C_FORCE_INLINE SbAtomic64
+SbAtomicAcquire_Load64(volatile const SbAtomic64* /*ptr*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+
+SB_C_FORCE_INLINE SbAtomic64
+SbAtomicRelease_Load64(volatile const SbAtomic64* /*ptr*/) {
+  SB_NOTIMPLEMENTED();
+  return 0;
+}
+#endif  // SB_HAS(64_BIT_ATOMICS)
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
 #endif  // STARBOARD_STUB_ATOMIC_PUBLIC_H_
