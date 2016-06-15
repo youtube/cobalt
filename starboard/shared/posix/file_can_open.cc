6f6235a852bed64496b714036a9301e879e53b3c
Cobalt Team <cobalt-team@google.com>
2016-06-15 16:53:07 -0700

    Implement PS4 file, adopt shared POSIX implementations
    Change-Id: I63be2be2f43ed73afb586d995d15efb37af8e5f0

--- starboard/shared/posix/file_can_open.cc
+++ starboard/shared/posix/file_can_open.cc
@@ -16,23 +16,30 @@
 
 #include "starboard/file.h"
 
-#include <unistd.h>
-
-#include "starboard/shared/posix/file_internal.h"
+#include <errno.h>
+#include <sys/stat.h>
 
 bool SbFileCanOpen(const char* path, int flags) {
-  int amode = 0;
-  if (flags & kSbFileRead) {
-    amode |= R_OK;
-  }
-
-  if (flags & kSbFileWrite) {
-    amode |= W_OK;
-  }
-
-  if (!amode) {
+  bool can_read = flags & kSbFileRead;
+  bool can_write = flags & kSbFileWrite;
+  if (!can_read && !can_write) {
     return false;
   }
 
-  return access(path, amode) == 0;
+  struct stat file_info;
+  if (stat(path, &file_info) != 0) {
+    return false;
+  }
+
+  if (can_read && !(file_info.st_mode & S_IRUSR)) {
+    errno = EACCES;
+    return false;
+  }
+
+  if (can_write && !(file_info.st_mode & S_IWUSR)) {
+    errno = EACCES;
+    return false;
+  }
+
+  return true;
 }
