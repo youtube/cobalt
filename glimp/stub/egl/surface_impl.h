c467d6da6b59d663d41079a36072922000935152
Jelle Foks <jfoks@google.com>
2016-06-23 19:09:46 -0700

    Add ps4 platform with stubs.
    This adds a ps4 platform definition and stubs.
    
    Change-Id: Ib12612703ebc1bd7488319f45db4fd3ceca8c95c

---
+++ glimp/stub/egl/surface_impl.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef GLIMP_STUB_EGL_SURFACE_IMPL_H_
+#define GLIMP_STUB_EGL_SURFACE_IMPL_H_
+
+#include "glimp/egl/surface_impl.h"
+
+namespace glimp {
+namespace egl {
+
+class SurfaceImplStub : public SurfaceImpl {
+ public:
+  virtual bool IsDisplayRenderTarget() const { return false; }
+};
+
+}  // namespace egl
+}  // namespace glimp
+
+#endif  // GLIMP_STUB_EGL_SURFACE_IMPL_H_
---
+++ glimp/stub/gles/program_impl.cc
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "glimp/stub/gles/program_impl.h"
+
+#include "glimp/nb/polymorphic_downcast.h"
+#include "glimp/stub/gles/shader_impl.h"
+#include "starboard/log.h"
+
+namespace glimp {
+namespace gles {
+
+ProgramImplStub::ProgramImplStub()
+    : linked_vertex_shader_(NULL), linked_fragment_shader_(NULL) {}
+
+ProgramImpl::LinkResults ProgramImplStub::Link(
+    const nb::scoped_refptr<Shader>& vertex_shader,
+    const nb::scoped_refptr<Shader>& fragment_shader) {
+  linked_vertex_shader_ =
+      nb::polymorphic_downcast<ShaderImplStub*>(vertex_shader->impl());
+  linked_fragment_shader_ =
+      nb::polymorphic_downcast<ShaderImplStub*>(fragment_shader->impl());
+
+  bound_attributes_.clear();
+
+  return ProgramImpl::LinkResults(true);
+}
+
+bool ProgramImplStub::BindAttribLocation(unsigned int index, const char* name) {
+  SB_DCHECK(linked_vertex_shader_);
+  return false;
+}
+
+int ProgramImplStub::GetUniformLocation(const char* name) {
+  return -1;
+}
+
+int ProgramImplStub::GetResourceForBoundAttrib(unsigned int index) const {
+  std::map<unsigned int, int>::const_iterator found =
+      bound_attributes_.find(index);
+  if (found == bound_attributes_.end()) {
+    return -1;
+  }
+
+  return found->second;
+}
+
+}  // namespace gles
+}  // namespace glimp
