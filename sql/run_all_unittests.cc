cbe22331f9d62b912347781127eba558b2b0947e
Cobalt Team <cobalt-team@google.com>
2016-07-06 10:55:05 -0700

    Ensure starboard initialization is done for crypto and sql tests
    - Add deploy target for net and sql unit tests
    
    b/29946805
    b/29946803
    
    Change-Id: I27b01a448f837ed05adaaf12712d996274bab27f

--- sql/run_all_unittests.cc
+++ sql/run_all_unittests.cc
@@ -6,6 +6,7 @@
 #include "base/test/test_suite.h"
 #include "sql/test_vfs.h"
 
+#if !defined(OS_STARBOARD)
 int main(int argc, char** argv) {
   MainHook hook(main, argc, argv);
   sql::RegisterTestVfs();
@@ -13,3 +14,19 @@ int main(int argc, char** argv) {
   sql::UnregisterTestVfs();
   return result;
 }
+#else
+#include "starboard/event.h"
+#include "starboard/system.h"
+
+void SbEventHandle(const SbEvent* event) {
+  if (event->type == kSbEventTypeStart) {
+    SbEventStartData* data = static_cast<SbEventStartData*>(event->data);
+    MainHook hook(NULL, data->argument_count, data->argument_values);
+    sql::RegisterTestVfs();
+    int error_level =
+        base::TestSuite(data->argument_count, data->argument_values).Run();
+    sql::UnregisterTestVfs();
+    SbSystemRequestStop(error_level);
+  }
+}
+#endif
