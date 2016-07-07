d853998caf6fae78658a2ccae793e6698fd6fbec
Cobalt Team <cobalt-team@google.com>
2016-07-06 17:16:49 -0700

    Framework for attribute bindings
    - Stub out a whole bunch of stuff
    - Implement binding for attributes
    - Only implement conversion for bool types to verify
      correct functionality of property getting/setting
    - Refactor how types in the bindings layer are inferred from the
      types in the IDL files, removing some JSC references and using
      a TypeTraits struct to allow handling of special cases for
      types that use ScriptObjects
    
    Change-Id: Id714704b2098043caf0cecc79c279419af23ae9b

---
+++ cobalt/script/mozjs/mozjs_exception_state.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef COBALT_SCRIPT_MOZJS_MOZJS_EXCEPTION_STATE_H_
+#define COBALT_SCRIPT_MOZJS_MOZJS_EXCEPTION_STATE_H_
+
+#include <string>
+
+#include "base/threading/thread_checker.h"
+#include "cobalt/script/exception_state.h"
+#include "third_party/mozjs/js/src/jsapi.h"
+
+namespace cobalt {
+namespace script {
+namespace mozjs {
+
+class MozjsExceptionState : public ExceptionState {
+ public:
+  explicit MozjsExceptionState(JSContext* context) : context_(context) {}
+  // ExceptionState interface
+  void SetException(const scoped_refptr<ScriptException>& exception) OVERRIDE;
+  void SetSimpleException(SimpleExceptionType simple_exception,
+                          const std::string& message) OVERRIDE;
+
+  bool IsExceptionSet();
+
+ private:
+  JSContext* context_;
+  base::ThreadChecker thread_checker_;
+};
+
+}  // namespace mozjs
+}  // namespace script
+}  // namespace cobalt
+
+#endif  // COBALT_SCRIPT_MOZJS_MOZJS_EXCEPTION_STATE_H_
