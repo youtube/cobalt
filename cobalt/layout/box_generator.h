db1862b302451b51c4db07a745fb067db77ad806
Cobalt Team <cobalt-team@google.com>
2016-07-12 11:18:50 -0700

    Change BoxGenerator::Context parameter ordering
    The ordering of the parameters to BoxGenerator::Context has been
    updated to match that of UpdateComputedStylesAndLayoutBoxTree.
    
    Change-Id: I2f9e3dc87d79ef3a6b899e91cb34fda36cf5daa4

--- cobalt/layout/box_generator.h
+++ cobalt/layout/box_generator.h
@@ -51,19 +51,19 @@ class BoxGenerator : public dom::NodeVisitor {
  public:
   struct Context {
     Context(UsedStyleProvider* used_style_provider,
+            LayoutStatTracker* layout_stat_tracker,
             icu::BreakIterator* line_break_iterator,
             icu::BreakIterator* character_break_iterator,
-            LayoutStatTracker* layout_stat_tracker,
             dom::HTMLElement* ignore_background_element)
         : used_style_provider(used_style_provider),
+          layout_stat_tracker(layout_stat_tracker),
           line_break_iterator(line_break_iterator),
           character_break_iterator(character_break_iterator),
-          layout_stat_tracker(layout_stat_tracker),
           ignore_background_element(ignore_background_element) {}
     UsedStyleProvider* used_style_provider;
+    LayoutStatTracker* layout_stat_tracker;
     icu::BreakIterator* line_break_iterator;
     icu::BreakIterator* character_break_iterator;
-    LayoutStatTracker* layout_stat_tracker;
 
     // The HTML and BODY tags may have had their background style properties
     // propagated up to the initial containing block.  If so, we should not
