96b26207eb76f039bd61ebaf021ecd1e41cd1015
Cobalt Team <cobalt-team@google.com>
2016-07-19 14:52:57 -0700

    Enable and stub out player functions to fix stub build compile.
    b/29398621
    
    Change-Id: I4ef4eaf842b776c8fea612340abd51d880371ce3

---
+++ starboard/shared/stub/player_write_sample.cc
@@ -0,0 +1,23 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/player.h"
+
+void SbPlayerWriteSample(SbPlayer /*player*/,
+                         SbMediaType /*sample_type*/,
+                         const void* /*sample_buffer*/,
+                         int /*sample_buffer_size*/,
+                         SbMediaTime /*sample_pts*/,
+                         const SbMediaVideoSampleInfo* /*video_sample_info*/,
+                         const SbDrmSampleInfo* /*sample_drm_info*/) {}
