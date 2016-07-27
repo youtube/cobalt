5403c8231fbef42bd895c3815537b00d548bbd2d
Cobalt Team <cobalt-team@google.com>
2016-07-26 21:07:33 -0700

    Use interface_name as class name
    binding_class has "Mozjs" prefix, so using interface_name instead.
    
    b/30041093
    
    Change-Id: I7942ca1b77219fe3959c99b69f5c385f8918ae53

--- cobalt/bindings/generated/mozjs/testing/MozjsConstructorWithArgumentsInterface.cc
+++ cobalt/bindings/generated/mozjs/testing/MozjsConstructorWithArgumentsInterface.cc
@@ -87,7 +87,7 @@ InterfaceData* CreateCachedInterfaceData() {
 
   JSClass* instance_class = &interface_data->instance_class_definition;
   const int kGlobalFlags = 0;
-  instance_class->name = "MozjsConstructorWithArgumentsInterface";
+  instance_class->name = "ConstructorWithArgumentsInterface";
   instance_class->flags = kGlobalFlags | JSCLASS_HAS_PRIVATE;
   instance_class->addProperty = JS_PropertyStub;
   instance_class->delProperty = JS_DeletePropertyStub;
@@ -102,7 +102,7 @@ InterfaceData* CreateCachedInterfaceData() {
   instance_class->trace = &WrapperPrivate::Trace;
 
   JSClass* prototype_class = &interface_data->prototype_class_definition;
-  prototype_class->name = "MozjsConstructorWithArgumentsInterfacePrototype";
+  prototype_class->name = "ConstructorWithArgumentsInterfacePrototype";
   prototype_class->flags = 0;
   prototype_class->addProperty = JS_PropertyStub;
   prototype_class->delProperty = JS_DeletePropertyStub;
@@ -113,7 +113,7 @@ InterfaceData* CreateCachedInterfaceData() {
   prototype_class->convert = JS_ConvertStub;
 
   JSClass* interface_object_class = &interface_data->interface_object_class_definition;
-  interface_object_class->name = "MozjsConstructorWithArgumentsInterfaceConstructor";
+  interface_object_class->name = "ConstructorWithArgumentsInterfaceConstructor";
   interface_object_class->flags = 0;
   interface_object_class->addProperty = JS_PropertyStub;
   interface_object_class->delProperty = JS_DeletePropertyStub;
--- cobalt/bindings/generated/mozjs/testing/MozjsGetOpaqueRootInterface.cc
+++ cobalt/bindings/generated/mozjs/testing/MozjsGetOpaqueRootInterface.cc
@@ -87,7 +87,7 @@ InterfaceData* CreateCachedInterfaceData() {
 
   JSClass* instance_class = &interface_data->instance_class_definition;
   const int kGlobalFlags = 0;
-  instance_class->name = "MozjsGetOpaqueRootInterface";
+  instance_class->name = "GetOpaqueRootInterface";
   instance_class->flags = kGlobalFlags | JSCLASS_HAS_PRIVATE;
   instance_class->addProperty = JS_PropertyStub;
   instance_class->delProperty = JS_DeletePropertyStub;
@@ -102,7 +102,7 @@ InterfaceData* CreateCachedInterfaceData() {
   instance_class->trace = &WrapperPrivate::Trace;
 
   JSClass* prototype_class = &interface_data->prototype_class_definition;
-  prototype_class->name = "MozjsGetOpaqueRootInterfacePrototype";
+  prototype_class->name = "GetOpaqueRootInterfacePrototype";
   prototype_class->flags = 0;
   prototype_class->addProperty = JS_PropertyStub;
   prototype_class->delProperty = JS_DeletePropertyStub;
@@ -113,7 +113,7 @@ InterfaceData* CreateCachedInterfaceData() {
   prototype_class->convert = JS_ConvertStub;
 
   JSClass* interface_object_class = &interface_data->interface_object_class_definition;
-  interface_object_class->name = "MozjsGetOpaqueRootInterfaceConstructor";
+  interface_object_class->name = "GetOpaqueRootInterfaceConstructor";
   interface_object_class->flags = 0;
   interface_object_class->addProperty = JS_PropertyStub;
   interface_object_class->delProperty = JS_DeletePropertyStub;
