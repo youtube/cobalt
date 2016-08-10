9cea8d659cb5a4b59f6f932a6fa10333bcdf0b61
Xiaoming Shi <xiaomings@google.com>
2016-08-10 14:58:52 -0700

    Enable frame rendering in media_source_sandbox.
    b/30090298
    
    Change-Id: Ic4de30669bd7ad997e29be1830534c2f4df158a9

--- cobalt/media/sandbox/media_source_sandbox.cc
+++ cobalt/media/sandbox/media_source_sandbox.cc
@@ -26,6 +26,7 @@
 #include "cobalt/base/wrap_main.h"
 #include "cobalt/media/sandbox/media_sandbox.h"
 #include "cobalt/media/sandbox/web_media_player_helper.h"
+#include "cobalt/render_tree/image.h"
 #include "media/base/video_frame.h"
 #include "net/base/net_util.h"
 
@@ -35,9 +36,11 @@ namespace sandbox {
 namespace {
 
 typedef ::media::WebMediaPlayer::AddIdStatus AddIdStatus;
+
 using base::TimeDelta;
 using ::media::VideoFrame;
 using ::media::WebMediaPlayer;
+using render_tree::Image;
 
 GURL ResolveUrl(const char* arg) {
   GURL video_url(arg);
@@ -137,6 +140,14 @@ void AppendData(const std::string& id, const std::vector<uint8_t>& data,
   }
 }
 
+scoped_refptr<Image> FrameCB(WebMediaPlayerHelper* player_helper,
+                             const base::TimeDelta& time) {
+  UNREFERENCED_PARAMETER(time);
+
+  scoped_refptr<VideoFrame> frame = player_helper->GetCurrentFrame();
+  return frame ? reinterpret_cast<Image*>(frame->texture_id()) : NULL;
+}
+
 int SandboxMain(int argc, char** argv) {
   if (argc != 3 && argc != 4) {
     LOG(ERROR) << "Usage: " << argv[0]
@@ -197,6 +208,9 @@ int SandboxMain(int argc, char** argv) {
   bool eos_appended = false;
 
   scoped_refptr<VideoFrame> last_frame;
+
+  media_sandbox.RegisterFrameCB(
+      base::Bind(FrameCB, base::Unretained(&player_helper)));
 
   for (;;) {
     AppendData(kAudioId, audio_loader.buffer(), &audio_offset, player);
