d3ec5d361bec1a13bdeea5367f48a143cb28f829
Cobalt Team <cobalt-team@google.com>
2016-08-10 18:16:25 -0700

    Add SpiderMonkey debugger stubs
    b/30764226
    
    Change-Id: Ibdcfb8a49597af948907518ad904f082c40fa056

---
+++ cobalt/script/mozjs/mozjs_debugger.cc
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cobalt/script/mozjs/mozjs_debugger.h"
+
+#include "base/logging.h"
+
+namespace cobalt {
+namespace script {
+
+// Static factory method declared in public interface.
+scoped_ptr<ScriptDebugger> ScriptDebugger::CreateDebugger(
+    GlobalObjectProxy* global_object_proxy, Delegate* delegate) {
+  return scoped_ptr<ScriptDebugger>(
+      new mozjs::MozjsDebugger(global_object_proxy, delegate));
+}
+
+namespace mozjs {
+
+MozjsDebugger::MozjsDebugger(GlobalObjectProxy* global_object_proxy,
+                             Delegate* delegate) {
+  NOTIMPLEMENTED();
+}
+
+MozjsDebugger::~MozjsDebugger() { NOTIMPLEMENTED(); }
+
+void MozjsDebugger::Attach() { NOTIMPLEMENTED(); }
+
+void MozjsDebugger::Detach() { NOTIMPLEMENTED(); }
+
+void MozjsDebugger::Pause() { NOTIMPLEMENTED(); }
+
+void MozjsDebugger::Resume() { NOTIMPLEMENTED(); }
+
+void MozjsDebugger::SetBreakpoint(const std::string& script_id, int line_number,
+                                  int column_number) {
+  NOTIMPLEMENTED();
+}
+
+script::ScriptDebugger::PauseOnExceptionsState
+MozjsDebugger::SetPauseOnExceptions(PauseOnExceptionsState state) {
+  NOTIMPLEMENTED();
+  return kNone;
+}
+
+void MozjsDebugger::StepInto() { NOTIMPLEMENTED(); }
+
+void MozjsDebugger::StepOut() { NOTIMPLEMENTED(); }
+
+void MozjsDebugger::StepOver() { NOTIMPLEMENTED(); }
+
+}  // namespace mozjs
+}  // namespace script
+}  // namespace cobalt
