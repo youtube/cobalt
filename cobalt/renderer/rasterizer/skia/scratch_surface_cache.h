1f8f40803b02c613887215e982a9b7dbb1332ccd
Cobalt Team <cobalt-team@google.com>
2016-08-25 15:45:45 -0700

    Make ScratchSurfaceCache generic and not Skia-specific.
    The logic in ScratchSurfaceCache can be reused for the Blitter API
    render tree rasterizer.  This CL factors out the platform-independent
    logic from the Skia-specific logic in ScratchSurfaceCache so that it
    can be ported over for use in the Blitter API render tree rasterizer
    as well.
    
    Change-Id: I79acbe8d44b53f5dc9e6cf3cb45d33138a301bc4

--- cobalt/renderer/rasterizer/skia/scratch_surface_cache.h
+++ cobalt/renderer/rasterizer/skia/scratch_surface_cache.h
@@ -22,96 +22,52 @@
 #include "base/callback.h"
 #include "base/logging.h"
 #include "cobalt/math/size.h"
+#include "cobalt/renderer/rasterizer/common/scratch_surface_cache.h"
 #include "third_party/skia/include/core/SkSurface.h"
-#include "third_party/skia/include/core/SkSurfaceProps.h"
 
 namespace cobalt {
 namespace renderer {
 namespace rasterizer {
 namespace skia {
 
-// The ScratchSurfaceCache manages a set of Skia SkSurface objects that can
-// be used for offscreen rendering (mainly by SkiaRenderTreeNodeVisitor).
-// In order to acquire surfaces from ScratchSurfaceCache, one should construct
-// a CachedScratchSurface object, from which they can get a pointer to the
-// SkSurface object.
-// It is unique because it caters to SkiaRenderTreeNodeVisitor's scratch
-// surface allocation pattern.  In particular, we don't mind handing out very
-// large scratch surfaces when very small surfaces are requested, because
-// typically only one surface is ever needed at a time.  When multiple surfaces
-// are necessary, the subsequent ones tend to be smaller than the previous
-// (because rendering is now happening within the previous ones), and so handing
-// out textures in large to small order makes sense.
 class ScratchSurfaceCache {
  public:
   typedef base::Callback<SkSurface*(const math::Size&)> CreateSkSurfaceFunction;
 
-  ScratchSurfaceCache(const CreateSkSurfaceFunction& create_sk_surface_function,
+  ScratchSurfaceCache(CreateSkSurfaceFunction create_sk_surface_function,
                       int cache_capacity_in_bytes);
-  ~ScratchSurfaceCache();
 
  private:
-  // Acquire (either via cache lookup or by creation if that fails) a scratch
-  // surface.
-  SkSurface* AcquireScratchSurface(const math::Size& size);
-  void ReleaseScratchSurface(SkSurface* surface);
+  class Delegate : public common::ScratchSurfaceCache::Delegate {
+   public:
+    explicit Delegate(CreateSkSurfaceFunction create_sk_surface_function);
 
-  // Searches through |unused_surfaces_| for one the one that best matches the
-  // requested size, among all those cached surfaces that have at least the
-  // requested size.
-  SkSurface* FindBestCachedSurface(const math::Size& size);
+    common::ScratchSurfaceCache::Surface* CreateSurface(
+        const math::Size& size) OVERRIDE;
+    void DestroySurface(common::ScratchSurfaceCache::Surface* surface) OVERRIDE;
+    void PrepareForUse(common::ScratchSurfaceCache::Surface* surface,
+                       const math::Size& area) OVERRIDE;
 
-  // Removes elements from the cache until we are below the cache limit.
-  void Purge();
+   private:
+    CreateSkSurfaceFunction create_sk_surface_function_;
+  };
 
-  // Allocate a new SkSurface and return it.
-  SkSurface* CreateScratchSurface(const math::Size& size);
-
-  // Called to allocate new SkSurfaces.
-  CreateSkSurfaceFunction create_sk_surface_function_;
-
-  // The maximum number of surface bytes that can be stored in the cache.
-  int cache_capacity_in_bytes_;
-
-  // We keep track of all surfaces we've handed out using |surface_stack_|.
-  // This is mostly for debug checks to verify that surfaces returned to us
-  // actually did come from us, and that they are returned in the expected
-  // order.
-  std::vector<SkSurface*> surface_stack_;
-
-  // |unused_surfaces_| is the heart of the cache, as it stores the surfaces
-  // that are unused but allocated and available to be handed out upon request.
-  // Most recently used surfaces are found at the end of this vector.
-  std::vector<SkSurface*> unused_surfaces_;
-
-  // Tracks how much total memory we've allocated towards surfaces.
-  size_t surface_memory_;
+  Delegate delegate_;
+  common::ScratchSurfaceCache cache_;
 
   friend class CachedScratchSurface;
 };
 
-// The primary interface to ScratchSurfaceCache is through CachedScratchSurface
-// objects.  These effectively scope the acquisition of a cached surface, RAII
-// style.
 class CachedScratchSurface {
  public:
   CachedScratchSurface(ScratchSurfaceCache* cache, const math::Size& size)
-      : cache_(cache) {
-    surface_ = cache_->AcquireScratchSurface(size);
-  }
-
-  ~CachedScratchSurface() {
-    if (surface_) {
-      cache_->ReleaseScratchSurface(surface_);
-    }
-  }
+      : common_scratch_surface_(&cache->cache_, size) {}
 
   // Returns a pointer to the Skia SkSurface.
-  SkSurface* GetSurface() { return surface_; }
+  SkSurface* GetSurface();
 
  private:
-  ScratchSurfaceCache* cache_;
-  SkSurface* surface_;
+  common::CachedScratchSurface common_scratch_surface_;
 };
 
 }  // namespace skia
