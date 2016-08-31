c0a4ab81dcaff6a980d39162e3b57d6f3e0b729e
Cobalt Team <cobalt-team@google.com>
2016-08-30 23:28:54 -0700

    add SbStringParseDouble, and a few poems
    Add SbStringParseDouble, a dropin replacement for strtod, in Starboard
    Change SbStringConcat to match strcat signature
    Add tests for abs "poem"
    
    Add stdio_poem to help porting functions defined in stdio.h
    Add stdlib_poem to help porting functions defined in stdlib.h
    Add string_poem to help porting functions defined in string.h
    Add strings_poem to help porting functions defined in strings.h
    Add wchar_poem to help porting functions defined in wchar.h
    
    Change-Id: I8e39b8bd302a54302c4978da261cde399dc5ecd0

---
+++ starboard/client_porting/poem/stdlib_poem.h
@@ -0,0 +1,39 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// A poem (POsix EMulation) for functions in stdlib.h
+
+#ifndef STARBOARD_CLIENT_PORTING_POEM_STDLIB_POEM_H_
+#define STARBOARD_CLIENT_PORTING_POEM_STDLIB_POEM_H_
+
+#include "starboard/string.h"
+#include "starboard/system.h"
+
+// number conversion functions
+#define strtol(s, o, b) SbStringParseSignedInteger(s, o, b)
+#define atoi(v) SbStringAToI(v)
+#define atol(v) SbStringAToL(v)
+#define strtol(s, o, b) SbStringParseSignedInteger(s, o, b)
+#define strtoul(s, o, b) SbStringParseUnsignedInteger(s, o, b)
+#define strtoull(s, o, b) SbStringParseUInt64(s, o, b)
+#define strtod(s, o) SbStringParseDouble(s, o)
+#define qsort(b, ec, ew, c) SbSystemSort(b, ec, ew, c);
+
+SB_C_INLINE int abs(int x) {
+  if (x < 0)
+    return -x;
+  return x;
+}
+
+#endif  // STARBOARD_CLIENT_PORTING_POEM_STDLIB_POEM_H_
---
+++ starboard/client_porting/poem/wchar_poem.h
@@ -0,0 +1,25 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// A poem (POsix EMulation) for functions in wchar.h
+
+#ifndef STARBOARD_CLIENT_PORTING_POEM_WCHAR_POEM_H_
+#define STARBOARD_CLIENT_PORTING_POEM_WCHAR_POEM_H_
+
+#include "starboard/string.h"
+
+#define vswprintf SbStringFormatWide
+#define wcsncmp(s1, s2, c) SbStringCompareWide(s1, s2, c)
+
+#endif  // STARBOARD_CLIENT_PORTING_POEM_WCHAR_POEM_H_
