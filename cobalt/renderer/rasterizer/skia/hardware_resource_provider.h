fda7badcc1c264f976d2bb03091e7a2bb5602acf
Cobalt Team <cobalt-team@google.com>
2016-08-31 17:40:30 -0700

    Create SkFontMgr in resource provider constructor
    Previously, the SkFontMgr would not be created until it was needed
    within various resource provider functions. However, this made it so
    that multiple threads could be requesting typefaces from the resource
    provider at the same time, resulting in multiple SkFontMgr objects
    briefly existing (which results in the extra ones immediately being
    destroyed after construction).
    
    To prevent multiple SkFontMgr objects from briefly existing, the
    SkFontMgr singleton is now created in the resource provider constructor.
    
    This resolves b/31224890.
    
    Change-Id: I5bbda9792889c2a87dda1cb5772689bfcbfd7aed

--- cobalt/renderer/rasterizer/skia/hardware_resource_provider.h
+++ cobalt/renderer/rasterizer/skia/hardware_resource_provider.h
@@ -24,6 +24,7 @@
 #include "cobalt/renderer/rasterizer/skia/hardware_image.h"
 #include "cobalt/renderer/rasterizer/skia/text_shaper.h"
 #include "third_party/skia/include/gpu/GrContext.h"
+#include "third_party/skia/include/ports/SkFontMgr.h"
 
 namespace cobalt {
 namespace renderer {
@@ -89,6 +90,7 @@ class HardwareResourceProvider : public render_tree::ResourceProvider {
   backend::GraphicsContextEGL* cobalt_context_;
   GrContext* gr_context_;
 
+  SkAutoTUnref<SkFontMgr> font_manager_;
   TextShaper text_shaper_;
 
   // We keep a handle to the message loop that this resource provider was
