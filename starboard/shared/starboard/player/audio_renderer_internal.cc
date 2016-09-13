39bafa8625fac24b338ef332eb10d40704bc0130
Xiaoming Shi <xiaomings@google.com>
2016-09-12 18:14:56 -0700

    Replace DCHECK by if statement.
    This avoids DCHECK when the input audio frame is invalid.
    
    b/31428529
    
    Change-Id: I26be52ce902b30654eaf5b8dcba30a5ed3addd00

--- starboard/shared/starboard/player/audio_renderer_internal.cc
+++ starboard/shared/starboard/player/audio_renderer_internal.cc
@@ -63,7 +63,9 @@ void AudioRenderer::WriteSample(const InputBuffer& input_buffer) {
   SbMediaTime input_pts = input_buffer.pts();
   std::vector<float> decoded_audio;
   decoder_->Decode(input_buffer, &decoded_audio);
-  SB_DCHECK(!decoded_audio.empty());
+  if (decoded_audio.empty()) {
+    return;
+  }
 
   ScopedLock lock(mutex_);
   if (seeking_) {
