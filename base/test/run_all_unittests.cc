8ff86ae5fd2c55106d198b572b0deff8fa9eab86
Joe Woodworth <joewoodworth@google.com>
2016-09-15 16:04:15 -0700

    STARBOARD_WRAP_SIMPLE_MAIN
    Change-Id: I224f2afa74853f269aebf69b591733fc54ca4a15

--- base/test/run_all_unittests.cc
+++ base/test/run_all_unittests.cc
@@ -5,22 +5,11 @@
 #include "base/test/main_hook.h"
 #include "base/test/test_suite.h"
 #include "build/build_config.h"
+#include "starboard/client_porting/wrap_main/wrap_main.h"
 
-#if !defined(OS_STARBOARD)
-int main(int argc, char** argv) {
-  MainHook hook(main, argc, argv);
+int TestSuiteRun(int argc, char** argv) {
+  MainHook hook(NULL, argc, argv);
   return base::TestSuite(argc, argv).Run();
 }
-#else
-#include "starboard/event.h"
-#include "starboard/system.h"
 
-void SbEventHandle(const SbEvent* event) {
-  if (event->type == kSbEventTypeStart) {
-    SbEventStartData* data = static_cast<SbEventStartData*>(event->data);
-    MainHook hook(NULL, data->argument_count, data->argument_values);
-    SbSystemRequestStop(
-        base::TestSuite(data->argument_count, data->argument_values).Run());
-  }
-}
-#endif
+STARBOARD_WRAP_SIMPLE_MAIN(TestSuiteRun);
