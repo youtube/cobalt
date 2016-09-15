ba8306834cf1d96fdff830fe5ce678b465a21640
Joe Woodworth <joewoodworth@google.com>
2016-09-15 16:04:15 -0700

    STARBOARD_WRAP_SIMPLE_MAIN
    Change-Id: I224f2afa74853f269aebf69b591733fc54ca4a15

--- crypto/run_all_unittests.cc
+++ crypto/run_all_unittests.cc
@@ -5,30 +5,17 @@
 #include "base/test/main_hook.h"
 #include "base/test/test_suite.h"
 #include "crypto/nss_util.h"
+#include "starboard/client_porting/wrap_main/wrap_main.h"
 
-#if !defined(OS_STARBOARD)
-int main(int argc, char** argv) {
-  MainHook hook(main, argc, argv);
-
+int TestSuiteRun(int argc, char** argv) {
+  MainHook hook(NULL, argc, argv);
 #if defined(USE_NSS)
   // This is most likely not needed, but it basically replaces a similar call
   // that was performed on test_support_base.
   // TODO(rvargas) Bug 79359: remove this.
   crypto::EnsureNSSInit();
 #endif  // defined(USE_NSS)
-
   return base::TestSuite(argc, argv).Run();
 }
-#else
-#include "starboard/event.h"
-#include "starboard/system.h"
 
-void SbEventHandle(const SbEvent* event) {
-  if (event->type == kSbEventTypeStart) {
-    SbEventStartData* data = static_cast<SbEventStartData*>(event->data);
-    MainHook hook(NULL, data->argument_count, data->argument_values);
-    SbSystemRequestStop(
-        base::TestSuite(data->argument_count, data->argument_values).Run());
-  }
-}
-#endif
+STARBOARD_WRAP_SIMPLE_MAIN(TestSuiteRun);
