2a7f2310ac11fba68cff46991380500c843591e0
Cobalt Team <cobalt-team@google.com>
2016-09-15 15:23:21 -0700

    Handle Starboard deep link events
    b/30687988
    
    A Starboard launch link that arrives during app initialization is
    passed as an additional argument (along with argc, argv) to |InitCobalt|
    and the wrap_main StartFunction. From there, it is used to initialize
    |h5vcc.runtime.initialDeepLink|.
    
    Starboard link events that arrive while the application is running are
    passed to the event target |h5vcc.runtime.onDeepLink|, which will notify
    all registered listeners.
    
    It is expected that ***REMOVED*** will use these new attributes in
    |h5vcc.runtime| to get the deep link data and act appropriately.
    
    Refactors H5vccRuntimeEventTarget to avoid duplicating code in
    H5vccDeepLinkEventTarget.
    
    Change-Id: Ic458208acdebf81384b474f7e337d3b948b4cdf3

--- cobalt/h5vcc/h5vcc.cc
+++ cobalt/h5vcc/h5vcc.cc
@@ -23,7 +23,8 @@ H5vcc::H5vcc(const Settings& settings) {
   account_info_ = new H5vccAccountInfo(settings.account_manager);
   audio_config_array_ = new H5vccAudioConfigArray();
   c_val_ = new H5vccCVal();
-  runtime_ = new H5vccRuntime(settings.event_dispatcher);
+  runtime_ =
+      new H5vccRuntime(settings.event_dispatcher, settings.initial_deep_link);
   settings_ = new H5vccSettings(settings.media_module);
   storage_ = new H5vccStorage(settings.network_module);
   system_ = new H5vccSystem();
