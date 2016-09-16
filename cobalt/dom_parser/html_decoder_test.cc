1b501170e17d711d31d0aa2c66077139bed7b45e
Cobalt Team <cobalt-team@google.com>
2016-09-16 16:37:04 -0700

    Fix test build breaks.
    Change-Id: I9205716eab8682c149336da9c791284a8f50fa58

--- cobalt/dom_parser/html_decoder_test.cc
+++ cobalt/dom_parser/html_decoder_test.cc
@@ -69,7 +69,7 @@ HTMLDecoderTest::HTMLDecoderTest()
       html_element_context_(&fetcher_factory_, &stub_css_parser_,
                             dom_parser_.get(), NULL /* can_play_type_handler */,
                             NULL /* web_media_player_factory */,
-                            &stub_script_runner_, NULL, NULL, NULL, NULL,
+                            &stub_script_runner_, NULL, NULL, NULL, NULL, NULL,
                             dom_stat_tracker_.get(), ""),
       document_(
           new dom::Document(&html_element_context_, dom::Document::Options())),
