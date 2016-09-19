72ef14b5cd9b45bccad834c13d73ac70c9b40872
Cobalt Team <cobalt-team@google.com>
2016-09-19 16:09:01 -0700

    Add and properly set should execute for scripts.
    The HTML5 spec mandates that script elements inserted through
    innerHTML and outerHTML are not executed.  This change adds an
    additional field to script elements to accommodate this, and selects
    the appropriate value for it in the parser.
    
    b/31428741
    b/31428527
    
    Change-Id: I3c1e6711572b5682315a885ca6239d9b429578ff

--- cobalt/dom_parser/html_decoder.h
+++ cobalt/dom_parser/html_decoder.h
@@ -49,7 +49,8 @@ class HTMLDecoder : public loader::Decoder {
               const int dom_max_element_depth,
               const base::SourceLocation& input_location,
               const base::Closure& done_callback,
-              const base::Callback<void(const std::string&)>& error_callback);
+              const base::Callback<void(const std::string&)>& error_callback,
+              const bool should_run_scripts);
 
   ~HTMLDecoder();
 
@@ -67,6 +68,8 @@ class HTMLDecoder : public loader::Decoder {
   scoped_refptr<dom::Document> document_;
   base::ThreadChecker thread_checker_;
   const base::Closure done_callback_;
+
+  const bool should_run_scripts_;
 
   DISALLOW_COPY_AND_ASSIGN(HTMLDecoder);
 };
