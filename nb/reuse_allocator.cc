53c143e6c50b7f749959f7e2f38a66f17f8f1d68
Xiaoming Shi <xiaomings@google.com>
2016-09-25 15:49:31 -0700

    Refactor ReuseAllocator.
    Previously the ReuseAllocator uses first-fit strategy for allocations.
    This causes serious fragmentations in some cases.
    For example, during media playback, more than 90% of the encoded buffers
    are smaller than 768 bytes but some of the buffers can be as large as
    800K.  Eventually the media source buffer will OOM because of
    fragmentation.  To allocate small buffers from the back of the pool can
    solve this problem.
    
    Also add a default implementation of AllocateForAlignment() into Allocator
    and remove unused implementations from its sub-classes.  Make |size| an
    in/out parameter to pass out the allocated block size, so the user
    (currently ReuseAllocator) can get the block size explicitly instead of
    calculating it from returned address, size, and alignment.
    
    Note that |verify_full_capacity | parameter of MemoryPool ctor starts to
    be a bit redundant.  The removing of it is tracked inside b/31732952.
    
    b/31707641
    
    Change-Id: I22d3a671b5d9ca708fa4fb0626600784bebb7dc7

--- nb/reuse_allocator.cc
+++ nb/reuse_allocator.cc
@@ -24,10 +24,104 @@
 
 namespace nb {
 
-ReuseAllocator::ReuseAllocator(Allocator* fallback_allocator) {
-  fallback_allocator_ = fallback_allocator;
-  capacity_ = 0;
-  total_allocated_ = 0;
+// Minimum block size to avoid extremely small blocks inside the block list and
+// to ensure that a zero sized allocation will return a non-zero sized block.
+const std::size_t kMinBlockSizeBytes = 16;
+
+bool ReuseAllocator::MemoryBlock::Merge(const MemoryBlock& other) {
+  if (AsInteger(address_) + size_ == AsInteger(other.address_)) {
+    size_ += other.size_;
+    return true;
+  }
+  if (AsInteger(other.address_) + other.size_ == AsInteger(address_)) {
+    address_ = other.address_;
+    size_ += other.size_;
+    return true;
+  }
+  return false;
+}
+
+bool ReuseAllocator::MemoryBlock::CanFullfill(std::size_t request_size,
+                                              std::size_t alignment) const {
+  const std::size_t extra_bytes_for_alignment =
+      AlignUp(AsInteger(address_), alignment) - AsInteger(address_);
+  const std::size_t aligned_size = request_size + extra_bytes_for_alignment;
+  return size_ >= aligned_size;
+}
+
+void ReuseAllocator::MemoryBlock::Allocate(std::size_t request_size,
+                                           std::size_t alignment,
+                                           bool allocate_from_front,
+                                           MemoryBlock* allocated,
+                                           MemoryBlock* free) const {
+  SB_DCHECK(allocated);
+  SB_DCHECK(free);
+  SB_DCHECK(CanFullfill(request_size, alignment));
+
+  // First we assume that the block is just enough to fulfill the allocation and
+  // leaves no free block.
+  allocated->address_ = address_;
+  allocated->size_ = size_;
+  free->address_ = NULL;
+  free->size_ = 0;
+
+  if (allocate_from_front) {
+    // |address_|
+    //     |     <- allocated_size ->     | <- remaining_size -> |
+    //     -------------------------------------------------------
+    //          |   <-  request_size ->   |                      |
+    //  |aligned_address|       |end_of_allocation|      |address_ + size_|
+    std::size_t aligned_address = AlignUp(AsInteger(address_), alignment);
+    std::size_t end_of_allocation = aligned_address + request_size;
+    std::size_t allocated_size = end_of_allocation - AsInteger(address_);
+    std::size_t remaining_size = size_ - allocated_size;
+    if (remaining_size < kMinBlockSizeBytes) {
+      return;
+    }
+    allocated->size_ = allocated_size;
+    free->address_ = AsPointer(end_of_allocation);
+    free->size_ = remaining_size;
+  } else {
+    // |address_|
+    //     |   <- remaining_size ->  |   <- allocated_size ->    |
+    //     -------------------------------------------------------
+    //                               |  <-  request_size -> |    |
+    //                       |aligned_address|           |address_ + size_|
+    std::size_t aligned_address =
+        AlignDown(AsInteger(address_) + size_ - request_size, alignment);
+    std::size_t allocated_size = AsInteger(address_) + size_ - aligned_address;
+    std::size_t remaining_size = size_ - allocated_size;
+    if (remaining_size < kMinBlockSizeBytes) {
+      return;
+    }
+    allocated->address_ = AsPointer(aligned_address);
+    allocated->size_ = allocated_size;
+    free->address_ = address_;
+    free->size_ = remaining_size;
+  }
+}
+
+ReuseAllocator::ReuseAllocator(Allocator* fallback_allocator)
+    : fallback_allocator_(fallback_allocator),
+      small_allocation_threshold_(0),
+      capacity_(0),
+      total_allocated_(0) {}
+
+ReuseAllocator::ReuseAllocator(Allocator* fallback_allocator,
+                               std::size_t capacity,
+                               std::size_t small_allocation_threshold)
+    : fallback_allocator_(fallback_allocator),
+      small_allocation_threshold_(small_allocation_threshold),
+      capacity_(0),
+      total_allocated_(0) {
+  // If |small_allocation_threshold_| is non-zero, this class will use last-fit
+  // strategy to fulfill small allocations.  Pre-allocator full capacity so
+  // last-fit makes sense.
+  if (small_allocation_threshold_ > 0) {
+    void* p = Allocate(capacity, 1);
+    SB_DCHECK(p);
+    Free(p);
+  }
 }
 
 ReuseAllocator::~ReuseAllocator() {
@@ -44,18 +138,14 @@ ReuseAllocator::~ReuseAllocator() {
   }
 }
 
-void ReuseAllocator::AddFreeBlock(void* address, std::size_t size) {
-  MemoryBlock new_block;
-  new_block.address = address;
-  new_block.size = size;
-
+ReuseAllocator::FreeBlockSet::iterator ReuseAllocator::AddFreeBlock(
+    MemoryBlock block_to_add) {
   if (free_blocks_.size() == 0) {
-    free_blocks_.insert(new_block);
-    return;
+    return free_blocks_.insert(block_to_add).first;
   }
 
   // See if we can merge this block with one on the right or left.
-  FreeBlockSet::iterator it = free_blocks_.lower_bound(new_block);
+  FreeBlockSet::iterator it = free_blocks_.lower_bound(block_to_add);
   // lower_bound will return an iterator to our neighbor on the right,
   // if one exists.
   FreeBlockSet::iterator right_to_erase = free_blocks_.end();
@@ -63,9 +153,7 @@ void ReuseAllocator::AddFreeBlock(void* address, std::size_t size) {
 
   if (it != free_blocks_.end()) {
     MemoryBlock right_block = *it;
-    if (AsInteger(new_block.address) + new_block.size ==
-        AsInteger(right_block.address)) {
-      new_block.size += right_block.size;
+    if (block_to_add.Merge(right_block)) {
       right_to_erase = it;
     }
   }
@@ -75,10 +163,7 @@ void ReuseAllocator::AddFreeBlock(void* address, std::size_t size) {
     it--;
     MemoryBlock left_block = *it;
     // Are we contiguous with the block to our left?
-    if (AsInteger(left_block.address) + left_block.size ==
-        AsInteger(new_block.address)) {
-      new_block.address = left_block.address;
-      new_block.size += left_block.size;
+    if (block_to_add.Merge(left_block)) {
       left_to_erase = it;
     }
   }
@@ -90,7 +175,7 @@ void ReuseAllocator::AddFreeBlock(void* address, std::size_t size) {
     free_blocks_.erase(left_to_erase);
   }
 
-  free_blocks_.insert(new_block);
+  return free_blocks_.insert(block_to_add).first;
 }
 
 void ReuseAllocator::RemoveFreeBlock(FreeBlockSet::iterator it) {
@@ -101,11 +186,6 @@ void* ReuseAllocator::Allocate(std::size_t size) {
   return Allocate(size, 1);
 }
 
-void* ReuseAllocator::AllocateForAlignment(std::size_t size,
-                                           std::size_t alignment) {
-  return Allocate(size, alignment);
-}
-
 void* ReuseAllocator::Allocate(std::size_t size, std::size_t alignment) {
   if (alignment == 0) {
     alignment = 1;
@@ -113,15 +193,14 @@ void* ReuseAllocator::Allocate(std::size_t size, std::size_t alignment) {
 
   // Try to satisfy request from free list.
   // First look for a block that is appropriately aligned.
-  // If we can't, look for a block that is big enough that we can
-  // carve out an aligned block.
+  // If we can't, look for a block that is big enough that we can carve out an
+  // aligned block.
   // If there is no such block, allocate more from our fallback allocator.
   void* user_address = 0;
 
-  // Keeping things rounded and aligned will help us
-  // avoid creating tiny and/or badly misaligned free blocks.
-  // Also ensure even for a 0-byte request we return a unique block.
-  const std::size_t kMinBlockSizeBytes = 16;
+  // Keeping things rounded and aligned will help us avoid creating tiny and/or
+  // badly misaligned free blocks.  Also ensure even for a 0-byte request we
+  // return a unique block.
   const std::size_t kMinAlignment = 16;
   size = std::max(size, kMinBlockSizeBytes);
   size = AlignUp(size, kMinBlockSizeBytes);
@@ -130,68 +209,59 @@ void* ReuseAllocator::Allocate(std::size_t size, std::size_t alignment) {
   // Worst case how much memory we need.
   MemoryBlock allocated_block;
 
-  // Start looking through the free list.
-  // If this is slow, we can store another map sorted by size.
-  for (FreeBlockSet::iterator it = free_blocks_.begin();
-       it != free_blocks_.end(); ++it) {
-    MemoryBlock block = *it;
-    const std::size_t extra_bytes_for_alignment =
-        (alignment - AsInteger(block.address) % alignment) % alignment;
-    const std::size_t aligned_size = size + extra_bytes_for_alignment;
-    if (block.size >= aligned_size) {
-      // The block is big enough.  We may waste some space due to alignment.
-      RemoveFreeBlock(it);
-      const std::size_t remaining_bytes = block.size - aligned_size;
-      if (remaining_bytes >= kMinBlockSizeBytes) {
-        AddFreeBlock(AsPointer(AsInteger(block.address) + aligned_size),
-                     remaining_bytes);
-        allocated_block.size = aligned_size;
-      } else {
-        allocated_block.size = block.size;
+  bool scan_from_front = size > small_allocation_threshold_;
+  FreeBlockSet::iterator free_block_iter = free_blocks_.end();
+
+  if (scan_from_front) {
+    // Start looking through the free list from the front.
+    for (FreeBlockSet::iterator it = free_blocks_.begin();
+         it != free_blocks_.end(); ++it) {
+      if (it->CanFullfill(size, alignment)) {
+        free_block_iter = it;
+        break;
       }
-      user_address = AlignUp(block.address, alignment);
-      allocated_block.address = block.address;
-      SB_DCHECK(allocated_block.size <= block.size);
-      break;
+    }
+  } else {
+    // Start looking through the free list from the back.
+    for (FreeBlockSet::reverse_iterator it = free_blocks_.rbegin();
+         it != free_blocks_.rend(); ++it) {
+      if (it->CanFullfill(size, alignment)) {
+        free_block_iter = it.base();
+        --free_block_iter;
+        break;
+      }
     }
   }
 
-  if (user_address == 0) {
-    // No free blocks found.
-    // Allocate one from the fallback allocator.
-    size = AlignUp(size, alignment);
-    void* ptr = fallback_allocator_->AllocateForAlignment(size, alignment);
+  if (free_block_iter == free_blocks_.end()) {
+    // No free blocks found, allocate one from the fallback allocator.
+    std::size_t block_size = size;
+    void* ptr =
+        fallback_allocator_->AllocateForAlignment(&block_size, alignment);
     if (ptr == NULL) {
       return NULL;
     }
-    uint8_t* memory_address = reinterpret_cast<uint8_t*>(ptr);
-    user_address = AlignUp(memory_address, alignment);
-    allocated_block.size = size;
-    allocated_block.address = user_address;
-
-    if (memory_address != user_address) {
-      std::size_t alignment_padding_size =
-          AsInteger(user_address) - AsInteger(memory_address);
-      if (alignment_padding_size >= kMinBlockSizeBytes) {
-        // Register the memory range skipped for alignment as a free block for
-        // later use.
-        AddFreeBlock(memory_address, alignment_padding_size);
-        capacity_ += alignment_padding_size;
-      } else {
-        // The memory range skipped for alignment is too small for a free block.
-        // Adjust the allocated block to include the alignment padding.
-        allocated_block.size += alignment_padding_size;
-        allocated_block.address = AsPointer(AsInteger(allocated_block.address) -
-                                            alignment_padding_size);
-      }
-    }
-
-    capacity_ += allocated_block.size;
+    free_block_iter = AddFreeBlock(MemoryBlock(ptr, block_size));
+    capacity_ += block_size;
     fallback_allocations_.push_back(ptr);
   }
+
+  MemoryBlock block = *free_block_iter;
+  // The block is big enough.  We may waste some space due to alignment.
+  RemoveFreeBlock(free_block_iter);
+
+  MemoryBlock free_block;
+  block.Allocate(size, alignment, scan_from_front, &allocated_block,
+                 &free_block);
+  if (free_block.size() > 0) {
+    SB_DCHECK(free_block.address());
+    AddFreeBlock(free_block);
+  }
+  user_address = AlignUp(allocated_block.address(), alignment);
+
   SB_DCHECK(allocated_blocks_.find(user_address) == allocated_blocks_.end());
   allocated_blocks_[user_address] = allocated_block;
-  total_allocated_ += allocated_block.size;
+  total_allocated_ += allocated_block.size();
   return user_address;
 }
 
@@ -205,10 +275,10 @@ void ReuseAllocator::Free(void* memory) {
 
   // Mark this block as free and remove it from the allocated set.
   const MemoryBlock& block = (*it).second;
-  AddFreeBlock(block.address, block.size);
+  AddFreeBlock(block);
 
-  SB_DCHECK(block.size <= total_allocated_);
-  total_allocated_ -= block.size;
+  SB_DCHECK(block.size() <= total_allocated_);
+  total_allocated_ -= block.size();
 
   allocated_blocks_.erase(it);
 }
@@ -218,7 +288,7 @@ void ReuseAllocator::PrintAllocations() const {
   SizesHistogram sizes_histogram;
   for (AllocatedBlockMap::const_iterator iter = allocated_blocks_.begin();
        iter != allocated_blocks_.end(); ++iter) {
-    std::size_t block_size = iter->second.size;
+    std::size_t block_size = iter->second.size();
     if (sizes_histogram.find(block_size) == sizes_histogram.end()) {
       sizes_histogram[block_size] = 0;
     }
