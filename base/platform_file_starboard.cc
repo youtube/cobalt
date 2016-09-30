7804994375a5903061aa3977bcf3690c065425e2
Joe Woodworth <joewoodworth@google.com>
2016-09-30 13:39:41 -0700

    Fix ReadPlatformFileAtCurrentPos
    Use SbFileReadAll and SbFileWriteAll
    
    Change-Id: I496a25e3336b1aacee9b3b70887d21606faaf2b5

--- base/platform_file_starboard.cc
+++ base/platform_file_starboard.cc
@@ -161,21 +161,7 @@ int ReadPlatformFile(PlatformFile file, int64 offset, char* data, int size) {
 
 int ReadPlatformFileAtCurrentPos(PlatformFile file, char *data, int size) {
   base::ThreadRestrictions::AssertIOAllowed();
-  if (file < 0 || size < 0)
-    return -1;
-
-  int bytes_read = 0;
-  int rv;
-  do {
-    rv = SbFileRead(file, data, size);
-    if (rv <= 0) {
-      break;
-    }
-
-    bytes_read += rv;
-  } while (bytes_read < size);
-
-  return bytes_read ? bytes_read : rv;
+  return SbFileReadAll(file, data, size);
 }
 
 int ReadPlatformFileNoBestEffort(PlatformFile file,
@@ -251,22 +237,7 @@ int WritePlatformFileAtCurrentPos(PlatformFile file,
                                   const char* data,
                                   int size) {
   base::ThreadRestrictions::AssertIOAllowed();
-  if (size < 0) {
-    return -1;
-  }
-
-  int bytes_written = 0;
-  int result;
-  do {
-    result = SbFileWrite(file, data, size);
-    if (result <= 0) {
-      break;
-    }
-
-    bytes_written += result;
-  } while (bytes_written < size);
-
-  return bytes_written ? bytes_written : result;
+  return SbFileWriteAll(file, data, size);
 }
 
 int WritePlatformFileCurPosNoBestEffort(PlatformFile file,
