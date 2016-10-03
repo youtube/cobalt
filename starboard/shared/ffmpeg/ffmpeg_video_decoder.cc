64afeb6deaeb8d7eb7ff661e85f5ad87c7bfea8f
Xiaoming Shi <xiaomings@google.com>
2016-10-03 10:45:33 -0700

    Check |codec_context_| before flushing it.
    It can be called inside dtor where it is already NULL.
    
    b/31903816
    
    Change-Id: I8711e6abfee5fbb0b1c6e8e058f52ac380f1c197

--- starboard/shared/ffmpeg/ffmpeg_video_decoder.cc
+++ starboard/shared/ffmpeg/ffmpeg_video_decoder.cc
@@ -148,7 +148,9 @@ void VideoDecoder::Reset() {
     SbThreadJoin(decoder_thread_, NULL);
   }
 
-  avcodec_flush_buffers(codec_context_);
+  if (codec_context_ != NULL) {
+    avcodec_flush_buffers(codec_context_);
+  }
 
   decoder_thread_ = kSbThreadInvalid;
   stream_ended_ = false;
