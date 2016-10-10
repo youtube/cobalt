6e2d0ca435b95325495c41aabf21e5f1b7654c84
Cobalt Team <cobalt-team@google.com>
2016-10-10 14:27:32 -0700

    Disabling unnamed union/struct warnings on VC compiler to fix Windows builds.
    Change-Id: Ie002a7b24b1ce7137cb0129f1dc452f473bb2650

--- third_party/glm/glm/detail/type_vec4.hpp
+++ third_party/glm/glm/detail/type_vec4.hpp
@@ -36,6 +36,10 @@ namespace glm
 #				pragma clang diagnostic ignored "-Wgnu-anonymous-struct"
 #				pragma clang diagnostic ignored "-Wnested-anon-types"
 #			endif
+#			if GLM_COMPILER & GLM_COMPILER_VC
+#				pragma warning(push)
+#				pragma warning(disable : 4201)
+#			endif
 		
 			union
 			{
@@ -58,6 +62,9 @@ namespace glm
 #				endif//GLM_SWIZZLE
 			};
 
+#			if GLM_COMPILER & GLM_COMPILER_VC
+#				pragma warning(pop)
+#			endif
 #			if GLM_COMPILER & GLM_COMPILER_CLANG
 #				pragma clang diagnostic pop
 #			endif
