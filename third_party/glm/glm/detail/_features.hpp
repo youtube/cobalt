382b717776367eb87f763fa494e08cb5bc855837
Cobalt Team <cobalt-team@google.com>
2016-10-11 10:25:32 -0700

    Fix PS3 build errors due to __has_feature builtin redefinition
    b/32088029
    
    Change-Id: I00ace8266ff9f36764c84e5d88178f5fda5ddae6

--- third_party/glm/glm/detail/_features.hpp
+++ third_party/glm/glm/detail/_features.hpp
@@ -256,143 +256,143 @@
 #	endif
 
 #elif(GLM_COMPILER & GLM_COMPILER_CLANG)
-#	if(__has_feature(cxx_exceptions))
+#	if(GLM_HAS_FEATURE(cxx_exceptions))
 #		define GLM_CXX98_EXCEPTIONS
 #	endif
 
-#	if(__has_feature(cxx_rtti))
+#	if(GLM_HAS_FEATURE(cxx_rtti))
 #		define GLM_CXX98_RTTI
 #	endif
 
-#	if(__has_feature(cxx_access_control_sfinae))
+#	if(GLM_HAS_FEATURE(cxx_access_control_sfinae))
 #		define GLM_CXX11_ACCESS_CONTROL_SFINAE
 #	endif
 
-#	if(__has_feature(cxx_alias_templates))
+#	if(GLM_HAS_FEATURE(cxx_alias_templates))
 #		define GLM_CXX11_ALIAS_TEMPLATE
 #	endif
 
-#	if(__has_feature(cxx_alignas))
+#	if(GLM_HAS_FEATURE(cxx_alignas))
 #		define GLM_CXX11_ALIGNAS
 #	endif
 
-#	if(__has_feature(cxx_attributes))
+#	if(GLM_HAS_FEATURE(cxx_attributes))
 #		define GLM_CXX11_ATTRIBUTES
 #	endif
 
-#	if(__has_feature(cxx_constexpr))
+#	if(GLM_HAS_FEATURE(cxx_constexpr))
 #		define GLM_CXX11_CONSTEXPR
 #	endif
 
-#	if(__has_feature(cxx_decltype))
+#	if(GLM_HAS_FEATURE(cxx_decltype))
 #		define GLM_CXX11_DECLTYPE
 #	endif
 
-#	if(__has_feature(cxx_default_function_template_args))
+#	if(GLM_HAS_FEATURE(cxx_default_function_template_args))
 #		define GLM_CXX11_DEFAULT_FUNCTION_TEMPLATE_ARGS
 #	endif
 
-#	if(__has_feature(cxx_defaulted_functions))
+#	if(GLM_HAS_FEATURE(cxx_defaulted_functions))
 #		define GLM_CXX11_DEFAULTED_FUNCTIONS
 #	endif
 
-#	if(__has_feature(cxx_delegating_constructors))
+#	if(GLM_HAS_FEATURE(cxx_delegating_constructors))
 #		define GLM_CXX11_DELEGATING_CONSTRUCTORS
 #	endif
 
-#	if(__has_feature(cxx_deleted_functions))
+#	if(GLM_HAS_FEATURE(cxx_deleted_functions))
 #		define GLM_CXX11_DELETED_FUNCTIONS
 #	endif
 
-#	if(__has_feature(cxx_explicit_conversions))
+#	if(GLM_HAS_FEATURE(cxx_explicit_conversions))
 #		define GLM_CXX11_EXPLICIT_CONVERSIONS
 #	endif
 
-#	if(__has_feature(cxx_generalized_initializers))
+#	if(GLM_HAS_FEATURE(cxx_generalized_initializers))
 #		define GLM_CXX11_GENERALIZED_INITIALIZERS
 #	endif
 
-#	if(__has_feature(cxx_implicit_moves))
+#	if(GLM_HAS_FEATURE(cxx_implicit_moves))
 #		define GLM_CXX11_IMPLICIT_MOVES
 #	endif
 
-#	if(__has_feature(cxx_inheriting_constructors))
+#	if(GLM_HAS_FEATURE(cxx_inheriting_constructors))
 #		define GLM_CXX11_INHERITING_CONSTRUCTORS
 #	endif
 
-#	if(__has_feature(cxx_inline_namespaces))
+#	if(GLM_HAS_FEATURE(cxx_inline_namespaces))
 #		define GLM_CXX11_INLINE_NAMESPACES
 #	endif
 
-#	if(__has_feature(cxx_lambdas))
+#	if(GLM_HAS_FEATURE(cxx_lambdas))
 #		define GLM_CXX11_LAMBDAS
 #	endif
 
-#	if(__has_feature(cxx_local_type_template_args))
+#	if(GLM_HAS_FEATURE(cxx_local_type_template_args))
 #		define GLM_CXX11_LOCAL_TYPE_TEMPLATE_ARGS
 #	endif
 
-#	if(__has_feature(cxx_noexcept))
+#	if(GLM_HAS_FEATURE(cxx_noexcept))
 #		define GLM_CXX11_NOEXCEPT
 #	endif
 
-#	if(__has_feature(cxx_nonstatic_member_init))
+#	if(GLM_HAS_FEATURE(cxx_nonstatic_member_init))
 #		define GLM_CXX11_NONSTATIC_MEMBER_INIT
 #	endif
 
-#	if(__has_feature(cxx_nullptr))
+#	if(GLM_HAS_FEATURE(cxx_nullptr))
 #		define GLM_CXX11_NULLPTR
 #	endif
 
-#	if(__has_feature(cxx_override_control))
+#	if(GLM_HAS_FEATURE(cxx_override_control))
 #		define GLM_CXX11_OVERRIDE_CONTROL
 #	endif
 
-#	if(__has_feature(cxx_reference_qualified_functions))
+#	if(GLM_HAS_FEATURE(cxx_reference_qualified_functions))
 #		define GLM_CXX11_REFERENCE_QUALIFIED_FUNCTIONS
 #	endif
 
-#	if(__has_feature(cxx_range_for))
+#	if(GLM_HAS_FEATURE(cxx_range_for))
 #		define GLM_CXX11_RANGE_FOR
 #	endif
 
-#	if(__has_feature(cxx_raw_string_literals))
+#	if(GLM_HAS_FEATURE(cxx_raw_string_literals))
 #		define GLM_CXX11_RAW_STRING_LITERALS
 #	endif
 
-#	if(__has_feature(cxx_rvalue_references))
+#	if(GLM_HAS_FEATURE(cxx_rvalue_references))
 #		define GLM_CXX11_RVALUE_REFERENCES
 #	endif
 
-#	if(__has_feature(cxx_static_assert))
+#	if(GLM_HAS_FEATURE(cxx_static_assert))
 #		define GLM_CXX11_STATIC_ASSERT
 #	endif
 
-#	if(__has_feature(cxx_auto_type))
+#	if(GLM_HAS_FEATURE(cxx_auto_type))
 #		define GLM_CXX11_AUTO_TYPE
 #	endif
 
-#	if(__has_feature(cxx_strong_enums))
+#	if(GLM_HAS_FEATURE(cxx_strong_enums))
 #		define GLM_CXX11_STRONG_ENUMS
 #	endif
 
-#	if(__has_feature(cxx_trailing_return))
+#	if(GLM_HAS_FEATURE(cxx_trailing_return))
 #		define GLM_CXX11_TRAILING_RETURN
 #	endif
 
-#	if(__has_feature(cxx_unicode_literals))
+#	if(GLM_HAS_FEATURE(cxx_unicode_literals))
 #		define GLM_CXX11_UNICODE_LITERALS
 #	endif
 
-#	if(__has_feature(cxx_unrestricted_unions))
+#	if(GLM_HAS_FEATURE(cxx_unrestricted_unions))
 #		define GLM_CXX11_UNRESTRICTED_UNIONS
 #	endif
 
-#	if(__has_feature(cxx_user_literals))
+#	if(GLM_HAS_FEATURE(cxx_user_literals))
 #		define GLM_CXX11_USER_LITERALS
 #	endif
 
-#	if(__has_feature(cxx_variadic_templates))
+#	if(GLM_HAS_FEATURE(cxx_variadic_templates))
 #		define GLM_CXX11_VARIADIC_TEMPLATES
 #	endif
 
