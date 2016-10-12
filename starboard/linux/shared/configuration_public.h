219584612e3f0307b381c21f5d40876111a89765
Cobalt Team <cobalt-team@google.com>
2016-10-12 14:17:11 -0700

    Adjust SB_NETWORK_RECEIVE_BUFFER_SIZE
    Network recieve buffer size can dramatically affect throughput.
    
    On linux-based platforms such as Roku and raspi: Set it to 0,
    which means for starboard "Don't set". This will allow Linux
    auto-tuning to occur.
    
    On PS3 and PS4, set to 128k, which is sufficient to download
    https://storage.googleapis.com/yt-cobalt-media-element-demo/progressive.mp4
    in ~4 seconds.
    
    b/32097576
    
    Change-Id: I370f6ae0f3ec3800fb3f38cd37bf9b8559192472

--- starboard/linux/shared/configuration_public.h
+++ starboard/linux/shared/configuration_public.h
@@ -355,8 +355,19 @@
 
 // --- Tuneable Parameters ---------------------------------------------------
 
-// Specifies the network receive buffer size.
-#define SB_NETWORK_RECEIVE_BUFFER_SIZE (16 * 1024)
+// Specifies the network receive buffer size in bytes, set via
+// SbSocketSetReceiveBufferSize().
+//
+// Setting this to 0 indicates that SbSocketSetReceiveBufferSize() should
+// not be called. Use this for OSs (such as Linux) where receive buffer
+// auto-tuning is better.
+//
+// On some platforms, this may affect max TCP window size which may
+// dramatically affect throughput in the presence of latency.
+//
+// If your platform does not have a good TCP auto-tuning mechanism,
+// a setting of (128 * 1024) here is recommended.
+#define SB_NETWORK_RECEIVE_BUFFER_SIZE (0)
 
 // --- User Configuration ----------------------------------------------------
 
