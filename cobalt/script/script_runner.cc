b85e1444d2328f082ae9501490025334b07b63fe
Cobalt Team <cobalt-team@google.com>
2016-10-11 17:53:23 -0700

    Add core dump handler for script runner
    Change-Id: Ic51b879578be20c4da5b192a38c6e761513be5b0

--- cobalt/script/script_runner.cc
+++ cobalt/script/script_runner.cc
@@ -19,11 +19,50 @@
 #include "base/logging.h"
 #include "cobalt/script/global_environment.h"
 #include "cobalt/script/source_code.h"
+#if defined(OS_STARBOARD)
+#include "starboard/configuration.h"
+#if SB_HAS(CORE_DUMP_HANDLER_SUPPORT)
+#define HANDLE_CORE_DUMP
+#include "base/lazy_instance.h"
+#include "starboard/ps4/core_dump_handler.h"
+#endif  // SB_HAS(CORE_DUMP_HANDLER_SUPPORT)
+#endif  // defined(OS_STARBOARD)
 
 namespace cobalt {
 namespace script {
 
 namespace {
+
+#if defined(HANDLE_CORE_DUMP)
+
+class ScriptRunnerLog {
+ public:
+  ScriptRunnerLog() : success_count_(0), fail_count_(0) {
+    SbCoreDumpRegisterHandler(CoreDumpHandler, this);
+  }
+  ~ScriptRunnerLog() { SbCoreDumpUnregisterHandler(CoreDumpHandler, this); }
+
+  static void CoreDumpHandler(void* context) {
+    SbCoreDumpLogInteger(
+        "ScriptRunner successful executions",
+        static_cast<ScriptRunnerLog*>(context)->success_count_);
+    SbCoreDumpLogInteger("ScriptRunner failed executions",
+                         static_cast<ScriptRunnerLog*>(context)->fail_count_);
+  }
+
+  void IncrementSuccessCount() { success_count_++; }
+  void IncrementFailCount() { fail_count_++; }
+
+ private:
+  int success_count_;
+  int fail_count_;
+  DISALLOW_COPY_AND_ASSIGN(ScriptRunnerLog);
+};
+
+base::LazyInstance<ScriptRunnerLog> script_runner_log =
+    LAZY_INSTANCE_INITIALIZER;
+
+#endif  // defined(HANDLE_CORE_DUMP)
 
 class ScriptRunnerImpl : public ScriptRunner {
  public:
@@ -53,8 +92,14 @@ std::string ScriptRunnerImpl::Execute(
   std::string result;
   if (!global_environment_->EvaluateScript(source_code, &result)) {
     DLOG(WARNING) << "Failed to execute JavaScript: " << result;
+#if defined(HANDLE_CORE_DUMP)
+    script_runner_log.Get().IncrementFailCount();
+#endif
     return "";
   }
+#if defined(HANDLE_CORE_DUMP)
+  script_runner_log.Get().IncrementSuccessCount();
+#endif
   return result;
 }
 
