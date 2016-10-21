55e1b89ca571bbee57d1858924f7b9f5647d4bfe
sungho12.kim <sungho12.kim@samsung.com>
2016-10-21 10:41:20 +0900

    Support Tizen Port 1. Build Tizen wihtout touching source codes except :    add a line :      src/cobalt/build/gyp_cobalt    add files :      packaging/      src/cobalt/build/ninja/ninja-linux32.armv7l      src/starboard/egl_and_gles/egl_and_gles_system_tizen.gyp      src/starboard/tizen/ 2. add .lines and .tmp files in .gitignore 3. Add Public build - use public repo for tizen build 4. Use SpiderMonkey instead Of JSC
    Change-Id: Ic6595aa1e500f1d0e1d43e7b37189c72a1673b60

---
+++ src/starboard/tizen/armv7l/gyp_configuration.py
@@ -0,0 +1,53 @@
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Starboard Tizen armv7l platform configuration for gyp_cobalt."""
+
+import logging
+import os
+import sys
+
+import config.starboard
+
+
+
+def CreatePlatformConfig():
+  try:
+    return _PlatformConfig('tizen-armv7l')
+  except RuntimeError as e:
+    logging.critical(e)
+    return None
+
+
+class _PlatformConfig(config.starboard.PlatformConfigStarboard):
+  """Starboard Tizen Armv7l platform configuration."""
+
+  def __init__(self, platform):
+    super(_PlatformConfig, self).__init__(platform)
+
+  def GetVariables(self, configuration):
+    variables = super(_PlatformConfig, self).GetVariables(configuration)
+    variables.update({
+        'clang': 0,
+    })
+
+    return variables
+
+  def GetEnvironmentVariables(self):
+    env_variables = {
+        'CC': 'armv7l-tizen-linux-gnueabi-gcc',
+        'CXX': 'armv7l-tizen-linux-gnueabi-g++',
+        'CC_host': 'armv7l-tizen-linux-gnueabi-gcc',
+        'CXX_host': 'armv7l-tizen-linux-gnueabi-g++',
+    }
+    return env_variables
