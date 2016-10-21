d4c3e734865dee546782628f62dae66a98f266ba
sungho12.kim <sungho12.kim@samsung.com>
2016-10-21 10:41:20 +0900

    Support Tizen Port 1. Build Tizen wihtout touching source codes except :    add a line :      src/cobalt/build/gyp_cobalt    add files :      packaging/      src/cobalt/build/ninja/ninja-linux32.armv7l      src/starboard/egl_and_gles/egl_and_gles_system_tizen.gyp      src/starboard/tizen/ 2. add .lines and .tmp files in .gitignore 3. Add Public build - use public repo for tizen build 4. Use SpiderMonkey instead Of JSC
    Change-Id: Ic6595aa1e500f1d0e1d43e7b37189c72a1673b60

---
+++ packaging/README.md
@@ -0,0 +1,28 @@
+## Introduction
+
+cobalt tizen is a Cobalt engine port to tizen platform. The port
+implements starboard/tizen platform APIs.
+
+## Precondition
+
+1. Install gbs package
+  - Add Source list
+    $ sudo vi /etc/apt/sources.list
+      deb http://download.tizen.org/tools/latest-release/Ubuntu_14.04/ /
+    $ sudo apt-get update
+    $ sudo apt-get install gbs
+    $ gbs --version
+	
+## Building
+
+0. libavresample, libavresample-devel
+    $ mv libavresample-11.4-0.armv7l.rpm $HOME/GBS-ROOT-COBALT/local/repos/cobalt/armv7l/RPMS/
+	$ mv libavresample-devel-11.4-0.armv7l.rpm $HOME/GBS-ROOT-COBALT/local/repos/cobalt/armv7l/RPMS/
+      You should create directory or build is generated automatically this directory.
+
+1. Build commands:
+  * armv7l
+    $ gbs -c packaging/gbs.conf -v build -A armv7l -P profile.cobalt [options]
+    Detail option see "gbs --help" and "gbs -v build --help"
+
+cobalt tizen code uses the BSD license, see our `LICENSE` file.
---
+++ src/starboard/tizen/shared/main.cc
@@ -0,0 +1,43 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include <time.h>
+#include <stdio.h>
+
+#include "starboard/configuration.h"
+#include "starboard/shared/signal/crash_signals.h"
+#include "starboard/shared/signal/suspend_signals.h"
+
+void main2()
+{
+  printf("GDB Test - main2\n");
+}
+
+void main3()
+{
+  printf("GDB Test - main3\n");
+}
+
+int main(int argc, char** argv) {
+  printf("Hello Cobalt!\n");
+  main2();
+  main3(); 
+  tzset();
+  starboard::shared::signal::InstallCrashSignalHandlers();
+  starboard::shared::signal::InstallSuspendSignalHandlers();
+  starboard::shared::signal::UninstallSuspendSignalHandlers();
+  starboard::shared::signal::UninstallCrashSignalHandlers();
+  return 0;
+}
