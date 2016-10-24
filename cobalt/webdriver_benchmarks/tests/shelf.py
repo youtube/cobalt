e2e17f2d0028b8a4c5beeeee1a69eecd65f0745b
Cobalt Team <cobalt-team@google.com>
2016-10-24 11:00:59 -0700

    Layout Stats: Support recording duration
    When "h5vcc.system.recordStats=true" is set, the system
    will record the duration for each layout associated
    with an input event that happens.
    
    This result is then available as a JSON array in
    Event.Durations.*.*
    
    Hitting the "h5vcc.system.recordStats" field resets
    any recorded measurements.
    
    Additionaly, the "shelf.py" test fetches this result
    and logs it.
    
    Change-Id: I043a36027903ba17eb759fd291ff7901d84e7c7c

--- cobalt/webdriver_benchmarks/tests/shelf.py
+++ cobalt/webdriver_benchmarks/tests/shelf.py
@@ -7,10 +7,11 @@ from __future__ import print_function
 
 import os
 import sys
+import time
 
 # The parent directory is a module
-sys.path.insert(0, os.path.abspath(
-    os.path.dirname(os.path.dirname(__file__))))
+sys.path.insert(0, os.path.dirname(os.path.dirname(
+    os.path.realpath(__file__))))
 
 # pylint: disable=C6204,C6203
 import tv
@@ -23,6 +24,10 @@ keys = partial_layout_benchmark.ImportSeleniumModule("webdriver.common.keys")
 DEFAULT_SHELVES_COUNT = 10
 SHELF_ITEMS_COUNT = 10
 
+# Seconds to sleep between each shelf step
+# TODO Wait for pending animations/layouts to complete instead.
+SHELF_STEP_TIME_SLEEP_SECONDS = 0.5
+
 
 class ShelfTest(tv_testcase.TvTestCase):
 
@@ -30,15 +35,25 @@ class ShelfTest(tv_testcase.TvTestCase):
     self.load_tv()
     self.assert_displayed(tv.FOCUSED_SHELF)
 
+    print(str(self.get_webdriver().execute_script(
+        "h5vcc.system.recordStats = true")))
+
     for _ in xrange(DEFAULT_SHELVES_COUNT):
       self.send_keys(tv.FOCUSED_SHELF, keys.Keys.ARROW_DOWN)
       self.poll_until_found(tv.FOCUSED_SHELF)
       self.assert_displayed(tv.FOCUSED_SHELF_TITLE)
+      time.sleep(SHELF_STEP_TIME_SLEEP_SECONDS)
 
     for _ in xrange(SHELF_ITEMS_COUNT):
       self.send_keys(tv.FOCUSED_TILE, keys.Keys.ARROW_RIGHT)
       self.poll_until_found(tv.FOCUSED_TILE)
       self.assert_displayed(tv.FOCUSED_SHELF_TITLE)
+      time.sleep(SHELF_STEP_TIME_SLEEP_SECONDS)
+
+    print("ShelfTest event durations"
+          + str(self.get_webdriver().execute_script(
+              "return h5vcc.cVal.getValue("
+              "'Event.Durations.MainWebModule.KeyUp')")))
 
 if __name__ == "__main__":
   tv_testcase.main()
