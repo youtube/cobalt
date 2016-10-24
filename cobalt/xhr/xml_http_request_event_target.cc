a761fce7910afb4c3312ec651e96949e085d0eed
Cobalt Team <cobalt-team@google.com>
2016-10-24 16:46:06 -0700

    Clear the EventListenerScriptObject::Reference when set to null
    b/31903817
    
    Change-Id: Ia6bc35d9214d3b6559ad5a32adec672fd5c8170a

--- cobalt/xhr/xml_http_request_event_target.cc
+++ cobalt/xhr/xml_http_request_event_target.cc
@@ -64,37 +64,65 @@ XMLHttpRequestEventTarget::ontimeout() const {
 
 void XMLHttpRequestEventTarget::set_onabort(
     const EventListenerScriptObject& listener) {
-  onabort_listener_.emplace(this, listener);
+  if (listener.IsNull()) {
+    onabort_listener_ = base::nullopt;
+  } else {
+    onabort_listener_.emplace(this, listener);
+  }
   SetAttributeEventListener(base::Tokens::abort(), listener);
 }
 void XMLHttpRequestEventTarget::set_onerror(
     const EventListenerScriptObject& listener) {
-  onerror_listener_.emplace(this, listener);
+  if (listener.IsNull()) {
+    onerror_listener_ = base::nullopt;
+  } else {
+    onerror_listener_.emplace(this, listener);
+  }
   SetAttributeEventListener(base::Tokens::error(), listener);
 }
 void XMLHttpRequestEventTarget::set_onload(
     const EventListenerScriptObject& listener) {
-  onload_listener_.emplace(this, listener);
+  if (listener.IsNull()) {
+    onload_listener_ = base::nullopt;
+  } else {
+    onload_listener_.emplace(this, listener);
+  }
   SetAttributeEventListener(base::Tokens::load(), listener);
 }
 void XMLHttpRequestEventTarget::set_onloadend(
     const EventListenerScriptObject& listener) {
-  onloadend_listener_.emplace(this, listener);
+  if (listener.IsNull()) {
+    onloadend_listener_ = base::nullopt;
+  } else {
+    onloadend_listener_.emplace(this, listener);
+  }
   SetAttributeEventListener(base::Tokens::loadend(), listener);
 }
 void XMLHttpRequestEventTarget::set_onloadstart(
     const EventListenerScriptObject& listener) {
-  onloadstart_listener_.emplace(this, listener);
+  if (listener.IsNull()) {
+    onloadstart_listener_ = base::nullopt;
+  } else {
+    onloadstart_listener_.emplace(this, listener);
+  }
   SetAttributeEventListener(base::Tokens::loadstart(), listener);
 }
 void XMLHttpRequestEventTarget::set_onprogress(
     const EventListenerScriptObject& listener) {
-  onprogress_listener_.emplace(this, listener);
+  if (listener.IsNull()) {
+    onprogress_listener_ = base::nullopt;
+  } else {
+    onprogress_listener_.emplace(this, listener);
+  }
   SetAttributeEventListener(base::Tokens::progress(), listener);
 }
 void XMLHttpRequestEventTarget::set_ontimeout(
     const EventListenerScriptObject& listener) {
-  ontimeout_listener_.emplace(this, listener);
+  if (listener.IsNull()) {
+    ontimeout_listener_ = base::nullopt;
+  } else {
+    ontimeout_listener_.emplace(this, listener);
+  }
   SetAttributeEventListener(base::Tokens::timeout(), listener);
 }
 }  // namespace xhr
