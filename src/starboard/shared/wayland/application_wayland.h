c211afcaf4de8f960a122373d596579304bc89ed
sungho12.kim <sungho12.kim@samsung.com>
2016-10-26 15:43:29 +0900

    Implement Wayland Patch
    Change-Id: I2a9964abca0bdb11dc1066fa105c43f2762e96e7

---
+++ src/starboard/shared/wayland/application_wayland.h
@@ -0,0 +1,58 @@
+// Copyright 2016 Samsung Electronics. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef STARBOARD_TIZEN_APPLICATION_WAYLAND_H_
+#define STARBOARD_TIZEN_APPLICATION_WAYLAND_H_
+
+#include "starboard/configuration.h"
+#include "starboard/shared/internal_only.h"
+#include "starboard/shared/starboard/application.h"
+#include "starboard/shared/starboard/queue_application.h"
+#include "starboard/types.h"
+#include "starboard/window.h"
+
+namespace starboard {
+namespace shared {
+namespace wayland {
+
+class ApplicationWayland : public shared::starboard::QueueApplication {
+ public:
+  ApplicationWayland();
+  ~ApplicationWayland() SB_OVERRIDE;
+
+  static ApplicationWayland* Get() {
+    return static_cast<ApplicationWayland*>(
+        shared::starboard::Application::Get());
+  }
+
+  SbWindow CreateWindow(const SbWindowOptions* options);
+  bool DestroyWindow(SbWindow window);
+
+ protected:
+  // --- Application overrides ---
+  void Initialize() SB_OVERRIDE;
+  void Teardown() SB_OVERRIDE;
+
+  // --- QueueApplication overrides ---
+  bool MayHaveSystemEvents() SB_OVERRIDE;
+  Event* PollNextSystemEvent() SB_OVERRIDE;
+  Event* WaitForSystemEventWithTimeout(SbTime time) SB_OVERRIDE;
+  void WakeSystemEventWait() SB_OVERRIDE;
+};
+
+}  // namespace wayland
+}  // namespace tizen
+}  // namespace starboard
+
+#endif  // STARBOARD_TIZEN_APPLICATION_WAYLAND_H_
