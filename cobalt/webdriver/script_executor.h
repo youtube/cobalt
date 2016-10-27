36df051a8624dff96da8cff5047a182bb2a3a33c
Cobalt Team <cobalt-team@google.com>
2016-10-27 16:53:11 -0700

    Implement WebDriver execute_async command
    Change-Id: I75d01ad0085e5a973c92a498fb3471daefc94a53

--- cobalt/webdriver/script_executor.h
+++ cobalt/webdriver/script_executor.h
@@ -31,6 +31,8 @@
 #include "cobalt/script/wrappable.h"
 #include "cobalt/webdriver/element_mapping.h"
 #include "cobalt/webdriver/protocol/element_id.h"
+#include "cobalt/webdriver/script_executor_params.h"
+#include "cobalt/webdriver/script_executor_result.h"
 
 namespace cobalt {
 namespace webdriver {
@@ -40,14 +42,32 @@ class ScriptExecutor :
     public base::SupportsWeakPtr<ScriptExecutor>,
     public script::Wrappable {
  public:
-  typedef script::CallbackFunction<std::string(
-      const script::OpaqueHandleHolder*, const std::string&)>
-      ExecuteFunctionCallback;
+  typedef script::CallbackFunction<void(
+      const scoped_refptr<ScriptExecutorParams>&,
+      const scoped_refptr<ScriptExecutorResult>&)> ExecuteFunctionCallback;
   typedef script::ScriptObject<ExecuteFunctionCallback>
       ExecuteFunctionCallbackHolder;
 
-  explicit ScriptExecutor(ElementMapping* mapping)
-      : element_mapping_(mapping) {}
+  // This can be called on any thread to preload the webdriver javascript code.
+  // If this is not called, then it will be lazily loaded the first time a
+  // ScriptExecutor is created, which will be the Web Module thread in most
+  // cases.
+  static void LoadExecutorSourceCode();
+
+  // Create a new ScriptExecutor instance.
+  static scoped_refptr<ScriptExecutor> Create(
+      ElementMapping* element_mapping,
+      const scoped_refptr<script::GlobalEnvironment>& global_environment);
+
+  // Calls the function set to the executeScriptHarness property with the
+  // provided |param|. The result of script execution will be passed to the
+  // caller through |result_handler|.
+  // Returns false on execution failure.
+  bool Execute(const scoped_refptr<ScriptExecutorParams>& params,
+               ScriptExecutorResult::ResultHandler* result_handler);
+
+  // ScriptExecutor bindings implementation.
+  //
 
   void set_execute_script_harness(
       const ExecuteFunctionCallbackHolder& callback);
@@ -56,19 +76,19 @@ class ScriptExecutor :
   scoped_refptr<dom::Element> IdToElement(const std::string& id);
   std::string ElementToId(const scoped_refptr<dom::Element>& id);
 
-  // Calls the function set to the executeScriptHarness property with the
-  // provided |function_body| and |json_arguments|. Returns a JSON
-  // serialized result string, of base::nullopt on execution failure.
-  base::optional<std::string> Execute(
-      const script::OpaqueHandleHolder* function_object,
-      const std::string& json_arguments);
-
   DEFINE_WRAPPABLE_TYPE(ScriptExecutor);
 
  private:
+  explicit ScriptExecutor(ElementMapping* mapping)
+      : element_mapping_(mapping) {}
+
+  bool ExecuteInternal(
+      const scoped_refptr<ScriptExecutorParams>& params,
+      const scoped_refptr<ScriptExecutorResult>& result_handler);
+
   base::ThreadChecker thread_checker_;
   ElementMapping* element_mapping_;
-  base::optional<ExecuteFunctionCallbackHolder::Reference> callback_;
+  base::optional<ExecuteFunctionCallbackHolder::Reference> execute_callback_;
 };
 
 }  // namespace webdriver
