9061e5484f7481c3ef74b81a7eb457e552572770
Cobalt Team <cobalt-team@google.com>
2016-10-27 17:01:03 -0700

    Fix string poems.
    This CL fixes string poems for strncpy and strncat.  Tests for those
    poems were also added in string_poem_tests.cc.
    
    Fixes b/32460561
    
    Change-Id: Id8ff0bbb23a0a8f4815fae240c75a3fd53fcf2e3

--- starboard/client_porting/poem/string_poem.h
+++ starboard/client_porting/poem/string_poem.h
@@ -19,8 +19,9 @@
 
 #if defined(STARBOARD)
 
-#include "starboard/string.h"
+#include "starboard/log.h"
 #include "starboard/memory.h"
+#include "starboard/string.h"
 
 #ifdef __cplusplus
 
@@ -31,7 +32,7 @@
 // Finds the last occurrence of |character| in |str|, returning a pointer to
 // the found character in the given string, or NULL if not found.
 // Meant to be a drop-in replacement for strchr, C++ signature
-inline char* PoemFindLastCharacter(char* str, int character) {
+inline char* PoemFindLastCharacterInString(char* str, int character) {
   const char* const_str = static_cast<const char*>(str);
   const char c = static_cast<char>(character);
   return const_cast<char*>(SbStringFindLastCharacter(const_str, c));
@@ -40,7 +41,8 @@ inline char* PoemFindLastCharacter(char* str, int character) {
 // Finds the last occurrence of |character| in |str|, returning a pointer to
 // the found character in the given string, or NULL if not found.
 // Meant to be a drop-in replacement for strchr, C++ signature
-inline const char* PoemFindLastCharacter(const char* str, int character) {
+inline const char* PoemFindLastCharacterInString(const char* str,
+                                                 int character) {
   const char c = static_cast<char>(character);
   return SbStringFindLastCharacter(str, c);
 }
@@ -48,7 +50,7 @@ inline const char* PoemFindLastCharacter(const char* str, int character) {
 // Finds the first occurrence of |character| in |str|, returning a pointer to
 // the found character in the given string, or NULL if not found.
 // Meant to be a drop-in replacement for strchr
-inline char* PoemFindCharacter(char* str, int character) {
+inline char* PoemFindCharacterInString(char* str, int character) {
   const char* const_str = static_cast<const char*>(str);
   const char c = static_cast<char>(character);
   return const_cast<char*>(SbStringFindCharacter(const_str, c));
@@ -57,7 +59,7 @@ inline char* PoemFindCharacter(char* str, int character) {
 // Finds the first occurrence of |character| in |str|, returning a pointer to
 // the found character in the given string, or NULL if not found.
 // Meant to be a drop-in replacement for strchr
-inline const char* PoemFindCharacter(const char* str, int character) {
+inline const char* PoemFindCharacterInString(const char* str, int character) {
   const char c = static_cast<char>(character);
   return SbStringFindCharacter(str, c);
 }
@@ -67,7 +69,8 @@ inline const char* PoemFindCharacter(const char* str, int character) {
 // Finds the first occurrence of |character| in |str|, returning a pointer to
 // the found character in the given string, or NULL if not found.
 // Meant to be a drop-in replacement for strchr
-static SB_C_INLINE char* PoemFindCharacter(const char* str, int character) {
+static SB_C_INLINE char* PoemFindCharacterInString(const char* str,
+                                                   int character) {
   // C-style cast used for C code
   return (char*)(SbStringFindCharacter(str, character));
 }
@@ -75,7 +78,8 @@ static SB_C_INLINE char* PoemFindCharacter(const char* str, int character) {
 // Finds the last occurrence of |character| in |str|, returning a pointer to
 // the found character in the given string, or NULL if not found.
 // Meant to be a drop-in replacement for strchr
-static SB_C_INLINE char* PoemFindLastCharacter(const char* str, int character) {
+static SB_C_INLINE char* PoemFindLastCharacterInString(const char* str,
+                                                       int character) {
   // C-style cast used for C code
   return (char*)(SbStringFindLastCharacter(str, character));
 }
@@ -86,21 +90,60 @@ extern "C" {
 #endif
 
 // Concatenates |source| onto the end of |out_destination|, presuming it has
-// |destination_size| total characters of storage available. Returns
-// |out_destination|.  This method is a drop-in replacement for strncat
-static SB_C_INLINE char* PoemConcat(char* out_destination,
-                                    const char* source,
-                                    int destination_size) {
-  SbStringConcat(out_destination, source, destination_size);
+// atleast strlen(out_destination) + |num_chars_to_copy| + 1 total characters of
+// storage available. Returns |out_destination|.  This method is a drop-in
+// replacement for strncat.
+// Note: even if num_chars_to_copy == 0, we will still write a NULL character.
+// This is consistent with the language of linux's strncat man page:
+// "Therefore, the size of dest must be at least strlen(dest)+n+1."
+static SB_C_INLINE char* PoemStringConcat(char* out_destination,
+                                          const char* source,
+                                          int num_chars_to_copy) {
+  int destination_length = (int)(SbStringGetLength(out_destination));
+  SbStringConcat(out_destination, source,
+                 destination_length + num_chars_to_copy + 1);
   return out_destination;
 }
 
-// Inline wrapper for an unsafe PoemConcat that assumes |out_destination| is
-// big enough. Returns |out_destination|.  Meant to be a drop-in replacement for
-// strcat.
-static SB_C_INLINE char* PoemConcatUnsafe(char* out_destination,
-                                          const char* source) {
-  return PoemConcat(out_destination, source, INT_MAX);
+// Inline wrapper for an unsafe PoemStringConcat that assumes |out_destination|
+// is big enough. Returns |out_destination|.  Meant to be a drop-in replacement
+// for strcat.
+static SB_C_INLINE char* PoemStringConcatUnsafe(char* out_destination,
+                                                const char* source) {
+  return PoemStringConcat(out_destination, source, INT_MAX);
+}
+
+// Inline wrapper for a drop-in replacement for |strncpy|.  This function
+// copies the null terminated string from src to dest.  If the src string is
+// shorter than num_chars_to_copy, then null padding is used.
+// Warning: As with strncpy spec, if there is no null character within the first
+// num_chars_to_copy in src, this function will write a null character at the
+// end.
+static SB_C_INLINE char* PoemStringCopyN(char* dest,
+                                         const char* src,
+                                         int num_chars_to_copy) {
+  SB_DCHECK(num_chars_to_copy >= 0);
+  if (num_chars_to_copy < 0) {
+    return dest;
+  }
+
+  char* dest_write_iterator = dest;
+  char* dest_write_iterator_end = dest + num_chars_to_copy;
+  const char* src_iterator = src;
+
+  while ((*src_iterator != '\0') &&
+         (dest_write_iterator != dest_write_iterator_end)) {
+    *dest_write_iterator = *src_iterator;
+
+    ++src_iterator;
+    ++dest_write_iterator;
+  }
+
+  SB_DCHECK(dest_write_iterator_end >= dest_write_iterator);
+  SbMemorySet(dest_write_iterator, '\0',
+              dest_write_iterator_end - dest_write_iterator);
+
+  return dest;
 }
 
 #ifdef __cplusplus
@@ -111,12 +154,12 @@ static SB_C_INLINE char* PoemConcatUnsafe(char* out_destination,
 
 #define strlen(s) SbStringGetLength(s)
 #define strcpy(o, s) SbStringCopyUnsafe(o, s)
-#define strncpy(o, s, ds) SbStringCopy(o, s, ds)
-#define strcat(o, s) PoemConcatUnsafe(o, s)
-#define strncat(o, s, ds) PoemConcat(o, s, ds)
+#define strncpy(o, s, ds) PoemStringCopyN(o, s, ds)
+#define strcat(o, s) PoemStringConcatUnsafe(o, s)
+#define strncat(o, s, ds) PoemStringConcat(o, s, ds)
 #define strdup(s) SbStringDuplicate(s)
-#define strchr(s, c) PoemFindCharacter(s, c)
-#define strrchr(s, c) PoemFindLastCharacter(s, c)
+#define strchr(s, c) PoemFindCharacterInString(s, c)
+#define strrchr(s, c) PoemFindLastCharacterInString(s, c)
 #define strstr(s, c) SbStringFindString(s, c)
 #define strncmp(s1, s2, c) SbStringCompare(s1, s2, c)
 #define strcmp(s1, s2) SbStringCompareAll(s1, s2)
