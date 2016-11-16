b65c5802a53b204274fe3b45ff0a4c2c372d324b
Cobalt Team <cobalt-team@google.com>
2016-11-16 15:12:13 -0800

    Beginnings of SbSpeechSynthesis abstraction
    Starboard text-to-speech interface for WAI-ARIA/Audio accessibility
    
    Change-Id: I38934b34f68bcc8efd55adf06bc6b6c6b1b79acb

--- starboard/stub/configuration_public.h
+++ starboard/stub/configuration_public.h
@@ -128,6 +128,9 @@
 // Whether the current platform has microphone supported.
 #define SB_HAS_MICROPHONE 1
 
+// Whether the current platform has speech synthesis.
+#define SB_HAS_SPEECH_SYNTHESIS 1
+
 // Type detection for wchar_t.
 #if defined(__WCHAR_MAX__) && \
     (__WCHAR_MAX__ == 0x7fffffff || __WCHAR_MAX__ == 0xffffffff)
