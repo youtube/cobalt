60f24b7ed537f896f03ce9571fb26b4251ceaabd
Cobalt Team <cobalt-team@google.com>
2016-11-22 17:28:34 -0800

    Corrupt saved file robustness
    Previously:
     Only certain types of invalid save data (a bad header version)
     would have resulted in fallback to the default save file
    
     Buffer overruns were easily possible when deserializing corrupt
     VirtualFileSystems
    Now:
     Neither of the above are the case
    
    Specifically:
    
    - When VirtualFileSystem deserializes each file, it checkes for
      buffer out of bounds
    - When any deserialization error happens, VirtualFileSystem::Deserialize
      returns false
    - When ever that happens, we fall back to a default saved game.
    
    Part of b/32831230
    
    Change-Id: Iaec64557f5547d8bf18d668f917a38324379d557

--- cobalt/storage/virtual_file_system_test.cc
+++ cobalt/storage/virtual_file_system_test.cc
@@ -21,6 +21,8 @@
 #include "base/memory/scoped_ptr.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#include "starboard/client_porting/poem/string_poem.h"
+
 namespace cobalt {
 namespace storage {
 
@@ -125,7 +127,7 @@ TEST_F(VirtualFileSystemTest, SerializeDeserialize) {
 
   // Deserialize the data into a new vfs
   VirtualFileSystem new_vfs;
-  new_vfs.Deserialize(buffer.get(), bytes);
+  ASSERT_TRUE(new_vfs.Deserialize(buffer.get(), bytes));
 
   // Make sure the new vfs contains all the expected data.
   char file_contents[VirtualFile::kMaxVfsPathname];
@@ -140,6 +142,62 @@ TEST_F(VirtualFileSystemTest, SerializeDeserialize) {
   bytes = file->Read(file_contents, sizeof(file_contents), 0);
   EXPECT_EQ(data2_size, bytes);
   EXPECT_EQ(0, memcmp(file_contents, data2, static_cast<size_t>(bytes)));
+}
+
+TEST_F(VirtualFileSystemTest, DeserializeTruncated) {
+  // Create a few files and write some data
+  VirtualFile* file = vfs_.Open("file1.tmp");
+  EXPECT_TRUE(file != NULL);
+  const char data1[] = "abc";
+  int data1_size = 3;
+  file->Write(data1, data1_size, 0);
+
+  file = vfs_.Open("file2.tmp");
+  EXPECT_TRUE(file != NULL);
+  const char data2[] = "defg";
+  int data2_size = 4;
+  file->Write(data2, data2_size, 0);
+
+  // First perform a dry run to figure out how much space we need.
+  int bytes = vfs_.Serialize(NULL, true /*dry run*/);
+  scoped_array<uint8> buffer(new uint8[static_cast<size_t>(bytes)]);
+
+  // Now serialize and deserialize
+  vfs_.Serialize(buffer.get(), false /*dry run*/);
+
+  for (int i = 1; i < bytes; i++) {
+    // Corrupt the header
+    VirtualFileSystem::SerializedHeader header;
+    memcpy(&header, buffer.get(), sizeof(header));
+    header.file_size = header.file_size - i;
+    memcpy(buffer.get(), &header, sizeof(header));
+
+    // Deserialize the data into a new vfs
+    VirtualFileSystem new_vfs;
+    EXPECT_FALSE(new_vfs.Deserialize(buffer.get(), bytes - i));
+  }
+}
+
+TEST_F(VirtualFileSystemTest, DeserializeBadData) {
+  scoped_array<uint8> buffer(new uint8[0]);
+  EXPECT_EQ(false, vfs_.Deserialize(buffer.get(), 0));
+  EXPECT_EQ(false, vfs_.Deserialize(buffer.get(), -1));
+  buffer.reset(new uint8[1]);
+  EXPECT_EQ(false, vfs_.Deserialize(buffer.get(), 1));
+  buffer.reset(new uint8[sizeof(VirtualFileSystem::SerializedHeader)]);
+  VirtualFileSystem::SerializedHeader header = {};
+  header.version = -1;
+  memcpy(buffer.get(), &header, sizeof(header));
+  EXPECT_EQ(false,
+            vfs_.Deserialize(buffer.get(),
+                             sizeof(VirtualFileSystem::SerializedHeader)));
+  memcpy(&(header.version), "SAV0", sizeof(header.version));
+  header.file_size = sizeof(VirtualFileSystem::SerializedHeader);
+  memcpy(buffer.get(), &header, sizeof(header));
+  EXPECT_EQ(true,
+            vfs_.Deserialize(buffer.get(),
+                             sizeof(VirtualFileSystem::SerializedHeader)));
+  ASSERT_EQ(0, vfs_.ListFiles().size());
 }
 
 TEST_F(VirtualFileSystemTest, GetHeaderVersion) {
