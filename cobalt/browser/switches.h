ed48717c8e23eebb5cc923670adb644494b23abe
Cobalt Team <cobalt-team@google.com>
2016-11-28 11:01:23 -0800

    Enables the memory tracker from the command line.
    After this cl, -memory_tracker will enable the memory tracker
    and start outputting memory statistics to the console.
    
    Change-Id: I5c9d46038499bb0ab9aaa4c93a30b5228452aedb

--- cobalt/browser/switches.h
+++ cobalt/browser/switches.h
@@ -45,6 +45,7 @@ extern const char kVideoContainerSizeOverride[];
 extern const char kVideoDecoderStub[];
 extern const char kWebDriverPort[];
 extern const char kWebDriverListenIp[];
+extern const char kMemoryTracker[];
 #endif  // ENABLE_DEBUG_COMMAND_LINE_SWITCHES
 
 extern const char kImageCacheSizeInBytes[];
