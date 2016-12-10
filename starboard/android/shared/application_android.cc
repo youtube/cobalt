ad8336bbc3543f8d31b375e24f6e48261464d663
Cobalt Team <cobalt-team@google.com>
2016-12-09 23:34:22 -0800

    Use NDK standalone toolchain to build Android
    This just compiles and links, but doesn't run.
    - Stubs used for audio, a/v codec, and window
    - Use iso instead of dlmalloc memory (not tested)
    - Dummy main and stub application to link test executables
    - Custom stub for socket_get_local_interface_address
    - Custom stub for system_get_stack
    
    Change-Id: Ice4efd922ee9d66af46b89fc6fce0c44d06ed575

---
+++ starboard/android/shared/application_android.cc
@@ -0,0 +1,61 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/android/shared/application_android.h"
+
+#include "starboard/event.h"
+#include "starboard/log.h"
+
+namespace starboard {
+namespace android {
+
+ApplicationAndroid::ApplicationAndroid() {
+  SB_NOTIMPLEMENTED();
+}
+
+ApplicationAndroid::~ApplicationAndroid() {
+  SB_NOTIMPLEMENTED();
+}
+
+void ApplicationAndroid::Initialize() {
+  SB_NOTIMPLEMENTED();
+}
+
+void ApplicationAndroid::Teardown() {
+  SB_NOTIMPLEMENTED();
+}
+
+bool ApplicationAndroid::MayHaveSystemEvents() {
+  SB_NOTIMPLEMENTED();
+  return false;
+}
+
+shared::starboard::Application::Event*
+    ApplicationAndroid::PollNextSystemEvent() {
+  SB_NOTIMPLEMENTED();
+  return NULL;
+}
+
+shared::starboard::Application::Event*
+ApplicationAndroid::WaitForSystemEventWithTimeout(SbTime time) {
+  SB_NOTIMPLEMENTED();
+  return NULL;
+}
+
+void ApplicationAndroid::WakeSystemEventWait() {
+  SB_NOTIMPLEMENTED();
+}
+
+}  // namespace android
+}  // namespace starboard
