f0058e47889500e73313d917db2bb37be9f56a39
Cobalt Team <cobalt-team@google.com>
2016-12-15 11:32:48 -0800

    Forward more raw memory calls to Starboard
    b/33645161
    
    Another couple of files from third-party code that seem to have slipped
    through the cracks.
    
    Change-Id: I54ff22a2e3a8be346808824ffa82ab08d6466157

--- third_party/libwebp/enc/token.c
+++ third_party/libwebp/enc/token.c
@@ -16,9 +16,13 @@
 //
 // Author: Skal (pascal.massimino@gmail.com)
 
+#if defined(STARBOARD)
+#include "third_party/libwebp/starboard_private.h"
+#else
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#endif
 
 #include "./vp8enci.h"
 
