eeb8ee939eefb3e1e94658a0eb1db0c8617984fd
Andy Diamondstein <adiamondstein@google.com>
2016-12-20 16:57:02 -0500

    Update test-links.js

--- js/test-links.js
+++ js/test-links.js
@@ -18,8 +18,8 @@ $(document).ready(function() {
   /**
    * Rewrite links to serve correctly on cobalt.foo.
    */
-  var letsPretend = true;
-  if (window.location.hostname.indexOf('cobalt.foo') !== -1 || letsPretend == true) { 
+  var letsPretend = False;
+  if (window.location.hostname.indexOf('cobalt.foo') !== -1 || letsPretend == True) { 
     $('a').each(function() {
       var href = this.href;
       console.log(this.href);
