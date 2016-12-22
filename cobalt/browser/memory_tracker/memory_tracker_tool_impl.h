57c03ea434b959315f187bc2050c9c9cf0edff25
Cobalt Team <cobalt-team@google.com>
2016-12-21 18:20:28 -0800

    Adds allocation logger for legacy toolset.
    This allows cobalt to dump out an allocation log for lb_shell tools.
    
    Change-Id: I160593c3414a7d09d5734c8b5f18656944e6959e

--- cobalt/browser/memory_tracker/memory_tracker_tool_impl.h
+++ cobalt/browser/memory_tracker/memory_tracker_tool_impl.h
@@ -17,15 +17,25 @@
 #ifndef COBALT_BROWSER_MEMORY_TRACKER_MEMORY_TRACKER_TOOL_IMPL_H_
 #define COBALT_BROWSER_MEMORY_TRACKER_MEMORY_TRACKER_TOOL_IMPL_H_
 
+#include <deque>
 #include <map>
 #include <string>
 #include <vector>
 
 #include "base/compiler_specific.h"
+#include "base/debug/stack_trace.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/threading/simple_thread.h"
 #include "base/time.h"
+#include "cobalt/browser/memory_tracker/buffered_file_writer.h"
 #include "nb/analytics/memory_tracker.h"
+#include "nb/analytics/memory_tracker_helpers.h"
+#include "starboard/memory_reporter.h"
+
+namespace starboard {
+class ScopedFile;
+}  // namespace starboard
 
 namespace nb {
 namespace analytics {
@@ -292,6 +302,42 @@ class FindTopSizes : public nb::analytics::AllocationVisitor {
   size_t maximum_size_;
   const nb::analytics::AllocationGroup* group_filter_;
   SizeCounterMap size_counter_;
+};
+
+// Outputs memory_log.txt to the output log location. This log contains
+// allocations with a stack trace (non-symbolized) and deallocations without
+// a stack.
+class MemoryTrackerLogWriter : public AbstractMemoryTrackerTool {
+ public:
+  MemoryTrackerLogWriter();
+  virtual ~MemoryTrackerLogWriter();
+
+  // Interface AbstrctMemoryTrackerTool
+  virtual std::string tool_name() const OVERRIDE;
+  virtual void Run(Params* params) OVERRIDE;
+
+  void OnMemoryAllocation(const void* memory_block, size_t size);
+  void OnMemoryDeallocation(const void* memory_block);
+
+ private:
+  // Callbacks for MemoryReporter
+  static void OnAlloc(void* context, const void* memory, size_t size);
+  static void OnDealloc(void* context, const void* memory);
+  static void OnMapMemory(void* context, const void* memory, size_t size);
+  static void OnUnMapMemory(void* context, const void* memory, size_t size);
+  static std::string MemoryLogPath();
+
+  static base::TimeTicks NowTime();
+  static const size_t kStartIndex = 5;
+  static const size_t kNumAddressPrints = 1;
+  static const size_t kMaxStackSize = 10;
+
+  int GetTimeSinceStartMs() const;
+  void InitAndRegisterMemoryReporter();
+
+  base::TimeTicks start_time_;
+  scoped_ptr<SbMemoryReporter> memory_reporter_;
+  scoped_ptr<BufferedFileWriter> buffered_file_writer_;
 };
 
 }  // namespace memory_tracker
