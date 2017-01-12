3dc9564eb574b54cbe0eb78fe93b24e6d9fec1ea
Jelle Foks <jfoks@google.com>
2017-01-12 15:07:24 -0800

    Change for move of GetToolchainsDir to gyp_utils.
    This adapts for the move of GetToolchainsDir from config.base to
    gyp_utils.
    
    Change-Id: If81553c6674e0a9cc51132172d7654bdf87e407c

--- starboard/linux/x64x11/gcc/4.4/gyp_configuration.py
+++ starboard/linux/x64x11/gcc/4.4/gyp_configuration.py
@@ -23,6 +23,7 @@ sys.path.append(
         os.path.join(
             os.path.dirname(__file__), os.pardir, os.pardir, os.pardir,
             'shared')))
+# pylint: disable=import-self,g-import-not-at-top
 import gyp_configuration as shared_configuration
 
 
