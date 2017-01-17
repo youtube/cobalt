876781436a238f2174d16f73bcda3713358dab8a
Cobalt Team <cobalt-team@google.com>
2017-01-17 09:56:03 -0800

    Fix HTMLImageElement-related memory leak.
    The web app was often pinging stats-related URLs by fetching images
    from them.  The returned data was not actually an image and so the
    image decoder would return a failure.  However on image decode
    failure, we don't signal javascript that there was an error, and
    so JavaScript is never made aware that the fetch is complete and that
    it should clean up its images.
    
    We now also log a warning for all image resource load errors, since
    it is not an error from Cobalt's perspective, just from the web
    app's perspective.  This will reduce the amount of log error noise.
    
    Change-Id: I27e26a838322508ad9b6b0f2b574a3c4cb7665fa

--- cobalt/dom/html_image_element.cc
+++ cobalt/dom/html_image_element.cc
@@ -144,8 +144,6 @@ void HTMLImageElement::UpdateImageData() {
       new loader::image::CachedImage::OnLoadedCallbackHandler(
           cached_image, base::Bind(&HTMLImageElement::OnLoadingSuccess,
                                    base::Unretained(this)),
-          base::Bind(&HTMLImageElement::OnLoadingFailure,
-                     base::Unretained(this)),
           base::Bind(&HTMLImageElement::OnLoadingError,
                      base::Unretained(this))));
   node_document()->IncreaseLoadingCounter();
@@ -154,16 +152,6 @@ void HTMLImageElement::UpdateImageData() {
 void HTMLImageElement::OnLoadingSuccess() {
   TRACE_EVENT0("cobalt::dom", "HTMLImageElement::OnLoadingSuccess()");
   AllowGarbageCollectionAfterEventIsDispatched(base::Tokens::load());
-  if (node_document()) {
-    node_document()->DecreaseLoadingCounterAndMaybeDispatchLoadEvent();
-  }
-  cached_image_loaded_callback_handler_.reset();
-}
-
-void HTMLImageElement::OnLoadingFailure() {
-  TRACE_EVENT0("cobalt::dom", "HTMLImageElement::OnLoadingFailure()");
-  // No event is dispatched.
-  AllowGarbageCollection();
   if (node_document()) {
     node_document()->DecreaseLoadingCounterAndMaybeDispatchLoadEvent();
   }
