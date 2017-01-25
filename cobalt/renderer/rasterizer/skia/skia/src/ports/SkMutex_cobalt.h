56456f1f62bc4a01136a2bd1dc1b362e50b5863f
Xiaoming Shi <xiaomings@google.com>
2017-01-25 15:46:43 -0800

    Do not destroy static SkBaseMutex on exit.
    As other static objects may still use them after their dtors are called.
    Keep them around can avoid crash on exit.
    
    b/34690959
    
    Change-Id: Ic7bf9302922f5d4d2b59a495123443afcfb244b1

--- cobalt/renderer/rasterizer/skia/skia/src/ports/SkMutex_cobalt.h
+++ cobalt/renderer/rasterizer/skia/skia/src/ports/SkMutex_cobalt.h
@@ -16,7 +16,21 @@
 // so that we can forward-declare cleanly.
 class SkBaseMutex {
  public:
-  SkBaseMutex() {}
+  enum Leaky { kLeaky };
+  SkBaseMutex() : lock_(*new (buffer_) base::Lock), leaky_(false) {}
+  // Objects constructed using this ctor will not destroy its underlying Lock
+  // during destructing.  This is because many SkBaseMutex are statically
+  // constructed along with other static objects in Skia.  As the order of
+  // static initialization is undeterminated it is possible that a mutex is
+  // destroyed before the object that relies on it has been destroyed, which
+  // leads to crash on exit.
+  explicit SkBaseMutex(Leaky)
+      : lock_(*new (buffer_) base::Lock), leaky_(true) {}
+  ~SkBaseMutex() {
+    if (!leaky_) {
+      lock_.~Lock();
+    }
+  }
 
   void acquire() { lock_.Acquire(); }
 
@@ -28,7 +42,9 @@ class SkBaseMutex {
   SkBaseMutex(const SkBaseMutex&);
   SkBaseMutex& operator=(const SkBaseMutex&);
 
-  base::Lock lock_;
+  uint8_t buffer_[sizeof(base::Lock)];
+  base::Lock& lock_;
+  bool leaky_;
 };
 
 class SkMutex : public SkBaseMutex {};
@@ -37,6 +53,6 @@ class SkMutex : public SkBaseMutex {};
 // As a result, it is illegal to SK_DECLARE_STATIC_MUTEX in a function.
 #define SK_DECLARE_STATIC_MUTEX(name) \
   namespace {}                        \
-  static SkBaseMutex name  // NOLINT(build/namespaces)
+  static SkBaseMutex name(SkBaseMutex::kLeaky)  // NOLINT(build/namespaces)
 
 #endif  // COBALT_RENDERER_RASTERIZER_SKIA_SKIA_SRC_PORTS_SKMUTEX_COBALT_H_
