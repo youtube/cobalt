18ac8916b47c5eb39918a8d1917ed2dc1b124441
Cobalt Team <cobalt-team@google.com>
2017-01-26 14:11:25 -0800

    Create a linux-x64x11-lib platform for building Cobalt into a shared library.
    This can be linked into the Youtube VR engine for running Cobalt in the
    YouTube VR app. This is is just a starting shell at this point for
    creating something that can be built, linked into a ***REMOVED***-built
    cc_binary and run.
    
    The intention is for most of the actual exported logic to be shared across the
    platforms we would care about integrating with the YouTube VR engine in the
    forseeable future (Windows, Xbox 2/Scorpio (?), Linux), but for now
    it is kept entirely within Linux until we have another platform
    directory to add to.
    
    Change-Id: I2cddbf8c08d9106b5dac42f89695dbe1ffc8f434

--- starboard/linux/x64x11/atomic_public.h
+++ starboard/linux/x64x11/atomic_public.h
@@ -14,4 +14,9 @@
  * limitations under the License.
  */
 
+#ifndef STARBOARD_LINUX_X64X11_ATOMIC_PUBLIC_H_
+#define STARBOARD_LINUX_X64X11_ATOMIC_PUBLIC_H_
+
 #include "starboard/linux/shared/atomic_public.h"
+
+#endif  // STARBOARD_LINUX_X64X11_ATOMIC_PUBLIC_H_
