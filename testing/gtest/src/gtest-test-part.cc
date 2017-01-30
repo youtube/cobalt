2f5e8fdc29a832504626f321876107c03c763691
Jelle Foks <jfoks@google.com>
2017-01-30 10:28:03 -0800

    Rebase gtest to upstream 1.7.0.
    This rebases gtest to upstream version used by current chromium base library.
    
    Change-Id: I1a083f08a170237576853da18743a3d55eb23966
    From: https://chromium.googlesource.com/external/github.com/google/googletest.git/+/6f8a66431cb592dad629028a50b3dd418a408c87

--- testing/gtest/src/gtest-test-part.cc
+++ testing/gtest/src/gtest-test-part.cc
@@ -35,9 +35,9 @@
 
 // Indicates that this translation unit is part of Google Test's
 // implementation.  It must come before gtest-internal-inl.h is
-// included, or there will be a compiler error.  This trick is to
-// prevent a user from accidentally including gtest-internal-inl.h in
-// his code.
+// included, or there will be a compiler error.  This trick exists to
+// prevent the accidental inclusion of gtest-internal-inl.h in the
+// user's code.
 #define GTEST_IMPLEMENTATION_ 1
 #include "src/gtest-internal-inl.h"
 #undef GTEST_IMPLEMENTATION_
@@ -72,8 +72,7 @@ void TestPartResultArray::Append(const TestPartResult& result) {
 // Returns the TestPartResult at the given index (0-based).
 const TestPartResult& TestPartResultArray::GetTestPartResult(int index) const {
   if (index < 0 || index >= size()) {
-    internal::posix::PrintF("\nInvalid index (%d) into TestPartResultArray.\n",
-                            index);
+    printf("\nInvalid index (%d) into TestPartResultArray.\n", index);
     internal::posix::Abort();
   }
 
