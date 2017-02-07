a6b8ca8343a069895f3a4947e84fc3156394b5dd
Cobalt Team <cobalt-team@google.com>
2017-02-07 14:01:02 -0800

    MutationObserver implementation
    - Queueing of mutation records from DOM mutation is
      not yet implemented.
    
    b/34891236
    
    Change-Id: I05663d0301e7b3641bac25de858d33bac51d348e

--- cobalt/dom/window.h
+++ cobalt/dom/window.h
@@ -34,6 +34,7 @@
 #include "cobalt/dom/event_target.h"
 #include "cobalt/dom/media_query_list.h"
 #include "cobalt/dom/media_source.h"
+#include "cobalt/dom/mutation_observer_task_manager.h"
 #include "cobalt/dom/parser.h"
 #include "cobalt/network_bridge/cookie_jar.h"
 #include "cobalt/network_bridge/net_poster.h"
