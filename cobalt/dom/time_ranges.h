84a112545dc9f13a6f927b0397d18e7e898ec8a2
Xiaoming Shi <xiaomings@google.com>
2017-02-21 11:58:25 -0800

    Refactor TimeRanges.
    Implement TimeRanges::Clone(), TimeRanges::UnionWith(), and
    TimeRanges::IntersectWith().  They are required by MediaSource
    implementation.
    
    b/35259101
    
    Change-Id: I66d9424b3ccadd33589b0ae41e6d6158d0451208

--- cobalt/dom/time_ranges.h
+++ cobalt/dom/time_ranges.h
@@ -30,7 +30,8 @@ namespace dom {
 //   https://www.w3.org/TR/html5/embedded-content-0.html#timeranges
 //
 // Note that in our implementation it is always normalized, i.e. all contained
-// ranges are sorted ascendantly and not overlapped.
+// ranges are sorted ascendantly and not overlapped.  The limits of the range
+// are [-Infinity, Infinity].
 class TimeRanges : public script::Wrappable {
  public:
   TimeRanges() {}
@@ -52,9 +53,22 @@ class TimeRanges : public script::Wrappable {
   // ranges that is closest to time.
   double Nearest(double time) const;
 
+  // Returns a copy of the current object.
+  scoped_refptr<TimeRanges> Clone() const;
+  // Returns a TimeRanges object contains the union of the current object and
+  // |that|.
+  scoped_refptr<TimeRanges> UnionWith(
+      const scoped_refptr<TimeRanges>& that) const;
+  // Returns a TimeRanges object contains the intersection of the current object
+  // and |that|.
+  scoped_refptr<TimeRanges> IntersectWith(
+      const scoped_refptr<TimeRanges>& that) const;
+
   DEFINE_WRAPPABLE_TYPE(TimeRanges);
 
  private:
+  scoped_refptr<TimeRanges> Invert() const;
+
   class TimeRange {
    public:
     TimeRange(double start, double end) : start_(start), end_(end) {}
