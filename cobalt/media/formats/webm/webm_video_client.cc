0319f56c07912c26c1de778e8f2ff166b2bf145d
Xiaoming Shi <xiaomings@google.com>
2017-02-28 12:15:15 -0800

    Refactor color metadata in media.
    This makes media2 work with the existing HDR implementation.
    
    b/35259101
    b/35847102
    
    Change-Id: If79167a8c25422fe912553888b55099f08ad9e7f

--- cobalt/media/formats/webm/webm_video_client.cc
+++ cobalt/media/formats/webm/webm_video_client.cc
@@ -87,8 +87,7 @@ bool WebMVideoClient::InitializeConfig(
                      encryption_scheme);
   if (colour_parsed_) {
     WebMColorMetadata color_metadata = colour_parser_.GetWebMColorMetadata();
-    config->set_color_space_info(color_metadata.color_space);
-    config->set_hdr_metadata(color_metadata.hdr_metadata);
+    config->set_webm_color_metadata(color_metadata);
   }
   return config->IsValidConfig();
 }
