d72b5fb6cda039cdcee7da9bee11e7563e42d108
Cobalt Team <cobalt-team@google.com>
2017-03-02 11:38:32 -0800

    Convert video decoder to JNI
    Change-Id: I284896061ba801990a4fa777969b41c4b67733e8

--- starboard/android/shared/video_decoder.cc
+++ starboard/android/shared/video_decoder.cc
@@ -19,6 +19,7 @@
 #include "starboard/android/shared/application_android.h"
 #include "starboard/android/shared/decode_target_internal.h"
 #include "starboard/android/shared/jni_env_ext.h"
+#include "starboard/android/shared/media_common.h"
 #include "starboard/android/shared/video_window.h"
 #include "starboard/android/shared/window_internal.h"
 #include "starboard/decode_target.h"
@@ -33,19 +34,9 @@ namespace shared {
 
 namespace {
 
-const int64_t kSecondInMicroseconds = 1000 * 1000;
-const int64_t kDequeueTimeout = kSecondInMicroseconds;
+const jlong kDequeueTimeout = 0;
 const SbMediaTime kOutputBufferWorkFailure = -1;
-const SbMediaTime kSbMediaTimeNone = -1;
-const SbTime kFrameTooEarlyThreshold = -15000;
-
-int64_t ConvertToMicroseconds(const SbMediaTime media_time) {
-  return media_time * kSecondInMicroseconds / kSbMediaTimeSecond;
-}
-
-SbMediaTime ConvertToMediaTime(const int64_t microseconds) {
-  return microseconds * kSbMediaTimeSecond / kSecondInMicroseconds;
-}
+const SbTime kSbTimeNone = -1;
 
 // Map an |SbMediaVideoCodec| into its corresponding mime type string.
 const char* GetMimeTypeFromCodecType(const SbMediaVideoCodec type) {
@@ -82,17 +73,15 @@ VideoDecoder::VideoDecoder(SbMediaVideoCodec video_codec,
       stream_ended_(false),
       error_occured_(false),
       decoder_thread_(kSbThreadInvalid),
-      media_format_(NULL),
-      media_codec_(NULL),
-      width_(-1),
-      height_(-1),
-      color_format_(-1),
+      media_codec_bridge_(NULL),
+      current_time_(kSbTimeNone),
       output_mode_(output_mode),
       decode_target_provider_(decode_target_provider),
       decode_target_(kSbDecodeTargetInvalid),
       frame_width_(0),
       frame_height_(0) {
   if (!InitializeCodec()) {
+    SB_LOG(ERROR) << "Failed to initialize video decoder.";
     TeardownCodec();
   }
 }
@@ -110,7 +99,7 @@ void VideoDecoder::SetHost(Host* host) {
 }
 
 void VideoDecoder::WriteInputBuffer(const InputBuffer& input_buffer) {
-  SB_DCHECK(queue_.Poll().type == kInvalid);
+  SB_DCHECK(event_queue_.Poll().type == kInvalid);
   SB_DCHECK(host_ != NULL);
 
   if (stream_ended_) {
@@ -125,7 +114,7 @@ void VideoDecoder::WriteInputBuffer(const InputBuffer& input_buffer) {
     SB_DCHECK(SbThreadIsValid(decoder_thread_));
   }
 
-  queue_.Put(Event(input_buffer));
+  event_queue_.Put(Event(input_buffer));
 }
 
 void VideoDecoder::WriteEndOfStream() {
@@ -134,13 +123,13 @@ void VideoDecoder::WriteEndOfStream() {
   // We have to flush the decoder to decode the rest frames and to ensure that
   // Decode() is not called when the stream is ended.
   stream_ended_ = true;
-  queue_.Put(Event(kWriteEndOfStream));
+  event_queue_.Put(Event(kWriteEndOfStream));
 }
 
 void VideoDecoder::Reset() {
   // Join the thread to ensure that all callbacks in process are finished.
   if (SbThreadIsValid(decoder_thread_)) {
-    queue_.Put(Event(kReset));
+    event_queue_.Put(Event(kReset));
     SbThreadJoin(decoder_thread_, NULL);
   }
 
@@ -149,19 +138,20 @@ void VideoDecoder::Reset() {
 }
 
 bool VideoDecoder::InitializeCodec() {
-  video_window_ = GetVideoWindow();
-  if (!video_window_) {
-    SB_LOG(ERROR) << "Got null video window.";
-    return false;
+  const char* mime = GetMimeTypeFromCodecType(video_codec_);
+  if (!mime) {
+    SB_LOG(ERROR) << "Mime type " << mime << " is not supported.";
   }
 
-  // Setup the output ANativeWindow object.  If we are in punch-out mode, target
-  // the passed in Android video window.  If we are in decode-to-texture mode,
-  // create a ANativeWindow from a new texture target and use that as the
-  // output window.
+  // Setup the output surface object.  If we are in punch-out mode, target
+  // the passed in Android video surface.  If we are in decode-to-texture
+  // mode, create a surface from a new texture target and use that as the
+  // output surface.
+  jobject j_output_surface = NULL;
   switch (output_mode_) {
     case kSbPlayerOutputModePunchOut: {
-      output_window_ = video_window_;
+      j_output_surface = GetVideoSurface();
+      SB_DCHECK(j_output_surface);
     } break;
     case kSbPlayerOutputModeDecodeToTexture: {
       starboard::ScopedLock lock(decode_target_mutex_);
@@ -175,54 +165,24 @@ bool VideoDecoder::InitializeCodec() {
         SB_LOG(ERROR) << "Could not acquire a decode target from provider.";
         return false;
       }
-      output_window_ = decode_target_->data->native_window;
+      j_output_surface = decode_target_->data->surface;
     } break;
     case kSbPlayerOutputModeInvalid: {
       SB_NOTREACHED();
     } break;
   }
+  SB_DCHECK(j_output_surface);
 
-  width_ = ANativeWindow_getWidth(video_window_);
-  height_ = ANativeWindow_getHeight(video_window_);
-  color_format_ = ANativeWindow_getFormat(video_window_);
-  if (color_format_ == -1) {
-    SB_LOG(ERROR) << "Failed to get color format of window.";
-    return false;
-  }
+  ANativeWindow* video_window = GetVideoWindow();
+  SB_DCHECK(video_window);
+  int width = ANativeWindow_getWidth(video_window);
+  int height = ANativeWindow_getHeight(video_window);
 
-  const char* mime_type = GetMimeTypeFromCodecType(video_codec_);
-  if (!mime_type) {
-    SB_LOG(INFO) << "Unsupported video codec type: " << video_codec_;
-    return false;
-  }
-
-  media_format_ = AMediaFormat_new();
-  SB_DCHECK(media_format_);
-  AMediaFormat_setString(media_format_, AMEDIAFORMAT_KEY_MIME, mime_type);
-  AMediaFormat_setInt32(media_format_, AMEDIAFORMAT_KEY_WIDTH, width_);
-  AMediaFormat_setInt32(media_format_, AMEDIAFORMAT_KEY_HEIGHT, height_);
-  AMediaFormat_setInt32(media_format_, AMEDIAFORMAT_KEY_COLOR_FORMAT,
-                        color_format_);
-  AMediaFormat_setInt32(media_format_,
-                        AMEDIAFORMAT_KEY_PUSH_BLANK_BUFFERS_ON_STOP, 1);
-
-  media_codec_ = AMediaCodec_createDecoderByType(mime_type);
-  if (!media_codec_) {
-    SB_LOG(ERROR) << "|AMediaCodec_createDecoderByType| failed.";
-    return false;
-  }
-
-  media_status_t status =
-      AMediaCodec_configure(media_codec_, media_format_, output_window_,
-                            /*crypto=*/NULL, /*flags=*/0);
-  if (status != AMEDIA_OK) {
-    SB_LOG(ERROR) << "|AMediaCodec_configure| failed with status: " << status;
-    return false;
-  }
-
-  status = AMediaCodec_start(media_codec_);
-  if (status != AMEDIA_OK) {
-    SB_LOG(ERROR) << "|AMediaCodec_start| failed with status: " << status;
+  jobject j_media_crypto = NULL;
+  media_codec_bridge_ = MediaCodecBridge::CreateVideoMediaCodecBridge(
+      mime, width, height, j_output_surface, j_media_crypto);
+  if (!media_codec_bridge_) {
+    SB_LOG(ERROR) << "Failed to create video media codec bridge.";
     return false;
   }
 
@@ -230,14 +190,7 @@ bool VideoDecoder::InitializeCodec() {
 }
 
 void VideoDecoder::TeardownCodec() {
-  if (media_codec_) {
-    AMediaCodec_delete(media_codec_);
-    media_codec_ = NULL;
-  }
-  if (media_format_) {
-    AMediaFormat_delete(media_format_);
-    media_format_ = NULL;
-  }
+  media_codec_bridge_.reset();
 
   starboard::ScopedLock lock(decode_target_mutex_);
   if (SbDecodeTargetIsValid(decode_target_)) {
@@ -256,12 +209,15 @@ void* VideoDecoder::ThreadEntryPoint(void* context) {
 
 void VideoDecoder::DecoderThreadFunc() {
   for (;;) {
-    Event event = queue_.Get();
+    Event event = event_queue_.Get();
     if (event.type == kReset) {
       SB_LOG(INFO) << "Reset event occurred.";
-      media_status_t status = AMediaCodec_flush(media_codec_);
-      if (status != AMEDIA_OK) {
-        SB_LOG(ERROR) << "|AMediaCodec_flush| failed with status: " << status;
+      jint status = media_codec_bridge_->Flush();
+      if (status != MEDIA_CODEC_OK) {
+        SB_LOG(ERROR) << "Failed to flush video media codec.";
+      }
+      while (!pending_work_.empty()) {
+        pending_work_.pop_front();
       }
       return;
     }
@@ -269,15 +225,16 @@ void VideoDecoder::DecoderThreadFunc() {
       continue;
     }
     if (event.type == kWriteInputBuffer) {
-      // Send |input_buffer| to |media_codec_| and try to decode one frame.
-      DecodeInputBuffer(event.input_buffer);
+      pending_work_.push_back(event.input_buffer);
+      bool did_input = true;
+      bool did_output = true;
+      while (did_input || did_output) {
+        did_input = ProcessOneInputBuffer();
+        did_output = ProcessOneOutputBuffer();
+      }
       host_->OnDecoderStatusUpdate(kNeedMoreInput, NULL);
     } else if (event.type == kWriteEndOfStream) {
-      ssize_t index =
-          AMediaCodec_dequeueInputBuffer(media_codec_, kDequeueTimeout);
-      media_status_t status = AMediaCodec_queueInputBuffer(
-          media_codec_, index, 0, 0, 0, AMEDIACODEC_BUFFER_FLAG_END_OF_STREAM);
-      AMediaCodec_flush(media_codec_);
+      pending_work_.push_back(InputBufferOrEos(InputBufferOrEos::CreateEos()));
       host_->OnDecoderStatusUpdate(kBufferFull, VideoFrame::CreateEOSFrame());
     } else {
       SB_NOTREACHED();
@@ -285,67 +242,91 @@ void VideoDecoder::DecoderThreadFunc() {
   }
 }
 
-bool VideoDecoder::DecodeInputBuffer(
-    const InputBuffer& starboard_input_buffer) {
-  const uint8_t* data = starboard_input_buffer.data();
-  size_t input_size = starboard_input_buffer.size();
-  ssize_t index = AMediaCodec_dequeueInputBuffer(media_codec_, kDequeueTimeout);
-  if (index < 0) {
-    SB_LOG(ERROR) << "|AMediaCodec_dequeueInputBuffer| failed with status: "
-                  << index;
+bool VideoDecoder::ProcessOneInputBuffer() {
+  if (pending_work_.empty()) {
     return false;
   }
-  size_t size = 0;
-  uint8_t* input_buffer =
-      AMediaCodec_getInputBuffer(media_codec_, index, &size);
-  SB_DCHECK(input_buffer);
-  if (input_size >= size) {
-    SB_LOG(ERROR) << "Media codec input buffer too small to write to.";
+  InputBufferOrEos input_buffer_or_eos = pending_work_.front();
+  InputBuffer input_buffer = input_buffer_or_eos.input_buffer;
+  bool is_eos = input_buffer_or_eos.is_eos;
+  pending_work_.pop_front();
+  SB_CHECK(media_codec_bridge_);
+
+  DequeueInputResult dequeue_input_result =
+      media_codec_bridge_->DequeueInputBuffer(kDequeueTimeout);
+  if (dequeue_input_result.index < 0) {
+    if (dequeue_input_result.status != MEDIA_CODEC_DEQUEUE_INPUT_AGAIN_LATER) {
+      SB_LOG(ERROR) << "|dequeueInputBuffer| failed with status: "
+                    << dequeue_input_result.status;
+    }
+    pending_work_.push_front(input_buffer_or_eos);
     return false;
   }
-  SbMemoryCopy(input_buffer, data, input_size);
-  int64_t pts_us = ConvertToMicroseconds(starboard_input_buffer.pts());
-  media_status_t status = AMediaCodec_queueInputBuffer(media_codec_, index, 0,
-                                                       input_size, pts_us, 0);
-  if (status != AMEDIA_OK) {
-    SB_LOG(ERROR) << "|AMediaCodec_queueInputBuffer| failed with status: "
-                  << status;
+  ScopedJavaByteBuffer byte_buffer(
+      media_codec_bridge_->GetInputBuffer(dequeue_input_result.index));
+  if (!is_eos &&
+      (byte_buffer.IsNull() || byte_buffer.capacity() < input_buffer.size())) {
+    SB_LOG(ERROR) << "Unable to write to MediaCodec input buffer.";
     return false;
   }
 
-  AMediaCodecBufferInfo info;
-  ssize_t out_index =
-      AMediaCodec_dequeueOutputBuffer(media_codec_, &info, kDequeueTimeout);
-  if (out_index < 0) {
-    SB_LOG(ERROR) << "|AMediaCodec_dequeueOutputBuffer| failed with status: "
-                  << out_index;
+  if (!is_eos) {
+    byte_buffer.CopyInto(input_buffer.data(), input_buffer.size());
+  }
+
+  jint status = media_codec_bridge_->QueueInputBuffer(
+      dequeue_input_result.index, /*offset=*/0,
+      is_eos ? 0 : input_buffer.size(),
+      ConvertSbMediaTimeToMicroseconds(input_buffer.pts()),
+      is_eos ? BUFFER_FLAG_END_OF_STREAM : 0);
+  if (status != MEDIA_CODEC_OK) {
+    SB_LOG(ERROR) << "|queueInputBuffer| failed with status: " << status;
+    return false;
+  }
+  if (is_eos) {
+    status = media_codec_bridge_->Flush();
+    if (status != MEDIA_CODEC_OK) {
+      SB_LOG(ERROR) << "Failed to flush media codec.";
+    }
+    host_->OnDecoderStatusUpdate(kBufferFull, VideoFrame::CreateEOSFrame());
+  }
+
+  return true;
+}
+
+bool VideoDecoder::ProcessOneOutputBuffer() {
+  SB_CHECK(media_codec_bridge_);
+  DequeueOutputResult dequeue_output_result =
+      media_codec_bridge_->DequeueOutputBuffer(kDequeueTimeout);
+  if (dequeue_output_result.index < 0) {
+    // Don't bother logging a try again later status, it will happen a lot.
+    if (dequeue_output_result.status !=
+        MEDIA_CODEC_DEQUEUE_OUTPUT_AGAIN_LATER) {
+      SB_LOG(ERROR) << "|dequeueOutputBuffer| failed with status: "
+                    << dequeue_output_result.status;
+    }
     return false;
   }
 
-  const SbTime delay =
-      (current_time_ == -1) ? 0 : info.presentationTimeUs -
-                                      ConvertToMicroseconds(current_time_);
-  const bool frame_too_early = delay < kFrameTooEarlyThreshold;
+  SbTime delay = (current_time_ == kSbTimeNone)
+                     ? 0
+                     : (dequeue_output_result.presentation_time_microseconds -
+                        GetAdjustedCurrentTime());
+
   if (delay > 0) {
     SbThreadSleep(delay);
-  } else if (frame_too_early) {
-    SB_LOG(INFO) << "Frame too early.";
   }
 
-  status = AMediaCodec_releaseOutputBuffer(media_codec_, out_index,
-                                           !frame_too_early && info.size > 0);
-  if (status != AMEDIA_OK) {
-    SB_LOG(ERROR) << "|AMediaCodec_releaseOutputBuffer| failed with status: "
-                  << status << ", at index:" << out_index;
-    return false;
-  }
+  media_codec_bridge_->ReleaseOutputBuffer(dequeue_output_result.index, true);
 
   // Record the latest width/height of the decoded input.
-  AMediaFormat* format = AMediaCodec_getOutputFormat(media_codec_);
-  AMediaFormat_getInt32(format, AMEDIAFORMAT_KEY_WIDTH, &frame_width_);
-  AMediaFormat_getInt32(format, AMEDIAFORMAT_KEY_HEIGHT, &frame_height_);
+  SurfaceDimensions output_dimensions =
+      media_codec_bridge_->GetOutputDimensions();
+  frame_width_ = output_dimensions.width;
+  frame_height_ = output_dimensions.height;
 
-  const SbMediaTime out_pts = ConvertToMediaTime(info.presentationTimeUs);
+  SbMediaTime out_pts = ConvertMicrosecondsToSbMediaTime(
+      dequeue_output_result.presentation_time_microseconds);
   host_->OnDecoderStatusUpdate(kBufferFull,
                                VideoFrame::CreateEmptyFrame(out_pts));
   return true;
