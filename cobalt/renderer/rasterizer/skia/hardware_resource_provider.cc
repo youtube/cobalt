cf91ac430151c8b3729552c5cac4c01af4789fb4
Cobalt Team <cobalt-team@google.com>
2017-03-03 15:32:28 -0800

    Update Cobalt's Skia font logic to use SkStreamAsset
    Change Cobalt's Skia port to use SkStreamAsset rather than SkSteam
    after cherrypicking a couple Skia CLs that improve SkFileStream
    performance:
    https://codereview.chromium.org/869763002
    https://codereview.chromium.org/882763002
    
    Change-Id: I8221dd2c27e3c9f344cbc4d652ee7a3d95943c78

--- cobalt/renderer/rasterizer/skia/hardware_resource_provider.cc
+++ cobalt/renderer/rasterizer/skia/hardware_resource_provider.cc
@@ -345,7 +345,7 @@ HardwareResourceProvider::CreateTypefaceFromRawData(
   SkAutoTUnref<SkData> skia_data(SkData::NewWithCopy(
       sanitized_data.get(), static_cast<size_t>(sanitized_data.Tell())));
 
-  SkAutoTUnref<SkStream> stream(new SkMemoryStream(skia_data));
+  SkAutoTUnref<SkStreamAsset> stream(new SkMemoryStream(skia_data));
   SkAutoTUnref<SkTypeface> typeface(SkTypeface::CreateFromStream(stream));
   if (typeface) {
     return scoped_refptr<render_tree::Typeface>(new SkiaTypeface(typeface));
