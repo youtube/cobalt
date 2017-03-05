e1de9564ad5752d2fbd405466614d4e1e3e66c73
Cobalt Team <cobalt-team@google.com>
2017-03-04 20:28:07 -0800

    Support equirectangular map-to-mesh filter in Cobalt.
    This enables 360 video viewing within Cobalt.
    
    - Move some code out of renderer/rasterizer/skia/ps4/vr and into
      renderer/rasterizer/skia so that they can be accessed by the
      general skia hardware renderer.
    
    Change-Id: Ibe4028caecbfb52eb4470fd3c69afa89eebaae77

--- cobalt/renderer/rasterizer/skia/render_tree_node_visitor.h
+++ cobalt/renderer/rasterizer/skia/render_tree_node_visitor.h
@@ -75,12 +75,15 @@ class RenderTreeNodeVisitor : public render_tree::NodeVisitor {
   // |render_image_fallback_function| is specified, it will be invoked whenever
   // standard Skia processing of the image is not possible, which usually is
   // when the image is backed by a SbDecodeTarget that requires special
-  // consideration.
+  // consideration.  |render_equirectangular_image_function| must be specified
+  // in order to support the map-to-mesh filter since Skia is unable to draw
+  // 3D meshes natively.
   RenderTreeNodeVisitor(
       SkCanvas* render_target,
       const CreateScratchSurfaceFunction* create_scratch_surface_function,
       const base::Closure& reset_skia_context_function,
       const RenderImageFallbackFunction& render_image_fallback_function,
+      const RenderImageFallbackFunction& render_equirectangular_image_function,
       SurfaceCacheDelegate* surface_cache_delegate,
       common::SurfaceCache* surface_cache, Type visitor_type = kType_Normal);
 
@@ -115,6 +118,7 @@ class RenderTreeNodeVisitor : public render_tree::NodeVisitor {
   base::Closure reset_skia_context_function_;
 
   RenderImageFallbackFunction render_image_fallback_function_;
+  RenderImageFallbackFunction render_equirectangular_image_function_;
 
   DISALLOW_COPY_AND_ASSIGN(RenderTreeNodeVisitor);
 };
