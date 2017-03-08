72b58cda36f076b73fe248c1603a0fe9a9369215
Cobalt Team <cobalt-team@google.com>
2017-03-08 10:08:52 -0800

    Add Raspi app launcher
    The Raspi app launcher is now fully functional. It uses the RASPI_ADDR
    environment variable as the IP address for the test device and assumes
    that the name is 'pi' and the password is 'raspberry'; they are not
    currently configurable.
    
    One thing to note is that PubkeyAuthentication currently is expected
    to be turned off on the raspi device for the tests. This is because
    the authentication triggers a request to touch the gnubby, which will
    not work with automated tests and is a hassle with local testing.
    
    Additionally, the pexpect is required and can be installed with the
    following command: 'sudo apt-get install python-pexpect'
    
    This resolves b/33112981.
    
    Change-Id: Ic390559fede121ae7de9e0ed43c19596c19f8d8f

---
+++ starboard/tools/raspi/run_test.py
@@ -0,0 +1,194 @@
+#!/usr/bin/python2
+#
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Run a test on Raspi device and print results of test to stdout."""
+
+from __future__ import print_function
+
+import argparse
+import functools
+import logging
+import os
+import signal
+import sys
+import textwrap
+
+import pexpect
+
+_COBALT_SRC = os.path.abspath(os.path.join(*([__file__] + 4 * [os.pardir])))
+
+_RASPI_USERNAME = 'pi'
+_RASPI_PASSWORD = 'raspberry'
+
+
+# pylint: disable=unused-argument
+def _SigIntOrSigTermHandler(process, signum, frame):
+  """Clean up and exit with status |signum|.
+
+  Args:
+    process: Current pexpect process.
+    signum: Signal number that triggered this callback.  Passed in when the
+      signal handler is called by python runtime.
+    frame: Current stack frame.  Passed in when the signal handler is called by
+      python runtime.
+  """
+  if process.isalive():
+    # Send ctrl-c to the raspi.
+    process.sendline(chr(3))
+    process.close()
+  sys.exit(signum)
+
+
+def RunTest(test_path, raspi_ip, flags):
+  """Run a test on a Raspi machine and print relevant stdout.
+
+  Args:
+    test_path: path to the binary test file to be run
+    raspi_ip: the IP address of the Raspi device to run the test on
+    flags: list of flags to pass into binary.
+
+  Returns:
+    Exit status of running the test.
+
+  Raises:
+    ValueError: Raised if test_path is invalid.
+  """
+  if not os.path.isfile(test_path):
+    raise ValueError('test_path ({}) must be a file.'.format(test_path))
+
+  sys.stdout.write('Process launched, ID={}\n'.format(os.getpid()))
+  sys.stdout.flush()
+
+  test_dir_path, test_file = os.path.split(test_path)
+  test_base_dir = os.path.basename(os.path.normpath(test_dir_path))
+
+  raspi_user_hostname = _RASPI_USERNAME + '@' + raspi_ip
+  raspi_test_path = os.path.join(test_base_dir, test_file)
+
+  # rsync the test files to the raspi
+  options = '-avzh --exclude obj*'
+  source = test_dir_path
+  destination = raspi_user_hostname + ':~/'
+  command = 'rsync ' + options + ' ' + source + ' ' + destination
+  process = pexpect.spawn(command, timeout=120)
+
+  signal.signal(signal.SIGINT,
+                functools.partial(_SigIntOrSigTermHandler, process))
+  signal.signal(signal.SIGTERM,
+                functools.partial(_SigIntOrSigTermHandler, process))
+
+  process.expect(r'\S+ password:')
+  process.sendline(_RASPI_PASSWORD)
+
+  while True:
+    line = process.readline()
+    if line:
+      sys.stdout.write(line)
+      sys.stdout.flush()
+    else:
+      break
+
+  # ssh into the raspi and run the test
+  command = 'ssh ' + raspi_user_hostname
+  process = pexpect.spawn(command, timeout=120)
+
+  signal.signal(signal.SIGINT,
+                functools.partial(_SigIntOrSigTermHandler, process))
+  signal.signal(signal.SIGTERM,
+                functools.partial(_SigIntOrSigTermHandler, process))
+
+  process.expect(r'\S+ password:')
+  process.sendline(_RASPI_PASSWORD)
+  process.sendline(raspi_test_path + ' ' + flags)
+
+  while True:
+    line = process.readline()
+    if line:
+      sys.stdout.write(line)
+      sys.stdout.flush()
+    else:
+      break
+
+  return 0
+
+
+def AddTargetFlag(parser):
+  """Add target to argument parser."""
+  parser.add_argument(
+      '-t',
+      '--target',
+      default=None,
+      help=(
+          'IP address of the Raspi device to send package to.  If no value is '
+          'specified for this argument then it will default to the '
+          'environment variable `RASPI_ADDR\' if that is set, and '
+          'otherwise exit with status code 1.'),
+      nargs='?')
+
+
+def AddFlagsFlag(parser, default=None):
+  """Add flags to argument parser.
+
+  Args:
+    parser: An ArgumentParser instance.
+    default: A string consisting of default value.
+  """
+  if default is None:
+    default = ''
+  parser.add_argument(
+      '-f',
+      '--flags',
+      default=default,
+      help=('Space separated flags that will be forwarded into Cobalt upon '
+            'launch. Note that there is no way to pass in a flag that has a '
+            'space in it.'),
+      nargs='?')
+
+
+def main():
+  """Run RunTest using path to binary provided by command line arguments.
+
+  Returns:
+    exit status
+  """
+  sys.path.append(os.path.join(_COBALT_SRC, 'cobalt', 'build'))
+
+  logging.basicConfig(
+      level=logging.INFO,
+      format='%(levelname)s|%(filename)s:%(lineno)s|%(message)s')
+
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+      description=textwrap.dedent(__doc__))
+
+  AddFlagsFlag(parser)
+  AddTargetFlag(parser)
+  parser.add_argument('test_path', help='Path of test to be run.', type=str)
+
+  args = parser.parse_args()
+
+  try:
+    return_value = RunTest(
+        args.test_path, raspi_ip=args.target, flags=args.flags)
+  # pylint: disable=W0703
+  except Exception:
+    logging.exception('Error occured while running binary.')
+    return_value = 1
+
+  return return_value
+
+
+if __name__ == '__main__':
+  sys.exit(main())
