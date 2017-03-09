6288df0a12d8c7107ada9b3ff498e54f4a903408
Cobalt Team <cobalt-team@google.com>
2017-03-08 22:14:58 -0800

    Add 3d to the special token list
    The d in the "3d" should be a capital letter.
    
    b/36079419
    
    Change-Id: Ia95fc07c94b7ab5d9658b080f09f7e652bd1815b

--- third_party/blink/Source/bindings/scripts/utilities.py
+++ third_party/blink/Source/bindings/scripts/utilities.py
@@ -36,7 +36,7 @@ KNOWN_COMPONENTS = frozenset(
 # List of regular expressions finding tokens that would appear in a name that
 # was converted from snake_case to TitleCase, but by convention should be in
 # ALL CAPS i.e. html_html_element -> HtmlHtmlElement -> HTMLHtmlElement
-special_token_list = ['Br', 'Cdata', 'Css', 'Dom', '^Html', 'Idl?', 'Ui(?!nt)', 'Url', 'Xml']
+special_token_list = ['3d', 'Br', 'Cdata', 'Css', 'Dom', '^Html', 'Idl?', 'Ui(?!nt)', 'Url', 'Xml']
 
 
 # Regular expression to capture all of the special tokens.
