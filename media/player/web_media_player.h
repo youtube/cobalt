bbcabb964cd0d5248d49d8451386e4fe1e9c5e54
Cobalt Team <cobalt-team@google.com>
2017-03-12 21:06:04 -0700

    Add support for dynamic switch to decode-to-texture for 360 video.
    This allows html_media_element.cc to indicate a desire for
    decode-to-texture output mode when rendering spherical videos.
    
    Change-Id: I77c304d1af8420c3512f3cd948a4344e4bfa8bdd

--- media/player/web_media_player.h
+++ media/player/web_media_player.h
@@ -239,6 +239,12 @@ class WebMediaPlayerClient {
   virtual float Volume() const = 0;
   virtual void SourceOpened() = 0;
   virtual std::string SourceURL() const = 0;
+  // Clients should implement this in order to indicate a preference for whether
+  // a video should be decoded to a texture or through a punch out system.  If
+  // the preferred output mode is not supported, the player will fallback to
+  // one that is.  This can be used to indicate that, say, for spherical video
+  // playback, we would like a decode-to-texture output mode.
+  virtual bool PreferDecodeToTexture() const { return false; }
   // TODO: Make the EME related functions pure virtual again once
   // we have proper EME implementation. Currently empty implementation are
   // provided to make media temporarily work.
