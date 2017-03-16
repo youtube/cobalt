caa8f1895da3386fd17adf64d1838920ae903350
Cobalt Team <cobalt-team@google.com>
2017-03-16 16:54:37 -0700

    Implement Android DRM
    b/34822842
    b/34354465
    b/35708612
    
    Change-Id: I062a0281fd904ffa90e9b235ca98499ee4cde610

--- starboard/android/shared/drm_system.cc
+++ starboard/android/shared/drm_system.cc
@@ -14,34 +14,146 @@
 
 #include "starboard/android/shared/drm_system.h"
 
+#include "starboard/android/shared/jni_env_ext.h"
+#include "starboard/android/shared/jni_utils.h"
 #include "starboard/android/shared/media_common.h"
+
+namespace {
+
+const char kNoUrl[] = "";
+
+}  // namespace
+
+extern "C" SB_EXPORT_PLATFORM void
+Java_foo_cobalt_media_MediaDrmBridge_nativeOnSessionMessage(
+    JNIEnv* env,
+    jobject unused_this,
+    jlong native_media_drm_bridge,
+    jbyteArray j_session_id,
+    jint request_type,
+    jbyteArray j_message) {
+  using starboard::android::shared::DrmSystem;
+
+  jbyte* session_id_elements = env->GetByteArrayElements(j_session_id, NULL);
+  jsize session_id_size = env->GetArrayLength(j_session_id);
+
+  jbyte* message_elements = env->GetByteArrayElements(j_message, NULL);
+  jsize message_size = env->GetArrayLength(j_message);
+
+  SB_DCHECK(session_id_elements);
+  SB_DCHECK(message_elements);
+
+  DrmSystem* drm_system = reinterpret_cast<DrmSystem*>(native_media_drm_bridge);
+  SB_DCHECK(drm_system);
+  drm_system->CallUpdateRequestCallback(session_id_elements, session_id_size,
+                                        message_elements, message_size, kNoUrl);
+}
 
 namespace starboard {
 namespace android {
 namespace shared {
+
+namespace {
+
+jbyteArray ByteArrayFromRaw(const void* data, int size) {
+  return JniEnvExt::Get()->NewByteArrayFromRaw(static_cast<const jbyte*>(data),
+                                               size);
+}
+
+}  // namespace
 
 DrmSystem::DrmSystem(void* context,
                      SbDrmSessionUpdateRequestFunc update_request_callback,
                      SbDrmSessionUpdatedFunc session_updated_callback)
     : context_(context),
       update_request_callback_(update_request_callback),
-      session_updated_callback_(session_updated_callback) {}
+      session_updated_callback_(session_updated_callback),
+      j_media_drm_bridge_(NULL),
+      j_media_crypto_(NULL) {
+  JniEnvExt* env = JniEnvExt::Get();
+  j_media_drm_bridge_ = env->CallStaticObjectMethod(
+      "foo/cobalt/media/MediaDrmBridge", "create",
+      "(J)Lfoo/cobalt/media/MediaDrmBridge;", reinterpret_cast<jlong>(this));
+  if (!j_media_drm_bridge_) {
+    SB_LOG(ERROR) << "Failed to create MediaDrmBridge.";
+    return;
+  }
+  j_media_drm_bridge_ = env->ConvertLocalRefToGlobalRef(j_media_drm_bridge_);
+  j_media_crypto_ = env->CallObjectMethod(j_media_drm_bridge_, "getMediaCrypto",
+                                          "()Landroid/media/MediaCrypto;");
+  if (!j_media_crypto_) {
+    SB_LOG(ERROR) << "Failed to create MediaCrypto.";
+    return;
+  }
+  j_media_crypto_ = env->ConvertLocalRefToGlobalRef(j_media_crypto_);
+}
 
-DrmSystem::~DrmSystem() {}
+DrmSystem::~DrmSystem() {
+  JniEnvExt* env = JniEnvExt::Get();
+  if (j_media_crypto_) {
+    env->DeleteGlobalRef(j_media_crypto_);
+    j_media_crypto_ = NULL;
+  }
+  if (j_media_drm_bridge_) {
+    env->CallVoidMethod(j_media_drm_bridge_, "destroy", "()V");
+    env->DeleteGlobalRef(j_media_drm_bridge_);
+    j_media_drm_bridge_ = NULL;
+  }
+}
 
 void DrmSystem::GenerateSessionUpdateRequest(const char* type,
                                              const void* initialization_data,
-                                             int initialization_data_size) {}
+                                             int initialization_data_size) {
+  ScopedLocalJavaRef j_init_data(
+      ByteArrayFromRaw(initialization_data, initialization_data_size));
+  JniEnvExt* env = JniEnvExt::Get();
+  jstring j_mime = env->NewStringUTF(type);
+  env->CallVoidMethod(j_media_drm_bridge_, "createSession",
+                      "([BLjava/lang/String;)V", j_init_data.Get(), j_mime);
+  // |update_request_callback_| will be called by Java calling into
+  // |onSessionMessage|.
+}
 
 void DrmSystem::UpdateSession(const void* key,
                               int key_size,
                               const void* session_id,
-                              int session_id_size) {}
+                              int session_id_size) {
+  ScopedLocalJavaRef j_session_id(
+      ByteArrayFromRaw(session_id, session_id_size));
+  ScopedLocalJavaRef j_response(ByteArrayFromRaw(key, key_size));
 
-void DrmSystem::CloseSession(const void* session_id, int session_id_size) {}
+  jboolean status = JniEnvExt::Get()->CallBooleanMethod(
+      j_media_drm_bridge_, "updateSession", "([B[B)Z", j_session_id.Get(),
+      j_response.Get());
+  session_updated_callback_(this, context_, session_id, session_id_size,
+                            status == JNI_TRUE);
+}
+
+void DrmSystem::CloseSession(const void* session_id, int session_id_size) {
+  JniEnvExt* env = JniEnvExt::Get();
+  ScopedLocalJavaRef j_session_id(
+      ByteArrayFromRaw(session_id, session_id_size));
+  env->CallVoidMethod(j_media_drm_bridge_, "closeSession", "([B)V",
+                      j_session_id.Get());
+}
 
 DrmSystem::DecryptStatus DrmSystem::Decrypt(InputBuffer* buffer) {
-  return kFailure;
+  SB_DCHECK(buffer);
+  SB_DCHECK(buffer->drm_info());
+  SB_DCHECK(j_media_crypto_);
+  // The actual decryption will take place by calling |queueSecureInputBuffer|
+  // in the decoders.  Our existence implies that there is enough information
+  // to perform the decryption.
+  return kSuccess;
+}
+
+void DrmSystem::CallUpdateRequestCallback(const void* session_id,
+                                          int session_id_size,
+                                          const void* content,
+                                          int content_size,
+                                          const char* url) {
+  update_request_callback_(this, context_, session_id, session_id_size, content,
+                           content_size, url);
 }
 
 }  // namespace shared
