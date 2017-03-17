b8fec350fcf91187e7ba969d0dc0b95141366f4f
Xiaoming Shi <xiaomings@google.com>
2017-03-17 16:39:22 -0700

    Refactor SbPlayerWriteSample().
    Now it can write sample data stored in multiple meory blocks
    which can be discontinuous.
    
    b/34861032
    
    Change-Id: Iedeae920bf4d648d2dc7bd7c66f2d4077e6272b5

--- starboard/shared/starboard/player/player_write_sample.cc
+++ starboard/shared/starboard/player/player_write_sample.cc
@@ -17,6 +17,43 @@
 #include "starboard/log.h"
 #include "starboard/shared/starboard/player/player_internal.h"
 
+#if SB_API_VERSION >= SB_PLAYER_WRITE_SAMPLE_SCATTERED_VERSION
+
+void SbPlayerWriteSample(SbPlayer player,
+                         SbMediaType sample_type,
+                         const void** sample_buffers,
+                         int* sample_buffer_sizes,
+                         int number_of_sample_buffers,
+                         SbMediaTime sample_pts,
+                         const SbMediaVideoSampleInfo* video_sample_info,
+                         const SbDrmSampleInfo* sample_drm_info) {
+  if (!SbPlayerIsValid(player)) {
+    SB_DLOG(WARNING) << "player is invalid.";
+    return;
+  }
+
+  if (number_of_sample_buffers != 1) {
+    SB_DLOG(WARNING) << "SbPlayerWriteSample() doesn't support"
+                     << " |number_of_sample_buffers| other than one.";
+    return;
+  }
+
+  if (sample_buffers != NULL) {
+    SB_DLOG(WARNING) << "|sample_buffers| cannot be NULL";
+    return;
+  }
+
+  if (sample_buffer_sizes != NULL) {
+    SB_DLOG(WARNING) << "|sample_buffer_sizes| cannot be NULL";
+    return;
+  }
+
+  player->WriteSample(sample_type, sample_buffers[0], sample_buffer_sizes[0],
+                      sample_pts, video_sample_info, sample_drm_info);
+}
+
+#else  // SB_API_VERSION >= SB_PLAYER_WRITE_SAMPLE_SCATTERED_VERSION
+
 void SbPlayerWriteSample(SbPlayer player,
                          SbMediaType sample_type,
                          const void* sample_buffer,
@@ -32,3 +69,5 @@ void SbPlayerWriteSample(SbPlayer player,
   player->WriteSample(sample_type, sample_buffer, sample_buffer_size,
                       sample_pts, video_sample_info, sample_drm_info);
 }
+
+#endif  // SB_API_VERSION >= SB_PLAYER_WRITE_SAMPLE_SCATTERED_VERSION
