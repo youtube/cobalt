2902f71f44338f6b38c7fe16ae588892e69d237d
Cobalt Team <cobalt-team@google.com>
2017-03-20 16:44:40 -0700

    Support resolution-dependent mesh selection on MTM
    Change-Id: If8e581c253edfab3761d355a491397204c90e4a8

--- cobalt/render_tree/map_to_mesh_filter.h
+++ cobalt/render_tree/map_to_mesh_filter.h
@@ -15,8 +15,13 @@
 #ifndef COBALT_RENDER_TREE_MAP_TO_MESH_FILTER_H_
 #define COBALT_RENDER_TREE_MAP_TO_MESH_FILTER_H_
 
+#include <map>
+#include <utility>
+
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/scoped_ptr.h"
+#include "cobalt/math/size.h"
 #include "cobalt/render_tree/mesh.h"
 
 namespace cobalt {
@@ -37,41 +42,94 @@ enum StereoMode {
 };
 
 // A MapToMeshFilter can be used to map source content onto a 3D mesh, within a
-// specified well-defined viewport. A null mesh indicates the equirectangular
-// mesh should be used.
+// specified well-defined viewport.
 class MapToMeshFilter {
  public:
-  MapToMeshFilter(StereoMode stereo_mode,
-                  scoped_refptr<render_tree::Mesh> left_eye_mesh,
-                  scoped_refptr<render_tree::Mesh> right_eye_mesh = NULL)
-      : stereo_mode_(stereo_mode),
-        left_eye_mesh_(left_eye_mesh),
-        right_eye_mesh_(right_eye_mesh) {
-    DCHECK(left_eye_mesh_);
+  struct Builder {
+    Builder() {}
+    void SetDefaultMeshes(
+        const scoped_refptr<render_tree::Mesh>& left_eye_mesh,
+        const scoped_refptr<render_tree::Mesh>& right_eye_mesh) {
+      left_eye_default_mesh_ = left_eye_mesh;
+      right_eye_default_mesh_ = right_eye_mesh;
+    }
+    void AddResolutionMatchedMeshes(
+        math::Size resolution,
+        const scoped_refptr<render_tree::Mesh>& left_eye_mesh,
+        const scoped_refptr<render_tree::Mesh>& right_eye_mesh) {
+      DCHECK_GT(resolution.width(), 0);
+      DCHECK_GT(resolution.height(), 0);
+      DCHECK(left_eye_mesh);
+      resolution_matched_meshes_[resolution] =
+          std::make_pair(left_eye_mesh, right_eye_mesh);
+    }
+    const scoped_refptr<render_tree::Mesh>& left_eye_mesh(
+        math::Size resolution) const {
+      MeshMap::const_iterator match =
+          resolution_matched_meshes_.find(resolution);
+      if (match != resolution_matched_meshes_.end()) {
+        return match->second.first;
+      }
+      return left_eye_default_mesh_;
+    }
+    const scoped_refptr<render_tree::Mesh>& right_eye_mesh(
+        math::Size resolution) const {
+      MeshMap::const_iterator match =
+          resolution_matched_meshes_.find(resolution);
+      if (match != resolution_matched_meshes_.end()) {
+        return match->second.second;
+      }
+      return right_eye_default_mesh_;
+    }
+
+   private:
+    struct SizeLessThan {
+      bool operator()(const math::Size& a, const math::Size& b) const {
+        return a.width() < b.width() ||
+               (a.width() == b.width() && a.height() < b.height());
+      }
+    };
+    typedef std::pair<scoped_refptr<render_tree::Mesh>,
+                      scoped_refptr<render_tree::Mesh> > MeshPair;
+    typedef std::map<math::Size, MeshPair, SizeLessThan> MeshMap;
+    MeshMap resolution_matched_meshes_;
+    scoped_refptr<render_tree::Mesh> left_eye_default_mesh_;
+    scoped_refptr<render_tree::Mesh> right_eye_default_mesh_;
+  };
+
+  MapToMeshFilter(StereoMode stereo_mode, const Builder& builder)
+      : stereo_mode_(stereo_mode), data_(builder) {
+    DCHECK(left_eye_mesh());
     if (stereo_mode == kLeftRightUnadjustedTextureCoords) {
       // This stereo mode implies there are two meshes.
-      DCHECK(left_eye_mesh_ && right_eye_mesh_);
+      DCHECK(right_eye_mesh());
     }
   }
 
   StereoMode stereo_mode() const { return stereo_mode_; }
 
-  const scoped_refptr<render_tree::Mesh>& mono_mesh() const {
-    return left_eye_mesh_;
+  // The omission of the |resolution| parameter will yield the default
+  // meshes in each of the following functions (by failing to match the
+  // invalid resolution).
+  const scoped_refptr<render_tree::Mesh>& mono_mesh(
+      math::Size resolution = InvalidSize()) const {
+    return data_.left_eye_mesh(resolution);
   }
 
-  const scoped_refptr<render_tree::Mesh>& left_eye_mesh() const {
-    return left_eye_mesh_;
+  const scoped_refptr<render_tree::Mesh>& left_eye_mesh(
+      math::Size resolution = InvalidSize()) const {
+    return data_.left_eye_mesh(resolution);
   }
 
-  const scoped_refptr<render_tree::Mesh>& right_eye_mesh() const {
-    return right_eye_mesh_;
+  const scoped_refptr<render_tree::Mesh>& right_eye_mesh(
+      math::Size resolution = InvalidSize()) const {
+    return data_.right_eye_mesh(resolution);
   }
 
  private:
+  static math::Size InvalidSize() { return math::Size(-1, -1); }
   StereoMode stereo_mode_;
-  scoped_refptr<render_tree::Mesh> left_eye_mesh_;
-  scoped_refptr<render_tree::Mesh> right_eye_mesh_;
+  Builder data_;
 };
 
 }  // namespace render_tree
