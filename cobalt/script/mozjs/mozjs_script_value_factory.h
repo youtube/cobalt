24e206113e608d4e41013b6a00cd214777e76285
Cobalt Team <cobalt-team@google.com>
2017-03-20 15:02:38 -0700

    Support bindings for Promises
    Change-Id: Id2d4e488ba4d8d1c7f17c1288ec78c8be7ac2e79

--- cobalt/script/mozjs/mozjs_script_value_factory.h
+++ cobalt/script/mozjs/mozjs_script_value_factory.h
@@ -18,6 +18,8 @@
 #define COBALT_SCRIPT_MOZJS_MOZJS_SCRIPT_VALUE_FACTORY_H_
 
 #include "cobalt/script/mozjs/mozjs_global_environment.h"
+#include "cobalt/script/mozjs/native_promise.h"
+#include "cobalt/script/mozjs/promise_wrapper.h"
 #include "cobalt/script/script_value_factory.h"
 #include "third_party/mozjs/js/src/jsapi.h"
 
@@ -27,8 +29,26 @@ namespace mozjs {
 class MozjsScriptValueFactory : public ScriptValueFactory {
  public:
   explicit MozjsScriptValueFactory(MozjsGlobalEnvironment* global_environment);
-  scoped_ptr<ScriptValueFactory::ScriptPromise> CreatePromise() OVERRIDE;
   ~MozjsScriptValueFactory() OVERRIDE {}
+
+  template <typename T>
+  scoped_ptr<ScriptValue<Promise<T> > > CreatePromise() {
+    typedef ScriptValue<Promise<T> > ScriptPromiseType;
+    typedef MozjsUserObjectHolder<NativePromise<T> > MozjsPromiseHolderType;
+
+    JSContext* context = global_environment_->context();
+    JS::RootedObject global_object(context,
+                                   global_environment_->global_object());
+    JSAutoRequest auto_request(context);
+    JSAutoCompartment auto_compartment(context, global_object);
+
+    JS::RootedObject promise_wrapper(
+        context, PromiseWrapper::Create(context, global_object));
+    DCHECK(promise_wrapper);
+    scoped_ptr<ScriptPromiseType> promise(new MozjsPromiseHolderType(
+        promise_wrapper, context, global_environment_->wrapper_factory()));
+    return promise.Pass();
+  }
 
  private:
   MozjsGlobalEnvironment* global_environment_;
