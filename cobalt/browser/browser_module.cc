f84654d1b18fb9f8c1f698584e31f6d52db12161
Cobalt Team <cobalt-team@google.com>
2017-03-22 18:26:55 -0700

    Call BrowserModule::OnError() when a rendering timeout happend.
    - Includes these changes:
      https://lbshell-internal-review.git.corp.google.com/#/c/58656/
      https://lbshell-internal-review.git.corp.google.com/#/c/58692/
    
    Change-Id: I9e269a81ba33a9bdf89bcefb6494b0e183df2e01

--- cobalt/browser/browser_module.cc
+++ cobalt/browser/browser_module.cc
@@ -19,11 +19,13 @@
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/debug/trace_event.h"
+#include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/path_service.h"
 #include "base/stl_util.h"
 #include "base/string_number_conversions.h"
 #include "base/string_split.h"
+#include "base/time.h"
 #include "cobalt/base/cobalt_paths.h"
 #include "cobalt/base/source_location.h"
 #include "cobalt/base/tokens.h"
@@ -36,10 +38,66 @@
 #include "cobalt/h5vcc/h5vcc.h"
 #include "cobalt/input/input_device_manager_fuzzer.h"
 #include "nb/memory_scope.h"
+#include "starboard/atomic.h"
+#include "starboard/configuration.h"
+#include "starboard/system.h"
+#include "starboard/time.h"
+
+#if SB_HAS(CORE_DUMP_HANDLER_SUPPORT)
+#include "starboard/ps4/core_dump_handler.h"
+#endif  // SB_HAS(CORE_DUMP_HANDLER_SUPPORT)
 
 namespace cobalt {
+
+#if defined(COBALT_CHECK_RENDER_TIMEOUT)
+namespace timestamp {
+// This is a temporary workaround.
+extern SbAtomic64 g_last_render_timestamp;
+}  // namespace timestamp
+
+namespace {
+struct NonTrivialGlobalVariables {
+  NonTrivialGlobalVariables();
+
+  SbAtomic64* last_render_timestamp;
+};
+
+NonTrivialGlobalVariables::NonTrivialGlobalVariables() {
+  last_render_timestamp = &cobalt::timestamp::g_last_render_timestamp;
+  SbAtomicNoBarrier_Exchange64(last_render_timestamp,
+                               static_cast<SbAtomic64>(SbTimeGetNow()));
+}
+
+base::LazyInstance<NonTrivialGlobalVariables> non_trivial_global_variables =
+    LAZY_INSTANCE_INITIALIZER;
+
+}  // namespace
+#endif
+
 namespace browser {
 namespace {
+
+#if defined(COBALT_CHECK_RENDER_TIMEOUT)
+// Timeout for last render.
+const int kLastRenderTimeoutSeconds = 15;
+
+// Polling interval for timeout_polling_thread_.
+const int kRenderTimeOutPollingDelaySeconds = 1;
+
+// Minimum number of continuous times the timeout expirations. This is used to
+// prevent unintended behavior in situations such as when returning from
+// suspended state. Note that the timeout response trigger will be delayed
+// after the actual timeout expiration by this value times the polling delay.
+const int kMinimumContinuousRenderTimeoutExpirations = 2;
+
+// Name for timeout_polling_thread_.
+const char* kTimeoutPollingThreadName = "TimeoutPolling";
+
+// This specifies the percentage of calls to OnRenderTimeout() that result in a
+// call to OnError().
+const int kRenderTimeoutErrorPercentage = 99;
+
+#endif
 
 // This constant defines the maximum rate at which the layout engine will
 // refresh over time.  Since there is little benefit in performing a layout
@@ -188,9 +246,29 @@ BrowserModule::BrowserModule(const GURL& url,
           h5vcc_url_handler_(this, system_window)),
       web_module_options_(options.web_module_options),
       has_resumed_(true, false),
+#if defined(COBALT_CHECK_RENDER_TIMEOUT)
+      timeout_polling_thread_(kTimeoutPollingThreadName),
+      render_timeout_count_(0),
+#endif
       will_quit_(false),
       suspended_(false),
       system_window_(system_window) {
+#if SB_HAS(CORE_DUMP_HANDLER_SUPPORT)
+  SbCoreDumpRegisterHandler(BrowserModule::CoreDumpHandler, this);
+  on_error_triggered_count_ = 0;
+#if defined(COBALT_CHECK_RENDER_TIMEOUT)
+  recovery_mechanism_triggered_count_ = 0;
+  timeout_response_trigger_count_ = 0;
+#endif  // defined(COBALT_CHECK_RENDER_TIMEOUT)
+#endif  // SB_HAS(CORE_DUMP_HANDLER_SUPPORT)
+
+#if defined(COBALT_CHECK_RENDER_TIMEOUT)
+  timeout_polling_thread_.Start();
+  timeout_polling_thread_.message_loop()->PostDelayedTask(
+      FROM_HERE, base::Bind(&BrowserModule::OnPollForRenderTimeout,
+                            base::Unretained(this), url),
+      base::TimeDelta::FromSeconds(kRenderTimeOutPollingDelaySeconds));
+#endif
   TRACE_EVENT0("cobalt::browser", "BrowserModule::BrowserModule()");
   // All allocations for media will be tracked by "Media" memory scope.
   {
@@ -260,6 +338,9 @@ BrowserModule::BrowserModule(const GURL& url,
 
 BrowserModule::~BrowserModule() {
   DCHECK_EQ(MessageLoop::current(), self_message_loop_);
+#if SB_HAS(CORE_DUMP_HANDLER_SUPPORT)
+  SbCoreDumpUnregisterHandler(BrowserModule::CoreDumpHandler, this);
+#endif
 }
 
 void BrowserModule::Navigate(const GURL& url) {
@@ -279,6 +360,22 @@ void BrowserModule::Reload() {
       "location.reload();",
       base::SourceLocation("[object BrowserModule]", 1, 1));
 }
+
+#if SB_HAS(CORE_DUMP_HANDLER_SUPPORT)
+// static
+void BrowserModule::CoreDumpHandler(void* browser_module_as_void) {
+  BrowserModule* browser_module =
+      static_cast<BrowserModule*>(browser_module_as_void);
+  SbCoreDumpLogInteger("BrowserModule.on_error_triggered_count_",
+                       browser_module->on_error_triggered_count_);
+#if defined(COBALT_CHECK_RENDER_TIMEOUT)
+  SbCoreDumpLogInteger("BrowserModule.recovery_mechanism_triggered_count_",
+                       browser_module->recovery_mechanism_triggered_count_);
+  SbCoreDumpLogInteger("BrowserModule.timeout_response_trigger_count_",
+                       browser_module->timeout_response_trigger_count_);
+#endif  // defined(COBALT_CHECK_RENDER_TIMEOUT)
+}
+#endif  // SB_HAS(CORE_DUMP_HANDLER_SUPPORT)
 
 void BrowserModule::NavigateInternal(const GURL& url) {
   TRACE_EVENT0("cobalt::browser", "BrowserModule::NavigateInternal()");
@@ -543,6 +640,9 @@ void BrowserModule::InjectKeyEventToMainWebModule(
 
 void BrowserModule::OnError(const GURL& url, const std::string& error) {
   TRACE_EVENT0("cobalt::browser", "BrowserModule::OnError()");
+#if SB_HAS(CORE_DUMP_HANDLER_SUPPORT)
+  on_error_triggered_count_++;
+#endif
   LOG(ERROR) << error;
   std::string url_string = "h5vcc://network-failure";
 
@@ -797,6 +897,53 @@ void BrowserModule::OnRendererSubmissionRasterized() {
   }
 }
 #endif  // OS_STARBOARD
+
+#if defined(COBALT_CHECK_RENDER_TIMEOUT)
+void BrowserModule::OnPollForRenderTimeout(const GURL& url) {
+  SbTime last_render_timestamp = static_cast<SbTime>(SbAtomicAcquire_Load64(
+      non_trivial_global_variables.Get().last_render_timestamp));
+  base::Time last_render =
+      base::Time::FromSbTime(last_render_timestamp);
+  bool timeout_expiration =
+      base::Time::Now() -
+          base::TimeDelta::FromSeconds(kLastRenderTimeoutSeconds) >
+      last_render;
+  bool timeout_response_trigger = false;
+  if (timeout_expiration) {
+    // The timeout only triggers if the timeout expiration has been detected
+    // without interruption at least kMinimumContinuousRenderTimeoutExpirations
+    // times.
+    ++render_timeout_count_;
+    timeout_response_trigger =
+        render_timeout_count_ >= kMinimumContinuousRenderTimeoutExpirations;
+  } else {
+    render_timeout_count_ = 0;
+  }
+
+  if (timeout_response_trigger) {
+#if SB_HAS(CORE_DUMP_HANDLER_SUPPORT)
+    timeout_response_trigger_count_++;
+#endif
+    SbAtomicNoBarrier_Exchange64(
+        non_trivial_global_variables.Get().last_render_timestamp,
+        static_cast<SbAtomic64>(kSbTimeMax));
+    if (SbSystemGetRandomUInt64() <
+        kRenderTimeoutErrorPercentage * (UINT64_MAX / 100)) {
+      OnError(url, std::string("Rendering Timeout"));
+#if SB_HAS(CORE_DUMP_HANDLER_SUPPORT)
+      recovery_mechanism_triggered_count_++;
+#endif
+    } else {
+      SB_DLOG(INFO) << "Received OnRenderTimeout, ignoring by random chance.";
+    }
+  } else {
+    timeout_polling_thread_.message_loop()->PostDelayedTask(
+        FROM_HERE, base::Bind(&BrowserModule::OnPollForRenderTimeout,
+                              base::Unretained(this), url),
+        base::TimeDelta::FromSeconds(kRenderTimeOutPollingDelaySeconds));
+  }
+}
+#endif
 
 }  // namespace browser
 }  // namespace cobalt
