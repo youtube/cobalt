15d0d45cf36992b9f5a205eca014601e0c28f971
Xiaoming Shi <xiaomings@google.com>
2017-03-23 18:57:39 +0000

    Revert "Pass maximum video resolution to SbPlayerCreate()."
    This reverts commit a71b8fe62df18201428e3c2bf44cbf2fc3c0de7a.
    
    Change-Id: Ic956cd86ab03690131372e342b2a12defed8012f

--- starboard/configuration.h
+++ starboard/configuration.h
@@ -93,9 +93,6 @@
 // ints.
 #define SB_INPUT_FLOATING_POINT_INPUT_VECTOR_VERSION SB_EXPERIMENTAL_API_VERSION
 
-// SbPlayerCreate() will accept SbMediaVideoHeader as a parameter.
-#define SB_PLAYER_CREATE_WITH_VIDEO_HEADER_VERSION SB_EXPERIMENTAL_API_VERSION
-
 // Deleted the vestigal struct SbUserApplicationTokenResults from user.h.
 #define SB_DELETE_USER_APPLICATION_TOKEN_VERSION SB_EXPERIMENTAL_API_VERSION
 
