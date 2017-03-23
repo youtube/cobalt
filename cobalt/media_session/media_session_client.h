5387f93b70ab9a5411b8d31cabd6844987afbf58
Cobalt Team <cobalt-team@google.com>
2017-03-22 17:30:55 -0700

    Beginnings of a real MediaSession
    Ultimately, MediaSession will need to connect to different things
    on different platforms.
    
    For now, I propose a Starboard API (until we get some more experience
    with this) and instead use the new cobalt_platform_dependencies
    mechanism.
    
    This change simply creates a do-nothing DefaultMediaSessionClient,
    which will ultimately be replaced by an (eg) AndroidMediaSessionClient.
    
    Change-Id: Ia5f55081afaf52bbe05de0b21839192083b7506f

---
+++ cobalt/media_session/media_session_client.h
@@ -0,0 +1,96 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef COBALT_MEDIA_SESSION_MEDIA_SESSION_CLIENT_H_
+#define COBALT_MEDIA_SESSION_MEDIA_SESSION_CLIENT_H_
+
+#include <bitset>
+
+#include "base/threading/thread_checker.h"
+
+#include "cobalt/media_session/media_session.h"
+
+namespace cobalt {
+namespace media_session {
+
+// Base class for a platform-level implementation of MediaSession.
+// Platforms should subclass this to connect MediaSession to their platform.
+class MediaSessionClient {
+ public:
+  typedef std::bitset<MediaSession::MediaSessionAction::kFinalElement>
+      AvailableActionsSet;
+  MediaSessionClient()
+      : media_session_(new MediaSession(this)),
+        platform_playback_state_(MediaSession::kNone) {}
+
+  virtual ~MediaSessionClient() {}
+
+  // Retrieves the singleton MediaSession associated with this client.
+  scoped_refptr<MediaSession>& GetMediaSession() { return media_session_; }
+
+  // Retrieves the current guessed playback state.
+  // https://wicg.github.io/mediasession/#guessed-playback-state
+  // Must be called on the browser thread.
+  MediaSession::MediaSessionPlaybackState GetGuessedPlaybackState() {
+    DCHECK(thread_checker_.CalledOnValidThread());
+    NOTIMPLEMENTED();
+    return MediaSession::MediaSessionPlaybackState::kNone;
+  }
+
+  // Retrieves the set of currently available mediasession actions
+  // per "media session actions update algorithm"
+  // https://wicg.github.io/mediasession/#actions-model
+  AvailableActionsSet GetAvailableActions() {
+    DCHECK(thread_checker_.CalledOnValidThread());
+    NOTIMPLEMENTED();
+    return AvailableActionsSet();
+  }
+
+  // Sets the platform's current playback state. This is used to compute
+  // the "guessed playback state"
+  // https://wicg.github.io/mediasession/#guessed-playback-state
+  void UpdatePlatformPlaybackState(
+      MediaSession::MediaSessionPlaybackState state) {
+    DCHECK(thread_checker_.CalledOnValidThread());
+    MediaSession::MediaSessionPlaybackState prev_guessed_state =
+        GetGuessedPlaybackState();
+    platform_playback_state_ = state;
+
+    if (prev_guessed_state != GetGuessedPlaybackState()) {
+      OnMediaSessionChanged();
+    }
+  }
+
+  // Invokes a given media session action
+  // https://wicg.github.io/mediasession/#actions-model
+  void InvokeAction(MediaSession::MediaSessionAction action) {
+    UNREFERENCED_PARAMETER(action);
+    DCHECK(thread_checker_.CalledOnValidThread());
+    NOTIMPLEMENTED();
+  }
+
+  // Invoked on the browser thread when any metadata, playback state,
+  // or supported session actions change.
+  virtual void OnMediaSessionChanged() = 0;
+
+ private:
+  base::ThreadChecker thread_checker_;
+  scoped_refptr<MediaSession> media_session_;
+  MediaSession::MediaSessionPlaybackState platform_playback_state_;
+};
+
+}  // namespace media_session
+}  // namespace cobalt
+
+#endif  // COBALT_MEDIA_SESSION_MEDIA_SESSION_CLIENT_H_
