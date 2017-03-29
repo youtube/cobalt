d86d8211a1b60686b216bf06e7d4a4181bb05f3f
Cobalt Team <cobalt-team@google.com>
2017-03-29 10:13:45 -0700

    Fix bug and complete remaining interfaces for NamedNodeMap
    NamedNodeMap implementation is not live as it should be, and some of the
    methods are missing IDL or not correct. Fixed those issues.
    b/36696921
    b/36676952
    
    Change-Id: I83ff8be4fee0af7df5f15cc26d98c13c79288a9e

--- cobalt/dom/named_node_map.cc
+++ cobalt/dom/named_node_map.cc
@@ -15,7 +15,9 @@
 #include "cobalt/dom/named_node_map.h"
 
 #include <algorithm>
+#include <iterator>
 
+#include "base/optional.h"
 #include "cobalt/dom/attr.h"
 #include "cobalt/dom/element.h"
 #include "cobalt/dom/global_stats.h"
@@ -26,86 +28,114 @@ namespace dom {
 
 NamedNodeMap::NamedNodeMap(const scoped_refptr<Element>& element)
     : element_(element) {
-  ConstructProxyAttributes();
+  DCHECK(element_);
   GlobalStats::GetInstance()->Add(this);
 }
 
+// The length attribute's getter must return the attribute list's size.
+//  https://dom.spec.whatwg.org/#dom-namednodemap-length
 unsigned int NamedNodeMap::length() const {
-  return static_cast<unsigned int>(attribute_names_.size());
+  return static_cast<unsigned int>(element_->attribute_map().size());
 }
 
+// Algorithm for Item:
+//   https://dom.spec.whatwg.org/#dom-namednodemap-item
 scoped_refptr<Attr> NamedNodeMap::Item(unsigned int item) {
-  if (item < attribute_names_.size()) {
-    return GetNamedItem(attribute_names_[item]);
+  // 1. If index is equal to or greater than context object's attribute list's
+  // size, then return null.
+  if (item >= element_->attribute_map().size()) {
+    return NULL;
   }
-  return NULL;
+
+  // 2. Otherwise, return context object's attribute list[index].
+  Element::AttributeMap::const_iterator iter =
+      element_->attribute_map().begin();
+  std::advance(iter, item);
+  return GetOrCreateAttr(iter->first);
 }
 
+// The getNamedItem(qualifiedName) method, when invoked, must return the result
+// of getting an attribute given qualifiedName and element.
+//   https://dom.spec.whatwg.org/#dom-namednodemap-getnameditem
 scoped_refptr<Attr> NamedNodeMap::GetNamedItem(const std::string& name) const {
+  // 1. if element is in the HTML namespace and its node document is an HTML
+  // document, then set qualifiedName to qualifiedName in ASCII lowercase.
+  // 2. Return the first attribute in element's attribute list whose qualified
+  // name is qualifiedName, and null otherwise.
   if (!element_->HasAttribute(name)) {
-    // Not need to throw an exception, fail gracefully.
     return NULL;
   }
   return GetOrCreateAttr(name);
 }
 
+// The setNamedItem(attr) and setNamedItemNS(attr) methods, when invoked, must
+// return the result of setting an attribute given attr and element.
+//   https://dom.spec.whatwg.org/#dom-namednodemap-setnameditem
 scoped_refptr<Attr> NamedNodeMap::SetNamedItem(
     const scoped_refptr<Attr>& attribute) {
+  // Custom, not in any spec.
   if (!attribute) {
     // TODO: Throw JS NotFoundError.
     return NULL;
   }
 
-  if (attribute->container()) {
-    if (attribute->container() == this) {
-      // Already attached to this node map, don't do anything.
-      return attribute;
-    }
-    // Attribute is already attached to a different NamedNodeMap.
+  // To set an attribute given an attr and element, run these steps:
+  //   https://dom.spec.whatwg.org/#concept-element-attributes-set
+
+  // 1. If attr's element is neither null nor element, throw an
+  // InUseAttributeError.
+  if (attribute->container() && attribute->container() != this) {
     // TODO: Throw JS InUseAttributeError.
     return NULL;
   }
 
+  // 2. Let oldAttr be the result of getting an attribute given attr's
+  // namespace, attr's local name, and element.
+  // 3. If oldAttr is attr, return attr.
+  // 4. If oldAttr is non-null, replace it by attr in element.
+  // 5. Otherwise, append attr to element.
   const std::string& name = attribute->name();
-  scoped_refptr<Attr> previous_attribute;
-
+  scoped_refptr<Attr> old_attr;
   if (element_->HasAttribute(name)) {
-    // Get the previous attribute that was associated with 'name' and detach it
-    // from NamedNodeMap.
-    previous_attribute = GetOrCreateAttr(name);
-    previous_attribute->set_container(NULL);
-  }
+    old_attr = GetOrCreateAttr(name);
 
-  // Insert the new attribute and attach it to the NamedNodeMap.
+    if (attribute->value() == old_attr->value()) {
+      return old_attr;
+    }
+    old_attr->set_container(NULL);
+  }
   attribute->set_container(this);
   proxy_attributes_[name] = attribute->AsWeakPtr();
-
   // Inform the element about the new attribute. This should trigger a call to
   // NamedNodeMap::SetAttributeInternal and continue the update there.
   element_->SetAttribute(name, attribute->value());
-  return previous_attribute;
+
+  // 6. Return oldAttr.
+  return old_attr;
 }
 
+// Algorithm for RemoveNamedItem:
+//  https://dom.spec.whatwg.org/#dom-namednodemap-removenameditem
 scoped_refptr<Attr> NamedNodeMap::RemoveNamedItem(const std::string& name) {
-  ProxyAttributeMap::iterator iter = proxy_attributes_.find(name);
-  if (iter == proxy_attributes_.end()) {
-    // TODO: Throw JS NotFoundError.
-    return NULL;
-  }
-
-  scoped_refptr<Attr> previous_attribute;
+  // 1. Let attr be the result of removing an attribute given qualifiedName and
+  // element.
+  // 2. If attr is null, then throw a NotFoundError.
+  scoped_refptr<Attr> attr;
   if (element_->HasAttribute(name)) {
     // Get the previous attribute that was associated with 'name' and detach it
     // from NamedNodeMap.
-    previous_attribute = GetOrCreateAttr(name);
-    previous_attribute->set_container(NULL);
+    attr = GetOrCreateAttr(name);
+    attr->set_container(NULL);
+  } else {
+    // TODO: Throw JS NotFoundError.
+    return NULL;
   }
-
   // Inform the element about the removed attribute. This should trigger a call
   // to NamedNodeMap::RemoveAttributeInternal and continue the update there.
   element_->RemoveAttribute(name);
 
-  return previous_attribute;
+  // 3. Return attr.
+  return attr;
 }
 
 bool NamedNodeMap::CanQueryNamedProperty(const std::string& name) const {
@@ -114,34 +144,25 @@ bool NamedNodeMap::CanQueryNamedProperty(const std::string& name) const {
 
 void NamedNodeMap::EnumerateNamedProperties(
     script::PropertyEnumerator* enumerator) const {
-  for (size_t i = 0; i < attribute_names_.size(); ++i) {
-    enumerator->AddProperty(attribute_names_[i]);
+  const Element::AttributeMap& attribute_map = element_->attribute_map();
+  for (Element::AttributeMap::const_iterator iter = attribute_map.begin();
+       iter != attribute_map.end(); ++iter) {
+    enumerator->AddProperty(iter->first);
   }
 }
 
 void NamedNodeMap::SetAttributeInternal(const std::string& name,
                                         const std::string& value) {
-  // If this a new name, add to it to the name vector.
-  AttributeNameVector::const_iterator name_iter =
-      std::find(attribute_names_.begin(), attribute_names_.end(), name);
-  if (name_iter == attribute_names_.end()) {
-    attribute_names_.push_back(name);
-  }
-
   // Update the associated Attr object.
-  ProxyAttributeMap::iterator attribute_iter = proxy_attributes_.find(name);
+  NameToAttrMap::iterator attribute_iter = proxy_attributes_.find(name);
   if (attribute_iter != proxy_attributes_.end() && attribute_iter->second) {
     attribute_iter->second->SetValueInternal(value);
   }
 }
 
 void NamedNodeMap::RemoveAttributeInternal(const std::string& name) {
-  attribute_names_.erase(
-      std::remove(attribute_names_.begin(), attribute_names_.end(), name),
-      attribute_names_.end());
-
   // Make sure to detach the proxy attribute from NamedNodeMap.
-  ProxyAttributeMap::iterator iter = proxy_attributes_.find(name);
+  NameToAttrMap::iterator iter = proxy_attributes_.find(name);
   if (iter != proxy_attributes_.end()) {
     if (iter->second) {
       iter->second->set_container(NULL);
@@ -154,23 +175,10 @@ scoped_refptr<Element> NamedNodeMap::element() const { return element_; }
 
 NamedNodeMap::~NamedNodeMap() { GlobalStats::GetInstance()->Remove(this); }
 
-void NamedNodeMap::ConstructProxyAttributes() {
-  // Construct the attribute name vector.
-  attribute_names_.clear();
-  const Element::AttributeMap& attribute_map = element_->attribute_map();
-  for (Element::AttributeMap::const_iterator iter = attribute_map.begin();
-       iter != attribute_map.end(); ++iter) {
-    attribute_names_.push_back(iter->first);
-  }
-
-  // There's no need to create the ProxyAttributeMap since it'll be created
-  // on demand when accessed by the user.
-}
-
 scoped_refptr<Attr> NamedNodeMap::GetOrCreateAttr(
     const std::string& name) const {
   TRACK_MEMORY_SCOPE("DOM");
-  ProxyAttributeMap::iterator iter = proxy_attributes_.find(name);
+  NameToAttrMap::iterator iter = proxy_attributes_.find(name);
   if (iter != proxy_attributes_.end() && iter->second) {
     return iter->second.get();
   }
