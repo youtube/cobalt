90735e141422c5024b714e078b789b0dd073a64a
Cobalt Team <cobalt-team@google.com>
2017-03-29 17:04:41 -0700

    Adds StdDynamicAllocator.
    This allows better accounting of memory allocated
    to containers.
    
    This is will be used by the memory_tracker so that the significant
    memory that is used by the system can be tracked.
    
    Change-Id: I86f0d301150f481c7cf305163c2055e8c4443998

--- nb/std_allocator_test.cc
+++ nb/std_allocator_test.cc
@@ -102,5 +102,179 @@ TEST(StdAllocator, map) {
   CountingAllocator::Reset();
 }
 
+class CountingDynamicAllocator : public nb::Allocator {
+ public:
+  CountingDynamicAllocator() : bytes_allocated_(0), num_allocations_(0) {}
+  void* Allocate(size_t size) SB_OVERRIDE {
+    bytes_allocated_ += size;
+    ++num_allocations_;
+    return SbMemoryAllocate(size);
+  }
+
+  virtual void* Allocate(std::size_t size, std::size_t alignment) SB_OVERRIDE {
+    EXPECT_TRUE(false) << "Unexpected that aligned version is called.";
+    return Allocate(size);
+  }
+
+  virtual void Free(void* memory) SB_OVERRIDE {
+    FreeWithSize(memory, 0);
+    ASSERT_TRUE(false) << "Unexpected that free without size "
+                          "version is called.";
+  }
+
+  void FreeWithSize(void* ptr, size_t optional_size) SB_OVERRIDE {
+    SbMemoryDeallocate(ptr);
+
+    bytes_allocated_ -= optional_size;
+    --num_allocations_;
+  }
+
+  std::size_t GetCapacity() const SB_OVERRIDE {
+    EXPECT_TRUE(false) << "Unexpected that GetCapacity().";
+    return 0;
+  }
+
+  std::size_t GetAllocated() const SB_OVERRIDE {
+    return static_cast<size_t>(bytes_allocated_);
+  }
+
+  void PrintAllocations() const SB_OVERRIDE {}
+
+  int64_t bytes_allocated_;
+  int64_t num_allocations_;
+};
+
+// Test the expectation that vector will go through the supplied allocator.
+TEST(StdDynamicAllocator, vector) {
+  typedef StdDynamicAllocator<int> IntAllocator;
+  typedef std::vector<int, IntAllocator> IntVector;
+
+  CountingDynamicAllocator counting_allocator;
+
+  EXPECT_EQ(0, counting_allocator.bytes_allocated_);
+  EXPECT_EQ(0, counting_allocator.num_allocations_);
+
+  IntVector* int_vector = new IntVector(IntAllocator(&counting_allocator));
+  int_vector->push_back(0);
+
+  for (int i = 0; i < 10; ++i) {
+    int_vector->push_back(i);
+  }
+
+  // We aren't sure how much allocation is here, but we know it's more than 0.
+  EXPECT_LT(0, counting_allocator.bytes_allocated_);
+  EXPECT_LT(0, counting_allocator.num_allocations_);
+
+  delete int_vector;
+
+  // Expect that all allocations are destroyed now.
+  EXPECT_EQ(0, counting_allocator.bytes_allocated_);
+  EXPECT_EQ(0, counting_allocator.num_allocations_);
+}
+
+// Test the expectation that vector will go through the supplied allocator.
+TEST(StdDynamicAllocator, map) {
+  typedef StdDynamicAllocator<int> IntAllocator;
+  typedef std::map<int, int, std::less<int>, IntAllocator> IntMap;
+
+  CountingDynamicAllocator counting_allocator;
+
+  EXPECT_EQ(0, counting_allocator.bytes_allocated_);
+  EXPECT_EQ(0, counting_allocator.num_allocations_);
+
+  IntMap* int_map = new IntMap(std::less<int>(),  // required.
+                               IntAllocator(&counting_allocator));
+  for (int i = 0; i < 10; ++i) {
+    (*int_map)[i] = i;
+  }
+
+  // We aren't sure how much allocation is here, but we know it's more than 0.
+  EXPECT_LT(0, counting_allocator.bytes_allocated_);
+  EXPECT_LT(0, counting_allocator.num_allocations_);
+
+  delete int_map;
+
+  // Expect that all allocations are destroyed now.
+  EXPECT_EQ(0, counting_allocator.bytes_allocated_);
+  EXPECT_EQ(0, counting_allocator.num_allocations_);
+}
+
+// Test the expectation that a copied vector will go through the allocator from
+// the first vector.
+TEST(StdDynamicAllocator, vector_copy) {
+  typedef StdDynamicAllocator<int> IntAllocator;
+  typedef std::vector<int, IntAllocator> IntVector;
+
+  CountingDynamicAllocator counting_allocator;
+
+  EXPECT_EQ(0, counting_allocator.bytes_allocated_);
+  EXPECT_EQ(0, counting_allocator.num_allocations_);
+
+  IntVector* int_vector = new IntVector(IntAllocator(&counting_allocator));
+  int_vector->push_back(0);
+
+  for (int i = 0; i < 10; ++i) {
+    int_vector->push_back(i);
+  }
+
+  // We aren't sure how much allocation is here, but we know it's more than 0.
+  EXPECT_LT(0, counting_allocator.bytes_allocated_);
+  EXPECT_LT(0, counting_allocator.num_allocations_);
+
+  int64_t saved_bytes_allocated = counting_allocator.bytes_allocated_;
+  int64_t saved_num_allocations = counting_allocator.num_allocations_;
+
+  IntVector* int_vector_copy = new IntVector(*int_vector);
+  for (int i = 0; i < 10; ++i) {
+    int_vector_copy->push_back(i);
+  }
+
+  EXPECT_LT(saved_bytes_allocated, counting_allocator.bytes_allocated_);
+  EXPECT_LT(saved_num_allocations, counting_allocator.num_allocations_);
+
+  delete int_vector_copy;
+  delete int_vector;
+
+  // Expect that all allocations are destroyed now.
+  EXPECT_EQ(0, counting_allocator.bytes_allocated_);
+  EXPECT_EQ(0, counting_allocator.num_allocations_);
+}
+
+// Test the expectation that vector will go through the supplied allocator.
+TEST(StdDynamicAllocator, map_copy) {
+  typedef StdDynamicAllocator<int> IntAllocator;
+  typedef std::map<int, int, std::less<int>, IntAllocator> IntMap;
+
+  CountingDynamicAllocator counting_allocator;
+
+  EXPECT_EQ(0, counting_allocator.bytes_allocated_);
+  EXPECT_EQ(0, counting_allocator.num_allocations_);
+
+  IntMap* int_map = new IntMap(std::less<int>(),  // required.
+                               IntAllocator(&counting_allocator));
+  for (int i = 0; i < 10; ++i) {
+    (*int_map)[i] = i;
+  }
+
+  int64_t saved_bytes_allocated = counting_allocator.bytes_allocated_;
+  int64_t saved_num_allocations = counting_allocator.num_allocations_;
+
+  IntMap* int_map_copy = new IntMap(*int_map);
+
+  for (int i = 0; i < 10; ++i) {
+    (*int_map_copy)[i] = i;
+  }
+
+  EXPECT_LT(saved_bytes_allocated, counting_allocator.bytes_allocated_);
+  EXPECT_LT(saved_num_allocations, counting_allocator.num_allocations_);
+
+  delete int_map_copy;
+  delete int_map;
+
+  // Expect that all allocations are destroyed now.
+  EXPECT_EQ(0, counting_allocator.bytes_allocated_);
+  EXPECT_EQ(0, counting_allocator.num_allocations_);
+}
+
 }  // namespace
 }  // namespace nb
