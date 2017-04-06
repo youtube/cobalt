864a51bf49936b852069107a19344d2d60947a97
Cobalt Team <cobalt-team@google.com>
2017-04-06 10:33:20 -0700

    MediaSession: implement "available actions"
    The MediaSession spec has an algorithm for determining which
    actions (eg, start playing, pause) are available based on
    what the current media session state is.
    
    Change-Id: Ic93b5f689ae815a7dd48fcb1f7cb2e1c00d7de15

--- cobalt/media_session/media_session_test.cc
+++ cobalt/media_session/media_session_test.cc
@@ -16,6 +16,9 @@
 
 #include "cobalt/bindings/testing/script_object_owner.h"
 #include "cobalt/media_session/media_session_client.h"
+#include "cobalt/script/script_value.h"
+#include "cobalt/script/testing/fake_script_value.h"
+#include "cobalt/script/wrappable.h"
 
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -23,11 +26,27 @@
 #include "base/message_loop.h"
 #include "base/run_loop.h"
 
+using ::cobalt::script::testing::FakeScriptValue;
+using ::cobalt::script::CallbackResult;
+using ::cobalt::script::ScriptValue;
+using ::cobalt::script::Wrappable;
+
+using ::testing::AnyNumber;
 using ::testing::AtLeast;
 using ::testing::InvokeWithoutArgs;
+using ::testing::Return;
+using ::testing::_;
 
 namespace cobalt {
 namespace media_session {
+namespace {
+
+class MockCallbackFunction : public MediaSession::MediaSessionActionHandler {
+ public:
+  virtual ReturnValue Run() const {
+    return CallbackResult<void>();
+  }
+};
 
 class MockMediaSessionClient : public MediaSessionClient {
  public:
@@ -96,5 +115,105 @@ TEST(MediaSessionTest, GetActualPlaybackState) {
   run_loop.Run();
 }
 
+TEST(MediaSessionTest, NullActionClears) {
+  MessageLoop message_loop(MessageLoop::TYPE_DEFAULT);
+  base::RunLoop run_loop;
+
+  MockMediaSessionClient client;
+
+  ON_CALL(client, OnMediaSessionChanged())
+      .WillByDefault(InvokeWithoutArgs(&run_loop, &base::RunLoop::Quit));
+  EXPECT_CALL(client, OnMediaSessionChanged()).Times(AtLeast(0));
+
+  scoped_refptr<MediaSession> session = client.GetMediaSession();
+
+  EXPECT_EQ(kMediaSessionPlaybackStateNone, client.GetActualPlaybackState());
+  EXPECT_EQ(0, client.GetAvailableActions().to_ulong());
+
+  MockCallbackFunction cf;
+  FakeScriptValue<MediaSession::MediaSessionActionHandler> holder(&cf);
+
+  FakeScriptValue<MediaSession::MediaSessionActionHandler> null_holder(NULL);
+
+  session->SetActionHandler(kMediaSessionActionPlay, holder);
+  EXPECT_EQ(1, client.GetAvailableActions().to_ulong());
+  session->SetActionHandler(kMediaSessionActionPlay, null_holder);
+  EXPECT_EQ(0, client.GetAvailableActions().to_ulong());
+}
+
+TEST(MediaSessionTest, GetAvailableActions) {
+  MessageLoop message_loop(MessageLoop::TYPE_DEFAULT);
+  base::RunLoop run_loop;
+
+  MockMediaSessionClient client;
+
+  ON_CALL(client, OnMediaSessionChanged())
+      .WillByDefault(InvokeWithoutArgs(&run_loop, &base::RunLoop::Quit));
+  EXPECT_CALL(client, OnMediaSessionChanged()).Times(AtLeast(0));
+
+  scoped_refptr<MediaSession> session = client.GetMediaSession();
+
+  EXPECT_EQ(kMediaSessionPlaybackStateNone, client.GetActualPlaybackState());
+  EXPECT_EQ(0, client.GetAvailableActions().to_ulong());
+
+  MockCallbackFunction cf;
+  FakeScriptValue<MediaSession::MediaSessionActionHandler> holder(&cf);
+
+  session->SetActionHandler(kMediaSessionActionPlay, holder);
+
+  EXPECT_EQ(1, client.GetAvailableActions().to_ulong());
+
+  session->SetActionHandler(kMediaSessionActionPause, holder);
+
+  EXPECT_EQ(1, client.GetAvailableActions().to_ulong());
+
+  session->SetActionHandler(kMediaSessionActionSeekbackward, holder);
+
+  EXPECT_EQ(5, client.GetAvailableActions().to_ulong());
+
+  client.UpdatePlatformPlaybackState(kMediaSessionPlaybackStatePlaying);
+
+  EXPECT_EQ(kMediaSessionPlaybackStatePlaying, client.GetActualPlaybackState());
+  EXPECT_EQ(6, client.GetAvailableActions().to_ulong());
+
+  session->set_playback_state(kMediaSessionPlaybackStatePlaying);
+
+  EXPECT_EQ(kMediaSessionPlaybackStatePlaying, client.GetActualPlaybackState());
+  EXPECT_EQ(6, client.GetAvailableActions().to_ulong());
+
+  session->set_playback_state(kMediaSessionPlaybackStatePaused);
+
+  EXPECT_EQ(kMediaSessionPlaybackStatePlaying, client.GetActualPlaybackState());
+  EXPECT_EQ(6, client.GetAvailableActions().to_ulong());
+
+  session->set_playback_state(kMediaSessionPlaybackStatePlaying);
+
+  EXPECT_EQ(kMediaSessionPlaybackStatePlaying, client.GetActualPlaybackState());
+  EXPECT_EQ(6, client.GetAvailableActions().to_ulong());
+
+  client.UpdatePlatformPlaybackState(kMediaSessionPlaybackStatePaused);
+
+  EXPECT_EQ(kMediaSessionPlaybackStatePlaying, client.GetActualPlaybackState());
+  EXPECT_EQ(6, client.GetAvailableActions().to_ulong());
+
+  session->set_playback_state(kMediaSessionPlaybackStateNone);
+
+  EXPECT_EQ(kMediaSessionPlaybackStateNone, client.GetActualPlaybackState());
+  EXPECT_EQ(5, client.GetAvailableActions().to_ulong());
+
+  session->set_playback_state(kMediaSessionPlaybackStatePaused);
+
+  EXPECT_EQ(kMediaSessionPlaybackStatePaused, client.GetActualPlaybackState());
+  EXPECT_EQ(5, client.GetAvailableActions().to_ulong());
+
+  client.UpdatePlatformPlaybackState(kMediaSessionPlaybackStateNone);
+
+  EXPECT_EQ(kMediaSessionPlaybackStatePaused, client.GetActualPlaybackState());
+  EXPECT_EQ(5, client.GetAvailableActions().to_ulong());
+
+  run_loop.Run();
+}
+
+}  // namespace
 }  // namespace media_session
 }  // namespace cobalt
