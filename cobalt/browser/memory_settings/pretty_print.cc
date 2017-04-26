e5f90d329a418117489f2bf6e56e96affb605b44
Cobalt Team <cobalt-team@google.com>
2017-04-25 17:20:16 -0700

    Breaks out TablePrinter into it's own file.
    Generalizes TablePrinter to take N number of columns.
    Adds an additional test to table printer for smaller size.
    
    Change-Id: Ibadd120783e71aa2154ef19c4888565579322c36

--- cobalt/browser/memory_settings/pretty_print.cc
+++ cobalt/browser/memory_settings/pretty_print.cc
@@ -17,126 +17,17 @@
 #include "cobalt/browser/memory_settings/pretty_print.h"
 
 #include <algorithm>
+#include <string>
 #include <vector>
 
 #include "cobalt/browser/memory_settings/memory_settings.h"
+#include "cobalt/browser/memory_settings/table_printer.h"
 #include "starboard/log.h"
 
 namespace cobalt {
 namespace browser {
 namespace memory_settings {
 namespace {
-
-class TablePrinter {
- public:
-  struct Row {
-    std::string name;
-    std::string value;
-    std::string source;
-  };
-
-  static std::string ToString(const std::vector<Row>& rows) {
-    // First column is the name, the second is the value, then source.
-    size_t name_col_width = 0;
-    size_t value_col_width = 0;
-    size_t source_col_width = 0;
-
-    for (size_t i = 0; i < rows.size(); ++i) {
-      const Row& row = rows[i];
-      name_col_width = std::max<size_t>(name_col_width, row.name.size());
-      value_col_width = std::max<size_t>(value_col_width, row.value.size());
-      source_col_width = std::max<size_t>(source_col_width, row.source.size());
-    }
-
-    TablePrinter printer(name_col_width, value_col_width, source_col_width);
-
-    std::stringstream output_ss;
-    std::string row_delimiter = printer.MakeDelimiterRow();
-
-    output_ss << printer.MakeHeaderRow() << "\n";
-    output_ss << row_delimiter << "\n";
-
-    for (size_t i = 0; i < rows.size(); ++i) {
-      const Row& row = rows[i];
-      std::string row_str =
-          printer.MakeDataRow(row.name, row.value, row.source);
-      output_ss << row_str << "\n";
-      output_ss << row_delimiter << "\n";
-    }
-
-    return output_ss.str();
-  }
-
- private:
-  TablePrinter(size_t name_col_width, size_t value_col_width,
-               size_t source_col_width)
-      : name_col_width_(name_col_width),
-        value_col_width_(value_col_width),
-        source_col_width_(source_col_width) {
-    Init();
-  }
-
-  void Init() {
-    std::stringstream fmt_row_ss;
-    // Example: "| %-30s | %9s | %9s |\n"
-    fmt_row_ss << "| %-" << name_col_width_ << "s | %" << value_col_width_
-               << "s | %" << source_col_width_ << "s |";
-    fmt_data_row_ = fmt_row_ss.str();
-  }
-
-  std::string MakeHeaderRow() const {
-    std::string name_str = "NAME";
-    std::string value_str = "VALUE";
-    std::string source_str = "SOURCE";
-
-    std::stringstream ss;
-    ss << name_str;
-    for (size_t i = 0; i < name_col_width_ + 3 - name_str.size(); ++i) {
-      ss << " ";
-    }
-
-    ss << value_str;
-    for (size_t i = 0; i < value_col_width_ + 3 - value_str.size(); ++i) {
-      ss << " ";
-    }
-
-    ss << source_str;
-    for (size_t i = 0; i < source_col_width_ + 4 - source_str.size(); ++i) {
-      ss << " ";
-    }
-    return ss.str();
-  }
-
-  std::string MakeDataRow(const std::string& name, const std::string& value,
-                          const std::string& source) const {
-    std::string row_str = base::StringPrintf(
-        fmt_data_row_.c_str(), name.c_str(), value.c_str(), source.c_str());
-    return row_str;
-  }
-
-  std::string MakeDelimiterRow() const {
-    std::stringstream ss;
-    ss << "+";
-    for (size_t i = 0; i < name_col_width_ + 2; ++i) {
-      ss << "-";
-    }
-    ss << "+";
-    for (size_t i = 0; i < value_col_width_ + 2; ++i) {
-      ss << "-";
-    }
-    ss << "+";
-    for (size_t i = 0; i < source_col_width_ + 2; ++i) {
-      ss << "-";
-    }
-    ss << "+";
-    return ss.str();
-  }
-
-  const size_t name_col_width_;
-  const size_t value_col_width_;
-  const size_t source_col_width_;
-  std::string fmt_data_row_;
-};
 
 std::string StringifySourceType(MemorySetting::SourceType source_type) {
   switch (source_type) {
@@ -147,7 +38,7 @@ std::string StringifySourceType(MemorySetting::SourceType source_type) {
       return "CmdLine";
     }
     case MemorySetting::kBuildSetting: {
-      return "BuildSetting";
+      return "Build";
     }
     case MemorySetting::kAutoSet: {
       return "AutoSet";
@@ -165,20 +56,25 @@ std::string StringifySourceType(MemorySetting::SourceType source_type) {
 
 std::string GeneratePrettyPrintTable(
     const std::vector<const MemorySetting*>& settings) {
-  std::vector<TablePrinter::Row> rows;
+  TablePrinter printer;
+
+  std::vector<std::string> header;
+  header.push_back("NAME");
+  header.push_back("VALUE");
+  header.push_back("SOURCE");
+  printer.AddRow(header);
 
   for (size_t i = 0; i < settings.size(); ++i) {
     const MemorySetting* setting = settings[i];
 
-    TablePrinter::Row row;
-    row.name = setting->name();
-    row.value = setting->ValueToString();
-    row.source = StringifySourceType(setting->source_type());
-
-    rows.push_back(row);
+    std::vector<std::string> row;
+    row.push_back(setting->name());
+    row.push_back(setting->ValueToString());
+    row.push_back(StringifySourceType(setting->source_type()));
+    printer.AddRow(row);
   }
 
-  std::string table_string = TablePrinter::ToString(rows);
+  std::string table_string = printer.ToString();
   return table_string;
 }
 
