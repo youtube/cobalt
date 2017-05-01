2d6ca141890ac955e0d6962a2009b2fcaaec12aa
Cobalt Team <cobalt-team@google.com>
2017-05-01 15:20:42 -0700

    Add support for rounded rect box shadows.
    b/27155517
    
    Change-Id: I5ce28ef678811f62309786ce55d71d40aff958ab

---
+++ cobalt/renderer/glimp_shaders/glsl/fragment_skia_rrect_ring_masked_ellipse_edge.glsl
@@ -0,0 +1,55 @@
+#version 100
+precision mediump float;
+uniform vec4 uinnerRect_Stage1;
+uniform float uRTHeight;
+uniform vec2 uinvRadiiXY_Stage1;
+uniform vec4 uinnerRect_Stage2;
+uniform vec2 uinvRadiiXY_Stage2;
+varying vec4 vColor;
+varying vec2 vEllipseOffsets_Stage0;
+varying vec4 vEllipseRadii_Stage0;
+void main() 
+{
+	vec4 fragCoordYDown = vec4(gl_FragCoord.x, uRTHeight - gl_FragCoord.y, 1.0, 1.0);
+	vec4 output_Stage0;
+	{
+		// Stage 0: EllipseEdge
+		vec2 scaledOffset = vEllipseOffsets_Stage0*vEllipseRadii_Stage0.xy;
+		float test = dot(scaledOffset, scaledOffset) - 1.0;
+		vec2 grad = 2.0*scaledOffset*vEllipseRadii_Stage0.xy;
+		float grad_dot = dot(grad, grad);
+		grad_dot = max(grad_dot, 1.0e-4);
+		float invlen = inversesqrt(grad_dot);
+		float edgeAlpha = clamp(0.5-test*invlen, 0.0, 1.0);
+		output_Stage0 = vec4(edgeAlpha);
+	}
+	vec4 output_Stage1;
+	{
+		// Stage 1: EllipticalRRect
+		vec2 dxy0 = uinnerRect_Stage1.xy - fragCoordYDown.xy;
+		vec2 dxy1 = fragCoordYDown.xy - uinnerRect_Stage1.zw;
+		vec2 dxy = max(max(dxy0, dxy1), 0.0);
+		vec2 Z = dxy * uinvRadiiXY_Stage1;
+		float implicit = dot(Z, dxy) - 1.0;
+		float grad_dot = 4.0 * dot(Z, Z);
+		grad_dot = max(grad_dot, 1.0e-4);
+		float approx_dist = implicit * inversesqrt(grad_dot);
+		float alpha = clamp(0.5 + approx_dist, 0.0, 1.0);
+		output_Stage1 = (output_Stage0 * alpha);
+	}
+	vec4 output_Stage2;
+	{
+		// Stage 2: EllipticalRRect
+		vec2 dxy0 = uinnerRect_Stage2.xy - fragCoordYDown.xy;
+		vec2 dxy1 = fragCoordYDown.xy - uinnerRect_Stage2.zw;
+		vec2 dxy = max(max(dxy0, dxy1), 0.0);
+		vec2 Z = dxy * uinvRadiiXY_Stage2;
+		float implicit = dot(Z, dxy) - 1.0;
+		float grad_dot = 4.0 * dot(Z, Z);
+		grad_dot = max(grad_dot, 1.0e-4);
+		float approx_dist = implicit * inversesqrt(grad_dot);
+		float alpha = clamp(0.5 - approx_dist, 0.0, 1.0);
+		output_Stage2 = (output_Stage1 * alpha);
+	}
+	gl_FragColor = (vColor * output_Stage2);
+}
