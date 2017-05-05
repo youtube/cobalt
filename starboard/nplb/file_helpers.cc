d82dd7dbed1435e3cf3038f4e5e4474b6df230ee
Cobalt Team <cobalt-team@google.com>
2017-05-04 19:04:20 -0700

    Use platform specific path separator charactor
    Change-Id: I9269edcc7e61f6f4ca7a80978f6d237d0e4de87f

--- starboard/nplb/file_helpers.cc
+++ starboard/nplb/file_helpers.cc
@@ -62,7 +62,7 @@ std::string ScopedRandomFile::MakeRandomFilename() {
   }
 
   std::ostringstream filename_stream;
-  filename_stream << path << "/ScopedRandomFile.File_"
+  filename_stream << path << SB_FILE_SEP_CHAR << "ScopedRandomFile.File_"
                   << SbSystemGetRandomUInt64();
   return filename_stream.str();
 }
