31e880a85befe957b693e44c0b531a1d0dfcde40
Toan Pham <toanpham@google.com>
2017-05-08 13:47:34 -0700

    Update some streams tests to run on mozjs 24 and 45.
    Change-Id: If6333ef5ba74313fd9d72dfa531b7babd14c0059

--- third_party/web_platform_tests/streams/count-queuing-strategy.js
+++ third_party/web_platform_tests/streams/count-queuing-strategy.js
@@ -12,9 +12,9 @@ test(() => {
 
 test(() => {
 
-  for (const highWaterMark of [-Infinity, NaN, 'foo', {}, () => {}]) {
-    const strategy = new CountQueuingStrategy({ highWaterMark });
-    assert_equals(strategy.highWaterMark, highWaterMark, `${highWaterMark} gets set correctly`);
+  for (var highWaterMark of [-Infinity, NaN, 'foo', {}, () => {}]) {
+    const strategy = new CountQueuingStrategy({ highWaterMark: highWaterMark });
+    assert_equals(strategy.highWaterMark, highWaterMark, '${highWaterMark} gets set correctly');
   }
 
 }, 'Can construct a CountQueuingStrategy with any value as its high water mark');
