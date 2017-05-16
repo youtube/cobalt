63bc3167c191322e9591530903103245335f2a5b
Joe Woodworth <joewoodworth@google.com>
2017-05-16 15:00:43 -0700

    Implement more of SbSystemGetPath for xb1
    Change-Id: I4f6f71ed749f07f9d31f34eb7d7c37650d4fd74e

--- starboard/win/shared/system_get_path.cc
+++ starboard/win/shared/system_get_path.cc
@@ -15,11 +15,12 @@
 #include "starboard/system.h"
 
 #include <windows.h>
+// pathcch.h must come after windows.h.
+#include <pathcch.h>
+
 #include <codecvt>
 #include <cstring>
 #include <locale>
-
-#include "Pathcch.h"
 
 #include "starboard/directory.h"
 #include "starboard/log.h"
@@ -27,24 +28,105 @@
 #include "starboard/shared/win32/wchar_utils.h"
 #include "starboard/string.h"
 
+namespace {
+
+// Places up to |path_size| - 1 characters of the path to the current
+// executable in |out_path|, ensuring it is NULL-terminated. Returns success
+// status. The result being greater than |path_size| - 1 characters is a
+// failure. |out_path| may be written to in unsuccessful cases.
 bool GetExecutablePath(char* out_path, int path_size) {
   if (!out_path || !path_size) {
     return false;
   }
 
-  static const int kPathSize = SB_FILE_MAX_PATH;
+  wchar_t w_file_path[SB_FILE_MAX_PATH];
+  DWORD characters_written =
+      GetModuleFileName(NULL, w_file_path, SB_FILE_MAX_PATH);
+  if (characters_written < 1) {
+    return false;
+  }
+  std::string utf8_string =
+      starboard::shared::win32::wchar_tToUTF8(w_file_path);
+  if (utf8_string.length() >= path_size) {
+    return false;
+  }
+  return SbStringCopy(out_path, utf8_string.c_str(), path_size);
+}
+
+// Places up to |path_size| - 1 characters of the path to the directory
+// containing the current executable in |out_path|, ensuring it is
+// NULL-terminated. Returns success status. The result being greater than
+// |path_size| - 1 characters is a failure. |out_path| may be written to in
+// unsuccessful cases.
+bool GetExecutableDirectory(char* out_path, int path_size) {
+  if (!out_path || !path_size) {
+    return false;
+  }
 
   wchar_t w_file_path[SB_FILE_MAX_PATH];
   DWORD characters_written =
       GetModuleFileName(NULL, w_file_path, SB_FILE_MAX_PATH);
-  if (characters_written == 0) {
+  if (characters_written < 1) {
     return false;
   }
   PathCchRemoveFileSpec(w_file_path, SB_FILE_MAX_PATH);
   std::string utf8_string =
-      starboard::shared::win32::wchar_tToUTF8(w_file_path, SB_FILE_MAX_PATH);
-  return SbStringCopy(out_path, utf8_string.c_str(), SB_FILE_MAX_PATH);
+      starboard::shared::win32::wchar_tToUTF8(w_file_path);
+  if (utf8_string.length() >= path_size) {
+    return false;
+  }
+  return SbStringCopy(out_path, utf8_string.c_str(), path_size);
 }
+
+// Places up to |path_size| - 1 characters of the path to the content directory
+// in |out_path|, ensuring it is NULL-terminated. Returns success
+// status. The result being greater than |path_size| - 1 characters is a
+// failure. |out_path| may be written to in unsuccessful cases.
+bool GetContentPath(char* out_path, int path_size) {
+  if (!out_path || !path_size) {
+    return false;
+  }
+  char file_path[SB_FILE_MAX_PATH];
+  file_path[0] = '\0';
+  if (!GetExecutableDirectory(file_path, path_size)) {
+    return false;
+  }
+  if (SbStringConcat(file_path, "\\content\\data", SB_FILE_MAX_PATH) >=
+      path_size) {
+    return false;
+  }
+  return SbStringCopy(out_path, file_path, path_size);
+}
+
+bool CreateAndGetTempPath(char* out_path, int path_size) {
+  if (!out_path || !path_size) {
+    return false;
+  }
+  wchar_t w_file_path[SB_FILE_MAX_PATH];
+  w_file_path[0] = L'\0';
+
+  DWORD characters_written = GetTempPathW(SB_FILE_MAX_PATH, w_file_path);
+  if (characters_written >= (path_size + 1) || characters_written < 1) {
+    return false;
+  }
+  // Remove the last slash, to match other Starboard implementations.
+  w_file_path[characters_written - 1] = L'\0';
+
+  std::string utf8_string =
+      starboard::shared::win32::wchar_tToUTF8(w_file_path);
+
+  if (SbStringCopy(out_path, utf8_string.c_str(), path_size) >= path_size) {
+    return false;
+  }
+  SbDirectoryCreate(out_path);
+
+  size_t length = SbStringGetLength(out_path);
+  if (length < 1 || length > path_size) {
+    return false;
+  }
+  return true;
+}
+}  // namespace
 
 // Note: This function is only minimally implemented to allow tests to run.
 bool SbSystemGetPath(SbSystemPathId path_id, char* out_path, int path_size) {
@@ -52,61 +134,18 @@ bool SbSystemGetPath(SbSystemPathId path_id, char* out_path, int path_size) {
     return false;
   }
 
-  static const int kPathSize = SB_FILE_MAX_PATH;
-
-  char file_path[kPathSize];
-  file_path[0] = '\0';
-
   switch (path_id) {
-    case kSbSystemPathContentDirectory: {
-      if (!GetExecutablePath(file_path, kPathSize)) {
-        return false;
-      }
-      if (SbStringConcat(file_path, "/content/data", kPathSize) >= kPathSize) {
-        return false;
-      }
-      SbStringCopy(out_path, file_path, path_size);
-      break;
-    }
-    case kSbSystemPathTempDirectory: {
-      wchar_t w_file_path[kPathSize];
-      w_file_path[0] = '\0';
-
-      DWORD characters_written = GetTempPathW(kPathSize, w_file_path);
-      if (characters_written >= (kPathSize - 1)) {
-        return false;
-      }
-
-      if (characters_written < 1) {
-        return false;
-      }
-      // Remove the last slash, to match other Starboard implementations.
-      w_file_path[characters_written - 1] = L'\0';
-
-      std::string utf8_string =
-          starboard::shared::win32::wchar_tToUTF8(w_file_path);
-
-      if (SbStringCopy(file_path, utf8_string.c_str(), kPathSize) >=
-          kPathSize) {
-        return false;
-      }
-      SbDirectoryCreate(file_path);
-
-      size_t length = SbStringGetLength(file_path);
-      if (length < 1 || length > path_size) {
-        return false;
-      }
-
-      SbStringCopy(out_path, file_path, path_size);
-      break;
-    }
-    case kSbSystemPathExecutableFile: {
+    case kSbSystemPathContentDirectory:
+      return GetContentPath(out_path, path_size);
+    case kSbSystemPathDebugOutputDirectory:
+      return SbSystemGetPath(kSbSystemPathTempDirectory, out_path, path_size);
+    case kSbSystemPathExecutableFile:
       return GetExecutablePath(out_path, path_size);
-    }
+    case kSbSystemPathTempDirectory:
+      return CreateAndGetTempPath(out_path, path_size);
     // TODO: implement all the other cases.
     default:
       SB_NOTIMPLEMENTED();
       return false;
   }
-  return true;
 }
