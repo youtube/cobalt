7a8fc5f381dd4c68a3203169451f45a4792186f2
Cobalt Team <cobalt-team@google.com>
2017-05-19 14:49:33 -0700

    Add shaders for updated Skia oval rendering code.
    Also add renderer test for large ellipses to catch a rendering
    issue that was noticed on Android devices.
    
    b/37743412
    
    Change-Id: Ia899ed30b70faf8e20c1f5b78ffa2177bf9706a4

--- cobalt/renderer/glimp_shaders/glsl/fragment_skia_ellipse_masked_texture_domain.glsl
+++ cobalt/renderer/glimp_shaders/glsl/fragment_skia_ellipse_masked_texture_domain.glsl
@@ -2,6 +2,7 @@
 precision mediump float;
 uniform sampler2D uSampler0_Stage0;
 uniform vec4 uTexDom_Stage0;
+uniform vec2 uscale_Stage1;
 uniform vec4 uellipse_Stage1;
 uniform float uRTHeight;
 varying vec4 vColor;
@@ -22,11 +23,13 @@ void main() {
   {
     // Stage 1: Ellipse
     vec2 d = fragCoordYDown.xy - uellipse_Stage1.xy;
+    d *= uscale_Stage1.y;
     vec2 Z = d * uellipse_Stage1.zw;
     float implicit = dot(Z, d) - 1.0;
     float grad_dot = 4.0 * dot(Z, Z);
     grad_dot = max(grad_dot, 1.0e-4);
     float approx_dist = implicit * inversesqrt(grad_dot);
+    approx_dist *= uscale_Stage1.x;
     float alpha = clamp(0.5 - approx_dist, 0.0, 1.0);
     output_Stage1 = (output_Stage0 * alpha);
   }
