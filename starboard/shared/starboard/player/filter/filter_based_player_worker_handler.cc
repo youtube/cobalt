17d1450989025d98459cea9f859c1adcf93e3ab2
Xiaoming Shi <xiaomings@google.com>
2017-05-24 12:13:19 -0700

    Handle unsupported audio channels on Linux.
    The SbPlayer implementation relies on that the Cobalt side doesn't send
    any unsupported format to it.  This is usually true when running YT
    player on Cobalt as the format is pre-filtered by isTypeSupported().
    
    However during unittests it is possible that unsupported data (in this
    case, 5.1 audio) is sent to the SbPlayer implementation, which causes
    DCHECKs internally.
    
    Add checks in PlayerComponents::Create() to ensure that the audio
    parameters are supported on the platform.  Also guard member functions
    of FilterBasedPlayerWorkerHandler on creation failures.
    
    b/62056243
    
    Change-Id: Ifa3d994daf863a6e9924a4e8fccc3f09949145bb

--- starboard/shared/starboard/player/filter/filter_based_player_worker_handler.cc
+++ starboard/shared/starboard/player/filter/filter_based_player_worker_handler.cc
@@ -83,6 +83,7 @@ FilterBasedPlayerWorkerHandler::FilterBasedPlayerWorkerHandler(
   }
 #endif  // SB_API_VERSION >= SB_AUDIO_SPECIFIC_CONFIG_AS_POINTER
 
+  update_closure_ = Bind(&FilterBasedPlayerWorkerHandler::Update, this);
   bounds_ = PlayerWorker::Bounds();
 }
 
@@ -135,7 +136,6 @@ bool FilterBasedPlayerWorkerHandler::Init(
   ::starboard::ScopedLock lock(video_renderer_existence_mutex_);
   media_components->GetRenderers(&audio_renderer_, &video_renderer_);
 
-  update_closure_ = Bind(&FilterBasedPlayerWorkerHandler::Update, this);
   job_queue_->Schedule(update_closure_, kUpdateInterval);
 
   return true;
@@ -143,6 +143,10 @@ bool FilterBasedPlayerWorkerHandler::Init(
 
 bool FilterBasedPlayerWorkerHandler::Seek(SbMediaTime seek_to_pts, int ticket) {
   SB_DCHECK(job_queue_->BelongsToCurrentThread());
+
+  if (!audio_renderer_ || !video_renderer_) {
+    return false;
+  }
 
   if (seek_to_pts < 0) {
     SB_DLOG(ERROR) << "Try to seek to negative timestamp " << seek_to_pts;
@@ -159,6 +163,10 @@ bool FilterBasedPlayerWorkerHandler::WriteSample(InputBuffer input_buffer,
                                                  bool* written) {
   SB_DCHECK(job_queue_->BelongsToCurrentThread());
   SB_DCHECK(written != NULL);
+
+  if (!audio_renderer_ || !video_renderer_) {
+    return false;
+  }
 
   *written = true;
 
@@ -210,6 +218,10 @@ bool FilterBasedPlayerWorkerHandler::WriteSample(InputBuffer input_buffer,
 bool FilterBasedPlayerWorkerHandler::WriteEndOfStream(SbMediaType sample_type) {
   SB_DCHECK(job_queue_->BelongsToCurrentThread());
 
+  if (!audio_renderer_ || !video_renderer_) {
+    return false;
+  }
+
   if (sample_type == kSbMediaTypeAudio) {
     if (audio_renderer_->IsEndOfStreamWritten()) {
       SB_LOG(WARNING) << "Try to write audio EOS after EOS is enqueued";
@@ -232,6 +244,10 @@ bool FilterBasedPlayerWorkerHandler::WriteEndOfStream(SbMediaType sample_type) {
 bool FilterBasedPlayerWorkerHandler::SetPause(bool pause) {
   SB_DCHECK(job_queue_->BelongsToCurrentThread());
 
+  if (!audio_renderer_) {
+    return false;
+  }
+
   paused_ = pause;
 
   if (pause) {
@@ -248,6 +264,10 @@ bool FilterBasedPlayerWorkerHandler::SetPause(bool pause) {
 #if SB_API_VERSION >= 4
 bool FilterBasedPlayerWorkerHandler::SetPlaybackRate(double playback_rate) {
   SB_DCHECK(job_queue_->BelongsToCurrentThread());
+
+  if (!audio_renderer_) {
+    return false;
+  }
 
   audio_renderer_->SetPlaybackRate(playback_rate);
   return true;
@@ -271,6 +291,10 @@ bool FilterBasedPlayerWorkerHandler::SetBounds(
 // TODO: This should be driven by callbacks instead polling.
 void FilterBasedPlayerWorkerHandler::Update() {
   SB_DCHECK(job_queue_->BelongsToCurrentThread());
+
+  if (!audio_renderer_ || !video_renderer_) {
+    return;
+  }
 
   if ((*player_worker_.*get_player_state_cb_)() == kSbPlayerStatePrerolling) {
     if (!audio_renderer_->IsSeekingInProgress() &&
@@ -336,6 +360,7 @@ void FilterBasedPlayerWorkerHandler::Stop() {
 #if SB_API_VERSION >= 4
 SbDecodeTarget FilterBasedPlayerWorkerHandler::GetCurrentDecodeTarget() {
   ::starboard::ScopedLock lock(video_renderer_existence_mutex_);
+
   if (video_renderer_) {
     return video_renderer_->GetCurrentDecodeTarget();
   } else {
