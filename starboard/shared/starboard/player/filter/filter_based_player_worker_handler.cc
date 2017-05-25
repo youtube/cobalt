69c2f17bb52153ad1061947216dfbb2662b7edf7
Cobalt Team <cobalt-team@google.com>
2017-05-25 17:48:21 +0000

    Revert "Handle unsupported audio channels on Linux."
    This reverts commit 1e7b85ff756e2d7ef20f6b38bf1546d1aca738ed.
    
    b/62073428
    
    Change-Id: I72432baf9bc8b7e09e7a80d93faab318cbc170e9

--- starboard/shared/starboard/player/filter/filter_based_player_worker_handler.cc
+++ starboard/shared/starboard/player/filter/filter_based_player_worker_handler.cc
@@ -71,7 +71,6 @@ FilterBasedPlayerWorkerHandler::FilterBasedPlayerWorkerHandler(
       decode_target_provider_(provider)
 #endif  // SB_API_VERSION >= 4
 {
-  update_closure_ = Bind(&FilterBasedPlayerWorkerHandler::Update, this);
   bounds_ = PlayerWorker::Bounds();
 }
 
@@ -124,6 +123,7 @@ bool FilterBasedPlayerWorkerHandler::Init(
   ::starboard::ScopedLock lock(video_renderer_existence_mutex_);
   media_components->GetRenderers(&audio_renderer_, &video_renderer_);
 
+  update_closure_ = Bind(&FilterBasedPlayerWorkerHandler::Update, this);
   job_queue_->Schedule(update_closure_, kUpdateInterval);
 
   return true;
@@ -131,10 +131,6 @@ bool FilterBasedPlayerWorkerHandler::Init(
 
 bool FilterBasedPlayerWorkerHandler::Seek(SbMediaTime seek_to_pts, int ticket) {
   SB_DCHECK(job_queue_->BelongsToCurrentThread());
-
-  if (!audio_renderer_ || !video_renderer_) {
-    return false;
-  }
 
   if (seek_to_pts < 0) {
     SB_DLOG(ERROR) << "Try to seek to negative timestamp " << seek_to_pts;
@@ -151,10 +147,6 @@ bool FilterBasedPlayerWorkerHandler::WriteSample(InputBuffer input_buffer,
                                                  bool* written) {
   SB_DCHECK(job_queue_->BelongsToCurrentThread());
   SB_DCHECK(written != NULL);
-
-  if (!audio_renderer_ || !video_renderer_) {
-    return false;
-  }
 
   *written = true;
 
@@ -206,10 +198,6 @@ bool FilterBasedPlayerWorkerHandler::WriteSample(InputBuffer input_buffer,
 bool FilterBasedPlayerWorkerHandler::WriteEndOfStream(SbMediaType sample_type) {
   SB_DCHECK(job_queue_->BelongsToCurrentThread());
 
-  if (!audio_renderer_ || !video_renderer_) {
-    return false;
-  }
-
   if (sample_type == kSbMediaTypeAudio) {
     if (audio_renderer_->IsEndOfStreamWritten()) {
       SB_LOG(WARNING) << "Try to write audio EOS after EOS is enqueued";
@@ -232,10 +220,6 @@ bool FilterBasedPlayerWorkerHandler::WriteEndOfStream(SbMediaType sample_type) {
 bool FilterBasedPlayerWorkerHandler::SetPause(bool pause) {
   SB_DCHECK(job_queue_->BelongsToCurrentThread());
 
-  if (!audio_renderer_) {
-    return false;
-  }
-
   paused_ = pause;
 
   if (pause) {
@@ -252,10 +236,6 @@ bool FilterBasedPlayerWorkerHandler::SetPause(bool pause) {
 #if SB_API_VERSION >= 4
 bool FilterBasedPlayerWorkerHandler::SetPlaybackRate(double playback_rate) {
   SB_DCHECK(job_queue_->BelongsToCurrentThread());
-
-  if (!audio_renderer_) {
-    return false;
-  }
 
   audio_renderer_->SetPlaybackRate(playback_rate);
   return true;
@@ -279,10 +259,6 @@ bool FilterBasedPlayerWorkerHandler::SetBounds(
 // TODO: This should be driven by callbacks instead polling.
 void FilterBasedPlayerWorkerHandler::Update() {
   SB_DCHECK(job_queue_->BelongsToCurrentThread());
-
-  if (!audio_renderer_ || !video_renderer_) {
-    return;
-  }
 
   if ((*player_worker_.*get_player_state_cb_)() == kSbPlayerStatePrerolling) {
     if (!audio_renderer_->IsSeekingInProgress() &&
@@ -348,7 +324,6 @@ void FilterBasedPlayerWorkerHandler::Stop() {
 #if SB_API_VERSION >= 4
 SbDecodeTarget FilterBasedPlayerWorkerHandler::GetCurrentDecodeTarget() {
   ::starboard::ScopedLock lock(video_renderer_existence_mutex_);
-
   if (video_renderer_) {
     return video_renderer_->GetCurrentDecodeTarget();
   } else {
