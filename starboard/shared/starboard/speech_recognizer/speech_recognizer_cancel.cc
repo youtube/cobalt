0341783ed23e09c4ce86f0f08e5192ac0a7222c1
Cobalt Team <cobalt-team@google.com>
2017-06-16 16:08:25 -0700

    Freeze Starboard 5
    Change-Id: Ie6b33746130930b8f1d4b6d7b06ef3dda6ec6645

--- starboard/shared/starboard/speech_recognizer/speech_recognizer_cancel.cc
+++ starboard/shared/starboard/speech_recognizer/speech_recognizer_cancel.cc
@@ -14,8 +14,7 @@
 
 #include "starboard/speech_recognizer.h"
 
-#if SB_HAS(SPEECH_RECOGNIZER) && \
-    SB_API_VERSION >= SB_SPEECH_RECOGNIZER_API_VERSION
+#if SB_HAS(SPEECH_RECOGNIZER) && SB_API_VERSION >= 5
 
 #include "starboard/log.h"
 #include "starboard/shared/starboard/speech_recognizer/speech_recognizer_internal.h"
@@ -26,5 +25,4 @@ void SbSpeechRecognizerCancel(SbSpeechRecognizer recognizer) {
   }
 }
 
-#endif  // SB_HAS(SPEECH_RECOGNIZER) && SB_API_VERSION >=
-        // SB_SPEECH_RECOGNIZER_API_VERSION
+#endif  // SB_HAS(SPEECH_RECOGNIZER) && SB_API_VERSION >= 5
