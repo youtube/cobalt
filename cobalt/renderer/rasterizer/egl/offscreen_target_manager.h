cc7846244dffb7ffa7a626fe0e95f1d35c1d5406
Toan Pham <toanpham@google.com>
2017-06-28 16:53:46 -0700

    Allow skia to render directly to the render target of the GLES rasterizer.
    This also changes direct-GLES to use bottom-left origin for the skia
    canvas.
    
    Change-Id: Ia04d05cd870d59db68d7169074040d0382241707

--- cobalt/renderer/rasterizer/egl/offscreen_target_manager.h
+++ cobalt/renderer/rasterizer/egl/offscreen_target_manager.h
@@ -15,6 +15,7 @@
 #ifndef COBALT_RENDERER_RASTERIZER_EGL_OFFSCREEN_TARGET_MANAGER_H_
 #define COBALT_RENDERER_RASTERIZER_EGL_OFFSCREEN_TARGET_MANAGER_H_
 
+#include "base/callback.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/scoped_vector.h"
 #include "cobalt/math/rect_f.h"
@@ -23,7 +24,7 @@
 #include "cobalt/renderer/backend/egl/framebuffer_render_target.h"
 #include "cobalt/renderer/backend/egl/graphics_context.h"
 #include "third_party/skia/include/core/SkCanvas.h"
-#include "third_party/skia/include/gpu/GrContext.h"
+#include "third_party/skia/include/core/SkSurface.h"
 
 namespace cobalt {
 namespace renderer {
@@ -35,6 +36,9 @@ namespace egl {
 // minimize the cost of switching render targets.
 class OffscreenTargetManager {
  public:
+  typedef base::Callback<SkSurface*(const backend::RenderTarget*)>
+      CreateFallbackSurfaceFunction;
+
   struct TargetInfo {
     TargetInfo()
         : framebuffer(NULL),
@@ -47,7 +51,8 @@ class OffscreenTargetManager {
   };
 
   OffscreenTargetManager(backend::GraphicsContextEGL* graphics_context,
-                         GrContext* skia_context, size_t memory_limit);
+      const CreateFallbackSurfaceFunction& create_fallback_surface,
+      size_t memory_limit);
   ~OffscreenTargetManager();
 
   // Update must be called once per frame, before any allocation requests are
@@ -79,7 +84,7 @@ class OffscreenTargetManager {
   OffscreenAtlas* CreateOffscreenAtlas(const math::Size& size);
 
   backend::GraphicsContextEGL* graphics_context_;
-  GrContext* skia_context_;
+  CreateFallbackSurfaceFunction create_fallback_surface_;
 
   ScopedVector<OffscreenAtlas> offscreen_atlases_;
   scoped_ptr<OffscreenAtlas> offscreen_cache_;
