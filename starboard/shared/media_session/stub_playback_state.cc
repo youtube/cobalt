b7f0546070d79544e132be7de42497f60d6c7fd2
Cobalt Team <cobalt-team@google.com>
2017-07-24 14:26:39 -0700

    xb1: Implement mediasession
    This CL implements partial support for media session intended to work
    with Xbox One Media Remote.
    
    Change-Id: I8d0f33dc98acde0601f66d4c9e8cd85ddfa34a5f

--- starboard/shared/media_session/stub_playback_state.cc
+++ starboard/shared/media_session/stub_playback_state.cc
@@ -1,4 +1,4 @@
-// Copyright 2016 Google Inc. All Rights Reserved.
+// Copyright 2017 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,19 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "starboard/player.h"
+#include "starboard/log.h"
 
 #include "starboard/shared/media_session/playback_state.h"
-#include "starboard/shared/starboard/player/player_internal.h"
 
-using starboard::shared::media_session::kNone;
-using starboard::shared::media_session::
-    UpdateActiveSessionPlatformPlaybackState;
+namespace starboard {
+namespace shared {
+namespace media_session {
 
-void SbPlayerDestroy(SbPlayer player) {
-  if (!SbPlayerIsValid(player)) {
-    return;
-  }
-  UpdateActiveSessionPlatformPlaybackState(kNone);
-  delete player;
+void UpdateActiveSessionPlatformPlaybackState(PlaybackState state) {
+  SB_UNREFERENCED_PARAMETER(state);
+  SB_NOTIMPLEMENTED();
 }
+
+}  // namespace media_session
+}  // namespace shared
+}  // namespace starboard
