60615b41d488d8dae0f1a8fc52419b714ea56e1a
Cobalt Team <cobalt-team@google.com>
2017-07-26 18:56:48 -0700

    Fix warnings
    This CL fixes some warnings found by MSVC++.
    
    Change-Id: Iaac021209ae2e462cd3e781a3df007371fa5a608

--- starboard/shared/starboard/player/video_frame_internal.cc
+++ starboard/shared/starboard/player/video_frame_internal.cc
@@ -55,13 +55,18 @@ void EnsureYUVToRGBLookupTableInitialized() {
   SbMemorySet(s_clamp_table, 0, 512);
   SbMemorySet(s_clamp_table + 768, 0xff, 512);
 
-  for (int i = 0; i < 256; ++i) {
-    s_y_to_rgb[i] = (static_cast<uint8_t>(i) - 16) * 1.164f;
-    s_v_to_r[i] = (static_cast<uint8_t>(i) - 128) * 1.793f;
-    s_u_to_g[i] = (static_cast<uint8_t>(i) - 128) * -0.213;
-    s_v_to_g[i] = (static_cast<uint8_t>(i) - 128) * -0.533f;
-    s_u_to_b[i] = (static_cast<uint8_t>(i) - 128) * 2.112f;
-    s_clamp_table[512 + i] = i;
+  uint8_t i = 0;
+  while (true) {
+    s_y_to_rgb[i] = static_cast<int>((i - 16) * 1.164f);
+    s_v_to_r[i] = static_cast<int>((i - 128) * 1.793f);
+    s_u_to_g[i] = static_cast<int>((i - 128) * -0.213f);
+    s_v_to_g[i] = static_cast<int>((i - 128) * -0.533f);
+    s_u_to_b[i] = static_cast<int>((i - 128) * 2.112f);
+    s_clamp_table[512 + static_cast<std::size_t>(i)] = i;
+    if (i == 255) {
+      break;
+    }
+    ++i;
   }
 
   s_yuv_to_rgb_lookup_table_initialized = true;
