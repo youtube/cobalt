5f6d0c84878e36cf2438d84f09131b36a2cd1eef
Cobalt Team <cobalt-team@google.com>
2017-07-28 16:12:58 -0700

    Fix Typo
    We should be using int32 max instead of int32 min for clamping purposes.
    
    Change-Id: Ib990e7fb6806712fc7a9332c05c1078f81c06176
    (cherry picked from commit 4fa4b8c0a93d0b7af2b2edfe876ea06dc2842b55)

--- starboard/shared/starboard/localized_strings.cc
+++ starboard/shared/starboard/localized_strings.cc
@@ -71,7 +71,7 @@ bool ReadFile(const std::string& filename, std::string* out_result) {
   char* buffer_pos = buffer;
   while (bytes_to_read > 0) {
     int max_bytes_to_read = static_cast<int>(
-        std::min(static_cast<int64_t>(kSbInt32Min), bytes_to_read));
+        std::min(static_cast<int64_t>(kSbInt32Max), bytes_to_read));
     int bytes_read = file.Read(buffer_pos, max_bytes_to_read);
     if (bytes_read < 0) {
       SB_DLOG(ERROR) << "Read from i18n file failed.";
