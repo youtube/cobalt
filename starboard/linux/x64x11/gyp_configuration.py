ddea16f0a2c5382e13a13cc788d47e22bc41f924
Cobalt Team <cobalt-team@google.com>
2017-07-28 18:35:16 -0700

    Implement abstract toolchain
    - Defined tool abstractions.
    - Provided tool implementations required by x86 X11 Linux.
      Some subconfigurations, such as Widevine, require a dynamic linker.
      Migrating them to the abstract toolchain is outside of the scope
      of this CL.
    - BONUS: Implemented libtool which is only used on Darwin.
    
    Change-Id: Ic24d0b64011cc8b3d7e54259b320683be2da6936

--- starboard/linux/x64x11/gyp_configuration.py
+++ starboard/linux/x64x11/gyp_configuration.py
@@ -14,19 +14,45 @@
 """Starboard Linux X64 X11 platform configuration for gyp_cobalt."""
 
 import logging
-import os
-import sys
 
-# Import the shared Linux platform configuration.
-sys.path.append(
-    os.path.realpath(
-        os.path.join(os.path.dirname(__file__), os.pardir, 'shared')))
-import gyp_configuration
+from starboard.linux.shared import gyp_configuration
+from starboard.tools.toolchain import ar
+from starboard.tools.toolchain import bash
+from starboard.tools.toolchain import clang
+from starboard.tools.toolchain import clangxx
+from starboard.tools.toolchain import cp
+from starboard.tools.toolchain import touch
+
+
+class PlatformConfig(gyp_configuration.PlatformConfig):
+
+  def __init__(self):
+    super(PlatformConfig, self).__init__('linux-x64x11')
+
+  def GetTargetToolchain(self):
+    return self.GetHostToolchain()
+
+  def GetHostToolchain(self):
+    environment_variables = self.GetEnvironmentVariables()
+    cc_path = environment_variables['CC']
+    cxx_path = environment_variables['CXX']
+
+    return [
+        clang.CCompiler(path=cc_path),
+        clang.CxxCompiler(path=cxx_path),
+        clang.AssemblerWithCPreprocessor(path=cc_path),
+        ar.StaticThinLinker(),
+        ar.StaticLinker(),
+        clangxx.ExecutableLinker(path=cxx_path),
+        cp.Copy(),
+        touch.Stamp(),
+        bash.Shell(),
+    ]
 
 
 def CreatePlatformConfig():
   try:
-    return gyp_configuration.PlatformConfig('linux-x64x11')
+    return PlatformConfig()
   except RuntimeError as e:
     logging.critical(e)
     return None
