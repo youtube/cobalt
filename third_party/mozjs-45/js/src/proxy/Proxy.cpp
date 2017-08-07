03ce1d924c4153dcaae363c5783990b8ae4e0281
Cobalt Team <cobalt-team@google.com>
2017-08-06 19:22:49 -0700

    Add too much recursion check to Proxy::isArray
    b/64129986
    
    Change-Id: I6b6614e447823c81212346463a9b4d7f0ae27f6d

--- third_party/mozjs-45/js/src/proxy/Proxy.cpp
+++ third_party/mozjs-45/js/src/proxy/Proxy.cpp
@@ -443,6 +443,7 @@ Proxy::getBuiltinClass(JSContext* cx, HandleObject proxy, ESClassValue* classVal
 bool
 Proxy::isArray(JSContext* cx, HandleObject proxy, JS::IsArrayAnswer* answer)
 {
+    JS_CHECK_RECURSION(cx, return false);
     return proxy->as<ProxyObject>().handler()->isArray(cx, proxy, answer);
 }
 
