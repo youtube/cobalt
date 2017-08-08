61cdc7f543fcfd318f9f6b26341f7e5b00ee5c7d
Cobalt Team <cobalt-team@google.com>
2017-08-08 16:27:43 -0700

    Add conditional weak heap object manager tracking
    Change-Id: If4c36a0cee4a16ab447bab27d475e2443a235487

--- cobalt/script/mozjs-45/weak_heap_object_manager.h
+++ cobalt/script/mozjs-45/weak_heap_object_manager.h
@@ -14,7 +14,21 @@
 #ifndef COBALT_SCRIPT_MOZJS_45_WEAK_HEAP_OBJECT_MANAGER_H_
 #define COBALT_SCRIPT_MOZJS_45_WEAK_HEAP_OBJECT_MANAGER_H_
 
+#include <vector>
+
 #include "base/hash_tables.h"
+
+// Can be locally set to 1 to track the call stack where a weak heap object
+// was tracked from, and then dump the call stacks of any remaining objects in
+// |WeakHeapObjectManager|'s destructor before hitting the DCHECK that
+// verifies that no objects are remaining.
+//
+// Example workflow for processing the output of this on linux:
+//   # Copy a dumped callstack to the clipboard.
+//   $ xclip -selection c -o |
+//       grep -o '0x[[:xdigit:]]*' |
+//       addr2line -e out/linux-x64x11_debug/cobalt
+#define COBALT_TRACK_WEAK_HEAP_OBJECT_ADDED_FROM_LOCATION 0
 
 namespace cobalt {
 namespace script {
@@ -43,6 +57,10 @@ class WeakHeapObjectManager {
 
   typedef base::hash_set<WeakHeapObject*> WeakHeapObjects;
   WeakHeapObjects weak_objects_;
+
+#if COBALT_TRACK_WEAK_HEAP_OBJECT_ADDED_FROM_LOCATION
+  base::hash_map<WeakHeapObject*, std::vector<void*>> added_from_;
+#endif
 
   friend class WeakHeapObject;
 };
