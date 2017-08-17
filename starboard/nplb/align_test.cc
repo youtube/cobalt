ca05a214192d2e056a0925d4c8718870a402c72c
Cobalt Team <cobalt-team@google.com>
2017-08-17 14:24:15 -0700

    Remove flaky alignment tests
    Change-Id: I2134a441c8f1a11f4ee1bacee97514d9b391ca50

--- starboard/nplb/align_test.cc
+++ starboard/nplb/align_test.cc
@@ -65,24 +65,9 @@ struct AlignedFieldsOf {
   char unaligned4;
 };
 
-TEST(SbAlignTest, AlignAsStructFieldOnHeap) {
-  scoped_ptr<AlignedFields> aligned(new AlignedFields());
-  EXPECT_LE(2, GetAlignment(&(aligned->by_2)));
-  EXPECT_LE(4, GetAlignment(&(aligned->by_4)));
-  EXPECT_LE(8, GetAlignment(&(aligned->by_8)));
-  EXPECT_LE(16, GetAlignment(&(aligned->by_16)));
-
-#if !SB_HAS_QUIRK(DOES_NOT_ALIGN_FIELDS_IN_HEAP_OVER_16_BYTES)
-  EXPECT_LE(32, GetAlignment(&(aligned->by_32)));
-  EXPECT_LE(64, GetAlignment(&(aligned->by_64)));
-  EXPECT_LE(128, GetAlignment(&(aligned->by_128)));
-#endif  // !SB_HAS_QUIRK(DOES_NOT_ALIGN_FIELDS_IN_HEAP_OVER_16_BYTES)
-
-  // Even Linux clang does not support heap-allocated struct fields aligned to
-  // 256 bytes.
-}
-
 TEST(SbAlignTest, AlignAsStructFieldOnStack) {
+  char unaligned = 0;
+  EXPECT_NE(1, unaligned);
   AlignedFields aligned;
   EXPECT_LE(2, GetAlignment(&(aligned.by_2)));
   EXPECT_LE(4, GetAlignment(&(aligned.by_4)));
@@ -95,10 +80,20 @@ TEST(SbAlignTest, AlignAsStructFieldOnStack) {
 }
 
 TEST(SbAlignTest, AlignAsStackVariable) {
+  char unaligned1 = 1;
   SB_ALIGNAS(2) char by_2;
+  char unaligned2 = 2;
+  EXPECT_NE(unaligned2, unaligned1);  // These are to try to keep the stack
+                                      // variables around.
   SB_ALIGNAS(4) char by_4;
+  char unaligned3 = 3;
+  EXPECT_NE(unaligned3, unaligned1);
   SB_ALIGNAS(8) char by_8;
+  char unaligned4 = 4;
+  EXPECT_NE(unaligned4, unaligned1);
   SB_ALIGNAS(16) char by_16;
+  char unaligned5 = 5;
+  EXPECT_NE(unaligned5, unaligned1);
 
   EXPECT_LE(2, GetAlignment(&by_2));
   EXPECT_LE(4, GetAlignment(&by_4));
@@ -107,9 +102,17 @@ TEST(SbAlignTest, AlignAsStackVariable) {
 
 #if !SB_HAS_QUIRK(DOES_NOT_STACK_ALIGN_OVER_16_BYTES)
   SB_ALIGNAS(32) char by_32;
+  char unaligned6 = 6;
+  EXPECT_NE(unaligned6, unaligned1);
   SB_ALIGNAS(64) char by_64;
+  char unaligned7 = 7;
+  EXPECT_NE(unaligned7, unaligned1);
   SB_ALIGNAS(128) char by_128;
+  char unaligned8 = 8;
+  EXPECT_NE(unaligned8, unaligned1);
   SB_ALIGNAS(256) char by_256;
+  char unaligned9 = 9;
+  EXPECT_NE(unaligned9, unaligned1);
 
   EXPECT_LE(32, GetAlignment(&by_32));
   EXPECT_LE(64, GetAlignment(&by_64));
@@ -128,20 +131,18 @@ TEST(SbAlignTest, AlignOf) {
 }
 
 TEST(SbAlignTest, AlignAsAlignOfStructFieldOnStack) {
+  char unaligned = 0;
+  EXPECT_NE(1, unaligned);
+
   AlignedFieldsOf aligned;
   EXPECT_LE(SB_ALIGNOF(void*), GetAlignment(&(aligned.by_void_star)));
   EXPECT_LE(SB_ALIGNOF(int), GetAlignment(&(aligned.by_int)));
   EXPECT_LE(SB_ALIGNOF(uint64_t), GetAlignment(&(aligned.by_uint64_t)));
 }
 
-TEST(SbAlignTest, AlignAsAlignOfStructFieldOnHeap) {
-  scoped_ptr<AlignedFieldsOf> aligned(new AlignedFieldsOf());
-  EXPECT_LE(SB_ALIGNOF(void*), GetAlignment(&(aligned->by_void_star)));
-  EXPECT_LE(SB_ALIGNOF(int), GetAlignment(&(aligned->by_int)));
-  EXPECT_LE(SB_ALIGNOF(uint64_t), GetAlignment(&(aligned->by_uint64_t)));
-}
-
 TEST(SbAlignTest, AlignAsAlignOfStackVariable) {
+  char unaligned = 0;
+  EXPECT_NE(1, unaligned);
   SB_ALIGNAS(SB_ALIGNOF(void*)) char by_void_star;
   SB_ALIGNAS(SB_ALIGNOF(int)) char by_int;
   SB_ALIGNAS(SB_ALIGNOF(uint64_t)) char by_uint64_t;
