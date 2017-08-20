26bd40785aa7fc899b491f7ea3667380e3ec69f9
Toan Pham <toanpham@google.com>
2017-08-19 20:51:45 -0700

    Add support for index buffers in the direct-gles rasterizer.
    Change-Id: I13c34a2fc5681333273c44d3091fee307a2a3604

--- cobalt/renderer/rasterizer/egl/graphics_state.cc
+++ cobalt/renderer/rasterizer/egl/graphics_state.cc
@@ -45,10 +45,15 @@ GraphicsState::GraphicsState()
     : render_target_handle_(0),
       render_target_serial_(0),
       frame_index_(0),
+      vertex_data_capacity_(0),
       vertex_data_reserved_(kVertexDataAlignment - 1),
       vertex_data_allocated_(0),
-      vertex_data_buffer_updated_(false) {
-  GL_CALL(glGenBuffers(kNumFramesBuffered, &vertex_data_buffer_handle_[0]));
+      vertex_index_capacity_(0),
+      vertex_index_reserved_(0),
+      vertex_index_allocated_(0),
+      vertex_buffers_updated_(false) {
+  GL_CALL(glGenBuffers(kNumFramesBuffered, vertex_data_buffer_handle_));
+  GL_CALL(glGenBuffers(kNumFramesBuffered, vertex_index_buffer_handle_));
   memset(clip_adjustment_, 0, sizeof(clip_adjustment_));
   SetDirty();
   blend_enabled_ = false;
@@ -56,7 +61,8 @@ GraphicsState::GraphicsState()
 }
 
 GraphicsState::~GraphicsState() {
-  GL_CALL(glDeleteBuffers(kNumFramesBuffered, &vertex_data_buffer_handle_[0]));
+  GL_CALL(glDeleteBuffers(kNumFramesBuffered, vertex_data_buffer_handle_));
+  GL_CALL(glDeleteBuffers(kNumFramesBuffered, vertex_index_buffer_handle_));
 }
 
 void GraphicsState::SetDirty() {
@@ -66,8 +72,14 @@ void GraphicsState::SetDirty() {
 
 void GraphicsState::BeginFrame() {
   DCHECK_EQ(vertex_data_allocated_, 0);
-  if (vertex_data_reserved_ > vertex_data_buffer_.size()) {
-    vertex_data_buffer_.resize(vertex_data_reserved_);
+  if (vertex_data_reserved_ > vertex_data_capacity_) {
+    vertex_data_capacity_ = vertex_data_reserved_;
+    vertex_data_buffer_.reset(new uint8_t[vertex_data_capacity_]);
+  }
+  DCHECK_EQ(vertex_index_allocated_, 0);
+  if (vertex_index_reserved_ > vertex_index_capacity_) {
+    vertex_index_capacity_ = vertex_index_reserved_;
+    vertex_index_buffer_.reset(new uint16_t[vertex_index_capacity_]);
   }
 
   // Reset to default GL state. Assume the current state is dirty, so just
@@ -78,10 +90,12 @@ void GraphicsState::BeginFrame() {
 }
 
 void GraphicsState::EndFrame() {
-  // Reset the vertex data buffer.
+  // Reset the vertex data and index buffers.
   vertex_data_reserved_ = kVertexDataAlignment - 1;
   vertex_data_allocated_ = 0;
-  vertex_data_buffer_updated_ = false;
+  vertex_index_reserved_ = 0;
+  vertex_index_allocated_ = 0;
+  vertex_buffers_updated_ = false;
   frame_index_ = (frame_index_ + 1) % kNumFramesBuffered;
 
   // Force default GL state. The current state may be marked dirty, so don't
@@ -121,6 +135,10 @@ void GraphicsState::UseProgram(GLuint program) {
   if (array_buffer_handle_ != vertex_data_buffer_handle_[frame_index_]) {
     array_buffer_handle_ = vertex_data_buffer_handle_[frame_index_];
     GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, array_buffer_handle_));
+  }
+  if (index_buffer_handle_ != vertex_index_buffer_handle_[frame_index_]) {
+    index_buffer_handle_ = vertex_index_buffer_handle_[frame_index_];
+    GL_CALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer_handle_));
   }
 
   // Disable any vertex attribute arrays that will not be used.
@@ -277,13 +295,13 @@ void GraphicsState::UpdateTransformMatrix(GLint handle,
 
 void GraphicsState::ReserveVertexData(size_t bytes) {
   DCHECK_EQ(vertex_data_allocated_, 0);
-  DCHECK(!vertex_data_buffer_updated_);
+  DCHECK(!vertex_buffers_updated_);
   vertex_data_reserved_ += bytes + (kVertexDataAlignment - 1) &
                            ~(kVertexDataAlignment - 1);
 }
 
 uint8_t* GraphicsState::AllocateVertexData(size_t bytes) {
-  DCHECK(!vertex_data_buffer_updated_);
+  DCHECK(!vertex_buffers_updated_);
 
   // Ensure the start address is aligned.
   uintptr_t start_address =
@@ -298,16 +316,50 @@ uint8_t* GraphicsState::AllocateVertexData(size_t bytes) {
   return reinterpret_cast<uint8_t*>(start_address);
 }
 
-void GraphicsState::UpdateVertexData() {
-  DCHECK(!vertex_data_buffer_updated_);
-  vertex_data_buffer_updated_ = true;
+void GraphicsState::ReserveVertexIndices(size_t count) {
+  DCHECK_EQ(vertex_index_allocated_, 0);
+  DCHECK(!vertex_buffers_updated_);
+  vertex_index_reserved_ += count;
+}
+
+uint16_t* GraphicsState::AllocateVertexIndices(size_t count) {
+  uint16_t* client_pointer = &vertex_index_buffer_[vertex_index_allocated_];
+  vertex_index_allocated_ += count;
+  DCHECK_LE(vertex_index_allocated_, vertex_index_reserved_);
+  return client_pointer;
+}
+
+const void* GraphicsState::GetVertexIndexPointer(
+    const uint16_t* client_pointer) {
+  DCHECK_GE(client_pointer, &vertex_index_buffer_[0]);
+  DCHECK_LE(client_pointer, &vertex_index_buffer_[vertex_index_allocated_]);
+  const GLvoid* gl_pointer = reinterpret_cast<const GLvoid*>(
+      reinterpret_cast<const uint8_t*>(client_pointer) -
+      reinterpret_cast<const uint8_t*>(&vertex_index_buffer_[0]));
+  return gl_pointer;
+}
+
+void GraphicsState::UpdateVertexBuffers() {
+  DCHECK(!vertex_buffers_updated_);
+  vertex_buffers_updated_ = true;
+
   if (array_buffer_handle_ != vertex_data_buffer_handle_[frame_index_]) {
     array_buffer_handle_ = vertex_data_buffer_handle_[frame_index_];
     GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, array_buffer_handle_));
   }
   if (vertex_data_allocated_ > 0) {
     GL_CALL(glBufferData(GL_ARRAY_BUFFER, vertex_data_allocated_,
-                         &vertex_data_buffer_[0], GL_STREAM_DRAW));
+                         &vertex_data_buffer_[0], GL_DYNAMIC_DRAW));
+  }
+
+  if (index_buffer_handle_ != vertex_index_buffer_handle_[frame_index_]) {
+    index_buffer_handle_ = vertex_index_buffer_handle_[frame_index_];
+    GL_CALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer_handle_));
+  }
+  if (vertex_index_allocated_ > 0) {
+    GL_CALL(glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                         vertex_index_allocated_ * sizeof(uint16_t),
+                         &vertex_index_buffer_[0], GL_DYNAMIC_DRAW));
   }
 }
 
@@ -351,6 +403,7 @@ void GraphicsState::Reset() {
   GL_CALL(glEnable(GL_SCISSOR_TEST));
 
   array_buffer_handle_ = 0;
+  index_buffer_handle_ = 0;
   texture_unit_ = 0;
   memset(&texunit_target_, 0, sizeof(texunit_target_));
   memset(&texunit_texture_, 0, sizeof(texunit_texture_));
@@ -358,9 +411,11 @@ void GraphicsState::Reset() {
   disable_vertex_attrib_array_mask_ = 0;
   clip_adjustment_dirty_ = true;
 
-  if (vertex_data_buffer_updated_) {
+  if (vertex_buffers_updated_) {
     array_buffer_handle_ = vertex_data_buffer_handle_[frame_index_];
     GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, array_buffer_handle_));
+    index_buffer_handle_ = vertex_index_buffer_handle_[frame_index_];
+    GL_CALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer_handle_));
   }
 
   if (blend_enabled_) {
