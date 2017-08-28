9e97bb373a40e1bbafb704324b66d08620c1d02a
Jelle Foks <jfoks@google.com>
2017-08-28 13:55:53 -0700

    Fixes for dictionary initializers.
    This fixes dictionary initialization for dictionaries derived from
    other dictionaries.
    
    This fixes the dictionary initializer for the 'any' value.
    
    This adds tests for dictionary member values.
    
    This fixes b/65063775
    This fixes b/65119301
    
    Change-Id: I58c56378486affb3339e2efb8de3a0f71482cf53

--- cobalt/bindings/generated/mozjs45/testing/cobalt/bindings/testing/mozjs_operations_test_interface.cc
+++ cobalt/bindings/generated/mozjs45/testing/cobalt/bindings/testing/mozjs_operations_test_interface.cc
@@ -1817,7 +1817,7 @@ void InitializePrototypeAndInterfaceObject(
 }
 
 inline InterfaceData* GetInterfaceData(JSContext* context) {
-  const int kInterfaceUniqueId = 38;
+  const int kInterfaceUniqueId = 39;
   MozjsGlobalEnvironment* global_environment =
       static_cast<MozjsGlobalEnvironment*>(JS_GetContextPrivate(context));
   // By convention, the |MozjsGlobalEnvironment| that we are associated with
