fbe1907a747bf26ff53828064fddedf9b5a10cd7
Cobalt Team <cobalt-team@google.com>
2017-08-30 14:33:45 -0700

    Implement GCC SbAtomic* using __atomic_* intrinsics
    This change swaps out the __sync_* based GCC SbAtomic implementation
    for the newer __atomic_* intrinsics instead.  These have the benefit
    of better TSAN integration, as well as what appears to be slightly
    better performance, at least in initial raspi-2 measurements.
    
    Change-Id: If9f6a116b744f39297891fa5e63011782c86af57

--- starboard/shared/gcc/atomic_gcc_public.h
+++ starboard/shared/gcc/atomic_gcc_public.h
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// These definitions were mostly extracted from Chromium's
-// base/atomicops_internals_gcc.h.
-
 #ifndef STARBOARD_SHARED_GCC_ATOMIC_GCC_PUBLIC_H_
 #define STARBOARD_SHARED_GCC_ATOMIC_GCC_PUBLIC_H_
 
@@ -28,97 +25,79 @@ static SB_C_FORCE_INLINE SbAtomic32
 SbAtomicNoBarrier_CompareAndSwap(volatile SbAtomic32* ptr,
                                  SbAtomic32 old_value,
                                  SbAtomic32 new_value) {
-  SbAtomic32 prev_value;
-  do {
-    if (__sync_bool_compare_and_swap(ptr, old_value, new_value))
-      return old_value;
-    prev_value = *ptr;
-  } while (prev_value == old_value);
-  return prev_value;
-}
-
-static SB_C_FORCE_INLINE SbAtomic32
-SbAtomicNoBarrier_Exchange(volatile SbAtomic32* ptr, SbAtomic32 new_value) {
-  SbAtomic32 old_value;
-  do {
-    old_value = *ptr;
-  } while (!__sync_bool_compare_and_swap(ptr, old_value, new_value));
+  __atomic_compare_exchange_n(ptr, &old_value, new_value, false,
+                              __ATOMIC_RELAXED, __ATOMIC_RELAXED);
   return old_value;
 }
 
 static SB_C_FORCE_INLINE SbAtomic32
+SbAtomicNoBarrier_Exchange(volatile SbAtomic32* ptr, SbAtomic32 new_value) {
+  return __atomic_exchange_n(ptr, new_value, __ATOMIC_RELAXED);
+}
+
+static SB_C_FORCE_INLINE SbAtomic32
 SbAtomicNoBarrier_Increment(volatile SbAtomic32* ptr, SbAtomic32 increment) {
-  return SbAtomicBarrier_Increment(ptr, increment);
+  return increment + __atomic_fetch_add(ptr, increment, __ATOMIC_RELAXED);
 }
 
 static SB_C_FORCE_INLINE SbAtomic32
 SbAtomicBarrier_Increment(volatile SbAtomic32* ptr,
                           SbAtomic32 increment) {
-  for (;;) {
-    // Atomic exchange the old value with an incremented one.
-    SbAtomic32 old_value = *ptr;
-    SbAtomic32 new_value = old_value + increment;
-    if (__sync_bool_compare_and_swap(ptr, old_value, new_value)) {
-      // The exchange took place as expected.
-      return new_value;
-    }
-    // Otherwise, *ptr changed mid-loop and we need to retry.
-  }
+  return increment + __atomic_fetch_add(ptr, increment, __ATOMIC_SEQ_CST);
 }
 
 static SB_C_FORCE_INLINE SbAtomic32
 SbAtomicAcquire_CompareAndSwap(volatile SbAtomic32* ptr,
                                SbAtomic32 old_value,
                                SbAtomic32 new_value) {
-  // Since NoBarrier_CompareAndSwap uses __sync_bool_compare_and_swap, which
-  // is a full memory barrier, none is needed here or below in Release.
-  return SbAtomicNoBarrier_CompareAndSwap(ptr, old_value, new_value);
+  __atomic_compare_exchange_n(ptr, &old_value, new_value, false,
+                              __ATOMIC_ACQUIRE, __ATOMIC_ACQUIRE);
+  return old_value;
 }
 
 static SB_C_FORCE_INLINE SbAtomic32
 SbAtomicRelease_CompareAndSwap(volatile SbAtomic32* ptr,
                                SbAtomic32 old_value,
                                SbAtomic32 new_value) {
-  return SbAtomicNoBarrier_CompareAndSwap(ptr, old_value, new_value);
+  __atomic_compare_exchange_n(ptr, &old_value, new_value, false,
+                              __ATOMIC_RELEASE, __ATOMIC_RELAXED);
+  return old_value;
 }
 
 static SB_C_FORCE_INLINE void SbAtomicMemoryBarrier() {
-  __sync_synchronize();
+  __atomic_thread_fence(__ATOMIC_SEQ_CST);
 }
 
 static SB_C_FORCE_INLINE void SbAtomicNoBarrier_Store(volatile SbAtomic32* ptr,
                                                       SbAtomic32 value) {
-  *ptr = value;
+  __atomic_store_n(ptr, value, __ATOMIC_RELAXED);
 }
 
 static SB_C_FORCE_INLINE void SbAtomicAcquire_Store(volatile SbAtomic32* ptr,
                                                     SbAtomic32 value) {
-  *ptr = value;
+  __atomic_store_n(ptr, value, __ATOMIC_RELAXED);
   SbAtomicMemoryBarrier();
 }
 
 static SB_C_FORCE_INLINE void SbAtomicRelease_Store(volatile SbAtomic32* ptr,
                                                     SbAtomic32 value) {
-  SbAtomicMemoryBarrier();
-  *ptr = value;
+  __atomic_store_n(ptr, value, __ATOMIC_RELEASE);
 }
 
 static SB_C_FORCE_INLINE SbAtomic32
 SbAtomicNoBarrier_Load(volatile const SbAtomic32* ptr) {
-  return *ptr;
+  return __atomic_load_n(ptr, __ATOMIC_RELAXED);
 }
 
 static SB_C_FORCE_INLINE SbAtomic32
 SbAtomicAcquire_Load(volatile const SbAtomic32* ptr) {
-  SbAtomic32 value = *ptr;
-  SbAtomicMemoryBarrier();
-  return value;
+  return __atomic_load_n(ptr, __ATOMIC_ACQUIRE);
 }
 
 static SB_C_FORCE_INLINE SbAtomic32
 SbAtomicRelease_Load(volatile const SbAtomic32* ptr) {
   SbAtomicMemoryBarrier();
-  return *ptr;
+  return __atomic_load_n(ptr, __ATOMIC_RELAXED);
 }
 
 // 64-bit atomic operations (only available on 64-bit processors).
@@ -127,91 +106,75 @@ static SB_C_FORCE_INLINE SbAtomic64
 SbAtomicNoBarrier_CompareAndSwap64(volatile SbAtomic64* ptr,
                                    SbAtomic64 old_value,
                                    SbAtomic64 new_value) {
-  SbAtomic64 prev_value;
-  do {
-    if (__sync_bool_compare_and_swap(ptr, old_value, new_value))
-      return old_value;
-    prev_value = *ptr;
-  } while (prev_value == old_value);
-  return prev_value;
-}
-
-static SB_C_FORCE_INLINE SbAtomic64
-SbAtomicNoBarrier_Exchange64(volatile SbAtomic64* ptr, SbAtomic64 new_value) {
-  SbAtomic64 old_value;
-  do {
-    old_value = *ptr;
-  } while (!__sync_bool_compare_and_swap(ptr, old_value, new_value));
+  __atomic_compare_exchange_n(ptr, &old_value, new_value, false,
+                              __ATOMIC_RELAXED, __ATOMIC_RELAXED);
   return old_value;
 }
 
 static SB_C_FORCE_INLINE SbAtomic64
+SbAtomicNoBarrier_Exchange64(volatile SbAtomic64* ptr, SbAtomic64 new_value) {
+  return __atomic_exchange_n(ptr, new_value, __ATOMIC_RELAXED);
+}
+
+static SB_C_FORCE_INLINE SbAtomic64
 SbAtomicNoBarrier_Increment64(volatile SbAtomic64* ptr, SbAtomic64 increment) {
-  return SbAtomicBarrier_Increment64(ptr, increment);
+  return increment + __atomic_fetch_add(ptr, increment, __ATOMIC_RELAXED);
 }
 
 static SB_C_FORCE_INLINE SbAtomic64
 SbAtomicBarrier_Increment64(volatile SbAtomic64* ptr, SbAtomic64 increment) {
-  for (;;) {
-    // Atomic exchange the old value with an incremented one.
-    SbAtomic64 old_value = *ptr;
-    SbAtomic64 new_value = old_value + increment;
-    if (__sync_bool_compare_and_swap(ptr, old_value, new_value)) {
-      // The exchange took place as expected.
-      return new_value;
-    }
-    // Otherwise, *ptr changed mid-loop and we need to retry.
-  }
+  return increment + __atomic_fetch_add(ptr, increment, __ATOMIC_SEQ_CST);
 }
 
 static SB_C_FORCE_INLINE SbAtomic64
 SbAtomicAcquire_CompareAndSwap64(volatile SbAtomic64* ptr,
                                  SbAtomic64 old_value,
                                  SbAtomic64 new_value) {
-  return SbAtomicNoBarrier_CompareAndSwap64(ptr, old_value, new_value);
+  __atomic_compare_exchange_n(ptr, &old_value, new_value, false,
+                              __ATOMIC_ACQUIRE, __ATOMIC_ACQUIRE);
+  return old_value;
 }
 
 static SB_C_FORCE_INLINE SbAtomic64
 SbAtomicRelease_CompareAndSwap64(volatile SbAtomic64* ptr,
                                  SbAtomic64 old_value,
                                  SbAtomic64 new_value) {
-  return SbAtomicNoBarrier_CompareAndSwap64(ptr, old_value, new_value);
+  __atomic_compare_exchange_n(ptr, &old_value, new_value, false,
+                              __ATOMIC_RELEASE, __ATOMIC_RELAXED);
+  return old_value;
 }
 
 static SB_C_FORCE_INLINE void
 SbAtomicNoBarrier_Store64(volatile SbAtomic64* ptr,
                           SbAtomic64 value) {
-  *ptr = value;
+  __atomic_store_n(ptr, value, __ATOMIC_RELAXED);
 }
 
 static SB_C_FORCE_INLINE void SbAtomicAcquire_Store64(volatile SbAtomic64* ptr,
                                                       SbAtomic64 value) {
-  *ptr = value;
+  __atomic_store_n(ptr, value, __ATOMIC_RELAXED);
   SbAtomicMemoryBarrier();
 }
 
 static SB_C_FORCE_INLINE void SbAtomicRelease_Store64(volatile SbAtomic64* ptr,
                                                       SbAtomic64 value) {
-  SbAtomicMemoryBarrier();
-  *ptr = value;
+  __atomic_store_n(ptr, value, __ATOMIC_RELEASE);
 }
 
 static SB_C_FORCE_INLINE SbAtomic64
 SbAtomicNoBarrier_Load64(volatile const SbAtomic64* ptr) {
-  return *ptr;
+  return __atomic_load_n(ptr, __ATOMIC_RELAXED);
 }
 
 static SB_C_FORCE_INLINE SbAtomic64
 SbAtomicAcquire_Load64(volatile const SbAtomic64* ptr) {
-  SbAtomic64 value = *ptr;
-  SbAtomicMemoryBarrier();
-  return value;
+  return __atomic_load_n(ptr, __ATOMIC_ACQUIRE);
 }
 
 static SB_C_FORCE_INLINE SbAtomic64
 SbAtomicRelease_Load64(volatile const SbAtomic64* ptr) {
   SbAtomicMemoryBarrier();
-  return *ptr;
+  return __atomic_load_n(ptr, __ATOMIC_RELAXED);
 }
 #endif  // SB_HAS(64_BIT_ATOMICS)
 
