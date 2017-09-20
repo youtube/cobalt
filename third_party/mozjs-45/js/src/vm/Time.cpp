c637f49819a02325251018f56c8ade1c8eaa51ba
Cobalt Team <cobalt-team@google.com>
2017-09-20 16:29:52 -0700

    Enable body of PRMJ_FormatTime
    The body of this function was missing a "defined(STARBOARD)", which
    caused it to always return failure, which caused toLocalTimeString to
    always fall back to toString.  Add the condition in so it actually
    works.
    
    b/66215468
    
    Change-Id: I15504f469c815dbbb84b2aca5195fef80e59a431
    (cherry picked from commit 3a541033114d700f02b3e2e1579d636a55ccba01)

--- third_party/mozjs-45/js/src/vm/Time.cpp
+++ third_party/mozjs-45/js/src/vm/Time.cpp
@@ -279,7 +279,8 @@ size_t
 PRMJ_FormatTime(char* buf, int buflen, const char* fmt, PRMJTime* prtm)
 {
     size_t result = 0;
-#if defined(XP_UNIX) || defined(XP_WIN)
+    // Use this for Starboard until Internationalization API is enabled.
+#if defined(XP_UNIX) || defined(XP_WIN) || defined(STARBOARD)
     struct tm a;
     int fake_tm_year = 0;
 #ifdef XP_WIN
