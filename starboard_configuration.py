6ce03bc6d07e12dd0c3203e29e593f92cd628c33
Cobalt Team <cobalt-team@google.com>
2017-09-29 15:53:53 -0700

    Adding master lists of test targets
    Applications that use Starboard's new unit test runner should list
    the names of all of the test binaries they would like it to run
    in their "starboard_configuration.py" file.  This change defines
    that list for Cobalt.
    
    The new unit test runner is located at
    src/starboard/tools/testing/test_runner.py
    
    Change-Id: I607e6da5c0694ee33c9a7f44c77c85fa843dbd8f

--- starboard_configuration.py
+++ starboard_configuration.py
@@ -20,3 +20,40 @@ PORT_ROOTS = [
     ["starboard"],
     ["third_party", "starboard"]
 ]
+
+#  Names of all the unit test binaries that can be run by
+#  the Starboard unit test runner.
+TEST_TARGETS = [
+    'audio_test',
+    'base_test',
+    'base_unittests',
+    'bindings_test',
+    'crypto_unittests',
+    'csp_test',
+    'css_parser_test',
+    'cssom_test',
+    'dom_parser_test',
+    'dom_test',
+    'layout_test',
+    'layout_tests',
+    'loader_test',
+    'math_test',
+    'media_session_test',
+    'nb_test',
+    'nplb',
+    'nplb_blitter_pixel_tests',
+    'net_unittests',
+    'network_test',
+    'page_visibility_test',
+    'poem_unittests',
+    'render_tree_test',
+    'renderer_test',
+    'sql_unittests',
+    'starboard_platform_tests',
+    'storage_test',
+    'trace_event_test',
+    'web_animations_test',
+    'web_platform_tests',
+    'webdriver_test',
+    'xhr_test',
+]
