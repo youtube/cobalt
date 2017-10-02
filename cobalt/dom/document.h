ab58e6b7d7aefbac822c8bfb99b51406c2c2fa37
Cobalt Team <cobalt-team@google.com>
2017-10-02 15:38:56 -0700

    Enable document.cookie in production builds
    Adds SOP protection for document.cookie.
    
    b/66455552
    
    Change-Id: I4f8c193b73c0a4ac21739c6b1b943adbc7d9642b

--- cobalt/dom/document.h
+++ cobalt/dom/document.h
@@ -218,6 +218,11 @@ class Document : public Node,
   }
 
   // https://www.w3.org/TR/html5/dom.html#dom-document-cookie
+  void set_cookie(const std::string& cookie,
+                  script::ExceptionState* exception_state);
+  std::string cookie(script::ExceptionState* exception_state) const;
+
+  // For Cobalt code use only. Logs warnings instead of raising exceptions.
   void set_cookie(const std::string& cookie);
   std::string cookie() const;
 
@@ -265,7 +270,7 @@ class Document : public Node,
   // Returns whether the document has browsing context. Having the browsing
   // context means the document is shown on the screen.
   //   https://www.w3.org/TR/html5/browsers.html#browsing-context
-  bool HasBrowsingContext() { return !!window_; }
+  bool HasBrowsingContext() const { return !!window_; }
 
   void set_window(Window* window) { window_ = window; }
   const scoped_refptr<Window> window();
@@ -430,6 +435,8 @@ class Document : public Node,
   // Compiles/updates a set of all declared CSS keyframes used to define CSS
   // Animations, using all the style sheets in the document.
   void UpdateKeyframes();
+
+  bool IsCookieAverseDocument() const;
 
   // Reference to HTML element context.
   HTMLElementContext* const html_element_context_;
