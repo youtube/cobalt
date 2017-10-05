1bd523f942955837ba3d5a7a4a73b5b65b3db97d
Cobalt Team <cobalt-team@google.com>
2017-10-05 13:45:38 -0700

    Revert "Updates gyp_configuration.py files for new test runner."
    Try builds passed, but x64directfb-future is now broken.
    
    This reverts commit 66383188cc597a5dd6b26535d0a6553184add80d.
    
    Change-Id: I88f0e951b4575e06ffde7b8f3430c3552964ba5e

--- starboard/linux/x64directfb/future/gyp_configuration.py
+++ starboard/linux/x64directfb/future/gyp_configuration.py
@@ -16,7 +16,7 @@
 import logging
 
 # Import the shared Linux platform configuration.
-import starboard.linux.x64directfb.gyp_configuration as gyp_configuration
+from starboard.linux.shared import gyp_configuration
 
 
 def CreatePlatformConfig():
