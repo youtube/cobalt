eb8fd4153c9fd1ae9161877d45f4005e4fc7c463
Cobalt Team <cobalt-team@google.com>
2017-10-10 16:03:15 -0700

    Bring back local storage partitioning by initial URL
    The logic now uses the initial URL as the partition key. However, it is
    overridable by using the command line argument:
    "local_storage_partition_url."
    
    Change-Id: I55224af66414b69d5b250ef305cdd125ec08d05c

--- cobalt/browser/application.cc
+++ cobalt/browser/application.cc
@@ -501,18 +501,22 @@ Application::Application(const base::Closure& quit_closure, bool should_preload)
   }
 #endif
 
+  base::optional<std::string> partition_key;
   if (command_line->HasSwitch(browser::switches::kLocalStoragePartitionUrl)) {
     std::string local_storage_partition_url = command_line->GetSwitchValueASCII(
         browser::switches::kLocalStoragePartitionUrl);
-    base::optional<std::string> partition_key =
-        base::GetApplicationKey(GURL(local_storage_partition_url));
+    partition_key = base::GetApplicationKey(GURL(local_storage_partition_url));
     CHECK(partition_key) << "local_storage_partition_url is not a valid URL.";
+  } else {
+    partition_key = base::GetApplicationKey(initial_url);
+  }
+  options.storage_manager_options.savegame_options.id = partition_key;
 
-    base::optional<std::string> default_partition_key =
-        base::GetApplicationKey(GURL(kDefaultURL));
-    if (*partition_key != *default_partition_key) {
-      options.storage_manager_options.savegame_options.id = *partition_key;
-    }
+  base::optional<std::string> default_key =
+      base::GetApplicationKey(GURL(kDefaultURL));
+  if (partition_key == default_key) {
+    options.storage_manager_options.savegame_options.fallback_to_default_id =
+        true;
   }
 
   // User can specify an extra search path entry for files loaded via file://.
