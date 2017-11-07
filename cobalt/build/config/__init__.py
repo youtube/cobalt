bbd42a02a8572a6d3cf5af8c19a2c3121e634708
Cobalt Team <cobalt-team@google.com>
2017-11-07 11:40:20 -0800

    Move GYP infrastructure into Starboard
    This change preserves compatibility with existing workflows, but moves
    the non-Cobalt-specific build logic into Starboard.
    
    b/26496916
    
    Change-Id: Id27d8ef05f010e05310c4bfe10ae2846863bd457

--- cobalt/build/config/__init__.py
+++ cobalt/build/config/__init__.py
@@ -12,16 +12,24 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Initialization for the config package."""
+"""Initialization for the config package.
+
+Note that this is an obsolete and deprecated package, and just exists for
+backwards compatibility. Please use starboard.build.PlatformConfiguration or
+cobalt.build.CobaltConfiguration instead, as appropriate.
+"""
 
 import sys
 
 import _env  # pylint: disable=unused-import
 import base
-from base import GetPlatformConfig
 import starboard
+from starboard.tools.build import GetPlatformConfig
 
 # This bit of voodoo fakes out the config.starboard module so that legacy
 # platforms will still GYP properly.
 sys.modules['config.starboard'] = starboard
 starboard.PlatformConfigStarboard = base.PlatformConfigBase
+
+# TODO: Add deprecation logging.warning() here once references are mostly
+# cleaned up.
