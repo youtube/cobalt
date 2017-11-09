c42e76f67af4a5bbe10df0f6a02807f8dc67a9ee
Cobalt Team <cobalt-team@google.com>
2017-11-08 21:36:46 -0800

    Clean up cobalt/script/* cosmetics
    * Standardize license format (always use // instead of /**/).
    
    * Always use a newline in between license and opening header guard.
    
    * Always use a newline in between final closing namespace block and
     #endif header guard.
    
    * Include headers in gyp target.
    
    Change-Id: If30eacbd06ffc4da313b5d81ef3fc4025a099d6b

--- cobalt/script/logging_exception_state.h
+++ cobalt/script/logging_exception_state.h
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #ifndef COBALT_SCRIPT_LOGGING_EXCEPTION_STATE_H_
 #define COBALT_SCRIPT_LOGGING_EXCEPTION_STATE_H_
 
