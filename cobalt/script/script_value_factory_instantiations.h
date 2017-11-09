2db9cd909b759e14252519cd4a1efc56ae9aa434
Cobalt Team <cobalt-team@google.com>
2017-11-08 21:36:46 -0800

    Clean up cobalt/script/* cosmetics
    * Standardize license format (always use // instead of /**/).
    
    * Always use a newline in between license and opening header guard.
    
    * Always use a newline in between final closing namespace block and
     #endif header guard.
    
    * Include headers in gyp target.
    
    Change-Id: If30eacbd06ffc4da313b5d81ef3fc4025a099d6b

--- cobalt/script/script_value_factory_instantiations.h
+++ cobalt/script/script_value_factory_instantiations.h
@@ -1,18 +1,16 @@
-/*
- * Copyright 2017 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef COBALT_SCRIPT_SCRIPT_VALUE_FACTORY_INSTANTIATIONS_H_
 #define COBALT_SCRIPT_SCRIPT_VALUE_FACTORY_INSTANTIATIONS_H_
@@ -50,4 +48,5 @@ PROMISE_TEMPLATE_INSTANTIATION(scoped_refptr<Wrappable>);
 
 }  // namespace script
 }  // namespace cobalt
+
 #endif  // COBALT_SCRIPT_SCRIPT_VALUE_FACTORY_INSTANTIATIONS_H_
