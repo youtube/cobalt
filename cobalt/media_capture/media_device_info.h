a5d668dd59da3820720f17fb8ea6156c02cbaf89
Cobalt Team <cobalt-team@google.com>
2017-11-15 19:13:00 +0000

    Adds idl for microphone support.
    Change-Id: I1c8c50d844b58d70cee720ec4aba4494f4bd8179

---
+++ cobalt/media_capture/media_device_info.h
@@ -0,0 +1,50 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef COBALT_MEDIA_CAPTURE_MEDIA_DEVICE_INFO_H_
+#define COBALT_MEDIA_CAPTURE_MEDIA_DEVICE_INFO_H_
+
+#include "base/memory/ref_counted.h"
+#include "base/memory/scoped_ptr.h"
+#include "cobalt/media_capture/media_device_kind.h"
+#include "cobalt/script/promise.h"
+#include "cobalt/script/script_value.h"
+#include "cobalt/script/script_value_factory.h"
+#include "cobalt/script/sequence.h"
+#include "cobalt/script/wrappable.h"
+
+namespace cobalt {
+namespace media_capture {
+
+class MediaDeviceInfo : public script::Wrappable {
+ public:
+  MediaDeviceInfo(script::ScriptValueFactory* script_value_factory,
+                  MediaDeviceKind kind);
+
+  MediaDeviceKind kind() { return kind_; }
+  void set_kind(MediaDeviceKind k) { kind_ = k; }
+
+  DEFINE_WRAPPABLE_TYPE(MediaDeviceInfo);
+
+ private:
+  script::ScriptValueFactory* script_value_factory_;
+  MediaDeviceKind kind_;
+
+  DISALLOW_COPY_AND_ASSIGN(MediaDeviceInfo);
+};
+
+}  // namespace media_capture
+}  // namespace cobalt
+
+#endif  // COBALT_MEDIA_CAPTURE_MEDIA_DEVICE_INFO_H_
