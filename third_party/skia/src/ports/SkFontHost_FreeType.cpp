19a0321e9ee6def23f52d1d0ff59eaae54bd03bf
Cobalt Team <cobalt-team@google.com>
2017-11-14 18:35:32 -0800

    Forbid dlopen
    Cobalt doesn't need to use dlopen.
    
    Change-Id: Idea9dcc0583fd2d9b9dd769e09e1f88987439d9d

--- third_party/skia/src/ports/SkFontHost_FreeType.cpp
+++ third_party/skia/src/ports/SkFontHost_FreeType.cpp
@@ -44,7 +44,7 @@
 
 // SK_FREETYPE_MINIMUM_RUNTIME_VERSION 0x<major><minor><patch><flags>
 // Flag SK_FREETYPE_DLOPEN: also try dlopen to get newer features.
-#define SK_FREETYPE_DLOPEN (0x1)
+#define SK_FREETYPE_DLOPEN (0x0)
 #ifndef SK_FREETYPE_MINIMUM_RUNTIME_VERSION
 #  if defined(SK_BUILD_FOR_ANDROID_FRAMEWORK) || defined (GOOGLE3)
 #    define SK_FREETYPE_MINIMUM_RUNTIME_VERSION (((FREETYPE_MAJOR) << 24) | ((FREETYPE_MINOR) << 16) | ((FREETYPE_PATCH) << 8))
