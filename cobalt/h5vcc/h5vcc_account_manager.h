e521be8c12b6b5902b9e3f5470962ea27cbfc306
Cobalt Team <cobalt-team@google.com>
2017-11-16 17:44:48 -0800

    Allow H5vccAccountManager to shutdown while an auth request is pending.
    Use a weak pointer to H5vccAccountManager in the message loops so that
    it won't be called in case it gets destructed before the message is
    processed.
    
    Keep ownership of the ScriptValue reference to the callback in
    H5vccAccountManager rather than precariously passing it to the platform
    user authorizer and back.
    
    Notify the user authorizer when H5vccAccountManager is shutting down so
    that it can unblock any blocking request to avoid a deadlock when
    waiting for the account manager request thread to exit.
    
    b/68761280
    
    Change-Id: I25fc419ba1451e91752792235462f385a2808381

--- cobalt/h5vcc/h5vcc_account_manager.h
+++ cobalt/h5vcc/h5vcc_account_manager.h
@@ -19,6 +19,8 @@
 #include <string>
 
 #include "base/memory/scoped_ptr.h"
+#include "base/memory/scoped_vector.h"
+#include "base/memory/weak_ptr.h"
 #include "base/message_loop.h"
 #include "base/threading/thread.h"
 #include "base/threading/thread_checker.h"
@@ -34,7 +36,8 @@ namespace h5vcc {
 // one-at-time on another thread in FIFO order. When a request is complete, the
 // AccessTokenCallback will be fired on the thread that the H5vccAccountManager
 // was created on.
-class H5vccAccountManager : public script::Wrappable {
+class H5vccAccountManager : public script::Wrappable,
+                            public base::SupportsWeakPtr<H5vccAccountManager> {
  public:
   typedef script::CallbackFunction<bool(const std::string&, uint64_t)>
       AccessTokenCallback;
@@ -59,27 +62,49 @@ class H5vccAccountManager : public script::Wrappable {
 
   ~H5vccAccountManager();
 
-  void RequestOperationInternal(
-      OperationType operation,
-      scoped_ptr<AccessTokenCallbackReference> token_callback);
-  void SendResult(scoped_ptr<AccessTokenCallbackReference> token_callback,
-                        const std::string& token,
-                        uint64_t expiration_in_seconds);
+  // Posts an operation to the account manager thread.
+  void PostOperation(OperationType operation_type,
+                     const AccessTokenCallbackHolder& callback);
+
+  // Processes an operation on the account manager thread. Static because
+  // H5vccAccountManager may have been destructed before this runs.
+  static void RequestOperationInternal(
+      account::UserAuthorizer* user_authorizer, OperationType operation,
+      const base::Callback<void(const std::string&, uint64_t)>& post_result);
+
+  // Posts the result of an operation from the account manager thread back to
+  // the owning thread. Static because H5vccAccountManager may have been
+  // destructed before this runs.
+  static void PostResult(
+      MessageLoop* message_loop,
+      base::WeakPtr<H5vccAccountManager> h5vcc_account_manager,
+      AccessTokenCallbackReference* token_callback,
+      const std::string& token, uint64_t expiration_in_seconds);
+
+  // Sends the result of an operation to the callback on the owning thread.
+  void SendResult(AccessTokenCallbackReference* token_callback,
+                  const std::string& token, uint64_t expiration_in_seconds);
+
+  // The platform-specific user authorizer for getting access tokens.
+  scoped_ptr<account::UserAuthorizer> user_authorizer_;
+
+  // Scoped holder of the callbacks that are currently waiting for a response.
+  ScopedVector<AccessTokenCallbackReference> pending_callbacks_;
 
   // Thread checker for the thread that creates this instance.
   base::ThreadChecker thread_checker_;
-
-  // Each incoming request will have a corresponding task posted to this
-  // thread's message loop and will be handled in a FIFO manner.
-  base::Thread thread_;
 
   // The message loop that the H5vccAccountManager was created on. The public
   // interface must be called from this message loop, and callbacks will be
   // fired on this loop as well.
   MessageLoop* owning_message_loop_;
 
-  // The platform-specific user authorizer for getting access tokens.
-  scoped_ptr<account::UserAuthorizer> user_authorizer_;
+  // Each incoming request will have a corresponding task posted to this
+  // thread's message loop and will be handled in a FIFO manner.
+  // This is last so that all the other fields are valid before the thread gets
+  // constructed, and they remain valid until the thread gets destructed and its
+  // message loop gets flushed.
+  base::Thread thread_;
 
   friend class scoped_refptr<H5vccAccountManager>;
   DISALLOW_COPY_AND_ASSIGN(H5vccAccountManager);
