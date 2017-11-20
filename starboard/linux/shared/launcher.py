50a83e375ec2c63fcfec1818a8be653d641a2223
Cobalt Team <cobalt-team@google.com>
2017-11-20 11:36:15 -0800

    Update Linux launcher to ensure termination
    Currently, webdriver benchmark tests are hanging on Linux buildbots,
    and in some cases I've noticed segfaults directly after the tests
    try to kill Cobalt via SIGTERM.  This change updates the Linux launcher
    to kill processes with SIGKILL.
    
    b/69365420
    
    Change-Id: I5f14ef4d5f5eca5c0a04f166faf471c3740ac4f1

--- starboard/linux/shared/launcher.py
+++ starboard/linux/shared/launcher.py
@@ -19,6 +19,7 @@ import signal
 import socket
 import subprocess
 import sys
+import traceback
 
 import _env  # pylint: disable=unused-import
 from starboard.tools import abstract_launcher
@@ -52,7 +53,8 @@ class Launcher(abstract_launcher.AbstractLauncher):
         [self.executable] + self.target_command_line_params,
         stdout=self.output_file,
         stderr=self.output_file,
-        env=self.full_env)
+        env=self.full_env,
+        close_fds=True)
     self._CloseOutputFile()
     self.pid = self.proc.pid
     self.proc.wait()
@@ -62,9 +64,12 @@ class Launcher(abstract_launcher.AbstractLauncher):
     sys.stderr.write("\n***Killing Launcher***\n")
     if self.pid:
       try:
-        os.kill(self.pid, signal.SIGTERM)
+        self.proc.kill()
       except OSError:
-        sys.stderr.write("Cannot kill launcher.  Process already closed.\n")
+        sys.stderr.write("Error killing launcher with SIGKILL:\n")
+        traceback.print_exc(file=sys.stderr)
+    else:
+      sys.stderr.write("Kill() called before Run(), cannot kill.\n")
 
   def SupportsSuspendResume(self):
     return True
