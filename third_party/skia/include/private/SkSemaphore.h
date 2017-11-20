e98b3bb18fd3347405f19b08bd596a2e1bab33c5
Cobalt Team <cobalt-team@google.com>
2017-11-20 15:46:35 -0800

    Increase portability of code
    Some SDKs might #define __restrict to restrict.  This leads to errors
    while building Skia.  This CL renames restrict to restricted to get
    around these changes.  Note that these functions are removed in upstream
    anyway.
    
    This also includes some minor fixes that helps building the code on
    other platforms.
    
    Change-Id: Ied5160d923f64c88d534564e9931271035f42331

--- third_party/skia/include/private/SkSemaphore.h
+++ third_party/skia/include/private/SkSemaphore.h
@@ -14,7 +14,7 @@
 
 class SkBaseSemaphore {
 public:
-    constexpr SkBaseSemaphore(int count = 0)
+    SkBaseSemaphore(int count = 0)
         : fCount(count), fOSSemaphore(nullptr) {}
 
     // Increment the counter n times.
