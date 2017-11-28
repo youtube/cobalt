fbf6a3b24ef4892e226017f3a39c5df9b87f4941
Cobalt Team <cobalt-team@google.com>
2017-11-28 14:08:34 -0800

    Support --version and --help from the cobalt command
    b/36726167
    
    Change-Id: I99ca46ec33ee130cfd4b0278e53d8a6df15c8055

--- cobalt/browser/main.cc
+++ cobalt/browser/main.cc
@@ -14,15 +14,52 @@
 
 #include "base/callback.h"
 #include "base/logging.h"
+#include "base/stringprintf.h"
 #include "cobalt/base/wrap_main.h"
 #include "cobalt/browser/application.h"
+#include "cobalt/browser/switches.h"
+#include "cobalt/version.h"
 
 namespace {
 
 cobalt::browser::Application* g_application = NULL;
+bool g_is_startup_switch_set = false;
+
+// Get the current version if the switch is set
+bool CheckForAndExecuteVersionSwitch() {
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+          cobalt::browser::switches::kVersion)) {
+    SbLogRaw(StringPrintf("Cobalt version %s\n", COBALT_VERSION).c_str());
+    return true;
+  }
+  return false;
+}
+
+// Get the help message if the switch is set
+bool CheckForAndExecuteHelpSwitch() {
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+          cobalt::browser::switches::kHelp)) {
+    SbLogRaw("Options: \n");
+    SbLogRaw(cobalt::browser::switches::HelpMessage().c_str());
+    return true;
+  }
+  return false;
+}
+
+// If startup switches are set, the Cobalt browser application will not be
+// initiallized and will exit early.
+bool CheckForAndExecuteStartupSwitches() {
+  if (CheckForAndExecuteVersionSwitch()) g_is_startup_switch_set = true;
+  if (CheckForAndExecuteHelpSwitch()) g_is_startup_switch_set = true;
+  return g_is_startup_switch_set;
+}
 
 void PreloadApplication(int /*argc*/, char** /*argv*/, const char* /*link*/,
                         const base::Closure& quit_closure) {
+  if (CheckForAndExecuteStartupSwitches()) {
+    SbSystemRequestStop(0);
+    return;
+  }
   LOG(INFO) << "Preloading application.";
   DCHECK(!g_application);
   g_application =
@@ -32,6 +69,10 @@ void PreloadApplication(int /*argc*/, char** /*argv*/, const char* /*link*/,
 
 void StartApplication(int /*argc*/, char** /*argv*/, const char* /*link*/,
                       const base::Closure& quit_closure) {
+  if (CheckForAndExecuteStartupSwitches()) {
+    SbSystemRequestStop(0);
+    return;
+  }
   LOG(INFO) << "Starting application.";
   if (!g_application) {
     g_application = new cobalt::browser::Application(quit_closure,
@@ -44,7 +85,6 @@ void StartApplication(int /*argc*/, char** /*argv*/, const char* /*link*/,
 
 void StopApplication() {
   LOG(INFO) << "Stopping application.";
-  DCHECK(g_application);
   delete g_application;
   g_application = NULL;
 }
@@ -53,10 +93,9 @@ void HandleStarboardEvent(const SbEvent* starboard_event) {
   DCHECK(starboard_event);
   if (g_application) {
     g_application->HandleStarboardEvent(starboard_event);
-  } else {
+  } else if (!g_is_startup_switch_set) {
     LOG(ERROR) << "Unable to handle starboard event because there is no "
                   "existing application!";
-    NOTREACHED();
   }
 }
 
