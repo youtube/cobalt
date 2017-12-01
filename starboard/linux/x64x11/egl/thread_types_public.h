5c4a84a44b06d9da6f085ac4962457f136513f80
Cobalt Team <cobalt-team@google.com>
2017-11-30 18:28:33 -0800

    Make linux-x64x11 use GLX instead of EGL by default.
    Add a linux-x64x11-egl build so that we can still build
    and run the old EGL build.  The old EGL build may even
    still be necessary to maintain because it is proving
    difficult to run the GLX version on Forge.
    
    b/68991504
    b/68661646
    
    Change-Id: I1cacbbe3737696ec3e04468f3b8e6ad95fc20207

---
+++ starboard/linux/x64x11/egl/thread_types_public.h
@@ -0,0 +1,22 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Includes threading primitive types and initializers.
+
+#ifndef STARBOARD_LINUX_X64X11_EGL_THREAD_TYPES_PUBLIC_H_
+#define STARBOARD_LINUX_X64X11_EGL_THREAD_TYPES_PUBLIC_H_
+
+#include "starboard/linux/x64x11/thread_types_public.h"
+
+#endif  // STARBOARD_LINUX_X64X11_EGL_THREAD_TYPES_PUBLIC_H_
