02c6115c596d2e2118c9b366c05c8ad58583e687
Cobalt Team <cobalt-team@google.com>
2017-12-04 14:04:45 -0800

    Reduce code duplication and complexity around Promise
    This change adds a new type, |PromiseResultUndefined|, to represent
    resolving a Promise with undefined.  This allows the void
    specialization of |Promise| (as well as its implementation in each
    engine, |NativePromise|) to be a simple wrapper over the undefined
    version, which enables us to not have to copy paste code between the
    non-void and void versions.
    
    In the future, once we have our low level JavaScript abstraction layer,
    that exposes arbitrary values, we should use that instead of something
    specific to promises.
    
    Change-Id: I5a523e868c9d9da001974ad2fae65a754bc30cb4

--- cobalt/script/mozjs-45/native_promise.h
+++ cobalt/script/mozjs-45/native_promise.h
@@ -28,13 +28,44 @@ namespace cobalt {
 namespace script {
 namespace mozjs {
 
+// TODO: This lives here instead of conversion_helpers.h because right now
+// |PromiseResultUndefined| is specific to promises.  In the long run, we plan
+// on abstracting all JavaScript value types, and should just use however that
+// abstraction exposes "undefined" here instead.
+inline void ToJSValue(JSContext* context,
+                      const PromiseResultUndefined& in_undefined,
+                      JS::MutableHandleValue out_value) {
+  out_value.setUndefined();
+}
+
 // Shared functionality for NativePromise<T>. Does not implement the Resolve
 // function, since that needs to be specialized for Promise<T>.
 template <typename T>
-class NativePromiseBase : public Promise<T> {
+class NativePromise : public Promise<T> {
  public:
   // ScriptObject boilerplate.
   typedef Promise<T> BaseType;
+
+  // Handle special case T=void, by swapping the input parameter |T| for
+  // |PromiseResultUndefined|. Combined with how |Promise| handles this
+  // special case, we're left with something like:
+  //
+  //   NativePromise<T>    ->            Promise<T>
+  //                                         ^
+  //                                         | (T=PromiseResultUndefined)
+  //                                        /
+  //   NativePromise<void> -> Promise<void>
+  //
+  using ResolveType =
+      typename std::conditional<std::is_same<T, void>::value,
+                                PromiseResultUndefined, T>::type;
+
+  NativePromise(JSContext* context, JS::HandleValue resolver_value)
+      : context_(context) {
+    DCHECK(resolver_value.isObject());
+    promise_resolver_.emplace(context, resolver_value);
+  }
+
   JSObject* handle() const { return promise_resolver_->get().GetObject(); }
   const JS::Value& value() const { return promise_resolver_->get().GetValue(); }
   bool WasCollected() const { return promise_resolver_->get().WasCollected(); }
@@ -42,110 +73,82 @@ class NativePromiseBase : public Promise<T> {
   // The Promise JS object (not the resolver).
   JSObject* promise() const { return promise_resolver_->GetPromise(); }
 
+  void Resolve(const ResolveType& value) const override {
+    JS::RootedObject promise_wrapper(context_,
+                                     promise_resolver_->get().GetObject());
+    if (!promise_wrapper) {
+      return;
+    }
+    JSAutoRequest auto_request(context_);
+    JSAutoCompartment auto_compartment(context_, promise_wrapper);
+    JS::RootedValue converted_value(context_);
+    ToJSValue(context_, value, &converted_value);
+    promise_resolver_->Resolve(converted_value);
+  }
+
   void Reject() const override {
     JS::RootedObject promise_resolver(context_,
                                       promise_resolver_->get().GetObject());
-    if (promise_resolver) {
-      JSAutoRequest auto_request(context_);
-      JSAutoCompartment auto_compartment(context_, promise_resolver);
-      promise_resolver_->Reject(JS::UndefinedHandleValue);
+    if (!promise_resolver) {
+      return;
     }
+    JSAutoRequest auto_request(context_);
+    JSAutoCompartment auto_compartment(context_, promise_resolver);
+    promise_resolver_->Reject(JS::UndefinedHandleValue);
   }
 
   void Reject(SimpleExceptionType exception) const override {
     JS::RootedObject promise_resolver(context_,
                                       promise_resolver_->get().GetObject());
-    if (promise_resolver) {
-      JSAutoRequest auto_request(context_);
-      JSAutoCompartment auto_compartment(context_, promise_resolver);
-      JS::RootedValue error_result(context_);
-      error_result.setObject(
-          *MozjsExceptionState::CreateErrorObject(context_, exception));
-      promise_resolver_->Reject(error_result);
+    if (!promise_resolver) {
+      return;
     }
+    JSAutoRequest auto_request(context_);
+    JSAutoCompartment auto_compartment(context_, promise_resolver);
+    JS::RootedValue error_result(context_);
+    error_result.setObject(
+        *MozjsExceptionState::CreateErrorObject(context_, exception));
+    promise_resolver_->Reject(error_result);
   }
 
   void Reject(const scoped_refptr<ScriptException>& result) const override {
     JS::RootedObject promise_resolver(context_,
                                       promise_resolver_->get().GetObject());
-    if (promise_resolver) {
-      JSAutoRequest auto_request(context_);
-      JSAutoCompartment auto_compartment(context_, promise_resolver);
-      JS::RootedValue converted_result(context_);
-      ToJSValue(context_, result, &converted_result);
-      promise_resolver_->Reject(converted_result);
+    if (!promise_resolver) {
+      return;
     }
+    JSAutoRequest auto_request(context_);
+    JSAutoCompartment auto_compartment(context_, promise_resolver);
+    JS::RootedValue converted_result(context_);
+    ToJSValue(context_, result, &converted_result);
+    promise_resolver_->Reject(converted_result);
   }
 
- protected:
-  NativePromiseBase(JSContext* context, JS::HandleValue resolver_value)
-      : context_(context) {
-    DCHECK(resolver_value.isObject());
-    promise_resolver_.emplace(context, resolver_value);
-  }
-
+ private:
   JSContext* context_;
   base::optional<PromiseWrapper> promise_resolver_;
 };
 
-// Implements the Resolve() function for T != void.
 template <typename T>
-class NativePromise : public NativePromiseBase<T> {
- public:
-  NativePromise(JSContext* context, JS::HandleValue resolver_value)
-      : NativePromiseBase<T>(context, resolver_value) {}
-
-  void Resolve(const T& value) const override {
-    JS::RootedObject promise_wrapper(
-        this->context_, this->promise_resolver_->get().GetObject());
-    if (promise_wrapper) {
-      JSAutoRequest auto_request(this->context_);
-      JSAutoCompartment auto_compartment(this->context_, promise_wrapper);
-      JS::RootedValue converted_value(this->context_);
-      ToJSValue(this->context_, value, &converted_value);
-      this->promise_resolver_->Resolve(converted_value);
-    }
-  }
-};
-
-// Implements the Resolve() function for T == void.
-template <>
-class NativePromise<void> : public NativePromiseBase<void> {
- public:
-  NativePromise(JSContext* context, JS::HandleValue resolver_value)
-      : NativePromiseBase<void>(context, resolver_value) {}
-
-  void Resolve() const override {
-    JS::RootedObject promise_wrapper(context_,
-                                     promise_resolver_->get().GetObject());
-    if (promise_wrapper) {
-      JSAutoRequest auto_request(context_);
-      JSAutoCompartment auto_compartment(context_, promise_wrapper);
-      promise_resolver_->Resolve(JS::UndefinedHandleValue);
-    }
-  }
-};
-
-template <typename T>
-struct TypeTraits<NativePromise<T> > {
-  typedef MozjsUserObjectHolder<NativePromise<T> > ConversionType;
-  typedef const ScriptValue<Promise<T> >* ReturnType;
+struct TypeTraits<NativePromise<T>> {
+  typedef MozjsUserObjectHolder<NativePromise<T>> ConversionType;
+  typedef const ScriptValue<Promise<T>>* ReturnType;
 };
 
 // Promise<T> -> JSValue
 // Note that JSValue -> Promise<T> is not yet supported.
 template <typename T>
 inline void ToJSValue(JSContext* context,
-                      const ScriptValue<Promise<T> >* promise_holder,
+                      const ScriptValue<Promise<T>>* promise_holder,
                       JS::MutableHandleValue out_value) {
   TRACK_MEMORY_SCOPE("Javascript");
   if (!promise_holder) {
     out_value.set(JS::NullValue());
     return;
   }
-  const MozjsUserObjectHolder<NativePromise<T> >* user_object_holder =
+  const MozjsUserObjectHolder<NativePromise<T>>* user_object_holder =
       base::polymorphic_downcast<
-          const MozjsUserObjectHolder<NativePromise<T> >*>(promise_holder);
+          const MozjsUserObjectHolder<NativePromise<T>>*>(promise_holder);
 
   const NativePromise<T>* native_promise =
       base::polymorphic_downcast<const NativePromise<T>*>(
@@ -160,11 +163,10 @@ inline void ToJSValue(JSContext* context,
 // |promise_holder| is not const.
 template <typename T>
 inline void ToJSValue(JSContext* context,
-                      ScriptValue<Promise<T> >* promise_holder,
+                      ScriptValue<Promise<T>>* promise_holder,
                       JS::MutableHandleValue out_value) {
   TRACK_MEMORY_SCOPE("Javascript");
-  ToJSValue(context,
-            const_cast<const ScriptValue<Promise<T> >*>(promise_holder),
+  ToJSValue(context, const_cast<const ScriptValue<Promise<T>>*>(promise_holder),
             out_value);
 }
 
@@ -173,7 +175,7 @@ inline void ToJSValue(JSContext* context,
 // to a promise, typically when a promise is resolved or rejected synchronously.
 template <typename T>
 inline void ToJSValue(JSContext* context,
-                      scoped_ptr<ScriptValue<Promise<T> > > promise_holder,
+                      scoped_ptr<ScriptValue<Promise<T>>> promise_holder,
                       JS::MutableHandleValue out_value) {
   ToJSValue(context, promise_holder.get(), out_value);
 }
