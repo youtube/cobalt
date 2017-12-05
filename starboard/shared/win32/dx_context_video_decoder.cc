0468c6613aaa74ac90b45fbc8b8e9e2d585ba67d
Toan Pham <toanpham@google.com>
2017-12-05 22:30:18 +0000

    Revert "Fixes SbPlayerTest tests in nplb"
    This reverts commit 6c7d9c224425613030d543b3dd1854e27d7abdd3.
    The commit did not fix the test and also introduces a code path
    that is not normally used.
    
    Change-Id: I11f4e758604bd2b8581246d971b3e4632238b641

--- starboard/shared/win32/dx_context_video_decoder.cc
+++ starboard/shared/win32/dx_context_video_decoder.cc
@@ -22,84 +22,47 @@
 #include "third_party/angle/include/EGL/eglext.h"
 
 #include "starboard/log.h"
-#include "starboard/shared/win32/error_utils.h"
 
 namespace starboard {
 namespace shared {
 namespace win32 {
 
-namespace {
-
-using Microsoft::WRL::ComPtr;
-
-PFNEGLQUERYDISPLAYATTRIBEXTPROC GetQueryDisplayFunction() {
+HardwareDecoderContext GetDirectXForHardwareDecoding() {
+  HRESULT result = S_OK;
+  EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
   PFNEGLQUERYDISPLAYATTRIBEXTPROC query_display;
+
   query_display = reinterpret_cast<PFNEGLQUERYDISPLAYATTRIBEXTPROC>(
       eglGetProcAddress("eglQueryDisplayAttribEXT"));
   SB_DCHECK(query_display != nullptr);
-  return query_display;
-}
 
-PFNEGLQUERYDEVICEATTRIBEXTPROC GetQueryDeviceFunction() {
   PFNEGLQUERYDEVICEATTRIBEXTPROC query_device;
   query_device = reinterpret_cast<PFNEGLQUERYDEVICEATTRIBEXTPROC>(
       eglGetProcAddress("eglQueryDeviceAttribEXT"));
-  SB_DCHECK(query_device != nullptr);
-  return query_device;
-}
-
-ComPtr<ID3D11Device> GetSharedDxForHardwareDecoding(intptr_t egl_device) {
-  PFNEGLQUERYDEVICEATTRIBEXTPROC query_device = GetQueryDeviceFunction();
-  intptr_t device = 0;
-  query_device(reinterpret_cast<EGLDeviceEXT>(egl_device),
-               EGL_D3D11_DEVICE_ANGLE, &device);
-
-  ID3D11Device* output_dx_device = reinterpret_cast<ID3D11Device*>(device);
-  return output_dx_device;
-}
-
-ComPtr<ID3D11Device> CreateNewDxForHardwareDecoding() {
-  HRESULT hr;
-  Microsoft::WRL::ComPtr<ID3D11Device> d3d_device;
-  hr = D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0,
-                         nullptr, 0, D3D11_SDK_VERSION,
-                         d3d_device.GetAddressOf(), nullptr, nullptr);
-
-  return d3d_device;
-}
-}  // namespace.
-
-HardwareDecoderContext GetDirectXForHardwareDecoding() {
-  EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-
-  PFNEGLQUERYDISPLAYATTRIBEXTPROC query_display = GetQueryDisplayFunction();
+  SB_DCHECK(query_display != nullptr);
 
   intptr_t egl_device = 0;
   query_display(display, EGL_DEVICE_EXT, &egl_device);
+  SB_DCHECK(egl_device != 0);
 
-  ComPtr<ID3D11Device> d3d_device;
-  if (egl_device != 0) {
-    // Expected for production.
-    d3d_device = GetSharedDxForHardwareDecoding(egl_device);
-  } else {
-    // Expected for nplb testing.
-    d3d_device = CreateNewDxForHardwareDecoding();
-  }
+  intptr_t device;
+  query_device(reinterpret_cast<EGLDeviceEXT>(egl_device),
+      EGL_D3D11_DEVICE_ANGLE, &device);
 
-  // Generate DXGI device manager from DirectX.
-  UINT reset_token = 0;
-  ComPtr<IMFDXGIDeviceManager> device_manager;
-  if (d3d_device.Get()) {
-    HRESULT hr = MFCreateDXGIDeviceManager(&reset_token,
-                                           device_manager.GetAddressOf());
-    CheckResult(hr);
-    hr = device_manager->ResetDevice(d3d_device.Get(), reset_token);
-    CheckResult(hr);
-  }
+  ID3D11Device* output_dx_device = reinterpret_cast<ID3D11Device*>(device);
+  IMFDXGIDeviceManager* dxgi_device_mgr = nullptr;
+
+  UINT token = 0;
+  result = MFCreateDXGIDeviceManager(&token, &dxgi_device_mgr);
+  SB_DCHECK(result == S_OK);
+  SB_DCHECK(dxgi_device_mgr);
+
+  result = dxgi_device_mgr->ResetDevice(output_dx_device, token);
+  SB_DCHECK(SUCCEEDED(result));
 
   HardwareDecoderContext output = {
-    d3d_device,
-    device_manager
+    output_dx_device,
+    dxgi_device_mgr
   };
   return output;
 }
