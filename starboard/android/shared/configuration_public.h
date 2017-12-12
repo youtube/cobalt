ad438099b5ebda805625bf6acb514194230c013a
Cobalt Team <cobalt-team@google.com>
2017-12-12 15:14:14 -0800

    Android: Starboard level to EXPERIMENTAL
    In advance of implementing the new closed captions API, bring
    Android's starboard level up to EXPERIMENTAL
    
    Change-Id: I7df1f0f35ea0cb08a658b8d80a3bb5ec25b2bfea

--- starboard/android/shared/configuration_public.h
+++ starboard/android/shared/configuration_public.h
@@ -24,7 +24,7 @@
 #define STARBOARD_ANDROID_SHARED_CONFIGURATION_PUBLIC_H_
 
 // The API version implemented by this platform.
-#define SB_API_VERSION 5
+#define SB_API_VERSION SB_EXPERIMENTAL_API_VERSION
 
 // --- Architecture Configuration --------------------------------------------
 
