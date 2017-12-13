7d6d87f2166979c73ba08e38a4fec9301d4cd284
Cobalt Team <cobalt-team@google.com>
2017-12-12 21:09:16 -0800

    Introduce a new socket error code.
    The new error code can be used to help higher level
    code make decisions on whether to retry a socket
    transaction or not.
    
    The new enum value is designed to correspond to
    ERR_CONNECTION_RESET in Chromium net's network error list:
    https://cs.chromium.org/chromium/src/net/base/net_error_list.h
    
    I considered adding other errors that Chromium Net code
    will act on, but this was the only one that I was able
    to verify in tests.
    
    b/70510099
    
    Change-Id: I51e055ebd9687e6dfdbf37f07ee529a371a0e582

--- starboard/socket.h
+++ starboard/socket.h
@@ -64,10 +64,14 @@ typedef enum SbSocketError {
   // clever and wait on it with a SbSocketWaiter.
   kSbSocketPending,
 
-  // The operation failed for some reason.
-  //
-  // TODO: It's unclear if we actually care about why, so leaving the rest
-  // of this enumeration blank until it becomes clear that we do.
+#if SB_API_VERSION >= SB_ADDITIONAL_SOCKET_CONNECTION_ERRORS_API_VERSION
+  // This socket error is generated when the connection is reset unexpectedly
+  // and the connection is now invalid.
+  // This might happen for example if an read packet has the "TCP RST" bit set.
+  kSbSocketErrorConnectionReset,
+#endif  // SB_API_VERSION >= SB_ADDITIONAL_SOCKET_CONNECTION_ERRORS_API_VERSION
+
+  // The operation failed for some other reason not specified above.
   kSbSocketErrorFailed,
 } SbSocketError;
 
