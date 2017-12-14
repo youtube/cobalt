60a540be83f9b1d594ed9d75dd6a85d848e076a7
Cobalt Team <cobalt-team@google.com>
2017-12-13 17:40:28 -0800

    Documentation Improvements
    Change-Id: I150ae6a066918311bc137b04611d8a4f080e4816

--- starboard/memory.h
+++ starboard/memory.h
@@ -16,13 +16,13 @@
 //
 // Defines functions for memory allocation, alignment, copying, and comparing.
 //
-// #### Porters
+// # Porters
 //
 // All of the "Unchecked" and "Free" functions must be implemented, but they
 // should not be called directly. The Starboard platform wraps them with extra
 // accounting under certain circumstances.
 //
-// #### Porters and Application Developers
+// # Porters and Application Developers
 //
 // Nobody should call the "Checked", "Unchecked" or "Free" functions directly
 // because that evades Starboard's memory tracking. In both port
