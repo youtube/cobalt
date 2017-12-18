52271c091e1d0294ebec3cdde434940964ddb706
Toan Pham <toanpham@google.com>
2017-12-18 15:47:17 -0800

    Fix browser handling of simultaneous system platform errors
    This prevents the browser callback system from overwriting
    the error callback of an active error.
    
    Change-Id: Icaf17b5fcac84532ba641338d33b78749a89a00a

--- cobalt/browser/system_platform_error_handler.cc
+++ cobalt/browser/system_platform_error_handler.cc
@@ -18,36 +18,53 @@
 
 namespace cobalt {
 namespace browser {
-namespace {
-
-void OnSystemPlatformErrorResponse(SbSystemPlatformErrorResponse response,
-                                   void* user_data) {
-  DCHECK(user_data);
-  SystemPlatformErrorHandler* error_handler =
-      static_cast<SystemPlatformErrorHandler*>(user_data);
-  error_handler->HandleSystemPlatformErrorResponse(response);
-}
-
-}  // namespace
 
 void SystemPlatformErrorHandler::RaiseSystemPlatformError(
     const SystemPlatformErrorOptions& options) {
-  current_system_platform_error_callback_ = options.callback;
+  DCHECK(!options.callback.is_null());
 
-  SbSystemPlatformError handle = SbSystemRaisePlatformError(
-      options.error_type, OnSystemPlatformErrorResponse, this);
-  if (!SbSystemPlatformErrorIsValid(handle) &&
-      !current_system_platform_error_callback_.is_null()) {
+  CallbackData* callback_data = new CallbackData{ &mutex_, options.callback };
+
+  SbSystemPlatformError handle = SbSystemRaisePlatformError(options.error_type,
+      &SystemPlatformErrorHandler::HandleSystemPlatformErrorResponse,
+      callback_data);
+  if (!SbSystemPlatformErrorIsValid(handle)) {
     DLOG(WARNING) << "Did not handle error: " << options.error_type;
-    current_system_platform_error_callback_.Reset();
+    delete callback_data;
+    callback_data = nullptr;
+  }
+
+  // In case the response callback is never called, track the callback data
+  // for all active errors. When this object is destroyed, all dangling data
+  // will be released.
+  {
+    starboard::ScopedLock lock(mutex_);
+
+    // Delete any consumed callback data.
+    for (size_t i = 0; i < callback_data_.size();) {
+      if (callback_data_[i]->callback.is_null()) {
+        callback_data_.erase(callback_data_.begin() + i);
+      } else {
+        ++i;
+      }
+    }
+
+    if (callback_data) {
+      callback_data_.emplace_back(callback_data);
+    }
   }
 }
 
+// static
 void SystemPlatformErrorHandler::HandleSystemPlatformErrorResponse(
-    SbSystemPlatformErrorResponse response) {
-  DCHECK(!current_system_platform_error_callback_.is_null());
-  current_system_platform_error_callback_.Run(response);
-  current_system_platform_error_callback_.Reset();
+    SbSystemPlatformErrorResponse response, void* user_data) {
+  CallbackData* callback_data = static_cast<CallbackData*>(user_data);
+  callback_data->callback.Run(response);
+
+  {
+    starboard::ScopedLock lock(*(callback_data->mutex));
+    callback_data->callback.Reset();
+  }
 }
 
 }  // namespace browser
