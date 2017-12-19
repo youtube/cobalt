b2628a0d5856213a4d61f6cc631f9dd1d22f6007
Cobalt Team <cobalt-team@google.com>
2017-12-19 12:22:15 -0800

    CORS Preflight Cache comparison fix
    This in particular fixes "--url=https://mkf.sbo.corp.google.com:8000/tv;--ignor
    e_certificate_errors" on xb1-devel, where the Visual C++ STL verification
    catches the problem.
    
    This appears to cause no production problems because it appears that
    the CORs preflight cache doesn't get used today with ***REMOVED***.
    
    b/70854134
    
    Change-Id: I35117e5417e6f54514f76361b4ff3db814673552

--- cobalt/loader/cors_preflight_cache.h
+++ cobalt/loader/cors_preflight_cache.h
@@ -53,7 +53,7 @@ class CORSPreflightCache : public base::RefCounted<CORSPreflightCache> {
   // Case-insensitive comparator.
   struct CaseInsensitiveCompare {
     bool operator()(const std::string& lhs, const std::string& rhs) const {
-      return SbStringCompareNoCase(lhs.c_str(), rhs.c_str()) != 0;
+      return SbStringCompareNoCase(lhs.c_str(), rhs.c_str()) < 0;
     }
   };
 
