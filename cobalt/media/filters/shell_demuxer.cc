e15fb7016329b910bf2ae6ead120dc1ed5abd481
Xiaoming Shi <xiaomings@google.com>
2018-01-08 15:39:43 +0800

    Fix CPU spinning in ShellDemuxer request loop
    Add delay before calling ShellDemuxer::IssueNextRequest() inside
    ShellDemuxer::Download().
    
    b/70510642
    
    Change-Id: Ia62c55fba9149fe218d4ee605d49aab17d6fe435

--- cobalt/media/filters/shell_demuxer.cc
+++ cobalt/media/filters/shell_demuxer.cc
@@ -452,7 +452,14 @@ void ShellDemuxer::Download(scoped_refptr<DecoderBuffer> buffer) {
   // Notify host of each disjoint range.
   host_->OnBufferedTimeRangesChanged(buffered);
 
-  IssueNextRequest();
+  // Post the task with a delay to make the request loop a bit friendly to
+  // other tasks as otherwise IssueNextRequest(), Request(), AllocateBuffer(),
+  // and Download() can form a tight loop on the |blocking_thread_|.
+  const base::TimeDelta kDelay = base::TimeDelta::FromMilliseconds(5);
+  blocking_thread_.message_loop_proxy()->PostDelayedTask(
+      FROM_HERE,
+      base::Bind(&ShellDemuxer::IssueNextRequest, base::Unretained(this)),
+      kDelay);
 }
 
 void ShellDemuxer::IssueNextRequest() {
