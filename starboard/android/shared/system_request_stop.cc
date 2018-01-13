8c28f016cd0fa45895890bd4858133f8b0af273f
Cobalt Team <cobalt-team@google.com>
2018-01-12 18:57:42 -0800

    Disconnect Starboard app lifetime from the Activity.
    - Always initiate Starboard suspend & stop with the Java methods in
      StarboardBridge, whether it's Java or C++ that requests it.
    
    - Finishing the Activity is now how to suspend rather than stop
      Starboard since the Starboard app can outlive the Activity.
    
    - Stopping Starboard is done by sending a Stop event, and then informing
      StarboardBridge when the Starboard run loop has exited so it can
      shutdown the Activity and then kill the process.
    
    - StarboardBridge shuts down helpers when the run loop exits rather than
      when the Activity is destroyed.
    
    - Move closing the log file and writing the exit code for tests out of
      SbSystemRequestStop into the Starboard thread proc in android_main
      after the run loop has exited.
    
    - No need to cache the exit_error_level since it's just the return value
      from the run loop.
    
    - Remove "noHistory" from the Activity since StarboardBridge is now
      responsible for finishing it as appropriate. It's okay and desired to
      let the Activity remain in the background when Starboard is suspended,
      if that's what the Android system wants to do.
    
    - Remove the native destroy AndroidCommand since that no longer
      initates shutdown of the Starboard app.
    
    - Send a deeplink event from onCreate() if Starboard/Cobalt is still
      running from a previous Activity.
    
    - Only send Android lifecycle commands when the Starboard application is
      actually running to avoid waiting for processing that will never happen,
      and to not try to access the application after it's been destructed.
    
    - Set the activity in the Activity holder to null when the Activity is
      stopped.
    
    - Create the Activity holder in MainActivity so the StarboardBridge and
      UserAuthorizer get the same instance.
    
    b/69682799
    
    Change-Id: I22b9e292a59126dc07a80de8e8d96be353be50bd

--- starboard/android/shared/system_request_stop.cc
+++ starboard/android/shared/system_request_stop.cc
@@ -14,62 +14,11 @@
 
 #include "starboard/system.h"
 
-#include <android/native_activity.h>
-#include <stdio.h>
-#include <string>
-
-#include "starboard/android/shared/application_android.h"
 #include "starboard/android/shared/jni_env_ext.h"
-#include "starboard/android/shared/log_file_impl.h"
-#include "starboard/file.h"
-#include "starboard/shared/starboard/command_line.h"
 
-using starboard::android::shared::ApplicationAndroid;
-using starboard::shared::starboard::CommandLine;
-using starboard::android::shared::CloseLogFile;
 using starboard::android::shared::JniEnvExt;
 
-namespace {
-
-const char kExitFilePathSwitch[] = "android_exit_file";
-
-}
-
 void SbSystemRequestStop(int error_level) {
-  ApplicationAndroid* app = ApplicationAndroid::Get();
-  app->SetExitOnActivityDestroy(error_level);
-
-  const CommandLine* cl = app->GetCommandLine();
-
-  std::string path = cl->GetSwitchValue(kExitFilePathSwitch);
-
-  if (!path.empty()) {
-    // Since we cannot reliably flush the exit code in the log,
-    // we write a file to our app files directory.
-
-    // We are going to make a temporary file here and rename it
-    // to the actual exitcode file.  This is so that tools searching for
-    // the exitcode file don't find it until it has been written to.
-    std::string temp_path = path;
-    temp_path.append(".tmp");
-
-    SbFile exitcode_file = SbFileOpen(
-      temp_path.c_str(),
-      kSbFileCreateAlways | kSbFileRead | kSbFileWrite, NULL, NULL);
-
-    if (exitcode_file != NULL) {
-      std::ostringstream exit_stream;
-      exit_stream << std::dec << error_level;
-
-      SbFileWrite(exitcode_file, exit_stream.str().c_str(),
-                  exit_stream.str().size());
-      SbFileClose(exitcode_file);
-      rename(temp_path.c_str(), path.c_str());
-    }
-  }
-
-  CloseLogFile();
-
   JniEnvExt* env = JniEnvExt::Get();
-  env->CallStarboardVoidMethodOrAbort("requestStop", "()V");
+  env->CallStarboardVoidMethodOrAbort("requestStop", "(I)V", error_level);
 }
