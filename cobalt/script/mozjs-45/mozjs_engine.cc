da23dc0f99f3faa39d4a99a194348e8290041814
Cobalt Team <cobalt-team@google.com>
2018-01-25 21:00:58 -0800

    Remove now unused ReportJSError from MozjsEngine
    This function is now no longer used anywhere, is dead code, and should
    be removed.
    
    Change-Id: I62844024e3e2e12ed036c5bbf8c95068e9341d20

--- cobalt/script/mozjs-45/mozjs_engine.cc
+++ cobalt/script/mozjs-45/mozjs_engine.cc
@@ -278,33 +278,6 @@ void MozjsEngine::FinalizeCallback(JSFreeOp* free_op, JSFinalizeStatus status,
   }
 }
 
-bool MozjsEngine::ReportJSError(JSContext* context, const char* message,
-                                JSErrorReport* report) {
-  const bool is_invalid =
-      error_handler_.is_null() || !report || !report->filename;
-  if (is_invalid) {
-    return true;  // Allow error to propagate in the mozilla engine.
-  }
-  const bool do_report_error =
-      (report->flags == JSREPORT_ERROR) ||
-      (report->flags & JSREPORT_WARNING) ||
-      (report->errorNumber == JSMSG_UNCAUGHT_EXCEPTION);
-  if (do_report_error) {
-    std::string file_name = report->filename;
-    // Line/column can be zero for internal javascript exceptions. In this
-    // case set the values to 1, otherwise the base::SourceLocation object
-    // below will dcheck.
-    int line = std::max<int>(1, report->lineno);
-    int column = std::max<int>(1, report->column);
-    if (file_name.empty()) {
-      file_name = "<internal exception>";
-    }
-    base::SourceLocation source_location(file_name, line, column);
-    error_handler_.Run(source_location, message);
-  }
-  return true;  // Allow error to propagate in the mozilla engine.
-}
-
 }  // namespace mozjs
 
 // static
