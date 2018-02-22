8240e8784d71f04d1e5ee8580747f56ca6af3404
Jelle Foks <jfoks@google.com>
2018-02-22 13:47:14 -0800

    Add and use locally built clang-3.6 toolchain.
    This adds a script to fetch, compile, and install clang version 3.6 into
    ~/cobalt-toolchains
    
    This also changes the clang-3-6 variant to use this toolchain instead of a
    system provided one.
    
    b/72813847
    
    Change-Id: I36fde634934b0ec98af014f99b207dce6d4ca8ec

--- starboard/linux/x64x11/clang/3.6/gyp_configuration.py
+++ starboard/linux/x64x11/clang/3.6/gyp_configuration.py
@@ -13,7 +13,12 @@
 # limitations under the License.
 """Starboard Linux X64 X11 Clang 3.6 platform configuration."""
 
+import logging
+import os
+import subprocess
+
 from starboard.linux.shared import gyp_configuration as shared_configuration
+from starboard.tools import build
 
 
 class PlatformConfig(shared_configuration.LinuxConfiguration):
@@ -21,15 +26,29 @@ class PlatformConfig(shared_configuration.LinuxConfiguration):
 
   def __init__(self, platform, asan_enabled_by_default=True):
     super(PlatformConfig, self).__init__(
-        platform, goma_supports_compiler=False)
+        platform, asan_enabled_by_default, goma_supports_compiler=False)
+
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    # Run the script that ensures clang 3.6 is installed.
+    subprocess.call(
+        os.path.join(script_path, 'download_clang.sh'), cwd=script_path)
+    self.toolchain_dir = os.path.join(build.GetToolchainsDir(),
+                                      'x86_64-linux-gnu-clang-3.6', 'llvm',
+                                      'Release+Asserts')
 
   def GetEnvironmentVariables(self):
-    env_variables = {
-        'CC': 'clang-3.6',
-        'CXX': 'clang++-3.6',
-    }
+    env_variables = super(PlatformConfig, self).GetEnvironmentVariables()
+    toolchain_bin_dir = os.path.join(self.toolchain_dir, 'bin')
+    env_variables.update({
+        'CC': os.path.join(toolchain_bin_dir, 'clang'),
+        'CXX': os.path.join(toolchain_bin_dir, 'clang++'),
+    })
     return env_variables
 
 
 def CreatePlatformConfig():
-  return PlatformConfig('linux-x64x11-clang-3-6')
+  try:
+    return PlatformConfig('linux-x64x11-clang-3-6')
+  except RuntimeError as e:
+    logging.critical(e)
+    return None
