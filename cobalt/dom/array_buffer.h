408464d74ea33a92cb708301d3c23c9bba099f02
Cobalt Team <cobalt-team@google.com>
2018-03-13 11:32:34 -0700

    Remove dom::ArrayBuffer::{Allocator,Cache}
    Part 1 of the dom -> script typed arrays migration, removing the legacy
    array buffer allocator, only ever used on PS3 (which is now gone).
    
    b/67313140
    b/36898816
    b/36807955
    
    Change-Id: I3735cb126d6785ea7b139965d687c3a62592f64e

--- cobalt/dom/array_buffer.h
+++ cobalt/dom/array_buffer.h
@@ -30,66 +30,26 @@ class ArrayBuffer : public script::Wrappable {
   // the heap.
   enum AllocationType { kFromHeap };
 
-  class Cache;
-
-  // Optional Allocator to be used to allocate/free memory for ArrayBuffer.
-  // ArrayBuffer will allocate from the heap if an Allocator is not provided.
-  // This is because ArrayBuffer allocates its memory on the heap by default and
-  // ArrayBuffers may occupy a lot of memory.  It is possible to provide an
-  // allocator on some platforms so ArrayBuffer can possibly use memory that is
-  // not part of the heap.
-  class Allocator {
-   public:
-    virtual ~Allocator() {}
-    virtual void* Allocate(size_t size) = 0;
-    virtual void Free(void* p) = 0;
-  };
-
   // This class manages the internal buffer of an ArrayBuffer.  It deals the
   // fact that the buffer can be allocated from an Allocator or from the heap.
   class Data {
    public:
-    Data(script::EnvironmentSettings* settings, size_t size);
-    Data(script::EnvironmentSettings* settings, const uint8* data, size_t size);
+    explicit Data(size_t size);
+    Data(const uint8* data, size_t size);
     Data(scoped_array<uint8> data, size_t size);
 
     ~Data();
 
     uint8* data() const;
     size_t size() const { return size_; }
-    // Move the ArrayBuffer allocated on the heap to memory allocated using the
-    // provided allocator.  It returns true if such move is successful or if the
-    // ArrayBuffer has already been offloaded.
-    bool Offload();
-    bool offloaded() const { return offloaded_; }
 
    private:
-    void Initialize(script::EnvironmentSettings* settings, size_t size);
+    void Initialize(size_t size);
 
-    Allocator* allocator_;
-    Cache* cache_;
-    // True only if |data_| is allocated by |allocator_|.
-    bool offloaded_;
     uint8* data_;
     size_t size_;
 
     DISALLOW_COPY_AND_ASSIGN(Data);
-  };
-
-  class Cache {
-   public:
-    explicit Cache(size_t maximum_size_in_main_memory);
-
-    void Register(Data* data);
-    void Unregister(Data* data);
-    void ReportUsage(const Data* data);
-
-   private:
-    void TryToOffload();
-
-    size_t total_size_in_main_memory_;
-    size_t maximum_size_in_main_memory_;
-    std::vector<Data*> datas_;
   };
 
   ArrayBuffer(script::EnvironmentSettings* settings, uint32 length);
