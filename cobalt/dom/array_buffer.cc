f8adcfcf8b3b02252d9840996e818e6cdc0429c5
Cobalt Team <cobalt-team@google.com>
2018-03-13 11:32:34 -0700

    Remove dom::ArrayBuffer::{Allocator,Cache}
    Part 1 of the dom -> script typed arrays migration, removing the legacy
    array buffer allocator, only ever used on PS3 (which is now gone).
    
    b/67313140
    b/36898816
    b/36807955
    
    Change-Id: I3735cb126d6785ea7b139965d687c3a62592f64e

--- cobalt/dom/array_buffer.cc
+++ cobalt/dom/array_buffer.cc
@@ -37,146 +37,39 @@ int ClampIndex(int index, int length) {
 
 }  // namespace
 
-ArrayBuffer::Data::Data(script::EnvironmentSettings* settings, size_t size)
-    : allocator_(NULL), cache_(NULL), offloaded_(false), data_(NULL), size_(0) {
-  Initialize(settings, size);
+ArrayBuffer::Data::Data(size_t size) : data_(NULL), size_(0) {
+  Initialize(size);
   if (data_) {
     memset(data_, 0, size);
   }
-  if (cache_) {
-    cache_->Register(this);
-  }
 }
 
-ArrayBuffer::Data::Data(script::EnvironmentSettings* settings,
-                        const uint8* data, size_t size)
-    : allocator_(NULL), cache_(NULL), offloaded_(false), data_(NULL), size_(0) {
-  Initialize(settings, size);
+ArrayBuffer::Data::Data(const uint8* data, size_t size)
+    : data_(NULL), size_(0) {
+  Initialize(size);
   DCHECK(data_);
   memcpy(data_, data, size);
-  // Register() has to be called after copying the data as Register() will call
-  // TryToOffload() which may delete the |data_| passed in that belongs to
-  // another ArrayBuffer.
-  if (cache_) {
-    cache_->Register(this);
-  }
 }
 
 ArrayBuffer::Data::Data(scoped_array<uint8> data, size_t size)
-    : allocator_(NULL),
-      cache_(NULL),
-      offloaded_(false),
-      data_(data.release()),
-      size_(size) {
+    : data_(data.release()), size_(size) {
   DCHECK(data_);
 }
 
-ArrayBuffer::Data::~Data() {
-  if (offloaded_) {
-    allocator_->Free(data_);
-  } else {
-    delete[] data_;
-  }
-  if (cache_) {
-    cache_->Unregister(this);
-  }
-}
+ArrayBuffer::Data::~Data() { delete[] data_; }
 
 uint8* ArrayBuffer::Data::data() const {
-  if (cache_) {
-    cache_->ReportUsage(this);
-  }
   return data_;
 }
 
-bool ArrayBuffer::Data::Offload() {
-  if (offloaded_) {
-    return true;
-  }
-  if (!allocator_) {
-    return false;
-  }
-  uint8* data = reinterpret_cast<uint8*>(allocator_->Allocate(size()));
-  if (data) {
-    memcpy(data, data_, size());
-    delete[] data_;
-    data_ = data;
-    offloaded_ = true;
-  }
-  return offloaded_;
-}
-
-void ArrayBuffer::Data::Initialize(script::EnvironmentSettings* settings,
-                                   size_t size) {
+void ArrayBuffer::Data::Initialize(size_t size) {
   TRACK_MEMORY_SCOPE("DOM");
-  if (settings) {
-    DOMSettings* dom_settings =
-        base::polymorphic_downcast<dom::DOMSettings*>(settings);
-    allocator_ = dom_settings->array_buffer_allocator();
-    cache_ = dom_settings->array_buffer_cache();
-    if (allocator_) {
-      DCHECK(cache_);
-    } else {
-      DCHECK(!cache_);
-    }
-  }
   data_ = new uint8[size];
   size_ = size;
 }
 
-ArrayBuffer::Cache::Cache(size_t maximum_size_in_main_memory)
-    : total_size_in_main_memory_(0),
-      maximum_size_in_main_memory_(maximum_size_in_main_memory) {}
-
-void ArrayBuffer::Cache::Register(Data* data) {
-  total_size_in_main_memory_ += data->size();
-  // Offload before push_back to ensure that the last one is always not
-  // offloaded immediately.
-  TryToOffload();
-  datas_.push_back(data);
-}
-
-void ArrayBuffer::Cache::Unregister(Data* data) {
-  DCHECK(std::find(datas_.begin(), datas_.end(), data) != datas_.end());
-  datas_.erase(std::find(datas_.begin(), datas_.end(), data));
-  if (!data->offloaded()) {
-    DCHECK_GE(total_size_in_main_memory_, data->size());
-    total_size_in_main_memory_ -= data->size();
-  }
-}
-
-void ArrayBuffer::Cache::ReportUsage(const Data* data) {
-  DCHECK(data);
-  DCHECK(std::find(datas_.begin(), datas_.end(), data) != datas_.end());
-  if (data->offloaded() || datas_.back() == data) {
-    return;
-  }
-  // Move |data| to the end.
-  datas_.erase(std::find(datas_.begin(), datas_.end(), data));
-  datas_.push_back(const_cast<Data*>(data));
-}
-
-void ArrayBuffer::Cache::TryToOffload() {
-  TRACK_MEMORY_SCOPE("DOM");
-  if (total_size_in_main_memory_ <= maximum_size_in_main_memory_) {
-    return;
-  }
-  std::vector<Data*>::iterator iter = datas_.begin();
-  while (iter != datas_.end() &&
-         total_size_in_main_memory_ > maximum_size_in_main_memory_) {
-    if (!(*iter)->offloaded() && (*iter)->Offload()) {
-      total_size_in_main_memory_ -= (*iter)->size();
-    }
-    ++iter;
-  }
-  if (total_size_in_main_memory_ > maximum_size_in_main_memory_) {
-    LOG(WARNING) << "ArrayBuffer takes " << total_size_in_main_memory_
-                 << " of main memory and cannot be offloaded";
-  }
-}
-
 ArrayBuffer::ArrayBuffer(script::EnvironmentSettings* settings, uint32 length)
-    : data_(settings, length) {
+    : data_(length) {
   // TODO: Once we can have a reliable way to pass the
   // EnvironmentSettings to HTMLMediaElement, we should make EnvironmentSettings
   // mandatory for creating ArrayBuffer in non-testing code.
@@ -189,7 +82,7 @@ ArrayBuffer::ArrayBuffer(script::EnvironmentSettings* settings, uint32 length)
 
 ArrayBuffer::ArrayBuffer(script::EnvironmentSettings* settings,
                          const uint8* data, uint32 length)
-    : data_(settings, data, length) {
+    : data_(data, length) {
   // TODO: Make EnvironmentSettings mandatory for creating
   // ArrayBuffer in non-testing code.
   if (settings) {
