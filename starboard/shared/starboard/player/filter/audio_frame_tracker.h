8e8b927a0fe349c4376bbb685528c186895aebf8
Xiaoming Shi <xiaomings@google.com>
2018-03-16 00:57:54 +0000

    Refactor audio time reporting
    1. Remove usage of atomic variables in AudioRenderer and use try locks
       to avoid block the sink thread.
    2. Refactor audio time calculation in AudioRenderer and add debug log
       audio time accuracy.
    3. Refactor AudioFrameTracker to remove locks.
    4. Refactor frame consumption reporting on Android.
    
    b/74345529
    
    Change-Id: I4cb726c9d4c50bd26e118d85905eeabee7d33e1d

--- starboard/shared/starboard/player/filter/audio_frame_tracker.h
+++ starboard/shared/starboard/player/filter/audio_frame_tracker.h
@@ -15,12 +15,11 @@
 #ifndef STARBOARD_SHARED_STARBOARD_PLAYER_FILTER_AUDIO_FRAME_TRACKER_H_
 #define STARBOARD_SHARED_STARBOARD_PLAYER_FILTER_AUDIO_FRAME_TRACKER_H_
 
-#include <queue>
+#include <vector>
 
 #include "starboard/common/scoped_ptr.h"
 #include "starboard/log.h"
 #include "starboard/media.h"
-#include "starboard/mutex.h"
 #include "starboard/shared/internal_only.h"
 #include "starboard/shared/starboard/thread_checker.h"
 
@@ -39,64 +38,21 @@ class AudioFrameTracker {
  public:
   // Reset the class to its initial state.  In this state there is no frames
   // tracked and the playback frames is 0.
-  void Reset() {
-    ScopedLock lock(mutex_);
-    impl_.Reset();
-  }
-
-  void AddFrames(int number_of_frames, double playback_rate) {
-    if (number_of_frames == 0) {
-      return;
-    }
-
-    ScopedLock lock(mutex_);
-    impl_.AddFrames(number_of_frames, playback_rate);
-  }
-
-  void RecordPlayedFrames(int number_of_frames) {
-    if (number_of_frames == 0) {
-      return;
-    }
-    ScopedLock lock(mutex_);
-    impl_.RecordPlayedFrames(number_of_frames);
-  }
-
+  void Reset();
+  void AddFrames(int number_of_frames, double playback_rate);
+  void RecordPlayedFrames(int number_of_frames);
   int64_t GetFutureFramesPlayedAdjustedToPlaybackRate(
-      int number_of_frames) const {
-    Impl impl_copy;
-    {
-      ScopedLock lock(mutex_);
-      impl_copy = impl_;
-    }
-
-    impl_copy.RecordPlayedFrames(number_of_frames);
-
-    return impl_copy.GetFramesPlayedAdjustedToPlaybackRate();
-  }
+      int number_of_frames) const;
 
  private:
-  // Impl carries the core functionalities of AudioFrameTracker.  It doesn't
-  // have any synchronization.
-  class Impl {
-   public:
-    Impl();
-    void Reset();
-    void AddFrames(int number_of_frames, double playback_rate);
-    void RecordPlayedFrames(int number_of_frames);
-    int64_t GetFramesPlayedAdjustedToPlaybackRate() const;
-
-   private:
-    struct FrameRecord {
-      int number_of_frames;
-      double playback_rate;
-    };
-
-    std::queue<FrameRecord> frame_records_;
-    int64_t frames_played_adjusted_to_playback_rate_;
+  struct FrameRecord {
+    int number_of_frames;
+    double playback_rate;
   };
 
-  Mutex mutex_;
-  Impl impl_;
+  // Usually there are very few elements, so std::vector<> is efficient enough.
+  std::vector<FrameRecord> frame_records_;
+  int64_t frames_played_adjusted_to_playback_rate_ = 0;
 };
 
 }  // namespace filter
