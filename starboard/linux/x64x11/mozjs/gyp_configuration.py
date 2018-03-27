9b329185f0046b0e470f00b54f1dcbb4b69e289c
Cobalt Team <cobalt-team@google.com>
2018-03-26 18:07:28 -0700

    Enable V8 on linux
    This change enables V8 for all linux-* platforms.  It also adds the new
    target, linux-x64x11-mozjs, which will continue to use SpiderMonkey.
    
    b/67313140
    
    Change-Id: I8a9725cc02e2e67bd413461d1bae7a20d75626ed

---
+++ starboard/linux/x64x11/mozjs/gyp_configuration.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Starboard Linux X64 X11 future platform configuration."""
+
+from starboard.linux.x64x11 import gyp_configuration as linux_configuration
+
+
+def CreatePlatformConfig():
+  return linux_configuration.LinuxX64X11Configuration('linux-x64x11-mozjs')
