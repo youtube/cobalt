d9fff6ebab0a457cbb6fde74cbe6fcdd10fc0236
Xiaoming Shi <xiaomings@google.com>
2018-03-28 17:31:54 -0700

    Remove non-MSE 2016 code from MediaModuleStarboard
    All platforms using MediaModuleStarboard should be MSE 2016 only.
    Such code in ShellMediaPlatformStarboard is also removed.
    
    Change-Id: Ie55be0a6ee9b4af4919401ca7135730d8decb911

--- cobalt/media/shell_media_platform_starboard.h
+++ cobalt/media/shell_media_platform_starboard.h
@@ -17,11 +17,8 @@
 
 #include "base/compiler_specific.h"
 #include "base/logging.h"
-#include "cobalt/render_tree/resource_provider.h"
-
-#if defined(COBALT_MEDIA_SOURCE_2016)
-
 #include "cobalt/media/base/shell_media_platform.h"
+#include "cobalt/render_tree/resource_provider.h"
 
 namespace cobalt {
 namespace media {
@@ -77,77 +74,5 @@ class ShellMediaPlatformStarboard : public ShellMediaPlatform {
 
 }  // namespace media
 }  // namespace cobalt
-
-#else  // defined(COBALT_MEDIA_SOURCE_2016)
-
-#include "media/base/shell_media_platform.h"
-
-#include "base/memory/aligned_memory.h"
-#include "base/memory/scoped_ptr.h"
-#include "cobalt/media/shell_video_data_allocator_common.h"
-#include "nb/memory_pool.h"
-#include "starboard/common/locked_ptr.h"
-
-namespace media {
-
-class ShellMediaPlatformStarboard : public ShellMediaPlatform {
- public:
-  ShellMediaPlatformStarboard(
-      cobalt::render_tree::ResourceProvider* resource_provider);
-  ~ShellMediaPlatformStarboard() override;
-
-  void* AllocateBuffer(size_t size) override;
-  void FreeBuffer(void* ptr) override;
-  size_t GetSourceBufferStreamAudioMemoryLimit() const override {
-    return SB_MEDIA_SOURCE_BUFFER_STREAM_AUDIO_MEMORY_LIMIT;
-  }
-  size_t GetSourceBufferStreamVideoMemoryLimit() const override {
-    return SB_MEDIA_SOURCE_BUFFER_STREAM_VIDEO_MEMORY_LIMIT;
-  }
-  int GetMaxVideoPrerollFrames() const override {
-    return SB_MEDIA_MAXIMUM_VIDEO_PREROLL_FRAMES;
-  }
-  int GetMaxVideoFrames() const override {
-    return SB_MEDIA_MAXIMUM_VIDEO_FRAMES;
-  }
-  scoped_refptr<DecoderBuffer> ProcessBeforeLeavingDemuxer(
-      const scoped_refptr<DecoderBuffer>& buffer) override;
-  bool IsOutputProtected() override;
-
-  SbDecodeTargetGraphicsContextProvider*
-  GetSbDecodeTargetGraphicsContextProvider() override {
-#if SB_HAS(GRAPHICS)
-    return resource_provider_->GetSbDecodeTargetGraphicsContextProvider();
-#else   // SB_HAS(GRAPHICS)
-    return NULL;
-#endif  // SB_HAS(GRAPHICS)
-  }
-
-  void Suspend() override { resource_provider_ = NULL; }
-  void Resume(
-      cobalt::render_tree::ResourceProvider* resource_provider) override {
-    resource_provider_ = resource_provider;
-  }
-
- private:
-  cobalt::render_tree::ResourceProvider* resource_provider_;
-
-  // Optional GPU Memory buffer pool, for buffer offloading.
-  scoped_ptr<cobalt::render_tree::RawImageMemory> gpu_memory_buffer_space_;
-  starboard::LockedPtr<nb::MemoryPool> gpu_memory_pool_;
-
-  // Main Memory buffer pool.
-  scoped_ptr_malloc<uint8, base::ScopedPtrAlignedFree>
-      main_memory_buffer_space_;
-  starboard::LockedPtr<nb::MemoryPool> main_memory_pool_;
-
-  ShellVideoDataAllocatorCommon video_data_allocator_;
-
-  DISALLOW_COPY_AND_ASSIGN(ShellMediaPlatformStarboard);
-};
-
-}  // namespace media
-
-#endif  // defined(COBALT_MEDIA_SOURCE_2016)
 
 #endif  // COBALT_MEDIA_SHELL_MEDIA_PLATFORM_STARBOARD_H_
