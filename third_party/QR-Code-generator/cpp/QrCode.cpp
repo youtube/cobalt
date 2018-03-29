fe56d1b52324022714afc11b61d3668b57728245
Xiaoming Shi <xiaomings@google.com>
2018-03-29 10:44:18 -0700

    Get QR-Code-generator starboardized and built
    b/68224039
    
    Change-Id: Ie2352d86e31f2a91521092b0bcf295f2737ca1a6

--- third_party/QR-Code-generator/cpp/QrCode.cpp
+++ third_party/QR-Code-generator/cpp/QrCode.cpp
@@ -30,6 +30,10 @@
 #include "BitBuffer.hpp"
 #include "QrCode.hpp"
 
+#include "starboard/log.h"
+
+#define throw SB_CHECK(false) <<
+
 using std::int8_t;
 using std::uint8_t;
 using std::size_t;
@@ -490,11 +494,12 @@ long QrCode::getPenaltyScore() const {
 
 
 vector<int> QrCode::getAlignmentPatternPositions(int ver) {
-	if (ver < MIN_VERSION || ver > MAX_VERSION)
+	if (ver < MIN_VERSION || ver > MAX_VERSION) {
 		throw "Version number out of range";
-	else if (ver == 1)
 		return vector<int>();
-	else {
+	} else if (ver == 1) {
+		return vector<int>();
+	} else {
 		int numAlign = ver / 7 + 2;
 		int step;
 		if (ver != 32) {
