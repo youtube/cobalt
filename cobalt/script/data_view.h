683cadd51d0ecfc275534121cbf874020bd9cde8
Cobalt Team <cobalt-team@google.com>
2018-04-02 12:36:26 -0700

    Add array buffer (and friends) to script
    This change adds array buffers and related classes, as defined by ES6,
    to cobalt::script.  See below for a full list.
    
      * ArrayBuffer
      * ArrayBufferView
      * TypedArray
      * Int8Array
      * Uint8Array
      * Uint8Clamped
      * Int16Array
      * Uint16Array
      * Int32Array
      * Uint32Array
      * Float32Array
      * Float64Array
      * DataView
    
    It also adds SpiderMonkey 45 and V8 implementations, as well as some
    minimal testing for them.  I have some more extensive tests for these,
    however they depend on bindings implementing giving/receiving these
    classes, which we have to hold off on until all of dom/ has been
    converted to use script::ArrayBuffers instead of dom::ArrayBuffers.
    
    b/67313140
    b/36898816
    b/36807955
    
    Change-Id: I7c0f64823c56da14ca2e153d28d64919d1b1fc9f

---
+++ cobalt/script/data_view.h
@@ -0,0 +1,37 @@
+// Copyright 2018 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef COBALT_SCRIPT_DATA_VIEW_H_
+#define COBALT_SCRIPT_DATA_VIEW_H_
+
+#include "cobalt/script/array_buffer.h"
+#include "cobalt/script/array_buffer_view.h"
+#include "cobalt/script/script_value.h"
+
+namespace cobalt {
+namespace script {
+
+// http://www.ecma-international.org/ecma-262/6.0/#sec-dataview-constructor
+class DataView : public ArrayBufferView {
+ public:
+  // http://www.ecma-international.org/ecma-262/6.0/#sec-dataview-buffer-byteoffset-bytelength
+  static Handle<DataView> New(GlobalEnvironment* global_environment,
+                              Handle<ArrayBuffer> array_buffer,
+                              size_t byte_offset, size_t byte_length);
+};
+
+}  // namespace script
+}  // namespace cobalt
+
+#endif  // COBALT_SCRIPT_DATA_VIEW_H_
