b8b74548c490be7cbb619cbbdb250f38afa0b392
Joe Woodworth <joewoodworth@google.com>
2018-04-02 17:57:25 -0700

    Remove SbMediaTime from Starboard interface
    Introduce SbPlayerSeek2, and SbPlayerWriteSample2 which take SbTime
    instead of SbMediaTime. Also remove duration_pts from SbPlayerCreate
    and SbPlayerCreateWithUrl.
    
    b/69798168
    
    Change-Id: I6a1f272259383bf51d13fdbd9e306af271e8fedd

--- starboard/shared/starboard/player/player_get_info2.cc
+++ starboard/shared/starboard/player/player_get_info2.cc
@@ -1,4 +1,4 @@
-// Copyright 2016 Google Inc. All Rights Reserved.
+// Copyright 2018 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,8 +17,8 @@
 #include "starboard/log.h"
 #include "starboard/shared/starboard/player/player_internal.h"
 
-#if SB_API_VERSION < SB_DEPRECATE_SB_MEDIA_TIME_API_VERSION
-void SbPlayerGetInfo(SbPlayer player, SbPlayerInfo* out_player_info) {
+#if SB_API_VERSION >= SB_DEPRECATE_SB_MEDIA_TIME_API_VERSION
+void SbPlayerGetInfo2(SbPlayer player, SbPlayerInfo2* out_player_info) {
   if (!SbPlayerIsValid(player)) {
     SB_DLOG(WARNING) << "player is invalid.";
     return;
@@ -31,4 +31,4 @@ void SbPlayerGetInfo(SbPlayer player, SbPlayerInfo* out_player_info) {
 
   player->GetInfo(out_player_info);
 }
-#endif  // SB_API_VERSION < SB_DEPRECATE_SB_MEDIA_TIME_API_VERSION
+#endif  // SB_API_VERSION >= SB_DEPRECATE_SB_MEDIA_TIME_API_VERSION
