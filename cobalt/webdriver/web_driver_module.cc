e564f8e357d56f5898a345d645da7c54a77af06a
Cobalt Team <cobalt-team@google.com>
2018-04-06 11:57:29 -0700

    Fix flakiness in persistent_cookie test and re-enable it
    persistent_cookie used to be flaky because we have no way to ensure
    synchronous cookie flushing and we were sending kill signal in launcher
    to terminate the app. By calling window.close() we can gracefully shut
    down Cobalt, utilizing and testing the cookie flushinig logic in there.
    
    Also fixed a webdriver destructor error. Session/Window/Element drivers
    need to be destroyed on webdriver thread.
    
    b/78193111
    
    Change-Id: Ib5efd5423623101dc4f14496d17f9db175871d90

--- cobalt/webdriver/web_driver_module.cc
+++ cobalt/webdriver/web_driver_module.cc
@@ -454,8 +454,8 @@ WebDriverModule::WebDriverModule(
 }  // NOLINT(readability/fn_size)
 
 WebDriverModule::~WebDriverModule() {
-  webdriver_thread_.message_loop()->PostTask(
-      FROM_HERE, base::Bind(&WebDriverModule::StopServer,
+  webdriver_thread_.message_loop()->PostBlockingTask(
+      FROM_HERE, base::Bind(&WebDriverModule::StopServerAndSession,
                             base::Unretained(this)));
   webdriver_thread_.Stop();
 }  // NOLINT(readability/fn_size)
@@ -484,8 +484,9 @@ void WebDriverModule::StartServer(int server_port,
       "Cobalt.Server.WebDriver"));
 }
 
-void WebDriverModule::StopServer() {
+void WebDriverModule::StopServerAndSession() {
   webdriver_server_.reset();
+  session_.reset();
 }
 
 SessionDriver* WebDriverModule::GetSessionDriver(
