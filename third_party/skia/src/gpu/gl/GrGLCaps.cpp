655dad29beb6742017c307eeed34f4b58bc6d932
Toan Pham <toanpham@google.com>
2018-04-11 15:14:55 -0700

    Fix darwin crash introduced with overriding driver version
    On darwin, a custom EGL implementation is used which only
    supports GLES 2.0 features, but the driver supports GLES 3.0.
    
    Change-Id: I0c488236056f131e1ffe4413c3e5e35045e507f9

--- third_party/skia/src/gpu/gl/GrGLCaps.cpp
+++ third_party/skia/src/gpu/gl/GrGLCaps.cpp
@@ -1639,10 +1639,19 @@ void GrGLCaps::initConfigTable(const GrContextOptions& contextOptions,
         // We require some form of FBO support and all GLs with FBO support can render to RGBA8
         fConfigTable[kRGBA_8888_GrPixelConfig].fFlags |= allRenderFlags;
     } else {
+#if defined(STARBOARD)
+        // Starboard code in GrGLUtil.cpp may override the driver version from
+        // GLES 3.0 to 2.0 if the config specifies that only GLES 2.0 features
+        // should be used. This will confuse the regular capabilities check.
+        // Since all Starboard GLES platforms must support rendering to RGBA8
+        // buffers, no check is needed here.
+        fConfigTable[kRGBA_8888_GrPixelConfig].fFlags |= allRenderFlags;
+#else
         if (version >= GR_GL_VER(3,0) || ctxInfo.hasExtension("GL_OES_rgb8_rgba8") ||
             ctxInfo.hasExtension("GL_ARM_rgba8")) {
             fConfigTable[kRGBA_8888_GrPixelConfig].fFlags |= allRenderFlags;
         }
+#endif
     }
     if (texStorageSupported) {
         fConfigTable[kRGBA_8888_GrPixelConfig].fFlags |= ConfigInfo::kCanUseTexStorage_Flag;
