a621d3ac913b1d25369153ceb569f9159c62b75b
Cobalt Team <cobalt-team@google.com>
2018-04-18 11:08:14 -0700

    Starboardize atomic functions in V8.
    Our V8 repo uses gcc atomic function calls. Starboardizing them to
    enable V8 on other platforms(e.g. win32 and xbox).
    
    b/78194754
    
    Change-Id: I43296cd4f53007563b34b1084b7b63c3ad4c4bdf

--- v8/src/base/atomicops_internals_portable.h
+++ v8/src/base/atomicops_internals_portable.h
@@ -31,6 +31,10 @@
 
 #include <atomic>
 
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+#include "starboard/atomic.h"
+#endif
+
 #include "src/base/build_config.h"
 #include "src/base/macros.h"
 
@@ -41,135 +45,231 @@ namespace base {
 // atomicops.h.
 
 inline void SeqCst_MemoryFence() {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  SbAtomicMemoryBarrier();
+#else
 #if defined(__GLIBCXX__)
   // Work around libstdc++ bug 51038 where atomic_thread_fence was declared but
   // not defined, leading to the linker complaining about undefined references.
   __atomic_thread_fence(std::memory_order_seq_cst);
 #else
   std::atomic_thread_fence(std::memory_order_seq_cst);
-#endif
+#endif  //  defined(__GLIBCXX__)
+#endif  //  defined(V8_OS_STARBOARD)
 }
 
 inline Atomic32 Relaxed_CompareAndSwap(volatile Atomic32* ptr,
                                        Atomic32 old_value, Atomic32 new_value) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicNoBarrier_CompareAndSwap(ptr, old_value, new_value);
+#else
   __atomic_compare_exchange_n(ptr, &old_value, new_value, false,
                               __ATOMIC_RELAXED, __ATOMIC_RELAXED);
   return old_value;
+#endif
 }
 
 inline Atomic32 Relaxed_AtomicExchange(volatile Atomic32* ptr,
                                        Atomic32 new_value) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicNoBarrier_Exchange(ptr, new_value);
+#else
   return __atomic_exchange_n(ptr, new_value, __ATOMIC_RELAXED);
+#endif
 }
 
 inline Atomic32 Relaxed_AtomicIncrement(volatile Atomic32* ptr,
                                         Atomic32 increment) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicNoBarrier_Increment(ptr, increment);
+#else
   return increment + __atomic_fetch_add(ptr, increment, __ATOMIC_RELAXED);
+#endif
 }
 
 inline Atomic32 Barrier_AtomicIncrement(volatile Atomic32* ptr,
                                         Atomic32 increment) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicBarrier_Increment(ptr, increment);
+#else
   return increment + __atomic_fetch_add(ptr, increment, __ATOMIC_SEQ_CST);
+#endif
 }
 
 inline Atomic32 Acquire_CompareAndSwap(volatile Atomic32* ptr,
                                        Atomic32 old_value, Atomic32 new_value) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicAcquire_CompareAndSwap(ptr, old_value, new_value);
+#else
   __atomic_compare_exchange_n(ptr, &old_value, new_value, false,
                               __ATOMIC_ACQUIRE, __ATOMIC_ACQUIRE);
   return old_value;
+#endif
 }
 
 inline Atomic8 Release_CompareAndSwap(volatile Atomic8* ptr, Atomic8 old_value,
                                       Atomic8 new_value) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicRelease_CompareAndSwap8(ptr, old_value, new_value);
+#else
   bool result = __atomic_compare_exchange_n(ptr, &old_value, new_value, false,
                                             __ATOMIC_RELEASE, __ATOMIC_RELAXED);
   USE(result);  // Make gcc compiler happy.
   return old_value;
+#endif
 }
 
 inline Atomic32 Release_CompareAndSwap(volatile Atomic32* ptr,
                                        Atomic32 old_value, Atomic32 new_value) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicRelease_CompareAndSwap(ptr, old_value, new_value);
+#else
   __atomic_compare_exchange_n(ptr, &old_value, new_value, false,
                               __ATOMIC_RELEASE, __ATOMIC_RELAXED);
   return old_value;
+#endif
 }
 
 inline void Relaxed_Store(volatile Atomic8* ptr, Atomic8 value) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  SbAtomicNoBarrier_Store8(ptr, value);
+#else
   __atomic_store_n(ptr, value, __ATOMIC_RELAXED);
+#endif
 }
 
 inline void Relaxed_Store(volatile Atomic32* ptr, Atomic32 value) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  SbAtomicNoBarrier_Store(ptr, value);
+#else
   __atomic_store_n(ptr, value, __ATOMIC_RELAXED);
+#endif
 }
 
 inline void Release_Store(volatile Atomic32* ptr, Atomic32 value) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  SbAtomicRelease_Store(ptr, value);
+#else
   __atomic_store_n(ptr, value, __ATOMIC_RELEASE);
+#endif
 }
 
 inline Atomic8 Relaxed_Load(volatile const Atomic8* ptr) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicNoBarrier_Load8(ptr);
+#else
   return __atomic_load_n(ptr, __ATOMIC_RELAXED);
+#endif
 }
 
 inline Atomic32 Relaxed_Load(volatile const Atomic32* ptr) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicNoBarrier_Load(ptr);
+#else
   return __atomic_load_n(ptr, __ATOMIC_RELAXED);
+#endif
 }
 
 inline Atomic32 Acquire_Load(volatile const Atomic32* ptr) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicAcquire_Load(ptr);
+#else
   return __atomic_load_n(ptr, __ATOMIC_ACQUIRE);
+#endif
 }
 
 #if defined(V8_HOST_ARCH_64_BIT)
 
 inline Atomic64 Relaxed_CompareAndSwap(volatile Atomic64* ptr,
                                        Atomic64 old_value, Atomic64 new_value) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicNoBarrier_CompareAndSwap64(ptr, old_value, new_value);
+#else
   __atomic_compare_exchange_n(ptr, &old_value, new_value, false,
                               __ATOMIC_RELAXED, __ATOMIC_RELAXED);
   return old_value;
+#endif
 }
 
 inline Atomic64 Relaxed_AtomicExchange(volatile Atomic64* ptr,
                                        Atomic64 new_value) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicNoBarrier_Exchange64(ptr, new_value);
+#else
   return __atomic_exchange_n(ptr, new_value, __ATOMIC_RELAXED);
+#endif
 }
 
 inline Atomic64 Relaxed_AtomicIncrement(volatile Atomic64* ptr,
                                         Atomic64 increment) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicNoBarrier_Increment64(ptr, increment);
+#else
   return increment + __atomic_fetch_add(ptr, increment, __ATOMIC_RELAXED);
+#endif
 }
 
 inline Atomic64 Barrier_AtomicIncrement(volatile Atomic64* ptr,
                                         Atomic64 increment) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicBarrier_Increment64(ptr, increment);
+#else
   return increment + __atomic_fetch_add(ptr, increment, __ATOMIC_SEQ_CST);
+#endif
 }
 
 inline Atomic64 Acquire_CompareAndSwap(volatile Atomic64* ptr,
                                        Atomic64 old_value, Atomic64 new_value) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicAcquire_CompareAndSwap64(ptr, old_value, new_value);
+#else
   __atomic_compare_exchange_n(ptr, &old_value, new_value, false,
                               __ATOMIC_ACQUIRE, __ATOMIC_ACQUIRE);
   return old_value;
+#endif
 }
 
 inline Atomic64 Release_CompareAndSwap(volatile Atomic64* ptr,
                                        Atomic64 old_value, Atomic64 new_value) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicRelease_CompareAndSwap64(ptr, old_value, new_value);
+#else
   __atomic_compare_exchange_n(ptr, &old_value, new_value, false,
                               __ATOMIC_RELEASE, __ATOMIC_RELAXED);
   return old_value;
+#endif
 }
 
 inline void Relaxed_Store(volatile Atomic64* ptr, Atomic64 value) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  SbAtomicNoBarrier_Store64(ptr, value);
+#else
   __atomic_store_n(ptr, value, __ATOMIC_RELAXED);
+#endif
 }
 
 inline void Release_Store(volatile Atomic64* ptr, Atomic64 value) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  SbAtomicRelease_Store64(ptr, value);
+#else
   __atomic_store_n(ptr, value, __ATOMIC_RELEASE);
+#endif
 }
 
 inline Atomic64 Relaxed_Load(volatile const Atomic64* ptr) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicNoBarrier_Load64(ptr);
+#else
   return __atomic_load_n(ptr, __ATOMIC_RELAXED);
+#endif
 }
 
 inline Atomic64 Acquire_Load(volatile const Atomic64* ptr) {
+#if defined(V8_OS_STARBOARD) && SB_API_VERSION >= SB_INTRODUCE_ATOMIC8_VERSION
+  return SbAtomicAcquire_Load64(ptr);
+#else
   return __atomic_load_n(ptr, __ATOMIC_ACQUIRE);
+#endif
 }
 
 #endif  // defined(V8_HOST_ARCH_64_BIT)
