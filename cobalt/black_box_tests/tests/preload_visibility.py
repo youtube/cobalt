32d1df951b1a24a3baed8d1bd493251d0d036727
Cobalt Team <cobalt-team@google.com>
2018-04-19 18:48:32 -0700

    Use a more flexible server for blackbox tests.
    Previously, black box tests used a SimpleHTTPServer to serve assets.
    This had several limitations:
    1. Launched an external process, which was not easy to control.
    2. Hard coded port
      a. Only 1 instance per machine was possible.
      b. May not be available if the port was not cleaned up properly.
    3. Current solution was IPv4 only.
    4. SimpleHTTPServer is python2 only.
    
    Now, a test can spawn a HTTP server in a thread on demand.  In addition,
    the new server will work in IPv6 only environments, and will be easier
    to port to python3, when the time comes.  Also, a custom request handler
    can be passed, so that tests can finely control the behavior of the
    server when a request is received.
    
    Change-Id: Ica4249b6856cde49d14c66afeecdbebf985b7e93

--- cobalt/black_box_tests/tests/preload_visibility.py
+++ cobalt/black_box_tests/tests/preload_visibility.py
@@ -7,17 +7,19 @@ from __future__ import print_function
 import _env  # pylint: disable=unused-import
 
 from cobalt.black_box_tests import black_box_tests
+from cobalt.black_box_tests.threaded_web_server import ThreadedWebServer
 
 
 class PreloadVisibilityTest(black_box_tests.BlackBoxTestCase):
 
   def test_simple(self):
 
-    url = self.GetURL(file_name='preload_visibility.html')
+    with ThreadedWebServer() as server:
+      url = server.GetURL(file_name='testdata/preload_visibility.html')
 
-    with self.CreateCobaltRunner(
-        url=url, target_params=['--preload']) as runner:
-      runner.WaitForJSTestsSetup()
-      self.assertTrue(runner.IsInPreload())
-      runner.SendResume()
-      self.assertTrue(runner.JSTestsSucceeded())
+      with self.CreateCobaltRunner(
+          url=url, target_params=['--preload']) as runner:
+        runner.WaitForJSTestsSetup()
+        self.assertTrue(runner.IsInPreload())
+        runner.SendResume()
+        self.assertTrue(runner.JSTestsSucceeded())
