8741811abc8a062767eb2dabd563fc341a5e889c
Cobalt Team <cobalt-team@google.com>
2018-05-15 12:08:21 -0700

    Add rasterizer type to the user agent.
    Refactor some of the rasterizer selection code to separate making
    the choice of which rasterizer to use from actually constructing
    the chosen rasterizer.
    
    b/74759413
    
    Change-Id: Ib61a055fd8a97b227fea9b2a44d5ca7240aabb85

---
+++ cobalt/renderer/renderer_module_default_options.cc
@@ -0,0 +1,35 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "cobalt/renderer/get_default_rasterizer_for_platform.h"
+#include "cobalt/renderer/renderer_module.h"
+
+#include "base/logging.h"
+
+namespace cobalt {
+namespace renderer {
+
+void RendererModule::Options::SetPerPlatformDefaultOptions() {
+  // If there is no need to frequently flip the display buffer, then enable
+  // support for an optimization where the scene is not re-rasterized each frame
+  // if it has not changed from the last frame.
+  submit_even_if_render_tree_is_unchanged =
+      SB_MUST_FREQUENTLY_FLIP_DISPLAY_BUFFER;
+
+  create_rasterizer_function =
+      GetDefaultRasterizerForPlatform().create_rasterizer_callback;
+}
+
+}  // namespace renderer
+}  // namespace cobalt
