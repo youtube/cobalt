d6abb1637c9759c4289525ce84cac8c9fb9f5058
Xiaoming Shi <xiaomings@google.com>
2018-05-15 15:42:05 -0700

    Don't include "intrin.h" in anonymous namespace
    "lz4.c" includes "intrin.h".  "lz4.c" itself is included into an
    anonymous namespace, this causes link errors in MSVC 14.14.26428.
    See https://bugzilla.mozilla.org/show_bug.cgi?id=1345331 for more
    details.
    
    Change-Id: Ibd5541d127c629b6b5c2424d2281fbda5070e0af

--- third_party/mozjs-45/mfbt/mfbt_Compression.cpp
+++ third_party/mozjs-45/mfbt/mfbt_Compression.cpp
@@ -14,6 +14,15 @@
 
 using namespace mozilla::Compression;
 
+// Because we wrap lz4.c in an anonymous namespace, all of its #includes
+// go in the anonymous namespace too. This would create conflicting
+// declarations for intrinsic functions that are internally defined
+// at top-level. Including intrin.h now prevents it from being included
+// later within the anonymous namespace.
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+
 namespace {
 
 #include "lz4.c"
