f8ddbef23b8ecbb17c4a6b93ff96dfbbd0f79d7c
Cobalt Team <cobalt-team@google.com>
2018-05-15 12:08:21 -0700

    Add rasterizer type to the user agent.
    Refactor some of the rasterizer selection code to separate making
    the choice of which rasterizer to use from actually constructing
    the chosen rasterizer.
    
    b/74759413
    
    Change-Id: Ib61a055fd8a97b227fea9b2a44d5ca7240aabb85

--- cobalt/renderer/get_default_rasterizer_for_platform.cc
+++ cobalt/renderer/get_default_rasterizer_for_platform.cc
@@ -1,4 +1,4 @@
-// Copyright 2015 Google Inc. All Rights Reserved.
+// Copyright 2018 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,35 +12,43 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "cobalt/renderer/renderer_module.h"
+#include "cobalt/renderer/get_default_rasterizer_for_platform.h"
 
-#include "base/debug/trace_event.h"
-#include "base/logging.h"
+#include "cobalt/renderer/backend/graphics_context.h"
 #include "cobalt/renderer/rasterizer/blitter/hardware_rasterizer.h"
 #include "cobalt/renderer/rasterizer/blitter/software_rasterizer.h"
 #include "cobalt/renderer/rasterizer/egl/hardware_rasterizer.h"
 #include "cobalt/renderer/rasterizer/egl/software_rasterizer.h"
 #include "cobalt/renderer/rasterizer/skia/hardware_rasterizer.h"
 #include "cobalt/renderer/rasterizer/stub/rasterizer.h"
+#include "cobalt/renderer/renderer_module.h"
 
 namespace cobalt {
 namespace renderer {
 
 namespace {
 
-scoped_ptr<rasterizer::Rasterizer> CreateRasterizer(
+#if COBALT_FORCE_STUB_RASTERIZER
+scoped_ptr<rasterizer::Rasterizer> CreateStubRasterizer(
     backend::GraphicsContext* graphics_context,
     const RendererModule::Options& options) {
-  TRACE_EVENT0("cobalt::renderer", "CreateRasterizer");
-#if COBALT_FORCE_STUB_RASTERIZER
+  UNREFERENCED_PARAMETER(graphics_context);
   return scoped_ptr<rasterizer::Rasterizer>(new rasterizer::stub::Rasterizer());
-#else
+}
+#endif  // COBALT_FORCE_STUB_RASTERIZER
+
 #if SB_HAS(GLES2)
-#if COBALT_FORCE_SOFTWARE_RASTERIZER
+scoped_ptr<rasterizer::Rasterizer> CreateGLESSoftwareRasterizer(
+    backend::GraphicsContext* graphics_context,
+    const RendererModule::Options& options) {
   return scoped_ptr<rasterizer::Rasterizer>(
       new rasterizer::egl::SoftwareRasterizer(
           graphics_context, options.purge_skia_font_caches_on_destruction));
-#elif defined(COBALT_FORCE_DIRECT_GLES_RASTERIZER)
+}
+
+scoped_ptr<rasterizer::Rasterizer> CreateGLESHardwareRasterizer(
+    backend::GraphicsContext* graphics_context,
+    const RendererModule::Options& options) {
   return scoped_ptr<rasterizer::Rasterizer>(
       new rasterizer::egl::HardwareRasterizer(
           graphics_context, options.skia_glyph_texture_atlas_dimensions.width(),
@@ -50,7 +58,11 @@ scoped_ptr<rasterizer::Rasterizer> CreateRasterizer(
           options.offscreen_target_cache_size_in_bytes,
           options.purge_skia_font_caches_on_destruction,
           options.force_deterministic_rendering));
-#else
+}
+
+scoped_ptr<rasterizer::Rasterizer> CreateSkiaHardwareRasterizer(
+    backend::GraphicsContext* graphics_context,
+    const RendererModule::Options& options) {
   return scoped_ptr<rasterizer::Rasterizer>(
       new rasterizer::skia::HardwareRasterizer(
           graphics_context, options.skia_glyph_texture_atlas_dimensions.width(),
@@ -59,13 +71,21 @@ scoped_ptr<rasterizer::Rasterizer> CreateRasterizer(
           options.scratch_surface_cache_size_in_bytes,
           options.purge_skia_font_caches_on_destruction,
           options.force_deterministic_rendering));
-#endif  // COBALT_FORCE_SOFTWARE_RASTERIZER
-#elif SB_HAS(BLITTER)
-#if COBALT_FORCE_SOFTWARE_RASTERIZER
+}
+#endif  // #if SB_HAS(GLES2)
+
+#if SB_HAS(BLITTER)
+scoped_ptr<rasterizer::Rasterizer> CreateBlitterSoftwareRasterizer(
+    backend::GraphicsContext* graphics_context,
+    const RendererModule::Options& options) {
   return scoped_ptr<rasterizer::Rasterizer>(
       new rasterizer::blitter::SoftwareRasterizer(
           graphics_context, options.purge_skia_font_caches_on_destruction));
-#else
+}
+
+scoped_ptr<rasterizer::Rasterizer> CreateBlitterHardwareRasterizer(
+    backend::GraphicsContext* graphics_context,
+    const RendererModule::Options& options) {
   return scoped_ptr<rasterizer::Rasterizer>(
       new rasterizer::blitter::HardwareRasterizer(
           graphics_context, options.skia_glyph_texture_atlas_dimensions.width(),
@@ -73,23 +93,34 @@ scoped_ptr<rasterizer::Rasterizer> CreateRasterizer(
           options.scratch_surface_cache_size_in_bytes,
           options.software_surface_cache_size_in_bytes,
           options.purge_skia_font_caches_on_destruction));
+}
+#endif  // SB_HAS(BLITTER)
+
+}  // namespace
+
+RasterizerInfo GetDefaultRasterizerForPlatform() {
+#if COBALT_FORCE_STUB_RASTERIZER
+  return {"stub", base::Bind(&CreateStubRasterizer)};
+#else
+#if SB_HAS(GLES2)
+#if COBALT_FORCE_SOFTWARE_RASTERIZER
+  return {"gles-software", base::Bind(&CreateGLESSoftwareRasterizer)};
+#elif defined(COBALT_FORCE_DIRECT_GLES_RASTERIZER)
+  return {"gles", base::Bind(&CreateGLESHardwareRasterizer)};
+#else
+  return {"skia", base::Bind(&CreateSkiaHardwareRasterizer)};
+#endif  // COBALT_FORCE_SOFTWARE_RASTERIZER
+#elif SB_HAS(BLITTER)
+#if COBALT_FORCE_SOFTWARE_RASTERIZER
+  return {"blitter-software", base::Bind(&CreateBlitterSoftwareRasterizer)};
+#else
+  return {"blitter", base::Bind(&CreateBlitterHardwareRasterizer)};
 #endif  // COBALT_FORCE_SOFTWARE_RASTERIZER
 #else
 #error "Either GLES2 or the Starboard Blitter API must be available."
-  return scoped_ptr<rasterizer::Rasterizer>();
+  return {"", NULL};
 #endif
 #endif  // #if COBALT_FORCE_STUB_RASTERIZER
-}
-}  // namespace
-
-void RendererModule::Options::SetPerPlatformDefaultOptions() {
-  // If there is no need to frequently flip the display buffer, then enable
-  // support for an optimization where the scene is not re-rasterized each frame
-  // if it has not changed from the last frame.
-  submit_even_if_render_tree_is_unchanged =
-      SB_MUST_FREQUENTLY_FLIP_DISPLAY_BUFFER;
-
-  create_rasterizer_function = base::Bind(&CreateRasterizer);
 }
 
 }  // namespace renderer
