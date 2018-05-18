76150f8567f088381d61824e09bed3442511c457
Joe Woodworth <joewoodworth@google.com>
2018-05-17 18:58:23 -0700

    Initialize AudioTimeStretcher with sample type
    Replace kSbMediaAudioSampleTypeFloat32 with the AudioRenderer's
    sink_sample_type_. This enables AudioTimeStretcher to work when
    float32 is not supported.
    
    Change-Id: I50abe4517112d43f4d689309fcf26e0e672b7de8

--- starboard/shared/starboard/player/filter/decoded_audio_queue.cc
+++ starboard/shared/starboard/player/filter/decoded_audio_queue.cc
@@ -43,7 +43,6 @@ void DecodedAudioQueue::Clear() {
 
 void DecodedAudioQueue::Append(
     const scoped_refptr<DecodedAudio>& decoded_audio) {
-  SB_DCHECK(decoded_audio->sample_type() == kSbMediaAudioSampleTypeFloat32);
   SB_DCHECK(decoded_audio->storage_type() ==
             kSbMediaAudioFrameStorageTypeInterleaved)
       << decoded_audio->storage_type();
@@ -116,11 +115,21 @@ int DecodedAudioQueue::InternalRead(int frames,
       // if |dest| is NULL, there's no need to copy.
       if (dest) {
         SB_DCHECK(buffer->channels() == dest->channels());
-        const float* source = reinterpret_cast<const float*>(buffer->buffer()) +
-                              buffer->channels() * current_buffer_offset;
-        float* destination = reinterpret_cast<float*>(dest->buffer()) +
-                             dest->channels() * (dest_frame_offset + taken);
-        SbMemoryCopy(destination, source, copied * dest->channels() * 4);
+        if (dest->sample_type() == kSbMediaAudioSampleTypeFloat32) {
+          const float* source =
+              reinterpret_cast<const float*>(buffer->buffer()) +
+              buffer->channels() * current_buffer_offset;
+          float* destination = reinterpret_cast<float*>(dest->buffer()) +
+                               dest->channels() * (dest_frame_offset + taken);
+          SbMemoryCopy(destination, source, copied * dest->channels() * 4);
+        } else {
+          const int16_t* source =
+              reinterpret_cast<const int16_t*>(buffer->buffer()) +
+              buffer->channels() * current_buffer_offset;
+          int16_t* destination = reinterpret_cast<int16_t*>(dest->buffer()) +
+                                 dest->channels() * (dest_frame_offset + taken);
+          SbMemoryCopy(destination, source, copied * dest->channels() * 2);
+        }
       }
 
       // Increase total number of frames copied, which regulates when to end
