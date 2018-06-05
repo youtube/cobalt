e08ebf0a39829f20e6ea4a61133fc67f5985515d
Cobalt Team <cobalt-team@google.com>
2018-06-05 10:40:03 -0700

    add linux wayland platform
    this make cobalt run with wayland on Ubuntu 17.10
    or any other Ubuntus build local wayland and Weston.
    
    need mesa 10.1.3
    configuration:
    ./autogen.sh --prefix=$WLD --enable-egl --enable-opengl  --enable-gles2
    --with-egl-platforms=x11,wayland,drm --enable-gbm --enable-shared-glapi
    --with-gallium-drivers=r600,swrast,nouveau --enable-gallium-egl
    --disable-gallium-llvm
    building refer:
    https://wayland.freedesktop.org/building.html
    
    (from Samsung-contributed OSS CL:
       https://cobalt-review.googlesource.com/c/cobalt/+/4490)
    
    Change-Id: I4bf64d57bbf7255cb7f0c95bd6bc01ef95b5380b

---
+++ starboard/contrib/linux/x64wl/gyp_configuration.py
@@ -0,0 +1,57 @@
+# Copyright 2018 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Starboard Linux X64 Wayland platform configuration."""
+
+from starboard.linux.shared import gyp_configuration as shared_configuration
+from starboard.tools.toolchain import ar
+from starboard.tools.toolchain import bash
+from starboard.tools.toolchain import clang
+from starboard.tools.toolchain import clangxx
+from starboard.tools.toolchain import cp
+from starboard.tools.toolchain import touch
+
+
+class LinuxX64WaylandConfiguration(shared_configuration.LinuxConfiguration):
+  """Starboard Linux X64 Wayland platform configuration."""
+
+  def __init__(self,
+               platform_name='linux-x64wl',
+               asan_enabled_by_default=False,
+               goma_supports_compiler=True):
+    super(LinuxX64WaylandConfiguration, self).__init__(
+        platform_name, asan_enabled_by_default, goma_supports_compiler)
+
+  def GetTargetToolchain(self):
+    return self.GetHostToolchain()
+
+  def GetHostToolchain(self):
+    environment_variables = self.GetEnvironmentVariables()
+    cc_path = environment_variables['CC']
+    cxx_path = environment_variables['CXX']
+
+    return [
+        clang.CCompiler(path=cc_path),
+        clang.CxxCompiler(path=cxx_path),
+        clang.AssemblerWithCPreprocessor(path=cc_path),
+        ar.StaticThinLinker(),
+        ar.StaticLinker(),
+        clangxx.ExecutableLinker(path=cxx_path),
+        cp.Copy(),
+        touch.Stamp(),
+        bash.Shell(),
+    ]
+
+
+def CreatePlatformConfig():
+  return LinuxX64WaylandConfiguration()
