520a3eac9dbe7b4896500030bf77f95f74cda9d4
Joe Woodworth <joewoodworth@google.com>
2018-06-14 12:39:18 -0700

    Add SbMediaBuffer query functions
    Add SbMediaBuffer query function interfaces together with default
    implementations that retrieve GYP variable defines. Log warnings about
    future deprecation of those defines (except for functions that get
    very often). Add basic tests for the queries.
    
    b/109810256
    
    Change-Id: I3567a67eccb6cd8dbc21ef7961f77f0ece38e57e

---
+++ starboard/shared/starboard/media/media_get_buffer_garbage_collection_duration_threshold.cc
@@ -0,0 +1,41 @@
+// Copyright 2018 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/media.h"
+
+#include "starboard/log.h"
+
+#if SB_API_VERSION >= SB_MEDIA_BUFFER_SETTINGS_QUERIES_API_VERSION
+SbTime SbMediaGetBufferGarbageCollectionDurationThreshold(
+    SbMediaVideoCodec codec,
+    int resolution_width,
+    int resolution_height,
+    int bits_per_pixel) {
+  SB_UNREFERENCED_PARAMETER(codec);
+  SB_UNREFERENCED_PARAMETER(resolution_width);
+  SB_UNREFERENCED_PARAMETER(resolution_height);
+  SB_UNREFERENCED_PARAMETER(bits_per_pixel);
+#if defined( \
+    COBALT_MEDIA_SOURCE_GARBAGE_COLLECTION_DURATION_THRESHOLD_IN_SECONDS)
+  SB_DLOG(WARNING) << "COBALT_MEDIA_SOURCE_GARBAGE_COLLECTION_DURATION_"
+                      "THRESHOLD will be deprecated in a future Starboard "
+                      "version.";
+  // Use define forwarded from GYP variable.
+  return COBALT_MEDIA_SOURCE_GARBAGE_COLLECTION_DURATION_THRESHOLD_IN_SECONDS *
+         kSbTimeSecond;
+#else  // defined(COBALT_MEDIA_SOURCE_GARBAGE_COLLECTION_DURATION_THRESHOLD_IN_SECONDS)
+  return 170 * kSbTimeSecond;
+#endif  // defined(COBALT_MEDIA_SOURCE_GARBAGE_COLLECTION_DURATION_THRESHOLD_IN_SECONDS)
+}
+#endif  // SB_API_VERSION >= SB_MEDIA_BUFFER_SETTINGS_QUERIES_API_VERSION
