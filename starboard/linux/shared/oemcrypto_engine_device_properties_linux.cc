168aa165184a1a855457a8c1c812c5a67a4f4977
Xiaoming Shi <xiaomings@google.com>
2018-06-16 11:53:33 -0700

    Make WV3 work on Linux
    Change-Id: If68f8988e1a4e8c57bca2c2384906952c0b60be4

---
+++ starboard/linux/shared/oemcrypto_engine_device_properties_linux.cc
@@ -0,0 +1,44 @@
+// Copyright 2018 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "third_party/ce_cdm/oemcrypto/mock/src/oemcrypto_engine_mock.h"
+
+#include "starboard/media.h"
+
+namespace {
+const OEMCrypto_HDCP_Capability kWidevineMaximumHdcpVersion = HDCP_V1;
+}  // namespace
+
+namespace wvoec_mock {
+
+class CryptoEngineXb1 : public CryptoEngine {
+ public:
+  explicit CryptoEngineXb1(std::auto_ptr<wvcdm::FileSystem> file_system)
+      : CryptoEngine(file_system) {}
+
+  OEMCrypto_HDCP_Capability config_current_hdcp_capability() override {
+    return HDCP_NONE;
+  }
+
+  OEMCrypto_HDCP_Capability config_maximum_hdcp_capability() override {
+    return kWidevineMaximumHdcpVersion;
+  }
+};
+
+CryptoEngine* CryptoEngine::MakeCryptoEngine(
+    std::auto_ptr<wvcdm::FileSystem> file_system) {
+  return new CryptoEngineXb1(file_system);
+}
+
+}  // namespace wvoec_mock
