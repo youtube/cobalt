5e72fe6b837664352f8a9f9eb315c0c3cd8e91f5
Cobalt Team <cobalt-team@google.com>
2018-06-28 15:43:32 -0700

    Update license headers to read "The Cobalt Authors".
    b/110704583
    
    Change-Id: I566652fee4ed5adfdd24df5997ce4d2ad8049df3

--- starboard/android/apk/app/src/main/java/dev/cobalt/media/VideoFrameReleaseTimeHelper.java
+++ starboard/android/apk/app/src/main/java/dev/cobalt/media/VideoFrameReleaseTimeHelper.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// Modifications Copyright 2017 Google Inc. All Rights Reserved.
+// Modifications Copyright 2017 The Cobalt Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- starboard/tools/toolchain_deprecated.py
+++ starboard/tools/toolchain_deprecated.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Google Inc. All rights reserved.
+# Copyright 2017 The Cobalt Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 """Starboard's abstract Toolchain which is implemented for each platform."""
