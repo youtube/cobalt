251814dfd3ad64840b1e24326ac64a0f8240b8b4
Toan Pham <toanpham@google.com>
2018-07-06 11:08:11 -0700

    Move SetPlayerBounds from Application to Application_X11
    It doesn't seem like other platforms will go through Application
    to handle punch-out synchronization as initially expected. So move
    this function to be more platform-specific.
    
    Also updated x11 implementation of eglSwapBuffers to use
    ApplicationX11::Get() like the new SbPlayerSetBounds.
    
    Change-Id: I594b923b2cc49dc88213d917b1ba1b74ec9db92c

--- starboard/shared/x11/player_set_bounds.cc
+++ starboard/shared/x11/player_set_bounds.cc
@@ -1,4 +1,4 @@
-// Copyright 2016 The Cobalt Authors. All Rights Reserved.
+// Copyright 2018 The Cobalt Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 #include "starboard/log.h"
 #include "starboard/shared/starboard/player/player_internal.h"
+#include "starboard/shared/x11/application_x11.h"
 
 void SbPlayerSetBounds(SbPlayer player,
                        int z_index,
@@ -28,4 +29,6 @@ void SbPlayerSetBounds(SbPlayer player,
     return;
   }
   player->SetBounds(z_index, x, y, width, height);
+  starboard::shared::x11::ApplicationX11::Get()->PlayerSetBounds(
+      player, z_index, x, y, width, height);
 }
