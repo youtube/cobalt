e647126d5993117336482d307743abddb30323d6
Cobalt Team <cobalt-team@google.com>
2018-07-13 13:45:58 -0700

    Starboardize libjpeg-turbo for platform compatability
    Change-Id: I4f657e8773f9c1c3a0a72ca8d16d1c83e1890718

--- third_party/libjpeg-turbo/jmemnobs.c
+++ third_party/libjpeg-turbo/jmemnobs.c
@@ -22,9 +22,13 @@
 #include "jpeglib.h"
 #include "jmemsys.h"            /* import the system-dependent declarations */
 
+#if defined(NEED_STARBOARD_MEMORY)
+#include "starboard/client_porting/poem/stdio_poem.h"
+#else
 #ifndef HAVE_STDLIB_H           /* <stdlib.h> should declare malloc(),free() */
 extern void *malloc(size_t size);
 extern void free(void *ptr);
+#endif
 #endif
 
 
