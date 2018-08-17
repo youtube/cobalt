a020e0182260c651fd3093cde5feabad5d7d376b
Cobalt Team <cobalt-team@google.com>
2018-08-17 15:22:08 -0700

    Make SbStorageWriteRecord atomic
    For posix and win32 platforms, write to a temporary file first and
    replace old file with temporary file to prevent storage file corruption
    if a crash happens when writing storage.
    
    b/78941867
    
    Change-Id: Idfc103061e43ce886fdc905018b0d30a689dde27

---
+++ starboard/shared/win32/storage_write_record.cc
@@ -0,0 +1,98 @@
+// Copyright 2018 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/storage.h"
+
+#include <windows.h>
+
+#include <algorithm>
+
+#include "starboard/file.h"
+#include "starboard/log.h"
+#include "starboard/shared/starboard/file_storage/storage_internal.h"
+#include "starboard/shared/win32/file_internal.h"
+#include "starboard/shared/win32/wchar_utils.h"
+#include "starboard/string.h"
+
+const char kTempFileSuffix[] = ".temp";
+
+bool SbStorageWriteRecord(SbStorageRecord record,
+                          const char* data,
+                          int64_t data_size) {
+  if (!SbStorageIsValidRecord(record) || !data || data_size < 0) {
+    return false;
+  }
+
+#if SB_API_VERSION >= 6
+  const char* name = record->name.c_str();
+#else
+  const char* name = NULL;
+#endif
+  char original_file_path[SB_FILE_MAX_PATH];
+  if (!starboard::shared::starboard::GetUserStorageFilePath(
+          record->user, name, original_file_path, SB_FILE_MAX_PATH)) {
+    return false;
+  }
+  char temp_file_path[SB_FILE_MAX_PATH];
+  SbStringCopy(temp_file_path, original_file_path, SB_FILE_MAX_PATH);
+  SbStringConcat(temp_file_path, kTempFileSuffix, SB_FILE_MAX_PATH);
+
+  SbFileError error;
+  SbFile temp_file = SbFileOpen(
+      temp_file_path, kSbFileCreateAlways | kSbFileWrite | kSbFileRead, NULL,
+      &error);
+  if (error != kSbFileOk) {
+    return false;
+  }
+
+  SbFileTruncate(temp_file, 0);
+
+  const char* source = data;
+  int64_t to_write = data_size;
+  while (to_write > 0) {
+    int to_write_max =
+        static_cast<int>(std::min(to_write, static_cast<int64_t>(kSbInt32Max)));
+    int bytes_written = SbFileWrite(temp_file, source, to_write_max);
+    if (bytes_written < 0) {
+      SbFileClose(temp_file);
+      SbFileDelete(temp_file_path);
+      return false;
+    }
+
+    source += bytes_written;
+    to_write -= bytes_written;
+  }
+
+  SbFileFlush(temp_file);
+
+  if (!SbFileClose(record->file) || !SbFileClose(temp_file)) {
+    return false;
+  }
+
+  std::wstring original_path_wstring =
+      starboard::shared::win32::NormalizeWin32Path(
+          starboard::shared::win32::CStringToWString(original_file_path));
+  std::wstring temp_path_wstring = starboard::shared::win32::NormalizeWin32Path(
+      starboard::shared::win32::CStringToWString(temp_file_path));
+  if (ReplaceFileW(original_path_wstring.c_str(), temp_path_wstring.c_str(),
+                   NULL, 0, NULL, NULL) == 0) {
+    return false;
+  }
+  SbFile new_record_file =
+      SbFileOpen(original_file_path,
+                 kSbFileOpenOnly | kSbFileWrite | kSbFileRead, NULL, NULL);
+  record->file = new_record_file;
+
+  return true;
+}
