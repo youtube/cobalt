a96f56c5d652eb561328f03e1dc1552d5874f5e0
Cobalt Team <cobalt-team@google.com>
2018-08-20 15:10:03 -0700

    Initialize event |timestamp| value even if platform doesn't check.
    This makes things it easier for linux-based partners to "flip the
    switch" on event timestamps.
    
    Contributed by Samsung in:
    https://cobalt-review.googlesource.com/c/cobalt/+/4831
    
    Change-Id: I043e0bc1cc9ce6583aaf9bb89e06eae92b9b1e7d

--- starboard/shared/x11/application_x11.cc
+++ starboard/shared/x11/application_x11.cc
@@ -1119,6 +1119,9 @@ shared::starboard::Application::Event* ApplicationX11::GetPendingEvent() {
 
   scoped_ptr<SbInputData> data(new SbInputData());
   SbMemorySet(data.get(), 0, sizeof(*data));
+#if SB_API_VERSION >= 10
+  data->timestamp = SbTimeGetMonotonicNow();
+#endif  // SB_API_VERSION >= 10
   data->window = windows_[0];
   SB_DCHECK(SbWindowIsValid(data->window));
   data->type = paste_buffer_key_release_pending_ ? kSbInputEventTypeUnpress
@@ -1184,6 +1187,9 @@ shared::starboard::Application::Event* ApplicationX11::XEventToEvent(
 
       scoped_ptr<SbInputData> data(new SbInputData());
       SbMemorySet(data.get(), 0, sizeof(*data));
+#if SB_API_VERSION >= 10
+      data->timestamp = SbTimeGetMonotonicNow();
+#endif  // SB_API_VERSION >= 10
       data->window = FindWindow(x_key_event->window);
       SB_DCHECK(SbWindowIsValid(data->window));
       data->type = x_event->type == KeyPress ? kSbInputEventTypePress
@@ -1211,6 +1217,9 @@ shared::starboard::Application::Event* ApplicationX11::XEventToEvent(
 #endif
       scoped_ptr<SbInputData> data(new SbInputData());
       SbMemorySet(data.get(), 0, sizeof(*data));
+#if SB_API_VERSION >= 10
+      data->timestamp = SbTimeGetMonotonicNow();
+#endif  // SB_API_VERSION >= 10
       data->window = FindWindow(x_button_event->window);
       SB_DCHECK(SbWindowIsValid(data->window));
       data->key = XButtonEventToSbKey(x_button_event);
@@ -1241,6 +1250,9 @@ shared::starboard::Application::Event* ApplicationX11::XEventToEvent(
       XMotionEvent* x_motion_event = reinterpret_cast<XMotionEvent*>(x_event);
       scoped_ptr<SbInputData> data(new SbInputData());
       SbMemorySet(data.get(), 0, sizeof(*data));
+#if SB_API_VERSION >= 10
+      data->timestamp = SbTimeGetMonotonicNow();
+#endif  // SB_API_VERSION >= 10
       data->window = FindWindow(x_motion_event->window);
       SB_DCHECK(SbWindowIsValid(data->window));
 #if SB_API_VERSION >= 6
