f410cf9b129a75184f0483591285a9af65539fbb
Cobalt Team <cobalt-team@google.com>
2018-09-11 16:30:56 -0700

    Rename "channel" to just "command"
    The concept of a "channel" is confusing since it's not really a
    broadcast to multiple listeners at all. Instead a "command" is just the
    name of the command as entered in the debug console, and it's
    implemented by a single handler.
    
    Cleanup of console commands, follow-on to b/31266139
    
    Change-Id: I776ad493ff4cedbebb9eec212dfeadc01655bb7b

--- cobalt/base/console_commands.h
+++ cobalt/base/console_commands.h
@@ -29,15 +29,14 @@ namespace base {
 // triggered by the debug console.
 //
 // Any component can create a handler object of the CommandHandler class to
-// listen for commands on a particular channel. A CommandHandler object
-// automatically registers itself with the ConsoleCommandManager singleton on
-// construction and unregisters itself on destruction.
+// listen for commands. A CommandHandler object automatically registers itself
+// with the ConsoleCommandManager singleton on construction and unregisters
+// itself on destruction.
 //
-// Each CommandHandler object specifies a callback and a help string. When the
-// ConsoleCommandManager receives a command broadcast on a particular channel,
-// it runs the callback of the CommandHandler object registered to that
-// channel. The help string is used within the debug console to document the
-// channels that have been registered to listen for commands.
+// Each CommandHandler object specifies a callback and a help string. The
+// ConsoleCommandManager handles a particular command by running the callback of
+// the CommandHandler object registered for that command. The help string is
+// used within the debug console to document the registered commands.
 
 class ConsoleCommandManager {
  public:
@@ -47,17 +46,17 @@ class ConsoleCommandManager {
   // Command handler that registers itself with this object.
   class CommandHandler {
    public:
-    CommandHandler(const std::string& channel, const CommandCallback& callback,
+    CommandHandler(const std::string& command, const CommandCallback& callback,
                    const std::string& short_help, const std::string& long_help);
     ~CommandHandler();
 
-    const std::string& channel() const { return channel_; }
+    const std::string& command() const { return command_; }
     const CommandCallback& callback() const { return callback_; }
     const std::string& short_help() const { return short_help_; }
     const std::string& long_help() const { return long_help_; }
 
    private:
-    std::string channel_;
+    std::string command_;
     CommandCallback callback_;
     std::string short_help_;
     std::string long_help_;
@@ -69,16 +68,16 @@ class ConsoleCommandManager {
   static ConsoleCommandManager* GetInstance();
 
   // Handles a command by posting the message to the handler registered for
-  // the specified channel, if any.
-  void HandleCommand(const std::string& channel,
+  // the specified command, if any.
+  void HandleCommand(const std::string& command,
                      const std::string& message) const;
 
-  // Returns a set of all the currently registered channels.
-  std::set<std::string> GetRegisteredChannels() const;
+  // Returns a set of all the currently registered commands.
+  std::set<std::string> GetRegisteredCommands() const;
 
-  // Returns the help strings for a channel.
-  std::string GetShortHelp(const std::string& channel) const;
-  std::string GetLongHelp(const std::string& channel) const;
+  // Returns the help strings for a command.
+  std::string GetShortHelp(const std::string& command) const;
+  std::string GetLongHelp(const std::string& command) const;
 
  private:
   // Class can only be instanced via the singleton
@@ -97,8 +96,8 @@ class ConsoleCommandManager {
 
   mutable base::Lock lock_;
 
-  // Map of command handlers, one for each channel.
-  CommandHandlerMap command_channel_map_;
+  // Map of command handlers, one for each command.
+  CommandHandlerMap command_command_map_;
 #endif  // ENABLE_DEBUG_CONSOLE
 };
 
