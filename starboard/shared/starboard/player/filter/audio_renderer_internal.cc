59ee32052f0f71810fb625b9f17d2fdbfce776c3
Xiaoming Shi <xiaomings@google.com>
2018-09-13 17:26:36 -0700

    Map mono audio to stereo before playing
    The AudioOut on Switch doesn't support mono audio.
    
    b/115634947
    
    Change-Id: I6e753b399a825e5450262e800227f184c1c35bf2

--- starboard/shared/starboard/player/filter/audio_renderer_internal.cc
+++ starboard/shared/starboard/player/filter/audio_renderer_internal.cc
@@ -182,11 +182,14 @@ bool AudioRenderer::IsSeekingInProgress() const {
 void AudioRenderer::Initialize(const ErrorCB& error_cb,
                                const PrerolledCB& prerolled_cb,
                                const EndedCB& ended_cb) {
+  SB_DCHECK(error_cb);
   SB_DCHECK(prerolled_cb);
   SB_DCHECK(ended_cb);
+  SB_DCHECK(!error_cb_);
   SB_DCHECK(!prerolled_cb_);
   SB_DCHECK(!ended_cb_);
 
+  error_cb_ = error_cb;
   prerolled_cb_ = prerolled_cb;
   ended_cb_ = ended_cb;
 
@@ -509,7 +512,13 @@ void AudioRenderer::OnFirstOutput() {
       kSbMediaAudioFrameStorageTypeInterleaved,
       reinterpret_cast<SbAudioSinkFrameBuffers>(frame_buffers_),
       max_cached_frames_, this);
-  SB_DCHECK(audio_renderer_sink_->HasStarted());
+  if (!audio_renderer_sink_->HasStarted()) {
+#if SB_HAS(PLAYER_ERROR_MESSAGE)
+    error_cb_(kSbPlayerErrorDecode, "failed to start audio sink");
+#else   // SB_HAS(PLAYER_ERROR_MESSAGE)
+    error_cb_();
+#endif  // SB_HAS(PLAYER_ERROR_MESSAGE)
+  }
 }
 
 void AudioRenderer::LogFramesConsumed() {
