2ec624f69fd3ca02076dbac634cca9e3b26e2d9d
Cobalt Team <cobalt-team@google.com>
2018-09-21 14:58:58 -0700

    Handle mouse events in the debug console when it's visible.
    Following the same pattern as filtering key events, also filter pointer
    and wheel events for the debug console.
    
    - Don't allow clicking through the console to the page behind
    - Make the mouse wheel scroll the debug console
    
    Change-Id: I06840d6dedea2fea3202ffc11095bc567efe3b9d

--- cobalt/browser/browser_module.cc
+++ cobalt/browser/browser_module.cc
@@ -1045,6 +1045,14 @@ void BrowserModule::OnPointerEventProduced(base::Token type,
   }
 
 #if defined(ENABLE_DEBUG_CONSOLE)
+  // If the debug console is fully visible, it gets the next chance to handle
+  // pointer events.
+  if (debug_console_->GetMode() >= debug::DebugHub::kDebugConsoleOn) {
+    if (!debug_console_->FilterPointerEvent(type, event)) {
+      return;
+    }
+  }
+
   trace_manager_.OnInputEventProduced();
 #endif  // defined(ENABLE_DEBUG_CONSOLE)
 
@@ -1063,6 +1071,14 @@ void BrowserModule::OnWheelEventProduced(base::Token type,
   }
 
 #if defined(ENABLE_DEBUG_CONSOLE)
+  // If the debug console is fully visible, it gets the next chance to handle
+  // wheel events.
+  if (debug_console_->GetMode() >= debug::DebugHub::kDebugConsoleOn) {
+    if (!debug_console_->FilterWheelEvent(type, event)) {
+      return;
+    }
+  }
+
   trace_manager_.OnInputEventProduced();
 #endif  // defined(ENABLE_DEBUG_CONSOLE)
 
