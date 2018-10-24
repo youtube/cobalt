2b9e6713e310373b7b4c343cf9d3bf41888a846a
Cobalt Team <cobalt-team@google.com>
2018-10-24 13:49:16 -0700

    Disable runtime v8 snapshot reading/writing on trunk
    With the ability to generate v8 snapshot at build time, we would
    like to disable runtime snapshot reading/writing. But we will
    keep the ability to do this for our partners by hiding the old
    feature behind v8_buildtime_snapshot gyp variable.
    
    b/111220585
    
    Change-Id: Ibb7a7410b4529c213a809f4b7e5e02e10e72f045

--- cobalt/script/v8c/isolate_fellowship.cc
+++ cobalt/script/v8c/isolate_fellowship.cc
@@ -53,7 +53,7 @@ void V8FlagsInit() {
   }
 }
 
-} // namespace
+}  // namespace
 
 IsolateFellowship::IsolateFellowship() {
   TRACE_EVENT0("cobalt::script", "IsolateFellowship::IsolateFellowship");
@@ -68,7 +68,9 @@ IsolateFellowship::IsolateFellowship() {
   // to write the snapshot data. We need to make sure all global command line
   // flags are set before that.
   V8FlagsInit();
+#if !defined(COBALT_V8_BUILDTIME_SNAPSHOT)
   InitializeStartupData();
+#endif  // !defined(COBALT_V8_BUILDTIME_SNAPSHOT)
 }
 
 IsolateFellowship::~IsolateFellowship() {
@@ -80,14 +82,17 @@ IsolateFellowship::~IsolateFellowship() {
   delete array_buffer_allocator;
   array_buffer_allocator = nullptr;
 
+#if !defined(COBALT_V8_BUILDTIME_SNAPSHOT)
   // Note that both us and V8 will have created this with new[], see
   // "snapshot-common.cc".  Also note that both startup data creation failure
   // from V8 is possible, and deleting a null pointer is safe, so there is no
   // DCHECK here.
   delete[] startup_data.data;
   startup_data = {nullptr, 0};
+#endif
 }
 
+#if !defined(COBALT_V8_BUILDTIME_SNAPSHOT)
 void IsolateFellowship::InitializeStartupData() {
   TRACE_EVENT0("cobalt::script", "IsolateFellowship::InitializeStartupData");
   DCHECK(startup_data.data == nullptr);
@@ -223,6 +228,7 @@ void IsolateFellowship::InitializeStartupData() {
 
   maybe_remove_cache_file();
 }
+#endif  // !defined(COBALT_V8_BUILDTIME_SNAPSHOT)
 
 }  // namespace v8c
 }  // namespace script
