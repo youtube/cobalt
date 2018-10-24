c192ef9afdeaa86c7248b356ad2f5412ed09166a
Cobalt Team <cobalt-team@google.com>
2018-10-24 13:49:16 -0700

    Disable runtime v8 snapshot reading/writing on trunk
    With the ability to generate v8 snapshot at build time, we would
    like to disable runtime snapshot reading/writing. But we will
    keep the ability to do this for our partners by hiding the old
    feature behind v8_buildtime_snapshot gyp variable.
    
    b/111220585
    
    Change-Id: Ibb7a7410b4529c213a809f4b7e5e02e10e72f045

--- cobalt/script/v8c/isolate_fellowship.h
+++ cobalt/script/v8c/isolate_fellowship.h
@@ -18,8 +18,8 @@
 #include "base/memory/singleton.h"
 #include "cobalt/script/v8c/cobalt_platform.h"
 #include "v8/include/libplatform/libplatform.h"
-#include "v8/include/v8-platform.h"
 #include "v8/include/v8.h"
+#include "v8/include/v8-platform.h"
 
 namespace cobalt {
 namespace script {
@@ -28,7 +28,8 @@ namespace v8c {
 // A helper singleton to hold global state required by all V8 isolates across
 // the entire process (the fellowship).  Initialization logic for this data is
 // also handled here, most notably, the possible reading/writing of
-// |startup_data| from/to a cache file.
+// |startup_data| from/to a cache file if snapshot has not been generated at
+// build time.
 struct IsolateFellowship {
  public:
   static IsolateFellowship* GetInstance() {
@@ -38,7 +39,9 @@ struct IsolateFellowship {
 
   scoped_refptr<CobaltPlatform> platform = nullptr;
   v8::ArrayBuffer::Allocator* array_buffer_allocator = nullptr;
+#if !defined(COBALT_V8_BUILDTIME_SNAPSHOT)
   v8::StartupData startup_data = {nullptr, 0};
+#endif  // !defined(COBALT_V8_BUILDTIME_SNAPSHOT)
 
   friend struct StaticMemorySingletonTraits<IsolateFellowship>;
 
@@ -46,11 +49,13 @@ struct IsolateFellowship {
   IsolateFellowship();
   ~IsolateFellowship();
 
+#if !defined(COBALT_V8_BUILDTIME_SNAPSHOT)
   // Initialize |startup_data| by either reading it from a cache file or
   // creating it.  If creating it for the first time, then when appropriate
   // (i.e. the platform has a suitable directory) attempt to write it to a cache
   // file.
   void InitializeStartupData();
+#endif  // !defined(COBALT_V8_BUILDTIME_SNAPSHOT)
 };
 
 }  // namespace v8c
