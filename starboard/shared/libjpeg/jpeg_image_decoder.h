5b2346e06d6c5975a1bdf1d2e632057210f6dfe2
Xiaoming Shi <xiaomings@google.com>
2018-10-30 12:10:41 -0700

    Support SbImageDecode() based Jpeg decoder
    Note that they are not fully implemented and not included in build yet.
    The jpeg_image_decoder.* files are copied from cobalt/loader/image with
    the following modifications:
    1. The copyright year is modified.
    2. Include guard macro updated according to the new path.
    3. #include path updated.
    4. "git cl format" run.
    These files serve as the base for further modification.
    
    b/118690966
    
    Change-Id: Ia988874f04d70b5e1290e021ef4594291e985b71

---
+++ starboard/shared/libjpeg/jpeg_image_decoder.h
@@ -0,0 +1,60 @@
+// Copyright 2018 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef STARBOARD_SHARED_LIBJPEG_JPEG_IMAGE_DECODER_H_
+#define STARBOARD_SHARED_LIBJPEG_JPEG_IMAGE_DECODER_H_
+
+#include <setjmp.h>
+#include <string>
+
+#include "base/callback.h"
+#include "base/memory/scoped_ptr.h"
+#include "cobalt/loader/image/image_data_decoder.h"
+
+// Inhibit C++ name-mangling for libjpeg functions.
+extern "C" {
+#include "third_party/libjpeg/jpeglib.h"
+}
+
+namespace cobalt {
+namespace loader {
+namespace image {
+
+class JPEGImageDecoder : public ImageDataDecoder {
+ public:
+  explicit JPEGImageDecoder(render_tree::ResourceProvider* resource_provider);
+  ~JPEGImageDecoder() override;
+
+  // From ImageDataDecoder
+  std::string GetTypeString() const override { return "JPEGImageDecoder"; }
+
+ private:
+  // From ImageDataDecoder
+  size_t DecodeChunkInternal(const uint8* data, size_t size) override;
+
+  bool ReadHeader();
+  bool StartDecompress();
+  bool DecodeProgressiveJPEG();
+  bool ReadLines();
+
+  jpeg_decompress_struct info_;
+  jpeg_source_mgr source_manager_;
+  jpeg_error_mgr error_manager_;
+};
+
+}  // namespace image
+}  // namespace loader
+}  // namespace cobalt
+
+#endif  // STARBOARD_SHARED_LIBJPEG_JPEG_IMAGE_DECODER_H_
