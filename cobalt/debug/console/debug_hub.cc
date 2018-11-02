692e4f4633bceb6e0a7e3bd22041c3cc9510a2e9
Cobalt Team <cobalt-team@google.com>
2018-11-01 17:57:51 -0700

    Collapse Debugger web object into DebugHub
    b/31266139
    
    Change-Id: Ic9bcc77303869eb1d45a4c528f0e2c55d083dddd

--- cobalt/debug/console/debug_hub.cc
+++ cobalt/debug/console/debug_hub.cc
@@ -18,8 +18,11 @@
 
 #include <set>
 
-#include "base/compiler_specific.h"
+#include "base/json/json_writer.h"
+#include "base/values.h"
 #include "cobalt/base/c_val.h"
+#include "cobalt/base/console_commands.h"
+#include "cobalt/debug/json_object.h"
 
 namespace cobalt {
 namespace debug {
@@ -29,16 +32,10 @@ DebugHub::DebugHub(
     const GetHudModeCallback& get_hud_mode_callback,
     const CreateDebugClientCallback& create_debug_client_callback)
     : get_hud_mode_callback_(get_hud_mode_callback),
-      debugger_(new Debugger(create_debug_client_callback)) {}
+      create_debug_client_callback_(create_debug_client_callback),
+      on_event_(new DebuggerEventTarget()) {}
 
-DebugHub::~DebugHub() {
-  base::LogMessageHandler::GetInstance()->RemoveCallback(
-      log_message_handler_callback_id_);
-}
-
-void DebugHub::TraceMembers(script::Tracer* tracer) {
-  tracer->Trace(debugger_);
-}
+DebugHub::~DebugHub() {}
 
 // TODO: This function should be modified to return an array of strings instead
 // of a single space-separated string, once the bindings support return of a
@@ -79,6 +76,112 @@ std::string DebugHub::GetConsoleValue(const std::string& name) const {
 
 int DebugHub::GetDebugConsoleMode() const {
   return get_hud_mode_callback_.Run();
+}
+
+void DebugHub::Attach(const AttachCallbackArg& callback) {
+  last_error_ = base::nullopt;
+  debug_client_ = create_debug_client_callback_.Run(this);
+
+  // |debug_client_| may be NULL if the WebModule is not available at this time.
+  if (!debug_client_) {
+    DLOG(WARNING) << "Debug dispatcher unavailable.";
+    last_error_ = "Debug dispatcher unavailable.";
+  }
+
+  AttachCallbackArg::Reference callback_reference(this, callback);
+  callback_reference.value().Run();
+}
+
+void DebugHub::Detach(const AttachCallbackArg& callback) {
+  last_error_ = base::nullopt;
+  debug_client_.reset();
+  AttachCallbackArg::Reference callback_reference(this, callback);
+  callback_reference.value().Run();
+}
+
+void DebugHub::SendCommand(const std::string& method,
+                           const std::string& json_params,
+                           const ResponseCallbackArg& callback) {
+  last_error_ = base::nullopt;
+  if (!debug_client_ || !debug_client_->IsAttached()) {
+    scoped_ptr<base::DictionaryValue> response(new base::DictionaryValue);
+    response->SetString("error.message",
+                        "Debugger is not connected - call attach first.");
+    std::string json_response;
+    base::JSONWriter::Write(response.get(), &json_response);
+    ResponseCallbackArg::Reference callback_ref(this, callback);
+    callback_ref.value().Run(json_response);
+    return;
+  }
+
+  scoped_refptr<ResponseCallbackInfo> callback_info(
+      new ResponseCallbackInfo(this, callback));
+  debug_client_->SendCommand(
+      method, json_params,
+      base::Bind(&DebugHub::OnCommandResponse, this, callback_info));
+}
+
+const script::Sequence<ConsoleCommand> DebugHub::console_commands() const {
+  script::Sequence<ConsoleCommand> result;
+  base::ConsoleCommandManager* command_mananger =
+      base::ConsoleCommandManager::GetInstance();
+  DCHECK(command_mananger);
+  if (command_mananger) {
+    std::set<std::string> commands = command_mananger->GetRegisteredCommands();
+    for (std::set<std::string>::const_iterator it = commands.begin();
+         it != commands.end(); ++it) {
+      ConsoleCommand console_command;
+      console_command.set_command(*it);
+      console_command.set_short_help(command_mananger->GetShortHelp(*it));
+      console_command.set_long_help(command_mananger->GetLongHelp(*it));
+      result.push_back(console_command);
+    }
+  }
+  return result;
+}
+
+void DebugHub::SendConsoleCommand(const std::string& command,
+                                  const std::string& message) {
+  base::ConsoleCommandManager* console_command_manager =
+      base::ConsoleCommandManager::GetInstance();
+  DCHECK(console_command_manager);
+  console_command_manager->HandleCommand(command, message);
+}
+
+void DebugHub::TraceMembers(script::Tracer* tracer) {
+  tracer->Trace(on_event_);
+}
+
+void DebugHub::OnCommandResponse(
+    const scoped_refptr<ResponseCallbackInfo>& callback_info,
+    const base::optional<std::string>& response) const {
+  // Run the script callback on the message loop the command was sent from.
+  callback_info->message_loop_proxy->PostTask(
+      FROM_HERE, base::Bind(&DebugHub::RunResponseCallback, this, callback_info,
+                            response));
+}
+
+void DebugHub::OnDebugClientEvent(const std::string& method,
+                                  const base::optional<std::string>& params) {
+  // Pass to the onEvent handler. The handler will notify the JavaScript
+  // listener on the message loop the listener was registered on.
+  on_event_->DispatchEvent(method, params);
+}
+
+void DebugHub::OnDebugClientDetach(const std::string& reason) {
+  DLOG(INFO) << "Debugger detached: " + reason;
+  const std::string method = "Inspector.detached";
+  JSONObject params(new base::DictionaryValue());
+  params->SetString("reason", reason);
+  on_event_->DispatchEvent(method, JSONStringify(params));
+}
+
+void DebugHub::RunResponseCallback(
+    const scoped_refptr<ResponseCallbackInfo>& callback_info,
+    base::optional<std::string> response) const {
+  DCHECK_EQ(base::MessageLoopProxy::current(),
+            callback_info->message_loop_proxy);
+  callback_info->callback.value().Run(response);
 }
 
 }  // namespace console
--- cobalt/debug/console/debug_hub.h
+++ cobalt/debug/console/debug_hub.h
@@ -20,12 +20,16 @@
 #include <string>
 
 #include "base/callback.h"
-#include "base/memory/weak_ptr.h"
-#include "base/message_loop.h"
-#include "cobalt/base/log_message_handler.h"
-#include "cobalt/debug/console/debugger.h"
+#include "base/memory/ref_counted.h"
+#include "base/memory/scoped_ptr.h"
+#include "base/message_loop_proxy.h"
+#include "base/optional.h"
+#include "cobalt/debug/console/console_command.h"
+#include "cobalt/debug/console/debugger_event_target.h"
+#include "cobalt/debug/debug_client.h"
 #include "cobalt/script/callback_function.h"
 #include "cobalt/script/script_value.h"
+#include "cobalt/script/value_handle.h"
 #include "cobalt/script/wrappable.h"
 
 namespace cobalt {
@@ -33,29 +37,24 @@ namespace debug {
 namespace console {
 
 // This class implements an interface to JavaScript for debugging.
-// The public methods of this class are expected to be exposed in DebugHub.idl.
 // The main (and typically only) JavaScript client is the debug console.
-// A stub implementation is used if ENABLE_DEBUG_CONSOLE is not defined.
 //
-// The JavaScript client can examine the available list of registered console
-// values.
-//
-// There is a debugger client available via the |debugger_| member that can
-// connect to the debug dispatcher attached to the main web module. The debug
-// console uses this to send debugging commands and receive debugging events.
-// The debug dispatcher is accessed using the |get_debug_dispatcher_callback|
-// parameter specified in the constructor.
-//
-// The JavaScript client can send arbitrary messages to an object of this class
-// on a specific named channel using the SendCommand method. These messages are
-// forwarded to any component of the app that registers a message handler for
-// that channel by creating an instance of a class derived from CommandHandler.
-
-class DebugHub : public script::Wrappable {
+// This is modeled after chrome.debugger extension API.
+// https://developer.chrome.com/extensions/debugger
+class DebugHub : public script::Wrappable, public DebugClient::Delegate {
  public:
   // Function signature to call when we need to query for the Hud visibility
   // mode.
   typedef base::Callback<int()> GetHudModeCallback;
+
+  // JavaScript callback to be run when debugger attaches/detaches.
+  typedef script::CallbackFunction<void()> AttachCallback;
+  typedef script::ScriptValue<AttachCallback> AttachCallbackArg;
+
+  // JavaScript callback to receive the response after executing a command.
+  typedef script::CallbackFunction<void(base::optional<std::string>)>
+      ResponseCallback;
+  typedef script::ScriptValue<ResponseCallback> ResponseCallbackArg;
 
   // Debug console visibility modes.
   static const int kDebugConsoleOff = 0;
@@ -63,12 +62,23 @@ class DebugHub : public script::Wrappable {
   static const int kDebugConsoleOn = 2;
   static const int kDebugConsoleNumModes = kDebugConsoleOn + 1;
 
+  // Thread-safe ref-counted struct used to pass asynchronously executed
+  // response callbacks around. Stores the message loop the callback must be
+  // executed on as well as the callback itself.
+  struct ResponseCallbackInfo
+      : public base::RefCountedThreadSafe<ResponseCallbackInfo> {
+    ResponseCallbackInfo(DebugHub* const debugger,
+                         const ResponseCallbackArg& cb)
+        : callback(debugger, cb),
+          message_loop_proxy(base::MessageLoopProxy::current()) {}
+    ResponseCallbackArg::Reference callback;
+    scoped_refptr<base::MessageLoopProxy> message_loop_proxy;
+    friend class base::RefCountedThreadSafe<ResponseCallbackInfo>;
+  };
+
   DebugHub(const GetHudModeCallback& get_hud_mode_callback,
            const CreateDebugClientCallback& create_debug_client_callback);
   ~DebugHub();
-
-  // Gets the JavaScript debugger client interface.
-  const scoped_refptr<Debugger>& debugger() { return debugger_; }
 
   // Gets the collection of available CVal names as an alphabetically ordered,
   // space-separated list.
@@ -79,18 +89,70 @@ class DebugHub : public script::Wrappable {
 
   int GetDebugConsoleMode() const;
 
+  void Attach(const AttachCallbackArg& callback);
+  void Detach(const AttachCallbackArg& callback);
+
+  // Sends a devtools protocol command to be executed in the context of the main
+  // WebModule that is being debugged.
+  void SendCommand(const std::string& method, const std::string& json_params,
+                   const ResponseCallbackArg& callback);
+
+  const base::optional<std::string>& last_error() const { return last_error_; }
+  const scoped_refptr<DebuggerEventTarget>& on_event() const {
+    return on_event_;
+  }
+
+  const script::Sequence<ConsoleCommand> console_commands() const;
+
+  // Sends a console command to be handled in the context of the debug WebModule
+  // by a registered hander. This lets the JavaScript debug console trigger
+  // actions in the app.
+  void SendConsoleCommand(const std::string& command,
+                          const std::string& message);
+
   DEFINE_WRAPPABLE_TYPE(DebugHub);
   void TraceMembers(script::Tracer* tracer) override;
 
+ protected:
+  // Called by the debug dispatcher with the response of a command on the
+  // message loop the command was sent from (the message loop of this object).
+  // Passes the response to the JavaScript callback registered with the command.
+  void OnCommandResponse(
+      const scoped_refptr<ResponseCallbackInfo>& callback_info,
+      const base::optional<std::string>& response) const;
+
+  // DebugClient::Delegate implementation.
+  void OnDebugClientEvent(
+      const std::string& method,
+      const base::optional<std::string>& json_params) override;
+  void OnDebugClientDetach(const std::string& reason) override;
+
  private:
-  // A function that we can call to query the Hud visibility mode.
+  // Runs a script response callback with the specified response.
+  // Should be called from the same message loop as the script command that it
+  // is a response for.
+  void RunResponseCallback(
+      const scoped_refptr<ResponseCallbackInfo>& callback_info,
+      base::optional<std::string> response) const;
+
+  // A function to query the Hud visibility mode.
   const GetHudModeCallback get_hud_mode_callback_;
 
-  // The JavaScript callback to forward log messages to.
-  base::LogMessageHandler::CallbackId log_message_handler_callback_id_;
+  // A factory function to create a debug client.
+  const CreateDebugClientCallback create_debug_client_callback_;
 
-  // Interface to the JavaScript debugger client.
-  scoped_refptr<Debugger> debugger_;
+  // Handler for debugger events.
+  scoped_refptr<DebuggerEventTarget> on_event_;
+
+  // Debug client that connects to the dispatcher.
+  scoped_ptr<DebugClient> debug_client_;
+
+  // This will be defined if there was an error since the last operation.
+  // TODO: Chrome implements similar functionality throughout the
+  // app using the chrome.runtime.lastError object. When we add support for
+  // Cobalt's equivalent to the runtime extension API, we may wish to consider
+  // using that and removing this attribute.
+  base::optional<std::string> last_error_;
 };
 
 }  // namespace console
