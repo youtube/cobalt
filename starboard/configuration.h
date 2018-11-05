8da9307230ff2520bb83d84e95769276fbc2597c
Cobalt Team <cobalt-team@google.com>
2018-11-05 10:42:17 -0800

    SbWindowGetDiagonialSizeInInches -> SbWindowGetDiagonalSizeInInches
    Fixes typo.
    
    Change-Id: I54a1db9764fd86943453d80a8fcaeda343892565

--- starboard/configuration.h
+++ starboard/configuration.h
@@ -91,7 +91,7 @@
 #define SB_HAS_AC3_AUDIO_API_VERSION SB_EXPERIMENTAL_API_VERSION
 
 // Adds support for allowing the platform to override its screen diagonal
-// length via SbWindowGetDiagonialSizeInInches().
+// length via SbWindowGetDiagonalSizeInInches().
 #define SB_HAS_SCREEN_DIAGONAL_API_VERSION SB_EXPERIMENTAL_API_VERSION
 
 // --- Release Candidate Feature Defines -------------------------------------
