08efacb33ce9e65e22eea1749f0a21e056da04c5
Cobalt Team <cobalt-team@google.com>
2018-11-26 15:43:25 -0800

    Add offline messages to embedded resources.
    This adds support for 'template' files with localized strings for the
    embedded resources loader.
    
    This modifies base::LocalizedStrings to load csv files instead of xlb files
    and removes the obsolete gyp target for copying the xlb files.
    
    This adds application update message translations (from existing
    translations).
    
    Using the above, this adds a localized 'application update required' dialog
    page that can be navigated to off-line by navigating to
    h5vcc-embedded://update_message.html.template
    
    b/118907637
    b/120437108
    
    Change-Id: I73750ddc014a4f57c2b81e3ee8ee5b957df5ca0a

--- cobalt/loader/embedded_fetcher.cc
+++ cobalt/loader/embedded_fetcher.cc
@@ -15,11 +15,15 @@
 #include "cobalt/loader/embedded_fetcher.h"
 
 #include <algorithm>
+#include <vector>
 
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/message_loop.h"
+#include "base/string_split.h"
+#include "base/string_util.h"
 #include "base/stringprintf.h"
+#include "cobalt/base/localized_strings.h"
 #include "cobalt/loader/embedded_resources.h"  // Generated.
 
 namespace cobalt {
@@ -86,9 +90,19 @@ void EmbeddedFetcher::GetEmbeddedData(const std::string& key,
   const char* data = reinterpret_cast<const char*>(file_contents.data);
   size_t size = static_cast<size_t>(file_contents.size);
   data += start_offset;
-  size = std::min(size, static_cast<size_t>(bytes_to_read));
 
-  handler()->OnReceived(this, data, size);
+  // If the key is a template file, localized the strings in the file data.
+  if (EndsWith(key, ".template", true)) {
+    std::string output_file(data);
+    LocalizeFileData(&output_file);
+    const char* final_data = output_file.c_str();
+    size = std::min(static_cast<size_t>(output_file.size()),
+                    static_cast<size_t>(bytes_to_read));
+    handler()->OnReceived(this, final_data, size);
+  } else {
+    size = std::min(size, static_cast<size_t>(bytes_to_read));
+    handler()->OnReceived(this, data, size);
+  }
   handler()->OnDone(this);
 }
 
@@ -100,6 +114,32 @@ bool EmbeddedFetcher::IsAllowedByCsp() {
   } else {
     return false;
   }
+}
+
+void EmbeddedFetcher::LocalizeFileData(std::string* output_file) {
+  base::LocalizedStrings* localized_strings =
+      base::LocalizedStrings::GetInstance();
+  std::vector<std::string> parts;
+  std::string opening_delimiter = "[[";
+  base::SplitStringUsingSubstr(*output_file, opening_delimiter, &parts);
+  for (size_t idx = 1; idx < parts.size(); ++idx) {
+    std::string& value = parts[idx];
+    std::vector<std::string> key_result;
+    std::string closing_delimiter = "]]";
+    base::SplitStringUsingSubstr(value, closing_delimiter, &key_result);
+    size_t key_results = key_result.size();
+
+    if (key_results > 1) {
+      std::string& message_postfix = key_result[1];
+      std::string localized_message =
+          localized_strings->GetString(key_result[0], "");
+      value = localized_message + message_postfix;
+    } else {
+      value = key_result[0];
+    }
+  }
+
+  *output_file = JoinString(parts, std::string());
 }
 
 }  // namespace loader
