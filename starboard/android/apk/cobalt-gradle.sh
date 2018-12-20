29ebaabf6f9f36789c3cc37f3c8e1c6fd8913da7
Cobalt Team <cobalt-team@google.com>
2018-12-19 18:23:42 -0800

    Ensure the gradle cache ends up in the out directory.
    Compute the absolute path to the gradle cache in cobalt-gradle.sh using
    the relative path from the target source dir that GYP provides. Gradle
    would otherwise use the path relative to the Gradle project root dir
    rather than relative to the PWD when cobalt-gradle.sh was run.
    
    b/121041882
    
    Change-Id: Ib07ff443fd281aafcd3c12e16c283505a4b300e5

--- starboard/android/apk/cobalt-gradle.sh
+++ starboard/android/apk/cobalt-gradle.sh
@@ -18,15 +18,19 @@
 #
 # Also resets hung gradle builds when specified by a leading --reset arg.
 
+GRADLE_ARGS=()
 while [ "$1" ]; do
   case "$1" in
     --sdk) shift; ANDROID_HOME="$1" ;;
     --ndk) shift; ANDROID_NDK_HOME="$1" ;;
+    --cache) shift; mkdir -p "$1";
+             GRADLE_ARGS+=("--project-cache-dir" $(cd "$1"; pwd)) ;;
     --reset) RESET_GRADLE=1 ;;
     *) break ;;
   esac
   shift
 done
+GRADLE_ARGS+=("$@")
 
 # Cleanup Gradle from previous builds. Used as part of the GYP step.
 if [[ "${RESET_GRADLE}" ]]; then
@@ -49,12 +53,12 @@ export ANDROID_HOME
 export ANDROID_NDK_HOME
 echo "ANDROID_HOME=${ANDROID_HOME}"
 echo "ANDROID_NDK_HOME=${ANDROID_NDK_HOME}"
-echo "TASK: ${@: -1}"
+echo "TASK: ${GRADLE_ARGS[-1]}"
 
 # Allow parallel gradle builds, as defined by a COBALT_GRADLE_BUILD_COUNT envvar
 # or default to 1 if that's not set (so buildbot only runs 1 gradle at a time).
 BUCKETS=${COBALT_GRADLE_BUILD_COUNT:-1}
-MD5=$(echo "$@" | md5sum)
+MD5=$(echo "${GRADLE_ARGS[@]}" | md5sum)
 LOCKNUM=$(( ${BUCKETS} * 0x${MD5:0:6} / 0x1000000 ))
 
-flock /var/lock/cobalt-gradle.lock.${LOCKNUM} $(dirname "$0")/gradlew "$@"
+flock /var/lock/cobalt-gradle.lock.${LOCKNUM} $(dirname "$0")/gradlew "${GRADLE_ARGS[@]}"
