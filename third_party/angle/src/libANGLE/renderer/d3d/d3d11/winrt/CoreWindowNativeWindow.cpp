fedbd3dad16ed521a836cbf7018aba95e1f52dea
Jelle Foks <jfoks@google.com>
2019-01-07 22:42:44 +0000

    Revert "[XB1] Increase swap chain to triple buffering."
    This reverts commit e15ec1118e8ec2ba8c001a8f1b60173401c46056.
    
    Reason for revert: While improving the 4k24 playback from the bug, it worsens the related stuttering that occurs at 1080p60 playback.
    
    Change-Id: Ib04399c6827a6ec43af482bdb27b009e9319fbe5

--- third_party/angle/src/libANGLE/renderer/d3d/d3d11/winrt/CoreWindowNativeWindow.cpp
+++ third_party/angle/src/libANGLE/renderer/d3d/d3d11/winrt/CoreWindowNativeWindow.cpp
@@ -158,7 +158,7 @@ HRESULT CoreWindowNativeWindow::createSwapChain(ID3D11Device *device,
     swapChainDesc.SampleDesc.Quality = 0;
     swapChainDesc.BufferUsage =
         DXGI_USAGE_SHADER_INPUT | DXGI_USAGE_RENDER_TARGET_OUTPUT | DXGI_USAGE_BACK_BUFFER;
-    swapChainDesc.BufferCount = 3;
+    swapChainDesc.BufferCount = 2;
     swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
     swapChainDesc.Scaling = DXGI_SCALING_STRETCH;
     swapChainDesc.AlphaMode             = DXGI_ALPHA_MODE_UNSPECIFIED;
