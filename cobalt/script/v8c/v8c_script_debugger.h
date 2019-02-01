77e25a922c3577af9ce969a207a654a481e5b0a8
Cobalt Team <cobalt-team@google.com>
2019-02-01 11:16:32 -0800

    Delete legacy JavaScript debugger and the script methods it used.
    The JavaScriptDebuggerAgent is unused legacy code from the original
    debugger implementation for JavaScriptCore.
    
    We've moved on to V8, where debugging is implemented by its inspector,
    which has its own debugger agent that directly implements the "Runtime"
    and "Debugger" protocol domains (as well as a couple others, like
    "Profiler").
    
    Remove execution control methods from |ScriptDebugger| interface, which
    aren't needed since V8 directly implements the devtools protocol.
    
    Change-Id: I3259bf4895bcf21e93ba258ca34ab6a176f17de1

--- cobalt/script/v8c/v8c_script_debugger.h
+++ cobalt/script/v8c/v8c_script_debugger.h
@@ -45,17 +45,8 @@ class V8cScriptDebugger : public ScriptDebugger,
                     TraceDelegate* trace_delegate) override;
   void StopTracing() override;
 
-  void Pause() override { NOTIMPLEMENTED(); }
-  void Resume() override { NOTIMPLEMENTED(); }
-  void SetBreakpoint(const std::string& script_id, int line_number,
-                     int column_number) override {
-    NOTIMPLEMENTED();
-  }
   PauseOnExceptionsState SetPauseOnExceptions(
       PauseOnExceptionsState state) override;
-  void StepInto() override { NOTIMPLEMENTED(); }
-  void StepOut() override { NOTIMPLEMENTED(); }
-  void StepOver() override { NOTIMPLEMENTED(); }
 
   // v8_inspector::V8InspectorClient implementation.
   void runMessageLoopOnPause(int contextGroupId) override;
