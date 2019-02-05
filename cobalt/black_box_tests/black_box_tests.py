1f6351292ac069919f8740a52da45e7412a3abb0
brianting <brianting@google.com>
2019-02-04 18:03:00 -0800

    Add web-platform-tests to black box testing.
    Add web-platform-tests to black box testing. Includes eliminating several
    configuration steps by flattening the web-platform-tests repos into a
    monorepo, integrating starboard web-platform-tests platform specific
    test filters alongside all platform test filters, expanding on the
    functionality and robustness of the proxy server used, and eliminating
    the need for host file modification when the proxy is used by utilizing
    alternative hostname IP resolution.
    
    b/110783104
    
    Change-Id: I226df0bf0beea0d4aa8a9703e034c02a70663eee

--- cobalt/black_box_tests/black_box_tests.py
+++ cobalt/black_box_tests/black_box_tests.py
@@ -1,3 +1,18 @@
+# Copyright 2019 The Cobalt Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -5,14 +20,19 @@ from __future__ import print_function
 import argparse
 import importlib
 import logging
+import random
+import socket
 import sys
 import unittest
 
 import _env  # pylint: disable=unused-import
 from cobalt.black_box_tests import black_box_cobalt_runner
 from cobalt.tools.automated_testing import cobalt_runner
+from proxy_server import ProxyServer
 from starboard.tools import abstract_launcher
 
+_PORT_SELECTION_RETRY_LIMIT = 10
+_PORT_SELECTION_RANGE = [5000, 7000]
 _SERVER_EXIT_TIMEOUT_SECONDS = 30
 # These tests can only be run on platforms whose app launcher can send suspend/
 # resume signals.
@@ -29,6 +49,7 @@ _TESTS_NO_SIGNAL = [
     'allow_eval',
     'disable_eval_with_csp',
     'persistent_cookie',
+    'web_platform_tests',
 ]
 # Location of test files.
 _TEST_DIR_PATH = 'cobalt.black_box_tests.tests.'
@@ -97,34 +118,83 @@ def LoadTests(platform, config, device_id, out_directory):
 class BlackBoxTests(object):
   """Helper class to run all black box tests and return results."""
 
-  def __init__(self, test_name=None):
-
-    self.test_name = test_name
-
-  def Run(self):
+  def __init__(self, test_name=None, proxy_port_number=None):
     logging.basicConfig(level=logging.DEBUG)
     GetDeviceParams()
 
-    if self.test_name:
-      suite = unittest.TestLoader().loadTestsFromModule(
-          importlib.import_module(_TEST_DIR_PATH + self.test_name))
-    else:
-      suite = LoadTests(_device_params.platform, _device_params.config,
-                        _device_params.device_id, _device_params.out_directory)
-    return_code = not unittest.TextTestRunner(
-        verbosity=0, stream=sys.stdout).run(suite).wasSuccessful()
-    return return_code
+    # Port number used to create the proxy server. If the --proxy target param
+    # is not set, a random free port is used.
+    if proxy_port_number is None:
+      proxy_port_number = str(self.GetUnusedPort(_binding_address))
+      _device_params.target_params.append('--proxy=%s:%s' % (_binding_address,
+                                          proxy_port_number))
+
+    self.test_name = test_name
+    self.proxy_port_number = proxy_port_number
+
+    # Test domains used in web platform tests to be resolved to the server
+    # binding address.
+    hosts = [
+        'web-platform.test',
+        'www.web-platform.test',
+        'www1.web-platform.test',
+        'www2.web-platform.test',
+        'xn--n8j6ds53lwwkrqhv28a.web-platform.test',
+        'xn--lve-6lad.web-platform.test'
+    ]
+    self.host_resolve_map = dict([(host, _binding_address) for host in hosts])
+
+  def Run(self):
+    if self.proxy_port_number == '-1':
+      return 1
+    logging.info('Using proxy port number: %s' % self.proxy_port_number)
+
+    with ProxyServer(port=self.proxy_port_number, host_resolve_map=self.host_resolve_map):
+      if self.test_name:
+        suite = unittest.TestLoader().loadTestsFromModule(
+            importlib.import_module(_TEST_DIR_PATH + self.test_name))
+      else:
+        suite = LoadTests(_device_params.platform, _device_params.config,
+                          _device_params.device_id, _device_params.out_directory)
+      return_code = not unittest.TextTestRunner(
+          verbosity=0, stream=sys.stdout).run(suite).wasSuccessful()
+      return return_code
+
+  def GetUnusedPort(self, machine_address):
+    """Find a free port on the machine address by pinging with socket."""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+      for i in range(1, _PORT_SELECTION_RETRY_LIMIT):
+        port_number = random.randint(_PORT_SELECTION_RANGE[0],
+                                     _PORT_SELECTION_RANGE[1])
+        result = sock.connect_ex((machine_address, port_number))
+        if result != 0:
+          return port_number
+        if i == _PORT_SELECTION_RETRY_LIMIT - 1:
+          logging.error(
+              'Can not find unused port on target machine within %s attempts.' %
+              _PORT_SELECTION_RETRY_LIMIT)
+          return -1
+    finally:
+      sock.close()
 
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--test_name')
-  parser.add_argument('--server_binding_address')
+  parser.add_argument('--test_name',
+                      help=('Name of test to be run. If not specified, all '
+                      'tests are run.'))
+  parser.add_argument('--server_binding_address',
+                      help='Binding address used to create the test server.')
+  parser.add_argument('--proxy_port_number',
+                      help=('Port number used to create the proxy http server'
+                      'that all black box tests are run through. If not'
+                      'specified, a random free port is used.'))
   args, _ = parser.parse_known_args()
 
   global _binding_address
   _binding_address = args.server_binding_address
-  test_object = BlackBoxTests(args.test_name)
+  test_object = BlackBoxTests(args.test_name, args.proxy_port_number)
   sys.exit(test_object.Run())
 
 
---
+++ cobalt/black_box_tests/proxy_server.py
@@ -0,0 +1,58 @@
+# Copyright 2019 The Cobalt Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Starts a HTTP proxy server.
+
+The HTTP proxy server is an improvement over SimpleHTTPServer as it can handle
+more complex requests and responses including HTTP methods like POST, PUT,
+OPTIONS, and others in addition to dealing with CORS and chunked messages. This
+is a modified fork of proxy.py from https://pypi.org/project/proxy.py/ that
+includes some bug fixes.
+"""
+
+import json
+import os
+import signal
+import subprocess
+import sys
+SRC_DIR = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+import tempfile
+import time
+
+
+class ProxyServer(object):
+
+  def __init__(self, hostname='0.0.0.0', port='8000', host_resolve_map=None):
+    self.command = [
+        'python',
+        os.path.join(SRC_DIR, 'third_party', 'proxy_py', 'proxy.py'),
+        '--hostname', hostname,
+        '--port', port
+    ]
+    self.host_resolver_path = None
+
+    if host_resolve_map:
+        with tempfile.NamedTemporaryFile(delete=False) as hosts:
+            json.dump(host_resolve_map, hosts)
+        self.host_resolver_path = hosts.name
+        self.command += ['--host_resolver', self.host_resolver_path]
+
+  def __enter__(self):
+    self.proc = subprocess.Popen(self.command)
+    time.sleep(1)
+    return self
+
+  def __exit__(self, exc_type, exc_value, traceback):
+    self.proc.send_signal(signal.SIGINT)
+    if self.host_resolver_path:
+        os.unlink(self.host_resolver_path)
