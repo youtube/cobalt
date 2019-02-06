78a55725b1cb60d521d672efff846e028a8c68a0
Joe Woodworth <joewoodworth@google.com>
2019-02-06 11:24:37 -0800

    Totally deprecate NetworkEvent in Cobalt
    Ignore all kSbEventTypeNetworkDisconnect, kSbEventTypeNetworkConnect
    Starboard events regardless of Starboard version, and stop generating
    and handling kConnection, kDisconnection NetworkEvents.
    
    b/123905432
    
    Change-Id: I65915b0d1c0a9f49d6cb26e4ba8659ee6580562d

--- cobalt/browser/application.cc
+++ cobalt/browser/application.cc
@@ -57,7 +57,6 @@
 #include "cobalt/browser/switches.h"
 #include "cobalt/loader/image/image_decoder.h"
 #include "cobalt/math/size.h"
-#include "cobalt/network/network_event.h"
 #include "cobalt/script/javascript_engine.h"
 #if defined(ENABLE_DEBUG_COMMAND_LINE_SWITCHES)
 #include "cobalt/storage/savegame_fake.h"
@@ -720,10 +719,6 @@ Application::Application(const base::Closure& quit_closure, bool should_preload)
   app_status_ = (should_preload ? kPreloadingAppStatus : kRunningAppStatus);
 
   // Register event callbacks.
-  network_event_callback_ =
-      base::Bind(&Application::OnNetworkEvent, base::Unretained(this));
-  event_dispatcher_.AddEventCallback(network::NetworkEvent::TypeId(),
-                                     network_event_callback_);
   deep_link_event_callback_ =
       base::Bind(&Application::OnDeepLinkEvent, base::Unretained(this));
   event_dispatcher_.AddEventCallback(base::DeepLinkEvent::TypeId(),
@@ -823,8 +818,6 @@ Application::~Application() {
   memory_tracker_tool_.reset(NULL);
 
   // Unregister event callbacks.
-  event_dispatcher_.RemoveEventCallback(network::NetworkEvent::TypeId(),
-                                        network_event_callback_);
   event_dispatcher_.RemoveEventCallback(base::DeepLinkEvent::TypeId(),
                                         deep_link_event_callback_);
 #if SB_API_VERSION >= 8
@@ -940,16 +933,6 @@ void Application::HandleStarboardEvent(const SbEvent* starboard_event) {
       break;
 #endif  // SB_API_VERSION >= SB_ON_SCREEN_KEYBOARD_SUGGESTIONS_VERSION
 #endif  // SB_HAS(ON_SCREEN_KEYBOARD)
-#if SB_API_VERSION < SB_DEPRECATE_DISCONNECT_VERSION
-    case kSbEventTypeNetworkConnect:
-      DispatchEventInternal(
-          new network::NetworkEvent(network::NetworkEvent::kConnection));
-      break;
-    case kSbEventTypeNetworkDisconnect:
-      DispatchEventInternal(
-          new network::NetworkEvent(network::NetworkEvent::kDisconnection));
-      break;
-#endif  // SB_API_VERSION < SB_DEPRECATE_DISCONNECT_VERSION
     case kSbEventTypeLink: {
       const char* link = static_cast<const char*>(starboard_event->data);
       DispatchEventInternal(new base::DeepLinkEvent(link));
@@ -970,6 +953,10 @@ void Application::HandleStarboardEvent(const SbEvent* starboard_event) {
 #if SB_API_VERSION >= 6
     case kSbEventTypePreload:
 #endif  // SB_API_VERSION >= 6
+#if SB_API_VERSION < SB_DEPRECATE_DISCONNECT_VERSION
+    case kSbEventTypeNetworkConnect:
+    case kSbEventTypeNetworkDisconnect:
+#endif  // SB_API_VERSION < SB_DEPRECATE_DISCONNECT_VERSION
     case kSbEventTypeScheduled:
     case kSbEventTypeStart:
     case kSbEventTypeStop:
@@ -977,28 +964,6 @@ void Application::HandleStarboardEvent(const SbEvent* starboard_event) {
     case kSbEventTypeVerticalSync:
       DLOG(WARNING) << "Unhandled Starboard event of type: "
                     << starboard_event->type;
-  }
-}
-
-void Application::OnNetworkEvent(const base::Event* event) {
-  TRACE_EVENT0("cobalt::browser", "Application::OnNetworkEvent()");
-  DCHECK(network_event_thread_checker_.CalledOnValidThread());
-  const network::NetworkEvent* network_event =
-      base::polymorphic_downcast<const network::NetworkEvent*>(event);
-  if (network_event->type() == network::NetworkEvent::kDisconnection) {
-    LOG(INFO) << "Detected a network disconnection.";
-    network_status_ = kDisconnectedNetworkStatus;
-    ++network_disconnect_count_;
-    // Wait patiently for the app to show a dialog, or for a reconnection event.
-  } else if (network_event->type() == network::NetworkEvent::kConnection) {
-    network_status_ = kConnectedNetworkStatus;
-    ++network_connect_count_;
-    if (network_disconnect_count_ > 0) {
-      DLOG(INFO) << "Got network connection event, reloading browser.";
-      browser_module_->Reload();
-    } else {
-      DLOG(INFO) << "Got network connection event, NOT reloading browser.";
-    }
   }
 }
 
