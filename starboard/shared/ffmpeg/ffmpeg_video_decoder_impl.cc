cb410e903d10a7521cf117a2c0b02928b77edb59
Cobalt Team <cobalt-team@google.com>
2019-02-26 15:25:49 -0800

    Plug leaks of std::vsnprintf
    Move the definition of FormatString from string.h to a new header
    format_string.h to break the dependency between string.h and standard
    header <string>. This helps with plugging the leaks. Also create a
    header stdio_leaks_poem.h to fix the leaks of std::vsnprintf. This
    header will potentially be enriched to plug more leaks. The leaks
    whitelist is updated too.
    
    b/123779885
    
    Change-Id: I997bfd9244c842c3da3a04f75cc784b637c3622d

--- starboard/shared/ffmpeg/ffmpeg_video_decoder_impl.cc
+++ starboard/shared/ffmpeg/ffmpeg_video_decoder_impl.cc
@@ -17,6 +17,9 @@
 
 #include "starboard/shared/ffmpeg/ffmpeg_video_decoder_impl.h"
 
+#if SB_API_VERSION >= SB_MOVE_FORMAT_STRING_VERSION
+#include "starboard/format_string.h"
+#endif  // SB_API_VERSION >= SB_MOVE_FORMAT_STRING_VERSION
 #include "starboard/linux/shared/decode_target_internal.h"
 #include "starboard/memory.h"
 #include "starboard/string.h"
