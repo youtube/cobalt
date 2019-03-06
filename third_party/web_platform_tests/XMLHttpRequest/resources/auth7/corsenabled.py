1c0de27af6b7eddfea56186e6458f8b280fac3a1
brianting <brianting@google.com>
2019-03-05 17:18:42 -0800

    Update wpt absolute paths to include third_party.
    Update wpt absolute paths to include third_party.
    
    b/110783104

--- third_party/web_platform_tests/XMLHttpRequest/resources/auth7/corsenabled.py
+++ third_party/web_platform_tests/XMLHttpRequest/resources/auth7/corsenabled.py
@@ -8,6 +8,8 @@ def main(request, response):
     response.headers.set('Access-Control-Allow-Headers', 'authorization, x-user, x-pass');
     response.headers.set('Access-Control-Expose-Headers', 'x-challenge, xhr-user, ses-user');
     auth = imp.load_source("", os.path.join(os.path.abspath(os.curdir),
+                                            "third_party",
+                                            "web_platform_tests",
                                             "XMLHttpRequest",
                                             "resources",
                                             "authentication.py"))
