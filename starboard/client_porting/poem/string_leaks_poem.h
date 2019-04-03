fea19c3d8f9645eb746077e33204e26abd3d59f7
Cobalt Team <cobalt-team@google.com>
2019-04-03 15:56:53 -0700

    Add string_leaks_poem.h
    This header is required to plug the leaks of at least `strlen` in
    multiple libraries (libaccount.a, libaccessiblity.a, etc). It is used to
    plug leaks which call functions of the form `std::<function>`.
    
    b/129758809
    
    Change-Id: I063a9a76138595705ae8a0b6f7c9e3dc2c566ea2

---
+++ starboard/client_porting/poem/string_leaks_poem.h
@@ -0,0 +1,53 @@
+// Copyright 2019 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This header plugs in the leaks of stdio functions called as std::<function>.
+
+#ifndef STARBOARD_CLIENT_PORTING_POEM_STRING_LEAKS_POEM_H_
+#define STARBOARD_CLIENT_PORTING_POEM_STRING_LEAKS_POEM_H_
+
+// This header plugs in the leaks of string functions that are called as
+// std::<function>. To plug these leaks we need to be able to strictly defined
+// when <cstring> is included and redefine the desired functions afterwared.
+// Additionally, unlike the other poems, we also need to define a function in
+// the std namespace that forwards its call to the globally namespaced
+// counterpart.
+//
+// *IMPORTANT*
+// This header MUST be included at the top of the targeted file or we cannot
+// guarantee when <cstring> will be included. The order of this include must be
+// preserved or the leaks will not be plugged.
+
+#include <cstring>
+
+#include "starboard/string.h"
+
+namespace std {
+
+inline size_t SbStringGetLength(const char* str) {
+  return ::SbStringGetLength(str);
+}
+
+}  // namespace std
+
+// The following macros specify a fully qualified function call to
+// SbStringGetLength to avoid function call ambiguities when building for
+// Android.
+//
+#undef __builtin_strlen
+#define __builtin_strlen std::SbStringGetLength
+#undef strlen
+#define strlen std::SbStringGetLength
+
+#endif  // STARBOARD_CLIENT_PORTING_POEM_STRING_LEAKS_POEM_H_
