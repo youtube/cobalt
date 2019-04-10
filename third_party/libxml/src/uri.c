e17685f2e6df6e8f2515513a259b261647919d53
Cobalt Team <cobalt-team@google.com>
2019-04-10 14:31:52 -0700

    Address critical security issue CVE-2017-7376 reported by Samsung.
    CVE-2017-7376: https://nvd.nist.gov/vuln/detail/CVE-2017-7376
    
    Fixes cherry-picked from:
    https://android.googlesource.com/platform/external/libxml2/+/51e0cb2e5ec18eaf6fb331bc573ff27b743898f4
    https://gitlab.gnome.org/GNOME/libxml2/commit/5dca9eea1bd4263bfa4d037ab2443de1cd730f7e
    
    b/121235149
    
    Change-Id: I3126000cc91667bdb1a0fde2da3564ddb52949ac

--- third_party/libxml/src/uri.c
+++ third_party/libxml/src/uri.c
@@ -327,17 +327,18 @@ static int
 xmlParse3986Port(xmlURIPtr uri, const char **str)
 {
     const char *cur = *str;
+    unsigned port = 0; /* unsigned for defined overflow behavior */
 
     if (ISA_DIGIT(cur)) {
-	if (uri != NULL)
-	    uri->port = 0;
-	while (ISA_DIGIT(cur)) {
-	    if (uri != NULL)
-		uri->port = uri->port * 10 + (*cur - '0');
-	    cur++;
-	}
-	*str = cur;
-	return(0);
+        while (ISA_DIGIT(cur)) {
+            port = port * 10 + (*cur - '0');
+
+            cur++;
+        }
+        if (uri != NULL)
+            uri->port = port & USHRT_MAX; /* port value modulo INT_MAX+1 */
+        *str = cur;
+        return(0);
     }
     return(1);
 }
