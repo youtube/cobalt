88810baeb15a69f8e177a940ef41ccf03ee8be26
Joe Woodworth <joewoodworth@google.com>
2019-05-13 15:34:52 -0700

    Remove sb_media_platform and references
    Assume all platforms have sb_media_platform starboard.
    
    Also remove SB_CAN_MEDIA_USE_STARBOARD_PIPELINE.
    
    b/131642872
    
    Change-Id: Ide51b87f3f1aa07bfdcb7ed5389d7824877e6cd7

--- starboard/configuration.h
+++ starboard/configuration.h
@@ -795,9 +795,4 @@ SB_COMPILE_ASSERT(sizeof(long) == 8,  // NOLINT(runtime/int)
 #endif
 #endif
 
-// Specifies whether the starboard media pipeline components (SbPlayerPipeline
-// and StarboardDecryptor) are used.  Set to 0 means they are not used.
-#define SB_CAN_MEDIA_USE_STARBOARD_PIPELINE \
-  SB_GYP_CAN_MEDIA_USE_STARBOARD_PIPELINE
-
 #endif  // STARBOARD_CONFIGURATION_H_
