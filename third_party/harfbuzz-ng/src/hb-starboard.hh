ee8a4465b5a3e4dd95f76d513e8d3be0c53edbf6
Cobalt Team <cobalt-team@google.com>
2019-05-14 12:01:45 -0700

    //starboard/log.h -> //starboard/common/log.h in //*
    b/130880800
    
    Change-Id: Ic93ce85ef81c5791badce00985315a1b1e7f0a6f

--- third_party/harfbuzz-ng/src/hb-starboard.hh
+++ third_party/harfbuzz-ng/src/hb-starboard.hh
@@ -18,7 +18,7 @@
 #include "starboard/memory.h"
 #include "starboard/string.h"
 #include "starboard/system.h"
-#include "starboard/log.h"
+#include "starboard/common/log.h"
 
 #define hb_malloc_impl SbMemoryAllocate
 #define hb_realloc_impl SbMemoryReallocate
