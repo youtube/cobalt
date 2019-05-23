edf4c03e553320a8e35f5a2c74ae4b2437e09b6b
Cobalt Team <cobalt-team@google.com>
2019-05-22 20:40:07 -0700

    Moves trampoline implementations to ./impl
    This allows better inspection for valid trampolines in the cobalt
    runner.
    
    Change-Id: Ifd82f408de88438ea0d94b44ad12a2b2f4e25fa8

--- cobalt/build/cobalt_archive_content/__cobalt_archive/run/impl/__init__.py
+++ cobalt/build/cobalt_archive_content/__cobalt_archive/run/impl/__init__.py
@@ -13,26 +13,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
-"""A trampoline for running the unit tests from a Cobalt Archive.
-   See readme.md for more information on trampolines.
-
-   Example: python __cobalt_archive/run/run_tests.py -t nplb -t nb_test
-"""
-
-import sys
-sys.path.insert(0, '.')
-import trampoline  # pylint: disable=relative-import
-
-TRAMPOLINE = [
-    'python starboard/tools/testing/test_runner.py',
-    '{target_names_arg}',
-    '{platform_arg}',
-    '{config_arg}',
-    '{device_id_arg}',
-    '{target_params_arg}',
-]
-
-if __name__ == '__main__':
-  trampoline.RunTrampolineThenExit(TRAMPOLINE)
