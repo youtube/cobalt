721f27bce5893556eaeb5e5949666cb8b37c5856
Cobalt Team <cobalt-team@google.com>
2019-05-23 15:11:04 -0700

    Adds run.py to the trampoline folder.
    Removes run_cobalt.py.
    
    * Lints all trampolines.
    * Breaks apart testing.
    
    Change-Id: Ie850da3670d7b5bdbeec9ab2ec1baf7e01676182

---
+++ cobalt/build/cobalt_archive_content/__cobalt_archive/run/impl/run_test.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+#
+# Copyright 2019 The Cobalt Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Unit tests the run.py trampoline."""
+
+import sys
+import unittest
+
+import trampoline  # pylint: disable=relative-import,g-bad-import-order,g-import-not-at-top
+sys.path.insert(0, '..')
+import run  # pylint: disable=relative-import,g-bad-import-order,g-import-not-at-top
+
+
+class RunGeneralTrampolineTest(unittest.TestCase):
+  """Tests trampoline substitutions for run_cobalt.py."""
+
+  def setUp(self):
+    """Change the trampoline internals for testing purposes."""
+    super(RunGeneralTrampolineTest, self).setUp()
+    self.prev_platform, trampoline.PLATFORM = trampoline.PLATFORM, 'MY_PLATFORM'
+    self.prev_config, trampoline.CONFIG = trampoline.CONFIG, 'MY_CONFIG'
+
+  def tearDown(self):
+    super(RunGeneralTrampolineTest, self).tearDown()
+    trampoline.PLATFORM = self.prev_platform
+    trampoline.CONFIG = self.prev_config
+
+  def testGeneral(self):
+    """Tests that target_params resolves to the expected value."""
+    tramp = run.TRAMPOLINE
+    expected_output = (
+        'python starboard/tools/example/app_launcher_client.py'
+        ' --target_name cobalt --platform MY_PLATFORM --config MY_CONFIG'
+        ' --target_params="--url=http://my.server.test"')
+    cmd_str = trampoline.ResolveTrampoline(
+        tramp,
+        argv=['-t', 'cobalt', '--target_params',
+              '"--url=http://my.server.test"'])
+    self.assertEqual(expected_output, cmd_str)
+
+
+if __name__ == '__main__':
+  unittest.main(verbosity=2)
