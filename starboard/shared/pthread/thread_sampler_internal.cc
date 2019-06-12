e88c0613b7e91ca0bc167d22c643b966243ed551
Cobalt Team <cobalt-team@google.com>
2019-06-12 15:00:19 -0700

    Starboard changes for Evergreen external threading
    While working on libcxx external threading, it became apparent that we
    would need to split mutex and recursive mutex into interface and
    implementation files since it would otherwise cause an include cycle and
    result in symbols not being defined correctly.
    
    b/134495972
    
    Change-Id: I6b3b930a6457cc4b8ac9eb02ac6e01810f0756dd

--- starboard/shared/pthread/thread_sampler_internal.cc
+++ starboard/shared/pthread/thread_sampler_internal.cc
@@ -19,6 +19,7 @@
 #include <semaphore.h>
 #include <signal.h>
 
+#include "starboard/common/log.h"
 #include "starboard/common/mutex.h"
 #include "starboard/shared/pthread/thread_context_internal.h"
 #include "starboard/thread.h"
