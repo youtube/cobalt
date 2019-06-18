92d15fbe49a0a3d8ad639f64dd65213b33d9196b
Cobalt Team <cobalt-team@google.com>
2019-06-18 09:09:51 -0700

    Refactor to force EGLConfig creation.
    Instead of waiting for an EGLConfig in the linux-x64x11-blittergles
    Blitter API, force its creation by using the last-created window (if
    it exists), or using best-guess during SbBlitterCreateDefaultDevice().
    
    b/130420533
    
    Change-Id: I2aa28d75a5814f249e7f11e3c074055a303930cb

--- starboard/shared/blittergles/blitter_create_default_device.cc
+++ starboard/shared/blittergles/blitter_create_default_device.cc
@@ -19,7 +19,66 @@
 #include <memory>
 
 #include "starboard/common/log.h"
+#include "starboard/common/optional.h"
 #include "starboard/shared/blittergles/blitter_internal.h"
+#include "starboard/shared/gles/gl_call.h"
+#include "starboard/shared/x11/application_x11.h"
+#include "starboard/window.h"
+
+namespace {
+
+// This function will search for an EGLConfig that enables a successful
+// eglCreateWindowSurface(). If there's no existing window, create a best-guess
+// config.
+starboard::optional<EGLConfig> GetEGLConfig(EGLDisplay display) {
+  SbWindow sample_window =
+      starboard::shared::x11::ApplicationX11::Get()->GetFirstWindow();
+  EGLint num_configs = 0;
+  EGLConfig config;
+  if (sample_window == kSbWindowInvalid) {
+    EGL_CALL(eglChooseConfig(
+        display, starboard::shared::blittergles::kConfigAttributeList, &config,
+        1, &num_configs));
+    if (num_configs == 1) {
+      return config;
+    }
+  } else {
+    EGL_CALL(eglChooseConfig(
+        display, starboard::shared::blittergles::kConfigAttributeList, NULL, 0,
+        &num_configs));
+    if (num_configs <= 0) {
+      SB_DLOG(ERROR) << ": Found no matching configs.";
+      return starboard::nullopt;
+    }
+
+    std::unique_ptr<EGLConfig[]> configs(new EGLConfig[num_configs]);
+    eglChooseConfig(display,
+                    starboard::shared::blittergles::kConfigAttributeList,
+                    configs.get(), num_configs, &num_configs);
+    if (eglGetError() != EGL_SUCCESS) {
+      SB_DLOG(ERROR) << ": Error getting matching EGLConfigs.";
+      return starboard::nullopt;
+    }
+
+    // Find first config that allows eglCreateWindowSurface().
+    EGLSurface surface;
+    EGLNativeWindowType native_window =
+        (EGLNativeWindowType)SbWindowGetPlatformHandle(sample_window);
+    for (int config_number = 0; config_number < num_configs; ++config_number) {
+      config = configs[config_number];
+      surface = eglCreateWindowSurface(display, config, native_window, NULL);
+      if (eglGetError() == EGL_SUCCESS) {
+        EGL_CALL(eglDestroySurface(display, surface));
+        return config;
+      }
+    }
+  }
+
+  SB_DLOG(ERROR) << ": Couldn't find a compatible config.";
+  return starboard::nullopt;
+}
+
+}  // namespace
 
 SbBlitterDevice SbBlitterCreateDefaultDevice() {
   starboard::shared::blittergles::SbBlitterDeviceRegistry* device_registry =
@@ -38,7 +97,6 @@ SbBlitterDevice SbBlitterCreateDefaultDevice() {
     device.reset();
     return kSbBlitterInvalidDevice;
   }
-
   eglInitialize(device->display, NULL, NULL);
   if (eglGetError() != EGL_SUCCESS) {
     SB_DLOG(ERROR) << ": Failed to initialize device.";
@@ -46,9 +104,13 @@ SbBlitterDevice SbBlitterCreateDefaultDevice() {
     return kSbBlitterInvalidDevice;
   }
 
-  device->config = NULL;
-  device->context = kSbBlitterInvalidContext;
+  starboard::optional<EGLConfig> config = GetEGLConfig(device->display);
+  if (!config) {
+    return kSbBlitterInvalidDevice;
+  }
+  device->config = *config;
 
+  device->context = kSbBlitterInvalidContext;
   device_registry->default_device = device.release();
 
   return device_registry->default_device;
