1f3608ead089a56c7ad3735cfc418006b28edc6d
Kaido Kert <kaidokert@google.com>
2019-07-08 16:53:35 -0700

    Update wpt tools for running newer tests
    Imported at fed14eca94f6b0ee96a5efe9ef77c73cd05cba19
    
    b/133879266
    
    Change-Id: I348edfdab45c874fd627538c820eb578d21b16b7

--- third_party/web_platform_tests/tools/wptserve/wptserve/response.py
+++ third_party/web_platform_tests/tools/wptserve/wptserve/response.py
@@ -2,12 +2,13 @@ from collections import OrderedDict
 from datetime import datetime, timedelta
 import Cookie
 import json
+import os
 import types
 import uuid
 import socket
 
-from constants import response_codes
-from logger import get_logger
+from .constants import response_codes
+from .logger import get_logger
 
 missing = object()
 
@@ -168,13 +169,25 @@ class Response(object):
         self.set_cookie(name, None, path=path, domain=domain, max_age=0,
                         expires=timedelta(days=-1))
 
-    def iter_content(self):
+    def iter_content(self, read_file=False):
         """Iterator returning chunks of response body content.
 
         If any part of the content is a function, this will be called
-        and the resulting value (if any) returned."""
-        if type(self.content) in types.StringTypes:
+        and the resulting value (if any) returned.
+
+        :param read_file: - boolean controlling the behaviour when content
+        is a file handle. When set to False the handle will be returned directly
+        allowing the file to be passed to the output in small chunks. When set to
+        True, the entire content of the file will be returned as a string facilitating
+        non-streaming operations like template substitution.
+        """
+        if isinstance(self.content, types.StringTypes):
             yield self.content
+        elif hasattr(self.content, "read"):
+            if read_file:
+                yield self.content.read()
+            else:
+                yield self.content
         else:
             for item in self.content:
                 if hasattr(item, "__call__"):
@@ -332,7 +345,7 @@ class ResponseHeaders(object):
 
     def update(self, items_iter):
         for name, value in items_iter:
-            self.set(name, value)
+            self.append(name, value)
 
     def __repr__(self):
         return repr(self.data)
@@ -355,6 +368,7 @@ class ResponseWriter(object):
         self._headers_complete = False
         self.content_written = False
         self.request = response.request
+        self.file_chunk_size = 32 * 1024
 
     def write_status(self, code, message=None):
         """Write out the status line of a response.
@@ -392,6 +406,11 @@ class ResponseWriter(object):
             #Would be nice to avoid double-encoding here
             self.write_header("Content-Length", len(self.encode(self._response.content)))
 
+        if (type(self._response.content) in (file,) and
+            "content-length" not in self._headers_seen):
+            file_size = os.stat( self._response.content.name ).st_size
+            self.write_header("Content-Length", file_size )
+
     def end_headers(self):
         """Finish writing headers and write the separator.
 
@@ -411,7 +430,10 @@ class ResponseWriter(object):
 
     def write_content(self, data):
         """Write the body of the response."""
-        self.write(self.encode(data))
+        if isinstance(data, types.StringTypes):
+            self.write(data)
+        else:
+            self.write_content_file(data)
         if not self._response.explicit_flush:
             self.flush()
 
@@ -425,6 +447,20 @@ class ResponseWriter(object):
             # This can happen if the socket got closed by the remote end
             pass
 
+    def write_content_file(self, data):
+        """Write a file-like object directly to the response in chunks.
+        Does not flush."""
+        self.content_written = True
+        while True:
+            buf = data.read(self.file_chunk_size)
+            if not buf:
+                break
+            try:
+                self._wfile.write(buf)
+            except socket.error:
+                break
+        data.close()
+
     def encode(self, data):
         """Convert unicode to bytes according to response.encoding."""
         if isinstance(data, str):
@@ -435,9 +471,11 @@ class ResponseWriter(object):
             raise ValueError
 
     def flush(self):
-        """Flush the output."""
+        """Flush the output. Returns False if the flush failed due to
+        the socket being closed by the remote end."""
         try:
             self._wfile.flush()
+            return True
         except socket.error:
             # This can happen if the socket got closed by the remote end
-            pass
+            return False
