0d612db818f94c6b6d31c1274fba25da143c4758
Cobalt Team <cobalt-team@google.com>
2019-07-22 11:34:59 -0700

    Add dummy draw call to avoid memory leak.
    When linux-x64x11-blittergles unit tests run on Xvfb, the 1st
    glDrawArrays() call leaks memory. Make a dummy draw call on
    SbBlitterContext creation and wrap it in lsan_disable/enable to avoid
    unit test failures.
    
    b/130422778
    
    Change-Id: I6336374aabb08ce46bb6f18a412dfa579cb0fe79

--- starboard/shared/blittergles/blitter_create_default_device.cc
+++ starboard/shared/blittergles/blitter_create_default_device.cc
@@ -22,6 +22,8 @@
 #include "starboard/common/optional.h"
 #include "starboard/shared/blittergles/blitter_context.h"
 #include "starboard/shared/blittergles/blitter_internal.h"
+#include "starboard/shared/blittergles/blitter_surface.h"
+#include "starboard/shared/blittergles/color_shader_program.h"
 #include "starboard/shared/gles/gl_call.h"
 #include "starboard/shared/x11/application_x11.h"
 #include "starboard/window.h"
@@ -79,6 +81,21 @@ starboard::optional<EGLConfig> GetEGLConfig(EGLDisplay display) {
   return starboard::nullopt;
 }
 
+// When using Xvfb, the selected drivers will leak memory on the 1st call to
+// glDrawArrays(). We get that draw out of the way with a dummy draw here.
+void DummyDraw(SbBlitterContext context) {
+  SbBlitterSurface dummy_surface = SbBlitterCreateRenderTargetSurface(
+      context->device, 1, 1, kSbBlitterSurfaceFormatRGBA8);
+  SbBlitterSetRenderTarget(context, dummy_surface->render_target);
+  SbBlitterContextPrivate::ScopedCurrentContext scoped_current_context(context);
+  const starboard::shared::blittergles::ColorShaderProgram&
+      color_shader_program = context->GetColorShaderProgram();
+  color_shader_program.DummyDraw(dummy_surface->render_target);
+  context->current_render_target = kSbBlitterInvalidRenderTarget;
+  context->scissor = SbBlitterMakeRect(0, 0, 0, 0);
+  SbBlitterDestroySurface(dummy_surface);
+}
+
 }  // namespace
 
 SbBlitterDevice SbBlitterCreateDefaultDevice() {
@@ -119,6 +136,8 @@ SbBlitterDevice SbBlitterCreateDefaultDevice() {
       starboard::shared::blittergles::GetBlitterContextRegistry();
   starboard::ScopedLock context_lock(context_registry->mutex);
   context_registry->context = context.release();
+
+  DummyDraw(context_registry->context);
 
   return device_registry->default_device;
 }
