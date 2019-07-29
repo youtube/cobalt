9f127ec0020c568ea3e9b4c680a72cd9c9d929f6
Cobalt Team <cobalt-team@google.com>
2019-07-29 16:34:23 +0200

    Revive the debug console for mozjs.
    Restore our own implementation of RuntimeAgent to be used when not using
    V8, which implements the Runtime domain in its inspector. The
    implementation here has been updated to align with changes to the
    debugger architecture since it was deleted.
    
    This restored implementation does not include the glue needed for
    dom_agent.js to call |CreateRemoteObject| in its implementation of the
    |DOM.resolveNode| command, so some functionality in the "Elements" panel
    of the remote DevTools may be broken.
    
    The RuntimeAgent restored here was deleted in:
    https://lbshell-internal-review.git.corp.google.com/c/cobalt/+/125282/
    
    b/136201377
    
    Change-Id: I1dbf4aea5b53e006249c699b5b626713a53ab7c8
    (cherry picked from commit 02021259ff2cdc16f86c64f1e2dc3dd36627b92c)

---
+++ cobalt/debug/backend/runtime_agent.h
@@ -0,0 +1,58 @@
+// Copyright 2016 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef COBALT_DEBUG_BACKEND_RUNTIME_AGENT_H_
+#define COBALT_DEBUG_BACKEND_RUNTIME_AGENT_H_
+
+#include "cobalt/debug/backend/command_map.h"
+#include "cobalt/debug/backend/debug_dispatcher.h"
+#include "cobalt/debug/command.h"
+#include "cobalt/debug/json_object.h"
+#include "cobalt/dom/window.h"
+
+namespace cobalt {
+namespace debug {
+namespace backend {
+
+// Implements a small part of the the "Runtime" inspector protocol domain with
+// just enough to support console input. When using the V8 JavaScript engine,
+// this class is not needed since the V8 inspector implements the Runtime domain
+// for us.
+class RuntimeAgent {
+ public:
+  RuntimeAgent(DebugDispatcher* dispatcher, dom::Window* window);
+
+  void Thaw(JSONObject agent_state);
+  JSONObject Freeze();
+
+ private:
+  void CompileScript(const Command& command);
+  void Disable(const Command& command);
+  void Enable(const Command& command);
+
+  DebugDispatcher* dispatcher_;
+  dom::Window* window_;
+
+  // Map of member functions implementing commands.
+  CommandMap<RuntimeAgent> commands_;
+
+  // Whether we successfully loaded the agent's JavaScript implementation.
+  bool script_loaded_ = false;
+};
+
+}  // namespace backend
+}  // namespace debug
+}  // namespace cobalt
+
+#endif  // COBALT_DEBUG_BACKEND_RUNTIME_AGENT_H_
