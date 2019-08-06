38c50d93d99ea355a0a22bba0aab02ba87544dfe
Cobalt Team <cobalt-team@google.com>
2019-08-05 18:18:49 -0700

    Add cpu features implementation to x86 and arm64
    b/138617898
    
    Change-Id: Ibb30ba405977111f688840856aac3f7ef80100ea

--- starboard/shared/linux/cpu_features_get.cc
+++ starboard/shared/linux/cpu_features_get.cc
@@ -12,6 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// This file contains implementations of SbCPUFeaturesGet() on all supported
+// architectures defined in Starboard header cpu_features.h on linux platform.
+// We use the most applicable and precise approach to get CPU features on each
+// architecture. To be specific,
+//
+// On Arm/Arm64, we read CPU version information from /proc/cpuinfo. We get
+// feature flags by parsing HWCAP bitmasks retrieved from function getauxval()
+// or /proc/self/auxv, or by reading the flags from /proc/cpuinfo.
+//
+// On X86/X86_64, we read CPU version strings from /proc/cpuinfo. We get other
+// CPU version information and feature flags by CPUID instruction, which gives
+// more precise and complete information than reading from /proc/cpuinfo.
+//
+// TODO: Mips/Mips64 and Ppc/Ppc64
+
 #include "starboard/cpu_features.h"
 
 #include <dlfcn.h>         // dlsym, dlclose, dlopen
@@ -19,7 +34,7 @@
 #include <stdio.h>         // fopen, fclose
 #include <string.h>
 #include <strings.h>  // strcasecmp
-#include <sys/auxv.h>
+#include <sys/auxv.h>  // getauxval()
 #include <unistd.h>  // sysconf()
 #include <memory>
 
@@ -32,7 +47,10 @@
 
 namespace {
 
-// See <asm/hwcap.h> kernel header.
+#if SB_IS(ARCH_ARM)
+
+#if SB_IS(32_BIT)
+// See <arch/arm/include/uapi/asm/hwcap.h> kernel header.
 #define HWCAP_VFP (1 << 6)
 #define HWCAP_IWMMXT (1 << 9)
 #define HWCAP_NEON (1 << 12)
@@ -44,7 +62,6 @@ namespace {
 #define HWCAP_VFPD32 (1 << 19) /* set if VFP has 32 regs (not 16) */
 #define HWCAP_IDIV (HWCAP_IDIVA | HWCAP_IDIVT)
 
-// see <uapi/asm/hwcap.h> kernel header
 #define HWCAP2_AES (1 << 0)
 #define HWCAP2_PMULL (1 << 1)
 #define HWCAP2_SHA1 (1 << 2)
@@ -55,38 +72,53 @@ namespace {
 #define HWCAP_SET_FOR_ARMV8 \
   (HWCAP_VFP | HWCAP_NEON | HWCAP_VFPv3 | HWCAP_VFPv4 | HWCAP_IDIV)
 
+#elif SB_IS(64_BIT)
+// See <arch/arm64/include/uapi/asm/hwcap.h> kernel header
+#define HWCAP_FP (1 << 0)
+#define HWCAP_ASIMD (1 << 1)
+#define HWCAP_AES (1 << 3)
+#define HWCAP_PMULL (1 << 4)
+#define HWCAP_SHA1 (1 << 5)
+#define HWCAP_SHA2 (1 << 6)
+#define HWCAP_CRC32 (1 << 7)
+
+#endif  // SB_IS(32_BIT)
+
+#elif SB_IS(ARCH_X86)
+
+// x86_xgetbv returns the value of an Intel Extended Control Register (XCR).
+// Currently only XCR0 is defined by Intel so |xcr| should always be zero.
+uint64_t x86_xgetbv(uint32_t xcr) {
+  uint32_t eax, edx;
+
+  __asm__ volatile("xgetbv" : "=a"(eax), "=d"(edx) : "c"(xcr));
+  return (static_cast<uint64_t>(edx) << 32) | eax;
+}
+
+#if SB_IS(32_BIT)
+void x86_cpuid(int cpuid_info[4], int info_type) {
+  __asm__ volatile(
+      "mov %%ebx, %%edi\n"
+      "cpuid\n"
+      "xchg %%edi, %%ebx\n"
+      : "=a"(cpuid_info[0]), "=D"(cpuid_info[1]), "=c"(cpuid_info[2]),
+        "=d"(cpuid_info[3])
+      : "a"(info_type), "c"(0));
+}
+#elif SB_IS(64_BIT)
+void x86_cpuid(int cpuid_info[4], int info_type) {
+  __asm__ volatile("cpuid\n"
+                   : "=a"(cpuid_info[0]), "=b"(cpuid_info[1]),
+                     "=c"(cpuid_info[2]), "=d"(cpuid_info[3])
+                   : "a"(info_type), "c"(0));
+}
+#endif  // SB_IS(32_BIT)
+
+#endif  // SB_IS(ARCH_ARM)
+
 using starboard::shared::SetX86FeaturesInvalid;
 using starboard::shared::SetArmFeaturesInvalid;
 using starboard::shared::SetGeneralFeaturesInvalid;
-
-// Checks if a space-separated list of items |list|, in the form of a string,
-// contains one given item |item|.
-bool HasItemInList(const char* list, const char* flag) {
-  ssize_t flag_length = strlen(flag);
-  const char* list_ptr = list;
-  if (list_ptr == nullptr) {
-    return false;
-  }
-  while (*list_ptr != '\0') {
-    // Skip whitespace.
-    while (isspace(*list_ptr))
-      ++list_ptr;
-
-    // Find end of current list flag.
-    const char* end_ptr = list_ptr;
-    while (*end_ptr != '\0' && !isspace(*end_ptr))
-      ++end_ptr;
-
-    if (flag_length == end_ptr - list_ptr &&
-        memcmp(list_ptr, flag, flag_length) == 0) {
-      return true;
-    }
-
-    // Continue to the next flag.
-    list_ptr = end_ptr;
-  }
-  return false;
-}
 
 // Class that holds the information in system file /proc/cpuinfo.
 class ProcCpuInfo {
@@ -304,6 +336,36 @@ uint32_t ReadElfHwcaps(uint32_t hwcap_type) {
   return hwcap;
 }
 
+#if SB_IS(ARCH_ARM)
+// Checks if a space-separated list of items |list|, in the form of a string,
+// contains one given item |item|.
+bool HasItemInList(const char* list, const char* flag) {
+  ssize_t flag_length = strlen(flag);
+  const char* list_ptr = list;
+  if (list_ptr == nullptr) {
+    return false;
+  }
+  while (*list_ptr != '\0') {
+    // Skip whitespace.
+    while (isspace(*list_ptr))
+      ++list_ptr;
+
+    // Find end of current list flag.
+    const char* end_ptr = list_ptr;
+    while (*end_ptr != '\0' && !isspace(*end_ptr))
+      ++end_ptr;
+
+    if (flag_length == end_ptr - list_ptr &&
+        memcmp(list_ptr, flag, flag_length) == 0) {
+      return true;
+    }
+
+    // Continue to the next flag.
+    list_ptr = end_ptr;
+  }
+  return false;
+}
+
 // Construct hwcap bitmask by the feature flags in /proc/cpuinfo
 uint32_t ConstructHwcapFromCPUInfo(ProcCpuInfo* cpu_info,
                                    int16_t architecture_generation,
@@ -352,8 +414,13 @@ uint32_t ConstructHwcapFromCPUInfo(ProcCpuInfo* cpu_info,
 bool SbCPUFeaturesGet_ARM(SbCPUFeatures* features) {
   memset(features, 0, sizeof(*features));
 
-  // Raspi is a 32-bit ARM platform.
+#if SB_IS(32_BIT)
   features->architecture = kSbCPUFeaturesArchitectureArm;
+#elif SB_IS(64_BIT)
+  features->architecture = kSbCPUFeaturesArchitectureArm64;
+#else
+#error "Your platform must be either 32-bit or 64-bit."
+#endif
 
   // Set the default value of the features to be invalid, then fill them in
   // if appropriate.
@@ -399,7 +466,11 @@ bool SbCPUFeaturesGet_ARM(SbCPUFeatures* features) {
     // ARMv7-based CPU, and "(v6l)" for an ARMv6-one. For example,
     // the Raspberry Pi is one popular ARMv6 device that reports
     // architecture 7. The 'Processor' or 'model name' fields
-    // also contain processor brand information.
+    // also contain processor brand information. "model name" is used in
+    // Linux 3.8 and later (3.7
+    // and later for arm64) and is shown once per CPU. "Processor" is used in
+    // earler versions and is shown only once at the top of /proc/cpuinfo
+    // regardless of the number CPUs.
 
     // Allocate 256 bytes for "Processor"/"model name" field.
     static char brand_buffer[256] = {0};
@@ -418,6 +489,7 @@ bool SbCPUFeaturesGet_ARM(SbCPUFeatures* features) {
     }
   }
 
+#if SB_IS(32_BIT)
   // Get hwcap bitmask and extract the CPU feature flags from it.
   features->hwcap = ReadElfHwcaps(AT_HWCAP);
   if (features->hwcap == 0) {
@@ -481,9 +553,21 @@ bool SbCPUFeaturesGet_ARM(SbCPUFeatures* features) {
 
   features->arm.has_aes = (features->hwcap2 & HWCAP2_AES) != 0;
   features->arm.has_pmull = (features->hwcap2 & HWCAP2_PMULL) != 0;
-  features->arm.has_sha1 = (features->hwcap2 & HWCAP2_PMULL) != 0;
+  features->arm.has_sha1 = (features->hwcap2 & HWCAP2_SHA1) != 0;
   features->arm.has_sha2 = (features->hwcap2 & HWCAP2_SHA2) != 0;
   features->arm.has_crc32 = (features->hwcap2 & HWCAP2_CRC32) != 0;
+
+#elif SB_IS(64_BIT)
+  // Read hwcaps bitmask and extract the CPU feature flags from it.
+  features->hwcap = ReadElfHwcaps(AT_HWCAP);
+
+  features->arm.has_aes = (features->hwcap & HWCAP_AES) != 0;
+  features->arm.has_pmull = (features->hwcap & HWCAP_PMULL) != 0;
+  features->arm.has_sha1 = (features->hwcap & HWCAP_SHA1) != 0;
+  features->arm.has_sha2 = (features->hwcap & HWCAP_SHA2) != 0;
+  features->arm.has_crc32 = (features->hwcap & HWCAP_CRC32) != 0;
+
+#endif  // SB_IS(32_BIT)
 
   // Get L1 ICACHE and DCACHE line size.
   features->icache_line_size = sysconf(_SC_LEVEL1_ICACHE_LINESIZE);
@@ -492,12 +576,158 @@ bool SbCPUFeaturesGet_ARM(SbCPUFeatures* features) {
   return true;
 }
 
+#elif SB_IS(ARCH_X86)
+
+bool SbCPUFeaturesGet_X86(SbCPUFeatures* features) {
+  memset(features, 0, sizeof(*features));
+
+#if SB_IS(32_BIT)
+  features->architecture = kSbCPUFeaturesArchitectureX86;
+#elif SB_IS(64_BIT)
+  features->architecture = kSbCPUFeaturesArchitectureX86_64;
+#else
+#error "Your platform must be either 32-bit or 64-bit."
+#endif
+
+  // Set the default value of the features to be invalid, then fill them in
+  // if appropriate.
+  SetGeneralFeaturesInvalid(features);
+  SetArmFeaturesInvalid(features);
+  SetX86FeaturesInvalid(features);
+
+  ProcCpuInfo cpu_info;
+
+  // Extract brand and vendor information.
+  // Allocate 256 bytes for "model name" field.
+  static char brand_buffer[256] = {0};
+  if (cpu_info.ExtractStringFeature(
+          "model name", brand_buffer,
+          sizeof(brand_buffer) / sizeof(brand_buffer[0]))) {
+    features->brand = brand_buffer;
+  }
+
+  // Allocate 128 bytes for "vendor_id" field.
+  static char vendor_buffer[128] = {0};
+  if (cpu_info.ExtractStringFeature(
+          "vendor_id", vendor_buffer,
+          sizeof(vendor_buffer) / sizeof(vendor_buffer[0]))) {
+    features->x86.vendor = vendor_buffer;
+  }
+
+  // Get L1 ICACHE and DCACHE line size.
+  features->icache_line_size = sysconf(_SC_LEVEL1_ICACHE_LINESIZE);
+  features->dcache_line_size = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
+
+  // Hwcap bitmasks are not applicable to x86/x86_64. They'll remain 0.
+
+  // Use cpuid instruction to get feature flags.
+  int cpuid_info[4] = {0};
+
+  // x86_cpuid with an InfoType argument of 0 returns the number of
+  // valid Ids in cpuid_info[0].
+  x86_cpuid(cpuid_info, 0);
+  unsigned int num_ids = cpuid_info[0];
+
+  // Interpret CPU feature information of CPUID level 1. EAX is returned
+  // in cpuid_info[0], EBX in cpuid_info[1], ECX in cpuid_info[2],
+  // EDX in cpuid_info[3].
+  if (num_ids > 0) {
+    x86_cpuid(cpuid_info, 1);
+    features->x86.signature = cpuid_info[0];
+    features->x86.stepping = cpuid_info[0] & 0xF;
+    features->x86.model =
+        ((cpuid_info[0] >> 4) & 0xF) + ((cpuid_info[0] >> 12) & 0xF0);
+    features->x86.family = (cpuid_info[0] >> 8) & 0xF;
+    features->x86.type = (cpuid_info[0] >> 12) & 0x3;
+    features->x86.ext_model = (cpuid_info[0] >> 16) & 0xF;
+    features->x86.ext_family = (cpuid_info[0] >> 20) & 0xFF;
+
+    features->has_fpu = (cpuid_info[3] & (1 << 0)) != 0;
+
+    features->x86.has_cmov = (cpuid_info[3] & (1 << 15)) != 0;
+    features->x86.has_mmx = (cpuid_info[3] & (1 << 23)) != 0;
+    features->x86.has_sse = (cpuid_info[3] & (1 << 25)) != 0;
+    features->x86.has_sse2 = (cpuid_info[3] & (1 < 26)) != 0;
+    features->x86.has_tsc = (cpuid_info[3] & (1 << 4)) != 0;
+
+    features->x86.has_sse3 = (cpuid_info[2] & (1 << 0)) != 0;
+    features->x86.has_ssse3 = (cpuid_info[2] & (1 << 9)) != 0;
+    features->x86.has_sse41 = (cpuid_info[2] & (1 << 19)) != 0;
+    features->x86.has_sse42 = (cpuid_info[2] & (1 << 20)) != 0;
+    features->x86.has_movbe = (cpuid_info[2] & (1 << 22)) != 0;
+    features->x86.has_popcnt = (cpuid_info[2] & (1 << 23)) != 0;
+    features->x86.has_osxsave = (cpuid_info[2] & (1 << 27)) != 0;
+    features->x86.has_f16c = (cpuid_info[2] & (1 << 29)) != 0;
+    features->x86.has_fma3 = (cpuid_info[2] & (1 << 12)) != 0;
+    features->x86.has_aesni = (cpuid_info[2] & (1 << 25)) != 0;
+
+    // AVX instructions will generate an illegal instruction exception unless
+    //   a) they are supported by the CPU,
+    //   b) XSAVE is supported by the CPU and
+    //   c) XSAVE is enabled by the kernel.
+    // See http://software.intel.com/en-us/blogs/2011/04/14/is-avx-enabled
+    //
+    // In addition, we have observed some crashes with the xgetbv instruction
+    // even after following Intel's example code. (See crbug.com/375968.)
+    // Because of that, we also test the XSAVE bit because its description in
+    // the CPUID documentation suggests that it signals xgetbv support.
+    features->x86.has_avx =
+        (cpuid_info[2] & (1 << 28)) != 0 &&
+        (cpuid_info[2] & (1 << 26)) != 0 /* XSAVE */ &&
+        (cpuid_info[2] & (1 << 27)) != 0 /* OSXSAVE */ &&
+        (x86_xgetbv(0) & 6) == 6 /* XSAVE enabled by kernel */;
+  }
+
+  // Interpret CPU feature information of CPUID level 7.
+  if (num_ids >= 7) {
+    x86_cpuid(cpuid_info, 7);
+    features->x86.has_avx2 =
+        features->x86.has_avx && (cpuid_info[1] & (1 << 5)) != 0;
+    features->x86.has_avx512f =
+        features->x86.has_avx && (cpuid_info[1] & (1 << 16)) != 0;
+    features->x86.has_avx512dq =
+        features->x86.has_avx && (cpuid_info[1] & (1 << 17)) != 0;
+    features->x86.has_avx512ifma =
+        features->x86.has_avx && (cpuid_info[1] & (1 << 21)) != 0;
+    features->x86.has_avx512pf =
+        features->x86.has_avx && (cpuid_info[1] & (1 << 26)) != 0;
+    features->x86.has_avx512er =
+        features->x86.has_avx && (cpuid_info[1] & (1 << 27)) != 0;
+    features->x86.has_avx512cd =
+        features->x86.has_avx && (cpuid_info[1] & (1 << 28)) != 0;
+    features->x86.has_avx512bw =
+        features->x86.has_avx && (cpuid_info[1] & (1 << 30)) != 0;
+    features->x86.has_avx512vl =
+        features->x86.has_avx && (cpuid_info[1] & (1 << 31)) != 0;
+    features->x86.has_bmi1 = (cpuid_info[1] & (1 << 3)) != 0;
+    features->x86.has_bmi2 = (cpuid_info[1] & (1 << 8)) != 0;
+  }
+
+  // Query extended IDs.
+  x86_cpuid(cpuid_info, 0x80000000);
+  unsigned int num_ext_ids = cpuid_info[0];
+
+  // Interpret extended CPU feature information.
+  if (num_ext_ids > 0x80000000) {
+    // Interpret CPU feature information of CPUID level 0x80000001.
+    x86_cpuid(cpuid_info, 0x80000001);
+    features->x86.has_lzcnt = (cpuid_info[2] & (1 << 5)) != 0;
+    // SAHF must be probed in long mode.
+    features->x86.has_sahf = (cpuid_info[2] & (1 << 0)) != 0;
+  }
+
+  return true;
+}
+#endif
+
 }  // namespace
 
-// TODO: Only ARM is currently implemented and tested
+// TODO: Only ARM/ARM64 and X86/X86_64 are currently implemented and tested
 bool SbCPUFeaturesGet(SbCPUFeatures* features) {
 #if SB_IS(ARCH_ARM)
   return SbCPUFeaturesGet_ARM(features);
+#elif SB_IS(ARCH_X86)
+  return SbCPUFeaturesGet_X86(features);
 #else
   SB_NOTIMPLEMENTED();
 
