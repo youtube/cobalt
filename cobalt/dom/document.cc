5e2d61b4231b96e7a1e44d63088d3c4fa3114c2c
Jelle Foks <jfoks@google.com>
2019-08-08 10:56:13 -0700

    Also consider siblings when clearing rule matching state.
    Also clear the rule matching state of siblings of the root element.
    
    When there are two sibling <html> elements in the document, still only one can be the root element,
    resulting in some rule matching state not being cleared unless it is also cleared for siblings of
    the root element.
    
    This fixes b/134768217
    
    Change-Id: I5854d47c26a9e33b7988804a329923ed181b9dd2

--- cobalt/dom/document.cc
+++ cobalt/dom/document.cc
@@ -927,7 +927,7 @@ void Document::UpdateSelectorTree() {
 
     scoped_refptr<HTMLHtmlElement> current_html = html();
     if (current_html) {
-      current_html->ClearRuleMatchingStateOnElementAndDescendants();
+      current_html->ClearRuleMatchingStateOnElementAndSiblingsAndDescendants();
     }
 
     is_selector_tree_dirty_ = false;
