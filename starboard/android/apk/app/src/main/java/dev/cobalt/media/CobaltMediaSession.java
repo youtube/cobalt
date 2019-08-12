a638cdf7c9209c35da06ca4f8ca9689005c720be
Joe Woodworth <joewoodworth@google.com>
2019-08-12 13:35:19 -0700

    [Android] Migrate to MediaSessionCompat
    Migrate to MediaSessionCompat from MediaSession.
    
    b/139205921
    
    Change-Id: Ibbf11c42634c176fecda9bb3388a3bfcd054be54

--- starboard/android/apk/app/src/main/java/dev/cobalt/media/CobaltMediaSession.java
+++ starboard/android/apk/app/src/main/java/dev/cobalt/media/CobaltMediaSession.java
@@ -24,12 +24,12 @@ import android.media.AudioAttributes;
 import android.media.AudioAttributes.Builder;
 import android.media.AudioFocusRequest;
 import android.media.AudioManager;
-import android.media.MediaMetadata;
-import android.media.session.MediaSession;
-import android.media.session.PlaybackState;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.v4.media.MediaMetadataCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.view.WindowManager;
 import dev.cobalt.util.DisplayUtil;
 import dev.cobalt.util.Holder;
@@ -47,7 +47,7 @@ public class CobaltMediaSession
   // deprecated and unnecessary on API 26+.
   @SuppressWarnings("deprecation")
   private static final int MEDIA_SESSION_FLAG_HANDLES_TRANSPORT_CONTROLS =
-      MediaSession.FLAG_HANDLES_TRANSPORT_CONTROLS;
+      MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS;
 
   private AudioFocusRequest audioFocusRequest;
 
@@ -69,15 +69,15 @@ public class CobaltMediaSession
 
   private final UpdateVolumeListener volumeListener;
   private final ArtworkLoader artworkLoader;
-  private MediaSession mediaSession;
+  private MediaSessionCompat mediaSession;
 
   // We re-use the builder to hold onto the most recent metadata and add artwork later.
-  private MediaMetadata.Builder metadataBuilder = new MediaMetadata.Builder();
+  private MediaMetadataCompat.Builder metadataBuilder = new MediaMetadataCompat.Builder();
   // We re-use the builder to hold onto the most recent playback state.
-  private PlaybackState.Builder playbackStateBuilder = new PlaybackState.Builder();
+  private PlaybackStateCompat.Builder playbackStateBuilder = new PlaybackStateCompat.Builder();
 
   // Duplicated in starboard/android/shared/android_media_session_client.h
-  // PlaybackState
+  // PlaybackStateCompat
   private static final int PLAYBACK_STATE_PLAYING = 0;
   private static final int PLAYBACK_STATE_PAUSED = 1;
   private static final int PLAYBACK_STATE_NONE = 2;
@@ -99,50 +99,50 @@ public class CobaltMediaSession
   }
 
   private void setMediaSession() {
-    mediaSession = new MediaSession(context, TAG);
+    mediaSession = new MediaSessionCompat(context, TAG);
     mediaSession.setFlags(MEDIA_SESSION_FLAG_HANDLES_TRANSPORT_CONTROLS);
     mediaSession.setCallback(
-        new MediaSession.Callback() {
+        new MediaSessionCompat.Callback() {
           @Override
           public void onFastForward() {
             Log.i(TAG, "MediaSession action: FAST FORWARD");
-            nativeInvokeAction(PlaybackState.ACTION_FAST_FORWARD);
+            nativeInvokeAction(PlaybackStateCompat.ACTION_FAST_FORWARD);
           }
 
           @Override
           public void onPause() {
             Log.i(TAG, "MediaSession action: PAUSE");
-            nativeInvokeAction(PlaybackState.ACTION_PAUSE);
+            nativeInvokeAction(PlaybackStateCompat.ACTION_PAUSE);
           }
 
           @Override
           public void onPlay() {
             Log.i(TAG, "MediaSession action: PLAY");
-            nativeInvokeAction(PlaybackState.ACTION_PLAY);
+            nativeInvokeAction(PlaybackStateCompat.ACTION_PLAY);
           }
 
           @Override
           public void onRewind() {
             Log.i(TAG, "MediaSession action: REWIND");
-            nativeInvokeAction(PlaybackState.ACTION_REWIND);
+            nativeInvokeAction(PlaybackStateCompat.ACTION_REWIND);
           }
 
           @Override
           public void onSkipToNext() {
             Log.i(TAG, "MediaSession action: SKIP NEXT");
-            nativeInvokeAction(PlaybackState.ACTION_SKIP_TO_NEXT);
+            nativeInvokeAction(PlaybackStateCompat.ACTION_SKIP_TO_NEXT);
           }
 
           @Override
           public void onSkipToPrevious() {
             Log.i(TAG, "MediaSession action: SKIP PREVIOUS");
-            nativeInvokeAction(PlaybackState.ACTION_SKIP_TO_PREVIOUS);
+            nativeInvokeAction(PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS);
           }
 
           @Override
           public void onSeekTo(long pos) {
             Log.i(TAG, "MediaSession action: SEEK " + pos);
-            nativeInvokeAction(PlaybackState.ACTION_SEEK_TO, pos);
+            nativeInvokeAction(PlaybackStateCompat.ACTION_SEEK_TO, pos);
           }
         });
     // |metadataBuilder| may still have no fields at this point, yielding empty metadata.
@@ -209,7 +209,7 @@ public class CobaltMediaSession
       // This shouldn't happen, but pause playback to be nice if it does.
       if (res != AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
         Log.w(TAG, "Audiofocus action: PAUSE (not granted)");
-        nativeInvokeAction(PlaybackState.ACTION_PAUSE);
+        nativeInvokeAction(PlaybackStateCompat.ACTION_PAUSE);
       }
     } else {
       if (Build.VERSION.SDK_INT < 26) {
@@ -264,7 +264,7 @@ public class CobaltMediaSession
         Log.i(TAG, "Audiofocus loss" + logExtra);
         if (playbackState == PLAYBACK_STATE_PLAYING) {
           Log.i(TAG, "Audiofocus action: PAUSE");
-          nativeInvokeAction(PlaybackState.ACTION_PAUSE);
+          nativeInvokeAction(PlaybackStateCompat.ACTION_PAUSE);
         }
         break;
       case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
@@ -281,7 +281,7 @@ public class CobaltMediaSession
         volumeListener.onUpdateVolume(1.0f);
         if (transientPause && playbackState == PLAYBACK_STATE_PAUSED) {
           Log.i(TAG, "Audiofocus action: PLAY");
-          nativeInvokeAction(PlaybackState.ACTION_PLAY);
+          nativeInvokeAction(PlaybackStateCompat.ACTION_PLAY);
         }
         break;
       default: // fall out
@@ -334,7 +334,7 @@ public class CobaltMediaSession
     // be none the wiser.
     playbackStateBuilder.setState(
         playbackState,
-        PlaybackState.PLAYBACK_POSITION_UNKNOWN,
+        PlaybackStateCompat.PLAYBACK_POSITION_UNKNOWN,
         playbackState == PLAYBACK_STATE_PLAYING ? 1.0f : 0.0f);
     configureMediaFocus(PLAYBACK_STATE_NONE);
   }
@@ -359,8 +359,7 @@ public class CobaltMediaSession
           @Override
           public void run() {
             updateMediaSessionInternal(
-                playbackState, actions, positionMs, speed,
-                title, artist, album, artwork);
+                playbackState, actions, positionMs, speed, title, artist, album, artwork);
           }
         });
   }
@@ -398,43 +397,46 @@ public class CobaltMediaSession
     String stateName;
     switch (playbackState) {
       case PLAYBACK_STATE_PLAYING:
-        androidPlaybackState = PlaybackState.STATE_PLAYING;
+        androidPlaybackState = PlaybackStateCompat.STATE_PLAYING;
         stateName = "PLAYING";
         break;
       case PLAYBACK_STATE_PAUSED:
-        androidPlaybackState = PlaybackState.STATE_PAUSED;
+        androidPlaybackState = PlaybackStateCompat.STATE_PAUSED;
         stateName = "PAUSED";
         break;
       case PLAYBACK_STATE_NONE:
       default:
-        androidPlaybackState = PlaybackState.STATE_NONE;
+        androidPlaybackState = PlaybackStateCompat.STATE_NONE;
         stateName = "NONE";
         break;
     }
 
-    Log.i(TAG, String.format(
-        "MediaSession state: %s, position: %d ms, speed: %f x", stateName, positionMs, speed));
+    Log.i(
+        TAG,
+        String.format(
+            "MediaSession state: %s, position: %d ms, speed: %f x", stateName, positionMs, speed));
 
     playbackStateBuilder =
-        new PlaybackState.Builder()
+        new PlaybackStateCompat.Builder()
             .setActions(actions)
             .setState(androidPlaybackState, positionMs, speed);
     mediaSession.setPlaybackState(playbackStateBuilder.build());
 
     // Reset the metadata to make sure we don't retain any fields from previous playback.
-    metadataBuilder = new MediaMetadata.Builder();
+    metadataBuilder = new MediaMetadataCompat.Builder();
     metadataBuilder
-        .putString(MediaMetadata.METADATA_KEY_TITLE, title)
-        .putString(MediaMetadata.METADATA_KEY_ARTIST, artist)
-        .putString(MediaMetadata.METADATA_KEY_ALBUM, album)
-        .putBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART, artworkLoader.getOrLoadArtwork(artwork));
+        .putString(MediaMetadataCompat.METADATA_KEY_TITLE, title)
+        .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, artist)
+        .putString(MediaMetadataCompat.METADATA_KEY_ALBUM, album)
+        .putBitmap(
+            MediaMetadataCompat.METADATA_KEY_ALBUM_ART, artworkLoader.getOrLoadArtwork(artwork));
     // Update the metadata as soon as we can - even before artwork is loaded.
     mediaSession.setMetadata(metadataBuilder.build());
   }
 
   @Override
   public void onArtworkLoaded(Bitmap bitmap) {
-    metadataBuilder.putBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART, bitmap);
+    metadataBuilder.putBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, bitmap);
     mediaSession.setMetadata(metadataBuilder.build());
   }
 }
