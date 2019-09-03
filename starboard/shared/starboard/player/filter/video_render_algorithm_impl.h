61cf6e112e191f8c0b3feb5d2b97bb9859985b6a
Xiaoming Shi <xiaomings@google.com>
2019-09-03 16:21:59 -0700

    Implement cadence release control
    1. Implemented VideoFrameCadencePatternGenerator to generate the video
       frame cadence pattern according to the graphics refresh rate and the
       video frame rate.
    2. Implemented VideoFrameRateEstimator to estimate the frame rate of the
       video usings the timestamps of its frames.
    3. Implemented tests for the above classes.
    4. Refine VideoRenderAlgorithmImpl to release the video frames according
       to the right cadence.
    5. Refine VideoRenderer to only accept video frame with timestamps
       greater than the timestamp of the last video frame, to not allow two
       frames with the same timestamps in the backlog.
    5. Enable the cadence release control on linux and tvos.
    6. Reduce log message on loop filter enable/disable on tvos.
    
    b/140424759
    
    Change-Id: Ia9b89e6751f7e264157d65f4264c74b42123ef78

--- starboard/shared/starboard/player/filter/video_render_algorithm_impl.h
+++ starboard/shared/starboard/player/filter/video_render_algorithm_impl.h
@@ -15,6 +15,7 @@
 #ifndef STARBOARD_SHARED_STARBOARD_PLAYER_FILTER_VIDEO_RENDER_ALGORITHM_IMPL_H_
 #define STARBOARD_SHARED_STARBOARD_PLAYER_FILTER_VIDEO_RENDER_ALGORITHM_IMPL_H_
 
+#include <functional>
 #include <list>
 
 #include "starboard/common/ref_counted.h"
@@ -22,7 +23,9 @@
 #include "starboard/shared/internal_only.h"
 #include "starboard/shared/starboard/player/filter/common.h"
 #include "starboard/shared/starboard/player/filter/media_time_provider.h"
+#include "starboard/shared/starboard/player/filter/video_frame_cadence_pattern_generator.h"
 #include "starboard/shared/starboard/player/filter/video_frame_internal.h"
+#include "starboard/shared/starboard/player/filter/video_frame_rate_estimator.h"
 #include "starboard/shared/starboard/player/filter/video_render_algorithm.h"
 #include "starboard/shared/starboard/player/filter/video_renderer_sink.h"
 #include "starboard/time.h"
@@ -35,20 +38,35 @@ namespace filter {
 
 class VideoRenderAlgorithmImpl : public VideoRenderAlgorithm {
  public:
-  VideoRenderAlgorithmImpl();
+  typedef std::function<double()> GetRefreshRateFn;
+
+  explicit VideoRenderAlgorithmImpl(
+      const GetRefreshRateFn& get_refresh_rate_fn = GetRefreshRateFn());
+
   void Render(MediaTimeProvider* media_time_provider,
               std::list<scoped_refptr<VideoFrame>>* frames,
               VideoRendererSink::DrawFrameCB draw_frame_cb) override;
+  void Reset() override;
   int GetDroppedFrames() override { return dropped_frames_; }
 
  private:
+  void RenderWithCadence(MediaTimeProvider* media_time_provider,
+                         std::list<scoped_refptr<VideoFrame>>* frames,
+                         VideoRendererSink::DrawFrameCB draw_frame_cb);
+
+  const GetRefreshRateFn get_refresh_rate_fn_;
+
+  VideoFrameCadencePatternGenerator cadence_pattern_generator_;
+  VideoFrameRateEstimator frame_rate_estimate_;
+
 #if SB_PLAYER_FILTER_ENABLE_STATE_CHECK
   SbTime media_time_of_last_render_call_;
   SbTime system_time_of_last_render_call_;
 #endif  // SB_PLAYER_FILTER_ENABLE_STATE_CHECK
 
-  SbTime last_frame_timestamp_;
-  int dropped_frames_;
+  SbTime last_frame_timestamp_ = -1;
+  int current_frame_rendered_times_ = -1;
+  int dropped_frames_ = 0;
 };
 
 }  // namespace filter
