23a59ff3bb2cf39d2190154c7d002420e7880904
Arjun Menon <agmenon@google.com>
2019-09-13 16:01:24 -0700

    [android] Clean up version checks for SB API
    The preprocessor if checks for the API version that are affected in this
    CL are not necessary because the configuration_public.h file defines the
    SB_API_VERSION as experimental.
    
    Change-Id: I7ea3bde25aa46710710a2316a2651dedf6176340

--- starboard/android/shared/media_decoder.cc
+++ starboard/android/shared/media_decoder.cc
@@ -18,11 +18,9 @@
 #include "starboard/android/shared/jni_utils.h"
 #include "starboard/android/shared/media_common.h"
 #include "starboard/audio_sink.h"
-#if SB_API_VERSION >= 11
-#include "starboard/format_string.h"
-#endif  // SB_API_VERSION >= 11
 #include "starboard/common/log.h"
 #include "starboard/common/string.h"
+#include "starboard/format_string.h"
 #include "starboard/shared/pthread/thread_create_priority.h"
 
 namespace starboard {
