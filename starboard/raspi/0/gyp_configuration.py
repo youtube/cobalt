57d21fe640004d48005df7500c221775cf9dd016
brianting <brianting@google.com>
2019-09-17 15:46:47 -0700

    Filter player_filter_tests from raspi-0.
    Update player_filter_tests from individually filtering all tests to
    using test_filter.FILTER_ALL so that player_filter_tests is not run or
    loaded. This avoids the case where a SIGABRT crash occurs even though
    all player_filter_tests were individually filtered.
    
    b/138579487
    
    Change-Id: I62dfeebd89d1e84712ddfeec341d0d21b274a721

--- starboard/raspi/0/gyp_configuration.py
+++ starboard/raspi/0/gyp_configuration.py
@@ -41,10 +41,10 @@ class Raspi0PlatformConfig(shared_configuration.RaspiPlatformConfig):
           # TODO: debug these failures.
           'SbPlayerTest.MultiPlayer',  # crashes
       ],
-      # Temporarily disable most of the tests until we can narrow it down to the
-      # minimum number of cases that are real test failures.
+      # Consistently failing. If all tests are individually filtered it still
+      # fails due to SIGABRT despite no tests run.
       'player_filter_tests': [
-          'VideoDecoderTests/VideoDecoderTest.DecodeFullGOP/0'
+          test_filter.FILTER_ALL
       ]
   }
 
