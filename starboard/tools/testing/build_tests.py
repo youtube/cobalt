3ca6e48b156b05c7c11ff40c68cf7d1eff9c488b
Cobalt Team <cobalt-team@google.com>
2019-09-25 16:24:27 -0700

    Build app_launcher_zip whenever building tests.
    This ensures that app_launcher.zip is in the out directory whenever unit
    tests, blackbox tests, or performance tests are built so that it's there
    for Mobile Harness in case it's used for running the tests.
    
    b/141478649
    
    Change-Id: Ib14229c4e04cd25fb7a7f3ea606893e648d0c301
    (cherry picked from commit 678886fe9d9679756e67a0f6846791908f0593e0)

--- starboard/tools/testing/build_tests.py
+++ starboard/tools/testing/build_tests.py
@@ -1,6 +1,24 @@
+# Copyright 2018 The Cobalt Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Common code for building various types of targets, typically tests."""
+
 import logging
 import os
 import subprocess
+
+APP_LAUNCHER_TARGET = 'app_launcher_zip'
+
 
 def BuildTargets(targets, out_directory, dry_run=False, extra_build_flags=[]):
   """Builds all specified targets.
@@ -16,6 +34,7 @@ def BuildTargets(targets, out_directory, dry_run=False, extra_build_flags=[]):
   if dry_run:
     args_list.append("-n")
 
+  args_list.append(APP_LAUNCHER_TARGET)
   args_list.extend(["{}_deploy".format(test_name) for test_name in targets])
   args_list.extend(extra_build_flags)
 
