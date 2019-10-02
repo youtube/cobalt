f8838b07fbd120b024c0a7697934bdf94c08ad90
Cobalt Team <cobalt-team@google.com>
2019-10-02 21:54:39 +0000

    Suppress UnknownElement error logging for custom elements.
    b/141876897
    
    Spec for custom element naming found here:
    https://html.spec.whatwg.org/multipage/custom-elements.html#custom-elements-core-concepts:prod-pcenchar
    
    Change-Id: I926e04482282f5fff076ce27fe3be9999acee34e

--- cobalt/dom/html_element_factory.cc
+++ cobalt/dom/html_element_factory.cc
@@ -15,6 +15,7 @@
 #include "cobalt/dom/html_element_factory.h"
 
 #include "base/bind.h"
+#include "base/third_party/icu/icu_utf.h"
 #include "cobalt/dom/html_anchor_element.h"
 #include "cobalt/dom/html_audio_element.h"
 #include "cobalt/dom/html_body_element.h"
@@ -54,6 +55,91 @@ scoped_refptr<HTMLElement> CreateHTMLElementWithTagNameT(
   return new T(document, base::Token(local_name));
 }
 
+bool IsValidAsciiChar(char32_t c) {
+  const bool isLowerAscii = c >= 'a' && c <= 'z';
+  const bool isLowerDigit = c >= '0' && c <= '9';
+  return isLowerAscii || isLowerDigit || c == '.' || c == '_';
+}
+
+// Grandfathered HTML elements that meet CustomElement naming spec:
+// https://html.spec.whatwg.org/multipage/custom-elements.html#valid-custom-element-name
+bool IsBlacklistedTag(const char* tag, int length) {
+  switch (length) {
+    case 9:
+      if (SbStringCompareAll(tag, "font-face") == 0) {
+        return true;
+      }
+      break;
+    case 13:
+      if (SbStringCompareAll(tag, "font-face-src") == 0 ||
+          SbStringCompareAll(tag, "missing-glyph") == 0 ||
+          SbStringCompareAll(tag, "color-profile") == 0 ||
+          SbStringCompareAll(tag, "font-face-uri") == 0) {
+        return true;
+      }
+      break;
+    case 14:
+      if (SbStringCompareAll(tag, "font-face-name") == 0 ||
+          SbStringCompareAll(tag, "annotation-xml") == 0) {
+        return true;
+      }
+      break;
+    case 16:
+      if (SbStringCompareAll(tag, "font-face-format") == 0) {
+        return true;
+      }
+      break;
+    default:
+      return false;
+  }
+  return false;
+}
+
+// Follows naming spec at
+// https://html.spec.whatwg.org/multipage/custom-elements.html#valid-custom-element-name
+bool IsValidCustomElementName(base::Token tag_name) {
+  // Consider adding support for customElements.define in order to
+  // formally register CustomElements rather than filter out errors
+  // for all potential custom names.
+
+  const char* tag = tag_name.c_str();
+  char c = tag[0];
+  if (c < 'a' || c > 'z') {
+    return false;
+  }
+  bool contains_hyphen = false;
+  int length = 1;
+
+  while ((c = tag[length]) != '\0') {
+    // Early return to avoid utf32 conversion cost for most cases.
+    if (IsValidAsciiChar(c)) {
+      length++;
+      continue;
+    }
+    if (c == '-') {
+      contains_hyphen = true;
+      length++;
+      continue;
+    }
+    base_icu::UChar32 c32;
+
+    CBU8_NEXT(tag, length, -1, c32);
+    bool is_valid_char =
+        c32 == 0xb7 || (c32 >= 0xc0 && c32 <= 0xd6) ||
+        (c32 >= 0xd8 && c32 <= 0xf6) || (c32 >= 0xf8 && c32 <= 0x037d) ||
+        (c32 >= 0x037f && c32 <= 0x1fff) || (c32 >= 0x200c && c32 <= 0x200d) ||
+        (c32 >= 0x203f && c32 <= 0x2040) || (c32 >= 0x2070 && c32 <= 0x218f) ||
+        (c32 >= 0x2c00 && c32 <= 0x2fef) || (c32 >= 0x3001 && c32 <= 0xd7ff) ||
+        (c32 >= 0xf900 && c32 <= 0xfdcf) || (c32 >= 0xfdf0 && c32 <= 0xfffd) ||
+        (c32 >= 0x00010000 && c32 <= 0x000effff);
+    if (!is_valid_char) {
+      return false;
+    }
+  }
+
+  return contains_hyphen && !IsBlacklistedTag(tag, length);
+}
+
 }  // namespace
 
 HTMLElementFactory::HTMLElementFactory() {
@@ -89,7 +175,8 @@ scoped_refptr<HTMLElement> HTMLElementFactory::CreateHTMLElement(
   if (iter != tag_name_to_create_html_element_t_callback_map_.end()) {
     return iter->second.Run(document);
   } else {
-    LOG(WARNING) << "Unknown HTML element: <" << tag_name << ">.";
+    LOG_IF(WARNING, !IsValidCustomElementName(tag_name))
+        << "Unknown HTML element: <" << tag_name << ">.";
     return new HTMLUnknownElement(document, tag_name);
   }
 }
