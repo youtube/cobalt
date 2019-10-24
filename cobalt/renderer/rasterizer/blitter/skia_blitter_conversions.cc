976b535d1b03b9cc026c4ea9d0e1ffe0aa6a4f1a
Andrew Savage <andrewsavage@google.com>
2019-10-24 13:59:58 -0700

    Require blitter API.
    This change requires the blitter API to be implemented for Starboard
    versions 12 and higher. Most of the change only changes the macros in
    files, but auto_mem_settings.cc performs a runtime check on the system.
    
    Cobalt's binary size changes when we include all different
    renderers: measuring the stripped binary size without the changes,
    linux-x64x11_gold/cobalt is 30977488 bytes and
    linux-x64x11-blittergles/cobalt is 28929424 bytes.
    With the changes,
    linux-x64x11_gold/cobalt is 30993872 bytes and
    linux-x64x11-blittergles_gold/cobalt is 31002064 bytes,
    which results in a delta of 16.4Ki and 2072.6Ki, respectively.
    
    b/139739956
    
    Change-Id: I92f02f6145db5531c406a99776633d4e1aa37a6c

--- cobalt/renderer/rasterizer/blitter/skia_blitter_conversions.cc
+++ cobalt/renderer/rasterizer/blitter/skia_blitter_conversions.cc
@@ -16,7 +16,7 @@
 
 #include "base/logging.h"
 
-#if SB_HAS(BLITTER)
+#if SB_API_VERSION >= SB_BLITTER_REQUIRED_VERSION || SB_HAS(BLITTER)
 
 namespace cobalt {
 namespace renderer {
@@ -40,4 +40,4 @@ SbBlitterPixelDataFormat SkiaToBlitterPixelFormat(SkColorType skia_format) {
 }  // namespace renderer
 }  // namespace cobalt
 
-#endif  // #if SB_HAS(BLITTER)
+#endif  // #if SB_API_VERSION >= SB_BLITTER_REQUIRED_VERSION || SB_HAS(BLITTER)
