52b77aab6d539a91a0d72cd0ae90d94fc8b0710e
Cobalt Team <cobalt-team@google.com>
2019-10-25 09:09:31 -0700

    Add library + content directory switches to ELF Loader
    This change adds the ability to use two new command line switches with
    the ELF Loader. These switches, evergreen_library and evergreen_content,
    are used to specify which shared library to load and where it's content
    is.
    
    This change also makes the ELF Loader a singleton. The static method
    ElfLoader::Get() will return the currently allocated instance.
    
    These changes are a prerequisite for using the test_runner.py script
    with evergreen builds.
    
    b/143380059
    
    Change-Id: I7a291322a0a05a1017ec71b4411804da8446b245

---
+++ starboard/elf_loader/elf_loader_switches.h
@@ -0,0 +1,29 @@
+// Copyright 2019 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef STARBOARD_ELF_LOADER_ELF_LOADER_SWITCHES_H_
+#define STARBOARD_ELF_LOADER_ELF_LOADER_SWITCHES_H_
+
+#include "starboard/configuration.h"
+
+namespace starboard {
+namespace elf_loader {
+
+extern const char kEvergreenLibrary[];
+extern const char kEvergreenContent[];
+
+}  // namespace elf_loader
+}  // namespace starboard
+
+#endif  // STARBOARD_ELF_LOADER_ELF_LOADER_SWITCHES_H_
