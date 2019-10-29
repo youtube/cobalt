83d71fce446281a99cdcde553857e8fafb35e7f8
Jason Zhang <jasonzh@google.com>
2019-10-29 01:30:23 +0000

    Revert "Support attribute HTMLMediaElement.muted"
    This reverts commit 14a630dcd4d728ba433b4dd0af5d8125c5f8b49e.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I21ad9e7617f90439fb812c67fb30264ce809db75

--- cobalt/dom/html_media_element.cc
+++ cobalt/dom/html_media_element.cc
@@ -593,10 +593,6 @@ void HTMLMediaElement::OnInsertedIntoDocument() {
   if (!src.empty()) {
     set_src(src);
   }
-
-  if (HasAttribute("muted")) {
-    set_muted(true);
-  }
 }
 
 void HTMLMediaElement::TraceMembers(script::Tracer* tracer) {
@@ -1603,7 +1599,7 @@ void HTMLMediaElement::PlaybackStateChanged() {
 
 void HTMLMediaElement::SawUnsupportedTracks() { NOTIMPLEMENTED(); }
 
-float HTMLMediaElement::Volume() const { return muted_ ? 0 : volume(NULL); }
+float HTMLMediaElement::Volume() const { return volume(NULL); }
 
 void HTMLMediaElement::SourceOpened(ChunkDemuxer* chunk_demuxer) {
   TRACE_EVENT0("cobalt::dom", "HTMLMediaElement::SourceOpened()");
