95c9ee97c2cceafb4e43eb3316f53f855a514dd9
Cobalt Team <cobalt-team@google.com>
2019-10-28 17:13:29 -0700

    Update Android configurations to use sabi.json manifests
    This change updates the android configurations to use sabi.json
    manifests.
    
    b/143107780
    
    Change-Id: Iea29b070910774291b8ac9e1bea4c68b609a9d7f

--- starboard/android/x86/gyp_configuration.py
+++ starboard/android/x86/gyp_configuration.py
@@ -17,4 +17,7 @@ from starboard.android.shared import gyp_configuration as shared_configuration
 
 
 def CreatePlatformConfig():
-  return shared_configuration.AndroidConfiguration('android-x86', 'x86')
+  return shared_configuration.AndroidConfiguration(
+      'android-x86',
+      'x86',
+      sabi_json_path='starboard/evergreen/sabi/ia32/sabi.json')
