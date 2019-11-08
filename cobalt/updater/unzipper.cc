56808760635a14aaafda8b582b1559d381cc29d7
Cobalt Team <cobalt-team@google.com>
2019-11-08 14:02:18 -0800

    Calculate and output the time to unzip cobalt.crx
    This measurement is not perfectly precise. The precise method is to
    record the finish time in the callback function of zip::Unzip, which is
    ComponentUnpacker::EndUnzipping. But to do that, more changes to the
    update_client and zip library are required, which increase the
    complexity to rebase in the future. I measured the offset of the current
    method: it's only about 0.1ms.
    
    b/144180696
    
    Change-Id: I42182c8db2d4aed4683743f2581446996d40e6db

--- cobalt/updater/unzipper.cc
+++ cobalt/updater/unzipper.cc
@@ -7,6 +7,7 @@
 #include <utility>
 #include "base/callback.h"
 #include "base/files/file_path.h"
+#include "starboard/time.h"
 #include "third_party/zlib2/google/zip.h"
 
 namespace cobalt {
@@ -20,9 +21,14 @@ class UnzipperImpl : public update_client::Unzipper {
 
   void Unzip(const base::FilePath& zip_path, const base::FilePath& output_path,
              UnzipCompleteCallback callback) override {
+    SbTimeMonotonic time_before_unzip = SbTimeGetMonotonicNow();
     std::move(callback).Run(zip::Unzip(zip_path, output_path));
+    SbTimeMonotonic time_unzip_took =
+        SbTimeGetMonotonicNow() - time_before_unzip;
     SB_LOG(INFO) << "Unzip file path = " << zip_path;
     SB_LOG(INFO) << "output_path = " << output_path;
+    SB_LOG(INFO) << "Unzip took " << time_unzip_took / kSbTimeMillisecond
+                 << " milliseconds.";
   }
 };
 
