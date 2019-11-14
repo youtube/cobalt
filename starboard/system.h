75b1d7029e10e9b889dfe46bbd49cea49f334d85
Andrew Savage <andrewsavage@google.com>
2019-11-14 12:06:51 -0800

    Add necessary SB_EXPORT to starboard function
    b/144511384
    
    Change-Id: I334599c2d376f4b4efb5faf55b7a4344601d1bd2

--- starboard/system.h
+++ starboard/system.h
@@ -630,10 +630,11 @@ SB_EXPORT const void* SbSystemGetExtension(const char* name);
 // (or greater), since 32-bytes will be written into it.
 // Returns false in the case of an error, or if it is not implemented.  In this
 // case the contents of |digest| will be undefined.
-bool SbSystemSignWithCertificationSecretKey(const uint8_t* message,
-                                            size_t message_size_in_bytes,
-                                            uint8_t* digest,
-                                            size_t digest_size_in_bytes);
+SB_EXPORT bool SbSystemSignWithCertificationSecretKey(
+    const uint8_t* message,
+    size_t message_size_in_bytes,
+    uint8_t* digest,
+    size_t digest_size_in_bytes);
 #endif
 
 #ifdef __cplusplus
