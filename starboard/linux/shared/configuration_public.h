d2775bf9110a0e82fc77b9ab655fae137e63b3f3
Andrew Savage <andrewsavage@google.com>
2019-12-11 17:10:35 -0800

    Replace SB_MALLOC_ALIGNMENT
    This change replaces the macro SB_MALLOC_ALIGNMENT with the extern
    variable kSbMallocAlignment.
    
    b/142124156
    
    Change-Id: Ib2f37bab6b2a0c08ccf9ff39e37b48f86ee0172a

--- starboard/linux/shared/configuration_public.h
+++ starboard/linux/shared/configuration_public.h
@@ -287,8 +287,10 @@
 // specify that.
 #define SB_NETWORK_IO_BUFFER_ALIGNMENT 16
 
+#if SB_API_VERSION < SB_FEATURE_RUNTIME_CONFIGS_VERSION
 // Determines the alignment that allocations should have on this platform.
 #define SB_MALLOC_ALIGNMENT ((size_t)16U)
+#endif  // SB_API_VERSION < SB_FEATURE_RUNTIME_CONFIGS_VERSION
 
 // Determines the threshhold of allocation size that should be done with mmap
 // (if available), rather than allocated within the core heap.
