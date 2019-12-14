162fc6d784da8a35dbf7ca2f45e7354d06171641
Cobalt Team <cobalt-team@google.com>
2019-12-13 17:03:36 -0800

    Make ELF Loader propagate all events through, load on preload
    Change-Id: I874541fe6998e40a62c6557687840d6afa86160b

--- starboard/elf_loader/sandbox.cc
+++ starboard/elf_loader/sandbox.cc
@@ -25,60 +25,55 @@ starboard::elf_loader::ElfLoader g_elf_loader;
 void (*g_sb_event_func)(const SbEvent*) = NULL;
 
 void SbEventHandle(const SbEvent* event) {
-  switch (event->type) {
-    case kSbEventTypeStart: {
-      SbEventStartData* data = static_cast<SbEventStartData*>(event->data);
+  if ((event->type == kSbEventTypePreload) ||
+      (event->type == kSbEventTypeStart)) {
+    if (!g_sb_event_func) {
+      const SbEventStartData* data =
+          static_cast<SbEventStartData*>(event->data);
+      const starboard::shared::starboard::CommandLine command_line(
+          data->argument_count,
+          const_cast<const char**>(data->argument_values));
+      std::string library_path =
+          command_line.GetSwitchValue(
+              starboard::elf_loader::kEvergreenLibrary);
+      std::string content_path =
+          command_line.GetSwitchValue(
+              starboard::elf_loader::kEvergreenContent);
+
+      if (library_path.empty()) {
+        SB_LOG(ERROR) << "Library must be specified with --"
+                      << starboard::elf_loader::kEvergreenLibrary
+                      << "=path/to/library/relative/to/loader/content.";
+        return;
+      }
+      if (content_path.empty()) {
+        SB_LOG(ERROR) << "Content must be specified with --"
+                      << starboard::elf_loader::kEvergreenContent
+                      << "=path/to/content/relative/to/loader/content.";
+        return;
+      }
+      if (!g_elf_loader.Load(library_path, content_path, true)) {
+        SB_NOTREACHED() << "Failed to load library at '"
+                        << g_elf_loader.GetLibraryPath() << "'.";
+        return;
+      }
+
+      SB_LOG(INFO) << "Successfully loaded '" << g_elf_loader.GetLibraryPath()
+                   << "'.";
+
+      g_sb_event_func = reinterpret_cast<void (*)(const SbEvent*)>(
+          g_elf_loader.LookupSymbol("SbEventHandle"));
+
       if (!g_sb_event_func) {
-        const starboard::shared::starboard::CommandLine command_line(
-            data->argument_count,
-            const_cast<const char**>(data->argument_values));
-        std::string library_path =
-            command_line.GetSwitchValue(
-                starboard::elf_loader::kEvergreenLibrary);
-        std::string content_path =
-            command_line.GetSwitchValue(
-                starboard::elf_loader::kEvergreenContent);
-
-        if (library_path.empty()) {
-          SB_LOG(ERROR) << "Library must be specified with --"
-                        << starboard::elf_loader::kEvergreenLibrary
-                        << "=path/to/library/relative/to/loader/content.";
-          return;
-        }
-        if (content_path.empty()) {
-          SB_LOG(ERROR) << "Content must be specified with --"
-                        << starboard::elf_loader::kEvergreenContent
-                        << "=path/to/content/relative/to/loader/content.";
-          return;
-        }
-        if (!g_elf_loader.Load(library_path, content_path, true)) {
-          SB_LOG(INFO) << "Failed to load library at '"
-                       << g_elf_loader.GetLibraryPath() << "'.";
-          SB_NOTREACHED();
-        }
-
-        SB_LOG(INFO) << "Successfully loaded '" << g_elf_loader.GetLibraryPath()
-                     << "'.";
-
-        g_sb_event_func = reinterpret_cast<void (*)(const SbEvent*)>(
-            g_elf_loader.LookupSymbol("SbEventHandle"));
-
-        if (!g_sb_event_func) {
-          SB_LOG(INFO) << "Failed to find SbEventHandle.";
-          return;
-        }
-
-        SB_LOG(INFO) << "Found SbEventHandle at address=0x" << std::hex
-                     << g_sb_event_func << ".";
-
-        g_sb_event_func(event);
+        SB_LOG(ERROR) << "Failed to find SbEventHandle.";
+        return;
       }
-      break;
+
+      SB_LOG(INFO) << "Found SbEventHandle at address=0x" << std::hex
+                   << g_sb_event_func << ".";
     }
-    default: {
-      if (g_sb_event_func) {
-        g_sb_event_func(event);
-      }
-    }
+  }
+  if (g_sb_event_func) {
+    g_sb_event_func(event);
   }
 }
