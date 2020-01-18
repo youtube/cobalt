5cfc58a75f2dc95d3c3a211db66081e6315ab966
Cobalt Team <cobalt-team@google.com>
2020-01-17 16:02:15 -0800

    Add CobaltStorageLoader.setObserver() to notify when storage changes.
    The construction of the inner StorageFileObserver is complicated by the
    fact that the FileObserver(String) constructor was deprecated in API 29
    in favor of FileObserver(File). Therefore, there's a static create()
    factory method to call one constructor or the other, and that led to the
    inner class being static so it needs to keep its own filesDir File
    object rather than being able to access the one in its containing
    StorageFileObserver instance.
    
    b/136026790
    
    Change-Id: I03a75003f3a0d21cfff6cb3d3671309a0fd1533c

--- starboard/android/apk/app/src/main/java/dev/cobalt/storage/CobaltStorageLoader.java
+++ starboard/android/apk/app/src/main/java/dev/cobalt/storage/CobaltStorageLoader.java
@@ -16,8 +16,10 @@ package dev.cobalt.storage;
 
 import static dev.cobalt.util.Log.TAG;
 
-import android.text.TextUtils;
+import android.os.Build;
+import android.os.FileObserver;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import com.google.protobuf.InvalidProtocolBufferException;
 import dev.cobalt.storage.StorageProto.Storage;
 import dev.cobalt.util.Log;
@@ -27,9 +29,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
-/** A class to load Cobalt storage from the file system. */
+/**
+ * A class to load Cobalt storage from the file system.
+ */
 public class CobaltStorageLoader {
+
   private final File filesDir;
+  private final StorageFileObserver fileObserver;
 
   private static final String STORAGE_NAME_PREFIX = ".starboard";
   private static final String STORAGE_NAME_SUFFIX = ".storage";
@@ -45,6 +51,7 @@ public class CobaltStorageLoader {
       throw new IllegalArgumentException("A valid filesDir object is required");
     }
     this.filesDir = filesDir;
+    this.fileObserver = StorageFileObserver.create(filesDir);
   }
 
   /**
@@ -57,14 +64,20 @@ public class CobaltStorageLoader {
     if (fileName == null) {
       return Storage.getDefaultInstance();
     }
-    byte[] storageBlob = getStorageBlob(fileName);
+    Storage storage = loadStorageFile(new File(filesDir, fileName));
+    return (storage != null) ? storage : Storage.getDefaultInstance();
+  }
+
+  @Nullable
+  private static Storage loadStorageFile(File file) {
+    byte[] storageBlob = getStorageBlob(file);
     if (storageBlob == null) {
       Log.e(TAG, "Failed to get storage blob");
-      return Storage.getDefaultInstance();
+      return null;
     }
     if (!validateStorageBlob(storageBlob)) {
       Log.e(TAG, "Invalid storage blob");
-      return Storage.getDefaultInstance();
+      return null;
     }
     storageBlob = Arrays.copyOfRange(storageBlob, STORAGE_HEADER.length(), storageBlob.length);
     try {
@@ -72,10 +85,10 @@ public class CobaltStorageLoader {
     } catch (InvalidProtocolBufferException e) {
       Log.e(TAG, "Failed parsing the blob", e);
     }
-    return Storage.getDefaultInstance();
+    return null;
   }
 
-  private boolean validateStorageBlob(byte[] storageBlob) {
+  private static boolean validateStorageBlob(byte[] storageBlob) {
     if (storageBlob == null || storageBlob.length < STORAGE_HEADER.length()) {
       return false;
     }
@@ -84,12 +97,8 @@ public class CobaltStorageLoader {
   }
 
   @Nullable
-  private byte[] getStorageBlob(String fileName) {
-    if (TextUtils.isEmpty(fileName)) {
-      Log.e(TAG, "Invalid empty file name");
-      return null;
-    }
-    try (FileInputStream in = new FileInputStream(new File(filesDir, fileName));
+  private static byte[] getStorageBlob(File file) {
+    try (FileInputStream in = new FileInputStream(file);
         ByteArrayOutputStream out = new ByteArrayOutputStream()) {
       byte[] buffer = new byte[8192];
       int len;
@@ -111,7 +120,7 @@ public class CobaltStorageLoader {
       return null;
     }
     for (String fileName : fileNames) {
-      if (fileName.startsWith(STORAGE_NAME_PREFIX) && fileName.endsWith(STORAGE_NAME_SUFFIX)) {
+      if (isStorageFile(fileName)) {
         File storageFile = new File(filesDir, fileName);
         if (storageFile.length() > 0) {
           return fileName;
@@ -120,5 +129,88 @@ public class CobaltStorageLoader {
     }
     Log.w(TAG, "Failed to find storage file name");
     return null;
+  }
+
+  private static boolean isStorageFile(String fileName) {
+    return fileName != null
+        && fileName.startsWith(STORAGE_NAME_PREFIX)
+        && fileName.endsWith(STORAGE_NAME_SUFFIX);
+  }
+
+  /**
+   * Set the observer to be notified when storage changes.
+   *
+   * @param storageObserver will be called when storage changes.
+   */
+  public void setObserver(@Nullable CobaltStorageObserver storageObserver) {
+    fileObserver.setStorageObserver(storageObserver);
+  }
+
+  /**
+   * Observer that is called when Cobalt storage changes.
+   */
+  public interface CobaltStorageObserver {
+
+    /**
+     * Called when Cobalt storage changes.
+     *
+     * <p>This method is invoked on a special thread. It runs independently of any threads, so take
+     * care to use appropriate synchronization! Consider using Handler#post(Runnable) to shift event
+     * handling work to the main thread to avoid concurrency problems.
+     *
+     * @param snapshot the Cobalt storage as a proto message.
+     */
+    void onCobaltStorageChanged(Storage snapshot);
+  }
+
+  private static class StorageFileObserver extends FileObserver {
+
+    private final File filesDir;
+    private CobaltStorageObserver storageObserver = null;
+
+    /**
+     * Factory method to differentiate which constructor to use.
+     */
+    static StorageFileObserver create(File filesDir) {
+      if (Build.VERSION.SDK_INT >= 29) {
+        return new StorageFileObserver(filesDir);
+      } else {
+        return new StorageFileObserver(filesDir.getAbsolutePath());
+      }
+    }
+
+    @SuppressWarnings("deprecation")
+    StorageFileObserver(String filesDirPath) {
+      super(filesDirPath, MOVED_TO);
+      this.filesDir = new File(filesDirPath);
+    }
+
+    @RequiresApi(29)
+    StorageFileObserver(File filesDir) {
+      super(filesDir, MOVED_TO);
+      this.filesDir = filesDir;
+    }
+
+    void setStorageObserver(@Nullable CobaltStorageObserver storageObserver) {
+      if (this.storageObserver != null) {
+        stopWatching();
+      }
+      this.storageObserver = storageObserver;
+      if (this.storageObserver != null) {
+        startWatching();
+      }
+    }
+
+    @Override
+    public void onEvent(int event, @Nullable String fileName) {
+      // Expect a MOVED_TO event since SbStorageWrite() creates a temp file then moves it in place.
+      if (event != MOVED_TO || storageObserver == null || !isStorageFile(fileName)) {
+        return;
+      }
+      Storage storage = loadStorageFile(new File(filesDir, fileName));
+      if (storage != null) {
+        storageObserver.onCobaltStorageChanged(storage);
+      }
+    }
   }
 }
