761789bb05efa7abbe643980e832b7a37881b111
Liviu Rau <liviurau@chromium.org>
2020-02-11 16:19:50 +0100

    Whitespace to trigger bots
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ib15dd68154821b937d2b28f62411ba09b88e0b39
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2050235
    Reviewed-by: Liviu Rau <liviurau@chromium.org>
    git-subtree-dir: third_party/devtools
    git-subtree-split: 0d4095e47ca0e972e04efecf7772a8d12b60c665

---
+++ front_end/css_overview/CSSOverviewStartView.js
@@ -0,0 +1,45 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @unrestricted
+ */
+export class CSSOverviewStartView extends UI.Widget {
+  constructor(controller) {
+    super();
+    this.registerRequiredCSS('css_overview/cssOverviewStartView.css');
+
+    this._controller = controller;
+    this._render();
+  }
+
+  _render() {
+    const startButton = UI.createTextButton(
+        ls`Capture overview`, () => this._controller.dispatchEventToListeners(CssOverview.Events.RequestOverviewStart),
+        '', true /* primary */);
+
+    this.setDefaultFocusedElement(startButton);
+
+    const fragment = UI.Fragment.build`
+      <div class="vbox overview-start-view">
+        <h1>${ls`CSS Overview`}</h1>
+        <div>${startButton}</div>
+      </div>
+    `;
+
+    this.contentElement.appendChild(fragment.element());
+    this.contentElement.style.overflow = 'auto';
+  }
+}
+
+/* Legacy exported object */
+self.CssOverview = self.CssOverview || {};
+
+/* Legacy exported object */
+CssOverview = CssOverview || {};
+
+/**
+ * @constructor
+ */
+CssOverview.CSSOverviewStartView = CSSOverviewStartView;
