a66740c5adf5eb26d3aed421e9feb064bdf79217
Cobalt Team <cobalt-team@google.com>
2020-02-24 17:17:14 -0800

    Make some StarboardBridge methods 'protected'.
    getUserAgentAuxField() and the methods called for Cobalt lifecycle
    changes may be useful for apps to override and extend. Since they're
    called via JNI, it actually works now, but making them protected
    communicates the intention, and it allows the overridden implementation
    to call the base implementation.
    
    b/150170890
    
    Change-Id: Id5152de610ae9fb7ab2bacdf6fa30d69106f2cd5

--- starboard/android/apk/app/src/main/java/dev/cobalt/coat/StarboardBridge.java
+++ starboard/android/apk/app/src/main/java/dev/cobalt/coat/StarboardBridge.java
@@ -147,7 +147,7 @@ public class StarboardBridge {
 
   @SuppressWarnings("unused")
   @UsedByNative
-  void beforeStartOrResume() {
+  protected void beforeStartOrResume() {
     Log.i(TAG, "Prepare to resume");
     // Bring our platform services to life before resuming so that they're ready to deal with
     // whatever the web app wants to do with them as part of its start/resume logic.
@@ -160,7 +160,7 @@ public class StarboardBridge {
 
   @SuppressWarnings("unused")
   @UsedByNative
-  void beforeSuspend() {
+  protected void beforeSuspend() {
     Log.i(TAG, "Prepare to suspend");
     // We want the MediaSession to be deactivated immediately before suspending so that by the time
     // the launcher is visible our "Now Playing" card is already gone. Then Cobalt and the web app
@@ -174,7 +174,7 @@ public class StarboardBridge {
 
   @SuppressWarnings("unused")
   @UsedByNative
-  void afterStopped() {
+  protected void afterStopped() {
     starboardStopped = true;
     ttsHelper.shutdown();
     userAuthorizer.shutdown();
@@ -453,7 +453,7 @@ public class StarboardBridge {
   /** Returns string for kSbSystemPropertyUserAgentAuxField */
   @SuppressWarnings("unused")
   @UsedByNative
-  String getUserAgentAuxField() {
+  protected String getUserAgentAuxField() {
     StringBuilder sb = new StringBuilder();
 
     String packageName = appContext.getApplicationInfo().packageName;
