49cd4870ebbf5efc13ca6b3c2989c3d7ab3c9b76
Cobalt Team <cobalt-team@google.com>
2020-03-05 13:42:43 -0800

    Copy pointers out of ucontext while still in the signal handler.
    The ucontext pointer isn't valid after leaving the signal handler, so
    copy the interesting data out of it that we'll need to record for the
    profiler while still in the signal handler.
    
    b/144928511
    
    Change-Id: I91652b116c4226d1c912e8c124bf5cd4a7925d9e
    (cherry picked from commit a1655d97f498e6a0ff41f1270831d2eec166cf2d)

--- starboard/shared/pthread/thread_context_get_pointer.cc
+++ starboard/shared/pthread/thread_context_get_pointer.cc
@@ -21,88 +21,24 @@
 #include "starboard/common/log.h"
 #include "starboard/shared/pthread/thread_context_internal.h"
 
-// TODO: Implement for e.g. Mac OSX, BSD flavors, QNX - maybe in another file.
-#if !defined(__gnu_linux__)  // Note: __linux__ is undef'd for Starboard builds.
-#error "SbThreadContext is only implemented for Linux"
-#endif
-
-// UCLIBC pretends to be GLIBC.
-#if (defined(__GLIBC__) || defined(__GNU_LIBRARY__)) && !defined(__UCLIBC__)
-#define SB_IS_GLIBC 1
-#else
-#define SB_IS_GLIBC 0
-#endif
-
-// clang-format off
-#if SB_IS_ARCH_X86
-# if SB_IS_32_BIT
-#  error("TODO: Validate UCONTEXT macros on 32-bit X86")
-#  define UCONTEXT_IP(ucontext) ((ucontext)->uc_mcontext.gregs[REG_EIP])
-#  define UCONTEXT_SP(ucontext) ((ucontext)->uc_mcontext.gregs[REG_ESP])
-#  define UCONTEXT_FP(ucontext) ((ucontext)->uc_mcontext.gregs[REG_EBP])
-# elif SB_IS_64_BIT
-#  define UCONTEXT_IP(ucontext) ((ucontext)->uc_mcontext.gregs[REG_RIP])
-#  define UCONTEXT_SP(ucontext) ((ucontext)->uc_mcontext.gregs[REG_RSP])
-#  define UCONTEXT_FP(ucontext) ((ucontext)->uc_mcontext.gregs[REG_RBP])
-# endif
-#elif SB_IS_ARCH_ARM
-# if SB_IS_32_BIT
-#  if SB_IS_GLIBC && ((__GLIBC__ * 100 + __GLIBC_MINOR__) < 204)
-#   error("TODO: Validate UCONTEXT macros on 32-bit ARM w/ old GLIBC")
-#   define UCONTEXT_IP(ucontext) ((ucontext)->uc_mcontext.gregs[R15])
-#   define UCONTEXT_SP(ucontext) ((ucontext)->uc_mcontext.gregs[R13])
-#   define UCONTEXT_FP(ucontext) ((ucontext)->uc_mcontext.gregs[R11])
-#  else  // SB_IS_GLIBC < 2.04
-#   define UCONTEXT_IP(ucontext) ((ucontext)->uc_mcontext.arm_pc)
-#   define UCONTEXT_SP(ucontext) ((ucontext)->uc_mcontext.arm_sp)
-#   define UCONTEXT_FP(ucontext) ((ucontext)->uc_mcontext.arm_fp)
-#  endif  // SB_IS_GLIBC < 2.04
-# elif SB_IS_64_BIT
-#  error("TODO: Validate UCONTEXT macros on 64-bit ARM")
-#  define UCONTEXT_IP(ucontext) ((ucontext)->uc_mcontext.pc)
-#  define UCONTEXT_SP(ucontext) ((ucontext)->uc_mcontext.sp)
-#  define UCONTEXT_FP(ucontext) ((ucontext)->uc_mcontext.regs[29])
-# endif
-#elif SB_IS_ARCH_MIPS
-#  error("TODO: Validate UCONTEXT macros on MIPS")
-#  define UCONTEXT_IP(ucontext) ((ucontext)->uc_mcontext.pc)
-#  define UCONTEXT_SP(ucontext) ((ucontext)->uc_mcontext.gregs[29])
-#  define UCONTEXT_FP(ucontext) ((ucontext)->uc_mcontext.gregs[30])
-#elif SB_IS_ARCH_PPC
-# if SB_IS_GLIBC
-#  error("TODO: Validate UCONTEXT macros on PPC w/ GLIBC")
-#  define UCONTEXT_IP(ucontext) ((ucontext)->uc_mcontext.regs->nip)
-#  define UCONTEXT_SP(ucontext) ((ucontext)->uc_mcontext.regs->gpr[PT_R1])
-#  define UCONTEXT_FP(ucontext) ((ucontext)->uc_mcontext.regs->gpr[PT_R31])
-# else  // SB_IS_GLIBC
-#  error("TODO: Validate UCONTEXT macros on PPC")
-#  define UCONTEXT_IP(ucontext) ((ucontext)->uc_mcontext.gp_regs[32])
-#  define UCONTEXT_SP(ucontext) ((ucontext)->uc_mcontext.gp_regs[1])
-#  define UCONTEXT_FP(ucontext) ((ucontext)->uc_mcontext.gp_regs[31])
-# endif  // SB_IS_GLIBC
-#else  // SB_IS_ARCH_XXX
-# error "SbThreadContext isn't implemented for this CPU architecture"
-#endif  // SB_IS_ARCH_XXX
-// clang-format on
-
 bool SbThreadContextGetPointer(SbThreadContext context,
                                SbThreadContextProperty property,
                                void** out_value) {
-  if (context == kSbThreadContextInvalid || context->ucontext == nullptr) {
+  if (context == kSbThreadContextInvalid || context->ip_ == nullptr) {
     return false;
   }
 
   switch (property) {
     case kSbThreadContextInstructionPointer:
-      *out_value = reinterpret_cast<void*>(UCONTEXT_IP(context->ucontext));
+      *out_value = context->ip_;
       return true;
 
     case kSbThreadContextStackPointer:
-      *out_value = reinterpret_cast<void*>(UCONTEXT_SP(context->ucontext));
+      *out_value = context->sp_;
       return true;
 
     case kSbThreadContextFramePointer:
-      *out_value = reinterpret_cast<void*>(UCONTEXT_FP(context->ucontext));
+      *out_value = context->fp_;
       return true;
 
     default:
