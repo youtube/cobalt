b07c8ac7c3a5a68aa8eac8477374b522c13fa723
Yavor Goulishev <yavor@google.com>
2020-03-10 14:55:23 -0700

    Adding nplb Evergreen compatibility tests.
    This test is optional and should be passed only if
    the platform is deployed as Evergreen.
    
    b/145286186
    
    Change-Id: I56028cc3537afe5804cfc6a8d3cde18f5fff789e

---
+++ starboard/nplb/nplb_evergreen_compat_tests/executable_memory_test.cc
@@ -0,0 +1,52 @@
+// Copyright 2020 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/memory.h"
+#include "starboard/nplb/nplb_evergreen_compat_tests/checks.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+#if SB_IS(EVERGREEN_COMPATIBLE)
+
+namespace starboard {
+namespace nplb {
+namespace nplb_evergreen_compat_tests {
+
+namespace {
+
+const size_t kSize = 30 * 1024 * 1024;
+const size_t kSmallerSize = 15 * 1024 * 1024;
+
+class ExecutableMemoryTest : public ::testing::Test {
+ protected:
+  ExecutableMemoryTest() {}
+  ~ExecutableMemoryTest() {}
+};
+
+TEST_F(ExecutableMemoryTest, VerifyMemoryProtection) {
+  void* memory =
+      SbMemoryMap(kSize, kSbMemoryMapProtectWrite, "evergreen_buffer");
+  ASSERT_NE(SB_MEMORY_MAP_FAILED, memory);
+  SbMemorySet(memory, 0, kSize);
+  ASSERT_TRUE(SbMemoryProtect(
+      memory, kSmallerSize, kSbMemoryMapProtectRead | kSbMemoryMapProtectExec));
+  SbMemoryUnmap(memory, kSize);
+  EXPECT_TRUE(true);
+}
+
+}  // namespace
+}  // namespace nplb_evergreen_compat_tests
+}  // namespace nplb
+}  // namespace starboard
+
+#endif  // SB_IS(EVERGREEN_COMPATIBLE)
