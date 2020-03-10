5aa475ed6bb712d6ef6b20b28012d3039b3898dc
Yavor Goulishev <yavor@google.com>
2020-03-10 14:55:23 -0700

    Adding nplb Evergreen compatibility tests.
    This test is optional and should be passed only if
    the platform is deployed as Evergreen.
    
    b/145286186
    
    Change-Id: I56028cc3537afe5804cfc6a8d3cde18f5fff789e

---
+++ starboard/nplb/nplb_evergreen_compat_tests/nplb_evergreen_compat_tests.gyp
@@ -0,0 +1,45 @@
+# Copyright 2020 The Cobalt Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+{
+  'targets': [
+    {
+      'target_name': 'nplb_evergreen_compat_tests',
+      'type': '<(gtest_target_type)',
+      'sources': [
+        'checks.h',
+        'executable_memory_test.cc',
+        'sabi_test.cc',
+        'storage_test.cc',
+        '<(DEPTH)/starboard/common/test_main.cc',
+      ],
+      'dependencies': [
+        '<(DEPTH)/starboard/starboard.gyp:starboard',
+        '<(DEPTH)/testing/gmock.gyp:gmock',
+        '<(DEPTH)/testing/gtest.gyp:gtest',
+      ],
+    },
+    {
+      'target_name': 'nplb_evergreen_compat_tests_deploy',
+      'type': 'none',
+      'dependencies': [
+        'nplb_evergreen_compat_tests',
+      ],
+      'variables': {
+        'executable_name': 'nplb_evergreen_compat_tests',
+      },
+      'includes': [ '<(DEPTH)/starboard/build/deploy.gypi' ],
+    },
+  ],
+}
---
+++ starboard/nplb/nplb_evergreen_compat_tests/storage_test.cc
@@ -0,0 +1,93 @@
+// Copyright 2020 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+#include <vector>
+
+#include "starboard/common/log.h"
+#include "starboard/configuration.h"
+#include "starboard/file.h"
+#include "starboard/memory.h"
+#include "starboard/nplb/nplb_evergreen_compat_tests/checks.h"
+#include "starboard/system.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+#if SB_IS(EVERGREEN_COMPATIBLE)
+
+namespace starboard {
+namespace nplb {
+namespace nplb_evergreen_compat_tests {
+
+namespace {
+
+const char kFileName[] = "test_file.data";
+const size_t kBufSize = 128 * 1024 * 1024;  // 128 MB
+
+class StorageTest : public ::testing::Test {
+ protected:
+  StorageTest() {}
+  ~StorageTest() {}
+};
+
+void WriteBuffer(const char* file_path,
+                 const char* buffer,
+                 size_t buffer_size) {
+  SbFileError error;
+  ScopedFile file(file_path, kSbFileOpenAlways | kSbFileWrite, nullptr, &error);
+  ASSERT_EQ(kSbFileOk, error) << "Failed to open file for writing";
+  int bytes_written = file.WriteAll(buffer, buffer_size);
+  ASSERT_EQ(kBufSize, bytes_written);
+}
+
+void ReadBuffer(const char* file_path, char* buffer, size_t buffer_size) {
+  SbFileError error;
+  ScopedFile file(file_path, kSbFileOpenOnly | kSbFileRead, nullptr, &error);
+  ASSERT_EQ(kSbFileOk, error) << "Failed to open file for reading";
+  int count = file.ReadAll(buffer, buffer_size);
+  ASSERT_EQ(kBufSize, count);
+}
+
+TEST_F(StorageTest, VerifyStorageDirectory) {
+  std::vector<char> storage_dir(kSbFileMaxPath);
+  ASSERT_TRUE(SbSystemGetPath(kSbSystemPathStorageDirectory, storage_dir.data(),
+                              kSbFileMaxPath));
+
+  std::string file_path = storage_dir.data();
+  file_path += kSbFileSepString;
+  file_path += kFileName;
+  SB_LOG(INFO) << "file: " << file_path;
+
+  std::vector<char> buf(kBufSize);
+  SbMemorySet(buf.data(), 'A', kBufSize);
+
+  WriteBuffer(file_path.c_str(), buf.data(), kBufSize);
+
+  SbMemorySet(buf.data(), 0, kBufSize);
+
+  ReadBuffer(file_path.c_str(), buf.data(), kBufSize);
+
+  for (int i = 0; i < kBufSize; i++) {
+    ASSERT_EQ('A', buf[i]);
+  }
+
+  ASSERT_TRUE(SbFileDelete(file_path.data()));
+  ASSERT_FALSE(SbFileExists(file_path.data()));
+}
+
+}  // namespace
+}  // namespace nplb_evergreen_compat_tests
+}  // namespace nplb
+}  // namespace starboard
+
+#endif  // SB_IS(EVERGREEN_COMPATIBLE)
