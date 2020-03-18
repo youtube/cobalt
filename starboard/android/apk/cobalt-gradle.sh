973df54badd587cd39a237b7ed57b5dbd672fbe6
Cobalt Team <cobalt-team@google.com>
2020-03-17 17:57:09 -0700

    Install Android NDK using sdkmanager
    Now that we can specify a specific NDK version, use the SDK manager to
    install the NDK as just another component rather than downloading and
    installing the ZIP ourselves.
    
    Since the SDK manager takes care of updates, we no longer need the logic
    to generate and check our own stamp files.
    
    Always use the NDK that's downloaded as an SDK component, and ignore the
    deprecated ANDROID_NDK_HOME environment variable.
    
    This continues to use NDK 20 at this point, even though NDK 21 is
    already available. That can be updated separately in its own CL.
    
    b/151456894
    
    Change-Id: Ia47009652510bcc5a33f9780f45ec2dd1004bcd5

--- starboard/android/apk/cobalt-gradle.sh
+++ starboard/android/apk/cobalt-gradle.sh
@@ -22,7 +22,6 @@ GRADLE_ARGS=()
 while [ "$1" ]; do
   case "$1" in
     --sdk) shift; ANDROID_HOME="$1" ;;
-    --ndk) shift; ANDROID_NDK_HOME="$1" ;;
     --cache) shift; mkdir -p "$1";
              GRADLE_ARGS+=("--project-cache-dir" $(cd "$1"; pwd)) ;;
     --reset) RESET_GRADLE=1 ;;
@@ -50,9 +49,7 @@ if [[ "${RESET_GRADLE}" ]]; then
 fi
 
 export ANDROID_HOME
-export ANDROID_NDK_HOME
 echo "ANDROID_HOME=${ANDROID_HOME}"
-echo "ANDROID_NDK_HOME=${ANDROID_NDK_HOME}"
 
 # Allow parallel gradle builds, as defined by a COBALT_GRADLE_BUILD_COUNT envvar
 # or default to 1 if that's not set (so buildbot only runs 1 gradle at a time).
