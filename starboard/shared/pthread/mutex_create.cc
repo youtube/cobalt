9a27a620751f99a0df1eafad13335aeb3923c5b6
Yavor Goulishev <yavor@google.com>
2020-03-18 18:15:01 +0000

    Revert "Revert "Fix the pthread implementation of SbMutex and Sb..."
    Revert submission 156441-revert-156101-fix_pthread-IKXGAOXYNO
    
    Reason for revert: <Only Evergreen Failed, and probably needed a clean build>
    Reverted Changes:
    Iaeb0f858d:Revert "Re-enable tvOS simulator tests."
    Ia2d7d13b4:Revert "Fix the pthread implementation of SbMutex ...
    
    Change-Id: I4d94c35c37ee63742e0d4d2ada58dd3d32674d78

--- starboard/shared/pthread/mutex_create.cc
+++ starboard/shared/pthread/mutex_create.cc
@@ -19,13 +19,21 @@
 #include "starboard/common/log.h"
 #include "starboard/shared/pthread/is_success.h"
 #include "starboard/shared/pthread/types_internal.h"
+#include "starboard/shared/starboard/lazy_initialization_internal.h"
+
+using starboard::shared::starboard::SetInitialized;
 
 bool SbMutexCreate(SbMutex* mutex) {
-  SB_COMPILE_ASSERT(sizeof(SbMutex) >= sizeof(pthread_mutex_t),
-                    pthread_mutex_t_larger_than_sb_mutex);
+#if SB_API_VERSION >= SB_PORTABLE_THREAD_TYPES_VERSION
+  SB_COMPILE_ASSERT(sizeof(SbMutex) >= sizeof(SbMutexPrivate),
+                    sb_mutex_private_larger_than_sb_mutex);
+#endif
   if (!mutex) {
     return false;
   }
 
+#if SB_API_VERSION >= SB_PORTABLE_THREAD_TYPES_VERSION
+  SetInitialized(&(SB_INTERNAL_MUTEX(mutex)->initialized_state));
+#endif
   return IsSuccess(pthread_mutex_init(SB_PTHREAD_INTERNAL_MUTEX(mutex), NULL));
 }
