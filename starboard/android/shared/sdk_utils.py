f0c36408ec7eb5813684c10b54f61a13251d31f0
Kaido Kert <kaidokert@google.com>
2020-05-01 09:02:36 -0700

    Fix GYP errors when re-downloading android SDK
    b/155438178
    
    Change-Id: I6169cb3459617f4ea8fbf89251acd8295add8873

--- starboard/android/shared/sdk_utils.py
+++ starboard/android/shared/sdk_utils.py
@@ -88,7 +88,10 @@ def GetSdkPath():
 
 
 def _DownloadAndUnzipFile(url, destination_path):
-  os.makedirs(destination_path)
+  try:
+    os.makedirs(destination_path)
+  except FileExistsError:
+    pass
   dl_file = os.path.join(destination_path, 'tmp.zip')
   request = requests.get(url, allow_redirects=True)
   open(dl_file, 'wb').write(request.content)
