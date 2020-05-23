7957a0978a143aa2673b021b1d506594e85c4f03
Xiaoming Shi <xiaomings@google.com>
2020-05-23 14:01:12 -0700

    [media] Add statistics to logging and error message
    b/137410957
    b/142614264
    
    Change-Id: I4e8896754746d73c4236d8aadcdc002199bdcc31
    (cherry picked from commit 6877c56c436ec0ac9f1f9abfbef2fd542d971a74)

---
+++ cobalt/media/base/playback_statistics.h
@@ -0,0 +1,44 @@
+// Copyright 2020 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef COBALT_MEDIA_BASE_PLAYBACK_STATISTICS_H_
+#define COBALT_MEDIA_BASE_PLAYBACK_STATISTICS_H_
+
+#include <string>
+
+#include "cobalt/media/base/video_decoder_config.h"
+
+namespace cobalt {
+namespace media {
+
+class PlaybackStatistics {
+ public:
+  class Record {
+   public:
+    // TODO: Implement record for audio streams.
+    explicit Record(const VideoDecoderConfig& video_config);
+    ~Record();
+
+    void OnPresenting(const VideoDecoderConfig& video_config);
+    void OnConfigChange(const VideoDecoderConfig& video_config);
+  };
+
+  static std::string GetStatistics(
+      const VideoDecoderConfig& current_video_config);
+};
+
+}  // namespace media
+}  // namespace cobalt
+
+#endif  // COBALT_MEDIA_BASE_PLAYBACK_STATISTICS_H_
