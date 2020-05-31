0f9b0e72b90fbcbcde60e7d576e8ad0e0723cf0c
Cobalt Team <cobalt-team@google.com>
2020-05-31 01:06:58 -0700

    [BG] Starboard: Add new SbSystemRequest* functions in starboard.
    Add the new SbSystemRequest* based on the new starboard lifecycle design.
    
    b/155435817
    
    Change-Id: Ic3fae3efb0e987a9d266779d981814b764cb38bf

--- starboard/shared/starboard/system_request_reveal.cc
+++ starboard/shared/starboard/system_request_reveal.cc
@@ -1,4 +1,4 @@
-// Copyright 2017 The Cobalt Authors. All Rights Reserved.
+// Copyright 2020 The Cobalt Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,9 +14,12 @@
 
 #include "starboard/system.h"
 
-#if SB_API_VERSION < SB_ADD_CONCEALED_STATE_SUPPORT_VERSION && \
-    !SB_HAS(CONCEALED_STATE)
-void SbSystemRequestPause() {
+#include "starboard/shared/starboard/application.h"
+
+#if SB_API_VERSION >= SB_ADD_CONCEALED_STATE_SUPPORT_VERSION || \
+    SB_HAS(CONCEALED_STATE)
+void SbSystemRequestReveal() {
+  starboard::shared::starboard::Application::Get()->Reveal(NULL, NULL);
 }
-#endif  // SB_API_VERSION < SB_ADD_CONCEALED_STATE_SUPPORT_VERSION &&
-        // !SB_HAS(CONCEALED_STATE)
+#endif  // SB_API_VERSION >= SB_ADD_CONCEALED_STATE_SUPPORT_VERSION ||
+        // SB_HAS(CONCEALED_STATE)
--- starboard/shared/stub/system_request_reveal.cc
+++ starboard/shared/stub/system_request_reveal.cc
@@ -1,4 +1,4 @@
-// Copyright 2017 The Cobalt Authors. All Rights Reserved.
+// Copyright 2020 The Cobalt Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 
 #include "starboard/system.h"
 
-#if SB_API_VERSION < SB_ADD_CONCEALED_STATE_SUPPORT_VERSION && \
-    !SB_HAS(CONCEALED_STATE)
-void SbSystemRequestPause() {
+#if SB_API_VERSION >= SB_ADD_CONCEALED_STATE_SUPPORT_VERSION || \
+    SB_HAS(CONCEALED_STATE)
+void SbSystemRequestReveal() {
 }
-#endif  // SB_API_VERSION < SB_ADD_CONCEALED_STATE_SUPPORT_VERSION &&
-        // !SB_HAS(CONCEALED_STATE)
+#endif  // SB_API_VERSION >= SB_ADD_CONCEALED_STATE_SUPPORT_VERSION ||
+        // SB_HAS(CONCEALED_STATE)
