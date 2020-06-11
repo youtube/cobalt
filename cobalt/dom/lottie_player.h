c41349f27ce2e1a05ec445f90ad35fa88aa90fca
Cobalt Team <cobalt-team@google.com>
2020-06-10 20:50:09 -0700

    Add Lottie frame event
    b/156115005
    
    Change-Id: Ifb1f7963c1a94c27a4473e89c81ce5ff24a8e626

--- cobalt/dom/lottie_player.h
+++ cobalt/dom/lottie_player.h
@@ -17,6 +17,7 @@
 
 #include <string>
 
+#include "base/message_loop/message_loop.h"
 #include "cobalt/dom/event_queue.h"
 #include "cobalt/dom/html_element.h"
 #include "cobalt/loader/image/image_cache.h"
@@ -122,11 +123,17 @@ class LottiePlayer : public HTMLElement {
   void ScheduleEvent(base::Token event_name);
   void SetAnimationEventCallbacks();
 
+  // These are callbacks triggered during animation playback.
   void OnPlay();
   void OnPause();
   void OnStop();
   void OnComplete();
   void OnLoop();
+  void OnEnterFrame(double frame, double seeker);
+  static void CallOnEnterFrame(
+      scoped_refptr<base::SingleThreadTaskRunner> callback_task_runner,
+      base::Callback<void(double, double)> enter_frame_callback, double frame,
+      double seeker);
 
   scoped_refptr<loader::image::CachedImage> cached_image_;
   std::unique_ptr<loader::image::CachedImage::OnLoadedCallbackHandler>
@@ -141,6 +148,7 @@ class LottiePlayer : public HTMLElement {
   LottieAnimation::LottieProperties properties_;
 
   EventQueue event_queue_;
+  scoped_refptr<base::SingleThreadTaskRunner> callback_task_runner_;
 };
 
 }  // namespace dom
