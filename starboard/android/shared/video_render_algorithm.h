993e745c448c67e0a8ec99b07d9c02f3d2edceeb
Xiaoming Shi <xiaomings@google.com>
2020-06-16 20:31:35 -0700

    Pass seek to time to VideoRenderAlgorithm::Seek()
    It is also renamed from Reset() to Seek() to be more explicit.
    
    b/158851220
    
    Change-Id: I97993b446751bd96059f84c86dfcabc07c3f5e7a

--- starboard/android/shared/video_render_algorithm.h
+++ starboard/android/shared/video_render_algorithm.h
@@ -30,7 +30,7 @@ class VideoRenderAlgorithm : public ::starboard::shared::starboard::player::
   void Render(MediaTimeProvider* media_time_provider,
               std::list<scoped_refptr<VideoFrame>>* frames,
               VideoRendererSink::DrawFrameCB draw_frame_cb) override;
-  void Reset() override {}
+  void Seek(SbTime seek_to_time) override {}
   int GetDroppedFrames() override { return dropped_frames_; }
 
  private:
