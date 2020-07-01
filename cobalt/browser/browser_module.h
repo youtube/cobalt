dfedd6aab773c9a8c1a733ab481850bd04b540ec
Cobalt Team <cobalt-team@google.com>
2020-07-01 23:42:52 +0000

    Revert "[BG] Cobalt: Clean up the deprecated start event handlers."
    This reverts commit 8210940fd686119100fe5ab6c49b3241b93bc24f.
    
    Reason for revert: <Build errors>
    
    Change-Id: I5894e16a6c73ffbea913c25aa2c7bb09c03f0f73

--- cobalt/browser/browser_module.h
+++ cobalt/browser/browser_module.h
@@ -173,6 +173,7 @@ class BrowserModule {
   void SetProxy(const std::string& proxy_rules);
 
   // LifecycleObserver-similar interface.
+  void Start();
   void Blur();
   void Conceal();
   void Freeze();
