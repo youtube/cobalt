2b643bcc34f330d38f15ac03ef8bd2b07ef9512e
Cobalt Team <cobalt-team@google.com>
2020-07-11 00:10:01 +0000

    Revert "Create the drain file API to decide update priority between Evergreen applications."
    This reverts commit 0e83d044816f5a3e7bd6efb8bc25736de62ff06c.
    
    Reason for revert: Breaks builds.
    
    Change-Id: I7554f7a8c2d90f5ee956b198499987f67e300c69

--- starboard/build/platform_configuration.py
+++ starboard/build/platform_configuration.py
@@ -366,7 +366,6 @@ class PlatformConfiguration(object):
     """
     tests = [
         'app_key_files_test',
-        'drain_file_test',
         'elf_loader_test',
         'installation_manager_test',
         'nplb',
