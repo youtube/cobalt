3e79740300381c7eeb8bca5577c0fae4dfbf89c9
Jason Zhang <jasonzh@google.com>
2020-07-15 14:17:40 -0700

    [Android] Reset surface view on nexus player
    The SurfaceView sometimes displays video frames in distorted color after
    playing video of different ratios on Nexus Player. It may be caused by
    internal wrong states. Re-create the SurfaceView to avoid the issue.
    
    b/159073388
    
    Change-Id: Ie596f01f247aa664443fbf54d680577480d3e345

--- starboard/android/apk/app/src/main/java/dev/cobalt/media/VideoSurfaceView.java
+++ starboard/android/apk/app/src/main/java/dev/cobalt/media/VideoSurfaceView.java
@@ -18,11 +18,14 @@ import static dev.cobalt.media.Log.TAG;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import dev.cobalt.util.Log;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * A Surface view to be used by the video decoder. It informs the Starboard application when the
@@ -31,6 +34,17 @@ import dev.cobalt.util.Log;
 public class VideoSurfaceView extends SurfaceView {
 
   private static Surface currentSurface = null;
+
+  private static final Set<String> needResetSurfaceList = new HashSet<>();
+
+  static {
+    needResetSurfaceList.add("Nexus Player");
+
+    // Reset video surface on nexus player to avoid b/159073388.
+    if (needResetSurfaceList.contains(Build.MODEL)) {
+      nativeSetNeedResetSurface();
+    }
+  }
 
   public VideoSurfaceView(Context context) {
     super(context);
@@ -61,7 +75,9 @@ public class VideoSurfaceView extends SurfaceView {
     // punch-out video when the position / size is animated.
   }
 
-  private native void nativeOnVideoSurfaceChanged(Surface surface);
+  private static native void nativeOnVideoSurfaceChanged(Surface surface);
+
+  private static native void nativeSetNeedResetSurface();
 
   private class SurfaceHolderCallback implements SurfaceHolder.Callback {
 
