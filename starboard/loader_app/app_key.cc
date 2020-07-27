8543aa8c73a08dd7c4f1508fcb24759c43463c4a
Cobalt Team <cobalt-team@google.com>
2020-07-27 16:43:43 -0700

    Limit the app key size to kSbFileMaxName - 24.
    b/162115458
    
    Change-Id: I80c119587b35734a046c061b8d3593d1d9a46dea
    (cherry picked from commit cb233db70666b69f0ffc72c3c3e8c8729ff486bb)

--- starboard/loader_app/app_key.cc
+++ starboard/loader_app/app_key.cc
@@ -14,13 +14,31 @@
 
 #include "starboard/loader_app/app_key.h"
 
+#include "starboard/common/log.h"
+#include "starboard/configuration_constants.h"
 #include "starboard/loader_app/app_key_internal.h"
 
 namespace starboard {
 namespace loader_app {
+namespace {
+
+// With the maximum length of an app key being 24 bytes less than the maximum
+// file name size we provide enough room for not-insignificant sized prefixes
+// and suffixes. This leaves room for prefixes and suffixes while maintaining
+// all, or most of, the app key.
+const size_t kAppKeyMax = kSbFileMaxName - 24;
+
+}  // namespace
 
 std::string GetAppKey(const std::string& url) {
-  return EncodeAppKey(ExtractAppKey(url));
+  SB_DCHECK(kAppKeyMax > 0);
+
+  const std::string app_key = EncodeAppKey(ExtractAppKey(url));
+
+  if (app_key.size() > kAppKeyMax) {
+    return app_key.substr(app_key.size() - kAppKeyMax, kAppKeyMax);
+  }
+  return app_key;
 }
 
 }  // namespace loader_app
