4cd5ee83a709a2651ecbe753599888c67e3e9d2c
Cobalt Team <cobalt-team@google.com>
2020-08-02 00:15:41 -0700

    Restructure media demo
    b/165128073
    
    Change-Id: Ia1ee9fc57838670c9f2fadb07e870520ecc0e15c

---
+++ cobalt/demos/content/media-element-demo/README.md
@@ -0,0 +1,12 @@
+# Cobalt Media Demo
+
+## Get Started
+
+```
+# Install node modules if running for the first time.
+npm install
+
+# Start a server and build the package. Also watch script changes to regenerate
+# script bundles.
+npm start
+```
---
+++ cobalt/demos/content/media-element-demo/src/components/error_logger.ts
@@ -0,0 +1,44 @@
+import {errors} from '../utils/shared_values';
+
+import {Component} from './component';
+
+/** A component that holds error logs. */
+export class ErrorLogger extends Component<{}> {
+  /** @override */
+  render() {
+    return '';
+  }
+
+  /** @override */
+  afterRender() {
+    errors.observe((messages: string[]) => {
+      this.renderError(messages);
+    });
+    window.addEventListener('error', (message) => {
+      if (typeof message === 'string') {
+        logError(message);
+        return;
+      }
+    });
+    window.addEventListener('unhandledrejection', (evt: any) => {
+      logError(evt.detail.reason);
+    });
+  }
+
+  private renderError(messages: string[]) {
+    const elements = messages.map(message => `
+      <div class="error">${message}</div>
+    `);
+    this.el.innerHTML = elements.join('');
+  }
+}
+
+/**
+ * Logs an error message. The logged error will be automatically picked up by
+ * the error logger.
+ * @param message The error message.
+ */
+export function logError(message: string) {
+  const current = errors.get();
+  errors.set([...current, message]);
+}
