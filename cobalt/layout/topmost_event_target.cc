9baf67073d37299bb496e3bdab36cbb3628c2b6e
Jelle Foks <jfoks@google.com>
2020-08-11 19:38:11 +0000

    Revert "Revert "Fix for mouseenter and mouseleave events, and webdriver moveto.""
    This reverts commit efcce6fa2c9038943c8840732d737c0120ab77dd.
    
    Reason for revert: Revert only needed for AppleTV.
    
    Change-Id: Ie681e3d9c109d053c11121b187d4d88d13ca3f75

--- cobalt/layout/topmost_event_target.cc
+++ cobalt/layout/topmost_event_target.cc
@@ -53,7 +53,6 @@ scoped_refptr<dom::HTMLElement> TopmostEventTarget::FindTopmostEventTarget(
   ConsiderElement(html_element_, coordinate);
   box_ = NULL;
   render_sequence_.clear();
-  document->SetIndicatedElement(html_element_);
   scoped_refptr<dom::HTMLElement> topmost_element;
   topmost_element.swap(html_element_);
   DCHECK(!html_element_);
@@ -131,10 +130,10 @@ void TopmostEventTarget::ConsiderBoxes(
 }
 
 namespace {
-void SendStateChangeEvents(bool is_pointer_event,
-                           scoped_refptr<dom::HTMLElement> previous_element,
-                           scoped_refptr<dom::HTMLElement> target_element,
-                           dom::PointerEventInit* event_init) {
+void SendStateChangeLeaveEvents(
+    bool is_pointer_event, scoped_refptr<dom::HTMLElement> previous_element,
+    scoped_refptr<dom::HTMLElement> target_element,
+    dom::PointerEventInit* event_init) {
   // Send enter/leave/over/out (status change) events when needed.
   if (previous_element != target_element) {
     const scoped_refptr<dom::Window>& view = event_init->view();
@@ -151,13 +150,6 @@ void SendStateChangeEvents(bool is_pointer_event,
       }
 
       event_init->set_related_target(target_element);
-      if (is_pointer_event) {
-        previous_element->DispatchEvent(new dom::PointerEvent(
-            base::Tokens::pointerout(), view, *event_init));
-      }
-      previous_element->DispatchEvent(
-          new dom::MouseEvent(base::Tokens::mouseout(), view, *event_init));
-
       // Find the nearest common ancestor, if there is any.
       dom::Document* previous_document = previous_element->node_document();
       if (previous_document) {
@@ -173,15 +165,33 @@ void SendStateChangeEvents(bool is_pointer_event,
             nearest_common_ancestor = nearest_common_ancestor->parent_element();
           }
         }
+      }
 
-        for (scoped_refptr<dom::Element> element = previous_element;
-             element && element != nearest_common_ancestor;
-             element = element->parent_element()) {
-          if (is_pointer_event) {
+      if (is_pointer_event) {
+        previous_element->DispatchEvent(new dom::PointerEvent(
+            base::Tokens::pointerout(), view, *event_init));
+        if (previous_document) {
+          for (scoped_refptr<dom::Element> element = previous_element;
+               element && element != nearest_common_ancestor;
+               element = element->parent_element()) {
+            DCHECK(element->AsHTMLElement()->IsDesignated());
             element->DispatchEvent(new dom::PointerEvent(
                 base::Tokens::pointerleave(), dom::Event::kNotBubbles,
                 dom::Event::kNotCancelable, view, *event_init));
           }
+        }
+      }
+
+      // Send compatibility mapping mouse events for state changes.
+      //   https://www.w3.org/TR/pointerevents/#mapping-for-devices-that-do-not-support-hover
+      previous_element->DispatchEvent(
+          new dom::MouseEvent(base::Tokens::mouseout(), view, *event_init));
+
+      if (previous_document) {
+        for (scoped_refptr<dom::Element> element = previous_element;
+             element && element != nearest_common_ancestor;
+             element = element->parent_element()) {
+          DCHECK(element->AsHTMLElement()->IsDesignated());
           element->DispatchEvent(new dom::MouseEvent(
               base::Tokens::mouseleave(), dom::Event::kNotBubbles,
               dom::Event::kNotCancelable, view, *event_init));
@@ -193,6 +203,20 @@ void SendStateChangeEvents(bool is_pointer_event,
         }
       }
     }
+  }
+}
+
+void SendStateChangeEnterEvents(
+    bool is_pointer_event, scoped_refptr<dom::HTMLElement> previous_element,
+    scoped_refptr<dom::HTMLElement> target_element,
+    dom::PointerEventInit* event_init) {
+  // Send enter/leave/over/out (status change) events when needed.
+  if (previous_element != target_element) {
+    const scoped_refptr<dom::Window>& view = event_init->view();
+
+    // The enter/leave status change events apply to all ancestors up to the
+    // nearest common ancestor between the previous and current element.
+    scoped_refptr<dom::Element> nearest_common_ancestor;
 
     // Send over and enter events.
     if (target_element) {
@@ -205,18 +229,22 @@ void SendStateChangeEvents(bool is_pointer_event,
       if (is_pointer_event) {
         target_element->DispatchEvent(new dom::PointerEvent(
             base::Tokens::pointerover(), view, *event_init));
-      }
-      target_element->DispatchEvent(
-          new dom::MouseEvent(base::Tokens::mouseover(), view, *event_init));
-
-      for (scoped_refptr<dom::Element> element = target_element;
-           element != nearest_common_ancestor;
-           element = element->parent_element()) {
-        if (is_pointer_event) {
+        for (scoped_refptr<dom::Element> element = target_element;
+             element != nearest_common_ancestor;
+             element = element->parent_element()) {
           element->DispatchEvent(new dom::PointerEvent(
               base::Tokens::pointerenter(), dom::Event::kNotBubbles,
               dom::Event::kNotCancelable, view, *event_init));
         }
+      }
+
+      // Send compatibility mapping mouse events for state changes.
+      //   https://www.w3.org/TR/pointerevents/#mapping-for-devices-that-do-not-support-hover
+      target_element->DispatchEvent(
+          new dom::MouseEvent(base::Tokens::mouseover(), view, *event_init));
+      for (scoped_refptr<dom::Element> element = target_element;
+           element != nearest_common_ancestor;
+           element = element->parent_element()) {
         element->DispatchEvent(new dom::MouseEvent(
             base::Tokens::mouseenter(), dom::Event::kNotBubbles,
             dom::Event::kNotCancelable, view, *event_init));
@@ -368,6 +396,12 @@ void TopmostEventTarget::MaybeSendPointerEvents(
     target_element = FindTopmostEventTarget(view->document(), coordinate);
   }
 
+  scoped_refptr<dom::HTMLElement> previous_html_element(
+      previous_html_element_weak_);
+
+  SendStateChangeLeaveEvents(pointer_event, previous_html_element,
+                             target_element, &event_init);
+
   if (target_element) {
     target_element->DispatchEvent(event);
   }
@@ -403,13 +437,14 @@ void TopmostEventTarget::MaybeSendPointerEvents(
     }
   }
 
-  scoped_refptr<dom::HTMLElement> previous_html_element(
-      previous_html_element_weak_);
-
-  SendStateChangeEvents(pointer_event, previous_html_element, target_element,
-                        &event_init);
+  SendStateChangeEnterEvents(pointer_event, previous_html_element,
+                             target_element, &event_init);
 
   if (target_element) {
+    dom::Document* document = target_element->node_document();
+    if (document) {
+      document->SetIndicatedElement(target_element);
+    }
     previous_html_element_weak_ = base::AsWeakPtr(target_element.get());
   } else {
     previous_html_element_weak_.reset();
