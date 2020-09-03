88ed24ef0457332dc0c67444a269bbf643cd016d
Cobalt Team <cobalt-team@google.com>
2020-09-03 16:41:24 -0700

    Add Evergreen test cases.
    This change adds a set of Evergreen test cases to be run by the
    Evergreen automated test framework. These tests correspond 1:1 to those
    in go/cobalt-evergreen-test-cases, with the exception of the "out of
    storage" test case.
    
    b/158488347
    b/158485220
    
    Change-Id: Id79e62e234cd1c86fce680211c0a93ade0457d79

---
+++ starboard/evergreen/testing/tests/disabled_updater_test.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Copyright 2020 The Cobalt Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Unset the previous test's name and runner function.
+unset TEST_NAME
+unset TEST_FILE
+unset -f run_test
+
+TEST_NAME="DisabledUpdater"
+TEST_FILE="test.html"
+
+function run_test() {
+  clear_storage
+
+  start_cobalt "file:///tests/${TEST_FILE}" "${TEST_NAME}.0.log" "update from test channel installed"
+
+  if [[ $? -ne 0 ]]; then
+    error "Failed to download and install the test package"
+    return 1
+  fi
+
+  start_cobalt "file:///tests/${TEST_FILE}" "${TEST_NAME}.1.log" "App is up to date"
+
+  if [[ $? -ne 0 ]]; then
+    error "Failed to run downloaded installation"
+    return 1
+  fi
+
+  start_cobalt "file:///tests/${TEST_FILE}" "${TEST_NAME}.2.log" "disable_updates=1" "--disable_updates"
+
+  if [[ $? -ne 0 ]]; then
+    error "Failed to run system installation"
+    return 1
+  fi
+
+  return 0
+}
+
---
+++ starboard/evergreen/testing/tests/load_slot_being_updated_test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# Copyright 2020 The Cobalt Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Unset the previous test's name and runner function.
+unset TEST_NAME
+unset TEST_FILE
+unset -f run_test
+
+TEST_NAME="LoadSlotBeingUpdated"
+TEST_FILE="test.html"
+
+function run_test() {
+  clear_storage
+
+  start_cobalt "file:///tests/${TEST_FILE}" "${TEST_NAME}.0.log" "update from test channel installed"
+
+  if [[ $? -ne 0 ]]; then
+    error "Failed to download and install the test package"
+    return 1
+  fi
+
+  FILENAME=$(get_temporary_drain_file_path "${TEST_NAME}.0.log")
+
+  create_file "${FILENAME}"
+
+  start_cobalt "file:///tests/${TEST_FILE}" "${TEST_NAME}.1.log" "Active slot draining"
+
+  if [[ $? -ne 0 ]]; then
+    error "Failed to abort loading a slot being drained"
+    return 1
+  fi
+
+  return 0
+}
+
