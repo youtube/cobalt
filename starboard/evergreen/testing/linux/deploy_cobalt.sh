f2a5b626ae984722acbac7cbe1c6b80e204ace37
Cobalt Team <cobalt-team@google.com>
2020-09-03 16:43:30 -0700

    Add Linux support for the automated Evergreen test framework.
    This change adds all of the necessary functionality to run the Evergreen
    automated tests on Linux.
    
    b/158485220
    
    Change-Id: I498ab88bf773d0dbe14d95eeee63283a76f38338
    (cherry picked from commit 173c4cd1732160804e5c90a6c76aaf2f20079d06)

---
+++ starboard/evergreen/testing/linux/deploy_cobalt.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Copyright 2020 The Cobalt Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+function deploy_cobalt() {
+  if [[ -z "${OUT}" ]]; then
+    info "Please set the environment variable 'OUT'"
+    exit 1
+  fi
+
+  echo " Checking '${OUT}'"
+
+  PATHS=("${OUT}/deploy/loader_app/loader_app"                          \
+         "${OUT}/deploy/loader_app/content/app/cobalt/lib/libcobalt.so" \
+         "${OUT}/deploy/loader_app/content/app/cobalt/content/")
+
+  for file in "${PATHS[@]}"; do
+    if [[ ! -e "${file}" ]]; then
+      echo " Failed to find '${file}'"
+      exit 1
+    fi
+  done
+
+  echo " Required files were found within '${OUT}'"
+
+  echo " Deploying Cobalt on local device"
+
+  echo " Generating HTML test directory"
+  eval "mkdir -p ${OUT}/deploy/loader_app/content/app/cobalt/content/web/tests/" 1> /dev/null
+
+  echo " Copying HTML test files to HTML test directory"
+  eval "cp ${1}/../tests/*.html ${OUT}/deploy/loader_app/content/app/cobalt/content/web/tests/" 1> /dev/null
+
+  clear_storage
+
+  echo " Successfully deployed!"
+}
+
