f7841c13c06148803c46c827e1c83836310d1428
Cobalt Team <cobalt-team@google.com>
2020-09-03 16:44:35 -0700

    Add Raspberry Pi support for the automated Evergreen test framework.
    This change adds all of the necessary functionality to run the Evergreen
    automated tests on Raspberry Pi.
    
    b/158485220
    
    Change-Id: Iffc981ccbef9a3d639a38f3ad4eb35c4da5d39e3
    (cherry picked from commit de611997dca0e0853917a0d15b74f86edf328fa8)

---
+++ starboard/evergreen/testing/raspi/deploy_cobalt.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# Copyright 2020 The Cobalt Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+function deploy_cobalt() {
+  if [[ -z "${OUT}" ]]; then
+    info "Please set the environment variable 'OUT'"
+    exit 1
+  fi
+
+  echo " Checking '${OUT}'"
+
+  PATHS=("${OUT}/deploy/loader_app/loader_app"                          \
+         "${OUT}/deploy/loader_app/content/app/cobalt/lib/libcobalt.so" \
+         "${OUT}/deploy/loader_app/content/app/cobalt/content/")
+
+  for file in "${PATHS[@]}"; do
+    if [[ ! -e "${file}" ]]; then
+      echo " Failed to find '${file}'"
+      exit 1
+    fi
+  done
+
+  echo " Required files were found within '${OUT}'"
+
+  echo " Deploying to the Raspberry Pi 2 at ${RASPI_ADDR}"
+
+  echo " Regenerating Cobalt-on-Evergreen directory"
+  eval "${SSH} rm -rf /home/pi/coeg/" 1> /dev/null
+  eval "${SSH} mkdir /home/pi/coeg/" 1> /dev/null
+
+  echo " Copying loader_app to Cobalt-on-Evergreen directory"
+  eval "${SCP} ${OUT}/deploy/loader_app/loader_app pi@${RASPI_ADDR}:/home/pi/coeg/" 1> /dev/null
+
+  echo " Regenerating system image directory"
+  eval "${SSH} mkdir -p /home/pi/coeg/content/app/cobalt/lib" 1> /dev/null
+
+  echo " Copying cobalt to system image directory"
+  eval "${SCP} ${OUT}/deploy/loader_app/content/app/cobalt/lib/libcobalt.so pi@${RASPI_ADDR}:/home/pi/coeg/content/app/cobalt/lib/" 1> /dev/null
+
+  echo " Copying content to system image directory"
+  eval "${SCP} -r ${OUT}/deploy/loader_app/content/app/cobalt/content/ pi@${RASPI_ADDR}:/home/pi/coeg/content/app/cobalt/" 1> /dev/null
+
+  echo " Generating HTML test directory"
+  eval "${SSH} mkdir -p /home/pi/coeg/content/app/cobalt/content/web/tests/" 1> /dev/null
+
+  echo " Copying HTML test files to HTML test directory"
+  eval "${SCP} ${1}/../tests/*.html pi@${RASPI_ADDR}:/home/pi/coeg/content/app/cobalt/content/web/tests/" 1> /dev/null
+
+  echo " Successfully deployed!"
+}
+
