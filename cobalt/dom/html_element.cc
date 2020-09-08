382680ada1a6b7b5e03c088ffc983ce71e24b284
Toan Pham <toanpham@google.com>
2020-09-08 03:38:24 -0700

    Remove focus from hybrid nav system if the current focus is disabled
    This prevents focus from jumping around if a series of focus items
    are being disabled (e.g. the web app is updating the UI layout).
    
    b/166516925
    
    Change-Id: I1816e38aa7114f19b9f1c6591b2b924a976ddf6c

--- cobalt/dom/html_element.cc
+++ cobalt/dom/html_element.cc
@@ -2148,6 +2148,7 @@ bool HTMLElement::UpdateUiNavigationAndReturnIfLayoutBoxesAreValid() {
       // when all references to it are released.
       if (g_ui_nav_focus_ == this) {
         g_ui_nav_focus_ = nullptr;
+        ui_nav_item_->UnfocusAll();
       }
       ui_nav_item_->SetEnabled(false);
       ui_nav_item_ = nullptr;
@@ -2176,6 +2177,7 @@ bool HTMLElement::UpdateUiNavigationAndReturnIfLayoutBoxesAreValid() {
     // This navigation item is no longer relevant.
     if (g_ui_nav_focus_ == this) {
       g_ui_nav_focus_ = nullptr;
+      ui_nav_item_->UnfocusAll();
     }
     ui_nav_item_->SetEnabled(false);
     ui_nav_item_ = nullptr;
@@ -2198,6 +2200,7 @@ void HTMLElement::ReleaseUiNavigationItem() {
     // while being released.
     if (g_ui_nav_focus_ == this) {
       g_ui_nav_focus_ = nullptr;
+      ui_nav_item_->UnfocusAll();
     }
     ui_nav_item_->SetEnabled(false);
     ui_nav_item_ = nullptr;
