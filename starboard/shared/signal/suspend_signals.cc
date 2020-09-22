adb3129c8c0294668fc9213e4e2abc2a8097c722
Yavor Goulishev <yavor@google.com>
2020-09-22 09:39:39 -0700

    Move the pending restart checks in SbSystemRequestSuspend, SbSystemRequestFreeze
    Having the pending restart checks part of the shared starboard
    implementation of SbSystemRequestSuspend, SbSystemRequestFreeze
    will make it easier for partners to comply with our requirements.
    
    b/169187195
    
    Change-Id: Ib6e5ad45d0364a785b1b87b8aae62f2a4832788d

--- starboard/shared/signal/suspend_signals.cc
+++ starboard/shared/signal/suspend_signals.cc
@@ -61,27 +61,10 @@ void SetSignalHandler(int signal_id, SignalHandlerFunction handler) {
 
 #if SB_API_VERSION >= SB_ADD_CONCEALED_STATE_SUPPORT_VERSION || \
     SB_HAS(CONCEALED_STATE)
-
-#if SB_IS(EVERGREEN_COMPATIBLE)
-void RequestConcealOrStop() {
-  if (loader_app::IsPendingRestart()) {
-    SbLogRawFormatF("\nPending update restart. Stopping.\n");
-    SbLogFlush();
-    SbSystemRequestStop(0);
-  } else {
-    SbSystemRequestConceal();
-  }
-}
-#endif
-
 void Conceal(int signal_id) {
   SignalMask(kAllSignals, SIG_BLOCK);
   LogSignalCaught(signal_id);
-#if SB_IS(EVERGREEN_COMPATIBLE)
-  RequestConcealOrStop();
-#else
   SbSystemRequestConceal();
-#endif
   SignalMask(kAllSignals, SIG_UNBLOCK);
 }
 
@@ -107,27 +90,10 @@ void Stop(int signal_id) {
   SignalMask(kAllSignals, SIG_UNBLOCK);
 }
 #else
-
-#if SB_IS(EVERGREEN_COMPATIBLE)
-void RequestSuspendOrStop() {
-  if (loader_app::IsPendingRestart()) {
-    SbLogRawFormatF("\nPending update restart . Stopping.\n");
-    SbLogFlush();
-    SbSystemRequestStop(0);
-  } else {
-    SbSystemRequestSuspend();
-  }
-}
-#endif
-
 void Suspend(int signal_id) {
   SignalMask(kAllSignals, SIG_BLOCK);
   LogSignalCaught(signal_id);
-#if SB_IS(EVERGREEN_COMPATIBLE)
-  RequestSuspendOrStop();
-#else
   SbSystemRequestSuspend();
-#endif
   SignalMask(kAllSignals, SIG_UNBLOCK);
 }
 
