42fe244d3941cb9097f0ae3bcaffcc0b9ffaa6a0
Cobalt Team <cobalt-team@google.com>
2020-10-22 14:30:10 -0700

    Make noop test binary exit automatically
    This change will support the integration of this noop test to YTS.
    
    Change-Id: Ia074212eb3900c6fcea0fdb3e9258a2a3f874592
    (cherry picked from commit f8f1685b09fdc1477fb1e360a0ae7e2aa6d38098)

--- cobalt/updater/noop_sandbox.cc
+++ cobalt/updater/noop_sandbox.cc
@@ -15,7 +15,12 @@
 // This is a test app for Evergreen that does nothing.
 
 #include "starboard/event.h"
+#include "starboard/system.h"
+#include "starboard/thread.h"
+#include "starboard/time.h"
 
 void SbEventHandle(const SbEvent* event) {
-  // noop
+  // No-op app. Exit after 1s.
+  SbThreadSleep(kSbTimeSecond);
+  SbSystemRequestStop(0);
 }
