55558b35ca641092e58607184d51bb02a0ee9b26
Cobalt Team <cobalt-team@google.com>
2020-10-22 14:21:14 -0700

    [Android] Remove UpdateActiveSessionPlatformPlaybackState.
    Based on the description of b/34690599,
    the UpdateActiveSessionPlatformPlaybackState function was added
    for allowing the removal of timeout that would hold audio
    focus/visible behind for a small period of time after the video
    ended. However, this request if not valid anymore. Meanwhile,
    we don't need the sbplayerdestroy to set the media session state
    value to none, which would trigger the maybefreezecallback and
    freeze the Cobalt. Instead, we need the media session from Js
    script side to control the media session state.
    
    b/171509019
    
    Change-Id: Ic004171a8bec827a5fffdcc80c2fbbd2634c2ef8

--- starboard/android/shared/player_create.cc
+++ starboard/android/shared/player_create.cc
@@ -14,7 +14,6 @@
 
 #include "starboard/player.h"
 
-#include "starboard/android/shared/android_media_session_client.h"
 #include "starboard/android/shared/video_decoder.h"
 #include "starboard/android/shared/video_window.h"
 #include "starboard/common/log.h"
@@ -28,8 +27,6 @@
 using starboard::shared::starboard::player::filter::
     FilterBasedPlayerWorkerHandler;
 using starboard::shared::starboard::player::PlayerWorker;
-using starboard::android::shared::kPlaying;
-using starboard::android::shared::UpdateActiveSessionPlatformPlaybackState;
 using starboard::android::shared::VideoDecoder;
 
 SbPlayer SbPlayerCreate(SbWindow window,
@@ -125,8 +122,6 @@ SbPlayer SbPlayerCreate(SbWindow window,
         kMaxNumberOfHardwareDecoders) {
       return kSbPlayerInvalid;
     }
-    // Only update session state for main player.
-    UpdateActiveSessionPlatformPlaybackState(kPlaying);
   }
 
   if (creation_param->output_mode != kSbPlayerOutputModeDecodeToTexture &&
