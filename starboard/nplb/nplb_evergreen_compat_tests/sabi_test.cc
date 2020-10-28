154a273deff8ce03d9961495f20d885158f6c967
Yavor Goulishev <yavor@google.com>
2020-10-28 15:47:34 -0700

    Add a test to detect usage of the experimental starboard version.
    - Evergreen should use only stable released starboard versions.
    
    Change-Id: Ice95d44c1f179ef7a1e589d430942933b732f079
    (cherry picked from commit af824e910aa31c767c19da2ae263aae3e5675a3c)

--- starboard/nplb/nplb_evergreen_compat_tests/sabi_test.cc
+++ starboard/nplb/nplb_evergreen_compat_tests/sabi_test.cc
@@ -155,6 +155,12 @@ class SabiTest : public ::testing::Test {
 };
 
 TEST_F(SabiTest, VerifySABI) {
+  SB_LOG(INFO) << "Using SB_API_VERSION=" << SB_API_VERSION;
+  SB_LOG(INFO) << "Using SABI=" << SB_SABI_JSON_ID;
+
+  ASSERT_LT(SB_API_VERSION, SB_EXPERIMENTAL_API_VERSION)
+      << "Evergreen should use SB_API_VERSION < SB_EXPERIMENTAL_API_VERSION";
+
   std::set<std::string> sabi_set;
   sabi_set.insert(kSabiJsonIdArmHardfp);
   sabi_set.insert(kSabiJsonIdArmSoftfp);
