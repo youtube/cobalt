8b4f0d579ddeef0bad0032db622d74c01f96abf1
V8 Autoroll <v8-ci-autoroll-builder@chops-service-accounts.iam.gserviceaccount.com>
2021-02-11 02:33:46 -0800

    Version 8.8.278.17
    TBR=v8-ci-autoroll-builder@chops-service-accounts.iam.gserviceaccount.com
    
    Change-Id: Icacc432e46f8f38ae7dca18175540bad5d4fbd58
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2689678
    Reviewed-by: v8-ci-autoroll-builder <v8-ci-autoroll-builder@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/8.8@{#35}
    Cr-Branched-From: 2dbcdc105b963ee2501c82139eef7e0603977ff0-refs/heads/8.8.278@{#1}
    Cr-Branched-From: 366d30c99049b3f1c673f8a93deb9f879d0fa9f0-refs/heads/master@{#71094}
    git-subtree-dir: third_party/v8
    git-subtree-split: f7f4327516912dfca0d7401044e890ca0ce885ad

---
+++ src/wasm/wasm-js.h
@@ -0,0 +1,31 @@
+// Copyright 2015 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef V8_WASM_WASM_JS_H_
+#define V8_WASM_WASM_JS_H_
+
+#include "src/common/globals.h"
+
+namespace v8 {
+namespace internal {
+class JSProxy;
+class WasmFrame;
+
+namespace wasm {
+class StreamingDecoder;
+}  // namespace wasm
+
+// Exposes a WebAssembly API to JavaScript through the V8 API.
+class WasmJs {
+ public:
+  V8_EXPORT_PRIVATE static void Install(Isolate* isolate,
+                                        bool exposed_on_global_object);
+
+  V8_EXPORT_PRIVATE static Handle<JSProxy> GetJSDebugProxy(WasmFrame* frame);
+};
+
+}  // namespace internal
+}  // namespace v8
+
+#endif  // V8_WASM_WASM_JS_H_
