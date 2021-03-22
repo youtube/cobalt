a7caa8840cdf0b74a7161ae6768fead69b841f17
Cobalt Team <cobalt-team@google.com>
2021-03-22 20:31:31 +0000

    Revert "Modify Now function in Performance interface."
    Revert submission 184040-now
    
    Reason for revert: <breaks the unit test>
    Reverted Changes:
    I4e3c1280e:Add internal getter for time origin.
    I96e03ec71:Modify Now() unit test in performance test.
    Ia2ff98c6d:Modify Now function in Performance interface.
    
    Change-Id: Ia94093bd9c825b58b3571740e904595bb561a535

--- cobalt/dom/performance.cc
+++ cobalt/dom/performance.cc
@@ -25,10 +25,8 @@ Performance::Performance(const scoped_refptr<base::BasicClock>& clock)
       memory_(new MemoryInfo()),
       time_origin_(base::Time::Now() - base::Time::UnixEpoch()) {}
 
-DOMHighResTimeStamp Performance::Now() const {
-  base::TimeDelta now = base::Time::Now() - base::Time::UnixEpoch();
-  return ConvertTimeDeltaToDOMHighResTimeStamp(now - time_origin_,
-      Performance::kPerformanceTimerMinResolutionInMicroseconds);
+double Performance::Now() const {
+  return timing_->GetNavigationStartClock()->Now().InMillisecondsF();
 }
 
 scoped_refptr<PerformanceTiming> Performance::timing() const { return timing_; }
