4da2536f2fe1de0ecee6ed094c2c5ecff90a951e
Cobalt Team <cobalt-team@google.com>
2021-03-29 17:54:40 -0700

    Remove unneeded onConfigurationChanged() override
    Remove unneeded onConfigurationChanged() override that just calls
    super, and remove redundant "implements ComponentCallbacks".
    
    b/183767749
    
    Change-Id: I0bf5210a301e4f596659ca0caa895d48876bac3c

--- starboard/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
+++ starboard/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
@@ -17,12 +17,10 @@ package dev.cobalt.coat;
 import static dev.cobalt.util.Log.TAG;
 
 import android.app.NativeActivity;
-import android.content.ComponentCallbacks;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.res.Configuration;
 import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -40,7 +38,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /** Native activity that has the required JNI methods called by the Starboard implementation. */
-public abstract class CobaltActivity extends NativeActivity implements ComponentCallbacks {
+public abstract class CobaltActivity extends NativeActivity {
 
   // A place to put args while debugging so they're used even when starting from the launcher.
   // This should always be empty in submitted code.
@@ -342,10 +340,5 @@ public abstract class CobaltActivity extends NativeActivity implements Component
   public void onLowMemory() {
     super.onLowMemory();
     nativeLowMemoryEvent();
-  }
-
-  @Override
-  public void onConfigurationChanged(Configuration newConfig) {
-    super.onConfigurationChanged(newConfig);
   }
 }
