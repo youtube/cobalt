d6af8605ff54a2bac3577be4e66965d39d6b922f
Yavor Goulishev <yavor@google.com>
2021-05-10 14:10:40 -0700

    Enable logging in gold builds for Evergreen.
     - Allow the --min_log_level to be used in gold builds.
     - Set the default level to "fatal" to disable logging by
       default.
    
    b/187749444
    
    Change-Id: Ia0aaf10fde5b1b211c73a1dbe6682fe3a783f247

--- starboard/common/log.cc
+++ starboard/common/log.cc
@@ -29,7 +29,11 @@
 namespace starboard {
 namespace logging {
 namespace {
+#if SB_LOGGING_IS_OFFICIAL_BUILD
+SbLogPriority g_min_log_level = kSbLogPriorityFatal;
+#else
 SbLogPriority g_min_log_level = kSbLogPriorityUnknown;
+#endif
 
 #if defined(COMPILER_MSVC)
 #pragma optimize("", off)
@@ -51,11 +55,7 @@ void SetMinLogLevel(SbLogPriority priority) {
 }
 
 SbLogPriority GetMinLogLevel() {
-#if SB_LOGGING_IS_OFFICIAL_BUILD
-  return SB_LOG_FATAL;
-#else
   return g_min_log_level;
-#endif
 }
 
 SbLogPriority StringToLogLevel(const std::string& log_level) {
