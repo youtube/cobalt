ff6c506379e225c23c7b8f692f11a8dc1b6a6f9e
Kaido Kert <kaidokert@google.com>
2021-06-02 17:50:55 -0700

    Update Clang 3.6 to Clang 3.9
    Clang-3.9 is the first Clang version to do C++14
    constexpr initialization properly. This will help
    clean up large amounts of left-over C++11 compat
    hacks from upstream V8 and Skia code.
    
    b/184030883
    
    Change-Id: I01b03d1ed0cbf437df0d051d788639050edede43

--- starboard/linux/x64x11/clang/3.9/configuration_public.h
+++ starboard/linux/x64x11/clang/3.9/configuration_public.h
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef STARBOARD_LINUX_X64X11_CLANG_3_6_CONFIGURATION_PUBLIC_H_
-#define STARBOARD_LINUX_X64X11_CLANG_3_6_CONFIGURATION_PUBLIC_H_
+#ifndef STARBOARD_LINUX_X64X11_CLANG_3_9_CONFIGURATION_PUBLIC_H_
+#define STARBOARD_LINUX_X64X11_CLANG_3_9_CONFIGURATION_PUBLIC_H_
 
 #include "starboard/linux/x64x11/configuration_public.h"
 
-#endif  // STARBOARD_LINUX_X64X11_CLANG_3_6_CONFIGURATION_PUBLIC_H_
+#endif  // STARBOARD_LINUX_X64X11_CLANG_3_9_CONFIGURATION_PUBLIC_H_
