9ef5a093a458e5195a997da0b63fc1eb5bd7139d
haozheng <haozheng@google.com>
2021-06-22 11:55:49 -0700

    default webp background to transparent white
    b/170175941
    
    Change-Id: I77d8afacdbf5b75becc82b75cdb1114573e243f4

--- cobalt/loader/image/animated_webp_image.cc
+++ cobalt/loader/image/animated_webp_image.cc
@@ -118,14 +118,9 @@ void AnimatedWebPImage::AppendChunk(const uint8* data, size_t size) {
     received_first_frame_ = true;
     loop_count_ = WebPDemuxGetI(demux_, WEBP_FF_LOOP_COUNT);
 
-    // The default background color of the canvas in [Blue, Green, Red, Alpha],
-    // from most significant byte to least significant byte.
-    uint32_t background_color = WebPDemuxGetI(demux_, WEBP_FF_BACKGROUND_COLOR);
-    background_color_ =
-        render_tree::ColorRGBA((background_color >> 8 & 0xff) / 255.0f,
-                               (background_color >> 16 & 0xff) / 255.0f,
-                               (background_color >> 24 & 0xff) / 255.0f,
-                               (background_color >> 0 & 0xff) / 255.0f);
+    // The background color for webp is treated as transparent white,
+    // thus pulling in whatever color the document uses.
+    background_color_ = render_tree::ColorRGBA(0, 0, 0, 0);
 
     if (is_playing_) {
       StartDecoding();
