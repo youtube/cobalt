3b0e6f5a37022cc12cc47a4758c1ffc28acc7499
Cobalt Team <cobalt-team@google.com>
2021-07-02 00:14:02 -0700

    Fix black box test failure on PersistentCookieTest issue.
    b/192630725
    
    Change-Id: Ib22f7487c72d302ae7f754414db2941442b7bb0d

--- cobalt/browser/debug_console.h
+++ cobalt/browser/debug_console.h
@@ -95,15 +95,27 @@ class DebugConsole : public LifecycleObserver {
   }
 
   // LifecycleObserver implementation.
-  void Blur(SbTimeMonotonic timestamp) override {}
+  void Blur(SbTimeMonotonic timestamp) override {
+    web_module_->Blur(0);
+  }
   void Conceal(render_tree::ResourceProvider* resource_provider,
-               SbTimeMonotonic timestamp) override {}
-  void Freeze(SbTimeMonotonic timestamp) override {}
+               SbTimeMonotonic timestamp) override {
+    web_module_->Conceal(resource_provider, 0);
+  }
+  void Freeze(SbTimeMonotonic timestamp) override {
+    web_module_->Freeze(0);
+  }
   void Unfreeze(render_tree::ResourceProvider* resource_provider,
-                SbTimeMonotonic timestamp) override {}
+                SbTimeMonotonic timestamp) override {
+    web_module_->Unfreeze(resource_provider, 0);
+  }
   void Reveal(render_tree::ResourceProvider* resource_provider,
-              SbTimeMonotonic timestamp) override {}
-  void Focus(SbTimeMonotonic timestamp) override {}
+              SbTimeMonotonic timestamp) override {
+    web_module_->Reveal(resource_provider, 0);
+  }
+  void Focus(SbTimeMonotonic timestamp) override {
+     web_module_->Focus(0);
+  }
 
   void ReduceMemory() { web_module_->ReduceMemory(); }
 
