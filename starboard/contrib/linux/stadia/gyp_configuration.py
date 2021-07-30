39e5ef10b0bc0320505ea33c2a0c1fc78198c355
Cobalt Team <cobalt-team@google.com>
2021-07-30 10:57:50 -0700

    Adds an example build configuration for Stadia.
    This build configuration has been added to
    starboard/contrib/stadia/linux/stadia, but it needs to be moved to
    starboard/linux/stadia in order to compile.
    
    This configuration is a fork of the starboard/linux/x64x11
    configuration.
    
    Also included in this change is a fork of the starboard/shared/x11
    application class, which needs to be modified to initialize Stadia
    platform services.
    
    b/194504983
    
    Change-Id: Id673ac8d77ed8edad7cf93ca2433039eddc5044e

---
+++ starboard/contrib/linux/stadia/gyp_configuration.py
@@ -0,0 +1,59 @@
+# Copyright 2021 The Cobalt Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Starboard Linux Stadia platform configuration."""
+
+from starboard.linux.shared import gyp_configuration as shared_configuration
+from starboard.tools.toolchain import ar
+from starboard.tools.toolchain import bash
+from starboard.tools.toolchain import clang
+from starboard.tools.toolchain import clangxx
+from starboard.tools.toolchain import cp
+from starboard.tools.toolchain import touch
+
+
+class LinuxStadiaConfiguration(shared_configuration.LinuxConfiguration):
+  """Starboard Linux Stadia platform configuration."""
+
+  def __init__(self,
+               platform='linux-stadia',
+               asan_enabled_by_default=True,
+               sabi_json_path='starboard/sabi/default/sabi.json'):
+    super(LinuxStadiaConfiguration,
+          self).__init__(platform, asan_enabled_by_default, sabi_json_path)
+
+  def GetTargetToolchain(self, **kwargs):
+    return self.GetHostToolchain(**kwargs)
+
+  def GetHostToolchain(self, **kwargs):
+    environment_variables = self.GetEnvironmentVariables()
+    cc_path = environment_variables['CC']
+    cxx_path = environment_variables['CXX']
+
+    return [
+        clang.CCompiler(path=cc_path),
+        clang.CxxCompiler(path=cxx_path),
+        clang.AssemblerWithCPreprocessor(path=cc_path),
+        ar.StaticThinLinker(),
+        ar.StaticLinker(),
+        clangxx.ExecutableLinker(path=cxx_path),
+        clangxx.SharedLibraryLinker(path=cxx_path),
+        cp.Copy(),
+        touch.Stamp(),
+        bash.Shell(),
+    ]
+
+
+def CreatePlatformConfig():
+  return LinuxStadiaConfiguration(
+      sabi_json_path='starboard/sabi/x64/sysv/sabi-v{sb_api_version}.json')
