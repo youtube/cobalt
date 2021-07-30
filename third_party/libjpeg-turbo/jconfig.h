66f809e304b654732c25569e31ce4b80d7d41816
Cobalt Team <cobalt-team@google.com>
2021-07-30 14:23:28 -0700

    Add METADATA and .gyp file for libjpeg-turbo.
    b/191697519
    
    Change-Id: I0d1c4d9e80c0e84ac8d8eb7e3b038dad450d0f5a

--- third_party/libjpeg-turbo/jconfig.h
+++ third_party/libjpeg-turbo/jconfig.h
@@ -10,10 +10,10 @@
 #define LIBJPEG_TURBO_VERSION_NUMBER  2001000
 
 /* Support arithmetic encoding */
-/* #define C_ARITH_CODING_SUPPORTED 1 */
+#define C_ARITH_CODING_SUPPORTED 1
 
 /* Support arithmetic decoding */
-/* #define D_ARITH_CODING_SUPPORTED 1 */
+#define D_ARITH_CODING_SUPPORTED 1
 
 /* Support in-memory source/destination managers */
 #define MEM_SRCDST_SUPPORTED 1
