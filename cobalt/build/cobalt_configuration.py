93bc3913e3b27603b1bbd8dda3a98f036c23a26b
Cobalt Team <cobalt-team@google.com>
2021-08-04 01:30:33 +0000

    Revert "Generate the build number in gyp_runner, not cobalt_conf..."
    Revert submission 201082
    
    Reason for revert: cause failure in gyp_cobalt on Ganeti
    Reverted Changes:
    I38af7b36d:Get build number without gclient
    I247b37b4e:Generate the build number in gyp_runner, not cobal...
    
    Change-Id: I01f690bd51f2144407497d7fd9af0972b962f772

--- cobalt/build/cobalt_configuration.py
+++ cobalt/build/cobalt_configuration.py
@@ -13,9 +13,11 @@
 # limitations under the License.
 """Base cobalt configuration for GYP."""
 
+import logging
 import os
 
 import _env  # pylint: disable=unused-import
+from cobalt.build import gyp_utils
 from cobalt.tools import paths
 import cobalt.tools.webdriver_benchmark_config as wb_config
 from starboard.build import application_configuration
@@ -39,8 +41,7 @@ class CobaltConfiguration(application_configuration.ApplicationConfiguration):
   def GetVariables(self, config_name):
     variables = {
         'cobalt_fastbuild': os.environ.get('LB_FASTBUILD', 0),
-        'cobalt_version':
-            self._platform_configuration.GetBuildNumber(config_name),
+        'cobalt_version': gyp_utils.GetBuildNumber(),
 
         # This is here rather than cobalt_configuration.gypi so that it's
         # available for browser_bindings_gen.gyp.
@@ -49,6 +50,7 @@ class CobaltConfiguration(application_configuration.ApplicationConfiguration):
         # Cobalt uses OpenSSL on all platforms.
         'use_openssl': 1,
     }
+    logging.info('Build Number: {}'.format(variables['cobalt_version']))
     return variables
 
   def GetPostIncludes(self):
