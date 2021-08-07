843ea554b7603a881b3a8f4745a8411c555c27e9
Cobalt Team <cobalt-team@google.com>
2021-08-06 17:00:37 -0700

    [Android]: Fix black screen upon back navigation issue.
    We didn't call activity.finish() to manually suspend the app, which
    triggers the activity.onPause().
    To fix the issue, we need to explicitly call 'requestSuspend' before
    injecting the freeze event.
    
    b/192589273
    
    Change-Id: Ibf3d64936c2990b6b91251d81fab687f91cf8455

---
+++ starboard/android/shared/system_request_freeze_no_freezedone_callback.cc
@@ -0,0 +1,53 @@
+// Copyright 2020 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/system.h"
+
+#include "starboard/android/shared/jni_env_ext.h"
+#include "starboard/shared/signal/signal_internal.h"
+#include "starboard/shared/starboard/application.h"
+
+using starboard::android::shared::JniEnvExt;
+
+#if SB_IS(EVERGREEN_COMPATIBLE) && !SB_IS(EVERGREEN_COMPATIBLE_LITE)
+#include "starboard/loader_app/pending_restart.h"
+#endif  // SB_IS(EVERGREEN_COMPATIBLE) && !SB_IS(EVERGREEN_COMPATIBLE_LITE)
+
+#if SB_API_VERSION >= 13
+void SbSystemRequestFreeze() {
+#if SB_IS(EVERGREEN_COMPATIBLE) && !SB_IS(EVERGREEN_COMPATIBLE_LITE)
+  if (starboard::loader_app::IsPendingRestart()) {
+    SbLogRawFormatF("\nPending update restart . Stopping.\n");
+    SbLogFlush();
+    starboard::shared::starboard::Application::Get()->Stop(0);
+  } else {
+    // There is no FreezeDone callback for stopping all thread execution
+    // after fully transitioning into Frozen.
+    starboard::shared::starboard::Application::Get()->Freeze(NULL, NULL);
+
+    // Let Android platform directly transit into Frozen.
+    JniEnvExt* env = JniEnvExt::Get();
+    env->CallStarboardVoidMethodOrAbort("requestSuspend", "()V");
+  }
+#else
+  // There is no FreezeDone callback for stopping all thread execution
+  // after fully transitioning into Frozen.
+  starboard::shared::starboard::Application::Get()->Freeze(NULL, NULL);
+
+  // Let Android platform directly transit into Frozen.
+  JniEnvExt* env = JniEnvExt::Get();
+  env->CallStarboardVoidMethodOrAbort("requestSuspend", "()V");
+#endif  // SB_IS(EVERGREEN_COMPATIBLE) && !SB_IS(EVERGREEN_COMPATIBLE_LITE)
+}
+#endif  // SB_API_VERSION >= 13
