87f66b875d1a08d73d566c8dfa099c5cf221db69
Andrew Savage <andrewsavage@google.com>
2021-08-27 15:40:11 -0700

    Reword sabi SB_API_VERSION documentation
    b/197119407
    
    Change-Id: I680ee634dc388351f47150a1cad31e3af3103bf3
    (cherry picked from commit d7c92e98b62898a5f6ce97f45556a3a998bdc13c)

--- starboard/sabi/sabi.py
+++ starboard/sabi/sabi.py
@@ -11,6 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Source of truth of the default/experimental Starboard API version."""
+"""Source of truth of the default Starboard API version."""
 
 SB_API_VERSION = 13
