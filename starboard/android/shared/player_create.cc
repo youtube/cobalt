3a385090aaf7624080485098e4e32f924edecaee
Cobalt Team <cobalt-team@google.com>
2021-10-14 09:10:31 -0700

    [android] Remove unused include from player_create
    b/202604748
    
    Change-Id: I8d291d916fb7ef1b6a88497f05d2b8c99079dea1

--- starboard/android/shared/player_create.cc
+++ starboard/android/shared/player_create.cc
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <sstream>
-
 #include "starboard/player.h"
 
 #include "starboard/android/shared/video_decoder.h"
