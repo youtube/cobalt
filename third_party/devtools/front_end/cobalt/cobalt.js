5410af9a3a77b49d6197a4b1382f5187247ecd32
Jelle Foks <jfoks@google.com>
2022-01-28 00:36:11 -0800

    Add Trace Event buttons to Cobalt tab in devtools
    This adds buttons to Start, Stop, and Download a trace from the tab
    named 'Cobalt' in the devtools.
    
    b/116612480
    b/186152730
    
    Change-Id: Ie2150ab1d74fc5d5d5c57710422e3a6ec1d76d03

--- third_party/devtools/front_end/cobalt/cobalt.js
+++ third_party/devtools/front_end/cobalt/cobalt.js
@@ -7,8 +7,43 @@ export default class CobaltPanel extends UI.VBox {
     constructor() {
         super(true, false);
         SDK.targetManager.observeTargets(this);
+
         this._target = UI.context.flavor(SDK.Target);
+        this._runtimeAgent = this._target.runtimeAgent();
+
+        this.element = this._shadowRoot.createChild('div');
+        this.element.textContent = 'Cobalt Console'
+        let download_element = this._shadowRoot.createChild('a', 'download');
+        download_element.style.display = 'none';
+
+        const traceContainer = this.element.createChild('div', 'trace-container');
+        traceContainer.appendChild(UI.createTextButton(Common.UIString('Start Trace'), event => {
+            console.log("Start Trace");
+            this.run(`(function() { window.h5vcc.traceEvent.start();})()`);
+            console.log("Started Trace");
+        }));
+        traceContainer.appendChild(UI.createTextButton(Common.UIString('Stop Trace'), event => {
+            console.log("Stop Trace");
+            this.run(`(function() { window.h5vcc.traceEvent.stop();})()`);
+            console.log("Stopped Trace");
+        }));
+        traceContainer.appendChild(UI.createTextButton(Common.UIString('Download Trace'), event => {
+            console.log("Download Trace");
+            this.run(`(function() { return window.h5vcc.traceEvent.read();})()`).then(function (result) {
+                download_element.setAttribute('href', 'data:text/plain;charset=utf-8,' +
+                    encodeURIComponent(result.result.value));
+                download_element.setAttribute('download', 'trace.json');
+                console.log("Downloaded Trace");
+                download_element.click();
+                download_element.setAttribute('href', undefined);
+            });
+        }));
     }
+
+    async run(expression) {
+        return await this._runtimeAgent.invoke_evaluate({ expression, returnByValue: true });
+    }
+
 
     /**
      * @override
