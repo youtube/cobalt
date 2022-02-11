552c072f8a8e813edd285adbbc58da7c552cd072
Jelle Foks <jfoks@google.com>
2022-02-10 16:48:47 -0800

    Dedicated Worker stubs.
    This adds initial IDL files and stubs for Dedicated Worker support.
    
    There is no actual functionality in this yet, but some of the code
    structure is laid out.
    
    This also includes the first steps to connect the MessagePort on the
    client side, but some more changes are needed to allow code re-use
    for the client and server side interfaces.
    
    b/218791814
    
    Change-Id: Ibb4495afaf38eb4d6386316dff0fdabcce8997a9

---
+++ cobalt/worker/worker_settings.h
@@ -0,0 +1,36 @@
+// Copyright 2022 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef COBALT_WORKER_WORKER_SETTINGS_H_
+#define COBALT_WORKER_WORKER_SETTINGS_H_
+
+#include "cobalt/script/environment_settings.h"
+
+namespace cobalt {
+namespace worker {
+
+// Worker Environment Settings object as described in
+// https://html.spec.whatwg.org/commit-snapshots/465a6b672c703054de278b0f8133eb3ad33d93f4/#script-settings-for-workers
+
+class WorkerSettings : public script::EnvironmentSettings {
+ public:
+  WorkerSettings();
+
+ private:
+};
+
+}  // namespace worker
+}  // namespace cobalt
+
+#endif  // COBALT_WORKER_WORKER_SETTINGS_H_
