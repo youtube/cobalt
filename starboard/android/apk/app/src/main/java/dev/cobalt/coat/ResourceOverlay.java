b50b7e4f7fe47fc5ce31838283f2d682a4562fd8
Cobalt Team <cobalt-team@google.com>
2022-03-03 14:40:39 +0800

    [Android] Add overlayable variables in COAT package.
    b/185932291
    
    Change-Id: Ia7e07a2d154d4b6a70758077d15e7ff79b9c3e65
    Related-To: I43746eccd4344a6b0aca80bf8ec899bbb6865b5d

---
+++ starboard/android/apk/app/src/main/java/dev/cobalt/coat/ResourceOverlay.java
@@ -0,0 +1,32 @@
+// Copyright 2022 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dev.cobalt.coat;
+
+import android.content.Context;
+import dev.cobalt.util.UsedByNative;
+
+/**
+ * A class for managing the resource overlays of Cobalt. Client can turn on certain feature by
+ * setting the resource overlay.
+ */
+public class ResourceOverlay {
+  @UsedByNative public final int increaseAudioBufferSize;
+
+  public ResourceOverlay(Context context) {
+    // Load the values for all Overlay variables.
+    this.increaseAudioBufferSize =
+        context.getResources().getInteger(R.integer.increase_audio_buffer_size);
+  }
+}
--- starboard/android/shared/application_android.h
+++ starboard/android/shared/application_android.h
@@ -96,6 +96,14 @@ class ApplicationAndroid
 
   void SendDateTimeConfigurationChangedEvent();
 
+  // Methods to get the Runtime Resource Overlay variables.
+  // All RRO variables which can be retrieved here must be defined
+  // in res/values/rro_variables.xml and be loaded in
+  // dev/cobalt/coat/ResourceOverlay.java.
+  int GetOverlayedIntValue(const char* var_name);
+  std::string GetOverlayedStringValue(const char* var_name);
+  bool GetOverlayedBoolValue(const char* var_name);
+
   // Methods to start/stop Media playback service.
   void StartMediaPlaybackService();
   void StopMediaPlaybackService();
@@ -138,6 +146,8 @@ class ApplicationAndroid
   scoped_ptr<InputEventsGenerator> input_events_generator_;
 
   bool last_is_accessibility_high_contrast_text_enabled_;
+
+  jobject resource_overlay_;
 
   // Methods to process pipes attached to the Looper.
   void ProcessAndroidCommand();
