ff915b0bd6a257fa02eb4b293739e7dbac7f7458
Cobalt Team <cobalt-team@google.com>
2022-04-07 09:51:42 -0700

    Add shaders for crayola
    b/227504953, b/225220430
    
    Change-Id: I57713af453461d21db702c81d59a2fc53365cd84
    (cherry picked from commit 09d4ee9905865d06f43bbebf9b8c084cd06f4888)

---
+++ cobalt/renderer/glimp_shaders/glsl/fragment_skia_color_texture_13.glsl
@@ -0,0 +1,49 @@
+#version 100
+
+uniform highp float u_skRTHeight;
+precision mediump float;
+precision mediump sampler2D;
+uniform mediump vec4 uscaleAndTranslate_Stage2;
+uniform mediump vec4 uTexDom_Stage2;
+uniform mediump vec3 uDecalParams_Stage2;
+uniform sampler2D uTextureSampler_0_Stage1;
+uniform sampler2D uTextureSampler_0_Stage2;
+varying highp vec2 vTransformedCoords_0_Stage0;
+varying mediump vec4 vcolor_Stage0;
+mediump vec4 stage_Stage1_c0_c0(mediump vec4 _input) {
+    mediump vec4 child;
+    child = _input * texture2D(uTextureSampler_0_Stage1, vTransformedCoords_0_Stage0);
+    return child;
+}
+void main() {
+highp     vec4 sk_FragCoord = vec4(gl_FragCoord.x, u_skRTHeight - gl_FragCoord.y, gl_FragCoord.z, gl_FragCoord.w);
+    mediump vec4 outputColor_Stage0;
+    {
+        outputColor_Stage0 = vcolor_Stage0;
+    }
+    mediump vec4 output_Stage1;
+    {
+        mediump vec4 child;
+        child = stage_Stage1_c0_c0(vec4(1.0));
+        output_Stage1 = child * outputColor_Stage0.w;
+    }
+    mediump vec4 output_Stage2;
+    {
+        mediump vec2 coords = sk_FragCoord.xy * uscaleAndTranslate_Stage2.xy + uscaleAndTranslate_Stage2.zw;
+        {
+            highp vec2 origCoord = coords;
+            highp vec2 clampedCoord = clamp(origCoord, uTexDom_Stage2.xy, uTexDom_Stage2.zw);
+            mediump vec4 inside = texture2D(uTextureSampler_0_Stage2, clampedCoord).wwww;
+            mediump float err = max(abs(clampedCoord.x - origCoord.x) * uDecalParams_Stage2.x, abs(clampedCoord.y - origCoord.y) * uDecalParams_Stage2.y);
+            if (err > uDecalParams_Stage2.z) {
+                err = 1.0;
+            } else if (uDecalParams_Stage2.z < 1.0) {
+                err = 0.0;
+            }
+            output_Stage2 = mix(inside, vec4(0.0, 0.0, 0.0, 0.0), err);
+        }
+    }
+    {
+        gl_FragColor = output_Stage1 * output_Stage2;
+    }
+}
