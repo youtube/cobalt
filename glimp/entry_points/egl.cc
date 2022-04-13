11a9461228fc36c8a6fc58adcdcf1afb4cba06b4
Niranjan Yardi <nyardi@google.com>
2022-04-13 16:20:14 -0700

    [PS4] Call submitDone() regularly during app suspension.
    Sony requires us to call submitDone() every 5 secs.
    In conceal mode, rasterizer thread is deleted and submitDone() calls are
    stopped.
    
    This change calls submitDone() on the main application thread
    during app suspension every 4 secs.
    
    b/228112148
    
    Change-Id: Ica9e55e1b6ac96284c3e740daf86dc6ad8fd0865

--- glimp/entry_points/egl.cc
+++ glimp/entry_points/egl.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Google Inc. All Rights Reserved.
+ * Copyright 2015 The Cobalt Authors. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -197,6 +197,7 @@ EGLBoolean EGLAPIENTRY eglInitialize(EGLDisplay dpy,
   display->GetVersionInfo(major, minor);
 
   egl::SetError(EGL_SUCCESS);
+  egl::Display::RepeatSubmitDoneDuringSuspend(false);
   return true;
 }
 
@@ -264,6 +265,7 @@ EGLBoolean EGLAPIENTRY eglTerminate(EGLDisplay dpy) {
   egl::DisplayRegistry::TerminateDisplay(dpy);
 
   egl::SetError(EGL_SUCCESS);
+  egl::Display::RepeatSubmitDoneDuringSuspend(true);
   return true;
 }
 
