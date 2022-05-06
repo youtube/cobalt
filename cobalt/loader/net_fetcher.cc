9d4e15d8430f1baf9fc8e20d2b9ca6507fc05d79
Kaido Kert <kaidokert@google.com>
2022-05-06 15:48:35 -0700

    Add URL tracing to NetFetcher
    This helps identifying network fetches in trace view output.
    
    b/218698047
    
    Change-Id: Ibebf5b00fe8b0f6121c6bc537c531af5024962fd
    (cherry picked from commit 3d91803f686a5a4e1104fda260505e51c8744c16)

--- cobalt/loader/net_fetcher.cc
+++ cobalt/loader/net_fetcher.cc
@@ -18,6 +18,7 @@
 #include <string>
 
 #include "base/strings/stringprintf.h"
+#include "base/trace_event/trace_event.h"
 #include "cobalt/base/polymorphic_downcast.h"
 #include "cobalt/loader/cors_preflight.h"
 #include "cobalt/loader/url_fetcher_string_writer.h"
@@ -115,6 +116,8 @@ NetFetcher::NetFetcher(const GURL& url,
 void NetFetcher::Start() {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   const GURL& original_url = url_fetcher_->GetOriginalURL();
+  TRACE_EVENT1("cobalt::loader", "NetFetcher::Start", "url",
+               original_url.spec());
   if (security_callback_.is_null() ||
       security_callback_.Run(original_url, false /* did not redirect */)) {
     url_fetcher_->Start();
@@ -127,6 +130,8 @@ void NetFetcher::Start() {
 
 void NetFetcher::OnURLFetchResponseStarted(const net::URLFetcher* source) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  TRACE_EVENT1("cobalt::loader", "NetFetcher::OnURLFetchResponseStarted", "url",
+               url_fetcher_->GetOriginalURL().spec());
   if (source->GetURL() != source->GetOriginalURL()) {
     // A redirect occured. Re-check the security policy.
     if (!security_callback_.is_null() &&
@@ -166,6 +171,8 @@ void NetFetcher::OnURLFetchResponseStarted(const net::URLFetcher* source) {
 
 void NetFetcher::OnURLFetchComplete(const net::URLFetcher* source) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  TRACE_EVENT1("cobalt::loader", "NetFetcher::OnURLFetchComplete", "url",
+               url_fetcher_->GetOriginalURL().spec());
   const net::URLRequestStatus& status = source->GetStatus();
   const int response_code = source->GetResponseCode();
   if (status.is_success() && IsResponseCodeSuccess(response_code)) {
