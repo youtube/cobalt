d65c4b00f050e7a23e76cc7b904e62f48d36b976
Kaido Kert <kaidokert@google.com>
2022-05-17 12:01:41 -0700

    IFA properties implementation
    b/232430746
    b/207409974
    
    Change-Id: Ie144276dd6193746973184b29bb966f39e756b0a
    (cherry picked from commit 3c8a8b5f6bb09ed79148d59562f6944c223a2270)

--- starboard/shared/win32/system_get_property.cc
+++ starboard/shared/win32/system_get_property.cc
@@ -14,6 +14,7 @@
 
 #include "starboard/common/log.h"
 #include "starboard/common/string.h"
+#include "starboard/shared/environment.h"
 #include "starboard/system.h"
 
 namespace {
@@ -54,6 +55,18 @@ bool SbSystemGetProperty(SbSystemPropertyId property_id,
 
     case kSbSystemPropertyPlatformName:
       return CopyStringAndTestIfSuccess(out_value, value_length, kPlatformName);
+
+#if SB_API_VERSION >= 14
+    // Implementation provided for testing purposes only
+    case kSbSystemPropretyAdvertisingId:
+      return CopyStringAndTestIfSuccess(
+          out_value, value_length,
+          starboard::GetEnvironment("COBALT_ADVERTISING_ID").c_str());
+    case kSbSystemPropretyLimitAdTracking:
+      return CopyStringAndTestIfSuccess(
+          out_value, value_length,
+          starboard::GetEnvironment("COBALT_LIMIT_AD_TRACKING").c_str());
+#endif
 
     default:
       SB_DLOG(WARNING) << __FUNCTION__
