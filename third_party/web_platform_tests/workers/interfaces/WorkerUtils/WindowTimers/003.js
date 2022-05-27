5c5e9ea3bbba7332568b4368ec8826566cd6b55e
Tyler Holcombe <tholcombe@google.com>
2022-05-27 13:30:08 -0700

    Disable flaky web platform tests
    b/219103808
    
    Change-Id: I6a4ceefa2804828e7b90111ea3a2f7a11cbf70b3

--- third_party/web_platform_tests/workers/interfaces/WorkerUtils/WindowTimers/003.js
+++ third_party/web_platform_tests/workers/interfaces/WorkerUtils/WindowTimers/003.js
@@ -1,6 +1,3 @@
 var interval = setInterval(function () {
   postMessage('1');
-  // TODO (b/233788170): the below clearInterval line shouldn't be needed once
-  // worker appropriately stops timers on freeze.
-  clearInterval(interval);
 }, 10);
