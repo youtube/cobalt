86308b769ee45b1198bcbbac608d8f1a648e1085
Andrew Savage <andrewsavage@google.com>
2022-06-27 15:08:54 -0700

    Delete win32 unittests container
    This container is obsolete and not used. We'll rewrite it
    when necessary for a test container.
    
    b/178514489
    b/232273808
    
    Change-Id: Ie3bf53bfbc54307bc2436ee7bc616da8ca6622c2
    (cherry picked from commit 13913680ec28a00b92b0b4a719c4587102be89a2)
    NOKEYCHECK=True
    GitOrigin-RevId: 488fe12d32e986b14992c3aa1025c820e368f4ea

--- docker/windows/win32/unittest/unittest.cmd
+++
@@ -1,7 +0,0 @@
-if "%1" == "" set "PLATFORM=win-win32"
-if "%2" == "" set "CONFIG=devel"
-if "%3" == "" set "TEST=eztime_test"
-
-cd c:\code\out\%PLATFORM%_%CONFIG%\
-unzip app_launcher.zip -d c:\app_launcher_out\
-%python2% c:\app_launcher_out\starboard\tools\testing\test_runner.py --run --platform %PLATFORM% --config %CONFIG% -o c:\code\out\%PLATFORM%_%CONFIG%
