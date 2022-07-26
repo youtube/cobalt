62110c958301494ed790e29bda528ec41e138cdb
Brian Ting <brianting@google.com>
2022-07-26 16:15:39 -0700

    [Watchdog] Only unique Watchdog Violations JSON.
    Refactors Watchdog Violations JSON such that non-unique violations are
    stacked while unique violations are still individually displayed within
    a list. Uniqueness is determined by timestampLastPingedMicroseconds so
    that consecutive violations only update the violation duration. Replaces
    violation_delta_microseeconds with violationDurationMicroseeconds and
    adds timestampLastPingedMicroseconds to the JSON. Switches property
    naming from _ to camelCase.
    
    "Ping" timestamps are updated by Register() and Ping() calls. "Updated"
    timestamps are updated by Register(), Ping(), and Monitor() calls when
    in idle states or when a violation occurs.
    
    b/240341999
    
    Change-Id: Ic35b57a51e8126d796a7888d0e8f202e6de4dd12
    (cherry picked from commit 0d21374eb6e412e8df693c01ba56b3fe3a6eb1d8)
    NOKEYCHECK=True
    GitOrigin-RevId: 230700a05b9b55934d0f620af9052fc98355c30c

--- cobalt/watchdog/watchdog.cc
+++ cobalt/watchdog/watchdog.cc
@@ -158,17 +158,17 @@ void* Watchdog::Monitor(void* context) {
       Client* client = it.second.get();
       // Ignores and resets clients in idle states, clients whose monitor_state
       // is below the current application state. Resets time_wait_microseconds
-      // and time_interval_microseconds deltas.
+      // and time_interval_microseconds start values.
       if (static_cast<Watchdog*>(context)->state_ > client->monitor_state) {
         client->time_registered_monotonic_microseconds = current_monotonic_time;
-        client->time_last_pinged_monotonic_microseconds =
+        client->time_last_updated_monotonic_microseconds =
             current_monotonic_time;
         continue;
       }
 
       SbTimeMonotonic time_delta =
           current_monotonic_time -
-          client->time_last_pinged_monotonic_microseconds;
+          client->time_last_updated_monotonic_microseconds;
       SbTimeMonotonic time_wait =
           current_monotonic_time -
           client->time_registered_monotonic_microseconds;
@@ -178,55 +178,96 @@ void* Watchdog::Monitor(void* context) {
           time_wait > client->time_wait_microseconds) {
         watchdog_violation = true;
 
-        // Resets time last pinged.
-        client->time_last_pinged_monotonic_microseconds =
-            current_monotonic_time;
-
-        // Creates new violation.
-        base::Value violation(base::Value::Type::DICTIONARY);
-        violation.SetKey("ping_infos", client->ping_infos.Clone());
-        violation.SetKey("monitor_state",
-                         base::Value(std::string(GetApplicationStateString(
-                             client->monitor_state))));
-        violation.SetKey(
-            "time_interval_microseconds",
-            base::Value(static_cast<int>(client->time_interval_microseconds)));
-        violation.SetKey(
-            "time_wait_microseconds",
-            base::Value(static_cast<int>(client->time_wait_microseconds)));
-        violation.SetKey(
-            "registered_timestamp_microseconds",
-            base::Value(std::to_string(client->time_registered_microseconds)));
-        violation.SetKey("violation_timestamp_microseconds",
-                         base::Value(std::to_string(current_time)));
-        violation.SetKey("violation_delta_microseconds",
-                         base::Value(static_cast<int>(time_delta)));
-        if (registered_clients.GetList().empty()) {
-          for (auto& it : static_cast<Watchdog*>(context)->client_map_) {
-            registered_clients.GetList().emplace_back(base::Value(it.first));
-          }
-        }
-        violation.SetKey("registered_clients", registered_clients.Clone());
-
-        // Updates violations_map_.
+        // Gets violation dictionary with key client name from violations_map_.
         if (static_cast<Watchdog*>(context)->violations_map_ == nullptr)
           InitializeViolationsMap(context);
-        base::Value* value = (static_cast<Watchdog*>(context)->violations_map_)
-                                 ->FindKey(client->name);
-        if (value) {
-          base::Value* violations = value->FindKey("violations");
-          violations->GetList().emplace_back(violation.Clone());
-          if (violations->GetList().size() > kWatchdogMaxViolations)
-            violations->GetList().erase(violations->GetList().begin());
+        base::Value* violation_dict =
+            (static_cast<Watchdog*>(context)->violations_map_)
+                ->FindKey(client->name);
+
+        // Checks if new unique violation.
+        bool new_violation = false;
+        if (violation_dict == nullptr) {
+          new_violation = true;
         } else {
-          base::Value dict(base::Value::Type::DICTIONARY);
-          dict.SetKey("description", base::Value(client->description));
-          base::Value list(base::Value::Type::LIST);
-          list.GetList().emplace_back(violation.Clone());
-          dict.SetKey("violations", list.Clone());
-          (static_cast<Watchdog*>(context)->violations_map_)
-              ->SetKey(client->name, dict.Clone());
+          // Compares against last_pinged_timestamp_microsecond of last
+          // violation.
+          base::Value* violations = violation_dict->FindKey("violations");
+          int index = violations->GetList().size() - 1;
+          std::string timestamp_last_pinged_microseconds =
+              violations->GetList()[index]
+                  .FindKey("timestampLastPingedMicroseconds")
+                  ->GetString();
+          if (timestamp_last_pinged_microseconds !=
+              std::to_string(client->time_last_pinged_microseconds))
+            new_violation = true;
         }
+
+        // New unique violation.
+        if (new_violation) {
+          // Creates new violation.
+          base::Value violation(base::Value::Type::DICTIONARY);
+          violation.SetKey("pingInfos", client->ping_infos.Clone());
+          violation.SetKey("monitorState",
+                           base::Value(std::string(GetApplicationStateString(
+                               client->monitor_state))));
+          violation.SetKey(
+              "timeIntervalMicroseconds",
+              base::Value(std::to_string(client->time_interval_microseconds)));
+          violation.SetKey(
+              "timeWaitMicroseconds",
+              base::Value(std::to_string(client->time_wait_microseconds)));
+          violation.SetKey("timestampRegisteredMicroseconds",
+                           base::Value(std::to_string(
+                               client->time_registered_microseconds)));
+          violation.SetKey("timestampLastPingedMicroseconds",
+                           base::Value(std::to_string(
+                               client->time_last_pinged_microseconds)));
+          violation.SetKey("timestampViolationMicroseconds",
+                           base::Value(std::to_string(current_time)));
+          violation.SetKey(
+              "violationDurationMicroseconds",
+              base::Value(std::to_string(time_delta -
+                                         client->time_interval_microseconds)));
+          if (registered_clients.GetList().empty()) {
+            for (auto& it : static_cast<Watchdog*>(context)->client_map_) {
+              registered_clients.GetList().emplace_back(base::Value(it.first));
+            }
+          }
+          violation.SetKey("registeredClients", registered_clients.Clone());
+
+          // Adds new violation to violations_map_.
+          if (violation_dict == nullptr) {
+            base::Value dict(base::Value::Type::DICTIONARY);
+            dict.SetKey("description", base::Value(client->description));
+            base::Value list(base::Value::Type::LIST);
+            list.GetList().emplace_back(violation.Clone());
+            dict.SetKey("violations", list.Clone());
+            (static_cast<Watchdog*>(context)->violations_map_)
+                ->SetKey(client->name, dict.Clone());
+          } else {
+            base::Value* violations = violation_dict->FindKey("violations");
+            violations->GetList().emplace_back(violation.Clone());
+            if (violations->GetList().size() > kWatchdogMaxViolations)
+              violations->GetList().erase(violations->GetList().begin());
+          }
+          // Consecutive non-unique violation.
+        } else {
+          // Updates consecutive violation in violations_map_.
+          base::Value* violations = violation_dict->FindKey("violations");
+          int index = violations->GetList().size() - 1;
+          int64_t violation_duration =
+              std::stoll(violations->GetList()[index]
+                             .FindKey("violationDurationMicroseconds")
+                             ->GetString());
+          violations->GetList()[index].SetKey(
+              "violationDurationMicroseconds",
+              base::Value(std::to_string(violation_duration + time_delta)));
+        }
+
+        // Resets time last updated.
+        client->time_last_updated_monotonic_microseconds =
+            current_monotonic_time;
       }
     }
     if (watchdog_violation) {
@@ -297,6 +338,9 @@ bool Watchdog::Register(std::string name, std::string description,
 
   SB_CHECK(SbMutexAcquire(&mutex_) == kSbMutexAcquired);
 
+  int64_t current_time = SbTimeToPosix(SbTimeGetNow());
+  SbTimeMonotonic current_monotonic_time = SbTimeGetMonotonicNow();
+
   // If replace is PING or ALL, handles already registered cases.
   if (replace != NONE) {
     auto it = client_map_.find(name);
@@ -304,8 +348,9 @@ bool Watchdog::Register(std::string name, std::string description,
 
     if (already_registered) {
       if (replace == PING) {
-        it->second->time_last_pinged_monotonic_microseconds =
-            SbTimeGetMonotonicNow();
+        it->second->time_last_pinged_microseconds = current_time;
+        it->second->time_last_updated_monotonic_microseconds =
+            current_monotonic_time;
         SB_CHECK(SbMutexRelease(&mutex_));
         return true;
       }
@@ -321,10 +366,10 @@ bool Watchdog::Register(std::string name, std::string description,
   client->monitor_state = monitor_state;
   client->time_interval_microseconds = time_interval;
   client->time_wait_microseconds = time_wait;
-  client->time_registered_microseconds = SbTimeToPosix(SbTimeGetNow());
-  client->time_registered_monotonic_microseconds = SbTimeGetMonotonicNow();
-  client->time_last_pinged_monotonic_microseconds =
-      client->time_registered_monotonic_microseconds;
+  client->time_registered_microseconds = current_time;
+  client->time_registered_monotonic_microseconds = current_monotonic_time;
+  client->time_last_pinged_microseconds = current_time;
+  client->time_last_updated_monotonic_microseconds = current_monotonic_time;
 
   // Registers.
   auto result = client_map_.emplace(name, std::move(client));
@@ -375,16 +420,18 @@ bool Watchdog::Ping(const std::string& name, const std::string& info) {
   bool client_exists = it != client_map_.end();
 
   if (client_exists) {
+    int64_t current_time = SbTimeToPosix(SbTimeGetNow());
+    SbTimeMonotonic current_monotonic_time = SbTimeGetMonotonicNow();
+
     Client* client = it->second.get();
     // Updates last ping.
-    client->time_last_pinged_monotonic_microseconds = SbTimeGetMonotonicNow();
+    client->time_last_pinged_microseconds = current_time;
+    client->time_last_updated_monotonic_microseconds = current_monotonic_time;
 
     if (info != "") {
-      int64_t current_time = SbTimeToPosix(SbTimeGetNow());
-
       // Creates new ping_info.
       base::Value ping_info(base::Value::Type::DICTIONARY);
-      ping_info.SetKey("ping_timestamp_microseconds",
+      ping_info.SetKey("timestampMicroseconds",
                        base::Value(std::to_string(current_time)));
       ping_info.SetKey("info", base::Value(info));
 
@@ -479,6 +526,7 @@ void Watchdog::MaybeInjectDebugDelay(const std::string& name) {
   if (name != delay_name_) return;
 
   SbTimeMonotonic current_time = SbTimeGetMonotonicNow();
+
   if (time_last_delayed_microseconds_ == 0)
     time_last_delayed_microseconds_ = current_time;
 
