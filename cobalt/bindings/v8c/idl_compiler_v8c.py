468b2580b094d012cf98d4e50612586b101962af
Arjun Menon <agmenon@google.com>
2022-08-05 12:32:17 -0700

    Make IDL v8 compilation robust to transient flakes
    b/229148609
    
    Change-Id: Ie392b45762035f76f023bb51280b140b26d9afcf
    NOKEYCHECK=True
    GitOrigin-RevId: 010943be997fc9b2fa8b6ab229d585c97b0e9d16

--- cobalt/bindings/v8c/idl_compiler_v8c.py
+++ cobalt/bindings/v8c/idl_compiler_v8c.py
@@ -17,21 +17,29 @@ Calls into idl_compiler_cobalt.shared_main specifying the V8
 CodeGenerator class.
 """
 
+import logging
 import sys
 
-import _env  # pylint: disable=unused-import
+import _env  #pylint: disable=import-error,unused-import
 from cobalt.bindings.idl_compiler_cobalt import generate_bindings
 from cobalt.bindings.v8c.code_generator_v8c import CodeGeneratorV8c
 
-if __name__ == '__main__':
-  # TODO(b/225964218): The flakiness here should be resolved and retries
-  # removed.
-  # Retry up to 5 times as this can be flaky
+
+def main():
+  # TODO(b/225964218, b/229148609): The flakiness here should be resolved and
+  # retries removed. Retry up to 5 times as this can be flaky.
   latest_error = None
   for i in range(5):
     try:
+      if i > 0:
+        logging.warning('Failed IDL v8 compilation. Retry Attempt #%d', i)
       sys.exit(generate_bindings(CodeGeneratorV8c))
-    except EOFError as e:
+    except Exception as e:  #pylint: disable=broad-except
+      logging.warning('Failed IDL v8 compilation. Cause: %s', str(e))
       latest_error = e
   if latest_error:
     raise latest_error
+
+
+if __name__ == '__main__':
+  main()
