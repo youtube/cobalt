45872b019f6d11f943cee8ee93831b9d85ab571f
Yavor Goulishev <yavor@google.com>
2022-10-18 23:49:22 +0000

    Fix WasmCode::ConcatenateBytes.
    Change-Id: I241138296c16ff21b6d606fa2fbeb0bd73a22d97
    NOKEYCHECK=True
    GitOrigin-RevId: 8ebaf62d9208d3e006887b8a57d74cbebd47ac65

--- third_party/v8/src/wasm/wasm-code-manager.cc
+++ third_party/v8/src/wasm/wasm-code-manager.cc
@@ -178,13 +178,14 @@ std::unique_ptr<const byte[]> WasmCode::ConcatenateBytes(
   size_t total_size = 0;
   for (auto& vec : vectors) total_size += vec.size();
   // Use default-initialization (== no initialization).
-  byte* ptr = new byte[total_size];
+  byte* result = new byte[total_size];
+  byte* ptr = result;
   for (auto& vec : vectors) {
     if (vec.empty()) continue;  // Avoid nullptr in {memcpy}.
     memcpy(ptr, vec.begin(), vec.size());
     ptr += vec.size();
   }
-  return std::unique_ptr<const byte[]>(ptr);
+  return std::unique_ptr<const byte[]>(result);
 }
 
 void WasmCode::RegisterTrapHandlerData() {
