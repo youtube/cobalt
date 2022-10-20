a71f30cbdf445e3f90478e7555e1816843330580
Igor Sarkisov <isarkisov@google.com>
2022-10-20 16:36:34 -0700

    Bring GitHub Actions related changes from upstream.
    b/243188586
    
    Change-Id: Iefd4f5014244a116ad645069e86f7dc4ddccef39
    NOKEYCHECK=True
    GitOrigin-RevId: 37b9fac32d27d680863fe6e9fee8189e2cc71bde

--- starboard/build/platforms.py
+++ starboard/build/platforms.py
@@ -41,6 +41,7 @@ PLATFORMS = {
     'evergreen-arm-hardfp': 'starboard/evergreen/arm/hardfp',
     'evergreen-arm-softfp': 'starboard/evergreen/arm/softfp',
     'evergreen-arm64': 'starboard/evergreen/arm64',
+    'win-win32': 'starboard/win/win32',
 }
 PLATFORMS.update(INTERNAL_PLATFORMS)
 
