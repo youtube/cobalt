6349c4aaf6d55b75903f05b9f3f858b397115611
Cobalt Team <no-reply@google.com>
2023-01-06 10:21:40 -0800

    Fix typo in testharness.js
    b/262660692
    
    Change-Id: I0c61322a9f7f1908c3946d4aaeb9e3aa266e6aa9
    NOKEYCHECK=True
    GitOrigin-RevId: 0586e511692ed2a27de074d07849d1a33cf66a58

--- third_party/web_platform_tests/resources/testharness.js
+++ third_party/web_platform_tests/resources/testharness.js
@@ -197,8 +197,7 @@ policies and contribution forms [3].
         add_result_callback(function (test) {
             this_obj.output_handler.show_status();
         });
-        
-        // Only show test summary when browsering tests in Chrome.
+        // Only show test summary when browsing tests in Chrome.
         // Cobalt doesn't support html elements at this moment.
         if (navigator.userAgent.includes("Chrome")) {
             add_completion_callback(function (tests, harness_status) {
