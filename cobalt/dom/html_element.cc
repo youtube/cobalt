4f5546f0bced25fa8867b3a6e7d78563851c8994
Xiaoming Shi <xiaomings@google.com>
2023-01-10 19:16:17 -0800

    Fix crash in HTMLElement::GetLoadTimingInfoAndCreateResourceTiming()
    Ensure that the elements of `cached_background_images_` are always valid
    even when being cleared, as `cached_background_images_.clear()` may call
    HTMLElement::GetLoadTimingInfoAndCreateResourceTiming() indirectly, and
    the latter loops through `cached_background_images_` which is being
    cleared.
    
    This implementation removes element one by one to ensure that as many
    performance timing info can be created as possible, however, it will
    still lose performance timing on the image being removed (instead of
    crashing).
    
    b/265089478
    
    Change-Id: Ic1e3cb66f36ea4a66cec089d7733f242eff7b643
    NOKEYCHECK=True
    GitOrigin-RevId: 480c4d1d398d005c39e471cba7a2a4afd499d7e5

--- cobalt/dom/html_element.cc
+++ cobalt/dom/html_element.cc
@@ -1121,7 +1121,7 @@ void HTMLElement::PurgeCachedBackgroundImagesOfNodeAndDescendants() {
 void HTMLElement::PurgeCachedBackgroundImages() {
   ClearActiveBackgroundImages();
   if (!cached_background_images_.empty()) {
-    cached_background_images_.clear();
+    ClearCachedBackgroundImages();
     computed_style_valid_ = false;
     descendant_computed_styles_valid_ = false;
   }
@@ -2277,6 +2277,29 @@ void HTMLElement::ClearActiveBackgroundImages() {
   active_background_images_.clear();
 }
 
+void HTMLElement::ClearCachedBackgroundImages() {
+  // |cached_background_images_.clear()| cannot be used as it may lead to crash
+  // due to GetLoadTimingInfoAndCreateResourceTiming() being called indirectly
+  // from CachedImage dtor, and GetLoadTimingInfoAndCreateResourceTiming() loops
+  // on the |cached_background_images_| being cleared.
+  //
+  // To move and clear() like below is more straight-forward but leads to more
+  // in flight image loading performance timings get lost.
+  //   auto images = std::move(cached_background_images_);
+  //   DCHECK(cached_background_images_.empty());
+  //   images.clear();
+  //
+  // So images are moved out and cleared one by one.
+  while (!cached_background_images_.empty()) {
+    auto image = std::move(cached_background_images_.back());
+    DCHECK(!cached_background_images_.back());
+    cached_background_images_.pop_back();
+    // TODO(b/265089478): This implementation will lose the performance timing
+    // of |image|, consider refining to record all performance timings.
+    image = nullptr;
+  }
+}
+
 void HTMLElement::UpdateCachedBackgroundImagesFromComputedStyle() {
   ClearActiveBackgroundImages();
 
@@ -2320,10 +2343,11 @@ void HTMLElement::UpdateCachedBackgroundImagesFromComputedStyle() {
               cached_image, loaded_callback, base::Closure()));
     }
 
+    ClearCachedBackgroundImages();
     cached_background_images_ = std::move(cached_images);
   } else {
     // Clear the previous cached background image if the display is "none".
-    cached_background_images_.clear();
+    ClearCachedBackgroundImages();
   }
 }
 
