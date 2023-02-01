aff34ee7b0b6543c335710c1a0747dbeadee6236
Arjun Menon <agmenon@google.com>
2023-01-31 16:01:56 -0800

    Suppress C4189 compile error for VS2022 for Win32
    b/210151198
    
    Change-Id: I4af7e56c89a1f73296aa4b8a2b9bd02a6811aede
    NOKEYCHECK=True
    GitOrigin-RevId: 44cea28aff31b60a8acd97f65b193c2d612961cc

--- starboard/shared/win32/socket_set_tcp_keep_alive.cc
+++ starboard/shared/win32/socket_set_tcp_keep_alive.cc
@@ -22,7 +22,6 @@
 namespace sbwin32 = starboard::shared::win32;
 
 bool SbSocketSetTcpKeepAlive(SbSocket socket, bool value, SbTime period) {
-  const DWORD should_set_keepalive = value;
   bool result = sbwin32::SetBooleanSocketOption(
       socket, SOL_SOCKET, SO_KEEPALIVE, "SO_KEEPALIVE", value);
   return result;
