869284f31d6fbd4b794136ff5093f2e896cf8bd6
Cobalt Team <no-reply@google.com>
2023-02-06 14:58:24 -0800

    [Service Worker] Fix issue Service Worker Script is not JavaScript MIME type
    b/265815303
    
    Change-Id: I081b0c7239bfa8df99b3e70445cd0e22319a7516
    NOKEYCHECK=True
    GitOrigin-RevId: 407668329601ecc9085e5874ab72ca2950237a62

--- third_party/web_platform_tests/service-workers/service-worker/resources/test-request-headers-worker.py
+++ third_party/web_platform_tests/service-workers/service-worker/resources/test-request-headers-worker.py
@@ -3,14 +3,12 @@ import os
 import uuid
 import sys
 
-from wptserve.utils import isomorphic_decode
-
 def main(request, response):
-  path = os.path.join(os.path.dirname(isomorphic_decode(__file__)),
+  path = os.path.join(os.path.dirname(__file__),
                       u"test-request-headers-worker.js")
   body = open(path, u"rb").read()
 
-  data = {isomorphic_decode(key):isomorphic_decode(request.headers[key]) for key, value in request.headers.items()}
+  data = {key:request.headers[key] for key,value in request.headers.items()}
   body = body.replace(b"%HEADERS%", json.dumps(data).encode("utf-8"))
   body = body.replace(b"%UUID%", str(uuid.uuid4()).encode("utf-8"))
 
