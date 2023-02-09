641014f0ac445798795bb2810043f2403fdea19e
Igor Sarkisov <isarkisov@google.com>
2023-02-09 15:27:01 -0800

    Revert "Adjust keepalive settings in ODT client. (#232)" (#242)
    This reverts commit 5fd94a364ebb90a664cd49f16712b91ecf89f743.
    
    Reason: Keep alive setting change didn't fix the crash.

--- tools/on_device_tests_gateway_client.py
+++ tools/on_device_tests_gateway_client.py
@@ -51,8 +51,8 @@ class OnDeviceTestsGatewayClient():
     self.channel = grpc.insecure_channel(
         target=f'{_ON_DEVICE_TESTS_GATEWAY_SERVICE_HOST}:{_ON_DEVICE_TESTS_GATEWAY_SERVICE_PORT}',  # pylint:disable=line-too-long
         # These options need to match server settings.
-        options=[('grpc.keepalive_time_ms', 30000),
-                 ('grpc.keepalive_timeout_ms', 15000),
+        options=[('grpc.keepalive_time_ms', 10000),
+                 ('grpc.keepalive_timeout_ms', 5000),
                  ('grpc.keepalive_permit_without_calls', 1),
                  ('grpc.http2.max_pings_without_data', 0),
                  ('grpc.http2.min_time_between_pings_ms', 10000),
