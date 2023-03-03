fe9915304aaa881e5d1028c22b12f44e7fa7f5e0
Yavor Goulishev <yavor@google.com>
2023-03-03 21:28:58 +0000

    Enable the evergreen_tests.
    b/270441309
    
    Change-Id: I1dca9a2591b8c4486ae5404760dc2925ab5beaba
    NOKEYCHECK=True
    GitOrigin-RevId: a8266cde55a735cf6b3d8dc1d9c71fd0cad287bf

--- starboard/evergreen/testing/run_all_tests.sh
+++ starboard/evergreen/testing/run_all_tests.sh
@@ -24,8 +24,7 @@ AUTH_METHOD="public-key"
 USE_COMPRESSED_SYSTEM_IMAGE="false"
 SYSTEM_IMAGE_EXTENSION=".so"
 
-# Temporarily disable the tests b/270441309.
-DISABLE_TESTS="true"
+DISABLE_TESTS="false"
 
 while getopts "d:a:c" o; do
     case "${o}" in
