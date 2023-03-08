cf23adfadee518dbc6c5f7e76c646684cdbc84e0
Esmael Elmoslimany <aee@google.com>
2023-03-08 23:52:14 +0000

    Revert "Revert "Revert "Revert "Fix heap use after free when shutting down during fetch intercept.""""
    This reverts commit cbef4b25cc2d97c588012ee6683e6f3f35580e78.
    
    Reason for revert: fixing build
    
    Change-Id: I3a4207fb29554c194f435756859184316c49f055
    NOKEYCHECK=True
    GitOrigin-RevId: eec6f69b732a4ca0c3ef8ab0fc5cc4a055d5d289

--- cobalt/xhr/xml_http_request.h
+++ cobalt/xhr/xml_http_request.h
@@ -21,6 +21,8 @@
 #include <vector>
 
 #include "base/gtest_prod_util.h"
+#include "base/memory/weak_ptr.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/optional.h"
 #include "base/timer/timer.h"
 #include "cobalt/dom/document.h"
@@ -44,6 +46,7 @@
 #include "net/http/http_response_headers.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_fetcher_delegate.h"
+#include "starboard/atomic.h"
 #include "url/gurl.h"
 
 namespace cobalt {
@@ -220,12 +223,18 @@ class XMLHttpRequest : public XMLHttpRequestEventTarget,
 };
 
 
-class XMLHttpRequestImpl {
+class XMLHttpRequestImpl
+    : public base::SupportsWeakPtr<XMLHttpRequestImpl>,
+      public base::MessageLoopCurrent::DestructionObserver {
  public:
   explicit XMLHttpRequestImpl(XMLHttpRequest* xhr);
   XMLHttpRequestImpl(const XMLHttpRequestImpl&) = delete;
   XMLHttpRequestImpl& operator=(const XMLHttpRequestImpl&) = delete;
-  virtual ~XMLHttpRequestImpl() {}
+  virtual ~XMLHttpRequestImpl() {
+    if (!will_destroy_current_message_loop_.load()) {
+      base::MessageLoop::current()->RemoveDestructionObserver(this);
+    }
+  }
 
   void Abort();
   void Open(const std::string& method, const std::string& url, bool async,
@@ -298,6 +307,9 @@ class XMLHttpRequestImpl {
                                 int64 total);
   void OnRedirect(const net::HttpResponseHeaders& headers);
 
+  // base::MessageLoopCurrent::DestructionObserver
+  void WillDestroyCurrentMessageLoop();
+
   // Called from bindings layer to tie objects' lifetimes to this XHR instance.
   XMLHttpRequestUpload* upload_or_null() { return upload_.get(); }
 
@@ -335,6 +347,7 @@ class XMLHttpRequestImpl {
   bool upload_listener_;
   bool with_credentials_;
   XMLHttpRequest* xhr_;
+  starboard::atomic_bool will_destroy_current_message_loop_;
 
   // A corspreflight instance for potentially sending preflight
   // request and performing cors check for all cross origin requests.
@@ -448,7 +461,11 @@ class DOMXMLHttpRequestImpl : public XMLHttpRequestImpl {
   explicit DOMXMLHttpRequestImpl(XMLHttpRequest* xhr);
   DOMXMLHttpRequestImpl(const DOMXMLHttpRequestImpl&) = delete;
   DOMXMLHttpRequestImpl& operator=(const DOMXMLHttpRequestImpl&) = delete;
-  ~DOMXMLHttpRequestImpl() override {}
+  ~DOMXMLHttpRequestImpl() override {
+    if (!will_destroy_current_message_loop_.load()) {
+      base::MessageLoop::current()->RemoveDestructionObserver(this);
+    }
+  }
 
   scoped_refptr<dom::Document> response_xml(
       script::ExceptionState* exception_state) override;
