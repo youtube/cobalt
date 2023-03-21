154437ea28659e19698dc5186dcc60d5fd0e56bf
Andrew Savage <andrewsavage@google.com>
2023-03-21 18:12:34 +0000

    Filter some failing NPLB tests on evergreen
    b/274619048
    b/274519093
    
    Change-Id: I5ccaf5d64664d36c3d8613e50e95aa0d998d537e
    NOKEYCHECK=True
    GitOrigin-RevId: 35b8fbfb8000f50e94cee582171d67ad5c361e0d

--- starboard/evergreen/x64/test_filters.py
+++ starboard/evergreen/x64/test_filters.py
@@ -15,8 +15,27 @@
 
 import os
 
-from starboard.tools import paths
 from starboard.evergreen.shared import test_filters as shared_test_filters
+from starboard.tools import paths
+from starboard.tools.testing import test_filter
+
+# pylint: disable=line-too-long
+_FILTERED_TESTS = {
+    'nplb': [
+        'SbPlayerWriteSampleTests/SbPlayerWriteSampleTest.WriteSingleBatch/audio_sintel_329_ec3_dmp_video__null__output_DecodeToTexture',
+        'SbPlayerWriteSampleTests/SbPlayerWriteSampleTest.WriteSingleBatch/audio_sintel_329_ec3_dmp_video__null__output_Punchout',
+        'SbPlayerWriteSampleTests/SbPlayerWriteSampleTest.WriteSingleBatch/audio_sintel_381_ac3_dmp_video__null__output_DecodeToTexture',
+        'SbPlayerWriteSampleTests/SbPlayerWriteSampleTest.WriteSingleBatch/audio_sintel_381_ac3_dmp_video__null__output_Punchout',
+        'SbPlayerWriteSampleTests/SbPlayerWriteSampleTest.WriteMultipleBatches/audio_sintel_329_ec3_dmp_video__null__output_DecodeToTexture',
+        'SbPlayerWriteSampleTests/SbPlayerWriteSampleTest.WriteMultipleBatches/audio_sintel_329_ec3_dmp_video__null__output_Punchout',
+        'SbPlayerWriteSampleTests/SbPlayerWriteSampleTest.WriteMultipleBatches/audio_sintel_381_ac3_dmp_video__null__output_DecodeToTexture',
+        'SbPlayerWriteSampleTests/SbPlayerWriteSampleTest.WriteMultipleBatches/audio_sintel_381_ac3_dmp_video__null__output_Punchout',
+        'SbSocketAddressTypes/SbSocketGetInterfaceAddressTest.SunnyDayDestination/type_ipv6',
+        'SbSocketAddressTypes/SbSocketGetInterfaceAddressTest.SunnyDaySourceForDestination/type_ipv6',
+        'SbSocketAddressTypes/SbSocketGetInterfaceAddressTest.SunnyDaySourceNotLoopback/type_ipv6',
+    ],
+}
+# pylint: enable=line-too-long
 
 
 def CreateTestFilters():
@@ -27,11 +46,14 @@ class EvergreenX64TestFilters(shared_test_filters.TestFilters):
   """Starboard Evergreen X64 Platform Test Filters."""
 
   def GetTestFilters(self):
-    filters = super(EvergreenX64TestFilters, self).GetTestFilters()
+    filters = super().GetTestFilters()
+
+    for target, tests in _FILTERED_TESTS.items():
+      filters.extend(test_filter.TestFilter(target, test) for test in tests)
+
     # Remove the exclusion filter on SbDrmTest.AnySupportedKeySystems.
     # Generally, children of linux/shared do not support widevine, but children
     # of linux/x64x11 do, if the content decryption module is present.
-
     has_cdm = os.path.isfile(
         os.path.join(paths.REPOSITORY_ROOT, 'third_party', 'cdm', 'cdm',
                      'include', 'content_decryption_module.h'))
@@ -39,10 +61,11 @@ class EvergreenX64TestFilters(shared_test_filters.TestFilters):
     if not has_cdm:
       return filters
 
-    test_filters = []
-    for test_filter in filters:
-      if (test_filter.target_name == 'nplb' and
-          test_filter.test_name == 'SbDrmTest.AnySupportedKeySystems'):
-        continue
-      test_filters.append(test_filter)
-    return test_filters
+    def IsAllowedTest(test_filter_item):
+      return (test_filter_item.target_name == 'nplb' and
+              test_filter_item.test_name == 'SbDrmTest.AnySupportedKeySystems')
+
+    return [
+        test_filter_item for test_filter_item in filters
+        if not IsAllowedTest(test_filter_item)
+    ]
