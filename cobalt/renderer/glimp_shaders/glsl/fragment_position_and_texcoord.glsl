f0ac58761e40acaf247de178acafe8da56012e99
Andrew Savage <andrewsavage@google.com>
2023-03-28 22:53:05 +0000

    Move PS shaders to PS directory
    b/230921556
    
    Change-Id: I1f05f8cc9b754c5201e8663eda55cd608bdaa503
    NOKEYCHECK=True
    GitOrigin-RevId: 4d2896a2d28e2f94b8602972b8499d715245e712

--- cobalt/renderer/glimp_shaders/glsl/fragment_position_and_texcoord.glsl
+++
@@ -1,7 +0,0 @@
-precision mediump float;
-varying vec2 v_tex_coord;
-uniform sampler2D tex;
-
-void main() {
-  gl_FragColor = texture2D(tex, v_tex_coord);
-}
--- cobalt/renderer/glimp_shaders/glsl/fragment_textured_vbo_rgba.glsl
+++
@@ -1,9 +0,0 @@
-precision mediump float;
-varying vec2 v_tex_coord_rgba;
-uniform sampler2D texture_rgba;
-
-void main() {
-  vec4 untransformed_color = vec4(texture2D(texture_rgba, v_tex_coord_rgba).rgba);
-  vec4 color = untransformed_color;
-  gl_FragColor = color;
-}
