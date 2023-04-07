7ad1957959ec3926bc704b6f4fe9eb3ab4a266a8
Andrew Savage <andrewsavage@google.com>
2023-04-07 15:23:26 +0000

    Get build number from commits if available
    b/257323971
    
    Change-Id: I9aefb1f4881eb3eaf8be092523c1dd263a47d1d2
    NOKEYCHECK=True
    GitOrigin-RevId: 3d27bc748fcd80d59f2568a10f7bd336561a0938

--- cobalt/build/get_build_id.py
+++ cobalt/build/get_build_id.py
@@ -15,20 +15,62 @@
 """Prints out the Cobalt Build ID."""
 
 import os
+import re
+import subprocess
+
 from cobalt.build.build_number import GetOrGenerateNewBuildNumber
+
+COMMIT_COUNT_BUILD_NUMBER_OFFSET = 1000000
+
+# Matches numbers > 1000000. The pattern is basic so git log --grep is able to
+# interpret it.
+GIT_BUILD_NUMBER_PATTERN = r'[1-9]' + r'[0-9]' * 6 + r'[0-9]*'
+BUILD_NUMBER_TAG_PATTERN = r'^BUILD_NUMBER={}$'
+
+# git log --grep can't handle capture groups.
+BUILD_NUBER_PATTERN_WITH_CAPTURE = f'({GIT_BUILD_NUMBER_PATTERN})'
+
+
+def get_build_number_from_commits():
+  full_pattern = BUILD_NUMBER_TAG_PATTERN.format(GIT_BUILD_NUMBER_PATTERN)
+  output = subprocess.check_output(
+      ['git', 'log', '--grep', full_pattern, '-1', '--pretty=%b']).decode()
+
+  full_pattern_with_capture = re.compile(
+      BUILD_NUMBER_TAG_PATTERN.format(BUILD_NUBER_PATTERN_WITH_CAPTURE),
+      flags=re.MULTILINE)
+  match = full_pattern_with_capture.match(output)
+  return match.group(1) if match else None
+
+
+def get_build_number_from_server():
+  # Note $BUILD_ID_SERVER_URL will always be set in CI.
+  build_id_server_url = os.environ.get('BUILD_ID_SERVER_URL')
+  if not build_id_server_url:
+    return None
+
+  build_num = GetOrGenerateNewBuildNumber(version_server=build_id_server_url)
+  if build_num == 0:
+    raise ValueError('The build number received was zero.')
+  return build_num
+
+
+def get_build_number_from_commit_count():
+  output = subprocess.check_output(['git', 'rev-list', '--count', 'HEAD'])
+  build_number = int(output.strip().decode('utf-8'))
+  return build_number + COMMIT_COUNT_BUILD_NUMBER_OFFSET
 
 
 def main():
-  # Note $BUILD_ID_SERVER_URL will always be set in CI.
-  build_id_server_url = os.environ.get('BUILD_ID_SERVER_URL')
-  if build_id_server_url:
-    build_num = GetOrGenerateNewBuildNumber(version_server=build_id_server_url)
-    if build_num == 0:
-      raise ValueError('The build number received was zero.')
-    print(build_num)
-  else:
-    # No need to generate a build id for local builds.
-    print('0')
+  build_number = get_build_number_from_commits()
+
+  if not build_number:
+    build_number = get_build_number_from_server()
+
+  if not build_number:
+    build_number = get_build_number_from_commit_count()
+
+  print(build_number)
 
 
 if __name__ == '__main__':
