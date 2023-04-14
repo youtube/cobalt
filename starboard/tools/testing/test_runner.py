29b4f9177130426e760a7045cc0fe763f7a73fea
Oscar Vestlie <oxv@google.com>
2023-04-14 16:11:26 -0700

    Add shard index to xml path/filename
    Change-Id: Id4f1f6eb06ec8d6152e1bb381acecf8db8004b48

--- starboard/tools/testing/test_runner.py
+++ starboard/tools/testing/test_runner.py
@@ -462,7 +462,8 @@ class TestRunner(object):
          (self.log_xml_results or self.xml_output_dir) else "disabled"))
     if self.log_xml_results:
       out_path = MakeLauncher().GetDeviceOutputPath()
-      xml_filename = f"{target_name}_testoutput.xml"
+      shard_suffix = "" if shard_count is None else f"_{shard_index}"
+      xml_filename = f"{target_name}{shard_suffix}_testoutput.xml"
       if out_path:
         test_result_xml_path = os.path.join(out_path, xml_filename)
       else:
@@ -472,11 +473,13 @@ class TestRunner(object):
                     "be logged to '%s'."), test_result_xml_path)
     elif self.xml_output_dir:
       # Have gtest create and save a test result xml
-      xml_output_subdir = os.path.join(self.xml_output_dir, target_name)
+      shard_dir = "" if shard_count is None else f"{shard_index}"
+      xml_output_subdir = os.path.join(self.xml_output_dir, shard_dir,
+                                       target_name)
       try:
         os.makedirs(xml_output_subdir)
-      except OSError:
-        pass
+      except OSError as ose:
+        logging.warning("Unable to create xml output directory: %s", ose)
       test_result_xml_path = os.path.join(xml_output_subdir, "sponge_log.xml")
       logging.info("Xml output for this test will be saved to: %s",
                    test_result_xml_path)
