ba4b707b8c23f28c69d02afcf73a97436681e88a
Arjun Menon <agmenon@google.com>
2023-04-20 12:37:43 -0700

    Support compilation with Visual Studio 2022 for PS4/5 (#116)
    * Add helper script to install Visual Studio 2022
    * Support VS22 builds for on-host builds for PS4/5
    
    b/265873968

---
+++ tools/install_visual_studio_22.ps1
@@ -0,0 +1,14 @@
+# Helper script to automate installing Visual Studio 22 and required components.
+
+New-Item -Path "C:\" -Name "src" -ItemType "directory" -Force
+
+Invoke-WebRequest -Uri https://aka.ms/vs/17/release/vs_professional.exe `
+    -OutFile C:\src\vs_professional.exe
+
+C:\src\vs_professional.exe --wait --installPath `
+    "C:/Program Files (x86)/Microsoft Visual Studio/2022/Professional" `
+    --add Microsoft.VisualStudio.Component.VC.CoreIde `
+    --add Microsoft.VisualStudio.Component.VC.14.34.17.4.x86.x64 `
+    --add Microsoft.VisualStudio.Component.VC.Llvm.Clang `
+    --add Microsoft.VisualStudio.Component.VC.Llvm.ClangToolset `
+    --add Microsoft.VisualStudio.Component.Windows10SDK.18362
