bd72af839d7f3a621ea3a0e8c3b09938a69e2e8a
Esmael Elmoslimany <aee@google.com>
2023-04-27 11:54:58 -0700

    Set gn arg in shared platform configurations.
    b/277955457

--- third_party/v8/src/wasm/wasm-code-manager.cc
+++ third_party/v8/src/wasm/wasm-code-manager.cc
@@ -178,14 +178,14 @@ std::unique_ptr<const byte[]> WasmCode::ConcatenateBytes(
   size_t total_size = 0;
   for (auto& vec : vectors) total_size += vec.size();
   // Use default-initialization (== no initialization).
-  std::unique_ptr<byte[]> result{new byte[total_size]};
-  byte* ptr = result.get();
+  byte* result = new byte[total_size];
+  byte* ptr = result;
   for (auto& vec : vectors) {
     if (vec.empty()) continue;  // Avoid nullptr in {memcpy}.
     memcpy(ptr, vec.begin(), vec.size());
     ptr += vec.size();
   }
-  return result;
+  return std::unique_ptr<const byte[]>(result);
 }
 
 void WasmCode::RegisterTrapHandlerData() {
