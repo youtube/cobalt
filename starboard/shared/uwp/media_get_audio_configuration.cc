9f5c1ba3f6ede39d4ba499911c12fdfb77be2a96
Tyler Holcombe <tholcombe@google.com>
2023-05-01 14:27:18 -0700

    [XB1] Fix UWP build error on previous SB versions
    Fix build error for UWP builds on starboard versions less than 15.
    
    b/267678497
    
    Change-Id: I48b6a9357b46b0fa4f218a32e99a88ef6b6f043c

--- starboard/shared/uwp/media_get_audio_configuration.cc
+++ starboard/shared/uwp/media_get_audio_configuration.cc
@@ -23,8 +23,12 @@ bool SbMediaGetAudioConfiguration(
   if (output_index != 0 || out_configuration == NULL) {
     return false;
   }
-
+#if SB_API_VERSION >= SB_MEDIA_ENHANCED_AUDIO_API_VERSION
   out_configuration->connector = kSbMediaAudioConnectorUnknown;
+#else   // SB_API_VERSION >= SB_MEDIA_ENHANCED_AUDIO_API_VERSION
+  out_configuration->index = 0;
+  out_configuration->connector = kSbMediaAudioConnectorNone;
+#endif  // SB_API_VERSION >= SB_MEDIA_ENHANCED_AUDIO_API_VERSION
   out_configuration->latency = 0;
   out_configuration->coding_type = kSbMediaAudioCodingTypePcm;
 
