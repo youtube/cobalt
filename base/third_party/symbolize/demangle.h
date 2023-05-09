a1dcbbe7860f1aa01228711b5c95a433b5b64104
Brian Ting <brianting@google.com>
2023-05-09 15:56:43 -0700

    Update base/third_party/symbolize.
    Update base/third_party/symbolize to d2073a5c from
    https://chromium.googlesource.com/chromium/src/base. This is a manual
    copy paste update as base/third_party/symbolize is not a repo but only a
    directory of the base repo which we are not updating in its entirety
    yet.
    
    b/278782593
    
    Change-Id: I6393ac2560585a91841d01db5378d80f96a5eae1

--- base/third_party/symbolize/demangle.h
+++ base/third_party/symbolize/demangle.h
@@ -70,14 +70,17 @@
 #ifndef BASE_DEMANGLE_H_
 #define BASE_DEMANGLE_H_
 
+#include <stddef.h>
+
 #include "config.h"
+#include "glog/logging.h"
 
 _START_GOOGLE_NAMESPACE_
 
 // Demangle "mangled".  On success, return true and write the
 // demangled symbol name to "out".  Otherwise, return false.
 // "out" is modified even if demangling is unsuccessful.
-bool Demangle(const char *mangled, char *out, int out_size);
+bool GLOG_EXPORT Demangle(const char* mangled, char* out, size_t out_size);
 
 _END_GOOGLE_NAMESPACE_
 
