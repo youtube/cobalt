b1ff31085ddc857ed960e039760b43ad16a8b5b0
David Benjamin <davidben@chromium.org>
2023-05-09 05:15:57 +0000

    Upgrade some DCHECKs in net/base/io_buffer.cc to CHECK
    Even by the original guidance, they should have been CHECK instead of
    DCHECK, as failing them would rather badly mess up spatial memory
    safety.
    
    Also add some CHECKs to SocketBIOAdapter.
    
    Bug: 1440692
    Change-Id: I0f97f726ab5e3398fbe5e0e95580ce045d6d5607
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4513783
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141194}
    (cherry picked from commit 71ec175d18c7b7feb1294f6be1492cd326f71f86)

--- net/base/io_buffer.cc
+++ net/base/io_buffer.cc
@@ -95,8 +95,8 @@ int DrainableIOBuffer::BytesConsumed() const {
 }
 
 void DrainableIOBuffer::SetOffset(int bytes) {
-  DCHECK_GE(bytes, 0);
-  DCHECK_LE(bytes, size_);
+  CHECK_GE(bytes, 0);
+  CHECK_LE(bytes, size_);
   used_ = bytes;
   data_ = base_->data() + used_;
 }
@@ -109,7 +109,7 @@ DrainableIOBuffer::~DrainableIOBuffer() {
 GrowableIOBuffer::GrowableIOBuffer() = default;
 
 void GrowableIOBuffer::SetCapacity(int capacity) {
-  DCHECK_GE(capacity, 0);
+  CHECK_GE(capacity, 0);
 // Calling reallocate with size 0 and a non-null pointer causes memory leaks
 // on many platforms, since it may return nullptr while also not deallocating
 // the previously allocated memory.
@@ -122,6 +122,7 @@ void GrowableIOBuffer::SetCapacity(int capacity) {
     return;
   }
 #endif
+
   // this will get reset in `set_offset`.
   data_ = nullptr;
   // realloc will crash if it fails.
@@ -135,8 +136,8 @@ void GrowableIOBuffer::SetCapacity(int capacity) {
 }
 
 void GrowableIOBuffer::set_offset(int offset) {
-  DCHECK_GE(offset, 0);
-  DCHECK_LE(offset, capacity_);
+  CHECK_GE(offset, 0);
+  CHECK_LE(offset, capacity_);
   offset_ = offset;
   data_ = real_data_.get() + offset;
 }
