2a7dec039b30514daa67a64897a01c550af9e003
aee <117306596+aee-google@users.noreply.github.com>
2023-05-12 12:13:35 -0700

    Add basic WASM test. (#328)
    b/277955457

--- cobalt/black_box_tests/black_box_tests.py
+++ cobalt/black_box_tests/black_box_tests.py
@@ -81,6 +81,7 @@ _TESTS_NO_SIGNAL = [
     'service_worker_persist_test',
     'soft_mic_platform_service_test',
     'text_encoding_test',
+    'wasm_basic_test',
     'web_debugger',
     'web_platform_tests',
     'web_worker_test',
