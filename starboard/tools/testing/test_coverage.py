391cf822800f28cade5b177b005857b560d5a8aa
Oscar Vestlie <oxv@google.com>
2023-05-11 23:32:21 -0700

    Comments, documentation, minor fixes

--- starboard/tools/testing/test_coverage.py
+++ starboard/tools/testing/test_coverage.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-"""Cross-platform unit test runner."""
+"""Generate coverage reports from llvm coverage data."""
 
 import argparse
 import glob
@@ -29,25 +29,31 @@ from starboard.tools import build
 from starboard.tools.util import SetupDefaultLoggingConfig
 
 
-def _get_raw_reports(targets, coverage_dir):
+def _get_raw_reports(test_binaries, coverage_dir):
+  """Get a list of raw coverage reports from the coverage directory."""
   raw_reports = []
-  for target in targets:
+  for target in test_binaries:
+    # Find coverage reports from the supplied test binaries.
     profraw_file = os.path.join(coverage_dir, target + '.profraw')
-    if os.path.isfile(profraw_file):
-      raw_reports.append(profraw_file)
+    if not os.path.isfile(profraw_file):
+      raise RuntimeError(f'Missing coverage report file: \'{profraw_file}\'')
+    raw_reports.append(profraw_file)
   else:
+    # If no test binaries were supplied we use all coverage data we find.
     raw_reports = glob.glob(os.path.join(coverage_dir, '*.profraw'))
   return raw_reports
 
 
-def _get_target_paths(binaries_dir, targets, coverage_dir):
-  raw_reports = _get_raw_reports(targets, coverage_dir)
-  if not targets:
-    targets = [pathlib.Path(report).stem for report in raw_reports]
-  return [os.path.join(binaries_dir, target) for target in targets]
+def _get_test_binary_paths(binaries_dir, test_binaries, coverage_dir):
+  if not test_binaries:
+    # Use the coverage files to deduce the paths if no binaries were specified.
+    raw_reports = _get_raw_reports(test_binaries, coverage_dir)
+    test_binaries = [pathlib.Path(report).stem for report in raw_reports]
+  return [os.path.join(binaries_dir, binary) for binary in test_binaries]
 
 
 def _get_exclude_opts():
+  """Get list of exclude options,"""
   # TODO(oxv): This requires the script to be invoked from the source root.
   all_dirs = next(os.walk(os.getcwd()))[1]
   # TODO(oxv): Make configurable?
@@ -67,7 +73,8 @@ def _get_exclude_opts():
   ] + [f'--ignore-filename-regex={d}/.*' for d in exclude_dirs]
 
 
-def _generate_reports(target_paths, profdata_path, report_dir, report_type):
+def _generate_reports(target_paths, merged_data_path, report_dir, report_type):
+  """Generate reports based on the combined coverage data."""
   toolchain_dir = build.GetClangBinPath(clang.GetClangSpecification())
   llvm_cov = os.path.join(toolchain_dir, 'llvm-cov')
 
@@ -76,58 +83,57 @@ def _generate_reports(target_paths, profdata_path, report_dir, report_type):
   test_binary_opts = [target_paths[0]] + \
     ['-object=' + target for target in target_paths[1:]]
   if report_type in ['html', 'both']:
+    logging.info('Creating html coverage report')
     show_cmd_list = [
-        llvm_cov, 'show', *exclude_opts, '-instr-profile=' + profdata_path,
+        llvm_cov, 'show', *exclude_opts, '-instr-profile=' + merged_data_path,
         '-format=html', '-output-dir=' + os.path.join(report_dir, 'html'),
         *test_binary_opts
     ]
-    logging.info('Creating html coverage report')
     subprocess.check_call(show_cmd_list, text=True)
 
   if report_type in ['text', 'both']:
+    logging.info('Creating text coverage report')
     report_cmd_list = [
-        llvm_cov, 'show', *exclude_opts, '-instr-profile=' + profdata_path,
+        llvm_cov, 'show', *exclude_opts, '-instr-profile=' + merged_data_path,
         '-format=text', *test_binary_opts
     ]
-    logging.info('Creating text coverage report')
     with open(os.path.join(report_dir, 'report.txt'), 'wb') as out:
       subprocess.check_call(report_cmd_list, stdout=out, text=True)
 
 
 def _merge_coverage_data(targets, coverage_dir):
-  profdata_path = os.path.join(coverage_dir, 'report.profdata')
-
+  """Create a combined coverage file from the raw reports."""
   toolchain_dir = build.GetClangBinPath(clang.GetClangSpecification())
   llvm_profdata = os.path.join(toolchain_dir, 'llvm-profdata')
 
   raw_reports = _get_raw_reports(targets, coverage_dir)
   if not raw_reports:
-    logging.warning('Found no coverage reports')
     raise RuntimeError('No coverage data found in \'coverage_dir\'')
 
-  merge_cmd_list = [
-      llvm_profdata, 'merge', '-sparse=true', '-o', profdata_path, *raw_reports
-  ]
   logging.info('Merging coverage files')
+  merged_data_path = os.path.join(coverage_dir, 'report.profdata')
+  merge_cmd_list = [
+      llvm_profdata, 'merge', '-sparse=true', '-o', merged_data_path
+  ] + raw_reports
   subprocess.check_call(merge_cmd_list, text=True)
 
-  return profdata_path
+  return merged_data_path
 
 
-def create_report(binaries_dir, targets, coverage_dir, report_type='both'):
-  """Generate the source code coverage report."""
-  profdata_path = _merge_coverage_data(targets, coverage_dir)
+def create_report(binary_dir, test_binaries, coverage_dir, report_type='both'):
+  """Generate source code coverage reports."""
+  merged_data_path = _merge_coverage_data(test_binaries, coverage_dir)
 
   # TODO(oxv): Make configurable?
   report_dir = coverage_dir
-  target_paths = _get_target_paths(binaries_dir, targets, coverage_dir)
-  _generate_reports(target_paths, profdata_path, report_dir, report_type)
+  target_paths = _get_test_binary_paths(binary_dir, test_binaries, coverage_dir)
+  _generate_reports(target_paths, merged_data_path, report_dir, report_type)
 
 
 if __name__ == '__main__':
   SetupDefaultLoggingConfig()
 
-  def type_checker(param):
+  def report_type_checker(param):
     if param not in ['text', 'html', 'both']:
       raise argparse.ArgumentTypeError('invalid value: ' + param)
     return param
@@ -140,20 +146,20 @@ if __name__ == '__main__':
   arg_parser.add_argument(
       '--coverage_dir',
       required=True,
-      help='Directory where coverage files are located and where the report '
+      help='Directory where coverage files are located and where the reports '
       'will be generated.')
   arg_parser.add_argument(
-      '--targets',
+      '--test_binaries',
       nargs='+',
       default=[],
       help='List of test binaries that should be used for the coverage report. '
       'If not passed all coverage files in --coverage_dir will be used.')
   arg_parser.add_argument(
       '--report_type',
-      type=type_checker,
+      type=report_type_checker,
       default='both',
       help='Specifies the report type to create. Must be one of \'html\', '
-      '\'text\', or \'both\'. Default is \'both\'')
+      '\'text\', or \'both\'. Default is \'both\'.')
 
   args = arg_parser.parse_args()
 
