c28c35f1adadc2127b0ea63076e012b66730702a
Kert <kaidokert@google.com>
2023-05-16 09:30:09 -0700

    Re-enable Pytest runs on Windows (#385)
    Enables Pytest runs on windows with a few changes:
    
    * Enable Windows pytest runs
    * Enable pip caching - Speeds up tests by caching pip install results
    * Add missing atomicwrites - Required on Windows for pytest+coverage runs. It's an undeclared
    indirect dependency of some involved packages.
    * Disable Raspi launcher tests on Windows - The pexpect and shell handling as written wont work on Windows,
    and this module isn't written or intended to run on it.
    * Disable Build-id tests on Win - These are newly added and fail with absolute/temporary path
    handling. Need to be looked at.
    * Disable failing SABI test on Windows - Old test that fails due to invalid temporary path handling on Win
    
    b/279747505
    b/282040638
    b/281098312

--- starboard/raspi/shared/launcher_test.py
+++ starboard/raspi/shared/launcher_test.py
@@ -28,6 +28,7 @@ import pexpect
 # pylint: disable=missing-class-docstring
 
 
+@unittest.skipIf(os.name == 'nt', 'Pexpect does not work on Windows')
 class LauncherTest(unittest.TestCase):
 
   def setUp(self):
