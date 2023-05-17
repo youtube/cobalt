f48f548f6de6a5931e396404882c6efb089f44d4
Brian Ting <brianting@google.com>
2023-05-17 16:35:46 -0700

    Fix linking errors in third_party/musl.
    b/277595686
    
    Change-Id: I3faa641a560b76aac6974a4ec2aa157a5b860244

--- third_party/musl/src/include/features.h
+++ third_party/musl/src/include/features.h
@@ -4,7 +4,7 @@
 #include "../../include/features.h"
 
 #define weak __attribute__((__weak__))
-#define hidden __attribute__((__visibility__("default")))
+#define hidden __attribute__((__visibility__("hidden")))
 #define weak_alias(old, new) \
 	extern __typeof(old) new __attribute__((__weak__, __alias__(#old)))
 
