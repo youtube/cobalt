4a47c76e3aeb6b44753afbf5401f6861ac77910e
Yavor Goulishev <yavor@google.com>
2023-05-25 19:12:01 +0000

    Enable base_unittests and net_unittests.
    - Fix the GLog implementation to use SB_LOG instead of std::cerr.
    
    b/273820939

--- testing/gtest/include/gtest/internal/gtest-port.h
+++ testing/gtest/include/gtest/internal/gtest-port.h
@@ -1297,10 +1297,13 @@ class GTEST_API_ GTestLog {
 };
 
 #if !defined(GTEST_LOG_)
-
+#if GTEST_OS_STARBOARD
+#define GTEST_LOG_ SB_LOG
+#else
 # define GTEST_LOG_(severity) \
     ::testing::internal::GTestLog(::testing::internal::GTEST_##severity, \
                                   __FILE__, __LINE__).GetStream()
+#endif
 
 inline void LogToStderr() {}
 #if GTEST_OS_STARBOARD
