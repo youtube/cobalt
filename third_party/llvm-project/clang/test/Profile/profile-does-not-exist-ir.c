1020b7fea504872c9b9f963c724f5ba4321416c3
Brian Ting <brian.t.ting@gmail.com>
2023-05-25 02:05:28 +0000

    Remove unused llvm files part1. (#454)
    b/284200202
    
    Co-authored-by: Brian Ting <brianting@google.com>
    (cherry picked from commit 2674b38ad6143423e4ea87677d5808efda0367b3)

--- third_party/llvm-project/clang/test/Profile/profile-does-not-exist-ir.c
+++
@@ -1,4 +0,0 @@
-// RUN: not %clang_cc1 -emit-llvm -x ir %s -o - -fprofile-instrument-use-path=%t.nonexistent.profdata 2>&1 | FileCheck %s
-
-// CHECK: error: Error in reading profile {{.*}}.nonexistent.profdata:
-// CHECK-NOT: Assertion failed
