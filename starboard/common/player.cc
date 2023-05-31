1ee39cb42f83f2cc52fadabe0d0c2cc2e2716e3c
Xiaoming Shi <xiaomings@google.com>
2023-05-30 17:37:46 -0700

    Refine decode-to-texture mode support
    Now the web app can explicitly choose to play a video in
    decode-to-texture mode by following the steps below:
    1. Query the playback capability with "decode-to-texture=true" to ensure
       that decode-to-texture mode is supported for the particular playback.
    2. Specify "decode-to-texture=true" as an mime attribute of the video
       stream or the max video capabilities set via
       `HTMLVideoElement::setMaxVideoCapabilities()`.
    
    Previously decode-to-texture mode can only be selected when the playback
    is in 360 mode, otherwise the playback will be default to punch-out.
    
    The CL also includes the following changes:
    1. Replaced `Pipeline::SetDecodeToTextureOutputMode()` by
       `Pipeline::SetPreferredOutputModeToDecodeToTexture()`, to explicitly
       signal that we can only set preferred output mode to
       decode-to-texture, but not set it to punch-out.
    2. No longer set the default output mode to punch-out, set it to
       `kSbPlayerOutputModeInvalid` instead when calling
       `SbPlayerGetPreferredOutputMode()`, to allow the platform select the
       default output mode.
    3. Add an h5vcc setting "Media.ForcePunchOutByDefault" to allow the web
       app to explicitly set the default output mode to punch-out.  This
       allows us to revert to the previous behavior in case the new behavior
       mentioned in #2 causes issues on some devices.
    4. Turned some functions in `Pipeline` (like `Suspend()`) to pure
       virtual, as `SbPlayerPipeline` is the only derived class of
       `Pipeline`, we no longer have to provide a default implementation for
       these functions in `Pipeline`.
    5. Implemented `starboard::GetPlayerOutputModeName()` in
       starboard/common/player.* with tests.
    
    b/261922568
    b/286121490

---
+++ starboard/common/player.cc
@@ -0,0 +1,35 @@
+// Copyright 2023 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/common/player.h"
+
+#include "starboard/common/log.h"
+
+namespace starboard {
+
+const char* GetPlayerOutputModeName(SbPlayerOutputMode output_mode) {
+  switch (output_mode) {
+    case kSbPlayerOutputModeDecodeToTexture:
+      return "decode-to-texture";
+    case kSbPlayerOutputModePunchOut:
+      return "punch-out";
+    case kSbPlayerOutputModeInvalid:
+      return "invalid";
+  }
+
+  SB_NOTREACHED();
+  return "invalid";
+}
+
+}  // namespace starboard
