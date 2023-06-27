e4607159f88f9675b5aab59a3d07e56b169ebc8d
Brian Ting <brianting@google.com>
2023-06-27 11:44:55 -0700

    Creates build_info.json alongside cobalt_build_id. (#685)
    * Creates build_info.json alongside cobalt_build_id.
    
    Creates build_info.json alongside the logic that creates
    cobalt_build_id.h. This offloads generating build_info.json from the CI
    system to GN and updates build_info.json with the build_id, build_rev,
    git_rev, build_time, author, and commit fields.
    
    On Git repos, git_rev is equivalent to build_rev. On Gerrit repos,
    git_rev represents the last Git hash synced, tied with Git build_id.
    
    b/284367797
    
    * Refactored to preserve backwards compatibility.
    
    b/284367797

--- cobalt/build/get_build_id_test.py
+++ cobalt/build/get_build_id_test.py
@@ -21,9 +21,10 @@ import subprocess
 import tempfile
 import unittest
 
+from cobalt.build import build_info
 from cobalt.build import get_build_id
 
-_TEST_BUILD_NUMBER = 1234 + get_build_id.COMMIT_COUNT_BUILD_NUMBER_OFFSET
+_TEST_BUILD_NUMBER = 1234 + build_info.COMMIT_COUNT_BUILD_ID_OFFSET
 
 
 # TODO(b/282040638): fix and re-enabled this
@@ -58,7 +59,7 @@ class GetBuildIdTest(unittest.TestCase):
 
   def testGetBuildNumberFromCommitsSunnyDay(self):
     self.make_commit_with_build_number()
-    build_number = get_build_id.get_build_number_from_commits(
+    build_number, _ = build_info.get_build_id_and_git_rev_from_commits(
         cwd=self.test_dir.name)
     self.assertEqual(int(build_number), _TEST_BUILD_NUMBER)
 
@@ -66,17 +67,17 @@ class GetBuildIdTest(unittest.TestCase):
     num_commits = 5
     for i in range(num_commits):
       self.make_commit_with_build_number(
-          get_build_id.COMMIT_COUNT_BUILD_NUMBER_OFFSET + i)
-    build_number = get_build_id.get_build_number_from_commits(
+          build_info.COMMIT_COUNT_BUILD_ID_OFFSET + i)
+    build_number, _ = build_info.get_build_id_and_git_rev_from_commits(
         cwd=self.test_dir.name)
     self.assertEqual(
         int(build_number),
-        num_commits + get_build_id.COMMIT_COUNT_BUILD_NUMBER_OFFSET - 1)
+        num_commits + build_info.COMMIT_COUNT_BUILD_ID_OFFSET - 1)
 
   def testGetBuildNumberFromCommitsRainyDayInvalidBuildNumber(self):
     self.make_commit()
     self.make_commit(f'BUILD_NUMBER={_TEST_BUILD_NUMBER}')
-    build_number = get_build_id.get_build_number_from_commits(
+    build_number, _ = build_info.get_build_id_and_git_rev_from_commits(
         cwd=self.test_dir.name)
     self.assertIsNone(build_number)
 
@@ -84,11 +85,10 @@ class GetBuildIdTest(unittest.TestCase):
     num_commits = 5
     for _ in range(num_commits):
       self.make_commit()
-    build_number = get_build_id.get_build_number_from_commit_count(
+    build_number = build_info.get_build_id_from_commit_count(
         cwd=self.test_dir.name)
-    self.assertEqual(
-        build_number,
-        num_commits + get_build_id.COMMIT_COUNT_BUILD_NUMBER_OFFSET)
+    self.assertEqual(build_number,
+                     num_commits + build_info.COMMIT_COUNT_BUILD_ID_OFFSET)
 
   def testCommitsOutrankCommitCount(self):
     self.make_commit()
@@ -102,9 +102,8 @@ class GetBuildIdTest(unittest.TestCase):
     for _ in range(num_commits):
       self.make_commit()
     build_number = get_build_id.main(cwd=self.test_dir.name)
-    self.assertEqual(
-        build_number,
-        num_commits + get_build_id.COMMIT_COUNT_BUILD_NUMBER_OFFSET)
+    self.assertEqual(build_number,
+                     num_commits + build_info.COMMIT_COUNT_BUILD_ID_OFFSET)
 
 
 if __name__ == '__main__':
