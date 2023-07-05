94eb6d71649d58be99692fe4f1333a0f11563b35
Oscar Vestlie <oxv@google.com>
2023-07-05 16:56:04 -0700

    Improve required checks script (#801)
    * Improve documentation
    * Improve debug printout
    * Filter checks that should not be included
    
    b/289552151

--- tools/update_required_branch_checks.py
+++ tools/update_required_branch_checks.py
@@ -11,23 +11,40 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Updates the requires status checks for a branch."""
+"""Updates the requires status checks for a branch.
+
+Requires PyGithub to run:
+
+  $ pip install PyGithub
+"""
 
 import argparse
 from github import Github
 from typing import List
 
+# Issue a Personal Access Token with 'repo' permission on
+# https://github.com/settings/tokens.
 YOUR_GITHUB_TOKEN = ''
 assert YOUR_GITHUB_TOKEN != '', 'YOUR_GITHUB_TOKEN must be set.'
 
 TARGET_REPO = 'youtube/cobalt'
 
 EXCLUDED_CHECK_PATTERNS = [
+    # Excludes non build/test checks.
     'feedback/copybara',
-    '_on_device_',
-    'codecov',
     'prepare_branch_list',
     'cherry_pick',
+
+    # Excludes coverage and test reports.
+    'linux-coverage',
+    'codecov',
+    'on-host-unit-test-report',
+
+    # Excludes blackbox, web platform and on-device tests.
+    'blackbox_test',
+    'wpt',
+    '_on_device_',
+
     # Excludes templated check names.
     '${{'
 ]
@@ -78,11 +95,23 @@ def get_required_checks_for_branch(repo, branch: str) -> List[str]:
   return list(check_names)
 
 
-def print_checks(branch: str, check_names: List[str]) -> None:
-  print(f'Checks for {branch}:')
-  for check_name in check_names:
-    print(check_name)
-  print()
+def print_checks(repo, branch_name: str, new_checks: List[str]) -> None:
+  branch = repo.get_branch(branch_name)
+  current_checks = branch.get_required_status_checks().contexts
+
+  def print_check_list(checks):
+    for check_name in sorted(checks):
+      print(check_name)
+    print()
+
+  print(f'Required checks to be ADDED for {branch_name}:')
+  print_check_list(set(new_checks) - set(current_checks))
+
+  print(f'Required checks to be REMOVED for {branch_name}:')
+  print_check_list(set(current_checks) - set(new_checks))
+
+  print(f'Required checks that will REMAIN for {branch_name}:')
+  print_check_list(set(current_checks).intersection(set(new_checks)))
 
 
 def update_protection_for_branch(repo, branch: str,
@@ -118,7 +147,7 @@ def main() -> None:
   for branch in args.branch:
     required_checks = get_required_checks_for_branch(repo, branch)
     if args.dry_run:
-      print_checks(branch, required_checks)
+      print_checks(repo, branch, required_checks)
     else:
       update_protection_for_branch(repo, branch, required_checks)
 
