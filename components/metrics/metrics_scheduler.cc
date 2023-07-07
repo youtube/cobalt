ef2b0580441ec9bab10640d59a4eaa4c019535d3
Joel Martinez <joeltine@google.com>
2023-07-06 19:41:43 -0600

    Add telemetry black box tests (#821)
    This change required new testing command line flags to shorten the
    upload interval of metrics. This included the startup delay and initial
    upload interval of the metrics libraries.
    
    I also used this as an opportunity to make some improvements to the
    black box README.
    
    b/287248166
    
    Change-Id: I469c6b3c4d4a34fb7d5770c5030e5cecb077b396

--- components/metrics/metrics_scheduler.cc
+++ components/metrics/metrics_scheduler.cc
@@ -5,6 +5,12 @@
 #include "components/metrics/metrics_scheduler.h"
 
 #include "build/build_config.h"
+#if defined(STARBOARD)
+#include "base/command_line.h"
+#include "base/logging.h"
+#include "base/strings/string_number_conversions.h"
+#include "components/metrics/metrics_switches.h"
+#endif
 
 namespace metrics {
 
@@ -21,11 +27,39 @@ const int kInitialIntervalSeconds = 60;
 
 }  // namespace
 
+// In Cobalt, we need a command line argument to override the initial upload
+// interval for testing.
+#if defined(STARBOARD)
+MetricsScheduler::MetricsScheduler(const base::Closure& task_callback)
+    : task_callback_(task_callback), running_(false), callback_pending_(false) {
+  const base::CommandLine* command_line =
+      base::CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(switches::kInitialMetricsUploadIntervalSec)) {
+    int custom_initial_upload_interval = -1;
+    if (base::StringToInt(command_line->GetSwitchValueASCII(
+                              switches::kInitialMetricsUploadIntervalSec),
+                          &custom_initial_upload_interval)) {
+      interval_ = base::TimeDelta::FromSeconds(custom_initial_upload_interval);
+      LOG(INFO) << "Initial upload interval overriden on the command line to: "
+                << custom_initial_upload_interval;
+    } else {
+      interval_ = base::TimeDelta::FromSeconds(kInitialIntervalSeconds);
+      LOG(WARNING)
+          << "Initial upload interval was set on the command line, but "
+             "converting it to an int failed. Falling back to the default: "
+          << kInitialIntervalSeconds;
+    }
+  } else {
+    interval_ = base::TimeDelta::FromSeconds(kInitialIntervalSeconds);
+  }
+}
+#else
 MetricsScheduler::MetricsScheduler(const base::Closure& task_callback)
     : task_callback_(task_callback),
       interval_(base::TimeDelta::FromSeconds(kInitialIntervalSeconds)),
       running_(false),
       callback_pending_(false) {}
+#endif
 
 MetricsScheduler::~MetricsScheduler() {}
 
