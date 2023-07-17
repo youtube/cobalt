027ddbc2a4560f0e787fefa1d1dca735e300bc00
Max Zhang <60412045+maxz-lab@users.noreply.github.com>
2023-07-17 17:58:38 +0000

    [WIP] enable arm tests in black_box_test
    Experimental: enable arm tests in black_box_test
    b/291648592

--- cobalt/black_box_tests/black_box_tests.py
+++ cobalt/black_box_tests/black_box_tests.py
@@ -34,7 +34,7 @@ from starboard.tools import command_line
 from starboard.tools import log_level
 
 _DISABLED_BLACKBOXTEST_CONFIGS = [
-    'android-arm/devel',
+    'android-arm/devel',  
     'android-arm64/devel',
     'android-x86/devel',
     'evergreen-arm/devel',
