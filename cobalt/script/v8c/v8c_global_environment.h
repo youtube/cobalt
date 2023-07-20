3719a5e2efdb6495dce3152d9fe83bf60b51ef89
Dana Dahlstrom <dahlstrom@google.com>
2023-07-19 19:00:00 -0700

    Replace SetAllowCodeGenerationFromStringsCallback
    … deprecated by V8 commits 1e7e3e9b and aabe6406
    with SetModifyCodeGenerationFromStringsCallback as specified.
    
    See http://crbug.com/v8/10096 and http://crbug.com/1096017.
    
    b/156155426

--- cobalt/script/v8c/v8c_global_environment.h
+++ cobalt/script/v8c/v8c_global_environment.h
@@ -151,8 +151,10 @@ class V8cGlobalEnvironment : public GlobalEnvironment,
     v8::Isolate* isolate_;
   };
 
-  static bool AllowCodeGenerationFromStringsCallback(
-      v8::Local<v8::Context> context, v8::Local<v8::String> source);
+  static v8::ModifyCodeGenerationFromStringsResult
+  ModifyCodeGenerationFromStringsCallback(v8::Local<v8::Context> context,
+                                          v8::Local<v8::Value> source,
+                                          bool is_code_like);
 
   static void MessageHandler(v8::Local<v8::Message> message,
                              v8::Local<v8::Value> data);
