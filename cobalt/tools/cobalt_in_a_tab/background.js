b47dfca6d05193c66d96ed9a2978e623d7cdb7e4
Kaido Kert <kaidokert@google.com>
2023-07-23 10:08:41 -0700

    Remove "Cobalt in a tab" tool
    Remove a legacy tool - it's not currently used. Also delete
    unused shell script and diff files that fail presubmits.
    
    b/150410605

--- cobalt/tools/cobalt_in_a_tab/background.js
+++
@@ -1,9 +0,0 @@
-// Local machine is the first "Default" IP.
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({
-        ip_endpoints: [{
-            device: 'Local',
-            endpoint: 'http://localhost'
-        }],
-    });
-});
