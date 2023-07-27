8aa813f0f86398d2677d19c428f28069ab275c24
Dana Dahlstrom <dahlstrom@google.com>
2023-07-26 21:00:00 -0700

    Avoid use of thread_local by commit 076687ab

--- third_party/v8/src/common/assert-scope.cc
+++ third_party/v8/src/common/assert-scope.cc
@@ -21,15 +21,30 @@ template <PerIsolateAssertType kType>
 using PerIsolateDataBit = base::BitField<bool, kType, 1>;
 
 // Thread-local storage for assert data. Default all asserts to "allow".
+#if defined(V8_OS_STARBOARD)
+static_assert(sizeof(uint32_t) <= sizeof(void*), "uint32_t won't fit in void*");
+DEFINE_LAZY_LEAKY_OBJECT_GETTER(base::Thread::LocalStorageKey, GetAssertDataKey,
+                                base::Thread::CreateThreadLocalKey())
+#else
 thread_local uint32_t current_per_thread_assert_data(~0);
+#endif  // defined(V8_OS_STARBOARD)
 
 }  // namespace
 
 template <PerThreadAssertType kType, bool kAllow>
 PerThreadAssertScope<kType, kAllow>::PerThreadAssertScope()
+#if defined(V8_OS_STARBOARD)
+    : old_data_(static_cast<uint32_t>(reinterpret_cast<uintptr_t>(
+          base::Thread::GetThreadLocal(*GetAssertDataKey())))) {
+  uint32_t data = PerThreadDataBit<kType>::update(old_data_.value(), kAllow);
+  base::Thread::SetThreadLocal(
+      *GetAssertDataKey(),
+      reinterpret_cast<void*>(static_cast<uintptr_t>(data)));
+#else
     : old_data_(current_per_thread_assert_data) {
   current_per_thread_assert_data =
       PerThreadDataBit<kType>::update(old_data_.value(), kAllow);
+#endif  // defined(V8_OS_STARBOARD)
 }
 
 template <PerThreadAssertType kType, bool kAllow>
@@ -40,13 +55,24 @@ PerThreadAssertScope<kType, kAllow>::~PerThreadAssertScope() {
 
 template <PerThreadAssertType kType, bool kAllow>
 void PerThreadAssertScope<kType, kAllow>::Release() {
+#if defined(V8_OS_STARBOARD)
+  base::Thread::SetThreadLocal(
+      *GetAssertDataKey(),
+      reinterpret_cast<void*>(static_cast<uintptr_t>(old_data_.value())));
+#else
   current_per_thread_assert_data = old_data_.value();
+#endif  // defined(V8_OS_STARBOARD)
   old_data_.reset();
 }
 
 // static
 template <PerThreadAssertType kType, bool kAllow>
 bool PerThreadAssertScope<kType, kAllow>::IsAllowed() {
+#if defined(V8_OS_STARBOARD)
+  uint32_t current_per_thread_assert_data =
+      static_cast<uint32_t>(reinterpret_cast<uintptr_t>(
+          base::Thread::GetThreadLocal(*GetAssertDataKey())));
+#endif  // defined(V8_OS_STARBOARD)
   return PerThreadDataBit<kType>::decode(current_per_thread_assert_data);
 }
 
