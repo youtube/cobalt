fd9c679026071ec1babeb188b956c82dee26f14c
Antonio Rivera <141369695+antoniori-eng@users.noreply.github.com>
2023-08-08 12:34:45 -0700

    Add support for PCM and FLAC to the default linux starboard impl. (#1135)
    We expect that most partners will copy/modify the code in
    media_is_audio_supported.cc and mime_util.cc. The change in the FFmpeg
    decoder is for local testing purposes, though it is currently limited to
    S16 content. It would be possible to support all formats via PCM
    resampling logic in the decoder.
    
    Bug: b/293515731
    
    Test:
    Played S16 PCM and FLAC content via serving a simple <audio> tag with
    flask and running, e.g.:
    cobalt --enable_demuxer_extension --url=http://localhost:5000/flac
    Also added some unit tests.
    
    (cherry picked from commit 1c7dfccb414efd5f63c8b98e491a936d02c95f12)

--- starboard/shared/starboard/media/codec_util.cc
+++ starboard/shared/starboard/media/codec_util.cc
@@ -103,6 +103,11 @@ SbMediaAudioCodec GetAudioCodecFromString(const char* codec) {
   if (strcmp(codec, "flac") == 0) {
     return kSbMediaAudioCodecFlac;
   }
+  // For WAV, the "codecs" parameter of a MIME type refers to the WAVE format
+  // ID, where 1 represents PCM: https://datatracker.ietf.org/doc/html/rfc2361
+  if (strcmp(codec, "1") == 0) {
+    return kSbMediaAudioCodecPcm;
+  }
 #endif  // SB_API_VERSION >= 14
 #if SB_API_VERSION >= 15
   if (strncmp(codec, "iamf.", 5) == 0) {
