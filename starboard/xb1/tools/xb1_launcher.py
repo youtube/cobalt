e59e75131ee8922127907bf1ecd650aa96cb8429
Tyler Holcombe <tholcombe@google.com>
2023-08-16 14:02:26 -0700

    [XB1] Fix automated install error
    Fix installer error for devel and debug builds due to missing
    dependency.
    
    b/291640038
    
    Change-Id: Ic81ad30874549ab19b4d9d8893e574095290f20f

--- starboard/xb1/tools/xb1_launcher.py
+++ starboard/xb1/tools/xb1_launcher.py
@@ -90,6 +90,15 @@ _STARBOARD_ARGUMENTS_FILE = 'starboard_arguments.txt'
 _DEFAULT_PACKAGE_NAME = 'GoogleInc.YouTube'
 _DEFAULT_APPX_NAME = 'cobalt.appx'
 _DEFAULT_STAGING_APP_NAME = 'appx'
+_WIN_SDK_DIR = os.path.normpath(
+    os.environ.get(
+        'WINDOWSSDKDIR',
+        os.path.expandvars('%ProgramFiles(x86)%'
+                           '\\Windows Kits\\10')))
+_DEBUG_VC_LIBS_PATH = os.path.join(_WIN_SDK_DIR, 'ExtensionSDKs',
+                                   'Microsoft.VCLibs', '14.0', 'Appx', 'Debug',
+                                   'x64',
+                                   'Microsoft.VCLibs.x64.Debug.14.00.appx')
 _XB1_LOG_FILE_PARAM = 'xb1_log_file'
 _XB1_PORT = 11443
 _XB1_NET_LOG_PORT = 49353
@@ -427,14 +436,16 @@ class Launcher(abstract_launcher.AbstractLauncher):
 
     try:
       self._LogLn('Installing appx file ' + appx_package_file)
-      self.WinAppDeployCmd('install -file ' + appx_package_file)
+      self.WinAppDeployCmd(
+          f'install -file {appx_package_file} -d {_DEBUG_VC_LIBS_PATH}')
     except subprocess.CalledProcessError:
       # Install exited with non-zero status code, clear everything out, restart,
       # and attempt another install.
       self._LogLn('Error installing appx. Attempting a clean install...')
       self.UninstallSubPackages()
       self.RestartDevkit()
-      self.WinAppDeployCmd('install -file ' + appx_package_file)
+      self.WinAppDeployCmd(
+          f'install -file {appx_package_file} -d {_DEBUG_VC_LIBS_PATH}')
 
     # Cleanup starboard arguments file.
     self.InstallStarboardArgument(None)
