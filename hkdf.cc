44a3ea38e750ed64d7a180e9a39d7127f67bf3ab
Chrome Release Bot (LUCI) <chrome-official-brancher@chops-service-accounts.iam.gserviceaccount.com>
2023-08-16 14:10:37 +0000

    Publish DEPS for 114.0.5735.331
    git-subtree-dir: crypto
    git-subtree-split: fed499399d3f44d3a7957549d493bf30a5d8c867

--- hkdf.cc
+++ hkdf.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,7 +9,7 @@
 
 #include <memory>
 
-#include "base/logging.h"
+#include "base/check.h"
 #include "crypto/hmac.h"
 #include "third_party/boringssl/src/include/openssl/digest.h"
 #include "third_party/boringssl/src/include/openssl/hkdf.h"
@@ -29,6 +29,19 @@ std::string HkdfSha256(base::StringPiece secret,
       reinterpret_cast<const uint8_t*>(info.data()), info.size());
   DCHECK(result);
   return key;
+}
+
+std::vector<uint8_t> HkdfSha256(base::span<const uint8_t> secret,
+                                base::span<const uint8_t> salt,
+                                base::span<const uint8_t> info,
+                                size_t derived_key_size) {
+  std::vector<uint8_t> ret;
+  ret.resize(derived_key_size);
+  int result =
+      ::HKDF(ret.data(), derived_key_size, EVP_sha256(), secret.data(),
+             secret.size(), salt.data(), salt.size(), info.data(), info.size());
+  DCHECK(result);
+  return ret;
 }
 
 }  // namespace crypto
