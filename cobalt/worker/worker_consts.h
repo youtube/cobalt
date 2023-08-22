99740f6d93f4cf918a4e40680d63afec18b9cf78
Kaido Kert <kaidokert@google.com>
2023-08-21 19:23:50 -0700

    Make Worker names constexpr
    These constants are currently used from within the Web module,
    this creates a reverse symbol dependency in modular builds. Turning
    these names to constexpr ensures no symbols are emitted.
    
    b/296578318

--- cobalt/worker/worker_consts.h
+++ cobalt/worker/worker_consts.h
@@ -18,32 +18,61 @@
 namespace cobalt {
 namespace worker {
 
-struct WorkerConsts {
-  // Constants for error messages.
-  static const char kServiceWorkerRegisterBadMIMEError[];
-  static const char kServiceWorkerRegisterNoMIMEError[];
-  static const char kServiceWorkerRegisterScriptOriginNotSameError[];
-  static const char kServiceWorkerRegisterScopeOriginNotSameError[];
-  static const char kServiceWorkerRegisterBadScopeError[];
-  static const char kServiceWorkerUnregisterScopeOriginNotSameError[];
-  static const char kServiceWorkerAllowed[];
+namespace WorkerConsts {
 
-  // Constants for ServiceWorkerPersistentSettings
-  static const char kSettingsJson[];
+// Constants for error messages.
+constexpr const char kServiceWorkerRegisterBadMIMEError[] =
+    "Service Worker Register failed: The script has an unsupported MIME type "
+    "('%s').";
+constexpr const char kServiceWorkerRegisterNoMIMEError[] =
+    "Service Worker Register failed: The script does not have a MIME type.";
+constexpr const char kServiceWorkerRegisterScriptOriginNotSameError[] =
+    "Service Worker Register failed: Script URL ('%s') and referrer ('%s') "
+    "origin are not the same.";
+constexpr const char kServiceWorkerRegisterScopeOriginNotSameError[] =
+    "Service Worker Register failed: Scope URL ('%s') and referrer ('%s') "
+    "origin are not the same.";
+constexpr const char kServiceWorkerRegisterBadScopeError[] =
+    "Service Worker Register failed: Scope ('%s') is not allowed.";
+constexpr const char kServiceWorkerUnregisterScopeOriginNotSameError[] =
+    "Service Worker Unregister failed: Scope origin does not match.";
 
-  // Array of JavaScript mime types, according to the MIME Sniffinc spec:
-  //   https://mimesniff.spec.whatwg.org/#javascript-mime-type
-  static const char* const kJavaScriptMimeTypes[16];
+constexpr const char kServiceWorkerAllowed[] = "Service-Worker-Allowed";
 
-  // The name of a Service Worker thread.
-  static const char kServiceWorkerName[];
+// Constants for ServiceWorkerPersistentSettings
+constexpr const char kSettingsJson[] = "service_worker_settings.json";
 
-  // The name of a Service Worker Registry thread.
-  static const char kServiceWorkerRegistryName[];
+// Array of JavaScript mime types, according to the MIME Sniffinc spec:
+//   https://mimesniff.spec.whatwg.org/#javascript-mime-type
+constexpr const char* const kJavaScriptMimeTypes[16] = {
+    "application/ecmascript",
+    "application/javascript",
+    "application/x-ecmascript",
+    "application/x-javascript",
+    "text/ecmascript",
+    "text/javascript",
+    "text/javascript1.0",
+    "text/javascript1.1",
+    "text/javascript1.2",
+    "text/javascript1.3",
+    "text/javascript1.4",
+    "text/javascript1.5",
+    "text/jscript",
+    "text/livescript",
+    "text/x-ecmascript",
+    "text/x-javascript"};
 
-  // The name of a Dedicated Worker thread.
-  static const char kDedicatedWorkerName[];
-};
+// The name of a Service Worker thread.
+constexpr const char kServiceWorkerName[] = "ServiceWorker";
+
+// The name of a Service Worker Registry thread.
+constexpr const char kServiceWorkerRegistryName[] = "ServiceWorkerRegistry";
+
+// The name of a Dedicated Worker thread.
+constexpr const char kDedicatedWorkerName[] = "DedicatedWorker";
+
+};  // namespace WorkerConsts
+
 
 }  // namespace worker
 }  // namespace cobalt
