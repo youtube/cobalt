youtube/cobalt

commit f7b737cf696861a3ef276e9efcdc4f4e6ab2f352

Fix ANR from long device provisioning time. (#1337)
This avoids the application from blocking during device provisioning.

This separates creation of the media crypto session into its own
function, which is then called from a thread to allow it to complete
asynchronously off the main thread.

This also adds a 10 second timeout to the http post for provisioning.

b/296283608

starboard/android/apk/app/src/main/java/dev/cobalt/media/MediaDrmBridge.java
@@ -535,41 +535,10 @@ public class MediaDrmBridge {
     if (mMediaDrm == null) {
       throw new IllegalStateException("Cannot create media crypto with null mMediaDrm.");
     }
-    if (mMediaCryptoSession != null) {
-      throw new IllegalStateException(
-          "Cannot create media crypto with non-null mMediaCryptoSession.");
-    }
-    // TODO: Cannot do this during provisioning pending.
-
-    // Open media crypto session.
-    try {
-      mMediaCryptoSession = openSession();
-    } catch (NotProvisionedException e) {
-      Log.d(TAG, "Device not provisioned", e);
-      if (!attemptProvisioning()) {
-        Log.e(TAG, "Failed to provision device during MediaCrypto creation.");
-        return false;
-      }
-      try {
-        mMediaCryptoSession = openSession();
-      } catch (NotProvisionedException e2) {
-        Log.e(TAG, "Device still not provisioned after supposedly successful provisioning", e2);
-        return false;
-      }
-    }
-
-    if (mMediaCryptoSession == null) {
-      Log.e(TAG, "Cannot create MediaCrypto Session.");
-      return false;
-    }
-    Log.d(
-        TAG,
-        String.format("MediaCrypto Session created: %s", bytesToHexString(mMediaCryptoSession)));
-
     // Create MediaCrypto object.
     try {
       if (MediaCrypto.isCryptoSchemeSupported(mSchemeUUID)) {
-        MediaCrypto mediaCrypto = new MediaCrypto(mSchemeUUID, mMediaCryptoSession);
+        MediaCrypto mediaCrypto = new MediaCrypto(mSchemeUUID, new byte[0]);
         Log.d(TAG, "MediaCrypto successfully created!");
         mMediaCrypto = mediaCrypto;
         return true;
@@ -579,14 +548,6 @@ public class MediaDrmBridge {
     } catch (MediaCryptoException e) {
       Log.e(TAG, "Cannot create MediaCrypto", e);
     }
-
-    try {
-      // Some implementations let this method throw exceptions.
-      mMediaDrm.closeSession(mMediaCryptoSession);
-    } catch (Exception e) {
-      Log.e(TAG, "closeSession failed: ", e);
-    }
-    mMediaCryptoSession = null;
 
     return false;
   }
@@ -619,6 +580,58 @@ public class MediaDrmBridge {
       release();
       return null;
     }
+  }
+
+  @UsedByNative
+  boolean createMediaCryptoSession() {
+    if (mMediaCryptoSession != null) {
+      return true;
+    }
+    Log.w(TAG, "MediaDrmBridge createMediaCryptoSession");
+    if (mMediaCrypto == null) {
+      throw new IllegalStateException("Cannot create media crypto session with null mMediaCrypto.");
+    }
+
+    // Open media crypto session.
+    try {
+      mMediaCryptoSession = openSession();
+    } catch (NotProvisionedException e) {
+      Log.w(TAG, "Device not provisioned", e);
+      if (!attemptProvisioning()) {
+        Log.e(TAG, "Failed to provision device during MediaCrypto creation.");
+        return false;
+      }
+      try {
+        mMediaCryptoSession = openSession();
+      } catch (NotProvisionedException e2) {
+        Log.e(TAG, "Device still not provisioned after supposedly successful provisioning", e2);
+        return false;
+      }
+    }
+
+    try {
+      mMediaCrypto.setMediaDrmSession(mMediaCryptoSession);
+    } catch (MediaCryptoException e3) {
+      Log.e(TAG, "Unable to set media drm session", e3);
+      try {
+        // Some implementations let this method throw exceptions.
+        mMediaDrm.closeSession(mMediaCryptoSession);
+      } catch (Exception e) {
+        Log.e(TAG, "closeSession failed: ", e);
+      }
+      mMediaCryptoSession = null;
+      return false;
+    }
+
+    if (mMediaCryptoSession == null) {
+      Log.e(TAG, "Cannot create MediaCrypto Session.");
+      return false;
+    }
+    Log.d(
+        TAG,
+        String.format("MediaCrypto Session created: %s", bytesToHexString(mMediaCryptoSession)));
+
+    return true;
   }
 
   /**

starboard/android/shared/drm_system.cc
@@ -14,10 +14,15 @@
 
 #include "starboard/android/shared/drm_system.h"
 
+#include <memory>
+#include <utility>
+
 #include "starboard/android/shared/jni_env_ext.h"
 #include "starboard/android/shared/jni_utils.h"
 #include "starboard/android/shared/media_common.h"
+#include "starboard/common/atomic.h"
 #include "starboard/common/instance_counter.h"
+#include "starboard/common/thread.h"
 
 namespace {
 
@@ -177,7 +182,8 @@ DrmSystem::DrmSystem(
     SbDrmSessionUpdateRequestFunc update_request_callback,
     SbDrmSessionUpdatedFunc session_updated_callback,
     SbDrmSessionKeyStatusesChangedFunc key_statuses_changed_callback)
-    : key_system_(key_system),
+    : Thread("DrmSystemThread"),
+      key_system_(key_system),
       context_(context),
       update_request_callback_(update_request_callback),
       session_updated_callback_(session_updated_callback),
@@ -206,10 +212,34 @@ DrmSystem::DrmSystem(
     return;
   }
   j_media_crypto_ = env->ConvertLocalRefToGlobalRef(j_media_crypto_);
+
+  Start();
+}
+
+void DrmSystem::Run() {
+  JniEnvExt* env = JniEnvExt::Get();
+  bool result = env->CallBooleanMethodOrAbort(
+      j_media_drm_bridge_, "createMediaCryptoSession", "()Z");
+  if (result) {
+    created_media_crypto_session_.store(true);
+  }
+  if (!result && j_media_crypto_) {
+    env->DeleteGlobalRef(j_media_crypto_);
+    j_media_crypto_ = NULL;
+  }
+
+  ScopedLock scoped_lock(mutex_);
+  if (!deferred_session_update_requests_.empty()) {
+    for (const auto& update_request : deferred_session_update_requests_) {
+      update_request->Generate(j_media_drm_bridge_);
+    }
+    deferred_session_update_requests_.clear();
+  }
 }
 
 DrmSystem::~DrmSystem() {
   ON_INSTANCE_RELEASED(AndroidDrmSystem);
+  Join();
 
   JniEnvExt* env = JniEnvExt::Get();
   if (j_media_crypto_) {
@@ -223,17 +253,64 @@ DrmSystem::~DrmSystem() {
   }
 }
 
+DrmSystem::SessionUpdateRequest::SessionUpdateRequest(
+    int ticket,
+    const char* type,
+    const void* initialization_data,
+    int initialization_data_size) {
+  JniEnvExt* env = JniEnvExt::Get();
+  j_ticket_ = static_cast<jint>(ticket);
+  j_init_data_ =
+      ByteArrayFromRaw(initialization_data, initialization_data_size);
+  j_mime_ = env->NewStringStandardUTFOrAbort(type);
+}
+
+void DrmSystem::SessionUpdateRequest::ConvertLocalRefToGlobalRef() {
+  if (!references_are_global_) {
+    JniEnvExt* env = JniEnvExt::Get();
+    j_init_data_ = env->ConvertLocalRefToGlobalRef(j_init_data_);
+    j_mime_ = env->ConvertLocalRefToGlobalRef(j_mime_);
+    references_are_global_ = true;
+  }
+}
+
+DrmSystem::SessionUpdateRequest::~SessionUpdateRequest() {
+  JniEnvExt* env = JniEnvExt::Get();
+  if (references_are_global_) {
+    env->DeleteGlobalRef(j_init_data_);
+    env->DeleteGlobalRef(j_mime_);
+  } else {
+    env->DeleteLocalRef(j_init_data_);
+    env->DeleteLocalRef(j_mime_);
+  }
+  j_init_data_ = nullptr;
+  j_mime_ = nullptr;
+}
+
+void DrmSystem::SessionUpdateRequest::Generate(
+    jobject j_media_drm_bridge) const {
+  JniEnvExt* env = JniEnvExt::Get();
+  env->CallVoidMethodOrAbort(j_media_drm_bridge, "createSession",
+                             "(I[BLjava/lang/String;)V", j_ticket_,
+                             j_init_data_, j_mime_);
+}
+
 void DrmSystem::GenerateSessionUpdateRequest(int ticket,
                                              const char* type,
                                              const void* initialization_data,
                                              int initialization_data_size) {
-  ScopedLocalJavaRef<jbyteArray> j_init_data(
-      ByteArrayFromRaw(initialization_data, initialization_data_size));
-  JniEnvExt* env = JniEnvExt::Get();
-  ScopedLocalJavaRef<jstring> j_mime(env->NewStringStandardUTFOrAbort(type));
-  env->CallVoidMethodOrAbort(
-      j_media_drm_bridge_, "createSession", "(I[BLjava/lang/String;)V",
-      static_cast<jint>(ticket), j_init_data.Get(), j_mime.Get());
+  std::unique_ptr<SessionUpdateRequest> session_update_request(
+      new SessionUpdateRequest(ticket, type, initialization_data,
+                               initialization_data_size));
+  if (created_media_crypto_session_.load()) {
+    session_update_request->Generate(j_media_drm_bridge_);
+  } else {
+    // Defer generating the update request.
+    session_update_request->ConvertLocalRefToGlobalRef();
+    ScopedLock scoped_lock(mutex_);
+    deferred_session_update_requests_.push_back(
+        std::move(session_update_request));
+  }
   // |update_request_callback_| will be called by Java calling into
   // |onSessionMessage|.
 }

starboard/android/shared/media_decoder.cc
@@ -35,6 +35,9 @@ const jlong kNoPts = 0;
 const jint kNoSize = 0;
 const jint kNoBufferFlags = 0;
 
+// Delay to use after a retryable error has been encountered.
+const SbTime kErrorRetryDelay = 50 * kSbTimeMillisecond;
+
 const char* GetNameForMediaCodecStatus(jint status) {
   switch (status) {
     case MEDIA_CODEC_OK:
@@ -461,15 +464,23 @@ bool MediaDecoder::ProcessOneInputBuffer(
 
   jint status;
   if (event.type == Event::kWriteCodecConfig) {
-    status = media_codec_bridge_->QueueInputBuffer(dequeue_input_result.index,
-                                                   kNoOffset, size, kNoPts,
-                                                   BUFFER_FLAG_CODEC_CONFIG);
+    if (drm_system_ && drm_system_->IsReady()) {
+      status = media_codec_bridge_->QueueInputBuffer(dequeue_input_result.index,
+                                                     kNoOffset, size, kNoPts,
+                                                     BUFFER_FLAG_CODEC_CONFIG);
+    } else {
+      status = MEDIA_CODEC_NO_KEY;
+    }
   } else if (event.type == Event::kWriteInputBuffer) {
     jlong pts_us = input_buffer->timestamp();
     if (drm_system_ && input_buffer->drm_info()) {
-      status = media_codec_bridge_->QueueSecureInputBuffer(
-          dequeue_input_result.index, kNoOffset, *input_buffer->drm_info(),
-          pts_us);
+      if (drm_system_->IsReady()) {
+        status = media_codec_bridge_->QueueSecureInputBuffer(
+            dequeue_input_result.index, kNoOffset, *input_buffer->drm_info(),
+            pts_us);
+      } else {
+        status = MEDIA_CODEC_NO_KEY;
+      }
     } else {
       status = media_codec_bridge_->QueueInputBuffer(
           dequeue_input_result.index, kNoOffset, size, pts_us, kNoBufferFlags);
@@ -534,6 +545,7 @@ void MediaDecoder::HandleError(const char* action_name, jint status) {
     SB_LOG(INFO) << "|" << action_name << "| failed with status: "
                  << GetNameForMediaCodecStatus(status)
                  << ", will try again after a delay.";
+    SbThreadYield();
   } else {
     SB_LOG(ERROR) << "|" << action_name << "| failed with status: "
                   << GetNameForMediaCodecStatus(status) << ".";

starboard/android/apk/app/src/main/java/dev/cobalt/coat/CobaltHttpHelper.java
@@ -25,6 +25,7 @@ import java.net.URL;
 
 /** Helper class that implements an HTTP POST function used by DRM one-time provisioning. */
 public class CobaltHttpHelper {
+  private static final int HTTP_TIMEOUT_MILLIS = 10000;
   private static final int RETRY_SLEEP_MILLIS = 250;
   private static final int MAX_ATTEMPTS = 3;
 
@@ -72,6 +73,8 @@ public class CobaltHttpHelper {
   private byte[] internalPerformHttpPost(String url) throws IOException, TransientFailure {
     HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
     try {
+      conn.setConnectTimeout(HTTP_TIMEOUT_MILLIS);
+      conn.setReadTimeout(HTTP_TIMEOUT_MILLIS);
       conn.setRequestMethod("POST");
       conn.setDoOutput(false);
       conn.setDoInput(true);

starboard/android/shared/drm_system.h
@@ -18,21 +18,25 @@
 #include "starboard/shared/starboard/drm/drm_system_internal.h"
 
 #include <jni.h>
+#include <memory>
 
 #include <string>
 #include <unordered_map>
 #include <vector>
 
+#include "starboard/android/shared/jni_utils.h"
 #include "starboard/android/shared/media_common.h"
+#include "starboard/common/atomic.h"
 #include "starboard/common/log.h"
 #include "starboard/common/mutex.h"
+#include "starboard/common/thread.h"
 #include "starboard/types.h"
 
 namespace starboard {
 namespace android {
 namespace shared {
 
-class DrmSystem : public ::SbDrmSystemPrivate {
+class DrmSystem : public ::SbDrmSystemPrivate, private Thread {
  public:
   DrmSystem(const char* key_system,
             void* context,
@@ -81,8 +85,32 @@ class DrmSystem : public ::SbDrmSystemPrivate {
     return IsWidevineL1(key_system_.c_str());
   }
 
+  // Return true when the drm system is ready for secure input buffers.
+  bool IsReady() { return created_media_crypto_session_.load(); }
+
  private:
+  class SessionUpdateRequest {
+   public:
+    SessionUpdateRequest(int ticket,
+                         const char* type,
+                         const void* initialization_data,
+                         int initialization_data_size);
+    ~SessionUpdateRequest();
+
+    void ConvertLocalRefToGlobalRef();
+    void Generate(jobject j_media_drm_bridge) const;
+
+   private:
+    bool references_are_global_ = false;
+    jint j_ticket_;
+    jobject j_init_data_;
+    jobject j_mime_;
+  };
+
   void CallKeyStatusesChangedCallbackWithKeyStatusRestricted_Locked();
+
+  // From Thread.
+  void Run() override;
 
   const std::string key_system_;
   void* context_;
@@ -94,9 +122,13 @@ class DrmSystem : public ::SbDrmSystemPrivate {
   jobject j_media_drm_bridge_;
   jobject j_media_crypto_;
 
+  std::vector<std::unique_ptr<SessionUpdateRequest>>
+      deferred_session_update_requests_;
+
   Mutex mutex_;
-  std::unordered_map<std::string, std::vector<SbDrmKeyId> > cached_drm_key_ids_;
+  std::unordered_map<std::string, std::vector<SbDrmKeyId>> cached_drm_key_ids_;
   bool hdcp_lost_;
+  atomic_bool created_media_crypto_session_;
 
   std::vector<uint8_t> metrics_;
 };

commit 61e71279d2fe3563db28eb70d6de14d0f42a479b

[Android] Build fixes. (#1376)
Some build fixes for error cases:
- If there is no DrmSystem active, don't wait for it to be ready to
write the codec config.
- Don't generate deferred requests when creation failed.
- Test mMediaCryptoSession for null before using .

This was broken in #1337.

b/296283608

starboard/android/apk/app/src/main/java/dev/cobalt/media/MediaDrmBridge.java
@@ -609,6 +609,11 @@ public class MediaDrmBridge {
       }
     }
 
+    if (mMediaCryptoSession == null) {
+      Log.e(TAG, "Cannot create MediaCrypto Session.");
+      return false;
+    }
+
     try {
       mMediaCrypto.setMediaDrmSession(mMediaCryptoSession);
     } catch (MediaCryptoException e3) {
@@ -623,10 +628,6 @@ public class MediaDrmBridge {
       return false;
     }
 
-    if (mMediaCryptoSession == null) {
-      Log.e(TAG, "Cannot create MediaCrypto Session.");
-      return false;
-    }
     Log.d(
         TAG,
         String.format("MediaCrypto Session created: %s", bytesToHexString(mMediaCryptoSession)));

starboard/android/shared/drm_system.cc
@@ -226,6 +226,7 @@ void DrmSystem::Run() {
   if (!result && j_media_crypto_) {
     env->DeleteGlobalRef(j_media_crypto_);
     j_media_crypto_ = NULL;
+    return;
   }
 
   ScopedLock scoped_lock(mutex_);

starboard/android/shared/media_decoder.cc
@@ -464,7 +464,7 @@ bool MediaDecoder::ProcessOneInputBuffer(
 
   jint status;
   if (event.type == Event::kWriteCodecConfig) {
-    if (drm_system_ && drm_system_->IsReady()) {
+    if (!drm_system_ || (drm_system_ && drm_system_->IsReady())) {
       status = media_codec_bridge_->QueueInputBuffer(dequeue_input_result.index,
                                                      kNoOffset, size, kNoPts,
                                                      BUFFER_FLAG_CODEC_CONFIG);