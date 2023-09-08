b6b108c9cdd48fb31961bed53d3841ca49a00a69
Thorsten Sideboard <sideboard@google.com>
2023-09-08 15:39:18 -0700

    Initial setup test.
    Change-Id: I810d406f719e9b78e20ce5ed25a0f90f60ec9a1b

--- cobalt/media/player/web_media_player_impl_unittest.cc
+++ cobalt/media/player/web_media_player_impl_unittest.cc
@@ -19,7 +19,25 @@
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-class MockWebMediaPlayerClient : public cobalt::media::WebMediaPlayerClient {
+namespace cobalt {
+namespace media {
+
+using ::cobalt::media::DefaultSbPlayerInterface;
+using ::cobalt::media::SbPlayerInterface;
+using ::cobalt::media::WebMediaPlayerImpl;
+using ::media::ChunkDemuxer;
+using ::testing::NiceMock;
+
+
+namespace {
+SbDecodeTargetGraphicsContextProvider*
+MockGetSbDecodeTargetGraphicsContextProvider() {
+  return NULL;
+}
+}  // namespace
+
+class MockWebMediaPlayerClient : public cobalt::media::WebMediaPlayerClient,
+                                 public cobalt::media::WebMediaPlayerDelegate {
  public:
   MockWebMediaPlayerClient() = default;
 
@@ -29,33 +47,64 @@ class MockWebMediaPlayerClient : public cobalt::media::WebMediaPlayerClient {
   MockWebMediaPlayerClient& operator=(const MockWebMediaPlayerClient&&) =
       delete;
 
+  // WebMediaPlayerClient methods
   MOCK_METHOD0(NetworkStateChanged, void());
   MOCK_METHOD1(NetworkError, void(const std::string& message));
+  MOCK_METHOD0(ReadyStateChanged, void());
+  MOCK_METHOD1(TimeChanged, void(bool eos_played));
+  MOCK_METHOD0(DurationChanged, void());
+  MOCK_METHOD0(OutputModeChanged, void());
+  MOCK_METHOD0(ContentSizeChanged, void());
+  MOCK_METHOD0(PlaybackStateChanged, void());
+  MOCK_CONST_METHOD0(Volume, float());
+  MOCK_METHOD1(SourceOpened, void(ChunkDemuxer* chunk_demuxer));
+  MOCK_CONST_METHOD0(SourceURL, std::string());
+  MOCK_CONST_METHOD0(MaxVideoCapabilities, std::string());
+  MOCK_METHOD3(EncryptedMediaInitDataEncountered,
+               void(const char* init_data_type, const unsigned char* init_data,
+                    unsigned int init_data_length));
+
+  // WebMediaPlayerDelegate methods
+  MOCK_METHOD1(RegisterPlayer, void(WebMediaPlayer* player));
+  MOCK_METHOD1(UnregisterPlayer, void(WebMediaPlayer* player));
 };
 
 class WebMediaPlayerImplTest : public ::testing::Test {
  public:
-  WebMediaPlayerImplTest() : media_thread_("MediaThreadForTest") {
-    media_thread_.StartAndWaitForTesting();
+  WebMediaPlayerImplTest() : sbplayer_interface_(new DefaultSbPlayerInterface) {
+    wmpi_ = std::make_unique<WebMediaPlayerImpl>(
+        sbplayer_interface_.get(), nullptr,
+        base::Bind(MockGetSbDecodeTargetGraphicsContextProvider), &client_,
+        &client_, true, false, true,
+#if SB_API_VERSION >= 15
+        10, 10,
+#endif  // SB_API_VERSION >= 15
+        nullptr);
   }
 
-  typedef ::cobalt::media::WebMediaPlayerImpl WebMediaPlayerImpl;
+ protected:
+  bool HasAudio() { return wmpi_->HasAudio(); }
+  bool HasVideo() { return wmpi_->HasVideo(); }
+  bool IsPaused() { return wmpi_->IsPaused(); }
+  float GetPlaybackRate() { return wmpi_->GetPlaybackRate(); }
 
  protected:
-  void InitializeWebMediaPlayerImpl() {}
-  // bool IsSuspended() { return wmpi_->pipeline_controller_->IsSuspended(); }
-  bool IsSuspended() { return false; }
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 
- protected:
-  // "Media" thread. This is necessary because WMPI destruction waits on a
-  // WaitableEvent.
-  base::Thread media_thread_;
+  // The client interface used by |wmpi_|.
+  NiceMock<MockWebMediaPlayerClient> client_;
 
+  std::unique_ptr<SbPlayerInterface> sbplayer_interface_;
   // The WebMediaPlayerImpl instance under test.
   std::unique_ptr<WebMediaPlayerImpl> wmpi_;
 };
 
 TEST_F(WebMediaPlayerImplTest, ConstructAndDestroy) {
-  InitializeWebMediaPlayerImpl();
-  EXPECT_FALSE(IsSuspended());
+  EXPECT_TRUE(IsPaused());
+  EXPECT_FALSE(HasAudio());
+  EXPECT_FALSE(HasVideo());
+  EXPECT_EQ(GetPlaybackRate(), 0);
 }
+
+}  // namespace media
+}  // namespace cobalt
