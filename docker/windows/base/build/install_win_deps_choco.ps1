35853097513c0e29b7898374ac10c1a58455764e
Arjun Menon <agmenon@google.com>
2023-09-29 17:11:10 -0700

    Move chocolately deps installs to script

---
+++ docker/windows/base/build/install_win_deps_choco.ps1
@@ -0,0 +1,23 @@
+# Pin Choco to 1.4.0 to avoid required reboot in 2.0.0
+$env:chocolateyVersion='1.4.0'
+
+iex ((New-Object System.Net.WebClient).DownloadString(
+      'https://chocolatey.org/install.ps1'))`
+
+mkdir C:\choco-cache
+
+# Note: We pinned python 3.11.0 version here because 3.11.2 has a regression
+# where the install arguments like TargetDir were being ignored.
+choco install -y -c C:\choco-cache python3 --version 3.11.0 -ia `
+  '/quiet InstallAllUsers=1 PrependPath=1 TargetDir="C:\Python3"'
+choco install -y -c C:\choco-cache winflexbison3 --params '/InstallDir:C:\bison'
+choco install -y -c C:\choco-cache ninja
+choco install -y -c C:\choco-cache nodejs-lts
+choco install -y -c C:\choco-cache git
+choco install -y -c C:\choco-cache cmake --installargs 'ADD_CMAKE_TO_PATH=System'
+
+Write-Host ('Deleting the chocolately download cache')
+C:\fast-win-rmdir.cmd $env:TEMP
+C:\fast-win-rmdir.cmd C:\choco-cache
+
+Copy-Item C:\Python3\python.exe C:\Python3\python3.exe
