b0289a4bdd1397eb93b4dd6e4dbda8e64a63a67b
Sherry Zhou <sherryzy@google.com>
2023-10-04 12:52:44 -0700

    Add pointer event test on cropped element
    b/238885425
    b/266714023

--- cobalt/black_box_tests/black_box_tests.py
+++ cobalt/black_box_tests/black_box_tests.py
@@ -79,6 +79,7 @@ _TESTS_NO_SIGNAL = [
     'http_cache',
     'persistent_cookie',
     'pointer_event_on_fixed_element_test',
+    'pointer_event_on_cropped_element_test',
     'scroll',
     'scroll_with_none_pointer_events',
     'service_worker_add_to_cache_test',
