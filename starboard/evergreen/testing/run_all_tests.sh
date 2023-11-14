829beab799e9dd28c904f0929e907deb978be6ec
FirstName LastName <yavor@google.com>
2023-11-14 20:21:23 +0000

    Disable evergreen_tests to pickup SbMemory changes.
    b/302332972
    
    Change-Id: I105e85181cadbc2c04be6dcd2ef06449e06bc95b

--- starboard/evergreen/testing/run_all_tests.sh
+++ starboard/evergreen/testing/run_all_tests.sh
@@ -24,7 +24,7 @@ AUTH_METHOD="public-key"
 USE_COMPRESSED_SYSTEM_IMAGE="false"
 SYSTEM_IMAGE_EXTENSION=".so"
 
-DISABLE_TESTS="false"
+DISABLE_TESTS="true"
 
 while getopts "d:a:c" o; do
     case "${o}" in
