497c0c5f12d3d87708222f871f40dcb26434fff0
Madhura Jayaraman <madjay@google.com>
2023-12-13 10:49:14 -0800

    Deprecate SBDirectoryCreate
    b/302730696
    
    Change-Id: Ia2b302a7bb4ecc38fef13ac527efe5df23628a2f

--- third_party/crashpad/wrapper/wrapper.cc
+++ third_party/crashpad/wrapper/wrapper.cc
@@ -87,7 +87,7 @@ base::FilePath GetDatabasePath() {
   std::string crashpad_directory_path(cache_directory_path.data());
   crashpad_directory_path.push_back(kSbFileSepChar);
   crashpad_directory_path.append("crashpad_database");
-  if (!mkdir(crashpad_directory_path.c_str(), 0700)) {
+  if (mkdir(crashpad_directory_path.c_str(), 0700) != 0) {
     LOG(ERROR) << "Couldn't create directory for crashpad database";
     return base::FilePath("");
   }
