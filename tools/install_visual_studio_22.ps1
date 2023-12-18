a9b7f5fcb02bb69f73fc91b0ebea0de6e1844194
Arjun Menon <agmenon@google.com>
2023-12-18 15:53:49 -0800

    Update the dev workstation VS installer script (#2111)
    After pinning the Visual Studio version in docker containers, we also
    should update the installer script to use the pinned version.
    
    b/316212112
    
    Change-Id: I96c1a994c603f59a261ab7f6fd06ab42e61ac8bd
    (cherry picked from commit 8be6017613341e156ee6ce62bddfb908f29aa50c)

--- tools/install_visual_studio_22.ps1
+++ tools/install_visual_studio_22.ps1
@@ -2,8 +2,9 @@
 
 New-Item -Path "C:\" -Name "src" -ItemType "directory" -Force
 
-Invoke-WebRequest -Uri https://aka.ms/vs/17/release/vs_professional.exe `
-    -OutFile C:\src\vs_professional.exe
+$VS_INSTALL_URL="https://download.visualstudio.microsoft.com/download/pr/d1ed8638-9e88-461e-92b7-4e29cc6172c3/30be9c2a5e40d5666eeae683e319b472c8bcc8231c7b346fe798f0ad0f7e498b/vs_Professional.exe"
+
+Invoke-WebRequest -Uri $VS_INSTALL_URL -OutFile C:\src\vs_professional.exe
 
 C:\src\vs_professional.exe --wait --installPath `
     "C:/Program Files (x86)/Microsoft Visual Studio/2022/Professional" `
