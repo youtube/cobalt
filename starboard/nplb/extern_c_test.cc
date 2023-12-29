375fbfc79dc2f06a2cffe49f1183d065b6ff35a7
iuriionishchenko <136322748+iuriionishchenko@users.noreply.github.com>
2023-12-29 23:43:41 +0200

    Convert hybrid nav to starboard extension (#1930)
    Refactoring to move the AppleTV hybrid nav implementation from a
    Starboard API to a Starboard extension
    
    b/299481829
    
    Co-authored-by: Iurii Onishchenko <ionishchenko@IONISHCHEN3-MAC.local>

--- starboard/nplb/extern_c_test.cc
+++ starboard/nplb/extern_c_test.cc
@@ -55,8 +55,8 @@ extern "C" {
 #include "starboard/time.h"
 #include "starboard/time_zone.h"
 #include "starboard/types.h"
-#include "starboard/ui_navigation.h"
 #if SB_API_VERSION < 16
+#include "starboard/ui_navigation.h"
 #include "starboard/user.h"
 #endif  // SB_API_VERSION < 16
 #include "starboard/window.h"
