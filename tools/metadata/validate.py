f6c49cc443191b60d9673cf1c188b5f4921ef9ec
Kaido Kert <kaidokert@google.com>
2023-12-29 20:28:21 -0800

    Metadata works

--- tools/metadata/validate.py
+++ tools/metadata/validate.py
@@ -16,25 +16,27 @@ def validate_file(metadata_file_path):
   # pylint: disable=import-outside-toplevel
   from tools.metadata.gen import metadata_file_pb2
   metadata = metadata_file_pb2.Metadata()
+  logging.info(metadata_file_path)
 
   with open(metadata_file_path, 'r', encoding='utf-8') as f:
     textproto_content = f.read()
     text_format.Parse(textproto_content, metadata)
 
   if not metadata.name:
-    log.warning('`name` field is missing')
+    log.warning('%s: `name` field should be present', metadata_file_path)
   if not metadata.description:
-    log.warning('`description` field is missing')
+    log.warning('%s: `description` field should be present', metadata_file_path)
   if not metadata.HasField('third_party'):
     raise RuntimeError('`third_party` field must be present')
 
   third_party = metadata.third_party
   if not third_party.license_type:
-    log.warning('third_party.licence_type is missing')
+    log.warning('%s: third_party.licence_type is missing', metadata_file_path)
   if not third_party.version:
-    log.warning('third_party.version field must be present')
-  if len(third_party.url) > 0:
-    log.warning('"url" field is deprecated, please use "identifier" instead')
+    log.warning('%s: third_party.version field should be present',
+                metadata_file_path)
+  # if len(third_party.url) > 0:
+  #  log.warning('"url" field is deprecated, please use "identifier" instead')
 
 
 def pregen():
@@ -49,6 +51,6 @@ def pregen():
 
 
 if __name__ == '__main__':
-  print(sys.argv[1])
-  pregen()
-  validate_file(sys.argv[1])
+  validate_files = sys.argv[1:]
+  # pregen()
+  res = [validate_file(file) for file in validate_files]
