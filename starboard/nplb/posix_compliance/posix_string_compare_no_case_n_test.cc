604b910e533af03419f96e773d0976b51c08e3eb
Madhura Jayaraman <madjay@google.com>
2024-01-08 10:36:26 -0800

    Add POSIX tests
    b/316954263
    
    Change-Id: I3e0452f0bb623042a905c8a65841fac1282d0820

---
+++ starboard/nplb/posix_compliance/posix_string_compare_no_case_n_test.cc
@@ -0,0 +1,61 @@
+// Copyright 2015 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/common/string.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace starboard {
+namespace nplb {
+namespace {
+
+#if SB_API_VERSION >= 16
+TEST(SbStringCompareNoCaseNTest, SunnyDaySelf) {
+  const char kString[] = "0123456789";
+  EXPECT_EQ(0, strncasecmp(kString, kString, strlen(kString)));
+  EXPECT_EQ(0, strncasecmp("", "", 0));
+}
+
+TEST(SbStringCompareNoCaseNTest, SunnyDayEmptyLessThanNotEmpty) {
+  const char kString[] = "0123456789";
+  EXPECT_GT(0, strncasecmp("", kString, strlen(kString)));
+}
+
+TEST(SbStringCompareNoCaseNTest, SunnyDayEmptyZeroNEqual) {
+  const char kString[] = "0123456789";
+  EXPECT_EQ(0, strncasecmp("", kString, 0));
+}
+
+TEST(SbStringCompareNoCaseNTest, SunnyDayBigN) {
+  const char kString[] = "0123456789";
+  EXPECT_EQ(0, strncasecmp(kString, kString, strlen(kString) * 2));
+}
+
+TEST(SbStringCompareNoCaseNTest, SunnyDayCase) {
+  const char kString1[] = "aBcDeFgHiJkLmNoPqRsTuVwXyZ";
+  const char kString2[] = "AbCdEfGhIjKlMnOpQrStUvWxYz";
+  EXPECT_EQ(0, strncasecmp(kString1, kString2, strlen(kString1)));
+  EXPECT_EQ(0, strncasecmp(kString2, kString1, strlen(kString2)));
+
+  const char kString3[] = "aBcDeFgHiJkLmaBcDeFgHiJkLm";
+  const char kString4[] = "AbCdEfGhIjKlMnOpQrStUvWxYz";
+  EXPECT_GT(0, strncasecmp(kString3, kString4, strlen(kString3)));
+  EXPECT_LT(0, strncasecmp(kString4, kString3, strlen(kString4)));
+  EXPECT_EQ(0, strncasecmp(kString3, kString4, strlen(kString3) / 2));
+  EXPECT_EQ(0, strncasecmp(kString4, kString3, strlen(kString4) / 2));
+}
+#endif  // SB_API_VERSION >= 16
+
+}  // namespace
+}  // namespace nplb
+}  // namespace starboard
---
+++ starboard/nplb/posix_compliance/posix_string_compare_no_case_test.cc
@@ -0,0 +1,43 @@
+// Copyright 2015 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/common/string.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace starboard {
+namespace nplb {
+namespace {
+
+#if SB_API_VERSION >= 16
+TEST(SbStringCompareNoCaseTest, SunnyDaySelf) {
+  const char kString[] = "0123456789";
+  EXPECT_EQ(0, strcasecmp(kString, kString));
+  EXPECT_EQ(0, strcasecmp("", ""));
+}
+
+TEST(SbStringCompareNoCaseTest, SunnyDayEmptyLessThanNotEmpty) {
+  const char kString[] = "0123456789";
+  EXPECT_GT(0, strcasecmp("", kString));
+}
+
+TEST(SbStringCompareNoCaseTest, SunnyDayCase) {
+  const char kString1[] = "aBcDeFgHiJkLmNoPqRsTuVwXyZ";
+  const char kString2[] = "AbCdEfGhIjKlMnOpQrStUvWxYz";
+  EXPECT_EQ(0, strcasecmp(kString1, kString2));
+  EXPECT_EQ(0, strcasecmp(kString2, kString1));
+}
+#endif  // SB_API_VERSION >= 16
+}  // namespace
+}  // namespace nplb
+}  // namespace starboard
