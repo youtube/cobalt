e9cde161370e5284534aa9d8834e472bd528f00d
Holden Warriner <hwarriner@google.com>
2024-01-11 19:56:51 -0500

    Disable EG tests after SbTime API deprecation (#2203)
    These tests are no longer blocking but the failures can still be
    confusing for Cobalt developers.
    
    b/302733082
    
    Change-Id: I4d546e71ffa13fff4d6fe7f9903dd492de0bedb2

--- starboard/evergreen/testing/run_all_tests.sh
+++ starboard/evergreen/testing/run_all_tests.sh
@@ -24,7 +24,8 @@ AUTH_METHOD="public-key"
 USE_COMPRESSED_SYSTEM_IMAGE="false"
 SYSTEM_IMAGE_EXTENSION=".so"
 
-DISABLE_TESTS="false"
+# TODO(b/302733082): enable the tests when new packages are up on Lorry.
+DISABLE_TESTS="true"
 
 while getopts "d:a:c" o; do
     case "${o}" in
