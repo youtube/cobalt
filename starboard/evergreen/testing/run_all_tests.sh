0c02c7aba13d0a0103b60f4b0413c64acfdb6733
Holden Warriner <hwarriner@google.com>
2024-01-12 12:43:54 -0500

    Revert "Disable EG tests after SbTime API deprecation"

--- starboard/evergreen/testing/run_all_tests.sh
+++ starboard/evergreen/testing/run_all_tests.sh
@@ -24,8 +24,7 @@ AUTH_METHOD="public-key"
 USE_COMPRESSED_SYSTEM_IMAGE="false"
 SYSTEM_IMAGE_EXTENSION=".so"
 
-# TODO(b/302733082): enable the tests when new packages are up on Lorry.
-DISABLE_TESTS="true"
+DISABLE_TESTS="false"
 
 while getopts "d:a:c" o; do
     case "${o}" in
