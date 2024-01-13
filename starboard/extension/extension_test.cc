516bc13e4b688d36a5c3b93c0ae8f9997f168c2c
Colin Liang <zhongqi.liang.4u@gmail.com>
2024-01-12 17:04:21 -0800

    Revert "Disable MediaPlaybackService by default"

--- starboard/extension/extension_test.cc
+++ starboard/extension/extension_test.cc
@@ -21,7 +21,6 @@
 #include "starboard/extension/font.h"
 #include "starboard/extension/free_space.h"
 #include "starboard/extension/graphics.h"
-#include "starboard/extension/h5vcc_config.h"
 #include "starboard/extension/ifa.h"
 #include "starboard/extension/installation_manager.h"
 #include "starboard/extension/javascript_cache.h"
@@ -475,26 +474,6 @@ TEST(ExtensionTest, Ifa) {
   EXPECT_EQ(extension_api->version, 1u);
   EXPECT_NE(extension_api->GetAdvertisingId, nullptr);
   EXPECT_NE(extension_api->GetLimitAdTracking, nullptr);
-
-  const ExtensionApi* second_extension_api =
-      static_cast<const ExtensionApi*>(SbSystemGetExtension(kExtensionName));
-  EXPECT_EQ(second_extension_api, extension_api)
-      << "Extension struct should be a singleton";
-}
-
-TEST(ExtensionTest, H5vccConfig) {
-  typedef StarboardExtensionH5vccConfigApi ExtensionApi;
-  const char* kExtensionName = kStarboardExtensionH5vccConfigName;
-
-  const ExtensionApi* extension_api =
-      static_cast<const ExtensionApi*>(SbSystemGetExtension(kExtensionName));
-  if (!extension_api) {
-    return;
-  }
-
-  EXPECT_STREQ(extension_api->name, kExtensionName);
-  EXPECT_EQ(extension_api->version, 1u);
-  EXPECT_NE(extension_api->EnableBackgroundPlayback, nullptr);
 
   const ExtensionApi* second_extension_api =
       static_cast<const ExtensionApi*>(SbSystemGetExtension(kExtensionName));
