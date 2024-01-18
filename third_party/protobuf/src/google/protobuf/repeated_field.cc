a68ae38680947753e31c310053405c069b9dae4e
Garo Bournoutian <gbournou@google.com>
2024-01-17 21:06:44 -0800

    Remove usages of no-op POEMs (protobuf) (#2228)
    - stdio_poem, string_poem, strings_poem, wchar_poem
    
    b/307941391
    b/320764165
    b/320764087

--- third_party/protobuf/src/google/protobuf/repeated_field.cc
+++ third_party/protobuf/src/google/protobuf/repeated_field.cc
@@ -32,10 +32,6 @@
 //  Based on original Protocol Buffers design by
 //  Sanjay Ghemawat, Jeff Dean, and others.
 
-#ifdef STARBOARD
-#include "starboard/client_porting/poem/string_poem.h"
-#endif
-
 #include <algorithm>
 
 #include <google/protobuf/repeated_field.h>
