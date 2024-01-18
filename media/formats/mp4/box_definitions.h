b10ed5a3d1bed6cd94924db97bda6998fa1c06b4
Austin Osagie <austinosagie@google.com>
2024-01-18 00:49:49 +0000

    Audio: Implement IAMF audio parsing
    Adds an IAMF codec definition and implements IAMF MP4 parsing for
    v1.0.0. The implementation is disabled by default.
    
    Immersive Audio Model and Formats v1.0.0
    https://aomediacodec.github.io/iamf/v1.0.0.html
    
    Bug: b/271301103
    Change-Id: I05ceec8df03335e671016224d4800c2ea122323e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5128755
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Austin Osagie <austinosagie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1248498}

--- media/formats/mp4/box_definitions.h
+++ media/formats/mp4/box_definitions.h
@@ -421,6 +421,29 @@ struct MEDIA_EXPORT EC3SpecificBox : Box {
 };
 #endif  // BUILDFLAG(ENABLE_PLATFORM_AC3_EAC3_AUDIO)
 
+#if BUILDFLAG(ENABLE_PLATFORM_AC4_AUDIO)
+struct MEDIA_EXPORT AC4SpecificBox : Box {
+  DECLARE_BOX_METHODS(AC4SpecificBox);
+  AC4 dac4;
+};
+#endif  // BUILDFLAG(ENABLE_PLATFORM_AC4_AUDIO)
+
+#if BUILDFLAG(ENABLE_PLATFORM_IAMF_AUDIO)
+struct MEDIA_EXPORT IamfSpecificBox : Box {
+  DECLARE_BOX_METHODS(IamfSpecificBox);
+  bool ReadOBU(BufferReader* reader);
+  bool ReadOBUHeader(BufferReader* reader,
+                     uint8_t* obu_type,
+                     uint32_t* obu_size);
+  bool ReadLeb128Value(BufferReader* reader, uint32_t* value) const;
+
+  uint8_t profile;
+  bool redundant_copy = false;
+
+  std::vector<uint8_t> ia_descriptors;
+};
+#endif  // BUILDFLAG(ENABLE_PLATFORM_IAMF_AUDIO)
+
 struct MEDIA_EXPORT AudioSampleEntry : Box {
   DECLARE_BOX_METHODS(AudioSampleEntry);
 
@@ -442,6 +465,12 @@ struct MEDIA_EXPORT AudioSampleEntry : Box {
   AC3SpecificBox ac3;
   EC3SpecificBox eac3;
 #endif  // BUILDFLAG(ENABLE_PLATFORM_AC3_EAC3_AUDIO)
+#if BUILDFLAG(ENABLE_PLATFORM_AC4_AUDIO)
+  AC4SpecificBox ac4;
+#endif  // BUILDFLAG(ENABLE_PLATFORM_AC4_AUDIO)
+#if BUILDFLAG(ENABLE_PLATFORM_IAMF_AUDIO)
+  IamfSpecificBox iamf;
+#endif  // BUILDFLAG(ENABLE_PLATFORM_IAMF_AUDIO)
 };
 
 struct MEDIA_EXPORT SampleDescription : Box {
