b19f4890930eaafd04ad5f5dd2d22eb9a434d66a
Madhura Jayaraman <madjay@google.com>
2024-02-06 13:54:18 -0800

    Fix build errors for EG SB < 16
    Change-Id: I9b851c63c69418a41f7f8f9f4740c5fc6de3891f

--- third_party/musl/src/starboard/sys/stat.c
+++ third_party/musl/src/starboard/sys/stat.c
@@ -14,17 +14,20 @@ int mkdir(const char *path, mode_t mode)
     return -1;
 }
 
+inline time_t WindowsUsecToTimeT(int64_t time) {
+    int64_t posix_time = time - 11644473600000000ULL;
+    posix_time = posix_time / 1000000;
+    return posix_time;
+}
+
 // SbDirectoryCanOpen, SbFileGetPathInfo, SbFileExists, SbFileCanOpen
 int stat(const char *path, struct stat *file_info)
 {
-    //SbDirectoryCanOpen calls FileGetPathInto and returns a single property
     SbFileInfo* out_info;
     if (!SbFileGetPathInfo(path, &out_info)){
         return -1;
     }
 
-    // out_info->is_symbolic_link = S_ISLNK(file_info.st_mode); ((((file_info.st_mode)) & 0170000) == (0120000))
-    // out_info->is_directory = S_ISDIR(file_info.st_mode); ((((file_info.st_mode)) & 0170000) == (0040000))
     if (out_info -> is_directory){
         file_info->st_mode = 16877;
     } else if (out_info -> is_symbolic_link){
@@ -32,21 +35,11 @@ int stat(const char *path, struct stat *file_info)
     } else {
         file_info->st_mode = 0;
     }
-    // out_info->creation_time = TimeTToWindowsUsec(file_info.st_ctime);
     file_info->st_ctime = WindowsUsecToTimeT(out_info->creation_time);
-    // out_info->last_accessed = TimeTToWindowsUsec(file_info.st_atime);
     file_info->st_atime = WindowsUsecToTimeT(out_info->last_accessed);
-    // out_info->last_modified = TimeTToWindowsUsec(file_info.st_mtime);
     file_info->st_mtime = WindowsUsecToTimeT(out_info->last_modified);
-    // out_info->size = file_info.st_size;
     file_info->st_size = out_info->size;
 
     return 0;
-}
-
-inline time_t WindowsUsecToTimeT(int64_t time) {
-    int64_t posix_time = time - 11644473600000000ULL;
-    posix_time = posix_time / 1000000;
-    return (time_t)posix_time;
 }
 #endif  // SB_API_VERSION < 16
