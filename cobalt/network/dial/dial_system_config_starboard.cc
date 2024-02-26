381d3bb3b4144b62445cc05d90a1c279acc65f63
Andrew Savage <andrewsavage@google.com>
2024-02-26 10:48:58 -0800

    Get other linux configs building (#2487)
    This should get gold/qa monolithic linux configs to build

--- cobalt/network/dial/dial_system_config_starboard.cc
+++ cobalt/network/dial/dial_system_config_starboard.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <vector>
+
 #include "base/logging.h"
 #include "cobalt/network/dial/dial_system_config.h"
 #include "starboard/common/file.h"
