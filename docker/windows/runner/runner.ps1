886ea065a5815795f320e16899c8232766a3c676
Igor Sarkisov <isarkisov@google.com>
2024-02-26 17:31:59 -0800

    Resolve merge conflicts in runner.ps1

--- docker/windows/runner/runner.ps1
+++ docker/windows/runner/runner.ps1
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-.\actions-runner\config.cmd --unattended --replace --url https://github.com/${env:RUNNER_REPO} --token $env:RUNNER_TOKEN --name $env:RUNNER_NAME --work $env:RUNNER_WORKDIR;
-.\actions-runner\run.cmd;
-=======
 C:\actions-runner\config.cmd --unattended --replace --url https://github.com/${env:RUNNER_REPO} --token $env:RUNNER_TOKEN --name $env:RUNNER_NAME --work $env:RUNNER_WORKDIR --labels $env:RUNNER_LABELS;
 C:\actions-runner\run.cmd;
->>>>>>> 7fff8289c3c ([XB1] Add XB1 container for external builds (#2296))
