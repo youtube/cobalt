16a305c14dcfeacf7756ec9c6b24c4a1fdf6a696
Madhura Jayaraman <madjay@google.com>
2024-02-27 14:45:13 -0800

    Lint error

--- starboard/shared/win32/posix_emu/stat.cc
+++ starboard/shared/win32/posix_emu/stat.cc
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <sys/stat.h>
 #include <stat.h>
+#include <sys/stat.h>
 
 extern "C" {
 
