a6b9042cbcb828b87c473313f3c85002f4473af5
Ahmed Elzeiny <ahmedelzeiny@google.com>
2024-02-27 06:00:01 +0000

    Fix cobalt Copyright headers
    Change-Id: I21d8e31328d8fd515909ab89345811ba0cbc0501

--- cobalt/js_profiler/profiler_group.h
+++ cobalt/js_profiler/profiler_group.h
@@ -1,4 +1,4 @@
-// Copyright 2023 The Cobalt Authors. All Rights Reserved.
+// Copyright 2024 The Cobalt Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 
 #ifndef COBALT_JS_PROFILER_PROFILER_GROUP_H_
 #define COBALT_JS_PROFILER_PROFILER_GROUP_H_
