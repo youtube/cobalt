bad62a32744570c3e4946c91eaa8992edea51560
haozheng <haozheng@google.com>
2024-03-19 13:13:27 -0700

    Add support for <errno.h> in Evergreen and Modular builds
    Change-Id: Id7bb43900ea7ded8e162be904b9fe9a0dee6fde6

--- starboard/nplb/posix_compliance/posix_errno_test.cc
+++ starboard/nplb/posix_compliance/posix_errno_test.cc
@@ -13,11 +13,14 @@
 // limitations under the License.
 
 #include <errno.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 #include "starboard/common/log.h"
+#include "starboard/nplb/socket_helpers.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace starboard {
@@ -30,6 +33,88 @@ TEST(PosixErrnoTest, AcceptInvalidSocket) {
   EXPECT_TRUE(errno == EBADF);
   SB_DLOG(INFO) << "Failed to accept invalid socket, errno = "
                 << strerror(errno);
+}
+
+TEST(PosixErrnoTest, BindPrivilegedPort) {
+  int socket_domain = AF_INET;
+  int socket_type = SOCK_STREAM;
+  int socket_protocol = IPPROTO_TCP;
+
+  int socket_fd = socket(socket_domain, socket_type, socket_protocol);
+  ASSERT_TRUE(socket_fd > 0);
+
+  sockaddr_in address = {};
+  address.sin_family = AF_INET;
+  address.sin_port = htons(80);  // Try to bind to a privileged port
+  address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  int bind_result =
+      bind(socket_fd, reinterpret_cast<struct sockaddr*>(&address),
+           sizeof(struct sockaddr));
+  EXPECT_TRUE(bind_result == -1);
+  EXPECT_TRUE(errno == EACCES);
+  SB_DLOG(INFO) << "Failed to bind, errno = " << strerror(errno);
+
+  EXPECT_TRUE(close(socket_fd) == 0);
+}
+
+TEST(PosixErrnoTest, ConnectAddressRefused) {
+  int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+  ASSERT_TRUE(socket_fd > 0);
+
+  sockaddr_in6 address = {};
+#if SB_HAS(IPV6)
+  EXPECT_TRUE(PosixGetLocalAddressiIPv4(
+                  reinterpret_cast<struct sockaddr*>(&address)) == 0 ||
+              PosixGetLocalAddressiIPv6(&address) == 0);
+#else
+  EXPECT_TRUE(
+      PosixGetLocalAddressiIPv4(reinterpret_cast<sockaddr*>(&address)) == 0);
+#endif
+
+  // Attempt to connect to an address where we expect connection to be refused
+  connect(socket_fd, (struct sockaddr*)&address, sizeof(address));
+
+  EXPECT_TRUE(errno == ECONNREFUSED);
+  SB_DLOG(INFO) << "Failed to connect, errno = " << strerror(errno);
+
+  close(socket_fd);
+}
+
+TEST(PosixErrnoTest, SendBeforeConnect) {
+  int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+  ASSERT_TRUE(socket_fd > 0);
+
+  char buffer[1024] = "test";
+  ssize_t result = send(socket_fd, buffer, sizeof(buffer), 0);
+
+  EXPECT_TRUE(result == -1 && errno == ENOTCONN);
+  SB_DLOG(INFO) << "Failed to send, errno = " << strerror(errno);
+
+  close(socket_fd);
+}
+
+TEST(PosixErrnoTest, ConnectToInvalidAddress) {
+  int socket_domain = AF_INET;
+  int socket_type = SOCK_STREAM;
+  int socket_protocol = IPPROTO_TCP;
+
+  int socket_fd = socket(socket_domain, socket_type, socket_protocol);
+  ASSERT_TRUE(socket_fd > 0);
+
+  sockaddr_in address = {};
+  address.sin_family = AF_INET;
+  address.sin_port = htons(12345);  // Try to connect to an invalid port
+  address.sin_addr.s_addr = (((((250 << 8) | 43) << 8) | 244) << 8) | 18;
+
+  int connect_result =
+      connect(socket_fd, reinterpret_cast<struct sockaddr*>(&address),
+              sizeof(struct sockaddr));
+  EXPECT_TRUE(connect_result == -1);
+  EXPECT_TRUE(errno == EADDRNOTAVAIL);
+  SB_DLOG(INFO) << "Failed to connect, errno = " << strerror(errno);
+
+  EXPECT_TRUE(close(socket_fd) == 0);
 }
 }  // namespace
 }  // namespace nplb
