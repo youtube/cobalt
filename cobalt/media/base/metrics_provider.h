ebafea7ffac4a63b3c03611c23905beea4d225f5
Drew Thomas <async@google.com>
2024-04-02 11:04:54 -0700

    Resolve merge conflicts
    Change-Id: I8c5dc822bd8ba19f0add35c24fd1858647b2217a

--- cobalt/media/base/metrics_provider.h
+++ cobalt/media/base/metrics_provider.h
@@ -18,26 +18,17 @@
 #include <map>
 #include <string>
 
-<<<<<<< HEAD
+#include "base/containers/small_map.h"
+#include "base/time/default_tick_clock.h"
+#include "base/time/tick_clock.h"
+#include "base/time/time.h"
 #include "starboard/common/mutex.h"
+#include "starboard/types.h"
 #include "third_party/chromium/media/base/audio_codecs.h"
 #include "third_party/chromium/media/base/container_names.h"
 #include "third_party/chromium/media/base/pipeline_status.h"
 #include "third_party/chromium/media/base/timestamp_constants.h"
 #include "third_party/chromium/media/base/video_codecs.h"
-=======
-#include "base/containers/small_map.h"
-#include "base/time/default_tick_clock.h"
-#include "base/time/tick_clock.h"
-#include "base/time/time.h"
-#include "media/base/audio_codecs.h"
-#include "media/base/container_names.h"
-#include "media/base/pipeline_status.h"
-#include "media/base/timestamp_constants.h"
-#include "media/base/video_codecs.h"
-#include "starboard/common/mutex.h"
-#include "starboard/types.h"
->>>>>>> a712047ec57 (Add Seek Latency Telemetry (#2735))
 
 namespace cobalt {
 namespace media {
