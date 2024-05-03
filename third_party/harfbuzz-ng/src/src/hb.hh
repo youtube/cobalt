d199c774aad4068e3019fdcdd58e2af9eb2d17d1
Dana Dahlstrom <dahlstrom@google.com>
2024-05-02 17:00:00 -0700

    Don't call getenv in Starboard

--- third_party/harfbuzz-ng/src/src/hb.hh
+++ third_party/harfbuzz-ng/src/src/hb.hh
@@ -212,6 +212,10 @@
 #define HB_PASTE1(a,b) a##b
 #define HB_PASTE(a,b) HB_PASTE1(a,b)
 
+#if defined(STARBOARD)
+#define getenv(x) NULL
+#endif  // defined(STARBOARD)
+
 
 /* Compile-time custom allocator support. */
 
