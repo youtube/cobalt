a3e7ea14fee222007d307817de269a9e2ce39ad4
Yavor Goulishev <yavor@google.com>
2024-05-08 23:28:45 +0000

    Fix setting thread names on ATV (#3194)
    b/339454606
    
    Change-Id: I69ea6af11ba9ade344a1250e67bef4f6b5dbce54
    (cherry picked from commit 58da1e555390f3c59b2c4b3a24bc927cddeeb4a7)

--- base/threading/platform_thread_starboard.cc
+++ base/threading/platform_thread_starboard.cc
@@ -21,6 +21,7 @@
 #include "base/logging.h"
 #include "base/threading/thread_id_name_manager.h"
 #include "base/threading/thread_restrictions.h"
+#include "starboard/configuration_constants.h"
 #include "starboard/thread.h"
 
 namespace base {
@@ -128,7 +129,9 @@ void PlatformThread::Sleep(TimeDelta duration) {
 // static
 void PlatformThread::SetName(const std::string& name) {
   ThreadIdNameManager::GetInstance()->SetName(name);
-  pthread_setname_np(pthread_self(), name.c_str());
+
+  std::string buffer(name, 0, kSbMaxThreadNameLength - 1);
+  pthread_setname_np(pthread_self(), buffer.c_str());
 }
 
 // static
