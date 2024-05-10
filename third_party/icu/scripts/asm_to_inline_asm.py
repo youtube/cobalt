8338bb99913df8ddebc4c46bf4b52edb20b550fd
Holden Warriner <hwarriner@google.com>
2024-05-10 09:31:48 -0400

    Make Python 3 fix in asm_to_inline_asm.py
    Change-Id: Ia906aab5f249c8eb9f4b99f371e2e8f3a6802840

--- third_party/icu/scripts/asm_to_inline_asm.py
+++ third_party/icu/scripts/asm_to_inline_asm.py
@@ -14,7 +14,7 @@ import argparse
 import sys
 
 def asm_to_inl_asm(in_filename, out_filename):
-  with open(in_filename, 'r') as infile, open(out_filename, 'wb') as outfile:
+  with open(in_filename, 'r') as infile, open(out_filename, 'w') as outfile:
     outfile.write('__asm__(\n')
     for line in infile:
       # Escape " in .S file before outputing it to inline asm file.
