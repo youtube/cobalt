c88ef08e4110fae9b5ee7b6653e9bd71b76d1eea
Igor Sarkisov <isarkisov@google.com>
2024-05-14 15:40:24 -0700

    Add --ephemeral option to win runner (#3239)
    This option needs to be hardcoded into runner start up mechanism as ARC
    doesn't have proper Windows support.
    
    b/258687270
    
    (cherry picked from commit ae22bddb308ced3fdedeba26fdc29874278fcff0)

--- docker/windows/runner/runner.ps1
+++ docker/windows/runner/runner.ps1
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 .\actions-runner\config.cmd --unattended --replace --url https://github.com/${env:RUNNER_REPO} --token $env:RUNNER_TOKEN --name $env:RUNNER_NAME --work $env:RUNNER_WORKDIR --labels $env:RUNNER_LABELS;
 .\actions-runner\run.cmd;
+=======
+C:\actions-runner\config.cmd --unattended --replace --url https://github.com/${env:RUNNER_REPO} --token $env:RUNNER_TOKEN --name $env:RUNNER_NAME --work $env:RUNNER_WORKDIR --ephemeral --labels $env:RUNNER_LABELS;
+C:\actions-runner\run.cmd;
+>>>>>>> ae22bddb308 (Add --ephemeral option to win runner (#3239))
