youtube/cobalt

commit 115f461093183ac1e3f83a3229136f25d0cf7529

Deprecate SB_HAS_PIPE (#2787)
Removes SB_PIPE config, in favor of using standard preprocessor macros.
The control is localized to a single implementation file, there's low
risk of this going wrong.

Test-On-Device: true

b/150410605

starboard/shared/libevent/socket_waiter_internal.cc
@@ -32,6 +32,13 @@
 
 namespace sbposix = starboard::shared::posix;
 
+#if (defined(_GNU_SOURCE) || defined(_POSIX_VERSION)) && \
+    !(PLAYSTATION_GENERATION <= 5)
+#define USE_POSIX_PIPE 1
+#else
+#define USE_POSIX_PIPE 0
+#endif
+
 namespace {
 // We do this because it's our style to use explicitly-sized ints when not just
 // using int, but libevent uses shorts explicitly in its interface.
@@ -70,7 +77,7 @@ SbSocket AcceptBySpinning(SbSocket server_socket, int64_t timeout) {
   return kSbSocketInvalid;
 }
 
-#if !SB_HAS(PIPE)
+#if !USE_POSIX_PIPE
 void GetSocketPipe(SbSocket* client_socket, SbSocket* server_socket) {
   int result;
   SbSocketError sb_socket_result;
@@ -115,7 +122,7 @@ SbSocketWaiterPrivate::SbSocketWaiterPrivate()
       base_(event_base_new()),
       waiting_(false),
       woken_up_(false) {
-#if SB_HAS(PIPE)
+#if USE_POSIX_PIPE
   int fds[2];
   int result = pipe(fds);
   SB_DCHECK(result == 0);
@@ -155,7 +162,7 @@ SbSocketWaiterPrivate::~SbSocketWaiterPrivate() {
   event_del(&wakeup_event_);
   event_base_free(base_);
 
-#if SB_HAS(PIPE)
+#if USE_POSIX_PIPE
   close(wakeup_read_fd_);
   close(wakeup_write_fd_);
 #else
@@ -387,3 +394,5 @@ SbSocketWaiterPrivate::Waitee* SbSocketWaiterPrivate::RemoveWaitee(
   waitees_.erase(it);
   return result;
 }
+
+#undef USE_POSIX_PIPE

starboard/android/shared/configuration_public.h
@@ -77,7 +77,4 @@
 // Specifies whether this platform supports IPV6.
 #define SB_HAS_IPV6 1
 
-// Specifies whether this platform supports pipe.
-#define SB_HAS_PIPE 1
-
 #endif  // STARBOARD_ANDROID_SHARED_CONFIGURATION_PUBLIC_H_

starboard/evergreen/arm/hardfp/configuration_public.h
@@ -75,7 +75,4 @@
 // Specifies whether this platform supports IPV6.
 #define SB_HAS_IPV6 1
 
-// Specifies whether this platform supports pipe.
-#define SB_HAS_PIPE 1
-
 #endif  // STARBOARD_EVERGREEN_ARM_HARDFP_CONFIGURATION_PUBLIC_H_

starboard/evergreen/arm/softfp/configuration_public.h
@@ -75,7 +75,4 @@
 // Specifies whether this platform supports IPV6.
 #define SB_HAS_IPV6 1
 
-// Specifies whether this platform supports pipe.
-#define SB_HAS_PIPE 1
-
 #endif  // STARBOARD_EVERGREEN_ARM_SOFTFP_CONFIGURATION_PUBLIC_H_

starboard/evergreen/arm64/configuration_public.h
@@ -75,7 +75,4 @@
 // Specifies whether this platform supports IPV6.
 #define SB_HAS_IPV6 1
 
-// Specifies whether this platform supports pipe.
-#define SB_HAS_PIPE 1
-
 #endif  // STARBOARD_EVERGREEN_ARM64_CONFIGURATION_PUBLIC_H_

starboard/evergreen/x64/configuration_public.h
@@ -75,7 +75,4 @@
 // Specifies whether this platform supports IPV6.
 #define SB_HAS_IPV6 1
 
-// Specifies whether this platform supports pipe.
-#define SB_HAS_PIPE 1
-
 #endif  // STARBOARD_EVERGREEN_X64_CONFIGURATION_PUBLIC_H_

starboard/evergreen/x86/configuration_public.h
@@ -70,9 +70,4 @@
 // required for platforms that want to JIT.
 #define SB_CAN_MAP_EXECUTABLE_MEMORY 1
 
-// --- Network Configuration -------------------------------------------------
-
-// Specifies whether this platform supports pipe.
-#define SB_HAS_PIPE 1
-
 #endif  // STARBOARD_EVERGREEN_X86_CONFIGURATION_PUBLIC_H_

starboard/linux/shared/configuration_public.h
@@ -77,9 +77,6 @@
 // Specifies whether this platform supports IPV6.
 #define SB_HAS_IPV6 1
 
-// Specifies whether this platform supports pipe.
-#define SB_HAS_PIPE 1
-
 // --- Media Configuration ---------------------------------------------------
 
 // The path of video_dmp_writer.h. Defined here to avoid errors building on

commit e91418dfc8d3ea6d53818d2a54f09aed9674defc

Fix USE_POSIX_PIPE condition on all platforms (#3344)
Fix the wrong condition introduced in #2787, and ensure Posixy platforms
do use Pipe implementation of socket waiter.

Test-On-Device: true

b/150410605

starboard/shared/libevent/socket_waiter_internal.cc
@@ -33,9 +33,12 @@
 
 namespace sbposix = starboard::shared::posix;
 
-#if (defined(_GNU_SOURCE) || defined(_POSIX_VERSION)) && \
-    !(PLAYSTATION_GENERATION <= 5)
+#if defined(_GNU_SOURCE) || defined(_POSIX_VERSION)
+#if defined(PLAYSTATION_GENERATION) && (PLAYSTATION_GENERATION <= 5)
+#define USE_POSIX_PIPE 0
+#else
 #define USE_POSIX_PIPE 1
+#endif
 #else
 #define USE_POSIX_PIPE 0
 #endif