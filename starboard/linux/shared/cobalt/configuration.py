4a5393992dd0ae17990d4aff128c3b90d31e70d6
Oscar Vestlie <oxv@google.com>
2024-05-28 11:01:24 -0700

    Filter flaky tests (#3341)
    Move some tests that are failing on multiple platforms to the global
    filter. Add others where appropriate.
    
    b/329269559
    b/329507754
    b/342452253
    b/342649087
    b/342454908

--- starboard/linux/shared/cobalt/configuration.py
+++ starboard/linux/shared/cobalt/configuration.py
@@ -21,21 +21,6 @@ from starboard.tools.testing import test_filter
 _FILTERED_TESTS = {
     # Tracked by b/185820828
     'net_unittests': ['SpdyNetworkTransactionTest.SpdyBasicAuth',],
-    'base_unittests': [
-        # TODO: b/329269559 These have flaky ASAN heap-use-after-free
-        # during metrics collection.
-        'All/SequenceManagerTest.DelayedTasksDontBadlyStarveNonDelayedWork_DifferentQueue/WithMessagePump',  # pylint: disable=line-too-long
-        'All/SequenceManagerTest.DelayedTasksDontBadlyStarveNonDelayedWork_DifferentQueue/WithMessagePumpAlignedWakeUps',  # pylint: disable=line-too-long
-        'All/SequenceManagerTest.DelayedTasksDontBadlyStarveNonDelayedWork_DifferentQueue/WithMockTaskRunner',  # pylint: disable=line-too-long
-        'All/SequenceManagerTest.DelayedTasksDontBadlyStarveNonDelayedWork_SameQueue/WithMessagePump',  # pylint: disable=line-too-long
-        'All/SequenceManagerTest.DelayedTasksDontBadlyStarveNonDelayedWork_SameQueue/WithMessagePumpAlignedWakeUps',  # pylint: disable=line-too-long
-        'All/SequenceManagerTest.DelayedTasksDontBadlyStarveNonDelayedWork_SameQueue/WithMockTaskRunner',  # pylint: disable=line-too-long
-        'All/SequenceManagerTest.SweepCanceledDelayedTasks_ManyTasks/WithMessagePump',  # pylint: disable=line-too-long
-        'All/SequenceManagerTest.SweepCanceledDelayedTasks_ManyTasks/WithMessagePumpAlignedWakeUps',  # pylint: disable=line-too-long
-        # TODO: b/329507754 - Flaky after recent rebase.
-        'ScopedBlockingCallIOJankMonitoringTest.MultiThreadedOverlappedWindows',
-        'TaskEnvironmentTest.MultiThreadedMockTimeAndThreadPoolQueuedMode'
-    ],
 }
 
 if os.getenv('MODULAR_BUILD', '0') == '1':
