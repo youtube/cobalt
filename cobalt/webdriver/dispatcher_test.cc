youtube/cobalt

commit 86dd36727e2256edce32de0f5195ba85463a894a

Get all targets building off of updated base/net (#2382)

cobalt/webdriver/BUILD.gn
@@ -30,7 +30,7 @@ static_library("webdriver") {
     "//cobalt/math",
     "//cobalt/script",
     "//net",
-    "//net:http_server",
+    "//net/server:http_server",
     "//starboard/common",
     "//third_party/icu:icuuc",
     "//url",

cobalt/webdriver/protocol/window_id.cc
@@ -22,11 +22,9 @@ const char kWindowNameKey[] = "name";
 }
 
 base::Optional<WindowId> WindowId::FromValue(const base::Value* value) {
-  std::string window_id;
-  const base::DictionaryValue* dictionary_value;
-  if (value->GetAsDictionary(&dictionary_value) &&
-      dictionary_value->GetString(kWindowNameKey, &window_id)) {
-    return WindowId(window_id);
+  const base::Value::Dict* dictionary_value = value->GetIfDict();
+  if (!dictionary_value) {
+    return absl::nullopt;
   }
   return base::nullopt;
 }

BUILD.gn
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if (true) {
+if (false) {
   group("default") {
     deps = [ "//net" ]
   }

base/BUILD.gn
@@ -985,6 +985,7 @@ component("base") {
       "sys_info_starboard.cc",
       "threading/platform_thread_starboard.cc",
       "threading/thread_local_storage_starboard.cc",
+      "threading/thread_task_runner_handle.cc",
       "time/time_now_starboard.cc",
       "time/time_starboard.cc",
     ]

cobalt/debug/BUILD.gn
@@ -76,7 +76,7 @@ static_library("debug") {
     "//cobalt/speech",
     "//cobalt/web",
     "//net",
-    "//net:http_server",
+    "//net/server:http_server",
     "//starboard",
     "//third_party/devtools:devtools_frontend_resources",
   ]

cobalt/h5vcc/BUILD.gn
@@ -96,8 +96,8 @@ static_library("h5vcc") {
       "dial/dial_server.h",
     ]
     deps += [
-      "//net:cobalt_dial_server",
-      "//net:http_server",
+      "//cobalt/network:cobalt_dial_server",
+      "//net/server:http_server",
     ]
   }
 

cobalt/network/BUILD.gn
@@ -18,6 +18,7 @@ static_library("network") {
   sources = [
     "cookie_jar_impl.cc",
     "cookie_jar_impl.h",
+    "disk_cache/impl.cc",
     "job_factory_config.h",
     "local_network.cc",
     "local_network.h",
@@ -63,7 +64,7 @@ static_library("network") {
   }
 
   if (enable_in_app_dial) {
-    deps += [ "//net:cobalt_dial_server" ]
+    deps += [ ":cobalt_dial_server" ]
   }
 
   # Enable network logging on all but gold builds.
@@ -246,4 +247,43 @@ target(gtest_target_type, "network_test") {
     "//testing/gtest",
     "//url",
   ]
+  if (enable_in_app_dial) {
+    sources += [
+      "dial/dial_http_server_unittest.cc",
+      "dial/dial_service_unittest.cc",
+      "dial/dial_test_helpers.h",
+      "dial/dial_udp_server_unittests.cc",
+    ]
+    deps += [
+      ":cobalt_dial_server",
+      "//net",
+      "//net:test_support",
+      "//net/server:http_server",
+    ]
+  }
+}
+
+if (enable_in_app_dial) {
+  static_library("cobalt_dial_server") {
+    sources = [
+      "dial/dial_http_server.cc",
+      "dial/dial_http_server.h",
+      "dial/dial_service.cc",
+      "dial/dial_service.h",
+      "dial/dial_service_handler.h",
+      "dial/dial_system_config.cc",
+      "dial/dial_system_config.h",
+      "dial/dial_system_config_starboard.cc",
+      "dial/dial_udp_server.cc",
+      "dial/dial_udp_server.h",
+      "dial/dial_udp_socket_factory.cc",
+      "dial/dial_udp_socket_factory.h",
+    ]
+    deps = [
+      "//base:base",
+      "//net",
+      "//net/server:http_server",
+      "//starboard/common",
+    ]
+  }
 }

cobalt/renderer/BUILD.gn
@@ -570,6 +570,7 @@ static_library("default_options") {
   sources = [ "//cobalt/renderer/get_default_rasterizer_for_platform.cc" ]
 
   deps = [
+    "//base",
     "//cobalt/configuration",
     "//cobalt/renderer/backend:renderer_backend",
     "//cobalt/renderer/rasterizer/egl:hardware_rasterizer",

commit 0317c6968344ec4ea0b992185acf1863505258ae

Webdriver: fix window ID parsing
Regression snuck with 86dd36727e2. Added a test that should work
on older branches to doublecheck.

b/343311752

cobalt/webdriver/BUILD.gn
@@ -134,6 +134,7 @@ target(gtest_target_type, "webdriver_test") {
   testonly = true
 
   sources = [
+    "dispatcher_test.cc",
     "execute_test.cc",
     "get_element_text_test.cc",
     "is_displayed_test.cc",

cobalt/webdriver/protocol/window_id.cc
@@ -26,7 +26,11 @@ base::Optional<WindowId> WindowId::FromValue(const base::Value* value) {
   if (!dictionary_value) {
     return absl::nullopt;
   }
-  return base::nullopt;
+  auto window_id = dictionary_value->FindString(kWindowNameKey);
+  if (!window_id) {
+    return absl::nullopt;
+  }
+  return WindowId(*window_id);
 }
 
 }  // namespace protocol

cobalt/webdriver/dispatcher_test.cc
@@ -1,0 +1,46 @@
+// Copyright 2024 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "cobalt/webdriver/dispatcher.h"
+
+#include <utility>
+
+#include "base/optional.h"
+#include "base/values.h"
+#include "cobalt/webdriver/protocol/window_id.h"
+#include "cobalt/webdriver/session_driver.h"
+#include "cobalt/webdriver/util/command_result.h"
+#include "cobalt/webdriver/util/dispatch_command_factory.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+
+namespace cobalt {
+namespace webdriver {
+
+TEST(FromValueTest, WindowID) {
+  base::Value::Dict dict;
+  dict.Set("name", "foo");
+  base::Value value(std::move(dict));
+  auto result = util::internal::FromValue<protocol::WindowId>(&value);
+  EXPECT_EQ(result->id(), "foo");
+}
+
+TEST(FromValueTest, WindowID_Fail) {
+  base::Value empty;
+  auto result = util::internal::FromValue<protocol::WindowId>(&empty);
+  EXPECT_EQ(result, base::nullopt);
+}
+
+}  // namespace webdriver
+}  // namespace cobalt