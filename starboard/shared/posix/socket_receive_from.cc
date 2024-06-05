5eda5a249453661653c47e3dd1e7f4407493a17e
Jelle Foks <jfoks@google.com>
2024-06-05 16:57:14 -0700

    very hacky but apparently working UDP_GRO

--- starboard/shared/posix/socket_receive_from.cc
+++ starboard/shared/posix/socket_receive_from.cc
@@ -22,6 +22,14 @@
 #include "starboard/shared/posix/handle_eintr.h"
 #include "starboard/shared/posix/socket_internal.h"
 
+#ifndef UDP_GRO
+#define UDP_GRO 104
+#endif
+
+#ifndef SOL_UDP
+#define SOL_UDP 17
+#endif
+
 namespace sbposix = starboard::shared::posix;
 
 namespace {
@@ -31,6 +39,37 @@ bool IsReportableErrno(int code) {
 }
 
 }  // namespace
+
+static int recv_msg(int fd, char* buf, int len, int* gso_size) {
+  char control[CMSG_SPACE(sizeof(int))] = {0};
+  struct msghdr msg = {0};
+  struct iovec iov = {0};
+  struct cmsghdr* cmsg;
+  int ret;
+
+  iov.iov_base = buf;
+  iov.iov_len = len;
+
+  msg.msg_iov = &iov;
+  msg.msg_iovlen = 1;
+
+  msg.msg_control = control;
+  msg.msg_controllen = sizeof(control);
+
+  *gso_size = -1;
+  ret = recvmsg(fd, &msg, MSG_TRUNC | MSG_DONTWAIT);
+  if (ret != -1) {
+    for (cmsg = CMSG_FIRSTHDR(&msg); cmsg != NULL;
+         cmsg = CMSG_NXTHDR(&msg, cmsg)) {
+      if (cmsg->cmsg_level == SOL_UDP && cmsg->cmsg_type == UDP_GRO) {
+        *gso_size = *(int*)CMSG_DATA(cmsg);
+        // SB_LOG(INFO) << __FUNCTION__ << " gso_size=" << *gso_size;
+        break;
+      }
+    }
+  }
+  return ret;
+}
 
 int SbSocketReceiveFrom(SbSocket socket,
                         char* out_data,
@@ -79,6 +118,7 @@ int SbSocketReceiveFrom(SbSocket socket,
     socket->error = sbposix::TranslateSocketErrno(errno);
     return -1;
   } else if (socket->protocol == kSbSocketProtocolUdp) {
+#if 0
     if (!out_source || (socket->message_idx < socket->message_count)) {
       // Batch read from the socket.
       // SB_LOG(INFO) << __FUNCTION__ << " // Batch read from the socket.";
@@ -111,12 +151,44 @@ int SbSocketReceiveFrom(SbSocket socket,
       socket->message_idx++;
       return static_cast<int>(bytes_read);
     }
-
+#endif
     sbposix::SockAddr sock_addr;
+#if 0
     ssize_t bytes_read = recvfrom(socket->socket_fd, out_data, data_size,
-                                  kRecvFlags | MSG_DONTWAIT,
+                                  kRecvFlags | MSG_TRUNC | MSG_DONTWAIT,
                                   sock_addr.sockaddr(), &sock_addr.length);
-
+#else
+    ssize_t bytes_read = 0;
+    if (socket->message_idx >= socket->message_count) {
+      socket->gso_size = 0;
+      bytes_read = recv_msg(socket->socket_fd, &socket->buffer[0],
+                            socket->kBufferSize - 1, &socket->gso_size);
+      if (bytes_read > 0) {
+        if (socket->gso_size > 0) {
+          socket->message_count = bytes_read;
+          socket->message_idx = 0;
+#if 0
+          SB_LOG(INFO) << __FUNCTION__ << " bytes_read=" << bytes_read
+                       << " data_size = " << data_size
+                       << " gso_size=" << socket->gso_size;
+#endif
+        } else {
+          bytes_read = std::min(static_cast<int>(data_size),
+                                static_cast<int>(bytes_read));
+          memcpy(out_data, socket->buffer, bytes_read);
+        }
+      }
+    }
+    if (socket->message_idx < socket->message_count) {
+      errno = 0;
+      unsigned int bytes_read = std::min(
+          socket->message_count - socket->message_idx, socket->gso_size);
+      memcpy(out_data, &socket->buffer[socket->message_idx],
+             std::min(static_cast<unsigned int>(data_size), bytes_read));
+      socket->message_idx += socket->gso_size;
+      return static_cast<int>(bytes_read);
+    }
+#endif
     if (bytes_read >= 0) {
       if (out_source) {
         if (!sock_addr.ToSbSocketAddress(out_source)) {
