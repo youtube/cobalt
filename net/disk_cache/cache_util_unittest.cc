b63b0f52c8fd35a2b3ac2951365727af74facc53
Andrew Savage <andrewsavage@google.com>
2024-06-06 16:19:37 +0000

    Change file utils in unittest

--- net/disk_cache/cache_util_unittest.cc
+++ net/disk_cache/cache_util_unittest.cc
@@ -35,6 +35,7 @@ class CacheUtilTest : public PlatformTest {
     dir1_ = base::FilePath(cache_dir_.Append(FILE_PATH_LITERAL("dir01")));
     file3_ = base::FilePath(dir1_.Append(FILE_PATH_LITERAL("file03")));
     ASSERT_TRUE(base::CreateDirectory(cache_dir_));
+#if defined(STARBOARD)
     int fp = -1;
     fp = open(file1_.value().c_str(), O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     ASSERT_TRUE(fp != -1);
@@ -48,6 +49,18 @@ class CacheUtilTest : public PlatformTest {
     fp = open(file3_.value().c_str(), O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     ASSERT_TRUE(fp != -1);
     ASSERT_TRUE(close(fp) == 0);
+#else
+    FILE *fp = base::OpenFile(file1_, "w");
+    ASSERT_TRUE(fp != nullptr);
+    base::CloseFile(fp);
+    fp = base::OpenFile(file2_, "w");
+    ASSERT_TRUE(fp != nullptr);
+    base::CloseFile(fp);
+    ASSERT_TRUE(base::CreateDirectory(dir1_));
+    fp = base::OpenFile(file3_, "w");
+    ASSERT_TRUE(fp != nullptr);
+    base::CloseFile(fp);
+#endif
     dest_dir_ = tmp_dir_.GetPath().Append(FILE_PATH_LITERAL("old_Cache_001"));
     dest_file1_ = base::FilePath(dest_dir_.Append(FILE_PATH_LITERAL("file01")));
     dest_file2_ =
