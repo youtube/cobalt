ed5137305a3a196db2539371fa1ed38aded9291a
Kaido Kert <kaidokert@google.com>
2024-06-07 11:39:42 -0700

    Try to force texture filtering

--- cobalt/renderer/rasterizer/egl/graphics_state.cc
+++ cobalt/renderer/rasterizer/egl/graphics_state.cc
@@ -253,6 +253,8 @@ void GraphicsState::ActiveBindTexture(GLenum texture_unit, GLenum target,
 
   GL_CALL(glTexParameteri(target, GL_TEXTURE_WRAP_S, texture_wrap_mode));
   GL_CALL(glTexParameteri(target, GL_TEXTURE_WRAP_T, texture_wrap_mode));
+  GL_CALL(glTexParameteri(target, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+  GL_CALL(glTexParameteri(target, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
 }
 
 void GraphicsState::SetClipAdjustment() {
