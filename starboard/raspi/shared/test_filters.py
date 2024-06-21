2cbec9125dbbc38a2d1c9080083057296341eea1
Hao <131711973+haozheng-cobalt@users.noreply.github.com>
2024-06-21 15:02:33 -0700

    disable FileTest.WriteDataToLargeOffset for raspi (#3624)
    b/348648386

--- starboard/raspi/shared/test_filters.py
+++ starboard/raspi/shared/test_filters.py
@@ -18,6 +18,8 @@ from starboard.tools.testing import test_filter
 # pylint: disable=line-too-long
 
 _FILTERED_TESTS = {
+    # TODO: this test is only failing for raspi after SBFile deprecation.
+    'base_unittests': ['FileTest.WriteDataToLargeOffset'],
     'nplb': [
         'SbAudioSinkTest.*',
 
