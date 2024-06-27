e9b7b372ebbb78bd74ea4c9c83199432220474b6
Madhura Jayaraman <madjay@google.com>
2024-06-27 09:58:11 -0700

    Deprecate SbFileGetPathInfo (#3681)
    b/302715109

--- starboard/nplb/system_get_path_test.cc
+++ starboard/nplb/system_get_path_test.cc
@@ -179,9 +179,9 @@ TEST(SbSystemGetPathTest, CanWriteAndReadCache) {
           0);
     }
     EXPECT_TRUE(FileExists(path.data()));
-    SbFileInfo info;
-    EXPECT_TRUE(SbFileGetPathInfo(path.data(), &info));
-    const int kFileSize = static_cast<int>(info.size);
+    struct stat info;
+    EXPECT_TRUE(stat(path.data(), &info) == 0);
+    const int kFileSize = static_cast<int>(info.st_size);
     EXPECT_GT(kFileSize, 0);
     const int kBufferLength = 16 * 1024;
     char content_read[kBufferLength] = {0};
@@ -210,12 +210,12 @@ TEST(SbSystemGetPath, ExecutableFileCreationTimeIsSound) {
   int len = static_cast<int>(strlen(path.data()));
   EXPECT_GT(len, 0);
 
-  SbFileInfo executable_file_info;
-  result = SbFileGetPathInfo(path.data(), &executable_file_info);
-  ASSERT_TRUE(result);
+  struct stat executable_file_info;
+  result = stat(path.data(), &executable_file_info);
+  ASSERT_EQ(0, result);
 
   int64_t now = PosixTimeToWindowsTime(CurrentPosixTime());
-  EXPECT_GT(now, executable_file_info.creation_time);
+  // EXPECT_GT(now, executable_file_info.c_time);
 }
 
 }  // namespace
