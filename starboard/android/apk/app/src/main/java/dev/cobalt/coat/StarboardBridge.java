06dcbcbfe4c8e5786db481ebcc310c3b9d15d5fc
Kaido Kert <kaidokert@google.com>
2024-07-08 18:00:32 -0700

    Remove Android user authorizer
    b/352168728

--- starboard/android/apk/app/src/main/java/dev/cobalt/coat/StarboardBridge.java
+++ starboard/android/apk/app/src/main/java/dev/cobalt/coat/StarboardBridge.java
@@ -39,7 +39,6 @@ import android.view.InputDevice;
 import android.view.accessibility.AccessibilityManager;
 import android.view.accessibility.CaptioningManager;
 import androidx.annotation.Nullable;
-import dev.cobalt.account.UserAuthorizer;
 import dev.cobalt.media.ArtworkDownloader;
 import dev.cobalt.media.AudioOutputManager;
 import dev.cobalt.media.CaptionSettings;
@@ -71,7 +70,6 @@ public class StarboardBridge {
 
   private CobaltSystemConfigChangeReceiver sysConfigChangeReceiver;
   private CobaltTextToSpeechHelper ttsHelper;
-  private UserAuthorizer userAuthorizer;
   private AudioOutputManager audioOutputManager;
   private CobaltMediaSession cobaltMediaSession;
   private AudioPermissionRequester audioPermissionRequester;
@@ -122,7 +120,6 @@ public class StarboardBridge {
       Context appContext,
       Holder<Activity> activityHolder,
       Holder<Service> serviceHolder,
-      UserAuthorizer userAuthorizer,
       ArtworkDownloader artworkDownloader,
       String[] args,
       String startDeepLink) {
@@ -138,7 +135,6 @@ public class StarboardBridge {
     this.startDeepLink = startDeepLink;
     this.sysConfigChangeReceiver = new CobaltSystemConfigChangeReceiver(appContext, stopRequester);
     this.ttsHelper = new CobaltTextToSpeechHelper(appContext);
-    this.userAuthorizer = userAuthorizer;
     this.audioOutputManager = new AudioOutputManager(appContext);
     this.cobaltMediaSession =
         new CobaltMediaSession(appContext, activityHolder, audioOutputManager, artworkDownloader);
@@ -223,7 +219,6 @@ public class StarboardBridge {
   protected void afterStopped() {
     starboardApplicationStopped = true;
     ttsHelper.shutdown();
-    userAuthorizer.shutdown();
     for (CobaltService service : cobaltServices.values()) {
       service.afterStopped();
     }
@@ -581,13 +576,6 @@ public class StarboardBridge {
     }
   }
 
-  /** Returns Java layer implementation for AndroidUserAuthorizer */
-  @SuppressWarnings("unused")
-  @UsedByNative
-  public UserAuthorizer getUserAuthorizer() {
-    return userAuthorizer;
-  }
-
   @SuppressWarnings("unused")
   @UsedByNative
   void updateMediaSession(
@@ -665,12 +653,9 @@ public class StarboardBridge {
     return audioPermissionRequester;
   }
 
-  void onActivityResult(int requestCode, int resultCode, Intent data) {
-    userAuthorizer.onActivityResult(requestCode, resultCode, data);
-  }
+  void onActivityResult(int requestCode, int resultCode, Intent data) {}
 
   void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-    userAuthorizer.onRequestPermissionsResult(requestCode, permissions, grantResults);
     audioPermissionRequester.onRequestPermissionsResult(requestCode, permissions, grantResults);
   }
 
