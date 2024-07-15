a4199abfe321cbb49acbc9a2856038fd3bb31d42
Colin Liang <zhongqi.liang.4u@gmail.com>
2024-07-15 09:40:52 -0700

    Update StarboardBridge.java
    add back import android.content.Intent; because MediaPlaybackService is not removed in Cobalt 24.

--- starboard/android/apk/app/src/main/java/dev/cobalt/coat/StarboardBridge.java
+++ starboard/android/apk/app/src/main/java/dev/cobalt/coat/StarboardBridge.java
@@ -22,6 +22,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.input.InputManager;
 import android.media.AudioDeviceInfo;
