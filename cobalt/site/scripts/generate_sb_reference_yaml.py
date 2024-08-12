17f4fea9983fc6de0884fd1955c0f4be53c94c35
Oscar Vestlie <oxv@google.com>
2024-08-12 10:14:02 +0000

    Handle all sbversions equally, add function doc strings

--- cobalt/site/scripts/generate_sb_reference_yaml.py
+++ cobalt/site/scripts/generate_sb_reference_yaml.py
@@ -12,7 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Generate the part of the _book.yaml TOC that lists Starboard modules."""
+"""
+Generate the part of the _book.yaml Table of Contents for Starboard modules.
+"""
 
 import os
 import sys
@@ -24,48 +26,54 @@ _DEVSITE_ROOT = os.path.relpath(os.path.join(_REPO_ROOT, "cobalt", "site"))
 
 
 def _get_modules(path: str) -> typing.List[str]:
-  return sorted([
+  """
+  Get a list of all generated Starboard module markdown files in the specified
+  folder.
+  """
+  sb_modules = [
       os.path.join(path, name)
       for name in os.listdir(path)
       if os.path.isfile(os.path.join(path, name)) and name[-3:] == ".md"
-  ])
+  ]
+  return sorted(sb_modules)
 
 
-def _get_old_sb_versions(path: str) -> typing.List[str]:
-  return sorted([
-      os.path.join(path, name)
-      for name in os.listdir(path)
-      if os.path.isdir(os.path.join(path, name))
-  ])[:-1]  # Skip the latest version.
+def _get_sb_versions(module_reference_path: str) -> typing.List[str]:
+  """
+  Get the Starboard folders in the module reference path.
+  """
+  sb_versions = [
+      os.path.join(module_reference_path, name)
+      for name in os.listdir(module_reference_path)
+      if os.path.isdir(os.path.join(module_reference_path, name))
+  ]
+  return sorted(sb_versions)
 
 
-def _print_header(title: str, indent: int):
-  print("  " * indent + f"  - title: {title}")
-  print("  " * indent + "    section:")
+def _print_header(title: str):
+  print(f"  - title: {title}")
+  print("    section:")
 
 
-def _print_modules(modules: typing.List[str], indent: int):
+def _print_modules(modules: typing.List[str]):
   for module_path in modules:
     rel_path = os.path.relpath(module_path, _DEVSITE_ROOT)
-    print("  " * indent + f"    - title: {pathlib.Path(module_path).stem}.h")
-    print("  " * indent + f"      path: /youtube/cobalt/{rel_path}")
+    print(f"    - title: {pathlib.Path(module_path).stem}.h")
+    print(f"      path: /youtube/cobalt/{rel_path}")
 
 
 def generate_toc() -> None:
+  """Generate the Starboard module Table of Contents."""
   if len(sys.argv) > 2:
     print(f"Usage: {sys.argv[0]} <path>")
     sys.exit(1)
 
   path = sys.argv[1]
 
-  _print_header("Starboard Modules", 0)
-  # Modules in current Starboard version.
-  _print_modules(_get_modules(path), 0)
-
-  # Modules in old SB versions. Should be indented one step.
-  for sb_version_path in _get_old_sb_versions(path):
-    _print_header(f"Starboard {os.path.basename(sb_version_path)}", 1)
-    _print_modules(_get_modules(sb_version_path), 1)
+  # Print a header and link to individual module reference docs.
+  for sb_version_path in _get_sb_versions(path):
+    _print_header(f"Starboard {os.path.basename(sb_version_path)}")
+    _print_modules(_get_modules(sb_version_path))
 
 
 if __name__ == "__main__":
