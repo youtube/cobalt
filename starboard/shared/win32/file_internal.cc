471c22fa56f46f52e267f7e1a09431ed4b1ffdb6
Yijia Zhang <45114178+yjzhang111@users.noreply.github.com>
2024-08-23 14:23:28 -0700

    Remove SbFile, SbFileError, SbFileInfo references. (#4012)
    b/302715109
    
    (cherry picked from commit 20408dd3799f29d5b85ea88299b78e9526c75fa2)
    Change-Id: I13cdb4148f3751c6d531c98766c5b37cffab80c4

--- starboard/shared/win32/file_internal.cc
+++ starboard/shared/win32/file_internal.cc
@@ -25,7 +25,7 @@
 #include "starboard/shared/win32/error_utils.h"
 #include "starboard/shared/win32/wchar_utils.h"
 
-#define O_ACCMODE O_RDWR | O_WRONLY | O_RDONLY
+#define O_ACCMODE (O_RDONLY | O_WRONLY | O_RDWR)
 
 namespace sbwin32 = starboard::shared::win32;
 
@@ -110,6 +110,10 @@ HANDLE OpenFileOrDirectory(const char* path, int flags) {
   }
 
   DWORD creation_disposition = 0;
+  if (!flags) {
+    creation_disposition = OPEN_EXISTING;
+  }
+
   if (flags & O_CREAT && flags & O_EXCL) {
     SB_DCHECK(!creation_disposition);
     creation_disposition = CREATE_NEW;
