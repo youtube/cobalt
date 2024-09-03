4d181b39517537d6d76ca04c6eb3e853cac84290
Niranjan Yardi <nyardi@google.com>
2024-09-03 16:35:16 -0700

    Add test to check compiler version (#4077)
    b/356067446
    
    Add check to compile nplb with clang17 or higher

---
+++ starboard/nplb/compiler_compliance/cpp_compiler_version_check.cc
@@ -0,0 +1,28 @@
+// Copyright 2024 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the aLicense is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <iostream>
+
+#ifdef __clang__
+// Check Clang major version required for building Nplb tests.
+// Clang major version can be deduced from "clang_revision" in
+// starboard/build/config/clang.gni
+static_assert(
+    __clang_major__ >= 17,
+    "We compile above Starboard targets like Cobalt, Nplb  with "
+    "clang17 or higher. If you are building with a non-Cobalt "
+    "provided toolchain you may run into this error. To fix this error"
+    "build libnplb using the Evergreen toolchain : "
+    "cobalt.dev/development/setup-raspi");
+#endif  // __clang__
