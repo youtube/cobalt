a16cd867b7cd1919553a15b7c71416167f6b3094
Austin Osagie <austinosagie@google.com>
2024-10-01 15:58:56 -0700

    [android] Add garbage collection threshold RRO

---
+++ starboard/android/shared/media_get_buffer_garbage_collection_duration_threshold.cc
@@ -0,0 +1,27 @@
+// Copyright 2024 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/media.h"
+
+#include "starboard/android/shared/application_android.h"
+
+int64_t SbMediaGetBufferGarbageCollectionDurationThreshold() {
+  const int64_t overlayed_threshold =
+      starboard::android::shared::ApplicationAndroid::Get()
+          ->GetOverlayedIntValue("garbage_collection_duration_threshold");
+  if (overlayed_threshold != 0) {
+    return overlayed_threshold;
+  }
+  return 170'000'000;  // 170 seconds
+}
