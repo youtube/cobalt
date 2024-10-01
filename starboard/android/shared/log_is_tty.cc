b2cb949b8c0e37db53eea26f6cc32e55bbcfe3f3
Holden Warriner <hwarriner@google.com>
2024-10-01 10:22:51 -0400

    Add SB version guards
    Change-Id: I9d07c85e7fea622068680e1e0b212093f966a7e0

--- starboard/android/shared/log_is_tty.cc
+++ starboard/android/shared/log_is_tty.cc
@@ -12,8 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#if SB_API_VERSION < 16
 #include "starboard/log.h"
 
 bool SbLogIsTty() {
   return false;
 }
+#endif  // SB_API_VERSION < 16
