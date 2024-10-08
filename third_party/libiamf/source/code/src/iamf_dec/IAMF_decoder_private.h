9591503a2ca7223fd7590bd6fe18b39c0966a548
Austin Osagie <austinosagie@google.com>
2024-10-08 16:21:06 -0700

    Import libiamf to third_party/ (#4231)
    Import libiamf to third_party/
    
    b/341792042

---
+++ third_party/libiamf/source/code/src/iamf_dec/IAMF_decoder_private.h
@@ -0,0 +1,345 @@
+/*
+ * Copyright (c) 2024, Alliance for Open Media. All rights reserved
+ *
+ * This source code is subject to the terms of the BSD 3-Clause Clear License
+ * and the Alliance for Open Media Patent License 1.0. If the BSD 3-Clause Clear
+ * License was not distributed with this source code in the LICENSE file, you
+ * can obtain it at www.aomedia.org/license/software-license/bsd-3-c-c. If the
+ * Alliance for Open Media Patent License 1.0 was not distributed with this
+ * source code in the PATENTS file, you can obtain it at
+ * www.aomedia.org/license/patent.
+ */
+
+/**
+ * @file IAMF_decoder_private.h
+ * @brief IAMF decoder internal APIs.
+ * @version 0.1
+ * @date Created 03/03/2023
+ **/
+
+#ifndef IAMF_DECODER_PRIVATE_H
+#define IAMF_DECODER_PRIVATE_H
+
+#include <stdint.h>
+
+#include "IAMF_OBU.h"
+#include "IAMF_core_decoder.h"
+#include "IAMF_decoder.h"
+#include "IAMF_defines.h"
+#include "IAMF_types.h"
+#include "ae_rdr.h"
+#include "audio_effect_peak_limiter.h"
+#include "demixer.h"
+#include "downmix_renderer.h"
+#include "queue_t.h"
+#include "speex_resampler.h"
+
+#define IAMF_FLAG_MAGIC_CODE 0x01
+#define IAMF_FLAG_CODEC_CONFIG 0x02
+#define IAMF_FLAG_AUDIO_ELEMENT 0x04
+#define IAMF_FLAG_MIX_PRESENTATION 0x08
+#define IAMF_FLAG_CONFIG 0x10
+#define IAMF_FLAG_FRAME_START 0x20
+#define IAMF_FLAG_DESCRIPTORS                                                \
+  (IAMF_FLAG_MAGIC_CODE | IAMF_FLAG_CODEC_CONFIG | IAMF_FLAG_AUDIO_ELEMENT | \
+   IAMF_FLAG_MIX_PRESENTATION)
+
+#define DEC_BUF_CNT 3
+
+typedef enum {
+  IA_CH_GAIN_RTF,
+  IA_CH_GAIN_LTF,
+  IA_CH_GAIN_RS,
+  IA_CH_GAIN_LS,
+  IA_CH_GAIN_R,
+  IA_CH_GAIN_L,
+  IA_CH_GAIN_COUNT
+} IAOutputGainChannel;
+
+typedef enum {
+  IAMF_DECODER_STATUS_UNINIT,
+  IAMF_DECODER_STATUS_INIT,
+  IAMF_DECODER_STATUS_CONFIGURE,
+  IAMF_DECODER_STATUS_RECONFIGURE,
+  IAMF_DECODER_STATUS_RECEIVE,
+  IAMF_DECODER_STATUS_RUN,
+} IAMF_DecoderStatus;
+
+/* >>>>>>>>>>>>>>>>>> DATABASE >>>>>>>>>>>>>>>>>> */
+
+typedef void (*IAMF_Free)(void *);
+
+typedef struct ObjectSet {
+  void **items;
+  int count;
+  int capacity;
+  IAMF_Free objFree;
+} ObjectSet;
+
+typedef struct MixGainUnit {
+  int count;
+  float constant_gain;
+  float *gains;
+} MixGainUnit;
+
+typedef struct MixGain {
+  float default_mix_gain;
+  int use_default;
+} MixGain;
+
+typedef struct ParameterValue {
+  queue_t *params;
+  union {
+    MixGain mix_gain;
+  };
+} ParameterValue;
+
+typedef struct ParameterItem {
+  uint64_t id;
+  uint64_t type;
+  uint64_t timestamp;
+  uint64_t duration;
+  uint64_t elapse;
+  uint64_t parent_id;
+  int rate;
+
+  ParameterBase *param_base;
+
+  ParameterValue value;
+} ParameterItem;
+
+typedef struct ElementItem {
+  uint64_t id;
+
+  IAMF_CodecConf *codecConf;
+  IAMF_Element *element;
+
+  ParameterItem *demixing;
+  ParameterItem *reconGain;
+  ParameterItem *mixGain;
+
+} ElementItem;
+
+typedef void (*free_tp)(void *);
+
+typedef struct Viewer {
+  void **items;
+  int count;
+  free_tp freeF;
+} Viewer;
+
+typedef struct IAMF_DataBase {
+  IAMF_Object *version;
+
+  ObjectSet *codecConf;
+  ObjectSet *element;
+  ObjectSet *mixPresentation;
+
+  Viewer eViewer;
+  Viewer pViewer;
+
+  IAMF_Profile profile;
+} IAMF_DataBase;
+
+/* <<<<<<<<<<<<<<<<<< DATABASE <<<<<<<<<<<<<<<<<< */
+
+typedef struct LayoutInfo {
+  IAMF_SP_LAYOUT sp;
+  IAMF_Layout layout;
+  int channels;
+} LayoutInfo;
+
+typedef struct IAMF_OutputGain {
+  uint32_t flags;
+  float gain;
+} IAMF_OutputGain;
+
+typedef struct IAMF_ReconGain {
+  uint16_t flags;
+  uint16_t nb_channels;
+  float recon_gain[IA_CH_RE_COUNT];
+  IAChannel order[IA_CH_RE_COUNT];
+} IAMF_ReconGain;
+
+typedef struct SubLayerConf {
+  uint8_t layout;
+  uint8_t nb_channels;
+  uint8_t nb_substreams;
+  uint8_t nb_coupled_substreams;
+  float loudness;
+  IAMF_OutputGain *output_gain;
+  IAMF_ReconGain *recon_gain;
+} SubLayerConf;
+
+typedef struct ChannelLayerContext {
+  int nb_layers;
+  SubLayerConf *conf_s;
+
+  int layer;
+  int layout;
+  int channels;
+  IAChannel channels_order[IA_CH_LAYOUT_MAX_CHANNELS];
+
+  int dmx_mode;
+  int dmx_default_mode;
+  int dmx_default_w_idx;
+} ChannelLayerContext;
+
+typedef struct AmbisonicsContext {
+  int mode;
+  uint8_t *mapping;
+  int mapping_size;
+} AmbisonicsContext;
+
+typedef struct IAMF_Stream {
+  uint64_t element_id;
+  uint64_t codecConf_id;
+  IAMF_CodecID codec_id;
+
+  int sampling_rate;
+  uint8_t scheme;  // audio element type: 0, CHANNEL_BASED; 1, SCENE_BASED
+
+  int nb_channels;
+  int nb_substreams;
+  int nb_coupled_substreams;
+
+  LayoutInfo *final_layout;
+  void *priv;
+
+  uint64_t timestamp;  // sync time
+
+  uint64_t trimming_start;
+  uint64_t trimming_end;
+
+  uint32_t max_frame_size;
+
+  uint8_t headphones_rendering_mode;
+} IAMF_Stream;
+
+typedef struct ScalableChannelDecoder {
+  int nb_layers;
+  IAMF_CoreDecoder **sub_decoders;
+  Demixer *demixer;
+} ScalableChannelDecoder;
+
+typedef struct AmbisonicsDecoder {
+  IAMF_CoreDecoder *decoder;
+} AmbisonicsDecoder;
+
+#define IAMF_FRAME_EXTRA_DEMIX_MODE 0x01
+#define IAMF_FRAME_EXTRA_RECON_GAIN 0x02
+#define IAMF_FRAME_EXTRA_MIX_GAIN 0x04
+
+typedef struct Packet {
+  uint8_t **sub_packets;
+  uint32_t *sub_packet_sizes;
+  uint32_t nb_sub_packets;
+  uint32_t count;
+} Packet;
+
+typedef struct Frame {
+  uint64_t id;
+  uint64_t pts;
+  uint32_t samples;
+  uint64_t strim;
+  uint64_t etrim;
+  int channels;
+
+  float *data;
+} Frame;
+
+typedef struct IAMF_StreamDecoder {
+  union {
+    ScalableChannelDecoder *scale;
+    AmbisonicsDecoder *ambisonics;
+  };
+
+  IAMF_Stream *stream;
+  float *buffers[DEC_BUF_CNT];
+
+  Packet packet;
+  Frame frame;
+
+  uint32_t frame_size;
+  int frame_padding;
+  int delay;
+
+} IAMF_StreamDecoder;
+
+typedef struct IAMF_StreamRenderer {
+  IAMF_Stream *stream;
+  DMRenderer *downmixer;
+  uint32_t offset;
+  uint32_t frame_size;
+  uint8_t headphones_rendering_mode;
+  struct {
+    IAMF_SP_LAYOUT *layout;
+    union {
+      struct m2m_rdr_t mr;
+      struct h2m_rdr_t hr;
+    };
+  } renderer;
+} IAMF_StreamRenderer;
+
+typedef struct IAMF_Mixer {
+  uint64_t *element_ids;
+  int nb_elements;
+  Frame **frames;
+} IAMF_Mixer;
+
+typedef struct IAMF_Resampler {
+  float *buffer;
+  uint32_t rest_flag;
+  SpeexResamplerState *speex_resampler;
+} IAMF_Resampler;
+
+typedef struct IAMF_Presentation {
+  IAMF_MixPresentation *obj;
+
+  IAMF_Stream **streams;
+  uint32_t nb_streams;
+  IAMF_StreamDecoder **decoders;
+  IAMF_StreamRenderer **renderers;
+  IAMF_Resampler *resampler;
+  IAMF_Mixer mixer;
+  uint64_t output_gain_id;
+  Frame frame;
+} IAMF_Presentation;
+
+typedef struct IAMF_DecoderContext {
+  IAMF_DataBase db;
+  uint32_t flags;
+  IAMF_DecoderStatus status;
+
+  IAMF_Layout layout;
+  LayoutInfo *output_layout;
+  int sampling_rate;
+
+  uint64_t mix_presentation_id;
+  IAMF_Presentation *presentation;
+
+  float loudness;
+  float normalization_loudness;
+  uint32_t bit_depth;
+  float threshold_db;
+  IAMF_StreamInfo info;
+
+  uint32_t need_configure;
+
+  // PTS
+  uint64_t duration;
+  int64_t pts;
+  uint32_t pts_time_base;
+  uint32_t last_frame_size;
+  uint32_t time_precision;
+
+  IAMF_extradata metadata;
+
+} IAMF_DecoderContext;
+
+struct IAMF_Decoder {
+  IAMF_DecoderContext ctx;
+  AudioEffectPeakLimiter *limiter;
+};
+
+#endif /* IAMF_DECODER_PRIVATE_H */
---
+++ third_party/libiamf/source/code/src/iamf_dec/aac/aac_multistream_decoder.c
@@ -0,0 +1,254 @@
+/*
+ * Copyright (c) 2024, Alliance for Open Media. All rights reserved
+ *
+ * This source code is subject to the terms of the BSD 3-Clause Clear License
+ * and the Alliance for Open Media Patent License 1.0. If the BSD 3-Clause Clear
+ * License was not distributed with this source code in the LICENSE file, you
+ * can obtain it at www.aomedia.org/license/software-license/bsd-3-c-c. If the
+ * Alliance for Open Media Patent License 1.0 was not distributed with this
+ * source code in the PATENTS file, you can obtain it at
+ * www.aomedia.org/license/patent.
+ */
+
+/**
+ * @file aac_multistream_decoder.c
+ * @brief AAC decoder.
+ * @version 0.1
+ * @date Created 03/03/2023
+ **/
+
+#include "aac_multistream_decoder.h"
+
+#include "fdk-aac/aacdecoder_lib.h"
+#include <stdlib.h>
+#include <string.h>
+
+#include "IAMF_debug.h"
+#include "IAMF_defines.h"
+#include "IAMF_types.h"
+#include "IAMF_utils.h"
+
+#ifdef IA_TAG
+#undef IA_TAG
+#endif
+
+#define IA_TAG "AACMS"
+
+#define MAX_BUFFER_SIZE MAX_AAC_FRAME_SIZE * 2
+
+struct AACMSDecoder {
+  uint32_t flags;
+  int streams;
+  int coupled_streams;
+  int delay;
+
+  HANDLE_AACDECODER *handles;
+  INT_PCM buffer[MAX_BUFFER_SIZE];
+};
+
+typedef void (*aac_copy_channel_out_func)(void *dst, const void *src,
+                                          int frame_size, int channes);
+
+void aac_copy_channel_out_short_plane(void *dst, const void *src,
+                                      int frame_size, int channels) {
+  if (channels == 1) {
+    memcpy(dst, src, sizeof(INT_PCM) * frame_size);
+  } else if (channels == 2) {
+    INT_PCM *in, *out;
+    in = (INT_PCM *)src;
+    out = (INT_PCM *)dst;
+    for (int s = 0; s < frame_size; ++s) {
+      out[s] = in[channels * s];
+      out[s + frame_size] = in[channels * s + 1];
+    }
+  }
+}
+
+static int aac_config_set_channels(uint8_t *conf, uint32_t size, int channels) {
+  int nbytes = 0;
+  uint8_t byte = 0;
+
+  // audioObjectType = 2 (5bits)
+  // samplingFrequencyIndex (4 or 28 bits)
+  // channelConfiguration (4 bits)
+  byte = conf[nbytes] & 0x7;
+  byte = byte << 1 | conf[++nbytes] >> 7;
+  if (byte == 0xf) nbytes += 3;
+  byte = ~(0xf << 3) & conf[nbytes];
+  byte = (channels & 0xf) << 3 | byte;
+  ia_logd("%d byte 1 bit: 0x%x->0x%x", nbytes, conf[nbytes], byte);
+  if (nbytes < size)
+    conf[nbytes] = byte;
+  else
+    return IAMF_ERR_BAD_ARG;
+
+  return IAMF_OK;
+}
+
+static int aac_multistream_decode_native(
+    AACMSDecoder *st, uint8_t *buffer[], uint32_t size[], void *pcm,
+    int frame_size, aac_copy_channel_out_func copy_channel_out) {
+  UINT valid;
+  AAC_DECODER_ERROR err;
+  INT_PCM *out = (INT_PCM *)pcm;
+  int fs = 0;
+  UINT flags = 0;
+  CStreamInfo *info = 0;
+
+  for (int i = 0; i < st->streams; ++i) {
+    ia_logt("stream %d", i);
+    valid = size[i];
+    flags = buffer[i] ? 0 : AACDEC_FLUSH;
+    if (!flags) {
+      err = aacDecoder_Fill(st->handles[i], &buffer[i], &size[i], &valid);
+      if (err != AAC_DEC_OK) {
+        return IAMF_ERR_INVALID_PACKET;
+      }
+    }
+    err = aacDecoder_DecodeFrame(st->handles[i], st->buffer, MAX_BUFFER_SIZE,
+                                 flags);
+    if (err == AAC_DEC_NOT_ENOUGH_BITS) {
+      return IAMF_ERR_BUFFER_TOO_SMALL;
+    }
+    if (err != AAC_DEC_OK) {
+      ia_loge("stream %d : fail to decode", i);
+      return IAMF_ERR_INTERNAL;
+    }
+
+    info = aacDecoder_GetStreamInfo(st->handles[i]);
+    if (info) {
+      fs = info->frameSize;
+      if (fs > MAX_AAC_FRAME_SIZE) {
+        ia_logw("frame size %d , is larger than %u", fs, MAX_AAC_FRAME_SIZE);
+        fs = MAX_AAC_FRAME_SIZE;
+      }
+
+      ia_logt("aac decoder %d:", i);
+      ia_logt(" > sampleRate : %d.", info->sampleRate);
+      ia_logt(" > frameSize  : %d.", info->frameSize);
+      ia_logt(" > numChannels: %d.", info->numChannels);
+      ia_logt(" > outputDelay: %u.", info->outputDelay);
+
+    } else {
+      fs = frame_size;
+    }
+
+    if (info) {
+      (*copy_channel_out)(out, st->buffer, fs, info->numChannels);
+      out += (fs * info->numChannels);
+    } else {
+      ia_logw("Can not get stream info.");
+    }
+  }
+
+  if (info && st->delay < 0) st->delay = info->outputDelay;
+
+  return fs;
+}
+
+AACMSDecoder *aac_multistream_decoder_open(uint8_t *config, uint32_t size,
+                                           int streams, int coupled_streams,
+                                           uint32_t flags, int *error) {
+  AACMSDecoder *st = 0;
+  HANDLE_AACDECODER *handles = 0;
+  HANDLE_AACDECODER handle;
+
+  UCHAR *conf[2] = {0, 0};
+  UINT clen;
+
+  int i, ret = 0;
+
+  if (!config || !size || streams < 1) {
+    ret = IAMF_ERR_BAD_ARG;
+    goto end;
+  }
+
+  conf[0] = config;
+  clen = size;
+  conf[1] = IAMF_MALLOC(UCHAR, clen);
+  if (!conf[1]) {
+    ret = IAMF_ERR_ALLOC_FAIL;
+    goto end;
+  }
+
+  st = IAMF_MALLOCZ(AACMSDecoder, 1);
+  handles = IAMF_MALLOCZ(HANDLE_AACDECODER, streams);
+
+  if (!st || !handles) {
+    ret = IAMF_ERR_ALLOC_FAIL;
+    IAMF_FREE(handles);
+    goto end;
+  }
+
+  st->flags = flags;
+  st->streams = streams;
+  st->coupled_streams = coupled_streams;
+  st->delay = -1;
+  st->handles = handles;
+  memcpy(conf[1], conf[0], clen);
+  aac_config_set_channels(conf[1], clen, 1);
+
+  for (i = 0; i < st->streams; ++i) {
+    handle = aacDecoder_Open(TT_MP4_RAW, 1);
+    if (!handle) {
+      ret = IAMF_ERR_INVALID_STATE;
+      break;
+    }
+
+    st->handles[i] = handle;
+
+    if (i < coupled_streams)
+      ret = aacDecoder_ConfigRaw(handle, conf, &clen);
+    else
+      ret = aacDecoder_ConfigRaw(handle, &conf[1], &clen);
+    if (ret != AAC_DEC_OK) {
+      ia_loge("aac config raw error code %d", ret);
+      ret = IAMF_ERR_INTERNAL;
+      break;
+    }
+    ret = aacDecoder_SetParam(handle, AAC_CONCEAL_METHOD, 1);
+    if (ret != AAC_DEC_OK) {
+      ia_loge("aac set parameter error code %d", ret);
+      ret = IAMF_ERR_INTERNAL;
+      break;
+    }
+  }
+
+end:
+  if (ret < 0) {
+    if (error) *error = ret;
+    if (st) aac_multistream_decoder_close(st);
+    st = 0;
+  }
+
+  IAMF_FREE(conf[1]);
+
+  return st;
+}
+
+int aac_multistream_decode(AACMSDecoder *st, uint8_t *buffer[], uint32_t size[],
+                           void *pcm, uint32_t frame_size) {
+  if (st->flags & AUDIO_FRAME_PLANE)
+    return aac_multistream_decode_native(st, buffer, size, pcm, frame_size,
+                                         aac_copy_channel_out_short_plane);
+  ia_loge("flags is 0x%x, is not implmeneted.", st->flags);
+  return IAMF_ERR_UNIMPLEMENTED;
+}
+
+void aac_multistream_decoder_close(AACMSDecoder *st) {
+  if (st) {
+    if (st->handles) {
+      for (int i = 0; i < st->streams; ++i) {
+        if (st->handles[i]) {
+          aacDecoder_Close(st->handles[i]);
+        }
+      }
+      free(st->handles);
+    }
+    free(st);
+  }
+}
+
+int aac_multistream_decoder_get_delay(AACMSDecoder *st) {
+  return st->delay < 0 ? 0 : st->delay;
+}
