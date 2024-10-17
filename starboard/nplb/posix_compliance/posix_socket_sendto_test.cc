24f989751269276dec181b00c19f9565070aa6f8
Madhura Jayaraman <madjay@google.com>
2024-10-17 10:34:05 -0700

    Add Posix tests for sockets (#4201) (#4262)
    - Create Posix version of GetPortForTests
    - Remove references to socket_helper
    
    b/369182833
    original-hexsha: 6ceb79258fa8b63dbde53bb743a62723dee0d408

--- starboard/nplb/posix_compliance/posix_socket_sendto_test.cc
+++ starboard/nplb/posix_compliance/posix_socket_sendto_test.cc
@@ -82,7 +82,7 @@ TEST(PosixSocketSendtoTest, RainyDayUnconnectedSocket) {
 TEST(PosixSocketSendtoTest, RainyDaySendToClosedSocket) {
   int listen_socket_fd = -1, client_socket_fd = -1, server_socket_fd = -1;
   int result = PosixSocketCreateAndConnect(
-      AF_INET, AF_INET, htons(GetPortNumberForTests()), kSocketTimeout,
+      AF_INET, AF_INET, htons(PosixGetPortNumberForTests()), kSocketTimeout,
       &listen_socket_fd, &client_socket_fd, &server_socket_fd);
   ASSERT_TRUE(result == 0);
 
@@ -128,7 +128,7 @@ TEST(PosixSocketSendtoTest, RainyDaySendToSocketUntilBlocking) {
   int result = -1;
   int listen_socket_fd = -1, client_socket_fd = -1, server_socket_fd = -1;
   result = PosixSocketCreateAndConnect(
-      AF_INET, AF_INET, htons(GetPortNumberForTests()), kSocketTimeout,
+      AF_INET, AF_INET, htons(PosixGetPortNumberForTests()), kSocketTimeout,
       &listen_socket_fd, &client_socket_fd, &server_socket_fd);
   ASSERT_TRUE(result == 0);
 
@@ -174,9 +174,9 @@ TEST(PosixSocketSendtoTest, RainyDaySendToSocketConnectionReset) {
 
   // create listen socket, bind and listen on <port>
   int listen_socket_fd = -1, client_socket_fd = -1, server_socket_fd = -1;
-  PosixSocketCreateAndConnect(AF_INET, AF_INET, htons(GetPortNumberForTests()),
-                              kSocketTimeout, &listen_socket_fd,
-                              &client_socket_fd, &server_socket_fd);
+  PosixSocketCreateAndConnect(
+      AF_INET, AF_INET, htons(PosixGetPortNumberForTests()), kSocketTimeout,
+      &listen_socket_fd, &client_socket_fd, &server_socket_fd);
 
   // Kills the server, the client socket will have it's connection reset during
   // one of the subsequent writes.
