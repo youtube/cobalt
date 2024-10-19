57c6434f64cc824e5c236201629f8c7458a7cd50
John Xu <johnx@google.com>
2024-10-19 09:25:41 +0000

    `cobalt_apk` target to build dev.cobalt.coat
    This new target generate Cobalt.apk that has dev.cobalt.coat as its package name, and include most of Cobalt's java code.
    
    App starts from CobaltActivity now. CobaltActivity inherits from ContentShellActivity now instead of GameActivity.
    
    Remember to put treat_warnings_as_errors = false in gn args since Chromium treats java warnings as errors and we have many.
    
    There are many TODOs left in this CL and this is only to be served as a prototype to unblock SbPlayer and JS injection.
    
    b/374191454

--- build/android/gyp/util/resource_utils.py
+++ build/android/gyp/util/resource_utils.py
@@ -46,7 +46,7 @@ ALL_RESOURCE_TYPES = {
     'anim', 'animator', 'array', 'attr', 'bool', 'color', 'dimen', 'drawable',
     'font', 'fraction', 'id', 'integer', 'interpolator', 'layout', 'macro',
     'menu', 'mipmap', 'plurals', 'raw', 'string', 'style', 'styleable',
-    'transition', 'xml'
+    'transition', 'xml', 'overlayable'
 }
 
 AAPT_IGNORE_PATTERN = ':'.join([
