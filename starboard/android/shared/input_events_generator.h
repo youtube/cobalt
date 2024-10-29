bcbb108230edc37fe05dcc027b9aa1c507cd4a84
johnx <johnx@google.com>
2024-10-29 05:28:49 +0000

    `cobalt_apk` to build dev.cobalt.coat (#4304)
    This new target generate CobaltShell.apk that has `dev.cobalt.coat` as
    its package name, and include most of Cobalt's java code.
    
    App starts from CobaltActivity now. CobaltActivity inherits from
    ContentShellActivity now instead of GameActivity.
    
    Remember to put treat_warnings_as_errors = false in gn args since
    Chromium treats java warnings as errors and we have many.
    
    There are many TODOs left in this CL and this is only to be served as a
    prototype to unblock SbPlayer and JS injection.
    
    b/374191454
    original-hexsha: c9084fe634578b6e6556ea60b7d65ff096968666

--- starboard/android/shared/input_events_generator.h
+++ starboard/android/shared/input_events_generator.h
@@ -19,8 +19,6 @@
 #include <memory>
 #include <vector>
 
-#include "game-activity/GameActivity.h"
-
 #include "starboard/input.h"
 #include "starboard/shared/starboard/application.h"
 #include "starboard/window.h"
@@ -30,67 +28,72 @@ namespace android {
 namespace shared {
 
 class InputEventsGenerator {
- public:
-  typedef ::starboard::shared::starboard::Application::Event Event;
-  typedef std::vector<std::unique_ptr<Event>> Events;
+  //  public:
+  //   typedef ::starboard::shared::starboard::Application::Event Event;
+  //   typedef std::vector<std::unique_ptr<Event>> Events;
 
-  explicit InputEventsGenerator(SbWindow window);
-  virtual ~InputEventsGenerator();
+  //   explicit InputEventsGenerator(SbWindow window);
+  //   virtual ~InputEventsGenerator();
 
-  // Translates an Android input event into a series of Starboard application
-  // events. The caller owns the new events and must delete them when done with
-  // them.
-  bool CreateInputEventsFromGameActivityEvent(
-      GameActivityMotionEvent* android_event,
-      Events* events);
-  bool CreateInputEventsFromGameActivityEvent(
-      GameActivityKeyEvent* android_event,
-      Events* events);
+  //   // Translates an Android input event into a series of Starboard
+  //   application
+  //   // events. The caller owns the new events and must delete them when done
+  //   with
+  //   // them.
+  //   bool CreateInputEventsFromGameActivityEvent(
+  //       GameActivityMotionEvent* android_event,
+  //       Events* events);
+  //   bool CreateInputEventsFromGameActivityEvent(
+  //       GameActivityKeyEvent* android_event,
+  //       Events* events);
 
-  // Create press/unpress events from SbKey
-  // (for use with CobaltA11yHelper injection)
-  void CreateInputEventsFromSbKey(SbKey key, Events* events);
+  //   // Create press/unpress events from SbKey
+  //   // (for use with CobaltA11yHelper injection)
+  //   void CreateInputEventsFromSbKey(SbKey key, Events* events);
 
- private:
-  enum FlatAxis {
-    kLeftX,
-    kLeftY,
-    kRightX,
-    kRightY,
-    kNumAxes,
-  };
+  //  private:
+  //   enum FlatAxis {
+  //     kLeftX,
+  //     kLeftY,
+  //     kRightX,
+  //     kRightY,
+  //     kNumAxes,
+  //   };
 
-  bool ProcessPointerEvent(GameActivityMotionEvent* android_event,
-                           Events* events);
-  void ProcessJoyStickEvent(FlatAxis axis,
-                            int32_t motion_axis,
-                            GameActivityMotionEvent* android_event,
-                            Events* events);
-  void UpdateDeviceFlatMapIfNecessary(GameActivityMotionEvent* android_event,
-                                      jobject input_device);
-  void UpdateDeviceFlatMapIfNecessary(GameActivityKeyEvent* android_event);
+  //   bool ProcessPointerEvent(GameActivityMotionEvent* android_event,
+  //                            Events* events);
+  //   void ProcessJoyStickEvent(FlatAxis axis,
+  //                             int32_t motion_axis,
+  //                             GameActivityMotionEvent* android_event,
+  //                             Events* events);
+  //   void UpdateDeviceFlatMapIfNecessary(GameActivityMotionEvent*
+  //   android_event,
+  //                                       jobject input_device);
+  //   void UpdateDeviceFlatMapIfNecessary(GameActivityKeyEvent* android_event);
 
-  void ProcessFallbackDPadEvent(SbInputEventType type,
-                                SbKey key,
-                                GameActivityKeyEvent* android_event,
-                                Events* events);
-  void UpdateHatValuesAndPossiblySynthesizeKeyEvents(
-      GameActivityMotionEvent* android_event,
-      Events* events);
+  //   void ProcessFallbackDPadEvent(SbInputEventType type,
+  //                                 SbKey key,
+  //                                 GameActivityKeyEvent* android_event,
+  //                                 Events* events);
+  //   void UpdateHatValuesAndPossiblySynthesizeKeyEvents(
+  //       GameActivityMotionEvent* android_event,
+  //       Events* events);
 
-  SbWindow window_;
+  //   SbWindow window_;
 
-  // Map the device id with joystick flat position.
-  // Cache the flat area of joystick to avoid calling jni functions frequently.
-  std::map<int32_t, std::vector<float>> device_flat_;
+  //   // Map the device id with joystick flat position.
+  //   // Cache the flat area of joystick to avoid calling jni functions
+  //   frequently. std::map<int32_t, std::vector<float>> device_flat_;
 
-  // The current X/Y analog values of the "hat" (dpad on the game controller).
-  float hat_value_[2];
+  //   // The current X/Y analog values of the "hat" (dpad on the game
+  //   controller). float hat_value_[2];
 
-  // The last known value of the left thumbstick, used to track when we should
-  // generate key unpressed events for it.  We store values for horizontal and
-  // vertical directions independently.
-  SbKey left_thumbstick_key_pressed_[2];
+  //   // The last known value of the left thumbstick, used to track when we
+  //   should
+  //   // generate key unpressed events for it.  We store values for horizontal
+  //   and
+  //   // vertical directions independently.
+  //   SbKey left_thumbstick_key_pressed_[2];
 };
 
 }  // namespace shared
