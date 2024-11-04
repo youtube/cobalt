d4a8068af9449f1aa86a1ee505d99f8f08e347f7
Kaido Kert <kaidokert@google.com>
2024-11-04 10:00:36 -0800

    Fixing Cobalt APK JNI initialization (#4359)
    - Move JNI state variables into a standalone dynamic library to ensure
    all callers obtain consistent, shared state.
    
    - Disable POSIX emulation functions, they run into initialization order
    conflicts where JNI state isn't available when functions like `close`
    are already called.
    
    - Disable problematic extensions that interact early with JNI
    
    - Fix initialization order. Chromium library loader (
    LibraryLoader.ensureInitialized ) needs to run and load all dynamic
    libraries and initialize its JNI environment, before Starboard bridge
    loads. It also calls `JNI_OnLoad` in `cobalt_library_loader.cc` and
    passes it a VM pointer.
    
    - Restore StarboardBridge native JNI initialization, start the Starboard
    thread, and enable related minimal modules, including AudioManager and
    VideoSurfaceView
    
    - Ensure JNI_VERSION matches with what Chromium default uses (
    JNI_VERSION_1_6 is only used for Cronet, but not elsewhere ).
    
    - Ensure initial deeplink variable is never null to prevent crashing
    early init, use an empty string instead.
    
    b/376348543
    
    original-hexsha: 477c6279cf64a8b33cbb1ee333062ea7413536a6

---
+++ starboard/android/shared/jni_state.cc
@@ -0,0 +1,68 @@
+// Copyright 2024 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <jni.h>
+
+#include "jni_state.h"
+
+/*
+  This module stores shared JNI state variables.
+  It's contained in a standalone shared library, sharing the same state
+  across all calling modules.
+  Do not add any functionality here, besides setting and retrieving shared
+  state variables.
+  Also do not add any other dependencies besides <jni.h>
+
+  Unifying Starboard/Chrome JNI will remove the need for this.
+*/
+
+namespace {
+JavaVM* g_vm = NULL;
+jobject g_application_class_loader = NULL;
+jobject g_starboard_bridge = NULL;
+}  // namespace
+
+namespace starboard {
+namespace android {
+namespace shared {
+
+STARBOARD_JNI_STATE_EXPORT
+void JNIState::SetVM(JavaVM* vm) {
+  g_vm = vm;
+}
+
+STARBOARD_JNI_STATE_EXPORT
+JavaVM*& JNIState::GetVM() {
+  return g_vm;
+}
+STARBOARD_JNI_STATE_EXPORT
+void JNIState::SetStarboardBridge(jobject value) {
+  g_starboard_bridge = value;
+}
+STARBOARD_JNI_STATE_EXPORT
+jobject& JNIState::GetStarboardBridge() {
+  return g_starboard_bridge;
+}
+STARBOARD_JNI_STATE_EXPORT
+void JNIState::SetApplicationClassLoader(jobject value) {
+  g_application_class_loader = value;
+}
+STARBOARD_JNI_STATE_EXPORT
+jobject& JNIState::GetApplicationClassLoader() {
+  return g_application_class_loader;
+}
+
+}  // namespace shared
+}  // namespace android
+}  // namespace starboard
