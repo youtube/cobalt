8bfa5084aa10b0e8f90ed59715c6e42a8377dda8
Kaido Kert <kaidokert@google.com>
2024-11-04 10:00:36 -0800

    Fixing Cobalt APK JNI initialization (#4359)
    - Move JNI state variables into a standalone dynamic library to ensure
    all callers obtain consistent, shared state.
    
    - Disable POSIX emulation functions, they run into initialization order
    conflicts where JNI state isn't available when functions like `close`
    are already called.
    
    - Disable problematic extensions that interact early with JNI
    
    - Fix initialization order. Chromium library loader (
    LibraryLoader.ensureInitialized ) needs to run and load all dynamic
    libraries and initialize its JNI environment, before Starboard bridge
    loads. It also calls `JNI_OnLoad` in `cobalt_library_loader.cc` and
    passes it a VM pointer.
    
    - Restore StarboardBridge native JNI initialization, start the Starboard
    thread, and enable related minimal modules, including AudioManager and
    VideoSurfaceView
    
    - Ensure JNI_VERSION matches with what Chromium default uses (
    JNI_VERSION_1_6 is only used for Cronet, but not elsewhere ).
    
    - Ensure initial deeplink variable is never null to prevent crashing
    early init, use an empty string instead.
    
    b/376348543
    
    original-hexsha: 477c6279cf64a8b33cbb1ee333062ea7413536a6

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
@@ -18,33 +18,33 @@ import static dev.cobalt.util.Log.TAG;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.text.TextUtils;
-import android.view.KeyEvent;
-import android.widget.Toast;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Pair;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.view.ViewParent;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+import dev.cobalt.app.CobaltApplication;
+import dev.cobalt.media.AudioOutputManager;
 import dev.cobalt.media.MediaCodecCapabilitiesLogger;
 import dev.cobalt.media.VideoSurfaceView;
 import dev.cobalt.util.DisplayUtil;
 import dev.cobalt.util.Log;
 import dev.cobalt.util.UsedByNative;
-import dev.cobalt.app.CobaltApplication;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
-// import dev.cobalt.media.AudioOutputManager;
 import org.chromium.base.CommandLine;
 import org.chromium.base.MemoryPressureListener;
 import org.chromium.base.library_loader.LibraryLoader;
@@ -90,7 +90,7 @@ public abstract class CobaltActivity extends Activity {
 
   private VideoSurfaceView videoSurfaceView;
 
-  private boolean forceCreateNewVideoSurfaceView = false;
+  private boolean forceCreateNewVideoSurfaceView;
 
   private long timeInNanoseconds;
 
@@ -102,152 +102,174 @@ public abstract class CobaltActivity extends Activity {
 
   // Initially copied from ContentShellActiviy.java
   protected void createContent(final Bundle savedInstanceState) {
-      // Initializing the command line must occur before loading the library.
-      if (!CommandLine.isInitialized()) {
-          ((CobaltApplication) getApplication()).initCommandLine();
+    // Initializing the command line must occur before loading the library.
+    if (!CommandLine.isInitialized()) {
+      ((CobaltApplication) getApplication()).initCommandLine();
 
-          // Note that appendSwitchesAndArguments excludes cobaltCommandLineParams[0]
-          // as the program name, and all other arguments SHOULD start with '--'.
-          String[] cobaltCommandLineParams = new String[]{"",
-          // disable first run experience
-          "--disable-fre",
-          // disable user prompts in the first run
-          "--no-first-run",
-          // run Cobalt as a single process
-          "--single-process",
-          // enable Blink to work in overlay video mode
-          "--force-video-overlays",
-          // remove below if Cobalt rebase to m120+
-          "--user-level-memory-pressure-signal-params"};
-          CommandLine.getInstance().appendSwitchesAndArguments(cobaltCommandLineParams);
+      // Note that appendSwitchesAndArguments excludes cobaltCommandLineParams[0]
+      // as the program name, and all other arguments SHOULD start with '--'.
+      String[] cobaltCommandLineParams =
+          new String[] {
+            "",
+            // disable first run experience
+            "--disable-fre",
+            // disable user prompts in the first run
+            "--no-first-run",
+            // run Cobalt as a single process
+            "--single-process",
+            // enable Blink to work in overlay video mode
+            "--force-video-overlays",
+            // remove below if Cobalt rebase to m120+
+            "--user-level-memory-pressure-signal-params"
+          };
+      CommandLine.getInstance().appendSwitchesAndArguments(cobaltCommandLineParams);
 
-          String[] commandLineParams = getCommandLineParamsFromIntent(getIntent());
-          if (commandLineParams != null) {
-              CommandLine.getInstance().appendSwitchesAndArguments(commandLineParams);
-          }
+      String[] commandLineParams = getCommandLineParamsFromIntent(getIntent());
+      if (commandLineParams != null) {
+        CommandLine.getInstance().appendSwitchesAndArguments(commandLineParams);
       }
+    }
 
-      DeviceUtils.addDeviceSpecificUserAgentSwitch();
+    DeviceUtils.addDeviceSpecificUserAgentSwitch();
 
-      LibraryLoader.getInstance().ensureInitialized();
+    // This initializes JNI and ends up calling JNI_OnLoad in native code
+    LibraryLoader.getInstance().ensureInitialized();
 
-      setContentView(R.layout.content_shell_activity);
-      mShellManager = findViewById(R.id.shell_container);
-      final boolean listenToActivityState = true;
-      mIntentRequestTracker = IntentRequestTracker.createFromActivity(this);
-      mWindowAndroid =
-              new ActivityWindowAndroid(this, listenToActivityState, mIntentRequestTracker);
-      mIntentRequestTracker.restoreInstanceState(savedInstanceState);
-      mShellManager.setWindow(mWindowAndroid);
-      // Set up the animation placeholder to be the SurfaceView. This disables the
-      // SurfaceView's 'hole' clipping during animations that are notified to the window.
-      mWindowAndroid.setAnimationPlaceholderView(
-              mShellManager.getContentViewRenderView().getSurfaceView());
+    // StarboardBridge initialization must happen right after library loading,
+    // before Browser/Content module is started. It currently tracks its own JNI state
+    // variables, and needs to set up an early copy.
 
-      // TODO(cobalt, b/376148547): set Chrobalt initial url and remove this function.
-      if (mStartupUrl.isEmpty()) {
-          mStartupUrl = getUrlFromIntent(getIntent());
-      }
-      if (!TextUtils.isEmpty(mStartupUrl)) {
-          mShellManager.setStartupUrl(Shell.sanitizeUrl(mStartupUrl));
-      }
+    // TODO(b/374147993): how to handle deeplink in Chrobalt?
+    String startDeepLink = getIntentUrlAsString(getIntent());
+    if (startDeepLink == null) {
+      throw new IllegalArgumentException("startDeepLink cannot be null, set it to empty string");
+    }
+    if (getStarboardBridge() == null) {
+      // Cold start - Instantiate the singleton StarboardBridge.
+      StarboardBridge starboardBridge = createStarboardBridge(getArgs(), startDeepLink);
+      ((StarboardBridge.HostApplication) getApplication()).setStarboardBridge(starboardBridge);
+    } else {
+      // Warm start - Pass the deep link to the running Starboard app.
+      getStarboardBridge().handleDeepLink(startDeepLink);
+    }
 
-      if (CommandLine.getInstance().hasSwitch(RUN_WEB_TESTS_SWITCH)) {
-          BrowserStartupController.getInstance().startBrowserProcessesSync(
-                  LibraryProcessType.PROCESS_BROWSER, false, false);
-      } else {
-          BrowserStartupController.getInstance().startBrowserProcessesAsync(
-                  LibraryProcessType.PROCESS_BROWSER, true, false,
-                  new BrowserStartupController.StartupCallback() {
-                      @Override
-                      public void onSuccess() {
-                          finishInitialization(savedInstanceState);
-                      }
+    setContentView(R.layout.content_shell_activity);
+    mShellManager = findViewById(R.id.shell_container);
+    final boolean listenToActivityState = true;
+    mIntentRequestTracker = IntentRequestTracker.createFromActivity(this);
+    mWindowAndroid = new ActivityWindowAndroid(this, listenToActivityState, mIntentRequestTracker);
+    mIntentRequestTracker.restoreInstanceState(savedInstanceState);
+    mShellManager.setWindow(mWindowAndroid);
+    // Set up the animation placeholder to be the SurfaceView. This disables the
+    // SurfaceView's 'hole' clipping during animations that are notified to the window.
+    mWindowAndroid.setAnimationPlaceholderView(
+        mShellManager.getContentViewRenderView().getSurfaceView());
 
-                      @Override
-                      public void onFailure() {
-                          initializationFailed();
-                      }
-                  });
-      }
+    // TODO(cobalt, b/376148547): set Chrobalt initial url and remove this function.
+    if (mStartupUrl.isEmpty()) {
+      mStartupUrl = getUrlFromIntent(getIntent());
+    }
+    if (!TextUtils.isEmpty(mStartupUrl)) {
+      mShellManager.setStartupUrl(Shell.sanitizeUrl(mStartupUrl));
+    }
+
+    // TODO(b/377025559): Bring back WebTests launch capability
+    BrowserStartupController.getInstance()
+        .startBrowserProcessesAsync(
+            LibraryProcessType.PROCESS_BROWSER,
+            false, // Do not start a separate GPU process
+            // TODO(b/377025565): Figure out what this means
+            false, // Do not start in "minimal" or paused mode
+            new BrowserStartupController.StartupCallback() {
+              @Override
+              public void onSuccess() {
+                Log.i(TAG, "Browser process init succeeded");
+                finishInitialization(savedInstanceState);
+              }
+
+              @Override
+              public void onFailure() {
+                Log.e(TAG, "Browser process init failed");
+                initializationFailed();
+              }
+            });
   }
 
   // Initially copied from ContentShellActiviy.java
   private void finishInitialization(Bundle savedInstanceState) {
-      String shellUrl;
-      if (!TextUtils.isEmpty(mStartupUrl)) {
-          shellUrl = mStartupUrl;
-      } else {
-          shellUrl = ShellManager.DEFAULT_SHELL_URL;
-      }
+    String shellUrl;
+    if (!TextUtils.isEmpty(mStartupUrl)) {
+      shellUrl = mStartupUrl;
+    } else {
+      shellUrl = ShellManager.DEFAULT_SHELL_URL;
+    }
 
-      if (savedInstanceState != null
-              && savedInstanceState.containsKey(ACTIVE_SHELL_URL_KEY)) {
-          shellUrl = savedInstanceState.getString(ACTIVE_SHELL_URL_KEY);
-      }
-      // Set to overlay video mode.
-      mShellManager.getContentViewRenderView().setOverlayVideoMode(true);
-      mShellManager.launchShell(shellUrl);
+    if (savedInstanceState != null && savedInstanceState.containsKey(ACTIVE_SHELL_URL_KEY)) {
+      shellUrl = savedInstanceState.getString(ACTIVE_SHELL_URL_KEY);
+    }
+    // Set to overlay video mode.
+    mShellManager.getContentViewRenderView().setOverlayVideoMode(true);
+    mShellManager.launchShell(shellUrl);
 
-      toggleFullscreenMode(true);
+    toggleFullscreenMode(true);
   }
 
   // Initially copied from ContentShellActiviy.java
   private void initializationFailed() {
-      Log.e(TAG, "ContentView initialization failed.");
-      Toast.makeText(CobaltActivity.this,
-              R.string.browser_process_initialization_failed,
-              Toast.LENGTH_SHORT).show();
-      finish();
+    Log.e(TAG, "ContentView initialization failed.");
+    Toast.makeText(
+            CobaltActivity.this, R.string.browser_process_initialization_failed, Toast.LENGTH_SHORT)
+        .show();
+    finish();
   }
 
   // Initially copied from ContentShellActiviy.java
   @Override
   protected void onSaveInstanceState(Bundle outState) {
-      super.onSaveInstanceState(outState);
-      WebContents webContents = getActiveWebContents();
-      if (webContents != null) {
-          // TODO(yfriedman): crbug/783819 - This should use GURL serialize/deserialize.
-          outState.putString(ACTIVE_SHELL_URL_KEY, webContents.getLastCommittedUrl().getSpec());
-      }
+    super.onSaveInstanceState(outState);
+    WebContents webContents = getActiveWebContents();
+    if (webContents != null) {
+      // TODO(yfriedman): crbug/783819 - This should use GURL serialize/deserialize.
+      outState.putString(ACTIVE_SHELL_URL_KEY, webContents.getLastCommittedUrl().getSpec());
+    }
 
-      mIntentRequestTracker.saveInstanceState(outState);
+    mIntentRequestTracker.saveInstanceState(outState);
   }
 
   // Initially copied from ContentShellActiviy.java
   @Override
   public boolean onKeyUp(int keyCode, KeyEvent event) {
-      if (keyCode == KeyEvent.KEYCODE_BACK) {
-          WebContents webContents = getActiveWebContents();
-          if (webContents != null && webContents.getNavigationController().canGoBack()) {
-              webContents.getNavigationController().goBack();
-              return true;
-          }
+    if (keyCode == KeyEvent.KEYCODE_BACK) {
+      WebContents webContents = getActiveWebContents();
+      if (webContents != null && webContents.getNavigationController().canGoBack()) {
+        webContents.getNavigationController().goBack();
+        return true;
       }
+    }
 
-      return super.onKeyUp(keyCode, event);
+    return super.onKeyUp(keyCode, event);
   }
 
   // Initially copied from ContentShellActiviy.java
   protected void shellHandleIntent(Intent intent) {
-      if (getCommandLineParamsFromIntent(intent) != null) {
-          Log.i(TAG, "Ignoring command line params: can only be set when creating the activity.");
-      }
+    if (getCommandLineParamsFromIntent(intent) != null) {
+      Log.i(TAG, "Ignoring command line params: can only be set when creating the activity.");
+    }
 
-      if (MemoryPressureListener.handleDebugIntent(this, intent.getAction())) return;
+    if (MemoryPressureListener.handleDebugIntent(this, intent.getAction())) return;
 
-      String url = getUrlFromIntent(intent);
-      if (!TextUtils.isEmpty(url)) {
-          Shell activeView = getActiveShell();
-          if (activeView != null) {
-              activeView.loadUrl(url);
-          }
+    String url = getUrlFromIntent(intent);
+    if (!TextUtils.isEmpty(url)) {
+      Shell activeView = getActiveShell();
+      if (activeView != null) {
+        activeView.loadUrl(url);
       }
+    }
   }
 
   // TODO(cobalt, b/376148547): set Chrobalt initial url and remove this function.
   protected void setStartupUrl(String url) {
-      mStartupUrl = url;
+    mStartupUrl = url;
   }
 
   protected void toggleFullscreenMode(boolean enterFullscreen) {
@@ -258,54 +280,54 @@ public abstract class CobaltActivity extends Activity {
   // Initially copied from ContentShellActiviy.java
   @Override
   public void onActivityResult(int requestCode, int resultCode, Intent data) {
-      super.onActivityResult(requestCode, resultCode, data);
-      mIntentRequestTracker.onActivityResult(requestCode, resultCode, data);
+    super.onActivityResult(requestCode, resultCode, data);
+    mIntentRequestTracker.onActivityResult(requestCode, resultCode, data);
   }
 
   // Initially copied from ContentShellActiviy.java
   @Override
   public void startActivity(Intent i) {
-      mLastSentIntent = i;
-      super.startActivity(i);
+    mLastSentIntent = i;
+    super.startActivity(i);
   }
 
   public Intent getLastSentIntent() {
-      return mLastSentIntent;
+    return mLastSentIntent;
   }
 
   private static String getUrlFromIntent(Intent intent) {
-      return intent != null ? intent.getDataString() : null;
+    return intent != null ? intent.getDataString() : null;
   }
 
   private static String[] getCommandLineParamsFromIntent(Intent intent) {
-      return intent != null ? intent.getStringArrayExtra(COMMAND_LINE_ARGS_KEY) : null;
+    return intent != null ? intent.getStringArrayExtra(COMMAND_LINE_ARGS_KEY) : null;
   }
 
   /**
-   * @return The {@link ShellManager} configured for the activity or null if it has not been
-   *         created yet.
+   * @return The {@link ShellManager} configured for the activity or null if it has not been created
+   *     yet.
    */
   public ShellManager getShellManager() {
-      return mShellManager;
+    return mShellManager;
   }
 
   /**
    * @return The currently visible {@link Shell} or null if one is not showing.
    */
   public Shell getActiveShell() {
-      return mShellManager != null ? mShellManager.getActiveShell() : null;
+    return mShellManager != null ? mShellManager.getActiveShell() : null;
   }
 
   /**
-   * @return The {@link WebContents} owned by the currently visible {@link Shell} or null if
-   *         one is not showing.
+   * @return The {@link WebContents} owned by the currently visible {@link Shell} or null if one is
+   *     not showing.
    */
   public WebContents getActiveWebContents() {
-      Shell shell = getActiveShell();
-      return shell != null ? shell.getWebContents() : null;
+    Shell shell = getActiveShell();
+    return shell != null ? shell.getWebContents() : null;
   }
 
-  //TODO(b/375442742): re-enable native code.
+  // TODO(b/375442742): re-enable native code.
   // private static native void nativeLowMemoryEvent();
 
   // TODO(cobalt): make WebContent accessible in CobaltActivity or StarboardBridge.
@@ -320,17 +342,6 @@ public abstract class CobaltActivity extends Activity {
     // early in the app's lifecycle. This connects the volume controls to
     // STREAM_MUSIC whenever the target activity or fragment is visible.
     setVolumeControlStream(AudioManager.STREAM_MUSIC);
-
-    // TODO(b/374147993): how to handle deeplink in Chrobalt?
-    String startDeepLink = getIntentUrlAsString(getIntent());
-    if (getStarboardBridge() == null) {
-      // Cold start - Instantiate the singleton StarboardBridge.
-      StarboardBridge starboardBridge = createStarboardBridge(getArgs(), startDeepLink);
-      ((StarboardBridge.HostApplication) getApplication()).setStarboardBridge(starboardBridge);
-    } else {
-      // Warm start - Pass the deep link to the running Starboard app.
-      getStarboardBridge().handleDeepLink(startDeepLink);
-    }
 
     setStartupUrl("https://www.youtube.com/tv");
     super.onCreate(savedInstanceState);
@@ -355,8 +366,7 @@ public abstract class CobaltActivity extends Activity {
   @Override
   protected void onStart() {
     if (!isReleaseBuild()) {
-      // TODO(cobalt): re-enable Cobalt AudioOutputManager.
-      // getStarboardBridge().getAudioOutputManager().dumpAllOutputDevices();
+      getStarboardBridge().getAudioOutputManager().dumpAllOutputDevices();
       MediaCodecCapabilitiesLogger.dumpAllDecoders();
     }
     if (forceCreateNewVideoSurfaceView) {
@@ -366,7 +376,7 @@ public abstract class CobaltActivity extends Activity {
 
     DisplayUtil.cacheDefaultDisplay(this);
     DisplayUtil.addDisplayListener(this);
-    // AudioOutputManager.addAudioDeviceListener(this);
+    AudioOutputManager.addAudioDeviceListener(this);
 
     getStarboardBridge().onActivityStart(this);
 
@@ -562,7 +572,7 @@ public abstract class CobaltActivity extends Activity {
    */
   protected String getIntentUrlAsString(Intent intent) {
     Uri intentUri = intent.getData();
-    return (intentUri == null) ? null : intentUri.toString();
+    return (intentUri == null) ? "" : intentUri.toString();
   }
 
   // TODO(cobalt): re-eanble microphone permission request at startup or remove code.
