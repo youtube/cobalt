846daa7b6a9153c44d26db77ebe7e6e83185ed55
Colin Liang <zhongqi.liang.4u@gmail.com>
2024-11-05 19:15:48 -0800

    Chrobalt Java Bridge (#4335)
    This change enables communication between Java and JavaScript in
    Cobalt's Android app. It introduces a Java bridge, allowing JavaScript
    to call Java methods and access Java objects. This is achieved by
    injecting Java objects into the WebView and loading corresponding
    JavaScript code. An example (CobaltJavaScriptAndroidObjectExample.java,
    example.js) demonstrates the usage.
    
    b/372558900
    
    ---------
    
    Co-authored-by: Colin Liang <colinliang@google.com>
    
    original-hexsha: ccba39f30857152e40be1da279d43c15444584cc

---
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/javabridge/AmatiDeviceInspector.java
@@ -0,0 +1,50 @@
+// Copyright 2024 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dev.cobalt.coat.javabridge;
+
+import static dev.cobalt.util.Log.TAG;
+
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * A simple example to implement CobaltJavaScriptAndroidObject.
+ * Allows querying the platform for whether the device supports
+ * GoogleTV (Googles see go/what-is-amati).
+ */
+public class AmatiDeviceInspector implements CobaltJavaScriptAndroidObject {
+
+    private final Context context;
+
+    public AmatiDeviceInspector(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public String getJavaScriptInterfaceName() {
+        return "Android_AmatiDeviceInspector";
+    }
+
+    @Override
+    public String getJavaScriptAssetName() {
+        return "amati_device_inspector.js";
+    }
+
+    @CobaltJavaScriptInterface
+    public void printIsAmatiDevice() {
+        boolean isAmatiDevice = context.getPackageManager().hasSystemFeature("com.google.android.feature.AMATI_EXPERIENCE");
+        Log.i(TAG, "It is running on an Amati device? " + isAmatiDevice);
+    }
+}
