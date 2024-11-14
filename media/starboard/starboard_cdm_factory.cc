6d979993fcb8dc20d58202eb4b419de96c0ed49a
thorsten sideb0ard <sideboard@google.com>
2024-11-14 14:37:32 -0800

    Implement media::ContentDecryptionModule interface for Starboard (#4368)
    b/328305808
    
    Allows playback of encrypted content by wrapping SbDrm in a
    media::ContentDecryptionModule interface.
    
    original-hexsha: 3b13623643828f1dafaf6277d018c923874ef383

---
+++ media/starboard/starboard_cdm_factory.cc
@@ -0,0 +1,50 @@
+// Copyright 2024 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard_cdm_factory.h"
+#include "starboard_cdm.h"
+
+#include "base/functional/bind.h"
+#include "base/task/bind_post_task.h"
+#include "media/base/cdm_config.h"
+#include "media/base/key_systems.h"
+
+namespace media {
+
+StarboardCdmFactory::StarboardCdmFactory() {}
+
+StarboardCdmFactory::~StarboardCdmFactory() = default;
+
+void StarboardCdmFactory::Create(
+    const CdmConfig& cdm_config,
+    const SessionMessageCB& session_message_cb,
+    const SessionClosedCB& session_closed_cb,
+    const SessionKeysChangeCB& session_keys_change_cb,
+    const SessionExpirationUpdateCB& session_expiration_update_cb,
+    CdmCreatedCB cdm_created_cb) {
+  auto cdm = base::MakeRefCounted<StarboardCdm>(
+      cdm_config, session_message_cb, session_closed_cb, session_keys_change_cb,
+      session_expiration_update_cb);
+
+  auto bound_cdm_created_cb =
+      base::BindPostTaskToCurrentDefault(std::move(cdm_created_cb));
+
+  if (cdm->HasValidSbDrm()) {
+    std::move(bound_cdm_created_cb).Run(std::move(cdm), "Success");
+  } else {
+    std::move(bound_cdm_created_cb).Run(nullptr, "CdmFactoryCreationFailed");
+  }
+}
+
+}  // namespace media
