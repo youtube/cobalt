2770c58bc07127441e4dc4322f3d0b8d687b4ba5
Colin Liang <zhongqi.liang.4u@gmail.com>
2024-11-14 11:00:25 -0800

    Enable Javascript console log (#4426)
    b/375411072
    
    Co-authored-by: Colin Liang <colinliang@google.com>

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
@@ -96,18 +96,20 @@ public abstract class CobaltActivity extends Activity {
       String[] cobaltCommandLineParams =
           new String[] {
             "",
-            // disable first run experience
+            // Disable first run experience.
             "--disable-fre",
-            // disable user prompts in the first run
+            // Disable user prompts in the first run.
             "--no-first-run",
-            // run Cobalt as a single process
+            // Run Cobalt as a single process.
             "--single-process",
-            // enable Blink to work in overlay video mode
+            // Enable Blink to work in overlay video mode.
             "--force-video-overlays",
-            // autoplay video with url
+            // Autoplay video with url.
             "--autoplay-policy=no-user-gesture-required",
             // Disable rescaling Webpage.
             "--force-device-scale-factor=1",
+            // Pass javascript console log to adb log.
+            "--enable-features=LogJsConsoleMessages"
           };
       CommandLine.getInstance().appendSwitchesAndArguments(cobaltCommandLineParams);
 
