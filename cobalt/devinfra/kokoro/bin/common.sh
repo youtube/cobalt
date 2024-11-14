bb8e3f6974868c0b384c9695daebe5d4921b3953
Dana Dahlstrom <dahlstrom@google.com>
2024-11-14 11:00:00 -0800

    Add kokoro tree from commit 73c47431
    Issue: 365150653
    
    original-hexsha: f4c26a295f01046b42d14146162c02c5944fa24f

---
+++ cobalt/devinfra/kokoro/bin/common.sh
@@ -0,0 +1,330 @@
+#!/bin/bash
+set -eux
+
+#---------------------------------
+# Reusable common kokoro functions.
+# --------------------------------
+
+
+log_cobalt_header () {
+  set +x
+  echo "############################################################################################################"
+  echo "#::'######:::'#######::'########:::::'###::::'##::::'########::::'##::::::::'#######:::'######::::'######::#"
+  echo "#:'##... ##:'##.... ##: ##.... ##:::'## ##::: ##::::... ##..::::: ##:::::::'##.... ##:'##... ##::'##... ##:#"
+  echo "#: ##:::..:: ##:::: ##: ##:::: ##::'##:. ##:: ##::::::: ##::::::: ##::::::: ##:::: ##: ##:::..::: ##:::..::#"
+  echo "#: ##::::::: ##:::: ##: ########::'##:::. ##: ##::::::: ##::::::: ##::::::: ##:::: ##: ##::'####:. ######::#"
+  echo "#: ##::::::: ##:::: ##: ##.... ##: #########: ##::::::: ##::::::: ##::::::: ##:::: ##: ##::: ##:::..... ##:#"
+  echo "#: ##::: ##: ##:::: ##: ##:::: ##: ##.... ##: ##::::::: ##::::::: ##::::::: ##:::: ##: ##::: ##::'##::: ##:#"
+  echo "#:. ######::. #######:: ########:: ##:::: ##: ########: ##::::::: ########:. #######::. ######:::. ######::#"
+  echo "#::......::::.......:::........:::..:::::..::........::..::::::::........:::.......::::......:::::......:::#"
+  echo "############################################################################################################"
+  set -x
+}
+log_cobalt_header
+
+
+get_kokoro_env () {
+  # Kokoro doesn't provide a direct way to detect which environment the job is
+  # running in: https://yaqs.corp.google.com/eng/q/3810666508825133056
+  # This is a workaround that uses the artifacts directory where the first path
+  # segment is the env: "<environment>/<job name>/<build number>/<date>"
+  echo "${KOKORO_BUILD_ARTIFACTS_SUBDIR}" | cut -d'/' -f1
+}
+
+
+get_project_name () {
+  if [[ "$(get_kokoro_env)" == "prod" ]]; then
+    echo "cobalt-kokoro"
+  elif [[ "$(get_kokoro_env)" == "qa" ]]; then
+    echo "cobalt-kokoro-qa"
+  else
+    exit 1
+  fi
+}
+
+
+get_bucket_name () {
+  if [[ "$(get_kokoro_env)" == "prod" ]]; then
+    echo "cobalt-internal-build-artifacts"
+  elif [[ "$(get_kokoro_env)" == "qa" ]]; then
+    echo "cobalt-internal-build-artifacts-qa"
+  else
+    exit 1
+  fi
+}
+
+
+configure_environment () {
+  # Tell git the repo is safe.
+  git config --global --add safe.directory "${WORKSPACE_COBALT}"
+
+  # Use Kokor's default credentials instead of boto file.
+  unset BOTO_PATH
+
+  # Add repository root to PYTHONPATH.
+  export PYTHONPATH="${WORKSPACE_COBALT}${PYTHONPATH:+:${PYTHONPATH}}"
+
+  # Setup SCCACHE.
+  if [ -n "${SCCACHE_GCS_KEY_FILE_NAME+set}" ]; then
+    export SCCACHE_GCS_KEY_PATH="${KOKORO_KEYSTORE_DIR}/${SCCACHE_GCS_KEY_FILE_NAME}"
+  fi
+
+  # In some instances KOKORO_GOB_BRANCH_src is not set, set by default to 'COBALT'.
+  if [ -z "${KOKORO_GOB_BRANCH_src+set}" ]; then
+    echo 'WARNING: $KOKORO_GOB_BRANCH_src is unset. Defaulting to "COBALT"'
+    export KOKORO_GOB_BRANCH_src="COBALT"
+  fi
+
+  # In some instances kokoro uses KOKORO_GOB_BRANCH instead.
+  if [ -z "${KOKORO_GOB_BRANCH+set}" ]; then
+    echo 'WARNING: $KOKORO_GOB_BRANCH is unset. Defaulting to "COBALT"'
+    export KOKORO_GOB_BRANCH="COBALT"
+  fi
+
+  env | sort
+}
+
+configure_dind_environment () {
+  export REGISTRY_PATH="$(get_registry_bucket_path)"
+
+  # Authenticate with GCR or GAR.
+  echo "Logging in to registry with access token"
+  set +x  #  Temporarily turn off debug logging to hide access token.
+  printf "$(get_registry_authentication_token)" | docker login -u \
+    oauth2accesstoken --password-stdin "https://${REGISTRY_HOSTNAME}"
+
+  printf "$(get_registry_authentication_token)" | docker login -u \
+    oauth2accesstoken --password-stdin "https://marketplace.gcr.io"
+  set -x
+}
+
+get_registry_authentication_token () {
+  # Hide access token in logs.
+  set +x
+  # Helper method that uses a well-known workaround for authenticating Kokoro
+  # workers to access GCP hosted container/artifiact registries.
+  # go/gcs-tpc-bigstore-user-guide#authentication-http-bearer-tokensaccess-tokens
+  local svc_account="${REGISTRY_METADATA}/instance/service-accounts/default"
+  local access_token=$(curl -H 'Metadata-Flavor: Google' ${svc_account}/token | cut -d'"' -f 4)
+  echo "${access_token}"
+  set -x
+}
+
+get_registry_bucket_path () {
+  # Outputs the fully specified URL to the registry to push/pull images to/from.
+  echo "${REGISTRY_HOSTNAME}/${REGISTRY_GCP_PROJECT}/$(get_kokoro_env)"
+}
+
+init_gcloud () {
+  # Check if gsutil is pre-installed on Kokoro windows container.
+  local gsutil_win="c:/gcloud-sdk/google-cloud-sdk/bin/gsutil.cmd"
+  if [[ -f "${gsutil_win}" ]]; then
+    export GSUTIL="${gsutil_win}"
+    return
+  fi
+
+  local gsutil="$(command -v gsutil)"
+
+  # Installs Google Cloud CLI if not already present.
+  if [[ ! -f "${gsutil}" ]]; then
+    apt-get update -y
+    apt-get install -y apt-transport-https ca-certificates gnupg
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
+      gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | \
+      tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+    apt-get update -y
+    apt-get install -y google-cloud-cli
+    rm -rf "/var/lib/apt/lists"/* "/tmp"/* "/var/tmp"/*
+    rm -rf "/var/lib"/{apt,dpkg,cache,log}
+
+    gsutil="$(command -v gsutil)"
+  fi
+
+  export GSUTIL="${gsutil}"
+}
+
+
+is_release_build () {
+  [[ "${KOKORO_ROOT_JOB_TYPE}" == "RELEASE" ]]
+}
+
+
+is_release_config () {
+  [[ "${RELEASE_CONFIGS}" =~ "${CONFIG}" ]]
+}
+
+
+run_gn () {
+  if [ $# -ne 3 ]; then
+    echo "Error: Exactly 3 arguments required!"
+    exit 1
+  fi
+
+  local out_dir=$1
+  local target_platform=$2
+  local gn_arguments=$3
+
+  set +u
+  echo 'Running GN...'
+  gn gen "${out_dir}" --args="target_platform=\"${target_platform}\" \
+    ${SB_API_VERSION} \
+    ${TARGET_OS} \
+    ${TARGET_CPU} \
+    ${gn_arguments} \
+    build_type=\"${CONFIG}\""
+  set -u
+  gn check "${out_dir}"
+}
+
+
+ninja_build () {
+  if [ $# -ne 2 ]; then
+    echo "Error: Exactly 2 arguments required!"
+    exit 1
+  fi
+
+  local out_dir=$1
+  local targets=("$2")
+
+  sccache -z
+
+  echo 'Running Ninja build...'
+  time ninja -C "${out_dir}" ${targets[@]}
+
+  sccache -s
+
+  echo "Generated build info"
+  build_info_file="${out_dir}/gen/build_info.json"
+  if [ -f $build_info_file ]; then
+    echo "$(<$build_info_file)"
+  else
+    echo "No build_info.json file was generated."
+  fi
+}
+
+
+upload_on_device_test_artifacts () {
+  if [[ $# -ne 4 ]]; then
+    echo "Error: Exactly 4 arguments required!"
+    exit 1
+  fi
+
+  local target_platform=$1
+  local platform=$2
+  local config=$3
+  local out_dir=$4
+
+  local artifact="${WORKSPACE_COBALT}/builder_tmp/${platform}_${config}/artifacts.tgz"
+  local gcs_archive_path="gs://$(get_project_name)-test-artifacts/${target_platform}/${KOKORO_GIT_COMMIT_src}/${KOKORO_ROOT_BUILD_ID}/${platform}_${config}/artifacts.tgz"
+
+  # TODO(b/294130306): Simple file filtering to reduce package sizes.
+  # Disable globbing to prevent the wildcard expansion in the patterns below.
+  set -f
+  # Include the test_infra patterns by default. This covers raspi and android.
+  # The flag can not be used with any of the platforms below as it replaces the
+  # patterns passed via the --pattern flag.
+  local pattern_arg="--test_infra"
+  if [[ "${platform}" == "ps5" ]]; then
+    pattern_arg='--patterns */ app_launcher/'
+  elif [[ "${platform}" == "xb1" ]]; then
+    pattern_arg='--patterns appx/ app_launcher/'
+  fi
+
+  time python3 "${WORKSPACE_COBALT}/tools/create_archive.py" \
+    ${pattern_arg} \
+    -s "${out_dir}" \
+    -d "${artifact}"
+  # Re-enable globbing.
+  set +f
+
+  init_gcloud
+
+  # Uploads test artifact.
+  time "${GSUTIL}" cp "${artifact}" "${gcs_archive_path}"
+
+  # Passes gcs path to on device test jobs.
+  echo -n "${gcs_archive_path}" > "${KOKORO_ARTIFACTS_DIR}/gcs_archive_path"
+}
+
+
+create_and_upload_nightly_archive () {
+  if [[ $# -ne 4 ]]; then
+    echo "Error: Exactly 4 arguments required!"
+    exit 1
+  fi
+
+  local platform=$1
+  local package_dir=$2
+  local local_archive_path=$3
+  local build_info_path=$4
+
+  local gcs_path_suffix="_nightly"
+  if [[ "${KOKORO_GOB_BRANCH_src}" != "COBALT" ]]; then
+    gcs_path_suffix="_${KOKORO_GOB_BRANCH_src}"
+  fi
+  local gcs_archive_path="gs://$(get_bucket_name)/${platform}${gcs_path_suffix}/$(date +%F)/${KOKORO_ROOT_BUILD_NUMBER}/"
+
+  # Creates nightly archive from package directory.
+  python3 "${WORKSPACE_COBALT}/tools/create_archive.py" \
+    -s "${package_dir}" \
+    -d "${local_archive_path}" \
+    --intermediate
+
+  init_gcloud
+
+  # Uploads nightly archive.
+  "${GSUTIL}" cp "${local_archive_path}" "${gcs_archive_path}"
+
+  # Uploads build_info.json.
+  "${GSUTIL}" cp "${build_info_path}" "${gcs_archive_path}"
+}
+
+run_package_release_pipeline () {
+  # NOTE: For DinD builds, we only run the GN and Ninja steps in the container.
+  # Artifacts and build-products from these steps are used in subsequent steps
+  # for packaging and nightly post-build tasks, and do not need to be run in the
+  # inner container environment (which has build tools and deps).
+
+  local out_dir="${WORKSPACE_COBALT}/out/${TARGET_PLATFORM}_${CONFIG}"
+
+  # Package and upload nightly release archive.
+  if is_release_build && is_release_config; then
+    # Setup package dir.
+    local package_dir="${WORKSPACE_COBALT}/package/${PLATFORM}_${CONFIG}"
+    mkdir -p "${package_dir}"
+
+    # TODO(b/294130306): Move build_info to gn packaging.
+    local build_info_path="${out_dir}/gen/build_info.json"
+    cp "${build_info_path}" "${package_dir}/"
+
+    # Create release package.
+    if [[ "${PLATFORM}" =~ "android" ]]; then
+      # Creates Android package directory.
+      python3 "${WORKSPACE_COBALT}/internal/kokoro/build/android/simple_packager.py" \
+        "${out_dir}" \
+        "${package_dir}" \
+        "${WORKSPACE_COBALT}"
+    elif [[ "${PLATFORM}" =~ "evergreen" ]]; then
+      local bootloader_out_dir=
+      if [ -n "${BOOTLOADER:-}" ]; then
+        bootloader_out_dir="${WORKSPACE_COBALT}/out/${BOOTLOADER}_${CONFIG}"
+      fi
+      # Creates Evergreen package directory.
+      python3 "${WORKSPACE_COBALT}/internal/kokoro/build/evergreen/simple_packager.py" \
+        "${out_dir}" \
+        "${package_dir}" \
+        "${bootloader_out_dir:-}"
+    else
+      # Sets package directory as out directory.
+      package_dir="${out_dir}"
+    fi
+
+    # Create and upload nightly archive.
+    create_and_upload_nightly_archive \
+      "${PLATFORM}" \
+      "${package_dir}" \
+      "${package_dir}.tar.gz" \
+      "${build_info_path}"
+  fi
+}
---
+++ cobalt/devinfra/kokoro/bin/dind_py/utils.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+#
+# Copyright 2024 The Cobalt Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Implements all DinD methods for running builds for Docker images or Cobalt
+"""
+
+import subprocess
+import logging
+import os
+
+# Environment Variables to fetch values for.
+_GIT_SHA_VAR = 'KOKORO_GIT_COMMIT_src'
+_REGISTRY_PATH_VAR = 'REGISTRY_PATH'
+_REGISTRY_IMAGE_VAR = 'REGISTRY_IMAGE_NAME'
+_PLATFORM_VAR = 'PLATFORM'
+_KOKORO_BUILD_NUM_VAR = 'KOKORO_BUILD_NUMBER'
+_WORKSPACE_COBALT_VAR = 'WORKSPACE_COBALT'
+
+# These values are used to determine Floating Tag parameters for images used in
+# Docker/Cobalt build steps.
+_BASE_BRANCH_VAR = 'KOKORO_GOB_BRANCH_src'
+_GERRIT_CHANGE_NUMBER_VAR = 'KOKORO_GERRIT_CHANGE_NUMBER_src'
+_GERRIT_PATCH_NUMBER_VAR = 'KOKORO_GERRIT_PATCHSET_NUMBER_src'
+_KOKORO_JOB_TYPE_VAR = 'KOKORO_ROOT_JOB_TYPE'
+
+
+class EnvArgs(object):
+  """
+  Simple struct to hold all relevant arguments that come from environment vars.
+  """
+  # The Git SHA for the current checked out HEAD of the Cobalt repo
+  git_sha = None
+
+  # Path to the registry and bucket which constitutes the fully-specified name:
+  # [REGISTRY_PATH]/[IMAGE_NAME]:[IMAGE_TAG]
+  registry_path = ''
+
+  # Name of the image which is part of the fully-specified name:
+  # [REGISTRY_PATH]/[IMAGE_NAME]:[IMAGE_TAG]
+  registry_img = ''
+
+  # Target platform to build Cobalt for
+  platform = None
+
+  # The cardinal number of the Kokoro job being run
+  build_num = None
+
+  # Path to the checkout of Cobalt repo's root
+  src_root = ''
+
+  # Branch name for the Cobalt checkout. This is one of the following:
+  #
+  # * "COBALT", which represents trunk or main
+  # * "NN.lts.1+", which is any numbered LTS release branch.
+  #
+  # This value is relevant for determining the floating tag that references the
+  # latest valid Docker image to use in postsubmit builds of Cobalt.
+  base_branch_name = ''
+
+  # This value is the cardinal number of the CL uploaded to Gerrit. This value
+  # is set when a change is uploaded to Gerrit for code review and uniquely
+  # identifies the change being tested. Additionally, all the PRs that are
+  # uploaded to Github are given a corresponding Gerrit CL.
+  #
+  # This value is used to set a floating tag for Docker images that can be
+  # reused for image build cache purposes when running presubmit tests.
+  gerrit_change_number = ''
+
+  # This value is the current iteration of the changelist uploaded to Gerrit.
+  # Whenever new changes are made to an existing CL, it will increment this
+  # value from 1 onwards. This is not used in the floating tag, but can help
+  # uniquely identify Kokoro jobs running on the same CL.
+  gerrit_change_patch = ''
+
+  # Whether the root job of the Kokoro job chain that triggered this build is a
+  # postsubmit card. This controls what type of floating tag is applied to any
+  # built docker image.
+  is_postsubmit = False
+
+  def __init__(self):
+    self.git_sha = os.environ[_GIT_SHA_VAR]
+    self.registry_path = os.environ[_REGISTRY_PATH_VAR]
+    self.platform = os.environ[_PLATFORM_VAR]
+
+    self.registry_img = os.environ.get(_REGISTRY_IMAGE_VAR)
+    if self.registry_img is None:
+      self.registry_img = f'cobalt-build-{self.platform}'
+
+    self.build_num = os.environ[_KOKORO_BUILD_NUM_VAR]
+    self.src_root = os.environ[_WORKSPACE_COBALT_VAR]
+    self.base_branch_name = os.environ.get(_BASE_BRANCH_VAR)
+    # Normalize LTS branch names: 24.lts.1+ => 24_lts
+    self.base_branch_name = self.base_branch_name.replace('.lts.1+', '_lts')
+    if self.base_branch_name is None:
+      raise ValueError('Branch name was not set in the environment.')
+    self.gerrit_change_number = int(
+        os.environ.get(_GERRIT_CHANGE_NUMBER_VAR, 0))
+    self.gerrit_change_patch = int(os.environ.get(_GERRIT_PATCH_NUMBER_VAR, 0))
+
+    job_type = os.environ.get(_KOKORO_JOB_TYPE_VAR, '')
+    self.is_postsubmit = job_type in ['CONTINUOUS_INTEGRATION', 'RELEASE']
+    if (not self.is_postsubmit) and (self.gerrit_change_number == 0 or
+                                     self.gerrit_change_patch == 0):
+      raise ValueError(
+          'Gerrit change number/patch not set in environment for presubmits.')
+
+
+def get_args_from_env():
+  """
+  Helper method to retrieve all relevant arguments from the kokoro environment.
+  """
+  return EnvArgs()
+
+
+def logging_info_spacer(msg):
+  """
+  Prints a message to the INFO channel with ASCII lines for emphasis.
+  """
+  msg_len = len(msg)
+  spacer = '*' * msg_len
+  logging.info(spacer)
+  logging.info(msg)
+  logging.info(spacer)
+
+
+def exec_cmd(command):
+  """
+  Helper method to run commands as string in the shell via subprocess.
+  """
+  try:
+    output = subprocess.check_output(command, shell=True, encoding='UTF-8')
+    # Leading newline inserted to ensure captured logs appear as if in stdout.
+    logging.info('%s\n%s', command, output)
+  except subprocess.CalledProcessError as e:
+    logging.error('%s\n%s', command, e.output)
+    raise e
+
+
+def set_logging_format():
+  """
+  Sets up standard config values for logging.
+  """
+  logging_format = ('[%(asctime)s:%(levelname)s:%(filename)s:%(lineno)s]' +
+                    ' ' + '%(message)s')
+  logging.basicConfig(
+      level=logging.INFO, format=logging_format, datefmt='%H:%M:%S')
