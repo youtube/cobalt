34382e9c56b119d785acbc001bec4497d5063694
Jason <jasonzh@google.com>
2024-11-18 11:50:10 -0800

    Support key system in HTMLMediaElement.canPlayType (#4429)
    b/378908084

---
+++ cobalt/android/apk/app/src/app/assets/html_media_element_extension.js
@@ -0,0 +1,8 @@
+/**
+ * @license
+ * Copyright The Cobalt Authors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+HTMLMediaElement.prototype.canPlayType = HTMLMediaElementExtension.canPlayType;
+console.log("HTMLMediaElement.canPlayType has been overwritten");
