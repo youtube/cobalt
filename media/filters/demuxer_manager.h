5fa7cae4f650309fe47ea429ea00355414c0d380
Bo-Rong Chen <borongchen@google.com>
2024-11-25 12:08:44 -0800

    [media] Support progressive playback (#4437)
    This PR brings the progressive demuxer from Cobalt C25 to support
    progressive videos.
    
    ProgressiveDemuxer is only enabled with `media_use_ffmpeg=false` and
    `use_starboard_media=true`, as Cobalt needs this for evergreen.
    
    We should re-enable unit-tests for ProgressDemuxer (b/380335617) after
    this PR.
    
    b/380285071
    
    original-hexsha: 110fd7d518f7291e8fa5392a0aef463d0333f56a

--- media/filters/demuxer_manager.h
+++ media/filters/demuxer_manager.h
@@ -173,6 +173,9 @@ class MEDIA_EXPORT DemuxerManager {
 
 #if BUILDFLAG(ENABLE_FFMPEG)
   std::unique_ptr<media::Demuxer> CreateFFmpegDemuxer();
+#elif BUILDFLAG(USE_STARBOARD_MEDIA)
+  std::unique_ptr<media::Demuxer> CreateDemuxerExtensionWrapper();
+  std::unique_ptr<media::Demuxer> CreateProgressiveDemuxer();
 #endif  // BUILDFLAG(ENABLE_FFMPEG)
 
 #if BUILDFLAG(ENABLE_HLS_DEMUXER)
