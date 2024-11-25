490d0d74bd00bf9074a1f091c6e826ad328b98aa
Bo-Rong Chen <borongchen@google.com>
2024-11-25 12:08:44 -0800

    [media] Support progressive playback (#4437)
    This PR brings the progressive demuxer from Cobalt C25 to support
    progressive videos.
    
    ProgressiveDemuxer is only enabled with `media_use_ffmpeg=false` and
    `use_starboard_media=true`, as Cobalt needs this for evergreen.
    
    We should re-enable unit-tests for ProgressDemuxer (b/380335617) after
    this PR.
    
    b/380285071
    
    original-hexsha: 110fd7d518f7291e8fa5392a0aef463d0333f56a

--- media/starboard/progressive/mock_data_source_reader.h
+++ media/starboard/progressive/mock_data_source_reader.h
@@ -1,26 +1,23 @@
-/*
- * Copyright 2012 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2012 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#ifndef COBALT_MEDIA_PROGRESSIVE_MOCK_DATA_SOURCE_READER_H_
-#define COBALT_MEDIA_PROGRESSIVE_MOCK_DATA_SOURCE_READER_H_
+#ifndef MEDIA_STARBOARD_PROGRESSIVE_MOCK_DATA_SOURCE_READER_H_
+#define MEDIA_STARBOARD_PROGRESSIVE_MOCK_DATA_SOURCE_READER_H_
 
-#include "cobalt/media/progressive/data_source_reader.h"
+#include "media/starboard/progressive/data_source_reader.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
-namespace cobalt {
 namespace media {
 
 class MockDataSourceReader : public DataSourceReader {
@@ -35,6 +32,5 @@ class MockDataSourceReader : public DataSourceReader {
 };
 
 }  // namespace media
-}  // namespace cobalt
 
-#endif  // COBALT_MEDIA_PROGRESSIVE_MOCK_DATA_SOURCE_READER_H_
+#endif  // MEDIA_STARBOARD_PROGRESSIVE_MOCK_DATA_SOURCE_READER_H_
