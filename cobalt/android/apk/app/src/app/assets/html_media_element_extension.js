66d7b7ed16a631242e1cd460f7a30312b79a9a51
Colin Liang <zhongqi.liang.4u@gmail.com>
2024-12-05 14:02:21 -0800

    JavaBridge Implementation of H5vccPlatformService (#4525)
    1. Fixed the race condition of injecting the java bridge object and
    loading the url from shell.
    2. The pattern to polyfill the embeded javascript in the APK binary
    before Kabuki loads won't work, see context on b/379731250, we will move
    the javascript file chrobalt_preload.js and its sub modules on to Kabuki
    codebase. I will create a backlog bug for it when this code is
    submitted, and work with Kabuki eng to integrate the code on Kabuki.
    3. A lot of H5vccPlatformService implementations are on google3. We had
    ClientLogInfo in the Cobalt repo, it did not cover all use cases
    H5vccPlatformService provides. I expand it to cover all use cases.
    
    b/379731250
    b/379184982
    b/378571210
    
    ---------
    
    Co-authored-by: Colin Liang <colinliang@google.com>
    original-hexsha: 399c7ca71fee5ea9b8b16891a43030895dee8e8b

--- cobalt/android/apk/app/src/app/assets/html_media_element_extension.js
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- * Copyright The Cobalt Authors.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-HTMLMediaElement.prototype.canPlayType = HTMLMediaElementExtension.canPlayType;
-console.log("HTMLMediaElement.canPlayType has been overwritten");
