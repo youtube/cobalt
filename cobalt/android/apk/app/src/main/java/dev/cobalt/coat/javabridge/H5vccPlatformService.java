399c7ca71fee5ea9b8b16891a43030895dee8e8b
Colin Liang <zhongqi.liang.4u@gmail.com>
2024-12-05 14:02:21 -0800

    JavaBridge Implementation of H5vccPlatformService (#4525)
    1. Fixed the race condition of injecting the java bridge object and
    loading the url from shell.
    2. The pattern to polyfill the embeded javascript in the APK binary
    before Kabuki loads won't work, see context on b/379731250, we will move
    the javascript file chrobalt_preload.js and its sub modules on to Kabuki
    codebase. I will create a backlog bug for it when this code is
    submitted, and work with Kabuki eng to integrate the code on Kabuki.
    3. A lot of H5vccPlatformService implementations are on google3. We had
    ClientLogInfo in the Cobalt repo, it did not cover all use cases
    H5vccPlatformService provides. I expand it to cover all use cases.
    
    b/379731250
    b/379184982
    b/378571210
    
    ---------
    
    Co-authored-by: Colin Liang <colinliang@google.com>

---
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/javabridge/H5vccPlatformService.java
@@ -0,0 +1,64 @@
+// Copyright 2024 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dev.cobalt.coat.javabridge;
+
+import android.content.Context;
+import android.util.Base64;
+import dev.cobalt.coat.StarboardBridge;
+
+/**
+ * H5vccPlatformService JavaScript object.
+ */
+public class H5vccPlatformService implements CobaltJavaScriptAndroidObject {
+
+    private final Context context;
+    private final StarboardBridge bridge;
+
+    public H5vccPlatformService(Context context, StarboardBridge bridge) {
+        this.context = context;
+        this.bridge = bridge;
+    }
+
+    @Override
+    public String getJavaScriptInterfaceName() {
+        return "Android_H5vccPlatformService";
+    }
+
+    @CobaltJavaScriptInterface
+    public boolean hasPlatformService(String servicename) {
+        return bridge.hasCobaltService(servicename);
+    }
+
+    @CobaltJavaScriptInterface
+    public void openPlatformService(long serviceId, String servicename) {
+        bridge.openCobaltService(serviceId, servicename);
+    }
+
+    @CobaltJavaScriptInterface
+    public void closePlatformService(String servicename) {
+        bridge.closeCobaltService(servicename);
+    }
+
+    @CobaltJavaScriptInterface
+    public String platformServiceSend(String servicename, String base64Data) {
+        byte[] data = Base64.decode(base64Data, Base64.DEFAULT);
+        byte[] result = bridge.sendToCobaltService(servicename, data);
+        if (result == null) {
+            return "";
+        }
+
+        return Base64.encodeToString(result, Base64.DEFAULT);
+    }
+}
