011602a95ded32ccedc08d940716581b8bfe6ce9
Austin Osagie <austinosagie@google.com>
2024-12-27 15:07:35 -0800

    Update check for progressive formats (#4624)
    Updates the separator used to parse codecs listed under the "codecs"
    parameter. Previously, `IsProgressiveFormat()` would reject content
    types that had no whitespace between codecs in the "codecs" paramter,
    but were otherwise valid.
    
    b/382791540
    
    original-hexsha: 7fce4a79c230895e7a437174ea235eda0248768e

--- third_party/blink/renderer/core/html/media/html_media_element_test.cc
+++ third_party/blink/renderer/core/html/media/html_media_element_test.cc
@@ -2205,6 +2205,8 @@ TEST_P(HTMLMediaElementTest,
 TEST(HTMLMediaElementTest, CanHandleCobaltProgressiveSupportQueries) {
   const ContentType progressive_type(
       "video/mp4; codecs=\"avc1.42001E, mp4a.40.2\"");
+  const ContentType progressive_type_missing_whitespace(
+      "video/mp4; codecs=\"avc1.42001E,mp4a.40.2\"");
   const ContentType adaptive_video_type(
       "video/mp4; codecs=\"avc1.4d4015\"; framerate=30");
   const ContentType adaptive_audio_type(
@@ -2217,6 +2219,11 @@ TEST(HTMLMediaElementTest, CanHandleCobaltProgressiveSupportQueries) {
     // disabled.
     EXPECT_EQ(HTMLMediaElement::GetSupportsType(progressive_type),
               MIMETypeRegistry::kNotSupported);
+    // Reject progressive content types when the "codecs" parameter lacks
+    // whitespace.
+    EXPECT_EQ(
+        HTMLMediaElement::GetSupportsType(progressive_type_missing_whitespace),
+        MIMETypeRegistry::kNotSupported);
     // Continue to support adaptive content types.
     EXPECT_NE(HTMLMediaElement::GetSupportsType(adaptive_video_type),
               MIMETypeRegistry::kNotSupported);
@@ -2230,6 +2237,11 @@ TEST(HTMLMediaElementTest, CanHandleCobaltProgressiveSupportQueries) {
     // enabled.
     EXPECT_NE(HTMLMediaElement::GetSupportsType(progressive_type),
               MIMETypeRegistry::kNotSupported);
+    // Continue to accept progressive content types when the "codecs" parameter
+    // lacks whitespace.
+    EXPECT_NE(
+        HTMLMediaElement::GetSupportsType(progressive_type_missing_whitespace),
+        MIMETypeRegistry::kNotSupported);
     // Continue to support adaptive content types.
     EXPECT_NE(HTMLMediaElement::GetSupportsType(adaptive_video_type),
               MIMETypeRegistry::kNotSupported);
