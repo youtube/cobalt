84602675dbb16c59ee29ad27cdf4b2e2e013923b
xiaomings <xiaomings@google.com>
2025-01-03 13:35:17 -0800

    [media] Move concrete SbAudioSink impl to namespace (#4634)
    SbAudioSink is a typedef to SbAudioSinkPrivate*, and the concrete
    implementation of SbAudioSinkPrivate was in the global namespace. Now
    it's moved into a new class named SbAudioSinkImpl in the namespace
    starboard::shared::starboard::audio_sink, and it's derived from
    SbAudioSinkPrivate, which is now an interface.
    
    This allows to have multiple SbAudioSink implementations in different
    namespaces.
    
    b/327287075
    original-hexsha: a6d7996af37e3df5e838e0bd3842c3d5b81a8002

--- starboard/shared/starboard/audio_sink/audio_sink_create.cc
+++ starboard/shared/starboard/audio_sink/audio_sink_create.cc
@@ -27,7 +27,7 @@ SbAudioSink SbAudioSinkCreate(
     SbAudioSinkUpdateSourceStatusFunc update_source_status_func,
     SbAudioSinkConsumeFramesFunc consume_frames_func,
     void* context) {
-  return SbAudioSinkPrivate::Create(
+  return ::starboard::shared::starboard::audio_sink::SbAudioSinkImpl::Create(
       channels, sampling_frequency_hz, audio_sample_type,
       audio_frame_storage_type, frame_buffers, frame_buffers_size_in_frames,
       update_source_status_func, consume_frames_func, NULL /*error_func*/,
