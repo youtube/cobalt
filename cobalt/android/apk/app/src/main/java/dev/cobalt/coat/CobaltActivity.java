5d3a5d003c5c7105bcb6e4fc20ed2f6b7910b861
Bo-Rong Chen <borongchen@google.com>
2025-01-05 20:23:17 -0800

    [android] Enable low end device mode (#4625)
    1. Enable low end device mode to reduce memory usage.
    2. The rendering issue was due to the memory limits in Blink, which is
    resolved in m126+, where two upstream commits
    (https://github.com/youtube/cobalt/pull/4636,
    https://github.com/youtube/cobalt/pull/4637) were cherry picked.
    
    b/380310632
    original-hexsha: 173ecc39274e037981eda5416fb216a4a7d901f4

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
@@ -104,11 +104,8 @@ public abstract class CobaltActivity extends Activity {
     if (!CommandLine.isInitialized()) {
       ((CobaltApplication) getApplication()).initCommandLine();
 
-      // Note that appendSwitchesAndArguments excludes cobaltCommandLineParams[0]
-      // as the program name, and all other arguments SHOULD start with '--'.
       String[] cobaltCommandLineParams =
           new String[] {
-            "",
             // Disable first run experience.
             "--disable-fre",
             // Disable user prompts in the first run.
@@ -125,6 +122,8 @@ public abstract class CobaltActivity extends Activity {
             "--enable-features=LogJsConsoleMessages",
             // Disable rescaling Webpage.
             "--force-device-scale-factor=1",
+            // Enable low end device mode.
+            "--enable-low-end-device-mode",
           };
       CommandLine.getInstance().appendSwitchesAndArguments(cobaltCommandLineParams);
 
