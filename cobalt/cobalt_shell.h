38a53f2a94e7925261aafbc9a1fe0367aeccc367
Kaido Kert <kaidokert@google.com>
2025-01-07 12:09:45 -0800

    Make shell overridden
    b/384979128

--- cobalt/cobalt_shell.h
+++ cobalt/cobalt_shell.h
@@ -1,12 +1,32 @@
 #ifndef COBALT_COBALT_SHELL_H
 #define COBALT_COBALT_SHELL_H
 
+#include "content/public/browser/browser_context.h"
+#include "content/public/browser/web_contents.h"
 #include "content/shell/browser/shell.h"
 
 namespace cobalt {
 
 class CobaltShell : public content::Shell {
-  // WebContentsObserver
+ public:
+  // Override content::Shell entry point
+  static content::Shell* CreateNewWindow(
+      content::BrowserContext* browser_context,
+      const GURL& url,
+      const scoped_refptr<content::SiteInstance>& site_instance,
+      const gfx::Size& initial_size);
+
+ private:
+  CobaltShell(std::unique_ptr<content::WebContents> web_contents,
+              bool should_set_delegate);
+
+  // Overridden from content::Shell
+  static content::Shell* CreateShell(
+      std::unique_ptr<content::WebContents> web_contents,
+      const gfx::Size& initial_size,
+      bool should_set_delegate);
+
+  // WebContentsObserver interface
   void PrimaryMainDocumentElementAvailable() override;
 };
 
