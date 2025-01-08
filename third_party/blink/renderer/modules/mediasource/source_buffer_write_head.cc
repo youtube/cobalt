78d0ab5962d5b04f8a5b07457da9af97c813333c
Austin Osagie <austinosagie@google.com>
2025-01-07 16:50:39 -0800

    Implement SourceBuffer.writeHead attribute (#4559)
    This change implements a partial interface for SourceBuffer, adding the
    writeHead attribute. This attribute returns the highest buffer
    presentation timestamp written to the Renderer. The writeHead attribute
    is only included in Cobalt Starboard media builds.
    
    Link to the original PR with most of the functional changes:
    https://github.com/youtube/cobalt/pull/121
    
    b/326652276
    
    original-hexsha: 7a689b653b6e8df23ab179520a797eee827959b3

---
+++ third_party/blink/renderer/modules/mediasource/source_buffer_write_head.cc
@@ -0,0 +1,31 @@
+// Copyright 2024 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "third_party/blink/renderer/modules/mediasource/source_buffer_write_head.h"
+
+#include "third_party/blink/renderer/modules/mediasource/source_buffer.h"
+#include "third_party/blink/renderer/platform/bindings/exception_state.h"
+
+#if !BUILDFLAG(USE_STARBOARD_MEDIA)
+#error "This file only works with Starboard media"
+#endif  // !BUILDFLAG(USE_STARBOARD_MEDIA)
+
+namespace blink {
+
+// static
+double SourceBufferWriteHead::writeHead(SourceBuffer& source_buffer,
+                                        ExceptionState& exception_state) {
+  return source_buffer.GetWriteHead(exception_state);
+}
+}  // namespace blink
---
+++ third_party/blink/renderer/modules/mediasource/source_buffer_write_head.h
@@ -0,0 +1,39 @@
+// Copyright 2024 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASOURCE_SOURCE_BUFFER_WRITE_HEAD_H_
+#define THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASOURCE_SOURCE_BUFFER_WRITE_HEAD_H_
+
+#include "build/build_config.h"
+#include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
+
+#if !BUILDFLAG(USE_STARBOARD_MEDIA)
+#error "This file only works with Starboard media"
+#endif  // !BUILDFLAG(USE_STARBOARD_MEDIA)
+
+namespace blink {
+
+class ExceptionState;
+class SourceBuffer;
+
+class SourceBufferWriteHead {
+  STATIC_ONLY(SourceBufferWriteHead);
+
+ public:
+  static double writeHead(SourceBuffer&, ExceptionState&);
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASOURCE_SOURCE_BUFFER_WRITE_HEAD_H_
