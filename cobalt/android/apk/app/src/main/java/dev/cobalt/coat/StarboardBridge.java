7b77f705afe533f4f5269dcb8892e53846fcfb2e
Hao <131711973+haozheng-cobalt@users.noreply.github.com>
2025-01-07 18:23:52 -0800

    Migrate StarboardBridge JNI functions in android_main.cc (#4632)
    Remove @UsedByNative annotations for beforeStartOrResume and
    beforeSuspend, for they are no longer needed in ApplicationAndroid.
    Reference b/377042903.
    
    Remove getLocalInterfaceAddressAndNetmask and isCurrentNetworkWireless,
    for it's not used in Chrobalt.
    
    Remove @UsedByNative annotations for getDisplaySize, for it's never used
    in native code.
    
    Change UA related fields getIsAmatiDevice, getBuildFingerprint,
    getPlayServicesVersion to @CalledByNative, we are currently not using
    them in UserAgentPlatformInfo::ToString(), but might need them for
    cobalt/browser/client_hint_headers.cc in the future.
    
    Migrate the rest of JNI calls in
    starboard/android/shared/android_main.cc to Chromium standard JNI,
    When testing locally:
    startDeepLink is set with empty string.
    GetArgs retrieves url.
    01-02 22:06:07.303 13993 13993 I starboard:
    [dev.cobalt.coat/13993:0103/060607.300538(UTC):INFO:android_main.cc(76)]
    Test GetArgs: android_main--url=https://www.youtube.com/tv
    
    b/372559388
    
    original-hexsha: 54f9c572a1f6a41e73dc4a17d54d339104f298d0

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/StarboardBridge.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/StarboardBridge.java
@@ -26,11 +26,7 @@ import android.content.pm.PackageManager;
 import android.hardware.input.InputManager;
 import android.media.AudioDeviceInfo;
 import android.media.AudioManager;
-import android.net.ConnectivityManager;
-import android.net.Network;
-import android.net.NetworkCapabilities;
 import android.os.Build;
-import android.util.Pair;
 import android.util.Size;
 import android.util.SizeF;
 import android.view.Display;
@@ -44,11 +40,7 @@ import dev.cobalt.util.Holder;
 import dev.cobalt.util.Log;
 import dev.cobalt.util.UsedByNative;
 import java.lang.reflect.Method;
-import java.net.InterfaceAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
 import java.util.Calendar;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -200,8 +192,6 @@ public class StarboardBridge {
     }
   }
 
-  @SuppressWarnings("unused")
-  @UsedByNative
   protected void beforeStartOrResume() {
     Log.i(TAG, "Prepare to resume");
     // Bring our platform services to life before resuming so that they're ready to deal with
@@ -215,8 +205,6 @@ public class StarboardBridge {
     advertisingId.refresh();
   }
 
-  @SuppressWarnings("unused")
-  @UsedByNative
   protected void beforeSuspend() {
     try {
       Log.i(TAG, "Prepare to suspend");
@@ -242,7 +230,7 @@ public class StarboardBridge {
 
   // Warning: "Stopped" refers to Starboard "Stopped" event, it's different from Android's "onStop".
   @SuppressWarnings("unused")
-  @UsedByNative
+  @CalledByNative
   protected void afterStopped() {
     applicationStopped = true;
     closeAllServices();
@@ -271,6 +259,8 @@ public class StarboardBridge {
     applicationStopped = true;
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/system_request_conceal.cc
   @SuppressWarnings("unused")
   @UsedByNative
   public void requestSuspend() {
@@ -291,6 +281,7 @@ public class StarboardBridge {
 
   // private native boolean nativeOnSearchRequested();
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
   @SuppressWarnings("unused")
   @UsedByNative
   public Context getApplicationContext() {
@@ -300,6 +291,8 @@ public class StarboardBridge {
     return appContext;
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/system_platform_error.cc
   @SuppressWarnings("unused")
   @UsedByNative
   void raisePlatformError(@PlatformError.ErrorType int errorType, long data) {
@@ -321,7 +314,7 @@ public class StarboardBridge {
   }
 
   @SuppressWarnings("unused")
-  @UsedByNative
+  @CalledByNative
   protected String[] getArgs() {
     if (args == null) {
       throw new IllegalArgumentException("args cannot be null");
@@ -331,7 +324,7 @@ public class StarboardBridge {
 
   /** Returns the URL from the Intent that started the app. */
   @SuppressWarnings("unused")
-  @UsedByNative
+  @CalledByNative
   protected String getStartDeepLink() {
     if (startDeepLink == null) {
       throw new IllegalArgumentException("startDeepLink cannot be null");
@@ -354,6 +347,8 @@ public class StarboardBridge {
     // TODO(b/374147993): Implement deep link
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/file_internal.cc
   /**
    * Returns the absolute path to the directory where application specific files should be written.
    * May be overridden for use cases that need to segregate storage.
@@ -364,6 +359,8 @@ public class StarboardBridge {
     return appContext.getFilesDir().getAbsolutePath();
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/file_internal.cc
   /**
    * Returns the absolute path to the application specific cache directory on the filesystem. May be
    * overridden for use cases that need to segregate storage.
@@ -374,62 +371,8 @@ public class StarboardBridge {
     return appContext.getCacheDir().getAbsolutePath();
   }
 
-  /**
-   * Returns non-loopback network interface address and its netmask, or null if none.
-   *
-   * <p>A Java function to help implement Starboard's SbSocketGetLocalInterfaceAddress.
-   */
-  @SuppressWarnings("unused")
-  @UsedByNative
-  // TODO: (cobalt b/372559388) Migrate complicated returned type functions to JNI zero.
-  // The @UsedByNative annotation has strict signature parsing rules,
-  // and Pair<byte[], byte[]> is not be supported well.
-  Pair<byte[], byte[]> getLocalInterfaceAddressAndNetmask(boolean wantIPv6) {
-    try {
-      Enumeration<NetworkInterface> it = NetworkInterface.getNetworkInterfaces();
-
-      while (it.hasMoreElements()) {
-        NetworkInterface ni = it.nextElement();
-        if (ni.isLoopback()) {
-          continue;
-        }
-        if (!ni.isUp()) {
-          continue;
-        }
-        if (ni.isPointToPoint()) {
-          continue;
-        }
-
-        for (InterfaceAddress ia : ni.getInterfaceAddresses()) {
-          byte[] address = ia.getAddress().getAddress();
-          boolean isIPv6 = (address.length > 4);
-          if (isIPv6 == wantIPv6) {
-            // Convert the network prefix length to a network mask.
-            int prefix = ia.getNetworkPrefixLength();
-            byte[] netmask = new byte[address.length];
-            for (int i = 0; i < netmask.length; i++) {
-              if (prefix == 0) {
-                netmask[i] = 0;
-              } else if (prefix >= 8) {
-                netmask[i] = (byte) 0xFF;
-                prefix -= 8;
-              } else {
-                netmask[i] = (byte) (0xFF << (8 - prefix));
-                prefix = 0;
-              }
-            }
-            return new Pair<>(address, netmask);
-          }
-        }
-      }
-    } catch (SocketException ex) {
-      // TODO should we have a logging story that strips logs for production?
-      Log.w(TAG, "sbSocketGetLocalInterfaceAddress exception", ex);
-      return null;
-    }
-    return null;
-  }
-
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/speech_synthesis_speak.cc
   @SuppressWarnings("unused")
   @UsedByNative
   CobaltTextToSpeechHelper getTextToSpeechHelper() {
@@ -439,6 +382,8 @@ public class StarboardBridge {
     return ttsHelper;
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/accessibility_get_caption_settings.cc
   /**
    * @return A new CaptionSettings object with the current system caption settings.
    */
@@ -450,6 +395,8 @@ public class StarboardBridge {
     return new CaptionSettings(cm);
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/system_get_locale_id.cc
   /** Java-layer implementation of SbSystemGetLocaleId. */
   @SuppressWarnings("unused")
   @UsedByNative
@@ -457,6 +404,8 @@ public class StarboardBridge {
     return Locale.getDefault().toLanguageTag();
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/time_zone_get_name.cc
   @SuppressWarnings("unused")
   @UsedByNative
   String getTimeZoneId() {
@@ -469,18 +418,19 @@ public class StarboardBridge {
     return timeZone.getID();
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/window_get_diagonal_size_in_inches.cc
   @SuppressWarnings("unused")
   @UsedByNative
   SizeF getDisplayDpi() {
     return DisplayUtil.getDisplayDpi();
   }
 
-  @SuppressWarnings("unused")
-  @UsedByNative
   Size getDisplaySize() {
     return DisplayUtil.getSystemDisplaySize();
   }
 
+  // TODO: (cobalt b/372559388) migrate JNI.
   @SuppressWarnings("unused")
   @UsedByNative
   public ResourceOverlay getResourceOverlay() {
@@ -503,6 +453,8 @@ public class StarboardBridge {
     }
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/window_get_size.cc
   @SuppressWarnings("unused")
   @UsedByNative
   Size getDeviceResolution() {
@@ -527,6 +479,8 @@ public class StarboardBridge {
     }
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/system_network_is_disconnected.cc
   @SuppressWarnings("unused")
   @UsedByNative
   boolean isNetworkConnected() {
@@ -536,6 +490,8 @@ public class StarboardBridge {
     return networkStatus.isConnected();
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/microphone_impl.cc
   /**
    * Checks if there is no microphone connected to the system.
    *
@@ -565,6 +521,8 @@ public class StarboardBridge {
     return true;
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/microphone_impl.cc
   /**
    * Checks if the microphone is muted.
    *
@@ -577,26 +535,8 @@ public class StarboardBridge {
     return audioManager.isMicrophoneMute();
   }
 
-  /**
-   * @return true if we have an active network connection and it's on an wireless network.
-   */
-  @SuppressWarnings("unused")
-  @UsedByNative
-  boolean isCurrentNetworkWireless() {
-    ConnectivityManager connMgr =
-        (ConnectivityManager) appContext.getSystemService(Context.CONNECTIVITY_SERVICE);
-    Network activeNetwork = connMgr.getActiveNetwork();
-    if (activeNetwork == null) {
-      return false;
-    }
-    NetworkCapabilities activeCapabilities = connMgr.getNetworkCapabilities(activeNetwork);
-    if (activeCapabilities == null) {
-      return false;
-    }
-    // Consider anything that's not definitely wired to be wireless.
-    return !activeCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET);
-  }
-
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/accessibility_get_display_settings.cc
   /**
    * @return true if the user has enabled accessibility high contrast text in the operating system.
    */
@@ -615,6 +555,8 @@ public class StarboardBridge {
     }
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/android_media_session_client.cc
   @SuppressWarnings("unused")
   @UsedByNative
   void updateMediaSession(
@@ -634,6 +576,8 @@ public class StarboardBridge {
     //     playbackState, actions, positionMs, speed, title, artist, album, artwork, duration);
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/android_media_session_client.cc
   @SuppressWarnings("unused")
   @UsedByNative
   public void deactivateMediaSession() {
@@ -642,6 +586,8 @@ public class StarboardBridge {
     // cobaltMediaSession.deactivateMediaSession();
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/system_get_property.cc
   /** Returns string for kSbSystemPropertyUserAgentAuxField */
   @SuppressWarnings("unused")
   @UsedByNative
@@ -670,6 +616,8 @@ public class StarboardBridge {
     return sb.toString();
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/system_get_property.cc
   /** Returns string for kSbSystemPropertyAdvertisingId */
   @SuppressWarnings("unused")
   @UsedByNative
@@ -677,6 +625,8 @@ public class StarboardBridge {
     return this.advertisingId.getId();
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/system_get_property.cc
   /** Returns boolean for kSbSystemPropertyLimitAdTracking */
   @SuppressWarnings("unused")
   @UsedByNative
@@ -684,6 +634,8 @@ public class StarboardBridge {
     return this.advertisingId.isLimitAdTrackingEnabled();
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/audio_track_bridge.cc
   @SuppressWarnings("unused")
   @UsedByNative
   AudioOutputManager getAudioOutputManager() {
@@ -704,6 +656,8 @@ public class StarboardBridge {
   //   audioPermissionRequester.onRequestPermissionsResult(requestCode, permissions, grantResults);
   // }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/video_window.cc
   @SuppressWarnings("unused")
   @UsedByNative
   public void resetVideoSurface() {
@@ -713,6 +667,8 @@ public class StarboardBridge {
     }
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/player_set_bounds.cc
   @SuppressWarnings("unused")
   @UsedByNative
   public void setVideoSurfaceBounds(final int x, final int y, final int width, final int height) {
@@ -722,6 +678,8 @@ public class StarboardBridge {
     }
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/media_capabilities_cache.cc
   /** Return supported hdr types. */
   @SuppressWarnings("unused")
   @UsedByNative
@@ -800,6 +758,7 @@ public class StarboardBridge {
     return response.data;
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
   /** Returns the application start timestamp. */
   @SuppressWarnings("unused")
   @CalledByNative
@@ -815,6 +774,8 @@ public class StarboardBridge {
     return 0;
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/graphics.cc
   @SuppressWarnings("unused")
   @UsedByNative
   void reportFullyDrawn() {
@@ -824,6 +785,8 @@ public class StarboardBridge {
     }
   }
 
+  // TODO: (cobalt b/372559388) remove or migrate JNI?
+  // Used in starboard/android/shared/crash_handler.cc
   @SuppressWarnings("unused")
   @UsedByNative
   public void setCrashContext(String key, String value) {
@@ -843,19 +806,19 @@ public class StarboardBridge {
   }
 
   @SuppressWarnings("unused")
-  @UsedByNative
+  @CalledByNative
   protected boolean getIsAmatiDevice() {
     return this.isAmatiDevice;
   }
 
   @SuppressWarnings("unused")
-  @UsedByNative
+  @CalledByNative
   protected String getBuildFingerprint() {
     return Build.FINGERPRINT;
   }
 
   @SuppressWarnings("unused")
-  @UsedByNative
+  @CalledByNative
   protected long getPlayServicesVersion() {
     try {
       if (android.os.Build.VERSION.SDK_INT < 28) {
