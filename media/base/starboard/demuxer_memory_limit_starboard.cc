1914d23ee9771bad5a36672ce271a54a9cefa606
Xiaoming Shi <xiaomings@google.com>
2025-01-08 16:48:47 -0800

    [media] Refine DecoderBufferAllocator memory budget
    Now SbMedia budget functions are properly respected when deciding
    MediaSource buffer memory limits.
    
    DecoderBuffer::Allocator interface has been refined to remove SbMedia
    types, and all Starboard dependency is resolved inside the concrete
    DecoderBufferAllocator implementation in //media/starboard.
    
    The logic to determine whether a video is 10 bits has been refined, as
    the video mime type is no longer available.
    
    b/322027866

--- media/base/starboard/demuxer_memory_limit_starboard.cc
+++ media/base/starboard/demuxer_memory_limit_starboard.cc
@@ -12,34 +12,72 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#if !defined(STARBOARD)
-#error "This file only works with Cobalt/Starboard."
-#endif  // !defined(STARBOARD)
-
 #include "media/base/demuxer_memory_limit.h"
 
 #include "base/logging.h"
+#include "build/build_config.h"
 #include "media/base/decoder_buffer.h"
-#include "media/base/starboard_utils.h"
 #include "media/base/video_codecs.h"
 
+#if !BUILDFLAG(USE_STARBOARD_MEDIA)
+#error "This file only works with Starboard media."
+#endif  // !BUILDFLAG(USE_STARBOARD_MEDIA)
+
 namespace media {
+namespace {
+
+int GetBitsPerPixel(const VideoDecoderConfig& video_config) {
+  bool is_hdr = false;
+
+  if (video_config.codec() == VideoCodec::kVP9) {
+    if (video_config.profile() == VP9PROFILE_PROFILE2 ||
+        video_config.profile() == VP9PROFILE_PROFILE3) {
+      is_hdr = true;
+    }
+  } else if (video_config.codec() == VideoCodec::kAV1) {
+    const VideoColorSpace& color_space = video_config.color_space_info();
+
+    if (color_space.primaries >= VideoColorSpace::PrimaryID::BT2020 ||
+        color_space.transfer >= VideoColorSpace::TransferID::BT2020_10) {
+      is_hdr = true;
+    }
+  }
+
+  int bits = is_hdr ? 10 : 8;
+
+  LOG(INFO) << "Assume " << bits << " bits for "
+            << video_config.AsHumanReadableString();
+
+  return bits;
+}
+
+}  // namespace
 
 size_t GetDemuxerStreamAudioMemoryLimit(
     const AudioDecoderConfig* /*audio_config*/) {
   return DecoderBuffer::Allocator::GetInstance()->GetAudioBufferBudget();
 }
 
-size_t GetDemuxerStreamVideoMemoryLimit(Demuxer::DemuxerTypes /*demuxer_type*/,
-                                        const VideoDecoderConfig* video_config,
-                                        const std::string& mime_type) {
-  return static_cast<size_t>(
-      GetSbMediaVideoBufferBudget(video_config, mime_type));
+size_t GetDemuxerStreamVideoMemoryLimit(
+    Demuxer::DemuxerTypes /*demuxer_type*/,
+    const VideoDecoderConfig* video_config) {
+  if (!video_config) {
+    return DecoderBuffer::Allocator::GetInstance()->GetVideoBufferBudget(
+        VideoCodec::kH264, 1920, 1080, 8);
+  }
+
+  auto codec = video_config->codec();
+  auto width = video_config->visible_rect().size().width();
+  auto height = video_config->visible_rect().size().height();
+  auto bits_per_pixel = GetBitsPerPixel(*video_config);
+
+  return DecoderBuffer::Allocator::GetInstance()->GetVideoBufferBudget(
+      codec, width, height, bits_per_pixel);
 }
 
 size_t GetDemuxerMemoryLimit(Demuxer::DemuxerTypes demuxer_type) {
   return GetDemuxerStreamAudioMemoryLimit(nullptr) +
-         GetDemuxerStreamVideoMemoryLimit(demuxer_type, nullptr, "");
+         GetDemuxerStreamVideoMemoryLimit(demuxer_type, nullptr);
 }
 
 }  // namespace media
