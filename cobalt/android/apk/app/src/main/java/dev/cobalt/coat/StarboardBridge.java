ee6cc7708927b73d28e17738308fa7f3cfd7ad6b
Hao <131711973+haozheng-cobalt@users.noreply.github.com>
2025-01-09 15:42:58 -0800

    [JNI Example] Call native in Java (#4657)
    This PR serves as an example of how we should migrate Java native API to
    Chromium JNI standard.
    
    Local test:
    Simply ensure the native API is invoked.
    haozheng@haozheng:~/chromium/src$ adb logcat | grep JNI
    01-08 14:56:20.158 13226 13226 I starboard:
    [dev.cobalt.coat/13226:0108/225620.158117(UTC):INFO:starboard_bridge.cc(93)]
    JNI: StartNativeStarboard: 0xec140a60
    
    Explanation:
    Replace
    Java_dev_cobalt_coat_StarboardBridge_startNativeStarboard(JniEnvExt*
    env) with JNI_StarboardBridge_StartNativeStarboard(JNIEnv* env).
    Move implementation over to starboard_bridge.cc because we should only
    include the _jni header from a single .cc file as the header defines
    functions, to avoid duplicate symbols issue.
    
    b/372559388
    
    original-hexsha: 632168a5d5be07479ca9acc5b1d89b791ed2836d

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/StarboardBridge.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/StarboardBridge.java
@@ -131,12 +131,10 @@ public class StarboardBridge {
     this.volumeStateReceiver = new VolumeStateReceiver(appContext);
     this.isAmatiDevice = appContext.getPackageManager().hasSystemFeature(AMATI_EXPERIENCE_FEATURE);
 
-    nativeApp = startNativeStarboard();
+    nativeApp = StarboardBridgeJni.get().startNativeStarboard();
   }
 
   private native boolean initJNI();
-
-  private native long startNativeStarboard();
 
   private native void closeNativeStarboard(long nativeApp);
 
@@ -146,10 +144,9 @@ public class StarboardBridge {
 
         long currentMonotonicTime();
 
+        long startNativeStarboard();
         // TODO(cobalt, b/372559388): move below native methods to the Natives interface.
         // boolean initJNI();
-
-        // long startNativeStarboard();
 
         // void closeNativeStarboard(long nativeApp);
     }
