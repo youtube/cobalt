a48b082ba662b9bd0f68262dfd7d9e966485483f
thorsten sideb0ard <sideboard@google.com>
2025-01-13 11:36:19 -0800

    Allow WebAudio FIFO size to match callback buffer if it is very large (#4676)
    b/388556893
    
    Allow WebAudio FIFO size to match callback buffer if it is very large
    
    The Web Audio FIFO is over-allocated to allow for growing if there are
    buffer xruns. However, on some Android devices there is a very large
    callback buffer which will fail a CHECK. Just match the callback buffer
    size in this case, to avoid crashing.
    
    This change is backported from m123+.
    
    (cherry picked from commit 01cb952243ee9efca53b66f50d3026aeb5443049)
    
    Bug: 324797343
    Reviewed-on:
    https://chromium-review.googlesource.com/c/chromium/src/+/5293168
    
    Co-authored-by: Michael Wilson <mjwilson@chromium.org>
    
    original-hexsha: 208fd2857f47ce2769abf26697f50bbf5cd93984

--- third_party/blink/renderer/platform/audio/audio_destination.cc
+++ third_party/blink/renderer/platform/audio/audio_destination.cc
@@ -360,9 +360,10 @@ AudioDestination::AudioDestination(
           context_sample_rate.has_value()
               ? context_sample_rate.value()
               : (web_audio_device_ ? web_audio_device_->SampleRate() : 0)),
-      fifo_(std::make_unique<PushPullFIFO>(number_of_output_channels,
-                                           kFIFOSize,
-                                           render_quantum_frames)),
+      fifo_(std::make_unique<PushPullFIFO>(
+          number_of_output_channels,
+          std::max(kFIFOSize, callback_buffer_size_ + render_quantum_frames),
+          render_quantum_frames)),
       output_bus_(AudioBus::Create(number_of_output_channels,
                                    render_quantum_frames,
                                    false)),
@@ -402,9 +403,6 @@ AudioDestination::AudioDestination(
       fifo_->Push(render_bus_.get());
     }
   }
-
-  // Check if the requested buffer size is too large.
-  DCHECK_LE(callback_buffer_size_ + render_quantum_frames, kFIFOSize);
 
   double scale_factor = 1.0;
 
