603ec5377366d296e58808e59884803612611028
johnx <johnx@google.com>
2025-01-14 20:58:54 +0000

    CoAT changes to support Kimono build (#4506)
    1. Removed most java dependencies on content/shell/android/ resources;
    2. Support dynamically adding "cobalt." java package prefix when
    invoking JNI;
    3. Avoid using hash names in JNI templates.
    
    go/kimono-chrobalt-build
    b/372559266
    
    Changes to cherry pick needed after
    https://chromium-review.googlesource.com/c/chromium/src/+/5234469.
    
    (cherry picked from commit 5e6a0d9502d2ad7c7a7e74210002783865c1a42f)

--- third_party/jni_zero/jni_zero.cc
+++ third_party/jni_zero/jni_zero.cc
@@ -25,9 +25,52 @@ jclass (*g_class_resolver)(JNIEnv*, const char*, const char*) = nullptr;
 
 void (*g_exception_handler_callback)(JNIEnv*) = nullptr;
 
+/* Cobalt specific hack to move Java classes to a custom namespace.
+   For every class org.chromium.foo moves them to cobalt.org.chromium.foo
+   This works around link-time conflicts when building the final
+   package against other Chromium release artifacts. */
+#if BUILDFLAG(IS_COBALT)
+const char* COBALT_ORG_CHROMIUM = "cobalt/org/chromium";
+const char* ORG_CHROMIUM = "org/chromium";
+
+bool g_add_cobalt_prefix = false;
+std::atomic<bool> g_checked_command_line(false);
+
+std::string getRepackagedName(const char* signature) {
+  std::string holder(signature);
+  size_t pos = 0;
+  while ((pos = holder.find(ORG_CHROMIUM, pos)) != std::string::npos) {
+    holder.replace(pos, strlen(ORG_CHROMIUM), COBALT_ORG_CHROMIUM);
+    pos += strlen(COBALT_ORG_CHROMIUM);
+  }
+  return holder;
+}
+
+bool shouldAddCobaltPrefix() {
+  if (!g_checked_command_line && base::CommandLine::InitializedForCurrentProcess()) {
+    g_add_cobalt_prefix = base::CommandLine::ForCurrentProcess()->HasSwitch(switches::kCobaltJniPrefix);
+    g_checked_command_line = true;
+  }
+  return g_add_cobalt_prefix;
+}
+#endif
+
 jclass GetClassInternal(JNIEnv* env,
+#if BUILDFLAG(IS_COBALT)
+                        const char* original_class_name,
+                        const char* split_name) {
+  const char* class_name;
+  std::string holder;
+  if (shouldAddCobaltPrefix()) {
+    holder = getRepackagedName(original_class_name);
+    class_name = holder.c_str();
+  } else {
+    class_name = original_class_name;
+  }
+#else
                         const char* class_name,
                         const char* split_name) {
+#endif
   jclass clazz;
   if (g_class_resolver != nullptr) {
     clazz = g_class_resolver(env, class_name, split_name);
@@ -234,7 +277,17 @@ jmethodID MethodID::LazyGet(JNIEnv* env,
   if (value) {
     return value;
   }
+#if BUILDFLAG(IS_COBALT)
+  jmethodID id;
+  if (shouldAddCobaltPrefix()) {
+    std::string holder = getRepackagedName(jni_signature);
+    id = MethodID::Get<type>(env, clazz, method_name, holder.c_str());
+  } else {
+    id = MethodID::Get<type>(env, clazz, method_name, jni_signature);
+  }
+#else
   jmethodID id = MethodID::Get<type>(env, clazz, method_name, jni_signature);
+#endif
   atomic_method_id->store(id, std::memory_order_release);
   return id;
 }
