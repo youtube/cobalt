c1fe7678035cd7a4bc7950eca45de500a8bed1f8
Colin Liang <zhongqi.liang.4u@gmail.com>
2025-01-19 08:58:58 -0800

    Integrate js_injection and make java bridge polyfill script work (#4715)
    1. Incorporate Chromium's js_injection module into the Chrobalt codebase
    to enable JavaScript injection capabilities.
    2. Move the Kabuki polyfill scripts into cobalt/embedded_resources.
    3. Utilize the embed_polyfilled_javascript GN rule to convert the
    JavaScript files in cobalt/embedded_resources into embedded_js.h.
    4. In the CobaltWebContentsObserver constructor, load the embedded
    JavaScript from embedded_js.h into the GeneratedResourceMap. Use
    AddDocumentStartJavaScript() to inject the polyfill code into web pages.
    This ensures the polyfills are executed early in the page lifecycle.
    5. Override the RunScriptsAtDocumentStart() method in
    CobaltContentRendererClient to execute the injected polyfill JavaScript
    code.
    
    This PR is built on top of Kaido 's draft PR
    https://github.com/youtube/cobalt/pull/4704.
    
    b/384742721
    
    ---------
    
    Co-authored-by: Kaido Kert <kaidokert@google.com>
    Co-authored-by: Colin Liang <colinliang@google.com>
    
    original-hexsha: 73018f4e1c425bedd3adae4deef56386c6c2ac3d

--- cobalt/cobalt_web_contents_observer.cc
+++ cobalt/cobalt_web_contents_observer.cc
@@ -13,8 +13,44 @@
 // limitations under the License.
 
 #include "cobalt/cobalt_web_contents_observer.h"
+#include "base/strings/utf_string_conversions.h"
+#include "cobalt/embedded_resources/embedded_js.h"
 
 namespace cobalt {
+
+CobaltWebContentsObserver::CobaltWebContentsObserver(
+    content::WebContents* web_contents)
+    : content::WebContentsObserver(web_contents) {
+  // Create browser-side mojo service component
+  js_communication_host_ =
+      std::make_unique<js_injection::JsCommunicationHost>(web_contents);
+
+  RegisterInjectedJavaScript();
+}
+
+void CobaltWebContentsObserver::RegisterInjectedJavaScript() {
+  // Get the embedded header resource
+  GeneratedResourceMap resource_map;
+  CobaltJavaScriptPolyfill::GenerateMap(resource_map);
+
+  for (const auto& [file_name, file_contents] : resource_map) {
+    LOG(INFO) << "JS injection for filename: " << file_name;
+    std::string js(reinterpret_cast<const char*>(file_contents.data),
+                   file_contents.size);
+
+    // Inject a script at document start for all origins
+    const std::u16string script(base::UTF8ToUTF16(js));
+    const std::vector<std::string> allowed_origins({"*"});
+    auto result = js_communication_host_->AddDocumentStartJavaScript(
+        script, allowed_origins);
+
+    if (result.error_message.has_value()) {
+      // error_message contains a value
+      LOG(WARNING) << "Failed to register JS injection for:" << file_name
+                   << ", error message: " << result.error_message.value();
+    }
+  }
+}
 
 // Placeholder for a WebContentsObserver override
 void CobaltWebContentsObserver::PrimaryMainDocumentElementAvailable() {
