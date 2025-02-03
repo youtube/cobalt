4ef76be017e11ff5902124abc6393bc9b0efe877
Colin Liang <zhongqi.liang.4u@gmail.com>
2025-02-03 10:31:38 -0800

    Add H5vccRuntime WEB IDL, set it as window.h5vcc.runtime (#4817)
    b/374147993
    
    ---------
    
    Co-authored-by: Colin Liang <colinliang@google.com>
    
    original-hexsha: 720846c12f06a031de69d19667546a2608ce5cfe

---
+++ third_party/blink/renderer/modules/cobalt/h5vcc_runtime/h_5_vcc_runtime.h
@@ -0,0 +1,41 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_H5VCC_RUNTIME_H_5_VCC_RUNTIME_H_
+#define THIRD_PARTY_BLINK_RENDERER_MODULES_H5VCC_RUNTIME_H_5_VCC_RUNTIME_H_
+
+#include "third_party/blink/renderer/modules/modules_export.h"
+#include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
+
+namespace blink {
+
+class LocalDOMWindow;
+class ScriptState;
+
+class MODULES_EXPORT H5vccRuntime final : public ScriptWrappable {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  explicit H5vccRuntime(LocalDOMWindow&);
+
+  // Web-exposed interface:
+  const String initialDeepLink() const;
+
+  void Trace(Visitor*) const override;
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_H5VCC_RUNTIME_H_5_VCC_RUNTIME_H_
