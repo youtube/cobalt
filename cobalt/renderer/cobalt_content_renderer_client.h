11a3b99c3498cc76e3c718883f4cd61d0d07d557
thorsten sideb0ard <sideboard@google.com>
2025-02-03 11:36:56 -0800

    Clean CobaltContentRendererClient and remove unused functions/overrides (#4718)
    b/388335917
    
    Followup for https://github.com/youtube/cobalt/pull/4661
    In the previous PR, we copied over the ShellContentRendererClient
    implementation.
    This PR removes the unused overrides and methods.
    
    Tested on Kirkwood, ensuring video playback works for normal and
    purchased content.
    original-hexsha: 5643e1eb854fb78af53b99a566508c74ca70769a

--- cobalt/renderer/cobalt_content_renderer_client.h
+++ cobalt/renderer/cobalt_content_renderer_client.h
@@ -5,24 +5,9 @@
 #ifndef COBALT_RENDERER_COBALT_CONTENT_RENDERER_CLIENT_H_
 #define COBALT_RENDERER_COBALT_CONTENT_RENDERER_CLIENT_H_
 
-#include <memory>
-#include <string>
-
-#include "build/build_config.h"
 #include "cobalt/media/audio/cobalt_audio_device_factory.h"
 #include "components/js_injection/renderer/js_communication.h"
-#include "content/public/common/alternative_error_page_override_info.mojom-forward.h"
 #include "content/public/renderer/content_renderer_client.h"
-#include "media/mojo/buildflags.h"
-
-namespace blink {
-class URLLoaderThrottleProvider;
-enum class URLLoaderThrottleProviderType;
-}  // namespace blink
-
-namespace web_cache {
-class WebCacheImpl;
-}
 
 namespace cobalt {
 
@@ -32,48 +17,15 @@ class CobaltContentRendererClient : public content::ContentRendererClient {
   ~CobaltContentRendererClient() override;
 
   // ContentRendererClient implementation.
-  void RenderThreadStarted() override;
-  void ExposeInterfacesToBrowser(mojo::BinderMap* binders) override;
   void RenderFrameCreated(content::RenderFrame* render_frame) override;
-  void PrepareErrorPage(content::RenderFrame* render_frame,
-                        const blink::WebURLError& error,
-                        const std::string& http_method,
-                        content::mojom::AlternativeErrorPageOverrideInfoPtr
-                            alternative_error_page_info,
-                        std::string* error_html) override;
-  void PrepareErrorPageForHttpStatusError(
-      content::RenderFrame* render_frame,
-      const blink::WebURLError& error,
-      const std::string& http_method,
-      int http_status,
-      content::mojom::AlternativeErrorPageOverrideInfoPtr
-          alternative_error_page_info,
-      std::string* error_html) override;
-
-  void DidInitializeWorkerContextOnWorkerThread(
-      v8::Local<v8::Context> context) override;
-
-  std::unique_ptr<blink::URLLoaderThrottleProvider>
-  CreateURLLoaderThrottleProvider(
-      blink::URLLoaderThrottleProviderType provider_type) override;
-
-#if BUILDFLAG(ENABLE_MOJO_CDM)
   void GetSupportedKeySystems(media::GetSupportedKeySystemsCB cb) override;
-#endif
-
-#if BUILDFLAG(USE_STARBOARD_MEDIA)
   bool IsSupportedAudioType(const media::AudioType& type) override;
   bool IsSupportedVideoType(const media::VideoType& type) override;
-#endif  // BUILDFLAG(USE_STARBOARD_MEDIA)
 
   // JS Injection hook
   void RunScriptsAtDocumentStart(content::RenderFrame* render_frame) override;
 
-  std::unique_ptr<blink::WebPrescientNetworking> CreatePrescientNetworking(
-      content::RenderFrame* render_frame) override;
-
  private:
-  std::unique_ptr<web_cache::WebCacheImpl> web_cache_impl_;
   // Registers a custom content::AudioDeviceFactory
   media::CobaltAudioDeviceFactory cobalt_audio_device_factory_;
 };
