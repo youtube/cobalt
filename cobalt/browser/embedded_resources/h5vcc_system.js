fb58b75c6b358ca94650f8cae7c099bc7c607b56
Colin Liang <colinliang@google.com>
2025-02-04 10:30:03 -0800

    Remove h5vcc_system.js polyfill script.

--- cobalt/browser/embedded_resources/h5vcc_system.js
+++
@@ -1,8 +0,0 @@
-const advertisingId = await window.h5vcc.system.getAdvertisingId();
-
-Object.defineProperty(window.h5vcc.system, "advertisingId", {
-    value: advertisingId,
-    writable: false,
-    enumerable: true,
-    configurable: false
-});
