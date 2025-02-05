eeb41db94857e1cd71d78a4e984a16d5f9a01ed3
Brian Ting <brianting@google.com>
2025-02-05 11:41:17 -0800

    Trim Android release packages to required files (#4847)
    The required files were manually determined. This moves the release
    packging logic to the builder side rather than the snapshot side
    allowing better developer access and maintaince.
    
    b/393447640
    
    original-hexsha: 5f042c83a21a4e37ad40d37ce50494e0f6e30061

--- cobalt/build/android/package.py
+++ cobalt/build/android/package.py
@@ -7,9 +7,33 @@ from cobalt.build import packaging
 
 
 def lay_out(out_dir, base_dir):
-  shutil.copytree(out_dir, base_dir, dirs_exist_ok=True)
-  shutil.rmtree(os.path.join(base_dir, 'lib.unstripped'))
-  shutil.rmtree(os.path.join(base_dir, 'thinlto-cache'))
+  directories = [
+      'content/data',
+      'gen/cobalt/android/cobalt_apk/generated_java/input_srcjars/androidx/appcompat',
+      'gen/cobalt/android/cobalt_apk/generated_java/input_srcjars/androidx/core',
+      'gen/cobalt/android/cobalt_apk/generated_java/input_srcjars/androidx/fragment',
+      'gen/cobalt/android/cobalt_apk/generated_java/input_srcjars/gen/base_module',
+      'gen/cobalt/android/cobalt_apk/generated_java/input_srcjars/org/chromium',
+      'lib.java',
+  ]
+
+  files = [
+      'content_shell.pak',
+      'icudtl.dat',
+      'obj/cobalt/android/cobalt_java_resources.resources.zip',
+      'obj/components/metrics/metrics_java.javac.jar',
+      'obj/content/shell/android/content_shell_java_resources.resources.zip',
+      'obj/third_party/android_deps/chromium_play_services_availability_java.javac.jar',
+  ]
+
+  for directory in directories:
+    shutil.copytree(
+        os.path.join(out_dir, directory),
+        os.path.join(base_dir, directory),
+        dirs_exist_ok=True)
+
+  for file in files:
+    packaging.copy(os.path.join(out_dir, file), os.path.join(base_dir, file))
 
 
 if __name__ == '__main__':
