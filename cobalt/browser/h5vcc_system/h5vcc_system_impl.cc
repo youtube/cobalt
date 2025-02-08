24e4caf4411fb6131e48755e39534cf60216ab89
Colin Liang <zhongqi.liang.4u@gmail.com>
2025-02-07 17:30:47 -0800

    Implementing Asynchronous Advertising ID Retrieval for H5vccSystem (#4794)
    1. A new mojom interface for H5vccSystem is created, including an
    asynchronous API method to retrieve the advertising ID.
    2. The mojom server implementation handles the request by calling JNI
    (Java Native Interface) functions. These JNI calls will interact with
    the StarboardBridge to fetch the actual advertising ID value.
    3. The renderer client then utilizes the mojom interface to call the
    asynchronous method and retrieve the advertising ID.
    
    Tested on devtools, https://screenshot.googleplex.com/43HotkbLgJSQdx3
    
    4. Added web_tests for the mojom interface.
    // Compile the web_test
    cobalt/build/gn.py -p linux-x64x11 -C devel &&
    autoninja -C out/linux-x64x11_devel blink_wpt_tests &&
    autoninja -C out/linux-x64x11_devel dump_syms &&
    autoninja -C out/linux-x64x11_devel minidump_stackwalk &&
    autoninja -C out/linux-x64x11_devel
    cobalt/browser/h5vcc_system/public/mojom:mojom_js
    // run the web_test
    third_party/blink/tools/run_web_tests.py -t linux-x64x11_devel
    wpt_internal/h5vcc/h5vcc-system
    
    b/377049113
    
    ---------
    
    Co-authored-by: Colin Liang <colinliang@google.com>
    original-hexsha: 7bbf2ef6d3e7c261c74ea44fe3690f879674c8cc

---
+++ cobalt/browser/h5vcc_system/h5vcc_system_impl.cc
@@ -0,0 +1,50 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "cobalt/browser/h5vcc_system/h5vcc_system_impl.h"
+#include "base/functional/bind.h"
+#include "base/functional/callback.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#include "starboard/android/shared/starboard_bridge.h"
+
+using starboard::android::shared::StarboardBridge;
+#endif
+
+namespace h5vcc_system {
+
+// TODO (b/395126160): refactor mojom implementation on Android
+H5vccSystemImpl::H5vccSystemImpl(
+    content::RenderFrameHost& render_frame_host,
+    mojo::PendingReceiver<mojom::H5vccSystem> receiver)
+    : content::DocumentService<mojom::H5vccSystem>(render_frame_host,
+                                                   std::move(receiver)) {}
+
+void H5vccSystemImpl::Create(
+    content::RenderFrameHost* render_frame_host,
+    mojo::PendingReceiver<mojom::H5vccSystem> receiver) {
+  new H5vccSystemImpl(*render_frame_host, std::move(receiver));
+}
+
+void H5vccSystemImpl::GetAdvertisingId(GetAdvertisingIdCallback callback) {
+  std::string advertising_id;
+#if BUILDFLAG(IS_ANDROID)
+  JNIEnv* env = base::android::AttachCurrentThread();
+  StarboardBridge* starbooard_bridge = StarboardBridge::GetInstance();
+  advertising_id = starbooard_bridge->GetAdvertisingId(env);
+#endif
+  std::move(callback).Run(advertising_id);
+}
+
+}  // namespace h5vcc_system
