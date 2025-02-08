7bfcd26bce60c669a6fb064c528e478d12ee5565
Colin Liang <zhongqi.liang.4u@gmail.com>
2025-02-07 17:30:47 -0800

    Implementing Asynchronous Advertising ID Retrieval for H5vccSystem (#4794)
    1. A new mojom interface for H5vccSystem is created, including an
    asynchronous API method to retrieve the advertising ID.
    2. The mojom server implementation handles the request by calling JNI
    (Java Native Interface) functions. These JNI calls will interact with
    the StarboardBridge to fetch the actual advertising ID value.
    3. The renderer client then utilizes the mojom interface to call the
    asynchronous method and retrieve the advertising ID.
    
    Tested on devtools, https://screenshot.googleplex.com/43HotkbLgJSQdx3
    
    4. Added web_tests for the mojom interface.
    // Compile the web_test
    cobalt/build/gn.py -p linux-x64x11 -C devel &&
    autoninja -C out/linux-x64x11_devel blink_wpt_tests &&
    autoninja -C out/linux-x64x11_devel dump_syms &&
    autoninja -C out/linux-x64x11_devel minidump_stackwalk &&
    autoninja -C out/linux-x64x11_devel
    cobalt/browser/h5vcc_system/public/mojom:mojom_js
    // run the web_test
    third_party/blink/tools/run_web_tests.py -t linux-x64x11_devel
    wpt_internal/h5vcc/h5vcc-system
    
    b/377049113
    
    ---------
    
    Co-authored-by: Colin Liang <colinliang@google.com>
    original-hexsha: 7bbf2ef6d3e7c261c74ea44fe3690f879674c8cc

---
+++ third_party/blink/web_tests/wpt_internal/h5vcc/h5vcc-system/resources/mock-h5vcc-system.js
@@ -0,0 +1,48 @@
+import {H5vccSystem, H5vccSystemReceiver} from '/gen/cobalt/browser/h5vcc_system/public/mojom/h5vcc_system.mojom.m.js';
+
+// Implementation of h5vcc_system.mojom.H5vccSystem.
+class MockH5vccSystem {
+  constructor() {
+    this.interceptor_ =
+        new MojoInterfaceInterceptor(H5vccSystem.$interfaceName);
+    this.interceptor_.oninterfacerequest = e => this.bind(e.handle);
+    this.receiver_ = new H5vccSystemReceiver(this);
+
+    this.stub_result_ = new Map();
+  }
+
+  STUB_KEY_ADVERTISING_ID = 'advertisingId';
+
+  start() {
+    this.interceptor_.start();
+  }
+
+  stop() {
+    this.interceptor_.stop();
+  }
+
+  bind(handle) {
+    this.receiver_.$.bindHandle(handle);
+  }
+
+  reset() {
+    this.stub_result_ = new Map();
+  }
+
+  stubResult(key, value) {
+    this.stub_result_.set(key, value);
+  }
+
+  stubAdvertisingID(id) {
+    this.stubResult(this.STUB_KEY_ADVERTISING_ID, id);
+  }
+
+  // h5vcc_system.mojom.H5vccSystem impl.
+  getAdvertisingId() {
+    // VERY IMPORTANT: this should return (a resolved Promise with) a dictionary
+    // with the same "key" as the mojom definition, find the name of the key in the generated js file imported on the top of this file.
+    return Promise.resolve({ advertisingId: this.stub_result_.get(this.STUB_KEY_ADVERTISING_ID) });
+  }
+}
+
+export const mockH5vccSystem = new MockH5vccSystem();
