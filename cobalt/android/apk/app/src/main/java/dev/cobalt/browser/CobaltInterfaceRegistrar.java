1ae2429912639310903e7e97be05e303fd7ca209
Holden Warriner <hwarriner@google.com>
2025-02-14 14:46:03 -0500

    Enable Java impls for Cobalt's Mojo interfaces (#4909)
    A Java implementation for the CrashAnnotator interface is added as an
    example. It is still stubbed, but now in Java; a quick follow-up will
    implement it using StarboardBridge.setCrashContext.
    
    The Mojo Java Bindings API doc was used as a reference.
    
    b/383301493
    original-hexsha: dc0f4c3c4afadb43860874db109b5a5fa73b0ae6

---
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/browser/CobaltInterfaceRegistrar.java
@@ -0,0 +1,43 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dev.cobalt.browser;
+
+import dev.cobalt.browser.crashannotator.CrashAnnotatorImplFactory;
+import org.chromium.base.annotations.CalledByNative;
+import org.chromium.content_public.browser.InterfaceRegistrar;
+import org.chromium.content_public.browser.RenderFrameHost;
+import org.chromium.crashannotator.mojom.CrashAnnotator;
+import org.chromium.services.service_manager.InterfaceRegistry;
+
+/** Registers Mojo interface implementations exposed to C++ code at the Cobalt layer. */
+class CobaltInterfaceRegistrar {
+    @CalledByNative
+    private static void registerMojoInterfaces() {
+        InterfaceRegistrar.Registry.addRenderFrameHostRegistrar(
+            new CobaltRenderFrameHostInterfaceRegistrar());
+    }
+
+    private static class CobaltRenderFrameHostInterfaceRegistrar
+            implements InterfaceRegistrar<RenderFrameHost> {
+        @Override
+        public void registerInterfaces(
+                InterfaceRegistry registry,
+                final RenderFrameHost renderFrameHost) {
+            registry.addInterface(
+                CrashAnnotator.MANAGER,
+                new CrashAnnotatorImplFactory(renderFrameHost));
+        }
+    }
+}
