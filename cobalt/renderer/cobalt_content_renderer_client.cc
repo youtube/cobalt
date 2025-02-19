ee280a55216678c2415db009c364ff6760a91e6e
Bo-Rong Chen <borongchen@google.com>
2025-02-19 09:35:17 -0800

    Reland "Add VideoGeometrySetter Service for Cobalt (#4810)" (#4921)
    This is a reland of 10d48512ebfe4a609106dfede497674b9537075a
    
    This CL fixed DCHECK failed caused by the PR
    (https://github.com/youtube/cobalt/pull/4810) and list the fixes below
    (b/397413189):
    - Removed `DCHECK(content::RenderThread::IsMainThread());` in
    CobaltContentRendererClient due to CobaltContentRendererClient is not in
    Render Thread.
    - Moved the order of `DCHECK(video_geometry_change_subcriber_remote_);`
    after `video_geometry_setter_service_->GetVideoGeometryChangeSubscriber`
    in StarboardRenderer.
    - Changed `gfx::ToNearestRect()` to `gfx::ToEnclosingRect()` in
    StarboardRenderer.
    
    Original change's description:
    > Add VideoGeometrySetter Service for Cobalt (#4810)
    >
    > StarboardRender needs to be informed with the video geometry
    information
    > from the display compositor. VideoGeometrySetter provides the IPC
    > between the StarobardRenderer and the display compositor so the video
    > geometry information can reach StarboardRenderer.
    >
    > This refers to
    > https://chromium-review.googlesource.com/c/chromium/src/+/1799692 with
    > the following modifications:
    > - VideoGeometrySetterService is in Renderer thread, and it is exposed
    to
    > ContentBrowserClient.
    > - ContentBrowserClient binds VideoGeometrySetterService after
    receiving
    > mojo::PendingRemote\<cobalt::media::mojom::VideoGeometrySetter\> from
    > compositing thread (viz) to Renderer thread.
    > - ContentRendererClient creates a custom StarboardRendererFactory,
    which
    > allows to bind |overlay_plane_id| for each StarboardRenderer with
    > VideoGeometrySetterService.
    >
    > This CL also cleans up the old implementations for setting video
    bounds:
    > https://github.com/youtube/cobalt/pull/4385, because it is unnecessary
    > with this PR.
    >
    > b/391938746
    
    original-hexsha: f19998dfc2cf89bb0dce2b8095441a32367107ec

--- cobalt/renderer/cobalt_content_renderer_client.cc
+++ cobalt/renderer/cobalt_content_renderer_client.cc
@@ -6,9 +6,23 @@
 
 #include <string>
 
+#include "base/time/time.h"
+#include "cobalt/media/service/mojom/video_geometry_setter.mojom.h"
+#include "cobalt/media/service/video_geometry_setter_service.h"
 #include "cobalt/renderer/cobalt_render_frame_observer.h"
 #include "components/cdm/renderer/widevine_key_system_info.h"
+#include "components/js_injection/renderer/js_communication.h"
+#include "content/public/renderer/render_frame.h"
+#include "content/public/renderer/render_thread.h"
+#include "media/base/decoder_factory.h"
+#include "media/base/media_log.h"
+#include "media/base/renderer_factory.h"
+#include "media/renderers/video_overlay_factory.h"
+#include "media/starboard/starboard_renderer_factory.h"
+#include "media/video/gpu_video_accelerator_factories.h"
+#include "mojo/public/cpp/bindings/binder_map.h"
 #include "starboard/media.h"
+#include "starboard/player.h"
 
 namespace cobalt {
 
@@ -19,14 +33,14 @@ namespace {
 // gets lost with hardcoded MIME string. This can sometimes cause issues. For
 // example, vp9 profile 2 indicates hdr support, so an implementation accepts
 // "codecs=vp9" may reject "codecs=vp9.2".
-std::string GetMimeFromVideoType(const media::VideoType& type) {
+std::string GetMimeFromVideoType(const ::media::VideoType& type) {
   // The MIME string is for very basic video codec supportability check.
   switch (type.codec) {
-    case media::VideoCodec::kH264:
+    case ::media::VideoCodec::kH264:
       return "video/mp4; codecs=\"avc1.4d4015\"";
-    case media::VideoCodec::kVP9:
+    case ::media::VideoCodec::kVP9:
       return "video/webm; codecs=\"vp9\"";
-    case media::VideoCodec::kAV1:
+    case ::media::VideoCodec::kAV1:
       return "video/mp4; codecs=\"av01.0.08M.08\"";
     default:
       return "";
@@ -35,16 +49,16 @@ std::string GetMimeFromVideoType(const media::VideoType& type) {
 
 // TODO(b/376542844): Eliminate the usage of hardcoded MIME string once we
 // support to query codec capabilities with configs.
-std::string GetMimeFromAudioType(const media::AudioType& type) {
+std::string GetMimeFromAudioType(const ::media::AudioType& type) {
   // The MIME string is for very basic audio codec supportability check.
   switch (type.codec) {
-    case media::AudioCodec::kAAC:
+    case ::media::AudioCodec::kAAC:
       return "audio/mp4; codecs=\"mp4a.40.2\"";
-    case media::AudioCodec::kAC3:
+    case ::media::AudioCodec::kAC3:
       return "audio/mp4; codecs=\"ac-3\"";
-    case media::AudioCodec::kEAC3:
+    case ::media::AudioCodec::kEAC3:
       return "audio/mp4; codecs=\"ec-3\"";
-    case media::AudioCodec::kOpus:
+    case ::media::AudioCodec::kOpus:
       return "audio/webm; codecs=\"opus\"";
     // TODO(b/375232937): Support IAMF
     default:
@@ -52,8 +66,8 @@ std::string GetMimeFromAudioType(const media::AudioType& type) {
   }
 }
 
-media::SupportedCodecs GetStarboardEmeSupportedCodecs() {
-  media::SupportedCodecs codecs =
+::media::SupportedCodecs GetStarboardEmeSupportedCodecs() {
+  ::media::SupportedCodecs codecs =
       ::media::EME_CODEC_AAC | ::media::EME_CODEC_AVC1 |
       ::media::EME_CODEC_VP9_PROFILE0 | ::media::EME_CODEC_VP9_PROFILE2 |
       ::media::EME_CODEC_VP8 | ::media::EME_CODEC_OPUS |
@@ -67,26 +81,56 @@ media::SupportedCodecs GetStarboardEmeSupportedCodecs() {
 
 }  // namespace
 
-CobaltContentRendererClient::CobaltContentRendererClient() {}
-CobaltContentRendererClient::~CobaltContentRendererClient() {}
+CobaltContentRendererClient::CobaltContentRendererClient()
+    : video_geometry_setter_service_(
+          std::unique_ptr<media::VideoGeometrySetterService,
+                          base::OnTaskRunnerDeleter>(
+              nullptr,
+              base::OnTaskRunnerDeleter(nullptr))) {
+  DETACH_FROM_THREAD(thread_checker_);
+}
+
+CobaltContentRendererClient::~CobaltContentRendererClient() = default;
+
+void CobaltContentRendererClient::RenderThreadStarted() {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  if (!video_geometry_setter_service_) {
+    video_geometry_setter_service_ =
+        std::unique_ptr<media::VideoGeometrySetterService,
+                        base::OnTaskRunnerDeleter>(
+            new media::VideoGeometrySetterService,
+            base::OnTaskRunnerDeleter(
+                base::SingleThreadTaskRunner::GetCurrentDefault()));
+  }
+}
+
+void CobaltContentRendererClient::ExposeInterfacesToBrowser(
+    mojo::BinderMap* binders) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  DCHECK(video_geometry_setter_service_);
+  binders->Add<media::mojom::VideoGeometrySetter>(
+      video_geometry_setter_service_->GetBindCallback(),
+      base::SingleThreadTaskRunner::GetCurrentDefault());
+}
 
 void CobaltContentRendererClient::RenderFrameCreated(
     content::RenderFrame* render_frame) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   new js_injection::JsCommunication(render_frame);
   new CobaltRenderFrameObserver(render_frame);
 }
 
 #if BUILDFLAG(IS_ANDROID)
 void AddStarboardCmaKeySystems(::media::KeySystemInfos* key_system_infos) {
-  media::SupportedCodecs codecs = GetStarboardEmeSupportedCodecs();
+  ::media::SupportedCodecs codecs = GetStarboardEmeSupportedCodecs();
 
   using Robustness = cdm::WidevineKeySystemInfo::Robustness;
 
-  const base::flat_set<media::EncryptionScheme> kEncryptionSchemes = {
-      media::EncryptionScheme::kCenc, media::EncryptionScheme::kCbcs};
+  const base::flat_set<::media::EncryptionScheme> kEncryptionSchemes = {
+      ::media::EncryptionScheme::kCenc, ::media::EncryptionScheme::kCbcs};
 
-  const base::flat_set<media::CdmSessionType> kSessionTypes = {
-      media::CdmSessionType::kTemporary};
+  const base::flat_set<::media::CdmSessionType> kSessionTypes = {
+      ::media::CdmSessionType::kTemporary};
 
   key_system_infos->emplace_back(new cdm::WidevineKeySystemInfo(
       codecs,                        // Regular codecs.
@@ -97,14 +141,15 @@ void AddStarboardCmaKeySystems(::media::KeySystemInfos* key_system_infos) {
       kSessionTypes,                 // Hardware secure session types.
       Robustness::HW_SECURE_CRYPTO,  // Max audio robustness.
       Robustness::HW_SECURE_ALL,     // Max video robustness.
-      media::EmeFeatureSupport::ALWAYS_ENABLED,    // Persistent state.
-      media::EmeFeatureSupport::ALWAYS_ENABLED));  // Distinctive identifier.
+      ::media::EmeFeatureSupport::ALWAYS_ENABLED,    // Persistent state.
+      ::media::EmeFeatureSupport::ALWAYS_ENABLED));  // Distinctive identifier.
 }
 #endif
 
 void CobaltContentRendererClient::GetSupportedKeySystems(
-    media::GetSupportedKeySystemsCB cb) {
-  media::KeySystemInfos key_systems;
+    ::media::GetSupportedKeySystemsCB cb) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  ::media::KeySystemInfos key_systems;
 #if BUILDFLAG(IS_ANDROID)
   AddStarboardCmaKeySystems(&key_systems);
   std::move(cb).Run(std::move(key_systems));
@@ -112,7 +157,8 @@ void CobaltContentRendererClient::GetSupportedKeySystems(
 }
 
 bool CobaltContentRendererClient::IsSupportedAudioType(
-    const media::AudioType& type) {
+    const ::media::AudioType& type) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   std::string mime = GetMimeFromAudioType(type);
   SbMediaSupportType support_type = kSbMediaSupportTypeNotSupported;
   if (!mime.empty()) {
@@ -125,7 +171,8 @@ bool CobaltContentRendererClient::IsSupportedAudioType(
 }
 
 bool CobaltContentRendererClient::IsSupportedVideoType(
-    const media::VideoType& type) {
+    const ::media::VideoType& type) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   std::string mime = GetMimeFromVideoType(type);
   SbMediaSupportType support_type = kSbMediaSupportTypeNotSupported;
   if (!mime.empty()) {
@@ -139,9 +186,32 @@ bool CobaltContentRendererClient::IsSupportedVideoType(
 
 void CobaltContentRendererClient::RunScriptsAtDocumentStart(
     content::RenderFrame* render_frame) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   js_injection::JsCommunication* communication =
       js_injection::JsCommunication::Get(render_frame);
   communication->RunScriptsAtDocumentStart();
+}
+
+std::unique_ptr<::media::RendererFactory>
+CobaltContentRendererClient::GetBaseRendererFactory(
+    content::RenderFrame* /* render_frame */,
+    ::media::MediaLog* media_log,
+    ::media::DecoderFactory* /* decoder_factory */,
+    base::RepeatingCallback<::media::GpuVideoAcceleratorFactories*()>
+    /* get_gpu_factories_cb */) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  // TODO(b/394368542): Add Content API to create StarboardRenderer.
+  auto overlay_factory = std::make_unique<::media::VideoOverlayFactory>();
+  base::TimeDelta audio_write_duration_local =
+      base::Microseconds(kSbPlayerWriteDurationLocal);
+  base::TimeDelta audio_write_duration_remote =
+      base::Microseconds(kSbPlayerWriteDurationRemote);
+  DCHECK(video_geometry_setter_service_);
+  return std::make_unique<::media::StarboardRendererFactory>(
+      media_log, std::move(overlay_factory),
+      // TODO(b/383327725) - Cobalt: Inject these values from the web app.
+      audio_write_duration_local, audio_write_duration_remote,
+      video_geometry_setter_service_.get());
 }
 
 }  // namespace cobalt
