7fd7594b5aa8558b746af41a44257a87ea14b77d
Holden Warriner <hwarriner@google.com>
2025-02-20 15:53:28 -0500

    Rename crashAnnotator web interface as crashLog (#4936)
    This is done so that web clients do not need to account for a name
    change when migrating from Cobalt to Chrobalt. The "crash annotator"
    name is still preferred and used for the Mojo interface and
    implementations.
    
    b/383301493

--- third_party/blink/web_tests/wpt_internal/cobalt/crash-log/crash_log_setString.https.any.js
+++ third_party/blink/web_tests/wpt_internal/cobalt/crash-log/crash_log_setString.https.any.js
@@ -2,17 +2,17 @@
 // META: script=/resources/test-only-api.js
 // META: script=resources/automation.js
 
-crash_annotator_test(async (t, fake) => {
+crash_log_test(async (t, fake) => {
   let expectedSetStringResult = true;
   fake.stubResult(expectedSetStringResult);
   let setStringResult =
-      await window.h5vcc.crashAnnotator.setString("some key", "some val");
+      await window.h5vcc.crashLog.setString("some key", "some val");
   assert_equals(setStringResult, expectedSetStringResult);
 }, 'setString() returns value provided by browser endpoint');
 
-crash_annotator_test(async (t, fake) => {
+crash_log_test(async (t, fake) => {
   let key = "some key";
   let value = "some value";
-  await window.h5vcc.crashAnnotator.setString(key, value);
+  await window.h5vcc.crashLog.setString(key, value);
   assert_equals(fake.getAnnotation(key), value);
 }, 'setString() sends expected key and value to browser endpoint');
