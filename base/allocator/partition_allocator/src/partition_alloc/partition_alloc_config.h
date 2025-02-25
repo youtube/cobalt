17d8dd2b5ad829d4a1001dfd80b992e750b7d58c
Niranjan Yardi <nyardi@google.com>
2025-02-25 15:47:27 -0800

    Add BUILDFLAG IS_COBALT_HERMETIC_BUILD (#4966)
    b/399173764
    original-hexsha: 3818b43c636a13cddd84c05939af6dd14f624d1e

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_config.h
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_config.h
@@ -55,7 +55,7 @@ static_assert(sizeof(void*) != 8, "");
 #endif  // BUILDFLAG(HAS_64_BIT_POINTERS) && BUILDFLAG(IS_IOS)
 
 #if BUILDFLAG(HAS_64_BIT_POINTERS) && \
-    (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_ANDROID)) && !defined(IS_COBALT_HERMETIC_BUILD)
+    (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_ANDROID)) && !BUILDFLAG(IS_COBALT_HERMETIC_BUILD)
 #include <linux/version.h>
 // TODO(bikineev): Enable for ChromeOS.
 #define PA_CONFIG_STARSCAN_UFFD_WRITE_PROTECTOR_SUPPORTED() \
@@ -94,7 +94,7 @@ static_assert(sizeof(void*) != 8, "");
 // POSIX is not only UNIX, e.g. macOS and other OSes. We do use Linux-specific
 // features such as futex(2).
 #define PA_CONFIG_HAS_LINUX_KERNEL() \
-  (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)) && !defined(IS_COBALT_HERMETIC_BUILD)
+  (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)) && !BUILDFLAG(IS_COBALT_HERMETIC_BUILD)
 
 // On some platforms, we implement locking by spinning in userspace, then going
 // into the kernel only if there is contention. This requires platform support,
