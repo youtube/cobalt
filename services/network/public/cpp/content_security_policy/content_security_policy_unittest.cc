a2be4fb88c69fa70442094377bfe132df7b782d3
johnx <johnx@google.com>
2025-02-28 00:47:02 -0800

    Add custom cobalt-insecure-local-network csp source (#4958)
    This was a must-have header for yts to load Kabuki modules without
    being blocked by CSP. We only allow websocket connection from trusted
    web client to host within the local network, so the security implication
    is limited. This is the exact same behavior Cobalt had in the past.
    
    b/389723405
    b/390022481
    
    original-hexsha: 482348975045c8c9abc8aef708ed2a57624147e4

--- services/network/public/cpp/content_security_policy/content_security_policy_unittest.cc
+++ services/network/public/cpp/content_security_policy/content_security_policy_unittest.cc
@@ -1507,6 +1507,19 @@ TEST(ContentSecurityPolicy, ParseSerializedSourceList) {
           "'script-src' contains an invalid source: ''wrong''. It will be "
           "ignored.",
       },
+#if BUILDFLAG(IS_COBALT)
+      {
+          "'wrong' 'cobalt_insecure_local_network'",
+          base::BindOnce([] {
+            auto csp = mojom::CSPSourceList::New();
+            csp->cobalt_insecure_local_network = true;
+            return csp;
+          }),
+          "The source list for the Content Security Policy directive "
+          "'script-src' contains an invalid source: ''wrong''. It will be "
+          "ignored.",
+      },
+#endif
   };
 
   for (auto& test : cases) {
