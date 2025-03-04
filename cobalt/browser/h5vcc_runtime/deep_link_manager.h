78bbba5370e6ef39ab9bed611389b275b4eaa1fd
Colin Liang <colinliang@google.com>
2025-03-04 13:24:22 -0800

    revise

--- cobalt/browser/h5vcc_runtime/deep_link_manager.h
+++ cobalt/browser/h5vcc_runtime/deep_link_manager.h
@@ -31,7 +31,10 @@ class DeepLinkManager {
   DeepLinkManager(const DeepLinkManager&) = delete;
   DeepLinkManager& operator=(const DeepLinkManager&) = delete;
 
-  void set_deep_link(const std::string& url) { deep_link_ = url; }
+  void set_deep_link(const std::string& url) {
+    DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+    deep_link_ = url;
+  }
   const std::string& get_deep_link() const { return deep_link_; }
 
   // To ensure exactly-once delivery, the deeplink is erased after delivery to
