791dfca7d01a737e00367d537521d64fb759a5b8
Andrew Savage <andrewsavage@google.com>
2025-03-05 14:39:44 -0800

    Move local_ip to be close to where it's used (#5024)
    This avoids exporting the functions defined in local_ip.
    
    b/394101660
    original-hexsha: 28d598abf1e0a74965330b491840182f75c2b04b

--- third_party/blink/renderer/core/frame/csp/source_list_directive.cc
+++ third_party/blink/renderer/core/frame/csp/source_list_directive.cc
@@ -15,10 +15,10 @@
 >>>>>>> a40bc8ae05d (Add custom cobalt-insecure-local-network csp source (#4958))
 #include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
 #include "third_party/blink/renderer/core/frame/csp/csp_source.h"
+#include "third_party/blink/renderer/core/frame/csp/local_ip.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_request.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
-#include "third_party/blink/renderer/platform/network/local_ip.h"
 
 namespace blink {
 
