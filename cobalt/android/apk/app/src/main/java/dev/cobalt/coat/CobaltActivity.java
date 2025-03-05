25f7b024b51adf76bfbd46ff549037023d30b63c
Thorsten Sideboard <sideboard@google.com>
2025-03-05 12:58:14 -0800

    [Android] Add functionality for recording mic.
    b/383379319
    
    * Enables permission checks to allow audio capture.
    
    * Very important type change for Google TV. SLES stream type
      has to be VOICE_RECOGNITION in order to trigger AtvRemote
      service which injects BLE audio. See go/atv-remote-service

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
@@ -124,6 +124,8 @@ public abstract class CobaltActivity extends Activity {
             "--enable-blink-features=MediaSourceNewAbortAndDuration",
             // Trades a little V8 performance for significant memory savings.
             "--js-flags=--optimize_for_size=true",
+             // Auto accept camera and mic capture permissions.
+            "--auto-accept-camera-and-microphone-capture",
           };
       CommandLine.getInstance().appendSwitchesAndArguments(cobaltCommandLineParams);
       if (shouldSetJNIPrefix) {
