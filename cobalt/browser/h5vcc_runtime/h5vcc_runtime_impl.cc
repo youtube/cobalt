c030a73113b0b50532d3f546517687e596ef6272
Colin Liang <zhongqi.liang.4u@gmail.com>
2025-03-05 18:42:08 -0800

    Add DeepLinkManager, move startDeepLink out of StarboardBridge  (#5002)
    1. Relocate startDeepLink from StarboardBridge.
    2. Create a DeepLinkManager singleton to manage startDeepLink.
    3. H5vccRuntimeImpl::GetInitialDeepLink() implementation in the Mojom
    service will now reset the deeplink to its default value after being
    called.
    
    Tested on devtools. https://screenshot.googleplex.com/8DZB3z5ykQu3aTk
    
    b/374147993
    
    original-hexsha: acdc02821bc34c5ee749baae89c41112cd1ae23b

--- cobalt/browser/h5vcc_runtime/h5vcc_runtime_impl.cc
+++ cobalt/browser/h5vcc_runtime/h5vcc_runtime_impl.cc
@@ -17,6 +17,7 @@
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "build/build_config.h"
+#include "cobalt/browser/h5vcc_runtime/deep_link_manager.h"
 
 #if BUILDFLAG(IS_ANDROID)
 #include "starboard/android/shared/starboard_bridge.h"
@@ -39,14 +40,10 @@ void H5vccRuntimeImpl::Create(
   new H5vccRuntimeImpl(*render_frame_host, std::move(receiver));
 }
 
-void H5vccRuntimeImpl::GetInitialDeepLink(GetInitialDeepLinkCallback callback) {
-  std::string start_deep_link;
-#if BUILDFLAG(IS_ANDROID)
-  JNIEnv* env = base::android::AttachCurrentThread();
-  StarboardBridge* starbooard_bridge = StarboardBridge::GetInstance();
-  start_deep_link = starbooard_bridge->GetStartDeepLink(env);
-#endif
-  std::move(callback).Run(start_deep_link);
+void H5vccRuntimeImpl::GetAndClearInitialDeepLink(
+    GetAndClearInitialDeepLinkCallback callback) {
+  auto* manager = cobalt::browser::DeepLinkManager::GetInstance();
+  std::move(callback).Run(manager->GetAndClearDeepLink());
 }
 
 }  // namespace h5vcc_runtime
