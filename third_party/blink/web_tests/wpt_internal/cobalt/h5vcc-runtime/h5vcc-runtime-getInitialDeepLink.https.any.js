ba7ed2d68416dded78ec5f1525ef3111ee5158c0
Colin Liang <zhongqi.liang.4u@gmail.com>
2025-03-05 18:42:08 -0800

    Add DeepLinkManager, move startDeepLink out of StarboardBridge  (#5002)
    1. Relocate startDeepLink from StarboardBridge.
    2. Create a DeepLinkManager singleton to manage startDeepLink.
    3. H5vccRuntimeImpl::GetInitialDeepLink() implementation in the Mojom
    service will now reset the deeplink to its default value after being
    called.
    
    Tested on devtools. https://screenshot.googleplex.com/8DZB3z5ykQu3aTk
    
    b/374147993
    
    original-hexsha: acdc02821bc34c5ee749baae89c41112cd1ae23b

--- third_party/blink/web_tests/wpt_internal/cobalt/h5vcc-runtime/h5vcc-runtime-getInitialDeepLink.https.any.js
+++ third_party/blink/web_tests/wpt_internal/cobalt/h5vcc-runtime/h5vcc-runtime-getInitialDeepLink.https.any.js
@@ -11,3 +11,15 @@ h5vcc_runtime_tests(async (t, mockH5vccRuntime) => {
     let actual = await window.h5vcc.runtime.getInitialDeepLink();
     assert_equals(actual, expected);
 }, 'exercises H5vccRuntime.getInitialDeepLink()');
+
+h5vcc_runtime_tests(async (t, mockH5vccRuntime) => {
+    assert_implements(window.h5vcc, "window.h5vcc not supported");
+    assert_implements(window.h5vcc.runtime, "window.h5vcc.runtime not supported");
+
+    const fakeInitialDeepLink = 'https://fake.domain.com/tv';
+    mockH5vccRuntime.stubInitialDeepLink(fakeInitialDeepLink);
+    let first_actual = await window.h5vcc.runtime.getInitialDeepLink();
+    assert_equals(first_actual, fakeInitialDeepLink);
+    let second_actual = await window.h5vcc.runtime.getInitialDeepLink();
+    assert_equals(second_actual, '');
+}, 'exercises H5vccRuntime.getInitialDeepLink(), the initial deep link is cleared after retrieving it once.');
