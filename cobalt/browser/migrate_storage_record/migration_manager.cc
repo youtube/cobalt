1ace205bd2be25876a3071b0b052672e7d4e96c9
aee <aee@google.com>
2025-03-06 14:34:25 -0800

    Add unit tests for storage record migration. (#4998)
    The changes are to support testing. No changes to the functionality are
    being made.
    
    b/399166308
    
    original-hexsha: b96d236fe7fe66445e1db6650674f417feaf9a57

--- cobalt/browser/migrate_storage_record/migration_manager.cc
+++ cobalt/browser/migrate_storage_record/migration_manager.cc
@@ -14,16 +14,9 @@
 
 #include "cobalt/browser/migrate_storage_record/migration_manager.h"
 
-#include <atomic>
-#include <memory>
-#include <string>
-#include <utility>
-#include <vector>
-
 #include "base/base64url.h"
 #include "base/command_line.h"
 #include "base/containers/adapters.h"
-#include "base/functional/bind.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
@@ -32,9 +25,6 @@
 #include "components/url_matcher/url_util.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/storage_partition.h"
-#include "content/public/browser/weak_document_ptr.h"
-#include "content/public/browser/web_contents.h"
-#include "net/cookies/canonical_cookie.h"
 #include "net/cookies/cookie_access_result.h"
 #include "net/cookies/cookie_options.h"
 #include "services/network/public/mojom/cookie_manager.mojom.h"
@@ -45,19 +35,6 @@ namespace cobalt {
 namespace migrate_storage_record {
 
 namespace {
-
-std::atomic_flag migration_attempted = ATOMIC_FLAG_INIT;
-
-using Task = base::OnceCallback<void(base::OnceClosure)>;
-
-void DoTasks(std::vector<Task> tasks) {
-  base::OnceClosure all_tasks_closure = base::DoNothing();
-  for (Task& task : base::Reversed(tasks)) {
-    all_tasks_closure =
-        base::BindOnce(std::move(task), std::move(all_tasks_closure));
-  }
-  std::move(all_tasks_closure).Run();
-}
 
 std::string GetApplicationKey(const GURL& url) {
   std::string encoded_url;
@@ -137,10 +114,10 @@ std::vector<std::unique_ptr<net::CanonicalCookie>> ToCanonicalCookies(
   return cookies;
 }
 
-std::vector<std::pair<std::string, std::string>> ToLocalStorageEntries(
-    const url::Origin& page_origin,
-    const cobalt::storage::Storage& storage) {
-  std::vector<std::pair<std::string, std::string>> entries;
+std::vector<std::unique_ptr<std::pair<std::string, std::string>>>
+ToLocalStorageItems(const url::Origin& page_origin,
+                    const cobalt::storage::Storage& storage) {
+  std::vector<std::unique_ptr<std::pair<std::string, std::string>>> entries;
   for (const auto& local_storages : storage.local_storages()) {
     GURL local_storage_origin(local_storages.serialized_origin());
     if (!local_storage_origin.SchemeIs("https") ||
@@ -150,161 +127,172 @@ std::vector<std::pair<std::string, std::string>> ToLocalStorageEntries(
 
     for (const auto& local_storage_entry :
          local_storages.local_storage_entries()) {
-      entries.push_back(std::make_pair<>(local_storage_entry.key(),
-                                         local_storage_entry.value()));
+      entries.emplace_back(new std::pair<std::string, std::string>(
+          local_storage_entry.key(), local_storage_entry.value()));
     }
   }
   return entries;
 }
 
-network::mojom::CookieManager* GetCookieManager(
+content::RenderFrameHost* RenderFrameHost(
     content::WeakDocumentPtr weak_document_ptr) {
   auto* render_frame_host = weak_document_ptr.AsRenderFrameHostIfValid();
   CHECK(render_frame_host);
-  auto* storage_partition = render_frame_host->GetStoragePartition();
+  return render_frame_host;
+}
+
+network::mojom::CookieManager* CookieManager(
+    content::WeakDocumentPtr weak_document_ptr) {
+  auto* storage_partition =
+      RenderFrameHost(weak_document_ptr)->GetStoragePartition();
   CHECK(storage_partition);
   auto* cookie_manager = storage_partition->GetCookieManagerForBrowserProcess();
   CHECK(cookie_manager);
   return cookie_manager;
 }
 
-void ClearCookies(content::WeakDocumentPtr weak_document_ptr,
-                  base::OnceClosure callback) {
-  GetCookieManager(weak_document_ptr)
-      ->DeleteCookies(
-          network::mojom::CookieDeletionFilter::New(),
-          base::BindOnce(
-              [](base::OnceClosure callback, uint32_t /* num_deleted */) {
-                std::move(callback).Run();
-              },
-              std::move(callback)));
+#if !defined(COBALT_IS_RELEASE_BUILD)
+Task LogElapsedTimeTask() {
+  return base::BindOnce(
+      [](std::unique_ptr<base::ElapsedTimer> elapsed_timer,
+         base::OnceClosure callback) {
+        LOG(INFO) << "Cookie and localStorage migration completed in "
+                  << elapsed_timer->Elapsed().InMilliseconds() << " ms.";
+        std::move(callback).Run();
+      },
+      std::make_unique<base::ElapsedTimer>());
 }
+#endif  // !defined(COBALT_IS_RELEASE_BUILD)
 
-void ClearLocalStorage(content::WeakDocumentPtr weak_document_ptr,
-                       base::OnceClosure callback) {
-  auto* render_frame_host = weak_document_ptr.AsRenderFrameHostIfValid();
-  CHECK(render_frame_host);
-  render_frame_host->ExecuteJavaScriptMethod(
-      base::ASCIIToUTF16(std::string("localStorage")),
-      base::ASCIIToUTF16(std::string("clear")), base::Value::List(),
-      base::BindOnce(
-          [](base::OnceClosure callback, base::Value /* result */) {
-            std::move(callback).Run();
-          },
-          std::move(callback)));
-}
-
-void MigrateCookiesHelper(
-    content::WeakDocumentPtr weak_document_ptr,
-    std::vector<std::unique_ptr<net::CanonicalCookie>> cookies,
-    base::OnceClosure callback,
-    net::CookieAccessResult /* set_cookie_result */) {
-  if (cookies.empty()) {
-    GetCookieManager(weak_document_ptr)->FlushCookieStore(std::move(callback));
-    return;
-  }
-
-  auto cookie = std::move(cookies.back());
-  cookies.pop_back();
-  GURL source_url("https://" + cookie->Domain() + cookie->Path());
-  GetCookieManager(weak_document_ptr)
-      ->SetCanonicalCookie(
-          *cookie, source_url, net::CookieOptions::MakeAllInclusive(),
-          base::BindOnce(&MigrateCookiesHelper, weak_document_ptr,
-                         std::move(cookies), std::move(callback)));
-}
-
-void MigrateCookies(content::WeakDocumentPtr weak_document_ptr,
-                    std::vector<std::unique_ptr<net::CanonicalCookie>> cookies,
-                    base::OnceClosure callback) {
-  if (cookies.empty()) {
-    std::move(callback).Run();
-    return;
-  }
-  MigrateCookiesHelper(weak_document_ptr, std::move(cookies),
-                       std::move(callback), net::CookieAccessResult());
-}
-
-void MigrateLocalStorageHelper(
-    content::WeakDocumentPtr weak_document_ptr,
-    std::vector<std::pair<std::string, std::string>> entries,
-    base::OnceClosure callback,
-    base::Value /* result */) {
-  if (entries.empty()) {
-    std::move(callback).Run();
-    return;
-  }
-
-  auto* render_frame_host = weak_document_ptr.AsRenderFrameHostIfValid();
-  CHECK(render_frame_host);
-
-  std::string key = entries.back().first;
-  std::string value = entries.back().second;
-  entries.pop_back();
-  base::Value::List arguments = base::Value::List().Append(key).Append(value);
-  render_frame_host->ExecuteJavaScriptMethod(
-      base::ASCIIToUTF16(std::string("localStorage")),
-      base::ASCIIToUTF16(std::string("setItem")), std::move(arguments),
-      base::BindOnce(&MigrateLocalStorageHelper, weak_document_ptr,
-                     std::move(entries), std::move(callback)));
-}
-
-void MigrateLocalStorage(
-    content::WeakDocumentPtr weak_document_ptr,
-    std::vector<std::pair<std::string, std::string>> entries,
-    base::OnceClosure callback) {
-  MigrateLocalStorageHelper(weak_document_ptr, std::move(entries),
-                            std::move(callback), base::Value());
-}
-
-void Reload(content::WeakDocumentPtr weak_document_ptr,
-            base::OnceClosure callback) {
-  auto* render_frame_host = weak_document_ptr.AsRenderFrameHostIfValid();
-  CHECK(render_frame_host);
-  render_frame_host->Reload();
-  std::move(callback).Run();
+Task ReloadTask(content::WeakDocumentPtr weak_document_ptr) {
+  return base::BindOnce(
+      [](content::WeakDocumentPtr weak_document_ptr,
+         base::OnceClosure callback) {
+        RenderFrameHost(weak_document_ptr)->Reload();
+        std::move(callback).Run();
+      },
+      weak_document_ptr);
 }
 
 }  // namespace
 
+// static
+std::atomic_flag MigrationManager::migration_attempted_ = ATOMIC_FLAG_INIT;
+
+// static
+Task MigrationManager::GroupTasks(std::vector<Task> tasks) {
+  return base::BindOnce(
+      [](std::vector<Task> tasks, base::OnceClosure callback) {
+        base::OnceClosure all_tasks_closure = std::move(callback);
+        for (Task& task : base::Reversed(tasks)) {
+          all_tasks_closure =
+              base::BindOnce(std::move(task), std::move(all_tasks_closure));
+        }
+        std::move(all_tasks_closure).Run();
+      },
+      std::move(tasks));
+}
+
 // TODO(b/399165612): Add metrics.
 // TODO(b/399165796): Disable and delete migration code when possible.
 // TODO(b/399166308): Add unit and/or integration tests for migration.
-void DoMigrationTasksOnce(content::WebContents* web_contents) {
-  if (migration_attempted.test_and_set()) {
+void MigrationManager::DoMigrationTasksOnce(
+    content::WebContents* web_contents) {
+  if (migration_attempted_.test_and_set()) {
     return;
   }
 
   auto storage = ReadStorage();
-  if (!storage) {
+  if (!storage ||
+      (storage->cookies_size() == 0 && storage->local_storages_size() == 0)) {
     return;
   }
 
   web_contents->Stop();
+
   auto* render_frame_host = web_contents->GetPrimaryMainFrame();
   CHECK(render_frame_host);
   auto weak_document_ptr = render_frame_host->GetWeakDocumentPtr();
-  auto entries = ToLocalStorageEntries(
-      render_frame_host->GetLastCommittedOrigin(), *storage);
-  auto cookies = ToCanonicalCookies(*storage);
+
   std::vector<Task> tasks;
-  tasks.push_back(base::BindOnce(&ClearCookies, weak_document_ptr));
-  tasks.push_back(base::BindOnce(&ClearLocalStorage, weak_document_ptr));
-  tasks.push_back(
-      base::BindOnce(&MigrateCookies, weak_document_ptr, std::move(cookies)));
-  tasks.push_back(base::BindOnce(&MigrateLocalStorage, weak_document_ptr,
-                                 std::move(entries)));
-  tasks.push_back(base::BindOnce(&Reload, weak_document_ptr));
+  tasks.push_back(CookieTask(weak_document_ptr, ToCanonicalCookies(*storage)));
+  const url::Origin& origin = render_frame_host->GetLastCommittedOrigin();
+  tasks.push_back(LocalStorageTask(weak_document_ptr,
+                                   ToLocalStorageItems(origin, *storage)));
 #if !defined(COBALT_IS_RELEASE_BUILD)
-  tasks.push_back(base::BindOnce(
-      [](std::unique_ptr<base::ElapsedTimer> elapsed_timer,
-         base::OnceClosure /* callback */) {
-        LOG(INFO) << "Cookie and localStorage migration completed in "
-                  << elapsed_timer->Elapsed().InMilliseconds() << " ms.";
-      },
-      std::make_unique<base::ElapsedTimer>()));
+  tasks.push_back(LogElapsedTimeTask());
 #endif  // !defined(COBALT_IS_RELEASE_BUILD)
-  DoTasks(std::move(tasks));
+  tasks.push_back(ReloadTask(weak_document_ptr));
+  std::move(GroupTasks(std::move(tasks))).Run(base::DoNothing());
+}
+
+// static
+Task MigrationManager::CookieTask(
+    content::WeakDocumentPtr weak_document_ptr,
+    std::vector<std::unique_ptr<net::CanonicalCookie>> cookies) {
+  std::vector<Task> tasks;
+  tasks.push_back(base::BindOnce(
+      [](content::WeakDocumentPtr weak_document_ptr,
+         base::OnceClosure callback) {
+        CookieManager(weak_document_ptr)
+            ->DeleteCookies(network::mojom::CookieDeletionFilter::New(),
+                            base::IgnoreArgs<uint32_t>(std::move(callback)));
+      },
+      weak_document_ptr));
+  for (auto& cookie : cookies) {
+    tasks.push_back(base::BindOnce(
+        [](content::WeakDocumentPtr weak_document_ptr,
+           std::unique_ptr<net::CanonicalCookie> cookie,
+           base::OnceClosure callback) {
+          GURL source_url("https://" + cookie->Domain() + cookie->Path());
+          CookieManager(weak_document_ptr)
+              ->SetCanonicalCookie(*cookie, source_url,
+                                   net::CookieOptions::MakeAllInclusive(),
+                                   base::IgnoreArgs<net::CookieAccessResult>(
+                                       std::move(callback)));
+        },
+        weak_document_ptr, std::move(cookie)));
+  }
+  tasks.push_back(base::BindOnce(
+      [](content::WeakDocumentPtr weak_document_ptr,
+         base::OnceClosure callback) {
+        CookieManager(weak_document_ptr)->FlushCookieStore(std::move(callback));
+      },
+      weak_document_ptr));
+  return GroupTasks(std::move(tasks));
+}
+
+// static
+Task MigrationManager::LocalStorageTask(
+    content::WeakDocumentPtr weak_document_ptr,
+    std::vector<std::unique_ptr<std::pair<std::string, std::string>>> pairs) {
+  std::vector<Task> tasks;
+  tasks.push_back(base::BindOnce(
+      [](content::WeakDocumentPtr weak_document_ptr,
+         base::OnceClosure callback) {
+        RenderFrameHost(weak_document_ptr)
+            ->ExecuteJavaScriptMethod(
+                base::ASCIIToUTF16(std::string("localStorage")),
+                base::ASCIIToUTF16(std::string("clear")), base::Value::List(),
+                base::IgnoreArgs<base::Value>(std::move(callback)));
+      },
+      weak_document_ptr));
+
+  for (auto& pair : pairs) {
+    tasks.push_back(base::BindOnce(
+        [](content::WeakDocumentPtr weak_document_ptr,
+           std::unique_ptr<std::pair<std::string, std::string>> pair,
+           base::OnceClosure callback) {
+          RenderFrameHost(weak_document_ptr)
+              ->ExecuteJavaScriptMethod(
+                  base::ASCIIToUTF16(std::string("localStorage")),
+                  base::ASCIIToUTF16(std::string("setItem")),
+                  base::Value::List().Append(pair->first).Append(pair->second),
+                  base::IgnoreArgs<base::Value>(std::move(callback)));
+        },
+        weak_document_ptr, std::move(pair)));
+  }
+  return GroupTasks(std::move(tasks));
 }
 
 }  // namespace migrate_storage_record
