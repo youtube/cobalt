f593f3fa5c449c9dc1f9b355d23740e21474ec84
Hao <131711973+haozheng-cobalt@users.noreply.github.com>
2025-03-07 21:28:57 -0500

    Migrates ByteBuffer return type functions in MediaCodecBridge (#5013)
    This PR migrates the ByteBuffer return type functions in
    MediaCodecBridge.
    
    Tested playing videos locally and inspected test log:
    https://paste.googleplex.com/5368973871546368
    https://paste.googleplex.com/6672999816888320
    
    ProcessOutputBuffer is not logged, I think it's because the emulator is
    not connected to an audio output. The change should be pretty safe.
    
    b/390481510

--- starboard/android/shared/jni_utils.h
+++ starboard/android/shared/jni_utils.h
@@ -56,32 +56,6 @@ class ScopedLocalJavaRef {
   void operator=(const ScopedLocalJavaRef&) = delete;
 };
 
-// Convenience class to manage the lifetime of a local Java ByteBuffer
-// reference, and provide accessors to its properties.
-class ScopedJavaByteBuffer {
- public:
-  explicit ScopedJavaByteBuffer(jobject j_byte_buffer)
-      : j_byte_buffer_(j_byte_buffer) {}
-  void* address() const {
-    return JniEnvExt::Get()->GetDirectBufferAddress(j_byte_buffer_.Get());
-  }
-  jint capacity() const {
-    return JniEnvExt::Get()->GetDirectBufferCapacity(j_byte_buffer_.Get());
-  }
-  bool IsNull() const { return !j_byte_buffer_ || !address(); }
-  void CopyInto(const void* source, jint count) {
-    SB_DCHECK(!IsNull());
-    SB_DCHECK(count >= 0 && count <= capacity());
-    memcpy(address(), source, count);
-  }
-
- private:
-  ScopedLocalJavaRef<jobject> j_byte_buffer_;
-
-  ScopedJavaByteBuffer(const ScopedJavaByteBuffer&) = delete;
-  void operator=(const ScopedJavaByteBuffer&) = delete;
-};
-
 }  // namespace shared
 }  // namespace android
 }  // namespace starboard
