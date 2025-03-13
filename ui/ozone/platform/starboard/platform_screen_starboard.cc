451e1ecf3f48f0c70576baf3a48f5e181d1ec39d
Sherry Zhou <sherryzy@google.com>
2025-03-13 14:21:15 -0700

    Removed obsolete TODO comments (#5074)
    1. Cobalt currently supports a single display configuration, remove
    display selection TODOs.
    2. Event support part is done, removes completed TODOs.
    
    b/371272304
    
    original-hexsha: fa2d41601b26597de2ba0de10619f2a8f33239a9

--- ui/ozone/platform/starboard/platform_screen_starboard.cc
+++ ui/ozone/platform/starboard/platform_screen_starboard.cc
@@ -24,29 +24,23 @@ PlatformScreenStarboard::~PlatformScreenStarboard() = default;
 
 const std::vector<display::Display>& PlatformScreenStarboard::GetAllDisplays()
     const {
-  // TODO(b/371272304): Initialize display_list_ in the constructor.
   return display_list_.displays();
 }
 
 display::Display PlatformScreenStarboard::GetPrimaryDisplay() const {
-  // TODO(b/371272304): Get the actual primary display from display_list_.
   return display::Display();
 }
 display::Display PlatformScreenStarboard::GetDisplayForAcceleratedWidget(
     gfx::AcceleratedWidget widget) const {
-  // TODO(b/371272304): Check if the widget exists within the current window and
-  // maps to a display. If not, return primary display.
   return GetPrimaryDisplay();
 }
 display::Display PlatformScreenStarboard::GetDisplayNearestPoint(
     const gfx::Point& point_in_dip) const {
-  // TODO(b/371272304): Iterate a display list and find the one nearest the
-  // point.
   return GetPrimaryDisplay();
 }
+
 display::Display PlatformScreenStarboard::GetDisplayMatching(
     const gfx::Rect& match_rect) const {
-  // TODO(b/371272304): Iterate a display list and find a matching display.
   return GetPrimaryDisplay();
 }
 
