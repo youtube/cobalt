2d20872be2c289e1f5a54126b646186f2777cec3
Jelle Foks <jfoks@google.com>
2025-03-17 10:34:26 -0700

    [Evergreen] Basic changes for base_unittests to start working (#5084)
    This adds some pthread wrapper implementations and adds
    base_paths_starboard.cc
    
    ```
    $ out/linux-x64x11-evergreen_devel/base_unittests_loader --gtest_filter="-BreakIteratorTest.*:MessageFormatterTest.*:ICUStringConversionsTest.*:CharacterEncodingTest.*:NumberFormattingTest.*:RTLTest.*:StringSearchTest.*:TimeFormattingTest.*:TimezoneTest.*:LoggingTest.*:ThreadPoolWorkerThreadCachePurgeTest.*:PostTaskAndReplyImplTest.PostTaskToStoppedTaskRunnerWithoutSequencedContext"
    [==========] 5400 tests from 502 test suites ran. (85389 ms total)
    [  PASSED  ] 5365 tests.
    [  FAILED  ] 20 tests, listed below:
    [snip]
    20 FAILED TESTS
      YOU HAVE 26 DISABLED TESTS
    ```
    
    b/399696581
    
    original-hexsha: fd48abc7a71ae9c1fbb7ca8aed52a9dd98945aa5

--- base/path_service.cc
+++ base/path_service.cc
@@ -34,7 +34,9 @@ bool EnvOverridePathProvider(int key, FilePath* result);
 
 bool PathProvider(int key, FilePath* result);
 
-#if BUILDFLAG(IS_WIN)
+#if BUILDFLAG(IS_COBALT_HERMETIC_BUILD)
+bool PathProviderStarboard(int key, FilePath* result);
+#elif BUILDFLAG(IS_WIN)
 bool PathProviderWin(int key, FilePath* result);
 #elif BUILDFLAG(IS_MAC)
 bool PathProviderMac(int key, FilePath* result);
@@ -74,6 +76,14 @@ Provider base_provider = {PathProvider, nullptr,
 #endif
                           true};
 
+#if BUILDFLAG(IS_COBALT_HERMETIC_BUILD)
+Provider base_provider_starboard = {base::PathProviderStarboard, &base_provider,
+#ifndef NDEBUG
+                                    base::PATH_STARBOARD_START,
+                                    base::PATH_STARBOARD_END,
+#endif
+                                    true};
+#else  // BUILDFLAG(IS_COBALT_HERMETIC_BUILD)
 #if BUILDFLAG(IS_WIN)
 Provider win_provider = {PathProviderWin, &base_provider,
 #ifndef NDEBUG
@@ -143,7 +153,7 @@ Provider base_provider_posix = {EnvOverridePathProvider, &posix_provider,
 #endif
                                 true};
 #endif
-
+#endif  // BUILDFLAG(IS_COBALT_HERMETIC_BUILD)
 
 struct PathData {
   Lock lock;
@@ -153,7 +163,9 @@ struct PathData {
   bool cache_disabled;  // Don't use cache if true;
 
   PathData() : cache_disabled(false) {
-#if BUILDFLAG(IS_WIN)
+#if BUILDFLAG(IS_COBALT_HERMETIC_BUILD)
+    providers = &base_provider_starboard;
+#elif BUILDFLAG(IS_WIN)
     providers = &base_provider_win;
 #elif BUILDFLAG(IS_MAC)
     providers = &base_provider_mac;
