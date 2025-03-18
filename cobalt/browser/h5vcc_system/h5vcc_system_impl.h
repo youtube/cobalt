b53321cab3b5c5dd067e24bab63280f7d9680baf
Andrew Savage <andrewsavage@google.com>
2025-03-18 11:52:04 -0400

    Add sync advertising properties (#5082)
    Needed by clients to avoid performance regressions.
    
    Also add stub trackingAuthorizationStatus and
    requestTrackingAuthorization functions.
    
    b/395650827
    original-hexsha: 4588f30eb7fe8ef5e3a70baf332d17af86c150ce

--- cobalt/browser/h5vcc_system/h5vcc_system_impl.h
+++ cobalt/browser/h5vcc_system/h5vcc_system_impl.h
@@ -17,6 +17,7 @@
 
 #include <string>
 
+#include "base/threading/thread_checker.h"
 #include "cobalt/browser/h5vcc_system/public/mojom/h5vcc_system.mojom.h"
 #include "content/public/browser/document_service.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -41,13 +42,24 @@ class H5vccSystemImpl : public content::DocumentService<mojom::H5vccSystem> {
   H5vccSystemImpl& operator=(const H5vccSystemImpl&) = delete;
 
   void GetAdvertisingId(GetAdvertisingIdCallback) override;
+  void GetAdvertisingIdSync(GetAdvertisingIdSyncCallback) override;
   void GetLimitAdTracking(GetLimitAdTrackingCallback) override;
+  void GetLimitAdTrackingSync(GetLimitAdTrackingSyncCallback) override;
+  void GetTrackingAuthorizationStatus(
+      GetTrackingAuthorizationStatusCallback) override;
+  void GetTrackingAuthorizationStatusSync(
+      GetTrackingAuthorizationStatusSyncCallback) override;
+  void RequestTrackingAuthorization(
+      RequestTrackingAuthorizationCallback) override;
   void GetUserOnExitStrategy(GetUserOnExitStrategyCallback) override;
   void Exit() override;
 
  private:
   H5vccSystemImpl(content::RenderFrameHost& render_frame_host,
                   mojo::PendingReceiver<mojom::H5vccSystem> receiver);
+  ~H5vccSystemImpl();
+
+  THREAD_CHECKER(thread_checker_);
 };
 
 }  // namespace h5vcc_system
