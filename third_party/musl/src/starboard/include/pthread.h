bbabf3d95e0aeb73b797ef3b72e14474aa18926b
Niranjan Yardi <nyardi@google.com>
2025-03-18 17:44:42 -0700

    fix linter

--- third_party/musl/src/starboard/include/pthread.h
+++ third_party/musl/src/starboard/include/pthread.h
@@ -166,10 +166,8 @@ typedef union pthread_once_t {
   void* ptr;
 } pthread_once_t;
 
-
-
 // Max size of the pthread_rwlock_t type.
-#define MUSL_PTHREAD_RWLOCK_MAX_SIZE 80 
+#define MUSL_PTHREAD_RWLOCK_MAX_SIZE 80
 
 // An opaque handle to pthread_rwlock_t type with
 // aligned at void  pointer type.
@@ -183,7 +181,7 @@ typedef union pthread_rwlock_t {
 } pthread_rwlock_t;
 
 // Max size of the pthread_rwlockattr_t type.
-#define MUSL_PTHREAD_RWLOCK_ATTR_MAX_SIZE 80 
+#define MUSL_PTHREAD_RWLOCK_ATTR_MAX_SIZE 80
 
 // An opaque handle to pthread_rwlockattr_t type with
 // aligned at void  pointer type.
@@ -195,8 +193,6 @@ typedef union pthread_rwlockattr_t {
   // Guarantees alignment of the type to a void pointer.
   void* ptr;
 } pthread_rwlockattr_t;
-
-
 
 #ifdef __cplusplus
 #define PTHREAD_ONCE_INIT \
@@ -286,16 +282,18 @@ int pthread_rwlock_unlock(pthread_rwlock_t*);
 int pthread_rwlock_tryrdlock(pthread_rwlock_t*);
 int pthread_rwlock_trywrlock(pthread_rwlock_t*);
 
-
-int pthread_rwlock_init(pthread_rwlock_t *__restrict, const pthread_rwlockattr_t *__restrict);
-int pthread_rwlock_destroy(pthread_rwlock_t *);
-int pthread_rwlock_rdlock(pthread_rwlock_t *);
-int pthread_rwlock_tryrdlock(pthread_rwlock_t *);
-int pthread_rwlock_timedrdlock(pthread_rwlock_t *__restrict, const struct timespec *__restrict);
-int pthread_rwlock_wrlock(pthread_rwlock_t *);
-int pthread_rwlock_trywrlock(pthread_rwlock_t *);
-int pthread_rwlock_timedwrlock(pthread_rwlock_t *__restrict, const struct timespec *__restrict);
-int pthread_rwlock_unlock(pthread_rwlock_t *);
+int pthread_rwlock_init(pthread_rwlock_t* __restrict,
+                        const pthread_rwlockattr_t* __restrict);
+int pthread_rwlock_destroy(pthread_rwlock_t*);
+int pthread_rwlock_rdlock(pthread_rwlock_t*);
+int pthread_rwlock_tryrdlock(pthread_rwlock_t*);
+int pthread_rwlock_timedrdlock(pthread_rwlock_t* __restrict,
+                               const struct timespec* __restrict);
+int pthread_rwlock_wrlock(pthread_rwlock_t*);
+int pthread_rwlock_trywrlock(pthread_rwlock_t*);
+int pthread_rwlock_timedwrlock(pthread_rwlock_t* __restrict,
+                               const struct timespec* __restrict);
+int pthread_rwlock_unlock(pthread_rwlock_t*);
 #ifdef __cplusplus
 }  // extern "C"
 #endif
