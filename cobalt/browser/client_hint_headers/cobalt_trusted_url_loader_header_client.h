cf54bebefc2abd20a40240e249adf39d9ea36e2b
Colin Liang <zhongqi.liang.4u@gmail.com>
2025-03-20 14:49:32 -0700

    Use TrustedHeaderClient to add Cobalt Client Hint header (#5102)
    Support custom HTTP headers to outgoing network requests originating
    from Cobalt. It leverages the Chromium TrustedHeaderClient Mojo
    interface to achieve this without directly modifying the network
    service.
    
    1. Added CobaltTrustedHeaderClient to implements the
    network::mojom::TrustedHeaderClient interface.
    2. Hook up CobaltTrustedURLLoaderHeaderClient with
    ContentBrowserClient::WillCreateURLLoaderFactory().
    3. Added CobaltHeaderValueProvider, it is a singleton class is
    introduced to fetch and cache the values for the custom client hint
    headers.
    
    The Cobalt 25 client hint headers are not hooked up into
    CobaltHeaderValueProvider in this PR.
    
    b/400459822
    
    ---------
    
    Co-authored-by: Colin Liang <colinliang@google.com>
    
    original-hexsha: 6d06d0649cad6e90d7286a9e0eb2fae1c413ae56

---
+++ cobalt/browser/client_hint_headers/cobalt_trusted_url_loader_header_client.h
@@ -0,0 +1,61 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef COBALT_BROWSER_CLIENT_HINT_HEADERS_COBALT_TRUSTED_URL_LOADER_HEADER_CLIENT_H_
+#define COBALT_BROWSER_CLIENT_HINT_HEADERS_COBALT_TRUSTED_URL_LOADER_HEADER_CLIENT_H_
+
+#include "cobalt/browser/client_hint_headers/cobalt_trusted_header_client.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "services/network/public/mojom/network_context.mojom.h"
+
+namespace cobalt {
+namespace browser {
+
+class CobaltTrustedURLLoaderHeaderClient
+    : public network::mojom::TrustedURLLoaderHeaderClient {
+ public:
+  CobaltTrustedURLLoaderHeaderClient(
+      mojo::PendingReceiver<network::mojom::TrustedURLLoaderHeaderClient>
+          receiver);
+
+  CobaltTrustedURLLoaderHeaderClient(
+      const CobaltTrustedURLLoaderHeaderClient&) = delete;
+  CobaltTrustedURLLoaderHeaderClient& operator=(
+      const CobaltTrustedURLLoaderHeaderClient&) = delete;
+
+  ~CobaltTrustedURLLoaderHeaderClient() override = default;
+
+  // network::mojom::TrustedURLLoaderHeaderClient:
+  void OnLoaderCreated(
+      int32_t request_id,
+      mojo::PendingReceiver<network::mojom::TrustedHeaderClient> receiver)
+      override;
+  void OnLoaderForCorsPreflightCreated(
+      const network::ResourceRequest& request,
+      mojo::PendingReceiver<network::mojom::TrustedHeaderClient> receiver)
+      override;
+
+ private:
+  void CreateAndBindCobaltTrustedHeaderClient(
+      mojo::PendingReceiver<network::mojom::TrustedHeaderClient> receiver);
+
+  mojo::Receiver<network::mojom::TrustedURLLoaderHeaderClient> receiver_;
+  std::vector<std::unique_ptr<CobaltTrustedHeaderClient>>
+      cobalt_header_clients_;
+};
+
+}  // namespace browser
+}  // namespace cobalt
+
+#endif  // COBALT_BROWSER_CLIENT_HINT_HEADERS_COBALT_TRUSTED_URL_LOADER_HEADER_CLIENT_H_
