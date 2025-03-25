09922eb82745d5bfa79e662f0db09cda7e0dc6a2
Andrew Savage <andrewsavage@google.com>
2025-03-25 12:07:21 -0400

    Allow hermetic net and net_unittests builds (#5157)
    b/397476666
    original-hexsha: 681a137a7cd20159f28df86135ddac50c3c67b5d

--- net/base/network_change_notifier.cc
+++ net/base/network_change_notifier.cc
@@ -31,7 +31,9 @@
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/gurl.h"
 
-#if BUILDFLAG(IS_WIN)
+#if BUILDFLAG(IS_STARBOARD)
+#include "net/base/network_change_notifier_starboard.h"
+#elif BUILDFLAG(IS_WIN)
 #include "net/base/network_change_notifier_win.h"
 #elif BUILDFLAG(IS_LINUX)
 #include "net/base/network_change_notifier_linux.h"
@@ -307,7 +309,9 @@ std::unique_ptr<NetworkChangeNotifier> NetworkChangeNotifier::CreateIfNeeded(
         initial_type, initial_subtype);
   }
 
-#if BUILDFLAG(IS_WIN)
+#if BUILDFLAG(IS_STARBOARD)
+return std::make_unique<NetworkChangeNotifierStarboard>();
+#elif BUILDFLAG(IS_WIN)
   std::unique_ptr<NetworkChangeNotifierWin> network_change_notifier =
       std::make_unique<NetworkChangeNotifierWin>();
   network_change_notifier->WatchForAddressChange();
