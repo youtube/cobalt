97f0dafce6f4724c7a667e9b10849dd0266053a8
Kyujung Youn <kjyoun@google.com>
2025-03-26 16:17:05 -0700

    Use absl::flat_hash_map for decoding_buffers (#5185)
    Use `absl::flat_hash_map` for decoding_buffers
    
    chromium guide[1] recommends to use `absl::flat_hash_map` over
    `std::map` for the small set of data, while discouraging the use of
    `std::unordered_map`. Test runs[2] showed `flat_hash_map` indeed
    performs better than other alternatives.
    
    This CL also includes some code refactoring to improve readability and
    use the latest API (e.g. try_emplace)
    
    b/403712928
    
    ---
    [1]
    https://chromium.googlesource.com/chromium/src/+/main/base/containers/README.md#map-and-set-selection-usage-advice
    [2] http://b/403712928#comment7
    
    original-hexsha: c2e7313f7e0ca8c8f25be7b195a83500219338e1

--- media/starboard/sbplayer_bridge.h
+++ media/starboard/sbplayer_bridge.h
@@ -15,7 +15,6 @@
 #ifndef MEDIA_STARBOARD_SBPLAYER_BRIDGE_H_
 #define MEDIA_STARBOARD_SBPLAYER_BRIDGE_H_
 
-#include <map>
 #include <string>
 #include <utility>
 #include <vector>
@@ -47,6 +46,7 @@
 #include "media/starboard/sbplayer_set_bounds_helper.h"
 #include "starboard/media.h"
 #include "starboard/player.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
 #include "ui/gfx/geometry/rect.h"
 
 namespace media {
@@ -203,11 +203,14 @@ class SbPlayerBridge {
   static const int64_t kClearDecoderCacheIntervalInMilliseconds = 1000;
 
   // A map from raw data pointer returned by DecoderBuffer::GetData() to the
-  // DecoderBuffer and a reference count.  The reference count indicates how
-  // many instances of the DecoderBuffer is currently being decoded in the
-  // pipeline.
-  typedef std::map<const void*, std::pair<scoped_refptr<DecoderBuffer>, int>>
-      DecodingBuffers;
+  // DecoderBuffer and an usage count.  The usage count indicates how
+  // many instances of the DecoderBuffer is currently being used (== being
+  // decoded) in the pipeline.
+  struct DecodingBuffer {
+    const scoped_refptr<DecoderBuffer> buffer;
+    int usage_count;
+  };
+  using DecodingBuffers = absl::flat_hash_map<const void*, DecodingBuffer>;
 
 #if SB_HAS(PLAYER_WITH_URL)
   OnEncryptedMediaInitDataEncounteredCB
