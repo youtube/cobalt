youtube/cobalt

commit 2be0a40968649d6ee2d0bd09e9aaae2dbe7a04ff

Cache initial_deep_link_ in the renderer (#5153)
Option 3 for 374147993#comment20.

b/374147993

---------

Co-authored-by: Colin Liang <colinliang@google.com>

third_party/blink/renderer/modules/cobalt/h5vcc_runtime/h_5_vcc_runtime.cc
@@ -24,7 +24,10 @@ namespace blink {
 H5vccRuntime::H5vccRuntime(LocalDOMWindow& window)
     : ExecutionContextLifecycleObserver(window.GetExecutionContext()),
       remote_h5vcc_runtime_(window.GetExecutionContext()),
-      receiver_(this, window.GetExecutionContext()) {}
+      receiver_(this, window.GetExecutionContext()) {
+  EnsureReceiverIsBound();
+  remote_h5vcc_runtime_->GetAndClearInitialDeepLinkSync(&initial_deep_link_);
+}
 
 void H5vccRuntime::ContextDestroyed() {
   remote_h5vcc_runtime_.reset();
@@ -32,30 +35,7 @@ void H5vccRuntime::ContextDestroyed() {
 }
 
 String H5vccRuntime::initialDeepLink() {
-  EnsureReceiverIsBound();
-  String initial_deep_link;
-  remote_h5vcc_runtime_->GetAndClearInitialDeepLinkSync(&initial_deep_link);
-  return initial_deep_link;
-}
-
-ScriptPromise H5vccRuntime::getInitialDeepLink(
-    ScriptState* script_state,
-    ExceptionState& exception_state) {
-  auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(
-      script_state, exception_state.GetContext());
-
-  EnsureReceiverIsBound();
-
-  remote_h5vcc_runtime_->GetAndClearInitialDeepLink(
-      WTF::BindOnce(&H5vccRuntime::OnGetInitialDeepLink, WrapPersistent(this),
-                    WrapPersistent(resolver)));
-
-  return resolver->Promise();
-}
-
-void H5vccRuntime::OnGetInitialDeepLink(ScriptPromiseResolver* resolver,
-                                        const String& result) {
-  resolver->Resolve(result);
+  return initial_deep_link_;
 }
 
 EventListener* H5vccRuntime::ondeeplink() {

third_party/blink/renderer/modules/cobalt/h5vcc_runtime/h_5_vcc_runtime.h
@@ -51,7 +51,6 @@ class MODULES_EXPORT H5vccRuntime final
 
   // Web-exposed interface:
   String initialDeepLink();
-  ScriptPromise getInitialDeepLink(ScriptState*, ExceptionState&);
   EventListener* ondeeplink();
   void setOndeeplink(EventListener* listener);
 
@@ -69,13 +68,13 @@ class MODULES_EXPORT H5vccRuntime final
   void Trace(Visitor*) const override;
 
  private:
-  void OnGetInitialDeepLink(ScriptPromiseResolver*, const String&);
   void MaybeFireDeepLinkEvent(const String&);
   void EnsureReceiverIsBound();
   HeapMojoRemote<h5vcc_runtime::mojom::blink::H5vccRuntime>
       remote_h5vcc_runtime_;
   HeapMojoReceiver<h5vcc_runtime::mojom::blink::DeepLinkListener, H5vccRuntime>
       receiver_;
+  String initial_deep_link_;
 };
 
 }  // namespace blink

third_party/blink/renderer/modules/cobalt/h5vcc_runtime/h_5_vcc_runtime.idl
@@ -19,15 +19,6 @@
     SecureContext
 ]
 interface H5vccRuntime : EventTarget {
-    // A synchronous version of the initialDeepLink API was provided for
-    // ease-of-use in the clients during the initial migration from Cobalt 25.
-    // Asynchronous APIs are preferred here as they offload work and won't
-    // affect Cobalt performance as much.
-    // We mark the synchronous APIs as nullable to signify we may deprecate
-    // them in the future in favor of the asynchronous equivalents.
     readonly attribute DOMString? initialDeepLink;
-    [CallWith=ScriptState, RaisesException]
-    Promise<DOMString> getInitialDeepLink();
-
     attribute EventHandler ondeeplink;
 };

commit 62633138e2b08974ef8bd72d4674b70057c46033

[CONFLICT] Original Commit: cafed3993429736f0045716ba680b70fb686eaf0
Original Message:
Add binder for H5vccRuntime to fix other web tests (#5183)

This is a fix-forward of a regression from #5153 that broke other web
tests, for example under cobalt/crash-log. I believe the tests under
cobalt/h5vcc-runtime, which were already broken (b/405993906), will
still be able to override the binding for H5vccRuntime using MojoJS. I

b/374147993
original-hexsha: 739078c0cd753b2daedfd792e1af208a07b32913


Files with conflicts:
- content/web_test/BUILD.gn
- content/web_test/browser/web_test_content_browser_client.cc
- content/web_test/browser/web_test_content_browser_client.h

cobalt/testing/h5vcc_runtime/BUILD.gn
@@ -1,0 +1,21 @@
+# Copyright 2025 The Cobalt Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+source_set("stub_h5vcc_runtime") {
+  sources = [
+    "stub_h5vcc_runtime_impl.cc",
+    "stub_h5vcc_runtime_impl.h",
+  ]
+  deps = [ "//cobalt/browser/h5vcc_runtime/public/mojom" ]
+}

cobalt/testing/h5vcc_runtime/stub_h5vcc_runtime_impl.cc
@@ -1,0 +1,45 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "cobalt/testing/h5vcc_runtime/stub_h5vcc_runtime_impl.h"
+
+#include <utility>
+
+namespace content {
+
+StubH5vccRuntimeImpl::StubH5vccRuntimeImpl() = default;
+
+StubH5vccRuntimeImpl::~StubH5vccRuntimeImpl() = default;
+
+void StubH5vccRuntimeImpl::Bind(
+    mojo::PendingReceiver<h5vcc_runtime::mojom::H5vccRuntime> receiver) {
+  receivers_.Add(this, std::move(receiver));
+}
+
+void StubH5vccRuntimeImpl::GetAndClearInitialDeepLinkSync(
+    GetAndClearInitialDeepLinkSyncCallback callback) {
+  // Prevents a dropped callback error.
+  std::move(callback).Run("");
+}
+
+void StubH5vccRuntimeImpl::GetAndClearInitialDeepLink(
+    GetAndClearInitialDeepLinkCallback callback) {
+  // Prevents a dropped callback error.
+  std::move(callback).Run("");
+}
+
+void StubH5vccRuntimeImpl::AddListener(
+    mojo::PendingRemote<h5vcc_runtime::mojom::DeepLinkListener> listener) {}
+
+}  // namespace content

cobalt/testing/h5vcc_runtime/stub_h5vcc_runtime_impl.h
@@ -1,0 +1,64 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef COBALT_TESTING_H5VCC_RUNTIME_STUB_H5VCC_RUNTIME_IMPL_H_
+#define COBALT_TESTING_H5VCC_RUNTIME_STUB_H5VCC_RUNTIME_IMPL_H_
+
+#include "cobalt/browser/h5vcc_runtime/public/mojom/h5vcc_runtime.mojom.h"
+#include "mojo/public/cpp/bindings/receiver_set.h"
+
+namespace content {
+
+// Provides a stub implementation of the H5vccRuntime Mojo interface.
+//
+// This implementation does nothing useful but is used to provide a binder for
+// the interface when an is_cobalt build of content_shell is run by
+// run_web_tests.py. A binder is required for this interface because the
+// constructor for blink::H5vccRuntime attempts to use the remote. The tests
+// under third_party/blink/web_tests/wpt_internal/cobalt/h5vcc-runtime use
+// MojoJS to intercept requests for the H5vccRuntime interface but cobalt's
+// other web tests rely on
+// WebTestContentBrowserClient::RegisterBrowserInterfaceBindersForFrame to
+// provide the binding, using this stub.
+//
+// See content::MockBadgeService as an example of a similar class from upstream.
+class StubH5vccRuntimeImpl : public h5vcc_runtime::mojom::H5vccRuntime {
+ public:
+  StubH5vccRuntimeImpl();
+  StubH5vccRuntimeImpl(const StubH5vccRuntimeImpl&) = delete;
+  StubH5vccRuntimeImpl& operator=(const StubH5vccRuntimeImpl&) = delete;
+  ~StubH5vccRuntimeImpl() override;
+
+  void Bind(mojo::PendingReceiver<h5vcc_runtime::mojom::H5vccRuntime> receiver);
+
+  // h5vcc_runtime::mojom::H5vccRuntime impl.
+  void GetAndClearInitialDeepLinkSync(
+      GetAndClearInitialDeepLinkSyncCallback callback) override;
+  void GetAndClearInitialDeepLink(
+      GetAndClearInitialDeepLinkCallback callback) override;
+  void AddListener(mojo::PendingRemote<h5vcc_runtime::mojom::DeepLinkListener>
+                       listener) override;
+
+ private:
+  // Although we don't expect multiple clients to connect, ReceiverSet is used
+  // instead of a Receiver to maintain consistency with upstream examples such
+  // as content::MockBadgeService. content::MockBadgeService also doesn't seem
+  // to have multiple clients connect, so ReceiverSet may have just been deemed
+  // easier to use in the mock/stub Mojo implementations and their clients.
+  mojo::ReceiverSet<h5vcc_runtime::mojom::H5vccRuntime> receivers_;
+};
+
+}  // namespace content
+
+#endif  // COBALT_TESTING_H5VCC_RUNTIME_STUB_H5VCC_RUNTIME_IMPL_H_

content/web_test/BUILD.gn
@@ -258,6 +258,20 @@ static_library("web_test_browser") {
   if (use_ozone) {
     deps += [ "//ui/ozone" ]
   }
+<<<<<<< HEAD
+=======
+
+  if (is_fuchsia) {
+    deps += [ "//third_party/fuchsia-sdk/sdk/fidl/fuchsia.ui.scenic:fuchsia.ui.scenic_hlcpp" ]
+  }
+
+  if (is_cobalt) {
+    deps += [
+      "//cobalt/browser/h5vcc_runtime/public/mojom",
+      "//cobalt/testing/h5vcc_runtime:stub_h5vcc_runtime",
+    ]
+  }
+>>>>>>> cafed399342 (Add binder for H5vccRuntime to fix other web tests (#5183))
 }
 
 # TODO(crbug.com/40159584): The `allow_circular_includes_from` declared in

content/web_test/browser/web_test_content_browser_client.cc
@@ -105,6 +105,11 @@
 #include "sandbox/win/src/sandbox.h"
 #endif
 
+#if BUILDFLAG(IS_COBALT)
+#include "cobalt/browser/h5vcc_runtime/public/mojom/h5vcc_runtime.mojom.h"
+#include "cobalt/testing/h5vcc_runtime/stub_h5vcc_runtime_impl.h"
+#endif
+
 namespace content {
 namespace {
 
@@ -571,6 +576,7 @@ void WebTestContentBrowserClient::RegisterBrowserInterfaceBindersForFrame(
       base::BindRepeating(
           &WebTestContentBrowserClient::BindDevicePostureProviderAutomation,
           base::Unretained(this)));
+<<<<<<< HEAD
   map->Add<blink::test::mojom::FederatedAuthRequestAutomation>(
       base::BindRepeating(&WebTestContentBrowserClient::BindFedCmAutomation,
                           base::Unretained(this)));
@@ -584,6 +590,12 @@ void WebTestContentBrowserClient::RegisterBrowserInterfaceBindersForFrame(
           &WebTestContentBrowserClient::BindWebPressureManagerAutomation,
           base::Unretained(this)));
 #endif  // BUILDFLAG(ENABLE_COMPUTE_PRESSURE)
+=======
+#if BUILDFLAG(IS_COBALT)
+  map->Add<h5vcc_runtime::mojom::H5vccRuntime>(base::BindRepeating(
+      &WebTestContentBrowserClient::BindH5vccRuntime, base::Unretained(this)));
+#endif
+>>>>>>> cafed399342 (Add binder for H5vccRuntime to fix other web tests (#5183))
 }
 
 bool WebTestContentBrowserClient::CanAcceptUntrustedExchangesIfNeeded() {
@@ -642,6 +654,7 @@ void WebTestContentBrowserClient::BindDevicePostureProviderAutomation(
       std::move(receiver));
 }
 
+<<<<<<< HEAD
 void WebTestContentBrowserClient::BindFedCmAutomation(
     RenderFrameHost* render_frame_host,
     mojo::PendingReceiver<blink::test::mojom::FederatedAuthRequestAutomation>
@@ -675,6 +688,17 @@ void WebTestContentBrowserClient::BindWebPressureManagerAutomation(
       ->Bind(std::move(receiver));
 }
 #endif  // BUILDFLAG(ENABLE_COMPUTE_PRESSURE)
+=======
+#if BUILDFLAG(IS_COBALT)
+void WebTestContentBrowserClient::BindH5vccRuntime(
+    RenderFrameHost* render_frame_host,
+    mojo::PendingReceiver<h5vcc_runtime::mojom::H5vccRuntime> receiver) {
+  if (!stub_h5vcc_runtime_impl_)
+    stub_h5vcc_runtime_impl_ = std::make_unique<StubH5vccRuntimeImpl>();
+  stub_h5vcc_runtime_impl_->Bind(std::move(receiver));
+}
+#endif
+>>>>>>> cafed399342 (Add binder for H5vccRuntime to fix other web tests (#5183))
 
 std::unique_ptr<LoginDelegate> WebTestContentBrowserClient::CreateLoginDelegate(
     const net::AuthChallengeInfo& auth_info,

content/web_test/browser/web_test_content_browser_client.h
@@ -28,6 +28,10 @@
 #include "third_party/blink/public/mojom/storage_access/storage_access_automation.mojom-forward.h"
 #include "third_party/blink/public/mojom/webid/federated_auth_request_automation.mojom-forward.h"
 
+#if BUILDFLAG(IS_COBALT)
+#include "cobalt/browser/h5vcc_runtime/public/mojom/h5vcc_runtime.mojom-forward.h"
+#endif
+
 namespace blink {
 namespace web_pref {
 struct WebPreferences;
@@ -42,6 +46,10 @@ class MockBadgeService;
 class MockClipboardHost;
 class WebTestBrowserContext;
 class WebTestSensorProviderManager;
+
+#if BUILDFLAG(IS_COBALT)
+class StubH5vccRuntimeImpl;
+#endif
 
 class WebTestContentBrowserClient : public ShellContentBrowserClient {
  public:
@@ -191,8 +199,16 @@ class WebTestContentBrowserClient : public ShellContentBrowserClient {
       int render_process_id,
       mojo::PendingAssociatedReceiver<mojom::WebTestControlHost> receiver);
 
+<<<<<<< HEAD
   void BindNonAssociatedWebTestControlHost(
       mojo::PendingReceiver<mojom::NonAssociatedWebTestControlHost> receiver);
+=======
+#if BUILDFLAG(IS_COBALT)
+  void BindH5vccRuntime(
+      RenderFrameHost* render_frame_host,
+      mojo::PendingReceiver<h5vcc_runtime::mojom::H5vccRuntime> receiver);
+#endif
+>>>>>>> cafed399342 (Add binder for H5vccRuntime to fix other web tests (#5183))
 
   bool block_popups_ = true;
 
@@ -206,8 +222,17 @@ class WebTestContentBrowserClient : public ShellContentBrowserClient {
   std::unique_ptr<WebTestSensorProviderManager> sensor_provider_manager_;
   mojo::UniqueReceiverSet<blink::test::mojom::CookieManagerAutomation>
       cookie_managers_;
+<<<<<<< HEAD
   mojo::UniqueReceiverSet<blink::test::mojom::FederatedAuthRequestAutomation>
       fedcm_managers_;
+=======
+  mojo::UniqueReceiverSet<blink::test::mojom::AttributionReportingAutomation>
+      attribution_reporting_receivers_;
+
+#if BUILDFLAG(IS_COBALT)
+  std::unique_ptr<StubH5vccRuntimeImpl> stub_h5vcc_runtime_impl_;
+#endif
+>>>>>>> cafed399342 (Add binder for H5vccRuntime to fix other web tests (#5183))
 };
 
 }  // namespace content