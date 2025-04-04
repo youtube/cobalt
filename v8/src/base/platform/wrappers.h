38c7e84b5e475708b2af428aefa3996c3549849b
Niranjan Yardi <nyardi@google.com>
2025-04-04 13:59:13 -0700

    More changess from the stash

--- v8/src/base/platform/wrappers.h
+++ v8/src/base/platform/wrappers.h
@@ -10,9 +10,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#ifdef V8_OS_STARBOARD
-#include "starboard/string.h"
-#endif
+// ../../v8/src/base/platform/wrappers.h:14:10: fatal error: 'starboard/string.h' file not found
+// #ifdef V8_OS_STARBOARD
+// #include "starboard/string.h"
+// #endif
 
 namespace v8::base {
 
