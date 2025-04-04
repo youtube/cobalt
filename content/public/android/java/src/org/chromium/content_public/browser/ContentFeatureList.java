92fac8791c882719afe820f00a51b591af667614
Hao <131711973+haozheng-cobalt@users.noreply.github.com>
2025-04-04 18:04:13 -0400

    Stop creating a11y node provder for Chrobalt (#5271)
    This is a better Chromium a11y tree bypass than before.
    Before we only bypassed navigation for WebViews, and we would ran into
    focus issues with videos which are VideoSurfaceView. This PR stops
    creating creating a11y node provder for Chrobalt.
    
    Test: https://paste.googleplex.com/4565574762627072
    
    b/391708407
    original-hexsha: 927dc8ab80334d58bef1163c7e2da0fce45f77ee

--- content/public/android/java/src/org/chromium/content_public/browser/ContentFeatureList.java
+++ content/public/android/java/src/org/chromium/content_public/browser/ContentFeatureList.java
@@ -30,8 +30,11 @@ public class ContentFeatureList {
             "BackgroundMediaRendererHasModerateBinding";
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     public static final String MOUSE_AND_TRACKPAD_DROPDOWN_MENU = "MouseAndTrackpadDropdownMenu";
 =======
+=======
+>>>>>>> ebd25df73d57 (Stop creating a11y node provder for Chrobalt (#5271))
     public static final String DO_NOT_GENERATE_CHROMIUM_A11Y_TREE = "DoNotGenerateChromiumA11yTree";
 
     public static final String ON_DEMAND_ACCESSIBILITY_EVENTS = "OnDemandAccessibilityEvents";
