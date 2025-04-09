a86c930dc17f20188f56fcd054aa84f082255b9f
xiaomings <xiaomings@google.com>
2025-04-09 13:10:09 -0700

    [media] Improve DecoderBufferAllocator logging (#5036)
    1. Add buffer type to DecoderBuffer allocator and
    DecoderBuffer::Allocator::Allocate(), so we may using different buffer
    pools for different types of buffers in future to reduce fragmentation.
    2. Add DecoderBufferAllocatorTest to verify against different allocation
    settings of DecoderBufferAllocator.
    3. Improve allocation logging in DecoderBufferAllocator with extra
    information for using in the above unit tests.
    4. Improve ReuseAllocatorBase::PrintAllocations() with extra information
    e.g. allocation percentage.
    5. Add allocation_extractor.py to extra allocation operations from a
    full log to be used in the above unit tests.
    
    b/369245553
    
    original-hexsha: 987ce29f7f55aac7c1da1907ace4ba69e04befd7

--- media/base/stream_parser_buffer.cc
+++ media/base/stream_parser_buffer.cc
@@ -80,7 +80,11 @@ StreamParserBuffer::StreamParserBuffer(const uint8_t* data,
                                        bool is_key_frame,
                                        Type type,
                                        TrackId track_id)
+#if BUILDFLAG(USE_STARBOARD_MEDIA)
+    : DecoderBuffer(type, data, data_size, side_data, side_data_size),
+#else // BUILDFLAG(USE_STARBOARD_MEDIA)
     : DecoderBuffer(data, data_size, side_data, side_data_size),
+#endif // BUILDFLAG(USE_STARBOARD_MEDIA)
       decode_timestamp_(kNoDecodeTimestamp),
       config_id_(kInvalidConfigId),
       type_(type),
