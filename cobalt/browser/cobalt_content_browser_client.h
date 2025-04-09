be5b3915a9ca4245a7633c4a3a515e6992c7ae2e
Yijia Zhang <yijiazhang@google.com>
2025-04-09 14:19:59 -0700

    Initialize Finch (#5187)
    b/392683875
    
    Add client side Finch initialization to Cobalt and enable Hang Watcher
    feature.
    
    original-hexsha: 11786bcc58ad39cf1d33710f1fb7ac2644177510

--- cobalt/browser/cobalt_content_browser_client.h
+++ cobalt/browser/cobalt_content_browser_client.h
@@ -17,6 +17,7 @@
 
 #include "base/threading/thread_checker.h"
 #include "cobalt/browser/client_hint_headers/cobalt_trusted_url_loader_header_client.h"
+#include "components/prefs/pref_registry_simple.h"
 #include "content/shell/browser/shell_content_browser_client.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 
@@ -85,6 +86,17 @@ class CobaltContentBrowserClient : public content::ShellContentBrowserClient {
       blink::AssociatedInterfaceRegistry* associated_registry,
       content::RenderProcessHost* render_process_host) override;
   void BindGpuHostReceiver(mojo::GenericPendingReceiver receiver) override;
+  // Initialize a PrefService instance for the experiment config.
+  void CreateExperimentConfig();
+  void CreateFeatureListAndFieldTrials();
+  // Read from the experiment config, override features, and associate feature
+  // params for Cobalt experiments.
+  void SetUpCobaltFeaturesAndParams(
+      std::unique_ptr<base::FeatureList>& feature_list);
+  // Setup Cobalt field trial and related initialization.
+  void SetUpFieldTrials();
+  // Registers experiment config prefs used by this class.
+  static void RegisterPrefs(PrefRegistrySimple* registry);
 
   bool WillCreateURLLoaderFactory(
       content::BrowserContext* browser_context,
@@ -109,6 +121,8 @@ class CobaltContentBrowserClient : public content::ShellContentBrowserClient {
       video_geometry_setter_service_;
   std::vector<std::unique_ptr<browser::CobaltTrustedURLLoaderHeaderClient>>
       cobalt_header_clients_;
+
+  std::unique_ptr<PrefService> exp_config_ = nullptr;
 
   THREAD_CHECKER(thread_checker_);
 };
