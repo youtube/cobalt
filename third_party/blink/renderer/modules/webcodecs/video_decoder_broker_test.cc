3cc85518f3422b6dc121b783a290e9a717b66b44
Bo-Rong Chen <borongchen@google.com>
2025-04-11 15:01:45 -0700

    [blink_unittests] Fix unimplemented CreateStarboardRenderer (#5384)
    Fix unimplemented CreateStarboardRenderer from
    https://github.com/youtube/cobalt/pull/5113 for blink_unittests.
    
    b/405401569
    b/410047452

--- third_party/blink/renderer/modules/webcodecs/video_decoder_broker_test.cc
+++ third_party/blink/renderer/modules/webcodecs/video_decoder_broker_test.cc
@@ -167,6 +167,19 @@ class FakeInterfaceFactory : public media::mojom::InterfaceFactory {
           ::media::mojom::MediaFoundationRendererClientExtension>
           client_extension_remote) override {}
 #endif  // BUILDFLAG(IS_WIN)
+
+#if BUILDFLAG(USE_STARBOARD_MEDIA)
+  void CreateStarboardRenderer(
+      mojo::PendingRemote<media::mojom::MediaLog> media_log_remote,
+      const base::UnguessableToken& overlay_plane_id,
+      base::TimeDelta audio_write_duration_local,
+      base::TimeDelta audio_write_duration_remote,
+      mojo::PendingReceiver<media::mojom::Renderer> receiver,
+      mojo::PendingReceiver<media::mojom::StarboardRendererExtension>
+          renderer_extension_receiver,
+      mojo::PendingRemote<media::mojom::StarboardRendererClientExtension>
+          client_extension_remote) override {}
+#endif  // BUILDFLAG(USE_STARBOARD_MEDIA)
  private:
   media::MojoCdmServiceContext cdm_service_context_;
   FakeMojoMediaClient mojo_media_client_;
