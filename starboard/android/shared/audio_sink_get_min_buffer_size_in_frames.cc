c44844e16206fe3b0f59304725ba1fa8da28661d
Bo-Rong Chen <borongchen@google.com>
2025-04-11 13:59:40 -0700

    [media] Retrieve RRO values on the same thread without mutex lock (#5371)
    GetOverlaidIntValue() is used in renderer thread for
    SbMediaGetVideoBufferBudget() and SbAudioSinkGetMinBufferSizeInFrames(),
    but RRO variables are stored in the browser main thread as
    ApplicationAndroid() is created on, resulting in crashes of locking
    mutex from different threads.
    
    This PR:
    - make all GetOverlaid{Int,String,Bool}Value() only with cache values,
    so it can be called from any thread.
    - add CacheOverlaid{Int,String,Bool}ValueLocked() to initialize and
    cache RRO values on browser main thread.
    - remove mutex lock as ApplicationAndroid() cache all RRO values when
    initializing.
    
    Note if GetOverlaid{Int,String,Bool}Value() is called before
    ApplicationAndroid() cache all RRO values, a default value (e.g., 0)
    will be returned.
    
    b/401383858
    b/409804377
    original-hexsha: fde6ae71fd72437219fd4ba13a15ef57a408534f

--- starboard/android/shared/audio_sink_get_min_buffer_size_in_frames.cc
+++ starboard/android/shared/audio_sink_get_min_buffer_size_in_frames.cc
@@ -38,12 +38,12 @@ int SbAudioSinkGetMinBufferSizeInFrames(int channels,
   int min_buffer_size = starboard::android::shared::AudioTrackAudioSinkType::
       GetMinBufferSizeInFrames(channels, sample_type, sampling_frequency_hz);
 
-  int overlayed_min_buffer_size =
+  int overlaid_min_buffer_size =
       starboard::android::shared::ApplicationAndroid::Get()
-          ->GetOverlayedIntValue("min_audio_sink_buffer_size_in_frames");
-  if (overlayed_min_buffer_size != 0 &&
-      overlayed_min_buffer_size > min_buffer_size) {
-    min_buffer_size = overlayed_min_buffer_size;
+          ->GetOverlaidIntValue("min_audio_sink_buffer_size_in_frames");
+  if (overlaid_min_buffer_size != 0 &&
+      overlaid_min_buffer_size > min_buffer_size) {
+    min_buffer_size = overlaid_min_buffer_size;
   }
   return min_buffer_size;
 }
