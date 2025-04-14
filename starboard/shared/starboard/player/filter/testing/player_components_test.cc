c3f58ef0c4bc7bb351ae4d0fbac4f62326c0b622
Miguel Casas <mcasas@chromium.org>
2025-04-14 16:27:31 -0700

    starboard: roll up the remaining unittest targets (#5127)
    This PR continues the work of #4831 and rolls up all other targets I
    could find that looked like unit tests into `starboard_unittests`.
    
    With this I see
    ```
    [==========] 1454 tests from 55 test suites ran. (8612 ms total)
    [  PASSED  ] 989 tests.
    [  SKIPPED ] 465 tests, listed below:
    ...
      YOU HAVE 1 DISABLED TEST
    ```
    
    (the disabled test was from before, the skipped ones are new, see
    further below).
    
    Media-related topics:
    
    - A number of tetst (e.g. `AudioDecoderTest` and others) needed a number
    of system libraries to support codecs like AAC, AC3 etc but those are
    part of non-free Debian repos, so couldn't install those in Docker. This
    CL adds a new method `IsProprietaryAudioCodec()` to be used to SKIP test
    cases related to those codecs, when the build flag
    `SKIP_PROPRIETARY_CODEC_TESTS` is present.
    - `VideoDecoderTests` and `PlayerComponentsTest` failed flakily due to
    X11 use not being torn down correctly; they need `xvfb-run`. This makes
    me think those are not pure unit-tests but integration tests like
    `nplb`. For the time being they are commented out with a TODO.
    - `media_util_test.cc` needed `operator==` on `SbMediaColorMetadata`s,
    so I added it as extern.
    - `FileCacheReader` needs to find the media files under the output
    folder's `content/` directory. Files are under the output folder's
    `starboard/content`, because of the starboard toolchain. This CL leaves
    a quick hack and a TODO() to find a proper solution.
    
    Finally:
    - `//starboard/raspi/shared/:starboard_platform_tests` target was empty,
    this CL removes it.
    
    Bug: b/384819454
    
    original-hexsha: 82430e30b3b9c269dca0f888893a7bcff3359490

--- starboard/shared/starboard/player/filter/testing/player_components_test.cc
+++ starboard/shared/starboard/player/filter/testing/player_components_test.cc
@@ -25,6 +25,7 @@
 #include "starboard/common/time.h"
 #include "starboard/media.h"
 #include "starboard/player.h"
+#include "starboard/shared/starboard/media/media_util.h"
 #include "starboard/shared/starboard/player/filter/testing/test_util.h"
 #include "starboard/shared/starboard/player/job_queue.h"
 #include "starboard/shared/starboard/player/video_dmp_reader.h"
@@ -77,6 +78,13 @@ class PlayerComponentsTest
   }
 
   void SetUp() override {
+#if defined(SKIP_PROPRIETARY_CODEC_TESTS)
+    if (::starboard::shared::starboard::media::IsProprietaryAudioCodec(
+            audio_filename_)) {
+      GTEST_SKIP() << "Skipping proprietary-audio related tests.";
+    }
+#endif
+
     if (audio_filename_.length() > 0) {
       audio_reader_.reset(new VideoDmpReader(
           audio_filename_.c_str(), VideoDmpReader::kEnableReadOnDemand));
