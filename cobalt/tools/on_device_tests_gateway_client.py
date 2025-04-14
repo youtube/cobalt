1c6666e562c8dae83d099fb3da8e8010bd33f693
Felipe Javier Mira Bocca <fmira@google.com>
2025-04-14 15:07:51 -0700

    ODT gateway change (#5401)
    original-hexsha: 0e2f008fef96889a514d1704818599627a4b2f9b

--- cobalt/tools/on_device_tests_gateway_client.py
+++ cobalt/tools/on_device_tests_gateway_client.py
@@ -123,10 +123,8 @@ def _process_test_requests(args):
 
   for gtest_target in args.targets.split(','):
     _, target_name = gtest_target.split(':')
-    print(f'  Processing gtest_target: {gtest_target}')
 
     gtest_filter = _get_gtest_filter(args.filter_json_dir, target_name)
-    print(f'  gtest_filter = {gtest_filter}')
     if gtest_filter == '-*':
       print(f'Skipping {target_name} due to test filter.')
       continue
@@ -176,6 +174,7 @@ def _process_test_requests(args):
         'test_args': tests_args,
         'files': files,
         'params': params,
+        'test_target': gtest_target,
     })
   return test_requests
 
@@ -186,6 +185,7 @@ def main() -> int:
   logging.basicConfig(
       level=logging.INFO, format='[%(filename)s:%(lineno)s] %(message)s')
   print('Starting main routine')
+  print('')
 
   parser = argparse.ArgumentParser(
       description='Client for interacting with the On-Device Tests gateway.',
