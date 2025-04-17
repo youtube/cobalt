ced654cb692956f2cc90d68d8e60a708f3df5007
Oscar Vestlie <oxv@google.com>
2025-04-17 12:58:00 -0700

    Add the web-tests test job (#5267)
    Start running blink-web-tests from wpt_internal/cobalt/crash-log. This should be expanded as tests are fixed.
    
    b/405988705
    
    original-hexsha: 580ac4028c4918e02f3526b0324a1e8941f57dca

--- cobalt/build/archive_test_artifacts.py
+++ cobalt/build/archive_test_artifacts.py
@@ -59,18 +59,17 @@ def create_archive(
     destination_dir: str,
     platform: str,
 ):
-  """Main logic. Collects runtime dependencies from the source directory for
-  each target."""
+  """Main logic. Collects runtime dependencies for each target."""
   # TODO(oxv): Move logic behind parameters instead of using platform.
   is_linux = platform.startswith('linux')
   is_android = platform.startswith('android')
 
   tar_root = '.' if is_android else out_dir
   combined_deps = set()
-  # TODO(oxv): Make output from build step instead.
   # Add test_targets.json to archive so that test runners know what to run.
-  combined_deps.add(
-      os.path.relpath(os.path.join(tar_root, 'test_targets.json')))
+  test_targets_json = os.path.join(tar_root, 'test_targets.json')
+  if os.path.exists(test_targets_json):
+    combined_deps.add(os.path.relpath(test_targets_json))
 
   for target in targets:
     target_path, target_name = target.split(':')
