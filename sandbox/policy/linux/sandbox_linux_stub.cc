f4c1c397b445b11eeb20a6ed1f9ab84812c6140e
Niranjan Yardi <nyardi@google.com>
2025-04-17 14:19:17 -0700

    Remove warning from fcntl.h

--- sandbox/policy/linux/sandbox_linux_stub.cc
+++ sandbox/policy/linux/sandbox_linux_stub.cc
@@ -10,49 +10,41 @@ namespace sandbox {
 namespace policy {
 
 // Note: This is a stub implementation. The actual implementation is in
-// sandbox_linux.cc, which is conditionally compiled.
+// sandbox_linux.cc.
+// Stub symbols have been provided here to satisfy the linker and build cobalt
 
 // static
 SandboxLinux* SandboxLinux::GetInstance() {
-  // This stub is not expected to be used in a context where GetInstance() is
-  // needed.
   NOTIMPLEMENTED();
   return nullptr;
 }
 
 std::vector<int> SandboxLinux::GetFileDescriptorsToClose() {
-  // The stub implementation doesn't manage file descriptors.
   NOTIMPLEMENTED();
   return std::vector<int>();
 }
 
 SetuidSandboxClient* SandboxLinux::setuid_sandbox_client() const {
-  // The stub implementation doesn't have a real setuid client.
   NOTIMPLEMENTED();
   return nullptr;
 }
 
 void SandboxLinux::PreinitializeSandbox() {
-  // The stub implementation doesn't actually pre-initialize the sandbox.
   NOTIMPLEMENTED();
 }
 
 void SandboxLinux::EngageNamespaceSandbox(bool from_zygote) {
-  // The stub implementation doesn't actually engage the namespace sandbox.
   NOTIMPLEMENTED();
 }
 
 bool SandboxLinux::EngageNamespaceSandboxIfPossible() {
-  // The stub implementation doesn't actually engage the namespace sandbox.
   NOTIMPLEMENTED();
   return false;
 }
 
-
 bool SandboxLinux::InitializeSandbox(sandbox::mojom::Sandbox sandbox_type,
                                      PreSandboxHook hook,
                                      const Options& options) {
-  // The stub implementation doesn't actually initialize a sandbox.
   NOTIMPLEMENTED();
   return false;
 }
@@ -60,7 +52,7 @@ bool SandboxLinux::InitializeSandbox(sandbox::mojom::Sandbox sandbox_type,
 int SandboxLinux::GetStatus() {
   // Return 0, indicating no sandbox features are active.
   NOTIMPLEMENTED();
-  return 0;
+  return kInvalid;
 }
 
 void SandboxLinux::StartBrokerProcess(
@@ -68,47 +60,16 @@ void SandboxLinux::StartBrokerProcess(
     std::vector<syscall_broker::BrokerFilePermission> permissions,
     PreSandboxHook broker_side_hook,
     const Options& options) {
-  // The stub implementation doesn't start a broker process.
   NOTIMPLEMENTED();
 }
 
 SandboxLinux::SandboxLinux() {
-  // Stub constructor.
   NOTIMPLEMENTED();
 }
 
 SandboxLinux::~SandboxLinux() {
-  // Stub destructor.
   NOTIMPLEMENTED();
 }
 
 }  // namespace policy
 }  // namespace sandbox
-
-
-// Copyright 2023 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// This could be in hardware_video_decoding_sandbox_hook_stub.cc
-// or within #else/#endif in hardware_video_decoding_sandbox_hook_linux.cc
-
-#include "media/gpu/sandbox/hardware_video_decoding_sandbox_hook_linux.h"
-
-#include "base/logging.h" // Or base/notreached.h
-#include "sandbox/policy/linux/sandbox_linux.h" // For SandboxLinux::Options
-
-namespace media {
-
-// This is a stub implementation for the hardware video decoding pre-sandbox
-// hook. It allows the sandboxing process to continue but performs no actual
-// setup.
-// bool HardwareVideoDecodingPreSandboxHook(
-//     sandbox::policy::SandboxLinux::Options options) {
-//   VLOG(1) << "HardwareVideoDecodingPreSandboxHook: Stub implementation running.";
-//   // Returning true allows the sandbox initialization to proceed.
-//   // Returning false would typically abort the process startup.
-//   return true;
-// }
-
-} // namespace media
--- third_party/musl/include/sys/fcntl.h
+++ third_party/musl/include/sys/fcntl.h
@@ -1,4 +1,2 @@
-// In file included from ../../media/capture/video/linux/v4l2_capture_device.h:9:
-// ../../third_party/musl/include/sys/fcntl.h:1:2: error: redirecting incorrect #include <sys/fcntl.h> to <fcntl.h> [-Werror,-W#warnings]
-// #warning redirecting incorrect #include <sys/fcntl.h> to <fcntl.h>
+#warning redirecting incorrect #include <sys/fcntl.h> to <fcntl.h>
 #include <fcntl.h>
