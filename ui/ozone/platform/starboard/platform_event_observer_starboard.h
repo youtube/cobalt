8f34d9b28b3b52e44768ba1a98ae33a42fea9de1
Sherry Zhou <sherryzy@google.com>
2025-04-21 14:46:15 -0700

    [Ozone] Updating Bounds on Resize via SetBoundsInPixels()  (#5336)
    This PR addresses a discrepancy in resize event handling compared to
    X11. The current implementation in this change provides the minimum
    necessary code to make resizing functional but does not yet fully align
    with X11's more robust logic (including delayed resizing and bounds
    validation). Further investigation is needed to determine the full set
    of PlatformWindow functions required for a proper implementation,
    potentially replacing the current stub window approach. This initial
    change focuses on updating bounds via SetBoundsInPixels() based on
    SbEvent and is being submitted for early feedback.
    
    b/395895913
    original-hexsha: 0091352f06de82b78d79dc54a2d3d7ced2a43e5f

---
+++ ui/ozone/platform/starboard/platform_event_observer_starboard.h
@@ -0,0 +1,30 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef UI_OZONE_PLATFORM_STARBOARD_PLATFORM_EVENT_OBSERVER_H_
+#define UI_OZONE_PLATFORM_STARBOARD_PLATFORM_EVENT_OBSERVER_H_
+
+namespace starboard {
+
+class EVENTS_EXPORT PlatformEventObserverStarboard {
+ public:
+  virtual void ProcessWindowSizeChangedEvent(int width, int height) = 0;
+
+ protected:
+  virtual ~PlatformEventObserverStarboard() = default;
+};
+
+}  // namespace starboard
+
+#endif  // UI_OZONE_PLATFORM_STARBOARD
