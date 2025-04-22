d13b5f3dd784724e88f6a62771da8d0ba7748a53
xiaomings <xiaomings@google.com>
2025-04-22 14:20:37 -0700

    [android] Fix crashes on accessing RRO values (#5492)
    Runtime Resource Overlay values were hosted in ApplicationAndroid, and
    caused multiple issues (including crashing) due to life time.
    
    Now they are abstracted into RuntimeResourceOverlay class, which is a
    singleton by itself. It uses SB_ONCE_INITIALIZE_FUNCTION() to guarantee
    its initialization is synchronized and the settings can be accessed
    without synchronization afterwards.
    
    It also updates comments to refer to files for their new locations.
    
    b/411690364
    original-hexsha: 2392c53e7595899c224d7025b889244b9bee14e4

---
+++ starboard/android/shared/runtime_resource_overlay.cc
@@ -0,0 +1,47 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/android/shared/runtime_resource_overlay.h"
+
+#include "starboard/android/shared/jni_env_ext.h"
+#include "starboard/common/log.h"
+#include "starboard/common/once.h"
+
+namespace starboard {
+namespace android {
+namespace shared {
+
+SB_ONCE_INITIALIZE_FUNCTION(RuntimeResourceOverlay,
+                            RuntimeResourceOverlay::GetInstance);
+
+RuntimeResourceOverlay::RuntimeResourceOverlay() {
+  JniEnvExt* env = JniEnvExt::Get();
+  jobject resource_overlay = env->CallStarboardObjectMethodOrAbort(
+      "getResourceOverlay", "()Ldev/cobalt/coat/ResourceOverlay;");
+
+  // Retrieve all Runtime Resource Overlay variables during initialization, so
+  // synchronization isn't needed on access.
+  min_audio_sink_buffer_size_in_frames_ = env->GetIntFieldOrAbort(
+      resource_overlay, "min_audio_sink_buffer_size_in_frames", "I");
+  max_video_buffer_budget_ =
+      env->GetIntFieldOrAbort(resource_overlay, "max_video_buffer_budget", "I");
+
+  SB_LOG(INFO) << "Loaded RRO values\n\tmin_audio_sink_buffer_size_in_frames: "
+               << min_audio_sink_buffer_size_in_frames_
+               << "\n\tmax_video_buffer_budget: " << max_video_buffer_budget_;
+}
+
+}  // namespace shared
+}  // namespace android
+}  // namespace starboard
