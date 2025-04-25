f2a81b59a08d4d1929586f97078c50021e5c2a93
Kyujung Youn <kjyoun@google.com>
2025-04-25 15:28:12 -0700

    Merge player_components_factory.cc into .h file (#5569)
    This is to remove player_components_factory.cc, so that in the next CL
    to move player_components_factory.h to .cc, the file, we can preserve
    the file history of player_components_factory.h
    
    Once this PR is merged, the actual PR to rename
    player_components_factory,.h to .cc file will come
    
    b/413446518
    original-hexsha: 8705fb92eb4261efc4994e15a3eb9746b8499e20

--- starboard/android/shared/platform_service.cc
+++ starboard/android/shared/platform_service.cc
@@ -22,6 +22,12 @@
 #include "starboard/common/string.h"
 #include "starboard/extension/platform_service.h"
 
+// For now, we include player_components_factory.h through this file.
+// This will be deleted once player_components_factory.h moves to
+// stand-alone cc file.
+// TODO: b/413446518 - Remove this include.
+#include "starboard/android/shared/player_components_factory.h"
+
 typedef struct CobaltExtensionPlatformServicePrivate {
   void* context;
   ReceiveMessageCallback receive_callback;
