4e35607c3399d023341c2d051b5fe5f9c8b06249
Niranjan Yardi <nyardi@google.com>
2025-04-25 22:36:45 +0000

    [CONFLICT] Original Commit: a2d91bbcf6d1c8c271f515df309fc11bfa3927c2
    Original Message:
    Build libcobalt.so hermetically (#5048)
    
    b/402243381
    
    Add symbol stubs in net/ to get libcobalt.so to link.
    Add cobalt to list of targets being built in CI.
    Stub out Thread Local Storage related variable in blink/
    original-hexsha: 277d2c2b49260c207932aab7fd886121dc034639
    
    
    Files with conflicts:
    - .github/config/linux-evergreen.json
    - third_party/blink/renderer/platform/heap/thread_state_storage.cc
    - third_party/blink/renderer/platform/heap/thread_state_storage.h

--- third_party/blink/renderer/platform/heap/thread_state_storage.cc
+++ third_party/blink/renderer/platform/heap/thread_state_storage.cc
@@ -10,8 +10,18 @@
 
 namespace blink {
 
+<<<<<<< HEAD
 constinit thread_local ThreadStateStorage* g_thread_specific_
+=======
+#if BUILDFLAG(ENABLE_COBALT_HERMETIC_HACKS)
+// TODO: b/412388549 - Cobalt: Fix this hack.
+ThreadStateStorage* g_thread_specific_ = nullptr;
+#else  // BUILDFLAG(ENABLE_COBALT_HERMETIC_HACKS)
+thread_local ThreadStateStorage* g_thread_specific_ CONSTINIT
+>>>>>>> a2d91bbcf6d (Build libcobalt.so hermetically (#5048))
     __attribute__((tls_model(BLINK_HEAP_THREAD_LOCAL_MODEL))) = nullptr;
+
+#endif  // BUILDFLAG(ENABLE_COBALT_HERMETIC_HACKS)
 
 // static
 ThreadStateStorage ThreadStateStorage::main_thread_state_storage_;
