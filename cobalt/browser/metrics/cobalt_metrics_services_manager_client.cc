7ed8ab92649bb6f67ee1bd373cb2c53309234f7f
Joel Martinez <joeltine@google.com>
2025-05-01 14:44:51 -0700

    Remove unnecessary CHECK (#5634)
    This CHECK was a bit aggressive. No need to crash.
    
    b/406250944
    
    original-hexsha: bf17b400b75827a911532f94d3b013c747f3ae14

--- cobalt/browser/metrics/cobalt_metrics_services_manager_client.cc
+++ cobalt/browser/metrics/cobalt_metrics_services_manager_client.cc
@@ -87,7 +87,7 @@ std::unique_ptr<::metrics::ClientInfo> LoadMetricsClientInfo() {
 CobaltMetricsServicesManagerClient::GetMetricsStateManager() {
   if (!metrics_state_manager_) {
     base::FilePath user_data_dir;
-    CHECK(base::PathService::Get(content::SHELL_DIR_USER_DATA, &user_data_dir));
+    base::PathService::Get(content::SHELL_DIR_USER_DATA, &user_data_dir);
     metrics_state_manager_ = ::metrics::MetricsStateManager::Create(
         local_state_.get(), enabled_state_provider_.get(), std::wstring(),
         user_data_dir, metrics::StartupVisibility::kForeground);
