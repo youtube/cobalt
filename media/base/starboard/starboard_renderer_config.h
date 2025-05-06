815be012da8590e255bb91ea344c555b17329adb
Bo-Rong Chen <borongchen@google.com>
2025-05-06 11:36:23 -0700

    [media] Refactor initializing StarboardRenderer via StarboardRendererConfig (#5473)
    The current way to add new customized arguments to StarboardRenderer
    needs to modify the code in many places (i.e., MediaInterfaceFactory,
    MediaInterfaceProxy, FramelessMediaInterfaceProxy, and
    FakeInterfaceFactory), which cause many efforts for maintenance.
    
    This PR populates all customized arguments as |StarboardRendererConfig|,
    so any additional customized arguments can be added without modifying
    codes in many places.
    
    b/411480729

---
+++ media/base/starboard/starboard_renderer_config.h
@@ -0,0 +1,40 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef MEDIA_BASE_STARBOARD_STARBOARD_RENDERER_CONFIG_H_
+#define MEDIA_BASE_STARBOARD_STARBOARD_RENDERER_CONFIG_H_
+
+#include "base/time/time.h"
+#include "base/unguessable_token.h"
+#include "media/base/media_export.h"
+
+namespace media {
+
+// Configs for StarboardRenderer.
+struct MEDIA_EXPORT StarboardRendererConfig {
+  StarboardRendererConfig();
+  StarboardRendererConfig(const base::UnguessableToken& overlay_plane_id,
+                          base::TimeDelta audio_write_duration_local,
+                          base::TimeDelta audio_write_duration_remote);
+  StarboardRendererConfig(const StarboardRendererConfig&);
+  StarboardRendererConfig& operator=(const StarboardRendererConfig&);
+
+  base::UnguessableToken overlay_plane_id;
+  base::TimeDelta audio_write_duration_local;
+  base::TimeDelta audio_write_duration_remote;
+};
+
+}  // namespace media
+
+#endif  // MEDIA_BASE_STARBOARD_STARBOARD_RENDERER_CONFIG_H_
