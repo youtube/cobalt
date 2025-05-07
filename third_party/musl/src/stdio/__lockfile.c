8aabfd632c03df6e653bbe80d7630401506e8456
Yavor Goulishev <yavor@google.com>
2025-05-07 16:14:00 +0000

    Cleanup starboard stubs in musl.
    - The stubs were added long time ago and were used only
    to prevent API leaks, but the functionality was never needed.
    
    - Disable locking APIs which would be implemented in
    subsequent changes.
    
    b/414579250

--- third_party/musl/src/stdio/__lockfile.c
+++ third_party/musl/src/stdio/__lockfile.c
@@ -1,8 +1,15 @@
 #include "stdio_impl.h"
+#include "build/build_config.h"
+
+#if !defined(STARBOARD)
 #include "pthread_impl.h"
+#endif
 
 int __lockfile(FILE *f)
 {
+
+ /* TODO: Implement it for Starboard. */
+#if !BUILDFLAG(IS_COBALT_HERMETIC_BUILD)
 	int owner = f->lock, tid = __pthread_self()->tid;
 	if ((owner & ~MAYBE_WAITERS) == tid)
 		return 0;
@@ -13,11 +20,16 @@ int __lockfile(FILE *f)
 		    a_cas(&f->lock, owner, owner|MAYBE_WAITERS)==owner)
 			__futexwait(&f->lock, owner|MAYBE_WAITERS, 1);
 	}
+#endif
 	return 1;
 }
 
 void __unlockfile(FILE *f)
 {
+
+/* TODO: Implement it for Starboard. */
+#if !BUILDFLAG(IS_COBALT_HERMETIC_BUILD)
 	if (a_swap(&f->lock, 0) & MAYBE_WAITERS)
 		__wake(&f->lock, 1, 1);
+#endif
 }
