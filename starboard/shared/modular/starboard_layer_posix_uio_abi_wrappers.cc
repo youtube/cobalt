6a833716387fe26b978ea317f7ca1f0e11033f37
Sherry Zhou <sherryzy@google.com>
2025-05-08 12:44:52 -0700

    [POSIX] Implement writev in Starboard API
    In Cobalt, writev is used in crashpad file_writer. In this PR, we
    add writev to Starboard by adding a wrapper for handling struct iovec.
    In addition, add nplb test cases to the existing posix_file_write_test.cc.
    
    b/412704523

---
+++ starboard/shared/modular/starboard_layer_posix_uio_abi_wrappers.cc
@@ -0,0 +1,28 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/shared/modular/starboard_layer_posix_uio_abi_wrappers.h"
+
+#include <sys/uio.h>
+
+ssize_t __abi_wrap_writev(int fildes,
+                          const struct musl_iovec* iov,
+                          int iovcnt) {
+  struct iovec platform_iov[iovcnt];
+  for (int i = 0; i < iovcnt; ++i) {
+    platform_iov[i].iov_base = iov[i].iov_base;
+    platform_iov[i].iov_len = iov[i].iov_len;
+  }
+  return writev(fildes, platform_iov, iovcnt);
+}
