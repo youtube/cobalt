83ec2d254e2e7bf3882dae1afb8ea8e525aae3d1
Yijia Zhang <yijiazhang@google.com>
2025-05-08 09:47:26 -0700

    Add h5vcc experiment APIs. (#5190)
    b/392683685
    
    original-hexsha: b21f222c1b168cb22bbf698eed78510f4157a99e

--- third_party/blink/renderer/modules/cobalt/h_5_vcc.h
+++ third_party/blink/renderer/modules/cobalt/h_5_vcc.h
@@ -27,6 +27,7 @@ namespace blink {
 class CrashLog;
 class LocalDOMWindow;
 class H5vccAccessibility;
+class H5vccExperiments;
 class H5vccMetrics;
 class H5vccStorage;
 class H5vccSystem;
@@ -50,6 +51,7 @@ class MODULES_EXPORT H5vcc final : public ScriptWrappable,
   CrashLog* crashLog() { return crash_log_; }
 
   H5vccAccessibility* accessibility() { return accessibility_; }
+  H5vccExperiments* experiments() { return experiments_; }
 
   H5vccMetrics* metrics() { return metrics_; }
 
@@ -64,6 +66,7 @@ class MODULES_EXPORT H5vcc final : public ScriptWrappable,
  private:
   Member<CrashLog> crash_log_;
   Member<H5vccAccessibility> accessibility_;
+  Member<H5vccExperiments> experiments_;
   Member<H5vccMetrics> metrics_;
   Member<H5vccStorage> storage_;
   Member<H5vccSystem> system_;
