69ee2e67d272b09ec0bd67ab1042eadefed22508
Yijia Zhang <yijiazhang@google.com>
2025-05-08 09:47:26 -0700

    Add h5vcc experiment APIs. (#5190)
    b/392683685
    
    original-hexsha: b21f222c1b168cb22bbf698eed78510f4157a99e

--- cobalt/browser/cobalt_content_browser_client.cc
+++ cobalt/browser/cobalt_content_browser_client.cc
@@ -26,6 +26,7 @@
 #include "base/path_service.h"
 #include "cobalt/browser/cobalt_browser_interface_binders.h"
 #include "cobalt/browser/cobalt_browser_main_parts.h"
+#include "cobalt/browser/cobalt_experiment_names.h"
 #include "cobalt/browser/cobalt_web_contents_observer.h"
 #include "cobalt/browser/global_features.h"
 #include "cobalt/media/service/mojom/video_geometry_setter.mojom.h"
@@ -57,8 +58,6 @@ namespace cobalt {
 namespace {
 
 constexpr base::FilePath::CharType kCacheDirname[] = FILE_PATH_LITERAL("Cache");
-const char kCobaltExperimentName[] = "CobaltExperiment";
-const char kCobaltGroupName[] = "CobaltGroup";
 constexpr base::FilePath::CharType kCookieFilename[] =
     FILE_PATH_LITERAL("Cookies");
 constexpr base::FilePath::CharType kNetworkDataDirname[] =
@@ -343,7 +342,7 @@ void CobaltContentBrowserClient::SetUpCobaltFeaturesAndParams(
   auto experiment_config = GlobalFeatures::GetInstance()->experiment_config();
 
   const base::Value::Dict& feature_map =
-      experiment_config->GetDict(kExperimentConfigFeature);
+      experiment_config->GetDict(kExperimentConfigFeatures);
   const base::Value::Dict& param_map =
       experiment_config->GetDict(kExperimentConfigFeatureParams);
 
@@ -383,14 +382,14 @@ void CobaltContentBrowserClient::SetUpCobaltFeaturesAndParams(
 void CobaltContentBrowserClient::CreateFeatureListAndFieldTrials() {
   metrics::TestEnabledStateProvider enabled_state_provider(/*consent=*/false,
                                                            /*enabled=*/false);
-  base::FilePath path;
-  base::PathService::Get(content::SHELL_DIR_USER_DATA, &path);
-
   GlobalFeatures::GetInstance()
       ->metrics_services_manager()
       ->InstantiateFieldTrialList();
 
   auto feature_list = std::make_unique<base::FeatureList>();
+
+  auto accessor = feature_list->ConstructAccessor();
+  GlobalFeatures::GetInstance()->set_accessor(std::move(accessor));
 
   const base::CommandLine& command_line =
       *base::CommandLine::ForCurrentProcess();
