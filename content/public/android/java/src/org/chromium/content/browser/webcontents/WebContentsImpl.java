206d5743ac922e86e979f0474f2d543de443802a
johnx <johnx@google.com>
2025-05-12 11:27:51 -0700

    Add freeze and resume events on suspend/resume (#5613)
    We are missing these two critical browser events right now. I am
    starting an upstreaming CL for the added WebContents API. If the
    upstreaming is denied, I will copy the JAVA API to Cobalt to avoid
    touching Content code.
    
    b/390022481
    
    original-hexsha: ad177a6fbd6499fd5ce6a558e5f7f53c1143fea3

--- content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
@@ -616,6 +616,18 @@ public class WebContentsImpl implements WebContents, RenderFrameHostDelegate, Wi
     }
 
     @Override
+    public void onFreeze() {
+        checkNotDestroyed();
+        WebContentsImplJni.get().onFreeze(mNativeWebContentsAndroid);
+    }
+
+    @Override
+    public void onResume() {
+        checkNotDestroyed();
+        WebContentsImplJni.get().onResume(mNativeWebContentsAndroid);
+    }
+
+    @Override
     public void onShow() {
         checkNotDestroyed();
         SelectionPopupControllerImpl controller = getSelectionPopupController();
@@ -1301,6 +1313,10 @@ public class WebContentsImpl implements WebContents, RenderFrameHostDelegate, Wi
         void onHide(long nativeWebContentsAndroid);
 
         void onShow(long nativeWebContentsAndroid);
+
+        void onFreeze(long nativeWebContentsAndroid);
+
+        void onResume(long nativeWebContentsAndroid);
 
         void setImportance(long nativeWebContentsAndroid, int importance);
 
