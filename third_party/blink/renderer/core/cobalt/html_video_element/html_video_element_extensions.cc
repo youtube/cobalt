ae1120fc945a1d6fa81a238197199da5a253c85c
John Ocampo <johneocampo@google.com>
2025-05-13 02:09:52 +0000

    Implement custom API for HTMLVideoElement (#5648)
    Implements the maxVideoCapabilities API for HTMLVideoElement. This
    allows platforms to properly allocate resources for secondary videos,
    which are assumed to be at much lower resolutions than the primary
    video.
    
    setMaxVideoCapabilities can enable the platform to optimize the playback
    pipeline for a low quality video stream if it knows that it will never
    adapt to a higher quality stream. The setMaxVideoCapabilities function
    must be called before the src attribute is set on element, otherwise an
    InvalidStateError exception is raised.
    
    The design document and example use cases for the
    setMaxVideoCapabilities function can be seen
    [here](https://docs.google.com/document/d/1LJNh1LrBhfblop9Xpevg0Z7yMBJtZX8eD1g-Nn1YjEA/edit?tab=t.0#heading=h.u0p1e6iom4ri).
    
    b/326827007
    
    (cherry picked from commit 6c593e01072b8102ad1e6897c329280f7b1d94de)

---
+++ third_party/blink/renderer/core/cobalt/html_video_element/html_video_element_extensions.cc
@@ -0,0 +1,31 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "third_party/blink/renderer/core/cobalt/html_video_element/html_video_element_extensions.h"
+
+#if !BUILDFLAG(USE_STARBOARD_MEDIA)
+#error "This file only works with Starboard media"
+#endif  // !BUILDFLAG(USE_STARBOARD_MEDIA)
+
+namespace blink {
+
+void HTMLVideoElementExtensions::setMaxVideoCapabilities(
+    HTMLVideoElement& video_element,
+    const String& max_video_capabilities,
+    ExceptionState& exception_state) {
+  video_element.setMaxVideoCapabilities(max_video_capabilities,
+                                        exception_state);
+}
+
+}  // namespace blink
