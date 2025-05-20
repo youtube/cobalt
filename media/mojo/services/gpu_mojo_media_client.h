82d581aa5e9032b2bb3dc2e3974886500bf79b3a
John Ocampo <johneocampo@google.com>
2025-05-20 20:07:49 +0000

    [media] Send maxVideoCapabilities to StarboardRenderer (#5756)
    Send non-standard API HTMLVideoElement extension maxVideoCapabilities
    from the HTML Video Element to StarboardRenderer.
    
    This change allows StarboardRenderer to use the HTMLVideoElement
    extension maxVideoCapabilities to set a video stream with the specified
    maximums described in maxVideoCapabilities.
    
    To learn more about the extension itself and its use cases, please refer
    to the commit that included this extension at
    https://github.com/youtube/cobalt/commit/6c593e01072b8102ad1e6897c329280f7b1d94de
    
    b/326827007
    
    original-hexsha: d7a2be9c132c2b9cd4871151444e22e3256c1ffc

--- media/mojo/services/gpu_mojo_media_client.h
+++ media/mojo/services/gpu_mojo_media_client.h
@@ -134,6 +134,7 @@ struct StarboardRendererTraits {
   const base::UnguessableToken& overlay_plane_id;
   base::TimeDelta audio_write_duration_local;
   base::TimeDelta audio_write_duration_remote;
+  const std::string& max_video_capabilities;
   mojo::PendingReceiver<mojom::StarboardRendererExtension>
         renderer_extension_receiver;
   mojo::PendingRemote<mojom::StarboardRendererClientExtension>
@@ -145,6 +146,7 @@ struct StarboardRendererTraits {
       const base::UnguessableToken& overlay_plane_id,
       base::TimeDelta audio_write_duration_local,
       base::TimeDelta audio_write_duration_remote,
+      const std::string& max_video_capabilities,
       mojo::PendingReceiver<mojom::StarboardRendererExtension>
           renderer_extension_receiver,
       mojo::PendingRemote<mojom::StarboardRendererClientExtension>
