f7ad9966f0d41ad3b292ebae9b99836e3b8253f9
John Ocampo <johneocampo@google.com>
2025-05-20 20:07:49 +0000

    [media] Send maxVideoCapabilities to StarboardRenderer (#5756)
    Send non-standard API HTMLVideoElement extension maxVideoCapabilities
    from the HTML Video Element to StarboardRenderer.
    
    This change allows StarboardRenderer to use the HTMLVideoElement
    extension maxVideoCapabilities to set a video stream with the specified
    maximums described in maxVideoCapabilities.
    
    To learn more about the extension itself and its use cases, please refer
    to the commit that included this extension at
    https://github.com/youtube/cobalt/commit/6c593e01072b8102ad1e6897c329280f7b1d94de
    
    b/326827007
    
    original-hexsha: d7a2be9c132c2b9cd4871151444e22e3256c1ffc

--- media/base/starboard/starboard_renderer_config.cc
+++ media/base/starboard/starboard_renderer_config.cc
@@ -25,9 +25,11 @@ StarboardRendererConfig& StarboardRendererConfig::operator=(
 StarboardRendererConfig::StarboardRendererConfig(
     const base::UnguessableToken& overlay_plane_id,
     base::TimeDelta audio_write_duration_local,
-    base::TimeDelta audio_write_duration_remote)
+    base::TimeDelta audio_write_duration_remote,
+    const std::string& max_video_capabilities)
     : overlay_plane_id(overlay_plane_id),
       audio_write_duration_local(audio_write_duration_local),
-      audio_write_duration_remote(audio_write_duration_remote) {}
+      audio_write_duration_remote(audio_write_duration_remote),
+      max_video_capabilities(max_video_capabilities) {}
 
 }  // namespace media
