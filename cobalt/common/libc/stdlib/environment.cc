933ee1c6a963dcb9ceaddd50a7ebe00721f42980
Jelle Foks <jfoks@google.com>
2025-05-21 15:39:29 -0700

    Add environment.cc in libc and tests.
    This adds an implementation of the POSIX environment setenv/getenv,
    and the global environ. This environment implemnentation is hermetically
    separate from the platform's environment.

---
+++ cobalt/common/libc/stdlib/environment.cc
@@ -0,0 +1,179 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <unistd.h>
+
+#include <algorithm>
+#include <cerrno>
+#include <cstring>
+#include <string>
+#include <utility>
+#include <vector>
+
+// This file can not depend on //base since that indirectly depends on this
+// file, but since base::NoDestructor is a header-only class, we can safely use
+// it here.
+#include "base/no_destructor.h"  // nogncheck
+
+namespace {
+
+class Environment {
+ public:
+  typedef std::vector<std::string> Strings;
+
+  static char* getenv(const char* name);
+  static int setenv(const char* name, const char* value, int overwrite);
+  static int unsetenv(const char* name);
+  static char** InitializeGlobalEnviron();
+
+ private:
+  static base::NoDestructor<Strings> stored_environment_;
+  static base::NoDestructor<std::vector<char*>> environ_pointers_;
+
+  static void RebuildAndSetGlobalEnviron();
+
+  // Returns true if name is valid as a name for an environment variable.
+  static bool EnvironmentNameIsValid(const char* name);
+
+  // Helper function to find an environment variable by name.
+  // Returns a pair containing an iterator and the starting position of the
+  // value. If not found, the iterator will be stored_environment_->end().
+  static std::pair<Strings::iterator, size_t> FindEnvironmentVariable(
+      const char* name);
+};
+
+base::NoDestructor<Environment::Strings> Environment::stored_environment_;
+base::NoDestructor<std::vector<char*>> Environment::environ_pointers_;
+
+std::pair<Environment::Strings::iterator, size_t>
+Environment::FindEnvironmentVariable(const char* name) {
+  auto& stored_strings = *stored_environment_;
+  const std::string prefix = std::string(name) + "=";
+
+  auto it = std::find_if(
+      stored_strings.begin(), stored_strings.end(),
+      [&](const std::string& entry) { return entry.find(prefix, 0) == 0; });
+
+  return {it, it == stored_strings.end() ? std::string::npos : prefix.length()};
+}
+
+void Environment::RebuildAndSetGlobalEnviron() {
+  auto& stored_strings = *stored_environment_;
+  auto& pointers_vec = *environ_pointers_;
+
+  pointers_vec.clear();
+
+  for (std::string& entry : stored_strings) {
+    pointers_vec.push_back(entry.data());
+  }
+  pointers_vec.push_back(nullptr);
+
+  ::environ = pointers_vec.data();
+}
+
+char** Environment::InitializeGlobalEnviron() {
+  RebuildAndSetGlobalEnviron();
+  return ::environ;
+}
+
+bool Environment::EnvironmentNameIsValid(const char* name) {
+  return name && name[0] != '\0' && std::strchr(name, '=') == nullptr;
+}
+
+char* Environment::getenv(const char* name) {
+  if (!EnvironmentNameIsValid(name)) {
+    return nullptr;
+  }
+
+  auto [it, value_pos] = FindEnvironmentVariable(name);
+
+  if (it != stored_environment_->end()) {
+    return it->data() + value_pos;
+  }
+  return nullptr;
+}
+
+int Environment::setenv(const char* name, const char* value, int overwrite) {
+  if (!EnvironmentNameIsValid(name)) {
+    errno = EINVAL;
+    return -1;
+  }
+  if (!value) {
+    value = "";
+  }
+
+  std::string new_entry = std::string(name) + "=" + std::string(value);
+  bool changed_in_storage = false;
+
+  auto [it, unused_pos] = FindEnvironmentVariable(name);
+
+  if (it != stored_environment_->end()) {
+    if (overwrite) {
+      *it = new_entry;
+      changed_in_storage = true;
+    }
+  } else {
+    stored_environment_->push_back(new_entry);
+    changed_in_storage = true;
+  }
+
+  if (changed_in_storage) {
+    RebuildAndSetGlobalEnviron();
+  }
+  return 0;
+}
+
+int Environment::unsetenv(const char* name) {
+  if (!EnvironmentNameIsValid(name)) {
+    errno = EINVAL;
+    return -1;
+  }
+
+  bool changed_in_storage = false;
+  auto [it, unused_pos] = FindEnvironmentVariable(name);
+
+  if (it != stored_environment_->end()) {
+    stored_environment_->erase(it);
+    changed_in_storage = true;
+  }
+
+  if (changed_in_storage) {
+    RebuildAndSetGlobalEnviron();
+  }
+  return 0;
+}
+
+}  // namespace
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char** environ = Environment::InitializeGlobalEnviron();
+
+char* getenv(const char* name) {
+  return Environment::getenv(name);
+}
+
+int setenv(const char* name, const char* value, int overwrite) {
+  return Environment::setenv(name, value, overwrite);
+}
+
+int unsetenv(const char* name) {
+  return Environment::unsetenv(name);
+}
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
