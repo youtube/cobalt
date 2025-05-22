37e086d6e52c3669a8ecf0667d1c9bb77e9e71dc
Austin Osagie <austinosagie@google.com>
2025-05-22 16:42:16 -0700

    Move include order

--- starboard/android/shared/media_drm_bridge.cc
+++ starboard/android/shared/media_drm_bridge.cc
@@ -12,12 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "starboard/android/shared/media_drm_bridge.h"
+
 #include <jni.h>
 
 #include <cstring>
 #include <vector>
-
-#include "starboard/android/shared/media_drm_bridge.h"
 
 #include "starboard/android/shared/drm_system.h"
 
