71867235e24354ea8089b6a6e7f8bb4913d908cc
Adam Walls <avvall@google.com>
2025-05-22 17:06:02 +0000

    Add pageload metrics initialization
    This adds a few key classes for observing page load metrics:
      * page_load_metrics_initialize
      * page_load_metrics_memory_tracker_factory
    
    b/418004687

---
+++ cobalt/browser/page_load_metrics_initialize.cc
@@ -0,0 +1,109 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "cobalt/browser/page_load_metrics_initialize.h"
+
+#include "cobalt/browser/page_load_metrics_memory_tracker_factory.h"
+#include "components/page_load_metrics/browser/metrics_web_contents_observer.h"
+#include "components/page_load_metrics/browser/page_load_metrics_embedder_base.h"
+#include "components/page_load_metrics/browser/page_load_metrics_memory_tracker.h"
+
+namespace content {
+class BrowserContext;
+}  // namespace content
+
+namespace page_load_metrics {
+class PageLoadMetricsMemoryTracker;
+}  // namespace page_load_metrics
+
+namespace cobalt {
+
+namespace {
+
+class PageLoadMetricsEmbedder
+    : public page_load_metrics::PageLoadMetricsEmbedderBase {
+ public:
+  explicit PageLoadMetricsEmbedder(content::WebContents* web_contents);
+
+  PageLoadMetricsEmbedder(const PageLoadMetricsEmbedder&) = delete;
+  PageLoadMetricsEmbedder& operator=(const PageLoadMetricsEmbedder&) = delete;
+
+  ~PageLoadMetricsEmbedder() override;
+
+  // page_load_metrics::PageLoadMetricsEmbedderBase:
+  bool IsNewTabPageUrl(const GURL& url) override;
+  bool IsNoStatePrefetch(content::WebContents* web_contents) override;
+  bool IsExtensionUrl(const GURL& url) override;
+  bool IsSidePanel(content::WebContents* web_contents) override;
+  page_load_metrics::PageLoadMetricsMemoryTracker*
+  GetMemoryTrackerForBrowserContext(
+      content::BrowserContext* browser_context) override;
+
+ protected:
+  // page_load_metrics::PageLoadMetricsEmbedderBase:
+  void RegisterEmbedderObservers(
+      page_load_metrics::PageLoadTracker* tracker) override;
+};
+
+PageLoadMetricsEmbedder::PageLoadMetricsEmbedder(
+    content::WebContents* web_contents)
+    : PageLoadMetricsEmbedderBase(web_contents) {}
+
+PageLoadMetricsEmbedder::~PageLoadMetricsEmbedder() = default;
+
+void PageLoadMetricsEmbedder::RegisterEmbedderObservers(
+    page_load_metrics::PageLoadTracker* tracker) {}
+
+bool PageLoadMetricsEmbedder::IsNewTabPageUrl(const GURL& url) {
+  return false;
+}
+
+bool PageLoadMetricsEmbedder::IsNoStatePrefetch(
+    content::WebContents* web_contents) {
+  return false;
+}
+
+bool PageLoadMetricsEmbedder::IsExtensionUrl(const GURL& url) {
+  return false;
+}
+
+bool PageLoadMetricsEmbedder::IsSidePanel(content::WebContents* web_contents) {
+  // The side panel is not supported on Android so this always returns false.
+  return false;
+}
+
+page_load_metrics::PageLoadMetricsMemoryTracker*
+PageLoadMetricsEmbedder::GetMemoryTrackerForBrowserContext(
+    content::BrowserContext* browser_context) {
+  if (!base::FeatureList::IsEnabled(features::kV8PerFrameMemoryMonitoring)) {
+    return nullptr;
+  }
+
+  return page_load_metrics::PageLoadMetricsMemoryTrackerFactory::
+      GetForBrowserContext(browser_context);
+}
+
+}  // namespace
+
+void InitializePageLoadMetricsForWebContents(
+    content::WebContents* web_contents) {
+  // Change this method? consider to modify the peer in
+  // chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
+  // weblayer/browser/page_load_metrics_initialize.cc
+  // as well.
+  page_load_metrics::MetricsWebContentsObserver::CreateForWebContents(
+      web_contents, std::make_unique<PageLoadMetricsEmbedder>(web_contents));
+}
+
+}  // namespace cobalt
---
+++ cobalt/browser/page_load_metrics_memory_tracker_factory.h
@@ -0,0 +1,47 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef COBALT_BROWSER_PAGE_LOAD_METRICS_MEMORY_TRACKER_FACTORY_H_
+#define COBALT_BROWSER_PAGE_LOAD_METRICS_MEMORY_TRACKER_FACTORY_H_
+
+#include "components/keyed_service/content/browser_context_keyed_service_factory.h"
+
+namespace page_load_metrics {
+
+class PageLoadMetricsMemoryTracker;
+
+class PageLoadMetricsMemoryTrackerFactory
+    : public BrowserContextKeyedServiceFactory {
+ public:
+  static page_load_metrics::PageLoadMetricsMemoryTracker* GetForBrowserContext(
+      content::BrowserContext* context);
+
+  static PageLoadMetricsMemoryTrackerFactory* GetInstance();
+
+  PageLoadMetricsMemoryTrackerFactory();
+
+ private:
+  // BrowserContextKeyedServiceFactory:
+  bool ServiceIsCreatedWithBrowserContext() const override;
+
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
+      content::BrowserContext* context) const override;
+
+  content::BrowserContext* GetBrowserContextToUse(
+      content::BrowserContext* context) const override;
+};
+
+}  // namespace page_load_metrics
+
+#endif  // COBALT_BROWSER_PAGE_LOAD_METRICS_MEMORY_TRACKER_FACTORY_H_
