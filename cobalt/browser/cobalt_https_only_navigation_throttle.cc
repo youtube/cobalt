d578728b1f4abbe026e57e1b3bc29dcc6a204a60
Adam Walls <avvall@google.com>
2025-05-22 21:21:22 +0000

    Allow navigations to chrome schemed sites (#5845)
    This enables site navigations on non-https schemes for non-release builds.
    
    
    Test: Connect to devtools and navigate to `chrome://histograms`. Verify screen is not blank.
    b/419581276

--- cobalt/browser/cobalt_https_only_navigation_throttle.cc
+++ cobalt/browser/cobalt_https_only_navigation_throttle.cc
@@ -17,25 +17,32 @@ CobaltHttpsOnlyNavigationThrottle::~CobaltHttpsOnlyNavigationThrottle() =
 // Called when a network request is about to be made for this navigation.
 content::NavigationThrottle::ThrottleCheckResult
 CobaltHttpsOnlyNavigationThrottle::WillStartRequest() {
+#if defined(COBALT_IS_RELEASE_BUILD)
   const GURL& url = navigation_handle()->GetURL();
-  if (!(navigation_handle()->GetURL().SchemeIs(url::kHttpsScheme))) {
+  if (!(url.SchemeIs(url::kHttpsScheme))) {
     LOG(WARNING) << "Navigation throttle canceling navigation due to "
                     "HTTPS-only violation";
     return content::NavigationThrottle::ThrottleCheckResult(
         content::NavigationThrottle::CANCEL, net::ERR_BLOCKED_BY_CLIENT);
   }
+#endif  // COBALT_IS_RELEASE_BUILD
+
   return content::NavigationThrottle::PROCEED;
 }
 
 // Called when a server redirect is received by the navigation.
 content::NavigationThrottle::ThrottleCheckResult
 CobaltHttpsOnlyNavigationThrottle::WillRedirectRequest() {
-  if (!(navigation_handle()->GetURL().SchemeIs(url::kHttpsScheme))) {
+#if defined(COBALT_IS_RELEASE_BUILD)
+  const GURL& url = navigation_handle()->GetURL();
+  if (!(url.SchemeIs(url::kHttpsScheme))) {
     LOG(WARNING) << "Navigation throttle canceling navigation due to "
                     "HTTPS-only violation";
     return content::NavigationThrottle::ThrottleCheckResult(
         content::NavigationThrottle::CANCEL, net::ERR_BLOCKED_BY_CLIENT);
   }
+#endif  // COBALT_IS_RELEASE_BUILD
+
   return content::NavigationThrottle::PROCEED;
 }
 
