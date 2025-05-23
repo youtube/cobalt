ac084a9d63dd12a0c5ed286839b737fef776026a
Bo-Rong Chen <borongchen@google.com>
2025-05-22 17:19:34 -0700

    [linux] Support decode-to-texture mode
    Support decode-to-texture mode on linux.
    
    b/375070492

--- media/gpu/starboard/starboard_gpu_factory_impl.cc
+++ media/gpu/starboard/starboard_gpu_factory_impl.cc
@@ -15,6 +15,8 @@
 #include "media/gpu/starboard/starboard_gpu_factory_impl.h"
 
 #include "base/memory/scoped_refptr.h"
+#include "gpu/command_buffer/service/shared_image/shared_image_factory.h"
+#include "gpu/command_buffer/service/texture_manager.h"
 #include "gpu/ipc/service/gpu_channel.h"
 #include "gpu/ipc/service/gpu_channel_manager.h"
 #include "ui/gl/gl_bindings.h"
@@ -22,7 +24,21 @@
 #include "ui/gl/scoped_make_current.h"
 #include "ui/gl/scoped_restore_texture.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "gpu/command_buffer/service/shared_image/android_video_image_backing.h"
+#include "media/gpu/android/starboard/starboard_codec_image.h"
+#else  // BUILDFLAG(IS_ANDROID)
+#include "gpu/command_buffer/service/shared_image/starboard/starboard_video_image_backing.h"
+#endif  // BUILDFLAG(IS_ANDROID)
+
 namespace media {
+namespace {
+
+bool MakeContextCurrent(gpu::CommandBufferStub* stub) {
+  return stub && stub->decoder_context()->MakeCurrent();
+}
+
+}  // namespace
 
 StarboardGpuFactoryImpl::StarboardGpuFactoryImpl(GetStubCB get_stub_cb)
     : get_stub_cb_(std::move(get_stub_cb)) {
@@ -45,6 +61,90 @@ void StarboardGpuFactoryImpl::Initialize(base::UnguessableToken channel_token,
     stub_->AddDestructionObserver(this);
   }
   std::move(callback).Run();
+}
+
+void StarboardGpuFactoryImpl::RunSbDecodeTargetFunctionOnGpu(
+    SbDecodeTargetGlesContextRunnerTarget target_function,
+    void* target_function_context,
+    base::WaitableEvent* done_event) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  if (!MakeContextCurrent(stub_)) {
+    done_event->Signal();
+    return;
+  }
+  target_function(target_function_context);
+  done_event->Signal();
+}
+
+void StarboardGpuFactoryImpl::RunCallbackOnGpu(
+    base::OnceCallback<void()> callback,
+    base::WaitableEvent* done_event) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  if (!MakeContextCurrent(stub_)) {
+    done_event->Signal();
+    return;
+  }
+  std::move(callback).Run();
+
+  // Notify ANGLE that the External texture binding has changed
+  if (gl::g_current_gl_driver->ext.b_GL_ANGLE_texture_external_update) {
+    glInvalidateTextureANGLE(GL_TEXTURE_EXTERNAL_OES);
+  }
+
+  done_event->Signal();
+}
+
+void StarboardGpuFactoryImpl::CreateImageOnGpu(
+    const gfx::Size& coded_size,
+    const gfx::ColorSpace& color_space,
+    int plane_count,
+    std::vector<gpu::Mailbox>& mailboxes,
+    std::vector<uint32_t>& texture_service_ids,
+#if BUILDFLAG(IS_ANDROID)
+    scoped_refptr<gpu::RefCountedLock> drdc_lock,
+#endif  // BUILDFLAG(IS_ANDROID)
+    base::WaitableEvent* done_event) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  if (!MakeContextCurrent(stub_)) {
+    done_event->Signal();
+    return;
+  }
+
+  gpu::ContextResult result;
+  auto shared_context =
+      stub_->channel()->gpu_channel_manager()->GetSharedContextState(&result);
+  if (result != gpu::ContextResult::kSuccess) {
+    done_event->Signal();
+    return;
+  }
+
+  DCHECK_EQ(plane_count, texture_service_ids.size());
+  std::vector<scoped_refptr<gpu::gles2::TexturePassthrough>> textures;
+  for (int plane = 0; plane < plane_count; plane++) {
+    auto mailbox = gpu::Mailbox::GenerateForSharedImage();
+    auto texture = base::MakeRefCounted<gpu::gles2::TexturePassthrough>(
+        texture_service_ids[plane], GL_TEXTURE_EXTERNAL_OES);
+    mailboxes.push_back(mailbox);
+    textures.push_back(texture);
+  }
+#if BUILDFLAG(IS_ANDROID)
+  DCHECK_EQ(mailboxes.size(), 1);
+  DCHECK_EQ(textures.size(), 1);
+  auto starboard_codec_image = base::MakeRefCounted<StarboardCodecImage>(
+      std::move(textures[0]), drdc_lock);
+  auto shared_image = gpu::AndroidVideoImageBacking::Create(
+      mailboxes[0], coded_size, color_space, kTopLeft_GrSurfaceOrigin,
+      kPremul_SkAlphaType, std::move(starboard_codec_image),
+      std::move(shared_context), std::move(drdc_lock));
+#else   // BUILDFLAG(IS_ANDROID)
+  auto shared_image = std::make_unique<gpu::StarboardVideoImageBacking>(
+      mailboxes[0], coded_size, color_space, kTopLeft_GrSurfaceOrigin,
+      kPremul_SkAlphaType, std::move(textures), std::move(shared_context));
+#endif  // BUILDFLAG(IS_ANDROID)
+  DCHECK(stub_->channel()->gpu_channel_manager()->shared_image_manager());
+  stub_->channel()->shared_image_stub()->factory()->RegisterBacking(
+      std::move(shared_image));
+  done_event->Signal();
 }
 
 void StarboardGpuFactoryImpl::OnWillDestroyStub(bool /*have_context*/) {
