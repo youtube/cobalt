9d86831fe651ad2fc05b6f65f529d644ff485c96
Miguel Casas <mcasas@chromium.org>
2025-05-23 11:44:33 -0700

    cobalt: Move //cobalt/user_agent to //cobalt/browser/user_agent (#5862)
    //cobalt/user_agent files are only used by //cobalt/browser files,
    this PR/CL moves them there to clarify what's used where and also
    to simplify future folders classification for OWNERS purposes.
    
    Done via `git mv` and `tools/git/mass-rename.py` (sadly the latter
    didn't update the BUILD.gn files, those were done by hand).
    
    Bug: b/419821444
    
    original-hexsha: 920f927a511fc14c60a19c23a7fe98269afe2c2f

--- cobalt/browser/user_agent/user_agent_platform_info_test.cc
+++ cobalt/browser/user_agent/user_agent_platform_info_test.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "cobalt/user_agent/user_agent_platform_info.h"
+#include "cobalt/browser/user_agent/user_agent_platform_info.h"
 
 #include <map>
 
