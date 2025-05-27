9e168c84eab5cdb897764758371f56eabf2d3283
Madhura Jayaraman <madjay@google.com>
2025-05-27 14:48:59 -0700

    Tests and ABI wrap for readdir (#5632)
    b/412715489
    
    original-hexsha: beb13f3de574903b7a8a373d0b0dfc84295beb8b

--- starboard/tools/api_leak_detector/api_leak_detector.py
+++ starboard/tools/api_leak_detector/api_leak_detector.py
@@ -213,6 +213,11 @@ _ALLOWED_SB_GE_16_POSIX_SYMBOLS = [
     'unlink',
     'usleep',
     'write',
+    'opendir',
+    'closedir',
+    'readdir_r',
+    'malloc_usable_size',
+    'readdir',
 ]
 
 
