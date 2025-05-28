385dc479fbbd9a905e5eb51d4d9ff7075649abc1
Miguel Casas <mcasas@chromium.org>
2025-05-28 10:33:32 -0700

    cobalt: Fold //cobalt/configuration under  //cobalt/browser/h5vcc_system (#5891)
    //cobalt/configuration files are only used by
    //cobalt/browser/h5vcc_system. This CL/PR folds the former under
    the latter to reduce the number of //cobalt folders, in turn to
    ease the OWNERS/reviewers set up.
    
    No functional changes intended, if it compiles it's good to go.
    
    Bug: b/419821444
    
    original-hexsha: 211d55adef0890ab92f6ee1c80e3c852f4e3551d

--- cobalt/browser/h5vcc_system/configuration.cc
+++ cobalt/browser/h5vcc_system/configuration.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "cobalt/configuration/configuration.h"
+#include "cobalt/browser/h5vcc_system/configuration.h"
 
 #include <string>
 
