71f56b2136017067afca5b0fa48ca6a1c18c4bd9
Austin Osagie <austinosagie@google.com>
2025-05-29 15:32:13 -0700

    Added more tests

--- starboard/nplb/posix_compliance/posix_isatty_test.cc
+++ starboard/nplb/posix_compliance/posix_isatty_test.cc
@@ -62,7 +62,7 @@ TEST_F(PosixIsattyTest, HandlesInvalidFd) {
 
 // Tests that isatty() does not recognize closed file descriptors as a tty.
 TEST_F(PosixIsattyTest, HandlesClosedFd) {
-  const char* kTestFileName = "isatty_test.txt";
+  const char* kTestFileName = "isatty_test_closed.txt";
 
   int fd = open(kTestFileName, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
   ASSERT_NE(-1, fd) << "Failed to open test file: " << strerror(errno);
@@ -72,6 +72,85 @@ TEST_F(PosixIsattyTest, HandlesClosedFd) {
   EXPECT_EQ(EBADF, errno) << "Expected EBADF, got " << strerror(errno);
 
   remove(kTestFileName);
+}
+
+// Tests that isatty() sets ENOTTY on an open file.
+TEST_F(PosixIsattyTest, HandlesOpenFile) {
+  const char* kTestFileName = "isatty_test_open.txt";
+
+  int fd = open(kTestFileName, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+  ASSERT_NE(-1, fd) << "Failed to open test file: " << strerror(errno);
+
+  EXPECT_FALSE(isatty(fd));
+  EXPECT_EQ(ENOTTY, errno) << "Expected ENOTTY, got " << strerror(errno);
+
+  close(fd);
+  remove(kTestFileName);
+}
+
+// Tests that isatty() doesn't recognize devices as ttys.
+TEST_F(PosixIsattyTest, HandlesDevices) {
+  int fd = open("/dev/null", O_RDWR);
+  ASSERT_NE(-1, fd) << "Failed to open /dev/null";
+  EXPECT_FALSE(isatty(fd));
+  EXPECT_TRUE(errno == 0 || errno == ENOTTY)
+      << "Expected 0 or ENOTTY, got " << strerror(errno);
+  close(fd);
+  errno = 0;
+
+  fd = open("/dev/zero", O_RDONLY);
+  ASSERT_NE(-1, fd) << "Failed to open /dev/zero";
+  EXPECT_FALSE(isatty(fd));
+  EXPECT_TRUE(errno == 0 || errno == ENOTTY)
+      << "Expected 0 or ENOTTY, got " << strerror(errno);
+  close(fd);
+}
+
+// Tests that isatty() recognizes a duplicate of a valid file descriptor as a
+// tty.
+TEST_F(PosixIsattyTest, HandlesValidDuplicateFd) {
+  ASSERT_TRUE(isatty(STDIN_FILENO));
+  ASSERT_EQ(0, errno) << "errno was set: " << strerror(errno);
+
+  int fd = dup(STDIN_FILENO);
+  EXPECT_TRUE(isatty(fd));
+  EXPECT_EQ(0, errno) << "errno was set: " << strerror(errno);
+}
+
+// Tests that isatty() does not recognize a duplicate of an invalid file
+// descriptor as a tty.
+TEST_F(PosixIsattyTest, HandlesInvalidDuplicateFd) {
+  int fd = open("/dev/null", O_RDWR);
+  ASSERT_NE(-1, fd) << "Failed to open /dev/null";
+  ASSERT_FALSE(isatty(fd));
+  ASSERT_TRUE(errno == 0 || errno == ENOTTY)
+      << "Expected 0 or ENOTTY, got " << strerror(errno);
+  errno = 0;
+
+  int fd_copy = dup(fd);
+  EXPECT_FALSE(isatty(fd_copy));
+  EXPECT_TRUE(errno == 0 || errno == ENOTTY)
+      << "Expected 0 or ENOTTY, got " << strerror(errno);
+
+  close(fd);
+}
+
+// Tests that isatty() doesn't recognize a pipe as a tty.
+TEST_F(PosixIsattyTest, HandlesPipes) {
+  int pipe_fds[2];
+  ASSERT_EQ(pipe(pipe_fds), 0);
+
+  EXPECT_FALSE(isatty(pipe_fds[0]));
+  EXPECT_TRUE(errno == 0 || errno == ENOTTY)
+      << "Expected 0 or ENOTTY, got " << strerror(errno);
+  errno = 0;
+
+  EXPECT_FALSE(isatty(pipe_fds[1]));
+  EXPECT_TRUE(errno == 0 || errno == ENOTTY)
+      << "Expected 0 or ENOTTY, got " << strerror(errno);
+
+  close(pipe_fds[0]);
+  close(pipe_fds[1]);
 }
 
 }  // namespace
