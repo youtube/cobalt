eaf4be2cf7dd7bfb345740964f1c3629cb6e08e3
Austin Osagie <austinosagie@google.com>
2025-05-30 12:26:51 -0700

    Reduce errno check inconsistency

--- starboard/nplb/posix_compliance/posix_isatty_test.cc
+++ starboard/nplb/posix_compliance/posix_isatty_test.cc
@@ -38,7 +38,8 @@ TEST_F(PosixIsattyTest, HandlesStdin) {
 // Tests that isatty() recognizes /dev/tty as a tty.
 TEST_F(PosixIsattyTest, HandlesDevTty) {
   int fd = open("/dev/tty", O_RDWR);
-  ASSERT_NE(-1, fd) << "Failed to open /dev/tty";
+  ASSERT_NE(-1, fd) << "Failed to open /dev/tty: " << strerror(errno);
+  
   EXPECT_TRUE(isatty(fd));
   EXPECT_EQ(0, errno) << "errno was set: " << strerror(errno);
   close(fd);
@@ -88,18 +89,19 @@ TEST_F(PosixIsattyTest, HandlesOpenFile) {
 // Tests that isatty() doesn't recognize non-terminal devices as ttys.
 TEST_F(PosixIsattyTest, HandlesDevices) {
   int fd = open("/dev/null", O_RDWR);
-  ASSERT_NE(-1, fd) << "Failed to open /dev/null";
+  ASSERT_NE(-1, fd) << "Failed to open /dev/null: " << strerror(errno);
+
   EXPECT_FALSE(isatty(fd));
-  EXPECT_TRUE(errno == 0 || errno == ENOTTY)
-      << "Expected 0 or ENOTTY, got " << strerror(errno);
+  EXPECT_EQ(ENOTTY, errno) << "Expected ENOTTY, got " << strerror(errno);
   close(fd);
+
   errno = 0;
 
   fd = open("/dev/zero", O_RDONLY);
-  ASSERT_NE(-1, fd) << "Failed to open /dev/zero";
+  ASSERT_NE(-1, fd) << "Failed to open /dev/zero: " << strerror(errno);
+
   EXPECT_FALSE(isatty(fd));
-  EXPECT_TRUE(errno == 0 || errno == ENOTTY)
-      << "Expected 0 or ENOTTY, got " << strerror(errno);
+  EXPECT_EQ(ENOTTY, errno) << "Expected ENOTTY, got " << strerror(errno);
   close(fd);
 }
 
@@ -119,16 +121,15 @@ TEST_F(PosixIsattyTest, HandlesValidDuplicateFd) {
 // descriptor as a tty.
 TEST_F(PosixIsattyTest, HandlesInvalidDuplicateFd) {
   int fd = open("/dev/null", O_RDWR);
-  ASSERT_NE(-1, fd) << "Failed to open /dev/null";
+  ASSERT_NE(-1, fd) << "Failed to open /dev/null: " << strerror(errno);
   ASSERT_FALSE(isatty(fd));
-  ASSERT_TRUE(errno == 0 || errno == ENOTTY)
-      << "Expected 0 or ENOTTY, got " << strerror(errno);
+  ASSERT_EQ(ENOTTY, errno) << "Expected ENOTTY, got " << strerror(errno);
+
   errno = 0;
 
   int fd_copy = dup(fd);
   EXPECT_FALSE(isatty(fd_copy));
-  EXPECT_TRUE(errno == 0 || errno == ENOTTY)
-      << "Expected 0 or ENOTTY, got " << strerror(errno);
+  EXPECT_EQ(ENOTTY, errno) << "Expected ENOTTY, got " << strerror(errno);
 
   close(fd);
   close(fd_copy);
@@ -140,13 +141,11 @@ TEST_F(PosixIsattyTest, HandlesPipes) {
   ASSERT_EQ(pipe(pipe_fds), 0);
 
   EXPECT_FALSE(isatty(pipe_fds[0]));
-  EXPECT_TRUE(errno == 0 || errno == ENOTTY)
-      << "Expected 0 or ENOTTY, got " << strerror(errno);
+  EXPECT_EQ(ENOTTY, errno) << "Expected ENOTTY, got " << strerror(errno);
   errno = 0;
 
   EXPECT_FALSE(isatty(pipe_fds[1]));
-  EXPECT_TRUE(errno == 0 || errno == ENOTTY)
-      << "Expected 0 or ENOTTY, got " << strerror(errno);
+  EXPECT_EQ(ENOTTY, errno) << "Expected ENOTTY, got " << strerror(errno);
 
   close(pipe_fds[0]);
   close(pipe_fds[1]);
