931df6fb1d361e3174378b7c77fcd8de371abd7f
Jelle Foks <jfoks@google.com>
2025-06-03 11:02:27 -0700

    Add clock() and time() from musl. (#5792)
    This adds clock() and time() from musl, implemented with the
    clock_gettime() that is available in Starboard.
    
    b/406083355
    original-hexsha: b9d62cd04992dfed54ead9d421a39814c73aea53

--- third_party/musl/src/include/time.h
+++ third_party/musl/src/include/time.h
@@ -3,7 +3,14 @@
 
 #include "../../include/time.h"
 
+#if defined(STARBOARD)
+hidden static inline int __clock_gettime(clockid_t clockid,
+                                         struct timespec* tp) {
+  clock_gettime(clockid, tp);
+}
+#else
 hidden int __clock_gettime(clockid_t, struct timespec *);
+#endif  // defined(STARBOARD)
 hidden int __clock_nanosleep(clockid_t, int, const struct timespec *, struct timespec *);
 
 hidden char *__asctime_r(const struct tm *, char *);
