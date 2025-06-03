db04bffab397a125c1070e0c52a2b908c04b8b57
Jelle Foks <jfoks@google.com>
2025-06-03 09:37:13 -0700

    This adds various sleep functions. (#5795)
    This adds clock_nanosleep() to Starboard, and
    adds sleep(), usleep(), nanosleep() from musl.
    
    b/406083355
    original-hexsha: d2d2cb336d65e4becc7172d0df5ba9352b7d70a1

--- starboard/elf_loader/exported_symbols.cc
+++ starboard/elf_loader/exported_symbols.cc
@@ -283,6 +283,7 @@ ExportedSymbols::ExportedSymbols() {
   REGISTER_WRAPPER(bind);
   REGISTER_WRAPPER(clock_gettime);
   REGISTER_WRAPPER(closedir);
+  REGISTER_WRAPPER(clock_nanosleep);
   REGISTER_WRAPPER(connect);
   if (errno_translation()) {
     REGISTER_WRAPPER(__errno_location);
