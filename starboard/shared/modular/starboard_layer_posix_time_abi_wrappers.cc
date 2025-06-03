506555a6965bed287013e74451788e6a3b77b822
Jelle Foks <jfoks@google.com>
2025-06-03 09:37:13 -0700

    This adds various sleep functions. (#5795)
    This adds clock_nanosleep() to Starboard, and
    adds sleep(), usleep(), nanosleep() from musl.
    
    b/406083355
    
    original-hexsha: d2d2cb336d65e4becc7172d0df5ba9352b7d70a1

--- starboard/shared/modular/starboard_layer_posix_time_abi_wrappers.cc
+++ starboard/shared/modular/starboard_layer_posix_time_abi_wrappers.cc
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include "starboard/shared/modular/starboard_layer_posix_time_abi_wrappers.h"
+#include "starboard/shared/modular/starboard_layer_posix_errno_abi_wrappers.h"
 
 int __abi_wrap_clock_gettime(int /* clockid_t */ musl_clock_id,
                              struct musl_timespec* mts) {
@@ -23,11 +24,51 @@ int __abi_wrap_clock_gettime(int /* clockid_t */ musl_clock_id,
   // The type from platform toolchain.
   // Map the MUSL clock_id constants to platform constants.
   clockid_t clock_id = musl_clock_id_to_clock_id(musl_clock_id);
+  if (clock_id == MUSL_CLOCK_INVALID) {
+    errno = EINVAL;
+    return -1;
+  }
   struct timespec ts;  // The type from platform toolchain.
   int retval = clock_gettime(clock_id, &ts);
-  mts->tv_sec = ts.tv_sec;
+  mts->tv_sec = static_cast<int64_t>(ts.tv_sec);
   mts->tv_nsec = ts.tv_nsec;
   return retval;
+}
+
+int __abi_wrap_clock_nanosleep(int /* clockid_t */ musl_clock_id,
+                               int flags,
+                               const struct musl_timespec* mts,
+                               struct musl_timespec* mremain) {
+  if (!mts) {
+    return MUSL_EFAULT;
+  }
+  if (mts->tv_sec < 0) {
+    // Invalid relative time.
+    return MUSL_EINVAL;
+  }
+  if ((flags & ~MUSL_TIMER_ABSTIME) != 0) {
+    // Invalid flags.
+    return MUSL_EINVAL;
+  }
+  struct timespec ts;
+  struct timespec remain;
+  ts.tv_sec = mts->tv_sec;
+  ts.tv_nsec = mts->tv_nsec;
+  if (musl_clock_id == MUSL_CLOCK_THREAD_CPUTIME_ID) {
+    return MUSL_EINVAL;
+  }
+  clockid_t clock_id = musl_clock_id_to_clock_id(musl_clock_id);
+  if (clock_id == MUSL_CLOCK_INVALID) {
+    return MUSL_EINVAL;
+  }
+  int retval =
+      clock_nanosleep(clock_id, musl_nanosleep_flags_to_nanosleep_flags(flags),
+                      &ts, mremain ? &remain : nullptr);
+  if (mremain && !((retval == EINTR) && (flags & MUSL_TIMER_ABSTIME))) {
+    mremain->tv_sec = static_cast<int64_t>(remain.tv_sec);
+    mremain->tv_nsec = remain.tv_nsec;
+  }
+  return errno_to_musl_errno(retval);
 }
 
 int64_t __abi_wrap_time(int64_t* /* time_t* */ musl_tloc) {
