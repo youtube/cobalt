341f18f31b1e7a3ca4baccfaf2cfca218fc9b9ea
Sherry Zhou <sherryzy@google.com>
2025-06-04 15:05:46 -0700

    [POSIX] Remove unnecessary pthread_atfork call (#5924)
    The pthread_atfork() function is designed to manage locks safely across
    fork() calls in multi-process applications.
    
    Since Cobalt operates as a single-process application, this function
    call is unnecessary. This PR removes the call for Starboardbuilds by
    adding a `!BUILDFLAG(IS_STARBOARD)` check.
    
    Note: By running api_leakage_detector.py, I think fork() is called in
    Cobalt as well. We may remove those calls as well?
    
    Fixed: 399696581
    
    original-hexsha: 9e086d9a06ff90ebcbcdd2d25744c50ba81d789c

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/threading/platform_thread_posix.cc
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/threading/platform_thread_posix.cc
@@ -55,12 +55,14 @@ std::atomic<bool> g_main_thread_tid_cache_valid = false;
 // also updated by PlatformThread::CurrentId().
 thread_local bool g_is_main_thread = true;
 
+#if !BUILDFLAG(IS_STARBOARD)
 class InitAtFork {
  public:
   InitAtFork() {
     pthread_atfork(nullptr, nullptr, internal::InvalidateTidCache);
   }
 };
+#endif // !BUILDFLAG(IS_STARBOARD)
 
 }  // namespace
 
@@ -81,7 +83,10 @@ PlatformThreadId PlatformThread::CurrentId() {
 #if BUILDFLAG(IS_APPLE)
   return pthread_mach_thread_np(pthread_self());
 #elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+
+#if !BUILDFLAG(IS_STARBOARD)
   static InitAtFork init_at_fork;
+#endif // !BUILDFLAG(IS_STARBOARD)
   if (g_thread_id == -1 ||
       (g_is_main_thread &&
        !g_main_thread_tid_cache_valid.load(std::memory_order_relaxed))) {
