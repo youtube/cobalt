youtube/cobalt

commit b7f1130069f942a3583e8ce1de75bd0fe953613c

[media] Migrate MediaDrmBridge methods to jni generator (#5833)
Creates a C++ wrapper class `MediaDrmBridge` as an interface between the
native code and Java `MediaDrmBridge`. Additionally, all calls between
the Java `MediaDrmBridge` and native code have been updated to use JNI
generator.

b/417241844

original-hexsha: cb4bf3c25112ab82a6b2923e93114117516c4001

starboard/android/shared/media_drm_bridge.cc
@@ -1,0 +1,279 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "starboard/android/shared/media_drm_bridge.h"
+
+#include <jni.h>
+
+#include <cstring>
+#include <vector>
+
+#include "base/android/jni_array.h"
+#include "base/android/jni_string.h"
+#include "starboard/common/log.h"
+
+// Must come after all headers that specialize FromJniType() / ToJniType().
+#include "cobalt/android/jni_headers/MediaDrmBridge_jni.h"
+
+namespace starboard::android::shared {
+namespace {
+
+using base::android::AttachCurrentThread;
+using base::android::ConvertJavaStringToUTF8;
+using base::android::ConvertUTF8ToJavaString;
+using base::android::ToJavaByteArray;
+
+const char kNoUrl[] = "";
+
+// Using all capital names to be consistent with other Android media statuses.
+// They are defined in the same order as in their Java counterparts.  Their
+// values should be kept in consistent with their Java counterparts defined in
+// android.media.MediaDrm.KeyStatus.
+constexpr jint MEDIA_DRM_KEY_STATUS_EXPIRED = 1;
+constexpr jint MEDIA_DRM_KEY_STATUS_INTERNAL_ERROR = 4;
+constexpr jint MEDIA_DRM_KEY_STATUS_OUTPUT_NOT_ALLOWED = 2;
+constexpr jint MEDIA_DRM_KEY_STATUS_PENDING = 3;
+constexpr jint MEDIA_DRM_KEY_STATUS_USABLE = 0;
+
+// They must have the same values as defined in MediaDrm.KeyRequest.
+constexpr jint REQUEST_TYPE_INITIAL = 0;
+constexpr jint REQUEST_TYPE_RENEWAL = 1;
+constexpr jint REQUEST_TYPE_RELEASE = 2;
+
+SbDrmSessionRequestType SbDrmSessionRequestTypeFromMediaDrmKeyRequestType(
+    jint request_type) {
+  if (request_type == REQUEST_TYPE_INITIAL) {
+    return kSbDrmSessionRequestTypeLicenseRequest;
+  }
+  if (request_type == REQUEST_TYPE_RENEWAL) {
+    return kSbDrmSessionRequestTypeLicenseRenewal;
+  }
+  if (request_type == REQUEST_TYPE_RELEASE) {
+    return kSbDrmSessionRequestTypeLicenseRelease;
+  }
+  SB_NOTREACHED();
+  return kSbDrmSessionRequestTypeLicenseRequest;
+}
+
+}  // namespace
+
+MediaDrmBridge::MediaDrmBridge(MediaDrmBridge::Host* host,
+                               const char* key_system)
+    : host_(host) {
+  SB_DCHECK(host_);
+  JNIEnv* env = AttachCurrentThread();
+
+  ScopedJavaLocalRef<jstring> j_key_system(
+      ConvertUTF8ToJavaString(env, key_system));
+  ScopedJavaLocalRef<jobject> j_media_drm_bridge(Java_MediaDrmBridge_create(
+      env, j_key_system, reinterpret_cast<jlong>(this)));
+
+  if (j_media_drm_bridge.is_null()) {
+    SB_LOG(ERROR) << "Failed to create MediaDrmBridge.";
+    return;
+  }
+
+  ScopedJavaLocalRef<jobject> j_media_crypto(
+      Java_MediaDrmBridge_getMediaCrypto(env, j_media_drm_bridge));
+
+  if (j_media_crypto.is_null()) {
+    SB_LOG(ERROR) << "Failed to create MediaCrypto.";
+    return;
+  }
+
+  j_media_drm_bridge_.Reset(env, j_media_drm_bridge.obj());
+  j_media_crypto_.Reset(env, j_media_crypto.obj());
+
+  SB_LOG(INFO) << "Created MediaDrmBridge.";
+}
+
+MediaDrmBridge::~MediaDrmBridge() {
+  if (!j_media_drm_bridge_.is_null()) {
+    Java_MediaDrmBridge_destroy(AttachCurrentThread(), j_media_drm_bridge_);
+  }
+}
+
+void MediaDrmBridge::CreateSession(int ticket,
+                                   const std::vector<const uint8_t>& init_data,
+                                   const std::string& mime) const {
+  JNIEnv* env = AttachCurrentThread();
+
+  JniIntWrapper j_ticket = static_cast<jint>(ticket);
+  ScopedJavaLocalRef<jbyteArray> j_init_data = ScopedJavaLocalRef(
+      ToJavaByteArray(env, init_data.data(), init_data.size()));
+  ScopedJavaLocalRef<jstring> j_mime =
+      ScopedJavaLocalRef(ConvertUTF8ToJavaString(env, mime.c_str()));
+
+  Java_MediaDrmBridge_createSession(env, j_media_drm_bridge_, j_ticket,
+                                    j_init_data, j_mime);
+}
+
+bool MediaDrmBridge::UpdateSession(int ticket,
+                                   const void* key,
+                                   int key_size,
+                                   const void* session_id,
+                                   int session_id_size,
+                                   std::string* error_msg) const {
+  JNIEnv* env = AttachCurrentThread();
+
+  ScopedJavaLocalRef<jbyteArray> j_session_id(ToJavaByteArray(
+      env, static_cast<const uint8_t*>(session_id), session_id_size));
+  ScopedJavaLocalRef<jbyteArray> j_response(
+      ToJavaByteArray(env, static_cast<const uint8_t*>(key), key_size));
+  JniIntWrapper j_ticket = JniIntWrapper(ticket);
+
+  ScopedJavaLocalRef<jobject> j_update_result(Java_MediaDrmBridge_updateSession(
+      env, j_media_drm_bridge_, ticket, j_session_id, j_response));
+  *error_msg = ConvertJavaStringToUTF8(
+      Java_UpdateSessionResult_getErrorMessage(env, j_update_result));
+
+  return Java_UpdateSessionResult_isSuccess(env, j_update_result) == JNI_TRUE;
+}
+
+void MediaDrmBridge::CloseSession(const std::string& session_id) const {
+  JNIEnv* env = AttachCurrentThread();
+
+  ScopedJavaLocalRef<jbyteArray> j_session_id =
+      ToJavaByteArray(env, reinterpret_cast<const uint8_t*>(session_id.data()),
+                      session_id.size());
+
+  Java_MediaDrmBridge_closeSession(env, j_media_drm_bridge_, j_session_id);
+}
+
+const void* MediaDrmBridge::GetMetrics(int* size) {
+  JNIEnv* env = AttachCurrentThread();
+
+  ScopedJavaLocalRef<jbyteArray> j_metrics =
+      Java_MediaDrmBridge_getMetricsInBase64(env, j_media_drm_bridge_);
+
+  if (j_metrics.is_null()) {
+    *size = 0;
+    return nullptr;
+  }
+
+  jbyte* metrics_elements = env->GetByteArrayElements(j_metrics.obj(), nullptr);
+  jsize metrics_size = base::android::SafeGetArrayLength(env, j_metrics);
+  SB_DCHECK(metrics_elements);
+
+  metrics_.assign(metrics_elements, metrics_elements + metrics_size);
+
+  env->ReleaseByteArrayElements(j_metrics.obj(), metrics_elements, JNI_ABORT);
+  *size = static_cast<int>(metrics_.size());
+
+  return metrics_.data();
+}
+
+bool MediaDrmBridge::CreateMediaCryptoSession() {
+  bool result = Java_MediaDrmBridge_createMediaCryptoSession(
+      AttachCurrentThread(), j_media_drm_bridge_);
+  if (!result && !j_media_crypto_.is_null()) {
+    j_media_crypto_.Reset();
+    return false;
+  }
+
+  return true;
+}
+
+void MediaDrmBridge::OnSessionMessage(JNIEnv* env,
+                                      jint ticket,
+                                      const JavaParamRef<jbyteArray>& sessionId,
+                                      jint requestType,
+                                      const JavaParamRef<jbyteArray>& message) {
+  jbyte* session_id_elements = env->GetByteArrayElements(sessionId, nullptr);
+  jsize session_id_size = env->GetArrayLength(sessionId);
+
+  jbyte* message_elements = env->GetByteArrayElements(message, nullptr);
+  jsize message_size = env->GetArrayLength(message);
+
+  SB_DCHECK(session_id_elements);
+  SB_DCHECK(message_elements);
+
+  host_->CallUpdateRequestCallback(
+      ticket, SbDrmSessionRequestTypeFromMediaDrmKeyRequestType(requestType),
+      session_id_elements, session_id_size, message_elements, message_size,
+      kNoUrl);
+  env->ReleaseByteArrayElements(sessionId, session_id_elements, JNI_ABORT);
+  env->ReleaseByteArrayElements(message, message_elements, JNI_ABORT);
+}
+
+void MediaDrmBridge::OnKeyStatusChange(
+    JNIEnv* env,
+    const JavaParamRef<jbyteArray>& sessionId,
+    const JavaParamRef<jobjectArray>& keyInformation) {
+  jbyte* session_id_elements = env->GetByteArrayElements(sessionId, nullptr);
+  jsize session_id_size = env->GetArrayLength(sessionId);
+
+  SB_DCHECK(session_id_elements);
+
+  // nullptr array indicates key status isn't supported (i.e. Android API < 23)
+  jsize length =
+      (keyInformation == nullptr) ? 0 : env->GetArrayLength(keyInformation);
+  std::vector<SbDrmKeyId> drm_key_ids(length);
+  std::vector<SbDrmKeyStatus> drm_key_statuses(length);
+
+  static jclass mediaDrmKeyStatusClass =
+      env->FindClass("android/media/MediaDrm$KeyStatus");
+  SB_DCHECK(mediaDrmKeyStatusClass);
+
+  static jmethodID getKeyIdMethod =
+      env->GetMethodID(mediaDrmKeyStatusClass, "getKeyId", "()[B");
+  SB_DCHECK(getKeyIdMethod);
+
+  static jmethodID getStatusCodeMethod =
+      env->GetMethodID(mediaDrmKeyStatusClass, "getStatusCode", "()I");
+  SB_DCHECK(getStatusCodeMethod);
+
+  for (jsize i = 0; i < length; ++i) {
+    jobject j_key_status = env->GetObjectArrayElement(keyInformation, i);
+    jbyteArray j_key_id = static_cast<jbyteArray>(
+        env->CallObjectMethod(j_key_status, getKeyIdMethod));
+
+    jbyte* key_id_elements = env->GetByteArrayElements(j_key_id, nullptr);
+    jsize key_id_size = env->GetArrayLength(j_key_id);
+    SB_DCHECK(key_id_elements);
+
+    SB_DCHECK(key_id_size <= sizeof(drm_key_ids[i].identifier));
+    memcpy(drm_key_ids[i].identifier, key_id_elements, key_id_size);
+    env->ReleaseByteArrayElements(j_key_id, key_id_elements, JNI_ABORT);
+    drm_key_ids[i].identifier_size = key_id_size;
+
+    jint j_status_code = env->CallIntMethod(j_key_status, getStatusCodeMethod);
+    if (j_status_code == MEDIA_DRM_KEY_STATUS_EXPIRED) {
+      drm_key_statuses[i] = kSbDrmKeyStatusExpired;
+    } else if (j_status_code == MEDIA_DRM_KEY_STATUS_INTERNAL_ERROR) {
+      drm_key_statuses[i] = kSbDrmKeyStatusError;
+    } else if (j_status_code == MEDIA_DRM_KEY_STATUS_OUTPUT_NOT_ALLOWED) {
+      drm_key_statuses[i] = kSbDrmKeyStatusRestricted;
+    } else if (j_status_code == MEDIA_DRM_KEY_STATUS_PENDING) {
+      drm_key_statuses[i] = kSbDrmKeyStatusPending;
+    } else if (j_status_code == MEDIA_DRM_KEY_STATUS_USABLE) {
+      drm_key_statuses[i] = kSbDrmKeyStatusUsable;
+    } else {
+      SB_NOTREACHED();
+      drm_key_statuses[i] = kSbDrmKeyStatusError;
+    }
+  }
+}
+
+// static
+bool MediaDrmBridge::IsWidevineSupported(JNIEnv* env) {
+  return Java_MediaDrmBridge_isWidevineCryptoSchemeSupported(env) == JNI_TRUE;
+}
+
+// static
+bool MediaDrmBridge::IsCbcsSupported(JNIEnv* env) {
+  return Java_MediaDrmBridge_isCbcsSchemeSupported(env) == JNI_TRUE;
+}
+
+}  // namespace starboard::android::shared

cobalt/android/BUILD.gn
@@ -41,6 +41,7 @@ generate_jni("jni_headers") {
     "apk/app/src/main/java/dev/cobalt/media/AudioOutputManager.java",
     "apk/app/src/main/java/dev/cobalt/media/MediaCodecBridge.java",
     "apk/app/src/main/java/dev/cobalt/media/MediaCodecBridgeBuilder.java",
+    "apk/app/src/main/java/dev/cobalt/media/MediaDrmBridge.java",
   ]
 
   if (!use_evergreen) {

cobalt/android/apk/app/src/main/java/dev/cobalt/media/MediaDrmBridge.java
@@ -41,9 +41,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
+import org.chromium.base.annotations.CalledByNative;
+import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.NativeMethods;
 
 /** A wrapper of the android MediaDrm class. */
-@UsedByNative
+@JNINamespace("starboard::android::shared")
 public class MediaDrmBridge {
   // Implementation Notes:
   // - A media crypto session (mMediaCryptoSession) is opened after MediaDrm
@@ -74,7 +77,7 @@ public class MediaDrmBridge {
   private static final UUID WIDEVINE_UUID = UUID.fromString("edef8ba9-79d6-4ace-a3c8-27dcd51d21ed");
 
   // Deprecated in API 26, but we still log it on earlier devices.
-  // We do handle STATUS_EXPIRED in nativeOnKeyStatusChange() for API 23+ devices.
+  // We do handle STATUS_EXPIRED in onKeyStatusChange() for API 23+ devices.
   @SuppressWarnings("deprecation")
   private static final int MEDIA_DRM_EVENT_KEY_EXPIRED = MediaDrm.EVENT_KEY_EXPIRED;
 
@@ -102,7 +105,6 @@ public class MediaDrmBridge {
 
   // Return value type for calls to updateSession(), which contains whether or not the call
   // succeeded, and optionally an error message (that is empty on success).
-  @UsedByNative
   private static class UpdateSessionResult {
     public enum Status {
       SUCCESS,
@@ -130,12 +132,12 @@ public class MediaDrmBridge {
           errorMessage + " StackTrace: " + android.util.Log.getStackTraceString(e));
     }
 
-    @UsedByNative
+    @CalledByNative("UpdateSessionResult")
     public boolean isSuccess() {
       return mIsSuccess;
     }
 
-    @UsedByNative
+    @CalledByNative("UpdateSessionResult")
     public String getErrorMessage() {
       return mErrorMessage;
     }
@@ -146,7 +148,7 @@ public class MediaDrmBridge {
    *
    * @param nativeMediaDrmBridge The native owner of this class.
    */
-  @UsedByNative
+  @CalledByNative
   static MediaDrmBridge create(String keySystem, long nativeMediaDrmBridge) {
     UUID cryptoScheme = WIDEVINE_UUID;
     if (!MediaDrm.isCryptoSchemeSupported(cryptoScheme)) {
@@ -180,7 +182,7 @@ public class MediaDrmBridge {
    *
    * @return true if the container and the crypto scheme is supported, or false otherwise.
    */
-  @UsedByNative
+  @CalledByNative
   static boolean isWidevineCryptoSchemeSupported() {
     return MediaDrm.isCryptoSchemeSupported(WIDEVINE_UUID);
   }
@@ -190,7 +192,7 @@ public class MediaDrmBridge {
    *
    * @return true if the `cbcs` encryption is supported, or false otherwise.
    */
-  @UsedByNative
+  @CalledByNative
   static boolean isCbcsSchemeSupported() {
     // While 'cbcs' scheme was originally implemented in N, there was a bug (in the
     // DRM code) which means that it didn't really work properly until N-MR1).
@@ -198,7 +200,7 @@ public class MediaDrmBridge {
   }
 
   /** Destroy the MediaDrmBridge object. */
-  @UsedByNative
+  @CalledByNative
   void destroy() {
     mNativeMediaDrmBridge = INVALID_NATIVE_MEDIA_DRM_BRIDGE;
     if (mMediaDrm != null) {
@@ -206,7 +208,7 @@ public class MediaDrmBridge {
     }
   }
 
-  @UsedByNative
+  @CalledByNative
   void createSession(int ticket, byte[] initData, String mime) {
     Log.d(TAG, "createSession()");
 
@@ -256,7 +258,7 @@ public class MediaDrmBridge {
    * @param sessionId Reference ID of session to be updated.
    * @param response Response data from the server.
    */
-  @UsedByNative
+  @CalledByNative
   UpdateSessionResult updateSession(int ticket, byte[] sessionId, byte[] response) {
     Log.d(TAG, "updateSession()");
     if (mMediaDrm == null) {
@@ -305,7 +307,7 @@ public class MediaDrmBridge {
    *
    * @param sessionId ID of session to be closed.
    */
-  @UsedByNative
+  @CalledByNative
   void closeSession(byte[] sessionId) {
     Log.d(TAG, "closeSession()");
     if (mMediaDrm == null) {
@@ -332,7 +334,7 @@ public class MediaDrmBridge {
     Log.d(TAG, "Session closed: sessionId=" + bytesToString(sessionId));
   }
 
-  @UsedByNative
+  @CalledByNative
   byte[] getMetricsInBase64() {
     if (Build.VERSION.SDK_INT < 28) {
       return null;
@@ -347,7 +349,7 @@ public class MediaDrmBridge {
     return Base64.encode(metrics, Base64.NO_PADDING | Base64.NO_WRAP | Base64.URL_SAFE);
   }
 
-  @UsedByNative
+  @CalledByNative
   MediaCrypto getMediaCrypto() {
     return mMediaCrypto;
   }
@@ -434,7 +436,7 @@ public class MediaDrmBridge {
               byte[] sessionId,
               List<MediaDrm.KeyStatus> keyInformation,
               boolean hasNewUsableKey) {
-            nativeOnKeyStatusChange(
+            MediaDrmBridgeJni.get().onKeyStatusChange(
                 mNativeMediaDrmBridge,
                 sessionId,
                 keyInformation.toArray(new MediaDrm.KeyStatus[keyInformation.size()]));
@@ -479,7 +481,7 @@ public class MediaDrmBridge {
 
     int requestType = request.getRequestType();
 
-    nativeOnSessionMessage(
+    MediaDrmBridgeJni.get().onSessionMessage(
         mNativeMediaDrmBridge, ticket, sessionId, requestType, request.getData());
   }
 
@@ -594,7 +596,7 @@ public class MediaDrmBridge {
     }
   }
 
-  @UsedByNative
+  @CalledByNative
   boolean createMediaCryptoSession() {
     if (mMediaCryptoSession != null) {
       return true;
@@ -770,9 +772,18 @@ public class MediaDrmBridge {
     return mNativeMediaDrmBridge != INVALID_NATIVE_MEDIA_DRM_BRIDGE;
   }
 
-  private native void nativeOnSessionMessage(
-      long nativeMediaDrmBridge, int ticket, byte[] sessionId, int requestType, byte[] message);
+  @NativeMethods
+  interface Natives {
+    void onSessionMessage(
+        long nativeMediaDrmBridge,
+        int ticket,
+        byte[] sessionId,
+        int requestType,
+        byte[] message);
 
-  private native void nativeOnKeyStatusChange(
-      long nativeMediaDrmBridge, byte[] sessionId, MediaDrm.KeyStatus[] keyInformation);
+    void onKeyStatusChange(
+        long nativeMediaDrmBridge,
+        byte[] sessionId,
+        MediaDrm.KeyStatus[] keyInformation);
+  }
 }

starboard/android/shared/BUILD.gn
@@ -216,6 +216,8 @@ static_library("starboard_platform") {
     "media_common.h",
     "media_decoder.cc",
     "media_decoder.h",
+    "media_drm_bridge.cc",
+    "media_drm_bridge.h",
     "media_get_audio_configuration.cc",
     "media_get_audio_output_count.cc",
     "media_get_initial_buffer_capacity.cc",

commit 0bd8ac940fd25197e1a76cc57fc45d7b53ff31f3

Call CallDrmSessionKeyStatusesChangedCallback on MediaDrmBridge::OnKeyStatusChange (#5992)
- This is to fix the regression in #5833, which skipped copying the code
to call `CallDrmSessionKeyStatusesChangedCallback`.
  - [original code](https://screenshot.googleplex.com/49oHAeeQHbrkVQt)

Bug: 417241844

starboard/android/shared/media_drm_bridge.cc
@@ -264,6 +264,10 @@ void MediaDrmBridge::OnKeyStatusChange(
       drm_key_statuses[i] = kSbDrmKeyStatusError;
     }
   }
+
+  host_->CallDrmSessionKeyStatusesChangedCallback(
+      session_id_elements, session_id_size, drm_key_ids, drm_key_statuses);
+  env->ReleaseByteArrayElements(sessionId, session_id_elements, JNI_ABORT);
 }
 
 // static