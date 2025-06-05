6a1082e336c885a3141bd671d03c08d93c1ef251
John Ocampo <johneocampo@google.com>
2025-06-05 15:57:19 +0000

    third_party/blink: Fix include path and build blink_unittests for linux (#5747)
    When b/326658232 was backported from Chromium to Cobalt, the added
    include in
    third_party/blink/renderer/modules/mediatream/webmediaplayer_ms_test.cc
    was a file that got renamed to webmediaplayer_ms.h in Chrobalt. This
    error currently makes it not possible to build 'blink_unittests', but
    was never caught due to 'blink_unittests' not building in CI. This
    change in include now makes it possible to fully build
    'blink_unittests'.
    
    This change also adds the ability to run blink_unittests to GitHub. This
    enables us to run blink_unittests and verify that they pass in our CI.
    **These tests are only ran on linux currently**.
    
    There are some issues getting the test suite to build on
    linux-evergreen, and after some discussion offline, we have decided to
    hold off on building blink_unittests for linux-evergreen until building
    tests for linux-evergreen is fully implemented.
    
    For android-arm and arm64, the test suite currently takes too long to
    fully run, which causes timeout errors for the on_device tests. After
    some discussion offline, we have decided to disable the tests for now
    until sharding for android-arm tests is implemented.
    
    Issue: 411506193
    Issue: 417541270
    
    original-hexsha: 53f563923b73e0ed455b66fa5a945b8be1cb7af3

--- third_party/blink/renderer/modules/mediastream/web_media_player_ms_test.cc
+++ third_party/blink/renderer/modules/mediastream/web_media_player_ms_test.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/blink/public/web/modules/mediastream/web_media_player_ms.h"
+#include "third_party/blink/public/web/modules/mediastream/webmediaplayer_ms.h"
 
 #include <stddef.h>
 
