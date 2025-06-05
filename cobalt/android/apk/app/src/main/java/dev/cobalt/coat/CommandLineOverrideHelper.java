1cd012f8717bbcdb7dd58eb892985f8beabebcad
Adam Walls <avvall@google.com>
2025-06-05 20:41:13 +0000

    Refactor to Split Parsing for Feature flags and CLI Switches (#5937)
    This modifies the way Cobalt accepts command line args. Prior to this,
    it would allow for multiple entries for any of those fields and was
    impossible to set multiple `enable-features`, among others.
    
    Since Cobalt has several different feature flags and command line
    switches it hard-codes into the APK, and also accepts others from the
    command line, this makes it so the CobaltActivity accumulates
    `enable-features`, `disable-features`, `blink-enable-features`, and
    `js-flags` separately using different command line fields.
    
    This refactors the commandline overrides into a separate utility class,
    making it more modular and testable. The junit tests for this are also
    added.
    
    Follow-ups include adding more tests to the class and modifying the
    android monitoring script under //cobalt//tools/performance to support
    the expected format of the various fields.
    
    Test: `out/android-arm_devel/bin/run_cobalt_coat_junit_tests`
    b/420947072
    
    original-hexsha: 7f639fec443c3ea8671629c8e65466c65a1a4943

---
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CommandLineOverrideHelper.java
@@ -0,0 +1,202 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dev.cobalt.coat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+import org.chromium.base.CommandLine;
+
+/** Helper class to provide commandLine Overrides. */
+public final class CommandLineOverrideHelper {
+    private CommandLineOverrideHelper() {} // Prevent instantiation.
+
+    /** Param class to simplify #getFlagOverrides method signature */
+    public static class CommandLineOverrideHelperParams {
+        public CommandLineOverrideHelperParams(
+            boolean shouldSetJNIPrefix,
+            boolean isOfficialBuild,
+            String[] commandLineOverrides,
+            String[] jsFlagCommandLineOverrides,
+            String[] enableFeaturesCommandLineOverrides,
+            String[] disableFeaturesCommandLineOverrides,
+            String[] blinkEnableFeaturesCommandLineOverrides) {
+            mShouldSetJNIPrefix = shouldSetJNIPrefix;
+            mIsOfficialBuild = isOfficialBuild;
+            mCommandLineOverrides = commandLineOverrides;
+            mJsFlagCommandLineOverrides = jsFlagCommandLineOverrides;
+            mEnableFeaturesCommandLineOverrides = enableFeaturesCommandLineOverrides;
+            mDisableFeaturesCommandLineOverrides = disableFeaturesCommandLineOverrides;
+            mBlinkEnableFeaturesCommandLineOverrides = blinkEnableFeaturesCommandLineOverrides;
+        }
+
+        private boolean mShouldSetJNIPrefix;
+        private boolean mIsOfficialBuild;
+        private String[] mCommandLineOverrides;
+        private String[] mJsFlagCommandLineOverrides;
+        private String[] mEnableFeaturesCommandLineOverrides;
+        private String[] mDisableFeaturesCommandLineOverrides;
+        private String[] mBlinkEnableFeaturesCommandLineOverrides;
+    }
+
+    // This can be returned as a list, since it does not need to be a single
+    // string object. The others can be combined into a single String because
+    // they need to be enclosed in the feature's enable/disable header.
+    public static List<String> getDefaultCommandLineOverridesList() {
+        List<String> paramOverrides = new ArrayList<>();
+
+        // Disable first run experience.
+        paramOverrides.add("--disable-fre");
+        paramOverrides.add("--no-first-run");
+        // Run Cobalt as a single process.
+        paramOverrides.add("--single-process");
+        // Enable Blink to work in overlay video mode.
+        paramOverrides.add("--force-video-overlays");
+        // Autoplay video with url.
+        paramOverrides.add("--autoplay-policy=no-user-gesture-required");
+        // Remove below if Cobalt rebase to m120+.
+        paramOverrides.add("--user-level-memory-pressure-signal-params");
+        // Disable rescaling Webpage.
+        paramOverrides.add("--force-device-scale-factor=1");
+        // Enable low end device mode.
+        paramOverrides.add("--enable-low-end-device-mode");
+        // Disables RGBA_4444 textures which
+        // causes rendering artifacts when
+        // low-end-device-mode is enabled.
+        paramOverrides.add("--disable-rgba-4444-textures");
+        // Use passthrough command decoder.
+        paramOverrides.add("--use-cmd-decoder=passthrough");
+
+        return paramOverrides;
+    }
+
+    public static StringJoiner getDefaultJsFlagOverridesList() {
+        StringJoiner paramOverrides = new StringJoiner(",");
+
+        // Trades a little V8 performance for significant memory savings.
+        paramOverrides.add("--optimize_for_size=true");
+        // Disable concurrent-marking due to b/415843979
+        paramOverrides.add("--concurrent_marking=false");
+
+        return paramOverrides;
+    }
+
+    public static StringJoiner getDefaultEnableFeatureOverridesList() {
+        StringJoiner paramOverrides = new StringJoiner(",");
+
+        // Pass javascript console log to adb log.
+        paramOverrides.add("LogJsConsoleMessages");
+        // Limit decoded image cache to 32 mbytes.
+        paramOverrides.add("LimitImageDecodeCacheSize:mb/32");
+
+        return paramOverrides;
+    }
+
+    public static StringJoiner getDefaultDisableFeatureOverridesList() {
+        StringJoiner paramOverrides = new StringJoiner(",");
+
+        // Use SurfaceTexture for decode-to-texture mode.
+        paramOverrides.add("AImageReader");
+
+        return paramOverrides;
+    }
+
+    public static StringJoiner getDefaultBlinkEnableFeatureOverridesList() {
+        StringJoiner paramOverrides = new StringJoiner(",");
+
+        // Align with MSE spec for MediaSource.duration.
+        paramOverrides.add("MediaSourceNewAbortAndDuration");
+
+        return paramOverrides;
+    }
+
+    public static void getFlagOverrides(
+        CommandLineOverrideHelperParams params) {
+        List<String> cliOverrides =
+            getDefaultCommandLineOverridesList();
+        StringJoiner jsFlagOverrides =
+            getDefaultJsFlagOverridesList();
+        StringJoiner enableFeatureOverrides =
+            getDefaultEnableFeatureOverridesList();
+        StringJoiner disableFeatureOverrides =
+            getDefaultDisableFeatureOverridesList();
+        StringJoiner blinkEnableFeatureOverrides =
+            getDefaultBlinkEnableFeatureOverridesList();
+
+        if (params != null) {
+            if (params.mShouldSetJNIPrefix) {
+                // Helps Kimono build avoid package name conflict with cronet.
+                cliOverrides.add("--cobalt-jni-prefix");
+            }
+            if (!params.mIsOfficialBuild) {
+                cliOverrides.add(
+                  "--remote-allow-origins="
+                  + "https://chrome-devtools-frontend.appspot.com");
+            }
+
+            if (params.mCommandLineOverrides != null) {
+                for (String param: params.mCommandLineOverrides) {
+                    cliOverrides.add(param);
+                }
+            }
+            if (params.mJsFlagCommandLineOverrides != null) {
+                for (String param: params.mJsFlagCommandLineOverrides) {
+                    if (param == null || param.isEmpty()) {
+                        continue; // Skip null or empty params
+                    }
+                    jsFlagOverrides.add(param);
+                }
+            }
+            if (params.mEnableFeaturesCommandLineOverrides != null) {
+                for (String param: params.mEnableFeaturesCommandLineOverrides) {
+                    if (param == null || param.isEmpty()) {
+                        continue; // Skip null or empty params
+                    }
+                    enableFeatureOverrides.add(param);
+                }
+            }
+
+            if (params.mDisableFeaturesCommandLineOverrides != null) {
+                for (String param: params.mDisableFeaturesCommandLineOverrides) {
+                    if (param == null || param.isEmpty()) {
+                        continue; // Skip null or empty params
+                    }
+                    disableFeatureOverrides.add(param);
+                }
+            }
+            if (params.mBlinkEnableFeaturesCommandLineOverrides != null) {
+                for (String param: params.mBlinkEnableFeaturesCommandLineOverrides) {
+                    if (param == null || param.isEmpty()) {
+                        continue; // Skip null or empty params
+                    }
+                    blinkEnableFeatureOverrides.add(param);
+                }
+            }
+        }
+        CommandLine.getInstance().appendSwitchesAndArguments(
+            cliOverrides.toArray(new String[0]));
+        CommandLine.getInstance().appendSwitchesAndArguments(
+            new String[]{"--js-flags=" + jsFlagOverrides.toString() });
+        CommandLine.getInstance().appendSwitchesAndArguments(
+            new String[]{"--enable-features="
+            + enableFeatureOverrides.toString() });
+        CommandLine.getInstance().appendSwitchesAndArguments(
+            new String[]{"--disable-features="
+            + disableFeatureOverrides.toString() });
+        CommandLine.getInstance().appendSwitchesAndArguments(
+            new String[]{"--blink-enable-features="
+            + blinkEnableFeatureOverrides.toString() });
+    }
+}
