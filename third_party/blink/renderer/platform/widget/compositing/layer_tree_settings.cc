fc2e85eebdfd0bceb66a6439a3d9dc28cbdfc42c
Miguel Casas <mcasas@chromium.org>
2025-06-06 12:49:34 -0700

    BACKPORT: blink: Return CUTOFF_ALLOW_NICE_TO_HAVE for all GpuMemoryPolicy paths (#6024)
    Reviewed and landed in https://crrev.com/c/6614297.
    
    Original CL description:
    
    On ToT all return paths from GetGpuMemoryPolicy() return a cutoff policy
    of CUTOFF_ALLOW_NICE_TO_HAVE -- except when the command line switch
    ForceGpuMemAvailableMb is used. This contravenes the unit tests in [1]
    (there's just two test cases).
    
    This CL fixes that by moving the policy setting to further up. It also
    adds a curly bracket for a multi-line if condition.
    
    No functional changes intended.
    
    [1]
    https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/widget/compositing/layer_tree_settings_unittest.cc;drc=42b97eaa9deace1c188434ccac37ee22223ade4f
    GetGpuMemoryPolicy(
    
    Bug: 420684984
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>
    Reviewed-by: Stephen White <senorblanco@chromium.org>
    
    original-hexsha: e14ff26cbe7397f233e85fd77551c53ea977b1bf

--- third_party/blink/renderer/platform/widget/compositing/layer_tree_settings.cc
+++ third_party/blink/renderer/platform/widget/compositing/layer_tree_settings.cc
@@ -163,6 +163,8 @@ cc::ManagedMemoryPolicy GetGpuMemoryPolicy(
     float initial_device_scale_factor) {
   cc::ManagedMemoryPolicy actual = default_policy;
   actual.bytes_limit_when_visible = 0;
+  actual.priority_cutoff_when_visible =
+      gpu::MemoryAllocation::CUTOFF_ALLOW_NICE_TO_HAVE;
 
   // If the value was overridden on the command line, use the specified value.
   static bool client_hard_limit_bytes_overridden =
@@ -172,8 +174,9 @@ cc::ManagedMemoryPolicy GetGpuMemoryPolicy(
     if (base::StringToSizeT(
             base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
                 ::switches::kForceGpuMemAvailableMb),
-            &actual.bytes_limit_when_visible))
+            &actual.bytes_limit_when_visible)) {
       actual.bytes_limit_when_visible *= 1024 * 1024;
+    }
     return actual;
   }
 
@@ -217,9 +220,6 @@ cc::ManagedMemoryPolicy GetGpuMemoryPolicy(
 
   actual.bytes_limit_when_visible = mb_limit_when_visible * 1024 * 1024;
 #endif
-  actual.priority_cutoff_when_visible =
-      gpu::MemoryAllocation::CUTOFF_ALLOW_NICE_TO_HAVE;
-
   return actual;
 }
 
