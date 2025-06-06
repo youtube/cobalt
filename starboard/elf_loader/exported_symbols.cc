3298f5cf64c66423e95e7300d597d118169cc986
Madhura Jayaraman <madjay@google.com>
2025-06-06 10:21:44 -0700

    Add aligned_alloc to starboard (#5896)
    Bug: 406081663
    
    original-hexsha: 55e7b6b26c9b2fe5df28c4dcc776be1e34896b06

--- starboard/elf_loader/exported_symbols.cc
+++ starboard/elf_loader/exported_symbols.cc
@@ -220,6 +220,7 @@ ExportedSymbols::ExportedSymbols() {
   REGISTER_SYMBOL(SbWindowSetDefaultOptions);
 
   // POSIX APIs
+  REGISTER_SYMBOL(aligned_alloc);
   REGISTER_SYMBOL(calloc);
   REGISTER_SYMBOL(close);
   REGISTER_SYMBOL(dup);
