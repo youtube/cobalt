9029acb6fcde54a5123ea2fa05f1a21f5fd8513d
Yavor Goulishev <yavor@google.com>
2025-06-11 16:45:12 -0700

    Remove program_invocation_short_name (#6065)
    Don't call SetProcessTitleFromCommandLine which is optional API and is
    only used for adjusting the title of the process after the exec call
    which we don't support in Cobalt.
    
    Issue: 423999465
    
    original-hexsha: 12646df90bb206868d1458425a683e950f503e86

--- base/process/set_process_title.cc
+++ base/process/set_process_title.cc
@@ -44,7 +44,8 @@ namespace base {
 
 // TODO(jrg): Find out if setproctitle or equivalent is available on Android.
 #if BUILDFLAG(IS_POSIX) && !BUILDFLAG(IS_APPLE) && !BUILDFLAG(IS_SOLARIS) && \
-    !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_FUCHSIA) && !BUILDFLAG(IS_NACL)
+    !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_FUCHSIA) && !BUILDFLAG(IS_NACL) && \
+    !BUILDFLAG(IS_STARBOARD)
 
 void SetProcessTitleFromCommandLine(const char** main_argv) {
   // Build a single string which consists of all the arguments separated
