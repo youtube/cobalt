4226440edc2da400b9d6829fbd72389060a53173
Adam Walls <avvall@google.com>
2025-06-12 17:43:15 +0000

    Remove dependency on //components/metrics:test_support
    Fixed: 424201886

--- cobalt/browser/cobalt_content_browser_client.cc
+++ cobalt/browser/cobalt_content_browser_client.cc
@@ -36,7 +36,6 @@
 #include "cobalt/shell/browser/shell.h"
 #include "cobalt/shell/browser/shell_paths.h"
 #include "components/metrics/metrics_state_manager.h"
-#include "components/metrics/test/test_enabled_state_provider.h"
 #include "components/metrics_services_manager/metrics_services_manager.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
@@ -399,8 +398,6 @@ void CobaltContentBrowserClient::SetUpCobaltFeaturesAndParams(
 }
 
 void CobaltContentBrowserClient::CreateFeatureListAndFieldTrials() {
-  metrics::TestEnabledStateProvider enabled_state_provider(/*consent=*/false,
-                                                           /*enabled=*/false);
   GlobalFeatures::GetInstance()
       ->metrics_services_manager()
       ->InstantiateFieldTrialList();
