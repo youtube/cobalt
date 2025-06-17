526a53d0bc25c5c1b1a41de4f7cab5572acea353
Miguel Casas <mcasas@chromium.org>
2025-06-17 10:01:24 -0700

    cobalt: merge common/shell_switches into cobalt/browser/switches (#6098)
    The removed files are a) not part of any BUILD.gn and b) not
    necessary since browser-level flags (i.e. Cobalt's) are defined
    in cobalt/browser/switches.h. This CL moves the only flag in
    the removed files.
    
    No new functionality intended.
    
    Bug: 420684984
    
    original-hexsha: 3f9826fefc847782d7d3da6710a79adc41d99735

--- cobalt/browser/switches.h
+++ cobalt/browser/switches.h
@@ -34,6 +34,10 @@ constexpr char kRequireCSP[] = "require_csp";
 // Specify the initial window size: --window-size=w,h
 constexpr char kWindowSize[] = "window-size";
 
+// Exposes the window.internals object to JavaScript for interactive development
+// and debugging of web tests that rely on it.
+constexpr char kExposeInternalsForTesting[] = "expose-internals-for-testing";
+
 }  // namespace switches
 }  // namespace cobalt
 
