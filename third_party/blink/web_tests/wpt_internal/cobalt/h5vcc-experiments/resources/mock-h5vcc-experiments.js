d1b39e5f919acff5278257b03a0746c9c0e5ce78
Yijia Zhang <yijiazhang@google.com>
2025-06-17 13:30:43 -0700

    Reject pending promises in H5vccExperiments when mojo disconnect (#5864)
    Bug: 407734389
    
    Add error handling to h5vcc experiments in the case where there's a mojo
    disconnection and web tests that cover this scenario. Also fix web tests
    for async functions in h5vcc experiments.
    
    original-hexsha: 62f96da2c2889b9e2d0b75646e8c79a4d76d4431

--- third_party/blink/web_tests/wpt_internal/cobalt/h5vcc-experiments/resources/mock-h5vcc-experiments.js
+++ third_party/blink/web_tests/wpt_internal/cobalt/h5vcc-experiments/resources/mock-h5vcc-experiments.js
@@ -7,10 +7,13 @@ import {
 class MockH5vccExperiments {
   constructor() {
     this.interceptor_ =
-        new MojoInterfaceInterceptor(H5vccExperiments.$interfaceName);
+      new MojoInterfaceInterceptor(H5vccExperiments.$interfaceName);
     this.interceptor_.oninterfacerequest = e => this.bind(e.handle);
     this.receiver_ = new H5vccExperimentsReceiver(this);
     this.stub_result_ = new Map();
+    this.experiment_ids_ = [];
+    this.called_reset_experiment_state_ = false;
+    this.called_set_experiment_state_ = false;
   }
 
   start() {
@@ -22,7 +25,11 @@ class MockH5vccExperiments {
   }
 
   reset() {
+    this.called_reset_experiment_state_ = false;
+    this.called_set_experiment_state_ = false;
     this.stub_result_ = new Map();
+    this.experiment_ids_ = [];
+    this.receiver_.$.close();
   }
 
   bind(handle) {
@@ -34,28 +41,86 @@ class MockH5vccExperiments {
     this.stub_result_.set(key, value);
   }
 
-  stubGetFeature(feature_name, stub_result) {
-    this.stubResult(feature_name, stub_result);
+  getActiveExperimentIds() {
+    throw new Error('Sync methods not supported in MojoJS (b/406809316');
   }
 
-  stubGetFeatureParam(feature_param_name, stub_result) {
-    this.stubResult(feature_param_name, stub_result);
+  // Added for verifying setExperimentState result in tests.
+  getExperimentIds() {
+    return this.experiment_ids_;
   }
 
   getFeature(feature_name) {
-    return this.stub_result_.get(feature_name);
+    throw new Error('Sync methods not supported in MojoJS (b/406809316');
   }
 
   getFeatureParam(feature_param_name) {
-    return this.stub_result_.get(feature_param_name);
+    throw new Error('Sync methods not supported in MojoJS (b/406809316');
   }
 
-  resetExperimentState() {
-    return Promise.resolve();
+  getFeatureState(feature_name) {
+    return this.stub_result_.get(feature_name);
   }
 
-  setExperimentState() {
-    return Promise.resolve();
+  hasCalledResetExperimentState() {
+    return this.called_reset_experiment_state_;
+  }
+
+  hasCalledSetExperimentState() {
+    return this.called_set_experiment_state_;
+  }
+
+  async resetExperimentState() {
+    this.called_reset_experiment_state_ = true;
+    return;
+  }
+
+  async setExperimentState(experiment_config) {
+    const root_storage = experiment_config.storage;
+    if (!root_storage) {
+      return;
+    }
+
+    // Process Features
+    const features_container = root_storage['experiment_config.features']?.dictionaryValue?.storage;
+    if (features_container) {
+      // The keys of this object are the feature names (e.g., "fake_feature").
+      for (const feature_name of Object.keys(features_container)) {
+        const value = features_container[feature_name]?.boolValue;
+        this.stubResult(feature_name, value);
+      }
+    }
+
+    // Process Feature Params
+    const params_container = root_storage['experiment_config.feature_params']?.dictionaryValue?.storage;
+    if (params_container) {
+      // The keys are the param names (e.g., "fake_feature_param").
+      for (const param_name of Object.keys(params_container)) {
+        const param_object = params_container[param_name];
+        // Feature param could be any of the following types.
+        const value = param_object?.stringValue ?? param_object?.boolValue ?? param_object?.intValue;
+        this.stubResult(param_name, value);
+      }
+    }
+
+    // Process Experiment IDs
+    const ids_container = root_storage['experiment_config.exp_ids']?.listValue?.storage;
+    if (ids_container) {
+      const id_list = [];
+      for (const id_key of Object.keys(ids_container)) {
+        const id_object = ids_container[id_key];
+        // Mojom often stores numbers as strings.
+        const value = id_object?.stringValue ?? id_object?.intValue;
+        if (value !== undefined) {
+          // Convert to number for consistency with the test.
+          id_list.push(Number(value));
+        }
+      }
+      this.experiment_ids_ = id_list;
+    }
+
+    this.called_set_experiment_state_ = true;
+    return;
   }
 }
 
