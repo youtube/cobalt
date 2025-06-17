4ff2db34a87270e7110a57f7224f079e30e13c2a
Yijia Zhang <yijiazhang@google.com>
2025-06-17 13:30:43 -0700

    Reject pending promises in H5vccExperiments when mojo disconnect (#5864)
    Bug: 407734389
    
    Add error handling to h5vcc experiments in the case where there's a mojo
    disconnection and web tests that cover this scenario. Also fix web tests
    for async functions in h5vcc experiments.
    
    original-hexsha: 62f96da2c2889b9e2d0b75646e8c79a4d76d4431

--- third_party/blink/web_tests/wpt_internal/cobalt/h5vcc-experiments/h5vcc-experiments-resetExperimentState.https.any.js
+++ third_party/blink/web_tests/wpt_internal/cobalt/h5vcc-experiments/h5vcc-experiments-resetExperimentState.https.any.js
@@ -2,11 +2,17 @@
 // META: script=/resources/test-only-api.js
 // META: script=resources/automation.js
 h5vcc_experiments_tests(async (t, mockH5vccExperiments) => {
-  let resetExperimentStateCalled = false;
-  try {
-    await window.h5vcc.experiments.resetExperimentState();
-    resetExperimentStateCalled = true;
-  } finally {
-    assert_true(resetExperimentStateCalled);
-  }
+  await window.h5vcc.experiments.resetExperimentState();
+  assert_true(mockH5vccExperiments.hasCalledResetExperimentState());
 }, 'exercises H5vccExperiments.resetExperimentState()');
+
+promise_test(async (t) => {
+  const { H5vccExperiments } = await import(
+    '/gen/cobalt/browser/h5vcc_experiments/public/mojom/h5vcc_experiments.mojom.m.js');
+  let interceptor =
+    new MojoInterfaceInterceptor(H5vccExperiments.$interfaceName);
+  interceptor.oninterfacerequest = e => e.handle.close();
+  interceptor.start();
+  return promise_rejects_exactly(
+    t, 'Mojo connection error.', window.h5vcc.experiments.resetExperimentState());
+}, 'resetExperimentState() rejects when unimplemented due to pipe closure');
--- third_party/blink/web_tests/wpt_internal/cobalt/h5vcc-experiments/h5vcc-experiments-setExperimentState.https.any.js
+++ third_party/blink/web_tests/wpt_internal/cobalt/h5vcc-experiments/h5vcc-experiments-setExperimentState.https.any.js
@@ -1,12 +1,40 @@
 // META: global=window
 // META: script=/resources/test-only-api.js
 // META: script=resources/automation.js
+const test_experiment_config = {
+  "features": {
+    "fake_feature": true
+  },
+  "featureParams": {
+    "fake_feature_param": 1,
+  },
+  "experimentIds": [123, 456]
+}
+
+// TODO(b/416325838) - Investigate issues with multiple test cases.
 h5vcc_experiments_tests(async (t, mockH5vccExperiments) => {
-  let resetExperimentStateCalled = false;
-  try {
-    await window.h5vcc.experiments.setExperimentState();
-    resetExperimentStateCalled = true;
-  } finally {
-    assert_true(resetExperimentStateCalled);
+  await window.h5vcc.experiments.setExperimentState(test_experiment_config);
+  assert_true(mockH5vccExperiments.hasCalledSetExperimentState());
+
+  // Verify experiment Ids.
+  let test_ids = test_experiment_config['experimentIds'];
+  for (let i = 0; i < test_ids.length; i++) {
+    assert_equals(mockH5vccExperiments.getExperimentIds()[i], test_ids[i]);
   }
-}, 'exercises H5vccExperiments.setExperimentState()');
+
+  assert_true(mockH5vccExperiments.getFeatureState('fake_feature'));
+  // All feature params are stored as strings.
+  assert_equals(mockH5vccExperiments.getFeatureState('fake_feature_param'), "1");
+}, 'setExperimentState() sends expected config content to browser endpoint');
+
+
+promise_test(async (t) => {
+  const { H5vccExperiments } = await import(
+    '/gen/cobalt/browser/h5vcc_experiments/public/mojom/h5vcc_experiments.mojom.m.js');
+  let interceptor =
+    new MojoInterfaceInterceptor(H5vccExperiments.$interfaceName);
+  interceptor.oninterfacerequest = e => e.handle.close();
+  interceptor.start();
+  return promise_rejects_exactly(t, 'Mojo connection error.',
+    window.h5vcc.experiments.setExperimentState(test_experiment_config));
+}, 'setExperimentState() rejects when unimplemented due to pipe closure');
