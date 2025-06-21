dfbd12e55aaae5c916f94af99772f1c51bd8b75c
Austin Osagie <austinosagie@google.com>
2025-06-20 21:09:32 -0700

    Force mediarouter version

--- third_party/android_deps/buildSrc/src/main/groovy/ChromiumPlugin.groovy
+++ third_party/android_deps/buildSrc/src/main/groovy/ChromiumPlugin.groovy
@@ -43,6 +43,8 @@ class ChromiumPlugin implements Plugin<Project> {
             resolutionStrategy.eachDependency { DependencyResolveDetails details ->
                 if (project.ext.has('versionOverrideMap') && project.ext.versionOverrideMap) {
                     String module = "${details.requested.group}:${details.requested.name}"
+                    println 'austin'
+                    println module
                     String version = project.ext.versionOverrideMap[module]
                     if (version != null) {
                         details.useVersion version
