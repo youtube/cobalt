0a6c4abdb6a3c61f067229d631c768e546bcf6cb
Andrew Savage <andrewsavage@google.com>
2025-06-21 16:17:44 -0400

    Build almost all linux targets hermetically (#6097)
    This only differs from non-hermetic builds in one target,
    service_manager_unittests. These try to launch other processes and don't
    make sense for hermetic builds as they currently are.
    
    Bug: 416307148
    Bug: 425715098
    
    original-hexsha: a71fbc37a4e7344a70f88d257b6700ceaad98ca0

--- gpu/ipc/service/gpu_channel_manager_unittest.cc
+++ gpu/ipc/service/gpu_channel_manager_unittest.cc
@@ -149,6 +149,7 @@ TEST_F(GpuChannelManagerTest, OnBackgroundedWithWebGL) {
 // Tests that peak memory usage is only reported for valid sequence numbers,
 // and that polling shuts down the monitoring.
 TEST_F(GpuChannelManagerTest, GpuPeakMemoryOnlyReportedForValidSequence) {
+#if !BUILDFLAG(ENABLE_COBALT_HERMETIC_HACKS)
   base::test::TestTraceProcessor ttp;
   ttp.StartTrace("gpu");
 
@@ -173,6 +174,7 @@ TEST_F(GpuChannelManagerTest, GpuPeakMemoryOnlyReportedForValidSequence) {
   EXPECT_EQ(0u, GetMonitorsPeakMemoryUsage(sequence_num));
   EXPECT_EQ(0u, GetManagersPeakMemoryUsage(sequence_num));
 
+#if !BUILDFLAG(ENABLE_COBALT_HERMETIC_HACKS)
   absl::Status status = ttp.StopAndParseTrace();
   ASSERT_TRUE(status.ok()) << status.message();
   std::string query =
