be88c16f4edef3342263ed2ae3123f9eeb694559
Andrew Savage <andrewsavage@google.com>
2025-06-23 22:10:35 +0000

    Switch to std::atomic

--- starboard/elf_loader/evergreen_info.cc
+++ starboard/elf_loader/evergreen_info.cc
@@ -15,26 +15,23 @@
 #include "starboard/elf_loader/evergreen_info.h"
 
 #include <string.h>
-
-#include "starboard/common/log.h"
+#include <atomic>
 
 static EvergreenInfo g_evergreen_info;
 static bool g_valid_info = false;
-static int32_t g_busy = 0;
+static std::atomic<int32_t> g_busy(0);
 
 bool SetEvergreenInfo(const EvergreenInfo* evergreen_info) {
-  SB_LOG(INFO) << "SetEvergreenInfo@";
   // Set the busy flag or bail.
-  int32_t busy_state_flag1 = 1;
-  int32_t ready_state_flag1 = 0;
-  __atomic_compare_exchange_n(&g_busy, &ready_state_flag1, busy_state_flag1,
-                              false, __ATOMIC_RELAXED, __ATOMIC_RELAXED);
-  if (ready_state_flag1) {
+  int32_t expected = 0;
+  if (!g_busy.compare_exchange_strong(expected, 1, std::memory_order_relaxed,
+                                      std::memory_order_relaxed)) {
     // Bailing out is OK as the process crashed
     // before we launched the application and in that
     // case the evergreen information is not needed.
     return false;
   }
+
   if (evergreen_info != NULL && evergreen_info->base_address != 0 &&
       strlen(evergreen_info->file_path_buf) != 0) {
     g_evergreen_info = *evergreen_info;
@@ -43,13 +40,12 @@ bool SetEvergreenInfo(const EvergreenInfo* evergreen_info) {
     g_valid_info = false;
   }
   // Publish local memory changes to all threads.
-  __atomic_thread_fence(__ATOMIC_SEQ_CST);
+  std::atomic_thread_fence(std::memory_order_seq_cst);
 
   // Clear the busy flag.
-  int32_t busy_state_flag2 = 1;
-  int32_t ready_state_flag2 = 0;
-  __atomic_compare_exchange_n(&g_busy, &busy_state_flag2, ready_state_flag2,
-                              false, __ATOMIC_RELAXED, __ATOMIC_RELAXED);
+  expected = 1;
+  g_busy.compare_exchange_strong(expected, 0, std::memory_order_relaxed,
+                                 std::memory_order_relaxed);
 
   return true;
 }
@@ -59,26 +55,26 @@ bool GetEvergreenInfo(EvergreenInfo* evergreen_info) {
     return false;
   }
 
-  int32_t busy_state_flag1 = 1;
-  int32_t ready_state_flag1 = 0;
-  __atomic_compare_exchange_n(&g_busy, &ready_state_flag1, busy_state_flag1,
-                              false, __ATOMIC_RELAXED, __ATOMIC_RELAXED);
-  if (ready_state_flag1) {
+  int32_t expected = 0;
+  if (!g_busy.compare_exchange_strong(expected, 1, std::memory_order_relaxed,
+                                      std::memory_order_relaxed)) {
     return false;
   }
 
   // Make sure all memory changes are visible to the current thread.
-  __atomic_thread_fence(__ATOMIC_SEQ_CST);
+  std::atomic_thread_fence(std::memory_order_seq_cst);
   if (!g_valid_info) {
+    expected = 1;
+    g_busy.compare_exchange_strong(expected, 0, std::memory_order_relaxed,
+                                   std::memory_order_relaxed);
     return false;
   }
 
   *evergreen_info = g_evergreen_info;
 
   // Clear the busy flag.
-  int32_t busy_state_flag2 = 1;
-  int32_t ready_state_flag2 = 0;
-  __atomic_compare_exchange_n(&g_busy, &busy_state_flag2, ready_state_flag2,
-                              false, __ATOMIC_RELAXED, __ATOMIC_RELAXED);
+  expected = 1;
+  g_busy.compare_exchange_strong(expected, 0, std::memory_order_relaxed,
+                                 std::memory_order_relaxed);
   return true;
 }
