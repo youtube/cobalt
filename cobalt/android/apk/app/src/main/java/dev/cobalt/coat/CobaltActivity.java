2c2ed3da1674c77c8923016ca0fe183188a371fd
johnx <johnx@google.com>
2025-06-24 13:21:43 -0700

    Remove Shell and ShellManager from native UI (#6074)
    Previously Shell and ShellManager classes are visible on screen and
    multiple Shells were supported. Chrobalt also used an xml layout res
    to initialize the activity layout that inflates ShellManager.
    
    This PR removes Shell and ShellManager from the Android layout tree
    and use ContentViewRenderView as the root View.
    
    Bug: 418021171
    Bug: 419887270
    
    original-hexsha: 38323d10c71209c651fd57a15828999203373352

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
@@ -154,13 +154,13 @@ public abstract class CobaltActivity extends Activity {
       getStarboardBridge().handleDeepLink(startDeepLink);
     }
 
-    setContentView(R.layout.content_shell_activity);
-    mShellManager = findViewById(R.id.shell_container);
+    mShellManager = new ShellManager(this);
     final boolean listenToActivityState = true;
     mIntentRequestTracker = IntentRequestTracker.createFromActivity(this);
     mWindowAndroid = new ActivityWindowAndroid(this, listenToActivityState, mIntentRequestTracker);
     mIntentRequestTracker.restoreInstanceState(savedInstanceState);
     mShellManager.setWindow(mWindowAndroid);
+    setContentView(mShellManager.getContentViewRenderView());
     // Set up the animation placeholder to be the SurfaceView. This disables the
     // SurfaceView's 'hole' clipping during animations that are notified to the window.
     mWindowAndroid.setAnimationPlaceholderView(
