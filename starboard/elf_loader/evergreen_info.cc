53032c48942364f2d6eb2da34de24fe7f78fb3d8
Andrew Savage <andrewsavage@google.com>
2025-06-25 11:24:09 -0400

    Partially fix stack traces for Evergreen (#6145)
    The main fix is fixing atomics for EvergreenInfo.
    
    There are still some missing frames, likely due to us missing
    _dl_find_object (see
    https://b.corp.google.com/issues/423906285#comment3).
    
    Bug: 423906285
    original-hexsha: 1176e32edccbe55e6d1e3241a66daaedfcec258b

--- starboard/elf_loader/evergreen_info.cc
+++ starboard/elf_loader/evergreen_info.cc
@@ -19,20 +19,19 @@
 
 static EvergreenInfo g_evergreen_info;
 static bool g_valid_info = false;
-static std::atomic<int32_t> g_busy{0};
+static std::atomic<int32_t> g_busy(0);
 
 bool SetEvergreenInfo(const EvergreenInfo* evergreen_info) {
   // Set the busy flag or bail.
-  int32_t busy_state_flag = 1;
-  int32_t ready_state_flag = 0;
-  if (g_busy.compare_exchange_weak(ready_state_flag, busy_state_flag,
-                                   std::memory_order_release,
-                                   std::memory_order_relaxed)) {
+  int32_t expected = 0;
+  if (!g_busy.compare_exchange_strong(expected, 1, std::memory_order_relaxed,
+                                      std::memory_order_relaxed)) {
     // Bailing out is OK as the process crashed
     // before we launched the application and in that
     // case the evergreen information is not needed.
     return false;
   }
+
   if (evergreen_info != NULL && evergreen_info->base_address != 0 &&
       strlen(evergreen_info->file_path_buf) != 0) {
     g_evergreen_info = *evergreen_info;
@@ -44,9 +43,9 @@ bool SetEvergreenInfo(const EvergreenInfo* evergreen_info) {
   std::atomic_thread_fence(std::memory_order_seq_cst);
 
   // Clear the busy flag.
-  g_busy.compare_exchange_weak(busy_state_flag, ready_state_flag,
-                               std::memory_order_release,
-                               std::memory_order_relaxed);
+  expected = 1;
+  g_busy.compare_exchange_strong(expected, 0, std::memory_order_relaxed,
+                                 std::memory_order_relaxed);
 
   return true;
 }
@@ -56,27 +55,26 @@ bool GetEvergreenInfo(EvergreenInfo* evergreen_info) {
     return false;
   }
 
-  int32_t busy_state_flag = 1;
-  int32_t ready_state_flag = 0;
-
-  // Set the busy flag or bail.
-  if (g_busy.compare_exchange_weak(ready_state_flag, busy_state_flag,
-                                   std::memory_order_release,
-                                   std::memory_order_relaxed)) {
+  int32_t expected = 0;
+  if (!g_busy.compare_exchange_strong(expected, 1, std::memory_order_relaxed,
+                                      std::memory_order_relaxed)) {
     return false;
   }
 
   // Make sure all memory changes are visible to the current thread.
   std::atomic_thread_fence(std::memory_order_seq_cst);
   if (!g_valid_info) {
+    expected = 1;
+    g_busy.compare_exchange_strong(expected, 0, std::memory_order_relaxed,
+                                   std::memory_order_relaxed);
     return false;
   }
 
   *evergreen_info = g_evergreen_info;
 
   // Clear the busy flag.
-  g_busy.compare_exchange_weak(busy_state_flag, ready_state_flag,
-                               std::memory_order_release,
-                               std::memory_order_relaxed);
+  expected = 1;
+  g_busy.compare_exchange_strong(expected, 0, std::memory_order_relaxed,
+                                 std::memory_order_relaxed);
   return true;
 }
