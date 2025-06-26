46f312b5c42abccd6c8d9f38dcb84364a9e54825
Miguel Casas <mcasas@chromium.org>
2025-06-26 17:51:39 +0000

    Revert "Make liberal use of constexpr in starboard/..../configurations_constants.cc"
    This reverts commit 505a93354536be44b90aab32e47e6601de89250b.

--- starboard/linux/shared/configuration_constants.cc
+++ starboard/linux/shared/configuration_constants.cc
@@ -18,49 +18,49 @@
 
 // The current platform's maximum length of the name of a single directory
 // entry, not including the absolute path.
-constexpr int32_t kSbFileMaxName = 64;
+const int32_t kSbFileMaxName = 64;
 
 // The current platform's maximum number of files that can be opened at the
 // same time by one process.
-constexpr uint32_t kSbFileMaxOpen = 256;
+const uint32_t kSbFileMaxOpen = 256;
 
 // The current platform's maximum length of an absolute path.
-constexpr uint32_t kSbFileMaxPath = 4096;
+const uint32_t kSbFileMaxPath = 4096;
 
 // The current platform's file path component separator character. This is the
 // character that appears after a directory in a file path. For example, the
 // absolute canonical path of the file "/path/to/a/file.txt" uses '/' as a path
 // component separator character.
-constexpr char kSbFileSepChar = '/';
+const char kSbFileSepChar = '/';
 
 // The string form of SB_FILE_SEP_CHAR.
 const char* kSbFileSepString = "/";
 
 // On default Linux desktop, you must be a superuser in order to set real time
 // scheduling on threads.
-constexpr bool kSbHasThreadPrioritySupport = false;
+const bool kSbHasThreadPrioritySupport = false;
 
 // The maximum number of thread local storage keys supported by this platform.
-constexpr uint32_t kSbMaxThreadLocalKeys = 512;
+const uint32_t kSbMaxThreadLocalKeys = 512;
 
 // The maximum length of a name for a thread, including the NULL-terminator.
-constexpr int32_t kSbMaxThreadNameLength = 16;
+const int32_t kSbMaxThreadNameLength = 16;
 
 // The maximum audio bitrate the platform can decode.  The following value
 // equals to 5M bytes per seconds which is more than enough for compressed
 // audio.
-constexpr uint32_t kSbMediaMaxAudioBitrateInBitsPerSecond = 40 * 1024 * 1024;
+const uint32_t kSbMediaMaxAudioBitrateInBitsPerSecond = 40 * 1024 * 1024;
 
 // The maximum video bitrate the platform can decode.  The following value
 // equals to 25M bytes per seconds which is more than enough for compressed
 // video.
-constexpr uint32_t kSbMediaMaxVideoBitrateInBitsPerSecond = 200 * 1024 * 1024;
+const uint32_t kSbMediaMaxVideoBitrateInBitsPerSecond = 200 * 1024 * 1024;
 
 // The memory page size, which controls the size of chunks on memory that
 // allocators deal with, and the alignment of those chunks. This doesn't have to
 // be the hardware-defined physical page size, but it should be a multiple of
 // it.
-constexpr size_t kSbMemoryPageSize = 4096;
+const size_t kSbMemoryPageSize = 4096;
 
 // Specifies the network receive buffer size in bytes, set via
 // SbSocketSetReceiveBufferSize().
@@ -74,25 +74,25 @@ constexpr size_t kSbMemoryPageSize = 4096;
 //
 // If your platform does not have a good TCP auto-tuning mechanism,
 // a setting of (128 * 1024) here is recommended.
-constexpr uint32_t kSbNetworkReceiveBufferSize = 0;
+const uint32_t kSbNetworkReceiveBufferSize = 0;
 
 // Defines the maximum number of simultaneous threads for this platform. Some
 // platforms require sharing thread handles with other kinds of system handles,
 // like mutexes, so we want to keep this manageable.
-constexpr uint32_t kSbMaxThreads = 90;
+const uint32_t kSbMaxThreads = 90;
 
 // The current platform's search path component separator character. When
 // specifying an ordered list of absolute paths of directories to search for a
 // given reason, this is the character that appears between entries. For
 // example, the search path of "/etc/search/first:/etc/search/second" uses ':'
 // as a search path component separator character.
-constexpr char kSbPathSepChar = ':';
+const char kSbPathSepChar = ':';
 
 // The string form of SB_PATH_SEP_CHAR.
 const char* kSbPathSepString = ":";
 
 // The maximum size the cache directory is allowed to use in bytes.
-constexpr uint32_t kSbMaxSystemPathCacheDirectorySize = 24 << 20;  // 24MiB
+const uint32_t kSbMaxSystemPathCacheDirectorySize = 24 << 20;  // 24MiB
 
 SB_EXPORT extern const bool kSbCanMapExecutableMemory = true;
 
