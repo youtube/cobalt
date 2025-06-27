ba2c8f58b042d31e639409af2000e9c0caf06821
Kaido Kert <kaidokert@google.com>
2025-06-27 13:15:21 -0700

    [Kokoro]: Attempt to work around file limits (#6206)
    Attempt to clean out workspace from majority of files, as we suspect we
    are hitting environment file count limits.
    
    Bug: 427807922
    
    original-hexsha: 69d4151e8503e61f10c9935ae943bb5839f73eb1

--- cobalt/devinfra/kokoro/bin/cleanup.sh
+++ cobalt/devinfra/kokoro/bin/cleanup.sh
@@ -7,4 +7,6 @@ set -x
 set +e
 git config --global --add safe.directory "${WORKSPACE_COBALT}"
 time git -C "${WORKSPACE_COBALT}" clean -dfx
+# Remove largest chunk of source to prevent running into executor file limits
+time rm -rf "${WORKSPACE_COBALT}/third_party"
 exit 0
