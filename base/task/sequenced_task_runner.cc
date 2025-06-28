773a674f23ae3c5efdd4a08a579ec93084290e90
Charley Kim <charleykim@google.com>
2025-06-28 05:20:42 +0000

    base/task + base/threading: Remove RecordQueuingDelayedTaskMetrics and TLS lookups (#6212)
    Bug: 421916650
    
    Currently seeing slow connections speeds and frame drops in 4K playback
    for Chrobalt. To address this, we're bringing back performance
    optimizations made by @jellefoks in C25 to improve performance and
    connection speed by eliminating TLS lookups and removing
    RecordQueuingDelayedTaskMetrics. Two performance improvements have
    already been landed: https://github.com/youtube/cobalt/pull/6109 and
    https://github.com/youtube/cobalt/pull/6188
    
    Changes:
    - [Small improvement for PostTaskAndReply
    - [remove RecordQueuingDelayedTaskMetrics
    
    original-hexsha: 3310ffb52c21c2600ba7e84cb242dfc414683318

--- base/task/sequenced_task_runner.cc
+++ base/task/sequenced_task_runner.cc
@@ -80,6 +80,14 @@ bool SequencedTaskRunner::PostDelayedTaskAt(
 // static
 const scoped_refptr<SequencedTaskRunner>&
 SequencedTaskRunner::GetCurrentDefault() {
+#if BUILDFLAG(IS_COBALT)
+  if (current_default_handle) {
+    return current_default_handle->task_runner_;
+  }
+  static const auto* const null_sequenced_task_runner = 
+    new scoped_refptr<SequencedTaskRunner>();
+  return *null_sequenced_task_runner;
+#endif
   CHECK(current_default_handle)
       << "Error: This caller requires a sequenced context (i.e. the current "
          "task needs to run from a SequencedTaskRunner). If you're in a test "
