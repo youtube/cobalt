445476a7299bdab21f791d6d7bb298657c47dc26
Yavor Goulishev <yavor@google.com>
2025-06-30 14:30:54 -0700

    Build raspi-2 hermetically. (#6216)
    - Add redirect from asm/hwcap.h to bits/hwcap.h
    - Modified the clang targets to indicate we are targeting Linux.
    - Remove the //base from gtest
    - Remove the original shared library from the list of link libraries as
    it causes starboard code to use partition_alloc from base.
    - Remove the symbol redirection in musl to time64. This would need a
    more holistic approach.
    - Adjusted v8 to include the elf.h
    
    Issue: 428259139
    Issue: 428685991
    
    original-hexsha: 9a69c6a6b1364e6f7b69d475fda5030a4bf738c4

--- third_party/musl/include/sys/stat.h
+++ third_party/musl/include/sys/stat.h
@@ -111,10 +111,8 @@ int lchmod(const char *, mode_t);
 #endif
 
 #if _REDIR_TIME64
-#if !defined(STARBOARD)
 __REDIR(stat, __stat_time64);
 __REDIR(fstat, __fstat_time64);
-#endif  // !defined(STARBOARD)
 __REDIR(lstat, __lstat_time64);
 __REDIR(fstatat, __fstatat_time64);
 __REDIR(futimens, __futimens_time64);
