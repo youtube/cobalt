f15d621afd245e8df816c277e3ae208044e050c3
Kyujung Youn <kjyoun@google.com>
2025-06-30 16:40:31 -0700

    rename macros

--- starboard/common/log.h
+++ starboard/common/log.h
@@ -150,7 +150,7 @@ class LogMessageVoidify {
   SB_LOG_IF(FATAL, !(condition)) << "Check failed: " #condition ". "
 #endif  // SB_LOGGING_IS_OFFICIAL_BUILD
 
-#define SB_CHECK_OP(a, b, op, op_string)                                    \
+#define SB_CHECK_OP_IMPL(a, b, op, op_string)                               \
   do {                                                                      \
     const auto& sb_check_a = (a);                                           \
     const auto& sb_check_b = (b);                                           \
@@ -159,12 +159,12 @@ class LogMessageVoidify {
         << sb_check_b << ")";                                               \
   } while (0)
 
-#define SB_CHECK_EQ(a, b) SB_CHECK_OP(a, b, ==, "equal to")
-#define SB_CHECK_NE(a, b) SB_CHECK_OP(a, b, !=, "not-equal to")
-#define SB_CHECK_GT(a, b) SB_CHECK_OP(a, b, >, "greater than")
-#define SB_CHECK_GE(a, b) SB_CHECK_OP(a, b, >=, "greater than or equal to")
-#define SB_CHECK_LT(a, b) SB_CHECK_OP(a, b, <, "less than")
-#define SB_CHECK_LE(a, b) SB_CHECK_OP(a, b, <=, "less than or equal to")
+#define SB_CHECK_EQ(a, b) SB_CHECK_OP_IMPL(a, b, ==, "equal to")
+#define SB_CHECK_NE(a, b) SB_CHECK_OP_IMPL(a, b, !=, "not-equal to")
+#define SB_CHECK_GT(a, b) SB_CHECK_OP_IMPL(a, b, >, "greater than")
+#define SB_CHECK_GE(a, b) SB_CHECK_OP_IMPL(a, b, >=, "greater than or equal to")
+#define SB_CHECK_LT(a, b) SB_CHECK_OP_IMPL(a, b, <, "less than")
+#define SB_CHECK_LE(a, b) SB_CHECK_OP_IMPL(a, b, <=, "less than or equal to")
 
 #if SB_LOGGING_IS_OFFICIAL_BUILD || \
     (defined(NDEBUG) && !defined(COBALT_LOGGING_ENABLED))
@@ -192,19 +192,21 @@ class SbDcheckNoOpStream {
 #endif
 
 #if SB_DCHECK_ENABLED
-#define SB_DCHECK_OP(a, b, op, op_string) SB_CHECK_OP(a, b, op, op_string)
+#define SB_DCHECK_OP_IMPL(a, b, op, op_string) \
+  SB_CHECK_OP_IMPL(a, b, op, op_string)
 #else
-#define SB_DCHECK_OP(a, b, op, op_string) \
-  do {                                    \
+#define SB_DCHECK_OP_IMPL(a, b, op, op_string) \
+  do {                                         \
   } while (0)
 #endif
 
-#define SB_DCHECK_EQ(a, b) SB_DCHECK_OP(a, b, ==, "equal to")
-#define SB_DCHECK_NE(a, b) SB_DCHECK_OP(a, b, !=, "not-equal to")
-#define SB_DCHECK_GT(a, b) SB_DCHECK_OP(a, b, >, "greater than")
-#define SB_DCHECK_GE(a, b) SB_DCHECK_OP(a, b, >=, "greater than or equal to")
-#define SB_DCHECK_LT(a, b) SB_DCHECK_OP(a, b, <, "less than")
-#define SB_DCHECK_LE(a, b) SB_DCHECK_OP(a, b, <=, "less than or equal to")
+#define SB_DCHECK_EQ(a, b) SB_DCHECK_OP_IMPL(a, b, ==, "equal to")
+#define SB_DCHECK_NE(a, b) SB_DCHECK_OP_IMPL(a, b, !=, "not-equal to")
+#define SB_DCHECK_GT(a, b) SB_DCHECK_OP_IMPL(a, b, >, "greater than")
+#define SB_DCHECK_GE(a, b) \
+  SB_DCHECK_OP_IMPL(a, b, >=, "greater than or equal to")
+#define SB_DCHECK_LT(a, b) SB_DCHECK_OP_IMPL(a, b, <, "less than")
+#define SB_DCHECK_LE(a, b) SB_DCHECK_OP_IMPL(a, b, <=, "less than or equal to")
 
 #define SB_DLOG(severity) SB_DLOG_IF(severity, SB_DLOG_IS_ON(severity))
 #define SB_DSTACK(severity) SB_STACK_IF(severity, SB_DLOG_IS_ON(severity))
