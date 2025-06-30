42f42d0a38fb93ecc1288005a02ef2087d94db97
Colin Liang <colinliang@google.com>
2025-06-30 16:34:24 -0700

    draft
    update build
    
    Change-Id: I1487e86a5a148d17528a944abdf8f549f9449977
    
    t
    
    Change-Id: Ie9844296e0f8df6218c59c46bf23cd4e725b29f0

--- cobalt/android/apk/app/src/main/java/dev/cobalt/shell/Shell.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/shell/Shell.java
@@ -1,8 +1,18 @@
-// Copyright 2012 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-package org.chromium.content_shell;
+package dev.cobalt.shell;
 
 import android.app.Activity;
 import android.content.Context;
@@ -28,10 +38,10 @@ import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.base.ViewAndroidDelegate;
 import org.chromium.ui.base.WindowAndroid;
 
-// Cobalt's own Java implementation of Shell. It does not have Shell UI.
-
 /**
- * Container for the various UI components that make up a shell window.
+ *  Copied from org.chromium.content_shell.Shell.
+ *  Cobalt's own Java implementation of Shell. It does not have Shell UI.
+ *  Container for the various UI components that make up a shell window.
  */
 @JNINamespace("content")
 public class Shell {
