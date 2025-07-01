5dce56572e00757773ebe095fdc72f539605c1ed
Colin Liang <colinliang@google.com>
2025-07-01 14:40:39 -0700

    revert Copyright, fix stylecheck
    Change-Id: Ia30d29273e6368ba3070226c03f8d4e8a6170fe2

--- cobalt/android/apk/app/src/main/java/dev/cobalt/shell/Shell.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/shell/Shell.java
@@ -1,16 +1,6 @@
-// Copyright 2025 The Cobalt Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2012 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 package dev.cobalt.shell;
 
@@ -18,8 +8,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Rect;
 import android.text.TextUtils;
-import android.util.AttributeSet;
-import android.util.Log;
 import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuItem;
