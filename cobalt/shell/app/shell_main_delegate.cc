847a5a9635eec3af528984caeb6939cf6275f0a5
Hao Zheng <haozheng@google.com>
2025-07-07 18:35:07 +0000

    Decouple content/shell/gpu and content/shell/utility

--- cobalt/shell/app/shell_main_delegate.cc
+++ cobalt/shell/app/shell_main_delegate.cc
@@ -1,6 +1,16 @@
-// Copyright 2012 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "cobalt/shell/app/shell_main_delegate.h"
 
@@ -25,7 +35,9 @@
 #endif
 #include "cobalt/shell/browser/shell_content_browser_client.h"
 #include "cobalt/shell/browser/shell_paths.h"
+#include "cobalt/shell/gpu/shell_content_gpu_client.h"
 #include "cobalt/shell/renderer/shell_content_renderer_client.h"
+#include "cobalt/shell/utility/shell_content_utility_client.h"
 #include "components/memory_system/initializer.h"
 #include "components/memory_system/parameters.h"
 #include "content/common/content_constants_internal.h"
@@ -37,8 +49,6 @@
 #include "content/shell/app/shell_crash_reporter_client.h"
 #include "content/shell/common/shell_content_client.h"
 #include "content/shell/common/shell_switches.h"
-#include "content/shell/gpu/shell_content_gpu_client.h"
-#include "content/shell/utility/shell_content_utility_client.h"
 #include "ipc/ipc_buildflags.h"
 #include "net/cookies/cookie_monster.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
