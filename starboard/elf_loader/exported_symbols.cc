a3f5b6de7710d7880773ec65447a63807d17976f
Holden Warriner <hwarriner@google.com>
2025-07-08 09:38:18 -0400

    Add (ABI-wrapped) eventfd symbol to Starboard (#6251)
    Issue: 412690906
    
    original-hexsha: 5841ac5dff35aab58bd39968f4f11fbb891648be

--- starboard/elf_loader/exported_symbols.cc
+++ starboard/elf_loader/exported_symbols.cc
@@ -53,6 +53,7 @@
 #include "starboard/shared/modular/starboard_layer_posix_auxv_abi_wrappers.h"
 #include "starboard/shared/modular/starboard_layer_posix_directory_abi_wrappers.h"
 #include "starboard/shared/modular/starboard_layer_posix_errno_abi_wrappers.h"
+#include "starboard/shared/modular/starboard_layer_posix_eventfd_abi_wrappers.h"
 #include "starboard/shared/modular/starboard_layer_posix_mmap_abi_wrappers.h"
 #include "starboard/shared/modular/starboard_layer_posix_pipe2_abi_wrappers.h"
 #include "starboard/shared/modular/starboard_layer_posix_pthread_abi_wrappers.h"
@@ -308,6 +309,7 @@ ExportedSymbols::ExportedSymbols() {
   } else {
     REGISTER_SYMBOL(__errno_location);
   }
+  REGISTER_WRAPPER(eventfd);
   REGISTER_WRAPPER(fstat);
   REGISTER_WRAPPER(freeaddrinfo);
   REGISTER_WRAPPER(ftruncate);
