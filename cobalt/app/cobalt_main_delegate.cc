767ecbe17a0193590a29df724d6bd9a4eecbc79c
Bo-Rong Chen <borongchen@google.com>
2025-07-10 15:16:59 -0700

    Create ContentUtilityClient for Cobalt (#6360)
    As ShellContentUtilityClient has several testing services, which is not
    needed for Cobalt and may potentially cause native crash.
    
    Create CobaltContentUtilityClient so it doesn't include these testing
    services.
    
    Issue: 429696627
    Issue: 429216273
    
    original-hexsha: 313b859920c8e13613edfd2a5759322663915671

--- cobalt/app/cobalt_main_delegate.cc
+++ cobalt/app/cobalt_main_delegate.cc
@@ -59,6 +59,12 @@ CobaltMainDelegate::CreateContentRendererClient() {
   return renderer_client_.get();
 }
 
+content::ContentUtilityClient*
+CobaltMainDelegate::CreateContentUtilityClient() {
+  utility_client_ = std::make_unique<CobaltContentUtilityClient>();
+  return utility_client_.get();
+}
+
 std::optional<int> CobaltMainDelegate::PostEarlyInitialization(
     InvokedIn invoked_in) {
   content::RenderFrameHost::AllowInjectingJavaScript();
