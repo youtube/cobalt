64580f1af82518f11da621f3d41aa5d7d4bdafee
Hao <131711973+haozheng-cobalt@users.noreply.github.com>
2025-07-14 00:26:38 -0400

    Remove unsupported platforms from cobalt/shell/app (#6386)
    This PR removes all unsupported platforms from cobalt/shell/app
    following the
    [protocol](https://docs.google.com/document/d/1CdpkKtoorTc_lEX22oEr9bCLlLSftUYkMihogM8Ik6o/edit?resourcekey=0-Vzx1A-FYKGs0dhE_xkx63g&tab=t.0#heading=h.5t3cvknsv58c).
    
    Bug: 431052671
    
    original-hexsha: 187cd7fa4390c21a4dfd3a015c14b835a24561c1

--- cobalt/shell/app/shell_crash_reporter_client.h
+++ cobalt/shell/app/shell_crash_reporter_client.h
@@ -29,17 +29,7 @@ class ShellCrashReporterClient : public crash_reporter::CrashReporterClient {
 
   ~ShellCrashReporterClient() override;
 
-#if BUILDFLAG(IS_WIN)
-  // Returns a textual description of the product type and version to include
-  // in the crash report.
-  void GetProductNameAndVersion(const std::wstring& exe_path,
-                                std::wstring* product_name,
-                                std::wstring* version,
-                                std::wstring* special_build,
-                                std::wstring* channel_name) override;
-#endif
-
-#if BUILDFLAG(IS_POSIX) && !BUILDFLAG(IS_MAC)
+#if BUILDFLAG(IS_POSIX)
   // Returns a textual description of the product type and version to include
   // in the crash report.
   void GetProductNameAndVersion(const char** product_name,
@@ -52,11 +42,7 @@ class ShellCrashReporterClient : public crash_reporter::CrashReporterClient {
 
   // The location where minidump files should be written. Returns true if
   // |crash_dir| was set.
-#if BUILDFLAG(IS_WIN)
-  bool GetCrashDumpLocation(std::wstring* crash_dir) override;
-#else
   bool GetCrashDumpLocation(base::FilePath* crash_dir) override;
-#endif
 
 #if BUILDFLAG(IS_ANDROID)
   // Returns the descriptor key of the android minidump global descriptor.
