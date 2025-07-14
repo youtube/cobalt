6856aa633f7c51ba6e30075872a37cadc6236577
Hao <131711973+haozheng-cobalt@users.noreply.github.com>
2025-07-14 00:26:38 -0400

    Remove unsupported platforms from cobalt/shell/app (#6386)
    This PR removes all unsupported platforms from cobalt/shell/app
    following the
    [protocol](https://docs.google.com/document/d/1CdpkKtoorTc_lEX22oEr9bCLlLSftUYkMihogM8Ik6o/edit?resourcekey=0-Vzx1A-FYKGs0dhE_xkx63g&tab=t.0#heading=h.5t3cvknsv58c).
    
    Bug: 431052671
    (cherry picked from commit 187cd7fa4390c21a4dfd3a015c14b835a24561c1)

--- cobalt/shell/app/shell_main.cc
+++ cobalt/shell/app/shell_main.cc
@@ -16,13 +16,6 @@
 #include "cobalt/shell/app/shell_main_delegate.h"
 #include "content/public/app/content_main.h"
 
-#if BUILDFLAG(IS_WIN)
-#include "base/win/dark_mode_support.h"
-#include "base/win/win_util.h"
-#include "content/public/app/sandbox_helper_win.h"
-#include "sandbox/win/src/sandbox_types.h"
-#endif
-
 #if BUILDFLAG(IS_IOS)
 #include "base/at_exit.h"       // nogncheck
 #include "base/command_line.h"  // nogncheck
@@ -30,31 +23,7 @@
 #include "content/public/common/content_switches.h"  // nogncheck
 #endif
 
-#if BUILDFLAG(IS_WIN)
-
-#if !defined(WIN_CONSOLE_APP)
-int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE, wchar_t*, int) {
-#else
-int main() {
-  HINSTANCE instance = GetModuleHandle(NULL);
-#endif
-  // Load and pin user32.dll and uxtheme.dll to avoid having to load them once
-  // tests start while on the main thread loop where blocking calls are
-  // disallowed. This will also ensure the Windows dark mode support is enabled
-  // for the app if available.
-  base::win::PinUser32();
-  base::win::AllowDarkModeForApp(true);
-  sandbox::SandboxInterfaceInfo sandbox_info = {nullptr};
-  content::InitializeSandboxInfo(&sandbox_info);
-  content::ShellMainDelegate delegate;
-
-  content::ContentMainParams params(&delegate);
-  params.instance = instance;
-  params.sandbox_info = &sandbox_info;
-  return content::ContentMain(std::move(params));
-}
-
-#elif BUILDFLAG(IS_IOS)
+#if BUILDFLAG(IS_IOS)
 
 int main(int argc, const char** argv) {
   // Create this here since it's needed to start the crash handler.
@@ -89,4 +58,4 @@ int main(int argc, const char** argv) {
   return content::ContentMain(std::move(params));
 }
 
-#endif  // BUILDFLAG(IS_WIN)
+#endif  // BUILDFLAG(IS_IOS)
