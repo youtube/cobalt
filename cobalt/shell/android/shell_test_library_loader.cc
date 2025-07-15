debe49354e48ad8f00236d0a7f7bec4d6bdbee03
Adam Walls <avvall@google.com>
2025-07-15 21:57:19 +0000

    Houston, we have broken tests. All tests run, but they all seem to fail.

--- cobalt/shell/android/shell_test_library_loader.cc
+++ cobalt/shell/android/shell_test_library_loader.cc
@@ -3,9 +3,9 @@
 // found in the LICENSE file.
 
 #include "base/android/jni_android.h"
+#include "cobalt/shell/app/shell_main_delegate.h"
 #include "content/public/app/content_jni_onload.h"
 #include "content/public/app/content_main.h"
-#include "content/shell/app/shell_main_delegate.h"
 
 // This is called by the VM when the shared library is first loaded.
 JNI_EXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
