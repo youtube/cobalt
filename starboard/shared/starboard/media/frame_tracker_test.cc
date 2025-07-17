2bfbc8f52fb4adcdfdd39ec9a41e4a6debd8c8f3
Kyujung Youn <kjyoun@google.com>
2025-07-17 10:45:33 -0700

    Add frame tracker

---
+++ starboard/shared/starboard/media/frame_tracker_test.cc
@@ -0,0 +1,115 @@
+#include "starboard/shared/starboard/media/frame_tracker.h"
+
+#include <sstream>
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace starboard::shared::starboard::media {
+namespace {
+
+constexpr int kMaxFrames = 6;
+
+TEST(FrameTrackerTest, InitialState) {
+  FrameTracker frame_tracker(kMaxFrames);
+
+  FrameTracker::State status = frame_tracker.GetCurrentState();
+
+  EXPECT_EQ(status.decoding_frames, 0);
+  EXPECT_EQ(status.decoded_frames, 0);
+  EXPECT_EQ(status.decoding_frames_high_water_mark, 0);
+  EXPECT_EQ(status.decoded_frames_high_water_mark, 0);
+  EXPECT_EQ(status.total_frames_high_water_mark, 0);
+}
+
+TEST(FrameTrackerTest, AddFrame) {
+  FrameTracker frame_tracker(kMaxFrames);
+
+  frame_tracker.AddFrame();
+  FrameTracker::State status = frame_tracker.GetCurrentState();
+
+  EXPECT_EQ(status.decoding_frames, 1);
+  EXPECT_EQ(status.decoded_frames, 0);
+  EXPECT_EQ(status.decoding_frames_high_water_mark, 1);
+  EXPECT_EQ(status.decoded_frames_high_water_mark, 0);
+  EXPECT_EQ(status.total_frames_high_water_mark, 1);
+}
+
+TEST(FrameTrackerTest, SetFrameDecoded) {
+  FrameTracker frame_tracker(kMaxFrames);
+  frame_tracker.AddFrame();
+
+  frame_tracker.SetFrameDecoded();
+  FrameTracker::State status = frame_tracker.GetCurrentState();
+
+  EXPECT_EQ(status.decoding_frames, 0);
+  EXPECT_EQ(status.decoded_frames, 1);
+  EXPECT_EQ(status.decoding_frames_high_water_mark, 1);
+  EXPECT_EQ(status.decoded_frames_high_water_mark, 1);
+  EXPECT_EQ(status.total_frames_high_water_mark, 1);
+}
+
+TEST(FrameTrackerTest, ReleaseFrame) {
+  FrameTracker frame_tracker(kMaxFrames);
+  frame_tracker.AddFrame();
+  frame_tracker.SetFrameDecoded();
+
+  frame_tracker.ReleaseFrame();
+  FrameTracker::State status = frame_tracker.GetCurrentState();
+
+  EXPECT_EQ(status.decoding_frames, 0);
+  EXPECT_EQ(status.decoded_frames, 0);
+  EXPECT_EQ(status.decoding_frames_high_water_mark, 1);
+  EXPECT_EQ(status.decoded_frames_high_water_mark, 1);
+  EXPECT_EQ(status.total_frames_high_water_mark, 1);
+}
+
+TEST(FrameTrackerTest, HighWaterMark) {
+  FrameTracker frame_tracker(kMaxFrames);
+  frame_tracker.AddFrame();
+  frame_tracker.AddFrame();
+  frame_tracker.SetFrameDecoded();
+  frame_tracker.SetFrameDecoded();
+  frame_tracker.ReleaseFrame();
+
+  FrameTracker::State status = frame_tracker.GetCurrentState();
+
+  EXPECT_EQ(status.decoding_frames, 0);
+  EXPECT_EQ(status.decoded_frames, 1);
+  EXPECT_EQ(status.decoding_frames_high_water_mark, 2);
+  EXPECT_EQ(status.decoded_frames_high_water_mark, 2);
+  EXPECT_EQ(status.total_frames_high_water_mark, 2);
+}
+
+TEST(FrameTrackerTest, Reset) {
+  FrameTracker frame_tracker(kMaxFrames);
+  frame_tracker.AddFrame();
+  frame_tracker.SetFrameDecoded();
+  frame_tracker.ReleaseFrame();
+
+  frame_tracker.Reset();
+  FrameTracker::State status = frame_tracker.GetCurrentState();
+
+  EXPECT_EQ(status.decoding_frames, 0);
+  EXPECT_EQ(status.decoded_frames, 0);
+  EXPECT_EQ(status.decoding_frames_high_water_mark, 0);
+  EXPECT_EQ(status.decoded_frames_high_water_mark, 0);
+  EXPECT_EQ(status.total_frames_high_water_mark, 0);
+}
+
+TEST(FrameTrackerTest, StreamInsertionOperator) {
+  FrameTracker::State status;
+  status.decoding_frames = 1;
+  status.decoded_frames = 2;
+  status.decoding_frames_high_water_mark = 3;
+  status.decoded_frames_high_water_mark = 4;
+  status.total_frames_high_water_mark = 5;
+
+  std::stringstream ss;
+  ss << status;
+
+  EXPECT_EQ(ss.str(),
+            "{decoding: 1, decoded: 2, decoding (hw): 3, decoded (hw): 4, "
+            "total (hw): 5}");
+}
+
+}  // namespace
+}  // namespace starboard::shared::starboard::media
