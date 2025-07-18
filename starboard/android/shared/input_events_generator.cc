youtube/cobalt

commit 2b8344672831c28a7a284c2331a0d52dddcc6937

Use nested namespace in starboard/shared (#5948)
- Nested namespace makes code more concise.
- Starboard supports c++17, which supports nested namespace
-
[software-certification-2025](https://developers.google.com/youtube/devices/living-room/2025/software-certification-2025#cobalt_runtime)
> The target device MUST support a toolchain capable of supporting C++17
and C11 to build Cobalt and Starboard.

b/421142168

original-hexsha: 51936ed9d64b9c48cdb7826869c309ba80b0f77b

starboard/shared/deviceauth/deviceauth_internal.cc
@@ -40,9 +40,7 @@ bool Base64Decode(const std::string& input, std::string* output) {
 }
 }  // namespace
 
-namespace starboard {
-namespace shared {
-namespace deviceauth {
+namespace starboard::shared::deviceauth {
 
 // This function is meant to exist purely for reference.
 //
@@ -80,5 +78,4 @@ bool SignWithCertificationSecretKey(const char* secret_key,
 }
 
 }  // namespace deviceauth
-}  // namespace shared
-}  // namespace starboard
+}  // namespace starboard::shared::deviceauth

starboard/shared/deviceauth/deviceauth_internal.h
@@ -17,9 +17,7 @@
 
 #include <cstddef>
 #include <cstdint>
-namespace starboard {
-namespace shared {
-namespace deviceauth {
+namespace starboard::shared::deviceauth {
 
 bool SignWithCertificationSecretKey(const char* secret_key,
                                     const uint8_t* message,
@@ -28,7 +26,7 @@ bool SignWithCertificationSecretKey(const char* secret_key,
                                     size_t digest_size_in_bytes);
 
 }  // namespace deviceauth
-}  // namespace shared
-}  // namespace starboard
+
+}  // namespace starboard::shared::deviceauth
 
 #endif  // STARBOARD_SHARED_DEVICEAUTH_DEVICEAUTH_INTERNAL_H_

starboard/shared/libaom/aom_library_loader.h
@@ -19,9 +19,7 @@
 
 #include "starboard/shared/internal_only.h"
 
-namespace starboard {
-namespace shared {
-namespace aom {
+namespace starboard::shared::aom {
 
 bool is_aom_supported();
 
@@ -43,7 +41,7 @@ extern aom_codec_err_t (*aom_codec_decode)(aom_codec_ctx_t*,
 extern aom_image_t* (*aom_codec_get_frame)(aom_codec_ctx_t*, aom_codec_iter_t*);
 
 }  // namespace aom
-}  // namespace shared
-}  // namespace starboard
+
+}  // namespace starboard::shared::aom
 
 #endif  // STARBOARD_SHARED_LIBAOM_AOM_LIBRARY_LOADER_H_

starboard/shared/libaom/aom_video_decoder.cc
@@ -19,9 +19,7 @@
 #include "starboard/linux/shared/decode_target_internal.h"
 #include "starboard/shared/libaom/aom_library_loader.h"
 
-namespace starboard {
-namespace shared {
-namespace aom {
+namespace starboard::shared::aom {
 
 using starboard::player::JobThread;
 
@@ -297,5 +295,5 @@ SbDecodeTarget VideoDecoder::GetCurrentDecodeTarget() {
 }
 
 }  // namespace aom
-}  // namespace shared
-}  // namespace starboard
+
+}  // namespace starboard::shared::aom

starboard/shared/libaom/aom_video_decoder.h
@@ -31,9 +31,7 @@
 #include "starboard/shared/starboard/player/job_queue.h"
 #include "starboard/shared/starboard/player/job_thread.h"
 
-namespace starboard {
-namespace shared {
-namespace aom {
+namespace starboard::shared::aom {
 
 class VideoDecoder : public starboard::player::filter::VideoDecoder,
                      private starboard::player::JobQueue::JobOwner {
@@ -109,7 +107,7 @@ class VideoDecoder : public starboard::player::filter::VideoDecoder,
 };
 
 }  // namespace aom
-}  // namespace shared
-}  // namespace starboard
+
+}  // namespace starboard::shared::aom
 
 #endif  // STARBOARD_SHARED_LIBAOM_AOM_VIDEO_DECODER_H_

starboard/shared/speechd/speechd_internal.cc
@@ -30,9 +30,7 @@
 #include "starboard/common/once.h"
 #include "starboard/shared/starboard/application.h"
 
-namespace starboard {
-namespace shared {
-namespace speechd {
+namespace starboard::shared::speechd {
 namespace {
 SbOnceControl init_once = SB_ONCE_INITIALIZER;
 }  // namespace
@@ -122,5 +120,5 @@ void SpeechDispatcher::Destroy() {
 }
 
 }  // namespace speechd
-}  // namespace shared
-}  // namespace starboard
+
+}  // namespace starboard::shared::speechd::

starboard/shared/speechd/speechd_internal.h
@@ -20,9 +20,7 @@
 #include "starboard/common/mutex.h"
 #include "starboard/shared/internal_only.h"
 
-namespace starboard {
-namespace shared {
-namespace speechd {
+namespace starboard::shared::speechd {
 
 // |SpeechDispatcher| is a helper class that is used to implement the
 // SbSpeechSynthesis* APIs in terms of the Speech Dispatcher C API:
@@ -70,7 +68,7 @@ class SpeechDispatcher {
 };
 
 }  // namespace speechd
-}  // namespace shared
-}  // namespace starboard
+
+}  // namespace starboard::shared::speechd
 
 #endif  // STARBOARD_SHARED_SPEECHD_SPEECHD_INTERNAL_H_

commit 41c8f407a99bc561bdd440573e04651082a601b7

fix: Remove unmatched namespace closing brackets (#6473)
Those unmatched brackets are the defects introduced by #5948 and #5908

Fixed: 432557456

original-hexsha: 7ba2c13ad1de3aff797e2c2937e09f9ce0d53519

starboard/android/shared/input_events_generator.cc
@@ -961,6 +961,4 @@ void InputEventsGenerator::CreateInputEventsFromSbKey(SbKey key,
                              &Application::DeleteDestructor<SbInputData>)));
 }
 
-}  // namespace shared
 }  // namespace starboard::android::shared
-}  // namespace starboard

starboard/shared/deviceauth/deviceauth_internal.cc
@@ -77,5 +77,4 @@ bool SignWithCertificationSecretKey(const char* secret_key,
   return result == digest;
 }
 
-}  // namespace deviceauth
 }  // namespace starboard::shared::deviceauth

starboard/shared/deviceauth/deviceauth_internal.h
@@ -25,8 +25,6 @@ bool SignWithCertificationSecretKey(const char* secret_key,
                                     uint8_t* digest,
                                     size_t digest_size_in_bytes);
 
-}  // namespace deviceauth
-
 }  // namespace starboard::shared::deviceauth
 
 #endif  // STARBOARD_SHARED_DEVICEAUTH_DEVICEAUTH_INTERNAL_H_

starboard/shared/libaom/aom_library_loader.h
@@ -40,8 +40,6 @@ extern aom_codec_err_t (*aom_codec_decode)(aom_codec_ctx_t*,
 
 extern aom_image_t* (*aom_codec_get_frame)(aom_codec_ctx_t*, aom_codec_iter_t*);
 
-}  // namespace aom
-
 }  // namespace starboard::shared::aom
 
 #endif  // STARBOARD_SHARED_LIBAOM_AOM_LIBRARY_LOADER_H_

starboard/shared/libaom/aom_video_decoder.cc
@@ -294,6 +294,4 @@ SbDecodeTarget VideoDecoder::GetCurrentDecodeTarget() {
   }
 }
 
-}  // namespace aom
-
 }  // namespace starboard::shared::aom

starboard/shared/libaom/aom_video_decoder.h
@@ -107,8 +107,6 @@ class VideoDecoder : public starboard::player::filter::VideoDecoder,
   std::queue<scoped_refptr<CpuVideoFrame>> frames_;
 };
 
-}  // namespace aom
-
 }  // namespace starboard::shared::aom
 
 #endif  // STARBOARD_SHARED_LIBAOM_AOM_VIDEO_DECODER_H_

starboard/shared/speechd/speechd_internal.cc
@@ -119,6 +119,4 @@ void SpeechDispatcher::Destroy() {
   instance = NULL;
 }
 
-}  // namespace speechd
-
-}  // namespace starboard::shared::speechd::
+}  // namespace starboard::shared::speechd

starboard/shared/speechd/speechd_internal.h
@@ -68,8 +68,6 @@ class SpeechDispatcher {
   static SpeechDispatcher* instance;
 };
 
-}  // namespace speechd
-
 }  // namespace starboard::shared::speechd
 
 #endif  // STARBOARD_SHARED_SPEECHD_SPEECHD_INTERNAL_H_