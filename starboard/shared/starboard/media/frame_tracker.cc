b8fbf61c3056dcf9ef4704d525ca19d74d0279c6
Kyujung Youn <kjyoun@google.com>
2025-07-18 12:53:59 -0700

    refactor: Simplify FrameTracker by removing the frames array
    This commit refactors the  to simplify its state management by removing the  vector. Instead of tracking individual frames, the tracker now directly maintains counts of decoding and decoded frames.
    
    This change also includes:
    - Fixing logic errors in the frame counting.
    - Correcting unit tests to account for the asynchronous nature of frame releases.
    - Ensuring all unit tests and the  build pass successfully.

--- starboard/shared/starboard/media/frame_tracker.cc
+++ starboard/shared/starboard/media/frame_tracker.cc
@@ -1,17 +1,29 @@
+// Copyright 2017 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "starboard/shared/starboard/media/frame_tracker.h"
 
 #include <algorithm>
 #include <numeric>
 #include <ostream>
 
+#include "starboard/common/check_op.h"
 #include "starboard/common/log.h"
 #include "starboard/common/time.h"
 
 namespace starboard::shared::starboard::media {
 namespace {
-constexpr int64_t kUnassigned = -1;
-constexpr int64_t kDecoding = -2;
-constexpr int64_t kDecoded = -3;
 
 constexpr int kMaxDecodingHistory = 30;
 
@@ -32,9 +44,10 @@ std::ostream& operator<<(std::ostream& os, const FrameTracker::State& status) {
 FrameTracker::FrameTracker(int max_frames,
                            int64_t log_interval_us,
                            FrameReleasedCB frame_released_cb)
-    : frames_(max_frames, kUnassigned),
-      task_runner_("frame_tracker"),
-      frame_released_cb_(std::move(frame_released_cb)) {
+    : max_frames_(max_frames),
+      frame_released_cb_(std::move(frame_released_cb)),
+      state_({}),
+      task_runner_("frame_tracker") {
   SB_DCHECK(frame_released_cb_);
   if (log_interval_us > 0) {
     task_runner_.Schedule(
@@ -47,129 +60,105 @@ FrameTracker::~FrameTracker() = default;
 
 bool FrameTracker::AddFrame() {
   std::lock_guard lock(mutex_);
-  PurgeReleasedFrames_Locked();
-
-  for (auto& frame : frames_) {
-    if (frame == kUnassigned) {
-      frame = kDecoding;
-      decoding_start_times_us_.push_back(CurrentMonotonicTime());
-      UpdateHighWaterMarks_Locked();
-      return true;
-    }
+  if (state_.total_frames() == max_frames_) {
+    return false;
   }
+  state_.decoding_frames++;
+  decoding_start_times_us_.push_back(CurrentMonotonicTime());
 
-  return false;
+  UpdateState_Locked();
+  return true;
 }
 
 bool FrameTracker::SetFrameDecoded() {
   std::lock_guard lock(mutex_);
-  for (auto& frame : frames_) {
-    if (frame == kDecoding) {
-      frame = kDecoded;
-      if (!decoding_start_times_us_.empty()) {
-        auto start_time = decoding_start_times_us_.front();
-        decoding_start_times_us_.pop_front();
-        auto decoding_time = CurrentMonotonicTime() - start_time;
-        previous_decoding_times_us_.push_back(decoding_time);
-        if (previous_decoding_times_us_.size() > kMaxDecodingHistory) {
-          previous_decoding_times_us_.pop_front();
-        }
-      }
-      UpdateHighWaterMarks_Locked();
-      return true;
+  if (state_.decoding_frames == 0) {
+    return false;
+  }
+  state_.decoding_frames--;
+  state_.decoded_frames++;
+  SB_CHECK_GE(state_.decoded_frames, 0);
+
+  if (!decoding_start_times_us_.empty()) {
+    auto start_time = decoding_start_times_us_.front();
+    decoding_start_times_us_.pop_front();
+    auto decoding_time = CurrentMonotonicTime() - start_time;
+    previous_decoding_times_us_.push_back(decoding_time);
+    if (previous_decoding_times_us_.size() > kMaxDecodingHistory) {
+      previous_decoding_times_us_.pop_front();
     }
   }
-  return false;
+
+  UpdateState_Locked();
+  UpdateDecodingStat_Locked();
+  return true;
 }
 
 bool FrameTracker::ReleaseFrame() {
   return ReleaseFrameAt(CurrentMonotonicTime());
 }
 
-bool FrameTracker::ReleaseFrameAt(int64_t release_time) {
+bool FrameTracker::ReleaseFrameAt(int64_t release_us) {
   std::lock_guard lock(mutex_);
-  for (auto& frame : frames_) {
-    if (frame == kDecoded) {
-      frame = release_time;
-      int64_t delay_us =
-          std::max<int64_t>(release_time - CurrentMonotonicTime(), 0);
-      task_runner_.Schedule([this] { frame_released_cb_(); }, delay_us);
 
-      UpdateHighWaterMarks_Locked();
-      return true;
-    }
+  if (state_.decoded_frames == 0) {
+    return false;
   }
 
-  return false;
+  int64_t delay_us = std::max<int64_t>(release_us - CurrentMonotonicTime(), 0);
+  task_runner_.Schedule(
+      [this] {
+        frame_released_cb_();
+
+        std::lock_guard lock(mutex_);
+        state_.decoded_frames--;
+        UpdateState_Locked();
+      },
+      delay_us);
+
+  return true;
 }
 
-FrameTracker::State FrameTracker::GetCurrentState() {
+FrameTracker::State FrameTracker::GetCurrentState() const {
   std::lock_guard lock(mutex_);
-  PurgeReleasedFrames_Locked();
-  auto [decoding_frames, decoded_frames] = UpdateHighWaterMarks_Locked();
-  int64_t min_decoding_time_us = 0;
-  int64_t max_decoding_time_us = 0;
-  int64_t avg_decoding_time_us = 0;
-
-  if (!previous_decoding_times_us_.empty()) {
-    min_decoding_time_us = previous_decoding_times_us_[0];
-    max_decoding_time_us = previous_decoding_times_us_[0];
-    int64_t sum = 0;
-    for (auto decoding_us : previous_decoding_times_us_) {
-      min_decoding_time_us = std::min(min_decoding_time_us, decoding_us);
-      max_decoding_time_us = std::max(max_decoding_time_us, decoding_us);
-      sum += decoding_us;
-    }
-    avg_decoding_time_us = sum / previous_decoding_times_us_.size();
-  }
-
-  return {decoding_frames,
-          decoded_frames,
-          decoding_frames_high_water_mark_,
-          decoded_frames_high_water_mark_,
-          total_frames_high_water_mark_,
-          min_decoding_time_us,
-          max_decoding_time_us,
-          avg_decoding_time_us};
+  return state_;
 }
 
 bool FrameTracker::IsFull() {
   std::lock_guard lock(mutex_);
-  PurgeReleasedFrames_Locked();
-  return std::find(frames_.cbegin(), frames_.cend(), kUnassigned) ==
-         frames_.cend();
+  return state_.total_frames() == max_frames_;
 }
 
-std::pair<int, int> FrameTracker::UpdateHighWaterMarks_Locked() {
-  int decoding_frames = 0;
-  int decoded_frames = 0;
-
-  for (auto& frame : frames_) {
-    if (frame == kUnassigned) {
-      continue;
-    } else if (frame == kDecoding) {
-      decoding_frames++;
-      continue;
-    }
-    decoded_frames++;
+void FrameTracker::UpdateDecodingStat_Locked() {
+  if (previous_decoding_times_us_.empty()) {
+    return;
+  }
+  int64_t min_decoding_time_us = previous_decoding_times_us_[0];
+  int64_t max_decoding_time_us = previous_decoding_times_us_[0];
+  int64_t sum = 0;
+  for (auto decoding_us : previous_decoding_times_us_) {
+    min_decoding_time_us = std::min(min_decoding_time_us, decoding_us);
+    max_decoding_time_us = std::max(max_decoding_time_us, decoding_us);
+    sum += decoding_us;
   }
 
-  decoding_frames_high_water_mark_ =
-      std::max(decoding_frames_high_water_mark_, decoding_frames);
-  decoded_frames_high_water_mark_ =
-      std::max(decoded_frames_high_water_mark_, decoded_frames);
-  total_frames_high_water_mark_ =
-      std::max(total_frames_high_water_mark_, decoding_frames + decoded_frames);
-  return {decoding_frames, decoded_frames};
+  state_.min_decoding_time_us = min_decoding_time_us;
+  state_.max_decoding_time_us = max_decoding_time_us;
+  state_.avg_decoding_time_us = sum / previous_decoding_times_us_.size();
 }
 
-void FrameTracker::PurgeReleasedFrames_Locked() {
-  int64_t now_us = CurrentMonotonicTime();
-  for (auto& frame : frames_) {
-    if (0 <= frame && frame <= now_us) {
-      frame = kUnassigned;
-    }
-  }
+void FrameTracker::UpdateState_Locked() {
+  SB_CHECK_LE(state_.total_frames(), max_frames_);
+  SB_CHECK_GE(state_.decoding_frames, 0);
+  SB_CHECK_GE(state_.decoded_frames, 0);
+
+  state_.decoding_frames_high_water_mark =
+      std::max(state_.decoding_frames_high_water_mark, state_.decoding_frames);
+  state_.decoded_frames_high_water_mark =
+      std::max(state_.decoded_frames_high_water_mark, state_.decoded_frames);
+  state_.total_frames_high_water_mark =
+      std::max(state_.total_frames_high_water_mark,
+               state_.decoding_frames + state_.decoded_frames);
 }
 
 void FrameTracker::LogStateAndReschedule(int64_t log_interval_us) {
