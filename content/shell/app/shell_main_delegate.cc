1cb16d946ec22e282ec9966a32ae42ff1c45838d
Adam <avvall@google.com>
2025-07-21 23:17:01 +0000

    [CONFLICT] Original Commit: 379386b0a1ccf12906d39365965d4179fe94c770
    Original Message:
    [Cobalt] Remove unused crashpad library (#5281)
    
    This removes the android crash logging library, since Cobalt on Android
    does not use crashpad.
    
    **Before (QA)**
    ```
    ls -la out/android-arm_qa/apks/
    total 88228
    drwxr-xr-x  2 avvall primarygroup     4096 Apr  2 21:23 .
    drwxr-xr-x 23 avvall primarygroup    12288 Apr  2 21:23 ..
    -rw-r--r--  1 avvall primarygroup 89875558 Apr  2 21:23 Cobalt.apk
    -rw-r--r--  2 avvall primarygroup   442966 Apr  1 16:51 Cobalt.apk.R.txt
    -rw-r--r--  1 avvall primarygroup      243 Apr  1 16:47 Trichrome.ssargs
    ```
    
    **After (QA)**
    ```
    ls -la out/android-arm_qa/apks/
    total 88188
    drwxr-xr-x  2 avvall primarygroup     4096 Apr  1 22:06 .
    drwxr-xr-x 23 avvall primarygroup    12288 Apr  1 22:05 ..
    -rw-r--r--  1 avvall primarygroup 89832799 Apr  1 22:06 Cobalt.apk
    -rw-r--r--  2 avvall primarygroup   442966 Apr  1 16:51 Cobalt.apk.R.txt
    -rw-r--r--  1 avvall primarygroup      243 Apr  1 16:47 Trichrome.ssargs
    ```
    
    **Delta (QA)**
    42759 B or 42.8 kB
    
    Test: Build and confirm size reduction in Cobalt.apk
    Bug: b/396490329
    original-hexsha: a892f602a7e7ce0cf182cd2bf521cd7209bfafe6
    
    Files with conflicts:
    - content/shell/app/shell_main_delegate.cc

--- content/shell/app/shell_main_delegate.cc
+++ content/shell/app/shell_main_delegate.cc
@@ -20,7 +20,9 @@
 #include "base/process/current_process.h"
 #include "base/trace_event/trace_log.h"
 #include "build/build_config.h"
+#if !BUILDFLAG(IS_ANDROIDTV)
 #include "components/crash/core/common/crash_key.h"
+#endif
 #include "components/memory_system/initializer.h"
 #include "components/memory_system/parameters.h"
 #include "content/common/content_constants_internal.h"
@@ -62,7 +64,7 @@
 #include "content/shell/android/shell_descriptors.h"
 #endif
 
-#if !BUILDFLAG(IS_FUCHSIA)
+#if !BUILDFLAG(IS_FUCHSIA) && !BUILDFLAG(IS_ANDROIDTV)
 #include "components/crash/core/app/crashpad.h"  // nogncheck
 #endif
 
@@ -94,6 +96,7 @@
 
 namespace {
 
+<<<<<<< HEAD
 enum class LoggingDest {
   kFile,
   kStderr,
@@ -103,6 +106,9 @@ enum class LoggingDest {
 };
 
 #if !BUILDFLAG(IS_FUCHSIA)
+=======
+#if !BUILDFLAG(IS_FUCHSIA) && !BUILDFLAG(IS_ANDROIDTV)
+>>>>>>> 379386b0a1c ([Cobalt] Remove unused crashpad library (#5281))
 base::LazyInstance<content::ShellCrashReporterClient>::Leaky
     g_shell_crash_client = LAZY_INSTANCE_INITIALIZER;
 #endif
@@ -258,8 +264,13 @@ bool ShellMainDelegate::ShouldInitializeMojo(InvokedIn invoked_in) {
 void ShellMainDelegate::PreSandboxStartup() {
 // Disable platform crash handling and initialize the crash reporter, if
 // requested.
+<<<<<<< HEAD
 // TODO(crbug.com/40188745): Implement crash reporter integration for Fuchsia.
 #if !BUILDFLAG(IS_FUCHSIA)
+=======
+// TODO(crbug.com/1226159): Implement crash reporter integration for Fuchsia.
+#if !BUILDFLAG(IS_FUCHSIA) && !BUILDFLAG(IS_ANDROIDTV)
+>>>>>>> 379386b0a1c ([Cobalt] Remove unused crashpad library (#5281))
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kEnableCrashReporter)) {
     std::string process_type =
@@ -275,9 +286,11 @@ void ShellMainDelegate::PreSandboxStartup() {
 #endif
     }
   }
-#endif  // !BUILDFLAG(IS_FUCHSIA)
+#endif  // !BUILDFLAG(IS_FUCHSIA) && !BUILDFLAG(IS_ANDROIDTV)
 
+#if !BUILDFLAG(IS_ANDROIDTV)
   crash_reporter::InitializeCrashKeys();
+#endif  // !BUILDFLAG(IS_ANDROIDTV)
 
   InitializeResourceBundle();
 }
@@ -331,6 +344,7 @@ absl::variant<int, MainFunctionParams> ShellMainDelegate::RunProcess(
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 void ShellMainDelegate::ZygoteForked() {
+#if !BUILDFLAG(IS_ANDROIDTV)
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kEnableCrashReporter)) {
     std::string process_type =
@@ -340,6 +354,7 @@ void ShellMainDelegate::ZygoteForked() {
     crash_reporter::SetFirstChanceExceptionHandler(
         v8::TryHandleWebAssemblyTrapPosix);
   }
+#endif  // !BUILDFLAG(IS_ANDROIDTV)
 }
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 
