cbb8cfcf1439ab1cbf623eba95e574c78aa05095
Bo-Rong Chen <borongchen@google.com>
2025-07-22 16:47:57 -0700

    Revert "media: Allow StarboardRenderer to RequestOverlayInfo from WebMediaPlayer" (#6545)
    Reverts youtube/cobalt#6399
    
    This currently crashes on linux devel build. We should fix it and
    re-land the PR again.
    
    Issue: 429224482
    
    original-hexsha: 8d2ee168cb89fa824d4ea2783de74920e13e37cf

--- media/mojo/clients/starboard/starboard_renderer_client.h
+++ media/mojo/clients/starboard/starboard_renderer_client.h
@@ -66,8 +66,7 @@ class MEDIA_EXPORT StarboardRendererClient
       mojo::PendingRemote<RendererExtension> pending_renderer_extension,
       mojo::PendingReceiver<ClientExtension> client_extension_receiver,
       BindHostReceiverCallback bind_host_receiver_callback,
-      GpuVideoAcceleratorFactories* gpu_factories,
-      RequestOverlayInfoCB request_overlay_info_cb);
+      GpuVideoAcceleratorFactories* gpu_factories);
 
   StarboardRendererClient(const StarboardRendererClient&) = delete;
   StarboardRendererClient& operator=(const StarboardRendererClient&) = delete;
@@ -106,7 +105,6 @@ class MEDIA_EXPORT StarboardRendererClient
   // mojom::StarboardRendererClientExtension implementation
   void PaintVideoHoleFrame(const gfx::Size& size) override;
   void UpdateStarboardRenderingMode(const StarboardRenderingMode mode) override;
-  void RequestOverlayInfo(bool restart_for_transitions) override;
 
   // cobalt::media::mojom::VideoGeometryChangeClient implementation.
   void OnVideoGeometryChange(const gfx::RectF& rect_f,
@@ -136,7 +134,6 @@ class MEDIA_EXPORT StarboardRendererClient
   void SetPlayingState(bool is_playing);
   void UpdateCurrentFrame();
   void OnGetCurrentVideoFrameDone(const scoped_refptr<VideoFrame>& frame);
-  void OnOverlayInfoChanged(const OverlayInfo& overlay_info);
   void StartVideoRendererSink();
   void StopVideoRendererSink();
 
@@ -149,7 +146,6 @@ class MEDIA_EXPORT StarboardRendererClient
   mojo::Receiver<ClientExtension> client_extension_receiver_;
   const BindHostReceiverCallback bind_host_receiver_callback_;
   raw_ptr<GpuVideoAcceleratorFactories> gpu_factories_ = nullptr;
-  RequestOverlayInfoCB request_overlay_info_cb_;
 
   mojo::Remote<RendererExtension> renderer_extension_;
 
@@ -161,7 +157,6 @@ class MEDIA_EXPORT StarboardRendererClient
 
   bool is_playing_ = false;
   bool video_renderer_sink_started_ = false;
-  bool overlay_info_requested_ = false;
   scoped_refptr<VideoFrame> next_video_frame_;
 
   mutable base::Lock lock_;
