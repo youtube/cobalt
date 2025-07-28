0c027d55b1eb9bc407c4ac97a7494e13053b94f9
Andrew Savage <andrewsavage@google.com>
2025-07-28 15:06:20 +0000

    Try to move tar package instead

--- starboard/build/util/generate_runfile.py
+++ starboard/build/util/generate_runfile.py
@@ -6,20 +6,13 @@ import argparse
 import os
 import stat
 
-# TODO: b/434191035 - Allowing for either elf_loader_sandbox path should be
-# removed once CI has been updated to support the preferred path.
 _TEMPLATE = """#!/usr/bin/env python3
 import os
 import subprocess
 import sys
 
-preferred_path = os.path.join(os.path.dirname(__file__), 'elf_loader_sandbox')
-path_to_use = preferred_path
-if not os.path.exists(preferred_path):
-  path_to_use = '{outdir}/elf_loader_sandbox'
-
 command = [
-    path_to_use,
+    os.path.join(os.path.dirname(__file__), 'elf_loader_sandbox'),
     '--evergreen_content=.', '--evergreen_library={library}.so'
 ] + sys.argv[1:]
 try:
@@ -35,14 +28,11 @@ except Exception as e:
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--output', type=str, required=True)
-
-# TODO: b/434191035 - Remove once CI supports preferred path
-parser.add_argument('--outdir', type=str, required=True)
 parser.add_argument('--library', type=str, required=True)
 args = parser.parse_args()
 
 with open(args.output, 'w', encoding='utf-8') as f:
-  f.write(_TEMPLATE.format(outdir=args.outdir, library=args.library))
+  f.write(_TEMPLATE.format(library=args.library))
 current_permissions = stat.S_IMODE(os.stat(args.output).st_mode)
 new_permissions = current_permissions | stat.S_IXUSR | stat.S_IXGRP
 os.chmod(args.output, new_permissions)
