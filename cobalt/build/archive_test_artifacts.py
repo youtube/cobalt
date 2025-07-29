15b2802721f494c4f2b6d9d0fca21e634a527e46
Adam Walls <avvall@google.com>
2025-07-29 18:04:58 +0000

    Skip generating test_runner_script.runtime_deps for cobalt_browwsertests

--- cobalt/build/archive_test_artifacts.py
+++ cobalt/build/archive_test_artifacts.py
@@ -74,23 +74,24 @@ def create_archive(
   combined_deps = set()
   for target in targets:
     target_path, target_name = target.split(':')
+    target_generates_runtime_deps = _target_generates_runtime_deps(target)
     # Paths are configured in test.gni:
     # https://github.com/youtube/cobalt/blob/main/testing/test.gni
-    if use_android_deps_path:
-      deps_file = os.path.join(
-          out_dir, 'gen.runtime', target_path,
-          f'{target_name}__test_runner_script.runtime_deps')
-    else:
-      deps_file = os.path.join(out_dir, f'{target_name}.runtime_deps')
-      if not os.path.exists(deps_file):
-        # If |deps_file| doesn't exist it could be due to being generated with
-        # the starboard_toolchain. In that case, we should look in subfolders.
-        # For the time being, just try with an extra starboard/ in the path.
-        deps_file = os.path.join(out_dir, 'starboard',
-                                 f'{target_name}.runtime_deps')
+    if target_generates_runtime_deps:
+      if use_android_deps_path:
+        deps_file = os.path.join(
+            out_dir, 'gen.runtime', target_path,
+            f'{target_name}__test_runner_script.runtime_deps')
+      else:
+        deps_file = os.path.join(out_dir, f'{target_name}.runtime_deps')
+        if not os.path.exists(deps_file):
+          # If |deps_file| doesn't exist it could be due to being generated with
+          # the starboard_toolchain. In that case, we should look in subfolders.
+          # For the time being, just try with an extra starboard/ in the path.
+          deps_file = os.path.join(out_dir, 'starboard',
+                                   f'{target_name}.runtime_deps')
 
-    print('Collecting runtime dependencies for', target)
-    if _target_generates_runtime_deps(target):
+      print('Collecting runtime dependencies for', target)
       with open(deps_file, 'r', encoding='utf-8') as runtime_deps_file:
         # The paths in the runtime_deps files are relative to the out folder.
         # Android tests expects files both in the out and source root folders
