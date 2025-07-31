42b2decad329938855df099be56e7d5aa551b94e
Jessica Qu <jxqu@google.com>
2025-07-31 22:58:14 +0000

    media: Fix StarboardRendererWrapper unit tests

--- media/mojo/services/starboard/starboard_renderer_wrapper_unittest.cc
+++ media/mojo/services/starboard/starboard_renderer_wrapper_unittest.cc
@@ -169,12 +169,8 @@ class StarboardRendererWrapperTest : public testing::Test {
         std::move(media_log_remote), base::UnguessableToken::Create(),
         base::Seconds(1), base::Seconds(1), std::string(),
         std::move(renderer_extension_receiver),
-        std::move(client_extension_remote), base::NullCallback()
-#if BUILDFLAG(IS_ANDROID)
-                                                ,
-        AndroidOverlayMojoFactoryCB()
-#endif  // BUILDFLAG(IS_ANDROID)
-    );
+        std::move(client_extension_remote), base::NullCallback(),
+        AndroidOverlayMojoFactoryCB());
     renderer_wrapper_ =
         std::make_unique<StarboardRendererWrapper>(std::move(traits));
     renderer_wrapper_->SetRendererForTesting(mock_renderer_.get());
