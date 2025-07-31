dd728b4e24475bf40cc0a66f0e4b31bbd1d2614c
John Ocampo <johneocampo@google.com>
2025-07-31 18:18:52 +0000

    [Starboard] Add Starboard Features Extension for Android TV (#6385)
    Add the Starboard Features Extension and code necessary to initalize and
    update features and parameters on the Starboard level.
    
    This PR introduces a Starboard Extension to allow us to use Chromium's
    Feature-Experiments under the Starboard level. This allows us to run our
    own experiments for code in our Starboard implementation. A follow-up PR
    will add testing to this starboard extension.
    
    At a high level, we collect the features and parameters defined in
    starboard/extension/feature_config.h with their updated values (if the
    value isn't updated, their specified default value is used) on the
    Cobalt side, and push them down to the Starboard level for them to be
    initialized in Starboard. Once initialized, we can used these features
    and parameters similarly to how features and parameters are used in
    Chromium.
    
    For a deeper dive into the design, the design doc can be accessed from
    the [bug id](https://b.corp.google.com/issues/416039556).
    
    Bug: 416039556
    
    original-hexsha: 6882331b45b552216cf605f7451b4777931747a9

---
+++ cobalt/common/features/starboard_features_initialization.h
@@ -0,0 +1,29 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef COBALT_COMMON_FEATURES_STARBOARD_FEATURES_INITIALIZATION_H_
+#define COBALT_COMMON_FEATURES_STARBOARD_FEATURES_INITIALIZATION_H_
+
+namespace cobalt::features {
+
+// InitializeStarboardFeatures() will access the Starboard Features
+// extension to send the updated features and params defined in
+// starboard/extension/feature_config.h and send them down to the
+// starboard level.
+
+void InitializeStarboardFeatures();
+
+}  // namespace cobalt::features
+
+#endif  // COBALT_COMMON_FEATURES_STARBOARD_FEATURES_INITIALIZATION_H
