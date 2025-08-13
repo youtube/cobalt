621497b26e2c7d7a1dff17ac02ebc75dc9bb9e45
Andrew Savage <andrewsavage@google.com>
2025-08-13 14:55:52 -0700

    [POSIX] Remove all syscalls (#6625)
    Fixed: 424002414

--- base/linux_util.cc
+++ base/linux_util.cc
@@ -20,6 +20,7 @@
 #include "base/files/dir_reader_posix.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_file.h"
+#include "base/logging.h"
 #include "base/strings/safe_sprintf.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
