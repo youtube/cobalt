26163ac2115599a4c2b95bc5108a6fb01c0dcafd
Niranjan Yardi <nyardi@google.com>
2025-08-18 16:59:37 -0700

    Change icu initialization

--- cobalt/common/icu_init/init.cc
+++ cobalt/common/icu_init/init.cc
@@ -24,6 +24,7 @@
 #include <string>
 #include <vector>
 
+#include "base/i18n/icu_util.h"
 #include "starboard/common/log.h"
 #include "starboard/configuration_constants.h"
 #include "starboard/file.h"
@@ -37,97 +38,14 @@ namespace icu_init {
 
 namespace {
 
-constexpr std::string_view kIcuDataFileName = "icudtl.dat";
-
 pthread_once_t g_initialization_once = PTHREAD_ONCE_INIT;
 bool g_initialization_result = false;
-
-// Gets the full path to the ICU data file.
-std::string GetIcuDataPath() {
-  std::vector<char> base_path(kSbFileMaxPath);
-  bool result = SbSystemGetPath(kSbSystemPathContentDirectory, base_path.data(),
-                                base_path.size());
-  SB_CHECK(result) << "Failed to get ICU data path.";
-  std::string data_path(base_path.data());
-  data_path += kSbFileSepString;
-  data_path += kIcuDataFileName;
-  return data_path;
-}
-
-off_t GetIcuDataLength(const std::string& data_path) {
-  struct stat st;
-  int stat_result = stat(data_path.c_str(), &st);
-  if (stat_result != 0 || st.st_size <= 0) {
-    SB_CHECK(stat_result != -1) << "Failed to stat ICU data " << data_path
-                                << ", error " << strerror(errno);
-    SB_CHECK(stat_result == 0 && st.st_size > 0)
-        << "ICU data file unexpectedly has zero length.";
-    return -1;
-  }
-  return st.st_size;
-}
-
-// Memory-maps the ICU data file and returns a pointer to the mapped region.
-void* MmapIcuData(const std::string& data_path, off_t length) {
-  int fd = open(data_path.c_str(), O_RDONLY);
-  if (fd == -1) {
-    SB_LOG(ERROR) << "Failed to open ICU data file " << data_path << ", error "
-                  << strerror(errno);
-    return nullptr;
-  }
-
-  void* icu_data = mmap(nullptr, length, PROT_READ, MAP_SHARED, fd, 0);
-  close(fd);
-
-  if (icu_data == MAP_FAILED) {
-    SB_LOG(ERROR) << "Failed to mmap ICU data " << data_path << ", error "
-                  << strerror(errno);
-    return nullptr;
-  }
-  return icu_data;
-}
-
-bool SetIcuDataPointer(void* icu_data) {
-  UErrorCode error_code = U_ZERO_ERROR;
-  udata_setCommonData(icu_data, &error_code);
-  if (U_FAILURE(error_code)) {
-    SB_CHECK(U_SUCCESS(error_code))
-        << "Failed to set ICU common data pointer, error "
-        << u_errorName(error_code);
-    return false;
-  }
-  return true;
-}
-
-void PrintIcuNotLoadedWarning() {
-  SB_LOG(WARNING) << "ICU Database could not be loaded!";
-}
 
 // Initialize the ICU using the file named kIcuDataFileName in the
 // kSbSystemPathContentDirectory folder. Note that this gives the ICU its
 // database, but it does not actually set a default timezone or locale.
 void InitializeIcuDatabase() {
-  std::string data_path = GetIcuDataPath();
-
-  off_t length = GetIcuDataLength(data_path);
-  if (length <= 0) {
-    PrintIcuNotLoadedWarning();
-    return;
-  }
-
-  void* icu_data = MmapIcuData(data_path, length);
-  if (!icu_data) {
-    PrintIcuNotLoadedWarning();
-    return;
-  }
-
-  // Inform the OS that the mapped data is accessed randomly.
-  madvise(icu_data, length, MADV_RANDOM);
-
-  if (!SetIcuDataPointer(icu_data)) {
-    PrintIcuNotLoadedWarning();
-    return;
-  }
+  base::i18n::InitializeICU();
   g_initialization_result = true;
 }
 
