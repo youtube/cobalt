5ad525ef8f02c5ab82cc47d763f4269fcba8bd66
Kyujung Youn <kjyoun@google.com>
2025-08-18 14:44:41 -0700

    Refactor: Use std::mutex and std::condition_variable
    This change replaces Starboard's synchronization primitives (Mutex,
    ConditionVariable, ScopedLock) with their standard C++ counterparts
    (std::mutex, std::condition_variable, std::lock_guard,
    std::unique_lock).
    
    This is part of a modernization effort to prefer standard C++ libraries
    over custom Starboard implementations, improving code readability and
    maintainability.

--- starboard/android/shared/media_decoder.h
+++ starboard/android/shared/media_decoder.h
@@ -205,8 +205,8 @@ class MediaDecoder final
 
   std::atomic<int32_t> number_of_pending_inputs_{0};
 
-  Mutex mutex_;
-  ConditionVariable condition_variable_;
+  std::mutex mutex_;
+  std::condition_variable condition_variable_;
   std::deque<PendingInput> pending_inputs_;
   std::vector<int> input_buffer_indices_;
   std::vector<DequeueOutputResult> dequeue_output_results_;
