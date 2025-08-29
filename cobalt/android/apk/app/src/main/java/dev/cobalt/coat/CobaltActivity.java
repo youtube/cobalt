7b4bdaee025d6042315f69b2261a1caa037d6fba
Charley Kim <charleykim@google.com>
2025-08-29 13:25:23 -0700

    cobalt/android: Diagnostics for startBrowserProcessesAsync crash (#7034)
    Seeing a [native primes
    crash](https://primes.corp.google.com/products/5063451/metrics/crashes/clusters/1000000001517437853?sortBy=6&sortOrder=2&duration_seconds=2592000&source=8594998787&reportType=JAVA,NATIVE&trendsTab=overview&timeGranularity=daily):
    Abort message:
    [15783:15783:0813/<PHONE_NUMBER>:FATAL:jni_android.cc(364)] JNI
    exception: MessageQueue.java:335. I believe it has to do with
    startBrowserProcessesAsync() in CobaltActivity.java. While I'm not able
    to pinpoint the exact cause, I was able to force and replicate the crash
    by throwing exceptions in the onSuccess and onFailure calls in
    StartupCallback. Adding more exceptions to try and see what exactly is
    causing the crash. Current hypotheses include a failed initialization,
    low memory, and pressing the back key right after startup.
    
    Bug: 439066169
    
    original-hexsha: 1807e4a4b0b2fd556929dd218eea0099b9260af1

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
@@ -95,7 +95,7 @@ public abstract class CobaltActivity extends Activity {
   protected Boolean mShouldReloadOnResume = false;
   // Tracks the status of the FLAG_KEEP_SCREEN_ON window flag.
   private Boolean isKeepScreenOnEnabled = false;
-
+  private String diagnosticFinishReason = "Unknown";
 
   // Initially copied from ContentShellActiviy.java
   protected void createContent(final Bundle savedInstanceState) {
@@ -103,15 +103,10 @@ public abstract class CobaltActivity extends Activity {
     if (!CommandLine.isInitialized()) {
       CommandLine.init(null);
 
-      String[] commandLineArgs =
-          getCommandLineParamsFromIntent(
-              getIntent(), COMMAND_LINE_ARGS_KEY);
+      String[] commandLineArgs = getCommandLineParamsFromIntent(getIntent(), COMMAND_LINE_ARGS_KEY);
       CommandLineOverrideHelper.getFlagOverrides(
           new CommandLineOverrideHelper.CommandLineOverrideHelperParams(
-              shouldSetJNIPrefix,
-              VersionInfo.isOfficialBuild(),
-              commandLineArgs
-        ));
+              shouldSetJNIPrefix, VersionInfo.isOfficialBuild(), commandLineArgs));
     }
 
     DeviceUtils.addDeviceSpecificUserAgentSwitch();
@@ -171,13 +166,62 @@ public abstract class CobaltActivity extends Activity {
             new BrowserStartupController.StartupCallback() {
               @Override
               public void onSuccess() {
+                // Verbose code to differentiate different possible crash reasons
+                // for JNI crash on prime. Only the line number of the exception
+                // is output, hence the else/if block. b/439066169
+                if (isFinishing() || isDestroyed()) {
+                  if ("ON_BACK_PRESSED".equals(diagnosticFinishReason)) {
+                    throw new RuntimeException("Finish reason: ON_BACK_PRESSED");
+                  } else if ("ON_LOW_MEMORY".equals(diagnosticFinishReason)) {
+                    throw new RuntimeException("Finish reason: ON_LOW_MEMORY");
+                  } else if ("APP_INIT_FAILURE".equals(diagnosticFinishReason)) {
+                    throw new RuntimeException("Finish reason: APP_INIT_FAILURE");
+                  } else if ("ON_DESTROY_UNKNOWN".equals(diagnosticFinishReason)) {
+                    throw new RuntimeException("Finish reason: ON_DESTROY_UNKNOWN");
+                  } else {
+                    throw new RuntimeException(
+                        "Callback called on finishing Activity. Finish reason: "
+                            + diagnosticFinishReason);
+                  }
+                }
                 Log.i(TAG, "Browser process init succeeded");
                 finishInitialization(savedInstanceState);
+
+                if (isFinishing() || isDestroyed()) {
+                  if ("ON_BACK_PRESSED".equals(diagnosticFinishReason)) {
+                    throw new RuntimeException("Finish reason: ON_BACK_PRESSED after init");
+                  } else if ("ON_LOW_MEMORY".equals(diagnosticFinishReason)) {
+                    throw new RuntimeException("Finish reason: ON_LOW_MEMORY after init");
+                  } else if ("APP_INIT_FAILURE".equals(diagnosticFinishReason)) {
+                    throw new RuntimeException("Finish reason: APP_INIT_FAILURE after init");
+                  } else if ("ON_DESTROY_UNKNOWN".equals(diagnosticFinishReason)) {
+                    throw new RuntimeException("Finish reason: ON_DESTROY_UNKNOWN after init");
+                  } else {
+                    throw new RuntimeException(
+                        "Callback called on finishing Activity after init. Finish reason: "
+                            + diagnosticFinishReason);
+                  }
+                }
                 getStarboardBridge().measureAppStartTimestamp();
               }
 
               @Override
               public void onFailure() {
+                if (isFinishing() || isDestroyed()) {
+                  if ("ON_BACK_PRESSED".equals(diagnosticFinishReason)) {
+                    throw new RuntimeException("Finish reason: ON_BACK_PRESSED");
+                  } else if ("ON_LOW_MEMORY".equals(diagnosticFinishReason)) {
+                    throw new RuntimeException("Finish reason: ON_LOW_MEMORY");
+                  } else if ("APP_INIT_FAILURE".equals(diagnosticFinishReason)) {
+                    throw new RuntimeException("Finish reason: APP_INIT_FAILURE");
+                  } else if ("ON_DESTROY_UNKNOWN".equals(diagnosticFinishReason)) {
+                    throw new RuntimeException("Finish reason: ON_DESTROY_UNKNOWN");
+                  } else {
+                    throw new RuntimeException(
+                        "Callback called on finishing Activity. Finish reason: "
+                            + diagnosticFinishReason);
+                  }
+                }
                 Log.e(TAG, "Browser process init failed");
                 initializationFailed();
               }
@@ -205,6 +249,11 @@ public abstract class CobaltActivity extends Activity {
 
   // Initially copied from ContentShellActiviy.java
   private void initializationFailed() {
+    if (isFinishing() || isDestroyed()) {
+      throw new RuntimeException(
+          "initializationFailed on finishing Activity. Reason: " + diagnosticFinishReason);
+    }
+    diagnosticFinishReason = "APP_INIT_FAILURE";
     Log.e(TAG, "ContentView initialization failed.");
     Toast.makeText(
             CobaltActivity.this, R.string.browser_process_initialization_failed, Toast.LENGTH_SHORT)
@@ -307,7 +356,8 @@ public abstract class CobaltActivity extends Activity {
 
     videoSurfaceView = new VideoSurfaceView(this);
     a11yHelper = new CobaltA11yHelper(this, videoSurfaceView);
-    addContentView(videoSurfaceView, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+    addContentView(
+        videoSurfaceView, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
   }
 
   /**
@@ -410,6 +460,7 @@ public abstract class CobaltActivity extends Activity {
   @Override
   protected void onResume() {
     super.onResume();
+    diagnosticFinishReason = "Unknown";
     if (mShouldReloadOnResume) {
       WebContents webContents = getActiveWebContents();
       if (webContents != null) {
@@ -425,6 +476,10 @@ public abstract class CobaltActivity extends Activity {
       mShellManager.destroy();
     }
     mWindowAndroid.destroy();
+    // If the reason is still unknown, it's likely a config change or system kill.
+    if ("Unknown".equals(diagnosticFinishReason)) {
+      diagnosticFinishReason = "ON_DESTROY_UNKNOWN";
+    }
     super.onDestroy();
     getStarboardBridge().onActivityDestroy(this);
   }
@@ -624,7 +679,8 @@ public abstract class CobaltActivity extends Activity {
 
   public void toggleKeepScreenOn(boolean keepOn) {
     if (isKeepScreenOnEnabled != keepOn) {
-      runOnUiThread(new Runnable() {
+      runOnUiThread(
+          new Runnable() {
             @Override
             public void run() {
               if (keepOn) {
@@ -638,5 +694,17 @@ public abstract class CobaltActivity extends Activity {
           });
       isKeepScreenOnEnabled = keepOn;
     }
+  }
+
+  @Override
+  public void onBackPressed() {
+    diagnosticFinishReason = "ON_BACK_PRESSED";
+    super.onBackPressed();
+  }
+
+  @Override
+  public void onLowMemory() {
+    diagnosticFinishReason = "ON_LOW_MEMORY";
+    super.onLowMemory();
   }
 }
