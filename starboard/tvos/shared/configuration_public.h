09fa4d0853e39d78bcf839b629fcab556a476a4a
Raphael Kubo da Costa <kubo@igalia.com>
2025-09-05 12:35:35 +0200

    Add tvos-arm64-simulator Cobalt build config
    This makes it possible to run e.g.
    
        cobalt/build/gn.py -p tvos-arm64-simulator -c debug
    
    and generate `out/tvos-arm64-simulator_debug/` and build the `starboard`
    target.
    
    Note: as in the upstream build, `.gclient` needs `target_os=['ios']` so that
    `gclient sync` fetches all required iOS/tvOS dependencies.
    
    The focus of this commit is on adding all the required scaffolding: setting
    the appropriate GN variables to the right values, adding tvOS configurations
    to `cobalt/build` and `starboard/build`, adding the minimum amount of
    C++ and GN configuration files under `starboard/tvos` and building a small
    number of files under the `starboard` target.
    
    The dependency on the cobalt targets in `//BUILD.gn` has been temporarily
    disabled for tvOS builds because they depend on further changes to build
    cobalt shell as a bundle, which will come later.
    
    Bug: 432823234
    Bug: 442923940

---
+++ starboard/tvos/shared/configuration_public.h
@@ -0,0 +1,39 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// The Starboard configuration for a stub implementation designed to be
+// built on Desktop Linux. Other devices will have real Starboard
+// implementations.
+
+// Other source files should never include this header directly, but should
+// include the generic "starboard/configuration.h" instead.
+
+#ifndef STARBOARD_TVOS_SHARED_CONFIGURATION_PUBLIC_H_
+#define STARBOARD_TVOS_SHARED_CONFIGURATION_PUBLIC_H_
+
+// --- System Header Configuration -------------------------------------------
+
+// Any system headers listed here that are not provided by the platform will be
+// emulated in starboard/types.h.
+
+// Type detection for wchar_t.
+#if defined(__WCHAR_MAX__) && \
+    (__WCHAR_MAX__ == 0x7fffffff || __WCHAR_MAX__ == 0xffffffff)
+#define SB_IS_WCHAR_T_UTF32 1
+#elif defined(__WCHAR_MAX__) && \
+    (__WCHAR_MAX__ == 0x7fff || __WCHAR_MAX__ == 0xffff)
+#define SB_IS_WCHAR_T_UTF16 1
+#endif
+
+#endif  // STARBOARD_TVOS_SHARED_CONFIGURATION_PUBLIC_H_
