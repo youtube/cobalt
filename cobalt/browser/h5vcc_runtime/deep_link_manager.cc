a4a807393e9396cd0be0bbe5f7b130bb27c6b392
Andrew Savage <andrewsavage@google.com>
2025-09-16 18:46:33 +0000

    Add deep links for 3p

--- cobalt/browser/h5vcc_runtime/deep_link_manager.cc
+++ cobalt/browser/h5vcc_runtime/deep_link_manager.cc
@@ -36,6 +36,7 @@ DeepLinkManager* DeepLinkManager::GetInstance() {
 
 void DeepLinkManager::set_deep_link(const std::string& url) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  LOG(INFO) << "Set deep link!!!!!!!!!!!!!";
   deep_link_ = url;
 }
 const std::string& DeepLinkManager::get_deep_link() const {
