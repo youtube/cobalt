c87cf2bfe50d2081d1f61469e9d58e4b2030bf70
Kyujung Youn <kjyoun@google.com>
2025-09-18 16:16:12 -0700

    starboard: Move player classes into flattened namespace
    [go/cobalt-flatten-starboard-namespace](http://go/cobalt-flatten-starboard-namespace)
    
    This change moves the core player and filter-based player classes from the `starboard::shared::starboard::player` and `starboard::shared::starboard::player::filter` namespaces into the top-level `starboard` namespace.
    
    This refactoring is part of the ongoing effort to simplify the Starboard
    namespace structure. It removes unnecessary nesting and makes the player implementation more accessible and consistent with the flattened namespace design.
    
    Bug: 441955897

--- starboard/shared/starboard/player/player_internal.cc
+++ starboard/shared/starboard/player/player_internal.cc
@@ -28,7 +28,7 @@
 #include SB_PLAYER_DMP_WRITER_INCLUDE_PATH
 #endif  // SB_PLAYER_ENABLE_VIDEO_DUMPER
 
-namespace starboard::shared::starboard::player {
+namespace starboard {
 namespace {
 
 using std::placeholders::_1;
@@ -255,4 +255,4 @@ SbPlayerPrivateImpl::~SbPlayerPrivateImpl() {
                << number_of_players_ << " players.";
 }
 
-}  // namespace starboard::shared::starboard::player
+}  // namespace starboard
