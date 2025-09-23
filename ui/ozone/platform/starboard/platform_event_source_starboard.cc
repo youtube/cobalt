5e4f7f8f11b6e32c8462ea75e6168f3b51c13ce3
Yavor Goulishev <yavor@google.com>
2025-09-23 13:41:42 -0700

    Map the SbKey values to DOM keyboard events. (#7268)
    The SbKey is mapped to the 3 representation taken by the ui::KeyEvent:
       1) SbKey to DomKey
       2) SbKey to DomCode
       3) SbKey to KeyboardCode
    
    The fist two represent the current Web starndard, while the 3rd is a
    legacy representation but still in use.
    
    The key modifiers are also propagated from the SbInputData. A simple
    unit test was added to validate that all keys that are covered and that
    the undefined mappings are explicit.
    
    Issue: 371429755
    
    original-hexsha: 8d04f0629965db62f2108ac0bb4aa517ee8238b9

--- ui/ozone/platform/starboard/platform_event_source_starboard.cc
+++ ui/ozone/platform/starboard/platform_event_source_starboard.cc
@@ -26,178 +26,11 @@
 #include "ui/events/keycodes/dom/dom_code.h"
 #include "ui/events/keycodes/dom/dom_key.h"
 #include "ui/events/keycodes/keyboard_code_conversion.h"
+#include "ui/events/keycodes/keyboard_code_conversion_starboard.h"
 
 #include "ui/events/types/event_type.h"
 
 namespace ui {
-
-constexpr auto kSbKeyToDomCodeMap = base::MakeFixedFlatMap<SbKey, ui::DomCode>({
-    // Convenience keys for keyboard support.
-    {kSbKeySpace, ui::DomCode::MEDIA_PLAY_PAUSE},
-    // {kSbKeySpace, ui::DomCode::SPACE}, Should this be space or play/pause
-
-    // Keys which are used by the Cast SDK.
-    {kSbKeyReturn, ui::DomCode::ENTER},
-    {kSbKeySelect, ui::DomCode::SELECT},
-    {kSbKeyUp, ui::DomCode::ARROW_UP},
-    {kSbKeyDown, ui::DomCode::ARROW_DOWN},
-    {kSbKeyLeft, ui::DomCode::ARROW_LEFT},
-    {kSbKeyRight, ui::DomCode::ARROW_RIGHT},
-    {kSbKeyBack, ui::DomCode::BROWSER_BACK},
-
-    // Keys which are used by the Cast SDK when the DPAD UI is enabled.
-    {kSbKeyMediaPlayPause, ui::DomCode::MEDIA_PLAY_PAUSE},
-    {kSbKeyMediaRewind, ui::DomCode::MEDIA_REWIND},
-    {kSbKeyMediaFastForward, ui::DomCode::MEDIA_FAST_FORWARD},
-    {kSbKeyMediaNextTrack, ui::DomCode::MEDIA_TRACK_NEXT},
-    {kSbKeyMediaPrevTrack, ui::DomCode::MEDIA_TRACK_PREVIOUS},
-    {kSbKeyPause, ui::DomCode::MEDIA_PAUSE},
-    {kSbKeyPlay, ui::DomCode::MEDIA_PLAY},
-    {kSbKeyMediaStop, ui::DomCode::MEDIA_STOP},
-
-    // Keys which are not used by the Cast SDK, but are defined in the HDMI
-    // CEC specification.
-    {kSbKeyMenu, ui::DomCode::HOME},
-    {kSbKeyChannelUp, ui::DomCode::CHANNEL_UP},
-    {kSbKeyChannelDown, ui::DomCode::CHANNEL_DOWN},
-    {kSbKeyClosedCaption, ui::DomCode::CLOSED_CAPTION_TOGGLE},
-    {kSbKeyRecord, ui::DomCode::MEDIA_RECORD},
-
-    // 0-9, a-z
-    {kSbKey0, ui::DomCode::DIGIT0},
-    {kSbKey1, ui::DomCode::DIGIT1},
-    {kSbKey2, ui::DomCode::DIGIT2},
-    {kSbKey3, ui::DomCode::DIGIT3},
-    {kSbKey4, ui::DomCode::DIGIT4},
-    {kSbKey5, ui::DomCode::DIGIT5},
-    {kSbKey6, ui::DomCode::DIGIT6},
-    {kSbKey7, ui::DomCode::DIGIT7},
-    {kSbKey8, ui::DomCode::DIGIT8},
-    {kSbKey9, ui::DomCode::DIGIT9},
-    {kSbKeyA, ui::DomCode::US_A},
-    {kSbKeyB, ui::DomCode::US_B},
-    {kSbKeyC, ui::DomCode::US_C},
-    {kSbKeyD, ui::DomCode::US_D},
-    {kSbKeyE, ui::DomCode::US_E},
-    {kSbKeyF, ui::DomCode::US_F},
-    {kSbKeyG, ui::DomCode::US_G},
-    {kSbKeyH, ui::DomCode::US_H},
-    {kSbKeyI, ui::DomCode::US_I},
-    {kSbKeyJ, ui::DomCode::US_J},
-    {kSbKeyK, ui::DomCode::US_K},
-    {kSbKeyL, ui::DomCode::US_L},
-    {kSbKeyM, ui::DomCode::US_M},
-    {kSbKeyN, ui::DomCode::US_N},
-    {kSbKeyO, ui::DomCode::US_O},
-    {kSbKeyP, ui::DomCode::US_P},
-    {kSbKeyQ, ui::DomCode::US_Q},
-    {kSbKeyR, ui::DomCode::US_R},
-    {kSbKeyS, ui::DomCode::US_S},
-    {kSbKeyT, ui::DomCode::US_T},
-    {kSbKeyU, ui::DomCode::US_U},
-    {kSbKeyV, ui::DomCode::US_V},
-    {kSbKeyW, ui::DomCode::US_W},
-    {kSbKeyX, ui::DomCode::US_X},
-    {kSbKeyY, ui::DomCode::US_Y},
-    {kSbKeyZ, ui::DomCode::US_Z},
-
-    // Numpad keys
-    {kSbKeyNumpad0, ui::DomCode::NUMPAD0},
-    {kSbKeyNumpad1, ui::DomCode::NUMPAD1},
-    {kSbKeyNumpad2, ui::DomCode::NUMPAD2},
-    {kSbKeyNumpad3, ui::DomCode::NUMPAD3},
-    {kSbKeyNumpad4, ui::DomCode::NUMPAD4},
-    {kSbKeyNumpad5, ui::DomCode::NUMPAD5},
-    {kSbKeyNumpad6, ui::DomCode::NUMPAD6},
-    {kSbKeyNumpad7, ui::DomCode::NUMPAD7},
-    {kSbKeyNumpad8, ui::DomCode::NUMPAD8},
-    {kSbKeyNumpad9, ui::DomCode::NUMPAD9},
-    {kSbKeyMultiply, ui::DomCode::NUMPAD_MULTIPLY},
-    {kSbKeyAdd, ui::DomCode::NUMPAD_ADD},
-    {kSbKeySubtract, ui::DomCode::NUMPAD_SUBTRACT},
-    {kSbKeyDecimal, ui::DomCode::NUMPAD_DECIMAL},
-    {kSbKeyDivide, ui::DomCode::NUMPAD_DIVIDE},
-    {kSbKeyNumlock, ui::DomCode::NUM_LOCK},
-
-    // function keys
-    {kSbKeyF1, ui::DomCode::F1},
-    {kSbKeyF2, ui::DomCode::F2},
-    {kSbKeyF3, ui::DomCode::F3},
-    {kSbKeyF4, ui::DomCode::F4},
-    {kSbKeyF5, ui::DomCode::F5},
-    {kSbKeyF6, ui::DomCode::F6},
-    {kSbKeyF7, ui::DomCode::F7},
-    {kSbKeyF8, ui::DomCode::F8},
-    {kSbKeyF9, ui::DomCode::F9},
-    {kSbKeyF10, ui::DomCode::F10},
-    {kSbKeyF11, ui::DomCode::F11},
-    {kSbKeyF12, ui::DomCode::F12},
-    {kSbKeyF13, ui::DomCode::F13},
-    {kSbKeyF14, ui::DomCode::F14},
-    {kSbKeyF15, ui::DomCode::F15},
-    {kSbKeyF16, ui::DomCode::F16},
-    {kSbKeyF17, ui::DomCode::F17},
-    {kSbKeyF18, ui::DomCode::F18},
-    {kSbKeyF19, ui::DomCode::F19},
-    {kSbKeyF20, ui::DomCode::F20},
-    {kSbKeyF21, ui::DomCode::F21},
-    {kSbKeyF22, ui::DomCode::F22},
-    {kSbKeyF23, ui::DomCode::F23},
-    {kSbKeyF24, ui::DomCode::F24},
-
-    // Browser keys
-    {kSbKeyBrowserBack, ui::DomCode::BROWSER_BACK},
-    {kSbKeyBrowserForward, ui::DomCode::BROWSER_FORWARD},
-    {kSbKeyBrowserRefresh, ui::DomCode::BROWSER_REFRESH},
-    {kSbKeyBrowserStop, ui::DomCode::BROWSER_STOP},
-    {kSbKeyBrowserSearch, ui::DomCode::BROWSER_SEARCH},
-    {kSbKeyBrowserFavorites, ui::DomCode::BROWSER_FAVORITES},
-    {kSbKeyBrowserHome, ui::DomCode::BROWSER_HOME},
-
-    // Left/Right specific keys
-    {kSbKeyLwin, ui::DomCode::META_LEFT},
-    {kSbKeyRwin, ui::DomCode::META_RIGHT},
-    {kSbKeyLshift, ui::DomCode::SHIFT_LEFT},
-    {kSbKeyRshift, ui::DomCode::SHIFT_RIGHT},
-    {kSbKeyLcontrol, ui::DomCode::CONTROL_LEFT},
-    {kSbKeyRcontrol, ui::DomCode::CONTROL_RIGHT},
-
-    // Oem keys
-    {kSbKeyOemPlus, ui::DomCode::EQUAL},
-    {kSbKeyOemComma, ui::DomCode::COMMA},
-    {kSbKeyOem1, ui::DomCode::SEMICOLON},
-    {kSbKeyOem2, ui::DomCode::SLASH},
-    {kSbKeyOem4, ui::DomCode::BRACKET_LEFT},
-    {kSbKeyOem5, ui::DomCode::BACKSLASH},
-    {kSbKeyOem6, ui::DomCode::BRACKET_RIGHT},
-
-    // Volume keys
-    {kSbKeyVolumeMute, ui::DomCode::VOLUME_MUTE},
-    {kSbKeyVolumeDown, ui::DomCode::VOLUME_DOWN},
-    {kSbKeyVolumeUp, ui::DomCode::VOLUME_UP},
-
-    // Brightness keys
-    {kSbKeyBrightnessDown, ui::DomCode::BRIGHTNESS_DOWN},
-    {kSbKeyBrightnessUp, ui::DomCode::BRIGHTNESS_UP},
-    {kSbKeyKbdBrightnessDown, ui::DomCode::BRIGHTNESS_DOWN},
-    {kSbKeyKbdBrightnessUp, ui::DomCode::BRIGHTNESS_UP},
-
-    {kSbKeyPrior, ui::DomCode::PAGE_UP},
-    {kSbKeyNext, ui::DomCode::PAGE_DOWN},
-    {kSbKeyEnd, ui::DomCode::END},
-    {kSbKeyHome, ui::DomCode::HOME},
-    {kSbKeyPrint, ui::DomCode::PRINT},
-    {kSbKeyExecute, ui::DomCode::OPEN},
-    {kSbKeyInsert, ui::DomCode::INSERT},
-    {kSbKeyDelete, ui::DomCode::DEL},
-    {kSbKeyCapital, ui::DomCode::CAPS_LOCK},
-
-    {kSbKeySleep, ui::DomCode::SLEEP},
-    {kSbKeyPower, ui::DomCode::POWER},
-    {kSbKeyCancel, ui::DomCode::ABORT},
-    {kSbKeyTab, ui::DomCode::TAB},
-    {kSbKeyEscape, ui::DomCode::ESCAPE},
-});
 
 void DeliverEventHandler(std::unique_ptr<ui::Event> ui_event) {
   CHECK(ui::PlatformEventSource::GetInstance());
@@ -249,19 +82,25 @@ void PlatformEventSourceStarboard::HandleEvent(const SbEvent* event) {
       return;
     }
 
-    auto it = kSbKeyToDomCodeMap.find(raw_key);
-    if (it == kSbKeyToDomCodeMap.end()) {
-      return;
-    }
-
-    ui::DomKey dom_key;
-    ui::KeyboardCode key_code;
-    ui::DomCode dom_code = it->second;
-
     int flags = 0;
-    if (!DomCodeToUsLayoutDomKey(dom_code, flags, &dom_key, &key_code)) {
-      return;
+    bool shift = false;
+    if (input_data->key_modifiers & kSbKeyModifiersShift) {
+      flags |= ui::EF_SHIFT_DOWN;
+      shift = true;
     }
+    if (input_data->key_modifiers & kSbKeyModifiersCtrl) {
+      flags |= ui::EF_CONTROL_DOWN;
+    }
+    if (input_data->key_modifiers & kSbKeyModifiersAlt) {
+      flags |= ui::EF_ALT_DOWN;
+    }
+    if (input_data->key_modifiers & kSbKeyModifiersMeta) {
+      flags |= ui::EF_COMMAND_DOWN;
+    }
+
+    ui::DomCode dom_code = ui::SbKeyToDomCode(raw_key);
+    ui::DomKey dom_key = ui::SbKeyToDomKey(raw_key, shift);
+    ui::KeyboardCode key_code = ui::SbKeyToKeyboardCode(raw_key);
 
     // Key press.
     ui::EventType event_type = raw_type == kSbInputEventTypePress
