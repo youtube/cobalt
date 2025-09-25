0e0c2210d753a00776c6a1c938aae18a44ca31d4
Kyujung Youn <kjyoun@google.com>
2025-09-25 10:56:38 -0700

    address gemini comment

--- cobalt/android/apk/app/src/main/java/dev/cobalt/media/MediaCodecBridge.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/media/MediaCodecBridge.java
@@ -713,7 +713,7 @@ class MediaCodecBridge {
     int status = MediaCodecStatus.OK;
     try {
       format = mMediaCodec.get().getOutputFormat();
-    } catch (Exception e) {
+    } catch (IllegalStateException | MediaCodec.CodecException e) {
       Log.e(TAG, "Failed to get output format", e);
       status = MediaCodecStatus.ERROR;
     }
