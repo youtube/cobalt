f06d6bd670d027052087499d8c04ee6dfd9186cb
Kyujung Youn <kjyoun@google.com>
2025-09-27 21:06:21 -0700

    fix build

--- starboard/nplb/maximum_player_configuration_explorer.cc
+++ starboard/nplb/maximum_player_configuration_explorer.cc
@@ -163,7 +163,7 @@ MaximumPlayerConfigurationExplorer::MaximumPlayerConfigurationExplorer(
   SB_DCHECK_GT(max_total_instances_, 0);
   SB_DCHECK(fake_graphics_context_provider_);
   SB_DCHECK_EQ(player_instances_.size(), player_configs_.size());
-  SB_DCHECK_LE(player_configs_.size(), 7)
+  SB_DCHECK_LE(player_configs_.size(), 7U)
       << "Exploring configs with that size may be a time-consuming process.";
   SB_DCHECK_LE(max_instances_per_config_, 7)
       << "Exploring configs with that size may be a time-consuming process.";
@@ -243,7 +243,7 @@ MaximumPlayerConfigurationExplorer::CreatePlayerInstance(
   SbPlayerOutputMode output_mode = config.output_mode;
   const char* key_system = config.key_system;
   SB_DCHECK(video_filename);
-  SB_DCHECK_GT(strlen(video_filename), 0);
+  SB_DCHECK_GT(strlen(video_filename), 0U);
   SB_DCHECK(output_mode == kSbPlayerOutputModeDecodeToTexture ||
             output_mode == kSbPlayerOutputModePunchOut);
   SB_DCHECK(key_system);
