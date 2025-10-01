ec6a542394ef892200f0813e87541ccae48eca99
Miguel Casas <mcasas@chromium.org>
2025-10-01 00:09:56 +0000

    Add a TODO to fold SbPlayerSetBoundsHelper, and write a forgotten TODO-bug#

--- media/starboard/sbplayer_set_bounds_helper.h
+++ media/starboard/sbplayer_set_bounds_helper.h
@@ -31,6 +31,7 @@ namespace media {
 
 class SbPlayerBridge;
 
+// TODO(b/448196546): Fold this class into SbPlayerBridge.
 class SbPlayerSetBoundsHelper {
  public:
   explicit SbPlayerSetBoundsHelper(base::SequencedTaskRunner* task_runner);
