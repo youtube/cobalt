dc9e3b280c3d4043ec53ccf97f0aa8ea2f7db76a
Kyujung Youn <kjyoun@google.com>
2025-10-03 11:05:35 -0700

    Add comment

--- starboard/common/expected.h
+++ starboard/common/expected.h
@@ -1,11 +1,80 @@
+// Copyright 2016 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef STARBOARD_COMMON_EXPECTED_H_
 #define STARBOARD_COMMON_EXPECTED_H_
 
 #include <string>
+#include <string_view>
 #include <utility>
 #include <variant>
 
 #include "starboard/common/log.h"
+
+// This file provides a simple implementation of a value-or-error type, similar
+// to Chromium's `base::expected` or C++23's `std::expected`. It is designed to
+// be a lightweight way to handle return values from functions that can either
+// succeed with a value or fail with an error message.
+//
+// Example Usage:
+//
+//   // Function that returns a value or an error.
+//   Expected<int> ParseInt(const std::string& s) {
+//     try {
+//       return std::stoi(s);
+//     } catch (const std::invalid_argument&) {
+//       return Unexpected("Invalid integer format");
+//     }
+//   }
+//
+//   // Function that returns void or an error.
+//   Expected<void> DoSomething() {
+//     if (/* condition */) {
+//       return {};  // Success
+//     }
+//     return Unexpected("Failed to do something");
+//   }
+//
+//   void Process() {
+//     Expected<int> result = ParseInt("123");
+//     if (result.ok()) {
+//       SB_LOG(INFO) << "Parsed value: " << result.value();
+//     } else {
+//       SB_LOG(ERROR) << "Error: " << result.error_message();
+//     }
+//
+//     Expected<void> void_result = DoSomething();
+//     if (!void_result) {
+//       SB_LOG(ERROR) << "DoSomething failed: " << void_result.error_message();
+//     }
+//   }
+//
+// For improved readability, you can also use the static factory methods:
+//
+//   Expected<int> GetPositive(int n) {
+//     if (n > 0) {
+//       return Expected<int>::Success(n);
+//     }
+//     return Expected<int>::Failure("Number must be positive");
+//   }
+//
+//   Expected<void> CheckSystem() {
+//     if (/* system is ok */) {
+//       return Expected<void>::Success();
+//     }
+//     return Expected<void>::Failure("System check failed");
+//   }
 
 // A simple wrapper for error messages to disambiguate from success types.
 struct Unexpected {
