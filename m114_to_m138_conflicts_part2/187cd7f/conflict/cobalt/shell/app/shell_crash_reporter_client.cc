ba01129a6768d8c660af0c90655c5882da43afbd
Brian Ting <brianting@google.com>
2025-10-07 22:45:38 +0000

    Update m114 to m138 part 2

---
+++ m114_to_m138_conflicts_part2/187cd7f/conflict/cobalt/shell/app/shell_crash_reporter_client.cc
@@ -0,0 +1,100 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "cobalt/shell/app/shell_crash_reporter_client.h"
+
+#include <string>
+#include <utility>
+
+#include "base/command_line.h"
+#include "base/files/file_path.h"
+#include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
+#include "content/public/common/content_switches.h"
+#include "content/shell/common/shell_switches.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#include "content/shell/android/shell_descriptors.h"
+#endif
+
+namespace content {
+
+ShellCrashReporterClient::ShellCrashReporterClient() {}
+ShellCrashReporterClient::~ShellCrashReporterClient() {}
+
+<<<<<<< HEAD
+#if BUILDFLAG(IS_WIN)
+void ShellCrashReporterClient::GetProductNameAndVersion(
+    const std::wstring& exe_path,
+    std::wstring* product_name,
+    std::wstring* version,
+    std::wstring* special_build,
+    std::wstring* channel_name) {
+  *product_name = L"content_shell";
+  *version = base::ASCIIToWide(CONTENT_SHELL_VERSION);
+  *special_build = std::wstring();
+  *channel_name = std::wstring();
+}
+#endif
+
+#if BUILDFLAG(IS_POSIX) && !BUILDFLAG(IS_MAC)
+=======
+#if BUILDFLAG(IS_POSIX)
+void ShellCrashReporterClient::GetProductNameAndVersion(
+    const char** product_name,
+    const char** version) {
+  *product_name = "content_shell";
+  *version = CONTENT_SHELL_VERSION;
+}
+
+void ShellCrashReporterClient::GetProductNameAndVersion(
+    std::string* product_name,
+    std::string* version,
+    std::string* channel) {
+  *product_name = "content_shell";
+  *version = CONTENT_SHELL_VERSION;
+  *channel = "";
+}
+
+>>>>>>> 64580f1af82 (Remove unsupported platforms from cobalt/shell/app (#6386))
+base::FilePath ShellCrashReporterClient::GetReporterLogFilename() {
+  return base::FilePath(FILE_PATH_LITERAL("uploads.log"));
+}
+#endif
+
+bool ShellCrashReporterClient::GetCrashDumpLocation(base::FilePath* crash_dir) {
+  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kCrashDumpsDir))
+    return false;
+  base::FilePath crash_directory =
+      base::CommandLine::ForCurrentProcess()->GetSwitchValuePath(
+          switches::kCrashDumpsDir);
+  *crash_dir = std::move(crash_directory);
+  return true;
+}
+
+void ShellCrashReporterClient::GetProductInfo(ProductInfo* product_info) {
+  product_info->product_name = "content_shell";
+  product_info->version = CONTENT_SHELL_VERSION;
+}
+
+bool ShellCrashReporterClient::EnableBreakpadForProcess(
+    const std::string& process_type) {
+  return process_type == switches::kRendererProcess ||
+         process_type == switches::kPpapiPluginProcess ||
+         process_type == switches::kZygoteProcess ||
+         process_type == switches::kGpuProcess;
+}
+
+}  // namespace content
---
+++ m114_to_m138_conflicts_part2/187cd7f/resolved/cobalt/shell/app/shell_crash_reporter_client.cc
@@ -0,0 +1,66 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "cobalt/shell/app/shell_crash_reporter_client.h"
+
+#include <string>
+#include <utility>
+
+#include "base/command_line.h"
+#include "base/files/file_path.h"
+#include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
+#include "content/public/common/content_switches.h"
+#include "content/shell/common/shell_switches.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#include "content/shell/android/shell_descriptors.h"
+#endif
+
+namespace content {
+
+ShellCrashReporterClient::ShellCrashReporterClient() {}
+ShellCrashReporterClient::~ShellCrashReporterClient() {}
+
+#if BUILDFLAG(IS_POSIX)
+base::FilePath ShellCrashReporterClient::GetReporterLogFilename() {
+  return base::FilePath(FILE_PATH_LITERAL("uploads.log"));
+}
+#endif
+
+bool ShellCrashReporterClient::GetCrashDumpLocation(base::FilePath* crash_dir) {
+  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kCrashDumpsDir))
+    return false;
+  base::FilePath crash_directory =
+      base::CommandLine::ForCurrentProcess()->GetSwitchValuePath(
+          switches::kCrashDumpsDir);
+  *crash_dir = std::move(crash_directory);
+  return true;
+}
+
+void ShellCrashReporterClient::GetProductInfo(ProductInfo* product_info) {
+  product_info->product_name = "content_shell";
+  product_info->version = CONTENT_SHELL_VERSION;
+}
+
+bool ShellCrashReporterClient::EnableBreakpadForProcess(
+    const std::string& process_type) {
+  return process_type == switches::kRendererProcess ||
+         process_type == switches::kPpapiPluginProcess ||
+         process_type == switches::kZygoteProcess ||
+         process_type == switches::kGpuProcess;
+}
+
+}  // namespace content
