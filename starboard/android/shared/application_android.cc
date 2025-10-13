21a3e04a8f830c0bda86be164bcf181e840a0c7a
Kyujung Youn <kjyoun@google.com>
2025-10-13 16:22:04 -0700

    android: Migrate canPlayType and dateTimeConfigurationChanged to JNI generator (#7506)
    Migrates the JNI bindings for `nativeDateTimeConfigurationChanged` and
    `nativeCanPlayType` to utilize the Chromium JNI generator. This change
    replaces the manual JNI boilerplate with generated C++ wrappers.
    
    The Java code now uses the `@JNINamespace` and `@NativeMethods`
    annotations, and the corresponding C++ code in `application_android.cc`
    implements the generated JNI functions. This standardizes the JNI
    interaction, reduces manual error-prone code, and improves the
    maintainability of the Android JNI layer.
    
    #vibe-coded
    
    Bug: 450024477

--- starboard/android/shared/application_android.cc
+++ starboard/android/shared/application_android.cc
@@ -25,23 +25,29 @@
 #include <vector>
 
 #include "base/android/jni_android.h"
-#include "starboard/extension/accessibility.h"
-
+#include "base/android/jni_string.h"
 #include "starboard/android/shared/file_internal.h"
 #include "starboard/android/shared/jni_env_ext.h"
-#include "starboard/android/shared/jni_utils.h"
 #include "starboard/android/shared/window_internal.h"
 #include "starboard/common/log.h"
 #include "starboard/common/string.h"
 #include "starboard/common/time.h"
 #include "starboard/event.h"
+#include "starboard/extension/accessibility.h"
 #include "starboard/key.h"
 #include "starboard/media.h"
 #include "starboard/shared/starboard/audio_sink/audio_sink_internal.h"
 
+#include "cobalt/android/jni_headers/CobaltSystemConfigChangeReceiver_jni.h"
+#include "cobalt/android/jni_headers/HTMLMediaElementExtension_jni.h"
+
 namespace starboard {
 
+using base::android::ConvertJavaStringToUTF8;
+using base::android::ConvertUTF8ToJavaString;
+using base::android::JavaParamRef;
 using base::android::ScopedJavaGlobalRef;
+using base::android::ScopedJavaLocalRef;
 
 // TODO(cobalt, b/378708359): Remove this dummy init.
 void stubSbEventHandle(const SbEvent* event) {
@@ -83,27 +89,23 @@ ApplicationAndroid::~ApplicationAndroid() {
   JniOnThreadShutdown();
 }
 
-extern "C" SB_EXPORT_PLATFORM void
-Java_dev_cobalt_coat_CobaltSystemConfigChangeReceiver_nativeDateTimeConfigurationChanged(
-    JNIEnv* env,
-    jobject jcaller) {
+void JNI_CobaltSystemConfigChangeReceiver_DateTimeConfigurationChanged(
+    JNIEnv* env) {
   // TODO(cobalt, b/378705729): Make sure tzset() is called on the right thread.
   // Set the timezone to allow SbTimeZoneGetName() to return updated timezone.
   tzset();
 }
 
-extern "C" SB_EXPORT_PLATFORM jstring
-Java_dev_cobalt_coat_javabridge_HTMLMediaElementExtension_nativeCanPlayType(
+ScopedJavaLocalRef<jstring> JNI_HTMLMediaElementExtension_CanPlayType(
     JNIEnv* env,
-    jobject jcaller,
-    jstring j_mime_type,
-    jstring j_key_system) {
+    const JavaParamRef<jstring>& j_mime_type,
+    const JavaParamRef<jstring>& j_key_system) {
   std::string mime_type, key_system;
   if (j_mime_type) {
-    mime_type = JniGetStringStandardUTFOrAbort(env, j_mime_type);
+    mime_type = ConvertJavaStringToUTF8(env, j_mime_type);
   }
   if (j_key_system) {
-    key_system = JniGetStringStandardUTFOrAbort(env, j_key_system);
+    key_system = ConvertJavaStringToUTF8(env, j_key_system);
   }
   SbMediaSupportType support_type =
       SbMediaCanPlayMimeAndKeySystem(mime_type.c_str(), key_system.c_str());
@@ -121,7 +123,7 @@ Java_dev_cobalt_coat_javabridge_HTMLMediaElementExtension_nativeCanPlayType(
   }
   SB_LOG(INFO) << __func__ << " (" << mime_type << ", " << key_system
                << ") --> " << ret;
-  return JniNewStringStandardUTFOrAbort(env, ret);
+  return ConvertUTF8ToJavaString(env, ret);
 }
 
 Application::Event* ApplicationAndroid::GetNextEvent() {
