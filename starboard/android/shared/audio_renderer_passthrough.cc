d77eaa47c63e50b5c62896e3dbb4c71b7a2f47bc
Kyujung Youn <kjyoun@google.com>
2025-10-14 15:39:31 -0700

    android: Remove deprecated ScopedLocalJavaRef  utility (#7533)
    The custom `starboard::ScopedLocalJavaRef` located in
    `starboard/android/shared/jni_utils.h` is a legacy helper, which is
    completely replaced with `base::android::ScopedJavaLocalRef` and used no
    more.
    
    This commit replaces usage of legacy helpers with the modern, standard
    `base/android` equivalents
    
    Bug: 450024477
    
    original-hexsha: 9658bd25d13031579598bad329190bda812093d5

--- starboard/android/shared/audio_renderer_passthrough.cc
+++ starboard/android/shared/audio_renderer_passthrough.cc
@@ -19,8 +19,6 @@
 
 #include "starboard/android/shared/audio_decoder.h"
 #include "starboard/android/shared/audio_decoder_passthrough.h"
-#include "starboard/android/shared/jni_env_ext.h"
-#include "starboard/android/shared/jni_utils.h"
 #include "starboard/common/check_op.h"
 #include "starboard/common/string.h"
 #include "starboard/common/time.h"
