a46f0e792b4ec4e26340fc436ef6d8deeb5489cd
Kyujung Youn <kjyoun@google.com>
2025-10-13 21:36:22 -0700

    additional comment

--- media/gpu/android/media_codec_video_decoder.cc
+++ media/gpu/android/media_codec_video_decoder.cc
@@ -8,7 +8,7 @@
 
 #include "base/android/build_info.h"
 #include "base/command_line.h"
-#include "base/functional/bind.h"
+#include "base/functional/bind.h
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
