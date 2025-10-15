86cc4b42d87f6b0ff9d4b3b7c8a505b740a25840
Kyujung Youn <kjyoun@google.com>
2025-10-15 14:59:26 -0700

    Replace stdint.h with cstdint in data_source_reader.cc

--- media/starboard/progressive/data_source_reader.cc
+++ media/starboard/progressive/data_source_reader.cc
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <stdint.h>
-
 #include "media/starboard/progressive/data_source_reader.h"
+
+#include <cstdint>
 
 namespace media {
 
