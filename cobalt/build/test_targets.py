3cd9296b9883f2d0b5266b7f27e2c561688d6cff
Oscar Vestlie <oxv@google.com>
2025-10-21 09:00:04 +0000

    Disable failing linux test suites
    Bug: 418842688

--- cobalt/build/test_targets.py
+++ cobalt/build/test_targets.py
@@ -30,7 +30,8 @@ _ALLOW_TESTS = {
     '//base:base_perftests',
     '//base:base_unittests',
     '//cobalt/renderer:renderer_browsertests',
-    '//cobalt:cobalt_unittests',
+    # TODO: b/418842688 - Broken due to missing files.
+    # '//cobalt:cobalt_unittests',
     # TODO: b/418842688 - Disabled temporarily.
     # '//gpu/gles2_conform_support:gles2_conform_test',
     '//mojo:mojo_perftests',
