d8f1ff604e256d2d59cb78775fbbf3cf74b83635
esmael <aee@google.com>
2025-10-22 13:34:28 -0700

    Revert "Revert flush local storage (#7669) and (#7587) (#7682)"
    This reverts commit 31d98c0cf01d41ad1e809d70169fc5a66b67a1a9.

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
@@ -53,6 +53,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 import org.chromium.base.CommandLine;
+import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.NativeMethods;
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.base.memory.MemoryPressureMonitor;
@@ -68,6 +70,7 @@ import org.chromium.ui.base.IntentRequestTracker;
 import org.chromium.net.NetworkChangeNotifier;
 
 /** Native activity that has the required JNI methods called by the Starboard implementation. */
+@JNINamespace("cobalt")
 public abstract class CobaltActivity extends Activity {
   private static final String URL_ARG = "--url=";
   private static final String META_DATA_APP_URL = "cobalt.APP_URL";
@@ -411,6 +414,15 @@ public abstract class CobaltActivity extends Activity {
       webContents.updateWebContentsVisibility(Visibility.VISIBLE);
     }
     MemoryPressureMonitor.INSTANCE.enablePolling(false);
+  }
+
+  @Override
+  protected void onPause() {
+    WebContents webContents = getActiveWebContents();
+    if (webContents != null) {
+      CobaltActivityJni.get().flushCookiesAndLocalStorage();
+    }
+    super.onPause();
   }
 
   @Override
