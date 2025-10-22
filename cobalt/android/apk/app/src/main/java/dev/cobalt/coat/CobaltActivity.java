14655418145de728b052ca86db9581b0c5e82707
aee <aee@google.com>
2025-10-22 07:56:47 -0700

    [Android] Flush cookies and local storage when activity is paused (#7587)
    Similar to c25 which flushes cookies and localStorage on the cobalt
    freeze event, this PR adds flushing cookies and localStorage to the
    Android activity onPause() handler.
    
    In addition to this, flush to cookies and localStorage is performed when
    h5vcc.system.exit() is called in support of YTS tests. In production the
    flush may be redundant but will act as a no-op if it is.
    
    Issue: 371433332

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
@@ -65,8 +65,11 @@ import org.chromium.content_public.browser.Visibility;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.base.ActivityWindowAndroid;
 import org.chromium.ui.base.IntentRequestTracker;
+import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.NativeMethods;
 
 /** Native activity that has the required JNI methods called by the Starboard implementation. */
+@JNINamespace("cobalt")
 public abstract class CobaltActivity extends Activity {
   private static final String URL_ARG = "--url=";
   private static final String META_DATA_APP_URL = "cobalt.APP_URL";
@@ -466,6 +469,15 @@ public abstract class CobaltActivity extends Activity {
   }
 
   @Override
+  protected void onPause() {
+    WebContents webContents = getActiveWebContents();
+    if (webContents != null) {
+      CobaltActivityJni.get().flushCookiesAndLocalStorage();
+    }
+    super.onPause();
+  }
+
+  @Override
   protected void onStop() {
     getStarboardBridge().onActivityStop(this);
     super.onStop();
@@ -791,5 +803,10 @@ public abstract class CobaltActivity extends Activity {
   public void onLowMemory() {
     diagnosticFinishReason = "ON_LOW_MEMORY";
     super.onLowMemory();
+  }
+
+  @NativeMethods
+  interface Natives {
+    void flushCookiesAndLocalStorage();
   }
 }
