fbe9d5ac8fa8ec4d9db58e715fcefc5a997681da
Madhura Jayaraman <madjay@google.com>
2025-10-22 13:16:48 -0700

    fix

--- third_party/musl/src/exit/assert.c
+++ third_party/musl/src/exit/assert.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct _IO_FILE {};
-struct _IO_FILE __stderr_FILE;
 
 _Noreturn void __assert_fail(const char *expr, const char *file, int line, const char *func)
 {
