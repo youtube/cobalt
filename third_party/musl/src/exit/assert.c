55f1f0e15585309a0b69b555d2f3388dde1d4b0f
Madhura Jayaraman <madjay@google.com>
2025-10-22 12:07:37 -0700

    hack

--- third_party/musl/src/exit/assert.c
+++ third_party/musl/src/exit/assert.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+typedef struct _IO_FILE {};
+struct _IO_FILE __stderr_FILE;
 
 _Noreturn void __assert_fail(const char *expr, const char *file, int line, const char *func)
 {
