5e76293247a7ee15f3f6c9283f00cb76c17ee55f
Andrew Savage <andrewsavage@google.com>
2025-10-22 15:51:35 +0000

    Fix references to TestTraceProcessorImpl

--- cc/metrics/compositor_frame_reporting_controller_unittest.cc
+++ cc/metrics/compositor_frame_reporting_controller_unittest.cc
@@ -2148,6 +2148,7 @@ TEST_F(CompositorFrameReportingControllerTest, MainFrameBeforeCommit) {
       CompositorFrameReportingController::PipelineStage::kActivate));
 }
 
+#if !BUILDFLAG(IS_COBALT_HERMETIC_BUILD)
 // Glossary of acronyms used in the tests below.
 // AMF - Activate Main Frame
 // AbMF - AbortMainFrame
@@ -2882,6 +2883,7 @@ TEST_F(CompositorFrameReportingControllerTest, VsyncIntervalArg) {
                                   std::vector<std::string>{"8", "1"},
                                   std::vector<std::string>{"32", "1"}));
 }
+#endif
 
 }  // namespace
 }  // namespace cc
