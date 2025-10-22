4e0cb7ab699874a2af327e055593ec1784583704
Yijia Zhang <yijiazhang@google.com>
2025-10-21 22:28:22 -0700

    Add opaque config data to experiment config (#7063)
    Bug: 442903190
    
    This PR adds three new fields - acgive_config_data, latest_config_hash,
    and finch_parameters and their respective getters/setters. Also removes
    exp_ids from the experiment config. These changes are proposed in Finch
    Detailed Design - Extended
    (https://docs.google.com/document/d/1my-OXArZ7B-a7X1RCKSm0PMKZPmq6JZP-T-9bXeSeUM)
    
    (cherry picked from commit 2e92a125e3a2051dc014597e102c190fc947a2ec)

--- cobalt/browser/experiments/experiment_config_manager_unittest.cc
+++ cobalt/browser/experiments/experiment_config_manager_unittest.cc
@@ -32,14 +32,23 @@ class ExperimentConfigManagerTest : public testing::Test {
     pref_service_ = std::make_unique<TestingPrefServiceSimple>();
     pref_service_->registry()->RegisterIntegerPref(
         variations::prefs::kVariationsCrashStreak, 0);
+    pref_service_->registry()->RegisterStringPref(
+        kExperimentConfigActiveConfigData, std::string());
     pref_service_->registry()->RegisterDictionaryPref(
         kExperimentConfigFeatures);
     pref_service_->registry()->RegisterDictionaryPref(
         kExperimentConfigFeatureParams);
-    pref_service_->registry()->RegisterListPref(kExperimentConfigExpIds);
+    pref_service_->registry()->RegisterStringPref(kSafeConfigActiveConfigData,
+                                                  std::string());
     pref_service_->registry()->RegisterDictionaryPref(kSafeConfigFeatures);
     pref_service_->registry()->RegisterDictionaryPref(kSafeConfigFeatureParams);
+<<<<<<< HEAD
     pref_service_->registry()->RegisterListPref(kSafeConfigExpIds);
+=======
+    pref_service_->registry()->RegisterDictionaryPref(kFinchParameters);
+    pref_service_->registry()->RegisterTimePref(
+        variations::prefs::kVariationsLastFetchTime, base::Time());
+>>>>>>> 2e92a125e3a (Add opaque config data to experiment config (#7063))
     experiment_config_manager_ =
         std::make_unique<ExperimentConfigManager>(pref_service_.get());
   }
--- cobalt/browser/global_features.cc
+++ cobalt/browser/global_features.cc
@@ -38,11 +38,11 @@ constexpr base::FilePath::CharType kMetricsConfigFilename[] =
 GlobalFeatures::GlobalFeatures() {
   CreateExperimentConfig();
   CreateMetricsServices();
-  // InitializeActiveExperimentIds needs ExperimentConfigManager to determine
-  // the experiment config type.
+  // InitializeActiveConfigData() needs ExperimentConfigManager to determine the
+  // experiment config type.
   experiment_config_manager_ =
       std::make_unique<ExperimentConfigManager>(experiment_config_.get());
-  InitializeActiveExperimentIds();
+  InitializeActiveConfigData();
 }
 
 // static
@@ -138,7 +138,7 @@ void GlobalFeatures::CreateMetricsLocalState() {
   metrics_local_state_ = pref_service_factory.Create(std::move(pref_registry));
 }
 
-void GlobalFeatures::InitializeActiveExperimentIds() {
+void GlobalFeatures::InitializeActiveConfigData() {
   DCHECK(experiment_config_);
   DCHECK(experiment_config_manager_);
   auto experiment_config_type =
@@ -146,26 +146,36 @@ void GlobalFeatures::InitializeActiveExperimentIds() {
   if (experiment_config_type == ExperimentConfigType::kEmptyConfig) {
     return;
   }
-  const base::Value::List& experiments = experiment_config_->GetList(
+
+  active_config_data_ = experiment_config_->GetString(
       (experiment_config_type == ExperimentConfigType::kSafeConfig)
-          ? kSafeConfigExpIds
-          : kExperimentConfigExpIds);
-  active_experiment_ids_.reserve(experiments.size());
-  for (const auto& experiment_id : experiments) {
-    active_experiment_ids_.push_back(experiment_id.GetInt());
-  }
+          ? kSafeConfigActiveConfigData
+          : kExperimentConfigActiveConfigData);
 }
 
 // static
 void GlobalFeatures::RegisterPrefs(PrefRegistrySimple* registry) {
   registry->RegisterDictionaryPref(kExperimentConfig);
+  registry->RegisterStringPref(kExperimentConfigActiveConfigData,
+                               std::string());
   registry->RegisterDictionaryPref(kExperimentConfigFeatures);
   registry->RegisterDictionaryPref(kExperimentConfigFeatureParams);
+<<<<<<< HEAD
   registry->RegisterListPref(kExperimentConfigExpIds);
+=======
+  registry->RegisterDictionaryPref(kFinchParameters);
+  registry->RegisterStringPref(kLatestConfigHash, std::string());
+>>>>>>> 2e92a125e3a (Add opaque config data to experiment config (#7063))
   registry->RegisterDictionaryPref(kSafeConfig);
+  registry->RegisterStringPref(kSafeConfigActiveConfigData, std::string());
   registry->RegisterDictionaryPref(kSafeConfigFeatures);
   registry->RegisterDictionaryPref(kSafeConfigFeatureParams);
+<<<<<<< HEAD
   registry->RegisterListPref(kSafeConfigExpIds);
+=======
+  registry->RegisterTimePref(variations::prefs::kVariationsLastFetchTime,
+                             base::Time(), PrefRegistry::LOSSY_PREF);
+>>>>>>> 2e92a125e3a (Add opaque config data to experiment config (#7063))
   metrics::MetricsService::RegisterPrefs(registry);
 }
 
