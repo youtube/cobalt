e2c4a7eec5d6415b392cde63e5570435800c5bfc
Colin Liang <colinliang@google.com>
2025-10-24 15:42:47 -0700

    Manually apply PR #7661
    Bug: 452059316
    Change-Id: I2697c74fd54319c66ac61580d32af845216356e5

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
@@ -94,7 +94,6 @@ public abstract class CobaltActivity extends Activity {
   private Intent mLastSentIntent;
   private String mStartupUrl;
   private IntentRequestTracker mIntentRequestTracker;
-  protected Boolean shouldSetJNIPrefix = true;
   // Tracks whether we should reload the page on resume, to re-trigger a network error dialog.
   protected Boolean mShouldReloadOnResume = false;
   // Tracks the status of the FLAG_KEEP_SCREEN_ON window flag.
@@ -114,7 +113,7 @@ public abstract class CobaltActivity extends Activity {
       }
       CommandLineOverrideHelper.getFlagOverrides(
           new CommandLineOverrideHelper.CommandLineOverrideHelperParams(
-              shouldSetJNIPrefix, VersionInfo.isOfficialBuild(), commandLineArgs));
+              VersionInfo.isOfficialBuild(), commandLineArgs));
     }
 
     DeviceUtils.updateDeviceSpecificUserAgentSwitch(this);
