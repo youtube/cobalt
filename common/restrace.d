05c9033cf303757f9a7272cea19bba125bc0f2f1
Hao Zheng <haozheng@google.com>
2025-10-24 08:10:41 +0000

    Revert "Override ContentUtilityClient in ShellMainTestDelegate"
    This reverts commit 5a18064c542881caee856869fe3782d02218bf4d.

--- common/restrace.d
+++
@@ -1,9 +0,0 @@
-restrace.o restrace.d : ../third_party/icu/source/common/restrace.cpp \
- ../third_party/icu/source/common/unicode/utypes.h \
- ../third_party/icu/source/common/unicode/umachine.h \
- ../third_party/icu/source/common/unicode/ptypes.h \
- ../third_party/icu/source/common/unicode/platform.h \
- ../third_party/icu/source/common/unicode/uconfig.h \
- ../third_party/icu/source/common/unicode/uvernum.h \
- ../third_party/icu/source/common/unicode/urename.h \
- ../third_party/icu/source/common/unicode/uversion.h
--- i18n/windtfmt.d
+++
@@ -1,9 +0,0 @@
-windtfmt.o windtfmt.d : ../third_party/icu/source/i18n/windtfmt.cpp \
- ../third_party/icu/source/common/unicode/utypes.h \
- ../third_party/icu/source/common/unicode/umachine.h \
- ../third_party/icu/source/common/unicode/ptypes.h \
- ../third_party/icu/source/common/unicode/platform.h \
- ../third_party/icu/source/common/unicode/uconfig.h \
- ../third_party/icu/source/common/unicode/uvernum.h \
- ../third_party/icu/source/common/unicode/urename.h \
- ../third_party/icu/source/common/unicode/uversion.h
