5c2cd2bd70d700cb0e794b3709ef5e9b0550477c
Kyujung Youn <kjyoun@google.com>
2025-10-25 10:22:36 -0700

    media: Decouple decoder memory budget from DecoderBufferAllocator

---
+++ media/starboard/decoder_buffer_memory_info.cc
@@ -0,0 +1,22 @@
+#include "media/starboard/decoder_buffer_memory_info.h"
+
+#include "media/base/video_codecs.h"
+#include "media/starboard/starboard_utils.h"
+#include "starboard/media.h"
+#include "ui/gfx/geometry/size.h"
+
+namespace media {
+
+int GetDecoderAudioBufferLimitBytes() {
+  return SbMediaGetAudioBufferBudget();
+}
+
+int GetDecoderVideoBufferLimitBytes(VideoCodec codec,
+                                    const gfx::Size& resolution,
+                                    int bits_per_pixel) {
+  return SbMediaGetVideoBufferBudget(MediaVideoCodecToSbMediaVideoCodec(codec),
+                                     resolution.width(), resolution.height(),
+                                     bits_per_pixel);
+}
+
+}  // namespace media
