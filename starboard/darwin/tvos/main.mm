6d4c1e36eb084720e4b40f8b057179655910e4a0
fayev-g <fayev@google.com>
2025-10-28 14:49:25 -0700

    Cherry-pick #6514 `Add tvOS code` into main (m138) (#7730)
    Commit https://github.com/youtube/cobalt/pull/6514 moved tvOS platform
    files into github C25. Apply this commit to main (m138), which now
    contains some upstreamed tvOS chromium support.
    
    Pull out cpplint change from pre-commit-config.yaml because it requires
    config changes to work in C26. Will follow up with tracking bug
    b/455923882.
    
    Bug: 454925565

---
+++ starboard/darwin/tvos/main.mm
@@ -0,0 +1,36 @@
+// Copyright 2017 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import <UIKit/UIKit.h>
+
+#import "starboard/shared/uikit/uikit.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+// BreakpadSetAnnotationsCallback should be in sync with
+// starboard/shared/uikit/crash_handler.h.
+typedef void (*BreakpadSetAnnotationsCallback)(const char* key,
+                                               const char* value);
+#ifdef __cplusplus
+}
+#endif
+
+BreakpadSetAnnotationsCallback gBreakpadSetAnnotationsCallback = nullptr;
+
+int main(int argc, char* argv[]) {
+  @autoreleasepool {
+    return SBDUIKitApplicationMain(argc, argv);
+  }
+}
