654581dfe42bf4962d556c92c5cc8b690e48b632
Miguel Casas-Sanchez <mcasas@chromium.org>
2025-10-28 20:19:29 -0700

    Isolate code needing Android API > 24, don't build it if the SDK is not such version.
    Same for media_unittests, via dependency on //gpu/command_buffer/service/.

--- base/android/android_image_reader_compat.cc
+++ base/android/android_image_reader_compat.cc
@@ -4,14 +4,18 @@
 
 #include "base/android/android_image_reader_compat.h"
 
+#include "build/build_config.h"
+
 namespace base {
 namespace android {
 
 bool EnableAndroidImageReader() {
+#if !BUILDFLAG(IS_COBALT)
   // Currently we want to enable AImageReader only for android P+ devices.
   if (__builtin_available(android 28, *)) {
     return true;
   }
+#endif
   return false;
 }
 
