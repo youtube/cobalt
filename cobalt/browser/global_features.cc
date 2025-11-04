c94051c28c3b3cfbe7eab1746c5db92c64932bdd
Jammel Yeboah <jammel@google.com>
2025-11-04 00:41:56 +0000

    comment nit

--- cobalt/browser/global_features.cc
+++ cobalt/browser/global_features.cc
@@ -155,12 +155,12 @@ void GlobalFeatures::InitializeActiveConfigData() {
           : kExperimentConfigActiveConfigData);
 }
 
+// Explicitly shuts down the metrics service. This is to ensure the
+// CobaltMetricsServiceClient destructor is called, which logs a clean
+// shutdown. The specific shutdown order here is required to nullify
+// a raw pointer and prevent a use-after-free crash
+// that would otherwise occur on exit.
 void GlobalFeatures::Shutdown() {
-  // Explicitly shuts down the metrics services. This is to ensure the
-  // CobaltMetricsServiceClient destructor is called, which logs a clean
-  // shutdown. The specific shutdown order here is required to nullify
-  // a raw pointer and prevent a use-after-free crash
-  // that would otherwise occur on exit.
   metrics_services_manager_client_->ClearMetricsServiceClient();
   metrics_services_manager_.reset();
   metrics_services_manager_client_ = nullptr;
