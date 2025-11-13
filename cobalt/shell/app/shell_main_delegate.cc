67599662fdb99931151970c9e2b859a8c1beb012
Raphael Kubo da Costa <kubo@igalia.com>
2025-11-13 07:03:13 +0100

    [tvOS] cobalt shell: Move kContentShellUserDataDir to shell_main_delegate.cc (#7816)
    This is similar to #7792 but applied to Cobalt Shell: we needed that
    backport first due to the changes made to `content/app/content_main.cc`.
    
    Authored by @jkim-julie.
    
    Bug: 432824509
    
    ---------
    
    Co-authored-by: Julie Jeongeun Kim <jkim@igalia.com>

--- cobalt/shell/app/shell_main_delegate.cc
+++ cobalt/shell/app/shell_main_delegate.cc
@@ -15,6 +15,8 @@
 #include "cobalt/shell/app/shell_main_delegate.h"
 
 #include <iostream>
+#include <memory>
+#include <string>
 #include <tuple>
 #include <utility>
 #include <variant>
@@ -103,7 +105,7 @@ enum class LoggingDest {
 
 #if !BUILDFLAG(IS_ANDROIDTV)
 base::LazyInstance<content::ShellCrashReporterClient>::Leaky
-    g_shell_crash_client = LAZY_INSTANCE_INITIALIZER;
+    g_shell_crash_client = LAZY_INSTANCE_INITIALIZER;  // NOLINT
 #endif
 
 void InitLogging(const base::CommandLine& command_line) {
