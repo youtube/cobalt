youtube/cobalt

commit 8711f0869a4541d3fda029936bdccaffd28f985a

media: Allow StarboardRenderer to RequestOverlayInfo from WebMediaPlayer (#6399)
Currently, StarboardRenderer does not have access to
RequestOverlayInfoCB or OverlayInfo's routing token, which is needed to
create AndroidOverlay in StarboardRenderer. This PR allows
StarboardRenderer to RequestOverlayInfo from WebMediaPlayer via
StarboardRendererClientExtension.

Issue: 429224482

media/mojo/clients/starboard/starboard_renderer_client.cc
@@ -15,6 +15,7 @@
 #include "media/mojo/clients/starboard/starboard_renderer_client.h"
 
 #include "base/functional/bind.h"
+#include "base/task/bind_post_task.h"
 #include "base/time/time.h"
 #include "base/unguessable_token.h"
 #include "media/base/media_log.h"
@@ -37,7 +38,8 @@ StarboardRendererClient::StarboardRendererClient(
     mojo::PendingRemote<RendererExtension> pending_renderer_extension,
     mojo::PendingReceiver<ClientExtension> client_extension_receiver,
     BindHostReceiverCallback bind_host_receiver_callback,
-    GpuVideoAcceleratorFactories* gpu_factories)
+    GpuVideoAcceleratorFactories* gpu_factories,
+    RequestOverlayInfoCB request_overlay_info_cb)
     : MojoRendererWrapper(std::move(mojo_renderer)),
       media_task_runner_(media_task_runner),
       media_log_(std::move(media_log)),
@@ -47,7 +49,8 @@ StarboardRendererClient::StarboardRendererClient(
       pending_client_extension_receiver_(std::move(client_extension_receiver)),
       client_extension_receiver_(this),
       bind_host_receiver_callback_(bind_host_receiver_callback),
-      gpu_factories_(gpu_factories) {
+      gpu_factories_(gpu_factories),
+      request_overlay_info_cb_(std::move(request_overlay_info_cb)) {
   DCHECK(media_task_runner_);
   DCHECK(video_renderer_sink_);
   DCHECK(video_overlay_factory_);
@@ -58,6 +61,9 @@ StarboardRendererClient::StarboardRendererClient(
 StarboardRendererClient::~StarboardRendererClient() {
   SetPlayingState(false);
   DCHECK(!video_renderer_sink_started_);
+  if (request_overlay_info_cb_ && overlay_info_requested_) {
+    request_overlay_info_cb_.Run(false, base::NullCallback());
+  }
 }
 
 void StarboardRendererClient::Initialize(MediaResource* media_resource,
@@ -249,6 +255,18 @@ void StarboardRendererClient::UpdateStarboardRenderingMode(
   }
 }
 
+void StarboardRendererClient::RequestOverlayInfo(bool restart_for_transitions) {
+  DCHECK(media_task_runner_->RunsTasksInCurrentSequence());
+  DCHECK(request_overlay_info_cb_);
+
+  overlay_info_requested_ = true;
+  request_overlay_info_cb_.Run(
+      restart_for_transitions,
+      base::BindPostTaskToCurrentDefault(
+          base::BindRepeating(&StarboardRendererClient::OnOverlayInfoChanged,
+                              weak_factory_.GetWeakPtr())));
+}
+
 void StarboardRendererClient::OnVideoGeometryChange(
     const gfx::RectF& rect_f,
     gfx::OverlayTransform /* transform */) {
@@ -399,6 +417,12 @@ void StarboardRendererClient::OnGetCurrentVideoFrameDone(
     base::AutoLock auto_lock(lock_);
     next_video_frame_ = std::move(frame);
   }
+}
+
+void StarboardRendererClient::OnOverlayInfoChanged(
+    const OverlayInfo& overlay_info) {
+  DCHECK(media_task_runner_->RunsTasksInCurrentSequence());
+  renderer_extension_->OnOverlayInfoChanged(overlay_info);
 }
 
 void StarboardRendererClient::StartVideoRendererSink() {

media/mojo/clients/starboard/starboard_renderer_client.h
@@ -64,7 +64,8 @@ class MEDIA_EXPORT StarboardRendererClient
       mojo::PendingRemote<RendererExtension> pending_renderer_extension,
       mojo::PendingReceiver<ClientExtension> client_extension_receiver,
       BindHostReceiverCallback bind_host_receiver_callback,
-      GpuVideoAcceleratorFactories* gpu_factories);
+      GpuVideoAcceleratorFactories* gpu_factories,
+      RequestOverlayInfoCB request_overlay_info_cb);
 
   StarboardRendererClient(const StarboardRendererClient&) = delete;
   StarboardRendererClient& operator=(const StarboardRendererClient&) = delete;
@@ -103,6 +104,7 @@ class MEDIA_EXPORT StarboardRendererClient
   // mojom::StarboardRendererClientExtension implementation
   void PaintVideoHoleFrame(const gfx::Size& size) override;
   void UpdateStarboardRenderingMode(const StarboardRenderingMode mode) override;
+  void RequestOverlayInfo(bool restart_for_transitions) override;
 
   // cobalt::media::mojom::VideoGeometryChangeClient implementation.
   void OnVideoGeometryChange(const gfx::RectF& rect_f,
@@ -132,6 +134,7 @@ class MEDIA_EXPORT StarboardRendererClient
   void SetPlayingState(bool is_playing);
   void UpdateCurrentFrame();
   void OnGetCurrentVideoFrameDone(const scoped_refptr<VideoFrame>& frame);
+  void OnOverlayInfoChanged(const OverlayInfo& overlay_info);
   void StartVideoRendererSink();
   void StopVideoRendererSink();
 
@@ -144,6 +147,7 @@ class MEDIA_EXPORT StarboardRendererClient
   mojo::Receiver<ClientExtension> client_extension_receiver_;
   const BindHostReceiverCallback bind_host_receiver_callback_;
   raw_ptr<GpuVideoAcceleratorFactories> gpu_factories_ = nullptr;
+  RequestOverlayInfoCB request_overlay_info_cb_;
 
   mojo::Remote<RendererExtension> renderer_extension_;
 
@@ -155,6 +159,7 @@ class MEDIA_EXPORT StarboardRendererClient
 
   bool is_playing_ = false;
   bool video_renderer_sink_started_ = false;
+  bool overlay_info_requested_ = false;
   scoped_refptr<VideoFrame> next_video_frame_;
 
   mutable base::Lock lock_;

media/mojo/clients/starboard/starboard_renderer_client_factory.cc
@@ -53,8 +53,9 @@ std::unique_ptr<Renderer> StarboardRendererClientFactory::CreateRenderer(
     const scoped_refptr<base::TaskRunner>& /*worker_task_runner*/,
     AudioRendererSink* /*audio_renderer_sink*/,
     VideoRendererSink* video_renderer_sink,
-    RequestOverlayInfoCB /*request_overlay_info_cb*/,
+    RequestOverlayInfoCB request_overlay_info_cb,
     const gfx::ColorSpace& /*target_color_space*/) {
+  DCHECK(request_overlay_info_cb);
   DCHECK(video_renderer_sink);
   DCHECK(media_log_);
   DCHECK(mojo_renderer_factory_);
@@ -108,7 +109,7 @@ std::unique_ptr<Renderer> StarboardRendererClientFactory::CreateRenderer(
       std::move(overlay_factory), video_renderer_sink,
       std::move(renderer_extension_remote),
       std::move(client_extension_receiver), bind_host_receiver_callback_,
-      gpu_factories);
+      gpu_factories, std::move(request_overlay_info_cb));
 }
 
 }  // namespace media

media/mojo/mojom/renderer_extensions.mojom
@@ -101,6 +101,9 @@ interface StarboardRendererClientExtension {
 
   // Notify the rendering mode from SbPlayer.
   UpdateStarboardRenderingMode(StarboardRenderingMode mode);
+
+  // Request OverlayInfo from StarboardRendererClient.
+  RequestOverlayInfo(bool restart_for_transitions);
 };
 
 // The Media Foundation Media Engine has two modes for playback.
@@ -166,6 +169,9 @@ interface StarboardRendererExtension {
   // Acquire frame from SbPlayer.
   [Sync]
   GetCurrentVideoFrame() => (VideoFrame? video_frame);
+
+  // Notify StarboardRendererWrapper when the current OverlayInfo changes.
+  OnOverlayInfoChanged(OverlayInfo overlay_info);
 };
 
 // This interface is used by the browser to determine if there are any renderers

media/mojo/services/starboard/starboard_renderer_wrapper.cc
@@ -60,6 +60,9 @@ void StarboardRendererWrapper::Initialize(MediaResource* media_resource,
           weak_factory_.GetWeakPtr()),
       base::BindRepeating(
           &StarboardRendererWrapper::OnUpdateStarboardRenderingModeByStarboard,
+          weak_factory_.GetWeakPtr()),
+      base::BindRepeating(
+          &StarboardRendererWrapper::OnRequestOverlayInfoByStarboard,
           weak_factory_.GetWeakPtr()));
 
   base::ScopedClosureRunner scoped_init_cb(
@@ -145,6 +148,12 @@ void StarboardRendererWrapper::GetCurrentVideoFrame(
   std::move(callback).Run(nullptr);
 }
 
+void StarboardRendererWrapper::OnOverlayInfoChanged(
+    const OverlayInfo& overlay_info) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  GetRenderer()->OnOverlayInfoChanged(overlay_info);
+}
+
 StarboardRenderer* StarboardRendererWrapper::GetRenderer() {
   if (test_renderer_) {
     return test_renderer_;
@@ -181,6 +190,12 @@ void StarboardRendererWrapper::OnUpdateStarboardRenderingModeByStarboard(
     const StarboardRenderingMode mode) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   client_extension_remote_->UpdateStarboardRenderingMode(mode);
+}
+
+void StarboardRendererWrapper::OnRequestOverlayInfoByStarboard(
+    bool restart_for_transitions) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  client_extension_remote_->RequestOverlayInfo(restart_for_transitions);
 }
 
 }  // namespace media

media/mojo/services/starboard/starboard_renderer_wrapper.h
@@ -78,6 +78,7 @@ class StarboardRendererWrapper : public Renderer,
   void OnGpuChannelTokenReady(
       mojom::CommandBufferIdPtr command_buffer_id) override;
   void GetCurrentVideoFrame(GetCurrentVideoFrameCallback callback) override;
+  void OnOverlayInfoChanged(const OverlayInfo& overlay_info) override;
 
   StarboardRenderer* GetRenderer();
   base::SequenceBound<StarboardGpuFactory>* GetGpuFactory();
@@ -95,6 +96,7 @@ class StarboardRendererWrapper : public Renderer,
   void OnPaintVideoHoleFrameByStarboard(const gfx::Size& size);
   void OnUpdateStarboardRenderingModeByStarboard(
       const StarboardRenderingMode mode);
+  void OnRequestOverlayInfoByStarboard(bool restart_for_transitions);
   void ContinueInitialization(MediaResource* media_resource,
                               RendererClient* client,
                               PipelineStatusCallback init_cb);

media/starboard/starboard_renderer.cc
@@ -443,15 +443,21 @@ TimeDelta StarboardRenderer::GetMediaTime() {
 
 void StarboardRenderer::SetStarboardRendererCallbacks(
     PaintVideoHoleFrameCallback paint_video_hole_frame_cb,
-    UpdateStarboardRenderingModeCallback update_starboard_rendering_mode_cb) {
+    UpdateStarboardRenderingModeCallback update_starboard_rendering_mode_cb,
+    RequestOverlayInfoCallBack request_overlay_info_cb) {
   paint_video_hole_frame_cb_ = std::move(paint_video_hole_frame_cb);
   update_starboard_rendering_mode_cb_ =
       std::move(update_starboard_rendering_mode_cb);
+  request_overlay_info_cb_ = std::move(request_overlay_info_cb);
 }
 
 void StarboardRenderer::OnVideoGeometryChange(const gfx::Rect& output_rect) {
   set_bounds_helper_->SetBounds(output_rect.x(), output_rect.y(),
                                 output_rect.width(), output_rect.height());
+}
+
+void StarboardRenderer::OnOverlayInfoChanged(const OverlayInfo& overlay_info) {
+  // TODO: b/429435008 - Request AndroidOverlay() for SbPlayer.
 }
 
 SbPlayerInterface* StarboardRenderer::GetSbPlayerInterface() {

media/starboard/starboard_renderer.h
@@ -103,10 +103,15 @@ class MEDIA_EXPORT StarboardRenderer : public Renderer,
       base::RepeatingCallback<void(const gfx::Size&)>;
   using UpdateStarboardRenderingModeCallback =
       base::RepeatingCallback<void(const StarboardRenderingMode mode)>;
+  using RequestOverlayInfoCallBack =
+      base::RepeatingCallback<void(bool restart_for_transitions)>;
   void SetStarboardRendererCallbacks(
       PaintVideoHoleFrameCallback paint_video_hole_frame_cb,
-      UpdateStarboardRenderingModeCallback update_starboard_rendering_mode_cb);
+      UpdateStarboardRenderingModeCallback update_starboard_rendering_mode_cb,
+      RequestOverlayInfoCallBack request_overlay_info_cb);
+
   void OnVideoGeometryChange(const gfx::Rect& output_rect);
+  void OnOverlayInfoChanged(const OverlayInfo& overlay_info);
 
   SbPlayerInterface* GetSbPlayerInterface();
 
@@ -174,6 +179,7 @@ class MEDIA_EXPORT StarboardRenderer : public Renderer,
   raw_ptr<RendererClient> client_ = nullptr;
   PaintVideoHoleFrameCallback paint_video_hole_frame_cb_;
   UpdateStarboardRenderingModeCallback update_starboard_rendering_mode_cb_;
+  RequestOverlayInfoCallBack request_overlay_info_cb_;
 
   // Temporary callback used for Initialize().
   PipelineStatusCallback init_cb_;

commit f2629499e1882ec74053a70946c835a467cd3b0f

Cherry pick PR #6560: Reland "media: Allow StarboardRenderer to RequestOverlayInfo from WebMediaPlayer (#6399)" (#8100)
Refer to the original PR: https://github.com/youtube/cobalt/pull/6560

This is a reland of
https://github.com/youtube/cobalt/commit/8711f0869a4541d3fda029936bdccaffd28f985a.

This CL fixed DCHECK failed caused by the PR
(https://github.com/youtube/cobalt/pull/6399).

Original change's description:

> media: Allow StarboardRenderer to RequestOverlayInfo from
WebMediaPlayer (https://github.com/youtube/cobalt/pull/6399)
> 
> Currently, StarboardRenderer does not have access to
> RequestOverlayInfoCB or OverlayInfo's routing token, which is needed
to
> create AndroidOverlay in StarboardRenderer. This PR allows
> StarboardRenderer to RequestOverlayInfo from WebMediaPlayer via
> StarboardRendererClientExtension.
> 
> Issue: 429224482

Issue: 429224482
Issue: 460831614

media/mojo/clients/starboard/starboard_renderer_client.cc
@@ -26,6 +26,10 @@
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "ui/gfx/geometry/rect_conversions.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "base/task/bind_post_task.h"
+#endif  // BUILDFLAG(IS_ANDROID)
+
 namespace media {
 
 StarboardRendererClient::StarboardRendererClient(
@@ -37,17 +41,27 @@ StarboardRendererClient::StarboardRendererClient(
     mojo::PendingRemote<RendererExtension> pending_renderer_extension,
     mojo::PendingReceiver<ClientExtension> client_extension_receiver,
     BindHostReceiverCallback bind_host_receiver_callback,
-    GpuVideoAcceleratorFactories* gpu_factories)
-    : media_task_runner_(media_task_runner),
+    GpuVideoAcceleratorFactories* gpu_factories
+#if BUILDFLAG(IS_ANDROID)
+    ,
+    RequestOverlayInfoCB request_overlay_info_cb
+#endif  // BUILDFLAG(IS_ANDROID)
+    )
+    : MojoRendererWrapper(std::move(mojo_renderer)),
+      media_task_runner_(media_task_runner),
       media_log_(std::move(media_log)),
-      MojoRendererWrapper(std::move(mojo_renderer)),
       video_overlay_factory_(std::move(video_overlay_factory)),
       video_renderer_sink_(video_renderer_sink),
       pending_renderer_extension_(std::move(pending_renderer_extension)),
       pending_client_extension_receiver_(std::move(client_extension_receiver)),
       client_extension_receiver_(this),
       bind_host_receiver_callback_(bind_host_receiver_callback),
-      gpu_factories_(gpu_factories) {
+      gpu_factories_(gpu_factories)
+#if BUILDFLAG(IS_ANDROID)
+      ,
+      request_overlay_info_cb_(std::move(request_overlay_info_cb))
+#endif  // BUILDFLAG(IS_ANDROID)
+{
   DCHECK(media_task_runner_);
   DCHECK(video_renderer_sink_);
   DCHECK(video_overlay_factory_);
@@ -58,6 +72,13 @@ StarboardRendererClient::StarboardRendererClient(
 StarboardRendererClient::~StarboardRendererClient() {
   SetPlayingState(false);
   DCHECK(!video_renderer_sink_started_);
+
+#if BUILDFLAG(IS_ANDROID)
+  if (request_overlay_info_cb_ && overlay_info_requested_) {
+    request_overlay_info_cb_.Run(false, base::NullCallback());
+    overlay_info_requested_ = false;
+  }
+#endif  // BUILDFLAG(IS_ANDROID)
 }
 
 void StarboardRendererClient::Initialize(MediaResource* media_resource,
@@ -249,6 +270,20 @@ void StarboardRendererClient::UpdateStarboardRenderingMode(
   }
 }
 
+#if BUILDFLAG(IS_ANDROID)
+void StarboardRendererClient::RequestOverlayInfo(bool restart_for_transitions) {
+  DCHECK(media_task_runner_->RunsTasksInCurrentSequence());
+  DCHECK(request_overlay_info_cb_);
+
+  overlay_info_requested_ = true;
+  request_overlay_info_cb_.Run(
+      restart_for_transitions,
+      base::BindPostTaskToCurrentDefault(
+          base::BindRepeating(&StarboardRendererClient::OnOverlayInfoChanged,
+                              weak_factory_.GetWeakPtr())));
+}
+#endif  // BUILDFLAG(IS_ANDROID)
+
 void StarboardRendererClient::OnVideoGeometryChange(
     const gfx::RectF& rect_f,
     gfx::OverlayTransform /* transform */) {
@@ -400,6 +435,14 @@ void StarboardRendererClient::OnGetCurrentVideoFrameDone(
     next_video_frame_ = std::move(frame);
   }
 }
+
+#if BUILDFLAG(IS_ANDROID)
+void StarboardRendererClient::OnOverlayInfoChanged(
+    const OverlayInfo& overlay_info) {
+  DCHECK(media_task_runner_->RunsTasksInCurrentSequence());
+  renderer_extension_->OnOverlayInfoChanged(overlay_info);
+}
+#endif  // BUILDFLAG(IS_ANDROID)
 
 void StarboardRendererClient::StartVideoRendererSink() {
   DCHECK(media_task_runner_->RunsTasksInCurrentSequence());

media/mojo/clients/starboard/starboard_renderer_client.h
@@ -64,7 +64,12 @@ class MEDIA_EXPORT StarboardRendererClient
       mojo::PendingRemote<RendererExtension> pending_renderer_extension,
       mojo::PendingReceiver<ClientExtension> client_extension_receiver,
       BindHostReceiverCallback bind_host_receiver_callback,
-      GpuVideoAcceleratorFactories* gpu_factories);
+      GpuVideoAcceleratorFactories* gpu_factories
+#if BUILDFLAG(IS_ANDROID)
+      ,
+      RequestOverlayInfoCB request_overlay_info_cb
+#endif  // BUILDFLAG(IS_ANDROID)
+  );
 
   StarboardRendererClient(const StarboardRendererClient&) = delete;
   StarboardRendererClient& operator=(const StarboardRendererClient&) = delete;
@@ -103,6 +108,9 @@ class MEDIA_EXPORT StarboardRendererClient
   // mojom::StarboardRendererClientExtension implementation
   void PaintVideoHoleFrame(const gfx::Size& size) override;
   void UpdateStarboardRenderingMode(const StarboardRenderingMode mode) override;
+#if BUILDFLAG(IS_ANDROID)
+  void RequestOverlayInfo(bool restart_for_transitions) override;
+#endif  // BUILDFLAG(IS_ANDROID)
 
   // cobalt::media::mojom::VideoGeometryChangeClient implementation.
   void OnVideoGeometryChange(const gfx::RectF& rect_f,
@@ -132,6 +140,9 @@ class MEDIA_EXPORT StarboardRendererClient
   void SetPlayingState(bool is_playing);
   void UpdateCurrentFrame();
   void OnGetCurrentVideoFrameDone(const scoped_refptr<VideoFrame>& frame);
+#if BUILDFLAG(IS_ANDROID)
+  void OnOverlayInfoChanged(const OverlayInfo& overlay_info);
+#endif  // BUILDFLAG(IS_ANDROID)
   void StartVideoRendererSink();
   void StopVideoRendererSink();
 
@@ -144,6 +155,9 @@ class MEDIA_EXPORT StarboardRendererClient
   mojo::Receiver<ClientExtension> client_extension_receiver_;
   const BindHostReceiverCallback bind_host_receiver_callback_;
   raw_ptr<GpuVideoAcceleratorFactories> gpu_factories_ = nullptr;
+#if BUILDFLAG(IS_ANDROID)
+  RequestOverlayInfoCB request_overlay_info_cb_;
+#endif  // BUILDFLAG(IS_ANDROID)
 
   mojo::Remote<RendererExtension> renderer_extension_;
 
@@ -155,6 +169,9 @@ class MEDIA_EXPORT StarboardRendererClient
 
   bool is_playing_ = false;
   bool video_renderer_sink_started_ = false;
+#if BUILDFLAG(IS_ANDROID)
+  bool overlay_info_requested_ = false;
+#endif  // BUILDFLAG(IS_ANDROID)
   scoped_refptr<VideoFrame> next_video_frame_;
 
   mutable base::Lock lock_;

media/mojo/clients/starboard/starboard_renderer_client_factory.cc
@@ -61,8 +61,11 @@ std::unique_ptr<Renderer> StarboardRendererClientFactory::CreateRenderer(
     const scoped_refptr<base::TaskRunner>& /*worker_task_runner*/,
     AudioRendererSink* /*audio_renderer_sink*/,
     VideoRendererSink* video_renderer_sink,
-    RequestOverlayInfoCB /*request_overlay_info_cb*/,
+    RequestOverlayInfoCB request_overlay_info_cb,
     const gfx::ColorSpace& /*target_color_space*/) {
+#if BUILDFLAG(IS_ANDROID)
+  DCHECK(request_overlay_info_cb);
+#endif  // BUILDFLAG(IS_ANDROID)
   DCHECK(video_renderer_sink);
   DCHECK(media_log_);
   DCHECK(mojo_renderer_factory_);
@@ -116,7 +119,12 @@ std::unique_ptr<Renderer> StarboardRendererClientFactory::CreateRenderer(
       std::move(overlay_factory), video_renderer_sink,
       std::move(renderer_extension_remote),
       std::move(client_extension_receiver), bind_host_receiver_callback_,
-      gpu_factories);
+      gpu_factories
+#if BUILDFLAG(IS_ANDROID)
+      ,
+      std::move(request_overlay_info_cb)
+#endif  // BUILDFLAG(IS_ANDROID)
+  );
 }
 
 }  // namespace media

media/mojo/mojom/renderer_extensions.mojom
@@ -101,6 +101,10 @@ interface StarboardRendererClientExtension {
 
   // Notify the rendering mode from SbPlayer.
   UpdateStarboardRenderingMode(StarboardRenderingMode mode);
+
+  [EnableIf=is_android]
+  // Request OverlayInfo from StarboardRendererClient.
+  RequestOverlayInfo(bool restart_for_transitions);
 };
 
 // The Media Foundation Media Engine has two modes for playback.
@@ -166,6 +170,10 @@ interface StarboardRendererExtension {
   // Acquire frame from SbPlayer.
   [Sync]
   GetCurrentVideoFrame() => (VideoFrame? video_frame);
+
+  [EnableIf=is_android]
+  // Notify StarboardRendererWrapper when the current OverlayInfo changes.
+  OnOverlayInfoChanged(OverlayInfo overlay_info);
 };
 
 // This interface is used by the browser to determine if there are any renderers

media/mojo/services/starboard/starboard_renderer_wrapper.cc
@@ -61,7 +61,14 @@ void StarboardRendererWrapper::Initialize(MediaResource* media_resource,
           weak_factory_.GetWeakPtr()),
       base::BindRepeating(
           &StarboardRendererWrapper::OnUpdateStarboardRenderingModeByStarboard,
-          weak_factory_.GetWeakPtr()));
+          weak_factory_.GetWeakPtr())
+#if BUILDFLAG(IS_ANDROID)
+          ,
+      base::BindRepeating(
+          &StarboardRendererWrapper::OnRequestOverlayInfoByStarboard,
+          weak_factory_.GetWeakPtr())
+#endif  // BUILDFLAG(IS_ANDROID)
+  );
 
   base::ScopedClosureRunner scoped_init_cb(
       base::BindOnce(&StarboardRendererWrapper::ContinueInitialization,
@@ -146,6 +153,14 @@ void StarboardRendererWrapper::GetCurrentVideoFrame(
   std::move(callback).Run(nullptr);
 }
 
+#if BUILDFLAG(IS_ANDROID)
+void StarboardRendererWrapper::OnOverlayInfoChanged(
+    const OverlayInfo& overlay_info) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  GetRenderer()->OnOverlayInfoChanged(overlay_info);
+}
+#endif  // BUILDFLAG(IS_ANDROID)
+
 StarboardRenderer* StarboardRendererWrapper::GetRenderer() {
   if (test_renderer_) {
     return test_renderer_;
@@ -183,5 +198,13 @@ void StarboardRendererWrapper::OnUpdateStarboardRenderingModeByStarboard(
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   client_extension_remote_->UpdateStarboardRenderingMode(mode);
 }
+
+#if BUILDFLAG(IS_ANDROID)
+void StarboardRendererWrapper::OnRequestOverlayInfoByStarboard(
+    bool restart_for_transitions) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  client_extension_remote_->RequestOverlayInfo(restart_for_transitions);
+}
+#endif  // BUILDFLAG(IS_ANDROID)
 
 }  // namespace media

media/mojo/services/starboard/starboard_renderer_wrapper.h
@@ -78,6 +78,9 @@ class StarboardRendererWrapper : public Renderer,
   void OnGpuChannelTokenReady(
       mojom::CommandBufferIdPtr command_buffer_id) override;
   void GetCurrentVideoFrame(GetCurrentVideoFrameCallback callback) override;
+#if BUILDFLAG(IS_ANDROID)
+  void OnOverlayInfoChanged(const OverlayInfo& overlay_info) override;
+#endif  // BUILDFLAG(IS_ANDROID)
 
   StarboardRenderer* GetRenderer();
   base::SequenceBound<StarboardGpuFactory>* GetGpuFactory();
@@ -95,6 +98,10 @@ class StarboardRendererWrapper : public Renderer,
   void OnPaintVideoHoleFrameByStarboard(const gfx::Size& size);
   void OnUpdateStarboardRenderingModeByStarboard(
       const StarboardRenderingMode mode);
+#if BUILDFLAG(IS_ANDROID)
+  void OnRequestOverlayInfoByStarboard(bool restart_for_transitions);
+#endif  // BUILDFLAG(IS_ANDROID)
+
   void ContinueInitialization(MediaResource* media_resource,
                               RendererClient* client,
                               PipelineStatusCallback init_cb);

media/starboard/starboard_renderer.cc
@@ -491,16 +491,30 @@ TimeDelta StarboardRenderer::GetMediaTime() {
 
 void StarboardRenderer::SetStarboardRendererCallbacks(
     PaintVideoHoleFrameCallback paint_video_hole_frame_cb,
-    UpdateStarboardRenderingModeCallback update_starboard_rendering_mode_cb) {
+    UpdateStarboardRenderingModeCallback update_starboard_rendering_mode_cb
+#if BUILDFLAG(IS_ANDROID)
+    ,
+    RequestOverlayInfoCallBack request_overlay_info_cb
+#endif  // BUILDFLAG(IS_ANDROID)
+) {
   paint_video_hole_frame_cb_ = std::move(paint_video_hole_frame_cb);
   update_starboard_rendering_mode_cb_ =
       std::move(update_starboard_rendering_mode_cb);
+#if BUILDFLAG(IS_ANDROID)
+  request_overlay_info_cb_ = std::move(request_overlay_info_cb);
+#endif  // BUILDFLAG(IS_ANDROID)
 }
 
 void StarboardRenderer::OnVideoGeometryChange(const gfx::Rect& output_rect) {
   set_bounds_helper_->SetBounds(output_rect.x(), output_rect.y(),
                                 output_rect.width(), output_rect.height());
 }
+
+#if BUILDFLAG(IS_ANDROID)
+void StarboardRenderer::OnOverlayInfoChanged(const OverlayInfo& overlay_info) {
+  // TODO: b/429435008 - Request AndroidOverlay() for SbPlayer.
+}
+#endif  // BUILDFLAG(IS_ANDROID)
 
 SbPlayerInterface* StarboardRenderer::GetSbPlayerInterface() {
   if (test_sbplayer_interface_) {

media/starboard/starboard_renderer.h
@@ -107,10 +107,23 @@ class MEDIA_EXPORT StarboardRenderer : public Renderer,
       base::RepeatingCallback<void(const gfx::Size&)>;
   using UpdateStarboardRenderingModeCallback =
       base::RepeatingCallback<void(const StarboardRenderingMode mode)>;
+#if BUILDFLAG(IS_ANDROID)
+  using RequestOverlayInfoCallBack =
+      base::RepeatingCallback<void(bool restart_for_transitions)>;
+#endif  // BUILDFLAG(IS_ANDROID)
   void SetStarboardRendererCallbacks(
       PaintVideoHoleFrameCallback paint_video_hole_frame_cb,
-      UpdateStarboardRenderingModeCallback update_starboard_rendering_mode_cb);
+      UpdateStarboardRenderingModeCallback update_starboard_rendering_mode_cb
+#if BUILDFLAG(IS_ANDROID)
+      ,
+      RequestOverlayInfoCallBack request_overlay_info_cb
+#endif  // BUILDFLAG(IS_ANDROID)
+  );
+
   void OnVideoGeometryChange(const gfx::Rect& output_rect);
+#if BUILDFLAG(IS_ANDROID)
+  void OnOverlayInfoChanged(const OverlayInfo& overlay_info);
+#endif  // BUILDFLAG(IS_ANDROID)
 
   SbPlayerInterface* GetSbPlayerInterface();
 
@@ -179,6 +192,9 @@ class MEDIA_EXPORT StarboardRenderer : public Renderer,
   raw_ptr<RendererClient> client_ = nullptr;
   PaintVideoHoleFrameCallback paint_video_hole_frame_cb_;
   UpdateStarboardRenderingModeCallback update_starboard_rendering_mode_cb_;
+#if BUILDFLAG(IS_ANDROID)
+  RequestOverlayInfoCallBack request_overlay_info_cb_;
+#endif  // BUILDFLAG(IS_ANDROID)
 
   // Temporary callback used for Initialize().
   PipelineStatusCallback init_cb_;

media/mojo/clients/starboard/starboard_renderer_client_unittest.cc
@@ -73,6 +73,9 @@ class FakeStarboardRendererExtension
 
   MOCK_METHOD1(GetCurrentVideoFrame, void(GetCurrentVideoFrameCallback cb));
   MOCK_METHOD1(OnVideoGeometryChange, void(const gfx::Rect&));
+#if BUILDFLAG(IS_ANDROID)
+  MOCK_METHOD1(OnOverlayInfoChanged, void(const OverlayInfo& overlay_info));
+#endif  // BUILDFLAG(IS_ANDROID)
   void OnGpuChannelTokenReady(
       mojom::CommandBufferIdPtr command_buffer_id) override {}
 };
@@ -153,7 +156,12 @@ class StarboardRendererClientTest : public ::testing::Test {
         std::move(starboard_renderer_extensions_remote),
         std::move(client_extension_receiver),
         /*bind_host_receiver_callback=*/base::DoNothing(),
-        with_gpu_factories ? mock_gpu_factories_.get() : nullptr);
+        with_gpu_factories ? mock_gpu_factories_.get() : nullptr
+#if BUILDFLAG(IS_ANDROID)
+        ,
+        /*request_overlay_info_cb=*/base::DoNothing()
+#endif  // BUILDFLAG(IS_ANDROID)
+    );
   }
 
   base::test::SingleThreadTaskEnvironment task_environment_;

media/starboard/starboard_renderer_unittest.cc
@@ -124,7 +124,12 @@ class StarboardRendererTest : public testing::Test {
     renderer_->SetSbPlayerInterfaceForTesting(&mock_sbplayer_interface_);
     renderer_->SetStarboardRendererCallbacks(
         /*paint_video_hole_frame_cb=*/base::DoNothing(),
-        /*update_starboard_rendering_mode_cb=*/base::DoNothing());
+        /*update_starboard_rendering_mode_cb=*/base::DoNothing()
+#if BUILDFLAG(IS_ANDROID)
+            ,
+        /*request_overlay_info_cb=*/base::DoNothing()
+#endif  // BUILDFLAG(IS_ANDROID)
+    );
 
     EXPECT_CALL(media_resource_, GetAllStreams())
         .WillRepeatedly(Invoke(this, &StarboardRendererTest::GetAllStreams));