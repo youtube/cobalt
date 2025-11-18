360536010ae3769866e334a0e380b17042928514
Holden Warriner <hwarriner@google.com>
2025-11-18 11:54:51 -0500

    Only wire up CrashHandler extension for Evergreen (#8054)
    This fixes a crash on monolithic and non-evergreen modular builds that
    was introduced by #7548. There are other reasonable ways to fix the
    crash but I think this makes sense, at least for now, since we currently
    do not support crashpad - or any real crash handler - for non-evergreen
    builds.
    
    Issue: 460489024

--- starboard/linux/shared/system_get_extensions.cc
+++ starboard/linux/shared/system_get_extensions.cc
@@ -12,20 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// clang-format off
 #include "starboard/system.h"
+// clang-format on
 
+#include "build/build_config.h"
 #include "starboard/common/string.h"
-#if SB_IS(EVERGREEN_COMPATIBLE)
-#include "starboard/elf_loader/evergreen_config.h"
-#endif
 #include "starboard/extension/configuration.h"
-#include "starboard/extension/crash_handler.h"
 #include "starboard/extension/enhanced_audio.h"
 #include "starboard/extension/free_space.h"
 #include "starboard/extension/ifa.h"
-#if SB_IS(EVERGREEN_COMPATIBLE)
-#include "starboard/extension/loader_app_metrics.h"
-#endif
 #include "starboard/extension/memory_mapped_file.h"
 #include "starboard/extension/platform_service.h"
 #include "starboard/linux/shared/configuration.h"
@@ -34,9 +30,16 @@
 #include "starboard/shared/enhanced_audio/enhanced_audio.h"
 #include "starboard/shared/posix/free_space.h"
 #include "starboard/shared/posix/memory_mapped_file.h"
-#include "starboard/shared/starboard/crash_handler.h"
+
 #if SB_IS(EVERGREEN_COMPATIBLE)
+#include "starboard/elf_loader/evergreen_config.h"
+#include "starboard/extension/loader_app_metrics.h"
 #include "starboard/shared/starboard/loader_app_metrics.h"
+#endif
+
+#if BUILDFLAG(USE_EVERGREEN)
+#include "starboard/extension/crash_handler.h"
+#include "starboard/shared/starboard/crash_handler.h"
 #endif
 
 const void* SbSystemGetExtension(const char* name) {
@@ -57,9 +60,11 @@ const void* SbSystemGetExtension(const char* name) {
   if (strcmp(name, kCobaltExtensionConfigurationName) == 0) {
     return starboard::GetConfigurationApiLinux();
   }
+#if BUILDFLAG(USE_EVERGREEN)
   if (strcmp(name, kCobaltExtensionCrashHandlerName) == 0) {
     return starboard::GetCrashHandlerApi();
   }
+#endif
   if (strcmp(name, kCobaltExtensionMemoryMappedFileName) == 0) {
     return starboard::GetMemoryMappedFileApi();
   }
