353ba144751bd4f046e4e2f8d4da17a9878d515c
Bo-Rong Chen <borongchen@google.com>
2025-11-18 11:29:02 -0800

    media: Expose SurfaceView to SbPlayer via Starboard Extension

--- starboard/extension/extension_test.cc
+++ starboard/extension/extension_test.cc
@@ -32,6 +32,7 @@
 #include "starboard/extension/platform_service.h"
 #include "starboard/extension/player_configuration.h"
 #include "starboard/extension/player_set_max_video_input_size.h"
+#include "starboard/extension/player_set_video_surface_view.h"
 #include "starboard/extension/system_info.h"
 #include "starboard/extension/updater_notification.h"
 #include "starboard/extension/url_fetcher_observer.h"
@@ -463,6 +464,26 @@ TEST(ExtensionTest, PlayerSetMaxVideoInputSize) {
   EXPECT_STREQ(extension_api->name, kExtensionName);
   EXPECT_EQ(extension_api->version, 1u);
   EXPECT_NE(extension_api->SetMaxVideoInputSizeForCurrentThread, nullptr);
+
+  const ExtensionApi* second_extension_api =
+      static_cast<const ExtensionApi*>(SbSystemGetExtension(kExtensionName));
+  EXPECT_EQ(second_extension_api, extension_api)
+      << "Extension struct should be a singleton";
+}
+
+TEST(ExtensionTest, PlayerSetVideoSurfaceView) {
+  typedef StarboardExtensionPlayerSetVideoSurfaceViewApi ExtensionApi;
+  const char* kExtensionName = kStarboardExtensionPlayerSetVideoSurfaceViewName;
+
+  const ExtensionApi* extension_api =
+      static_cast<const ExtensionApi*>(SbSystemGetExtension(kExtensionName));
+  if (!extension_api) {
+    return;
+  }
+
+  EXPECT_STREQ(extension_api->name, kExtensionName);
+  EXPECT_EQ(extension_api->version, 1u);
+  EXPECT_NE(extension_api->SetVideoSurfaceViewForCurrentThread, nullptr);
 
   const ExtensionApi* second_extension_api =
       static_cast<const ExtensionApi*>(SbSystemGetExtension(kExtensionName));
