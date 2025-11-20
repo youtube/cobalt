34a8816c8d454313fb3fb46db20f8ece6bf02596
Charley Kim <charleykim@google.com>
2025-11-20 11:18:17 -0800

    blink/renderer: backport Context Menu Controller upstream changes (#8101)
    Backporting [upstreamed changes to
    Chromium](https://chromium-review.googlesource.com/c/chromium/src/+/7142569)
    changes that were made in #7243 to make the base class not `final` and
    `ShowContextMenu()` a virtual bool to support a stub context menu
    controller for Cobalt.
    
    Bug: 461526107
    (cherry picked from commit a41bba55e10f381a2c1534ad909005d5b5e607ec)

--- third_party/blink/renderer/core/page/context_menu_controller.h
+++ third_party/blink/renderer/core/page/context_menu_controller.h
@@ -26,7 +26,6 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_PAGE_CONTEXT_MENU_CONTROLLER_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_PAGE_CONTEXT_MENU_CONTROLLER_H_
 
-#include "base/memory/scoped_refptr.h"
 #include "third_party/blink/public/common/input/web_menu_source_type.h"
 #include "third_party/blink/public/mojom/context_menu/context_menu.mojom-blink.h"
 #include "third_party/blink/renderer/core/core_export.h"
@@ -43,12 +42,8 @@ class MouseEvent;
 class Page;
 struct ContextMenuData;
 
-// TODO() remove buildflags and upstream to Chromium
-#if BUILDFLAG(IS_COBALT)
+// This class is not final to allow customization by embedders
 class CORE_EXPORT ContextMenuController
-#else
-class CORE_EXPORT ContextMenuController final
-#endif
     : public GarbageCollected<ContextMenuController>,
       public mojom::blink::ContextMenuClient {
  public:
@@ -124,6 +119,7 @@ class CORE_EXPORT ContextMenuController final
  private:
   friend class ContextMenuControllerTest;
 
+<<<<<<< HEAD
  protected:
   // Returns whether a Context Menu was actually shown.
   // TODO(): upstream virtual to Chromium
@@ -131,6 +127,15 @@ class CORE_EXPORT ContextMenuController final
                                const PhysicalOffset&,
                                WebMenuSourceType,
                                const MouseEvent* mouse_event = nullptr);
+=======
+  // Returns whether a Context Menu was actually shown. Changing this is not
+  // recommended.
+  bool ShowContextMenu(LocalFrame*, const PhysicalOffset&, WebMenuSourceType);
+  virtual bool ShowContextMenu(LocalFrame*,
+                               const PhysicalOffset&,
+                               WebMenuSourceType,
+                               const MouseEvent* mouse_event);
+>>>>>>> a41bba55e10 (blink/renderer: backport Context Menu Controller upstream changes (#8101))
 
   bool ShouldShowContextMenuFromTouch(const ContextMenuData&);
 
