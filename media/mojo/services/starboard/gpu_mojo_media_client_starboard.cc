youtube/cobalt

commit aaa1e62ce066c2c1ee0392f9a7aeafe1b52805a7

media: Reshape AndroidOverlayFactoryCB ownership (#8075)
Move the AndroidOverlayMojoFactoryCB from GpuMojoMediaClient to
GpuMojoMediaClientStarboard. This localizes the Android-specific
overlay factory management to the Starboard implementation, where it
is exclusively used.

Additionally, internalize the CreatePlatformStarboardRenderer free
function into GpuMojoMediaClientStarboard as a virtual method,
further improving modularity and separation of concerns within the
media pipeline for platform-specific rendering. This revisits and
refines earlier changes to the media client architecture.

This revisits the changes in
https://github.com/youtube/cobalt/commit/7c7e771e9a42a2bd9585efd562589a8ad5430563.

Issue: 433534482
Issue: 429257581

media/mojo/services/starboard/gpu_mojo_media_client_starboard.cc
@@ -27,7 +27,11 @@ namespace media {
 class GpuMojoMediaClientStarboard final : public GpuMojoMediaClient {
  public:
   GpuMojoMediaClientStarboard(GpuMojoMediaClientTraits& traits)
-      : GpuMojoMediaClient(traits) {}
+      : GpuMojoMediaClient(traits) {
+#if BUILDFLAG(IS_ANDROID)
+    android_overlay_factory_cb_ = std::move(traits.android_overlay_factory_cb);
+#endif  // BUILDFLAG(IS_ANDROID)
+  }
   ~GpuMojoMediaClientStarboard() final = default;
 
  protected:
@@ -56,16 +60,23 @@ class GpuMojoMediaClientStarboard final : public GpuMojoMediaClient {
     return nullptr;
   }
 
+  std::unique_ptr<Renderer> CreatePlatformStarboardRenderer(
+      StarboardRendererTraits traits) final {
+#if BUILDFLAG(IS_ANDROID)
+    traits.android_overlay_factory_cb = std::move(android_overlay_factory_cb_);
+#endif  // BUILDFLAG(IS_ANDROID)
+    return std::make_unique<StarboardRendererWrapper>(std::move(traits));
+  }
+
   std::unique_ptr<CdmFactory> CreatePlatformCdmFactory(
       mojom::FrameInterfaceFactory* frame_interfaces) final {
     return std::make_unique<media::StarboardCdmFactory>();
   }
-};
 
-std::unique_ptr<Renderer> CreatePlatformStarboardRenderer(
-    StarboardRendererTraits traits) {
-  return std::make_unique<StarboardRendererWrapper>(std::move(traits));
-}
+#if BUILDFLAG(IS_ANDROID)
+  AndroidOverlayMojoFactoryCB android_overlay_factory_cb_;
+#endif  // BUILDFLAG(IS_ANDROID)
+};
 
 std::unique_ptr<GpuMojoMediaClient> CreateGpuMediaService(
     GpuMojoMediaClientTraits& traits) {

media/mojo/services/gpu_mojo_media_client.cc
@@ -101,8 +101,7 @@ StarboardRendererTraits::StarboardRendererTraits(
         renderer_extension_receiver,
     mojo::PendingRemote<mojom::StarboardRendererClientExtension>
         client_extension_remote,
-    GetStarboardCommandBufferStubCB get_starboard_command_buffer_stub_cb,
-    AndroidOverlayMojoFactoryCB android_overlay_factory_cb)
+    GetStarboardCommandBufferStubCB get_starboard_command_buffer_stub_cb)
     : task_runner(std::move(task_runner)),
       gpu_task_runner(std::move(gpu_task_runner)),
       media_log_remote(std::move(media_log_remote)),
@@ -114,8 +113,7 @@ StarboardRendererTraits::StarboardRendererTraits(
       renderer_extension_receiver(std::move(renderer_extension_receiver)),
       client_extension_remote(std::move(client_extension_remote)),
       get_starboard_command_buffer_stub_cb(
-          std::move(get_starboard_command_buffer_stub_cb)),
-      android_overlay_factory_cb(std::move(android_overlay_factory_cb)) {}
+          std::move(get_starboard_command_buffer_stub_cb)) {}
 #endif  // BUILDFLAG(USE_STARBOARD_MEDIA)
 
 GpuMojoMediaClientTraits::~GpuMojoMediaClientTraits() = default;
@@ -151,12 +149,7 @@ GpuMojoMediaClient::GpuMojoMediaClient(GpuMojoMediaClientTraits& traits)
       gpu_feature_info_(std::move(traits.gpu_feature_info)),
       gpu_info_(std::move(traits.gpu_info)),
       gpu_task_runner_(std::move(traits.gpu_task_runner)),
-      media_gpu_channel_manager_(std::move(traits.media_gpu_channel_manager))
-#if BUILDFLAG(USE_STARBOARD_MEDIA)
-      ,
-      android_overlay_factory_cb_(std::move(traits.android_overlay_factory_cb))
-#endif
-  {}
+      media_gpu_channel_manager_(std::move(traits.media_gpu_channel_manager)) {}
 
 GpuMojoMediaClient::~GpuMojoMediaClient() = default;
 
@@ -294,8 +287,7 @@ std::unique_ptr<Renderer> GpuMojoMediaClient::CreateStarboardRenderer(
       config.audio_write_duration_remote, config.max_video_capabilities,
       config.viewport_size, std::move(renderer_extension_receiver),
       std::move(client_extension_remote), base::BindRepeating(
-        &GetCommandBufferStub, gpu_task_runner_, media_gpu_channel_manager_),
-      android_overlay_factory_cb_);
+        &GetCommandBufferStub, gpu_task_runner_, media_gpu_channel_manager_));
   return CreatePlatformStarboardRenderer(std::move(traits));
 }
 #endif  // BUILDFLAG(USE_STARBOARD_MEDIA)
@@ -317,6 +309,14 @@ std::unique_ptr<AudioEncoder> GpuMojoMediaClient::CreatePlatformAudioEncoder(
   NOTIMPLEMENTED();
   return nullptr;
 }
+
+#if BUILDFLAG(USE_STARBOARD_MEDIA)
+std::unique_ptr<Renderer> GpuMojoMediaClient::CreatePlatformStarboardRenderer(
+        StarboardRendererTraits traits) {
+  NOTIMPLEMENTED();
+  return nullptr;
+}
+#endif  // BUILDFLAG(USE_STARBOARD_MEDIA)
 
 std::unique_ptr<CdmFactory> GpuMojoMediaClient::CreatePlatformCdmFactory(
     mojom::FrameInterfaceFactory* frame_interfaces) {

media/mojo/services/gpu_mojo_media_client.h
@@ -101,16 +101,10 @@ struct StarboardRendererTraits {
       mojo::PendingRemote<mojom::StarboardRendererClientExtension>
           client_extension_remote,
       GetStarboardCommandBufferStubCB
-          get_starboard_command_buffer_stub_cb,
-      AndroidOverlayMojoFactoryCB android_overlay_factory_cb);
+          get_starboard_command_buffer_stub_cb);
   StarboardRendererTraits(StarboardRendererTraits&& that) = default;
   ~StarboardRendererTraits();
 };
-
-// Creates a platform-specific media::StarboardRenderer.
-// This is used on Cobalt (android/linux).
-std::unique_ptr<Renderer> CreatePlatformStarboardRenderer(
-    StarboardRendererTraits traits);
 #endif  // BUILDFLAG(USE_STARBOARD_MEDIA)
 
 struct MEDIA_MOJO_EXPORT GpuMojoMediaClientTraits {
@@ -239,6 +233,13 @@ class MEDIA_MOJO_EXPORT GpuMojoMediaClient : public MojoMediaClient {
   // anything here.
   virtual std::unique_ptr<AudioEncoder> CreatePlatformAudioEncoder(
       scoped_refptr<base::SequencedTaskRunner> task_runner);
+  
+#if BUILDFLAG(USE_STARBOARD_MEDIA)
+  // Creates a platform-specific media::StarboardRenderer.
+  // This is used on Cobalt (android/linux).
+  virtual std::unique_ptr<Renderer> CreatePlatformStarboardRenderer(
+        StarboardRendererTraits traits);
+#endif  // BUILDFLAG(USE_STARBOARD_MEDIA)
 
   // Creates a CDM factory, right now only used on android and chromeos.
   virtual std::unique_ptr<CdmFactory> CreatePlatformCdmFactory(
@@ -259,10 +260,6 @@ class MEDIA_MOJO_EXPORT GpuMojoMediaClient : public MojoMediaClient {
   // Note this should not be passed outside of this class since it must be
   // carefully used only on the GPU thread and not the media service thread.
   const base::WeakPtr<MediaGpuChannelManager> media_gpu_channel_manager_;
-
-#if BUILDFLAG(USE_STARBOARD_MEDIA)
-  AndroidOverlayMojoFactoryCB android_overlay_factory_cb_;
-#endif
 
   // Cross-platform cache supported config cache.
   std::optional<SupportedVideoDecoderConfigs> supported_config_cache_;

media/mojo/services/starboard/starboard_renderer_wrapper_unittest.cc
@@ -172,8 +172,7 @@ class StarboardRendererWrapperTest : public testing::Test {
         std::move(media_log_remote), base::UnguessableToken::Create(),
         base::Seconds(1), base::Seconds(1), std::string(), gfx::Size(),
         std::move(renderer_extension_receiver),
-        std::move(client_extension_remote), base::NullCallback(),
-        AndroidOverlayMojoFactoryCB());
+        std::move(client_extension_remote), base::NullCallback());
     renderer_wrapper_ =
         std::make_unique<StarboardRendererWrapper>(std::move(traits));
     renderer_wrapper_->SetRendererForTesting(mock_renderer_.get());

commit b13831d3c5d4f825fe917714cb4409708a62e6b8

media: Fix AndroidOverlayFactoryCB ownership (#8122)
The std::move on android_overlay_factory_cb_ incorrectly transferred
ownership of the callback. This led to the callback being consumed
after the first renderer creation, preventing subsequent renderers
from initializing correctly with an Android overlay factory.

Removing std::move ensures the factory callback is copied, allowing
it to remain valid and available for all renderer creations by the
GpuMojoMediaClientStarboard instance.

This fixes the issue introduced by
https://github.com/youtube/cobalt/pull/8075.

Issue: 433534482
Issue: 429257581

media/mojo/services/starboard/gpu_mojo_media_client_starboard.cc
@@ -63,7 +63,7 @@ class GpuMojoMediaClientStarboard final : public GpuMojoMediaClient {
   std::unique_ptr<Renderer> CreatePlatformStarboardRenderer(
       StarboardRendererTraits traits) final {
 #if BUILDFLAG(IS_ANDROID)
-    traits.android_overlay_factory_cb = std::move(android_overlay_factory_cb_);
+    traits.android_overlay_factory_cb = android_overlay_factory_cb_;
 #endif  // BUILDFLAG(IS_ANDROID)
     return std::make_unique<StarboardRendererWrapper>(std::move(traits));
   }