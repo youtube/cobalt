4b2c6f6f8105c9102366cb00dedaf8b7724ab81d
Sherry Zhou <sherryzy@google.com>
2025-11-25 10:37:05 -0800

    Fix: GrowableIOBuffer memory leak on SetCapacity(0) (#8183)
    Addressed a memory leak in GrowableIOBuffer::SetCapacity when resizing
    to 0. Previously, unique_ptr::release() followed by realloc could lead
    to memory not being deallocated if realloc returned NULL. The fix
    explicitly calls reset() on the unique_ptr for capacity 0, ensuring
    proper memory release.
    
    Bug: 454095852
    (cherry picked from commit c467e28d27c553eb9aaf4b908dd50786f086cf06)

--- net/base/io_buffer.cc
+++ net/base/io_buffer.cc
@@ -118,12 +118,30 @@ DrainableIOBuffer::~DrainableIOBuffer() {
 GrowableIOBuffer::GrowableIOBuffer() = default;
 
 void GrowableIOBuffer::SetCapacity(int capacity) {
+<<<<<<< HEAD
   CHECK_GE(capacity, 0);
 
   // The span will be set again in `set_offset()`. Need to clear raw pointers to
   // the data before reallocating the buffer.
   ClearSpan();
 
+=======
+  DCHECK_GE(capacity, 0);
+// Calling reallocate with size 0 and a non-null pointer causes memory leaks
+// on many platforms, since it may return nullptr while also not deallocating
+// the previously allocated memory.
+#if BUILDFLAG(IS_STARBOARD)
+  if (capacity == 0) {
+    real_data_.reset();
+    capacity_ = 0;
+    offset_ = 0;
+    data_ = nullptr;
+    return;
+  }
+#endif
+  // this will get reset in `set_offset`.
+  data_ = nullptr;
+>>>>>>> c467e28d27c (Fix: GrowableIOBuffer memory leak on SetCapacity(0) (#8183))
   // realloc will crash if it fails.
   real_data_.reset(
       static_cast<uint8_t*>(realloc(real_data_.release(), capacity)));
