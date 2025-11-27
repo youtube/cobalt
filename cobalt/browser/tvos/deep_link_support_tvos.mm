23b795242b40a2cc85525ed5cfeb9628ae9ef2e5
Julie Jeongeun Kim <jkim@igalia.com>
2025-11-27 14:50:04 +0900

    tvOS: Support App Intents (Siri and dictation)
    Patch by @jkim-julie with some suggestions by me.
    
    Bring back this feature from C25 but integrate it with the current Chrobalt
    architecture:
    
    * Do not use a completely separate Xcode project to build the Swift code
      that integrates with the App Intents framework. Instead, use Chromium's
      existing infrastructure to build Swift code with GN.
    
    * There is nothing Starboard-specific in the code since the UI bits are
      handled in Cobalt Shell, so the code now lives in `//cobalt`. There is no
      `SBProcessAppIntent()`, so the Swift code communicates directly with the
      Deep Link code via the newly-added DeepLinkSupportTvos interface.
    
    The behavior should match C25's and the official app's, including bugs and
    limitations that are described in the bugs below.
    
    Bug: 461350746
    Bug: 465023735

---
+++ cobalt/browser/tvos/deep_link_support_tvos.mm
@@ -0,0 +1,43 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "cobalt/browser/tvos/deep_link_support_tvos.h"
+
+#include "cobalt/browser/h5vcc_runtime/deep_link_manager.h"
+
+@implementation DeepLinkSupportTvos
+
++ (void)handleSiriIntents:(NSString*)query isSearch:(BOOL)isSearch {
+  if (query.length == 0) {
+    LOG(WARNING) << "AppIntent query is empty, ignoring.";
+    return;
+  }
+
+  NSString* urlBase =
+      [[NSBundle mainBundle] objectForInfoDictionaryKey:@"YTApplicationURL"];
+  if (!urlBase) {
+    LOG(WARNING) << "YTApplicationURL not in plist, ignoring AppIntent.";
+    return;
+  }
+
+  NSString* urlAction = isSearch ? @"?launch=voice&vs=11&va=search&vaa="
+                                 : @"?launch=voice&vs=11&va=play&vaa=";
+
+  const std::string deep_link = [[NSString
+      stringWithFormat:@"%@%@%@", urlBase, urlAction, query] UTF8String];
+  auto* manager = cobalt::browser::DeepLinkManager::GetInstance();
+  manager->OnDeepLink(deep_link);
+}
+
+@end
