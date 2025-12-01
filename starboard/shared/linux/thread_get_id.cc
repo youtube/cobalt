538c6eec7ecde75345dbac935f572a2124d1e77d
Kyujung Youn <kjyoun@google.com>
2025-12-01 14:27:59 -0800

    Gemini comments

--- starboard/shared/linux/thread_get_id.cc
+++ starboard/shared/linux/thread_get_id.cc
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <pthread.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
 #include <unistd.h>
