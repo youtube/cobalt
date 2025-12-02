b17379abd92a72f6664b7d414fbe8837b440bb83
Kyujung Youn <kjyoun@google.com>
2025-12-02 10:57:47 -0800

    starboard: gettid() for Android

---
+++ starboard/android/shared/thread_get_id.cc
@@ -0,0 +1,27 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <unistd.h>
+
+#include "starboard/thread.h"
+
+// static
+SbThreadId SbThreadGetId() {
+  // Note: do not cache the return value inside a thread_local variable on
+  // Android (as above). The reasons are:
+  // - thread_local is slow on Android (goes through emutls)
+  // - gettid() is fast, since its return value is cached in pthread (in the
+  //   thread control block of pthread). See gettid.c in bionic.
+  return static_cast<SbThreadId>(gettid());
+}
