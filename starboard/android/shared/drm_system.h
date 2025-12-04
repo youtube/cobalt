f2c41a73319edf03df110476676726390136ebdf
Kyujung Youn <kjyoun@google.com>
2025-12-04 13:35:44 -0800

    starboard: Use Starboard feature flag for DRM app provisioning (#8281)
    This change defines a new Starboard feature, kEnableAppProvisioning,
    to control app-level provisioning within DrmSystem. The feature state
    is now stored in a DrmSystem member variable, replacing direct checks
    against a compile-time constant. This centralizes the flag's state,
    improving consistency, readability, and efficiency.
    
    Bug: 79941850

--- starboard/android/shared/drm_system.h
+++ starboard/android/shared/drm_system.h
@@ -15,8 +15,6 @@
 #ifndef STARBOARD_ANDROID_SHARED_DRM_SYSTEM_H_
 #define STARBOARD_ANDROID_SHARED_DRM_SYSTEM_H_
 
-#include "starboard/shared/starboard/drm/drm_system_internal.h"
-
 #include <jni.h>
 
 #include <atomic>
@@ -31,6 +29,7 @@
 #include "starboard/android/shared/media_common.h"
 #include "starboard/android/shared/media_drm_bridge.h"
 #include "starboard/common/thread.h"
+#include "starboard/shared/starboard/drm/drm_system_internal.h"
 #include "starboard/shared/starboard/thread_checker.h"
 
 namespace starboard {
@@ -124,6 +123,7 @@ class DrmSystem : public ::SbDrmSystemPrivate,
   void Run() override;
 
   const std::string key_system_;
+  const bool enable_app_provisioning_;
   void* context_;
   SbDrmSessionUpdateRequestFunc update_request_callback_;
   SbDrmSessionUpdatedFunc session_updated_callback_;
