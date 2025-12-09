fb10b20ad87605a92023680436704728c2abe533
Adam Walls <avvall@google.com>
2025-12-08 19:33:33 -0800

    Remove file chooser tests

--- cobalt/testing/browser_tests/web_contents_impl_browsertest.cc
+++ cobalt/testing/browser_tests/web_contents_impl_browsertest.cc
@@ -2084,20 +2084,6 @@ class TestWCDelegateForDialogsAndFullscreen : public JavaScriptDialogManager,
   std::unique_ptr<base::RunLoop> run_loop_ = std::make_unique<base::RunLoop>();
 };
 
-class MockFileSelectListener : public FileChooserImpl::FileSelectListenerImpl {
- public:
-  MockFileSelectListener() : FileChooserImpl::FileSelectListenerImpl(nullptr) {
-    SetListenerFunctionCalledTrueForTesting();
-  }
-  void FileSelected(std::vector<blink::mojom::FileChooserFileInfoPtr> files,
-                    const base::FilePath& base_dir,
-                    blink::mojom::FileChooserParams::Mode mode) override {}
-  void FileSelectionCanceled() override {}
-
- private:
-  ~MockFileSelectListener() override = default;
-};
-
 // TODO(b/437415063): Investigate failing test.
 #if BUILDFLAG(IS_ANDROIDTV)
 #define MAYBE_JavaScriptDialogsInMainAndSubframes \
@@ -3257,21 +3243,6 @@ IN_PROC_BROWSER_TEST_F(WebContentsImplBrowserTest,
   EXPECT_TRUE(ExecJs(inner_contents, script));
   inner_test_delegate.Wait();
   EXPECT_FALSE(top_contents->IsFullscreen());
-}
-
-IN_PROC_BROWSER_TEST_F(WebContentsImplBrowserTest, FileChooserEndsFullscreen) {
-  WebContentsImpl* wc = static_cast<WebContentsImpl*>(shell()->web_contents());
-  TestWCDelegateForDialogsAndFullscreen test_delegate(wc);
-
-  GURL url("about:blank");
-  EXPECT_TRUE(NavigateToURL(shell(), url));
-
-  wc->EnterFullscreenMode(wc->GetPrimaryMainFrame(), {});
-  EXPECT_TRUE(wc->IsFullscreen());
-  wc->RunFileChooser(wc->GetPrimaryMainFrame(),
-                     base::MakeRefCounted<MockFileSelectListener>(),
-                     blink::mojom::FileChooserParams());
-  EXPECT_FALSE(wc->IsFullscreen());
 }
 
 IN_PROC_BROWSER_TEST_F(WebContentsImplBrowserTest,
