9e47ee112820b5548b8b41ff9c2619c07625ec47
fayev-g <fayev@google.com>
2025-12-08 16:33:25 -0800

    Batch 11: Move related headers and override clang-format

--- starboard/shared/starboard/file_storage/storage_get_record_size.cc
+++ starboard/shared/starboard/file_storage/storage_get_record_size.cc
@@ -12,8 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "starboard/common/file.h"
+// clang-format off
 #include "starboard/common/storage.h"
+// clang-format on
+
+#include "starboard/common/file.h"
 #include "starboard/shared/starboard/file_storage/storage_internal.h"
 
 int64_t SbStorageGetRecordSize(SbStorageRecord record) {
