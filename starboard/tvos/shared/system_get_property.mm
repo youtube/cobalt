d500e48930148c8b1cc95147e8d3069bc38439a1
Raphael Kubo da Costa <kubo@igalia.com>
2025-12-10 17:50:39 +0100

    tvOS: Get system_get_property.mm to build in the main branch (#8335)
    * Only try to include the keybox header in internal builds (which are
    broken at the moment anyway, see b/460479616).
    
    * For now, do nothing when the `kSbSystemPropertyCertificationScope`
    property is requested, as the current implementation depends on the
    keybox file mentioned above as well as some requirements that are
    unclear for Chrobalt (see b/464200271).
    
    Bug: 460479616
    Bug: 464200271

--- starboard/tvos/shared/system_get_property.mm
+++ starboard/tvos/shared/system_get_property.mm
@@ -20,8 +20,11 @@
 #include "starboard/common/device_type.h"
 #include "starboard/common/log.h"
 #include "starboard/common/string.h"
-#include "starboard/keyboxes/tvos/system_properties.h"
 #include "starboard/system.h"
+
+#if defined(INTERNAL_BUILD)
+#include "starboard/keyboxes/tvos/system_properties.h"
+#endif
 
 namespace {
 struct AppleTVDevice {
@@ -146,11 +149,10 @@ bool SbSystemGetProperty(SbSystemPropertyId property_id,
         return CopyStringAndTestIfSuccess(out_value, value_length,
                                           kPlatformName);
       case kSbSystemPropertyCertificationScope:
-        if (kCertificationScope[0] == '\0') {
-          return false;
-        }
-        return CopyStringAndTestIfSuccess(out_value, value_length,
-                                          kCertificationScope);
+        // TODO: b/460479616, b/464200271 - Reenable once the expected
+        // functionality is clear in Chrobalt.
+        SB_NOTIMPLEMENTED();
+        return false;
       case kSbSystemPropertyAdvertisingId:
       case kSbSystemPropertyLimitAdTracking: {
         SB_LOG(INFO) << "IFA is not supported via Starboard.";
