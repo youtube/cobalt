da80961ffa3e111da9b22baf9e2deb0ca22b823e
Raphael Kubo da Costa <kubo@igalia.com>
2025-12-11 14:59:20 +0100

    cobalt: Merge shell_application_ios.* into cobalt/app/main.mm
    `shell_application_ios.mm` was copied from Content Shell and does not work
    expected in Cobalt. Namely, it was instantiating a `ShellMainDelegate`
    instance instead of a `CobaltMainDelegate` one.
    
    To avoid making `cobalt/shell` directly reference `cobalt/browser`, it is
    easier to just merge the entire file into `cobalt/app/main.mm`, which now
    behaves more similarly to Starboard's `cobalt/app/cobalt.cc` (i.e. all the
    initial set up is done there, and the `//content` layer is launched from
    there).
    
    While here, also instantiate `cobalt::CommandLineProcessor` so that the
    right parameters are passed for tvOS.
    
    Note that connecting Cobalt and Starboard will be done in a separate commit.
    
    Patch by @abhijeetk and myself.
    
    Bug: 456692793

--- cobalt/app/main.mm
+++ cobalt/app/main.mm
@@ -12,12 +12,135 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#import <UIKit/UIKit.h>
+
+#include <algorithm>
+#include <iterator>
+#include <memory>
+#include <string>
+#include <vector>
+
 #include "base/at_exit.h"
-#include "cobalt/shell/app/ios/shell_application_ios.h"
+#include "base/command_line.h"
+#include "cobalt/app/cobalt_main_delegate.h"
+#include "cobalt/app/cobalt_switch_defaults.h"
+#include "cobalt/shell/browser/shell.h"
+#include "content/public/app/content_main.h"
+#include "content/public/app/content_main_runner.h"
+
+static int g_argc = 0;
+static const char** g_argv = nullptr;
+static std::unique_ptr<content::ContentMainRunner> g_main_runner;
+static std::unique_ptr<content::ShellMainDelegate> g_main_delegate;
+
+@interface CobaltAppSceneDelegate : UIResponder <UIWindowSceneDelegate>
+@end
+
+@implementation CobaltAppSceneDelegate
+
+- (void)scene:(UIScene*)scene
+    willConnectToSession:(UISceneSession*)session
+                 options:(UISceneConnectionOptions*)connectionOptions {
+  CHECK_EQ(1u, content::Shell::windows().size());
+  UIWindow* window = content::Shell::windows()[0]->window().Get();
+
+  // The rootViewController must be added after a windowScene is set
+  // so stash it in a temp variable and then reattach it. If we don't
+  // do this the safe area gets screwed up on orientation changes.
+  UIViewController* controller = window.rootViewController;
+  window.rootViewController = nil;
+  window.windowScene = (UIWindowScene*)scene;
+  window.rootViewController = controller;
+  [window makeKeyAndVisible];
+}
+
+@end
+
+@interface CobaltAppDelegate : UIResponder <UIApplicationDelegate>
+@end
+
+@implementation CobaltAppDelegate
+
+- (UISceneConfiguration*)application:(UIApplication*)application
+    configurationForConnectingSceneSession:
+        (UISceneSession*)connectingSceneSession
+                                   options:(UISceneConnectionOptions*)options {
+  UISceneConfiguration* configuration =
+      [[UISceneConfiguration alloc] initWithName:nil
+                                     sessionRole:connectingSceneSession.role];
+  configuration.delegateClass = CobaltAppSceneDelegate.class;
+  return configuration;
+}
+
+- (BOOL)application:(UIApplication*)application
+    willFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
+  const cobalt::CommandLinePreprocessor cobalt_cmd_line(g_argc, g_argv);
+  const base::CommandLine::StringVector& processed_argv =
+      cobalt_cmd_line.argv();
+
+  // content::ContentMainParams::argv uses regular char** for `argv`, so perform
+  // a type conversion here. Note that even though `char_argv` is dependent on
+  // the function-scoped `processed_argv`, content::RunContentProcess() copies
+  // the data by creating its own base::CommandLine instance.
+  std::vector<const char*> char_argv;
+  std::ranges::transform(processed_argv, std::back_inserter(char_argv),
+                         [](const std::string& arg) { return arg.c_str(); });
+
+  g_main_delegate = std::make_unique<cobalt::CobaltMainDelegate>();
+  g_main_runner = content::ContentMainRunner::Create();
+  content::ContentMainParams params(g_main_delegate.get());
+  params.argc = char_argv.size();
+  params.argv = char_argv.data();
+  content::RunContentProcess(std::move(params), g_main_runner.get());
+
+  return YES;
+}
+
+- (BOOL)application:(UIApplication*)application
+    didFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
+  return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication*)application {
+}
+
+- (void)applicationDidEnterBackground:(UIApplication*)application {
+}
+
+- (void)applicationWillEnterForeground:(UIApplication*)application {
+}
+
+- (void)applicationDidBecomeActive:(UIApplication*)application {
+}
+
+- (void)applicationWillTerminate:(UIApplication*)application {
+}
+
+- (BOOL)application:(UIApplication*)application
+    shouldSaveApplicationState:(NSCoder*)coder {
+  return YES;
+}
+
+- (BOOL)application:(UIApplication*)application
+    shouldRestoreApplicationState:(NSCoder*)coder {
+  // TODO(crbug.com/710329): Make this value configurable in the settings.
+  return YES;
+}
+
+@end
 
 int main(int argc, const char** argv) {
   // Create this here since it's needed to start the crash handler.
   base::AtExitManager at_exit;
-  // For now, just launch the default application from the shell.
-  return RunShellApplication(argc, argv);
+
+  // Even though it is possible to just use `[[NSProcessInfo processInfo]
+  // arguments]` later to get the same data, this is simpler since there is no
+  // need to convert the data from an NSArray<NSString*>*.
+  g_argc = argc;
+  g_argv = argv;
+
+  @autoreleasepool {
+    return UIApplicationMain(argc, const_cast<char**>(argv), nil,
+                             NSStringFromClass([CobaltAppDelegate class]));
+  }
 }
