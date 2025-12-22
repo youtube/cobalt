b43202efdae4cc7b97ff63991efe3223cfd2a4e4
Hao <131711973+haozheng-cobalt@users.noreply.github.com>
2025-12-22 13:00:31 -0800

    Add enabled to TextToSpeechChangeEvent and integrate with kSbEventTyp… (#8324)
    …eAccessibilityTextToSpeechSettingsChanged
    
    This change introduces a dedicated H5vccAccessibilityManager to
    centralize
    the handling of Text-to-Speech (TTS) state changes from the platform.
    It refactors the existing event propagation by replacing the direct
    TextToSpeechObserver pattern in Starboard with a new manager.
    
    The Mojo interface for accessibility events is updated to include an
    'enabled' boolean state, providing web applications with the current
    TTS status. The platform event
    kSbEventTypeAccessibilityTextToSpeechSettingsChanged is now integrated
    to notify the manager of system-level changes, which then dispatches
    a new TextToSpeechChangeEvent in Blink, carrying the updated
    'enabled' attribute.
    
    This improves the overall architecture for handling accessibility events
    and ensures a consistent and accurate reporting of the TTS state.
    
    Bug: 376150525
    
    Test: verified that the ontexttospeechchange API is invoked as expected
    on ATV: https://screenshot.googleplex.com/95JJnZsoFBHjqUR
    
    Bug: 376150525
    (cherry picked from commit 6f01687ef658d66bd08b774fd11114cdc87ce24f)

--- cobalt/app/cobalt.cc
+++ cobalt/app/cobalt.cc
@@ -30,6 +30,11 @@
 #include "cobalt/app/cobalt_main_delegate.h"
 #include "cobalt/app/cobalt_switch_defaults_starboard.h"
 #include "cobalt/browser/cobalt_content_browser_client.h"
+<<<<<<< HEAD
+=======
+#include "cobalt/browser/h5vcc_accessibility/h5vcc_accessibility_manager.h"
+#include "cobalt/browser/h5vcc_runtime/deep_link_manager.h"
+>>>>>>> 6f01687ef65 (Add enabled to TextToSpeechChangeEvent and integrate with kSbEventTyp… (#8324))
 #include "cobalt/shell/browser/shell.h"
 #include "cobalt/shell/browser/shell_paths.h"
 #include "content/public/app/content_main.h"
@@ -176,7 +181,19 @@ void SbEventHandle(const SbEvent* event) {
         g_platform_event_source->HandleEvent(event);
       }
       break;
+<<<<<<< HEAD
     case kSbEventTypeLink:
+=======
+    }
+    case kSbEventTypeAccessibilityTextToSpeechSettingsChanged: {
+      if (event->data) {
+        auto* enabled = static_cast<const bool*>(event->data);
+        cobalt::browser::H5vccAccessibilityManager::GetInstance()
+            ->OnTextToSpeechStateChanged(*enabled);
+      }
+      break;
+    }
+>>>>>>> 6f01687ef65 (Add enabled to TextToSpeechChangeEvent and integrate with kSbEventTyp… (#8324))
     case kSbEventTypeVerticalSync:
     case kSbEventTypeScheduled:
     case kSbEventTypeLowMemory:
