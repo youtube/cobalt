9ce856e0144221a1ffa29946f116f56b084d804a
Adam Walls <avvall@google.com>
2026-01-08 14:06:56 -0800

    feat(code-coverage): Add a bridge for cobalt code coverage
    This change introduces a bridge script for running code coverage on Cobalt.
    The new script cobalt_code_coverage_bridge.py is a wrapper around the existing tools/code_coverage/coverage.py script.
    
    The main purpose of this bridge is to:
    - Prevent the underlying script from calling update.py --package.
    - Automatically provide the path to the LLVM tools required for code coverage, by setting the --coverage-tools-dir argument.
    
    Tests are also added to ensure the bridge script works as expected.

---
+++ cobalt/tools/code_coverage/test_cobalt_code_coverage_bridge.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# Copyright 2025 The Cobalt Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Tests for the cobalt_code_coverage_bridge.py script.
+"""
+
+import cobalt_code_coverage_bridge
+import sys
+import unittest
+from unittest import mock
+
+
+class CodeCoverageToolTest(unittest.TestCase):
+  """
+  Tests for the code coverage tool.
+  """
+
+  @mock.patch('os.path.isdir', return_value=True)
+  @mock.patch(
+      'os.listdir',
+      return_value=[
+          'bin', 'cr_build_revision', 'lib', 'llvmobjdump_build_revision'
+      ])
+  @mock.patch('subprocess.check_call')
+  # pylint: disable=C0103
+  def test_coverage_tools_dir_not_provided(self, mock_check_call, _mock_listdir,
+                                           _mock_isdir):
+    """
+    Tests that the tool adds the --coverage-tools-dir argument when it's not
+    provided.
+    """
+    del _mock_listdir  # Unused argument, as per user instruction.
+    del _mock_isdir  # Unused argument, as per user instruction.
+    sys.argv = ['cobalt_code_coverage_bridge.py', 'test_arg']
+    cobalt_code_coverage_bridge.main()
+    mock_check_call.assert_called_once_with([
+        sys.executable,
+        cobalt_code_coverage_bridge.COVERAGE_PY_PATH,
+        'test_arg',
+        '--coverage-tools-dir',
+        cobalt_code_coverage_bridge.LLVM_BIN_DIR,
+    ])
+
+  @mock.patch('subprocess.check_call')
+  def test_coverage_tools_dir_provided(self, mock_check_call):
+    """
+    Tests that the tool doesn't add the --coverage-tools-dir argument when it's
+    already provided.
+    """
+    sys.argv = [
+        'cobalt_code_coverage_bridge.py',
+        '--coverage-tools-dir',
+        '/my/custom/dir',
+        'test_arg',
+    ]
+    cobalt_code_coverage_bridge.main()
+    mock_check_call.assert_called_once_with([
+        sys.executable,
+        cobalt_code_coverage_bridge.COVERAGE_PY_PATH,
+        '--coverage-tools-dir',
+        '/my/custom/dir',
+        'test_arg',
+    ])
+
+  @mock.patch('os.path.isdir', return_value=True)
+  @mock.patch(
+      'os.listdir',
+      return_value=[
+          'bin', 'cr_build_revision', 'lib', 'llvmobjdump_build_revision'
+      ])
+  @mock.patch('builtins.print')
+  # pylint: disable=C0103
+  def test_package_argument_provided(self, mock_print, _mock_listdir,
+                                     _mock_isdir):
+    """
+    Tests that the tool prints an error message and returns 1 when the
+    --package argument is provided.
+    """
+
+
+if __name__ == '__main__':
+  unittest.main()
