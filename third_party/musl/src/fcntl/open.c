8d9fb20519a7cdc6df0449cf1e5eeb81b952509f
Andrew Savage <andrewsavage@google.com>
2026-01-09 17:33:12 +0000

    Use upstream code and add to symbol list

--- third_party/musl/src/fcntl/open.c
+++ third_party/musl/src/fcntl/open.c
@@ -12,5 +12,10 @@ int open(const char *filename, int flags, ...)
 		mode = va_arg(ap, mode_t);
 		va_end(ap);
 	}
-	return openat(AT_FDCWD, filename, flags, mode);
+
+	int fd = __sys_open_cp(filename, flags, mode);
+	if (fd>=0 && (flags & O_CLOEXEC))
+		__syscall(SYS_fcntl, fd, F_SETFD, FD_CLOEXEC);
+
+	return __syscall_ret(fd);
 }
