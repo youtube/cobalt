d919555593b9a007c2d9dc3a4e42ee4c729dcb13
Madhura Jayaraman <madjay@google.com>
2026-01-09 09:33:11 -0800

    Mojo implementation for h5vcc updater functions

--- cobalt/browser/h5vcc_updater/h5vcc_updater_impl.cc
+++ cobalt/browser/h5vcc_updater/h5vcc_updater_impl.cc
@@ -27,6 +27,10 @@
 #include "starboard/system.h"
 #endif
 
+namespace {
+const uint16_t kInvalidInstallationIndex = 1000;
+}
+
 namespace h5vcc_updater {
 
 H5vccUpdaterImpl::H5vccUpdaterImpl(
@@ -49,42 +53,115 @@ void H5vccUpdaterImpl::Create(
 
 void H5vccUpdaterImpl::GetUpdaterChannel(GetUpdaterChannelCallback callback) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+#if BUILDFLAG(USE_EVERGREEN)
+  auto* updater_module = cobalt::updater::UpdaterModule::GetInstance();
+  if (!updater_module) {
+    std::move(callback).Run("");
+    return;
+  }
+  std::move(callback).Run(updater_module->GetUpdaterChannel());
+#else
   NOTIMPLEMENTED();
+#endif
 }
 
 void H5vccUpdaterImpl::SetUpdaterChannel(const std::string& channel,
                                          SetUpdaterChannelCallback callback) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+#if BUILDFLAG(USE_EVERGREEN)
+  auto* updater_module = cobalt::updater::UpdaterModule::GetInstance();
+  if (updater_module) {
+    // Force an update if SetUpdaterChannel is called, even if
+    // the channel doesn't change, as long as it's not
+    // "prod"/"experiment"/"control" channel
+    if (updater_module->GetUpdaterChannel().compare(channel) != 0) {
+      updater_module->SetUpdaterChannel(channel);
+    } else if (channel == "prod" || channel == "experiment" ||
+               channel == "control") {
+      std::move(callback).Run();
+      return;
+    }
+    updater_module->CompareAndSwapForcedUpdate(0, 1);
+    updater_module->RunUpdateCheck();
+  }
+  std::move(callback).Run();
+#else
   NOTIMPLEMENTED();
+#endif
 }
 
 void H5vccUpdaterImpl::GetUpdateStatus(GetUpdateStatusCallback callback) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+#if BUILDFLAG(USE_EVERGREEN)
+  auto* updater_module = cobalt::updater::UpdaterModule::GetInstance();
+  if (!updater_module) {
+    std::move(callback).Run("");
+    return;
+  }
+  std::move(callback).Run(updater_module->GetUpdaterStatus());
+#else
   NOTIMPLEMENTED();
+#endif
 }
 
 void H5vccUpdaterImpl::ResetInstallations(ResetInstallationsCallback callback) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+#if BUILDFLAG(USE_EVERGREEN)
+  auto* updater_module = cobalt::updater::UpdaterModule::GetInstance();
+  if (updater_module) {
+    updater_module->ResetInstallations();
+  }
+  std::move(callback).Run();
+#else
   NOTIMPLEMENTED();
+#endif
 }
 
 void H5vccUpdaterImpl::GetInstallationIndex(
     GetInstallationIndexCallback callback) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+#if BUILDFLAG(USE_EVERGREEN)
+  auto* updater_module = cobalt::updater::UpdaterModule::GetInstance();
+  if (!updater_module) {
+    std::move(callback).Run(kInvalidInstallationIndex);
+    return;
+  }
+  int index = updater_module->GetInstallationIndex();
+  std::move(callback).Run(index == -1 ? kInvalidInstallationIndex
+                                      : static_cast<uint16_t>(index));
+#else
   NOTIMPLEMENTED();
+#endif
 }
 
 void H5vccUpdaterImpl::GetAllowSelfSignedPackages(
     GetAllowSelfSignedPackagesCallback callback) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+#if BUILDFLAG(USE_EVERGREEN)
+  auto* updater_module = cobalt::updater::UpdaterModule::GetInstance();
+  if (!updater_module) {
+    std::move(callback).Run(false);
+    return;
+  }
+  std::move(callback).Run(updater_module->GetAllowSelfSignedPackages());
+#else
   NOTIMPLEMENTED();
+#endif
 }
 
 void H5vccUpdaterImpl::SetAllowSelfSignedPackages(
     bool allow_self_signed_packages,
     SetAllowSelfSignedPackagesCallback callback) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+#if !BUILDFLAG(COBALT_IS_RELEASE_BUILD) && ALLOW_EVERGREEN_SIDELOADING
+  auto* updater_module = cobalt::updater::UpdaterModule::GetInstance();
+  if (updater_module) {
+    updater_module->SetAllowSelfSignedPackages(allow_self_signed_packages);
+  }
+  std::move(callback).Run();
+#else
   NOTIMPLEMENTED();
+#endif
 }
 
 void H5vccUpdaterImpl::GetUpdateServerUrl(GetUpdateServerUrlCallback callback) {
@@ -95,9 +172,6 @@ void H5vccUpdaterImpl::GetUpdateServerUrl(GetUpdateServerUrlCallback callback) {
     std::string url = updater_module->GetUpdateServerUrl();
     std::move(callback).Run(url);
   } else {
-    LOG(ERROR)
-        << "H5vccUpdaterImpl::GetUpdateServerUrl: UpdaterModule instance "
-           "is not available.";
     std::move(callback).Run("");
   }
 #else
@@ -108,20 +182,45 @@ void H5vccUpdaterImpl::GetUpdateServerUrl(GetUpdateServerUrlCallback callback) {
 void H5vccUpdaterImpl::SetUpdateServerUrl(const std::string& update_server_url,
                                           SetUpdateServerUrlCallback callback) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+#if !BUILDFLAG(COBALT_IS_RELEASE_BUILD) && ALLOW_EVERGREEN_SIDELOADING
+  auto* updater_module = cobalt::updater::UpdaterModule::GetInstance();
+  if (updater_module) {
+    updater_module->SetUpdateServerUrl(update_server_url);
+  }
+  std::move(callback).Run();
+#else
   NOTIMPLEMENTED();
+#endif
 }
 
 void H5vccUpdaterImpl::GetRequireNetworkEncryption(
     GetRequireNetworkEncryptionCallback callback) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+#if BUILDFLAG(USE_EVERGREEN)
+  auto* updater_module = cobalt::updater::UpdaterModule::GetInstance();
+  if (!updater_module) {
+    std::move(callback).Run(false);
+    return;
+  }
+  std::move(callback).Run(updater_module->GetRequireNetworkEncryption());
+#else
   NOTIMPLEMENTED();
+#endif
 }
 
 void H5vccUpdaterImpl::SetRequireNetworkEncryption(
     bool require_network_encryption,
     SetRequireNetworkEncryptionCallback callback) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+#if !BUILDFLAG(COBALT_IS_RELEASE_BUILD) && ALLOW_EVERGREEN_SIDELOADING
+  auto* updater_module = cobalt::updater::UpdaterModule::GetInstance();
+  if (updater_module) {
+    updater_module->SetRequireNetworkEncryption(require_network_encryption);
+  }
+  std::move(callback).Run();
+#else
   NOTIMPLEMENTED();
+#endif
 }
 
 void H5vccUpdaterImpl::GetLibrarySha256(unsigned short index,
