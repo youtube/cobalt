1ed3015e33c53a8536a6ae51ae315a163e52e6e6
Bo-Rong Chen <borongchen@google.com>
2026-01-12 12:36:29 -0800

    cobalt: improve loading web contents during app startup

--- cobalt/shell/browser/shell.cc
+++ cobalt/shell/browser/shell.cc
@@ -171,7 +171,9 @@ Shell::Shell(std::unique_ptr<WebContents> web_contents,
     splash_state_ = STATE_SPLASH_SCREEN_INITIALIZED;
     splash_screen_web_contents_observer_ =
         std::make_unique<SplashScreenWebContentsObserver>(
-            splash_screen_web_contents_.get());
+            splash_screen_web_contents_.get(),
+            base::BindOnce(&Shell::OnSplashScreenLoadComplete,
+                           weak_factory_.GetWeakPtr()));
     splash_screen_web_contents_delegate_ =
         std::make_unique<SplashScreenWebContentsDelegate>(
             base::BindPostTaskToCurrentDefault(
@@ -435,7 +437,6 @@ void Shell::LoadSplashScreenWebContents() {
     // Display splash screen.
     LOG(INFO) << "NativeSplash: Loading splash screen WebContents.";
     splash_state_ = STATE_SPLASH_SCREEN_STARTED;
-    splash_screen_start_time_ = base::TimeTicks::Now();
     GetPlatform()->LoadSplashScreenContents(this);
 
     GURL splash_screen_url = GURL(switches::kSplashScreenURL);
@@ -946,6 +947,10 @@ void Shell::LoadProgressChanged(double progress) {
 }
 
 void Shell::ScheduleSwitchToMainWebContents() {
+  if (splash_screen_start_time_.is_null()) {
+    LOG(INFO) << "NativeSplash: Splash screen not loaded yet, waiting.";
+    return;
+  }
   base::TimeDelta splash_screen_elapsed =
       base::TimeTicks::Now() - splash_screen_start_time_;
 
@@ -998,6 +1003,7 @@ void Shell::SwitchToMainWebContents() {
     has_switched_to_main_frame_ = true;
     if (web_contents_) {
       GetPlatform()->UpdateContents(this);
+      web_contents_->WasShown();
       if (web_contents()->GetRenderWidgetHostView()) {
         web_contents()->GetRenderWidgetHostView()->Focus();
       }
@@ -1006,6 +1012,16 @@ void Shell::SwitchToMainWebContents() {
       splash_screen_web_contents_.reset();
       splash_screen_web_contents_observer_.reset();
       splash_screen_web_contents_delegate_.reset();
+    }
+  }
+}
+
+void Shell::OnSplashScreenLoadComplete() {
+  if (splash_state_ >= STATE_SPLASH_SCREEN_STARTED &&
+      splash_screen_start_time_.is_null()) {
+    splash_screen_start_time_ = base::TimeTicks::Now();
+    if (is_main_frame_loaded_) {
+      ScheduleSwitchToMainWebContents();
     }
   }
 }
