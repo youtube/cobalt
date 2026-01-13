8e75b456aa74201f82cf5e8301174b6cfa98641c
Bo-Rong Chen <borongchen@google.com>
2026-01-12 20:02:15 -0800

    cobalt: Add switch to disable native splash screen

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
@@ -82,6 +82,9 @@ public abstract class CobaltActivity extends Activity {
 
   private static final Pattern URL_PARAM_PATTERN = Pattern.compile("^[a-zA-Z0-9_=]*$");
 
+  public static final String SPLASH_ARGS_KEY = "disableNativeSplash";
+  private boolean disableNativeSplash;
+
   // Maintain the list of JavaScript-exposed objects as a member variable
   // to prevent them from being garbage collected prematurely.
   private List<CobaltJavaScriptAndroidObject> javaScriptAndroidObjectList = new ArrayList<>();
@@ -109,6 +112,7 @@ public abstract class CobaltActivity extends Activity {
   // Initially copied from ContentShellActiviy.java
   protected void createContent(final Bundle savedInstanceState) {
     // Initializing the command line must occur before loading the library.
+    disableNativeSplash = getIntent().getBooleanExtra(SPLASH_ARGS_KEY, false);
     if (!CommandLine.isInitialized()) {
       CommandLine.init(null);
 
@@ -255,8 +259,10 @@ public abstract class CobaltActivity extends Activity {
                   }
                 };
 
-            // Load splash screen.
-            mShellManager.getActiveShell().loadSplashScreenWebContents();
+            if (!disableNativeSplash) {
+              // Load splash screen.
+              mShellManager.getActiveShell().loadSplashScreenWebContents();
+            }
           }
         });
   }
