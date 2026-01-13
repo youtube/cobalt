6abfbd593c0e604ba6b3a51644fb8a1f82d026ac
Arjun Menon <agmenon@google.com>
2026-01-13 10:47:49 -0800

    Refactor IP checks for custom cobalt CSP directive (#8549)
    Previously we used string manipulation and bitwise operations but we
    should make use of //base and //net implementations to streamline it.
    
    Bug: 470105792
    (cherry picked from commit 0147fa5bd7edba6af87d696142d6b46f93b3509e)

--- third_party/blink/renderer/core/frame/csp/local_ip.cc
+++ third_party/blink/renderer/core/frame/csp/local_ip.cc
@@ -8,50 +8,32 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+#include "net/base/ip_address.h"
+#include "net/base/url_util.h"
+#include "services/network/public/cpp/ip_address_space_util.h"
+#include "services/network/public/mojom/ip_address_space.mojom.h"
 #include "third_party/blink/public/platform/platform.h"
+
+namespace blink {
 
 namespace {
 
-// TODO(b/470105792): Refactor the implementation of the private range IP checks
-// to avoid string manipulation and bitwise operations.
+std::optional<net::IPAddress> ExtractIPv4From6to4(const net::IPAddress& addr) {
+  // Check if it's IPv6 and starts with the 6to4 prefix: 20:02 (0x2002)
+  constexpr uint8_t k6to4PrefixUpperByte = 0x20;
+  constexpr uint8_t k6to4PrefixLowerByte = 0x02;
 
-std::string SanitizeIPAddressStr(const std::string& ip_str) {
-  std::string clean_ip = ip_str;
-  size_t start = clean_ip.find('[');
-  size_t end = clean_ip.find(']');
-  if (start != std::string::npos && end != std::string::npos) {
-      clean_ip = clean_ip.substr(start + 1, end - start - 1);
+  const auto bytes = addr.bytes();
+  if (!addr.IsIPv6() || bytes[0] != k6to4PrefixUpperByte || bytes[1] != k6to4PrefixLowerByte) {
+    return std::nullopt;
   }
-  return clean_ip;
+
+  // Extract the 4 bytes starting at index 2 for the embedded IPv4 address.
+  return net::IPAddress(bytes[2], bytes[3], bytes[4], bytes[5]);
 }
 
-bool IsIPInPrivateRangeIPv4(uint32_t addr) {
-  // NOTE: Input address must be in host-byte order.
+}  // namespace
 
-  // For IPv4, the private address range is defined by RFC 1918
-  // available at https://tools.ietf.org/html/rfc1918#section-3.
-
-  // 10.0.0.0/8
-  if ((addr & 0xFF000000) == 0x0A000000) {
-    return true;
-  }
-
-  // 172.16.0.0/12 (172.16.0.0 to 172.31.255.255)
-  if ((addr & 0xFFF00000) == 0xAC100000) {
-    return true;
-  }
-
-  // 192.168.0.0/16
-  if ((addr & 0xFFFF0000) == 0xC0A80000) {
-    return true;
-  }
-
-  return false;
-}
-
-}
-
-namespace blink {
 // Queries host IP address and matches against netmask of target ip to determine.
 bool IsIPInLocalNetwork(const std::string& target_ip_str) {
     if (target_ip_str == "localhost") {
@@ -94,18 +76,15 @@ bool IsIPInLocalNetwork(const std::string& target_ip_str) {
 }
 
 bool IsIPInPrivateRange(const std::string& raw_ip_str) {
-  // Removes enclosing '[]' if present.
-  std::string ip_str = SanitizeIPAddressStr(raw_ip_str);
+  net::IPAddress address;
 
-  // For IPv4, the private address range is defined by RFC 1918
-  // available at https://tools.ietf.org/html/rfc1918#section-3.
-  struct in_addr ipv4_addr;
-  if (inet_pton(AF_INET, ip_str.c_str(), &ipv4_addr) == 1) {
-      // Convert to host byte order for easier comparison
-      uint32_t addr = ntohl(ipv4_addr.s_addr);
-      return IsIPInPrivateRangeIPv4(addr);
+  // Parse the string into an IPAddress object
+  if (!net::ParseURLHostnameToAddress(raw_ip_str, &address)) {
+    LOG(ERROR) << "Received IP address is not valid: " << raw_ip_str;
+    return false;
   }
 
+<<<<<<< HEAD
   struct in6_addr ipv6_addr;
   if (inet_pton(AF_INET6, ip_str.c_str(), &ipv6_addr) == 1) {
     // Check for the 2002::/16 prefix (6to4 address).
@@ -128,11 +107,20 @@ bool IsIPInPrivateRange(const std::string& raw_ip_str) {
     // This covers both fc00::/8 and fd00::/8
     // We check the first byte: (byte & 11111110) == 11111100
     return (ipv6_addr.s6_addr[0] & 0xFE) == 0xFC;
+=======
+  std::optional<net::IPAddress> addr_ipv4 = ExtractIPv4From6to4(address);
+  if (addr_ipv4) {
+    // This is potentially breaking convention since 6to4 addresses are
+    // typically used as global unicast addresses routed by public relays.
+    // However, due to Vega's testing implementations we need this exception to
+    // allow such connections under the Cobalt CSP directive
+    // "cobalt-insecure-private-range".
+    LOG(WARNING) << "Treating 6to4 address as IPv4 for Private Range check.";
+    address = addr_ipv4.value();
+>>>>>>> 0147fa5bd7e (Refactor IP checks for custom cobalt CSP directive (#8549))
   }
 
-  // Not a valid IPv4 or IPv6 address
-  LOG(ERROR) << "Received non-IPv4 non-IPv6 address to check: " << raw_ip_str.c_str();
-  return false;
+  return network::IPAddressToIPAddressSpace(address) == network::mojom::IPAddressSpace::kPrivate;
 }
 
 }  // namespace blink
