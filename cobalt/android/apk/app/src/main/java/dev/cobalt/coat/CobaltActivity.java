fc34e084ebc3dece8b7f66fbd15c9469ed906c30
Bo-Rong Chen <borongchen@google.com>
2026-01-14 10:45:10 -0800

    Add missing HANG_APP_CRASH_TIMEOUT_SECONDS

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
@@ -84,6 +84,9 @@ public abstract class CobaltActivity extends Activity {
 
   private static final Pattern URL_PARAM_PATTERN = Pattern.compile("^[a-zA-Z0-9_=]*$");
 
+  // How many seconds before the app exits if it fails to land YouTube home page.
+  private static final int HANG_APP_CRASH_TIMEOUT_SECONDS = 60;
+
   // Maintain the list of JavaScript-exposed objects as a member variable
   // to prevent them from being garbage collected prematurely.
   private List<CobaltJavaScriptAndroidObject> javaScriptAndroidObjectList = new ArrayList<>();
