b0afae4150a3f0833a099788eb8f160b5476be4a
Xinan Lin <linxinan@google.com>
2026-01-16 18:26:06 +0000

    should not pass sharding parameter to test run as we already handle it in gtest-list

--- cobalt/testing/browser_tests/cobalt_browsertests_runner.sh
+++ cobalt/testing/browser_tests/cobalt_browsertests_runner.sh
@@ -58,13 +58,13 @@ if [ "$SHARD_INDEX" = "0" ] && [ -n "$GTEST_SHARD_INDEX" ]; then
 fi
 
 echo "Running tests from: $BINARY_PATH"
-if [ -n "$XML_OUTPUT_FILE" ]; then
-  echo "Merging results to: $XML_OUTPUT_FILE"
-  # Initialize the final XML file
-  echo '<?xml version="1.0" encoding="UTF-8"?>' > "$XML_OUTPUT_FILE"
-  echo '<testsuites tests="0" failures="0" disabled="0" errors="0" time="0" name="AllTests">' >> "$XML_OUTPUT_FILE"
-fi
-
+    if [ -n "$XML_OUTPUT_FILE" ]; then
+      echo "Merging results to: $XML_OUTPUT_FILE"
+      # Initialize the final XML file
+      echo '<?xml version="1.0" encoding="UTF-8"?>' > "$XML_OUTPUT_FILE"
+      echo '<testsuites tests="0" failures="0" disabled="0" errors="0" time="0" name="AllTests">' \
+        >> "$XML_OUTPUT_FILE"
+    fi
 # 1. List all tests using the binary
 # We rely on awk to parse the "Suite.\n  Test" format into "Suite.Test"
 # We also filter out DISABLED_ tests
@@ -104,7 +104,9 @@ for TEST in $TESTS_TO_RUN; do
   echo "[RUN] $TEST"
 
   # Construct flags
-  RUN_FLAGS="--gtest_filter=$TEST --no-sandbox --single-process --no-zygote --ozone-platform=starboard $EXTRA_ARGS"
+  RUN_FLAGS="--gtest_filter=$TEST --single-process-tests --no-sandbox"
+  RUN_FLAGS="$RUN_FLAGS --single-process --no-zygote"
+  RUN_FLAGS="$RUN_FLAGS --ozone-platform=starboard $EXTRA_ARGS"
 
   if [ -n "$XML_OUTPUT_FILE" ]; then
     RUN_FLAGS="$RUN_FLAGS --gtest_output=xml:$TEMP_XML"
@@ -113,7 +115,12 @@ for TEST in $TESTS_TO_RUN; do
   # Run the test
   # We use 'set +e' to allow failure without exiting the script
   set +e
-  $BINARY_PATH $RUN_FLAGS
+
+  # Explicitly disable sharding for the individual test run because we already
+  # filtered the tests in this script. The binary should run exactly what
+  # we ask. Also unset the env vars for this command.
+  GTEST_TOTAL_SHARDS=1 GTEST_SHARD_INDEX=0 $BINARY_PATH $RUN_FLAGS \
+    --test-launcher-total-shards=1 --test-launcher-shard-index=0
   RET=$?
   set -e
 
@@ -142,7 +149,8 @@ if [ -n "$XML_OUTPUT_FILE" ]; then
   # Update the counts in the header.
   # We use sed to replace the placeholder "0"s.
   # Using a temp file for sed portability (some versions don't support -i well).
-  sed "s/tests=\"0\"/tests=\"$TOTAL_RUN\"/; s/failures=\"0\"/failures=\"$FAILED_COUNT\"/" "$XML_OUTPUT_FILE" > "${XML_OUTPUT_FILE}.tmp"
+  sed "s/tests=\"0\"/tests=\"$TOTAL_RUN\"/; s/failures=\"0\"/failures=\"$FAILED_COUNT\"/" \
+    "$XML_OUTPUT_FILE" > "${XML_OUTPUT_FILE}.tmp"
   mv "${XML_OUTPUT_FILE}.tmp" "$XML_OUTPUT_FILE"
 fi
 
