38d78e53da05b851803ad3f72a2bf7811f1f9508
John Ocampo <johneocampo@google.com>
2026-01-26 17:23:50 +0000

    3p: Add nl_langinfo date_formatting (#8705)
    Implement locale-aware date and time formatting for nl_langinfo
    `D_FMT`, `T_FMT`, `D_T_FMT`, and `T_FMT_AMPM` items. Previously, these
    formats were hardcoded, leading to non-localized date and time
    representations.
    
    This change utilizes ICU's DateTimePatternGenerator and date formatters
    to dynamically generate locale-specific patterns. These ICU patterns are
    then translated into POSIX strftime format specifiers, ensuring proper
    internationalization of date and time strings according to the active
    locale settings.
    
    Bug: 466160361
    (cherry picked from commit 370fa080554bf810a2503f851609d2e76769ca00)

--- cobalt/common/libc/locale/nl_langinfo_support.h
+++ cobalt/common/libc/locale/nl_langinfo_support.h
@@ -36,6 +36,11 @@ std::string GetLocalizedDateSymbol(const std::string& locale,
 // supports the |RADIXCHAR| and |THOUSEP| nl_items.
 std::string NlGetNumericData(const std::string& locale, nl_item type);
 
+// Retrieves the date formatting string for |D_FMT|, |T_FMT|, |D_T_FMT|, and
+// |T_FMT_AMPM|. This function will first retrieve the date/time pattern stored
+// in ICU and will return the POSIX equivalent of that pattern.
+std::string GetPosixPattern(const std::string& locale, nl_item item);
+
 }  //  namespace cobalt
 
 #endif  // COBALT_COMMON_LIBC_LOCALE_NL_LANGINFO_SUPPORT_H_
