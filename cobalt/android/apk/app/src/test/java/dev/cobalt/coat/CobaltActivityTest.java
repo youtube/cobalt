387089f6b0a2671654298c1390efaf2bfb9b6475
Colin Liang <colinliang@google.com>
2026-01-27 13:52:53 -0800

    a

--- cobalt/android/apk/app/src/test/java/dev/cobalt/coat/CobaltActivityTest.java
+++ cobalt/android/apk/app/src/test/java/dev/cobalt/coat/CobaltActivityTest.java
@@ -14,8 +14,6 @@
 
 package dev.cobalt.coat;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.mock;
@@ -118,23 +116,5 @@ public class CobaltActivityTest {
       // Expected when calling super method.
     }
     verify(mockImeAdapterImpl, never()).dispatchKeyEvent(any());
-  }
-
-  @Test
-  public void shouldDisarmStartupGuard() {
-    assertTrue("Should disarm if URL is empty",
-      CobaltActivity.shouldDisarmStartupGuard(""));
-
-    assertTrue("Should disarm if URL is null",
-      CobaltActivity.shouldDisarmStartupGuard(null));
-
-    assertTrue("Should disarm for non-YouTube URLs",
-      CobaltActivity.shouldDisarmStartupGuard("https://www.google.com"));
-
-    assertTrue("Should disarm when YTS loader is present",
-      CobaltActivity.shouldDisarmStartupGuard("https://www.youtube.com/tv?loader=yts&extra=param"));
-
-    assertFalse("Should NOT disarm for standard YouTube Living Room URLs",
-      CobaltActivity.shouldDisarmStartupGuard("https://www.youtube.com/tv?launch=menu&topic=music"));
   }
 }
---
+++ cobalt/shell/android/java/src/dev/cobalt/shell/StartupGuardNavigationObserver.java
@@ -0,0 +1,27 @@
+package dev.cobalt.shell;
+
+import org.chromium.content_public.browser.NavigationHandle;
+import org.chromium.content_public.browser.WebContents;
+import org.chromium.content_public.browser.WebContentsObserver;
+
+/**
+ * An observer that monitors web content navigation to disarm the StartupGuard
+ * once the primary page has successfully loaded.
+ */
+public class StartupGuardNavigationObserver extends WebContentsObserver {
+    /**
+     * Constructs a new observer for the given WebContents.
+     *
+     * @param webContents The WebContents to observe.
+     */
+    public StartupGuardNavigationObserver(WebContents webContents) {
+        super(webContents);
+    }
+
+    @Override
+    public void didFinishNavigationInPrimaryMainFrame(NavigationHandle navigation) {
+        if (navigation.hasCommitted()) {
+            StartupGuard.getInstance().disarm();
+        }
+    }
+}
