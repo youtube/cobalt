f6da8882f977e84e54fe5d884e1bfd530c055027
Ying Yu <yuying@google.com>
2026-01-28 23:28:52 +0000

    evergreen: Move MarkSuccessful to Shell (#8819)
    Relocate the call to updater::UpdaterModule::MarkSuccessful from
    CobaltWebContentsObserver to Shell. This ensures the update is
    marked as successful only when the primary application content is
    ready, not when any web content, such as a splash screen, is
    available. This improves the reliability of Evergreen update
    success reporting.
    
    Issue: 431862767

--- cobalt/shell/browser/shell.cc
+++ cobalt/shell/browser/shell.cc
@@ -67,6 +67,10 @@
 #include "third_party/blink/public/mojom/mediastream/media_stream.mojom.h"
 #include "third_party/blink/public/mojom/window_features/window_features.mojom.h"
 
+#if BUILDFLAG(USE_EVERGREEN)
+#include "cobalt/updater/updater_module.h"  //nogncheck
+#endif
+
 #if BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(USE_STARBOARD_MEDIA)
@@ -375,6 +379,15 @@ void Shell::RenderFrameCreated(RenderFrameHost* frame_host) {
 void Shell::PrimaryMainDocumentElementAvailable() {
   cobalt::migrate_storage_record::MigrationManager::DoMigrationTasksOnce(
       web_contents());
+#if BUILDFLAG(USE_EVERGREEN)
+  cobalt::updater::UpdaterModule* updater_module =
+      cobalt::updater::UpdaterModule::GetInstance();
+  if (updater_module) {
+    LOG(INFO) << "Mark the current installation as successful after the "
+                 "PrimaryMainDocumentElement is available.";
+    updater_module->MarkSuccessful();
+  }
+#endif  // BUILDFLAG(USE_EVERGREEN)
 }
 
 void Shell::DidFinishNavigation(NavigationHandle* navigation_handle) {
