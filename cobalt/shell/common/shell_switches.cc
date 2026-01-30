56d434820e4c6f1db9081fe3764ea69e01df50e4
Bo-Rong Chen <borongchen@google.com>
2026-01-30 15:14:51 -0800

    cobalt: Add command-line switch to disable splash screen (#8885)
    Introduce the kDisableSplashScreen command-line switch. When this switch
    is present, the Cobalt shell will not create the initial splash screen
    WebContents. This allows for faster startup and easier debugging by
    bypassing the splash screen animation and rendering.
    
    Issue: 479899985
    (cherry picked from commit 3bc1a24c3f51441afbc9fbbbc14fea6e8a015497)

--- cobalt/shell/common/shell_switches.cc
+++ cobalt/shell/common/shell_switches.cc
@@ -14,6 +14,8 @@
 
 #include "cobalt/shell/common/shell_switches.h"
 
+#include "base/command_line.h"
+
 namespace switches {
 
 // Makes Content Shell use the given path for its data directory.
@@ -24,6 +26,9 @@ const char kContentShellDataPath[] = "data-path";
 
 // The directory breakpad should store minidumps in.
 const char kCrashDumpsDir[] = "crash-dumps-dir";
+
+// Disables showing splash screen.
+const char kDisableSplashScreen[] = "disable-splash-screen";
 
 // Disables the check for the system font when specified.
 const char kDisableSystemFontCheck[] = "disable-system-font-check";
@@ -58,5 +63,11 @@ const char kRemoteDebuggingAddress[] = "remote-debugging-address";
 
 // The delay in milliseconds before shutting down the splash screen.
 const char kSplashScreenShutdownDelayMs[] = "splash-screen-shutdown-delay-ms";
+
+bool ShouldCreateSplashScreen() {
+  const base::CommandLine* command_line =
+      base::CommandLine::ForCurrentProcess();
+  return !command_line->HasSwitch(kDisableSplashScreen);
+}
 
 }  // namespace switches
