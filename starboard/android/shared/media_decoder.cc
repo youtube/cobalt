8fb7fba956f26dcf5286533fa8a800446dc1b236
Kyujung Youn <kjyoun@google.com>
2026-02-02 10:33:19 -0800

    Merge stop into release

--- starboard/android/shared/media_decoder.cc
+++ starboard/android/shared/media_decoder.cc
@@ -190,8 +190,6 @@ MediaCodecDecoder::~MediaCodecDecoder() {
     if (status != MEDIA_CODEC_OK) {
       SB_LOG(ERROR) << "Failed to flush media codec.";
     }
-    // Call stop() here to notify MediaCodecBridge to not invoke any callbacks.
-    media_codec_bridge_->Stop();
   }
 }
 
