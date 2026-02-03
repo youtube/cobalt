eb1da1885d22e08a657d82ecb228e15e6def44cb
Kyujung Youn <kjyoun@google.com>
2026-02-03 13:00:56 -0800

    Update comment

--- cobalt/renderer/cobalt_content_renderer_client.cc
+++ cobalt/renderer/cobalt_content_renderer_client.cc
@@ -182,9 +182,9 @@ const T* GetSettingValue(
 
 constexpr int kMaxFramesInDecoderLimit = 10'000;
 constexpr int kMaxVideoDecoderPollIntervalMs = 60'000;  // 1 minute.
-// player_web_cobalt web experiment uses 0 as the sentinel value for unset.
+// Experiment framework uses 0 as the sentinel value for unset.
 // e.g.)
-// http://google3/googledata/experiments/youtube/player_web/features/player_web_cobalt.impl.gcl;l=310;rcl=862772714
+// http://go/latestexpcl/player_web/features/player_web_cobalt.impl.gcl;l=332;rcl=862772714
 constexpr int kH5vccUnsetSentinel = 0;
 
 std::optional<int> ProcessRangedIntH5vccSetting(
