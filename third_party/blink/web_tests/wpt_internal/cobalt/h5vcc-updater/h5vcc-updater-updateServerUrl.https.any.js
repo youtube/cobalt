586a7a3ef7c5de1082374a8fa64ef1be6a3d3eac
Madhura Jayaraman <madjay@google.com>
2026-02-03 08:59:23 -0800

    Remove old files

--- third_party/blink/web_tests/wpt_internal/cobalt/h5vcc-updater/h5vcc-updater-updateServerUrl.https.any.js
+++
@@ -1,8 +0,0 @@
-// META: global=window
-// META: script=/resources/test-only-api.js
-// META: script=resources/automation.js
-
-h5vcc_updater_mojo_disconnection_tests(async (t) => {
-  return promise_rejects_exactly(
-    t, 'API not supported for this platform.', window.h5vcc.updater.getUpdateServerUrl());
-}, 'getUpdateServerUrl() rejects when unimplemented due to pipe closure');
