ce990b3a55be24c057f21a5c857ad915224c99be
Colin Liang <colinliang@google.com>
2026-02-09 16:58:28 -0800

    android: Refactor StartupGuard disable logic (#8998)
    Refactor the handling of the StartupGuard's disabling logic within
    CobaltActivity. The conditional check for DISABLE_STARTUP_GUARD is
    moved from the onStart method to the onCreate method.
    
    This change prevents the StartupGuard from being unnecessarily
    scheduled in onCreate only to be disarmed immediately after in onStart
    when it is meant to be disabled via Java switches. It streamlines the
    initialization flow and ensures the StartupGuard is only scheduled if
    it is intended to be active, improving clarity and efficiency.
    
    Bug: 467729316

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltActivity.java
@@ -409,7 +409,11 @@ public abstract class CobaltActivity extends Activity {
 
     super.onCreate(savedInstanceState);
 
-    StartupGuard.getInstance().scheduleCrash(HANG_APP_CRASH_TIMEOUT_SECONDS);
+    if (getJavaSwitches().containsKey(JavaSwitches.DISABLE_STARTUP_GUARD)) {
+      Log.i(TAG, "StartupGuard is disabled by Java switch.");
+    } else {
+      StartupGuard.getInstance().scheduleCrash(HANG_APP_CRASH_TIMEOUT_SECONDS);
+    }
 
     mCobaltConnectivityDetector = new CobaltConnectivityDetector(this);
     createContent(savedInstanceState);
@@ -474,11 +478,6 @@ public abstract class CobaltActivity extends Activity {
 
   @Override
   protected void onStart() {
-    if (getJavaSwitches().containsKey(JavaSwitches.DISABLE_STARTUP_GUARD)) {
-      Log.i(TAG, "StartupGuard is disabled by Java switch.");
-      StartupGuard.getInstance().disarm();
-    }
-
     if (isDevelopmentBuild()) {
       getStarboardBridge().getAudioOutputManager().dumpAllOutputDevices();
       MediaCodecCapabilitiesLogger.dumpAllDecoders();
