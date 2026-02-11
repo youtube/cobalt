52985487d7ae02a52e3d9260ca3eccabf24e84da
Adam Walls <avvall@google.com>
2026-02-11 12:12:07 -0800

    cobalt/browser: Remove unrelated update switches from switches.h

--- cobalt/browser/switches.h
+++ cobalt/browser/switches.h
@@ -35,14 +35,6 @@ constexpr char kEnforceHTTPS[] = "https-enforcement";
 // Specify the initial window size: --window-size=w,h
 constexpr char kWindowSize[] = "window-size";
 
-// Whether to request, download, and install uncompressed (rather than
-// compressed) Evergreen binaries.
-constexpr char kUseUncompressedUpdates[] = "use_uncompressed_updates";
-
-// Uses the QA update server to test the changes to the configuration of the
-// PROD update server.
-constexpr char kUseQAUpdateServer[] = "use_qa_update_server";
-
 // Sets the interval for memory metrics collection in seconds.
 constexpr char kMemoryMetricsInterval[] = "memory-metrics-interval";
 
