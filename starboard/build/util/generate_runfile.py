116f28e233ae2f8361c28012faba25ac7cbd30e7
Oscar Vestlie <oxv@google.com>
2026-02-17 10:16:38 +0000

    Add RDK specifics to the runfile

--- starboard/build/util/generate_runfile.py
+++ starboard/build/util/generate_runfile.py
@@ -6,11 +6,12 @@ import argparse
 import os
 import stat
 
-_TEMPLATE = """#!/usr/bin/env python3
+TEMPLATE = """#!/usr/bin/env python3
 import os
 import subprocess
 import sys
 
+{env}
 command = [
     os.path.join(os.path.dirname(__file__), 'elf_loader_sandbox'),
     '--evergreen_content=.', '--evergreen_library={library}.so'
@@ -26,13 +27,20 @@ except Exception as e:
     sys.exit(1)
 """
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--output', type=str, required=True)
-parser.add_argument('--library', type=str, required=True)
-args = parser.parse_args()
 
-with open(args.output, 'w', encoding='utf-8') as f:
-  f.write(_TEMPLATE.format(library=args.library))
-current_permissions = stat.S_IMODE(os.stat(args.output).st_mode)
-new_permissions = current_permissions | stat.S_IXUSR | stat.S_IXGRP
-os.chmod(args.output, new_permissions)
+def generate():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--output', type=str, required=True)
+  parser.add_argument('--library', type=str, required=True)
+  parser.add_argument('--env', type=str, default='')
+  args = parser.parse_args()
+
+  with open(args.output, 'w', encoding='utf-8') as f:
+    f.write(TEMPLATE.format(library=args.library, env=args.env))
+  current_permissions = stat.S_IMODE(os.stat(args.output).st_mode)
+  new_permissions = current_permissions | stat.S_IXUSR | stat.S_IXGRP
+  os.chmod(args.output, new_permissions)
+
+
+if __name__ == '__main__':
+  generate()
