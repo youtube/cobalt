b55d2f8c21f3c4fcc970304ab3c062c91854297f
Adam Walls <avvall@google.com>
2026-02-25 10:33:35 -0800

    Remove unused include

--- cobalt/browser/metrics/cobalt_metrics_service_client.cc
+++ cobalt/browser/metrics/cobalt_metrics_service_client.cc
@@ -23,7 +23,6 @@
 #include "base/notreached.h"
 #include "base/posix/file_descriptor_shuffle.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/strings/stringprintf.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/thread_pool.h"
 #include "base/time/time.h"
