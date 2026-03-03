0814b70b935cda4caf89f7f481bbeeb5361973c8
Kyujung Youn <kjyoun@google.com>
2026-03-02 16:11:50 -0800

    Refactor filter_based_player_worker_handler.cc

--- starboard/shared/starboard/player/filter/filter_based_player_worker_handler.cc
+++ starboard/shared/starboard/player/filter/filter_based_player_worker_handler.cc
@@ -46,6 +46,26 @@ typedef shared::starboard::player::PlayerWorker::Handler::HandlerResult
 // TODO: Make this configurable inside SbPlayerCreate().
 const int64_t kUpdateIntervalUsec = 200'000;  // 200ms
 
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::optional<T>& optional) {
+  if (optional) {
+    os << *optional;
+  } else {
+    os << "(nullopt)";
+  }
+  return os;
+}
+
+template <typename T, typename U>
+void LogAndSetExperimentalFeature(const char* name, T& feature, U new_value) {
+  if (feature == new_value) {
+    return;
+  }
+  SB_LOG(INFO) << "Set experimental feature " << name << ": new=\"" << new_value
+               << "\", old=\"" << feature << "\"";
+  feature = new_value;
+}
+
 #if BUILDFLAG(COBALT_IS_RELEASE_BUILD)
 
 void DumpInputHash(const InputBuffer* input_buffer) {}
@@ -551,89 +571,41 @@ void FilterBasedPlayerWorkerHandler::SetMaxVideoInputSize(
   max_video_input_size_ = max_video_input_size;
 }
 
-void FilterBasedPlayerWorkerHandler::SetFlushDecoderDuringReset(
-    bool flush_decoder_during_reset) {
-  SB_LOG(INFO) << "Set flush_decoder_during_reset from "
-               << experimental_features_.flush_decoder_during_reset << " to "
-               << flush_decoder_during_reset;
-  experimental_features_.flush_decoder_during_reset =
-      flush_decoder_during_reset;
-}
+#define DEFINE_SET_EXPERIMENTAL_FEATURE(method_name, field_name, type)     \
+  void FilterBasedPlayerWorkerHandler::Set##method_name(type field_name) { \
+    LogAndSetExperimentalFeature(                                          \
+        #field_name, experimental_features_.field_name, field_name);       \
+  }
 
-void FilterBasedPlayerWorkerHandler::SetResetAudioDecoder(
-    bool reset_audio_decoder) {
-  SB_LOG(INFO) << "Set reset_audio_decoder from "
-               << experimental_features_.reset_audio_decoder << " to "
-               << reset_audio_decoder;
-  experimental_features_.reset_audio_decoder = reset_audio_decoder;
-}
+DEFINE_SET_EXPERIMENTAL_FEATURE(FlushDecoderDuringReset,
+                                flush_decoder_during_reset,
+                                bool)
+DEFINE_SET_EXPERIMENTAL_FEATURE(ResetAudioDecoder, reset_audio_decoder, bool)
 
 void FilterBasedPlayerWorkerHandler::SetVideoSurfaceView(void* surface_view) {
-  SB_LOG(INFO) << "Set surface_view from " << surface_view_ << " to "
-               << surface_view;
-  surface_view_ = surface_view;
+  LogAndSetExperimentalFeature("surface_view", surface_view_, surface_view);
 }
 
-void FilterBasedPlayerWorkerHandler::SetVideoInitialMaxFramesInDecoder(
-    int video_initial_max_frames_in_decoder) {
-  SB_LOG(INFO) << "Set video_initial_max_frames_in_decoder from "
-               << ToString(experimental_features_.video_initial_max_frames_in_decoder)
-               << " to " << video_initial_max_frames_in_decoder;
-  experimental_features_.video_initial_max_frames_in_decoder =
-      video_initial_max_frames_in_decoder;
-}
-
-void FilterBasedPlayerWorkerHandler::SetVideoMaxPendingInputFrames(
-    int video_max_pending_input_frames) {
-  SB_LOG(INFO) << "Set video_max_pending_input_frames from "
-               << ToString(experimental_features_.video_max_pending_input_frames)
-               << " to " << video_max_pending_input_frames;
-  experimental_features_.video_max_pending_input_frames =
-      video_max_pending_input_frames;
-}
-
-void FilterBasedPlayerWorkerHandler::SetVideoDecoderInitialPrerollCount(
-    int video_decoder_initial_preroll_count) {
-  SB_LOG(INFO) << "Set video_decoder_initial_preroll_count from "
-               << ToString(experimental_features_.video_decoder_initial_preroll_count)
-               << " to " << video_decoder_initial_preroll_count;
-  experimental_features_.video_decoder_initial_preroll_count =
-      video_decoder_initial_preroll_count;
-}
-
-void FilterBasedPlayerWorkerHandler::SetVideoDecoderPollIntervalMs(
-    int video_decoder_poll_interval_ms) {
-  SB_LOG(INFO) << "Set video_decoder_poll_interval_ms from "
-               << ToString(experimental_features_.video_decoder_poll_interval_ms)
-               << " to " << video_decoder_poll_interval_ms;
-  experimental_features_.video_decoder_poll_interval_ms =
-      video_decoder_poll_interval_ms;
-}
-
-void FilterBasedPlayerWorkerHandler::SetVideoRendererMinInputBuffers(
-    int video_renderer_min_input_buffers) {
-  SB_LOG(INFO) << "Set video_renderer_min_input_buffers from "
-               << ToString(experimental_features_.video_renderer_min_input_buffers)
-               << " to " << video_renderer_min_input_buffers;
-  experimental_features_.video_renderer_min_input_buffers =
-      video_renderer_min_input_buffers;
-}
-
-void FilterBasedPlayerWorkerHandler::SetVideoRendererMinDecodedFrames(
-    int video_renderer_min_decoded_frames) {
-  SB_LOG(INFO) << "Set video_renderer_min_decoded_frames from "
-               << ToString(experimental_features_.video_renderer_min_decoded_frames)
-               << " to " << video_renderer_min_decoded_frames;
-  experimental_features_.video_renderer_min_decoded_frames =
-      video_renderer_min_decoded_frames;
-}
-
-void FilterBasedPlayerWorkerHandler::SetMediaCodecResetDelayMs(
-    int media_codec_reset_delay_ms) {
-  SB_LOG(INFO) << "Set media_codec_reset_delay_ms from "
-               << ToString(experimental_features_.media_codec_reset_delay_ms)
-               << " to " << media_codec_reset_delay_ms;
-  experimental_features_.media_codec_reset_delay_ms = media_codec_reset_delay_ms;
-}
+DEFINE_SET_EXPERIMENTAL_FEATURE(VideoInitialMaxFramesInDecoder,
+                                video_initial_max_frames_in_decoder,
+                                int)
+DEFINE_SET_EXPERIMENTAL_FEATURE(VideoMaxPendingInputFrames,
+                                video_max_pending_input_frames,
+                                int)
+DEFINE_SET_EXPERIMENTAL_FEATURE(VideoDecoderInitialPrerollCount,
+                                video_decoder_initial_preroll_count,
+                                int)
+DEFINE_SET_EXPERIMENTAL_FEATURE(VideoDecoderPollIntervalMs,
+                                video_decoder_poll_interval_ms,
+                                int)
+DEFINE_SET_EXPERIMENTAL_FEATURE(VideoRendererMinInputBuffers,
+                                video_renderer_min_input_buffers,
+                                int)
+DEFINE_SET_EXPERIMENTAL_FEATURE(VideoRendererMinDecodedFrames,
+                                video_renderer_min_decoded_frames,
+                                int)
+DEFINE_SET_EXPERIMENTAL_FEATURE(MediaCodecResetDelayMs,
+                                media_codec_reset_delay_ms,
+                                int)
 
 }  // namespace starboard::shared::starboard::player::filter
