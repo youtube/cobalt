34b6f26585816ca21bd27f22b4f2d99bfccd6050
gurusai-voleti <gvoleti@google.com>
2026-03-03 16:51:49 +0530

    Update update.sh

--- native_client_sdk/src/web/update.sh
+++ native_client_sdk/src/web/update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 for file in index.html manifest.html; do
-  gcloud storage cp --predefined-acl=public-read $file gs://nativeclient-mirror/nacl/nacl_sdk
+  gcloud storage cp --predefined-acl=publicRead $file gs://nativeclient-mirror/nacl/nacl_sdk
 done
