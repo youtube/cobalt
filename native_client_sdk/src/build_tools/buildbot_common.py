b6c733a081f317a276a00f6329838923d7926f17
gurusai-voleti <gvoleti@google.com>
2026-03-03 16:59:01 +0530

    Update buildbot_common.py

--- native_client_sdk/src/build_tools/buildbot_common.py
+++ native_client_sdk/src/build_tools/buildbot_common.py
@@ -224,7 +224,7 @@ def Archive(filename, bucket_path, cwd=None, step_link=True):
   # search the PATH for the executable: http://bugs.python.org/issue8557
   shell = getos.GetPlatform() == 'win'
 
-  cmd = ['gcloud', 'storage', 'cp', filename, full_dst, '--predefined-acl=public-read']
+  cmd = [GetGsutil(), 'cp', '-a', 'public-read', filename, full_dst]
   Run(cmd, shell=shell, cwd=cwd)
   url = 'https://storage.googleapis.com/%s/%s' % (bucket_path, filename)
   if step_link:
