da7db4ccfe8741ba5b027f19fb44a3d635b8b90e
Colin Liang <colinliang@google.com>
2026-03-19 11:16:28 -0700

    Fix linux build error
    Change-Id: I233c90ec494ef63a4e5cf04f4a749e54386e89cb

--- cobalt/shell/browser/shell_browser_main_parts.cc
+++ cobalt/shell/browser/shell_browser_main_parts.cc
@@ -18,6 +18,7 @@
 
 #include "base/base_switches.h"
 #include "base/command_line.h"
+#include "base/files/file_util.h"
 #include "base/memory/ref_counted_memory.h"
 #include "build/build_config.h"
 #include "cc/base/switches.h"
