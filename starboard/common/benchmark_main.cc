e5e6ac0f4a4bf4d410cc07ca8e36db12ef31cf9e
Kyujung Youn <kjyoun@google.com>
2026-03-22 19:37:26 -0700

    Run benchmark
    ```
    $ autoninja -C out/linux-x64x11_qa starboard/benchmark:benchmark && ./out/linux-x64x11_qa/benchmark  --benchmark_filter=BM_.*Lock
    
    Running ./out/linux-x64x11_qa/benchmark
    Run on (128 X 4575.16 MHz CPU s)
    CPU Caches:
      L1 Data 32 KiB (x64)
      L1 Instruction 32 KiB (x64)
      L2 Unified 512 KiB (x64)
      L3 Unified 32768 KiB (x8)
    Load Average: 2.03, 2.50, 2.63
    ***WARNING*** CPU scaling is enabled, the benchmark real time measurements may be noisy and will incur extra overhead.
    ---------------------------------------------------------------------
    Benchmark                           Time             CPU   Iterations
    ---------------------------------------------------------------------
    BM_StarboardSequentialLock       14.7 ns         14.7 ns     47889250
    BM_StdScopedLock                 17.9 ns         17.9 ns     36990112
    BM_StdSequentialLock             16.6 ns         16.6 ns     41970155
    ```

--- starboard/common/benchmark_main.cc
+++ starboard/common/benchmark_main.cc
@@ -25,9 +25,21 @@ int RunAllBenchmarks(int argc, char** argv) {
 }
 }  // namespace
 
+#if BUILDFLAG(IS_STARBOARD)
 // When we are building Evergreen we need to export SbEventHandle so that the
 // ELF loader can find and invoke it.
 #if SB_IS(MODULAR)
 SB_EXPORT
 #endif  // SB_IS(MODULAR)
 STARBOARD_WRAP_SIMPLE_MAIN(RunAllBenchmarks);
+
+#if !SB_IS(EVERGREEN)
+int main(int argc, char** argv) {
+  return SbRunStarboardMain(argc, argv, SbEventHandle);
+}
+#endif  // !SB_IS(EVERGREEN)
+#else
+int main(int argc, char** argv) {
+  return RunAllBenchmarks(argc, argv);
+}
+#endif  // BUILDFLAG(IS_STARBOARD)
