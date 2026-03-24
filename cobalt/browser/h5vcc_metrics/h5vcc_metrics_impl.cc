6640e436f1b597eebf39f2f1648b8f1d53aa8ebf
Yijia Zhang <yijiazhang@google.com>
2026-03-23 22:02:58 -0700

    Fix more lint errors.

--- cobalt/browser/h5vcc_metrics/h5vcc_metrics_impl.cc
+++ cobalt/browser/h5vcc_metrics/h5vcc_metrics_impl.cc
@@ -75,7 +75,6 @@ void H5vccMetricsImpl::SetMetricEventInterval(
 }
 
 void H5vccMetricsImpl::RequestHistograms(RequestHistogramsCallback callback) {
-
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   auto* manager_client =
       cobalt::GlobalFeatures::GetInstance()->metrics_services_manager_client();
