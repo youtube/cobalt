e1557201a8cd764e1c3d644452b4d37878280ebe
Adam Walls <avvall@google.com>
2026-03-25 05:38:58 +0000

    Fix Mojom mapping for new memory metrics to enable UMA emission

--- services/resource_coordinator/memory_instrumentation/queued_request_dispatcher.cc
+++ services/resource_coordinator/memory_instrumentation/queued_request_dispatcher.cc
@@ -92,6 +92,11 @@ memory_instrumentation::mojom::OSMemDumpPtr CreatePublicOSDump(
   os_dump->partition_alloc_rss_kb = internal_os_dump.partition_alloc_rss_kb;
   os_dump->v8_rss_kb = internal_os_dump.v8_rss_kb;
   os_dump->malloc_rss_kb = internal_os_dump.malloc_rss_kb;
+  os_dump->code_other_rss_kb = internal_os_dump.code_other_rss_kb;
+  os_dump->fonts_rss_kb = internal_os_dump.fonts_rss_kb;
+  os_dump->ashmem_jit_rss_kb = internal_os_dump.ashmem_jit_rss_kb;
+  os_dump->android_runtime_rss_kb = internal_os_dump.android_runtime_rss_kb;
+  os_dump->stacks_rss_kb = internal_os_dump.stacks_rss_kb;
 #endif
   os_dump->mappings_count = internal_os_dump.mappings_count;
   os_dump->pss_kb = internal_os_dump.pss_kb;
