3c5c72995d10709eb819259d15ac248426828667
Adam Walls <avvall@google.com>
2026-03-27 23:34:49 +0000

    cobalt: Remove empty BUILDFLAG(IS_COBALT) block in os_metrics_linux.cc

--- services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics_linux.cc
+++ services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics_linux.cc
@@ -371,8 +371,6 @@ bool OSMetrics::FillOSMemoryDump(base::ProcessId pid,
   dump->platform_private_footprint->rss_anon_bytes = rss_anon_bytes;
   dump->platform_private_footprint->vm_swap_bytes = vm_swap_bytes;
   dump->resident_set_kb = process_metrics->GetResidentSetSize() / 1024;
-#if BUILDFLAG(IS_COBALT)
-#endif
   dump->peak_resident_set_kb = GetPeakResidentSetSize(pid);
   dump->is_peak_rss_resettable = ResetPeakRSSIfPossible(pid);
 
