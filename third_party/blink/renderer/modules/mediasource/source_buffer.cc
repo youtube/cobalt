0dd511870f18b8aee837274892445176c12d50ca
xiaomings <xiaomings@google.com>
2026-03-30 15:11:36 -0700

    Clean up includes in source_buffer.cc
    Remove merge conflict markers and clean up includes.

--- third_party/blink/renderer/modules/mediasource/source_buffer.cc
+++ third_party/blink/renderer/modules/mediasource/source_buffer.cc
@@ -40,14 +40,11 @@
 #include "base/numerics/checked_math.h"
 #include "media/base/logging_override_if_enabled.h"
 #include "media/base/stream_parser_buffer.h"
-<<<<<<< HEAD
 #include "partition_alloc/partition_alloc.h"
-=======
 #if BUILDFLAG(USE_STARBOARD_MEDIA)
 #include "third_party/blink/renderer/modules/cobalt/h_5_vcc.h"
 #include "third_party/blink/renderer/modules/cobalt/h5vcc_settings/h_5_vcc_settings.h"
 #endif  // BUILDFLAG(USE_STARBOARD_MEDIA)
->>>>>>> d692b4bac9 (cobalt: Reland "Support synchronous MSE init segment append" (#8796))
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/public/platform/web_source_buffer.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
