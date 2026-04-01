9b66f7f5bb57e2c42840541143c97439e2deaf2c
xiaomings <xiaomings@google.com>
2026-04-01 11:50:19 -0700

    Fix formatting and include directives in test file

--- media/starboard/decoder_buffer_allocator_strategy_test.cc
+++ media/starboard/decoder_buffer_allocator_strategy_test.cc
@@ -12,7 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// clang-format off
 #include "media/starboard/decoder_buffer_allocator.h"
+// clang-format on
 
 #include "media/base/demuxer_stream.h"
 #include "starboard/common/experimental/media_buffer_pool.h"
