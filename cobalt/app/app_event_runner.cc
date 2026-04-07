c1e8da1533cb022a2c1eb320f5ce2e60f27ea3b1
Jelle Foks <jfoks@google.com>
2026-04-07 11:59:19 -0700

    cobalt: Finalize lifecycle refactor and fix argument mismatch

--- cobalt/app/app_event_runner.cc
+++ cobalt/app/app_event_runner.cc
@@ -69,7 +69,8 @@ class AppEventRunnerImpl : public AppEventRunner {
   void CreateMainDelegate(bool is_visible,
                           const char* initial_deep_link) override {
     content_main_delegate_ = std::make_unique<cobalt::CobaltMainDelegate>(
-        false /* is_content_browsertests */, is_visible, initial_deep_link);
+        absl::nullopt /* startup_timestamp */, initial_deep_link,
+        false /* is_content_browsertests */, is_visible);
   }
 
   cobalt::CobaltMainDelegate* GetMainDelegate() override {
