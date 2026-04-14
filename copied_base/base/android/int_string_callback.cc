cd58ce68c20521261776e2a0affbe9ed15c77563
Henrique Ferreiro <hferreiro@igalia.com>
2026-04-14 11:03:45 +0100

    aosp: Add missing jni_android.h includes
    In AOSP, //copied_base/base uses jni_zero, but it's still using the
    compatibility namespace `base::android::` without including the proper
    header file.
    
    Bug: 495203133

--- copied_base/base/android/int_string_callback.cc
+++ copied_base/base/android/int_string_callback.cc
@@ -10,7 +10,10 @@
 
 #include <jni.h>
 
+#include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
+
+// Must come after all headers that specialize FromJniType() / ToJniType().
 #include "base/base_jni/IntStringCallback_jni.h"
 
 namespace base {
