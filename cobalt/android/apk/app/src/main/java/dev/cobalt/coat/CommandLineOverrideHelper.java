f9b4ff4a118f223dfc49c34d19eb2839ceb3c77e
Bo-Rong Chen <borongchen@google.com>
2026-04-15 09:50:11 -0700

    Cherry pick PR #10044: android: Remove deprecated AImageReader flag
    Refer to original PR: #10044
    
    Remove the explicit disabling of the AImageReader feature on Android.
    Chromium on Android now uses AImageReader for texture mode by default,
    deprecating the SurfaceTexture-based texture mode.
    
    See
    https://chromium-review.git.corp.google.com/c/chromium/src/+/6701329.
    
    Issue: 500463665
    
    (cherry picked from commit d4f28254d)

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CommandLineOverrideHelper.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CommandLineOverrideHelper.java
@@ -100,9 +100,6 @@ public final class CommandLineOverrideHelper {
     public static StringJoiner getDefaultDisableFeatureOverridesList() {
         StringJoiner paramOverrides = new StringJoiner(",");
 
-        // Use SurfaceTexture for decode-to-texture mode.
-        paramOverrides.add("AImageReader");
-
         // Disable BackupRefPtr and have shim allocator tracked by MemoryReclaimer.
         paramOverrides.add("PartitionAllocBackupRefPtr");
 
