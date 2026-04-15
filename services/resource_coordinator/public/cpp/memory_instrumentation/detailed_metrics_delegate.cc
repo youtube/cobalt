8693dea71b2138588d4beeaa8d4b5c5606e8a73d
Adam Walls <avvall@google.com>
2026-04-15 04:08:58 +0000

    CL2: Implement SmapsCategorizer for high-performance smaps parsing

---
+++ services/resource_coordinator/public/cpp/memory_instrumentation/detailed_metrics_delegate.cc
@@ -0,0 +1,8 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/resource_coordinator/public/cpp/memory_instrumentation/detailed_metrics_delegate.h"
+
+// This file is currently empty because SmapsMetrics is header-only and
+// DetailedMetricsDelegate is a pure virtual interface.
