ee0d087dd856356d8fa15c151675085eadc82592
Hao Zheng <haozheng@google.com>
2026-04-20 18:57:35 +0000

    Exclude on device model tests and on device internal resource

--- chrome/browser/optimization_guide/optimization_guide_keyed_service_browsertest.cc
+++ chrome/browser/optimization_guide/optimization_guide_keyed_service_browsertest.cc
@@ -70,7 +70,9 @@
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/test/test_network_connection_tracker.h"
 #include "services/network/test/test_url_loader_factory.h"
-#include "services/on_device_model/public/cpp/features.h"
+#if !BUILDFLAG(IS_COBALT)
+#include "services/on_device_model/public/cpp/features.h"  // nogncheck
+#endif  // !BUILDFLAG(IS_COBALT)
 
 namespace optimization_guide {
 
@@ -206,7 +208,9 @@ class OptimizationGuideKeyedServiceBrowserTest
          {features::kOptimizationGuideModelExecution, {}},
          {features::internal::kComposeSettingsVisibility, {}},
          {features::internal::kWallpaperSearchSettingsVisibility, {}},
+#if !BUILDFLAG(IS_COBALT)
          {on_device_model::features::kUseFakeChromeML, {}},
+#endif  // !BUILDFLAG(IS_COBALT)
          {features::kLogOnDeviceMetricsOnStartup,
           {
               {"on_device_startup_metric_delay", "0"},
