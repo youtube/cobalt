4a8addcaab2a50f662a943fde44cc143aae406bd
Austin Osagie <austinosagie@google.com>
2026-04-23 13:45:09 -0700

    Re remove kCobaltDecoderBufferAllocatorWithInPlaceMetadata

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -514,10 +514,6 @@ const base::FeatureParam<base::TimeDelta> kAudioWriteDurationLocal{
     &kCobaltAudioWriteDuration, "AudioWriteDurationLocal", base::Milliseconds(1000)};
 const base::FeatureParam<base::TimeDelta> kAudioWriteDurationRemote{
     &kCobaltAudioWriteDuration, "AudioWriteDurationRemote", base::Microseconds(kSbPlayerWriteDurationRemote)};
-// When enabled, Cobalt stores allocation meta data in place for DecoderBuffers.
-BASE_FEATURE(kCobaltDecoderBufferAllocatorWithInPlaceMetadata,
-             "CobaltDecoderBufferAllocatorWithInPlaceMetadata",
-             base::FEATURE_DISABLED_BY_DEFAULT);
 #if BUILDFLAG(IS_ANDROID)
 // When enabled, Cobalt uses AndroidOverlay for SbPlayer, otherwise it uses VideoSurfaceView.
 BASE_FEATURE(kCobaltUsingAndroidOverlay,
