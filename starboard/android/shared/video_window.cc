d7ff5841effcd69b28382d77b3f1f8f10b8efc42
John Ocampo <johneocampo@google.com>
2026-04-27 22:22:37 +0000

    android: Reset Surface View
    Bug: 429021006

--- starboard/android/shared/video_window.cc
+++ starboard/android/shared/video_window.cc
@@ -21,6 +21,7 @@
 #include <jni.h>
 
 #include <mutex>
+#include <vector>
 
 #include "cobalt/android/jni_headers/VideoSurfaceView_jni.h"
 #include "starboard/android/shared/starboard_bridge.h"
@@ -47,6 +48,89 @@ VideoSurfaceHolder* g_video_surface_holder = NULL;
 // Global boolean to indicate if we need to reset SurfaceView after playing
 // vertical video.
 bool g_reset_surface_on_clear_window = false;
+
+void ClearNativeWindow(void* raw_context) {
+  ANativeWindow* native_window = static_cast<ANativeWindow*>(raw_context);
+  EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+  if (display == EGL_NO_DISPLAY) {
+    SB_LOG(ERROR) << "Found no EGL display in ClearNativeWindow";
+    return;
+  }
+
+  const EGLint kAttributeList[] = {
+      EGL_RED_SIZE,
+      8,
+      EGL_GREEN_SIZE,
+      8,
+      EGL_BLUE_SIZE,
+      8,
+      EGL_ALPHA_SIZE,
+      8,
+      EGL_RENDERABLE_TYPE,
+      EGL_OPENGL_ES2_BIT,
+      EGL_NONE,
+  };
+  // First, query how many configs match the given attribute list.
+  EGLint num_configs = 0;
+  EGL_CALL(eglChooseConfig(display, kAttributeList, NULL, 0, &num_configs));
+  if (num_configs == 0) {
+    SB_LOG(ERROR) << "No matching EGL configs found in ClearNativeWindow";
+    return;
+  }
+
+  // Allocate space to receive the matching configs and retrieve them.
+  std::vector<EGLConfig> configs(num_configs);
+  EGL_CALL(eglChooseConfig(display, kAttributeList, configs.data(), num_configs,
+                           &num_configs));
+  EGLNativeWindowType egl_native_window =
+      static_cast<EGLNativeWindowType>(native_window);
+  EGLConfig config;
+
+  // Find the first config that successfully allow a window surface to be
+  // created.
+  EGLSurface surface = EGL_NO_SURFACE;
+  for (int config_number = 0; config_number < num_configs; ++config_number) {
+    config = configs[config_number];
+    surface = eglCreateWindowSurface(display, config, egl_native_window, NULL);
+    if (surface != EGL_NO_SURFACE) {
+      break;
+    }
+  }
+  if (surface == EGL_NO_SURFACE) {
+    SB_LOG(ERROR) << "Found no EGL surface in ClearVideoWindow";
+    return;
+  }
+
+  // Create an OpenGL ES 2.0 context.
+  EGLContext context = EGL_NO_CONTEXT;
+  EGLint context_attrib_list[] = {
+      EGL_CONTEXT_CLIENT_VERSION,
+      2,
+      EGL_NONE,
+  };
+  context =
+      eglCreateContext(display, config, EGL_NO_CONTEXT, context_attrib_list);
+  if (context == EGL_NO_CONTEXT) {
+    SB_LOG(ERROR) << "Failed to create EGL context in ClearNativeWindow";
+    EGL_CALL(eglDestroySurface(display, surface));
+    return;
+  }
+
+  /* connect the context to the surface */
+  EGL_CALL(eglMakeCurrent(display, surface, surface, context));
+
+  GL_CALL(glClearColor(0, 0, 0, 1));
+  GL_CALL(glClear(GL_COLOR_BUFFER_BIT));
+  GL_CALL(glFlush());
+
+  EGL_CALL(eglSwapBuffers(display, surface));
+
+  // Cleanup all used resources.
+  EGL_CALL(
+      eglMakeCurrent(display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT));
+  EGL_CALL(eglDestroyContext(display, context));
+  EGL_CALL(eglDestroySurface(display, surface));
+}
 
 }  // namespace
 
@@ -115,7 +199,9 @@ bool VideoSurfaceHolder::GetVideoWindowSize(int* width, int* height) {
   }
 }
 
-void VideoSurfaceHolder::ClearVideoWindow(bool force_reset_surface) {
+void VideoSurfaceHolder::CleanUpVideoWindow(
+    bool force_clear,
+    SbDecodeTargetGraphicsContextProvider* gpu_provider) {
   // Lock *GetViewSurfaceMutex() here, to avoid releasing g_native_video_window
   // during painting.
   std::lock_guard lock(*GetViewSurfaceMutex());
@@ -131,18 +217,20 @@ void VideoSurfaceHolder::ClearVideoWindow(bool force_reset_surface) {
     return;
   }
 
-  if (force_reset_surface) {
-    StarboardBridge::GetInstance()->ResetVideoSurface(env);
-    SB_LOG(INFO) << "Video surface has been reset.";
-    return;
-  } else if (g_reset_surface_on_clear_window) {
-    int width = ANativeWindow_getWidth(g_native_video_window);
-    int height = ANativeWindow_getHeight(g_native_video_window);
-    if (width <= height) {
-      StarboardBridge::GetInstance()->ResetVideoSurface(env);
+  if (force_clear) {
+    if (!gpu_provider) {
+      SB_LOG(WARNING) << "gpu_provider is null in ClearVideoWindow, will force "
+                         "reset surface.";
+    } else {
+      gpu_provider->gles_context_runner(gpu_provider, &ClearNativeWindow,
+                                        g_native_video_window);
+      SB_LOG(INFO) << "Video surface has been cleared.";
       return;
     }
   }
+
+  StarboardBridge::GetInstance()->ResetVideoSurface(env);
+  SB_LOG(INFO) << "Video surface has been reset.";
 }
 
 }  // namespace starboard
