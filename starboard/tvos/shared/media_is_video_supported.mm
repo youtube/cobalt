9e3f5fe94668c556790d614888380ba9c663257a
Abhijeet Kandalkar <abhijeet@igalia.com>
2026-05-11 23:20:11 +0530

    Cherry pick PR #10403: build: Rename INTERNAL_BUILD to COBALT_INTERNAL_BUILD
    Refer to original PR: #10403
    
    Rename the INTERNAL_BUILD preprocessor macro to COBALT_INTERNAL_BUILD
    across the build config and tvOS platform sources. The prefixed name
    avoids collisions with generic `INTERNAL_BUILD` defines from third-party
    dependencies. [1]
    
    [1]
    https://chromium.googlesource.com/external/github.com/google/cld_3.git/+/refs/heads/master/src/base.h#95
    
    Bug: 492080113
    
    (cherry picked from commit 521cd1fb)

--- starboard/tvos/shared/media_is_video_supported.mm
+++ starboard/tvos/shared/media_is_video_supported.mm
@@ -67,7 +67,7 @@ bool MediaIsVideoSupported(SbMediaVideoCodec video_codec,
       return !is_hdr && frame_height <= 1080 && frame_width <= 1920;
     }
     if (video_codec == kSbMediaVideoCodecVp9) {
-#if defined(INTERNAL_BUILD)
+#if defined(COBALT_INTERNAL_BUILD)
       if (is_hdr) {
         if (transfer_id == kSbMediaTransferIdSmpteSt2084 ||
             transfer_id == kSbMediaTransferIdAribStdB67) {
@@ -127,7 +127,7 @@ bool MediaIsVideoSupported(SbMediaVideoCodec video_codec,
 #else
       SB_LOG(INFO) << "Non-internal build, accepting all VP9";
       return true;
-#endif  // defined(INTERNAL_BUILD)
+#endif  // defined(COBALT_INTERNAL_BUILD)
     }
   }  // @autoreleasepool
 
