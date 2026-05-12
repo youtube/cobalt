72c9bb01b74e2eadfa1557e46a47d8fc1a2e1903
Oscar Vestlie <oxv@google.com>
2026-05-12 22:54:14 +0000

    Address cobalt-ci-reviewer feedback: fix variable names in script

--- .github/actions/maintain_summary_results/maintain_summary_results.sh
+++ .github/actions/maintain_summary_results/maintain_summary_results.sh
@@ -9,21 +9,22 @@ datadog_dir="$5"
 add_run_suffix_to_summary="$6"
 
 shopt -s globstar nullglob
-for f in $files_glob; do
-  if [[ -f "$f" ]]; then
-    base=$(basename "$f")
+# Assumption: paths do not contain spaces.
+for file_path in $files_glob; do
+  if [[ -f "$file_path" ]]; then
+    base=$(basename "$file_path")
     name="${base%.*}"
     ext="${base##*.}"
 
     # Always copy to datadog with run suffix
-    cp "$f" "${datadog_dir}/${name}_run${run}.${ext}"
+    cp "$file_path" "${datadog_dir}/${name}_run${run}.${ext}"
 
     # Copy to summary if it's run 1 or it passed
     if [[ $run -eq 1 ]] || [[ "${passed}" == "true" ]]; then
       if [[ "${add_run_suffix_to_summary}" == "true" ]]; then
-        cp "$f" "${summary_dir}/${name}_run${run}.${ext}"
+        cp "$file_path" "${summary_dir}/${name}_run${run}.${ext}"
       else
-        cp "$f" "${summary_dir}/${name}.${ext}"
+        cp "$file_path" "${summary_dir}/${name}.${ext}"
       fi
     fi
   fi
