59e573c4aa10b9d61346f80bd192fd457ce773a3
Andrew Savage <andrewsavage@google.com>
2026-05-13 18:51:21 +0000

    [nplb] Fix signedness test of enum in C++17
    In C++17, casting -1 to an unscoped enum without negative enumerators is not a constant expression because -1 is outside the range of values of the enumeration. This change adds a negative enumerator to GenericEnumType and uses std::is_signed with std::underlying_type to correctly verify SB_HAS_SIGNED_ENUM.
    
    BUG=b/452970985
    TAG=agy
    CONV=504f0713-4b79-4779-b16f-8283195380fb

--- starboard/nplb/sabi/signedness_and_size_of_enum_test.cc
+++ starboard/nplb/sabi/signedness_and_size_of_enum_test.cc
@@ -12,19 +12,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <type_traits>
+
 #include "starboard/configuration.h"
 
 namespace nplb {
 namespace {
 
 typedef enum GenericEnumType {
-  kOnlyTag,
+  kNegativeTag = -1,
+  kOnlyTag = 0,
 } GenericEnumType;
 
-// TODO b/452970985 : Fix this test to work with new compiler.
-// SB_COMPILE_ASSERT((static_cast<GenericEnumType>(-1) < 0) ==
-// SB_HAS_SIGNED_ENUM,
-//                   SB_HAS_SIGNED_ENUM_is_inconsistent_with_sign_of_enum);
+SB_COMPILE_ASSERT(
+    std::is_signed<std::underlying_type<GenericEnumType>::type>::value ==
+        SB_HAS_SIGNED_ENUM,
+    SB_HAS_SIGNED_ENUM_is_inconsistent_with_sign_of_enum);
 
 SB_COMPILE_ASSERT(sizeof(GenericEnumType) == SB_SIZE_OF_ENUM,
                   SB_SIZE_OF_ENUM_is_inconsistent_with_sizeof_enum);
