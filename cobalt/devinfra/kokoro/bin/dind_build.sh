0be8bda98213da497f5f7a7f1009ff8b578a2a25
linxinan-chops <linxinan@google.com>
2026-05-15 18:02:53 -0700

    Try gcloud

--- cobalt/devinfra/kokoro/bin/dind_build.sh
+++ cobalt/devinfra/kokoro/bin/dind_build.sh
@@ -82,7 +82,7 @@ pipeline () {
     echo "${gn_arg}" >> "out/${TARGET_PLATFORM}_${CONFIG}/args.gn"
   done
   # Use the legacy GCE metadata authentication, allowing RBE remote execution on Kokoro bots.
-  export SISO_CREDENTIAL_HELPER="none"
+  export SISO_CREDENTIAL_HELPER="gcloud"
   echo "Running autoninja build for ${TARGET_PLATFORM}_${CONFIG}..."
   autoninja -C "out/${TARGET_PLATFORM}_${CONFIG}" ${GN_TARGET}  # GN_TARGET may expand to multiple args
 
