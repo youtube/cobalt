d5fd916fbeedd61327f93167395d08bc4b9aa522
Sunny Tang <tasunny@google.com>
2026-05-21 18:15:55 -0500

    attach bug to comment

--- cobalt/app/cobalt_switch_defaults_starboard.cc
+++ cobalt/app/cobalt_switch_defaults_starboard.cc
@@ -84,12 +84,13 @@ CommandLinePreprocessor::GetCobaltParamSwitchDefaults() {
   static const base::CommandLine::SwitchMap kCobaltSwitchDefaults{
       // Disable Vulkan.
       {::switches::kDisableFeatures, "Vulkan"},
-      // When DefaultEnableANGLEValidation is disabled (e.g gold/qa), EGL
-      // attribute EGL_CONTEXT_OPENGL_NO_ERROR_KHR is set during egl context
-      // creation, but egl extension required to support the attribute is
-      // missing and causes errors. So Enable it by default.
       {::switches::kEnableFeatures,
        "LimitImageDecodeCacheSize:mb/24, "
+       // When DefaultEnableANGLEValidation is disabled (e.g gold/qa), EGL
+       // attribute EGL_CONTEXT_OPENGL_NO_ERROR_KHR is set during egl context
+       // creation, but egl extension required to support the attribute is
+       // missing and causes errors. So Enable it by default. (More context in
+       // b/444042898)
        "DefaultEnableANGLEValidation, "
        "SmallerInterestArea, "
        "ReclaimPrepaintTilesWhenIdle, "
