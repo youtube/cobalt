ba8e648173435dff9124b4d9d91bf21ef09e3f5b
Kyujung Youn <kjyoun@google.com>
2026-05-28 09:19:32 -0700

    refactor(starboard/android): decouple MediaCodecDecoder from JNI to enable unit testing
    Introduce pure virtual MediaCodec interface to abstract JNI/NDK implementations. Refactor MediaCodecBridge to implement MediaCodec, and MediaCodecDecoder to use it, supporting constructor injection of a mock/fake factory. Move JNI buffer access to MediaCodecBridge to make MediaCodecDecoder JNI-free. Add media_codec_decoder_test.cc with a FakeMediaCodec to verify basic decoding flow without a JVM.
    
    TAG=agy
    CONV=0a1c4a76-4a22-45e8-906b-f1abeef78a1d

--- starboard/android/shared/media_codec_bridge.h
+++ starboard/android/shared/media_codec_bridge.h
@@ -19,6 +19,7 @@
 #include <optional>
 #include <string>
 
+#include "starboard/android/shared/media_codec.h"
 #include "starboard/common/result.h"
 #include "starboard/common/size.h"
 #include "starboard/media.h"
@@ -27,102 +28,25 @@
 
 namespace starboard {
 
-// GENERATED_JAVA_ENUM_PACKAGE: dev.cobalt.media
-// GENERATED_JAVA_PREFIX_TO_STRIP: MEDIA_CODEC_
-enum MediaCodecStatus {
-  MEDIA_CODEC_OK,
-  MEDIA_CODEC_DEQUEUE_INPUT_AGAIN_LATER,
-  MEDIA_CODEC_DEQUEUE_OUTPUT_AGAIN_LATER,
-  MEDIA_CODEC_OUTPUT_BUFFERS_CHANGED,
-  MEDIA_CODEC_OUTPUT_FORMAT_CHANGED,
-  MEDIA_CODEC_INPUT_END_OF_STREAM,
-  MEDIA_CODEC_OUTPUT_END_OF_STREAM,
-  MEDIA_CODEC_NO_KEY,
-  MEDIA_CODEC_INSUFFICIENT_OUTPUT_PROTECTION,
-  MEDIA_CODEC_ABORT,
-  MEDIA_CODEC_ERROR,
-
-  MEDIA_CODEC_MAX = MEDIA_CODEC_ERROR,
-};
-
-const jint BUFFER_FLAG_CODEC_CONFIG = 2;
-const jint BUFFER_FLAG_END_OF_STREAM = 4;
-
 const jint CRYPTO_MODE_UNENCRYPTED = 0;
 const jint CRYPTO_MODE_AES_CTR = 1;
 const jint CRYPTO_MODE_AES_CBC = 2;
 
-struct DequeueInputResult {
-  jint status;
-  jint index;
-};
-
-struct DequeueOutputResult {
-  jint status;
-  jint index;
-  jint flags;
-  jint offset;
-  jlong presentation_time_microseconds;
-  jint num_bytes;
-};
-
-struct FrameSize {
-  Size display_size;
-  bool has_crop_values = false;
-
-  FrameSize();
-  FrameSize(int width, int height, bool has_crop_values);
-};
-
-std::ostream& operator<<(std::ostream& os, const FrameSize& size);
-
-struct AudioOutputFormatResult {
-  jint sample_rate;
-  jint channel_count;
-};
-
-class MediaCodecBridge {
+class MediaCodecBridge : public MediaCodec {
  public:
-  // The methods are called on the default Looper.  They won't get called after
-  // Flush() is returned.
-  class Handler {
-   public:
-    virtual void OnMediaCodecError(bool is_recoverable,
-                                   bool is_transient,
-                                   const std::string& diagnostic_info) = 0;
-    virtual void OnMediaCodecInputBufferAvailable(int buffer_index) = 0;
-    virtual void OnMediaCodecOutputBufferAvailable(int buffer_index,
-                                                   int flags,
-                                                   int offset,
-                                                   int64_t presentation_time_us,
-                                                   int size) = 0;
-    virtual void OnMediaCodecOutputFormatChanged() = 0;
-    // This is called when tunnel mode is enabled or on Android 14 and newer
-    // devices.
-    virtual void OnMediaCodecFrameRendered(int64_t frame_timestamp) = 0;
-    // This is only called on Android 12 and newer devices for tunnel mode.
-    virtual void OnMediaCodecFirstTunnelFrameReady() = 0;
+  using Handler = MediaCodec::Handler;
 
-   protected:
-    ~Handler() {}
-  };
-
-  static std::unique_ptr<MediaCodecBridge> CreateAudioMediaCodecBridge(
+  static std::unique_ptr<MediaCodecBridge> CreateAudioMediaCodec(
       const AudioStreamInfo& audio_stream_info,
       Handler* handler,
       jobject j_media_crypto);
 
-  static NonNullResult<std::unique_ptr<MediaCodecBridge>>
-  CreateVideoMediaCodecBridge(
+  static NonNullResult<std::unique_ptr<MediaCodecBridge>> CreateVideoMediaCodec(
       SbMediaVideoCodec video_codec,
-      // `frame_size_hint` is used to create the Android video format, which
-      // doesn't have to be directly related to the resolution of the video.
+      const std::string& decoder_name,
+      const char* mime,
       const Size& frame_size_hint,
       int fps,
-      // `max_frame_size` can be set to positive values to specify the maximum
-      // resolutions the video can be adapted to.  When they are not set,
-      // MediaCodecBridge will set them to the maximum resolutions the platform
-      // can decode.
       const std::optional<Size>& max_frame_size,
       Handler* handler,
       jobject j_surface,
@@ -137,35 +61,38 @@ class MediaCodecBridge {
       bool skip_video_frames_over_60_fps,
       bool ignore_mediacodec_callbacks_during_flushing);
 
-  ~MediaCodecBridge();
+  explicit MediaCodecBridge(Handler* handler);
+  ~MediaCodecBridge() override;
 
-  // It is the responsibility of the client to manage the lifetime of the
-  // jobject that |GetInputBuffer| returns.
-  jni_zero::ScopedJavaLocalRef<jobject> GetInputBuffer(jint index);
+  void Initialize(jobject j_media_codec_bridge);
+
+  // MediaCodec implementation
+  jni_zero::ScopedJavaLocalRef<jobject> GetInputBuffer(jint index) override;
+  void* GetInputBufferAddress(jint index, size_t* capacity) override;
   jint QueueInputBuffer(jint index,
                         jint offset,
                         jint size,
                         jlong presentation_time_microseconds,
                         jint flags,
-                        jboolean is_decode_only);
+                        jboolean is_decode_only) override;
   jint QueueSecureInputBuffer(jint index,
                               jint offset,
                               const SbDrmSampleInfo& drm_sample_info,
                               jlong presentation_time_microseconds,
-                              jboolean is_decode_only);
+                              jboolean is_decode_only) override;
 
-  // It is the responsibility of the client to manage the lifetime of the
-  // jobject that |GetOutputBuffer| returns.
-  jni_zero::ScopedJavaLocalRef<jobject> GetOutputBuffer(jint index);
-  void ReleaseOutputBuffer(jint index, jboolean render);
-  void ReleaseOutputBufferAtTimestamp(jint index, jlong render_timestamp_ns);
+  jni_zero::ScopedJavaLocalRef<jobject> GetOutputBuffer(jint index) override;
+  void ReleaseOutputBuffer(jint index, jboolean render) override;
+  void ReleaseOutputBufferAtTimestamp(jint index,
+                                      jlong render_timestamp_ns) override;
 
-  void SetPlaybackRate(double playback_rate);
-  bool Restart();
-  jint Flush();
-  std::optional<FrameSize> GetOutputSize();
-  std::optional<AudioOutputFormatResult> GetAudioOutputFormat();
+  void SetPlaybackRate(double playback_rate) override;
+  bool Restart() override;
+  jint Flush() override;
+  std::optional<FrameSize> GetOutputSize() override;
+  std::optional<AudioOutputFormatResult> GetAudioOutputFormat() override;
 
+  // JNI callback entry points
   void OnMediaCodecError(
       JNIEnv* env,
       jboolean is_recoverable,
@@ -185,10 +112,6 @@ class MediaCodecBridge {
   void OnMediaCodecFirstTunnelFrameReady(JNIEnv* env);
 
  private:
-  // |MediaCodecBridge|s must only be created through its factory methods.
-  explicit MediaCodecBridge(Handler* handler);
-  void Initialize(jobject j_media_codec_bridge);
-
   Handler* const handler_;
   jni_zero::ScopedJavaGlobalRef<jobject> j_media_codec_bridge_ = NULL;
 
