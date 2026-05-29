e571053c48f7118c10a20cdd1ac50bfc7da112d5
Sean Cuff <sacuff@users.noreply.github.com>
2026-05-29 13:09:32 -0700

    Cherry pick PR #10481: "Posix": Register ioctl
    Refer to original PR: #10481
    
    This adds compile-time "dispatch" for ioctl operations, similar to
    existing handling that was created for TIOCGWINSZ, and implements a
    passthrough wrapper for FIONREAD, which is currently the only ioctl
    command needed by C27 (in the file_path_watcher_inotify lib).
    
    Under this model, `ioctl(fd, op, arg)` is preprocessed to `ioctl_op(fd,
    arg)`, and individual `ioctl_op` symbols are handled as Starboard
    usually deals with them. For example, this PR retains the in-musl custom
    implementation of `ioctl_TIOCGWINSZ`, and registers an ABI wrapper for
    `ioctl_FIONREAD`.
    
    New ioctl operations added after this PR will fail to build.
    
    Bug: 500418052
    
    (cherry picked from commit ba2fa7536)

--- starboard/elf_loader/exported_symbols.cc
+++ starboard/elf_loader/exported_symbols.cc
@@ -38,6 +38,7 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <sys/epoll.h>
+#include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <sys/random.h>
 #include <sys/socket.h>
@@ -63,6 +64,7 @@
 #include "starboard/shared/modular/starboard_layer_posix_eventfd_abi_wrappers.h"
 #include "starboard/shared/modular/starboard_layer_posix_fcntl_abi_wrappers.h"
 #include "starboard/shared/modular/starboard_layer_posix_getrandom_abi_wrappers.h"
+#include "starboard/shared/modular/starboard_layer_posix_ioctl_abi_wrappers.h"
 #include "starboard/shared/modular/starboard_layer_posix_mmap_abi_wrappers.h"
 #include "starboard/shared/modular/starboard_layer_posix_pipe2_abi_wrappers.h"
 #include "starboard/shared/modular/starboard_layer_posix_poll_abi_wrappers.h"
@@ -279,6 +281,7 @@ ExportedSymbols::ExportedSymbols() {
 
   // Linux APIs
   REGISTER_SYMBOL(recvmmsg);
+  REGISTER_WRAPPER(ioctl_FIONREAD);
 
   // Custom mapped POSIX APIs to compatibility wrappers.
   // These will rely on Starboard-side implementations that properly translate
