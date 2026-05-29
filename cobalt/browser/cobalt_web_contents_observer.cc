c16c8404a3005678531cc57914b212056a23a877
Andrew Savage <andrewsavage@google.com>
2026-05-29 17:49:12 +0000

    WIP

--- cobalt/browser/cobalt_web_contents_observer.cc
+++ cobalt/browser/cobalt_web_contents_observer.cc
@@ -14,12 +14,124 @@
 
 #include "cobalt/browser/cobalt_web_contents_observer.h"
 
+#if BUILDFLAG(IS_ANDROIDTV) || BUILDFLAG(IS_STARBOARD)
+#include "base/functional/bind.h"
+#include "base/logging.h"
+#include "base/timer/timer.h"
+#include "content/public/browser/navigation_handle.h"
+#include "net/base/net_errors.h"
+#include "base/metrics/histogram_functions.h"
+#endif  // BUILDFLAG(IS_ANDROIDTV) || BUILDFLAG(IS_STARBOARD)
+
+#if BUILDFLAG(IS_ANDROIDTV)
+#include "starboard/android/shared/starboard_bridge.h"
+#endif  // BUILDFLAG(IS_ANDROIDTV)
+
+#if BUILDFLAG(IS_STARBOARD)
+#include "starboard/system.h"
+#endif  // BUILDFLAG(IS_STARBOARD)
+
 namespace cobalt {
+
+#if BUILDFLAG(IS_ANDROIDTV) || BUILDFLAG(IS_STARBOARD)
+namespace {
+const int kNavigationTimeoutSeconds = 30;
+#if BUILDFLAG(IS_ANDROIDTV)
+const int kJniErrorTypeConnectionError = 0;
+#endif  // BUILDFLAG(IS_ANDROIDTV)
+}  // namespace
+#endif  // BUILDFLAG(IS_ANDROIDTV) || BUILDFLAG(IS_STARBOARD)
 
 CobaltWebContentsObserver::CobaltWebContentsObserver(
     content::WebContents* web_contents)
     : content::WebContentsObserver(web_contents) {}
 
 CobaltWebContentsObserver::~CobaltWebContentsObserver() = default;
+
+#if BUILDFLAG(IS_ANDROIDTV) || BUILDFLAG(IS_STARBOARD)
+void CobaltWebContentsObserver::DidStartNavigation(
+    content::NavigationHandle* handle) {
+  LOG(INFO) << "DidStartNavigation to: " << handle->GetURL();
+  if (!handle->IsInPrimaryMainFrame()) {
+    LOG(INFO) << "DidStartNavigation: navigation to " << handle->GetURL()
+              << " not in primary mainframe, returning";
+    return;
+  }
+  LOG(INFO) << "DidStartNavigation: navigation to " << handle->GetURL()
+            << " in primary mainframe";
+
+  // Start a navigation timer with a timeout callback to raise a
+  // network error dialog
+  timeout_timer_.Stop();
+  timeout_timer_.Start(
+      FROM_HERE, base::Seconds(kNavigationTimeoutSeconds),
+      base::BindOnce(&CobaltWebContentsObserver::RaisePlatformError,
+                     weak_factory_.GetWeakPtr()));
+}
+
+void CobaltWebContentsObserver::DidFinishNavigation(
+    content::NavigationHandle* navigation_handle) {
+  if (!navigation_handle->IsInPrimaryMainFrame()) {
+    return;
+  }
+
+  timeout_timer_.Stop();
+  const auto net_error_code = navigation_handle->GetNetErrorCode();
+  if (net_error_code != net::OK && net_error_code != net::ERR_ABORTED) {
+    LOG(INFO) << "DidFinishNavigation: Raising platform error with code: "
+              << net::ErrorToString(net_error_code);
+    RaisePlatformError();
+  } else if (net_error_code == net::OK) {
+    platform_error_raised_count_ = 0;
+  }
+}
+
+void CobaltWebContentsObserver::RaisePlatformError() {
+#if BUILDFLAG(IS_ANDROIDTV)
+  JNIEnv* env = base::android::AttachCurrentThread();
+  auto* starboard_bridge = starboard::StarboardBridge::GetInstance();
+
+  // Don't raise a new platform error if one is already showing
+  if (starboard_bridge->IsPlatformErrorShowing(env)) {
+    return;
+  }
+  platform_error_raised_count_++;
+  base::UmaHistogramCounts100("Cobalt.Network.PlatformErrorCount",
+                              platform_error_raised_count_);
+  starboard_bridge->RaisePlatformError(env, kJniErrorTypeConnectionError, 0);
+#elif BUILDFLAG(IS_STARBOARD)
+  if (is_platform_error_showing_) {
+    return;
+  }
+  is_platform_error_showing_ = true;
+  platform_error_raised_count_++;
+  base::UmaHistogramCounts100("Cobalt.Network.PlatformErrorCount",
+                              platform_error_raised_count_);
+  if (!SbSystemRaisePlatformError(
+          kSbSystemPlatformErrorTypeConnectionError,
+          &CobaltWebContentsObserver::HandlePlatformErrorResponse,
+          this)) {
+    LOG(WARNING) << "Did not handle platform error";
+    is_platform_error_showing_ = false;
+  }
+#else
+  NOTIMPLEMENTED();
+#endif
+}
+#endif  // BUILDFLAG(IS_ANDROIDTV) || BUILDFLAG(IS_STARBOARD)
+
+#if BUILDFLAG(IS_STARBOARD)
+// static
+void CobaltWebContentsObserver::HandlePlatformErrorResponse(
+    SbSystemPlatformErrorResponse response, void* user_data) {
+  auto* observer = static_cast<CobaltWebContentsObserver*>(user_data);
+  observer->OnPlatformErrorResponse(response);
+}
+
+void CobaltWebContentsObserver::OnPlatformErrorResponse(
+    SbSystemPlatformErrorResponse response) {
+  is_platform_error_showing_ = false;
+}
+#endif  // BUILDFLAG(IS_STARBOARD)
 
 }  // namespace cobalt
