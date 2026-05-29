fe182d5376a04a2cd3694361f09aff780a8cca4f
Charley Kim <charleykim@google.com>
2026-05-29 20:17:01 +0000

    include utility and fix includes order

--- third_party/blink/renderer/modules/cobalt_modules_stubs.cc
+++ third_party/blink/renderer/modules/cobalt_modules_stubs.cc
@@ -12,11 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <utility>
+
 #include "third_party/blink/renderer/modules/ml/navigator_ml.h"  // nogncheck
-#include "third_party/blink/renderer/platform/bindings/wrapper_type_info.h"
-#include "third_party/blink/renderer/modules/xr/xr_system.h"  // nogncheck
 #include "third_party/blink/renderer/modules/xr/xr_frame_provider.h"  // nogncheck
 #include "third_party/blink/renderer/modules/xr/xr_session.h"  // nogncheck
+#include "third_party/blink/renderer/modules/xr/xr_system.h"  // nogncheck
+#include "third_party/blink/renderer/platform/bindings/wrapper_type_info.h"
 
 namespace blink {
 
