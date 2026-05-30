d245789e28f2b66052319d56b9570b51af9fd0e6
Andrew Savage <andrewsavage@google.com>
2026-05-30 08:40:02 -0400

    Cherry pick PR #10668: Cherry pick PR #8637: Web tests for h5vcc updater
    Refer to original PR: #10668
    
    Comprehensive web platform tests are added for all H5vccUpdater
    methods, including specific tests that verify promise rejection upon
    Mojo connection errors.
    
    Bug: 454440974
    Bug: 515122610
    
    ---------
    
    Co-authored-by: Madhura Jayaraman <madjay@google.com>
    
    (cherry picked from commit 7041d5db53e331b0a5fb39227fac90c96176c44b)

--- third_party/blink/web_tests/wpt_internal/cobalt/h5vcc-updater/resources/mock-h5vcc-updater.js
+++ third_party/blink/web_tests/wpt_internal/cobalt/h5vcc-updater/resources/mock-h5vcc-updater.js
@@ -1,47 +1,148 @@
 import {
-  H5vccUpdater, H5vccUpdaterReceiver
+  H5vccUpdater, H5vccUpdaterReceiver,
+  H5vccUpdaterSideloading, H5vccUpdaterSideloadingReceiver
 } from '/gen/cobalt/browser/h5vcc_updater/public/mojom/h5vcc_updater.mojom.m.js';
 
-
-// Implementation of h5vcc_updater.mojom.H5vccUpdater.
+// Implementation of h5vcc_updater.mojom.H5vccUpdater and H5vccUpdaterSideloading
 class MockH5vccUpdater {
   constructor() {
     this.interceptor_ =
       new MojoInterfaceInterceptor(H5vccUpdater.$interfaceName);
     this.interceptor_.oninterfacerequest = e => this.bind(e.handle);
+
+    this.sideloading_interceptor_ =
+      new MojoInterfaceInterceptor(H5vccUpdaterSideloading.$interfaceName);
+    this.sideloading_interceptor_.oninterfacerequest = e => this.bindSideloading(e.handle);
+
     this.receiver_ = new H5vccUpdaterReceiver(this);
+    this.sideloading_receiver_ = new H5vccUpdaterSideloadingReceiver(this);
 
-    this.stub_result_ = new Map();
+    this.reset();
   }
-
-  STUB_KEY_UPDATE_SERVER_URL = 'testUpdateServerUrl';
 
   start() {
     this.interceptor_.start();
+    this.sideloading_interceptor_.start();
   }
 
   stop() {
     this.interceptor_.stop();
+    this.sideloading_interceptor_.stop();
+  }
+
+  reset() {
+    this.called_reset_installations_ = false;
+    this.updater_channel_ = "";
+    this.update_status_ = "";
+    this.installation_index_ = 0;
+    this.library_sha256_ = new Map();
+    this.allow_self_signed_packages_ = false;
+    this.update_server_url_ = "";
+    this.require_network_encryption_ = false;
+    if (this.receiver_) {
+        this.receiver_.$.close();
+    }
+    if (this.sideloading_receiver_) {
+        this.sideloading_receiver_.$.close();
+    }
   }
 
   bind(handle) {
     this.receiver_.$.bindHandle(handle);
   }
 
-  reset() {
-    this.stub_result_ = new Map();
+  bindSideloading(handle) {
+    this.sideloading_receiver_.$.bindHandle(handle);
   }
 
-  stubResult(key, value) {
-    this.stub_result_.set(key, value);
+  hasCalledResetInstallations() {
+    return this.called_reset_installations_;
   }
 
-  stubGetUpdateServerUrl(updateServerUrl) {
-    this.stubResult(this.STUB_KEY_UPDATE_SERVER_URL, updateServerUrl);
+  async resetInstallations() {
+    this.called_reset_installations_ = true;
   }
 
-  exit() {
-    incrementExitCallCount();
+  // --- H5vccUpdater Interface Methods ---
+
+  async setUpdaterChannel(channel) {
+    this.updater_channel_ = channel;
+  }
+
+  async getUpdaterChannel() {
+    return { channel: this.updater_channel_ };
+  }
+
+  setMockUpdateStatus(status) {
+    this.update_status_ = status;
+  }
+
+  async getUpdateStatus() {
+    return { status: this.update_status_ };
+  }
+
+  setMockInstallationIndex(index) {
+    this.installation_index_ = index;
+  }
+
+  async getInstallationIndex() {
+    return { index: this.installation_index_ };
+  }
+
+  setMockAllowSelfSignedPackages(allow) {
+    this.allow_self_signed_packages_ = allow;
+  }
+
+  async getAllowSelfSignedPackages() {
+    return {
+        allowSelfSignedPackages: this.allow_self_signed_packages_,
+        allow_self_signed_packages: this.allow_self_signed_packages_
+    };
+  }
+
+  setMockUpdateServerUrl(url) {
+    this.update_server_url_ = url;
+  }
+
+  async getUpdateServerUrl() {
+    return {
+        updateServerUrl: this.update_server_url_,
+        update_server_url: this.update_server_url_
+    };
+  }
+
+  setMockRequireNetworkEncryption(require) {
+    this.require_network_encryption_ = require;
+  }
+
+  async getRequireNetworkEncryption() {
+    return {
+        requireNetworkEncryption: this.require_network_encryption_,
+        require_network_encryption: this.require_network_encryption_
+    };
+  }
+
+  setMockLibrarySha256(index, sha256) {
+    this.library_sha256_.set(index, sha256);
+  }
+
+  async getLibrarySha256(index) {
+    const sha256 = this.library_sha256_.get(index) || "";
+    return { sha256: sha256 };
+  }
+
+  // --- H5vccUpdaterSideloading Interface Methods ---
+
+  async setAllowSelfSignedPackages(allow) {
+    this.allow_self_signed_packages_ = allow;
+  }
+
+  async setUpdateServerUrl(url) {
+    this.update_server_url_ = url;
+  }
+
+  async setRequireNetworkEncryption(require) {
+    this.require_network_encryption_ = require;
   }
 }
 
