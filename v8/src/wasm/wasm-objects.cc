d39cfe6ef8b17692f7d7715946700140b7cde939
Sean Cuff <seancuff@google.com>
2026-05-29 17:15:23 -0700

    Disable memfd_create
    This is a followup to PR #10157 to remove additional usage of
    memfd_create that was missed in the original pass.
    
    Bug: 500418451

--- v8/src/wasm/wasm-objects.cc
+++ v8/src/wasm/wasm-objects.cc
@@ -1302,7 +1302,7 @@ DirectHandle<JSArrayBuffer> WasmMemoryObject::ToResizableBuffer(
 
 MaybeDirectHandle<WasmMemoryMapDescriptor>
 WasmMemoryMapDescriptor::NewFromAnonymous(Isolate* isolate, size_t length) {
-#if V8_TARGET_OS_LINUX
+#if V8_TARGET_OS_LINUX && !BUILDFLAG(IS_STARBOARD)
   CHECK(v8_flags.experimental_wasm_memory_control);
   DirectHandle<JSFunction> descriptor_ctor(
       isolate->native_context()->wasm_memory_map_descriptor_constructor(),
