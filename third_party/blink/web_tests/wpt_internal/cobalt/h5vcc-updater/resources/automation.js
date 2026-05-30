c5daf00ceddcd01383596b1464f9521891819a71
Andrew Savage <andrewsavage@google.com>
2026-05-30 08:40:02 -0400

    Cherry pick PR #10668: Cherry pick PR #8637: Web tests for h5vcc updater
    Refer to original PR: #10668
    
    Comprehensive web platform tests are added for all H5vccUpdater
    methods, including specific tests that verify promise rejection upon
    Mojo connection errors.
    
    Bug: 454440974
    Bug: 515122610
    
    ---------
    
    Co-authored-by: Madhura Jayaraman <madjay@google.com>
    
    (cherry picked from commit 7041d5db5)

--- third_party/blink/web_tests/wpt_internal/cobalt/h5vcc-updater/resources/automation.js
+++ third_party/blink/web_tests/wpt_internal/cobalt/h5vcc-updater/resources/automation.js
@@ -1,18 +1,19 @@
 'use strict';
 
-
 let mockH5vccUpdater = undefined;
 
 function h5vcc_updater_tests(func, name, properties) {
-  promise_test(async t => {
+  promise_test(async (test) => {
+    // Wait for any previous connection error to propagate.
+    await new Promise(resolve => test.step_timeout(resolve, 200));
+
+    assert_implements(window.h5vcc.updater,
+                      'missing window.h5vcc.updater');
     if (mockH5vccUpdater === undefined) {
-      if (isChromiumBased) {
-        const mocks = await import('./mock-h5vcc-updater.js');
-        mockH5vccUpdater = mocks.mockH5vccUpdater;
-      }
+      const mocks = await import('./mock-h5vcc-updater.js');
+      mockH5vccUpdater = mocks.mockH5vccUpdater;
     }
-    assert_implements(
-        mockH5vccUpdater, 'missing mockH5vccUpdater after initialization');
+    assert_implements(mockH5vccUpdater, 'missing mockH5vccUpdater');
 
     mockH5vccUpdater.start();
     try {
@@ -20,6 +21,29 @@ function h5vcc_updater_tests(func, name, properties) {
     } finally {
       mockH5vccUpdater.stop();
       mockH5vccUpdater.reset();
+    }
+  }, name, properties);
+}
+
+function h5vcc_updater_mojo_disconnection_tests(func, name, properties) {
+  promise_test(async (test) => {
+    const { H5vccUpdater, H5vccUpdaterSideloading } = await import(
+      '/gen/cobalt/browser/h5vcc_updater/public/mojom/h5vcc_updater.mojom.m.js');
+    let interceptor =
+      new MojoInterfaceInterceptor(H5vccUpdater.$interfaceName);
+    interceptor.oninterfacerequest = e => e.handle.close();
+    interceptor.start();
+
+    let sideloading_interceptor =
+      new MojoInterfaceInterceptor(H5vccUpdaterSideloading.$interfaceName);
+    sideloading_interceptor.oninterfacerequest = e => e.handle.close();
+    sideloading_interceptor.start();
+
+    try {
+      await func(test, interceptor);
+    } finally {
+      interceptor.stop();
+      sideloading_interceptor.stop();
     }
   }, name, properties);
 }
