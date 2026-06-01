d3adfbba4d917d6d5bed97796072400b4d69ad63
Andrew Savage <andrewsavage@google.com>
2026-06-01 11:54:29 -0400

    Cherry pick PR #10643: h5vcc: Re-enable passing web tests
    Refer to original PR: #10643
    
    And fix the logic in the failing sideloader tests.
    
    Bug: 515122610
    
    (cherry picked from commit c23349be23f89e646dfdf5a321f27aad025e252d)

--- third_party/blink/web_tests/wpt_internal/cobalt/h5vcc-updater/h5vcc-updater-getUpdateServerUrl.https.any.js
+++ third_party/blink/web_tests/wpt_internal/cobalt/h5vcc-updater/h5vcc-updater-getUpdateServerUrl.https.any.js
@@ -11,5 +11,5 @@ h5vcc_updater_tests(async (t, mockH5vccUpdater) => {
 
 h5vcc_updater_mojo_disconnection_tests(async (t) => {
   return promise_rejects_exactly(
-    t, 'API not supported for this platform.', window.h5vcc.updater.getUpdateServerUrl());
+    t, 'API not supported for this build configuration. Enabled for sideloading only.', window.h5vcc.updater.getUpdateServerUrl());
 }, 'getUpdateServerUrl() rejects when unimplemented due to pipe closure');
