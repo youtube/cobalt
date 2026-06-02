c6faaf2a8cd3b2eea53c61aa9b01cde88723bdab
Sean Cuff <seancuff@google.com>
2026-06-02 14:59:15 -0700

    Disable `sched_getcpu` in absl

--- third_party/abseil-cpp/absl/debugging/failure_signal_handler.cc
+++ third_party/abseil-cpp/absl/debugging/failure_signal_handler.cc
@@ -350,7 +350,7 @@ using GetTidType = decltype(absl::base_internal::GetTID());
 ABSL_CONST_INIT static std::atomic<GetTidType> failed_tid(0);
 
 static int GetCpuNumber() {
-#ifdef ABSL_HAVE_SCHED_GETCPU
+#if defined(ABSL_HAVE_SCHED_GETCPU) && !defined(STARBOARD)
   return sched_getcpu();
 #elif defined(ABSL_HAVE_PTHREAD_CPU_NUMBER_NP)
   size_t cpu_num;
