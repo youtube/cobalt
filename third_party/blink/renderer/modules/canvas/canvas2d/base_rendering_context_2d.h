2e66b8cb7e2173e550bbc103797dde001908ed40
Charley Kim <charleykim@google.com>
2026-06-03 20:49:55 +0000

    gate variable definition

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.h
@@ -5,6 +5,7 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_CANVAS_CANVAS2D_BASE_RENDERING_CONTEXT_2D_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_CANVAS_CANVAS2D_BASE_RENDERING_CONTEXT_2D_H_
 
+#include "build/buildflag.h"
 #include <cstddef>
 #include <memory>
 #include <utility>
@@ -312,7 +313,11 @@ class MODULES_EXPORT BaseRenderingContext2D : public CanvasRenderingContext,
 
   int num_readbacks_performed_ = 0;
   unsigned read_count_ = 0;
+#if !BUILDFLAG(IS_COBALT)
   Member<GPUTexture> webgpu_access_texture_ = nullptr;
+#else
+  void* webgpu_access_texture_ = nullptr;
+#endif
   std::unique_ptr<CanvasResourceProvider> resource_provider_from_webgpu_access_;
   Canvas2DColorParams color_params_;
   bool need_dispatch_context_restored_ = false;
