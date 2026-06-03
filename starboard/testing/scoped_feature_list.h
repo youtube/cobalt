025d4feccc65ba3bc1c17a3c026d7830edb37855
Kyujung Youn <kjyoun@google.com>
2026-06-03 12:35:07 -0700

    Add support for scoped feature overrides in Starboard FeatureList for testing
    Added SetFeatureForTesting, ClearFeatureForTesting, ClearAllFeaturesForTesting, and HasOverrideForTesting APIs to starboard::features::FeatureList. Relaxed IsEnabledByName to support checking overrides when FeatureList is not initialized.
    
    Added starboard::features::testing::ScopedFeatureList helper for RAII-style feature overrides in tests.
    
    Added RejectLowPerformanceSoftwareDecoder unit test to MediaCapabilitiesCacheTest to verify the feature override functionality on Android.
    
    Added unit tests for ScopedFeatureList in starboard/testing/scoped_feature_list_test.cc.

---
+++ starboard/testing/scoped_feature_list.h
@@ -0,0 +1,58 @@
+// Copyright 2026 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef STARBOARD_TESTING_SCOPED_FEATURE_LIST_H_
+#define STARBOARD_TESTING_SCOPED_FEATURE_LIST_H_
+
+#include <string>
+#include <vector>
+
+#include "starboard/shared/starboard/feature_list.h"
+
+namespace starboard::features {
+
+// Helper class to override features in tests.
+// This follows the pattern of base::test::ScopedFeatureList in Chromium.
+//
+// This class is NOT thread-safe. It modifies global state in FeatureList,
+// so it should only be used in single-threaded test contexts or when access
+// to FeatureList is otherwise synchronized. Multiple ScopedFeatureList
+// instances should not be active concurrently if they modify the same features.
+class ScopedFeatureList {
+ public:
+  ScopedFeatureList() = default;
+  ~ScopedFeatureList();
+
+  // Disallow copy and assign.
+  ScopedFeatureList(const ScopedFeatureList&) = delete;
+  ScopedFeatureList& operator=(const ScopedFeatureList&) = delete;
+
+  // Overrides the given feature to be enabled.
+  void InitAndEnableFeature(const SbFeature& feature);
+  void InitAndEnableFeature(const std::string& feature_name);
+
+  // Overrides the given feature to be disabled.
+  void InitAndDisableFeature(const SbFeature& feature);
+  void InitAndDisableFeature(const std::string& feature_name);
+
+  // Resets all overrides made by this instance.
+  void Reset();
+
+ private:
+  std::vector<std::string> overridden_features_;
+};
+
+}  // namespace starboard::features
+
+#endif  // STARBOARD_TESTING_SCOPED_FEATURE_LIST_H_
