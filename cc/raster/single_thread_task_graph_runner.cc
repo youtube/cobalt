b395cffb45e4b31255a00a7fd190aeca178efd6f
Adam Walls <avvall@google.com>
2026-06-10 20:15:40 +0000

    Instrumenting Base Threads

--- cc/raster/single_thread_task_graph_runner.cc
+++ cc/raster/single_thread_task_graph_runner.cc
@@ -16,6 +16,10 @@
 #include "base/trace_event/trace_event.h"
 #include "base/trace_event/typed_macros.h"
 
+#if BUILDFLAG(IS_COBALT)
+#include "base/memory/cobalt_memory_context.h"
+#endif
+
 namespace cc {
 
 SingleThreadTaskGraphRunner::SingleThreadTaskGraphRunner()
@@ -175,6 +179,10 @@ bool SingleThreadTaskGraphRunner::RunTaskWithLockAcquired() {
                 perfetto::TerminatingFlow::Global(
                     prioritized_task.task->trace_task_id()));
     base::AutoUnlock unlock(lock_);
+#if BUILDFLAG(IS_COBALT)
+    base::memory::ScopedMemoryContext scoped_context(
+        base::memory::MemoryContext::kGraphicsCompositor);
+#endif
     prioritized_task.task->RunOnWorkerThread();
   }
 
