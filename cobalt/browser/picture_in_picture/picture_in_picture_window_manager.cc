315b10cd67d1a74d8aea3ea85e7326fe9d0513c5
Ziliang Guo <ziliangguo@google.com>
2026-06-10 23:00:38 +0000

    connect shell.cc to pip_window_manager

---
+++ cobalt/browser/picture_in_picture/picture_in_picture_window_manager.cc
@@ -0,0 +1,27 @@
+// Copyright 2026 The Cobalt Authors. All Rights Reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "cobalt/browser/picture_in_picture/picture_in_picture_window_manager.h"
+
+#include "content/public/browser/picture_in_picture_window_controller.h"
+#include "content/public/browser/video_picture_in_picture_window_controller.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_delegate.h"
+#include "content/public/browser/web_contents_observer.h"
+#include "content/public/common/url_constants.h"
+
+
+PictureInPictureWindowManager* PictureInPictureWindowManager::GetInstance() {
+  return base::Singleton<PictureInPictureWindowManager>::get();
+}
+
+void PictureInPictureWindowManager::EnterPictureInPictureWithController(
+      content::PictureInPictureWindowController* pip_window_controller){
+        
+}
+
+
+
+class PictureInPictureWindowManager::VideoWebContentsObserver final
+    : public content::WebContentsObserver {};
