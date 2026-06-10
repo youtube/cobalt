6e169bf6ea510937f6e7492bd7e2cb76a3051e6e
Adam Walls <avvall@google.com>
2026-06-10 23:24:20 +0000

    Instrumenting Scripting Engine (V8 & Gin)

--- v8/src/wasm/module-decoder.cc
+++ v8/src/wasm/module-decoder.cc
@@ -4,6 +4,11 @@
 
 #include "src/wasm/module-decoder.h"
 
+#if BUILDFLAG(IS_COBALT)
+#include "base/memory/cobalt_memory_context.h" // nogncheck
+#include "cobalt/shell/buildflags.h"
+#endif
+
 #include "src/logging/metrics.h"
 #include "src/tracing/trace-event.h"
 #include "src/wasm/constant-expression.h"
@@ -529,6 +534,9 @@ class ValidateFunctionsTask : public JobTask {
   }
 
   void Run(JobDelegate* delegate) override {
+#if BUILDFLAG(IS_COBALT)
+    ::base::memory::ScopedMemoryContext scoped_context(::base::memory::MemoryContext::kScript);
+#endif
     TRACE_EVENT0(TRACE_DISABLED_BY_DEFAULT("v8.wasm.detailed"),
                  "wasm.ValidateFunctionsTask");
 
