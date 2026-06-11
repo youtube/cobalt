fbd20d29af22411fad2db7b0add4dfe4e2537b7b
Andrew Savage <andrewsavage@google.com>
2026-06-11 13:57:14 -0400

    Fix resume from preload showing white screen (#10842)
    Bug: 490989945

--- cobalt/shell/browser/shell_platform_delegate_views.cc
+++ cobalt/shell/browser/shell_platform_delegate_views.cc
@@ -455,6 +455,9 @@ void ShellPlatformDelegate::SetContents(Shell* shell) {
 void ShellPlatformDelegate::DidCreateOrAttachWebContents(
     Shell* shell,
     WebContents* web_contents) {
+  if (!is_visible_) {
+    previously_visible_web_contents_.insert(web_contents);
+  }
   auto it = shell_data_map_.find(shell);
   if (it == shell_data_map_.end()) {
     return;
