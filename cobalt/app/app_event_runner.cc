a473ea1e37a2f459cc86f2e3c878f12a1d5cd5d6
Jelle Foks <jfoks@google.com>
2026-06-10 20:38:21 -0700

    Initialize musl via static initializer to fix stack canary imbalance

--- cobalt/app/app_event_runner.cc
+++ cobalt/app/app_event_runner.cc
@@ -53,7 +53,6 @@
 #include <cstdio>
 
 #if BUILDFLAG(IS_COBALT_HERMETIC_BUILD)
-#include <init_musl.h>
 #if BUILDFLAG(USE_EVERGREEN)
 #include "cobalt/browser/loader_app_metrics.h"
 #endif
@@ -118,9 +117,6 @@ class AppEventRunnerImpl : public AppEventRunner,
 
   void DoStart(const SbEvent* event) override {
     SbEventStartData* data = static_cast<SbEventStartData*>(event->data);
-#if BUILDFLAG(IS_COBALT_HERMETIC_BUILD)
-    init_musl();
-#endif
     InitializeSystem();
 #if BUILDFLAG(IS_STARBOARD)
     platform_event_source_ =
