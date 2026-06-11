474b609cb21ed25490bd4a816d15284b0ece571e
Kyujung Youn <kjyoun@google.com>
2026-06-11 14:19:11 -0700

    no legacy JNI in platform_service.cc

--- starboard/android/shared/platform_service.cc
+++ starboard/android/shared/platform_service.cc
@@ -119,8 +119,9 @@ void* Send(CobaltExtensionPlatformService service,
     return nullptr;
   }
 
-  env->GetByteArrayRegion(j_out_data.obj(), 0, data_length,
-                          reinterpret_cast<jbyte*>(output));
+  base::android::JavaByteArrayToByteSpan(
+      env, j_out_data,
+      {static_cast<uint8_t*>(output), static_cast<size_t>(data_length)});
 
   *invalid_state = Java_ResponseToClient_getInvalidState(env, j_response);
   *output_length = data_length;
