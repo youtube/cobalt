b6869ac55319b7c890409793358847dac9a27557
Kyujung Youn <kjyoun@google.com>
2026-06-25 15:31:11 -0700

    Refactor tvOS-specific thread priority setting
    Move platform-specific thread naming and priority setting from common
    code in thread.cc to platform-specific thread_platform implementations.
    
    - Implement SetCurrentThreadPriority and SetCurrentThreadName in
      thread_platform_base.cc (POSIX), thread_platform_android.cc, and
      thread_platform.mm (tvOS).
    - Clean up thread.cc to use these platform functions, removing
      direct Apple-specific preprocessor checks.
    - Order functions alphabetically in declarations and implementations.
    - Rename thread_platform_tvos.cc to thread_platform.mm to match
      conventions.
    
    TAG=agy
    CONV=6f9d5b4f-ca52-4d90-a529-e5394a51d4c4

--- starboard/common/thread_platform_base.cc
+++ starboard/common/thread_platform_base.cc
@@ -14,9 +14,24 @@
  * limitations under the License.
  */
 
+#include <pthread.h>
+#include <sys/resource.h>
+
+#include "starboard/common/thread.h"
 #include "starboard/common/thread_platform.h"
 
 namespace starboard {
+
+void SetCurrentThreadName(const char* name) {
+  pthread_setname_np(pthread_self(), name);
+}
+
+bool SetCurrentThreadPriority(ThreadPriority priority) {
+  // setpriority returns 0 on success and -1 on failure. The default nice value
+  // is 0. See https://linux.die.net/man/2/setpriority
+  return (setpriority(PRIO_PROCESS, 0, ThreadPriorityToNiceValue(priority)) ==
+          0);
+}
 
 void TerminateOnThread() {}
 
