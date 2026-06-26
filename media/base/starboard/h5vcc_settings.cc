7effbec12f03c94eb3873d99d77bc23eaba1d702
Kyujung Youn <kjyoun@google.com>
2026-06-26 08:45:04 -0700

    Refactor H5VCC settings to generic string map
    Replaced the custom ExperimentalFeatures struct with H5vccSettingsMap
    (std::map<std::string, std::string>) across the Cobalt media factory and
    Starboard rendering pipeline. This simplifies CobaltContentRendererClient into
    a pass-through parser for H5VCC experiment flags, eliminating the need to update
    struct definitions and Mojo IPC boilerplate when new experiment flags are added.
    
    TAG=agy
    CONV=535a5e10-c69e-473c-b347-9d6a9a961ff7

---
+++ media/base/starboard/h5vcc_settings.cc
@@ -0,0 +1,68 @@
+// Copyright 2025 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "media/base/starboard/h5vcc_settings.h"
+
+#include "base/strings/string_number_conversions.h"
+
+namespace media {
+
+namespace {
+constexpr int kH5vccUnsetSentinel = 0;
+}  // namespace
+
+bool H5vccSettingsKey::GetBool(const H5vccSettingsMap& settings) const {
+  auto it = settings.find(std::string(key_));
+  if (it == settings.end()) {
+    return false;
+  }
+  return it->second != "0" && it->second != "false";
+}
+
+std::optional<bool> H5vccSettingsKey::GetOptionalBool(
+    const H5vccSettingsMap& settings) const {
+  auto it = settings.find(std::string(key_));
+  if (it == settings.end()) {
+    return std::nullopt;
+  }
+  return it->second != "0" && it->second != "false";
+}
+
+std::optional<int> H5vccSettingsKey::GetRangedInt(
+    const H5vccSettingsMap& settings,
+    int min_val,
+    int max_val) const {
+  return GetRangedInt(settings, min_val, max_val, kH5vccUnsetSentinel);
+}
+
+std::optional<int> H5vccSettingsKey::GetRangedInt(
+    const H5vccSettingsMap& settings,
+    int min_val,
+    int max_val,
+    int unset_sentinel) const {
+  auto it = settings.find(std::string(key_));
+  if (it == settings.end()) {
+    return std::nullopt;
+  }
+  int64_t val = 0;
+  if (!base::StringToInt64(it->second, &val)) {
+    return std::nullopt;
+  }
+  if (val == unset_sentinel || val < min_val || val > max_val) {
+    return std::nullopt;
+  }
+  return static_cast<int>(val);
+}
+
+}  // namespace media
