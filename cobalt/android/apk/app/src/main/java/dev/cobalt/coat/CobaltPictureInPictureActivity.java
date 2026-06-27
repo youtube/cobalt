88ee3ef50820e23c8578f2d303655592f2adc69a
Ziliang Guo <ziliangguo@google.com>
2026-06-27 21:45:59 +0000

    fix copyright and update demo

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltPictureInPictureActivity.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CobaltPictureInPictureActivity.java
@@ -1,6 +1,16 @@
 // Copyright 2026 The Cobalt Authors. All Rights Reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package dev.cobalt.coat;
 
@@ -17,7 +27,6 @@ import org.jni_zero.NativeMethods;
 import org.chromium.base.ContextUtils;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.base.WindowAndroid;
-
 import org.chromium.components.thinwebview.CompositorView;
 import org.chromium.components.thinwebview.CompositorViewFactory;
 import org.chromium.components.thinwebview.ThinWebViewConstraints;
@@ -25,6 +34,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import org.chromium.ui.base.ActivityWindowAndroid;
 import org.chromium.ui.base.IntentRequestTracker;
+import static dev.cobalt.util.Log.TAG;
 
 /**
  * A simple Activity to host the Video Picture-in-Picture window on Android TV.
@@ -33,8 +43,6 @@ import org.chromium.ui.base.IntentRequestTracker;
  */
 @JNINamespace("cobalt")
 public class CobaltPictureInPictureActivity extends Activity {
-
-  private static final String TAG = "CobaltPiPActivity";
   private long mNativeCobaltVideoOverlayWindow = 0;
   private CompositorView mCompositorView;
   private ActivityWindowAndroid mWindowAndroid;
