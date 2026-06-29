d352ad55b074b0ea0dcbdca77e65bb3fd239c82b
Drew Thomas <async@google.com>
2026-06-29 16:53:05 -0700

    android: Add OES_EGL_image_external support to WebGL (#10574)
    This PR introduces support for the `OES_EGL_image_external` WebGL
    extension in Cobalt. This extension allows WebGL contexts to bind EGL
    images from HTMLVideoElement video frames directly as external textures
    (`GL_TEXTURE_EXTERNAL_OES`).
    
    This enables high-performance, zero-copy video rendering pipelines in
    WebGL, which is critical for embedded platforms where hardware video
    decoders output directly to EGL images via Starboard's `SbDecodeTarget`
    interface.
    
    This has been added specifically for Android TV support. Support for
    other Starboard implementations is omitted, but can be re-visited in the
    future.
    
    Bug: 503046791
    
    ---------
    
    Co-authored-by: Drew Thomas <acthomas@google.com>

--- third_party/angle/src/libANGLE/Context.cpp
+++ third_party/angle/src/libANGLE/Context.cpp
@@ -4073,6 +4073,10 @@ Extensions Context::generateSupportedExtensions() const
     // Blob cache extension is provided by the ANGLE frontend
     supportedExtensions.blobCacheANGLE = true;
 
+#if defined(ENABLE_BUILDFLAG_IS_COBALT) && defined(__ANDROID__)
+    supportedExtensions.EGLImageExternalOES = true;
+#endif  // defined(ENABLE_BUILDFLAG_IS_COBALT) && defined(__ANDROID__)
+
     return supportedExtensions;
 }
 
@@ -4123,6 +4127,10 @@ void Context::initCaps()
 
     Extensions *extensions = mState.getMutableExtensions();
     *extensions            = mSupportedExtensions;
+#if defined(ENABLE_BUILDFLAG_IS_COBALT) && defined(__ANDROID__)
+    extensions->EGLImageExternalOES = true;
+#endif  // defined(ENABLE_BUILDFLAG_IS_COBALT) && defined(__ANDROID__)
+
 
     // GLES1 emulation: Initialize caps (Table 6.20 / 6.22 in the ES 1.1 spec)
     if (getClientVersion() < Version(2, 0))
@@ -4305,6 +4313,10 @@ void Context::initCaps()
             extensions->*(extensionInfo.second.ExtensionsMember) = false;
         }
     }
+
+#if defined(ENABLE_BUILDFLAG_IS_COBALT) && defined(__ANDROID__)
+    extensions->EGLImageExternalOES = true;
+#endif  // defined(ENABLE_BUILDFLAG_IS_COBALT) && defined(__ANDROID__)
 
     // Hide emulated ETC1 extension from WebGL contexts.
     if (mWebGLContext && limitations.emulatedEtc1)
