e82bb538a00bd7b2d98ce0745b811f45f2a883f4
Jelle Foks <jfoks@google.com>
2026-06-30 16:48:50 -0700

    Enable static angle

--- third_party/angle/src/libANGLE/renderer/gl/egl/FunctionsEGLDL.cpp
+++ third_party/angle/src/libANGLE/renderer/gl/egl/FunctionsEGLDL.cpp
@@ -10,10 +10,15 @@
 
 #include <dlfcn.h>
 
+#if defined(ANGLE_USE_STARBOARD)
+#include "starboard/egl.h"
+#endif
+
 namespace rx
 {
 namespace
 {
+#if !defined(ANGLE_USE_STARBOARD)
 // In ideal world, we would want this to be a member of FunctionsEGLDL,
 // and call dlclose() on it in ~FunctionsEGLDL().
 // However, some GL implementations are broken and don't allow multiple
@@ -22,6 +27,7 @@ namespace
 // This is consistent with Chromium's CleanupNativeLibraries() code,
 // referencing crbug.com/250813 and http://www.xfree86.org/4.3.0/DRI11.html
 void *nativeEGLHandle;
+#endif
 }  // anonymous namespace
 
 FunctionsEGLDL::FunctionsEGLDL() : mGetProcAddressPtr(nullptr) {}
@@ -33,7 +39,15 @@ egl::Error FunctionsEGLDL::initialize(EGLAttrib platformType,
                                       const char *libName,
                                       void *eglHandle)
 {
-
+#if defined(ANGLE_USE_STARBOARD)
+    const SbEglInterface* sb_egl = SbGetEglInterface();
+    if (!sb_egl || !sb_egl->eglGetProcAddress)
+    {
+        return egl::Error(EGL_NOT_INITIALIZED, "Could not find SbGetEglInterface or eglGetProcAddress");
+    }
+    mGetProcAddressPtr =
+        reinterpret_cast<PFNEGLGETPROCADDRESSPROC>(sb_egl->eglGetProcAddress);
+#else
     if (eglHandle)
     {
         // If the handle is provided, use it.
@@ -58,18 +72,65 @@ egl::Error FunctionsEGLDL::initialize(EGLAttrib platformType,
     {
         return egl::Error(EGL_NOT_INITIALIZED, "Could not find eglGetProcAddress");
     }
+#endif
 
     return FunctionsEGL::initialize(platformType, nativeDisplay);
 }
 
 void *FunctionsEGLDL::getProcAddress(const char *name) const
 {
-    void *f = reinterpret_cast<void *>(mGetProcAddressPtr(name));
-    if (f)
+    if (mGetProcAddressPtr)
     {
-        return f;
+        void *f = reinterpret_cast<void *>(mGetProcAddressPtr(name));
+        if (f)
+        {
+            return f;
+        }
     }
+#if defined(ANGLE_USE_STARBOARD)
+    const SbEglInterface* egl = SbGetEglInterface();
+    if (egl)
+    {
+        if (strcmp(name, "eglChooseConfig") == 0) return reinterpret_cast<void*>(egl->eglChooseConfig);
+        if (strcmp(name, "eglCopyBuffers") == 0) return reinterpret_cast<void*>(egl->eglCopyBuffers);
+        if (strcmp(name, "eglCreateContext") == 0) return reinterpret_cast<void*>(egl->eglCreateContext);
+        if (strcmp(name, "eglCreatePbufferSurface") == 0) return reinterpret_cast<void*>(egl->eglCreatePbufferSurface);
+        if (strcmp(name, "eglCreatePixmapSurface") == 0) return reinterpret_cast<void*>(egl->eglCreatePixmapSurface);
+        if (strcmp(name, "eglCreateWindowSurface") == 0) return reinterpret_cast<void*>(egl->eglCreateWindowSurface);
+        if (strcmp(name, "eglDestroyContext") == 0) return reinterpret_cast<void*>(egl->eglDestroyContext);
+        if (strcmp(name, "eglDestroySurface") == 0) return reinterpret_cast<void*>(egl->eglDestroySurface);
+        if (strcmp(name, "eglGetConfigAttrib") == 0) return reinterpret_cast<void*>(egl->eglGetConfigAttrib);
+        if (strcmp(name, "eglGetConfigs") == 0) return reinterpret_cast<void*>(egl->eglGetConfigs);
+        if (strcmp(name, "eglGetCurrentDisplay") == 0) return reinterpret_cast<void*>(egl->eglGetCurrentDisplay);
+        if (strcmp(name, "eglGetCurrentSurface") == 0) return reinterpret_cast<void*>(egl->eglGetCurrentSurface);
+        if (strcmp(name, "eglGetDisplay") == 0) return reinterpret_cast<void*>(egl->eglGetDisplay);
+        if (strcmp(name, "eglGetError") == 0) return reinterpret_cast<void*>(egl->eglGetError);
+        if (strcmp(name, "eglGetProcAddress") == 0) return reinterpret_cast<void*>(egl->eglGetProcAddress);
+        if (strcmp(name, "eglInitialize") == 0) return reinterpret_cast<void*>(egl->eglInitialize);
+        if (strcmp(name, "eglMakeCurrent") == 0) return reinterpret_cast<void*>(egl->eglMakeCurrent);
+        if (strcmp(name, "eglQueryContext") == 0) return reinterpret_cast<void*>(egl->eglQueryContext);
+        if (strcmp(name, "eglQueryString") == 0) return reinterpret_cast<void*>(egl->eglQueryString);
+        if (strcmp(name, "eglQuerySurface") == 0) return reinterpret_cast<void*>(egl->eglQuerySurface);
+        if (strcmp(name, "eglSwapBuffers") == 0) return reinterpret_cast<void*>(egl->eglSwapBuffers);
+        if (strcmp(name, "eglTerminate") == 0) return reinterpret_cast<void*>(egl->eglTerminate);
+        if (strcmp(name, "eglWaitGL") == 0) return reinterpret_cast<void*>(egl->eglWaitGL);
+        if (strcmp(name, "eglWaitNative") == 0) return reinterpret_cast<void*>(egl->eglWaitNative);
+        if (strcmp(name, "eglBindTexImage") == 0) return reinterpret_cast<void*>(egl->eglBindTexImage);
+        if (strcmp(name, "eglReleaseTexImage") == 0) return reinterpret_cast<void*>(egl->eglReleaseTexImage);
+        if (strcmp(name, "eglSurfaceAttrib") == 0) return reinterpret_cast<void*>(egl->eglSurfaceAttrib);
+        if (strcmp(name, "eglSwapInterval") == 0) return reinterpret_cast<void*>(egl->eglSwapInterval);
+        if (strcmp(name, "eglBindAPI") == 0) return reinterpret_cast<void*>(egl->eglBindAPI);
+        if (strcmp(name, "eglQueryAPI") == 0) return reinterpret_cast<void*>(egl->eglQueryAPI);
+        if (strcmp(name, "eglCreatePbufferFromClientBuffer") == 0) return reinterpret_cast<void*>(egl->eglCreatePbufferFromClientBuffer);
+        if (strcmp(name, "eglReleaseThread") == 0) return reinterpret_cast<void*>(egl->eglReleaseThread);
+        if (strcmp(name, "eglWaitClient") == 0) return reinterpret_cast<void*>(egl->eglWaitClient);
+        if (strcmp(name, "eglGetCurrentContext") == 0) return reinterpret_cast<void*>(egl->eglGetCurrentContext);
+    }
+    return nullptr;
+#else
     return dlsym(nativeEGLHandle, name);
+#endif
 }
 
 }  // namespace rx
+
