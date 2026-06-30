c350cc03a7af901445db4e564507a4e94f890294
Sunny Tang <tasunny@google.com>
2026-06-30 18:08:08 +0000

    remove constant

--- cobalt/android/apk/app/src/main/java/dev/cobalt/util/JavaSwitches.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/util/JavaSwitches.java
@@ -53,9 +53,6 @@ public class JavaSwitches {
   public static final String DISABLE_GPU_MEMORY_BUFFER_COMPOSITOR_RESOURCES =
       "DisableGpuMemoryBufferCompositorResources";
 
-  /** flag to disable v8 optimizing compilers (turbofan, maglev, sparkplug) */
-  public static final String DISABLE_V8_OPTIMIZING_COMPILERS = "DisableV8OptimizingCompilers";
-
   /** flag to enable concurrent marking for v8 garbage collection */
   public static final String ENABLE_V8_CONCURRENT_MARKING = "EnableV8ConcurrentMarking";
 
