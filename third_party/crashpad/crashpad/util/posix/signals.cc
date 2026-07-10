f610601bdda8d737ca0e8d728c4ae8fb000e12b2
Holden Warriner <hwarriner@google.com>
2026-07-10 19:09:13 +0000

    fix: Preserve siginfo when Crashpad re-raises sig
    We still wan to avoid building this code path when building libcobalt
    (with the default toolchain) in hermetic builds, as it introduces a
    compilation error. But we should build it when building the loader app
    with the starboard toolchain, so that signals are properly re-raised,
    with siginfo information, after they are handled by Crashpad. This
    information may be useful for other signal handlers on the system, for
    example for b/481094002.
    
    Issue: 476394156

--- third_party/crashpad/crashpad/util/posix/signals.cc
+++ third_party/crashpad/crashpad/util/posix/signals.cc
@@ -318,8 +318,12 @@ void Signals::RestoreHandlerAndReraiseSignalOnReturn(
   // signals that do not re-raise autonomously), such as signals delivered via
   // kill() and asynchronous hardware faults such as SEGV_MTEAERR, which would
   // otherwise be lost when re-raising the signal via raise().
-// TODO: (cobalt b/406511608) Re-enable once we use crashpad in hermetic builds.
-#if BUILDFLAG(IS_LINUX) && !BUILDFLAG(ENABLE_COBALT_HERMETIC_HACKS) || BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS)
+// TODO: (cobalt b/406511608) Re-enable if we decide to install Crashpad signal
+// handlers from the Cobalt layer in hermetic builds.
+#if BUILDFLAG(IS_LINUX) &&                       \
+    (!BUILDFLAG(ENABLE_COBALT_HERMETIC_HACKS) || \
+     BUILDFLAG(IS_STARBOARD_TOOLCHAIN)) ||       \
+    BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS)
   int retval = syscall(SYS_rt_tgsigqueueinfo,
                        getpid(),
                        syscall(SYS_gettid),
@@ -337,8 +341,10 @@ void Signals::RestoreHandlerAndReraiseSignalOnReturn(
   if (errno != EPERM) {
     _exit(kFailureExitCode);
   }
-#endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_ANDROID) ||
-        // BUILDFLAG(IS_CHROMEOS)
+#endif  // BUILDFLAG(IS_LINUX) &&
+        // (!BUILDFLAG(ENABLE_COBALT_HERMETIC_HACKS) ||
+        //  BUILDFLAG(IS_STARBOARD_TOOLCHAIN)) ||
+        // BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS)
 
   // Explicitly re-raise the signal if it will not re-raise itself. Because
   // signal handlers normally execute with their signal blocked, this raise()
