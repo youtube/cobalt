4be1adc1d82644767c9af2d8694eda587df8964c
Henrique Ferreiro <hferreiro@igalia.com>
2026-07-17 20:37:58 +0200

    Cherry pick PR #11390: aosp: Add GN rules to build APKs
    Refer to original PR: #11390
    
    Add the build rules that package a modular executable into an Android
    APK for
    the AOSP platform. This allows to create APKs for all test and
    modular_executable targets so that building <target>_loader builds
    <target>.apk.
    
    The APK bundles the evergreen loader as its native library (loader_app
    for
    cobalt and elf_loader_sandbox for tests) with the lz4-compressed inner
    library
    and its content as assets; MainActivity loads the loader and spawns the
    Starboard thread through a jni_zero binding.
    
    Most of the Java code is reused from AndroidTV, being the main exception
    separate MainActivity and CobaltApplication classes.
    
    Some non-working targets are currently excluded.
    
    Bug: 495203133
    
    (cherry picked from commit 97b9cb027b6e96cc29ac3c8167795879d3869c42)

---
+++ starboard/aosp/shared/main_activity.cc
@@ -0,0 +1,56 @@
+// Copyright 2026 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <jni.h>
+#include <pthread.h>
+
+#include <string>
+#include <thread>
+#include <vector>
+
+#include "cobalt/aosp/jni_headers/MainActivity_jni.h"
+#include "starboard/android/shared/starboard_bridge.h"
+#include "third_party/jni_zero/jni_zero.h"
+
+int main(int argc, char** argv);
+
+namespace {
+
+void StarboardMain() {
+  pthread_setname_np(pthread_self(), "StarboardMain");
+
+  JNIEnv* env = jni_zero::AttachCurrentThread();
+  std::vector<std::string> args;
+  args.push_back("cobalt_loader");
+  starboard::StarboardBridge::GetInstance()->AppendArgs(env, &args);
+
+  std::vector<char*> argv;
+  argv.reserve(args.size() + 1);
+  for (std::string& arg : args) {
+    argv.push_back(arg.data());
+  }
+  argv.push_back(nullptr);
+
+  main(static_cast<int>(args.size()), argv.data());
+}
+
+}  // namespace
+
+namespace starboard {
+
+void JNI_MainActivity_StartLoader(JNIEnv* env) {
+  std::thread(StarboardMain).detach();
+}
+
+}  // namespace starboard
