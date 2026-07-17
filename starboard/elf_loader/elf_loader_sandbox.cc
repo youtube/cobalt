f05661bfd1437a0f1942c5bdfe0d8351017ef361
Henrique Ferreiro <hferreiro@igalia.com>
2026-07-17 12:27:08 +0200

    Use starboard::EndsWith in elf_loader_sandbox

--- starboard/elf_loader/elf_loader_sandbox.cc
+++ starboard/elf_loader/elf_loader_sandbox.cc
@@ -33,15 +33,6 @@
 #include "starboard/shared/starboard/features_test_util.h"
 #endif
 
-namespace {
-
-bool EndsWith(const std::string& s, const std::string& suffix) {
-  return s.size() >= suffix.size() &&
-         s.compare(s.size() - suffix.size(), suffix.size(), suffix) == 0;
-}
-
-}  // namespace
-
 elf_loader::ElfLoader g_elf_loader;
 
 void (*g_sb_event_func)(const SbEvent*) = NULL;
@@ -62,9 +53,9 @@ void LoadLibraryAndInitialize(const std::string& library_path,
   }
 
   auto compression_type = elf_loader::CompressionType::kNone;
-  if (EndsWith(library_path, elf_loader::kLz4Suffix)) {
+  if (starboard::EndsWith(library_path, elf_loader::kLz4Suffix)) {
     compression_type = elf_loader::CompressionType::kLz4;
-  } else if (EndsWith(library_path, elf_loader::kZstdSuffix)) {
+  } else if (starboard::EndsWith(library_path, elf_loader::kZstdSuffix)) {
     compression_type = elf_loader::CompressionType::kZstd;
   }
   if (!g_elf_loader.Load(library_path, content_path, /*is_relative_path=*/true,
