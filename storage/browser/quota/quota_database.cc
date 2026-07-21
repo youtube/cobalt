b5dfa8d681f1dc6ae4040d97bf3d66e638dde2ab
Ying Yu <yuying@google.com>
2026-07-21 21:07:44 +0000

    cobalt: Stop timer when disabling QuotaDatabase
    TAG=agy
    CONV=d426d777-5359-4620-a4e0-ae79accb9bab

--- storage/browser/quota/quota_database.cc
+++ storage/browser/quota/quota_database.cc
@@ -1000,6 +1000,9 @@ void QuotaDatabase::Commit() {
 #if BUILDFLAG(IS_STARBOARD)
 void QuotaDatabase::DisableDatabase() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  if (timer_.IsRunning()) {
+    timer_.Stop();
+  }
   is_disabled_ = true;
   db_.reset();
   meta_table_.reset();
