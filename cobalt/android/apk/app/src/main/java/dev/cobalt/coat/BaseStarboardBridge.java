6872b8c67f94be2715d9c36c3773e7bc56276f44
Jelle Foks <jfoks@google.com>
2026-07-28 13:02:12 -0700

    android: Add RequestStop helper to send test completion signal
    Wire SbSystemRequestStop to call StarboardBridge.requestStop over JNI, allowing test execution runners to signal test completion and finish the test activity.
    
    BUG=b/338274026
    TAG=agy
    CONV=006ddb93-1731-4dba-b801-1f6fb67f1fd4

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/BaseStarboardBridge.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/BaseStarboardBridge.java
@@ -287,6 +287,7 @@ public class BaseStarboardBridge {
   }
 
   /* Immediate shutdown, used at least by StandalonePlayerActivity. */
+  @CalledByNative
   public void requestStop(int errorLevel) {
     applicationStopping();
     Activity activity = mActivityHolder.get();
