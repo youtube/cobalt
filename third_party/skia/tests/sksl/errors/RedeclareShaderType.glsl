f2d41b5e3b5798d3c1685ad17367cd1e7ebd55ea
GitHub Release Automation <github@google.com>
2026-07-29 22:48:42 +0000

    Restore submodules.

--- third_party/skia/tests/sksl/errors/RedeclareShaderType.glsl
+++
@@ -1,6 +0,0 @@
-### Compilation failed:
-
-error: 1: expected an identifier, but found type 'shader'
-uniform shader shader;
-               ^^^^^^
-1 error
--- third_party/skia/tests/sksl/errors/SpuriousFloat.glsl
+++
@@ -1,6 +0,0 @@
-### Compilation failed:
-
-error: 3: expected ';', but found '2.5'
-    x = 1.5 2.5;
-            ^^^
-1 error
