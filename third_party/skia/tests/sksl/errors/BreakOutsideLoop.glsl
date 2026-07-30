4992d479f0209b7f95eb67ed8e53b2b1187e105d
GitHub Release Automation <github@google.com>
2026-07-30 09:15:09 +0000

    Restore submodules.

--- third_party/skia/tests/sksl/errors/BreakOutsideLoop.glsl
+++
@@ -1,6 +0,0 @@
-### Compilation failed:
-
-error: 3: break statement must be inside a loop or switch
-    if (true) break;
-              ^^^^^
-1 error
--- third_party/skia/tests/sksl/errors/ReservedNameHvec2.glsl
+++
@@ -1,6 +0,0 @@
-### Compilation failed:
-
-error: 1: name 'hvec2' is reserved
-int hvec2;
-    ^^^^^
-1 error
