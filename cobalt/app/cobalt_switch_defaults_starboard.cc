8fc26221a2b169d1b8a738f3952256a940c19cd2
Anshu Gupta <anshugupta@google.com>
2026-08-04 21:29:51 +0000

    Fix lint error by wrapping long comment in cobalt_switch_defaults_starboard.cc

--- cobalt/app/cobalt_switch_defaults_starboard.cc
+++ cobalt/app/cobalt_switch_defaults_starboard.cc
@@ -110,7 +110,8 @@ CommandLinePreprocessor::GetCobaltParamSwitchDefaults() {
       {::switches::kContentShellHostWindowSize, "1920x1080"},
 #if !defined(COBALT_BUILD_TYPE_GOLD)
       // Enable remote DevTools, reachable from the network (e.g. SSH tunnel to
-      // the container IP) - matches the c25 dev experience, no on-device config.
+      // the container IP) - matches the c25 dev experience, no on-device
+      // config.
       {::switches::kRemoteDebuggingPort, "9222"},
 #endif
       // kEnableLowEndDeviceMode sets MSAA to 4 (and not 8, the default). But
