youtube/h5vcc2

Implement NdkAudioTrack using Android NDK AAudio
This change introduces NdkAudioTrack which implements the AudioTrack
interface using Android NDK AAudio APIs (AAudio.h) for API 28+ devices.
This enables NDK-based audio playback, bypassing JNI for audio writing.

Specifically:
- Implemented NdkAudioTrack using AAudio.
- Added AAudio dynamic loader (aaudio_loader) to support older Android devices at runtime.
- Updated AudioTrack::Create to instantiate NdkAudioTrack when kNdkAudio feature is enabled.
- Added kNdkAudio feature flag in feature_config.h.

TAG=agy
CONV=f34a5f73-1c82-4a7f-88aa-185850b85cb7

starboard/android/shared/audio_track.cc
@@ -14,12 +14,49 @@
 
 #include "starboard/android/shared/audio_track.h"
 
+#include <android/api-level.h>
+
 #include <memory>
 #include <optional>
 
+#include "starboard/android/shared/aaudio_loader.h"
 #include "starboard/android/shared/audio_track_bridge.h"
+#include "starboard/android/shared/ndk_audio_track.h"
+#include "starboard/common/log.h"
+#include "starboard/shared/starboard/features.h"
 
 namespace starboard {
+namespace {
+
+bool CanUseNdkAudioTrack(SbMediaAudioCodingType coding_type,
+                         std::optional<SbMediaAudioSampleType> sample_type,
+                         std::optional<int> tunnel_mode_audio_session_id) {
+  if (!features::FeatureList::IsEnabled(features::kNdkAudio)) {
+    return false;
+  }
+
+  if (coding_type != kSbMediaAudioCodingTypePcm ||
+      sample_type != kSbMediaAudioSampleTypeFloat32) {
+    return false;
+  }
+
+  if (!AAudioLoader::GetInstance()) {
+    return false;
+  }
+
+  // NDK audio track does not support tunnel mode.
+  if (tunnel_mode_audio_session_id) {
+    return false;
+  }
+  // NDK AAudio requires API level >= 28.
+  if (android_get_device_api_level() < 28) {
+    return false;
+  }
+
+  return true;
+}
+
+}  // namespace
 
 // static
 std::unique_ptr<AudioTrack> AudioTrack::Create(
@@ -30,6 +67,19 @@ std::unique_ptr<AudioTrack> AudioTrack::Create(
     int preferred_buffer_size_in_bytes,
     std::optional<int> tunnel_mode_audio_session_id,
     bool is_web_audio) {
+  if (CanUseNdkAudioTrack(coding_type, sample_type,
+                          tunnel_mode_audio_session_id)) {
+    auto ndk_audio_track = NdkAudioTrack::Create(
+        coding_type, sample_type, channels, sampling_frequency_hz,
+        preferred_buffer_size_in_bytes, tunnel_mode_audio_session_id,
+        is_web_audio);
+    if (ndk_audio_track) {
+      return ndk_audio_track;
+    }
+    SB_LOG(WARNING)
+        << "Failed to create NdkAudioTrack. Falling back to AudioTrackBridge.";
+  }
+
   return AudioTrackBridge::Create(coding_type, sample_type, channels,
                                   sampling_frequency_hz,
                                   preferred_buffer_size_in_bytes,

youtube/cobalt

starboard/android/shared/audio_track.cc
@@ -14,12 +14,49 @@
 
 #include "starboard/android/shared/audio_track.h"
 
+#include <android/api-level.h>
+
 #include <memory>
 #include <optional>
 
+#include "starboard/android/shared/aaudio_loader.h"
 #include "starboard/android/shared/audio_track_bridge.h"
+#include "starboard/android/shared/media_capabilities_cache.h"
+#include "starboard/android/shared/ndk_audio_track.h"
+#include "starboard/common/log.h"
 
 namespace starboard {
+namespace {
+
+bool CanUseNdkAudioTrack(SbMediaAudioCodingType coding_type,
+                         std::optional<SbMediaAudioSampleType> sample_type,
+                         std::optional<int> tunnel_mode_audio_session_id) {
+  if (!MediaCapabilitiesCache::GetInstance()->IsNdkAudioEnabled()) {
+    return false;
+  }
+
+  if (coding_type != kSbMediaAudioCodingTypePcm ||
+      sample_type != kSbMediaAudioSampleTypeFloat32) {
+    return false;
+  }
+
+  // NDK audio track does not support tunnel mode.
+  if (tunnel_mode_audio_session_id) {
+    return false;
+  }
+  // NDK AAudio requires API level >= 28.
+  if (android_get_device_api_level() < 28) {
+    return false;
+  }
+
+  if (!AAudioLoader::GetInstance()) {
+    return false;
+  }
+
+  return true;
+}
+
+}  // namespace
 
 // static
 std::unique_ptr<AudioTrack> AudioTrack::Create(
@@ -30,6 +67,19 @@ std::unique_ptr<AudioTrack> AudioTrack::Create(
     int preferred_buffer_size_in_bytes,
     std::optional<int> tunnel_mode_audio_session_id,
     bool is_web_audio) {
+  if (CanUseNdkAudioTrack(coding_type, sample_type,
+                          tunnel_mode_audio_session_id)) {
+    auto ndk_audio_track = NdkAudioTrack::Create(
+        coding_type, sample_type, channels, sampling_frequency_hz,
+        preferred_buffer_size_in_bytes, tunnel_mode_audio_session_id,
+        is_web_audio);
+    if (ndk_audio_track) {
+      return ndk_audio_track;
+    }
+    SB_LOG(WARNING)
+        << "Failed to create NdkAudioTrack. Falling back to AudioTrackBridge.";
+  }
+
   return AudioTrackBridge::Create(coding_type, sample_type, channels,
                                   sampling_frequency_hz,
                                   preferred_buffer_size_in_bytes,