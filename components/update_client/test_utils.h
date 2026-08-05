2406e76b9002da8655742ad8289174954f3e56fb
Anshu Gupta <anshugupta@google.com>
2026-08-05 20:43:24 +0000

    Port update_client unit tests for Starboard/Linux compatibility

--- components/update_client/test_utils.h
+++ components/update_client/test_utils.h
@@ -5,6 +5,8 @@
 #ifndef COMPONENTS_UPDATE_CLIENT_TEST_UTILS_H_
 #define COMPONENTS_UPDATE_CLIENT_TEST_UTILS_H_
 
+#include "build/build_config.h"
+
 namespace base {
 class FilePath;
 }
@@ -22,6 +24,20 @@ namespace update_client {
 // should be handled by the caller.
 [[nodiscard]] base::FilePath DuplicateTestFile(const base::FilePath& temp_path,
                                                const char* file);
+
+#if BUILDFLAG(IS_STARBOARD)
+class ScopedStarboardInstallationMock {
+ public:
+  ScopedStarboardInstallationMock();
+  ~ScopedStarboardInstallationMock();
+
+  void SetInstallationPath(const char* path);
+  void SetRequestRollForwardSuccess(bool success);
+
+  ScopedStarboardInstallationMock(const ScopedStarboardInstallationMock&) = delete;
+  ScopedStarboardInstallationMock& operator=(const ScopedStarboardInstallationMock&) = delete;
+};
+#endif
 
 }  // namespace update_client
 
