f13ef9d882e0a89657b810f7c66c51bdf02bb833
Sherry Zhou <sherryzy@google.com>
2026-08-05 09:33:46 +0800

    Cherry pick PR #11669: android: Allow configuring V8 initial old space size
    Refer to original PR: #11669
    
    - Define V8_INITIAL_OLD_SPACE_SIZE Java switch constant in
    JavaSwitches.java.
    - Handle V8InitialOldSpaceSize in
    JavaSwitches.getExtraCommandLineArgs(), defaulting to 64MB if not
    provided.
    - Remove hardcoded --initial-old-space-size=64 from static default list
    in CommandLineOverrideHelper.java.
    - Update CommandLineOverrideHelperTest.java accordingly.
    
    Bug: 540879465
    
    (cherry picked from commit 2c1138f4c108741925a62315ad8a3354ee86ed6f)

--- cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CommandLineOverrideHelper.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/coat/CommandLineOverrideHelper.java
@@ -90,8 +90,7 @@ public final class CommandLineOverrideHelper {
 
         // Trades a little V8 performance for significant memory savings.
         paramOverrides.add("--optimize-for-size");
-        // Set initial old space size to 64MB and max old space size to 512MB.
-        paramOverrides.add("--initial-old-space-size=64");
+        // Set max old space size to 512MB.
         paramOverrides.add("--max-old-space-size=512");
 
         // Disable decommitting pooled pages to prevent virtual memory fragmentation.
