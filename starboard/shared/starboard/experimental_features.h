4a1d83fef8813bfd3fc434d6a458450596a6d0f9
Xiaoming Shi <xiaomings@google.com>
2026-08-05 02:35:02 +0000

    Cherry pick PR #11696: starboard: Add option to force dual threads
    Refer to original PR: #11696
    
    Add Media.ForceDualThreads experimental feature to allow forcing dual
    threads in player components factory even when not using libopus
    decoder.
    
    Bug: 329686979
    
    (cherry picked from commit c80f0b5149068f853ddfb7a9bf1bef1c84904c74)

--- starboard/shared/starboard/experimental_features.h
+++ starboard/shared/starboard/experimental_features.h
@@ -192,6 +192,9 @@ inline constexpr ExperimentalFeatureKey<bool> kMediaFlushAudioTrackDuringSeek(
 inline constexpr ExperimentalFeatureKey<bool> kMediaForceClearSurfaceView(
     "Media.ForceClearSurfaceView");
 
+inline constexpr ExperimentalFeatureKey<bool> kMediaForceDualThreads(
+    "Media.ForceDualThreads");
+
 inline constexpr ExperimentalFeatureKey<bool>
     kMediaIgnoreMediaCodecCallbacksDuringFlushing(
         "Media.IgnoreMediaCodecCallbacksDuringFlushing");
