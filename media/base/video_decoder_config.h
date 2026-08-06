8d6fee7c6da708d2f04d8de588a0719a086104e5
John Ocampo <johneocampo@google.com>
2026-08-06 00:33:45 +0000

    android: Move mime_type from ChunkDemuxerStream to config

--- media/base/video_decoder_config.h
+++ media/base/video_decoder_config.h
@@ -164,6 +164,11 @@ class MEDIA_EXPORT VideoDecoderConfig {
   // useful for decryptors that decrypts an encrypted stream to a clear stream.
   void SetIsEncrypted(bool is_encrypted);
 
+#if BUILDFLAG(USE_STARBOARD_MEDIA)
+  void set_mime_type(std::string_view mime_type) { mime_type_ = mime_type; }
+  const std::string& mime_type() const { return mime_type_; }
+#endif  // BUILDFLAG(USE_STARBOARD_MEDIA)
+
  private:
   VideoCodec codec_ = VideoCodec::kUnknown;
   VideoCodecProfile profile_ = VIDEO_CODEC_PROFILE_UNKNOWN;
@@ -189,6 +194,10 @@ class MEDIA_EXPORT VideoDecoderConfig {
 
   VideoColorSpace color_space_info_;
   std::optional<gfx::HDRMetadata> hdr_metadata_;
+
+#if BUILDFLAG(USE_STARBOARD_MEDIA)
+  std::string mime_type_;
+#endif  // BUILDFLAG(USE_STARBOARD_MEDIA)
 
   // Not using DISALLOW_COPY_AND_ASSIGN here intentionally to allow the compiler
   // generated copy constructor and assignment operator. Since the extra data is
