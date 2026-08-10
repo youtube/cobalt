131ee325d58b6e517387690d0e1378a42d2fe476
Charley Kim <charleykim@google.com>
2026-08-10 19:34:46 +0000

    fix failing incognito unit test

--- gpu/command_buffer/service/gr_shader_cache.cc
+++ gpu/command_buffer/service/gr_shader_cache.cc
@@ -224,7 +224,16 @@ void GrShaderCache::WriteToDisk(const CacheKey& key, CacheData* data) {
     return;
 
   // Only cache the shader on disk if this client id is permitted.
-#if !BUILDFLAG(IS_COBALT)
+#if BUILDFLAG(IS_COBALT)
+  // In Cobalt, we bypass this restriction and allow caching if the
+  // 'enable-gpu-shader-disk-cache' experiment flag is active, unless
+  // the process explicitly specifies the 'incognito' flag.
+  if (client_ids_to_cache_on_disk_.count(current_client_id()) == 0 &&
+      (!base::CommandLine::ForCurrentProcess()->HasSwitch("enable-gpu-shader-disk-cache") ||
+       base::CommandLine::ForCurrentProcess()->HasSwitch("incognito"))) {
+    return;
+  }
+#else
   if (client_ids_to_cache_on_disk_.count(current_client_id()) == 0)
     return;
 #endif
