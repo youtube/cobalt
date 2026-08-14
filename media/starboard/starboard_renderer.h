b8f947d198d77991874c5a361ff3a5e5040e9697
Gyuyoung Kim <gyuyoung@igalia.com>
2026-08-14 10:05:42 +0900

    media/starboard/starboard_renderer.h: Adapt to https://crrev.com/c/6655456
    Needs base/notimplemented.h for NOTIMPLEMENTED(), same as stream_parser.cc.

--- media/starboard/starboard_renderer.h
+++ media/starboard/starboard_renderer.h
@@ -21,6 +21,7 @@
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/notimplemented.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
