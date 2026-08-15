911f721ade7158102d4c4441f844cb62a375f3d9
Jason Zhang <jasonzh@google.com>
2026-08-14 18:47:13 -0700

    cobalt: Make cc-image-cache-limit-bytes to mbs
    Make cc-image-cache-limit-bytes to support mbs, so it can be more
    compatible with C26 LimitImageDecodeCacheSize flag. Also, re-use
    LIMIT_IMAGE_DECODE_CACHE_SIZE_MB for experiments.
    
    Bug: 545216717

--- cobalt/android/apk/app/src/main/java/dev/cobalt/util/JavaSwitches.java
+++ cobalt/android/apk/app/src/main/java/dev/cobalt/util/JavaSwitches.java
@@ -62,6 +62,9 @@ public class JavaSwitches {
   /** flag to limit GPU image cache items */
   public static final String GPU_IMAGE_CACHE_LIMIT_ITEMS = "GpuImageCacheLimitItems";
 
+  /** flag to limit GPU image cache bytes, resuing LimitImageDecodeCacheSizeMb */
+  public static final String LIMIT_IMAGE_DECODE_CACHE_SIZE_MB = "LimitImageDecodeCacheSizeMb";
+
   /** flag to globally configure max HTTP cache size ceiling in bytes. */
   public static final String MAX_HTTP_CACHE_SIZE = "MaxHttpCacheSize";
 
@@ -167,6 +170,11 @@ public class JavaSwitches {
       String limit =
           javaSwitches.get(JavaSwitches.GPU_IMAGE_CACHE_LIMIT_ITEMS).replaceAll("[^0-9]", "");
       extraCommandLineArgs.add("--cc-image-cache-limit-items=" + limit);
+    }
+    if (javaSwitches.containsKey(JavaSwitches.LIMIT_IMAGE_DECODE_CACHE_SIZE_MB)) {
+      String limit =
+          javaSwitches.get(JavaSwitches.LIMIT_IMAGE_DECODE_CACHE_SIZE_MB).replaceAll("[^0-9]", "");
+      extraCommandLineArgs.add("--cc-image-cache-limit-mbs=" + limit);
     }
     if (javaSwitches.containsKey(JavaSwitches.DECODED_IMAGE_WORKING_SET_BUDGET_BYTES)) {
       String budget =
@@ -306,7 +314,8 @@ public class JavaSwitches {
     }
 
     if (javaSwitches.containsKey(JavaSwitches.EVICT_MEMORY_CACHE_ON_CRITICAL_MEMORY_PRESSURE)) {
-      extraCommandLineArgs.add("--enable-features=" + JavaSwitches.EVICT_MEMORY_CACHE_ON_CRITICAL_MEMORY_PRESSURE);
+      extraCommandLineArgs.add(
+          "--enable-features=" + JavaSwitches.EVICT_MEMORY_CACHE_ON_CRITICAL_MEMORY_PRESSURE);
     }
 
     if (javaSwitches.containsKey(JavaSwitches.DISABLE_LESS_AGGRESSIVE_PARKABLE_STRING)) {
