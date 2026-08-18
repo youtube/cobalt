f21e4602d5a6da959a34ab9b83b20221c94f1495
Jeongeun(Julie) Kim <jkim@igalia.com>
2026-08-18 14:51:51 +0900

    components/services/storage/dom_storage: Adapt to https://crrev.com/c/6715682

--- components/services/storage/dom_storage/dom_storage_database_leveldb.cc
+++ components/services/storage/dom_storage/dom_storage_database_leveldb.cc
@@ -77,6 +77,14 @@ leveldb_env::Options MakeOptions() {
   return options;
 }
 
+#if BUILDFLAG(IS_COBALT)
+leveldb::WriteOptions CreateSyncWriteOptions() {
+  leveldb::WriteOptions options;
+  options.sync = true;
+  return options;
+}
+#endif
+
 std::unique_ptr<leveldb::DB> TryOpenDB(
     const leveldb_env::Options& options,
     const std::string& name,
@@ -282,8 +290,13 @@ DbStatus DomStorageDatabaseLevelDB::Put(KeyView key, ValueView value) const {
   if (!db_) {
     return DbStatus::IOError(kInvalidDatabaseMessage);
   }
+#if BUILDFLAG(IS_COBALT)
+  return FromLevelDBStatus(
+      db_->Put(CreateSyncWriteOptions(), MakeSlice(key), MakeSlice(value)));
+#else
   return FromLevelDBStatus(
       db_->Put(leveldb::WriteOptions(), MakeSlice(key), MakeSlice(value)));
+#endif
 }
 
 DbStatus DomStorageDatabaseLevelDB::GetPrefixed(
@@ -344,7 +357,11 @@ DbStatus DomStorageDatabaseLevelDB::Commit(leveldb::WriteBatch* batch) const {
   if (fail_commits_for_testing_) {
     return DbStatus::IOError("Simulated I/O Error");
   }
+#if BUILDFLAG(IS_COBALT)
+  return FromLevelDBStatus(db_->Write(CreateSyncWriteOptions(), batch));
+#else
   return FromLevelDBStatus(db_->Write(leveldb::WriteOptions(), batch));
+#endif
 }
 
 DbStatus DomStorageDatabaseLevelDB::RewriteDB() {
