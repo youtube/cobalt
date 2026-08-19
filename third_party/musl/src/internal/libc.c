80011a611a733f96b6c3790447de4721bae496e9
Oscar Vestlie <oxv@google.com>
2026-08-19 19:08:13 +0000

    posix: Resolve conflict in musl libc.c
    Resolve conflict markers in third_party/musl/src/internal/libc.c
    introduced during cherry-pick of #11866.
    
    Tag: agy
    Conv: 191a5df1-f0f2-42bc-81f0-30aeae2d759c
    Bug: 546094794

--- third_party/musl/src/internal/libc.c
+++ third_party/musl/src/internal/libc.c
@@ -2,15 +2,9 @@
 
 #if defined(STARBOARD)
 struct __libc __libc = {
-<<<<<<< HEAD
 	.threaded = 1,
 	.need_locks = 1,
 	.can_do_threads = 1,
-=======
-	.can_do_threads = 1,
-	.threaded = 1,
-	.need_locks = 1,
->>>>>>> 1e6aea7c28 (posix: Fix race condition in musl fclose during global flush (#11866))
 };
 #else
 struct __libc __libc;
