rdkcentral/larboard

RDK-56073: initial support for user settings
Change-Id: I9bd3333d46a07969424a3f952684a01059f47afc

src/third_party/starboard/rdk/shared/application_rdk.cc
@@ -433,6 +433,18 @@ SbTime Application::CheckMemoryUsage() {
   return kSbTimeSecond;
 }
 
+void Application::InjectAccessibilitySettingsChanged() {
+  Inject(new Event(kSbEventTypeAccessibilitySettingsChanged, NULL, NULL));
+}
+
+void Application::InjectAccessibilityCaptionSettingsChanged() {
+  Inject(new Event(kSbEventTypeAccessibilityCaptionSettingsChanged, NULL, NULL));
+}
+
+void Application::InjectAccessibilityTextToSpeechSettingsChanged() {
+  Inject(new Event(kSbEventTypeAccessibilityTextToSpeechSettingsChanged, NULL, NULL));
+}
+
 }  // namespace shared
 }  // namespace rdk
 }  // namespace starboard

youtube/cobalt

starboard/contrib/rdk/src/third_party/starboard/rdk/shared/application_rdk.cc
@@ -463,11 +463,15 @@ int64_t ApplicationRdk::CheckMemoryUsage() {
   return kSbTimeSecond;
 }
 
-void ApplicationRdk::InjectAccessibilityTextToSpeechSettingsChanged(bool enabled) {
-  bool* enabled_data = new bool(enabled);
-  Inject(new Event(kSbEventTypeAccessibilityTextToSpeechSettingsChanged,
-                   enabled_data,
-                   &ApplicationRdk::DeleteDestructor<bool>));
+void ApplicationRdk::InjectAccessibilitySettingsChanged() {
+  Inject(new Event(kSbEventTypeAccessibilitySettingsChanged, NULL, NULL));
 }
 
+void ApplicationRdk::InjectAccessibilityCaptionSettingsChanged() {
+  Inject(new Event(kSbEventTypeAccessibilityCaptionSettingsChanged, NULL, NULL));
+}
+
+void ApplicationRdk::InjectAccessibilityTextToSpeechSettingsChanged() {
+  Inject(new Event(kSbEventTypeAccessibilityTextToSpeechSettingsChanged, NULL, NULL));
+}
 }  // namespace starboard