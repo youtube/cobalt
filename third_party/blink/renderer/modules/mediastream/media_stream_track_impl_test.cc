9b47d10aa64573c81561de01acb74b38d2fd3bd1
Raphael Kubo da Costa <kubo@igalia.com>
2026-08-20 10:47:36 +0200

    third_party/blink/renderer/modules/mediastream: Adapt to https://crrev.com/c/6712212

--- third_party/blink/renderer/modules/mediastream/media_stream_track_impl_test.cc
+++ third_party/blink/renderer/modules/mediastream/media_stream_track_impl_test.cc
@@ -38,11 +38,8 @@
 #include "third_party/blink/renderer/modules/mediastream/media_stream_video_track.h"
 #include "third_party/blink/renderer/modules/mediastream/mock_media_stream_video_sink.h"
 #include "third_party/blink/renderer/modules/mediastream/mock_media_stream_video_source.h"
-<<<<<<< HEAD
 #include "third_party/blink/renderer/modules/mediastream/speech_recognition_media_stream_audio_sink.h"
-=======
 #if BUILDFLAG(USE_WEBRTC_PEER_CONNECTION)
->>>>>>> parent of 7d60f81f606 (CONFLICTED Chromium Cherry pick: Revert Cobalt.)
 #include "third_party/blink/renderer/modules/peerconnection/mock_peer_connection_dependency_factory.h"
 #endif  // BUILDFLAG(USE_WEBRTC_PEER_CONNECTION)
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
