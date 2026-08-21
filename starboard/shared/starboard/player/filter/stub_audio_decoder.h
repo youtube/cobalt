9798e8291e4b732388265e95bfa1ef9c0d5a042f
Xiaoming Shi <xiaomings@google.com>
2026-08-21 14:20:11 -0700

    starboard: Refactor DecodedAudio to plain object
    Refactor DecodedAudio from RefCountedThreadSafe to a plain object
    passed by moving. This avoids reference counting overhead and clarifies
    ownership.
    
    Update all usages in Starboard player, filter, and platform
    implementations (Android, tvOS, FFmpeg, FDK AAC, Opus) to pass
    DecodedAudio by value/move.
    
    Bug: 550523546
    TAG=agy
    CONV=752f10e0-d286-46eb-8615-b4dc1a33d4d0

--- starboard/shared/starboard/player/filter/stub_audio_decoder.h
+++ starboard/shared/starboard/player/filter/stub_audio_decoder.h
@@ -40,7 +40,7 @@ class StubAudioDecoder : public AudioDecoder, private JobQueue::JobOwner {
   void Decode(const InputBuffers& input_buffer,
               const ConsumedCB& consumed_cb) override;
   void WriteEndOfStream() override;
-  scoped_refptr<DecodedAudio> Read(int* samples_per_second) override;
+  std::optional<DecodedAudio> Read(int* samples_per_second) override;
   void Reset() override;
 
  private:
@@ -59,7 +59,7 @@ class StubAudioDecoder : public AudioDecoder, private JobQueue::JobOwner {
 
   std::unique_ptr<JobThread> decoder_thread_;
   std::mutex decoded_audios_mutex_;
-  std::queue<scoped_refptr<DecodedAudio>> decoded_audios_;
+  std::queue<DecodedAudio> decoded_audios_;
   scoped_refptr<InputBuffer> last_input_buffer_;
   std::optional<int> frames_per_input_;
   // Used to determine when to send multiple DecodedAudios in DecodeOneBuffer().
