6de8de5b50a92e65df588c126fa54b1ab19e37bc
Yijia Zhang <yijiazhang@google.com>
2026-08-21 16:58:40 -0700

    Cherry pick PR #12105: finch: Defer active config initialization until crash streak is synced
    Refer to original PR: #12105
    
    Move the initialization of active configuration data from the
    GlobalFeatures constructor to CobaltContentBrowserClient. This allows
    the initialization to be deferred until the experiment config type is
    determined crash streak synchronization from beacon file to metrics
    local state is processed.
    
    Bug: 549905088
    
    (cherry picked from commit 7a491d340bb7ff1496db2899770f5295853be67c)

--- cobalt/browser/cobalt_content_browser_client.cc
+++ cobalt/browser/cobalt_content_browser_client.cc
@@ -640,10 +640,20 @@ void CobaltContentBrowserClient::SetUpCobaltFeaturesAndParams(
   auto* global_features = GlobalFeatures::GetInstance();
   auto* experiment_config_manager =
       global_features->experiment_config_manager();
+
+  // It is critical that GetExperimentConfigType() is evaluated after
+  // InstantiateFieldTrialList(), because the latter triggers
+  // CleanExitBeacon::Initialize(), which reads the 'Variations' beacon file
+  // from DIR_CACHE and increments/syncs the crash streak into
+  // metrics_local_state.
+  // ExperimentConfigManager can then see the true crash streak and fall back
+  // to Safe Mode when needed.
   auto config_type = experiment_config_manager->GetExperimentConfigType();
   if (config_type == ExperimentConfigType::kEmptyConfig) {
     return;
   }
+
+  global_features->InitializeActiveConfigData(config_type);
   auto* experiment_config = global_features->experiment_config();
   const bool use_safe_config =
       (config_type == ExperimentConfigType::kSafeConfig);
