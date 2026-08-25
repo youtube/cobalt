478e6c6ddb108891d0b5a9ee3145aff5103955fc
Oscar Vestlie <oxv@google.com>
2026-08-25 22:25:57 +0000

    Modify junit_mini_parser to output JSON and update callpoints
    - Update junit_mini_parser.py to output failing tests as structured JSON.
    - Update junit_mini_parser_test.py with full test coverage and typing annotations.
    - Update main.yaml and process_test_results to parse JSON output using jq.
    
    Tag: agy
    Conv: fb32e968-5c28-4711-9b18-47b6c44fd1d0

--- cobalt/tools/junit_mini_parser_test.py
+++ cobalt/tools/junit_mini_parser_test.py
@@ -1,4 +1,3 @@
-"""Tests for the simple JUnit parser."""
 #!/usr/bin/env python3
 #
 # Copyright 2025 The Cobalt Authors. All Rights Reserved.
@@ -7,17 +6,20 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#   http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Tests for the simple JUnit parser."""
 
-import unittest
-from unittest.mock import mock_open, patch
+import io
+import json
 import logging
+import unittest
+from unittest.mock import MagicMock, mock_open, patch
 
 from cobalt.tools.junit_mini_parser import find_failing_tests, main
 
@@ -51,7 +53,12 @@ class TestFindFailingTests(unittest.TestCase):
     """
     with patch('builtins.open', mock_open(read_data=xml_content)):
       failing_tests = find_failing_tests(['report.xml'])
-      expected = {'report.xml': [('TestSuiteA.test_fail', 'Assertion failed')]}
+      expected = {
+          'report.xml': [{
+              'name': 'TestSuiteA.test_fail',
+              'message': 'Assertion failed\n...'
+          }]
+      }
       self.assertEqual(failing_tests, expected)
 
   def test_multiple_failing_tests_in_single_file(self):
@@ -72,9 +79,13 @@ class TestFindFailingTests(unittest.TestCase):
     with patch('builtins.open', mock_open(read_data=xml_content)):
       failing_tests = find_failing_tests(['results.xml'])
       expected = {
-          'results.xml': [('UnitTests.test_two_failed', 'Something went wrong'),
-                          ('UnitTests.test_three_errored',
-                           'An exception occurred')]
+          'results.xml': [{
+              'name': 'UnitTests.test_two_failed',
+              'message': 'Something went wrong\n...'
+          }, {
+              'name': 'UnitTests.test_three_errored',
+              'message': 'An exception occurred\n...'
+          }]
       }
       self.assertEqual(failing_tests, expected)
 
@@ -107,8 +118,14 @@ class TestFindFailingTests(unittest.TestCase):
         ]):
       failing_tests = find_failing_tests(['report1.xml', 'report2.xml'])
       expected = {
-          'report1.xml': [('ModuleA.test_beta_fail', 'Failure A')],
-          'report2.xml': [('ModuleB.test_gamma_error', 'Error B')]
+          'report1.xml': [{
+              'name': 'ModuleA.test_beta_fail',
+              'message': 'Failure A\n...'
+          }],
+          'report2.xml': [{
+              'name': 'ModuleB.test_gamma_error',
+              'message': 'Error B\n...'
+          }]
       }
       self.assertEqual(failing_tests, expected)
 
@@ -125,7 +142,10 @@ class TestFindFailingTests(unittest.TestCase):
     with patch('builtins.open', mock_open(read_data=xml_content)):
       failing_tests = find_failing_tests(['empty_name.xml'])
       expected = {
-          'empty_name.xml': [('empty_name.xml.test_one_failed', 'Fail msg')]
+          'empty_name.xml': [{
+              'name': 'empty_name.xml.test_one_failed',
+              'message': 'Fail msg\n...'
+          }]
       }
       self.assertEqual(failing_tests, expected)
 
@@ -133,24 +153,52 @@ class TestFindFailingTests(unittest.TestCase):
 class TestMain(unittest.TestCase):
   """Test cases for main function in junit_mini_parser."""
 
-  def setUp(self):
+  def setUp(self) -> None:
     logging.disable(logging.CRITICAL)
 
-  def tearDown(self):
+  def tearDown(self) -> None:
     logging.disable(logging.NOTSET)
 
   @patch('cobalt.tools.junit_mini_parser.find_failing_tests')
-  def test_main_success(self, mock_find):
+  @patch('sys.stdout', new_callable=io.StringIO)
+  def test_main_success(self, mock_stdout: io.StringIO,
+                        mock_find: MagicMock) -> None:
     mock_find.return_value = {}
-    self.assertEqual(main(['results.xml']), 0)
+    exit_code = main(['results.xml'])
+    self.assertEqual(exit_code, 0)
+
+    expected_output = {'failing_tests': {}}
+    self.assertEqual(json.loads(mock_stdout.getvalue()), expected_output)
 
   @patch('cobalt.tools.junit_mini_parser.find_failing_tests')
-  def test_main_failure(self, mock_find):
-    mock_find.return_value = {'results.xml': [('Test.fail', 'msg')]}
-    self.assertEqual(main(['results.xml']), 1)
+  @patch('sys.stdout', new_callable=io.StringIO)
+  def test_main_failure(self, mock_stdout: io.StringIO,
+                        mock_find: MagicMock) -> None:
+    mock_find.return_value = {
+        'results.xml': [{
+            'name': 'Test.fail',
+            'message': 'msg'
+        }]
+    }
+    exit_code = main(['results.xml'])
+    self.assertEqual(exit_code, 1)
 
-  def test_main_no_files(self):
-    self.assertEqual(main([]), 0)
+    expected_output = {
+        'failing_tests': {
+            'results.xml': [{
+                'name': 'Test.fail',
+                'message': 'msg'
+            }]
+        }
+    }
+    self.assertEqual(json.loads(mock_stdout.getvalue()), expected_output)
+
+  @patch('sys.stdout', new_callable=io.StringIO)
+  def test_main_no_files(self, mock_stdout: io.StringIO) -> None:
+    exit_code = main([])
+    self.assertEqual(exit_code, 0)
+    expected_output = {'failing_tests': {}}
+    self.assertEqual(json.loads(mock_stdout.getvalue()), expected_output)
 
 
 if __name__ == '__main__':
