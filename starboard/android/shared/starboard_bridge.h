89ab2c4f274b5a2fc1dfe7070fad6870be1aa236
Austin Osagie <austinosagie@google.com>
2026-08-27 11:39:02 -0700

    Cherry pick PR #12068: android: Enable player_filter_tests
    Refer to original PR: #12068
    
    Enables `player_filter_tests` for Android arm and arm64.
    
    This change introduces `CobaltTestActivity` to handle the necessary JNI
    and application context initialization required for native unit tests
    on Android. It also configures the build system to package media test
    assets and updates the test runner to locate them on device storage.
    HDR capability checks are moved to DisplayUtil.
    
    The following suites are temporarily filtered due to their extended
    runtimes and flakiness:
      - `"*PlayerComponentsTest*"`
      - `"*AdaptiveAudioDecoderTest*"`
      - `"*AudioDecoderTest*"`
      - `"*VideoDecoderTest*"`
      - `"*AudioRendererTest*"`
    
    #vibe-coded
    
    Issue: 372559388
    Issue: 535995457
    
    (cherry picked from commit 9d12208dcfafb442662630e64de5812430c8a03e)

--- starboard/android/shared/starboard_bridge.h
+++ starboard/android/shared/starboard_bridge.h
@@ -43,8 +43,6 @@ class StarboardBridge {
 
   void AppendArgs(JNIEnv* env, std::vector<std::string>* args_vector);
 
-  jni_zero::ScopedJavaLocalRef<jintArray> GetSupportedHdrTypes(JNIEnv* env);
-
   void RaisePlatformError(JNIEnv* env,
                           jint errorType,
                           jlong data,
