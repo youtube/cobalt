74cfcd50f1d6b240943a287e50cbbbcb14b9f976
Jeongeun(Julie) Kim <jkim@igalia.com>
2026-08-28 18:32:51 +0900

    content/utility/.../on_device_model_sandbox_init.cc: Adapt to https://crrev.com/c/6822533

--- content/utility/on_device_model/on_device_model_sandbox_init.cc
+++ content/utility/on_device_model/on_device_model_sandbox_init.cc
@@ -81,7 +81,7 @@ BASE_FEATURE(kOnDeviceModelWarmDrivers,
 #endif
 
 bool ShouldWarmDrivers() {
-#if BUILDFLAG(IS_FUCHSIA) || \
+#if BUILDFLAG(IS_FUCHSIA) || BUILDFLAG(IS_COBALT_HERMETIC_BUILD) || \
     (BUILDFLAG(IS_LINUX) && BUILDFLAG(ENABLE_CAST_RECEIVER))
   return false;
 #else
@@ -117,17 +117,7 @@ bool PreSandboxInit() {
   }
 #endif
 
-<<<<<<< HEAD
   if (ShouldWarmDrivers()) {
-=======
-#if !BUILDFLAG(IS_FUCHSIA) && !BUILDFLAG(IS_COBALT_HERMETIC_BUILD) && \
-    !(BUILDFLAG(IS_LINUX) && BUILDFLAG(ENABLE_CAST_RECEIVER))
-  if (base::FeatureList::IsEnabled(kOnDeviceModelWarmDrivers)
-#if defined(ENABLE_ML_INTERNAL)
-      && !ml::IsGpuBlocked(ml::ChromeML::Get()->api(), /*log_histogram=*/false)
-#endif
-  ) {
->>>>>>> parent of affc325d4eb (CONFLICTED Chromium Cherry pick: Revert Cobalt.)
     // Warm any relevant drivers before attempting to bring up the sandbox. For
     // good measure we initialize a device instance for any adapter with an
     // appropriate backend on top of any integrated or discrete GPU.
