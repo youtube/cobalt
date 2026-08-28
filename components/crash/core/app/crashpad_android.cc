5bb2bd859794b77afccabbe6e15bf9f0574acdca
Jeongeun(Julie) Kim <jkim@igalia.com>
2026-08-28 19:01:41 +0900

    components/crash/.../crashpad_android.cc: Adapt to https://crrev.com/c/6835490

--- components/crash/core/app/crashpad_android.cc
+++ components/crash/core/app/crashpad_android.cc
@@ -319,16 +319,11 @@ bool GetHandlerTrampoline(std::string* handler_trampoline,
                           std::string* handler_library) {
   // The linker doesn't support loading executables passed on its command
   // line until Q.
-<<<<<<< HEAD
   if (base::android::android_info::sdk_int() <
       base::android::android_info::SDK_VERSION_Q) {
-=======
-  if (base::android::BuildInfo::GetInstance()->sdk_int() <
-      base::android::SDK_VERSION_Q) {
 #if BUILDFLAG(IS_COBALT)
     LOG(INFO) << "SDK version below Q: No linker support.";
 #endif
->>>>>>> parent of affc325d4eb (CONFLICTED Chromium Cherry pick: Revert Cobalt.)
     return false;
   }
 
