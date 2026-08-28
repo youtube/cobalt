caeaf6aeb58109b82efbad394040dba136721a30
Jeongeun(Julie) Kim <jkim@igalia.com>
2026-08-28 20:27:29 +0900

    BACKPORT: Fix undefined symbol gpu::WebNNTensorRepresentation
    This fixes a build break on tvOS for 141.7364 rebasing.
    
    Original CL description:
    
    Fix undefined symbol gpu::WebNNTensorRepresentation
    
    https://crrev.com/c/6857987 should update .cc file at the same time.
    Otherwise, it will throw error: undefined symbol vtable for gpu::WebNNTensorRepresentation
    
    Change-Id: I96430a6863d4e82487ef5337b837a8800d0b0bed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6910282
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510481}

--- gpu/command_buffer/service/shared_image/shared_image_representation.cc
+++ gpu/command_buffer/service/shared_image/shared_image_representation.cc
@@ -849,11 +849,11 @@ void WebNNTensorRepresentation::ConsumeWebNNTensor(
 }
 #endif
 
-#if BUILDFLAG(IS_MAC)
+#if BUILDFLAG(IS_APPLE)
 IOSurfaceRef WebNNTensorRepresentation::GetIOSurface() const {
   NOTREACHED();
 }
-#endif  // BUILDFLAG(IS_MAC)
+#endif  // BUILDFLAG(IS_APPLE)
 
 ///////////////////////////////////////////////////////////////////////////////
 // OverlayImageRepresentation
