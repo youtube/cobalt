a77be6153c975d52fe0faa35bc8cc3a3b67ad0e1
Chris Yuming Chang <chrisymchang@igalia.com>
2026-09-01 12:27:29 +0800

    build/android/gyp: Adapt to https://crrev.com/c/6941534

--- build/android/gyp/java_cpp_features.py
+++ build/android/gyp/java_cpp_features.py
@@ -22,13 +22,7 @@ class FeatureParserDelegate(java_cpp_utils.CppConstantParser.Delegate):
   # would parse as:
   #   ExtractConstantName() -> 'ConstantName'
   #   ExtractValue() -> '"StringNameOfTheFeature"' or '"ConstantName"'
-<<<<<<< HEAD
-  _FEATURE_RE = re.compile(r'BASE_FEATURE\(\s*(k\w+),')
-=======
-  # TODO(crbug.com/436274260): Drop support of the old
-  # 'BASE_FEATURE(ConstantName, ...);' format.
-  _FEATURE_RE = re.compile(r'(?:BASE_FEATURE|STARBOARD_FEATURE)\(([^,]+),')
->>>>>>> parent of 4830f91b2e9 (CONFLICTED Chromium Cherry pick: Revert Cobalt.)
+  _FEATURE_RE = re.compile(r'(?:BASE_FEATURE|STARBOARD_FEATURE)\(\s*(k\w+),')
   _STRING_LITERAL_RE = re.compile(r'"(?:\\"|[^"])*"')
   _constant_name = None  # The name of the current macro.
   _comma_count = 0  # Number of commas seen in the current macro.
