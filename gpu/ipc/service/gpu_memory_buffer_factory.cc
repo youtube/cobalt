ec27a8e044a7a45aa6c36c1b144b04dd33a7fd56
Chris Yuming Chang <chrisymchang@igalia.com>
2026-09-01 13:41:44 +0800

    gpu/ipc/service: Adapt to https://crrev.com/c/6929581

--- gpu/ipc/service/gpu_memory_buffer_factory.cc
+++ gpu/ipc/service/gpu_memory_buffer_factory.cc
@@ -33,18 +33,10 @@ GpuMemoryBufferFactory::CreateNativeType(
   return std::make_unique<GpuMemoryBufferFactoryIOSurface>();
 #elif BUILDFLAG(IS_ANDROID)
   // Android does not support creating native GMBs (i.e., from
-<<<<<<< HEAD
   // AHardwareBuffers), but the codebase is structured such that it is easier
   // to create a dummy factory than create no factory.
   return std::make_unique<GpuMemoryBufferFactory>();
-#elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_FUCHSIA)
-=======
-  // AHardwareBuffers), but the codebase is structured such that it is necessary
-  // to have a factory that vends invalid GMB handles rather than having no
-  // factory at all.
-  return std::make_unique<GpuMemoryBufferFactoryStub>();
 #elif BUILDFLAG(IS_LINUX) && !BUILDFLAG(IS_STARBOARD) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_FUCHSIA)
->>>>>>> parent of 4830f91b2e9 (CONFLICTED Chromium Cherry pick: Revert Cobalt.)
   return std::make_unique<GpuMemoryBufferFactoryNativePixmap>(
       vulkan_context_provider);
 #elif BUILDFLAG(IS_WIN)
