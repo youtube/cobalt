8512cfe85d5cb5b02f43c73de3f1e2635c0dba0a
Niranjan Yardi <nyardi@google.com>
2026-09-01 22:49:46 +0000

    Address PR feedback: Use hardcoded cgroup memory path
    Directly use the root fallback cgroup path /sys/fs/cgroup/memory/memory.usage_in_bytes
    assuming the process is running in local mode and not inside
    a nested lxc container, in order to simplify the path extraction.

--- starboard/contrib/rdk/src/third_party/starboard/rdk/shared/system/system_get_used_cpu_memory.cc
+++ starboard/contrib/rdk/src/third_party/starboard/rdk/shared/system/system_get_used_cpu_memory.cc
@@ -17,61 +17,28 @@
 // clang-format on
 
 #include <fcntl.h>
-#include <inttypes.h>
-#include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 #include "starboard/common/file.h"
 #include "starboard/common/log.h"
-#include "starboard/common/string.h"
-
-namespace {
-
-int64_t SearchForMemoryValue(const char* search_key, const char* buffer) {
-  const char* found = strstr(buffer, search_key);
-  if (!found) {
-    SB_LOG(ERROR) << "Could not find '" << search_key << "' in smaps_rollup.";
-    return 0;
-  }
-
-  const char* digits = strpbrk(found + strlen(search_key), "0123456789");
-  if (!digits) {
-    SB_LOG(ERROR) << "Could not find any digits for '" << search_key
-                  << "' in smaps_rollup.";
-    return 0;
-  }
-
-  int64_t memory_value_in_kilobytes = 0;
-  if (sscanf(digits, "%" PRId64, &memory_value_in_kilobytes) != 1) {
-    SB_LOG(ERROR) << "Could not parse integer value for '" << search_key
-                  << "' in smaps_rollup.";
-    return 0;
-  }
-
-  return memory_value_in_kilobytes * 1024;
-}
-
-}  // namespace
 
 int64_t SbSystemGetUsedCPUMemory() {
-  // On RDK devices, /proc/self/smaps_rollup is optimized to provide accurate
-  // memory footprint calculations avoiding double counting UMA GPU memory buffers
-  // that can be misreported by /proc/self/status VmRSS.
-  starboard::ScopedFile status_file("/proc/self/smaps_rollup", O_RDONLY);
-  if (!status_file.IsValid()) {
-    SB_LOG(ERROR)
-        << "Error opening /proc/self/smaps_rollup in order to query self memory usage.";
-    return 0;
+  starboard::ScopedFile status_file(
+      "/sys/fs/cgroup/memory/memory.usage_in_bytes", O_RDONLY);
+  if (status_file.IsValid()) {
+    const int kBufferSize = 512;
+    char buffer[kBufferSize];
+    int bytes_read = status_file.ReadAll(buffer, kBufferSize);
+    if (bytes_read == kBufferSize) {
+      bytes_read = kBufferSize - 1;
+    }
+    if (bytes_read >= 0) {
+      buffer[bytes_read] = '\0';
+      int64_t val = strtoll(buffer, nullptr, 10);
+      if (val > 0) {
+        return val;
+      }
+    }
   }
-
-  const int kBufferSize = 4096;
-  char buffer[kBufferSize];
-  int bytes_read = status_file.ReadAll(buffer, kBufferSize - 1);
-  if (bytes_read < 0) {
-    bytes_read = 0;
-  }
-  buffer[bytes_read] = '\0';
-
-  // Use Rss footprint from smaps_rollup which is robust on RDK.
-  return SearchForMemoryValue("Rss:", buffer);
+  return 0;
 }
