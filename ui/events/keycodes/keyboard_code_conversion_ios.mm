ee4d3b6a32631a3ddc340cbc6c6b0933a3d55c9c
Chris Yuming Chang <chrisymchang@igalia.com>
2026-09-01 14:07:37 +0800

    ui/events/keycodes/keyboard_code_conversion_ios.mm: Keep Cobalt's backported version
    The conflict exists because this rebase contains upstream
    https://crrev.com/c/6918027 ([tvos] Implement WebKeyboardEventBuilder) but not
    https://crrev.com/c/7149563 ([tvOS] Prevents forwarding handled events to
    super.), which only landed in M144. Cobalt carries the backport of 7149563
    (PR #8271), which adds the ReturnOrEnter -> VKEY_RETURN mapping, so we keep it.

--- ui/events/keycodes/keyboard_code_conversion_ios.mm
+++ ui/events/keycodes/keyboard_code_conversion_ios.mm
@@ -231,10 +231,7 @@ KeyboardCode KeyboardCodeFromUIKeyCode(UIKeyboardHIDUsage key_code) {
            {UIKeyboardHIDUsageKeyboardEscape, KeyboardCode::VKEY_ESCAPE},
            {UIKeyboardHIDUsageKeyboardInsert, KeyboardCode::VKEY_INSERT},
            {UIKeyboardHIDUsageKeyboardReturn, KeyboardCode::VKEY_RETURN},
-<<<<<<< HEAD
-=======
            {UIKeyboardHIDUsageKeyboardReturnOrEnter, KeyboardCode::VKEY_RETURN},
->>>>>>> parent of 4830f91b2e9 (CONFLICTED Chromium Cherry pick: Revert Cobalt.)
            {UIKeyboardHIDUsageKeyboardTab, KeyboardCode::VKEY_TAB},
            {UIKeyboardHIDUsageKeyboardF1, KeyboardCode::VKEY_F1},
            {UIKeyboardHIDUsageKeyboardF2, KeyboardCode::VKEY_F2},
