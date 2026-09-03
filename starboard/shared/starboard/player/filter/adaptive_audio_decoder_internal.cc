36605ea0ebcb5c968b75d625c748c85137a52324
Kyujung Youn <kjyoun@google.com>
2026-09-03 11:47:26 -0700

    Cherry pick PR #12401: media: Remove storage_type() from DecodedAudio
    Refer to original PR: #12401
    
    This is part of the effort to remove kSbMediaAudioFrameStorageTypePlanar
    from 1P implementation.
    
    DecodedAudio is always in interleaved format. This change removes the
    storage_type() accessor method from DecodedAudio, removes unused
    output_storage_type_ from AdaptiveAudioDecoder, simplifies audio channel
    layout mixing to direct interleaved indexing, and cleans up all related
    call sites and test assertions.
    
    Bug: 272837615
    
    (cherry picked from commit 7ca0c7fc4b11da68163d6f2244953c772ac31e27)

--- starboard/shared/starboard/player/filter/adaptive_audio_decoder_internal.cc
+++ starboard/shared/starboard/player/filter/adaptive_audio_decoder_internal.cc
@@ -162,8 +162,6 @@ std::optional<DecodedAudio> AdaptiveAudioDecoder::Read(
   SB_DCHECK(ret->is_end_of_stream() ||
             ret->sample_type() == output_sample_type_);
   SB_DCHECK(ret->is_end_of_stream() ||
-            ret->storage_type() == output_storage_type_);
-  SB_DCHECK(ret->is_end_of_stream() ||
             ret->channels() == output_number_of_channels_);
 
   SB_DCHECK(first_output_received_ || ret->is_end_of_stream());
@@ -237,10 +235,9 @@ void AdaptiveAudioDecoder::OnDecoderOutput() {
   if (!first_output_received_) {
     first_output_received_ = true;
     output_sample_type_ = decoded_audio->sample_type();
-    output_storage_type_ = decoded_audio->storage_type();
     output_samples_per_second_ = decoded_sample_rate;
     if (output_adjustment_callback_) {
-      output_adjustment_callback_(&output_sample_type_, &output_storage_type_,
+      output_adjustment_callback_(&output_sample_type_,
                                   &output_samples_per_second_,
                                   &output_number_of_channels_);
     }
@@ -289,8 +286,7 @@ void AdaptiveAudioDecoder::OnDecoderOutput() {
     if (input_audio_stream_info_.number_of_channels !=
         output_number_of_channels_) {
       channel_mixer_ = AudioChannelLayoutMixer::Create(
-          output_sample_type_, output_storage_type_,
-          output_number_of_channels_);
+          output_sample_type_, output_number_of_channels_);
     }
   }
   if (resampler_) {
