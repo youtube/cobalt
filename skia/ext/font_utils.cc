287bfcc6af762869aa21047c59001c8410c22044
Charley Kim <charleykim@google.com>
2026-09-02 18:07:37 -0700

    RESOLVED Cherry pick PR #12208: android: enable custom fonts.xml file by default
    Refer to original PR: #12208
    
    This PR removes the Java Switch guarding the custom fonts.xml file for
    android and its optimized loading.
    
    This isolated configuration avoids unnecessarily scanning system fonts,
    as the YouTube application relies primarily on 2 fonts (YTSans and
    Roboto).
    
    Experiment results showed significant increase in YouTube Time among
    other metrics (see bug below).
    
    See the original PR's: #11082 #11582
    
    Bug: 517249177
    
    (cherry picked from commit e90a807d3144771ff255b3499b8891787a335ba4)

--- skia/ext/font_utils.cc
+++ skia/ext/font_utils.cc
@@ -74,34 +74,13 @@ static sk_sp<SkFontMgr> fontmgr_factory() {
 #else
 #if BUILDFLAG(IS_ANDROID)
 #if BUILDFLAG(IS_COBALT)
-<<<<<<< HEAD
-  // When Cobalt optimized font loading is enabled, configure Skia to use hermetic custom
-  // XML font fallbacks (`cobalt_android_fonts.xml`) extracted into the app data directory.
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch("enable-optimized-font-loading")) {
-    base::FilePath app_data_dir;
-    if (base::PathService::Get(base::DIR_ANDROID_APP_DATA, &app_data_dir)) {
-      std::string xml_path = app_data_dir.Append("storage").Append("cobalt_android_fonts.xml").value();
-      SkFontMgr_Android_CustomFonts custom_fonts;
-      custom_fonts.fSystemFontUse = SkFontMgr_Android_CustomFonts::kPreferCustom;
-      custom_fonts.fBasePath = "/system/fonts/";
-      custom_fonts.fFontsXml = xml_path.c_str();
-      custom_fonts.fFallbackFontsXml = nullptr;
-      custom_fonts.fIsolated = true;
-      return SkFontMgr_New_Android(&custom_fonts,
-                                   SkFontScanner_Make_Fontations());
-=======
   // Configure Skia to use hermetic custom XML font fallbacks (`cobalt_android_fonts.xml`)
   // extracted into the app data directory.
   base::FilePath app_data_dir;
   if (base::PathService::Get(base::DIR_ANDROID_APP_DATA, &app_data_dir)) {
     std::string xml_path = app_data_dir.Append("storage").Append("cobalt_android_fonts.xml").value();
     if (!base::PathExists(base::FilePath(xml_path))) {
-      if (base::FeatureList::IsEnabled(skia::kFontationsAndroidSystemFonts)) {
-        return SkFontMgr_New_Android(nullptr, SkFontScanner_Make_Fontations());
-      } else {
-        return SkFontMgr_New_Android(nullptr);
-      }
->>>>>>> e90a807d31 (android: enable custom fonts.xml file by default (#12208))
+      return SkFontMgr_New_Android(nullptr, SkFontScanner_Make_Fontations());
     }
     SkFontMgr_Android_CustomFonts custom_fonts;
     custom_fonts.fSystemFontUse = SkFontMgr_Android_CustomFonts::kPreferCustom;
