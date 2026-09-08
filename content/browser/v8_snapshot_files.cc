a3e39eededc5e67b3139f1a7db6d45c6e219f827
Jeongeun(Julie) Kim <jkim@igalia.com>
2026-09-08 21:01:54 +0900

    content/browser/v8_snapshot_files.cc: Adapt to https://crrev.com/c/6973644

--- content/browser/v8_snapshot_files.cc
+++ content/browser/v8_snapshot_files.cc
@@ -16,13 +16,8 @@
 
 namespace content {
 
-<<<<<<< HEAD
 #if BUILDFLAG(IS_ANDROID) && (BUILDFLAG(USE_V8_CONTEXT_SNAPSHOT) || \
                               BUILDFLAG(INCLUDE_BOTH_V8_SNAPSHOTS))
-=======
-#if BUILDFLAG(IS_ANDROID) && \
-    (BUILDFLAG(USE_V8_CONTEXT_SNAPSHOT) || BUILDFLAG(INCLUDE_BOTH_V8_SNAPSHOTS))
->>>>>>> parent of 16db8b77b67 (CONFLICTED Chromium Cherry pick: Revert Cobalt.)
 namespace {
 void registerContextSnapshotAndroid(
     std::map<std::string, std::variant<base::FilePath, base::ScopedFD>>&
