c0e9d6c47d9010376c71f3902720db5d7be1cba7
Jeongeun(Julie) Kim <jkim@igalia.com>
2026-09-08 21:19:08 +0900

    gpu/config/gpu_finch_features.h: Adapt to https://crrev.com/c/6984404

--- gpu/config/gpu_finch_features.h
+++ gpu/config/gpu_finch_features.h
@@ -23,15 +23,10 @@ struct GpuFeatureInfo;
 
 namespace features {
 
-<<<<<<< HEAD
-=======
-GPU_CONFIG_EXPORT BASE_DECLARE_FEATURE(kUseGles2ForOopR);
-
 #if BUILDFLAG(IS_COBALT)
 GPU_CONFIG_EXPORT BASE_DECLARE_FEATURE(kCobaltInProcessDirectRaster);
 #endif  // BUILDFLAG(IS_COBALT)
 
->>>>>>> parent of 16db8b77b67 (CONFLICTED Chromium Cherry pick: Revert Cobalt.)
 // All features in alphabetical order. The features should be documented
 // alongside the definition of their values in the .cc file.
 GPU_CONFIG_EXPORT BASE_DECLARE_FEATURE(kAggressiveShaderCacheLimits);
