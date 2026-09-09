84d201e0b634e5bbd66bd472dac26e57274ca999
Henrique Ferreiro <hferreiro@igalia.com>
2026-09-09 15:22:17 +0200

    Keep the libunwind GN variable and the Evergreen Lite docs unchanged

--- starboard/shared/linux/system_get_stack.cc
+++ starboard/shared/linux/system_get_stack.cc
@@ -14,7 +14,7 @@
 
 #include "starboard/system.h"
 
-#if SB_IS(STARBOARD_USE_LIBUNWIND)
+#if SB_IS(EVERGREEN_COMPATIBLE_LIBUNWIND)
 #define UNW_LOCAL_ONLY
 #include "starboard/common/log.h"
 #include "third_party/llvm-project/libunwind/include/libunwind.h"
@@ -24,7 +24,7 @@
 #include <algorithm>
 #endif
 
-#if SB_IS(STARBOARD_USE_LIBUNWIND)
+#if SB_IS(EVERGREEN_COMPATIBLE_LIBUNWIND)
 int SbSystemGetStack(void** out_stack, int stack_size) {
   unw_cursor_t cursor;
   unw_context_t context;
