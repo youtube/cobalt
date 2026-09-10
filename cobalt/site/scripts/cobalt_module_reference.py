e10b1bca0ce5ebbd2448a334a1ac1c1d8ff18c2c
Arjun Menon <agmenon@google.com>
2026-09-10 15:45:10 -0700

    Fix PyLint inconsistent quotes format in python scripts

--- cobalt/site/scripts/cobalt_module_reference.py
+++ cobalt/site/scripts/cobalt_module_reference.py
@@ -213,7 +213,7 @@ def _node_to_markdown(out, node):
     assert not _strip(tail)
     out.paragraph()
   elif node.tag == 'bold':
-    assert len(node) == 0, f"bold node has children: {[c.tag for c in node]}"
+    assert len(node) == 0, f'bold node has children: {[c.tag for c in node]}'
     out.bold(text)
     text = ''
   elif node.tag == 'computeroutput':
@@ -250,7 +250,7 @@ def _node_to_markdown(out, node):
   elif node.tag == 'verbatim':
     # Verbatim tags can appear inside paragraphs.
     assert len(
-        node) == 0, f"verbatim node has children: {[c.tag for c in node]}"
+        node) == 0, f'verbatim node has children: {[c.tag for c in node]}'
     # Don't replace pipes in verbatim text.
     text = node.text if node.text else ''
     # Strip doxygen comment prefix '///' and one space if present
