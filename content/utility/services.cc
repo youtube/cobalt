e5b25cd0f7a447d69d69e3a8e262eac6386f2831
Chris Yuming Chang <chrisymchang@igalia.com>
2026-09-11 10:38:30 +0800

    content/utility/services.cc: Adapt to https://crrev.com/c/6991290

--- content/utility/services.cc
+++ content/utility/services.cc
@@ -437,14 +437,11 @@ void RegisterMainThreadServices(mojo::ServiceFactory& services) {
   services.Add(RunVideoEffects);
 #endif
 
-<<<<<<< HEAD
 #if BUILDFLAG(USE_LINUX_VIDEO_ACCELERATION)
   services.Add(RunOOPVideoDecoderFactoryProcessService);
 #endif
 
-=======
 #if !BUILDFLAG(IS_COBALT)
->>>>>>> parent of 7c028cef5839 (CONFLICTED Chromium Cherry pick: Revert Cobalt.)
   if (optimization_guide::features::CanLaunchOnDeviceModelService()) {
     services.Add(RunOnDeviceModel);
   }
