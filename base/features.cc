be7e098d78a9800b879e954991d470bf30cfa6fb
Sunny Tang <tasunny@google.com>
2026-09-15 19:32:08 +0000

    Cherry pick PR #12617: Disable kCobaltMemoryPressureCooldown by default for Finch setup
    Refer to original PR: #12617
    
    Feature flags need to be disabled by default to run Finch
    experiment, flip the value for kCobaltMemoryPressureCooldown.
    
    Bug: 521848285
    
    (cherry picked from commit 08d8daa212b312ab54967a3209b3d450b6ea4ca5)

--- base/features.cc
+++ base/features.cc
@@ -45,7 +45,7 @@ BASE_FEATURE(kCobaltEnableModerateMemoryPressure,
 
 BASE_FEATURE(kCobaltMemoryPressureCooldown,
              "CobaltMemoryPressureCooldown",
-             FEATURE_ENABLED_BY_DEFAULT);
+             FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE_PARAM(int,
                    kCobaltMemoryPressureCooldownSeconds,
