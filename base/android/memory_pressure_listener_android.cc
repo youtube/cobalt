56dac1a8a557bb8e82f85701e7bce33b003b1ca9
Sunny Tang <tasunny@google.com>
2026-09-15 19:32:08 +0000

    Cherry pick PR #12617: Disable kCobaltMemoryPressureCooldown by default for Finch setup
    Refer to original PR: #12617
    
    Feature flags need to be disabled by default to run Finch
    experiment, flip the value for kCobaltMemoryPressureCooldown.
    
    Bug: 521848285
    
    (cherry picked from commit 08d8daa212b312ab54967a3209b3d450b6ea4ca5)

--- base/android/memory_pressure_listener_android.cc
+++ base/android/memory_pressure_listener_android.cc
@@ -58,6 +58,10 @@ static jboolean JNI_MemoryPressureListener_IsModerateMemoryPressureEnabled(
 static jint JNI_MemoryPressureListener_GetMemoryPressureCooldownSeconds(
     JNIEnv* env) {
 #if BUILDFLAG(IS_COBALT)
+  if (!base::FeatureList::IsEnabled(
+          base::features::kCobaltMemoryPressureCooldown)) {
+    return 60;
+  }
   return base::features::kCobaltMemoryPressureCooldownSeconds.Get();
 #else
   return 60;
