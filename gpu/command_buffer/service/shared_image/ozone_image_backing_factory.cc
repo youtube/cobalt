2eb0fd848bf1e14766f8e0e96547fb5f2050732d
Gyuyoung Kim <gyuyoung@igalia.com>
2026-09-17 12:35:27 +0900

    ozone_image_backing_factory.cc: Adapt to https://crrev.com/c/7100738
    Take upstream's port to SharedImageFormat but keep the ENABLE_VULKAN guard from
    143.7457: upstream's inline GetDeviceQueue() dereferences an incomplete
    viz::VulkanContextProvider when Cobalt builds ozone with enable_vulkan = false.

--- gpu/command_buffer/service/shared_image/ozone_image_backing_factory.cc
+++ gpu/command_buffer/service/shared_image/ozone_image_backing_factory.cc
@@ -92,18 +92,6 @@ OzoneImageBackingFactory::CreateGpuMemoryBufferHandle(
     viz::SharedImageFormat format,
     gfx::BufferUsage usage) {
   CHECK(viz::HasEquivalentBufferFormat(format));
-<<<<<<< HEAD
-  scoped_refptr<gfx::NativePixmap> pixmap =
-      ui::OzonePlatform::GetInstance()
-          ->GetSurfaceFactoryOzone()
-          ->CreateNativePixmap(gpu::kNullSurfaceHandle,
-                               vulkan_context_provider
-                                   ? vulkan_context_provider->GetDeviceQueue()
-                                   : nullptr,
-                               size, format, usage, size);
-=======
-  gfx::BufferFormat buffer_format =
-      viz::SharedImageFormatToBufferFormat(format);
   VulkanDeviceQueue* device_queue = nullptr;
 #if BUILDFLAG(ENABLE_VULKAN)
   if (vulkan_context_provider) {
@@ -114,8 +102,7 @@ OzoneImageBackingFactory::CreateGpuMemoryBufferHandle(
       ui::OzonePlatform::GetInstance()
           ->GetSurfaceFactoryOzone()
           ->CreateNativePixmap(gpu::kNullSurfaceHandle, device_queue, size,
-                               buffer_format, usage, size);
->>>>>>> parent of 85b8f352370 (CONFLICTED Chromium Cherry pick: Revert Cobalt.)
+                               format, usage, size);
 
   if (!pixmap.get()) {
     DLOG(ERROR) << "Failed to create pixmap " << size.ToString() << ",  "
