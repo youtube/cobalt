f4f36cdbd2cb8c3d2c5f2b4f207bf1329c30480f
Raphael Kubo da Costa <kubo@igalia.com>
2026-09-17 17:10:40 +0200

    third_party/blink/../html_video_element.h: Adapt to https://crrev.com/c/7137418

--- third_party/blink/renderer/core/html/media/html_video_element.h
+++ third_party/blink/renderer/core/html/media/html_video_element.h
@@ -179,14 +179,13 @@ class CORE_EXPORT HTMLVideoElement final : public HTMLMediaElement,
     return visibility_tracker_.Get();
   }
 
-<<<<<<< HEAD
   VideoFrameCallbackRequester* GetVideoFrameCallbackRequester() const {
     return video_frame_callback_requester_;
   }
   void SetVideoFrameCallbackRequester(VideoFrameCallbackRequester* requester) {
     video_frame_callback_requester_ = requester;
   }
-=======
+
 #if BUILDFLAG(USE_STARBOARD_MEDIA)
   void SetMaxVideoCapabilities(const String& max_video_capabilities, ExceptionState& exception_state);
 
@@ -196,7 +195,6 @@ class CORE_EXPORT HTMLVideoElement final : public HTMLMediaElement,
 
   bool HasMaxVideoCapabilities() const { return !max_video_capabilities_.empty(); }
 #endif // BUILDFLAG(USE_STARBOARD_MEDIA)
->>>>>>> parent of fddd5727c2b (CONFLICTED Chromium Cherry pick: Revert Cobalt.)
 
  protected:
   // EventTarget overrides.
@@ -311,13 +309,12 @@ class CORE_EXPORT HTMLVideoElement final : public HTMLMediaElement,
       cc::PaintFlags::FilterQuality::kLow;
   cc::PaintFlags::DynamicRangeLimitMixture dynamic_range_limit_;
 
-<<<<<<< HEAD
+
   Member<VideoFrameCallbackRequester> video_frame_callback_requester_;
-=======
+
 #if BUILDFLAG(USE_STARBOARD_MEDIA)
   std::string max_video_capabilities_;
 #endif // BUILDFLAG(USE_STARBOARD_MEDIA)
->>>>>>> parent of fddd5727c2b (CONFLICTED Chromium Cherry pick: Revert Cobalt.)
 };
 
 }  // namespace blink
