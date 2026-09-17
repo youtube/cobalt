962b19767d101a18ff9ac8e4558ef711b5954e10
Gyuyoung Kim <gyuyoung@igalia.com>
2026-09-17 12:52:18 +0900

    third_party/perfetto/.../periodic_task.cc: Adapt to https://github.com/google/perfetto/pull/3467
    Keep Cobalt's !defined(STARBOARD) on the linux branch while taking upstream's
    new FreeBSD branch; perfetto is a flattened dep so there is no crrev CL.

--- third_party/perfetto/src/base/periodic_task.cc
+++ third_party/perfetto/src/base/periodic_task.cc
@@ -45,12 +45,8 @@ uint32_t GetNextDelayMs(const TimeMillis& now_ms,
 }
 
 ScopedPlatformHandle CreateTimerFd(const PeriodicTask::Args& args) {
-<<<<<<< HEAD
-#if PERFETTO_BUILDFLAG(PERFETTO_OS_LINUX_BUT_NOT_QNX) || \
-    PERFETTO_BUILDFLAG(PERFETTO_OS_FREEBSD) ||           \
-=======
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_LINUX_BUT_NOT_QNX) && !defined(STARBOARD) || \
->>>>>>> parent of 85b8f352370 (CONFLICTED Chromium Cherry pick: Revert Cobalt.)
+    PERFETTO_BUILDFLAG(PERFETTO_OS_FREEBSD) ||                                  \
     (PERFETTO_BUILDFLAG(PERFETTO_OS_ANDROID) && __ANDROID_API__ >= 19)
   ScopedPlatformHandle tfd(
       timerfd_create(CLOCK_BOOTTIME, TFD_CLOEXEC | TFD_NONBLOCK));
