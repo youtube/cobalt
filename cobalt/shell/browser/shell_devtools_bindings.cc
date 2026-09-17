6afaf45c99de8878c7c56a92836d00d05c4e6c90
Gyuyoung Kim <gyuyoung@igalia.com>
2026-09-17 14:19:57 +0900

    cobalt/shell/browser/shell_devtools_bindings.cc: Adapt to https://crrev.com/c/7097498
    The CL removed IPC::Channel::kMaximumMessageSize. Upstream's
    content/shell copy of this file had already switched to the
    ipc/constants.mojom replacement before this roll window, but cobalt/shell
    is a separate path so the stale use was never flagged as a conflict.
    Follow upstream and use IPC::mojom::kChannelMaximumMessageSize.

--- cobalt/shell/browser/shell_devtools_bindings.cc
+++ cobalt/shell/browser/shell_devtools_bindings.cc
@@ -47,6 +47,7 @@
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/buildflags.h"
 #include "content/public/common/content_client.h"
+#include "ipc/constants.mojom.h"
 #include "net/http/http_response_headers.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "services/network/public/cpp/resource_request.h"
@@ -165,7 +166,7 @@ class ShellDevToolsBindings::NetworkResourceLoader
 // the constant
 // kMaxMessageChunkSize in chrome/browser/devtools/devtools_ui_bindings.cc.
 constexpr size_t kShellMaxMessageChunkSize =
-    IPC::Channel::kMaximumMessageSize / 4;
+    IPC::mojom::kChannelMaximumMessageSize / 4;
 
 void ShellDevToolsBindings::InspectElementAt(int x, int y) {
   if (agent_host_) {
