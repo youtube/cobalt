ef30e57786ee402912233ecd0954cc1179273c77
Raphael Kubo da Costa <kubo@igalia.com>
2026-09-18 16:39:20 +0200

    base/allocator: adapt to https://crrev.com/c/7201387

--- base/allocator/partition_alloc_features.h
+++ base/allocator/partition_alloc_features.h
@@ -212,13 +212,11 @@ BASE_EXPORT BASE_DECLARE_FEATURE(kPartitionAllocUseSmallSingleSlotSpans);
 BASE_EXPORT BASE_DECLARE_FEATURE(kPartitionAllocUsePriorityInheritanceLocks);
 #endif  // PA_BUILDFLAG(ENABLE_PARTITION_LOCK_PRIORITY_INHERITANCE)
 
-<<<<<<< HEAD
 BASE_EXPORT BASE_DECLARE_FEATURE(kPartitionAllocFreeWithSize);
-=======
+
 #if BUILDFLAG(IS_COBALT)
 BASE_EXPORT BASE_DECLARE_FEATURE(kPartitionAllocReuseMainPartitionForBuffers);
 #endif  // BUILDFLAG(IS_COBALT)
->>>>>>> parent of 554d267192b (CONFLICTED Chromium Cherry pick: Revert Cobalt.)
 
 }  // namespace base::features
 
