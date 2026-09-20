youtube/cobalt

commit dacdd95048752b94c5279dad4db609361d48253b

components/update_client: Remove custom VerifyFileHash256() implemention (#12524)
This Starboard-specific implementation was added in #10141 with code
forward-ported from the 26.eap branch.

In main (M138), the non-Starboard version behaves very similarly (it
just uses a smaller buffer size for the chunks it reads), so it is
possible to drop the custom version. Not only do we no longer have to
worry about maintaining custom code that makes calls to //crypto code,
but it also makes rebases easier.

Fixed: 559011262

components/update_client/utils.cc
@@ -96,7 +96,6 @@ std::string GetCrxIdFromPublicKeyHash(base::span<const uint8_t> pk_hash) {
 
 #if BUILDFLAG(IS_STARBOARD)
 #if defined(IN_MEMORY_UPDATES)
-
 bool VerifyHash256(const std::string* content,
                    const std::string& expected_hash_str) {
   std::vector<uint8_t> expected_hash;
@@ -110,49 +109,6 @@ bool VerifyHash256(const std::string* content,
       crypto::SecureHash::Create(crypto::SecureHash::SHA256));
 
   hasher->Update(content->c_str(), content->size());
-  hasher->Finish(actual_hash, sizeof(actual_hash));
-
-  return base::span(actual_hash) == base::span(expected_hash);
-}
-#else  // defined(IN_MEMORY_UPDATES)
-bool VerifyFileHash256(const base::FilePath& filepath,
-                       const std::string& expected_hash_str) {
-  std::vector<uint8_t> expected_hash;
-  if (!base::HexStringToBytes(expected_hash_str, &expected_hash) ||
-      expected_hash.size() != crypto::kSHA256Length) {
-    return false;
-  }
-
-  base::File source_file(filepath,
-                         base::File::FLAG_OPEN | base::File::FLAG_READ);
-  if (!source_file.IsValid()) {
-    DPLOG(ERROR) << "VerifyFileHash256(): Unable to open source file: "
-                 << filepath.value();
-    return false;
-  }
-
-  const size_t kBufferSize = 32768;
-  std::vector<char> buffer(kBufferSize);
-  uint8_t actual_hash[crypto::kSHA256Length] = {0};
-  std::unique_ptr<crypto::SecureHash> hasher(
-      crypto::SecureHash::Create(crypto::SecureHash::SHA256));
-
-  while (true) {
-    int bytes_read = source_file.ReadAtCurrentPos(&buffer[0], buffer.size());
-    if (bytes_read < 0) {
-      DPLOG(ERROR) << "VerifyFileHash256(): error reading from source file: "
-                   << filepath.value();
-
-      return false;
-    }
-
-    if (bytes_read == 0) {
-      break;
-    }
-
-    hasher->Update(&buffer[0], bytes_read);
-  }
-
   hasher->Finish(actual_hash, sizeof(actual_hash));
 
   return base::span(actual_hash) == base::span(expected_hash);

commit 31751d055075c36320f47cfcbaf43da2880263e4

Cherry pick PR #12790: components/update_client: Always define and implement VerifyFileHash256()
Refer to original PR: #12790

This fixes a regression introduced in #12524. Due to a mix-up in the
ifdefs, VerifyFileHash256() was not being implemented in Starboard
builds, which led to a runtime crash on at least linux-x64x11-modular:

dlopen failure: /path/to/out/linux-x64x11-modular_devel/libcobalt.so:
undefined symbol:
_ZN13update_client17VerifyFileHash256ERKN4base8FilePathERKNSt4__Cr12basic_stringIcNS4_11char_traitsIcEENS4_9allocatorIcEEEE
dlsym failure: ./out/linux-x64x11-modular_devel/cobalt_loader: undefined
symbol: SbEventHandle

Now that the upstream implementation of VerifyFileHash256() is being
used, simplify the ifdefs move the function's declaration in the header
and its implementation outside any IS_STARBOARD checks.

Bug: 559011262

(cherry picked from commit feb7fdec054902c8d4b49ecc3efc6f6c28974715)

.github/AUTOROLL
@@ -1,1 +1,1 @@
-f041275d67784fcce50cad06b69cacf1c31766d3
+feb7fdec054902c8d4b49ecc3efc6f6c28974715

components/update_client/utils.cc
@@ -165,7 +165,7 @@ const std::map<UpdaterStatus, const char*>& GetUpdaterStatusStringMap() {
   });
   return *map;
 }
-#else  // BUILDFLAG(IS_STARBOARD)
+#endif  // BUILDFLAG(IS_STARBOARD)
 
 bool VerifyFileHash256(const base::FilePath& filepath,
                        const std::string& expected_hash_str) {
@@ -198,8 +198,6 @@ bool VerifyFileHash256(const base::FilePath& filepath,
 
   return base::span(sha256_hash) == base::span(expected_hash);
 }
-
-#endif  // BUILDFLAG(IS_STARBOARD)
 
 bool IsValidBrand(const std::string& brand) {
   const size_t kMaxBrandSize = 4;

components/update_client/utils.h
@@ -86,17 +86,17 @@ std::string GetCrxIdFromPublicKeyHash(base::span<const uint8_t> pk_hash);
 // |content| must refer to a valid string.
 bool VerifyHash256(const std::string* content,
                    const std::string& expected_hash);
-#else
-// Returns true if the actual SHA-256 hash of the |filepath| matches the
-// |expected_hash|.
-bool VerifyFileHash256(const base::FilePath& filepath,
-                       const std::string& expected_hash);
-#endif   
+#endif  // defined(IN_MEMORY_UPDATES)
 
 #if BUILDFLAG(IS_STARBOARD)
 // Reads the Evergreen version of the installation dir.
 base::Version ReadEvergreenVersion(base::FilePath installation_dir);
 #endif  // BUILDFLAG(IS_STARBOARD)
+
+// Returns true if the actual SHA-256 hash of the |filepath| matches the
+// |expected_hash|.
+bool VerifyFileHash256(const base::FilePath& filepath,
+                       const std::string& expected_hash);
 
 // Returns true if the |brand| parameter matches ^[a-zA-Z]{4}?$ .
 bool IsValidBrand(const std::string& brand);