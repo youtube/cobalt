b5693f59c2ea7fa2033ce0cba6246446af428861
Raphael Kubo da Costa <kubo@igalia.com>
2026-09-22 10:59:17 +0200

    copied_base: adapt to https://crrev.com/c/7170448

--- copied_base/base/memory/discardable_shared_memory.cc
+++ copied_base/base/memory/discardable_shared_memory.cc
@@ -126,7 +126,7 @@ size_t AlignToPageSize(size_t size) {
 
 #if BUILDFLAG(IS_ANDROID)
 bool UseAshmemUnpinningForDiscardableMemory() {
-  if (!ashmem_device_is_supported())
+  if (!AshmemDeviceIsSupported())
     return false;
 
   // If we are participating in the discardable memory backing trial, only
@@ -551,7 +551,7 @@ DiscardableSharedMemory::LockResult DiscardableSharedMemory::LockPages(
   if (region.IsValid()) {
     if (UseAshmemUnpinningForDiscardableMemory()) {
       int pin_result =
-          ashmem_pin_region(region.GetPlatformHandle(), offset, length);
+          AshmemPinRegion(region.GetPlatformHandle(), offset, length);
       if (pin_result == ASHMEM_WAS_PURGED)
         return PURGED;
       if (pin_result < 0)
@@ -571,7 +571,7 @@ void DiscardableSharedMemory::UnlockPages(
   if (region.IsValid()) {
     if (UseAshmemUnpinningForDiscardableMemory()) {
       int unpin_result =
-          ashmem_unpin_region(region.GetPlatformHandle(), offset, length);
+          AshmemUnpinRegion(region.GetPlatformHandle(), offset, length);
       DCHECK_EQ(0, unpin_result);
     }
   }
