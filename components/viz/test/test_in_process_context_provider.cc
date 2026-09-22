af4b94e378d04727b3146b177c1ffbe69f62f3da
Gyuyoung Kim <gyuyoung@igalia.com>
2026-09-22 21:55:16 +0900

    //components/viz/test: Adapt to https://crrev.com/c/7241687
    The CL dropped the enable_gpu_rasterization argument, shortening the
    Initialize() call into Cobalt's IS_STARBOARD guard around the CHECK_EQ.

--- components/viz/test/test_in_process_context_provider.cc
+++ components/viz/test/test_in_process_context_provider.cc
@@ -76,17 +76,12 @@ gpu::ContextResult TestInProcessContextProvider::BindToCurrentSequence() {
   } else {
     raster_context_ = std::make_unique<gpu::RasterInProcessContext>();
     auto result = raster_context_->Initialize(
-<<<<<<< HEAD
         holder->task_executor(), holder->gpu_service()->gr_shader_cache(),
         use_shader_cache_shm_count_);
-=======
-        holder->task_executor(), /*enable_gpu_rasterization=*/is_gpu_raster,
-        holder->gpu_service()->gr_shader_cache(), use_shader_cache_shm_count_);
 
 // TODO(sherryzy): Investigate why this ContextResult check fails
 // specifically in single-process-test mode.
 #if !BUILDFLAG(IS_STARBOARD)
->>>>>>> parent of 4600c87aa6d (CONFLICTED Chromium Cherry pick: Revert Cobalt.)
     CHECK_EQ(result, gpu::ContextResult::kSuccess);
 #else
     (void)result;
