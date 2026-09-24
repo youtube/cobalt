0f2382da2ee9a6e2cf539826a90a3acfd47569d2
Abhijeet Kandalkar <abhijeet@igalia.com>
2026-09-24 13:23:13 +0530

    base/trace_event/heap_profiler_allocation_context.{cc,h}: Adapt to https://crrev.com/c/7256960
    Upstream now declares and defaults the Backtrace copy ctor and dtor out of
    line unconditionally, which covers what https://github.com/youtube/cobalt/pull/7479
    added under BUILD_BASE_WITH_CPP17. Drop that guard and its build_config.h includes.

--- base/trace_event/heap_profiler_allocation_context.cc
+++ base/trace_event/heap_profiler_allocation_context.cc
@@ -11,7 +11,6 @@
 #include "base/compiler_specific.h"
 #include "base/containers/span.h"
 #include "base/hash/hash.h"
-#include "build/build_config.h"
 
 namespace base::trace_event {
 
@@ -25,15 +24,9 @@ bool operator==(const StackFrame& lhs, const StackFrame& rhs) {
 
 Backtrace::Backtrace() = default;
 
-<<<<<<< HEAD
 Backtrace::Backtrace(const Backtrace&) = default;
 
 Backtrace::~Backtrace() = default;
-=======
-#if BUILDFLAG(BUILD_BASE_WITH_CPP17)
-Backtrace::Backtrace(const Backtrace& other) = default;
-#endif
->>>>>>> parent of f2451b7cb7c (CONFLICTED Chromium Cherry pick: Revert Cobalt.)
 
 bool operator==(const Backtrace& lhs, const Backtrace& rhs) {
   return std::ranges::equal(base::span(lhs.frames).first(lhs.frame_count),
