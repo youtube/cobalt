55549aae3c06604748b4f052c680cd47dfa247c3
Raphael Kubo da Costa <kubo@igalia.com>
2026-09-24 16:02:35 +0200

    autoroll_chromium: Remove ANGLE from DEPS' recursedeps in a few milestones
    Milestones 145.7595 and 145.7613 have an ANGLE revision whose DEPS file
    contains an entry for the VK-GL-CTS repository pointing to a revision that
    no longer exists [1], as there are occasional force-pushes in that git
    repository.
    
    [1] https://chromium.googlesource.com/angle/angle/+/1ba91f472d0e4f5b8136152e8ddef69fe185e27d%5E%21/#F0
    
    This causes failures in the autoroll process, as checking out either of
    these revisions results in an error when invoking gclient sync due to the
    invalid revision mentioned above.
    
    Work around it by making the script remove `src/third_party/angle` from the
    top-level DEPS' recursedeps to make gclient sync not recurse into ANGLE's
    own DEPS whenever 145.7595 or 145.7613 is being processed (either as the
    milestone being moved _to_ or _from_). We then add a commit registering this
    change.
    
    This is not a problem because most dependencies are not checked
    out anyway, except for two:
    
    * glmark2: not used in Cobalt
    * vk-gl-cts: also not used, and causing this issue
    
    Bug: 565697787

--- .github/scripts/autoroll_chromium.py
+++ .github/scripts/autoroll_chromium.py
@@ -23,6 +23,22 @@ _COBALT_SUBMODULE_DIRS = [
     'v8',
 ]
 
+# These hashes correspond to 145.7595 and 145.7613 in the chromium/main branch.
+# See b/565697787 for more context.
+_REVISIONS_WITH_BROKEN_ANGLE_SUBDEP = (
+  '38f4bd69219cb5db170704cdb0221dc3ea6eb039',
+  'a829ba1ad70c664608ac2dd4005e0dee339edbe4',
+)
+
+
+def remove_angle_from_recursedeps():
+  """Removes ANGLE from the top-level DEPS' recursedeps.
+
+  See b/565697787 for more information.
+  """
+  lib.run(['sed', '-i', r"/'src\/third_party\/angle',/s/^/\#/", 'DEPS'])
+  lib.run(['git', 'commit', '-m', 'Remove angle from recursedeps', '--', 'DEPS'])
+
 
 def get_submodule_root_dirs():
   paths = lib.get_out(
@@ -120,6 +136,13 @@ def verify_chromium_commit(sha):
             f'{upstream_sha}.')
     return True
 
+  if sha in _REVISIONS_WITH_BROKEN_ANGLE_SUBDEP:
+    modified_files = lib.get_out(['git', 'diff', '--name-only', sha, 'HEAD']).strip()
+    if modified_files == 'DEPS':
+      lib.log(f'Verification passed: Tree {current_tree} matches Chromium '
+              f'{upstream_sha} with one change to DEPS to remove ANGLE from recursedeps')
+      return True
+
   diff_output = lib.get_out(['git', 'diff', '--name-status', sha,
                              'HEAD']).strip()
   lib.log(f'ERROR: Rolled-in tree ({current_tree}) differs from Chromium '
@@ -152,6 +175,9 @@ def chromium_cherry_pick(previous_sha, shas, metadata, autoroll_metadata):
   remove_local_checkout()
   lib.run(['git', 'checkout', previous_sha, '--', '.'])
 
+  if previous_sha in _REVISIONS_WITH_BROKEN_ANGLE_SUBDEP:
+    remove_angle_from_recursedeps()
+
   replace_submodules_with_dirs()
 
   lib.log('Committing Cobalt revert...')
@@ -173,6 +199,9 @@ def chromium_cherry_pick(previous_sha, shas, metadata, autoroll_metadata):
   for sha in shas:
     lib.log('Cherry picking Chromium...')
     lib.run(['git', 'cherry-pick', sha])
+
+    if sha in _REVISIONS_WITH_BROKEN_ANGLE_SUBDEP:
+      remove_angle_from_recursedeps()
 
     if not verify_chromium_commit(sha):
       raise RuntimeError(
