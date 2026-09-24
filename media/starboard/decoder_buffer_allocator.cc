24330125da0bf3758cd33e3a230c6ea4d1b3d4ed
Xiaoming Shi <xiaomings@google.com>
2026-09-24 22:26:37 +0000

    starboard: Remove MediaBufferPool extension (#12856)
    Remove the experimental Starboard MediaBufferPool extension and all of
    its references across the Cobalt codebase.
    
    The extension was added in https://github.com/youtube/cobalt/pull/8721,
    and it's risky to revert directly due to modifications on the related
    files.
    
    This removes:
    - The MediaBufferPool Starboard extension definition and tests.
    - MemFdMediaBufferPool implementation on Android.
    - MediaBufferPool C++ wrapper classes in starboard/common.
    - MediaBufferPoolDecoderBufferAllocatorStrategy in media/starboard.
    - Related H5vcc experiment setting plumbing.
    
    DecoderBuffer::Allocator::Handle remains preserved as it is utilized by
    StarboardMediaExternalMemoryAllocator, which will be removed in follow
    up CLs.
    
    Bug: 369245553

--- media/starboard/decoder_buffer_allocator.cc
+++ media/starboard/decoder_buffer_allocator.cc
@@ -16,26 +16,16 @@
 
 #include <sys/mman.h>  // For MADV_COLD
 
-#include <algorithm>
-
-#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/types/expected.h"
 #include "build/build_config.h"
-#include "media/base/media_switches.h"
 #include "media/base/starboard/sbmedia_interface.h"
-#include "media/base/video_codecs.h"
 #include "media/starboard/bidirectional_fit_decoder_buffer_allocator_strategy.h"
-#include "media/starboard/media_buffer_pool_decoder_buffer_allocator_strategy.h"
-#include "media/starboard/starboard_utils.h"
 #include "starboard/common/allocator.h"
 #include "starboard/common/embedded_metadata_reuse_allocator_base.h"
-#include "starboard/common/experimental/media_buffer_pool.h"
-#include "starboard/common/external_metadata_reuse_allocator_base.h"
 #include "starboard/common/log.h"
-#include "starboard/configuration.h"
 
 namespace media {
 
@@ -48,22 +38,9 @@ constexpr base::TimeDelta kDefaultPeriodicDecommitInterval = base::Seconds(5);
 using DefaultReuseAllocatorStrategy =
     BidirectionalFitDecoderBufferAllocatorStrategy<
         starboard::EmbeddedMetadataReuseAllocatorBase>;
-using starboard::experimental::MediaBufferPool;
 
 const char* ToString(bool value) {
   return value ? "enabled" : "disabled";
-}
-
-template <typename Callback>
-base::expected<void, std::string> ProcessEnableOnlySetting(
-    const std::string& name,
-    int value,
-    Callback on_enable) {
-  if (value == 0) {
-    return base::unexpected(name + " cannot be disabled.");
-  }
-  on_enable();
-  return base::ok();
 }
 
 }  // namespace
@@ -177,26 +154,6 @@ void DecoderBufferAllocator::Free(DemuxerStream::Type type,
               << " bytes of decoder buffer pool.";
     strategy_.reset();
   }
-}
-
-void DecoderBufferAllocator::Write(Handle handle,
-                                   const void* data,
-                                   size_t size) {
-  // The lock adds overhead to the cases where |handle| is a pointer, so we take
-  // a short cut to ensure that there is no overhead adding to our existing
-  // logic.
-  using ::starboard::experimental::IsPointerAnnotated;
-
-  if (!IsPointerAnnotated(handle)) {
-    memcpy(reinterpret_cast<void*>(handle), data, size);
-    return;
-  }
-
-  // TODO(b/369245553): Consider combining Allocate() and Write() into one
-  //                    function to avoid the extra lock.
-  base::AutoLock scoped_lock(mutex_);
-  DCHECK(strategy_);
-  strategy_->Write(reinterpret_cast<void*>(handle), data, size);
 }
 
 base::TimeDelta
@@ -321,10 +278,6 @@ base::expected<void, std::string> DecoderBufferAllocator::SetSetting(
         strategy_config, enable_decommit_on_suspend, periodic_decommit);
     return base::ok();
   }
-  if (name == "DecoderBuffer.EnableMediaBufferPoolAllocatorStrategy") {
-    return ProcessEnableOnlySetting(name, value,
-                                    [] { EnableMediaBufferPoolStrategy(); });
-  }
   return base::unexpected(name + " isn't a supported setting.");
 }
 
@@ -400,26 +353,6 @@ void DecoderBufferAllocator::EnableConfigurableDecommitStrategy(
         return std::make_unique<DefaultReuseAllocatorStrategy>(strategy_config);
       },
       strategy_config, enable_decommit_on_suspend, periodic_decommit));
-}
-
-// static
-void DecoderBufferAllocator::EnableMediaBufferPoolStrategy() {
-  auto* allocator = Get();
-  CHECK(allocator);
-  allocator->UpdateAllocatorStrategy(base::BindRepeating(
-      [](int initial_capacity, int allocation_unit)
-          -> std::unique_ptr<DecoderBufferAllocator::Strategy> {
-        auto pool = MediaBufferPool::Acquire();
-        if (pool) {
-          LOG(INFO) << "DecoderBufferAllocator is using MediaBufferPool.";
-          return std::make_unique<
-              MediaBufferPoolDecoderBufferAllocatorStrategy>(
-              pool, initial_capacity, allocation_unit);
-        }
-        LOG(INFO) << "DecoderBufferAllocator failed to enable MediaBufferPool"
-                  << " as MediaBufferPool::Acquire() returns nullptr.";
-        return nullptr;
-      }));
 }
 
 void DecoderBufferAllocator::EnsureStrategyIsCreated() {
